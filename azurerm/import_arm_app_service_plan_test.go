aa8e57de1800e38d1975667545c3a8fbef50ab5a
lstolyarov <leonid.stolyarov@kpmg.co.uk>
2017-06-12 15:20:23 +0100

    adding dependecies with govendor and import_arm_app_service_plan_test file

---
+++ azurerm/import_arm_app_service_plan_test.go
@@ -0,0 +1,33 @@
+package azurerm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccAzureRMAppServicePlan_importStandard(t *testing.T) {
+	resourceName := "azurerm_app_service_plan.test"
+
+	ri := acctest.RandInt()
+	config := fmt.Sprintf(testAccAzureRMAppServicePlan_standard, ri)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMAppServicePlanDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: config,
+			},
+
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
