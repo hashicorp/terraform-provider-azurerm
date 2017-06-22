82d7bef5ce0c1ef8641de583dd65054aee8198b6
tombuildsstuff <git@ibuildstuff.co.uk>
2017-06-22 08:53:10 +0100

    Fixing the broken subnet test

--- azurerm/resource_arm_subnet_test.go
+++ azurerm/resource_arm_subnet_test.go
@@ -72,7 +72,7 @@ func TestAccAzureRMSubnet_bug7986(t *testing.T) {
 			{
 				Config: initConfig,
 				Check: resource.ComposeTestCheckFunc(
-					testCheckAzureRMSubnetExists("azurerm_subnet.test"),
+					testCheckAzureRMSubnetExists("azurerm_subnet.first"),
 				),
 			},
 		},
@@ -445,7 +445,7 @@ resource "azurerm_subnet" "first" {
 }
 
 resource "azurerm_route_table" "second" {
-  name                = "$acctest%d-private-2"
+  name                = "acctest%d-private-2"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
 }
@@ -459,7 +459,7 @@ resource "azurerm_route" "second" {
 }
 
 resource "azurerm_subnet" "second" {
-  name                 = "acctest%d-private-1"
+  name                 = "acctest%d-private-2"
   resource_group_name  = "${azurerm_resource_group.test.name}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
   address_prefix       = "10.0.1.0/24"
