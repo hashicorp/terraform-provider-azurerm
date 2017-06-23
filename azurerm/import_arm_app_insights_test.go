ce4c4d94b2c0137f8539f6909c1dc186b44255b3
tombuildsstuff <git@ibuildstuff.co.uk>
2017-06-23 15:50:46 +0100

    Added tests covering the `other` application type

--- azurerm/import_arm_app_insights_test.go
+++ azurerm/import_arm_app_insights_test.go
@@ -7,11 +7,35 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
-func TestAccAzureRMApplicationInsights_importBasic(t *testing.T) {
+func TestAccAzureRMApplicationInsights_importBasicWeb(t *testing.T) {
 	resourceName := "azurerm_application_insights.test"
 
 	ri := acctest.RandInt()
-	config := testAccAzureRMApplicationInsights_basic(ri)
+	config := testAccAzureRMApplicationInsights_basicWeb(ri)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMApplicationInsightsDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+			},
+
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccAzureRMApplicationInsights_importBasicOther(t *testing.T) {
+	resourceName := "azurerm_application_insights.test"
+
+	ri := acctest.RandInt()
+	config := testAccAzureRMApplicationInsights_basicWeb(ri)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
--- azurerm/resource_arm_app_insights_test.go
+++ azurerm/resource_arm_app_insights_test.go
@@ -10,10 +10,30 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
-func TestAccAzureRMApplicationInsights_basic(t *testing.T) {
+func TestAccAzureRMApplicationInsights_basicWeb(t *testing.T) {
 
 	ri := acctest.RandInt()
-	config := testAccAzureRMApplicationInsights_basic(ri)
+	config := testAccAzureRMApplicationInsights_basicWeb(ri)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMApplicationInsightsDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMApplicationInsightsExists("azurerm_application_insights.test"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureRMApplicationInsights_basicOther(t *testing.T) {
+
+	ri := acctest.RandInt()
+	config := testAccAzureRMApplicationInsights_basicOther(ri)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -84,7 +104,7 @@ func testCheckAzureRMApplicationInsightsExists(name string) resource.TestCheckFu
 	}
 }
 
-func testAccAzureRMApplicationInsights_basic(rInt int) string {
+func testAccAzureRMApplicationInsights_basicWeb(rInt int) string {
 	return fmt.Sprintf(`
 resource "azurerm_resource_group" "test" {
   name = "acctestRG-%d"
@@ -96,6 +116,22 @@ resource "azurerm_application_insights" "test" {
   location = "West Europe"
   resource_group_name = "${azurerm_resource_group.test.name}"
   application_type = "web"
+}
+`, rInt, rInt)
+}
+
+func testAccAzureRMApplicationInsights_basicOther(rInt int) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name = "acctestRG-%d"
+  location = "West Europe"
+}
+
+resource "azurerm_application_insights" "test" {
+  name = "acctestappinsights-%d"
+  location = "West Europe"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  application_type = "other"
 }
 `, rInt, rInt)
 }
