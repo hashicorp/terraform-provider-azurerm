b3d80d690ef21e870350b239cb76a9d8dec8b63f
Jay Wang <jiewan@microsoft.com>
2017-06-23 18:48:44 -0700

    Acceptance tests

--- azurerm/import_arm_autoscale_setting_test.go
+++ azurerm/import_arm_autoscale_setting_test.go
@@ -7,16 +7,64 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
-func TestAccAzureRMAutoscaleSettings_importBasic(t *testing.T) {
-	resourceName := "azurerm_autoscale_settings.test"
+func TestAccAzureRMAutoscaleSetting_importBasic(t *testing.T) {
+	resourceName := "azurerm_autoscale_setting.test"
 
 	ri := acctest.RandInt()
-	config := testAccAzureRMAutoscaleSettings_basic(ri)
+	config := testAccAzureRMAutoscaleSetting_basic(ri)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testCheckAzureRMAvailabilitySetDestroy,
+		CheckDestroy: testCheckAzureRMAutoscaleSettingDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: config,
+			},
+
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccAzureRMAutoscaleSetting_importRecurrence(t *testing.T) {
+	resourceName := "azurerm_autoscale_setting.test"
+
+	ri := acctest.RandInt()
+	config := testAccAzureRMAutoscaleSetting_recurrence(ri)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMAutoscaleSettingDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: config,
+			},
+
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccAzureRMAutoscaleSetting_importFixedDate(t *testing.T) {
+	resourceName := "azurerm_autoscale_setting.test"
+
+	ri := acctest.RandInt()
+	config := testAccAzureRMAutoscaleSetting_fixedDate(ri)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMAutoscaleSettingDestroy,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: config,
