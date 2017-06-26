827d1f8862484073c41d706be2bf26588e8322a0
tombuildsstuff <git@ibuildstuff.co.uk>
2017-06-26 15:51:37 +0100

    Need to use a Premium SKU to set the field at all

--- azurerm/resource_arm_redis_cache_test.go
+++ azurerm/resource_arm_redis_cache_test.go
@@ -440,9 +440,9 @@ resource "azurerm_redis_cache" "test" {
     name                = "acctestRedis-%d"
     location            = "${azurerm_resource_group.test.location}"
     resource_group_name = "${azurerm_resource_group.test.name}"
-    capacity            = 1
-    family              = "c"
-    sku_name            = "Basic"
+    capacity            = 3
+    family              = "P"
+    sku_name            = "Premium"
     enable_non_ssl_port = false
     redis_configuration {
       maxclients         = "256"
