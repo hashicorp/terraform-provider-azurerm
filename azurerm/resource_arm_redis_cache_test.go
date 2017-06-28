91aa82b899e2b4f02d24849c910838d362f7d54a
tombuildsstuff <git@ibuildstuff.co.uk>
2017-06-28 12:53:32 +0100

    Added a test for the backup frequency

--- azurerm/resource_arm_redis_cache_test.go
+++ azurerm/resource_arm_redis_cache_test.go
@@ -77,6 +77,32 @@ func TestAccAzureRMRedisCacheMaxMemoryPolicy_validation(t *testing.T) {
 	}
 }
 
+func TestAccAzureRMRedisCacheBackupFrequency_validation(t* testing.T) {
+	cases := []struct {
+		Value    int
+		ErrCount int
+	}{
+		{Value: 1, ErrCount: 1},
+		{Value: 15, ErrCount: 0},
+		{Value: 30, ErrCount: 0},
+		{Value: 45, ErrCount: 1},
+		{Value: 60, ErrCount: 0},
+		{Value: 120, ErrCount: 1},
+		{Value: 240, ErrCount: 1},
+		{Value: 360, ErrCount: 0},
+		{Value: 720, ErrCount: 0},
+		{Value: 1440, ErrCount: 0},
+	}
+
+	for _, tc := range cases {
+		_, errors := validateRedisBackupFrequency(tc.Value, "azurerm_redis_cache")
+
+		if len(errors) != tc.ErrCount {
+			t.Fatalf("Expected the AzureRM Redis Cache Backup Frequency to trigger a validation error for '%d'", tc.Value)
+		}
+	}
+}
+
 func TestAccAzureRMRedisCache_basic(t *testing.T) {
 	ri := acctest.RandInt()
 	config := testAccAzureRMRedisCache_basic(ri)
