0abae03c2bd03dbed82d6b46d9d7b35d9d3ff22a
James Earle <jaearle@microsoft.com>
2017-08-16 11:09:05 -0700

    additional SQL Server outputs for module

--- examples/tf-sql-database/outputs.tf
+++ examples/tf-sql-database/outputs.tf
@@ -2,6 +2,20 @@ output "database_name" {
   value = "${azurerm_sql_database.db.name}"
 }
 
+output "sql_server_name" {
+  value = "${azurerm_sql_server.server.name}"
+}
+
+output "sql_server_location" {
+  value = "${azurerm_sql_server.server.location}"
+}
+
+output "sql_server_version" {
+  value = "${azurerm_sql_server.server.version}"
+}
+
 output "sql_server_fqdn" {
   value = "${azurerm_sql_server.server.fully_qualified_domain_name}"
 }
+
+
