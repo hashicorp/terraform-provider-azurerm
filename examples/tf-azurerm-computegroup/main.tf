9a122c278e74685e952b9cfef6d490eb597bbf96
dcaro <dcaro@Damiens-MacBook-Pro.local>
2017-08-16 15:28:44 -0700

    initial commit

---
+++ examples/tf-azurerm-computegroup/main.tf
@@ -0,0 +1,144 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_group_name}"
+  location = "${var.location}"
+  tags     = "${var.tags}"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "acctvn"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "acctsub"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  address_prefix       = "10.0.1.0/24"
+}
+
+resource "azurerm_public_ip" "publicip" {
+  name                         = "test"
+  location                     = "${var.location}"
+  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  public_ip_address_allocation = "static"
+  domain_name_label            = "${azurerm_resource_group.rg.name}"
+  tags                         = "${var.tags}"
+}
+
+resource "azurerm_lb" "loadbalancer" {
+  name                = "loadbalancer"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  tags                = "${var.tags}"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = "${azurerm_public_ip.publicip.id}"
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  loadbalancer_id     = "${azurerm_lb.loadbalancer.id}"
+  name                = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_nat_pool" "lbnatpool" {
+  count                          = "${length(var.port)}"
+ resource_group_name            = "${azurerm_resource_group.rg.name}"
+ name                           = "${element(keys(var.port), count.index)}"
+ loadbalancer_id                = "${azurerm_lb.loadbalancer.id}"
+ protocol                       = "${element(var.port["${element(keys(var.port), count.index)}"], 1)}"
+ frontend_port_start            = "${element(var.port["${element(keys(var.port), count.index)}"], 2)}"
+ frontend_port_end              = "${element(var.port["${element(keys(var.port), count.index)}"], 3)}"
+ backend_port                   = "${element(var.port["${element(keys(var.port), count.index)}"], 0)}"
+ frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+#resource "azurerm_lb_nat_pool" "lbnatpool1" {
+#  resource_group_name            = "${azurerm_resource_group.rg.name}"
+#  name                           = "ssh"
+#  loadbalancer_id                = "${azurerm_lb.loadbalancer.id}"
+#  protocol                       = "Tcp"
+#  frontend_port_start            = "50000"
+#  frontend_port_end              = "50119"
+#  backend_port                   = "22"
+#  frontend_ip_configuration_name = "PublicIPAddress"
+#}
+
+#resource "azurerm_lb_nat_pool" "lbnatpool2" {
+#  resource_group_name            = "${azurerm_resource_group.rg.name}"
+#  name                           = "http"
+#  loadbalancer_id                = "${azurerm_lb.loadbalancer.id}"
+#  protocol                       = "Tcp"
+#  frontend_port_start            = "51000"
+#  frontend_port_end              = "51119"
+#  backend_port                   = "80"
+#  frontend_ip_configuration_name = "PublicIPAddress"
+#}
+
+resource "azurerm_virtual_machine_scale_set" "vmss" {
+  name                = "vmscaleset"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  upgrade_policy_mode = "Manual"
+  tags                = "${var.tags}"
+
+  sku {
+    name     = "${var.vm_size}"
+    tier     = "Standard"
+    capacity = "${var.nb_instance}"
+  }
+
+  storage_profile_image_reference {
+    id        = "${var.os["id"]}"
+    publisher = "${var.os["publisher"]}"
+    offer     = "${var.os["offer"]}"
+    sku       = "${var.os["sku"]}"
+    version   = "${var.os["version"]}"
+  }
+
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_profile_data_disk {
+      lun          = 0
+    caching        = "ReadWrite"
+    create_option  = "Empty"
+    disk_size_gb   = 10
+  }
+
+  os_profile {
+    computer_name_prefix = "vmss"
+    admin_username       = "${var.admin_username}"
+    admin_password       = "${var.admin_password}"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path     = "/home/${var.admin_username}/.ssh/authorized_keys"
+      key_data = "${file("${var.ssh_key}")}"
+    }
+  }
+
+  network_profile {
+    name    = "terraformnetworkprofile"
+    primary = true
+
+    ip_configuration {
+      name                                   = "TestIPConfiguration"
+      subnet_id                              = "${azurerm_subnet.subnet1.id}"
+      load_balancer_backend_address_pool_ids = ["${azurerm_lb_backend_address_pool.bpepool.id}"]
+      load_balancer_inbound_nat_rules_ids    = ["${azurerm_lb_nat_pool.lbnatpool.*.id}"]
+    }
+  }
+  
+}
---
+++ examples/tf-azurerm-computegroup/variables.tf
@@ -0,0 +1,60 @@
+variable "resource_group_name" {
+    description = "The name of the resource group in which the resources will be created"
+    default = "vmssrg"
+}
+
+variable "location" { 
+    description = "The location where the resources will be created"
+    default = "West US"
+}
+
+variable "vm_size" {
+    default = "Standard_A0"
+    description = "Size of the Virtual Machine"
+}
+
+variable "admin_username" {
+    description = "The admin username of the VMSS being deployed"
+    default = "azureuser"
+ }
+
+variable "admin_password" {
+    default = ""
+}
+
+variable "ssh_key" {
+    description = "Path to the public key to be used for ssh access to the VM"
+    default = "~/.ssh/demo_key.pub"
+}
+
+variable "nb_instance" { 
+    description = "specify the number of vm instances"
+    default = "1"
+}
+
+
+variable "port"{ 
+    default = { 
+        ssh = ["22", "Tcp", "50000", "50119"]
+        http = ["80", "Tcp", "51000", "51119"]
+    }
+}
+
+variable "os" { 
+    type = "map"
+    default = {  
+        id = ""
+        publisher = "Canonical"
+        offer = "UbuntuServer"
+        sku = "14.04.2-LTS"
+        version = "latest"
+    }
+}
+
+variable "tags"{
+    type = "map"
+    default = {
+        tag1 = "dev"
+        tag2 = "demo"
+    }
+}
