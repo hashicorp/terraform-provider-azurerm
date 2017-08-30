b2deb954803bf25de3f2bcec999859aea5afc05c
tombuildsstuff <git@ibuildstuff.co.uk>
2017-08-30 09:41:19 +0100

    Added validation covering RFC3339 dates

--- azurerm/validators.go
+++ azurerm/validators.go
@@ -2,9 +2,21 @@ package azurerm
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/satori/uuid"
+	"github.com/Azure/go-autorest/autorest/date"
 )
+
+func validateRFC3339Date(v interface{}, k string) (ws []string, errors []error) {
+	dateString := v.(string)
+
+	if _, err := date.ParseTime(time.RFC3339, dateString); err != nil {
+		errors = append(errors, fmt.Errorf("`%s` is an invalid RFC3339 date: %+v", k, err))
+	}
+
+	return
+}
 
 func validateUUID(v interface{}, k string) (ws []string, errors []error) {
 	if _, err := uuid.FromString(v.(string)); err != nil {
---
+++ azurerm/validators_test.go
@@ -0,0 +1,42 @@
+package azurerm
+
+import (
+	"testing"
+)
+
+func TestValidateRFC3339Date(t *testing.T) {
+	cases := []struct {
+		Value    string
+		ErrCount int
+	}{
+		{
+			Value:    "",
+			ErrCount: 1,
+		},
+		{
+			Value:    "Random",
+			ErrCount: 1,
+		},
+		{
+			Value:    "2017-01-01",
+			ErrCount: 1,
+		},
+		{
+			Value:    "2017-01-01T01:23:45",
+			ErrCount: 1,
+		},
+		{
+			Value:    "2017-01-01T01:23:45+00:00",
+			ErrCount: 0,
+		},
+	}
+
+	for _, tc := range cases {
+		_, errors := validateRFC3339Date(tc.Value, "example")
+
+		if len(errors) != tc.ErrCount {
+			t.Fatalf("Expected validateRFC3339Date to trigger '%d' errors for '%s' - got '%d'", tc.ErrCount, tc.Value, len(errors))
+		}
+	}
+}
+
