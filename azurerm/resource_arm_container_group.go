2226646aa87d115d07059c6f944d49633854bd01
Abhijeet Gaiha <agaiha@microsoft.com>
2017-09-04 02:07:16 +0530

    WIP: Container group resource

--- azurerm/resource_arm_container_group.go
+++ azurerm/resource_arm_container_group.go
@@ -1,6 +1,9 @@
 package azurerm
 
-import "github.com/hashicorp/terraform/helper/schema"
+import (
+	"github.com/Azure/azure-sdk-for-go/arm/containerinstance"
+	"github.com/hashicorp/terraform/helper/schema"
+)
 
 func resourceArmContainerGroup() *schema.Resource {
 	return &schema.Resource{
@@ -24,16 +27,97 @@ func resourceArmContainerGroup() *schema.Resource {
 				ForceNew: true,
 			},
 
+			"image": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"cpu_cores": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+			},
+
+			"command_line": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+
+			"env_vars": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+
+			"ip_address_type": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+
+			"memory": {
+				Type:     schema.TypeFloat,
+				Optional: true,
+				ForceNew: true,
+			},
+
+			"os_type": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+
+			"port": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+			},
+
 			"tags": tagsSchema(),
 		},
 	}
 }
 
-func resourceArmContainerGroupCreate(d *schema.ResourceData, meta interface{}) error {
-	//client := meta.(*ArmClient)
-	//containterGroupsClient := client.containerGroupsClient
+// func containerSchema() *schema.Resource {
+// 	return &schema.Resource{
+// 		Type:schema
+// 	}
+// }
 
-	//containterGroupsClient.CreateOrUpdate()
+func resourceArmContainerGroupCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ArmClient)
+	containterGroupsClient := client.containerGroupsClient
+
+	// container group properties
+	resGroup := d.Get("resource_group_name").(string)
+	name := d.Get("name").(string)
+	location := d.Get("location").(string)
+	OSType := d.Get("os_type").(string)
+	tags := d.Get("tags").(map[string]interface{})
+
+	// per container properties
+	// image := d.Get("image").(string)
+	// cores := d.Get("cpu_cores").(int)
+	// cmdLine := d.Get("command_line").(string)
+	// envVars := d.Get("env_vars").(string)
+	// IPAddressType := d.Get("ip_address_type").(string)
+	// memory := d.Get("memory").(float32)
+	// port := d.Get("port").(int)
+
+	containerGroup := containerinstance.ContainerGroup{
+		Name:     &name,
+		Type:     &OSType,
+		Location: &location,
+		Tags:     expandTags(tags),
+	}
+
+	_, error := containterGroupsClient.CreateOrUpdate(resGroup, name, containerGroup)
+	if error != nil {
+		return error
+	}
+
 	return nil
 }
 func resourceArmContainerGroupRead(d *schema.ResourceData, meta interface{}) error {
