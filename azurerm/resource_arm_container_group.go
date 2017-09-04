c5847c3c7dca6d25c9be06f9580a8175b0f95785
Abhijeet Gaiha <agaiha@microsoft.com>
2017-09-04 18:49:54 +0530

    WIP: Container group resource

--- azurerm/resource_arm_container_group.go
+++ azurerm/resource_arm_container_group.go
@@ -1,6 +1,8 @@
 package azurerm
 
 import (
+	"strings"
+
 	"github.com/Azure/azure-sdk-for-go/arm/containerinstance"
 	"github.com/hashicorp/terraform/helper/schema"
 )
@@ -33,20 +35,8 @@ func resourceArmContainerGroup() *schema.Resource {
 				ForceNew: true,
 			},
 
-			"cpu_cores": {
-				Type:     schema.TypeInt,
-				Optional: true,
-				ForceNew: true,
-			},
-
-			"command_line": {
-				Type:     schema.TypeString,
-				Optional: true,
-				ForceNew: true,
-			},
-
-			"env_vars": {
-				Type:     schema.TypeString,
+			"cpu": {
+				Type:     schema.TypeFloat,
 				Optional: true,
 				ForceNew: true,
 			},
@@ -75,16 +65,16 @@ func resourceArmContainerGroup() *schema.Resource {
 				ForceNew: true,
 			},
 
+			"protocol": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+
 			"tags": tagsSchema(),
 		},
 	}
 }
-
-// func containerSchema() *schema.Resource {
-// 	return &schema.Resource{
-// 		Type:schema
-// 	}
-// }
 
 func resourceArmContainerGroupCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*ArmClient)
@@ -95,22 +85,77 @@ func resourceArmContainerGroupCreate(d *schema.ResourceData, meta interface{}) e
 	name := d.Get("name").(string)
 	location := d.Get("location").(string)
 	OSType := d.Get("os_type").(string)
+	IPAddressType := d.Get("ip_address_type").(string)
+	protocol := d.Get("protocol").(string)
 	tags := d.Get("tags").(map[string]interface{})
 
 	// per container properties
-	// image := d.Get("image").(string)
-	// cores := d.Get("cpu_cores").(int)
-	// cmdLine := d.Get("command_line").(string)
-	// envVars := d.Get("env_vars").(string)
-	// IPAddressType := d.Get("ip_address_type").(string)
-	// memory := d.Get("memory").(float32)
-	// port := d.Get("port").(int)
+	image := d.Get("image").(string)
+	cpu := d.Get("cpu").(float64)
+	memory := d.Get("memory").(float64)
+	port := d.Get("port").(int32)
+
+	// type ContainerGroupProperties struct {
+	// 	ProvisioningState        *string                    `json:"provisioningState,omitempty"`
+	// 	Containers               *[]Container               `json:"containers,omitempty"`
+	// 	ImageRegistryCredentials *[]ImageRegistryCredential `json:"imageRegistryCredentials,omitempty"`
+	// 	RestartPolicy            ContainerRestartPolicy     `json:"restartPolicy,omitempty"`
+	// 	IPAddress                *IPAddress                 `json:"ipAddress,omitempty"`
+	// 	OsType                   OperatingSystemTypes       `json:"osType,omitempty"`
+	// 	State                    *string                    `json:"state,omitempty"`
+	// 	Volumes                  *[]Volume                  `json:"volumes,omitempty"`
+	// }
+
+	// type ContainerProperties struct {
+	// 	Image                *string                          `json:"image,omitempty"`
+	// 	Command              *[]string                        `json:"command,omitempty"`
+	// 	Ports                *[]ContainerPort                 `json:"ports,omitempty"`
+	// 	EnvironmentVariables *[]EnvironmentVariable           `json:"environmentVariables,omitempty"`
+	// 	InstanceView         *ContainerPropertiesInstanceView `json:"instanceView,omitempty"`
+	// 	Resources            *ResourceRequirements            `json:"resources,omitempty"`
+	// 	VolumeMounts         *[]VolumeMount                   `json:"volumeMounts,omitempty"`
+	// }
+
+	// per container port (port number only)
+	containerPort := containerinstance.ContainerPort{
+		Port: &port,
+	}
+
+	container := containerinstance.Container{
+		Name: &name,
+		ContainerProperties: &containerinstance.ContainerProperties{
+			Image: &image,
+			Ports: &[]containerinstance.ContainerPort{containerPort},
+			Resources: &containerinstance.ResourceRequirements{
+				Requests: &containerinstance.ResourceRequests{
+					MemoryInGB: &memory,
+					CPU:        &cpu,
+				},
+			},
+		},
+	}
+
+	// container group port (port number + protocol)
+	containerGroupPort := containerinstance.Port{
+		Port: &port,
+	}
+
+	if strings.ToUpper(protocol) == "TCP" || strings.ToUpper(protocol) == "UDP" {
+		containerGroupPort.Protocol = containerinstance.ContainerGroupNetworkProtocol(strings.ToUpper(protocol))
+	}
 
 	containerGroup := containerinstance.ContainerGroup{
 		Name:     &name,
 		Type:     &OSType,
 		Location: &location,
 		Tags:     expandTags(tags),
+		ContainerGroupProperties: &containerinstance.ContainerGroupProperties{
+			Containers: &[]containerinstance.Container{container},
+			IPAddress: &containerinstance.IPAddress{
+				Type:  &IPAddressType,
+				Ports: &[]containerinstance.Port{containerGroupPort},
+			},
+		},
 	}
 
 	_, error := containterGroupsClient.CreateOrUpdate(resGroup, name, containerGroup)
@@ -121,8 +166,34 @@ func resourceArmContainerGroupCreate(d *schema.ResourceData, meta interface{}) e
 	return nil
 }
 func resourceArmContainerGroupRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ArmClient)
+	containterGroupsClient := client.containerGroupsClient
+
+	// container group properties
+	resGroup := d.Get("resource_group_name").(string)
+	name := d.Get("name").(string)
+
+	_, error := containterGroupsClient.Get(resGroup, name)
+
+	if error != nil {
+		return error
+	}
+
 	return nil
 }
 func resourceArmContainerGroupDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ArmClient)
+	containterGroupsClient := client.containerGroupsClient
+
+	// container group properties
+	resGroup := d.Get("resource_group_name").(string)
+	name := d.Get("name").(string)
+
+	_, error := containterGroupsClient.Delete(resGroup, name)
+
+	if error != nil {
+		return error
+	}
+
 	return nil
 }
