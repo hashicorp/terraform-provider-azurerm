e58ffdec52de7abef9c2d867133d1201d3b01690
Girish Ramnani <girishramnani95@gmail.com>
2017-09-29 16:59:40 +0530

    basic integration tests added

---
+++ azurerm/resource_arm_iothub_resource_provider_test.go
@@ -0,0 +1,105 @@
+package azurerm
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+const TestResourceGroup = "girishsandbox"
+
+func TestAccAzureRMIotHub_basicStandard(t *testing.T) {
+	name := acctest.RandString(6)
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMIothubDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMIotHub_basicStandard(name, TestResourceGroup, testLocation()),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMIothubExists("azurerm_iothub.test"),
+				),
+			},
+		},
+	})
+
+}
+
+func testCheckAzureRMIothubDestroy(s *terraform.State) error {
+
+	conn := testAccProvider.Meta().(*ArmClient).expressRouteCircuitClient
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "azurerm_iothub" {
+			continue
+		}
+
+		name := rs.Primary.Attributes["name"]
+		resourceGroup := rs.Primary.Attributes["resource_group_name"]
+
+		resp, err := conn.Get(resourceGroup, name)
+
+		if err != nil {
+			return nil
+		}
+
+		if resp.StatusCode != http.StatusNotFound {
+			return fmt.Errorf("Iothub still exists:\n%#v", resp.ExpressRouteCircuitPropertiesFormat)
+		}
+	}
+	return nil
+}
+func testCheckAzureRMIothubExists(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+		iothubName := rs.Primary.Attributes["name"]
+		resourceGroup, hasResourceGroup := rs.Primary.Attributes["resource_group_name"]
+		if !hasResourceGroup {
+			return fmt.Errorf("Bad: no resource group found in state for IotHub: %s", iothubName)
+		}
+
+		conn := testAccProvider.Meta().(*ArmClient).iothubResourceClient
+
+		resp, err := conn.Get(resourceGroup, iothubName)
+
+		if err != nil {
+			if resp.StatusCode == http.StatusNotFound {
+				return fmt.Errorf("Bad: IotHub %q (resource group: %q) does not exist", iothubName, resourceGroup)
+			}
+
+			return fmt.Errorf("Bad: Get on iothubResourceClient: %+v", err)
+		}
+
+		return nil
+
+	}
+}
+
+func testAccAzureRMIotHub_basicStandard(name, rg, location string) string {
+	return fmt.Sprintf(`
+	resource "azurerm_iothub" "test" {
+	name                             = "%s"
+	resource_group_name              = "%s"
+	location                         = "%s"
+		sku {
+			name = "S1"
+			tier = "Standard"
+			capacity = "1"    
+		}
+
+		tags {
+			"purpose" = "testing"
+		}
+	}
+
+
+	`, name, rg, location)
+}
