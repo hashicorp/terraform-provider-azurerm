c81adb3f241c02571dcc2a267a9fa805199fbfae
tombuildsstuff <git@ibuildstuff.co.uk>
2017-10-02 13:26:53 +0100

    Ensuring the formatting types are correct

--- azurerm/resource_arm_virtual_machine_unmanaged_disks_test.go
+++ azurerm/resource_arm_virtual_machine_unmanaged_disks_test.go
@@ -2440,7 +2440,7 @@ resource "azurerm_network_interface" "test" {
 }
 
 resource "azurerm_storage_account" "test" {
-	name                     = "accsa%d"
+	name                     = "accsa%s"
 	resource_group_name      = "${azurerm_resource_group.test.name}"
 	location                 = "${azurerm_resource_group.test.location}"
 	account_tier             = "Standard"
@@ -2527,7 +2527,7 @@ resource "azurerm_network_interface" "test" {
 }
 
 resource "azurerm_storage_account" "test" {
-	name                     = "accsa%d"
+	name                     = "accsa%s"
 	resource_group_name      = "${azurerm_resource_group.test.name}"
 	location                 = "${azurerm_resource_group.test.location}"
 	account_tier             = "Standard"
