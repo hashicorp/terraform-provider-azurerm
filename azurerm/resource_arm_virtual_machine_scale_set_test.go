d823fbdee2c77516dd0515da022b496dd182d9cb
tombuildsstuff <git@ibuildstuff.co.uk>
2017-11-01 11:46:05 +0100

    Updating `TestAccAzureRMVirtualMachineScaleSet_multipleNetworkProfiles`

--- azurerm/resource_arm_virtual_machine_scale_set_test.go
+++ azurerm/resource_arm_virtual_machine_scale_set_test.go
@@ -2826,7 +2826,8 @@ resource "azurerm_storage_account" "test" {
     name = "accsa%d"
     resource_group_name = "${azurerm_resource_group.test.name}"
     location = "${azurerm_resource_group.test.location}"
-    account_type = "Standard_LRS"
+    account_tier = "Standard"
+    account_replication_type = "LRS"
 
     tags {
         environment = "staging"
