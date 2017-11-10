6e75ab255e91948c44cc3fb33c8356e20550dbaf
tombuildsstuff <git@ibuildstuff.co.uk>
2017-11-10 13:30:45 +0000

    Fixing the broken `TestAccAzureRMServiceBusTopic_enablePartitioningPremium` test:
    ```
    $ acctests azurerm TestAccAzureRMServiceBusTopic_enablePartitioningPremium
    === RUN   TestAccAzureRMServiceBusTopic_enablePartitioningPremium
    --- PASS: TestAccAzureRMServiceBusTopic_enablePartitioningPremium (504.31s)
    PASS
    ok      github.com/terraform-providers/terraform-provider-azurerm/azurerm    504.341s
    ```

--- azurerm/resource_arm_servicebus_topic_test.go
+++ azurerm/resource_arm_servicebus_topic_test.go
@@ -363,6 +363,7 @@ resource "azurerm_servicebus_namespace" "test" {
     location = "${azurerm_resource_group.test.location}"
     resource_group_name = "${azurerm_resource_group.test.name}"
     sku = "premium"
+	capacity = 1
 }
 
 resource "azurerm_servicebus_topic" "test" {
