6b06ca814c61c42654bdd8e55530382cf385eb85
Greg Oliver <divingclimber@gmail.com>
2017-11-27 20:34:51 -0800

    add encryption test

--- azurerm/resource_arm_managed_disk_test.go
+++ azurerm/resource_arm_managed_disk_test.go
@@ -168,6 +168,45 @@ func TestAccAzureRMManagedDisk_encryption(t *testing.T) {
 	})
 }
 
+func TestAccAzureRMManagedDisk_encryptionDisableEnable(t *testing.T) {
+	var d disk.Model
+
+	resourceName := "azurerm_managed_disk.test"
+	ri := acctest.RandInt()
+	rs := acctest.RandString(4)
+	disabled := testAccAzureRMManagedDisk_encryptionDisabled(ri, rs, testLocation())
+	enabled := testAccAzureRMManagedDisk_encryption(ri, rs, testLocation())
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMManagedDiskDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: disabled,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMManagedDiskExists(resourceName, &d, true),
+					resource.TestCheckResourceAttr(resourceName, "encryption_settings.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "encryption_settings.0.enabled", "false"),
+				),
+			},
+			{
+				Config: enabled,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMManagedDiskExists(resourceName, &d, true),
+					resource.TestCheckResourceAttr(resourceName, "encryption_settings.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "encryption_settings.0.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "encryption_settings.0.disk_encryption_key.#", "1"),
+					resource.TestCheckResourceAttrSet(resourceName, "encryption_settings.0.disk_encryption_key.0.secret_url"),
+					resource.TestCheckResourceAttrSet(resourceName, "encryption_settings.0.disk_encryption_key.0.source_vault_id"),
+					resource.TestCheckResourceAttr(resourceName, "encryption_settings.0.key_encryption_key.#", "1"),
+					resource.TestCheckResourceAttrSet(resourceName, "encryption_settings.0.key_encryption_key.0.key_url"),
+					resource.TestCheckResourceAttrSet(resourceName, "encryption_settings.0.key_encryption_key.0.source_vault_id"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccAzureRMManagedDisk_NonStandardCasing(t *testing.T) {
 	var d disk.Model
 	ri := acctest.RandInt()
@@ -516,6 +555,95 @@ resource "azurerm_managed_disk" "test" {
 
     encryption_settings {
       enabled = true
+      disk_encryption_key {
+	    secret_url      = "${azurerm_key_vault_secret.test.id}"
+	    source_vault_id = "${azurerm_key_vault.test.id}"
+      }
+      key_encryption_key {
+        key_url         = "${azurerm_key_vault_key.test.id}"
+        source_vault_id = "${azurerm_key_vault.test.id}"
+      }
+    }
+
+    tags {
+        environment = "acctest"
+        cost-center = "ops"
+    }
+}
+`, rInt, location, rString, rString, rString, rInt)
+}
+
+func testAccAzureRMManagedDisk_encryptionDisabled(rInt int, rString string, location string) string {
+	return fmt.Sprintf(`
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "test" {
+    name = "acctestRG-%d"
+    location = "%s"
+}
+
+resource "azurerm_key_vault" "test" {
+  name                = "acctestkv-%s"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  tenant_id           = "${data.azurerm_client_config.current.tenant_id}"
+
+  sku {
+    name = "premium"
+  }
+
+  access_policy {
+    tenant_id = "${data.azurerm_client_config.current.tenant_id}"
+    object_id = "${data.azurerm_client_config.current.service_principal_object_id}"
+
+    key_permissions = [
+      "create",
+      "delete",
+      "get",
+    ]
+
+    secret_permissions = [
+      "delete",
+      "get",
+      "set",
+    ]
+  }
+
+  enabled_for_disk_encryption = true
+
+  tags {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_key_vault_secret" "test" {
+  name      = "secret-%s"
+  value     = "szechuan"
+  vault_uri = "${azurerm_key_vault.test.vault_uri}"
+}
+
+resource "azurerm_key_vault_key" "test" {
+  name      = "key-%s"
+  vault_uri = "${azurerm_key_vault.test.vault_uri}"
+  key_type  = "EC"
+  key_size  = 2048
+
+  key_opts = [
+    "sign",
+    "verify",
+  ]
+}
+
+resource "azurerm_managed_disk" "test" {
+    name = "acctestd-%d"
+    location = "${azurerm_resource_group.test.location}"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+    storage_account_type = "Standard_LRS"
+    create_option = "Empty"
+    disk_size_gb = "1"
+
+    encryption_settings {
+      enabled = false
       disk_encryption_key {
 	    secret_url      = "${azurerm_key_vault_secret.test.id}"
 	    source_vault_id = "${azurerm_key_vault.test.id}"
