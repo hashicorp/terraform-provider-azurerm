902ff64b9ac83c4c3cf3208a4ac3d570ef26e42d
Girish Ramnani <gramnani@splunk.com>
2017-12-13 19:10:46 +0530

    config bug resolved and partial output schema

--- azurerm/config.go
+++ azurerm/config.go
@@ -655,6 +655,30 @@ func (c *Config) getArmClient() (*ArmClient, error) {
 	aschc.Sender = sender
 	client.automationScheduleClient = aschc
 
+	sjc := streamanalytics.NewStreamingJobsClientWithBaseURI(endpoint, c.SubscriptionID)
+	setUserAgent(&sjc.Client)
+	sjc.Authorizer = auth
+	sjc.Sender = autorest.CreateSender(withRequestLogging())
+	client.streamingJobClient = sjc
+
+	sic := streamanalytics.NewInputsClientWithBaseURI(endpoint, c.SubscriptionID)
+	setUserAgent(&sic.Client)
+	sic.Authorizer = auth
+	sic.Sender = autorest.CreateSender(withRequestLogging())
+	client.inputsClient = sic
+
+	soc := streamanalytics.NewOutputsClientWithBaseURI(endpoint, c.SubscriptionID)
+	setUserAgent(&soc.Client)
+	soc.Authorizer = auth
+	soc.Sender = autorest.CreateSender(withRequestLogging())
+	client.outputsClient = soc
+
+	sfc := streamanalytics.NewFunctionsClientWithBaseURI(endpoint, c.SubscriptionID)
+	setUserAgent(&sfc.Client)
+	sfc.Authorizer = auth
+	sfc.Sender = autorest.CreateSender(withRequestLogging())
+	client.functionClient = sfc
+
 	client.registerAuthentication(endpoint, graphEndpoint, c.SubscriptionID, c.TenantID, auth, graphAuth, sender)
 	client.registerDatabases(endpoint, c.SubscriptionID, auth, sender)
 	client.registerDisks(endpoint, c.SubscriptionID, auth, sender)
@@ -682,29 +706,6 @@ func (c *ArmClient) registerAuthentication(endpoint, graphEndpoint, subscription
 	rdc.Sender = sender
 	c.roleDefinitionsClient = rdc
 
-	sjc := streamanalytics.NewStreamingJobsClientWithBaseURI(endpoint, c.SubscriptionID)
-	setUserAgent(&sjc.Client)
-	sjc.Authorizer = auth
-	sjc.Sender = autorest.CreateSender(withRequestLogging())
-	client.streamingJobClient = sjc
-
-	sic := streamanalytics.NewInputsClientWithBaseURI(endpoint, c.SubscriptionID)
-	setUserAgent(&sic.Client)
-	sic.Authorizer = auth
-	sic.Sender = autorest.CreateSender(withRequestLogging())
-	client.inputsClient = sic
-
-	soc := streamanalytics.NewOutputsClientWithBaseURI(endpoint, c.SubscriptionID)
-	setUserAgent(&soc.Client)
-	soc.Authorizer = auth
-	soc.Sender = autorest.CreateSender(withRequestLogging())
-	client.outputsClient = soc
-
-	sfc := streamanalytics.NewFunctionsClientWithBaseURI(endpoint, c.SubscriptionID)
-	setUserAgent(&sfc.Client)
-	sfc.Authorizer = auth
-	sfc.Sender = autorest.CreateSender(withRequestLogging())
-	client.functionClient = sfc
 }
 
 func (c *ArmClient) registerDatabases(endpoint, subscriptionId string, auth autorest.Authorizer, sender autorest.Sender) {
--- azurerm/stream_analytics_job_output.go
+++ azurerm/stream_analytics_job_output.go
@@ -1,7 +1,76 @@
 package azurerm
 
-import "github.com/hashicorp/terraform/helper/schema"
+import (
+	"github.com/Azure/azure-sdk-for-go/arm/streamanalytics"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
+)
+
+const (
+	DelimSpace       = " "
+	DelimComma       = ","
+	DelimTab         = "\t"
+	DelimSemiColon   = ";"
+	DelimVerticalBar = "|"
+)
 
 func streamAnalyticsOutputSchema() *schema.Schema {
-	return &schema.Schema{}
+	return &schema.Schema{
+		Type:     schema.TypeList,
+		Optional: false,
+		Elem: map[string]*schema.Schema{
+			"serialization": &schema.Schema{
+				Type:     schema.TypeList,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"type": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+							ValidateFunc: validation.StringInSlice([]string{
+								string(streamanalytics.TypeCsv),
+								string(streamanalytics.TypeAvro),
+								string(streamanalytics.TypeJSON),
+							}, false),
+						},
+						"field_delimiter": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							ValidateFunc: validation.StringInSlice([]string{
+								DelimTab,
+								DelimComma,
+								DelimSemiColon,
+								DelimVerticalBar,
+								DelimSpace,
+							}, false),
+						},
+						"encoding": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"format": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							ValidateFunc: validation.StringInSlice([]string{
+								string(streamanalytics.Array),
+								string(streamanalytics.LineSeparated),
+							}, false),
+						},
+					},
+				},
+			},
+			"datasource": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: false,
+				MinItems: 1,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"blob":  &schema.Schema{},
+						"table": &schema.Schema{},
+					},
+				},
+			},
+		},
+	}
 }
