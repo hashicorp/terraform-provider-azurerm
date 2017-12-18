19252997fc78922fa36a2456482b89cd0e50b930
Girish Ramnani <gramnani@splunk.com>
2017-12-18 11:14:50 +0530

    added 7 datasources in the output schema

--- azurerm/stream_analytics_job_output.go
+++ azurerm/stream_analytics_job_output.go
@@ -14,6 +14,27 @@ const (
 	DelimVerticalBar = "|"
 )
 
+// Allbut returns a list all possible datasource other than the specified arg
+func Allbut(source string) (ret []string) {
+	sources := []string{
+		"blob",
+		"table",
+		"event_hub",
+		"sql_database",
+		"service_bus_queues",
+		"service_bus_topics",
+		"documentdb",
+	}
+	for _, val := range sources {
+		if val != source {
+			ret = append(ret, val)
+		}
+	}
+
+	return
+
+}
+
 func streamAnalyticsOutputSchema() *schema.Schema {
 	return &schema.Schema{
 		Type:     schema.TypeList,
@@ -61,13 +82,231 @@ func streamAnalyticsOutputSchema() *schema.Schema {
 			},
 			"datasource": &schema.Schema{
 				Type:     schema.TypeList,
-				Optional: false,
+				Required: true,
 				MinItems: 1,
 				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"blob":  &schema.Schema{},
-						"table": &schema.Schema{},
+						"blob": &schema.Schema{
+							Type:          schema.TypeList,
+							MaxItems:      1,
+							Optional:      true,
+							ConflictsWith: Allbut("blob"),
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"storage_account_name": &schema.Schema{
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"storage_account_key": &schema.Schema{
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"container": &schema.Schema{
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"path_pattern": &schema.Schema{
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"date_format": &schema.Schema{
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"time_format": &schema.Schema{
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"source_partition_count": &schema.Schema{
+										Type:     schema.TypeInt,
+										Optional: true,
+									},
+								},
+							},
+						},
+						"table": &schema.Schema{
+							Type:          schema.TypeList,
+							MaxItems:      1,
+							Optional:      true,
+							ConflictsWith: Allbut("table"),
+							Elem: map[string]*schema.Schema{
+								"account_name": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"account_key": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"table": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"partition_key": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"row_key": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"columns_to_remove": &schema.Schema{
+									Type:     schema.TypeString,
+									Optional: true,
+								},
+								"batch_size": &schema.Schema{
+									Type:     schema.TypeString,
+									Optional: true,
+									Default:  100,
+								},
+							},
+						},
+						"event_hub": &schema.Schema{
+							Type:          schema.TypeList,
+							MaxItems:      1,
+							Optional:      true,
+							ConflictsWith: Allbut("event_hub"),
+							Elem: map[string]*schema.Schema{
+
+								"namespace": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"shared_access_policy_name": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"shared_access_policy_key": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"event_hub_name": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"partition_key": &schema.Schema{
+									Type:     schema.TypeString,
+									Optional: true,
+								},
+							},
+						},
+						"sql_database": &schema.Schema{
+							Type:          schema.TypeList,
+							MaxItems:      1,
+							Optional:      true,
+							ConflictsWith: Allbut("sql_database"),
+							Elem: map[string]*schema.Schema{
+								"server": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"database": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"user": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"password": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"table": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+							},
+						},
+						"service_bus_queues": &schema.Schema{
+							Type:          schema.TypeList,
+							MaxItems:      1,
+							Optional:      true,
+							ConflictsWith: Allbut("service_bus_queues"),
+							Elem: map[string]*schema.Schema{
+								"namespace": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"shared_access_policy_name": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"shared_access_policy_key": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"queue_name": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"property_columns": &schema.Schema{
+									Type:     schema.TypeString,
+									Optional: true,
+								},
+							},
+						},
+						"service_bus_topics": &schema.Schema{
+							Type:          schema.TypeList,
+							MaxItems:      1,
+							Optional:      true,
+							ConflictsWith: Allbut("service_bus_topics"),
+							Elem: map[string]*schema.Schema{
+								"namespace": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"shared_access_policy_name": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"shared_access_policy_key": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"topic_name": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"property_columns": &schema.Schema{
+									Type:     schema.TypeString,
+									Optional: true,
+								},
+							},
+						},
+						"documentdb": &schema.Schema{
+							Type:          schema.TypeList,
+							MaxItems:      1,
+							Optional:      true,
+							ConflictsWith: Allbut("documentdb"),
+							Elem: map[string]*schema.Schema{
+								"account_id": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"account_key": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"database": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"collection_name_pattern": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"partition_key": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"document_id": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: false,
+								},
+							},
+						},
 					},
 				},
 			},
