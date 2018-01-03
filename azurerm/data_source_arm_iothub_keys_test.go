4324420588a1c4fcbdc51393d59a799d9580ff26
shelley.bess <shelley.bess@vertivco.com>
2018-01-02 22:20:27 -0600

    initial keys test
    Signed-off-by: shelley.bess <shelley.bess@vertivco.com>

---
+++ azurerm/data_source_arm_iothub_keys_test.go
@@ -0,0 +1,58 @@
+package azurerm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccDataSourceAzureRMIotHubKeys_basic(t *testing.T) {
+	dataSourceName := "data.azurerm_iothub_keys.test"
+	ri := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceIotHubKeysConfig_basic(ri, testLocation()),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "name", "test"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceIotHubKeysConfig_basic(rInt int, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "foo" {
+	name = "acctestIot-%d"
+	location = "%s"
+}
+
+resource "azurerm_iothub" "bar" {
+	name = "acctestiothub-%d"
+	location = "${azurerm_resource_group.foo.location}"
+	resource_group_name = "${azurerm_resource_group.foo.name}"
+	sku {
+		name = "S1"
+		tier = "Standard"
+		capacity = "1"
+	}
+
+	tags {
+		"purpose" = "testing"
+	}
+}
+
+data "azurerm_iothub_keys" "test" {
+	name = "test"
+	resource_group_name = "${azurerm_resource_group.foo.name}"
+	iot_hub_name = "${azurerm_iothub.bar.name}"
+}
+
+	`, rInt, location, rInt)
+}
