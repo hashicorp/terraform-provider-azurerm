421eda6f667b3b1244ce6fff9da1ac42d1c5eb25
shelley.bess <shelley.bess@vertivco.com>
2018-01-02 18:13:19 -0600

    keys datasource initial commit
    Signed-off-by: shelley.bess <shelley.bess@vertivco.com>

--- azurerm/data_source_arm_iothub_keys.go
+++ azurerm/data_source_arm_iothub_keys.go
@@ -1,12 +1,10 @@
 package azurerm
 
 import (
+	"fmt"
 	"log"
 
-	"github.com/Azure/azure-sdk-for-go/dataplane/keyvault"
-
 	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/helper/validation"
 )
 
 func dataSourceArmIotHubKeys() *schema.Resource {
@@ -18,109 +16,33 @@ func dataSourceArmIotHubKeys() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"key_name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
 			"resource_group_name": resourceGroupNameForDataSourceSchema(),
-			"key_type": {
+			"iot_hub_name": {
 				Type:     schema.TypeString,
 				Required: true,
-				// turns out Azure's *really* sensitive about the casing of these
-				// issue: https://github.com/Azure/azure-rest-api-specs/issues/1739
-				ValidateFunc: validation.StringInSlice([]string{
-					// TODO: add `oct` back in once this is fixed
-					// https://github.com/Azure/azure-rest-api-specs/issues/1739#issuecomment-332236257
-					string(keyvault.EC),
-					string(keyvault.RSA),
-					string(keyvault.RSAHSM),
-				}, false),
 			},
-			"key_size": {
-				Type:     schema.TypeInt,
-				Required: true,
-			},
-			"key_opts": {
-				Type: schema.TypeList,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
-					// turns out Azure's *really* sensitive about the casing of these
-					// issue: https://github.com/Azure/azure-rest-api-specs/issues/1739
-					ValidateFunc: validation.StringInSlice([]string{
-						string(keyvault.Decrypt),
-						string(keyvault.Encrypt),
-						string(keyvault.Sign),
-						string(keyvault.UnwrapKey),
-						string(keyvault.Verify),
-						string(keyvault.WrapKey),
-					}, false),
-				},
-			},
-			// Computed
-			"version": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-
-			"n": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-
-			"e": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-
-			"tags": tagsSchema(),
 		},
 	}
 }
 
 func dataSourceArmIotHubKeysRead(d *schema.ResourceData, meta interface{}) error {
 	armClient := meta.(*ArmClient)
-
+	iothubClient := armClient.iothubResourceClient
 	log.Printf("[INFO] Acquiring Keys for Authentication")
 
 	name := d.Get("name").(string)
 	resourceGroup := d.Get("resource_group_name").(string)
-	keyName := d.Get("key_name").(string)
-	resourceId := &ResourceID{
-		SubscriptionID: armClient.subscriptionId,
-		ResourceGroup:  resourceGroup,
-	}
-	resourceIdString, err := composeAzureResourceID(resourceId)
+	iotHub := d.Get("iot_hub_name").(string)
 
+	keys, err := iothubClient.GetKeysForKeyName(resourceGroup, iotHub, name)
 	if err != nil {
-		return err
+		return fmt.Errorf("Error retrieving keys with name: %s", name)
 	}
 
-	d.SetId(resourceIdString)
-
-	if err := resourceArmResourceGroupRead(d, meta); err != nil {
-		return err
-	}
-	// query := &iothub.SharedAccessSignatureAuthorizationRule{
-	// 	KeyName: &keyName,
-	// }
-
-	// subscriptionID := armClient.subscriptionId
-	// resourceId := &ResourceID{
-	// 	SubscriptionID: armClient.iothubResourceClient.SubscriptionID,
-	// 	ResourceGroup:  name,
-	// }
-	// desc := iothub.Description{
-	// 	Resourcegroup:  &resourceGroup,
-	// 	Name:           &name,
-	// 	Subscriptionid: &subscriptionID,
-	// }
-	// resourceIdString, err := composeAzureResourceID(resourceId)
-
-	// if err != nil {
-	// 	return err
-	// }
-
-	// d.SetId(resourceIdString)
+	d.Set("key_name", keys.KeyName)
+	d.Set("primary_key", keys.PrimaryKey)
+	d.Set("secondary_key", keys.SecondaryKey)
+	d.Set("permissions", string(keys.Rights))
 
 	return nil
 }
