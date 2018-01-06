d79fa0251e2bb8c16507cc4285cc91f831a212dd
Sertaç Özercan <sozercan@gmail.com>
2018-01-05 16:10:13 -0800

    add aks managed cluster provider

---
+++ azurerm/resource_arm_managed_cluster_test.go
@@ -0,0 +1,209 @@
+package azurerm
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccAzureRMManagedCluster_agentProfilePoolCountValidation(t *testing.T) {
+	cases := []struct {
+		Value    int
+		ErrCount int
+	}{
+		{Value: 0, ErrCount: 1},
+		{Value: 1, ErrCount: 0},
+		{Value: 2, ErrCount: 0},
+		{Value: 99, ErrCount: 0},
+		{Value: 100, ErrCount: 0},
+		{Value: 101, ErrCount: 1},
+	}
+
+	for _, tc := range cases {
+		_, errors := validateArmManagedClusterAgentPoolProfileCount(tc.Value, "azurerm_managed_cluster")
+
+		if len(errors) != tc.ErrCount {
+			t.Fatalf("Expected the Azure RM AKS Managed Cluster Agent Pool Profile Count to trigger a validation error for '%d'", tc.Value)
+		}
+	}
+}
+
+func TestAccAzureRMManagedCluster_basic(t *testing.T) {
+	ri := acctest.RandInt()
+	clientId := os.Getenv("ARM_CLIENT_ID")
+	clientSecret := os.Getenv("ARM_CLIENT_SECRET")
+	config := testAccAzureRMManagedCluster_basic(ri, clientId, clientSecret, testLocation())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMManagedClusterDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMManagedClusterExists("azurerm_managed_cluster.test"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureRMManagedCluster_complete(t *testing.T) {
+	ri := acctest.RandInt()
+	clientId := os.Getenv("ARM_CLIENT_ID")
+	clientSecret := os.Getenv("ARM_CLIENT_SECRET")
+	config := testAccAzureRMManagedCluster_complete(ri, clientId, clientSecret, testLocation())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMManagedClusterDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMManagedClusterExists("azurerm_managed_cluster.test"),
+				),
+			},
+		},
+	})
+}
+
+func testAccAzureRMManagedCluster_basic(rInt int, clientId string, clientSecret string, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_managed_cluster" "test" {
+  name                   = "acctestaks%d"
+  location               = "${azurerm_resource_group.test.location}"
+  resource_group_name    = "${azurerm_resource_group.test.name}"
+  dns_prefix             = "acctestaks%d"
+
+  linux_profile {
+    admin_username = "acctestuser%d"
+
+    ssh_key {
+      key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCqaZoyiz1qbdOQ8xEf6uEu1cCwYowo5FHtsBhqLoDnnp7KUTEBN+L2NxRIfQ781rxV6Iq5jSav6b2Q8z5KiseOlvKA/RF2wqU0UPYqQviQhLmW6THTpmrv/YkUCuzxDpsH7DUDhZcwySLKVVe0Qm3+5N2Ta6UYH3lsDf9R9wTP2K/+vAnflKebuypNlmocIvakFWoZda18FOmsOoIVXQ8HWFNCuw9ZCunMSN62QGamCe3dL5cXlkgHYv7ekJE15IA9aOJcM7e90oeTqo+7HTcWfdu0qQqPWY5ujyMw/llas8tsXY85LFqRnr3gJ02bAscjc477+X+j/gkpFoN1QEmt terraform@demo.tld"
+    }
+  }
+
+  agent_pool_profile {
+    name       = "default"
+    count      = 1
+    dns_prefix = "acctestagent%d"
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  service_principal {
+    client_id     = "%s"
+    client_secret = "%s"
+  }
+}
+`, rInt, location, rInt, rInt, rInt, rInt, clientId, clientSecret)
+}
+
+func testAccAzureRMManagedCluster_complete(rInt int, clientId string, clientSecret string, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_managed_cluster" "test" {
+  name                   = "acctestaks%d"
+  location               = "${azurerm_resource_group.test.location}"
+  resource_group_name    = "${azurerm_resource_group.test.name}"
+	dns_prefix             = "acctestaks%d"
+	kubernetes_version     = "1.8.2"
+
+  linux_profile {
+    admin_username = "acctestuser%d"
+
+    ssh_key {
+      key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCqaZoyiz1qbdOQ8xEf6uEu1cCwYowo5FHtsBhqLoDnnp7KUTEBN+L2NxRIfQ781rxV6Iq5jSav6b2Q8z5KiseOlvKA/RF2wqU0UPYqQviQhLmW6THTpmrv/YkUCuzxDpsH7DUDhZcwySLKVVe0Qm3+5N2Ta6UYH3lsDf9R9wTP2K/+vAnflKebuypNlmocIvakFWoZda18FOmsOoIVXQ8HWFNCuw9ZCunMSN62QGamCe3dL5cXlkgHYv7ekJE15IA9aOJcM7e90oeTqo+7HTcWfdu0qQqPWY5ujyMw/llas8tsXY85LFqRnr3gJ02bAscjc477+X+j/gkpFoN1QEmt terraform@demo.tld"
+    }
+  }
+
+  agent_pool_profile {
+    name            = "default"
+    count           = 1
+    dns_prefix      = "acctestagent%d"
+    vm_size         = "Standard_DS2_v2"
+    storage_profile = "ManagedDisks"
+    os_type         = "Linux"
+  }
+
+  service_principal {
+    client_id     = "%s"
+    client_secret = "%s"
+  }
+
+  tags {
+    you = "me"
+  }
+}
+`, rInt, location, rInt, rInt, rInt, rInt, clientId, clientSecret)
+}
+
+func testCheckAzureRMManagedClusterExists(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		// Ensure we have enough information in state to look up in API
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		name := rs.Primary.Attributes["name"]
+		resourceGroup, hasResourceGroup := rs.Primary.Attributes["resource_group_name"]
+		if !hasResourceGroup {
+			return fmt.Errorf("Bad: no resource group found in state for AKS managed cluster instance: %s", name)
+		}
+
+		conn := testAccProvider.Meta().(*ArmClient).managedClustersClient
+
+		resp, err := conn.Get(testAccProvider.Meta().(*ArmClient).StopContext, resourceGroup, name)
+		if err != nil {
+			return fmt.Errorf("Bad: Get on managedClustersClient: %+v", err)
+		}
+
+		if resp.StatusCode == http.StatusNotFound {
+			return fmt.Errorf("Bad: AKS managed cluster instance %q (resource group: %q) does not exist", name, resourceGroup)
+		}
+
+		return nil
+	}
+}
+
+func testCheckAzureRMManagedClusterDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*ArmClient).managedClustersClient
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "azurerm_managed_cluster" {
+			continue
+		}
+
+		name := rs.Primary.Attributes["name"]
+		resourceGroup := rs.Primary.Attributes["resource_group_name"]
+
+		resp, err := conn.Get(testAccProvider.Meta().(*ArmClient).StopContext, resourceGroup, name)
+
+		if err != nil {
+			return nil
+		}
+
+		if resp.StatusCode != http.StatusNotFound {
+			return fmt.Errorf("AKS managed cluster instance still exists:\n%#v", resp)
+		}
+	}
+
+	return nil
+}
