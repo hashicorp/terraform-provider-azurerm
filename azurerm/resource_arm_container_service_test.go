b3346bdba00cee5d2e151f043ae304cedef8c45f
Sertaç Özercan <sozercan@gmail.com>
2018-01-17 10:28:06 -0800

    update kubernetesComplete test

--- azurerm/resource_arm_container_service_test.go
+++ azurerm/resource_arm_container_service_test.go
@@ -255,8 +255,10 @@ resource "azurerm_container_service" "test" {
   orchestration_platform = "Kubernetes"
 
   master_profile {
-    count      = 1
-    dns_prefix = "acctestmaster%d"
+    count           = 1
+    dns_prefix      = "acctestmaster%d"
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 50
   }
 
   linux_profile {
@@ -268,10 +270,11 @@ resource "azurerm_container_service" "test" {
   }
 
   agent_pool_profile {
-    name       = "default"
-    count      = 1
-    dns_prefix = "acctestagent%d"
-    vm_size    = "Standard_A0"
+    name            = "default"
+    count           = 1
+    dns_prefix      = "acctestagent%d"
+    vm_size         = "Standard_A0"
+    os_disk_size_gb = 50
   }
 
   service_principal {
