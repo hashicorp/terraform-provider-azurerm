e10e4ad4c9b16193f43969b6d199e9f2a6bb5169
Sander van Harmelen <sander@xanzy.io>
2018-02-01 14:56:50 +0100

    Add support for augmented security rules
    This support was already added to the `azurerm_network_security_rule` resource in PR #692, but it wasn’t added to the `azurerm_network_security_group` resource at that time.
    
    It did needed a bit of additional logic to make it work properly in the `azurerm_network_security_group` resource as well, but it all worked out nicely.
    
    All related unit- and acceptance tests were successfully run before creating this PR.

--- azurerm/resource_arm_network_security_group_test.go
+++ azurerm/resource_arm_network_security_group_test.go
@@ -149,6 +149,25 @@ func TestAccAzureRMNetworkSecurityGroup_addingExtraRules(t *testing.T) {
 	})
 }
 
+func TestAccAzureRMNetworkSecurityGroup_augmented(t *testing.T) {
+	resourceName := "azurerm_network_security_group.test"
+	rInt := acctest.RandInt()
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMNetworkSecurityGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMNetworkSecurityGroup_augmented(rInt, testLocation()),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMNetworkSecurityGroupExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "security_rule.#", "1"),
+				),
+			},
+		},
+	})
+}
+
 func testCheckAzureRMNetworkSecurityGroupExists(name string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 
@@ -375,5 +394,32 @@ resource "azurerm_network_security_group" "test" {
   }
 }
 
+`, rInt, location)
+}
+
+func testAccAzureRMNetworkSecurityGroup_augmented(rInt int, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_network_security_group" "test" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+
+  security_rule {
+    name                         = "test123"
+    priority                     = 100
+    direction                    = "Inbound"
+    access                       = "Allow"
+    protocol                     = "Tcp"
+    source_port_ranges           = [ "10000-40000" ]
+    destination_port_ranges      = [ "80", "443", "8080", "8190" ]
+    source_address_prefixes      = [ "10.0.0.0/8", "192.168.0.0/16" ]
+    destination_address_prefixes = [ "172.16.0.0/20", "8.8.8.8" ]
+  }
+}
 `, rInt, location)
 }
