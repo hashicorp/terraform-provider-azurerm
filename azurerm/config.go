95f5af5d18da5229caf0b6ad8a35b05ad8ebf291
jjcollinge <jonathancollinge@live.com>
2018-03-02 09:48:35 +0000

    Removed explicit polling duration - this seems to only be an issue in acc tests

--- azurerm/config.go
+++ azurerm/config.go
@@ -638,7 +638,6 @@ func (c *ArmClient) registerHDInsightClustersClients(endpoint, subscriptionId st
 	hdInsightClustersClient.Authorizer = auth
 	hdInsightClustersClient.Sender = sender
 	hdInsightClustersClient.SkipResourceProviderRegistration = c.skipProviderRegistration
-	hdInsightClustersClient.PollingDuration = 2 * time.Hour
 	c.hdInsightClustersClient = hdInsightClustersClient
 }
 
