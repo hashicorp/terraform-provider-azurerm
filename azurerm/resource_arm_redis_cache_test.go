6ee0f102d87e0ec1858aa383fbcca1fdb5a4a215
tombuildsstuff <git@ibuildstuff.co.uk>
2018-03-08 10:13:16 -0700

    Splitting the storage account fields

--- azurerm/resource_arm_redis_cache_test.go
+++ azurerm/resource_arm_redis_cache_test.go
@@ -615,22 +615,23 @@ resource "azurerm_storage_account" "test" {
   name                = "unlikely23exst2acct%s"
   resource_group_name = "${azurerm_resource_group.test.name}"
   location     = "${azurerm_resource_group.test.location}"
-  account_type = "Standard_GRS"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
   tags {
     environment = "staging"
   }
 }
 resource "azurerm_redis_cache" "test" {
-    name                = "acctestRedis-%d"
-    location            = "${azurerm_resource_group.test.location}"
-    resource_group_name = "${azurerm_resource_group.test.name}"
-    capacity            = 3
-    family              = "P"
-    sku_name            = "Premium"
-    enable_non_ssl_port = false
-    redis_configuration {
-	  notify_keyspace_events = "KAE"
-    }
+  name                = "acctestRedis-%d"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  capacity            = 3
+  family              = "P"
+  sku_name            = "Premium"
+  enable_non_ssl_port = false
+  redis_configuration {
+    notify_keyspace_events = "KAE"
+  }
 }
 `, rInt, location, rString, rInt)
 }
