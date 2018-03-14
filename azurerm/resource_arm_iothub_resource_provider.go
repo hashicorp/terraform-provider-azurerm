ae9b60452ec850558f26d6920c5dd698de540f10
tombuildsstuff <git@ibuildstuff.co.uk>
2018-03-14 12:24:17 -0500

    Refactoring / issues identified in the PR

--- azurerm/resource_arm_iothub_resource_provider.go
+++ azurerm/resource_arm_iothub_resource_provider.go
@@ -1,13 +1,14 @@
 package azurerm
 
 import (
-	"errors"
 	"fmt"
+	"log"
 
 	"github.com/Azure/azure-sdk-for-go/services/iothub/mgmt/2017-07-01/devices"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/response"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
 func resourceArmIotHub() *schema.Resource {
@@ -26,10 +27,11 @@ func resourceArmIotHub() *schema.Resource {
 				Required: true,
 				ForceNew: true,
 			},
-			"tags":     tagsSchema(),
+
 			"location": locationSchema(),
 
 			"resource_group_name": resourceGroupNameSchema(),
+
 			"sku": {
 				Type:     schema.TypeList,
 				MaxItems: 1,
@@ -101,140 +103,115 @@ func resourceArmIotHub() *schema.Resource {
 					},
 				},
 			},
+
+			"tags": tagsSchema(),
 		},
 	}
 
 }
 
 func resourceArmIotHubCreateAndUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ArmClient).iothubResourceClient
+	ctx := meta.(*ArmClient).StopContext
+	subscriptionID := meta.(*ArmClient).subscriptionId
 
-	armClient := meta.(*ArmClient)
-	iothubClient := armClient.iothubResourceClient
-	ctx := armClient.StopContext
-
-	rg := d.Get("resource_group_name").(string)
 	name := d.Get("name").(string)
-
-	res, err := iothubClient.CheckNameAvailability(ctx, devices.OperationInputs{
+	resourceGroup := d.Get("resource_group_name").(string)
+	res, err := client.CheckNameAvailability(ctx, devices.OperationInputs{
 		Name: &name,
 	})
 
 	if err != nil {
-		return err
+		return fmt.Errorf("An error occurred checking if the IoTHub name was unique: %+v", err)
 	}
 
 	if !*res.NameAvailable {
-		_, err := iothubClient.Get(ctx, rg, name)
+		_, err := client.Get(ctx, resourceGroup, name)
 		if err != nil {
-			return errors.New(string(res.Reason))
+			return fmt.Errorf("An IoTHub already exists with the name %q - please choose an alternate name: %s", name, string(res.Reason))
 		}
 	}
 
 	location := d.Get("location").(string)
+	skuInfo := expandIoTHubSku(d)
+	tags := d.Get("tags").(map[string]interface{})
 
-	subscriptionID := armClient.subscriptionId
-	skuInfo := expandAzureRmIotHubSku(d)
-
-	desc := devices.IotHubDescription{
-		Resourcegroup:  &rg,
-		Name:           &name,
-		Location:       &location,
-		Subscriptionid: &subscriptionID,
+	properties := devices.IotHubDescription{
+		Name:           utils.String(name),
+		Location:       utils.String(location),
+		Subscriptionid: utils.String(subscriptionID),
 		Sku:            &skuInfo,
+		Tags:           expandTags(tags),
 	}
 
-	if tagsI, ok := d.GetOk("tags"); ok {
-		tags := tagsI.(map[string]interface{})
-		desc.Tags = *expandTags(tags)
+	future, err := client.CreateOrUpdate(ctx, resourceGroup, name, properties, "")
+	if err != nil {
+		return fmt.Errorf("Error creating/updating IotHub %q (Resource Group %q): %+v", name, resourceGroup, err)
 	}
 
-	future, err := iothubClient.CreateOrUpdate(ctx, rg, name, desc, "")
+	err = future.WaitForCompletion(ctx, client.Client)
+	if err != nil {
+		return fmt.Errorf("Error waiting for the completion of the creating/updating of IotHub %q (Resource Group %q): %+v", name, resourceGroup, err)
+	}
+
+	resp, err := client.Get(ctx, resourceGroup, name)
 	if err != nil {
 		return err
 	}
 
-	err = future.WaitForCompletion(ctx, iothubClient.Client)
-	if err != nil {
-		return fmt.Errorf("Error creating or updating IotHub %q (Resource Group %q): %+v", name, rg, err)
-	}
-
-	desc, err = iothubClient.Get(ctx, rg, name)
-	if err != nil {
-		return err
-	}
-
-	d.SetId(*desc.ID)
+	d.SetId(*resp.ID)
 	return resourceArmIotHubRead(d, meta)
-
-}
-
-func expandAzureRmIotHubSku(d *schema.ResourceData) devices.IotHubSkuInfo {
-	skuList := d.Get("sku").([]interface{})
-	skuMap := skuList[0].(map[string]interface{})
-	cap := int64(skuMap["capacity"].(int))
-
-	name := skuMap["name"].(string)
-	tier := skuMap["tier"].(string)
-
-	return devices.IotHubSkuInfo{
-		Name:     devices.IotHubSku(name),
-		Tier:     devices.IotHubSkuTier(tier),
-		Capacity: &cap,
-	}
-
 }
 
 func resourceArmIotHubRead(d *schema.ResourceData, meta interface{}) error {
-	armClient := meta.(*ArmClient)
-	iothubClient := armClient.iothubResourceClient
-	ctx := armClient.StopContext
+	client := meta.(*ArmClient).iothubResourceClient
+	ctx := meta.(*ArmClient).StopContext
 
 	id, err := parseAzureResourceID(d.Id())
-
 	if err != nil {
 		return err
 	}
 
-	iothubName := id.Path["IotHubs"]
-	desc, err := iothubClient.Get(ctx, id.ResourceGroup, iothubName)
-
+	name := id.Path["IotHubs"]
+	resourceGroup := id.ResourceGroup
+	hub, err := client.Get(ctx, id.ResourceGroup, name)
 	if err != nil {
-		return err
+		if utils.ResponseWasNotFound(hub.Response) {
+			log.Printf("[DEBUG] IoTHub %q (Resource Group %q) was not found!", name, resourceGroup)
+			d.SetId("")
+			return nil
+		}
+
+		return fmt.Errorf("Error retrieving IotHub Client %q (Resource Group %q): %+v", name, resourceGroup, err)
 	}
 
-	keysResp, err := iothubClient.ListKeys(ctx, id.ResourceGroup, iothubName)
+	keysResp, err := client.ListKeys(ctx, resourceGroup, name)
+	if err != nil {
+		return fmt.Errorf("Error listing keys for IoTHub %q (Resource Group %q): %+v", name, resourceGroup, err)
+	}
+
 	keyList := keysResp.Response()
-
-	var keys []map[string]interface{}
-	for _, key := range *keyList.Value {
-		keyMap := make(map[string]interface{})
-
-		if keyName := key.KeyName; keyName != nil {
-			keyMap["key_name"] = *keyName
-		}
-
-		if primaryKey := key.PrimaryKey; primaryKey != nil {
-			keyMap["primary_key"] = *primaryKey
-		}
-
-		if secondaryKey := key.SecondaryKey; secondaryKey != nil {
-			keyMap["secondary_key"] = *secondaryKey
-		}
-
-		keyMap["permissions"] = string(key.Rights)
-		keys = append(keys, keyMap)
-	}
+	keys := flattenIoTHubSharedAccessPolicy(keyList.Value)
 
 	if err := d.Set("shared_access_policy", keys); err != nil {
-		return fmt.Errorf("Error flattening `shared_access_policy` in IoTHub %q: %+v", iothubName, err)
+		return fmt.Errorf("Error flattening `shared_access_policy` in IoTHub %q: %+v", name, err)
 	}
 
-	if properties := desc.Properties; properties != nil {
+	if properties := hub.Properties; properties != nil {
 		d.Set("hostname", properties.HostName)
 	}
 
-	d.Set("type", desc.Type)
-	flattenAndSetTags(d, &desc.Tags)
+	d.Set("name", name)
+	d.Set("resource_group_name", resourceGroup)
+	if location := hub.Location; location != nil {
+		d.Set("location", *location)
+	}
+	sku := flattenIoTHubSku(hub.Sku)
+	if err := d.Set("sku", sku); err != nil {
+		return fmt.Errorf("Error flattening `sku`: %+v", err)
+	}
+	d.Set("type", hub.Type)
+	flattenAndSetTags(d, hub.Tags)
 
 	return nil
 }
@@ -268,4 +245,58 @@ func resourceArmIotHubDelete(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	return nil
+}
+
+func expandIoTHubSku(d *schema.ResourceData) devices.IotHubSkuInfo {
+	skuList := d.Get("sku").([]interface{})
+	skuMap := skuList[0].(map[string]interface{})
+	cap := int64(skuMap["capacity"].(int))
+
+	name := skuMap["name"].(string)
+	tier := skuMap["tier"].(string)
+
+	return devices.IotHubSkuInfo{
+		Name:     devices.IotHubSku(name),
+		Tier:     devices.IotHubSkuTier(tier),
+		Capacity: &cap,
+	}
+}
+
+func flattenIoTHubSku(input *devices.IotHubSkuInfo) []interface{} {
+	output := make(map[string]interface{}, 0)
+
+	output["name"] = string(input.Name)
+	output["tier"] = string(input.Tier)
+	if capacity := input.Capacity; capacity != nil {
+		output["capacity"] = int(*capacity)
+	}
+
+	return []interface{}{output}
+}
+
+func flattenIoTHubSharedAccessPolicy(input *[]devices.SharedAccessSignatureAuthorizationRule) []interface{} {
+	results := make([]interface{}, 0)
+
+	if keys := input; keys != nil {
+		for _, key := range *keys {
+			keyMap := make(map[string]interface{})
+
+			if keyName := key.KeyName; keyName != nil {
+				keyMap["key_name"] = *keyName
+			}
+
+			if primaryKey := key.PrimaryKey; primaryKey != nil {
+				keyMap["primary_key"] = *primaryKey
+			}
+
+			if secondaryKey := key.SecondaryKey; secondaryKey != nil {
+				keyMap["secondary_key"] = *secondaryKey
+			}
+
+			keyMap["permissions"] = string(key.Rights)
+			results = append(results, keyMap)
+		}
+	}
+
+	return results
 }
