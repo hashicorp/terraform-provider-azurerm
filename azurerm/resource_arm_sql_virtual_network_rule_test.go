193e991b26267e90b86c740e14552669bf458951
Vanessa Louwagie <vlouwagie@horizontalintegration.com>
2018-03-17 14:09:30 -0500

    Added additional validation function acceptance test for SQL vnet rule

--- azurerm/resource_arm_sql_virtual_network_rule_test.go
+++ azurerm/resource_arm_sql_virtual_network_rule_test.go
@@ -173,7 +173,7 @@ func TestAccAzureRMSqlVirtualNetworkRule_multipleSubnets(t *testing.T) {
 }
 
 /*
-	Validation Function Tests - Invalid Name Validation
+	Validation Function Tests - Invalid Name Validations
 */
 func TestResourceAzureRMSqlVirtualNetworkRule_invalidNameValidation(t *testing.T) {
 	cases := []struct {
@@ -197,9 +197,9 @@ func TestResourceAzureRMSqlVirtualNetworkRule_invalidNameValidation(t *testing.T
 			Value:    "test'Rule",
 			ErrCount: 1,
 		},
-		// Cannot be more than 128 characters (1 case)
+		// Cannot be more than 128 characters (1 case - ensure starts with a letter)
 		{
-			Value:    acctest.RandString(129),
+			Value:    fmt.Sprintf("v%s", acctest.RandString(128)),
 			ErrCount: 1,
 		},
 		// Cannot be empty (1 case)
@@ -227,7 +227,71 @@ func TestResourceAzureRMSqlVirtualNetworkRule_invalidNameValidation(t *testing.T
 		_, errors := validateSqlVirtualNetworkRuleName(tc.Value, "azurerm_sql_virtual_network_rule")
 
 		if len(errors) != tc.ErrCount {
-			t.Fatalf("Expected the Azure RM SQL Virtual Network Rule Name to trigger a validation error.")
+			t.Fatalf("Bad: Expected the Azure RM SQL Virtual Network Rule Name to trigger a validation error.")
+		}
+	}
+}
+
+/*
+	Validation Function Tests - (Barely) Valid Name Validations
+*/
+func TestResourceAzureRMSqlVirtualNetworkRule_validNameValidation(t *testing.T) {
+	cases := []struct {
+		Value    string
+		ErrCount int
+	}{
+		// Test all lowercase
+		{
+			Value:    "thisisarule",
+			ErrCount: 0,
+		},
+		// Test all uppercase
+		{
+			Value:    "THISISARULE",
+			ErrCount: 0,
+		},
+		// Test alternating cases
+		{
+			Value:    "tHiSiSaRuLe",
+			ErrCount: 0,
+		},
+		// Test hyphens
+		{
+			Value:    "this-is-a-rule",
+			ErrCount: 0,
+		},
+		// Test multiple hyphens in a row
+		{
+			Value:    "this----1s----a----ru1e",
+			ErrCount: 0,
+		},
+		// Test numbers (except for first character)
+		{
+			Value:    "v1108501298509850810258091285091820-5",
+			ErrCount: 0,
+		},
+		// Test a lot of hyphens and numbers
+		{
+			Value:    "x-5-4-1-2-5-2-6-1-5-2-5-1-2-5-6-2-2",
+			ErrCount: 0,
+		},
+		// Test exactly 128 characters
+		{
+			Value:    fmt.Sprintf("v%s", acctest.RandString(127)),
+			ErrCount: 0,
+		},
+		// Test short, 1-letter name
+		{
+			Value:    "V",
+			ErrCount: 0,
+		},
+	}
+
+	for _, tc := range cases {
+		_, errors := validateSqlVirtualNetworkRuleName(tc.Value, "azurerm_sql_virtual_network_rule")
+
+		if len(errors) != tc.ErrCount {
+			t.Fatalf("Bad: Expected the Azure RM SQL Virtual Network Rule Name pass name validation successfully but triggered a validation error.")
 		}
 	}
 }
