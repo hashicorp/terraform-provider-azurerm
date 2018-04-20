ebba5afa170ac83e4dd385a4d241306636fd5488
Su Shi <1684739+metacpp@users.noreply.github.com>
2018-04-20 08:47:36 -0700

    Filter out  from properties of tags object. (#1107)
    * Filter out  from properties of tags object.
    
    * Limit the filtering for $type to rule resource only and add validation logic to avoid $type from end user as tag name.
    
    * Update acceptance tests to validate the $type in attributes.
    
    * Add more non-exist check for metric alert rule.
    
    * Update the validation logic for test cases.
    
    * Move filtering logic into seprated utility function for future reuse.
    
    * Update the comment for filterTags function.
    
    * Remove the validation of $type in common function.
    
    * add test case for filterTag function.
    
    * Refactoring tags to match the others
    
    ```
    $ acctests azurerm TestValidateMetricAlertRuleTags
    === RUN   TestValidateMetricAlertRuleTags
    --- PASS: TestValidateMetricAlertRuleTags (0.00s)
    PASS
    ok  	github.com/terraform-providers/terraform-provider-azurerm/azurerm	0.025s
    ```

--- azurerm/tags_test.go
+++ azurerm/tags_test.go
@@ -94,3 +94,22 @@ func TestExpandARMTags(t *testing.T) {
 		}
 	}
 }
+
+func TestFilterARMTags(t *testing.T) {
+	testData := make(map[string]*string)
+	valueData := [3]string{"value1", "value2", "value3"}
+
+	testData["key1"] = &valueData[0]
+	testData["key2"] = &valueData[1]
+	testData["key3"] = &valueData[2]
+
+	filtered := filterTags(testData, "key1", "key3", "")
+
+	if len(filtered) != 1 {
+		t.Fatalf("Expected 1 result in filtered tag map, got %d", len(filtered))
+	}
+
+	if filtered["key2"] != &valueData[1] {
+		t.Fatalf("Expected %v in filtered tag map, got %v", valueData[1], *filtered["key2"])
+	}
+}
