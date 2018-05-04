2da35ab81cfb07f09478ff9513bfc6c52b47ab89
tombuildsstuff <git@ibuildstuff.co.uk>
2018-05-04 11:50:06 +0100

    New Data Source: `azurerm_key_vault

--- azurerm/provider.go
+++ azurerm/provider.go
@@ -86,6 +86,7 @@ func Provider() terraform.ResourceProvider {
 			"azurerm_dns_zone":                              dataSourceArmDnsZone(),
 			"azurerm_eventhub_namespace":                    dataSourceEventHubNamespace(),
 			"azurerm_image":                                 dataSourceArmImage(),
+			"azurerm_key_vault":                             dataSourceArmKeyVault(),
 			"azurerm_key_vault_access_policy":               dataSourceArmKeyVaultAccessPolicy(),
 			"azurerm_key_vault_secret":                      dataSourceArmKeyVaultSecret(),
 			"azurerm_managed_disk":                          dataSourceArmManagedDisk(),
