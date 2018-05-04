32f0b80c7a1e746c6c5ab501294fbcd9329477e5
kt <kt@katbyte.me>
2018-05-04 13:56:24 -0700

    cosmos_db_account: added ip_range_filter validation and example

--- azurerm/resource_arm_cosmos_db_account.go
+++ azurerm/resource_arm_cosmos_db_account.go
@@ -71,6 +71,10 @@ func resourceArmCosmosDBAccount() *schema.Resource {
 			"ip_range_filter": {
 				Type:     schema.TypeString,
 				Optional: true,
+				ValidateFunc: validation.StringMatch(
+					regexp.MustCompile(`^(\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b[,]?){1,}$`),
+					"Cosmos DB ip_range_filter must be a set of CIDR IP addresses separated by commas with no spaces: '10.0.0.1,10.0.0.2'",
+				),
 			},
 
 			"enable_automatic_failover": {
