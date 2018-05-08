c6dc17c58b153be302bc15c06758d900f62c9216
Tom Harvey <tombuildsstuff@users.noreply.github.com>
2018-05-08 07:42:12 -0700

    New Data Source: `azurerm_kubernetes_cluster` (#1204)
    * New Data Source: `azurerm_kubernetes_cluster`
    
    * Documenting how to use the AKS data source with the K8S Provider
    
    * Fixing issues identified in code review
    
    * `service_principal` - setting as a List not a Set

--- azurerm/provider.go
+++ azurerm/provider.go
@@ -88,6 +88,7 @@ func Provider() terraform.ResourceProvider {
 			"azurerm_eventhub_namespace":                    dataSourceEventHubNamespace(),
 			"azurerm_image":                                 dataSourceArmImage(),
 			"azurerm_key_vault_access_policy":               dataSourceArmKeyVaultAccessPolicy(),
+			"azurerm_kubernetes_cluster":                    dataSourceArmKubernetesCluster(),
 			"azurerm_managed_disk":                          dataSourceArmManagedDisk(),
 			"azurerm_network_interface":                     dataSourceArmNetworkInterface(),
 			"azurerm_network_security_group":                dataSourceArmNetworkSecurityGroup(),
