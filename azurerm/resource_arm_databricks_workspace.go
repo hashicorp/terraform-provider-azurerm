7b62daa14f2bfb56e4fb9efd5193b0a3d506797c
yaron2 <schneider.yaron@live.com>
2018-05-16 20:35:14 -0700

    changed location to use locationSchema()

--- azurerm/resource_arm_databricks_workspace.go
+++ azurerm/resource_arm_databricks_workspace.go
@@ -28,12 +28,7 @@ func resourceArmDatabricksWorkspace() *schema.Resource {
 				ForceNew: true,
 			},
 
-			"location": {
-				Type:      schema.TypeString,
-				Required:  true,
-				ForceNew:  true,
-				StateFunc: azureRMNormalizeLocation,
-			},
+			"location": locationSchema(),
 
 			"resource_group_name": resourceGroupNameSchema(),
 
