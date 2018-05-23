9a9f793fbda47619e021a0b50b811a2531452976
Junyi Yi <junyi@microsoft.com>
2018-05-22 19:40:39 -0700

    Add VM timezone acceptance test.

--- azurerm/resource_arm_virtual_machine_migration_test.go
+++ azurerm/resource_arm_virtual_machine_migration_test.go
@@ -14,7 +14,7 @@ func TestAzureRMVirtualMachineMigrateState(t *testing.T) {
 		ExpectedAttributes map[string]string
 		Meta               interface{}
 	}{
-		"v0_1": {
+		"v0_1_simple": {
 			StateVersion: 0,
 			ID:           "azurevm-dummy-id",
 			InputAttributes: map[string]string{
@@ -31,6 +31,45 @@ func TestAzureRMVirtualMachineMigrateState(t *testing.T) {
 				"os_profile_windows_config.2229351482.provision_vm_agent":           "false",
 				"os_profile_windows_config.2229351482.timezone":                     "UTC",
 				"os_profile_windows_config.2229351482.winrm.#":                      "0",
+			},
+		},
+		"v0_1_full": {
+			StateVersion: 0,
+			ID:           "azurevm-dummy-id",
+			InputAttributes: map[string]string{
+				"os_profile_windows_config.#":                                                    "1",
+				"os_profile_windows_config.2256145325.additional_unattend_config.#":              "2",
+				"os_profile_windows_config.2256145325.additional_unattend_config.0.component":    "shellsetup",
+				"os_profile_windows_config.2256145325.additional_unattend_config.0.content":      "autologon content",
+				"os_profile_windows_config.2256145325.additional_unattend_config.0.pass":         "mypass",
+				"os_profile_windows_config.2256145325.additional_unattend_config.1.component":    "shellsetup2",
+				"os_profile_windows_config.2256145325.additional_unattend_config.0.setting_name": "AutoLogon",
+				"os_profile_windows_config.2256145325.additional_unattend_config.1.content":      "first logon content",
+				"os_profile_windows_config.2256145325.additional_unattend_config.1.pass":         "mypass2",
+				"os_profile_windows_config.2256145325.additional_unattend_config.1.setting_name": "FirstLogonCommands",
+				"os_profile_windows_config.2256145325.enable_automatic_upgrades":                 "true",
+				"os_profile_windows_config.2256145325.provision_vm_agent":                        "true",
+				"os_profile_windows_config.2256145325.winrm.#":                                   "1",
+				"os_profile_windows_config.2256145325.winrm.0.certificate_url":                   "",
+				"os_profile_windows_config.2256145325.winrm.0.protocol":                          "http",
+			},
+			ExpectedAttributes: map[string]string{
+				"os_profile_windows_config.#":                                                    "1",
+				"os_profile_windows_config.1257668355.additional_unattend_config.#":              "2",
+				"os_profile_windows_config.1257668355.additional_unattend_config.0.component":    "shellsetup",
+				"os_profile_windows_config.1257668355.additional_unattend_config.0.content":      "autologon content",
+				"os_profile_windows_config.1257668355.additional_unattend_config.0.pass":         "mypass",
+				"os_profile_windows_config.1257668355.additional_unattend_config.0.setting_name": "AutoLogon",
+				"os_profile_windows_config.1257668355.additional_unattend_config.1.component":    "shellsetup2",
+				"os_profile_windows_config.1257668355.additional_unattend_config.1.content":      "first logon content",
+				"os_profile_windows_config.1257668355.additional_unattend_config.1.pass":         "mypass2",
+				"os_profile_windows_config.1257668355.additional_unattend_config.1.setting_name": "FirstLogonCommands",
+				"os_profile_windows_config.1257668355.enable_automatic_upgrades":                 "true",
+				"os_profile_windows_config.1257668355.provision_vm_agent":                        "true",
+				"os_profile_windows_config.1257668355.timezone":                                  "UTC",
+				"os_profile_windows_config.1257668355.winrm.#":                                   "1",
+				"os_profile_windows_config.1257668355.winrm.0.certificate_url":                   "",
+				"os_profile_windows_config.1257668355.winrm.0.protocol":                          "http",
 			},
 		},
 	}
