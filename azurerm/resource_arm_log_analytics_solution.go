293a04ce7668fb259b6cb7f50f444e97b79900b8
Tom Harvey <tombuildsstuff@users.noreply.github.com>
2018-06-01 19:52:14 +0200

    SDK: moving off deprecated SDK's (#1332)
    * Moving to the undeprecated Container Instance SDK
    
    * Moving to the non-preview version of the Container Instance API
    
    * Moving to the GA version of the EventGrid API
    
    * Moving to an stable version of the Monitor API's
    
    * Moving to the undeprecated Preview API for Operational Insights
    
    * Moving to an undeprecated version of the Operations Management SDK
    
    * Moving to a non-deprecated version of the SQL SDK

--- azurerm/resource_arm_log_analytics_solution.go
+++ azurerm/resource_arm_log_analytics_solution.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"strings"
 
-	"github.com/Azure/azure-sdk-for-go/services/operationsmanagement/mgmt/2015-11-01-preview/operationsmanagement"
+	"github.com/Azure/azure-sdk-for-go/services/preview/operationsmanagement/mgmt/2015-11-01-preview/operationsmanagement"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
