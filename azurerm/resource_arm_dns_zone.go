a540fc022f99839b803e5fe7b8bcd3f37f014606
Stijn Tintel <stijn@linux-ipv6.be>
2018-06-05 17:33:41 +0300

    DNS: upgrade to the 2018-03-01 Preview SDK

--- azurerm/resource_arm_dns_zone.go
+++ azurerm/resource_arm_dns_zone.go
@@ -3,7 +3,7 @@ package azurerm
 import (
 	"fmt"
 
-	"github.com/Azure/azure-sdk-for-go/services/dns/mgmt/2017-10-01/dns"
+	"github.com/Azure/azure-sdk-for-go/services/dns/mgmt/2018-03-01-preview/dns"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/response"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
