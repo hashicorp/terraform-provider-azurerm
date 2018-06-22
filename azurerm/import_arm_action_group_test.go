d2f5e65b8d780db115f07a529123e7608925e60b
Junyi Yi <junyi@microsoft.com>
2018-06-21 18:32:17 -0700

    Add import tests for azurerm_action_group

---
+++ azurerm/import_arm_action_group_test.go
@@ -0,0 +1,77 @@
+package azurerm
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccAzureRMActionGroup_importBasic(t *testing.T) {
+	resourceName := "azurerm_action_group.test"
+
+	ri := acctest.RandInt()
+	config := testAccAzureRMActionGroup_basic(ri, testLocation())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMActionGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccAzureRMActionGroup_importEmpty(t *testing.T) {
+	resourceName := "azurerm_action_group.test"
+
+	ri := acctest.RandInt()
+	config := testAccAzureRMActionGroup_empty(ri, testLocation())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMActionGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccAzureRMActionGroup_importDisabledEmpty(t *testing.T) {
+	resourceName := "azurerm_action_group.test"
+
+	ri := acctest.RandInt()
+	config := testAccAzureRMActionGroup_disabledEmpty(ri, testLocation())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMActionGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
