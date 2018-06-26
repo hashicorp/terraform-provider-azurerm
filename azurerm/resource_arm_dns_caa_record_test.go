d61275577ba069e38d3cb6aaa59f71ed2ef90763
ranieuwe <ronieuwe@microsoft.com>
2018-06-26 17:28:02 +0200

    Structure for CAA records based on the SRV record type

---
+++ azurerm/resource_arm_dns_caa_record_test.go
@@ -0,0 +1,307 @@
+package azurerm
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/services/dns/mgmt/2017-10-01/dns"
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccAzureRMDnsCaaRecord_basic(t *testing.T) {
+	resourceName := "azurerm_dns_caa_record.test"
+	ri := acctest.RandInt()
+	config := testAccAzureRMDnsCaaRecord_basic(ri, testLocation())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMDnsCaaRecordDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMDnsCaaRecordExists(resourceName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureRMDnsCaaRecord_updateRecords(t *testing.T) {
+	resourceName := "azurerm_dns_caa_record.test"
+	ri := acctest.RandInt()
+	location := testLocation()
+	preConfig := testAccAzureRMDnsCaaRecord_basic(ri, location)
+	postConfig := testAccAzureRMDnsCaaRecord_updateRecords(ri, location)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMDnsCaaRecordDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: preConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMDnsCaaRecordExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "record.#", "2"),
+				),
+			},
+			{
+				Config: postConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMDnsCaaRecordExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "record.#", "3"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureRMDnsCaaRecord_withTags(t *testing.T) {
+	resourceName := "azurerm_dns_caa_record.test"
+	ri := acctest.RandInt()
+	location := testLocation()
+	preConfig := testAccAzureRMDnsCaaRecord_withTags(ri, location)
+	postConfig := testAccAzureRMDnsCaaRecord_withTagsUpdate(ri, location)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMDnsCaaRecordDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: preConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMDnsCaaRecordExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "2"),
+				),
+			},
+			{
+				Config: postConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMDnsCaaRecordExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+				),
+			},
+		},
+	})
+}
+
+func testCheckAzureRMDnsCaaRecordExists(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		// Ensure we have enough information in state to look up in API
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		srvName := rs.Primary.Attributes["name"]
+		zoneName := rs.Primary.Attributes["zone_name"]
+		resourceGroup, hasResourceGroup := rs.Primary.Attributes["resource_group_name"]
+		if !hasResourceGroup {
+			return fmt.Errorf("Bad: no resource group found in state for DNS SRV record: %s", srvName)
+		}
+
+		conn := testAccProvider.Meta().(*ArmClient).dnsClient
+		ctx := testAccProvider.Meta().(*ArmClient).StopContext
+		resp, err := conn.Get(ctx, resourceGroup, zoneName, srvName, dns.SRV)
+		if err != nil {
+			return fmt.Errorf("Bad: Get SRV RecordSet: %+v", err)
+		}
+
+		if resp.StatusCode == http.StatusNotFound {
+			return fmt.Errorf("Bad: DNS SRV record %s (resource group: %s) does not exist", srvName, resourceGroup)
+		}
+
+		return nil
+	}
+}
+
+func testCheckAzureRMDnsCaaRecordDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*ArmClient).dnsClient
+	ctx := testAccProvider.Meta().(*ArmClient).StopContext
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "azurerm_dns_caa_record" {
+			continue
+		}
+
+		srvName := rs.Primary.Attributes["name"]
+		zoneName := rs.Primary.Attributes["zone_name"]
+		resourceGroup := rs.Primary.Attributes["resource_group_name"]
+
+		resp, err := conn.Get(ctx, resourceGroup, zoneName, srvName, dns.SRV)
+
+		if err != nil {
+			if resp.StatusCode == http.StatusNotFound {
+				return nil
+			}
+
+			return err
+		}
+
+		return fmt.Errorf("DNS SRV record still exists:\n%#v", resp.RecordSetProperties)
+	}
+
+	return nil
+}
+
+func testAccAzureRMDnsCaaRecord_basic(rInt int, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_dns_zone" "test" {
+  name                = "acctestzone%d.com"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
+resource "azurerm_dns_caa_record" "test" {
+  name                = "myarecord%d"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  zone_name           = "${azurerm_dns_zone.test.name}"
+  ttl                 = 300
+
+  record {
+    priority = 1
+    weight   = 5
+    port     = 8080
+    target   = "target1.contoso.com"
+  }
+
+  record {
+    priority = 2
+    weight   = 25
+    port     = 8080
+    target   = "target2.contoso.com"
+  }
+}
+`, rInt, location, rInt, rInt)
+}
+
+func testAccAzureRMDnsCaaRecord_updateRecords(rInt int, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_dns_zone" "test" {
+  name                = "acctestzone%d.com"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
+resource "azurerm_dns_caa_record" "test" {
+  name                = "myarecord%d"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  zone_name           = "${azurerm_dns_zone.test.name}"
+  ttl                 = 300
+
+  record {
+    priority = 1
+    weight   = 5
+    port     = 8080
+    target   = "target1.contoso.com"
+  }
+
+  record {
+    priority = 2
+    weight   = 25
+    port     = 8080
+    target   = "target2.contoso.com"
+  }
+
+  record {
+    priority = 3
+    weight   = 100
+    port     = 8080
+    target   = "target3.contoso.com"
+  }
+}
+`, rInt, location, rInt, rInt)
+}
+
+func testAccAzureRMDnsCaaRecord_withTags(rInt int, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_dns_zone" "test" {
+  name                = "acctestzone%d.com"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
+resource "azurerm_dns_caa_record" "test" {
+  name                = "myarecord%d"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  zone_name           = "${azurerm_dns_zone.test.name}"
+  ttl                 = 300
+
+  record {
+    priority = 1
+    weight   = 5
+    port     = 8080
+    target   = "target1.contoso.com"
+  }
+
+  record {
+    priority = 2
+    weight   = 25
+    port     = 8080
+    target   = "target2.contoso.com"
+  }
+
+  tags {
+    environment = "Production"
+    cost_center = "MSFT"
+  }
+}
+`, rInt, location, rInt, rInt)
+}
+
+func testAccAzureRMDnsCaaRecord_withTagsUpdate(rInt int, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_dns_zone" "test" {
+  name                = "acctestzone%d.com"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
+resource "azurerm_dns_caa_record" "test" {
+  name                = "myarecord%d"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  zone_name           = "${azurerm_dns_zone.test.name}"
+  ttl                 = 300
+
+  record {
+    priority = 1
+    weight   = 5
+    port     = 8080
+    target   = "target1.contoso.com"
+  }
+
+  record {
+    priority = 2
+    weight   = 25
+    port     = 8080
+    target   = "target2.contoso.com"
+  }
+
+  tags {
+    environment = "staging"
+  }
+}
+`, rInt, location, rInt, rInt)
+}
