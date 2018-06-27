21e920715f5964618a652ed5d80df5cff6fcbf2a
Gabe <abinante@adobe.com>
2018-06-26 23:20:10 -0700

    address test failure and feedback regarding documentation

--- azurerm/resource_arm_postgresql_server_test.go
+++ azurerm/resource_arm_postgresql_server_test.go
@@ -386,7 +386,7 @@ resource "azurerm_postgresql_server" "test" {
   }
 
   storage_profile {
-    storage_mb = 4194304
+    storage_mb = 947200
     backup_retention_days = 7
     geo_redundant_backup = "Disabled"
   }
@@ -452,7 +452,7 @@ resource "azurerm_postgresql_server" "test" {
   }
 
   storage_profile {
-    storage_mb = 1048576
+    storage_mb = 4194304
     backup_retention_days = 7
     geo_redundant_backup = "Enabled"
   }
