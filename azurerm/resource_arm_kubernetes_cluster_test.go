7453f41a627d95614508a8174bf618d77c2ea2dd
Liam Fisher <LiamFisher1@gmail.com>
2018-07-04 21:35:30 +0100

    Made address space more sensible

--- azurerm/resource_arm_kubernetes_cluster_test.go
+++ azurerm/resource_arm_kubernetes_cluster_test.go
@@ -390,7 +390,7 @@ resource "azurerm_resource_group" "test" {
 
 resource "azurerm_virtual_network" "test" {
   name                = "acctestvirtnet%d"
-  address_space       = ["172.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
 
@@ -403,7 +403,7 @@ resource "azurerm_subnet" "test" {
   name                 = "acctestsubnet%d"
   resource_group_name  = "${azurerm_resource_group.test.name}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
-  address_prefix       = "172.0.2.0/24"
+  address_prefix       = "10.1.0.0/24"
 }
 
 resource "azurerm_kubernetes_cluster" "test" {
