1959c2f435e4da978ef17e19508cc3b41493b00f
Liam <LiamFisher1@gmail.com>
2018-07-06 13:17:05 +0100

    Added ForceNew to network_profile

--- azurerm/resource_arm_kubernetes_cluster.go
+++ azurerm/resource_arm_kubernetes_cluster.go
@@ -212,6 +212,7 @@ func resourceArmKubernetesCluster() *schema.Resource {
 			"network_profile": {
 				Type:     schema.TypeList,
 				Optional: true,
+				ForceNew: true,
 				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
