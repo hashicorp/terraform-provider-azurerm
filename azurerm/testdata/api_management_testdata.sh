3f08c538021a7917ae79feb5bb00f8a72d4cff59
Jon Arild Tørresdal <jon.torresdal@spv.no>
2018-07-08 11:27:44 +0200

    Made bash script for creating certs more generic

--- azurerm/testdata/api_management_testdata.sh
+++ azurerm/testdata/api_management_testdata.sh
@@ -3,65 +3,30 @@
 # This script creates nessesary certificates to execute integration
 # tests for Azure API Management
 
-# api_management_api_test.pfx
-openssl req \
-    -newkey rsa:2048 \
-    -x509 \
-    -nodes \
-    -keyout api_management_api_test.key \
-    -new \
-    -out api_management_api_test.crt \
-    -subj /CN=api.terraform.io \
-    -sha256 \
-    -days 3650
+declare -a certs=("api_management_api" "api_management_api2" "api_management_portal")
+declare -a domains=("api.terraform.io" "api2.terraform.io" "portal.terraform.io")
 
-openssl pkcs12 \
-    -export \
-    -out api_management_api_test.pfx \
-    -inkey api_management_api_test.key \
-    -in api_management_api_test.crt \
-    -password pass:terraform
+arraylength=${#certs[@]}
 
-rm -f api_management_api_test.key api_management_api_test.crt
+for (( i=0; i<${arraylength}; i++ ));
+do
+  openssl req \
+      -newkey rsa:2048 \
+      -x509 \
+      -nodes \
+      -keyout ${certs[$i]}_test.key \
+      -new \
+      -out ${certs[$i]}_test.crt \
+      -subj /CN=${domains[$i]} \
+      -sha256 \
+      -days 3650
 
-# api_management_api2_test.pfx
-openssl req \
-    -newkey rsa:2048 \
-    -x509 \
-    -nodes \
-    -keyout api_management_api2_test.key \
-    -new \
-    -out api_management_api2_test.crt \
-    -subj /CN=api2.terraform.io \
-    -sha256 \
-    -days 3650
+  openssl pkcs12 \
+      -export \
+      -out ${certs[$i]}_test.pfx \
+      -inkey ${certs[$i]}_test.key \
+      -in ${certs[$i]}_test.crt \
+      -password pass:terraform
 
-openssl pkcs12 \
-    -export \
-    -out api_management_api2_test.pfx \
-    -inkey api_management_api2_test.key \
-    -in api_management_api2_test.crt \
-    -password pass:terraform
-
-rm -f api_management_api2_test.key api_management_api2_test.crt
-
-# api_management_portal_test.pfx
-openssl req \
-    -newkey rsa:2048 \
-    -x509 \
-    -nodes \
-    -keyout api_management_portal_test.key \
-    -new \
-    -out api_management_portal_test.crt \
-    -subj /CN=portal.terraform.io \
-    -sha256 \
-    -days 3650
-
-openssl pkcs12 \
-    -export \
-    -out api_management_portal_test.pfx \
-    -inkey api_management_portal_test.key \
-    -in api_management_portal_test.crt \
-    -password pass:terraform
-
-rm -f api_management_portal_test.key api_management_portal_test.crt
+  rm -f ${certs[$i]}_test.key ${certs[$i]}_test.crt
+done
