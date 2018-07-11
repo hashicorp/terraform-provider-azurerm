30f0dfa8c5cf89b8a10054a7f21698acb548da4a
Stavrus <1096109+Stavrus@users.noreply.github.com>
2018-07-11 16:27:50 -0400

    New Resource: `azurerm_azuread_application` (#1269)
    * Add an AD Application provider.
    
    Split from PR #961.
    
    * Rename resource to azurerm_azuread_application and update function names to match.
    
    * Address code review comments.
    
    * Remove Password/Key Credentials to turn them into resources.
    
    * Refactoring:
    
    - updating the acceptance tests to prefix the name with `acctest` so it's easily identifyable
    - wrapping the error messages to display more useful information
    - ensuring we disable apps or other tenants before deleting them
    - making available_to_other_tenants / oauth2_allow_implicit_flow not computed
    - updating the docs
    
    ```
     $ acctests azurerm TestAccAzureRMActiveDirectoryApplication_
    === RUN   TestAccAzureRMActiveDirectoryApplication_importBasic
    --- PASS: TestAccAzureRMActiveDirectoryApplication_importBasic (38.38s)
    === RUN   TestAccAzureRMActiveDirectoryApplication_importComplete
    --- PASS: TestAccAzureRMActiveDirectoryApplication_importComplete (25.42s)
    === RUN   TestAccAzureRMActiveDirectoryApplication_basic
    --- PASS: TestAccAzureRMActiveDirectoryApplication_basic (20.18s)
    === RUN   TestAccAzureRMActiveDirectoryApplication_availableToOtherTenants
    --- PASS: TestAccAzureRMActiveDirectoryApplication_availableToOtherTenants (21.60s)
    === RUN   TestAccAzureRMActiveDirectoryApplication_complete
    --- PASS: TestAccAzureRMActiveDirectoryApplication_complete (20.19s)
    === RUN   TestAccAzureRMActiveDirectoryApplication_update
    --- PASS: TestAccAzureRMActiveDirectoryApplication_update (39.92s)
    PASS
    ok  	github.com/terraform-providers/terraform-provider-azurerm/azurerm	165.735s
    ```

--- azurerm/provider.go
+++ azurerm/provider.go
@@ -116,6 +116,7 @@ func Provider() terraform.ResourceProvider {
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
+			"azurerm_azuread_application":                     resourceArmActiveDirectoryApplication(),
 			"azurerm_application_gateway":                     resourceArmApplicationGateway(),
 			"azurerm_application_insights":                    resourceArmApplicationInsights(),
 			"azurerm_application_security_group":              resourceArmApplicationSecurityGroup(),
