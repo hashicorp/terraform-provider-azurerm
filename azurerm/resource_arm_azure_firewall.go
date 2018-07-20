e442d605419b8ecf4fff505afa1131eeac5f80a1
Henry Buckle <h.buckle@outlook.com>
2018-07-20 16:50:30 +0100

    complete firewall resource

--- azurerm/resource_arm_azure_firewall.go
+++ azurerm/resource_arm_azure_firewall.go
@@ -2,17 +2,18 @@ package azurerm
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-04-01/network"
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
 func resourceArmAzureFirewall() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceArmAzureFirewallCreate,
+		Create: resourceArmAzureFirewallCreateUpdate,
 		Read:   resourceArmAzureFirewallRead,
-		Update: resourceArmAzureFirewallCreate,
+		Update: resourceArmAzureFirewallCreateUpdate,
 		Delete: resourceArmAzureFirewallDelete,
 
 		Schema: map[string]*schema.Schema{
@@ -39,15 +40,17 @@ func resourceArmAzureFirewall() *schema.Resource {
 						},
 						"private_ip_address": {
 							Type:     schema.TypeString,
-							Optional: true,
+							Computed: true,
 						},
 						"subnet_id": {
-							Type:     schema.TypeString,
-							Required: true,
+							Type:         schema.TypeString,
+							Required:     true,
+							ValidateFunc: azure.ValidateResourceID,
 						},
 						"internal_public_ip_address_id": {
-							Type:     schema.TypeString,
-							Required: true,
+							Type:         schema.TypeString,
+							Required:     true,
+							ValidateFunc: azure.ValidateResourceID,
 						},
 					},
 				},
@@ -58,16 +61,24 @@ func resourceArmAzureFirewall() *schema.Resource {
 	}
 }
 
-func resourceArmAzureFirewallCreate(d *schema.ResourceData, meta interface{}) error {
+func resourceArmAzureFirewallCreateUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*ArmClient).azureFirewallsClient
 	ctx := meta.(*ArmClient).StopContext
 
-	log.Printf("[INFO] preparing arguments for Azure Firewall creation.")
 	resourceGroup := d.Get("resource_group_name").(string)
 	name := d.Get("name").(string)
 	location := azureRMNormalizeLocation(d.Get("location").(string))
 	tags := d.Get("tags").(map[string]interface{})
-	ipConfigs := expandArmAzureFirewallIPConfigurations(d)
+	ipConfigs, subnetToLock, vnetToLock, sgErr := expandArmAzureFirewallIPConfigurations(d)
+	if sgErr != nil {
+		return fmt.Errorf("Error Building list of Azure Firewall IP Configurations: %+v", sgErr)
+	}
+
+	azureRMLockMultipleByName(subnetToLock, subnetResourceName)
+	defer azureRMUnlockMultipleByName(subnetToLock, subnetResourceName)
+
+	azureRMLockMultipleByName(vnetToLock, virtualNetworkResourceName)
+	defer azureRMUnlockMultipleByName(vnetToLock, virtualNetworkResourceName)
 
 	parameters := network.AzureFirewall{
 		Location: &location,
@@ -79,12 +90,12 @@ func resourceArmAzureFirewallCreate(d *schema.ResourceData, meta interface{}) er
 
 	future, err := client.CreateOrUpdate(ctx, resourceGroup, name, parameters)
 	if err != nil {
-		return fmt.Errorf("Error creating Azure Firewall %q (Resource Group %q): %+v", name, resourceGroup, err)
+		return fmt.Errorf("Error creating/updating Azure Firewall %q (Resource Group %q): %+v", name, resourceGroup, err)
 	}
 
 	err = future.WaitForCompletion(ctx, client.Client)
 	if err != nil {
-		return fmt.Errorf("Error waiting for creation of Azure Firewall %q (Resource Group %q): %+v", name, resourceGroup, err)
+		return fmt.Errorf("Error waiting for creation/update of Azure Firewall %q (Resource Group %q): %+v", name, resourceGroup, err)
 	}
 
 	read, err := client.Get(ctx, resourceGroup, name)
@@ -93,7 +104,7 @@ func resourceArmAzureFirewallCreate(d *schema.ResourceData, meta interface{}) er
 	}
 
 	if read.ID == nil {
-		return fmt.Errorf("Cannot read Azure Firewall %q (resource group %q) ID", name, resourceGroup)
+		return fmt.Errorf("Cannot read Azure Firewall %q (Resource Group %q) ID", name, resourceGroup)
 	}
 
 	d.SetId(*read.ID)
@@ -112,6 +123,28 @@ func resourceArmAzureFirewallRead(d *schema.ResourceData, meta interface{}) erro
 	resourceGroup := id.ResourceGroup
 	name := id.Path["azureFirewalls"]
 
+	resp, err := client.Get(ctx, resourceGroup, name)
+	if err != nil {
+		if utils.ResponseWasNotFound(resp.Response) {
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("Error making Read request on Azure Firewall %q (Resource Group %q): %+v", name, resourceGroup, err)
+	}
+
+	if resp.IPConfigurations != nil {
+		ipConfigs := flattenArmAzureFirewallIPConfigurations(resp.IPConfigurations)
+		d.Set("ip_configuration", ipConfigs)
+	}
+
+	d.Set("name", name)
+	d.Set("resource_group_name", resourceGroup)
+	if location := resp.Location; location != nil {
+		d.Set("location", azureRMNormalizeLocation(*location))
+	}
+
+	flattenAndSetTags(d, resp.Tags)
+
 	return nil
 }
 
@@ -119,29 +152,90 @@ func resourceArmAzureFirewallDelete(d *schema.ResourceData, meta interface{}) er
 	client := meta.(*ArmClient).azureFirewallsClient
 	ctx := meta.(*ArmClient).StopContext
 
-	return nil
+	id, err := parseAzureResourceID(d.Id())
+	if err != nil {
+		return err
+	}
+	resourceGroup := id.ResourceGroup
+	name := id.Path["azureFirewalls"]
+
+	configs := d.Get("ip_configuration").([]interface{})
+	subnetNamesToLock := make([]string, 0)
+	virtualNetworkNamesToLock := make([]string, 0)
+
+	for _, configRaw := range configs {
+		data := configRaw.(map[string]interface{})
+
+		subnet_id := data["subnet_id"].(string)
+		subnetID, err := parseAzureResourceID(subnet_id)
+		if err != nil {
+			return err
+		}
+		subnetName := subnetID.Path["subnets"]
+		if !sliceContainsValue(subnetNamesToLock, subnetName) {
+			subnetNamesToLock = append(subnetNamesToLock, subnetName)
+		}
+
+		virtualNetworkName := subnetID.Path["virtualNetworks"]
+		if !sliceContainsValue(virtualNetworkNamesToLock, virtualNetworkName) {
+			virtualNetworkNamesToLock = append(virtualNetworkNamesToLock, virtualNetworkName)
+		}
+	}
+
+	azureRMLockMultipleByName(&subnetNamesToLock, subnetResourceName)
+	defer azureRMUnlockMultipleByName(&subnetNamesToLock, subnetResourceName)
+
+	azureRMLockMultipleByName(&virtualNetworkNamesToLock, virtualNetworkResourceName)
+	defer azureRMUnlockMultipleByName(&virtualNetworkNamesToLock, virtualNetworkResourceName)
+
+	future, err := client.Delete(ctx, resourceGroup, name)
+	if err != nil {
+		return fmt.Errorf("Error deleting Azure Firewall %q (Resource Group %q): %+v", name, resourceGroup, err)
+	}
+
+	err = future.WaitForCompletion(ctx, client.Client)
+	if err != nil {
+		return fmt.Errorf("Error waiting for the deletion of Azure Firewall %q (Resource Group %q): %+v", name, resourceGroup, err)
+	}
+
+	return err
 }
 
-func expandArmAzureFirewallIPConfigurations(d *schema.ResourceData) []network.AzureFirewallIPConfiguration {
+func expandArmAzureFirewallIPConfigurations(d *schema.ResourceData) ([]network.AzureFirewallIPConfiguration, *[]string, *[]string, error) {
 	configs := d.Get("ip_configuration").([]interface{})
 	ipConfigs := make([]network.AzureFirewallIPConfiguration, 0, len(configs))
+	subnetNamesToLock := make([]string, 0)
+	virtualNetworkNamesToLock := make([]string, 0)
+
 	for _, configRaw := range configs {
 		data := configRaw.(map[string]interface{})
 		name := data["name"].(string)
-		subnetID := data["subnet_id"].(string)
-		intPubID := data["private_ip_address"].(string)
+		subnet_id := data["subnet_id"].(string)
+		intPubID := data["internal_public_ip_address_id"].(string)
 
 		properties := network.AzureFirewallIPConfigurationPropertiesFormat{
 			Subnet: &network.SubResource{
-				ID: &subnetID,
+				ID: &subnet_id,
 			},
 			InternalPublicIPAddress: &network.SubResource{
 				ID: &intPubID,
 			},
 		}
 
-		if v := data["private_ip_address"].(string); v != "" {
-			properties.PrivateIPAddress = &v
+		subnetID, err := parseAzureResourceID(subnet_id)
+		if err != nil {
+			return []network.AzureFirewallIPConfiguration{}, nil, nil, err
+		}
+
+		subnetName := subnetID.Path["subnets"]
+		virtualNetworkName := subnetID.Path["virtualNetworks"]
+
+		if !sliceContainsValue(subnetNamesToLock, subnetName) {
+			subnetNamesToLock = append(subnetNamesToLock, subnetName)
+		}
+
+		if !sliceContainsValue(virtualNetworkNamesToLock, virtualNetworkName) {
+			virtualNetworkNamesToLock = append(virtualNetworkNamesToLock, virtualNetworkName)
 		}
 
 		ipConfig := network.AzureFirewallIPConfiguration{
@@ -150,5 +244,21 @@ func expandArmAzureFirewallIPConfigurations(d *schema.ResourceData) []network.Az
 		}
 		ipConfigs = append(ipConfigs, ipConfig)
 	}
-	return ipConfigs
+	return ipConfigs, &subnetNamesToLock, &virtualNetworkNamesToLock, nil
+}
+
+func flattenArmAzureFirewallIPConfigurations(ipConfigs *[]network.AzureFirewallIPConfiguration) []interface{} {
+	result := make([]interface{}, 0, len(*ipConfigs))
+	for _, ipConfig := range *ipConfigs {
+		afIPConfig := make(map[string]interface{})
+		props := ipConfig.AzureFirewallIPConfigurationPropertiesFormat
+
+		afIPConfig["name"] = *ipConfig.Name
+		afIPConfig["subnet_id"] = *props.Subnet.ID
+		afIPConfig["private_ip_address"] = *props.PrivateIPAddress
+		afIPConfig["internal_public_ip_address_id"] = *props.PublicIPAddress.ID
+		result = append(result, afIPConfig)
+	}
+
+	return result
 }
--- azurerm/resource_arm_azure_firewall_test.go
+++ azurerm/resource_arm_azure_firewall_test.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
+
 	"github.com/hashicorp/terraform/helper/acctest"
 )
 
@@ -11,6 +15,76 @@ func TestAccAzureRMAzureFirewall_basic(t *testing.T) {
 	resourceName := "azurerm_azure_firewall.test"
 	ri := acctest.RandInt()
 	config := testAccAzureRMAzureFirewall_basic(ri, testLocation())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMAzureFirewallDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMAzureFirewallExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "ip_configuration.0.name", "configuration"),
+					resource.TestCheckResourceAttrSet(resourceName, "ip_configuration.0.name", "private_ip_address"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureRMAzureFirewall_withTags(t * testing.T) {
+	resourceName := "azurerm_azure_firewall.test"
+	ri := acctest.RandInt()
+	preConfig := testAccAzureRMAzureFirewall_withTags(ri, testLocation())
+	postConfig := testAccAzureRMAzureFirewall_withUpdatedTags(ri, testLocation())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMAzureFirewallDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: preConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMAzureFirewallExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.environment", "Production"),
+					resource.TestCheckResourceAttr(resourceName, "tags.cost_center", "MSFT"),
+				),
+			},
+			{
+				Config: postConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMAzureFirewallExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.environment", "staging"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureRMAzureFirewall_disappears(t *testing.T) {
+	resourceName := "azurerm_azure_firewall.test"
+	ri := acctest.RandInt()
+	config:= testAccAzureRMAzureFirewall_basic(ri, testLocation())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMAzureFirewallDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check:  resource.ComposeTestCheckFunc(
+					testCheckAzureRMAzureFirewallExists(resourceName),
+					testCheckAzureRMAzureFirewallDisappears(resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
 }
 
 func testAccAzureRMAzureFirewall_basic(rInt int, location string) string {
@@ -49,4 +123,167 @@ resource "azurerm_azure_firewall" "test" {
   }
 }
 `, rInt, location, rInt, rInt, rInt)
+}
+
+func testAccAzureRMAzureFirewall_withTags(rInt int, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+resource "azurerm_virtual_network" "test" {
+  name                = "acctestvirtnet%d"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+resource "azurerm_subnet" "test" {
+  name                      = "AzureFirewallSubnet"
+  resource_group_name       = "${azurerm_resource_group.test.name}"
+  virtual_network_name      = "${azurerm_virtual_network.test.name}"
+  address_prefix            = "10.0.1.0/24"
+}
+resource "azurerm_public_ip" "test" {
+  name                         = "acctestpip%d"
+  location                     = "${azurerm_resource_group.test.location}"
+  resource_group_name          = "${azurerm_resource_group.test.name}"
+  public_ip_address_allocation = "Static"
+  sku                          = "Standard"
+}
+resource "azurerm_azure_firewall" "test" {
+  name = "acctestfirewall%d"
+  location                     = "${azurerm_resource_group.test.location}"
+  resource_group_name          = "${azurerm_resource_group.test.name}"
+  ip_configuration {
+    name                          = "configuration"
+    subnet_id                     = "${azurerm_subnet.test.id}"
+    internal_public_ip_address_id = "${azurerm_public_ip.test.id}"
+  }
+  tags {
+    environment = "Production"
+    cost_center = "MSFT"
+  }
+}
+`, rInt, location, rInt, rInt, rInt)
+}
+
+func testAccAzureRMAzureFirewall_withUpdatedTags(rInt int, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+resource "azurerm_virtual_network" "test" {
+  name                = "acctestvirtnet%d"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+resource "azurerm_subnet" "test" {
+  name                      = "AzureFirewallSubnet"
+  resource_group_name       = "${azurerm_resource_group.test.name}"
+  virtual_network_name      = "${azurerm_virtual_network.test.name}"
+  address_prefix            = "10.0.1.0/24"
+}
+resource "azurerm_public_ip" "test" {
+  name                         = "acctestpip%d"
+  location                     = "${azurerm_resource_group.test.location}"
+  resource_group_name          = "${azurerm_resource_group.test.name}"
+  public_ip_address_allocation = "Static"
+  sku                          = "Standard"
+}
+resource "azurerm_azure_firewall" "test" {
+  name = "acctestfirewall%d"
+  location                     = "${azurerm_resource_group.test.location}"
+  resource_group_name          = "${azurerm_resource_group.test.name}"
+  ip_configuration {
+    name                          = "configuration"
+    subnet_id                     = "${azurerm_subnet.test.id}"
+    internal_public_ip_address_id = "${azurerm_public_ip.test.id}"
+  }
+  tags {
+    environment = "staging"
+  }
+}
+`, rInt, location, rInt, rInt, rInt)
+}
+
+func testCheckAzureRMAzureFirewallExists(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		// Ensure we have enough information in state to look up in API
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		name := rs.Primary.Attributes["name"]
+		resourceGroup, hasResourceGroup := rs.Primary.Attributes["resource_group_name"]
+		if !hasResourceGroup {
+			return fmt.Errorf("Bad: no resource group found in state for Azure Firewall: %q", name)
+		}
+
+		client := testAccProvider.Meta().(*ArmClient).azureFirewallsClient
+		ctx := testAccProvider.Meta().(*ArmClient).StopContext
+		resp, err := client.Get(ctx, resourceGroup, name)
+		if err != nil {
+			if utils.ResponseWasNotFound(resp.Response) {
+				return fmt.Errorf("Bad: Azure Firewall %q (Resource Group: %q) does not exist", name, resourceGroup)
+			}
+
+			return fmt.Errorf("Bad: Get on azureFirewallsClient: %+v", err)
+		}
+
+		return nil
+	}
+}
+
+func testCheckAzureRMAzureFirewallDisappears(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		// Ensure we have enough information in state to look up in API
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		name := rs.Primary.Attributes["name"]
+		resourceGroup, hasResourceGroup := rs.Primary.Attributes["resource_group_name"]
+		if !hasResourceGroup {
+			return fmt.Errorf("Bad: no resource group found in state for Azure Firewall: %q", name)
+		}
+
+		client := testAccProvider.Meta().(*ArmClient).azureFirewallsClient
+		ctx := testAccProvider.Meta().(*ArmClient).StopContext
+		resp, err := cli4ent.Delete(ctx, resourceGroup, name)
+		if err != nil {
+			if !utils.ResponseWasNotFound(resp.Response) {
+				return fmt.Errorf("Bad: Delete on azureFirewallsClient: %+v", err)
+			}
+		}
+
+		return nil
+	}
+}
+
+func testCheckAzureRMAzureFirewallDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "azurerm_azure_firewall" {
+			continue
+		}
+
+		name := rs.Primary.Attributes["name"]
+		resourceGroup := rs.Primary.Attributes["resource_group_name"]
+
+		resp, err := client.Get(ctx, resourceGroup, name)
+		if err != nil {
+			if utils.ResponseWasNotFound(resp.Response) {
+				return nil
+			}
+
+			return err
+		}
+
+		return fmt.Errorf("Azure Firewall still exists:\n%#v", resp.InterfacePropertiesFormat)
+	}
+
+	return nil
 }
