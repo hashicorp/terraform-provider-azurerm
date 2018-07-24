5fe1348aadf9777f86f032d019db6272fc146374
kt <kt@katbyte.me>
2018-07-24 09:40:28 -0700

    azurerm_servicebus_topic name now allows ~s

--- azurerm/helpers/azure/servicebus.go
+++ azurerm/helpers/azure/servicebus.go
@@ -35,7 +35,7 @@ func ValidateServiceBusSubscriptionName() schema.SchemaValidateFunc {
 
 func ValidateServiceBusTopicName() schema.SchemaValidateFunc {
 	return validation.StringMatch(
-		regexp.MustCompile("^[a-zA-Z][-._a-zA-Z0-9]{0,258}([a-zA-Z0-9])?$"),
+		regexp.MustCompile("^[a-zA-Z][-._~a-zA-Z0-9]{0,258}([a-zA-Z0-9])?$"),
 		"The topic name can contain only letters, numbers, periods, hyphens and underscores. The namespace must start with a letter, and it must end with a letter or number and be less then 260 characters long.",
 	)
 }
