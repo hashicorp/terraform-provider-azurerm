512c1049c6b2fa5d94115c0c5b1fe0ffb096ce33
Sebastian Choren <sebastianchoren@gmail.com>
2018-07-25 00:09:40 -0300

    Add 'azurerm_container_image' data source

---
+++ azurerm/data_source_arm_container_registry_test.go
@@ -0,0 +1,46 @@
+package azurerm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccDataSourceAzureRMContainerRegistry_basic(t *testing.T) {
+	ri := acctest.RandInt()
+	config := testAccDataSourceAzureRMContainerRegistry_basic(ri)
+
+	acrName := "azurerm_container_registry.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMContainerRegistryDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(acrName, "name"),
+					resource.TestCheckResourceAttrSet(acrName, "resource_group_name"),
+					resource.TestCheckResourceAttrSet(acrName, "location"),
+					resource.TestCheckResourceAttrSet(acrName, "admin_enabled"),
+					resource.TestCheckResourceAttrSet(acrName, "login_server"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceAzureRMContainerRegistry_basic(rInt int) string {
+	resource := testAccAzureRMContainerRegistry_basicManaged(rInt, testLocation(), "Basic")
+	return fmt.Sprintf(`
+%s
+
+data "azurerm_container_registry" "test" {
+  name                = "testacccr%d"
+  resource_group_name = "testAccRg-%d"
+}
+`, resource, rInt, rInt)
+}
