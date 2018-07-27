2157381459fef9d6e94b646cb9f3b5975ec423c6
tombuildsstuff <git@ibuildstuff.co.uk>
2018-07-27 18:58:45 +0200

    Correctly documenting the delete methods

--- examples/virtual-machines/provisioners/linux/2-virtual-machine.tf
+++ examples/virtual-machines/provisioners/linux/2-virtual-machine.tf
@@ -5,7 +5,12 @@ resource "azurerm_virtual_machine" "example" {
   network_interface_ids = ["${azurerm_network_interface.main.id}"]
   vm_size               = "Standard_F2"
 
-  delete_os_disk_on_termination    = true
+  # This means the OS Disk will be deleted when Terraform destroys the Virtual Machine
+  # NOTE: This may not be optimal in all cases.
+  delete_os_disk_on_termination = true
+
+  # This means the Data Disk Disk will be deleted when Terraform destroys the Virtual Machine
+  # NOTE: This may not be optimal in all cases.
   delete_data_disks_on_termination = true
 
   storage_image_reference {
