cbc01aa626216ab635f52fe63303cdb6242b857a
tombuildsstuff <git@ibuildstuff.co.uk>
2018-07-30 09:08:10 +0200

    Switching over to Standard_F2's for the examples

--- examples/virtual-machines/managed-disks/basic-osdisk/2-virtual-machine.tf
+++ examples/virtual-machines/managed-disks/basic-osdisk/2-virtual-machine.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_machine" "test" {
   location              = "${azurerm_resource_group.main.location}"
   resource_group_name   = "${azurerm_resource_group.main.name}"
   network_interface_ids = ["${azurerm_network_interface.main.id}"]
-  vm_size               = "Standard_DS1_v2"
+  vm_size               = "Standard_F2"
 
   # This means the OS Disk will be deleted when Terraform destroys the Virtual Machine
   # NOTE: This may not be optimal in all cases.
