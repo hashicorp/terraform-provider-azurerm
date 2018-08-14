92479476a565fa52aa4cf188af44f30ffa1c8aab
tombuildsstuff <git@ibuildstuff.co.uk>
2018-08-14 13:51:05 +0200

    DNS CAA: requiring import/timeouts

--- azurerm/resource_arm_dns_caa_record.go
+++ azurerm/resource_arm_dns_caa_record.go
@@ -2,13 +2,16 @@ package azurerm
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/preview/dns/mgmt/2018-03-01-preview/dns"
 	"github.com/hashicorp/terraform/helper/hashcode"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
@@ -20,6 +23,11 @@ func resourceArmDnsCaaRecord() *schema.Resource {
 		Delete: resourceArmDnsCaaRecordDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(time.Minute * 10),
+			Update: schema.DefaultTimeout(time.Minute * 10),
+			Delete: schema.DefaultTimeout(time.Minute * 10),
 		},
 
 		Schema: map[string]*schema.Schema{
@@ -83,6 +91,21 @@ func resourceArmDnsCaaRecordCreateOrUpdate(d *schema.ResourceData, meta interfac
 	name := d.Get("name").(string)
 	resGroup := d.Get("resource_group_name").(string)
 	zoneName := d.Get("zone_name").(string)
+
+	if d.IsNewResource() {
+		// first check if there's one in this subscription requiring import
+		resp, err := client.Get(ctx, resGroup, zoneName, name, dns.CAA)
+		if err != nil {
+			if !utils.ResponseWasNotFound(resp.Response) {
+				return fmt.Errorf("Error checking for the existence of DNS CAA Record %q (Zone %q / Resource Group %q): %+v", name, zoneName, resGroup, err)
+			}
+		}
+
+		if resp.ID != nil {
+			return tf.ImportAsExistsError("azurerm_dns_caa_record", *resp.ID)
+		}
+	}
+
 	ttl := int64(d.Get("ttl").(int))
 	tags := d.Get("tags").(map[string]interface{})
 
@@ -102,7 +125,9 @@ func resourceArmDnsCaaRecordCreateOrUpdate(d *schema.ResourceData, meta interfac
 
 	eTag := ""
 	ifNoneMatch := "" // set to empty to allow updates to records after creation
-	resp, err := client.CreateOrUpdate(ctx, resGroup, zoneName, name, dns.CAA, parameters, eTag, ifNoneMatch)
+	waitCtx, cancel := context.WithTimeout(ctx, d.Timeout(tf.TimeoutForCreateUpdate(d)))
+	defer cancel()
+	resp, err := client.CreateOrUpdate(waitCtx, resGroup, zoneName, name, dns.CAA, parameters, eTag, ifNoneMatch)
 	if err != nil {
 		return err
 	}
@@ -164,7 +189,9 @@ func resourceArmDnsCaaRecordDelete(d *schema.ResourceData, meta interface{}) err
 	name := id.Path["CAA"]
 	zoneName := id.Path["dnszones"]
 
-	resp, error := client.Delete(ctx, resGroup, zoneName, name, dns.CAA, "")
+	waitCtx, cancel := context.WithTimeout(ctx, d.Timeout(schema.TimeoutDelete))
+	defer cancel()
+	resp, error := client.Delete(waitCtx, resGroup, zoneName, name, dns.CAA, "")
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("Error deleting DNS CAA Record %s: %+v", name, error)
 	}
@@ -173,7 +200,7 @@ func resourceArmDnsCaaRecordDelete(d *schema.ResourceData, meta interface{}) err
 }
 
 func flattenAzureRmDnsCaaRecords(records *[]dns.CaaRecord) []map[string]interface{} {
-	results := make([]map[string]interface{}, 0, len(*records))
+	results := make([]map[string]interface{}, 0)
 
 	if records != nil {
 		for _, record := range *records {
@@ -190,9 +217,9 @@ func flattenAzureRmDnsCaaRecords(records *[]dns.CaaRecord) []map[string]interfac
 
 func expandAzureRmDnsCaaRecords(d *schema.ResourceData) ([]dns.CaaRecord, error) {
 	recordStrings := d.Get("record").(*schema.Set).List()
-	records := make([]dns.CaaRecord, len(recordStrings))
+	records := make([]dns.CaaRecord, 0)
 
-	for i, v := range recordStrings {
+	for _, v := range recordStrings {
 		record := v.(map[string]interface{})
 		flags := int32(record["flags"].(int))
 		tag := record["tag"].(string)
@@ -204,7 +231,7 @@ func expandAzureRmDnsCaaRecords(d *schema.ResourceData) ([]dns.CaaRecord, error)
 			Value: &value,
 		}
 
-		records[i] = caaRecord
+		records = append(records, caaRecord)
 	}
 
 	return records, nil
