5e640de41a885c5b7e478ad1461fabe7fb1e6407
Alexander Stuart-Kregor <alexander.stuart-kregor@arnoldclark.com>
2018-08-15 11:41:36 +0100

    Bring across postgresql_server resource from postgresql_server_test.

--- azurerm/resource_arm_postgresql_virtual_network_rule_test.go
+++ azurerm/resource_arm_postgresql_virtual_network_rule_test.go
@@ -440,21 +440,33 @@ resource "azurerm_subnet" "test" {
   service_endpoints = ["Microsoft.Sql"]
 }
 resource "azurerm_postgresql_server" "test" {
-  name = "acctestsqlserver%d"
+  name                = "acctestpsqlsvr-%d"
+  location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  location = "${azurerm_resource_group.test.location}"
-  version = "12.0"
-  administrator_login = "missadmin"
-  administrator_login_password = "${md5(%d)}!"
+  sku {
+    name     = "B_Gen4_2"
+    capacity = 2
+    tier     = "Basic"
+    family   = "Gen4"
+  }
+  storage_profile {
+    storage_mb            = 51200
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
+  administrator_login          = "acctestun"
+  administrator_login_password = "H@Sh1CoR3!"
+  version                      = "9.5"
+  ssl_enforcement              = "Enabled"
 }
 resource "azurerm_postgresql_virtual_network_rule" "test" {
   name = "acctestsqlvnetrule%d"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  server_name = "${azurerm_sql_server.test.name}"
+  server_name = "${azurerm_postgresql_server.test.name}"
   subnet_id = "${azurerm_subnet.test.id}"
   ignore_missing_vnet_service_endpoint = false
 }
-`, rInt, location, rInt, rInt, rInt, rInt, rInt)
+`, rInt, location, rInt, rInt, rInt, rInt)
 }
 
 /*
@@ -482,21 +494,33 @@ resource "azurerm_subnet" "test" {
   service_endpoints = ["Microsoft.Sql"]
 }
 resource "azurerm_postgresql_server" "test" {
-  name = "acctestsqlserver%d"
+  name                = "acctestpsqlsvr-%d"
+  location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  location = "${azurerm_resource_group.test.location}"
-  version = "12.0"
-  administrator_login = "missadmin"
-  administrator_login_password = "${md5(%d)}!"
+  sku {
+    name     = "B_Gen4_2"
+    capacity = 2
+    tier     = "Basic"
+    family   = "Gen4"
+  }
+  storage_profile {
+    storage_mb            = 51200
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
+  administrator_login          = "acctestun"
+  administrator_login_password = "H@Sh1CoR3!"
+  version                      = "9.5"
+  ssl_enforcement              = "Enabled"
 }
 resource "azurerm_postgresql_virtual_network_rule" "test" {
   name = "acctestsqlvnetrule%d"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  server_name = "${azurerm_sql_server.test.name}"
+  server_name = "${azurerm_postgresql_server.test.name}"
   subnet_id = "${azurerm_subnet.test.id}"
   ignore_missing_vnet_service_endpoint = true
 }
-`, rInt, location, rInt, rInt, rInt, rInt, rInt)
+`, rInt, location, rInt, rInt, rInt, rInt)
 }
 
 /*
@@ -531,20 +555,32 @@ resource "azurerm_subnet" "test2" {
   service_endpoints = ["Microsoft.Sql"]
 }
 resource "azurerm_postgresql_server" "test" {
-  name = "acctestsqlserver%d"
+  name                = "acctestpsqlsvr-%d"
+  location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  location = "${azurerm_resource_group.test.location}"
-  version = "12.0"
-  administrator_login = "missadmin"
-  administrator_login_password = "${md5(%d)}!"
+  sku {
+    name     = "B_Gen4_2"
+    capacity = 2
+    tier     = "Basic"
+    family   = "Gen4"
+  }
+  storage_profile {
+    storage_mb            = 51200
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
+  administrator_login          = "acctestun"
+  administrator_login_password = "H@Sh1CoR3!"
+  version                      = "9.5"
+  ssl_enforcement              = "Enabled"
 }
 resource "azurerm_postgresql_virtual_network_rule" "test" {
   name = "acctestsqlvnetrule%d"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  server_name = "${azurerm_sql_server.test.name}"
+  server_name = "${azurerm_postgresql_server.test.name}"
   subnet_id = "${azurerm_subnet.test1.id}"
 }
-`, rInt, location, rInt, rInt, rInt, rInt, rInt, rInt)
+`, rInt, location, rInt, rInt, rInt, rInt, rInt)
 }
 
 /*
@@ -580,20 +616,32 @@ resource "azurerm_subnet" "test2" {
   service_endpoints = ["Microsoft.Sql"]
 }
 resource "azurerm_postgresql_server" "test" {
-  name = "acctestsqlserver%d"
+  name                = "acctestpsqlsvr-%d"
+  location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  location = "${azurerm_resource_group.test.location}"
-  version = "12.0"
-  administrator_login = "missadmin"
-  administrator_login_password = "${md5(%d)}!"
+  sku {
+    name     = "B_Gen4_2"
+    capacity = 2
+    tier     = "Basic"
+    family   = "Gen4"
+  }
+  storage_profile {
+    storage_mb            = 51200
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
+  administrator_login          = "acctestun"
+  administrator_login_password = "H@Sh1CoR3!"
+  version                      = "9.5"
+  ssl_enforcement              = "Enabled"
 }
 resource "azurerm_postgresql_virtual_network_rule" "test" {
   name = "acctestsqlvnetrule%d"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  server_name = "${azurerm_sql_server.test.name}"
+  server_name = "${azurerm_postgresql_server.test.name}"
   subnet_id = "${azurerm_subnet.test2.id}"
 }
-`, rInt, location, rInt, rInt, rInt, rInt, rInt, rInt)
+`, rInt, location, rInt, rInt, rInt, rInt, rInt)
 }
 
 /*
@@ -622,21 +670,33 @@ resource "azurerm_subnet" "test" {
   service_endpoints = ["Microsoft.Storage"]
 }
 resource "azurerm_postgresql_server" "test" {
-  name = "acctestsqlserver%d"
+  name                = "acctestpsqlsvr-%d"
+  location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  location = "${azurerm_resource_group.test.location}"
-  version = "12.0"
-  administrator_login = "missadmin"
-  administrator_login_password = "${md5(%d)}!"
+  sku {
+    name     = "B_Gen4_2"
+    capacity = 2
+    tier     = "Basic"
+    family   = "Gen4"
+  }
+  storage_profile {
+    storage_mb            = 51200
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
+  administrator_login          = "acctestun"
+  administrator_login_password = "H@Sh1CoR3!"
+  version                      = "9.5"
+  ssl_enforcement              = "Enabled"
 }
 resource "azurerm_postgresql_virtual_network_rule" "test" {
   name = "acctestsqlvnetrule%d"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  server_name = "${azurerm_sql_server.test.name}"
+  server_name = "${azurerm_postgresql_server.test.name}"
   subnet_id = "${azurerm_subnet.test.id}"
   ignore_missing_vnet_service_endpoint = true
 }
-`, rInt, location, rInt, rInt, rInt, rInt, rInt)
+`, rInt, location, rInt, rInt, rInt, rInt)
 }
 
 /*
@@ -665,21 +725,33 @@ resource "azurerm_subnet" "test" {
   service_endpoints = ["Microsoft.Storage"]
 }
 resource "azurerm_postgresql_server" "test" {
-  name = "acctestsqlserver%d"
+  name                = "acctestpsqlsvr-%d"
+  location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  location = "${azurerm_resource_group.test.location}"
-  version = "12.0"
-  administrator_login = "missadmin"
-  administrator_login_password = "${md5(%d)}!"
+  sku {
+    name     = "B_Gen4_2"
+    capacity = 2
+    tier     = "Basic"
+    family   = "Gen4"
+  }
+  storage_profile {
+    storage_mb            = 51200
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
+  administrator_login          = "acctestun"
+  administrator_login_password = "H@Sh1CoR3!"
+  version                      = "9.5"
+  ssl_enforcement              = "Enabled"
 }
 resource "azurerm_postgresql_virtual_network_rule" "test" {
   name = "acctestsqlvnetrule%d"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  server_name = "${azurerm_sql_server.test.name}"
+  server_name = "${azurerm_postgresql_server.test.name}"
   subnet_id = "${azurerm_subnet.test.id}"
   ignore_missing_vnet_service_endpoint = false
 }
-`, rInt, location, rInt, rInt, rInt, rInt, rInt)
+`, rInt, location, rInt, rInt, rInt, rInt)
 }
 
 /*
@@ -727,33 +799,45 @@ resource "azurerm_subnet" "vnet2_subnet1" {
   service_endpoints = ["Microsoft.Sql"]
 }
 resource "azurerm_postgresql_server" "test" {
-  name = "acctestsqlserver1%d"
+  name                = "acctestpsqlsvr-%d"
+  location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  location = "${azurerm_resource_group.test.location}"
-  version = "12.0"
-  administrator_login = "missadmin"
-  administrator_login_password = "${md5(%d)}!"
+  sku {
+    name     = "B_Gen4_2"
+    capacity = 2
+    tier     = "Basic"
+    family   = "Gen4"
+  }
+  storage_profile {
+    storage_mb            = 51200
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
+  administrator_login          = "acctestun"
+  administrator_login_password = "H@Sh1CoR3!"
+  version                      = "9.5"
+  ssl_enforcement              = "Enabled"
 }
 resource "azurerm_postgresql_virtual_network_rule" "rule1" {
   name = "acctestsqlvnetrule1%d"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  server_name = "${azurerm_sql_server.test.name}"
+  server_name = "${azurerm_postgresql_server.test.name}"
   subnet_id = "${azurerm_subnet.vnet1_subnet1.id}"
   ignore_missing_vnet_service_endpoint = false
 }
 resource "azurerm_postgresql_virtual_network_rule" "rule2" {
   name = "acctestsqlvnetrule2%d"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  server_name = "${azurerm_sql_server.test.name}"
+  server_name = "${azurerm_postgresql_server.test.name}"
   subnet_id = "${azurerm_subnet.vnet1_subnet2.id}"
   ignore_missing_vnet_service_endpoint = false
 }
 resource "azurerm_postgresql_virtual_network_rule" "rule3" {
   name = "acctestsqlvnetrule3%d"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  server_name = "${azurerm_sql_server.test.name}"
+  server_name = "${azurerm_postgresql_server.test.name}"
   subnet_id = "${azurerm_subnet.vnet2_subnet1.id}"
   ignore_missing_vnet_service_endpoint = false
 }
-`, rInt, location, rInt, rInt, rInt, rInt, rInt, rInt, rInt, rInt, rInt, rInt)
+`, rInt, location, rInt, rInt, rInt, rInt, rInt, rInt, rInt, rInt, rInt)
 }
