fb04b3220e67fa2207244ff343d7e9f212c11567
Jeffrey Cline <jcline@microsoft.com>
2018-09-11 12:50:22 -0700

    Updated test SKU

--- azurerm/resource_arm_mysql_virtual_network_rule_test.go
+++ azurerm/resource_arm_mysql_virtual_network_rule_test.go
@@ -238,10 +238,10 @@ resource "azurerm_mysql_server" "test" {
   ssl_enforcement              = "Enabled"
 
   sku {
-    name     = "B_Gen4_2"
+    name     = "GP_Gen5_2"
     capacity = 2
-    tier     = "Basic"
-    family   = "Gen4"
+    tier     = "GeneralPurpose"
+    family   = "Gen5"
   }
 
   storage_profile {
@@ -300,10 +300,10 @@ resource "azurerm_mysql_server" "test" {
   ssl_enforcement              = "Enabled"
 
   sku {
-    name     = "B_Gen4_2"
+    name     = "GP_Gen5_2"
     capacity = 2
-    tier     = "Basic"
-    family   = "Gen4"
+    tier     = "GeneralPurpose"
+    family   = "Gen5"
   }
 
   storage_profile {
@@ -362,10 +362,10 @@ resource "azurerm_mysql_server" "test" {
   ssl_enforcement              = "Enabled"
 
   sku {
-    name     = "B_Gen4_2"
+    name     = "GP_Gen5_2"
     capacity = 2
-    tier     = "Basic"
-    family   = "Gen4"
+    tier     = "GeneralPurpose"
+    family   = "Gen5"
   }
 
   storage_profile {
@@ -439,10 +439,10 @@ resource "azurerm_mysql_server" "test" {
   ssl_enforcement              = "Enabled"
 
   sku {
-    name     = "B_Gen4_2"
+    name     = "GP_Gen5_2"
     capacity = 2
-    tier     = "Basic"
-    family   = "Gen4"
+    tier     = "GeneralPurpose"
+    family   = "Gen5"
   }
 
   storage_profile {
