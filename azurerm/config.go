e0cfc3737de7d25ad7ccdbe321a295c96c4985cf
Tom Harvey <tombuildsstuff@users.noreply.github.com>
2018-09-12 16:55:04 +0200

    New Resource: `azurerm_mysql_virtual_network_rule` (#1879)
    * New Resource: `azurerm_mysql_virtual_network_rule`
    
    Fixes #1340
    
    * Updated test SKU
    
    * Added mysqlVirtualNetworkRulesClient back into config
    
    * Documenting the MySQL/PostgreSQL rules need the GeneralPurpose/MemoryOptimized tiers

--- azurerm/config.go
+++ azurerm/config.go
@@ -141,6 +141,7 @@ type ArmClient struct {
 	mysqlDatabasesClient                     mysql.DatabasesClient
 	mysqlFirewallRulesClient                 mysql.FirewallRulesClient
 	mysqlServersClient                       mysql.ServersClient
+	mysqlVirtualNetworkRulesClient           mysql.VirtualNetworkRulesClient
 	postgresqlConfigurationsClient           postgresql.ConfigurationsClient
 	postgresqlDatabasesClient                postgresql.DatabasesClient
 	postgresqlFirewallRulesClient            postgresql.FirewallRulesClient
@@ -601,6 +602,10 @@ func (c *ArmClient) registerDatabases(endpoint, subscriptionId string, auth auto
 	mysqlServersClient := mysql.NewServersClientWithBaseURI(endpoint, subscriptionId)
 	c.configureClient(&mysqlServersClient.Client, auth)
 	c.mysqlServersClient = mysqlServersClient
+
+	mysqlVirtualNetworkRulesClient := mysql.NewVirtualNetworkRulesClientWithBaseURI(endpoint, subscriptionId)
+	c.configureClient(&mysqlVirtualNetworkRulesClient.Client, auth)
+	c.mysqlVirtualNetworkRulesClient = mysqlVirtualNetworkRulesClient
 
 	// PostgreSQL
 	postgresqlConfigClient := postgresql.NewConfigurationsClientWithBaseURI(endpoint, subscriptionId)
