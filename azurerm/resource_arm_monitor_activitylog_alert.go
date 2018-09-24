cadf3c033416516ea908f1435ab8d8b3b7ce138d
Junyi Yi <junyi@microsoft.com>
2018-09-24 14:04:20 -0700

    Implement skeleton of create/update and read operations of activity log alert

--- azurerm/resource_arm_monitor_activitylog_alert.go
+++ azurerm/resource_arm_monitor_activitylog_alert.go
@@ -3,10 +3,12 @@ package azurerm
 import (
 	"fmt"
 
+	"github.com/Azure/azure-sdk-for-go/services/preview/monitor/mgmt/2018-03-01/insights"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/response"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
 func resourceArmMonitorActivityLogAlert() *schema.Resource {
@@ -109,9 +111,8 @@ func resourceArmMonitorActivityLogAlert() *schema.Resource {
 			},
 
 			"description": {
-				Type:         schema.TypeString,
-				Optional:     true,
-				ValidateFunc: validation.NoZeroValues,
+				Type:     schema.TypeString,
+				Optional: true,
 			},
 
 			"enabled": {
@@ -126,10 +127,63 @@ func resourceArmMonitorActivityLogAlert() *schema.Resource {
 }
 
 func resourceArmMonitorActivityLogAlertCreateOrUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ArmClient).monitorActivityLogAlertsClient
+	ctx := meta.(*ArmClient).StopContext
+
+	name := d.Get("name").(string)
+	resGroup := d.Get("resource_group_name").(string)
+
+	tags := d.Get("tags").(map[string]interface{})
+	expandedTags := expandTags(tags)
+
+	parameters := insights.ActivityLogAlertResource{
+		Location:         utils.String(azureRMNormalizeLocation("Global")),
+		ActivityLogAlert: &insights.ActivityLogAlert{},
+		Tags:             expandedTags,
+	}
+
+	if _, err := client.CreateOrUpdate(ctx, resGroup, name, parameters); err != nil {
+		return fmt.Errorf("Error creating or updating activity log alert %q (resource group %q): %+v", name, resGroup, err)
+	}
+
+	read, err := client.Get(ctx, resGroup, name)
+	if err != nil {
+		return err
+	}
+	if read.ID == nil {
+		return fmt.Errorf("Activity log alert %q (resource group %q) ID is empty", name, resGroup)
+	}
+	d.SetId(*read.ID)
+
 	return resourceArmMonitorActivityLogAlertRead(d, meta)
 }
 
 func resourceArmMonitorActivityLogAlertRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ArmClient).monitorActivityLogAlertsClient
+	ctx := meta.(*ArmClient).StopContext
+
+	id, err := parseAzureResourceID(d.Id())
+	if err != nil {
+		return err
+	}
+	resGroup := id.ResourceGroup
+	name := id.Path["activityLogAlerts"]
+
+	resp, err := client.Get(ctx, resGroup, name)
+	if err != nil {
+		if response.WasNotFound(resp.Response.Response) {
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("Error getting activity log alert %q (resource group %q): %+v", name, resGroup, err)
+	}
+
+	d.Set("name", name)
+	d.Set("resource_group_name", resGroup)
+	if alert := resp.ActivityLogAlert; alert != nil {
+	}
+	flattenAndSetTags(d, resp.Tags)
+
 	return nil
 }
 
