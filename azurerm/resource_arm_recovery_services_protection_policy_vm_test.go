8093d5b185ff5eb54ebdb42492e9edbd67c4b97b
kt <kt@katbyte.me>
2018-10-18 09:53:07 -0700

    Fix out of order import

--- azurerm/resource_arm_recovery_services_protection_policy_vm_test.go
+++ azurerm/resource_arm_recovery_services_protection_policy_vm_test.go
@@ -2,13 +2,13 @@ package azurerm
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
-	"strconv"
 )
 
 func TestAccAzureRMRecoveryServicesProtectionPolicyVm_basicDaily(t *testing.T) {
