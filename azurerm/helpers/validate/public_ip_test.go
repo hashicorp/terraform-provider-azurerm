b6569deb4d4ea91f20e59ab8c34a733eb0b2df8b
kt <kt@katbyte.me>
2018-10-19 18:10:14 -0700

    fixed test

--- azurerm/helpers/validate/public_ip_test.go
+++ azurerm/helpers/validate/public_ip_test.go
@@ -1,6 +1,8 @@
 package validate
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPublicIpDomainNameLabel(t *testing.T) {
 	cases := []struct {
@@ -20,7 +22,11 @@ func TestPublicIpDomainNameLabel(t *testing.T) {
 			ErrCount: 1,
 		},
 		{
-			Value:    acctest.RandString(80),
+			Value:    "k2345678-1-2345678-2-2345678-3-2345678-4-2345678-5-2345678-6-23",
+			ErrCount: 0,
+		},
+		{
+			Value:    "k2345678-1-2345678-2-2345678-3-2345678-4-2345678-5-2345678-6-234",
 			ErrCount: 1,
 		},
 	}
@@ -29,7 +35,7 @@ func TestPublicIpDomainNameLabel(t *testing.T) {
 		_, errors := PublicIpDomainNameLabel(tc.Value, "azurerm_public_ip")
 
 		if len(errors) != tc.ErrCount {
-			t.Fatalf("Expected the Azure RM Public IP Domain Name Label to trigger a validation error")
+			t.Fatalf("Expected the Azure RM Public IP Domain Name Label %s to trigger a validation error", tc.Value)
 		}
 	}
 }
