3c5c62398d8fb06dc746ff11ead7795e5e98f6ff
Tony Fortes Ramos <amfortesramos@hotmail.com>
2018-10-27 02:00:30 +0200

    azurerm_log_analytics_workspace_linked_service: move import tests

--- azurerm/resource_arm_log_analytics_workspace_linked_service_test.go
+++ azurerm/resource_arm_log_analytics_workspace_linked_service_test.go
@@ -118,6 +118,54 @@ func testCheckAzureRMLogAnalyticsWorkspaceLinkedServiceExists(name string) resou
 	}
 }
 
+func TestAccAzureRMLogAnalyticsWorkspaceLinkedService_importRequiredOnly(t *testing.T) {
+	resourceName := "azurerm_log_analytics_workspace_linked_service.test"
+
+	ri := acctest.RandInt()
+	config := testAccAzureRMLogAnalyticsWorkspaceLinkedServiceRequiredOnly(ri, testLocation())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMLogAnalyticsWorkspaceLinkedServiceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+			},
+
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccAzureRMLogAnalyticsWorkspaceLinkedService_importOptionalArguments(t *testing.T) {
+	resourceName := "azurerm_log_analytics_workspace_linked_service.test"
+
+	ri := acctest.RandInt()
+	config := testAccAzureRMLogAnalyticsWorkspaceLinkedServiceOptionalArguments(ri, testLocation())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMLogAnalyticsWorkspaceLinkedServiceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+			},
+
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func testAccAzureRMLogAnalyticsWorkspaceLinkedServiceRequiredOnly(rInt int, location string) string {
 	return fmt.Sprintf(`
 	resource "azurerm_resource_group" "test" {
