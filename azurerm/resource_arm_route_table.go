53aafdbc192f29ccc70934d942791e231eb46427
Tony Fortes Ramos <amfortesramos@hotmail.com>
2018-10-30 11:15:52 +0100

    Route now allows removing next hop and is now more similar to route_table

--- azurerm/resource_arm_route_table.go
+++ azurerm/resource_arm_route_table.go
@@ -71,7 +71,6 @@ func resourceArmRouteTable() *schema.Resource {
 						"next_hop_in_ip_address": {
 							Type:         schema.TypeString,
 							Optional:     true,
-							Computed:     true,
 							ValidateFunc: validation.NoZeroValues,
 						},
 					},
@@ -111,7 +110,7 @@ func resourceArmRouteTableCreateUpdate(d *schema.ResourceData, meta interface{})
 		Name:     &name,
 		Location: &location,
 		RouteTablePropertiesFormat: &network.RouteTablePropertiesFormat{
-			Routes: expandRouteTableRoutes(d),
+			Routes:                     expandRouteTableRoutes(d),
 			DisableBgpRoutePropagation: utils.Bool(d.Get("disable_bgp_route_propagation").(bool)),
 		},
 		Tags: expandTags(tags),
