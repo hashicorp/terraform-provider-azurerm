83f9ff477557cbf472d84a4ffe6605cbdadd3341
tombuildsstuff <git@ibuildstuff.co.uk>
2018-11-02 21:46:25 +0000

    New Data Source: `azurerm_monitor_diagnostic_categories`
    Tests pass:
    
    ```
    $ acctests azurerm TestAccDataSourceArmMonitorDiagnosticCategories_
    === RUN   TestAccDataSourceArmMonitorDiagnosticCategories_appService
    --- PASS: TestAccDataSourceArmMonitorDiagnosticCategories_appService (150.71s)
    === RUN   TestAccDataSourceArmMonitorDiagnosticCategories_storageAccount
    --- PASS: TestAccDataSourceArmMonitorDiagnosticCategories_storageAccount (108.97s)
    PASS
    ok  	github.com/terraform-providers/terraform-provider-azurerm/azurerm	261.573s
    ```

--- azurerm/provider.go
+++ azurerm/provider.go
@@ -97,6 +97,7 @@ func Provider() terraform.ResourceProvider {
 			"azurerm_logic_app_workflow":                    dataSourceArmLogicAppWorkflow(),
 			"azurerm_managed_disk":                          dataSourceArmManagedDisk(),
 			"azurerm_management_group":                      dataSourceArmManagementGroup(),
+			"azurerm_monitor_diagnostic_categories":         dataSourceArmMonitorDiagnosticCategories(),
 			"azurerm_monitor_log_profile":                   dataSourceArmMonitorLogProfile(),
 			"azurerm_network_interface":                     dataSourceArmNetworkInterface(),
 			"azurerm_network_security_group":                dataSourceArmNetworkSecurityGroup(),
