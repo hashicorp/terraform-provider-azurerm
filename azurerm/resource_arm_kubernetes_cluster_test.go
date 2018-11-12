12bad75647c0e9f0dcf47096e35ed594648fb157
Su Shi <1684739+metacpp@users.noreply.github.com>
2018-11-12 14:41:05 -0800

    fix the testing issue for advanced networking in aks.

--- azurerm/resource_arm_kubernetes_cluster_test.go
+++ azurerm/resource_arm_kubernetes_cluster_test.go
@@ -828,13 +828,6 @@ resource "azurerm_subnet" "test" {
   name                 = "acctestsubnet%d"
   resource_group_name  = "${azurerm_resource_group.test.name}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
-  address_prefix       = "10.1.0.0/24"
-}
-
-resource "azurerm_subnet" "test" {
-  name                 = "acctestsubnet%d"
-  resource_group_name  = "${azurerm_resource_group.test.name}"
-  virtual_network_name = "${azurerm_virtual_network.test.name}"
   address_prefix       = "172.0.2.0/24"
   route_table_id       = "${azurerm_route_table.test.id}"
 }
