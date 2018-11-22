2460dda943fdafed4e7532035571225b860ef5cc
Jon Arild Tørresdal <jon.torresdal@spv.no>
2018-11-22 15:14:33 +0100

    Fixed empty return statements

--- azurerm/helpers/validate/api_management.go
+++ azurerm/helpers/validate/api_management.go
@@ -65,8 +65,7 @@ func ApiManagementApiName(v interface{}, k string) (ws []string, es []error) {
 	if matched := regexp.MustCompile(`^[^*#&+]{1,256}$`).Match([]byte(value)); !matched {
 		es = append(es, fmt.Errorf("%q may only be up to 256 characters in length and not include the characters `*`, `#`, `&` or `+`", k))
 	}
-
-	return
+	return ws, es
 }
 
 func ApiManagementApiPath(v interface{}, k string) (ws []string, es []error) {
@@ -75,6 +74,5 @@ func ApiManagementApiPath(v interface{}, k string) (ws []string, es []error) {
 	if matched := regexp.MustCompile(`^[\w][\w-/.]+[\w-]$`).Match([]byte(value)); !matched {
 		es = append(es, fmt.Errorf("%q may only be up to 256 characters in length, not start or end with `/` and only contain valid url characters", k))
 	}
-
-	return
+	return ws, es
 }
