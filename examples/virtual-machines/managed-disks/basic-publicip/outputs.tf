2bc832f2a0a374e9bd5449989cd96892887b6bbe
tombuildsstuff <git@ibuildstuff.co.uk>
2018-11-22 11:32:08 +0100

    using the ip address rather than the id

--- examples/virtual-machines/managed-disks/basic-publicip/outputs.tf
+++ examples/virtual-machines/managed-disks/basic-publicip/outputs.tf
@@ -3,5 +3,5 @@ output "public_ip_address" {
 }
 
 output "ssh_command" {
-  value = "ssh ${local.admin_username}@${azurerm_public_ip.main.id}"
+  value = "ssh ${local.admin_username}@${azurerm_public_ip.main.ip_address}"
 }
