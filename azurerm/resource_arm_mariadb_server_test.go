51da5fcb599c82d4da8fced373862d4c4ef0db39
kt <kt@katbyte.me>
2018-11-30 11:31:34 -0800

    Update azurerm/resource_arm_mariadb_server_test.go
    Co-Authored-By: jeffreyCline <jcline@microsoft.com>

--- azurerm/resource_arm_mariadb_server_test.go
+++ azurerm/resource_arm_mariadb_server_test.go
@@ -277,7 +277,6 @@ func testCheckAzureRMMariaDbServerDestroy(s *terraform.State) error {
 		resourceGroup := rs.Primary.Attributes["resource_group_name"]
 
 		resp, err := client.Get(ctx, resourceGroup, name)
-
 		if err != nil {
 			if utils.ResponseWasNotFound(resp.Response) {
 				return nil
