6fda6af41a91fac05337d57f9cca34688aec3df4
Jeffrey Cline <jcline@microsoft.com>
2018-12-07 13:00:45 -0800

    Added special character test cases

--- azurerm/helpers/validate/strings_test.go
+++ azurerm/helpers/validate/strings_test.go
@@ -1,6 +1,8 @@
 package validate
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNoEmptyStrings(t *testing.T) {
 	cases := []struct {
@@ -58,13 +60,38 @@ func TestNoEmptyStrings(t *testing.T) {
 			TestName: "OneSpace",
 			ErrCount: 0,
 		},
+		{
+			Value:    "\r",
+			TestName: "CarriageReturn",
+			ErrCount: 1,
+		},
+		{
+			Value:    "\n",
+			TestName: "NewLine",
+			ErrCount: 1,
+		},
+		{
+			Value:    "\t",
+			TestName: "HorizontalTab",
+			ErrCount: 1,
+		},
+		{
+			Value:    "\f",
+			TestName: "FormFeed",
+			ErrCount: 1,
+		},
+		{
+			Value:    "\v",
+			TestName: "VerticalTab",
+			ErrCount: 1,
+		},
 	}
 
 	for _, tc := range cases {
 		t.Run(tc.TestName, func(t *testing.T) {
 			_, errors := NoEmptyStrings(tc.Value, tc.TestName)
 
-			if len(errors) < tc.ErrCount {
+			if len(errors) != tc.ErrCount {
 				t.Fatalf("Expected NoEmptyStrings to have %d not %d errors for %q", tc.ErrCount, len(errors), tc.TestName)
 			}
 		})
