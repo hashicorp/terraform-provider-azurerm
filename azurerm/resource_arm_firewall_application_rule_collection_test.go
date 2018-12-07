d6cfdf4580704768415a8879883598cd72d86eb7
Henry Buckle <h.buckle@outlook.com>
2018-12-07 07:10:56 +0000

    add test skeleton

---
+++ azurerm/resource_arm_firewall_application_rule_collection_test.go
@@ -0,0 +1,25 @@
+package azurerm
+
+import (
+	"testing"
+)
+
+func TestAccAzureRMFirewallApplicationRuleCollection_basic(t *testing.T) {}
+
+func TestAccAzureRMFirewallApplicationRuleCollection_updatedName(t *testing.T) {}
+
+func TestAccAzureRMFirewallApplicationRuleCollection_multipleRuleCollections(t *testing.T) {}
+
+func TestAccAzureRMFirewallApplicationRuleCollection_update(t *testing.T) {}
+
+func TestAccAzureRMFirewallApplicationRuleCollection_disappears(t *testing.T) {}
+
+func TestAccAzureRMFirewallApplicationRuleCollection_multipleRules(t *testing.T) {}
+
+func TestAccAzureRMFirewallApplicationRuleCollection_updateProtocols(t *testing.T) {}
+
+func testCheckAzureRMFirewallApplicationRuleCollectionExists(resourceName string) resource.TestCheckFunc {}
+
+func testCheckAzureRMFirewallApplicationRuleCollectionDoesNotExist(resourceName string, collectionName string) resource.TestCheckFunc {)
+
+func testCheckAzureRMFirewallApplicationRuleCollectionDisappears(resourceName string) resource.TestCheckFunc {}
