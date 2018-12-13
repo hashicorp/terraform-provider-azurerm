82d74a53bab9f79e7be328b7b948d259014df85d
Leandro López <inkel.ar@gmail.com>
2018-12-13 13:43:00 -0300

    Update virtual machine data source test configuration
    As suggested by @tombuildsstuff the test VM was changed to a Linux
    one, which tends to boot faster. I've also properly formattd the test
    using `terraform fmt`.

--- azurerm/data_source_arm_virtual_machine_test.go
+++ azurerm/data_source_arm_virtual_machine_test.go
@@ -33,50 +33,50 @@ func testAccDataSourceVirtualMachine_basic(rInt int, location string) string {
 	resource "azurerm_resource_group" "test" {
 		name     = "acctestRG-%[1]d"
 		location = "%[2]s"
-	  }
+	}
 
-	  resource "azurerm_virtual_network" "test" {
+	resource "azurerm_virtual_network" "test" {
 		name                = "acctvn-%[1]d"
 		address_space       = ["10.0.0.0/16"]
 		location            = "${azurerm_resource_group.test.location}"
 		resource_group_name = "${azurerm_resource_group.test.name}"
-	  }
+	}
 
-	  resource "azurerm_subnet" "test" {
+	resource "azurerm_subnet" "test" {
 		name                 = "acctsub-%[1]d"
 		resource_group_name  = "${azurerm_resource_group.test.name}"
 		virtual_network_name = "${azurerm_virtual_network.test.name}"
 		address_prefix       = "10.0.2.0/24"
-	  }
+	}
 
-	  resource "azurerm_network_interface" "test" {
+	resource "azurerm_network_interface" "test" {
 		name                = "acctni-%[1]d"
 		location            = "${azurerm_resource_group.test.location}"
 		resource_group_name = "${azurerm_resource_group.test.name}"
 
 		ip_configuration {
-		  name                          = "testconfiguration1"
-		  subnet_id                     = "${azurerm_subnet.test.id}"
-		  private_ip_address_allocation = "dynamic"
+			name                          = "testconfiguration1"
+			subnet_id                     = "${azurerm_subnet.test.id}"
+			private_ip_address_allocation = "dynamic"
 		}
-	  }
+	}
 
-	  resource "azurerm_storage_account" "test" {
+	resource "azurerm_storage_account" "test" {
 		name                     = "accsa%[1]d"
 		resource_group_name      = "${azurerm_resource_group.test.name}"
 		location                 = "${azurerm_resource_group.test.location}"
 		account_tier             = "Standard"
 		account_replication_type = "LRS"
-	  }
+	}
 
-	  resource "azurerm_storage_container" "test" {
+	resource "azurerm_storage_container" "test" {
 		name                  = "vhds"
 		resource_group_name   = "${azurerm_resource_group.test.name}"
 		storage_account_name  = "${azurerm_storage_account.test.name}"
 		container_access_type = "private"
-	  }
+	}
 
-	  resource "azurerm_virtual_machine" "test" {
+	resource "azurerm_virtual_machine" "test" {
 		name                  = "acctvm-%[1]d"
 		location              = "${azurerm_resource_group.test.location}"
 		resource_group_name   = "${azurerm_resource_group.test.name}"
@@ -84,34 +84,33 @@ func testAccDataSourceVirtualMachine_basic(rInt int, location string) string {
 		vm_size               = "Standard_D1_v2"
 
 		storage_image_reference {
-			publisher = "MicrosoftWindowsServer"
-			offer     = "WindowsServer"
-			sku       = "2012-Datacenter"
+			publisher = "Canonical"
+			offer     = "UbuntuServer"
+			sku       = "16.04-LTS"
 			version   = "latest"
-		  }
+		}
 
-		  storage_os_disk {
+		storage_os_disk {
 			name          = "myosdisk1"
 			vhd_uri       = "${azurerm_storage_account.test.primary_blob_endpoint}${azurerm_storage_container.test.name}/myosdisk1.vhd"
 			caching       = "ReadWrite"
 			create_option = "FromImage"
-		  }
+		}
 
-		  os_profile {
-			computer_name  = "winhost01"
+		os_profile {
+			computer_name  = "linuxhost01"
 			admin_username = "testadmin"
 			admin_password = "Password1234!"
-		  }
-
-		os_profile_windows_config {
-		  timezone = "Pacific Standard Time"
 		}
-	  }
 
-	  data "azurerm_virtual_machine" "test" {
+		os_profile_linux_config {
+			disable_password_authentication = false
+		}
+	}
+
+	data "azurerm_virtual_machine" "test" {
 		resource_group_name = "${azurerm_resource_group.test.name}"
 		name                = "${azurerm_virtual_machine.test.name}"
-	  }
-
-	`, rInt, location)
+	}
+`, rInt, location)
 }
