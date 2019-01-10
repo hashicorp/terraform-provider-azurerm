17741a20fe28eae624c02be99670b88def48ef11
Leandro López <inkel.ar@gmail.com>
2019-01-09 22:33:50 -0300

    Validate against empty strings instead of zero value

--- azurerm/data_source_application_insights.go
+++ azurerm/data_source_application_insights.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/helper/validation"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
@@ -17,7 +17,7 @@ func dataSourceArmApplicationInsights() *schema.Resource {
 			"name": {
 				Type:         schema.TypeString,
 				Required:     true,
-				ValidateFunc: validation.NoZeroValues,
+				ValidateFunc: validate.NoEmptyStrings,
 			},
 
 			"instrumentation_key": {
