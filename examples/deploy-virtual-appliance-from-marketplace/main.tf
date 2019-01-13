641a8b14ca0fb3dd72e8d8cdc006b389765945be
shay shahak <shays@microsoft.com>
2019-01-13 16:03:56 +0200

    Update main.tf Provider format as other examples

--- examples/deploy-virtual-appliance-from-marketplace/main.tf
+++ examples/deploy-virtual-appliance-from-marketplace/main.tf
@@ -1,8 +1,10 @@
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-   subscription_id = "${var.subscription_id}"
-   client_id       = "${var.client_id}"
-   client_secret   = "${var.client_secret}"
-   tenant_id       = "${var.tenant_id}"
+  # if you're using a Service Principal (shared account) then either set the environment variables, or fill these in: 
+  # subscription_id = "${var.subscription_id}"
+  # client_id       = "${var.client_id}"
+  # client_secret   = "${var.client_secret}"
+  # tenant_id       = "${var.tenant_id}"
 }
 resource "azurerm_resource_group" "rg" {
   name     = "${var.resource_group}"
