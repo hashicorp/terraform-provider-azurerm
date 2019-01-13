f12f12bdc28a27fa11272c6eb8082a7cd3217e2d
kt <kt@katbyte.me>
2019-01-13 15:43:13 +0200

    Update examples/deploy-virtual-appliance-from-marketplace/main.tf
    Co-Authored-By: shayshahak <shays@microsoft.com>

--- examples/deploy-virtual-appliance-from-marketplace/main.tf
+++ examples/deploy-virtual-appliance-from-marketplace/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "subnet" {
 }
 resource "azurerm_network_interface" "nic" {
   name                = "${var.rg_prefix}nic"
-  location            = "${var.location}"
+  location            = "${azurerm_resource_group.rg.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   ip_configuration {
     name                          = "${var.rg_prefix}ipconfig"
