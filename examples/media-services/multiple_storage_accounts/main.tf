cbdd5fa19032c294da1f7baadc49e902be910536
Sean Usher <seusher@microsoft.com>
2019-01-17 17:20:00 -0600

    Make examples more generic to remove my alias from them

--- examples/media-services/multiple_storage_accounts/main.tf
+++ examples/media-services/multiple_storage_accounts/main.tf
@@ -1,27 +1,14 @@
-provider "azurerm" {
-		version = "~> 999"
-
-        subscription_id="7060bca0-7a3c-44bd-b54c-4bb1e9facfac"
-        client_id="32c7a76b-6d19-4395-9ed2-ac1ee3e1aec0"
-        client_secret="4ba02547-5c24-46b1-a805-f243040b56cc"
-        tenant_id="72f988bf-86f1-41af-91ab-2d7cd011db47"
-}
-
 resource "azurerm_resource_group" "testrg" {
-  name     = "seusheramsrg"
+  name     = "amstestrg"
   location = "westus"
 }
 
-resource "azurerm_storage_account" "testsa1" {
-  name                     = "seusheramsstore1"
+resource "azurerm_storage_account" "testsa" {
+  name                     = "amstestsa"
   resource_group_name      = "${azurerm_resource_group.testrg.name}"
   location                 = "${azurerm_resource_group.testrg.location}"
   account_tier             = "Standard"
   account_replication_type = "GRS"
-
-  tags {
-    environment = "staging"
-  }
 }
 
 #
@@ -30,7 +17,7 @@ resource "azurerm_storage_account" "testsa1" {
 #
 /*
 resource "azurerm_storage_account" "testsa2" {
-  name                     = "seusheramsstore2"
+  name                     = "amstestsa2"
   resource_group_name      = "${azurerm_resource_group.testrg.name}"
   location                 = "${azurerm_resource_group.testrg.location}"
   account_tier             = "Standard"
@@ -44,22 +31,21 @@ resource "azurerm_storage_account" "testsa2" {
 
 resource "azurerm_media_services" "ams" {
 
-        name = "seushertestamstf"
+        name = "amstest"
         location = "${azurerm_resource_group.testrg.location}"
         resource_group_name = "${azurerm_resource_group.testrg.name}"
 		
         storage_account {
-				id = "${azurerm_storage_account.testsa1.id}"
-				is_primary = true
-		}
-		
-		/*
-		storage_account {
-				id = "${azurerm_storage_account.testsa2.id}"
-				is_primary = false
-		}
-		*/
-		
+            id = "${azurerm_storage_account.testsa.id}"
+            is_primary = true
+		    }
+
+        /*
+        storage_account {
+            id = "${azurerm_storage_account.testsa2.id}"
+            is_primary = false
+        }
+        */
 }
 
 output "rendered" {
--- examples/media-services/new_primary_storage/main.tf
+++ examples/media-services/new_primary_storage/main.tf
@@ -1,19 +1,10 @@
-provider "azurerm" {
-		version = "~> 999"
-
-        subscription_id="7060bca0-7a3c-44bd-b54c-4bb1e9facfac"
-        client_id="32c7a76b-6d19-4395-9ed2-ac1ee3e1aec0"
-        client_secret="4ba02547-5c24-46b1-a805-f243040b56cc"
-        tenant_id="72f988bf-86f1-41af-91ab-2d7cd011db47"
-}
-
 resource "azurerm_resource_group" "testrg" {
-  name     = "seusheramsrg"
+  name     = "amstestrg"
   location = "westus"
 }
 
 resource "azurerm_storage_account" "testsa" {
-  name                     = "seusheramsstore"
+  name                     = "amstestsa"
   resource_group_name      = "${azurerm_resource_group.testrg.name}"
   location                 = "${azurerm_resource_group.testrg.location}"
   account_tier             = "Standard"
@@ -26,14 +17,14 @@ resource "azurerm_storage_account" "testsa" {
 
 resource "azurerm_media_services" "ams" {
 
-        name = "seushertestamstf"
+        name = "amstest"
         location = "${azurerm_resource_group.testrg.location}"
         resource_group_name = "${azurerm_resource_group.testrg.name}"
 		
         storage_account {
-				id = "${azurerm_storage_account.testsa.id}"
-				is_primary = true
-		}
+				  id = "${azurerm_storage_account.testsa.id}"
+				  is_primary = true
+		    }
 }
 
 output "rendered" {
