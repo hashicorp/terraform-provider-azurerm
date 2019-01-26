1647b9d3c0b8f8238364c15a3a1a29a05e92dbda
Su Shi <1684739+metacpp@users.noreply.github.com>
2019-01-25 21:52:44 -0800

    Fix: unit testing error due to refactoring.

--- azurerm/resource_arm_kubernetes_cluster_test.go
+++ azurerm/resource_arm_kubernetes_cluster_test.go
@@ -2,6 +2,7 @@ package azurerm
 
 import (
 	"fmt"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/validate"
 	"net/http"
 	"os"
 	"testing"
@@ -59,7 +60,7 @@ func TestAzureRMKubernetesCluster_agentPoolName(t *testing.T) {
 	}
 
 	for _, tc := range cases {
-		_, errors := validateKubernetesClusterAgentPoolName()(tc.Input, "")
+		_, errors := validate.KubernetesAgentPoolName()(tc.Input, "")
 
 		hasError := len(errors) > 0
 
