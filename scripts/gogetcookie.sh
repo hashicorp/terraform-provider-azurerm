ea7a0392fec246ccf25b5134672f9a62303cc098
kt <kt@katbyte.me>
2019-02-06 10:04:55 -0800

    update go get cookie

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -6,6 +6,6 @@ chmod 0600 ~/.gitcookies
 git config --global http.cookiefile ~/.gitcookies
 
 tr , \\t <<\__END__ >>~/.gitcookies
-go.googlesource.com,FALSE,/,TRUE,2147483647,o,git-kt.katbyte.me=1/sEvv4P2NiGofB7kgPV7DBbsV5V8_od3JULgYIyZJnUM
-go-review.googlesource.com,FALSE,/,TRUE,2147483647,o,git-kt.katbyte.me=1/sEvv4P2NiGofB7kgPV7DBbsV5V8_od3JULgYIyZJnUM
+go.googlesource.com,TRUE,/,TRUE,2147483647,o,git-kt.katbyte.me=1/sEvv4P2NiGofB7kgPV7DBbsV5V8_od3JULgYIyZJnUM
+go-review.googlesource.com,TRUE,/,TRUE,2147483647,o,git-kt.katbyte.me=1/sEvv4P2NiGofB7kgPV7DBbsV5V8_od3JULgYIyZJnUM
 __END__
