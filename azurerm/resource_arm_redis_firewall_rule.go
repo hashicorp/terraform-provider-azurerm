1f1e4b68e24c1978fcc7a52bdc1cb34624dff750
Andrew Urwin <ellieniwru@hotmail.co.uk>
2019-02-18 12:26:56 +0000

    simplify regex for redis firewall rule name

--- azurerm/resource_arm_redis_firewall_rule.go
+++ azurerm/resource_arm_redis_firewall_rule.go
@@ -161,7 +161,7 @@ func resourceArmRedisFirewallRuleDelete(d *schema.ResourceData, meta interface{}
 func validateRedisFirewallRuleName(v interface{}, k string) (warnings []string, errors []error) {
 	value := v.(string)
 
-	if matched := regexp.MustCompile(`^[0-9a-zA-Z_]+$`).Match([]byte(value)); !matched {
+	if matched := regexp.MustCompile(`^\w+$`).Match([]byte(value)); !matched {
 		errors = append(errors, fmt.Errorf("%q may only contain alphanumeric characters and underscores", k))
 	}
 
