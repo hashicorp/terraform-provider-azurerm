b9c9121bfaefabf7964f68d952e3221b67d88f49
kt <kt@katbyte.me>
2019-02-28 00:27:45 -0800

    app service identity

--- azurerm/resource_arm_app_service_test.go
+++ azurerm/resource_arm_app_service_test.go
@@ -1737,7 +1737,7 @@ resource "azurerm_app_service" "test" {
   resource_group_name = "${azurerm_resource_group.test.name}"
   app_service_plan_id = "${azurerm_app_service_plan.test.id}"
 
-  identity = {
+  identity {
     type = "SystemAssigned"
   }
 }
