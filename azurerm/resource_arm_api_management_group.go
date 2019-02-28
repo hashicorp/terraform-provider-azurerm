44dedc0c582ebb59351a93bd1cf265a2dc682b4d
tombuildsstuff <git@ibuildstuff.co.uk>
2019-02-28 14:14:35 +0100

    r/api_management_group: requiring import

--- azurerm/resource_arm_api_management_group.go
+++ azurerm/resource_arm_api_management_group.go
@@ -8,6 +8,7 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
@@ -73,7 +74,18 @@ func resourceArmApiManagementGroupCreateUpdate(d *schema.ResourceData, meta inte
 	externalID := d.Get("external_id").(string)
 	groupType := d.Get("type").(string)
 
-	// TODO: support for Requiring Imports
+	if requireResourcesToBeImported && d.IsNewResource() {
+		existing, err := client.Get(ctx, resourceGroup, serviceName, name)
+		if err != nil {
+			if !utils.ResponseWasNotFound(existing.Response) {
+				return fmt.Errorf("Error checking for presence of existing Group %q (API Management Service %q / Resource Group %q): %s", name, serviceName, resourceGroup, err)
+			}
+		}
+
+		if existing.ID != nil && *existing.ID != "" {
+			return tf.ImportAsExistsError("azurerm_api_management_group", *existing.ID)
+		}
+	}
 
 	parameters := apimanagement.GroupCreateParameters{
 		GroupCreateParametersProperties: &apimanagement.GroupCreateParametersProperties{
