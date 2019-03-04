d963b19473e312fb866c71e261d0d29a7162032f
tombuildsstuff <git@ibuildstuff.co.uk>
2019-03-04 11:33:36 +0100

    fixing the test

--- azurerm/resource_arm_api_management_product_group_test.go
+++ azurerm/resource_arm_api_management_product_group_test.go
@@ -141,7 +141,7 @@ resource "azurerm_api_management_product" "test" {
   resource_group_name   = "${azurerm_resource_group.test.name}"
   display_name          = "Test Product"
   subscription_required = true
-  approval_required     = true
+  approval_required     = false
   published             = true
 }
 
