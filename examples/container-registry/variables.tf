5017c3b7b98856b27153dbc3e0e7b6d3fbeaa2ad
tombuildsstuff <git@ibuildstuff.co.uk>
2019-03-10 10:14:55 +0100

    r/example: refactoring the container registry example

--- examples/container-registry/variables.tf
+++ examples/container-registry/variables.tf
@@ -1,28 +1,7 @@
-variable "resource_group_name" {
-  type        = "string"
-  description = "Name of the azure resource group."
-  default     = "tfex-container-registry"
+variable "prefix" {
+  description = "The prefix used for all resources in this example"
 }
 
-variable "resource_group_location" {
-  type        = "string"
-  description = "Location of the azure resource group."
-  default     = "westus"
-}
-
-variable "sku" {
-  type        = "string"
-  description = "SKU for the Azure Container Registry"
-  default     = "Premium"
-}
-
-variable "admin_enabled" {
-  description = "Flag that indicates wether an admin account/password should be created or not"
-  default     = false
-}
-
-variable "georeplication_locations" {
-  type        = "list"
-  description = "Azure locations where the Container Registry should be geo-replicated - Only with Premium SKU"
-  default     = ["eastus"]
+variable "location" {
+  description = "The Azure location where all resources in this example should be created"
 }
