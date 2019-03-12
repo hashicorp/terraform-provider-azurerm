e180218028c2173e3581e5d9f72a08a49225c226
tombuildsstuff <git@ibuildstuff.co.uk>
2019-03-12 12:12:21 +0100

    r/example: refactoring the servicebus examples

--- examples/servicebus/forwarding/main.tf
+++ examples/servicebus/forwarding/main.tf
@@ -1,80 +1,53 @@
 resource "azurerm_resource_group" "example" {
-  name     = "${var.resource_group}"
+  name     = "${var.prefix}-resources"
   location = "${var.location}"
 }
 
-resource "random_integer" "ri" {
-  min = 10000
-  max = 99999
-}
-
 resource "azurerm_servicebus_namespace" "example" {
-  name                = "tfex-servicebus${random_integer.ri.result}"
+  name                = "${var.prefix}-sbnamespace"
   location            = "${azurerm_resource_group.example.location}"
   resource_group_name = "${azurerm_resource_group.example.name}"
   sku                 = "Standard"
 }
 
 resource "azurerm_servicebus_namespace_authorization_rule" "example" {
-  name                = "tfex_servicebus${random_integer.ri.result}_authrule"
+  name                = "${var.prefix}-sbnauth"
   namespace_name      = "${azurerm_servicebus_namespace.example.name}"
   resource_group_name = "${azurerm_resource_group.example.name}"
-
-  send   = true
-  listen = true
-  manage = true
+  send                = true
+  listen              = true
+  manage              = true
 }
 
 resource "azurerm_servicebus_topic" "source" {
-  name                = "tfex_servicebus${random_integer.ri.result}~topic"
+  name                = "${var.prefix}-sbt-source"
   resource_group_name = "${azurerm_resource_group.example.name}"
   namespace_name      = "${azurerm_servicebus_namespace.example.name}"
-
   enable_partitioning = true
 }
 
 resource "azurerm_servicebus_topic_authorization_rule" "example" {
-  name                = "tfex_servicebus${random_integer.ri.result}_authrule"
+  name                = "${var.prefix}-sbtauth-source"
   namespace_name      = "${azurerm_servicebus_namespace.example.name}"
   topic_name          = "${azurerm_servicebus_topic.source.name}"
   resource_group_name = "${azurerm_resource_group.example.name}"
-
-  send   = true
-  listen = true
-  manage = true
+  send                = true
+  listen              = true
+  manage              = true
 }
 
-resource "azurerm_servicebus_topic" "forward_to" {
-  name                = "tfex_servicebus${random_integer.ri.result}_forwardto"
+resource "azurerm_servicebus_topic" "destination" {
+  name                = "${var.prefix}-sbt-destination"
   resource_group_name = "${azurerm_resource_group.example.name}"
   namespace_name      = "${azurerm_servicebus_namespace.example.name}"
-
   enable_partitioning = true
 }
 
 resource "azurerm_servicebus_subscription" "example" {
-  name                = "tfex_servicebus${random_integer.ri.result}_subscription"
+  name                = "${var.prefix}-sbsubscription"
   resource_group_name = "${azurerm_resource_group.example.name}"
   namespace_name      = "${azurerm_servicebus_namespace.example.name}"
   topic_name          = "${azurerm_servicebus_topic.source.name}"
-  forward_to          = "${azurerm_servicebus_topic.forward_to.name}"
+  forward_to          = "${azurerm_servicebus_topic.destination.name}"
   max_delivery_count  = 1
-}
-
-resource "azurerm_servicebus_queue" "example" {
-  name                = "tfex_servicebus_queue"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  namespace_name      = "${azurerm_servicebus_namespace.example.name}"
-
-  enable_partitioning = true
-}
-
-resource "azurerm_servicebus_queue_authorization_rule" "example" {
-  name                = "tfex_servicebus${random_integer.ri.result}_authrule"
-  namespace_name      = "${azurerm_servicebus_namespace.example.name}"
-  queue_name          = "${azurerm_servicebus_queue.example.name}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-
-  send   = true
-  listen = true
 }
