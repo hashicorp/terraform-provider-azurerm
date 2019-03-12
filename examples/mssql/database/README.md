6ccd5f621ed24da6de71736651931b02b5a10081
tombuildsstuff <git@ibuildstuff.co.uk>
2019-03-12 13:38:45 +0100

    r/examples: removing examples whcih have since been refactored

--- examples/mssql/database/README.md
+++
@@ -1,3 +0,0 @@
-## Example: Azure SQL Database
-
-This example provisions an Azure SQL Database.
--- examples/mssql/database/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "prefix" {
-  description = "The prefix which should be used for all resources in this example"
-}
-
-variable "location" {
-  description = "The Azure Region in which all resources in this example should be created."
-}
