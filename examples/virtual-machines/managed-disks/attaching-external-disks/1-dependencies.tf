7d60d7756e3811b42824c0c1a3130704cb2d0755
tombuildsstuff <git@ibuildstuff.co.uk>
2019-03-13 10:48:51 +0100

    r/example: refactoring the attaching disks example

--- examples/virtual-machines/managed-disks/attaching-external-disks/1-dependencies.tf
+++ examples/virtual-machines/managed-disks/attaching-external-disks/1-dependencies.tf
@@ -1,33 +1,30 @@
-resource "azurerm_resource_group" "main" {
+resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-resources"
   location = "${var.location}"
-  tags     = "${var.tags}"
 }
 
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "example" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.main.location}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
-  tags                = "${var.tags}"
+  location            = "${azurerm_resource_group.example.location}"
+  resource_group_name = "${azurerm_resource_group.example.name}"
 }
 
-resource "azurerm_subnet" "internal" {
+resource "azurerm_subnet" "example" {
   name                 = "internal"
-  resource_group_name  = "${azurerm_resource_group.main.name}"
-  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  resource_group_name  = "${azurerm_resource_group.example.name}"
+  virtual_network_name = "${azurerm_virtual_network.example.name}"
   address_prefix       = "10.0.2.0/24"
 }
 
-resource "azurerm_network_interface" "main" {
+resource "azurerm_network_interface" "example" {
   name                = "${var.prefix}-nic"
-  location            = "${azurerm_resource_group.main.location}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
-  tags                = "${var.tags}"
+  location            = "${azurerm_resource_group.example.location}"
+  resource_group_name = "${azurerm_resource_group.example.name}"
 
   ip_configuration {
     name                          = "testconfiguration1"
-    subnet_id                     = "${azurerm_subnet.internal.id}"
+    subnet_id                     = "${azurerm_subnet.example.id}"
     private_ip_address_allocation = "Dynamic"
   }
 }
--- examples/virtual-machines/managed-disks/attaching-external-disks/variables.tf
+++ examples/virtual-machines/managed-disks/attaching-external-disks/variables.tf
@@ -5,14 +5,3 @@ variable "prefix" {
 variable "location" {
   description = "The Azure Region in which the resources in this example should exist"
 }
-
-variable "tags" {
-  type        = "map"
-  default     = {}
-  description = "Any tags which should be assigned to the resources in this example"
-}
-
-variable "number_of_disks" {
-  description = "The number of Data Disks which should be attached"
-  default     = 2
-}
