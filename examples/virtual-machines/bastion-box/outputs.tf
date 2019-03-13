bc77c6dad4563780532c757daf37289708db4014
tombuildsstuff <git@ibuildstuff.co.uk>
2019-03-13 11:47:28 +0100

    r/example: refactoring the bastion box example

--- examples/virtual-machines/bastion-box/outputs.tf
+++ examples/virtual-machines/bastion-box/outputs.tf
@@ -1,11 +1,7 @@
-output "hostname" {
-  value = "${var.hostname}"
-}
-
 output "vm_fqdn" {
-  value = "${azurerm_public_ip.pip.fqdn}"
+  value = "${azurerm_public_ip.example.fqdn}"
 }
 
 output "ssh_command" {
-  value = "ssh ${var.admin_username}@${azurerm_public_ip.pip.fqdn}"
+  value = "ssh ${local.admin_username}@${azurerm_public_ip.example.fqdn}"
 }
