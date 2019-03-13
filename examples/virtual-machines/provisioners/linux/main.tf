fc0060ba8199ce5a0aa0e372cd401c0f4425aeb1
tombuildsstuff <git@ibuildstuff.co.uk>
2019-03-13 10:30:55 +0100

    r/example: refactoring the linux provisioner example

--- examples/virtual-machines/provisioners/linux/main.tf
+++ examples/virtual-machines/provisioners/linux/main.tf
@@ -1,17 +1,13 @@
 resource "azurerm_virtual_machine" "example" {
   name                  = "${local.virtual_machine_name}"
-  location              = "${azurerm_resource_group.main.location}"
-  resource_group_name   = "${azurerm_resource_group.main.name}"
-  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+  location              = "${azurerm_resource_group.example.location}"
+  resource_group_name   = "${azurerm_resource_group.example.name}"
+  network_interface_ids = ["${azurerm_network_interface.example.id}"]
   vm_size               = "Standard_F2"
 
   # This means the OS Disk will be deleted when Terraform destroys the Virtual Machine
   # NOTE: This may not be optimal in all cases.
   delete_os_disk_on_termination = true
-
-  # This means the Data Disk Disk will be deleted when Terraform destroys the Virtual Machine
-  # NOTE: This may not be optimal in all cases.
-  delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
@@ -47,6 +43,4 @@ resource "azurerm_virtual_machine" "example" {
       "ls -la",
     ]
   }
-
-  tags = "${var.tags}"
 }
