4f9a5c81c0f3c5fb42b7af04c677722736e983da
tombuildsstuff <git@ibuildstuff.co.uk>
2019-03-13 11:05:13 +0100

    r/example: refactoring the from-image test

--- examples/virtual-machines/managed-disks/from-custom-image/main.tf
+++ examples/virtual-machines/managed-disks/from-custom-image/main.tf
@@ -4,20 +4,16 @@ data "azurerm_image" "custom" {
   resource_group_name = "${var.custom_image_resource_group_name}"
 }
 
-resource "azurerm_virtual_machine" "test" {
+resource "azurerm_virtual_machine" "example" {
   name                  = "${var.prefix}-vm"
-  location              = "${azurerm_resource_group.main.location}"
-  resource_group_name   = "${azurerm_resource_group.main.name}"
-  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+  location              = "${azurerm_resource_group.example.location}"
+  resource_group_name   = "${azurerm_resource_group.example.name}"
+  network_interface_ids = ["${azurerm_network_interface.example.id}"]
   vm_size               = "Standard_F2"
 
   # This means the OS Disk will be deleted when Terraform destroys the Virtual Machine
   # NOTE: This may not be optimal in all cases.
   delete_os_disk_on_termination = true
-
-  # This means the Data Disk Disk will be deleted when Terraform destroys the Virtual Machine
-  # NOTE: This may not be optimal in all cases.
-  delete_data_disks_on_termination = true
 
   storage_image_reference {
     id = "${data.azurerm_image.custom.id}"
@@ -39,6 +35,4 @@ resource "azurerm_virtual_machine" "test" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
-
-  tags = "${var.tags}"
 }
