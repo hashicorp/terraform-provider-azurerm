953a8d876c8fd1e9ec201af49f9747e38dd1447b
Robert Lippens <robertl@sirrus7.com>
2019-04-03 21:38:23 -0700

    add data source test

--- azurerm/data_source_kubernetes_cluster_test.go
+++ azurerm/data_source_kubernetes_cluster_test.go
@@ -42,6 +42,43 @@ func TestAccDataSourceAzureRMKubernetesCluster_basic(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceAzureRMKubernetesCluster_autoscaling(t *testing.T) {
+	dataSourceName := "data.azurerm_kubernetes_cluster.test"
+	ri := tf.AccRandTimeInt()
+	clientId := os.Getenv("ARM_CLIENT_ID")
+	clientSecret := os.Getenv("ARM_CLIENT_SECRET")
+	location := testLocation()
+	config := testAccDataSourceAzureRMKubernetesCluster_autoscaling(ri, clientId, clientSecret, location)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMKubernetesClusterDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMKubernetesClusterExists(dataSourceName),
+					resource.TestCheckResourceAttr(dataSourceName, "role_based_access_control.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "role_based_access_control.0.enabled", "false"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "kube_config.0.client_key"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "kube_config.0.client_certificate"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "kube_config.0.cluster_ca_certificate"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "kube_config.0.host"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "kube_config.0.username"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "kube_config.0.password"),
+					resource.TestCheckResourceAttr(dataSourceName, "kube_admin_config.#", "0"),
+					resource.TestCheckResourceAttr(dataSourceName, "kube_admin_config_raw", ""),
+					resource.TestCheckResourceAttr(dataSourceName, "agent_pool_profile.0.enable_autoscaling", "true"),
+					resource.TestCheckResourceAttr(dataSourceName, "agent_pool_profile.0.min_count", "0"),
+					resource.TestCheckResourceAttr(dataSourceName, "agent_pool_profile.0.count", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "agent_pool_profile.0.max_count", "3"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccDataSourceAzureRMKubernetesCluster_roleBasedAccessControl(t *testing.T) {
 	dataSourceName := "data.azurerm_kubernetes_cluster.test"
 	ri := tf.AccRandTimeInt()
@@ -357,6 +394,18 @@ func TestAccDataSourceAzureRMKubernetesCluster_addOnProfileRouting(t *testing.T)
 
 func testAccDataSourceAzureRMKubernetesCluster_basic(rInt int, clientId string, clientSecret string, location string) string {
 	r := testAccAzureRMKubernetesCluster_basic(rInt, clientId, clientSecret, location)
+	return fmt.Sprintf(`
+%s
+
+data "azurerm_kubernetes_cluster" "test" {
+  name                = "${azurerm_kubernetes_cluster.test.name}"
+  resource_group_name = "${azurerm_kubernetes_cluster.test.resource_group_name}"
+}
+`, r)
+}
+
+func testAccDataSourceAzureRMKubernetesCluster_autoscaling(rInt int, clientId string, clientSecret string, location string) string {
+	r := testAccAzureRMKubernetesCluster_autoscaling(rInt, clientId, clientSecret, location)
 	return fmt.Sprintf(`
 %s
 
