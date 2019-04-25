bab63c4ed0648785e35eb99c06f6d6c017c30675
Junyi Yi <junyi@microsoft.com>
2019-04-24 18:45:11 -0700

    Implement common parse value function for AutoVar

---
+++ azurerm/resource_arm_automation_variable_common.go
@@ -0,0 +1,41 @@
+package azurerm
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+func parseAzureRmAutomationVariableValue(resource string, input *string) (interface{}, error) {
+	if input == nil {
+		if resource != "azurerm_automation_null_variable" {
+			return nil, fmt.Errorf("Expect resource to be %q, but actual value is %v", resource, input)
+		}
+		return nil, nil
+	}
+
+	var value interface{}
+	var err error
+	actualResource := "Unknown"
+	datePattern := regexp.MustCompile(`"\\/Date\((-?[0-9]+)\)\\/"`)
+	matches := datePattern.FindStringSubmatch(*input)
+	if len(matches) == 2 && matches[0] == *input {
+		if ticks, err := strconv.ParseInt(matches[1], 10, 64); err == nil {
+			value = time.Unix(ticks/1000, ticks%1000*1000000)
+			actualResource = "azurerm_automation_datetime_variable"
+		}
+	} else if value, err = strconv.Unquote(*input); err == nil {
+		actualResource = "azurerm_automation_string_variable"
+	} else if value, err = strconv.ParseBool(*input); err == nil {
+		actualResource = "azurerm_automation_boolean_variable"
+	} else if value, err = strconv.ParseInt(*input, 10, 32); err == nil {
+		value = int32(value.(int64))
+		actualResource = "azurerm_automation_integer_variable"
+	}
+
+	if actualResource != resource {
+		return nil, fmt.Errorf("Expect value %q to be %q, but actually it is %q", *input, resource, actualResource)
+	}
+	return value, nil
+}
---
+++ azurerm/resource_arm_automation_variable_common_test.go
@@ -0,0 +1,83 @@
+package azurerm
+
+import (
+	"testing"
+	"time"
+)
+
+func TestParseAzureRmAutomationVariableValue(t *testing.T) {
+	type ExpectFunc func(interface{}) bool
+	cases := []struct {
+		Name        string
+		Resource    string
+		IsNil       bool
+		Value       string
+		HasError    bool
+		ExpectValue interface{}
+		Expect      ExpectFunc
+	}{
+		{
+			Name:        "null variable",
+			Resource:    "azurerm_automation_null_variable",
+			IsNil:       true,
+			Value:       "<nil>",
+			HasError:    false,
+			ExpectValue: nil,
+			Expect:      func(v interface{}) bool { return v == nil },
+		},
+		{
+			Name:        "string variable",
+			Resource:    "azurerm_automation_string_variable",
+			Value:       "\"Test String\"",
+			HasError:    false,
+			ExpectValue: "Test String",
+			Expect:      func(v interface{}) bool { return v.(string) == "Test String" },
+		},
+		{
+			Name:        "integer variable",
+			Resource:    "azurerm_automation_integer_variable",
+			Value:       "135",
+			HasError:    false,
+			ExpectValue: 135,
+			Expect:      func(v interface{}) bool { return v.(int32) == 135 },
+		},
+		{
+			Name:        "boolean variable",
+			Resource:    "azurerm_automation_boolean_variable",
+			Value:       "true",
+			HasError:    false,
+			ExpectValue: true,
+			Expect:      func(v interface{}) bool { return v.(bool) == true },
+		},
+		{
+			Name:        "datetime variable",
+			Resource:    "azurerm_automation_datetime_variable",
+			Value:       "\"\\/Date(1556142054074)\\/\"",
+			HasError:    false,
+			ExpectValue: time.Date(2019, time.April, 24, 14, 40, 54, 74000000, time.Local),
+			Expect: func(v interface{}) bool {
+				return v.(time.Time) == time.Date(2019, time.April, 24, 14, 40, 54, 74000000, time.Local)
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			value := &tc.Value
+			if tc.IsNil {
+				value = nil
+			}
+			actual, err := parseAzureRmAutomationVariableValue(tc.Resource, value)
+			if tc.HasError && err == nil {
+				t.Fatalf("Expect parseAzureRmAutomationVariableValue to return error for resource %q and value %s", tc.Resource, tc.Value)
+			}
+			if !tc.HasError {
+				if err != nil {
+					t.Fatalf("Expect parseAzureRmAutomationVariableValue to return no error for resource %q and value %s, err: %+v", tc.Resource, tc.Value, err)
+				} else if !tc.Expect(actual) {
+					t.Fatalf("Expect parseAzureRmAutomationVariableValue to return %v instead of %v for resource %q and value %s", tc.ExpectValue, actual, tc.Resource, tc.Value)
+				}
+			}
+		})
+	}
+}
