dd31a8103ecf9d0c0d4369debc00d56045437a94
Fraser <fraser@darwen.tf>
2019-05-06 12:28:47 +0100

    Correct registry name

--- azurerm/resource_arm_container_registry_test.go
+++ azurerm/resource_arm_container_registry_test.go
@@ -585,7 +585,7 @@ resource "azurerm_subnet" "test" {
 }
 
 resource "azurerm_container_registry" "test" {
-  name                = "testAccCr-%d"
+  name                = "testAccCr%d"
   resource_group_name = "${azurerm_resource_group.test.name}"
   location            = "${azurerm_resource_group.test.location}"
   sku                 = "%s"
