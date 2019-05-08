5e7b62b16532868174bb35696f1edbcc2cc31d76
Ryan Bartram <ryan@bartram.ch>
2019-05-08 14:10:04 +0200

    fix test code

--- azurerm/resource_arm_app_service_plan_test.go
+++ azurerm/resource_arm_app_service_plan_test.go
@@ -273,6 +273,27 @@ func TestAccAzureRMAppServicePlan_consumptionPlan(t *testing.T) {
 	})
 }
 
+func TestAccAzureRMAppServicePlan_premiumConsumptionPlan(t *testing.T) {
+	resourceName := "azurerm_app_service_plan.test"
+	ri := tf.AccRandTimeInt()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMAppServicePlanDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMAppServicePlan_premiumConsumptionPlan(ri, testLocation()),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMAppServicePlanExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "sku.0.tier", "ElasticPremium"),
+					resource.TestCheckResourceAttr(resourceName, "sku.0.size", "EP1"),
+				),
+			},
+		},
+	})
+}
+
 func testCheckAzureRMAppServicePlanDestroy(s *terraform.State) error {
 	conn := testAccProvider.Meta().(*ArmClient).appServicePlansClient
 
