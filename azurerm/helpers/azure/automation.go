5868c1ec81630a7e2acbef94817a50071591ee34
Tony Fortes Ramos <amfortesramos@hotmail.com>
2019-05-09 00:29:18 +0200

    Fix validation description

--- azurerm/helpers/azure/automation.go
+++ azurerm/helpers/azure/automation.go
@@ -11,7 +11,7 @@ import (
 func ValidateAutomationAccountName() schema.SchemaValidateFunc {
 	return validation.StringMatch(
 		regexp.MustCompile(`^[0-9a-zA-Z][-0-9a-zA-Z]{4,48}[0-9a-zA-Z]$`),
-		`The account name must not be empty, and must not exceed 50 characters in length.  The account name must start with a letter or number.  The account name can contain letters, numbers, and dashes. The final character must be a letter or a number. The account name length must be from 6 to 50 characters.`,
+		`The account name must start with a letter or number.  The account name can contain letters, numbers, and dashes. The final character must be a letter or a number. The account name length must be from 6 to 50 characters.`,
 	)
 }
 
