d924a0975b4d813affd859190d876e177d734689
Andy Moore <andy@impulsed.net>
2019-05-15 12:25:05 +0100

    expand tests

--- azurerm/resource_arm_application_insights_webtests_test.go
+++ azurerm/resource_arm_application_insights_webtests_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 )
 
-func TestAccAzureRMApplicationInsightsWebTests_basicWeb(t *testing.T) {
+func TestAccAzureRMApplicationInsightsWebTests_basic(t *testing.T) {
 	resourceName := "azurerm_application_insights_webtest.test"
 	ri := tf.AccRandTimeInt()
 	config := testAccAzureRMApplicationInsightsWebTests_basic(ri, testLocation())
@@ -31,6 +31,74 @@ func TestAccAzureRMApplicationInsightsWebTests_basicWeb(t *testing.T) {
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccAzureRMApplicationInsightsWebTests_complete(t *testing.T) {
+	resourceName := "azurerm_application_insights_webtest.test"
+	ri := tf.AccRandTimeInt()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMApplicationInsightsWebTestsDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMApplicationInsightsWebTests_complete(ri, testLocation()),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMApplicationInsightsWebTestExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "provisioning_state", "Succeeded"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccAzureRMApplicationInsightsWebTests_update(t *testing.T) {
+	resourceName := "azurerm_application_insights_webtest.test"
+	ri := tf.AccRandTimeInt()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMApplicationInsightsWebTestsDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMApplicationInsightsWebTests_basic(ri, testLocation()),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMApplicationInsightsWebTestExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "provisioning_state", "Succeeded"),
+					resource.TestCheckResourceAttr(resourceName, "geo_locations.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "frequency", "300"),
+					resource.TestCheckResourceAttr(resourceName, "timeout", "30"),
+				),
+			},
+			{
+				Config: testAccAzureRMApplicationInsightsWebTests_complete(ri, testLocation()),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMApplicationInsightsWebTestExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "provisioning_state", "Succeeded"),
+					resource.TestCheckResourceAttr(resourceName, "geo_locations.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "frequency", "900"),
+					resource.TestCheckResourceAttr(resourceName, "timeout", "120"),
+				),
+			},
+			{
+				Config: testAccAzureRMApplicationInsightsWebTests_basic(ri, testLocation()),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMApplicationInsightsWebTestExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "provisioning_state", "Succeeded"),
+					resource.TestCheckResourceAttr(resourceName, "geo_locations.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "frequency", "300"),
+					resource.TestCheckResourceAttr(resourceName, "timeout", "30"),
+				),
 			},
 		},
 	})
@@ -144,13 +212,46 @@ resource "azurerm_application_insights_webtest" "test" {
   location                = "${azurerm_resource_group.test.location}"
   resource_group_name     = "${azurerm_resource_group.test.name}"
   application_insights_id = "${azurerm_application_insights.test.id}"
-  kind                    = "Ping"
-  frequency               = 300
-  timeout                 = 120
-  enabled                 = true
+  kind                    = "ping"
   geo_locations           = ["us-tx-sn1-azr"]
 
-  test_configuration = << XML
+  test_configuration = <<XML
+<WebTest Name="WebTest1" Id="ABD48585-0831-40CB-9069-682EA6BB3583" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="0" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
+  <Items>
+    <Request Method="GET" Guid="a5f10126-e4cd-570d-961c-cea43999a200" Version="1.1" Url="http://microsoft.com" ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
+  </Items>
+</WebTest>
+XML
+}
+`, rInt, location, rInt, rInt)
+}
+
+func testAccAzureRMApplicationInsightsWebTests_complete(rInt int, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_application_insights" "test" {
+  name                = "acctestappinsights-%d"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  application_type    = "web"
+}
+
+resource "azurerm_application_insights_webtest" "test" {
+  name                    = "acctestappinsightswebtests-%d"
+  location                = "${azurerm_resource_group.test.location}"
+  resource_group_name     = "${azurerm_resource_group.test.name}"
+  application_insights_id = "${azurerm_application_insights.test.id}"
+  kind                    = "ping"
+  frequency               = 900
+  timeout                 = 120
+  enabled                 = true
+  geo_locations           = ["us-tx-sn1-azr", "us-il-ch1-azr"]
+
+  test_configuration = <<XML
 <WebTest Name="WebTest1" Id="ABD48585-0831-40CB-9069-682EA6BB3583" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="0" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
   <Items>
     <Request Method="GET" Guid="a5f10126-e4cd-570d-961c-cea43999a200" Version="1.1" Url="http://microsoft.com" ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
