e9c15421cc19e895802ea324fef3bf9451422592
tombuildsstuff <git@ibuildstuff.co.uk>
2019-05-16 17:13:29 +0200

    d/kubernetes_service_versions: changes from code review

--- azurerm/data_source_kubernetes_service_version.go
+++ azurerm/data_source_kubernetes_service_version.go
@@ -16,15 +16,18 @@ func dataSourceArmKubernetesServiceVersions() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"location": locationSchema(),
+
 			"version_prefix": {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
+
 			"versions": {
 				Type:     schema.TypeList,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 				Computed: true,
 			},
+
 			"latest_version": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -37,7 +40,7 @@ func dataSourceArmKubernetesServiceVersionsRead(d *schema.ResourceData, meta int
 	client := meta.(*ArmClient).containerServicesClient
 	ctx := meta.(*ArmClient).StopContext
 
-	location := d.Get("location").(string)
+	location := azureRMNormalizeLocation(d.Get("location").(string))
 
 	listResp, err := client.ListOrchestrators(ctx, location, "managedClusters")
 	if err != nil {
@@ -51,25 +54,44 @@ func dataSourceArmKubernetesServiceVersionsRead(d *schema.ResourceData, meta int
 	if err != nil {
 		return fmt.Errorf("Cannot set version baseline (likely an issue in go-version): %+v", err)
 	}
-	versions := []string{}
-	for _, rawV := range *listResp.Orchestrators {
-		if rawV.OrchestratorType != nil && rawV.OrchestratorVersion != nil {
-			if *rawV.OrchestratorType == "Kubernetes" {
-				if !strings.HasPrefix(*rawV.OrchestratorVersion, d.Get("version_prefix").(string)) {
+
+	var versions []string
+	versionPrefix := d.Get("version_prefix").(string)
+
+	if props := listResp.OrchestratorVersionProfileProperties; props != nil {
+		if orchestrators := props.Orchestrators; orchestrators != nil {
+			for _, rawV := range *orchestrators {
+				if rawV.OrchestratorType == nil || rawV.OrchestratorVersion == nil {
 					continue
 				}
-				versions = append(versions, *rawV.OrchestratorVersion)
-				v, err := version.NewVersion(*rawV.OrchestratorVersion)
+
+				orchestratorType := *rawV.OrchestratorType
+				kubeVersion := *rawV.OrchestratorVersion
+
+				if !strings.EqualFold(orchestratorType, "Kubernetes") {
+					log.Printf("[DEBUG] Orchestrator %q was not Kubernetes", orchestratorType)
+					continue
+				}
+
+				if versionPrefix != "" && !strings.HasPrefix(kubeVersion, versionPrefix) {
+					log.Printf("[DEBUG] Version %q doesn't match the prefix %q", kubeVersion, versionPrefix)
+					continue
+				}
+
+				versions = append(versions, kubeVersion)
+				v, err := version.NewVersion(kubeVersion)
 				if err != nil {
-					log.Printf("[WARN] Cannot parse orchestrator version '%s'. Skipping because %s", *rawV.OrchestratorVersion, err)
+					log.Printf("[WARN] Cannot parse orchestrator version %q - skipping: %s", kubeVersion, err)
 					continue
 				}
+
 				if v.GreaterThan(lv) {
 					lv = v
 				}
 			}
 		}
 	}
+
 	d.SetId(*listResp.ID)
 	d.Set("versions", versions)
 	d.Set("latest_version", lv.Original())
