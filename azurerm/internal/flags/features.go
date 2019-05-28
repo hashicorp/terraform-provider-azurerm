fdcd5f1ea52f8824ad396a129088d9e4c31741ba
tombuildsstuff <git@ibuildstuff.co.uk>
2019-05-28 15:28:33 +0100

    refactor: moving feature flags internally

--- azurerm/internal/flags/features.go
+++ azurerm/internal/flags/features.go
@@ -1,10 +1,13 @@
-package azurerm
+package flags
 
-import "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/flags"
+import (
+	"os"
+	"strings"
+)
 
 // NOTE: we'll need to add an infobox to MySQL|PostgreSQL Configuration when this goes live
 // since these resources can't support import
 // in addition the virtual resources will need adjusting
 
 // This file contains feature flags for functionality which will prove more challenging to implement en-mass
-var requireResourcesToBeImported = flags.RequireResourcesToBeImported
+var RequireResourcesToBeImported = strings.EqualFold(os.Getenv("ARM_PROVIDER_STRICT"), "true")
