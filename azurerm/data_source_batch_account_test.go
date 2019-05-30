c75bcb7f54ae5432401ea6d617bee6bde84f15fa
Julien Corioland <jucoriol@microsoft.com>
2019-05-30 16:23:06 +0200

    update batch account data source

--- azurerm/data_source_batch_account_test.go
+++ azurerm/data_source_batch_account_test.go
@@ -57,6 +57,30 @@ func TestAccDataSourceAzureRMBatchAccount_complete(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceAzureRMBatchAccount_userSubscription(t *testing.T) {
+	dataSourceName := "data.azurerm_batch_account.test"
+	ri := tf.AccRandTimeInt()
+	rs := acctest.RandString(4)
+	location := testLocation()
+	config := testAccDataSourceAzureBatchAccount_userSubscription(ri, rs, location)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "name", fmt.Sprintf("testaccbatch%s", rs)),
+					resource.TestCheckResourceAttr(dataSourceName, "location", azureRMNormalizeLocation(location)),
+					resource.TestCheckResourceAttr(dataSourceName, "pool_allocation_mode", "UserSubscription"),
+					resource.TestCheckResourceAttr(dataSourceName, "key_vault_reference.#", "1"),
+				),
+			},
+		},
+	})
+}
+
 func testAccDataSourceAzureRMBatchAccount_basic(rInt int, rString string, location string) string {
 	return fmt.Sprintf(`
 resource "azurerm_resource_group" "test" {
@@ -110,4 +134,37 @@ data "azurerm_batch_account" "test" {
   resource_group_name = "${azurerm_resource_group.test.name}"
 }
 `, rInt, location, rString, rString)
+}
+
+func testAccDataSourceAzureBatchAccount_userSubscription(rInt int, rString string, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "testaccRG-%d-batchaccount"
+  location = "%s"
+}
+
+# assuming that you have an existing keyvault and configured for Microsoft Azure Batch for this test to pass
+data "azurerm_key_vault" "test" {
+  name                = "azurebatchkv"
+  resource_group_name = "batch-custom-img-rg"
+}
+
+resource "azurerm_batch_account" "test" {
+  name                 = "testaccbatch%s"
+  resource_group_name  = "${azurerm_resource_group.test.name}"
+  location             = "${azurerm_resource_group.test.location}"
+  
+  pool_allocation_mode = "UserSubscription"
+  
+  key_vault_reference {
+	id  = "${data.azurerm_key_vault.test.id}"
+	url = "${data.azurerm_key_vault.test.vault_uri}"
+  }
+}
+
+data "azurerm_batch_account" "test" {
+  name                = "${azurerm_batch_account.test.name}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+`, rInt, location, rString)
 }
