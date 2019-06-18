8d6b60c923422a2a2f33ea0e93ba1d4fd63b2bb8
Alan Braggins <abraggins@pulsesecure.net>
2019-06-18 12:17:16 +0100

    azurerm_mariadb_firewall_rule validate.IPv4Address

--- azurerm/resource_arm_mariadb_firewall_rule.go
+++ azurerm/resource_arm_mariadb_firewall_rule.go
@@ -8,6 +8,7 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
@@ -37,13 +38,15 @@ func resourceArmMariaDbFirewallRule() *schema.Resource {
 			},
 
 			"start_ip_address": {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validate.IPv4Address,
 			},
 
 			"end_ip_address": {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validate.IPv4Address,
 			},
 		},
 	}
