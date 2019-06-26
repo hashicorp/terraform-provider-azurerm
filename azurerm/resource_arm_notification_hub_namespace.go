c7cb43b7ff34a554c35e48cf5f44c1418dae0ff3
Jeffrey Cline <jcline@microsoft.com>
2019-06-25 18:41:41 -0700

    Fix lint error

--- azurerm/resource_arm_notification_hub_namespace.go
+++ azurerm/resource_arm_notification_hub_namespace.go
@@ -13,7 +13,6 @@ import (
 	"github.com/hashicorp/terraform/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/response"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/suppress"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
