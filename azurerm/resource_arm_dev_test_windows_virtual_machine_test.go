88513444bce0e0d59f7a320b80a9cac174a5de14
Kevin Sisco <kevin.sisco@shell.com>
2019-07-03 00:34:27 -0500

    Fixed whitespace

--- azurerm/resource_arm_dev_test_windows_virtual_machine_test.go
+++ azurerm/resource_arm_dev_test_windows_virtual_machine_test.go
@@ -436,7 +436,7 @@ resource "azurerm_dev_test_virtual_network" "test" {
   name                = "acctestdtvn%d"
   lab_name            = "${azurerm_dev_test_lab.test.name}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  
+
   subnet {
     use_public_ip_address           = "Allow"
     use_in_virtual_machine_creation = "Allow"
