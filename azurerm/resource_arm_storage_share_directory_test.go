44867c447099c419b6439aeca7a6b6dc0a36f852
tombuildsstuff <git@ibuildstuff.co.uk>
2019-07-11 13:52:16 +0200

    doc updates: deprecating the rg field

--- azurerm/resource_arm_storage_share_directory_test.go
+++ azurerm/resource_arm_storage_share_directory_test.go
@@ -327,7 +327,6 @@ resource "azurerm_storage_account" "test" {
 
 resource "azurerm_storage_share" "test" {
   name                 = "fileshare"
-  resource_group_name  = "${azurerm_resource_group.test.name}"
   storage_account_name = "${azurerm_storage_account.test.name}"
   quota                = 50
 }
