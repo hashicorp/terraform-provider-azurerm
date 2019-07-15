c55a680324d8ddc3cb73d20ecfc153c77cf9a386
patst <patrick.steinig@googlemail.com>
2019-07-15 08:54:38 +0200

    Fix golangci-lint: goimports

--- azurerm/resource_arm_private_dns_a_record_test.go
+++ azurerm/resource_arm_private_dns_a_record_test.go
@@ -2,10 +2,10 @@ package azurerm
 
 import (
 	"fmt"
-	"github.com/Azure/azure-sdk-for-go/services/privatedns/mgmt/2018-09-01/privatedns"
 	"net/http"
 	"testing"
 
+	"github.com/Azure/azure-sdk-for-go/services/privatedns/mgmt/2018-09-01/privatedns"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
