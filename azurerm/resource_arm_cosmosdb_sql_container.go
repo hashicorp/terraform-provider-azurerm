32133ce63097cd8fa9fe1e33bf9d56bbdba43645
joseph.denheen <joseph.denheen@euromoneyplc.com>
2019-07-18 15:11:54 +0100

    Adding base resource that can't be changed via powershell mainly Unique Key Policy Paths

---
+++ azurerm/resource_arm_cosmosdb_sql_container.go
@@ -0,0 +1,215 @@
+package azurerm
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/Azure/azure-sdk-for-go/services/cosmos-db/mgmt/2015-04-08/documentdb"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/response"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/validate"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
+)
+
+func resourceArmCosmosDbSQLContainer() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceArmCosmosDbSQLContainerCreateUpdate,
+		Read:   resourceArmCosmosDbSQLContainerRead,
+		Update: resourceArmCosmosDbSQLContainerCreateUpdate,
+		Delete: resourceArmCosmosDbSQLContainerDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validate.CosmosEntityName,
+			},
+
+			"resource_group_name": azure.SchemaResourceGroupName(),
+
+			"database_name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validate.CosmosEntityName,
+			},
+
+			"account_name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validate.CosmosAccountName,
+			},
+
+			"partition_key_paths": {
+				Type:         schema.TypeString,
+				Required:     false,
+				Optional:     true,
+				ForceNew:     false,
+				ValidateFunc: validate.NoEmptyStrings,
+			},
+
+			"unique_key_policy": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"path": {
+							Type:         schema.TypeString,
+							Required:     true,
+							ValidateFunc: validate.NoEmptyStrings,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceArmCosmosDbSQLContainerCreateUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ArmClient).cosmosAccountsClient
+	ctx := meta.(*ArmClient).StopContext
+
+	name := d.Get("name").(string)
+	resourceGroup := d.Get("resource_group_name").(string)
+	database := d.Get("database_name").(string)
+	account := d.Get("account_name").(string)
+	partitionkeypaths := d.Get("partition_key_paths").(string)
+
+	if requireResourcesToBeImported && d.IsNewResource() {
+		existing, err := client.GetSQLContainer(ctx, resourceGroup, account, database, name)
+		if err != nil {
+			if !utils.ResponseWasNotFound(existing.Response) {
+				return fmt.Errorf("Error checking for presence of creating Cosmos SQL Container %s (Account: %s, Database:%s): %+v", name, account, database, err)
+			}
+		} else {
+			id, err := azure.CosmosGetIDFromResponse(existing.Response)
+			if err != nil {
+				return fmt.Errorf("Error generating import ID for Cosmos SQL Container '%s' (Account: %s, Database:%s)", name, account, database)
+			}
+
+			return tf.ImportAsExistsError("azurerm_cosmosdb_sql_container", id)
+		}
+	}
+
+	db := documentdb.SQLContainerCreateUpdateParameters{
+		SQLContainerCreateUpdateProperties: &documentdb.SQLContainerCreateUpdateProperties{
+			Resource: &documentdb.SQLContainerResource{
+				ID: &name,
+				PartitionKey: &documentdb.ContainerPartitionKey{
+					Paths: &[]string{partitionkeypaths},
+					Kind:  "Hash",
+				},
+				UniqueKeyPolicy: &documentdb.UniqueKeyPolicy{
+					UniqueKeys: expandSQLContainerUniqueKeyPolicy(d.Get("unique_key_policy")),
+				},
+			},
+			Options: map[string]*string{},
+		},
+	}
+
+	future, err := client.CreateUpdateSQLContainer(ctx, resourceGroup, account, database, name, db)
+	if err != nil {
+		return fmt.Errorf("Error issuing create/update request for Cosmos SQL Container %s (Account: %s, Database:%s): %+v", name, account, database, err)
+	}
+
+	if err = future.WaitForCompletionRef(ctx, client.Client); err != nil {
+
+		return fmt.Errorf("Error waiting on create/update future for Cosmos SQL Container %s (Account: %s, Database:%s): %+v", name, account, database, err)
+	}
+
+	resp, err := client.GetSQLContainer(ctx, resourceGroup, account, database, name)
+	if err != nil {
+		return fmt.Errorf("Error making get request for Cosmos SQL Container %s (Account: %s, Database:%s): %+v", name, account, database, err)
+	}
+
+	id, err := azure.CosmosGetIDFromResponse(resp.Response)
+	if err != nil {
+		return fmt.Errorf("Error retrieving the ID for Cosmos SQL Container '%s' (Account: %s, Database:%s) ID: %v", name, account, database, err)
+	}
+	d.SetId(id)
+
+	return resourceArmCosmosDbSQLContainerRead(d, meta)
+}
+
+func resourceArmCosmosDbSQLContainerRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ArmClient).cosmosAccountsClient
+	ctx := meta.(*ArmClient).StopContext
+	name := d.Get("name").(string)
+
+	id, err := azure.ParseCosmosDatabaseID(d.Id())
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.GetSQLContainer(ctx, id.ResourceGroup, id.Account, id.Database, name)
+	if err != nil {
+		if utils.ResponseWasNotFound(resp.Response) {
+			log.Printf("[INFO] Error reading Cosmos SQL Container %s (Account %s) - removing from state", id.Database, name)
+			d.SetId("")
+			return nil
+		}
+
+		return fmt.Errorf("Error reading Cosmos SQL Container %s (Account %s): %+v", id.Database, name, err)
+	}
+
+	d.Set("resource_group_name", id.ResourceGroup)
+	d.Set("account_name", id.Account)
+	d.Set("name", name)
+	d.Set("database_name", id.Database)
+	if props := resp.SQLContainerProperties; props != nil {
+		d.Set("name", props.ID)
+	}
+
+	return nil
+}
+
+func resourceArmCosmosDbSQLContainerDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ArmClient).cosmosAccountsClient
+	ctx := meta.(*ArmClient).StopContext
+	name := d.Get("name").(string)
+
+	id, err := azure.ParseCosmosDatabaseID(d.Id())
+	if err != nil {
+		return err
+	}
+
+	future, err := client.DeleteSQLContainer(ctx, id.ResourceGroup, id.Account, id.Database, name)
+	if err != nil {
+		if !response.WasNotFound(future.Response()) {
+			return fmt.Errorf("Error deleting Cosmos SQL Container %s (Account %s): %+v", id.Database, name, err)
+		}
+	}
+
+	err = future.WaitForCompletionRef(ctx, client.Client)
+	if err != nil {
+		return fmt.Errorf("Error waiting on delete future for Cosmos SQL Container %s (Account %s): %+v", id.Database, id.Account, err)
+	}
+
+	return nil
+}
+
+func expandSQLContainerUniqueKeyPolicy(input interface{}) *[]documentdb.UniqueKey {
+	outputs := make([]documentdb.UniqueKey, 0)
+	var paths []string
+
+	for _, i := range input.(*schema.Set).List() {
+		b := i.(map[string]interface{})
+		c := &[]string{b["path"].(string)}
+		d := *c
+		e := strings.Join(d, "")
+		paths = append(paths, e)
+	}
+	outputs = append(outputs, documentdb.UniqueKey{
+		Paths: &paths,
+	})
+	return &outputs
+}
