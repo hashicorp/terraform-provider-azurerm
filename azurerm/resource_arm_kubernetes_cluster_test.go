81d53edda59eac0e0e0403fdc727dad0ede9627c
Matthew Frahry <mbfrahry@gmail.com>
2019-07-29 11:52:23 -0700

    remove test change

--- azurerm/resource_arm_kubernetes_cluster_test.go
+++ azurerm/resource_arm_kubernetes_cluster_test.go
@@ -1051,8 +1051,7 @@ resource "azurerm_kubernetes_cluster" "test" {
     network_policy     = "azure"
     dns_service_ip     = "10.10.0.10"
     docker_bridge_cidr = "172.18.0.1/16"
-		service_cidr       = "10.10.0.0/16"
-		load_balancer_sku  = "Basic"
+    service_cidr       = "10.10.0.0/16"
   }
 }
 `, rInt, location, rInt, rInt, rInt, clientId, clientSecret)
