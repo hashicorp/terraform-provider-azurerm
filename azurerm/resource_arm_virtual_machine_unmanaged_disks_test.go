b068ba09384c54466e8b9e63418b57de75569787
Venkaiah Juvvigunta <venkaiah.juvvigunta@olacabs.com>
2019-08-04 04:08:35 +0530

    modified the test case of addtional capabilites to suit the requirements of ultra ssd

--- azurerm/resource_arm_virtual_machine_unmanaged_disks_test.go
+++ azurerm/resource_arm_virtual_machine_unmanaged_disks_test.go
@@ -1896,7 +1896,7 @@ resource "azurerm_resource_group" "test" {
 
 resource "azurerm_virtual_network" "test" {
   name                = "acctvn-%d"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/28"]
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
 }
@@ -1905,7 +1905,7 @@ resource "azurerm_subnet" "test" {
   name                 = "acctsub-%d"
   resource_group_name  = "${azurerm_resource_group.test.name}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
-  address_prefix       = "10.0.2.0/24"
+  address_prefix       = "10.0.0.0/29"
 }
 
 resource "azurerm_network_interface" "test" {
@@ -1920,63 +1920,55 @@ resource "azurerm_network_interface" "test" {
   }
 }
 
-resource "azurerm_storage_account" "test" {
-  name                     = "accsa%d"
-  resource_group_name      = "${azurerm_resource_group.test.name}"
-  location                 = "${azurerm_resource_group.test.location}"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "staging"
-  }
-}
-
-resource "azurerm_storage_container" "test" {
-  name                  = "vhds"
-  resource_group_name   = "${azurerm_resource_group.test.name}"
-  storage_account_name  = "${azurerm_storage_account.test.name}"
-  container_access_type = "private"
-}
-
 resource "azurerm_virtual_machine" "test" {
   name                  = "acctvm-%d"
   location              = "${azurerm_resource_group.test.location}"
   resource_group_name   = "${azurerm_resource_group.test.name}"
   network_interface_ids = ["${azurerm_network_interface.test.id}"]
-  vm_size               = "Standard_D1_v2"
+  vm_size               = "Standard_D2S_V3"
 
   additional_capabilities {
     ultra_ssd_enabled = true
   }
 
   storage_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2012-Datacenter"
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
 
   storage_os_disk {
-    name          = "myosdisk1"
-    vhd_uri       = "${azurerm_storage_account.test.primary_blob_endpoint}${azurerm_storage_container.test.name}/myosdisk1.vhd"
-    caching       = "ReadWrite"
-    create_option = "FromImage"
+    name              = "myosdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    os_type           = "linux"
+    managed_disk_type = "Premium_LRS"
+    disk_size_gb      = "64"
   }
 
+  storage_data_disk {
+    name          = "mydatadisk1"
+    caching       = "None"
+    create_option = "Empty"
+    managed_disk_type = "UltraSSD_LRS"
+    disk_size_gb      = "64"
+    lun = 1
+  }
   os_profile {
-    computer_name  = "winhost01"
+    computer_name  = "hostname"
     admin_username = "testadmin"
     admin_password = "Password1234!"
   }
 
-  os_profile_windows_config {
-    winrm {
-      protocol = "http"
-    }
+  os_profile_linux_config {
+    disable_password_authentication = false
+
+  }
+
+zones = ["3"]
 }
-}
-`, rInt, location, rInt, rInt, rInt, rInt, rInt)
+`, rInt, location, rInt, rInt, rInt, rInt)
 }
 
 func testAccAzureRMVirtualMachine_winRMConfig(rInt int, location string) string {
