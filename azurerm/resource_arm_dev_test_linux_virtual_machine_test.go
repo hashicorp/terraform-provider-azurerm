7730d0d59a603525c15fc8657a0b41c1444bdb51
Kevin Sisco <kevin.sisco@shell.com>
2019-08-04 22:49:50 -0500

    Updated test to use a VM with premium storage.

--- azurerm/resource_arm_dev_test_linux_virtual_machine_test.go
+++ azurerm/resource_arm_dev_test_linux_virtual_machine_test.go
@@ -429,13 +429,13 @@ resource "azurerm_dev_test_linux_virtual_machine" "test" {
   lab_name                   = "${azurerm_dev_test_lab.test.name}"
   resource_group_name        = "${azurerm_resource_group.test.name}"
   location                   = "${azurerm_resource_group.test.location}"
-  size                       = "Standard_F2"
+  size                       = "Standard_F2s"
   username                   = "acct5stU5er"
   password                   = "Pa$$w0rd1234!"
   disallow_public_ip_address = true
   lab_virtual_network_id     = "${azurerm_dev_test_virtual_network.test.id}"
   lab_subnet_name            = "${azurerm_dev_test_virtual_network.test.subnet.0.name}"
-  storage_type               = "Standard"
+  storage_type               = "Premium"
 
   gallery_image_reference {
     offer     = "UbuntuServer"
