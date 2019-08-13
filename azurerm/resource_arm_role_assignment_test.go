87f17bbd889fd1d84f50b1685965a4b2b0b7c77b
nexxai <nexxai@gmail.com>
2019-08-13 09:12:11 -0600

    Fixed broken test; is passing now

--- azurerm/resource_arm_role_assignment_test.go
+++ azurerm/resource_arm_role_assignment_test.go
@@ -473,7 +473,7 @@ resource "azurerm_management_group" "test" {
 
 resource "azurerm_role_assignment" "test" {
 	scope              = "${azurerm_management_group.test.id}"
-	role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.test.id}"
+	role_definition_id = "${data.azurerm_role_definition.test.id}"
 	principal_id       = "${data.azurerm_client_config.test.service_principal_object_id}"
 }
 `, groupId)
