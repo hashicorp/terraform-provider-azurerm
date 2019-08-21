98b946f4422eabd04b0b654383e1724c5d55feb4
Jeffrey Cline <jcline@microsoft.com>
2019-08-20 18:54:33 -0700

    [WIP] Tuning Schema

--- azurerm/resource_arm_front_door_firewall_policy.go
+++ azurerm/resource_arm_front_door_firewall_policy.go
@@ -28,43 +28,220 @@ func resourceArmFrontDoorFirewallPolicy() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Type:     schema.TypeString,
-				Computed: true
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
 			},
 
-			"location": azure.SchemaLocation(),
+			"location": {
+				Type:     schema.TypeString,
+				Computed: true,
+			}
 
-			"policy_settings": {
+			"enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+
+			"mode": {
+				Type: schema.TypeString,
+				Required: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					string(frontdoor.Detection),
+					string(frontdoor.Prevention),
+				}, false),
+			},
+
+			"custom_block_response_status_code": {
+				Type: schema.Int,
+				Optional: true,
+			},
+
+			"redirect_url" : {
+				Type: schema.TypeString,
+				Optional: true,
+			},
+
+			"custom_block_response_body": {
+				Type: schema.TypeString,
+				Optional: true,
+			},
+
+			"custom_rule": {
 				Type:     schema.TypeList,
 				MaxItems: 100,
 				Required: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
+						"name" : {
+							Type: schema.TypeString,
+							Optional: true,
+						},
+						"priority": {
+							Type: schema.Int,
+							Optional: true,
+							Default: 1,
+						},
 						"enabled": {
 							Type:     schema.TypeBool,
 							Optional: true,
 							Default:  true,
 						},
-						"policy_mode": {
+						"rule_type": {
 							Type: schema.TypeString,
 							Required: true,
 							ValidateFunc: validation.StringInSlice([]string{
-								string(frontdoor.Detection),
-								string(frontdoor.Prevention),
+								string(frontdoor.MatchRule),
+								string(frontdoor.RateLimitRule),
 							}, false),
 						},
-						"RedirectURL" : {
-							Type: schema.TypeString,
-							Optional: true,
-						},
-						"custom_block_response_status_code": {
+						"rate_limit_duration_in_minutes": {
 							Type: schema.Int,
 							Optional: true,
+							Default: 1,
+						},
+						"rate_limit_threshold": {
+							Type: schema.Int,
+							Optional: true,
+							Default: 10,
+						},
+						"action": {
+							Type: schema.TypeString,
+							ValidateFunc: validation.StringInSlice([]string{
+								string(frontdoor.Allow),
+								string(frontdoor.Block),
+								string(frontdoor.Trim),
+								string(frontdoor.Log),
+								string(frontdoor.Redirect),
+							}, false),
 						},
 						"custom_block_response_body": {
 							Type: schema.TypeString,
 							Optional: true,
-						}
+						},
+						"match_condition" : {
+							Type:     schema.TypeList,
+							Optional: true,
+							MaxItems: 100,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"match_variable": {
+										Type:     schema.TypeString,
+										Required: true,
+										ValidateFunc: validation.StringInSlice([]string{
+											string(frontdoor.Cookies),
+											string(frontdoor.PostArgs),
+											string(frontdoor.QueryString),
+											string(frontdoor.RemoteAddr),
+											string(frontdoor.RequestBody),
+											string(frontdoor.RequestHeader),
+											string(frontdoor.RequestMethod),
+											string(frontdoor.RequestURI),
+										}, false),
+									},
+									"selector": {
+										Type:     schema.TypeString,
+										Optional: true,
+										ValidateFunc: validation.StringInSlice([]string{
+											string(frontdoor.Cookies),
+											string(frontdoor.PostArgs),
+											string(frontdoor.QueryString),
+											string(frontdoor.RequestHeader),
+										}, false),
+									},
+									"operator": {
+										Type:     schema.TypeString,
+										Required: true,
+										ValidateFunc: validation.StringInSlice([]string{
+											string(frontdoor.Any),
+											string(frontdoor.BeginsWith),
+											string(frontdoor.Contains),
+											string(frontdoor.EndsWith),
+											string(frontdoor.Equal),
+											string(frontdoor.GeoMatch),
+											string(frontdoor.GreaterThan),
+											string(frontdoor.GreaterThanOrEqual),
+											string(frontdoor.IPMatch),
+											string(frontdoor.LessThan),
+											string(frontdoor.LessThanOrEqual),
+											string(frontdoor.RegEx),
+										}, false),
+									},
+									"condition": {
+										Type:     schema.TypeString,
+										Optional: true,
+										ValidateFunc: validation.StringInSlice([]string{
+											"Is",
+											"Is Not",
+											"Contains",
+											"Not Contains",
+										}, false),
+										Default: "Is"
+									},
+									"match_value": {
+										Type:     schema.TypeList,
+										Required: true,
+										MaxItems: 100,
+										Elem: &schema.Schema{
+											Type: schema.TypeString,
+										},
+									},
+									"transforms": {
+										Type:     schema.TypeList,
+										Optional: true,
+										MaxItems: 6,
+										Elem: &schema.Schema{
+											Type: schema.TypeString,
+											ValidateFunc: validation.StringInSlice([]string{
+												string(frontdoor.Lowercase),
+												string(frontdoor.RemoveNulls),
+												string(frontdoor.Trim),
+												string(frontdoor.Uppercase),
+												string(frontdoor.URLDecode),
+												string(frontdoor.URLEncode),
+											}, false),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+
+			"managed_rule": {
+				Type:     schema.TypeList,
+				MaxItems: 100,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"type" : {
+							Type: schema.TypeString,
+							Optional: true,
+						},
+						"version" : {
+							Type: schema.TypeString,
+							Optional: true,
+						},
+						"override" : {
+							Type:     schema.TypeList,
+							MaxItems: 100,
+							Required: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"rule_group_name" : {
+										Type: schema.TypeString,
+										Optional: true,
+									},
+									"rules" : {
+										Type: schema.TypeString,
+										Optional: true,
+									},
+
+								},
+							},
+						},
 					},
 				},
 			},
