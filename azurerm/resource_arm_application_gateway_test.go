ca20cc76113b2091dabea2d11e7e18282cabedcc
kt <kt@katbyte.me>
2019-09-02 09:55:46 -0700

    fix failing test

--- azurerm/resource_arm_application_gateway_test.go
+++ azurerm/resource_arm_application_gateway_test.go
@@ -1965,7 +1965,7 @@ locals {
   request_routing_rule_name      = "${azurerm_virtual_network.test.name}-rqrt"
 }
 
-resource "azurerm_public_ip" "testStd" {
+resource "azurerm_public_ip" "teststd" {
   name                = "acctest-PubIpStd-%[2]d"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
@@ -1996,7 +1996,7 @@ resource "azurerm_application_gateway" "test" {
 
   frontend_ip_configuration {
     name                 = "${local.frontend_ip_configuration_name}"
-    public_ip_address_id = "${azurerm_public_ip.testStd.id}"
+    public_ip_address_id = "${azurerm_public_ip.teststd.id}"
   }
 
   backend_address_pool {
