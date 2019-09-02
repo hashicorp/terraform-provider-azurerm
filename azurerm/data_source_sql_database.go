015e3419f53efcea855b83f512c8b53634df23d5
Lee Wilson <264361+leewilson86@users.noreply.github.com>
2019-09-02 14:20:09 +0100

    Added data source return values

--- azurerm/data_source_sql_database.go
+++ azurerm/data_source_sql_database.go
@@ -2,11 +2,11 @@ package azurerm
 
 import (
 	"fmt"
+	"github.com/Azure/azure-sdk-for-go/services/preview/sql/mgmt/2015-05-01-preview/sql"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
-	"github.com/Azure/azure-sdk-for-go/services/preview/sql/mgmt/2015-05-01-preview/sql"
 )
 
 func dataSourceSqlDatabase() *schema.Resource {
@@ -14,36 +14,14 @@ func dataSourceSqlDatabase() *schema.Resource {
 		Read: dataSourceArmSqlDatabaseRead,
 
 		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-
-			"resource_group_name": azure.SchemaResourceGroupNameForDataSource(),
-
-			"location": {
+			"collation": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
 
-			"server_name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-
-			"create_mode": {
+			"default_secondary_location": {
 				Type:     schema.TypeString,
 				Computed: true,
-			},
-
-			"source_database_id": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-
-			"restore_point_in_time": {
-				Type:         schema.TypeString,
-				Computed:     true,
 			},
 
 			"edition": {
@@ -51,13 +29,28 @@ func dataSourceSqlDatabase() *schema.Resource {
 				Computed: true,
 			},
 
-			"collation": {
+			"elastic_pool_name": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
 
-			"max_size_bytes": {
+			"failover_group_id": {
 				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"location": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"read_scale": {
+				Type:     schema.TypeBool,
 				Computed: true,
 			},
 
@@ -71,34 +64,11 @@ func dataSourceSqlDatabase() *schema.Resource {
 				Computed: true,
 			},
 
-			"source_database_deletion_date": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
+			"resource_group_name": azure.SchemaResourceGroupNameForDataSource(),
 
-			"elastic_pool_name": {
+			"server_name": {
 				Type:     schema.TypeString,
-				Computed: true,
-			},
-
-			"encryption": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-
-			"creation_date": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-
-			"default_secondary_location": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-
-			"read_scale": {
-				Type:     schema.TypeBool,
-				Computed: true,
+				Required: true,
 			},
 
 			"tags": tags.Schema(),
@@ -111,77 +81,53 @@ func dataSourceArmSqlDatabaseRead(d *schema.ResourceData, meta interface{}) erro
 	ctx := meta.(*ArmClient).StopContext
 
 	name := d.Get("name").(string)
-	serverName := d.Get("server_name").(string)
 	resourceGroup := d.Get("resource_group_name").(string)
+	serverName := d.Get("server_name").(string)
 
 	resp, err := client.Get(ctx, resourceGroup, serverName, name, "")
 	if err != nil {
 		if utils.ResponseWasNotFound(resp.Response) {
-			return fmt.Errorf("Sql Database %q was not found in Resource Group %q", name, resourceGroup)
+			return fmt.Errorf("SQL Database %q (server %q / resource group %q) was not found", name, serverName, resourceGroup)
 		}
 
-		return fmt.Errorf("Error retrieving Sql Database %q (Resource Group %q): %s", name, resourceGroup, err)
+		return fmt.Errorf("Error retrieving SQL Database %q (server %q / resource group %q): %s", name, serverName, resourceGroup, err)
 	}
 
 	if id := resp.ID; id != nil {
-		d.SetId(*resp.ID)
+		d.SetId(*id)
 	}
 
-	if location := resp.Location; location != nil {
-		d.Set("location", azure.NormalizeLocation(*location))
+	d.Set("collation", resp.Collation)
+
+	if dsLocation := resp.ElasticPoolName; dsLocation != nil {
+		d.Set("default_secondary_location", dsLocation)
 	}
 
-	if props := resp.DatabaseProperties; props != nil {
-		d.Set("collation", props.Collation)
-		d.Set("collation", props.Collation)
-		d.Set("default_secondary_location", props.DefaultSecondaryLocation)
-		d.Set("edition", string(props.Edition))
-		d.Set("elastic_pool_name", props.ElasticPoolName)
-		d.Set("max_size_bytes", props.MaxSizeBytes)
-		d.Set("requested_service_objective_name", string(props.RequestedServiceObjectiveName))
+	d.Set("edition", string(resp.Edition))
 
-		if cd := props.CreationDate; cd != nil {
-			d.Set("creation_date", cd.String())
-		}
-
-		if rsoid := props.RequestedServiceObjectiveID; rsoid != nil {
-			d.Set("requested_service_objective_id", rsoid.String())
-		}
-
-		if rpit := props.RestorePointInTime; rpit != nil {
-			d.Set("restore_point_in_time", rpit.String())
-		}
-
-		if sddd := props.SourceDatabaseDeletionDate; sddd != nil {
-			d.Set("source_database_deletion_date", sddd.String())
-		}
-
-		d.Set("encryption", flattenEncryptionStatus(props.TransparentDataEncryption))
-
-		readScale := props.ReadScale
-		if readScale == sql.ReadScaleEnabled {
-			d.Set("read_scale", true)
-		} else {
-			d.Set("read_scale", false)
-		}
+	if ep := resp.ElasticPoolName; ep != nil {
+		d.Set("elastic_pool_name", ep)
 	}
 
-	//
-	//
-	//d.Set("name", resp.Name)
-	//d.Set("resource_group_name", resourceGroup)
-	//if location := resp.Location; location != nil {
-	//	d.Set("location", azure.NormalizeLocation(*location))
-	//}
-	//
-	//d.Set("server_name", serverName)
-	//
-	//if props := resp.DatabaseProperties; props != nil {
-	//	// TODO: set `create_mode` & `source_database_id` once this issue is fixed:
-	//	// https://github.com/Azure/azure-rest-api-specs/issues/1604
-	//
+	if fogID := resp.FailoverGroupID; fogID != nil {
+		d.Set("failover_group_id", fogID)
+	}
 
-	//}
+	d.Set("location", azure.NormalizeLocation(*resp.Location))
+
+	readScale := false
+	if resp.ReadScale == sql.ReadScaleEnabled {
+		readScale = true
+	}
+	d.Set("read_scale", readScale)
+
+	if rsoID := resp.RequestedServiceObjectiveID; rsoID != nil {
+		d.Set("requested_service_objective_id", rsoID)
+	}
+
+	if rsoName := resp.RequestedServiceObjectiveID; rsoName != nil {
+		d.Set("requested_service_objective_name", rsoName)
+	}
 
 	return tags.FlattenAndSet(d, resp.Tags)
 }
