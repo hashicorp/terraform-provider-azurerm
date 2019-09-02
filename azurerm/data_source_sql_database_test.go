c2f3542329be8c5742083908a668e9bcd4c45856
Lee Wilson <264361+leewilson86@users.noreply.github.com>
2019-09-02 16:38:27 +0100

    Added more checks to basic test

--- azurerm/data_source_sql_database_test.go
+++ azurerm/data_source_sql_database_test.go
@@ -22,9 +22,13 @@ func TestAccDataSourceAzureRMSqlDatabase_basic(t *testing.T) {
 				Config: testAccDataSourceAzureRMSqlDatabase_basic(ri, location),
 				Check: resource.ComposeTestCheckFunc(
 					testCheckAzureRMSqlDatabaseExists(dataSourceName),
+					resource.TestCheckResourceAttrSet(dataSourceName, "collation"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "edition"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "location"),
 					resource.TestCheckResourceAttrSet(dataSourceName, "name"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "server_name"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "read_scale"),
 					resource.TestCheckResourceAttrSet(dataSourceName, "resource_group_name"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "server_name"),
 					resource.TestCheckResourceAttr(dataSourceName, "tags.%", "0"),
 				),
 			},
@@ -38,9 +42,9 @@ func testAccDataSourceAzureRMSqlDatabase_basic(rInt int, location string) string
 %s
 
 data "azurerm_sql_database" "test" {
-  name                = "${azurerm_sql_database.test.name}"
-  server_name         = "${azurerm_sql_database.test.server_name}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+	name                = "${azurerm_sql_database.test.name}"
+	server_name         = "${azurerm_sql_database.test.server_name}"
+	resource_group_name = "${azurerm_resource_group.test.name}"
 }
 `, template)
 }
