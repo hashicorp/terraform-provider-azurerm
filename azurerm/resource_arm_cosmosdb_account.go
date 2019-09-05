ef0378f13ef88a7d28adecdab1746567a00fabd6
Tracy Holmes <tracypholmes@gmail.com>
2019-09-05 09:00:12 -0500

    reverts sdk name check workaround

--- azurerm/resource_arm_cosmosdb_account.go
+++ azurerm/resource_arm_cosmosdb_account.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
 	"regexp"
 	"strings"
 	"time"
@@ -332,14 +331,11 @@ func resourceArmCosmosDbAccountCreate(d *schema.ResourceData, meta interface{}) 
 
 	r, err := client.CheckNameExists(ctx, name)
 	if err != nil {
-		// todo remove when https://github.com/Azure/azure-sdk-for-go/issues/5157 is fixed
-		if !utils.ResponseWasStatusCode(r, http.StatusInternalServerError) {
-			return fmt.Errorf("Error checking if CosmosDB Account %q already exists (Resource Group %q): %+v", name, resourceGroup, err)
-		}
-	} else {
-		if !utils.ResponseWasNotFound(r) {
-			return fmt.Errorf("CosmosDB Account %s already exists, please import the resource via terraform import", name)
-		}
+		return fmt.Errorf("Error checking if CosmosDB Account %q already exists (Resource Group %q): %+v", name, resourceGroup, err)
+	}
+
+	if !utils.ResponseWasNotFound(r) {
+		return fmt.Errorf("CosmosDB Account %s already exists, please import the resource via terraform import", name)
 	}
 
 	//hacky, todo fix up once deprecated field 'failover_policy' is removed
