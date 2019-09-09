31334d5dfa0b4e89e4d4f6a9131d705f7abb8a94
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2019-09-09 03:48:05 +0200

    Fix lint errors

--- azurerm/resource_arm_app_service_source_control.go
+++ azurerm/resource_arm_app_service_source_control.go
@@ -107,8 +107,8 @@ func resourceArmAppServiceSourceControlCreate(d *schema.ResourceData, meta inter
 
 	sourceControl := web.SiteSourceControl{
 		SiteSourceControlProperties: &web.SiteSourceControlProperties{
-			RepoURL: utils.String(repoUrl),
-			Branch:  utils.String(branch),
+			RepoURL: 				   utils.String(repoUrl),
+			Branch:                    utils.String(branch),
 			DeploymentRollbackEnabled: utils.Bool(deploymentRollbackEnabled),
 			IsManualIntegration:       utils.Bool(manualIntegrationEnabled),
 		},
