13204423b1f2b46aa3daf8ba8a3ebb23e661af8b
kt <kt@katbyte.me>
2019-09-09 20:03:48 -0700

    make fmt

--- azurerm/provider.go
+++ azurerm/provider.go
@@ -556,13 +556,13 @@ func Provider() terraform.ResourceProvider {
 func providerConfigure(p *schema.Provider) schema.ConfigureFunc {
 	return func(d *schema.ResourceData) (interface{}, error) {
 
-		auxTenants := []string{}
+		var auxTenants []string
 		if v, ok := d.Get("auxiliary_tenant_ids").([]interface{}); ok && len(v) > 0 {
 			auxTenants = *utils.ExpandStringSlice(v)
 		} else {
 			auxTenants = strings.Split(os.Getenv("ARM_AUXILIARY_TENANT_IDS"), ";")
 		}
-		
+
 		builder := &authentication.Builder{
 			SubscriptionID:     d.Get("subscription_id").(string),
 			ClientID:           d.Get("client_id").(string),
