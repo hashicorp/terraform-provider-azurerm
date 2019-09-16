ab864ae219e963c038dd32e24a5d4aa9d6fd3857
rob <r0bnet@users.noreply.github.com>
2019-09-16 15:47:43 +0200

    add test for vnet integration

--- azurerm/resource_arm_data_factory_integration_runtime_managed_test.go
+++ azurerm/resource_arm_data_factory_integration_runtime_managed_test.go
@@ -36,6 +36,29 @@ func TestAccAzureRMDataFactoryIntegrationRuntimeManaged_basic(t *testing.T) {
 	})
 }
 
+func TestAccAzureRMDataFactoryIntegrationRuntimeManaged_vnetIntegration(t *testing.T) {
+	ri := tf.AccRandTimeInt()
+	config := testAccAzureRMDataFactoryIntegrationRuntimeManaged_vnetIntegration(ri, testLocation())
+	resourceName := "azurerm_data_factory_integration_runtime_managed.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMDataFactoryIntegrationRuntimeManagedDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMDataFactoryIntegrationRuntimeManagedExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "vnet_integration.#", "1"),
+					resource.TestCheckResourceAttrSet(resourceName, "vnet_integration.0.vnet_id"),
+					resource.TestCheckResourceAttrSet(resourceName, "vnet_integration.0.subnet_name"),
+				),
+			},
+		},
+	})
+}
+
 func testAccAzureRMDataFactoryIntegrationRuntimeManaged_basic(rInt int, location string) string {
 	return fmt.Sprintf(`
 resource "azurerm_resource_group" "test" {
@@ -50,7 +73,7 @@ resource "azurerm_data_factory" "test" {
 }
 
 resource "azurerm_data_factory_integration_runtime_managed" "test" {
-  name                = "managed-ssis-integration-runtime"
+  name                = "managed-integration-runtime"
   data_factory_name   = azurerm_data_factory.test.name
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
@@ -62,6 +85,53 @@ resource "azurerm_data_factory_integration_runtime_managed" "test" {
   license_type                     = "LicenseIncluded"
 }
 `, rInt, location, rInt)
+}
+
+func testAccAzureRMDataFactoryIntegrationRuntimeManaged_vnetIntegration(rInt int, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_virtual_network" "test" {
+  name                = "acctestvnet%d"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = "acctestsubnet%d"
+  resource_group_name  = "${azurerm_resource_group.test.name}"
+  virtual_network_name = "${azurerm_virtual_network.test.name}"
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_data_factory" "test" {
+  name                = "acctestdfirm%d"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
+resource "azurerm_data_factory_integration_runtime_managed" "test" {
+  name                = "managed-integration-runtime"
+  data_factory_name   = azurerm_data_factory.test.name
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+
+  node_size                        = "Standard_D8_v3"
+  number_of_nodes                  = 2
+  max_parallel_executions_per_node = 8
+  edition                          = "Standard"
+  license_type                     = "LicenseIncluded"
+
+  vnet_integration {
+    vnet_id     = "${azurerm_virtual_network.test.id}"
+    subnet_name = "${azurerm_subnet.test.name}"
+  }
+}
+`, rInt, location, rInt, rInt, rInt)
 }
 
 func testCheckAzureRMDataFactoryIntegrationRuntimeManagedExists(name string) resource.TestCheckFunc {
