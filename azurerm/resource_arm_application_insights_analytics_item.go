ede1e7a5b36c1bed6583f77902845f449dd22ef9
Stuart Leeks <stuartle@microsoft.com>
2019-09-18 15:42:42 +0100

    Implement resource

--- azurerm/resource_arm_application_insights_analytics_item.go
+++ azurerm/resource_arm_application_insights_analytics_item.go
@@ -41,11 +41,6 @@ func resourceArmApplicationInsightsAnalyticsItem() *schema.Resource {
 				Computed: true,
 			},
 
-			"item_id": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-
 			"content": {
 				Type:     schema.TypeString,
 				Required: true,
@@ -93,13 +88,106 @@ func resourceArmApplicationInsightsAnalyticsItemUpdate(d *schema.ResourceData, m
 	return resourceArmApplicationInsightsAnalyticsItemCreateUpdate(d, meta, true)
 }
 func resourceArmApplicationInsightsAnalyticsItemCreateUpdate(d *schema.ResourceData, meta interface{}, overwrite bool) error {
-	return fmt.Errorf("Not implemented")
+	client := meta.(*ArmClient).appInsights.AnalyticsItemsClient
+	ctx := meta.(*ArmClient).StopContext
+
+	resourceGroupName := d.Get("resource_group_name").(string)
+	appInsightsID := d.Get("application_insights_id").(string)
+
+	id, err := azure.ParseAzureResourceID(appInsightsID)
+	if err != nil {
+		return fmt.Errorf("Error parsing resource ID: %s", err)
+	}
+
+	appInsightsName := id.Path["components"]
+
+	name := d.Get("name").(string)
+	content := d.Get("content").(string)
+	scopeName := d.Get("scope").(string)
+	typeName := d.Get("type").(string)
+
+	properties := insights.ApplicationInsightsComponentAnalyticsItem{
+		Name:    &name,
+		Type:    insights.ItemType(typeName),
+		Scope:   insights.ItemScope(scopeName),
+		Content: &content,
+	}
+	result, err := client.Put(ctx, resourceGroupName, appInsightsName, insights.AnalyticsItems, properties, &overwrite)
+	if err != nil {
+		return fmt.Errorf("Error Putting Application Insights Analytics Item %s (Resource Group %s, App Insights Name: %s): %s", name, resourceGroupName, appInsightsName, err)
+	}
+
+	d.SetId(*result.ID)
+
+	return resourceArmApplicationInsightsAnalyticsItemRead(d, meta)
 }
 
 func resourceArmApplicationInsightsAnalyticsItemRead(d *schema.ResourceData, meta interface{}) error {
-	return fmt.Errorf("Not implemented")
+	client := meta.(*ArmClient).appInsights.AnalyticsItemsClient
+	ctx := meta.(*ArmClient).StopContext
+
+	resourceGroupName := d.Get("resource_group_name").(string)
+	appInsightsID := d.Get("application_insights_id").(string)
+	scopeName := d.Get("scope").(string)
+	itemID := d.Id()
+
+	id, err := azure.ParseAzureResourceID(appInsightsID)
+	if err != nil {
+		return fmt.Errorf("Error parsing resource ID: %s", err)
+	}
+
+	appInsightsName := id.Path["components"]
+	name := d.Get("name").(string)
+
+	var scopePath insights.ItemScopePath
+	if scopeName == "user" {
+		scopePath = insights.MyanalyticsItems
+	} else {
+		scopePath = insights.AnalyticsItems
+	}
+	result, err := client.Get(ctx, resourceGroupName, appInsightsName, scopePath, itemID, name)
+	if err != nil {
+		return fmt.Errorf("Error Getting Application Insights Analytics Item %s (Resource Group %s, App Insights Name: %s): %s", name, resourceGroupName, appInsightsName, err)
+	}
+
+	d.Set("version", result.Version)
+	d.Set("content", result.Content)
+	d.Set("scope", string(result.Scope))
+	d.Set("type", string(result.Type))
+	d.Set("time_created", result.TimeCreated)
+	d.Set("time_modified", result.TimeModified)
+
+	return nil
 }
 
 func resourceArmApplicationInsightsAnalyticsItemDelete(d *schema.ResourceData, meta interface{}) error {
-	return fmt.Errorf("Not implemented")
+
+	client := meta.(*ArmClient).appInsights.AnalyticsItemsClient
+	ctx := meta.(*ArmClient).StopContext
+
+	resourceGroupName := d.Get("resource_group_name").(string)
+	appInsightsID := d.Get("application_insights_id").(string)
+	scopeName := d.Get("scope").(string)
+	itemID := d.Id()
+
+	id, err := azure.ParseAzureResourceID(appInsightsID)
+	if err != nil {
+		return fmt.Errorf("Error parsing resource ID: %s", err)
+	}
+
+	appInsightsName := id.Path["components"]
+	name := d.Get("name").(string)
+
+	var scopePath insights.ItemScopePath
+	if scopeName == "user" {
+		scopePath = insights.MyanalyticsItems
+	} else {
+		scopePath = insights.AnalyticsItems
+	}
+	_, err = client.Delete(ctx, resourceGroupName, appInsightsName, scopePath, itemID, name)
+	if err != nil {
+		return fmt.Errorf("Error Getting Application Insights Analytics Item %s (Resource Group %s, App Insights Name: %s): %s", name, resourceGroupName, appInsightsName, err)
+	}
+
+	return nil
 }
