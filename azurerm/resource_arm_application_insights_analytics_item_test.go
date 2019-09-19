f3c35f719c9c81eae2d573adbef6fc35c7002eec
Stuart Leeks <stuartle@microsoft.com>
2019-09-19 10:31:49 +0100

    Extend tests

--- azurerm/resource_arm_application_insights_analytics_item_test.go
+++ azurerm/resource_arm_application_insights_analytics_item_test.go
@@ -21,12 +21,12 @@ func TestAccAzureRMApplicationInsightsAnalyticsItem_basic(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testCheckAzureRMApplicationInsightAnalyticsItemDestroy("testquery"),
+		CheckDestroy: testCheckAzureRMApplicationInsightAnalyticsItemDestroy("testquery", insights.ItemScopeShared),
 		Steps: []resource.TestStep{
 			{
 				Config: config,
 				Check: resource.ComposeTestCheckFunc(
-					testCheckAzureRMApplicationInsightsAnalyticsItemExists(resourceName, "testquery"),
+					testCheckAzureRMApplicationInsightsAnalyticsItemExists(resourceName, "testquery", insights.ItemScopeShared),
 					resource.TestCheckResourceAttr(resourceName, "name", "testquery"),
 					resource.TestCheckResourceAttr(resourceName, "scope", "shared"),
 					resource.TestCheckResourceAttr(resourceName, "type", "query"),
@@ -37,7 +37,40 @@ func TestAccAzureRMApplicationInsightsAnalyticsItem_basic(t *testing.T) {
 	})
 }
 
-func testCheckAzureRMApplicationInsightAnalyticsItemDestroy(queryName string) resource.TestCheckFunc {
+func TestAccAzureRMApplicationInsightsAnalyticsItem_multiple(t *testing.T) {
+	resourceName1 := "azurerm_application_insights_analytics_item.test1"
+	resourceName2 := "azurerm_application_insights_analytics_item.test2"
+	ri := tf.AccRandTimeInt()
+	config := testAccAzureRMApplicationInsightsAnalyticsItem_multiple(ri, testLocation())
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		CheckDestroy: resource.ComposeTestCheckFunc(
+			testCheckAzureRMApplicationInsightAnalyticsItemDestroy("testquery1", insights.ItemScopeShared),
+			testCheckAzureRMApplicationInsightAnalyticsItemDestroy("testquery2", insights.ItemScopeUser),
+		),
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMApplicationInsightsAnalyticsItemExists(resourceName1, "testquery1", insights.ItemScopeShared),
+					testCheckAzureRMApplicationInsightsAnalyticsItemExists(resourceName2, "testquery2", insights.ItemScopeUser),
+					resource.TestCheckResourceAttr(resourceName1, "name", "testquery1"),
+					resource.TestCheckResourceAttr(resourceName1, "scope", "shared"),
+					resource.TestCheckResourceAttr(resourceName1, "type", "query"),
+					resource.TestCheckResourceAttr(resourceName1, "content", "requests #test1"),
+					resource.TestCheckResourceAttr(resourceName2, "name", "testquery2"),
+					resource.TestCheckResourceAttr(resourceName2, "scope", "user"),
+					resource.TestCheckResourceAttr(resourceName2, "type", "query"),
+					resource.TestCheckResourceAttr(resourceName2, "content", "requests #test2"),
+				),
+			},
+		},
+	})
+}
+
+func testCheckAzureRMApplicationInsightAnalyticsItemDestroy(queryName string, itemScope insights.ItemScope) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		for _, rs := range s.RootModule().Resources {
 			if rs.Type != "azurerm_application_insights_analytics_item" {
@@ -46,7 +79,7 @@ func testCheckAzureRMApplicationInsightAnalyticsItemDestroy(queryName string) re
 			name := rs.Primary.Attributes["name"]
 			resGroup := rs.Primary.Attributes["resource_group_name"]
 
-			exists, err := testCheckAzureRMApplicationInsightsAnalyticsItemExistsInternal(rs, queryName)
+			exists, err := testCheckAzureRMApplicationInsightsAnalyticsItemExistsInternal(rs, queryName, itemScope)
 			if err != nil {
 				return fmt.Errorf("Error checking if item has been destroyed: %s", err)
 			}
@@ -59,7 +92,7 @@ func testCheckAzureRMApplicationInsightAnalyticsItemDestroy(queryName string) re
 	}
 }
 
-func testCheckAzureRMApplicationInsightsAnalyticsItemExists(resourceName string, queryName string) resource.TestCheckFunc {
+func testCheckAzureRMApplicationInsightsAnalyticsItemExists(resourceName string, queryName string, itemScope insights.ItemScope) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[resourceName]
 		if !ok {
@@ -68,7 +101,7 @@ func testCheckAzureRMApplicationInsightsAnalyticsItemExists(resourceName string,
 		name := rs.Primary.Attributes["name"]
 		resGroup := rs.Primary.Attributes["resource_group_name"]
 
-		exists, err := testCheckAzureRMApplicationInsightsAnalyticsItemExistsInternal(rs, queryName)
+		exists, err := testCheckAzureRMApplicationInsightsAnalyticsItemExistsInternal(rs, queryName, itemScope)
 		if err != nil {
 			return fmt.Errorf("Error checking if item exists: %s", err)
 		}
@@ -80,7 +113,7 @@ func testCheckAzureRMApplicationInsightsAnalyticsItemExists(resourceName string,
 	}
 }
 
-func testCheckAzureRMApplicationInsightsAnalyticsItemExistsInternal(rs *terraform.ResourceState, queryName string) (bool, error) {
+func testCheckAzureRMApplicationInsightsAnalyticsItemExistsInternal(rs *terraform.ResourceState, queryName string, itemScope insights.ItemScope) (bool, error) {
 	resGroup := rs.Primary.Attributes["resource_group_name"]
 
 	appInsightsID := rs.Primary.Attributes["application_insights_id"]
@@ -95,7 +128,7 @@ func testCheckAzureRMApplicationInsightsAnalyticsItemExistsInternal(rs *terrafor
 	ctx := testAccProvider.Meta().(*ArmClient).StopContext
 
 	includeContent := false
-	resp, err := conn.List(ctx, resGroup, appInsightsName, insights.AnalyticsItems, insights.ItemScopeShared, insights.ItemTypeParameterQuery, &includeContent)
+	resp, err := conn.List(ctx, resGroup, appInsightsName, insights.AnalyticsItems, itemScope, insights.ItemTypeParameterQuery, &includeContent)
 	if resp.StatusCode == http.StatusNotFound {
 		return false, nil
 	}
@@ -103,7 +136,7 @@ func testCheckAzureRMApplicationInsightsAnalyticsItemExistsInternal(rs *terrafor
 		return false, fmt.Errorf("Bad: List on appInsightsAnalyticsItemsClient: %+v", err)
 	}
 	for _, item := range *resp.Value {
-		if *item.Name == queryName && item.Scope == insights.ItemScopeShared {
+		if *item.Name == queryName && item.Scope == itemScope {
 			return true, nil
 		}
 	}
@@ -130,8 +163,42 @@ resource "azurerm_application_insights_analytics_item" "test" {
   resource_group_name     = "${azurerm_resource_group.test.name}"
   application_insights_id = "${azurerm_application_insights.test.id}"
   content                 = "requests #test"
-	scope                   = "shared"
-	type                    = "query"
+  scope                   = "shared"
+  type                    = "query"
+}
+`, rInt, location, rInt)
+}
+
+func testAccAzureRMApplicationInsightsAnalyticsItem_multiple(rInt int, location string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_application_insights" "test" {
+  name                = "acctestappinsights-%d"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  application_type    = "web"
+}
+
+resource "azurerm_application_insights_analytics_item" "test1" {
+  name                    = "testquery1"
+  resource_group_name     = "${azurerm_resource_group.test.name}"
+  application_insights_id = "${azurerm_application_insights.test.id}"
+  content                 = "requests #test1"
+  scope                   = "shared"
+  type                    = "query"
+}
+
+resource "azurerm_application_insights_analytics_item" "test2" {
+  name                    = "testquery2"
+  resource_group_name     = "${azurerm_resource_group.test.name}"
+  application_insights_id = "${azurerm_application_insights.test.id}"
+  content                 = "requests #test2"
+  scope                   = "user"
+  type                    = "query"
 }
 `, rInt, location, rInt)
 }
