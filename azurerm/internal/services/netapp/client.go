c50b8155661f7064a981dcd3a8e29f9c808629f5
v-cheye <v-cheye@microsoft.com>
2019-09-26 17:35:03 +0800

    Update code for lint test

--- azurerm/internal/services/netapp/client.go
+++ azurerm/internal/services/netapp/client.go
@@ -10,7 +10,6 @@ type Client struct {
 }
 
 func BuildClient(o *common.ClientOptions) *Client {
-
 	AccountClient := netapp.NewAccountsClientWithBaseURI(o.ResourceManagerEndpoint, o.SubscriptionId)
 	o.ConfigureClient(&AccountClient.Client, o.ResourceManagerAuthorizer)
 
