4f1d97339f163bf4c87a1b464156f7dc74614586
tombuildsstuff <git@ibuildstuff.co.uk>
2019-10-01 08:39:48 +0200

    r/kubernetes_cluster: fixing TestAccAzureRMKubernetesCluster_internalNetwork

--- azurerm/resource_arm_kubernetes_cluster_test.go
+++ azurerm/resource_arm_kubernetes_cluster_test.go
@@ -1270,6 +1270,9 @@ resource "azurerm_subnet" "test" {
   resource_group_name  = "${azurerm_resource_group.test.name}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
   address_prefix       = "172.0.2.0/24"
+
+  # TODO: remove in 2.0
+  ignore_changes = ["route_table_id"]
 }
 
 resource "azurerm_kubernetes_cluster" "test" {
