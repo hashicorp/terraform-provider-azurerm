d2af05f2461c43c155174c1f9f1e8d1483303f9c
tombuildsstuff <git@ibuildstuff.co.uk>
2019-10-07 11:06:29 +0200

    switching to use the new plugin sdk

--- azurerm/internal/tf/base64/schema.go
+++ azurerm/internal/tf/base64/schema.go
@@ -1,7 +1,7 @@
 package base64
 
 import (
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/validate"
 )
 
--- azurerm/resource_arm_linux_virtual_machine_scale_set_test.go
+++ azurerm/resource_arm_linux_virtual_machine_scale_set_test.go
@@ -3,8 +3,8 @@ package azurerm
 import (
 	"fmt"
 
-	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
