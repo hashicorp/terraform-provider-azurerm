6e5ecf06c17224ae8fa8e07ccaab349b6fe365ab
Matthew Frahry <mbfrahry@gmail.com>
2019-10-06 21:44:23 -0700

    Fix failing test

--- azurerm/resource_arm_virtual_machine_unmanaged_disks_test.go
+++ azurerm/resource_arm_virtual_machine_unmanaged_disks_test.go
@@ -3068,7 +3068,7 @@ func testCheckAzureRMVirtualMachineDisappears(resourceName string) resource.Test
 			return fmt.Errorf("Bad: no resource group found in state for virtual machine: %s", vmName)
 		}
 
-		client := testAccProvider.Meta().(*ArmClient).compute.VMClient
+		client := testAccProvider.Meta().(*ArmClient).Compute.VMClient
 		ctx := testAccProvider.Meta().(*ArmClient).StopContext
 
 		future, err := client.Delete(ctx, resourceGroup, vmName)
