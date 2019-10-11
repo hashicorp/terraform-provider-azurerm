8e63462f9bd595b5f89ab80f6e1e7a3fcd32d57b
Lee Wilson <264361+leewilson86@users.noreply.github.com>
2019-10-11 13:59:38 +0100

    Data Source: azurerm_lb_backend_address_pool - exposed backend_ip_configurations and load_balancing_rules

--- azurerm/data_source_loadbalancer_backend_address_pool.go
+++ azurerm/data_source_loadbalancer_backend_address_pool.go
@@ -5,8 +5,8 @@ import (
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/validate"
 )
 
 func dataSourceArmLoadBalancerBackendAddressPool() *schema.Resource {
@@ -21,13 +21,35 @@ func dataSourceArmLoadBalancerBackendAddressPool() *schema.Resource {
 			"name": {
 				Type:         schema.TypeString,
 				Required:     true,
-				ValidateFunc: validation.NoZeroValues,
+				ForceNew:     true,
+				ValidateFunc: validate.NoEmptyStrings,
 			},
 
 			"loadbalancer_id": {
 				Type:         schema.TypeString,
 				Required:     true,
+				ForceNew:     true,
 				ValidateFunc: azure.ValidateResourceID,
+			},
+
+			"backend_ip_configurations": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type:         schema.TypeString,
+					ValidateFunc: validate.NoEmptyStrings,
+				},
+				Set: schema.HashString,
+			},
+
+			"load_balancing_rules": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type:         schema.TypeString,
+					ValidateFunc: validate.NoEmptyStrings,
+				},
+				Set: schema.HashString,
 			},
 		},
 	}
@@ -45,12 +67,32 @@ func dataSourceArmLoadBalancerBackendAddressPoolRead(d *schema.ResourceData, met
 		return fmt.Errorf("Unable to retrieve Backend Address Pool %q since Load Balancer %q was not found", name, loadBalancerId)
 	}
 
-	config, _, exists := findLoadBalancerBackEndAddressPoolByName(loadBalancer, name)
+	bap, _, exists := findLoadBalancerBackEndAddressPoolByName(loadBalancer, name)
 	if !exists {
 		return fmt.Errorf("Backend Address Pool %q was not found in Load Balancer %q", name, loadBalancerId)
 	}
 
-	d.SetId(*config.ID)
+	d.SetId(*bap.ID)
+
+	var backendIpConfigurations []string
+	var loadBalancingRules []string
+
+	if props := bap.BackendAddressPoolPropertiesFormat; props != nil {
+		if configs := props.BackendIPConfigurations; configs != nil {
+			for _, backendConfig := range *configs {
+				backendIpConfigurations = append(backendIpConfigurations, *backendConfig.ID)
+			}
+		}
+
+		if rules := props.LoadBalancingRules; rules != nil {
+			for _, rule := range *rules {
+				loadBalancingRules = append(loadBalancingRules, *rule.ID)
+			}
+		}
+	}
+
+	d.Set("backend_ip_configurations", backendIpConfigurations)
+	d.Set("load_balancing_rules", loadBalancingRules)
 
 	return nil
 }
