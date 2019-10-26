52fa42530b275edd8af4ec19fff5aed4cd0a2ee5
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2019-10-25 19:10:38 -0700

    Added private_ip_address as computed output

--- azurerm/data_source_private_link_endpoint.go
+++ azurerm/data_source_private_link_endpoint.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
@@ -16,26 +15,72 @@ func dataSourceArmPrivateLinkEndpoint() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Type:         schema.TypeString,
-				Required:     true,
-				ValidateFunc: validate.NoEmptyStrings,
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 
 			"location": azure.SchemaLocationForDataSource(),
 
 			"resource_group_name": azure.SchemaResourceGroupNameForDataSource(),
 
+			"subnet_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"private_service_connection": {
+				Type:     schema.TypeList,
+				Computed: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"is_manual_connection": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"private_connection_resource_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"subresource_names": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Schema{
+								Type:     schema.TypeString,
+								Computed: true,
+							},
+						},
+						"request_message": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"provisioning_state": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"status": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"private_ip_address": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+
 			"network_interface_ids": {
 				Type:     schema.TypeList,
 				Computed: true,
 				Elem: &schema.Schema{
-					Type: schema.TypeString,
+					Type:     schema.TypeString,
+					Computed: true,
 				},
-			},
-
-			"subnet_id": {
-				Type:     schema.TypeString,
-				Computed: true,
 			},
 
 			"tags": tags.SchemaDataSource(),
@@ -47,33 +92,76 @@ func dataSourceArmPrivateLinkEndpointRead(d *schema.ResourceData, meta interface
 	client := meta.(*ArmClient).Network.PrivateEndpointClient
 	ctx := meta.(*ArmClient).StopContext
 
-	name := d.Get("name").(string)
-	resourceGroup := d.Get("resource_group_name").(string)
+	id, err := azure.ParseAzureResourceID(d.Id())
+	if err != nil {
+		return err
+	}
+	resourceGroup := id.ResourceGroup
+	name := id.Path["privateEndpoints"]
 
 	resp, err := client.Get(ctx, resourceGroup, name, "")
 	if err != nil {
 		if utils.ResponseWasNotFound(resp.Response) {
-			return fmt.Errorf("Error: Private Endpoint %q (Resource Group %q) was not found", name, resourceGroup)
+			d.SetId("")
+			return nil
 		}
-		return fmt.Errorf("Error reading Private Endpoint %q (Resource Group %q): %+v", name, resourceGroup, err)
+		return fmt.Errorf("Error reading Private Link Endpoint %q (Resource Group %q): %+v", name, resourceGroup, err)
 	}
 
-	if resp.ID == nil || *resp.ID == "" { 
-		return fmt.Errorf("API returns a nil/empty id on Private Link Endpoint %q (Resource Group %q): %+v", name, resourceGroup, err) 
-	} 
-	d.SetId(*resp.ID)
-	
 	d.Set("name", resp.Name)
 	d.Set("resource_group_name", resourceGroup)
 	if location := resp.Location; location != nil {
 		d.Set("location", azure.NormalizeLocation(*location))
 	}
 	if props := resp.PrivateEndpointProperties; props != nil {
-		if err := d.Set("network_interfaces", flattenArmPrivateLinkEndpointInterface(props.NetworkInterfaces)); err != nil {
-			return fmt.Errorf("Error setting `network_interfaces`: %+v", err)
-		}
 		if subnet := props.Subnet; subnet != nil {
 			d.Set("subnet_id", subnet.ID)
+		}
+
+		privateIpAddress := ""
+
+		if props.NetworkInterfaces != nil {
+			if err := d.Set("network_interface_ids", flattenArmPrivateLinkEndpointInterface(props.NetworkInterfaces)); err != nil {
+				return fmt.Errorf("Error setting `network_interface_ids`: %+v", err)
+			}
+
+			// now we need to get the nic to get the private ip address for the private link endpoint
+			client := meta.(*ArmClient).Network.InterfacesClient
+			ctx := meta.(*ArmClient).StopContext
+
+			nic := d.Get("network_interface_ids").([]interface{})
+
+			nicId, err := azure.ParseAzureResourceID(nic[0].(string))
+			if err != nil {
+				return err
+			}
+			nicName := nicId.Path["networkInterfaces"]
+
+			nicResp, err := client.Get(ctx, resourceGroup, nicName, "")
+			if err != nil {
+				if utils.ResponseWasNotFound(nicResp.Response) {
+					return fmt.Errorf("Azure Network Interface %q (Resource Group %q): %+v", nicName, resourceGroup, err)
+				}
+				return fmt.Errorf("Error making Read request on Azure Network Interface %q (Resource Group %q): %+v", nicName, resourceGroup, err)
+			}
+
+			if nicProps := nicResp.InterfacePropertiesFormat; nicProps != nil {
+				if configs := nicProps.IPConfigurations; configs != nil {
+					for i, config := range *nicProps.IPConfigurations {
+						if ipProps := config.InterfaceIPConfigurationPropertiesFormat; ipProps != nil {
+							if v := ipProps.PrivateIPAddress; v != nil {
+								if i == 0 {
+									privateIpAddress = *v
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		if err := d.Set("private_service_connection", flattenArmPrivateLinkEndpointServiceConnection(props.PrivateLinkServiceConnections, props.ManualPrivateLinkServiceConnections, privateIpAddress)); err != nil {
+			return fmt.Errorf("Error setting `private_service_connection`: %+v", err)
 		}
 	}
 
--- azurerm/resource_arm_private_link_endpoint.go
+++ azurerm/resource_arm_private_link_endpoint.go
@@ -48,6 +48,7 @@ func resourceArmPrivateLinkEndpoint() *schema.Resource {
 			"private_service_connection": {
 				Type:     schema.TypeList,
 				Optional: true,
+				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"name": {
@@ -57,8 +58,7 @@ func resourceArmPrivateLinkEndpoint() *schema.Resource {
 						},
 						"is_manual_connection": {
 							Type:     schema.TypeBool,
-							Optional: true,
-							Default:  false,
+							Required: true,
 						},
 						"private_connection_resource_id": {
 							Type:         schema.TypeString,
@@ -74,20 +74,19 @@ func resourceArmPrivateLinkEndpoint() *schema.Resource {
 							},
 						},
 						"request_message": {
-							// Only valid if is_manual_connection == true. Restricted to 140 chars.
 							Type:         schema.TypeString,
 							Optional:     true,
 							ValidateFunc: validate.PrivateLinkEnpointRequestMessage,
 						},
 						"provisioning_state": {
-							// ProvisioningState - The provisioning state of the Private Link Endpoint. Possible values include: 'Succeeded', 'Updating', 'Deleting', 'Failed'
-							// PrivateLinkServiceConnectionProperties.ProvisioningState
 							Type:     schema.TypeString,
 							Computed: true,
 						},
 						"status": {
-							// Status - Indicates whether the connection has been Approved/Rejected/Removed by the owner of the service.
-							// PrivateLinkServiceConnection.PrivateLinkServiceConnectionProperties.PrivateLinkServiceConnectionState.Status
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"private_ip_address": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
@@ -139,7 +138,7 @@ func resourceArmPrivateLinkEndpointCreateUpdate(d *schema.ResourceData, meta int
 	parameters := network.PrivateEndpoint{
 		Location: utils.String(location),
 		PrivateEndpointProperties: &network.PrivateEndpointProperties{
-			PrivateLinkServiceConnections: expandArmPrivateLinkEndpointServiceConnection(privateServiceConnections, false),
+			PrivateLinkServiceConnections:       expandArmPrivateLinkEndpointServiceConnection(privateServiceConnections, false),
 			ManualPrivateLinkServiceConnections: expandArmPrivateLinkEndpointServiceConnection(privateServiceConnections, true),
 			Subnet: &network.Subnet{
 				ID: utils.String(subnetId),
@@ -194,14 +193,55 @@ func resourceArmPrivateLinkEndpointRead(d *schema.ResourceData, meta interface{}
 	if location := resp.Location; location != nil {
 		d.Set("location", azure.NormalizeLocation(*location))
 	}
-	if privateEndpointProperties := resp.PrivateEndpointProperties; privateEndpointProperties != nil {
-		if privateEndpointProperties.NetworkInterfaces != nil {
-			if err := d.Set("network_interface_ids", flattenArmPrivateLinkEndpointInterface(privateEndpointProperties.NetworkInterfaces)); err != nil {
+	if props := resp.PrivateEndpointProperties; props != nil {
+		if subnet := props.Subnet; subnet != nil {
+			d.Set("subnet_id", subnet.ID)
+		}
+
+		privateIpAddress := ""
+
+		if props.NetworkInterfaces != nil {
+			if err := d.Set("network_interface_ids", flattenArmPrivateLinkEndpointInterface(props.NetworkInterfaces)); err != nil {
 				return fmt.Errorf("Error setting `network_interface_ids`: %+v", err)
 			}
+
+			// now we need to get the nic to get the private ip address for the private link endpoint
+			client := meta.(*ArmClient).Network.InterfacesClient
+			ctx := meta.(*ArmClient).StopContext
+
+			nic := d.Get("network_interface_ids").([]interface{})
+
+			nicId, err := azure.ParseAzureResourceID(nic[0].(string))
+			if err != nil {
+				return err
+			}
+			nicName := nicId.Path["networkInterfaces"]
+
+			nicResp, err := client.Get(ctx, resourceGroup, nicName, "")
+			if err != nil {
+				if utils.ResponseWasNotFound(nicResp.Response) {
+					return fmt.Errorf("Azure Network Interface %q (Resource Group %q): %+v", nicName, resourceGroup, err)
+				}
+				return fmt.Errorf("Error making Read request on Azure Network Interface %q (Resource Group %q): %+v", nicName, resourceGroup, err)
+			}
+
+			if nicProps := nicResp.InterfacePropertiesFormat; nicProps != nil {
+				if configs := nicProps.IPConfigurations; configs != nil {
+					for i, config := range *nicProps.IPConfigurations {
+						if ipProps := config.InterfaceIPConfigurationPropertiesFormat; ipProps != nil {
+							if v := ipProps.PrivateIPAddress; v != nil {
+								if i == 0 {
+									privateIpAddress = *v
+								}
+							}
+						}
+					}
+				}
+			}
 		}
-		if subnet := privateEndpointProperties.Subnet; subnet != nil {
-			d.Set("subnet_id", subnet.ID)
+
+		if err := d.Set("private_service_connection", flattenArmPrivateLinkEndpointServiceConnection(props.PrivateLinkServiceConnections, props.ManualPrivateLinkServiceConnections, privateIpAddress)); err != nil {
+			return fmt.Errorf("Error setting `private_service_connection`: %+v", err)
 		}
 	}
 
@@ -266,47 +306,89 @@ func expandArmPrivateLinkEndpointServiceConnection(input []interface{}, parseMan
 	return &results
 }
 
-func flattenArmPrivateLinkEndpointServiceConnection(input *[]network.PrivateLinkServiceConnection) []interface{} {
+func flattenArmPrivateLinkEndpointServiceConnection(serviceConnections *[]network.PrivateLinkServiceConnection, manualServiceConnections *[]network.PrivateLinkServiceConnection, privateIpAddress string) []interface{} {
 	results := make([]interface{}, 0)
-	if input == nil {
+	if serviceConnections == nil && manualServiceConnections == nil {
 		return results
 	}
 
-	for _, item := range *input {
-		v := make(map[string]interface{})
+	if serviceConnections != nil {
+		for _, item := range *serviceConnections {
+			v := make(map[string]interface{})
 
-		if name := item.Name; name != nil {
-			v["name"] = *name
-		}
-		if props := item.PrivateLinkServiceConnectionProperties; props != nil {
-			if subresourceNames := props.GroupIds; subresourceNames != nil {
-				v["subresource_names"] = utils.FlattenStringSlice(subresourceNames)
+			if name := item.Name; name != nil {
+				v["name"] = *name
 			}
-			if privateConnectionResourceId := props.PrivateLinkServiceID; privateConnectionResourceId != nil {
-				v["private_connection_resource_id"] = *privateConnectionResourceId
-			}
-			if requestMessage := props.RequestMessage; requestMessage != nil {
-				v["request_message"] = *requestMessage
-			}
-			if provisioningState := props.ProvisioningState; provisioningState != "" {
-				v["provisioning_state"] = provisioningState
-			}
-			if s := props.PrivateLinkServiceConnectionState; s != nil {
-				if status := s.Status; status != nil {
-					v["status"] = *status
+
+			v["is_manual_connection"] = false
+			v["private_ip_address"] = privateIpAddress
+
+			if props := item.PrivateLinkServiceConnectionProperties; props != nil {
+				if subresourceNames := props.GroupIds; subresourceNames != nil {
+					v["subresource_names"] = utils.FlattenStringSlice(subresourceNames)
+				}
+				if privateConnectionResourceId := props.PrivateLinkServiceID; privateConnectionResourceId != nil {
+					v["private_connection_resource_id"] = *privateConnectionResourceId
+				}
+				if requestMessage := props.RequestMessage; requestMessage != nil {
+					v["request_message"] = *requestMessage
+				}
+				if provisioningState := props.ProvisioningState; provisioningState != "" {
+					v["provisioning_state"] = provisioningState
+				}
+
+				if s := props.PrivateLinkServiceConnectionState; s != nil {
+					if status := s.Status; status != nil {
+						v["status"] = *status
+					}
 				}
 			}
-		}
 
-		results = append(results, v)
+			results = append(results, v)
+		}
+	}
+
+	if manualServiceConnections != nil {
+		for _, item := range *manualServiceConnections {
+			v := make(map[string]interface{})
+
+			if name := item.Name; name != nil {
+				v["name"] = *name
+			}
+
+			v["is_manual_connection"] = true
+			v["private_ip_address"] = privateIpAddress
+
+			if props := item.PrivateLinkServiceConnectionProperties; props != nil {
+				if subresourceNames := props.GroupIds; subresourceNames != nil {
+					v["subresource_names"] = utils.FlattenStringSlice(subresourceNames)
+				}
+				if privateConnectionResourceId := props.PrivateLinkServiceID; privateConnectionResourceId != nil {
+					v["private_connection_resource_id"] = *privateConnectionResourceId
+				}
+				if requestMessage := props.RequestMessage; requestMessage != nil {
+					v["request_message"] = *requestMessage
+				}
+				if provisioningState := props.ProvisioningState; provisioningState != "" {
+					v["provisioning_state"] = provisioningState
+				}
+				if s := props.PrivateLinkServiceConnectionState; s != nil {
+					if status := s.Status; status != nil {
+						v["status"] = *status
+					}
+				}
+			}
+
+			results = append(results, v)
+		}
 	}
 
 	return results
 }
 
-func flattenArmPrivateLinkEndpointInterface(input *[]network.Interface) []interface{} {
+func flattenArmPrivateLinkEndpointInterface(input *[]network.Interface) []string {
 	if input == nil {
-		return make([]interface{}, 0)
+		return make([]string, 0)
 	}
 
 	results := make([]string, 0)
@@ -317,5 +399,5 @@ func flattenArmPrivateLinkEndpointInterface(input *[]network.Interface) []interf
 		}
 	}
 
-	return []interface{}{results}
+	return results
 }
