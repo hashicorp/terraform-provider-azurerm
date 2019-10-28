944ebbe015d2459f81406973f116e10cde17e82f
Matthew Frahry <mbfrahry@gmail.com>
2019-10-28 08:17:57 -0700

    Fix crash when MSI auth is used

--- azurerm/data_source_client_config.go
+++ azurerm/data_source_client_config.go
@@ -90,10 +90,14 @@ func dataSourceArmClientConfigRead(d *schema.ResourceData, meta interface{}) err
 	}
 
 	d.Set("object_id", "")
-	if v, err := client.getAuthenticatedObjectID(ctx); err != nil {
-		return fmt.Errorf("Error getting authenticated object ID: %v", err)
-	} else {
-		d.Set("object_id", v)
+
+	// TODO remove this when we confirm that MSI no longer returns nil with getAuthenticatedObjectID
+	if client.getAuthenticatedObjectID != nil {
+		if v, err := client.getAuthenticatedObjectID(ctx); err != nil {
+			return fmt.Errorf("Error getting authenticated object ID: %v", err)
+		} else {
+			d.Set("object_id", v)
+		}
 	}
 
 	return nil
