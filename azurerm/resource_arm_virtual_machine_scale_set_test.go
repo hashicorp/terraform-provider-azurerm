26a59a409fa605ceeec689fc450ea8def96e4d36
Mike Brancato <mike@mikebrancato.com>
2019-11-01 23:20:14 -0400

    Fix spacing chars

--- azurerm/resource_arm_virtual_machine_scale_set_test.go
+++ azurerm/resource_arm_virtual_machine_scale_set_test.go
@@ -6188,7 +6188,7 @@ resource "azurerm_lb" "test" {
   name                = "acctestlb-%[1]d"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-	sku                 = "Standard"
+  sku                 = "Standard"
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
