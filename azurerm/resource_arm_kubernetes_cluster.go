0f8d8eb8fd860f58effa5e5e8b0e3408893b4b18
tombuildsstuff <git@ibuildstuff.co.uk>
2019-11-06 13:07:29 +0100

    r/kubernetes_cluster: refactoring the addon helpers out

--- azurerm/resource_arm_kubernetes_cluster.go
+++ azurerm/resource_arm_kubernetes_cluster.go
@@ -15,6 +15,7 @@ import (
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/features"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/containers"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
@@ -254,105 +255,7 @@ func resourceArmKubernetesCluster() *schema.Resource {
 			},
 
 			// Optional
-			"addon_profile": {
-				Type:     schema.TypeList,
-				MaxItems: 1,
-				Optional: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"http_application_routing": {
-							Type:     schema.TypeList,
-							MaxItems: 1,
-							ForceNew: true,
-							Optional: true,
-							Computed: true, // TODO: remove in 2.0
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"enabled": {
-										Type:     schema.TypeBool,
-										ForceNew: true,
-										Required: true,
-									},
-									"http_application_routing_zone_name": {
-										Type:     schema.TypeString,
-										Computed: true,
-									},
-								},
-							},
-						},
-
-						"oms_agent": {
-							Type:     schema.TypeList,
-							MaxItems: 1,
-							Optional: true,
-							Computed: true, // TODO: remove in 2.0
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"enabled": {
-										Type:     schema.TypeBool,
-										Required: true,
-									},
-									"log_analytics_workspace_id": {
-										Type:         schema.TypeString,
-										Optional:     true,
-										ValidateFunc: azure.ValidateResourceID,
-									},
-								},
-							},
-						},
-
-						"aci_connector_linux": {
-							Type:     schema.TypeList,
-							MaxItems: 1,
-							Optional: true,
-							Computed: true, // TODO: remove in 2.0
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"enabled": {
-										Type:     schema.TypeBool,
-										Required: true,
-									},
-									"subnet_name": {
-										Type:         schema.TypeString,
-										Optional:     true,
-										ValidateFunc: validate.NoEmptyStrings,
-									},
-								},
-							},
-						},
-
-						"kube_dashboard": {
-							Type:     schema.TypeList,
-							MaxItems: 1,
-							Optional: true,
-							Computed: true, // TODO: remove in 2.0
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"enabled": {
-										Type:     schema.TypeBool,
-										Required: true,
-									},
-								},
-							},
-						},
-
-						"azure_policy": {
-							Type:     schema.TypeList,
-							MaxItems: 1,
-							Optional: true,
-							Computed: true, // TODO: remove in 2.0
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"enabled": {
-										Type:     schema.TypeBool,
-										Required: true,
-									},
-								},
-							},
-						},
-					},
-				},
-			},
+			"addon_profile": containers.SchemaKubernetesAddOnProfiles(),
 
 			"api_server_authorized_ip_ranges": {
 				Type:     schema.TypeSet,
@@ -695,7 +598,7 @@ func resourceArmKubernetesClusterCreate(d *schema.ResourceData, meta interface{}
 	}
 
 	addOnProfilesRaw := d.Get("addon_profile").([]interface{})
-	addonProfiles := expandKubernetesClusterAddonProfiles(addOnProfilesRaw)
+	addonProfiles := containers.ExpandKubernetesAddOnProfiles(addOnProfilesRaw)
 
 	networkProfileRaw := d.Get("network_profile").([]interface{})
 	networkProfile := expandKubernetesClusterNetworkProfile(networkProfileRaw)
@@ -823,7 +726,7 @@ func resourceArmKubernetesClusterUpdate(d *schema.ResourceData, meta interface{}
 	if d.HasChange("addon_profile") {
 		updateCluster = true
 		addOnProfilesRaw := d.Get("addon_profile").([]interface{})
-		addonProfiles := expandKubernetesClusterAddonProfiles(addOnProfilesRaw)
+		addonProfiles := containers.ExpandKubernetesAddOnProfiles(addOnProfilesRaw)
 		existing.ManagedClusterProperties.AddonProfiles = addonProfiles
 	}
 
@@ -976,7 +879,7 @@ func resourceArmKubernetesClusterRead(d *schema.ResourceData, meta interface{}) 
 			return fmt.Errorf("Error setting `api_server_authorized_ip_ranges`: %+v", err)
 		}
 
-		addonProfiles := flattenKubernetesClusterAddonProfiles(props.AddonProfiles)
+		addonProfiles := containers.FlattenKubernetesAddOnProfiles(props.AddonProfiles)
 		if err := d.Set("addon_profile", addonProfiles); err != nil {
 			return fmt.Errorf("Error setting `addon_profile`: %+v", err)
 		}
@@ -1088,187 +991,6 @@ func flattenKubernetesClusterAccessProfile(profile containerservice.ManagedClust
 		}
 	}
 	return nil, []interface{}{}
-}
-
-func expandKubernetesClusterAddonProfiles(input []interface{}) map[string]*containerservice.ManagedClusterAddonProfile {
-	disabled := containerservice.ManagedClusterAddonProfile{
-		Enabled: utils.Bool(false),
-	}
-
-	profiles := map[string]*containerservice.ManagedClusterAddonProfile{
-		// note: the casing on these keys is important
-		"aciConnectorLinux":      &disabled,
-		"azurepolicy":            &disabled,
-		"kubeDashboard":          &disabled,
-		"httpApplicationRouting": &disabled,
-		"omsagent":               &disabled,
-	}
-	if len(input) == 0 {
-		return profiles
-	}
-
-	profile := input[0].(map[string]interface{})
-	addonProfiles := map[string]*containerservice.ManagedClusterAddonProfile{}
-
-	httpApplicationRouting := profile["http_application_routing"].([]interface{})
-	if len(httpApplicationRouting) > 0 {
-		value := httpApplicationRouting[0].(map[string]interface{})
-		enabled := value["enabled"].(bool)
-		addonProfiles["httpApplicationRouting"] = &containerservice.ManagedClusterAddonProfile{
-			Enabled: utils.Bool(enabled),
-		}
-	}
-
-	omsAgent := profile["oms_agent"].([]interface{})
-	if len(omsAgent) > 0 {
-		value := omsAgent[0].(map[string]interface{})
-		config := make(map[string]*string)
-		enabled := value["enabled"].(bool)
-
-		if workspaceId, ok := value["log_analytics_workspace_id"]; ok && workspaceId != "" {
-			config["logAnalyticsWorkspaceResourceID"] = utils.String(workspaceId.(string))
-		}
-
-		addonProfiles["omsagent"] = &containerservice.ManagedClusterAddonProfile{
-			Enabled: utils.Bool(enabled),
-			Config:  config,
-		}
-	}
-
-	aciConnector := profile["aci_connector_linux"].([]interface{})
-	if len(aciConnector) > 0 {
-		value := aciConnector[0].(map[string]interface{})
-		config := make(map[string]*string)
-		enabled := value["enabled"].(bool)
-
-		if subnetName, ok := value["subnet_name"]; ok && subnetName != "" {
-			config["SubnetName"] = utils.String(subnetName.(string))
-		}
-
-		addonProfiles["aciConnectorLinux"] = &containerservice.ManagedClusterAddonProfile{
-			Enabled: utils.Bool(enabled),
-			Config:  config,
-		}
-	}
-
-	kubeDashboard := profile["kube_dashboard"].([]interface{})
-	if len(kubeDashboard) > 0 {
-		value := kubeDashboard[0].(map[string]interface{})
-		enabled := value["enabled"].(bool)
-
-		addonProfiles["kubeDashboard"] = &containerservice.ManagedClusterAddonProfile{
-			Enabled: utils.Bool(enabled),
-			Config:  nil,
-		}
-	}
-
-	azurePolicy := profile["azure_policy"].([]interface{})
-	if len(azurePolicy) > 0 && azurePolicy[0] != nil {
-		value := azurePolicy[0].(map[string]interface{})
-		enabled := value["enabled"].(bool)
-
-		addonProfiles["azurepolicy"] = &containerservice.ManagedClusterAddonProfile{
-			Enabled: utils.Bool(enabled),
-			Config:  nil,
-		}
-	}
-
-	return addonProfiles
-}
-
-func flattenKubernetesClusterAddonProfiles(profile map[string]*containerservice.ManagedClusterAddonProfile) []interface{} {
-	aciConnectors := make([]interface{}, 0)
-	if aciConnector := profile["aciConnectorLinux"]; aciConnector != nil {
-		enabled := false
-		if enabledVal := aciConnector.Enabled; enabledVal != nil {
-			enabled = *enabledVal
-		}
-
-		subnetName := ""
-		if v := aciConnector.Config["SubnetName"]; v != nil {
-			subnetName = *v
-		}
-
-		aciConnectors = append(aciConnectors, map[string]interface{}{
-			"enabled":     enabled,
-			"subnet_name": subnetName,
-		})
-	}
-
-	azurePolicies := make([]interface{}, 0)
-	if azurePolicy := profile["azurepolicy"]; azurePolicy != nil {
-		enabled := false
-		if enabledVal := azurePolicy.Enabled; enabledVal != nil {
-			enabled = *enabledVal
-		}
-
-		azurePolicies = append(azurePolicies, map[string]interface{}{
-			"enabled": enabled,
-		})
-	}
-
-	httpApplicationRoutes := make([]interface{}, 0)
-	if httpApplicationRouting := profile["httpApplicationRouting"]; httpApplicationRouting != nil {
-		enabled := false
-		if enabledVal := httpApplicationRouting.Enabled; enabledVal != nil {
-			enabled = *enabledVal
-		}
-
-		zoneName := ""
-		if v := httpApplicationRouting.Config["HTTPApplicationRoutingZoneName"]; v != nil {
-			zoneName = *v
-		}
-
-		httpApplicationRoutes = append(httpApplicationRoutes, map[string]interface{}{
-			"enabled":                            enabled,
-			"http_application_routing_zone_name": zoneName,
-		})
-	}
-
-	kubeDashboards := make([]interface{}, 0)
-	if kubeDashboard := profile["kubeDashboard"]; kubeDashboard != nil {
-		enabled := false
-		if enabledVal := kubeDashboard.Enabled; enabledVal != nil {
-			enabled = *enabledVal
-		}
-
-		kubeDashboards = append(kubeDashboards, map[string]interface{}{
-			"enabled": enabled,
-		})
-	}
-
-	omsAgents := make([]interface{}, 0)
-	if omsAgent := profile["omsagent"]; omsAgent != nil {
-		enabled := false
-		if enabledVal := omsAgent.Enabled; enabledVal != nil {
-			enabled = *enabledVal
-		}
-
-		workspaceId := ""
-		if workspaceResourceID := omsAgent.Config["logAnalyticsWorkspaceResourceID"]; workspaceResourceID != nil {
-			workspaceId = *workspaceResourceID
-		}
-
-		omsAgents = append(omsAgents, map[string]interface{}{
-			"enabled":                    enabled,
-			"log_analytics_workspace_id": workspaceId,
-		})
-	}
-
-	// this is a UX hack, since if the top level block isn't defined everything should be turned off
-	if len(aciConnectors) == 0 && len(azurePolicies) == 0 && len(httpApplicationRoutes) == 0 && len(kubeDashboards) == 0 && len(omsAgents) == 0 {
-		return []interface{}{}
-	}
-
-	return []interface{}{
-		map[string]interface{}{
-			"aci_connector_linux": aciConnectors
-			"azure_policy": azurePolicies,
-			"http_application_routing": httpApplicationRoutes,
-			"kube_dashboard": kubeDashboards,
-			"oms_agent": omsAgents,
-		},
-	}
 }
 
 func expandKubernetesClusterAgentPoolProfiles(input []interface{}, isNewResource bool) ([]containerservice.ManagedClusterAgentPoolProfile, error) {
