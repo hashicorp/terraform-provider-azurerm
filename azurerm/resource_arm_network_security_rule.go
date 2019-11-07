2230025ecce37f2e60c8762574d257c936ff64a8
mike-luminal <mike@fugue.co>
2019-11-07 16:32:26 -0500

    add ICMP as allowable protocol

--- azurerm/resource_arm_network_security_rule.go
+++ azurerm/resource_arm_network_security_rule.go
@@ -48,6 +48,7 @@ func resourceArmNetworkSecurityRule() *schema.Resource {
 					string(network.SecurityRuleProtocolAsterisk),
 					string(network.SecurityRuleProtocolTCP),
 					string(network.SecurityRuleProtocolUDP),
+					string(network.SecurityRuleProtocolIcmp),
 				}, true),
 				DiffSuppressFunc: ignoreCaseDiffSuppressFunc,
 			},
