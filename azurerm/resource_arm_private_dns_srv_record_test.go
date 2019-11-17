4bb7e7d328da1835f7ed2e3024f80d5229edc892
tombuildsstuff <git@ibuildstuff.co.uk>
2019-11-17 14:55:43 +0200

    r/privtae_dns_srv_record: updating the test name to be lower-cased

--- azurerm/resource_arm_private_dns_srv_record_test.go
+++ azurerm/resource_arm_private_dns_srv_record_test.go
@@ -203,7 +203,7 @@ resource "azurerm_private_dns_zone" "test" {
 }
 
 resource "azurerm_private_dns_srv_record" "test" {
-  name                = "testaccSRV%d"
+  name                = "testaccsrv%d"
   resource_group_name = "${azurerm_resource_group.test.name}"
   zone_name           = "${azurerm_private_dns_zone.test.name}"
   ttl                 = 300
