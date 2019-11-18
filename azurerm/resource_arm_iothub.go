7f5ad6b44391948b2525a90f8af43deac167b804
Matthew Frahry <mbfrahry@gmail.com>
2019-11-18 15:35:10 -0800

    New resources: `iothub_endpoint_storage_container`, `iothub_endpoint_eventhub`, `iothub_endpoint_servicebus_queue`, `iothub_endpoint_servicebus_topic` (#4823)

--- azurerm/resource_arm_iothub.go
+++ azurerm/resource_arm_iothub.go
@@ -232,6 +232,7 @@ func resourceArmIotHub() *schema.Resource {
 			"endpoint": {
 				Type:     schema.TypeList,
 				Optional: true,
+				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"type": {
@@ -262,7 +263,7 @@ func resourceArmIotHub() *schema.Resource {
 						"name": {
 							Type:         schema.TypeString,
 							Required:     true,
-							ValidateFunc: validateIoTHubEndpointName,
+							ValidateFunc: validate.IoTHubEndpointName,
 						},
 						"batch_frequency_in_seconds": {
 							Type:             schema.TypeInt,
@@ -468,11 +469,20 @@ func resourceArmIotHubCreateUpdate(d *schema.ResourceData, meta interface{}) err
 	location := azure.NormalizeLocation(d.Get("location").(string))
 	skuInfo := expandIoTHubSku(d)
 	t := d.Get("tags").(map[string]interface{})
-	fallbackRoute := expandIoTHubFallbackRoute(d)
 
-	endpoints, err := expandIoTHubEndpoints(d, subscriptionID)
-	if err != nil {
-		return fmt.Errorf("Error expanding `endpoint`: %+v", err)
+	fallbackRoute := expandIoTHubFallbackRoute(d)
+	routes := expandIoTHubRoutes(d)
+	routingProperties := devices.RoutingProperties{
+		Routes:        routes,
+		FallbackRoute: fallbackRoute,
+	}
+
+	if _, ok := d.GetOk("endpoint"); ok {
+		endpoints, err := expandIoTHubEndpoints(d, subscriptionID)
+		if err != nil {
+			return fmt.Errorf("Error expanding `endpoint`: %+v", err)
+		}
+		routingProperties.Endpoints = endpoints
 	}
 
 	storageEndpoints, messagingEndpoints, enableFileUploadNotifications, err := expandIoTHubFileUpload(d)
@@ -480,7 +490,6 @@ func resourceArmIotHubCreateUpdate(d *schema.ResourceData, meta interface{}) err
 		return fmt.Errorf("Error expanding `file_upload`: %+v", err)
 	}
 
-	routes := expandIoTHubRoutes(d)
 	ipFilterRules := expandIPFilterRules(d)
 
 	properties := devices.IotHubDescription{
@@ -488,12 +497,8 @@ func resourceArmIotHubCreateUpdate(d *schema.ResourceData, meta interface{}) err
 		Location: utils.String(location),
 		Sku:      skuInfo,
 		Properties: &devices.IotHubProperties{
-			IPFilterRules: ipFilterRules,
-			Routing: &devices.RoutingProperties{
-				Endpoints:     endpoints,
-				Routes:        routes,
-				FallbackRoute: fallbackRoute,
-			},
+			IPFilterRules:                 ipFilterRules,
+			Routing:                       &routingProperties,
 			StorageEndpoints:              storageEndpoints,
 			MessagingEndpoints:            messagingEndpoints,
 			EnableFileUploadNotifications: &enableFileUploadNotifications,
@@ -1063,25 +1068,6 @@ func flattenIoTHubFallbackRoute(input *devices.RoutingProperties) []interface{} 
 	output["endpoint_names"] = utils.FlattenStringSlice(route.EndpointNames)
 
 	return []interface{}{output}
-}
-
-func validateIoTHubEndpointName(v interface{}, _ string) (warnings []string, errors []error) {
-	value := v.(string)
-
-	reservedNames := []string{
-		"events",
-		"operationsMonitoringEvents",
-		"fileNotifications",
-		"$default",
-	}
-
-	for _, name := range reservedNames {
-		if name == value {
-			errors = append(errors, fmt.Errorf("The reserved endpoint name %s could not be used as a name for a custom endpoint", name))
-		}
-	}
-
-	return warnings, errors
 }
 
 func validateIoTHubFileNameFormat(v interface{}, k string) (warnings []string, errors []error) {
