06197490f1640226a40236e3d2e0cc6ca72ce79e
tombuildsstuff <git@ibuildstuff.co.uk>
2019-11-26 12:18:45 +0100

    r/virtual_wan: removing the unused security_provider_name / adding validation

---
+++ azurerm/internal/services/network/virtual_wan_test.go
@@ -0,0 +1,101 @@
+package network
+
+import (
+	"testing"
+
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
+)
+
+func TestParseVirtualWan(t *testing.T) {
+	testData := []struct {
+		Name     string
+		Input    string
+		Expected *VirtualWanResourceID
+	}{
+		{
+			Name:     "Empty",
+			Input:    "",
+			Expected: nil,
+		},
+		{
+			Name:     "No Virtual Wans Segment",
+			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/foo",
+			Expected: nil,
+		},
+		{
+			Name:     "No Virtual Wans Value",
+			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/foo/virtualWans/",
+			Expected: nil,
+		},
+		{
+			Name:  "Completed",
+			Input: "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/foo/virtualWans/example",
+			Expected: &VirtualWanResourceID{
+				Name: "example",
+				Base: azure.ResourceID{
+					ResourceGroup: "foo",
+				},
+			},
+		},
+	}
+
+	for _, v := range testData {
+		t.Logf("[DEBUG] Testing %q", v.Name)
+
+		actual, err := ParseVirtualWanID(v.Input)
+		if err != nil {
+			if v.Expected == nil {
+				continue
+			}
+
+			t.Fatalf("Expected a value but got an error: %s", err)
+		}
+
+		if actual.Name != v.Expected.Name {
+			t.Fatalf("Expected %q but got %q for Name", v.Expected.Name, actual.Name)
+		}
+
+		if actual.Base.ResourceGroup != v.Expected.Base.ResourceGroup {
+			t.Fatalf("Expected %q but got %q for ResourceGroup", v.Expected.Base.ResourceGroup, actual.Base.ResourceGroup)
+		}
+	}
+}
+
+func TestValidateVirtualWan(t *testing.T) {
+	testData := []struct {
+		Name  string
+		Input string
+		Valid bool
+	}{
+		{
+			Name:  "Empty",
+			Input: "",
+			Valid: false,
+		},
+		{
+			Name:  "No Virtual Wans Segment",
+			Input: "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/foo",
+			Valid: false,
+		},
+		{
+			Name:  "No Virtual Wans Value",
+			Input: "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/foo/virtualWans/",
+			Valid: false,
+		},
+		{
+			Name:  "Completed",
+			Input: "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/foo/virtualWans/example",
+			Valid: true,
+		},
+	}
+
+	for _, v := range testData {
+		t.Logf("[DEBUG] Testing %q", v.Input)
+
+		_, errors := ValidateVirtualWanID(v.Input, "virtual_wan_id")
+		isValid := len(errors) == 0
+		if v.Valid != isValid {
+			t.Fatalf("Expected %t but got %t", v.Valid, isValid)
+		}
+	}
+}
