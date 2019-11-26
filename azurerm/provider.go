9d3199629cd95b75e5448b53d1ed99b845e527d3
Hilton Giesenow <hiltatwork@giesenow.com>
2019-11-26 14:12:51 +0200

    Adding MsTeams Bot Channel

--- azurerm/provider.go
+++ azurerm/provider.go
@@ -217,6 +217,7 @@ func Provider() terraform.ResourceProvider {
 		"azurerm_batch_certificate":                                  resourceArmBatchCertificate(),
 		"azurerm_bot_channel_email":                                  resourceArmBotChannelEmail(),
 		"azurerm_bot_channel_slack":                                  resourceArmBotChannelSlack(),
+		"azurerm_bot_channel_msteams":                                resourceArmBotChannelMsTeams(),
 		"azurerm_bot_channels_registration":                          resourceArmBotChannelsRegistration(),
 		"azurerm_bot_connection":                                     resourceArmBotConnection(),
 		"azurerm_bot_web_app":                                        resourceArmBotWebApp(),
