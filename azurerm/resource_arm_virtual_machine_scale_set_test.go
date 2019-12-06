37ed6308019dba44fa1529b7af98ace5dd8fc5ae
v-cheye <v-cheye@microsoft.com>
2019-12-06 21:37:49 +0800

    Update code

--- azurerm/resource_arm_virtual_machine_scale_set_test.go
+++ azurerm/resource_arm_virtual_machine_scale_set_test.go
@@ -1929,6 +1929,12 @@ resource "azurerm_subnet" "test" {
   address_prefix       = "10.0.2.0/24"
 }
 
+resource "azurerm_network_security_group" "test" {
+  name                = "acceptanceTestSecurityGroup-%[1]d"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
 resource "azurerm_storage_account" "test" {
   name                     = "accsa%[1]d"
   resource_group_name      = "${azurerm_resource_group.test.name}"
@@ -1969,8 +1975,9 @@ resource "azurerm_virtual_machine_scale_set" "test" {
   }
 
   network_profile {
-    name    = "TestNetworkProfile-%[1]d"
-    primary = true
+    name                      = "TestNetworkProfile-%[1]d"
+    primary                   = true
+    network_security_group_id = "${azurerm_network_security_group.test.id}"
 
     ip_configuration {
       name      = "TestIPConfiguration"
