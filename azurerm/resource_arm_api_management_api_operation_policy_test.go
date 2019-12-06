91e7b87e74d4a3a15b115b5795e6f45ee9a03cda
Torstein Nicolaysen <tnicolaysen@gmail.com>
2019-12-06 14:21:40 +0100

    fix test failures

--- azurerm/resource_arm_api_management_api_operation_policy_test.go
+++ azurerm/resource_arm_api_management_api_operation_policy_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/Azure/azure-sdk-for-go/services/apimanagement/mgmt/2019-01-01/apimanagement"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
@@ -113,7 +114,7 @@ func testCheckAzureRMApiManagementAPIOperationPolicyExists(resourceName string) 
 
 		conn := testAccProvider.Meta().(*ArmClient).ApiManagement.ApiOperationPoliciesClient
 		ctx := testAccProvider.Meta().(*ArmClient).StopContext
-		resp, err := conn.Get(ctx, resourceGroup, serviceName, apiName, operationID)
+		resp, err := conn.Get(ctx, resourceGroup, serviceName, apiName, operationID, apimanagement.PolicyExportFormatXML) // TODO: VERIFY FORMAT
 		if err != nil {
 			if utils.ResponseWasNotFound(resp.Response) {
 				return fmt.Errorf("Bad: API Policy (API Management Service %q / API %q / Operation %q / Resource Group %q) does not exist", serviceName, apiName, operationID, resourceGroup)
@@ -140,7 +141,7 @@ func testCheckAzureRMApiManagementAPIOperationPolicyDestroy(s *terraform.State) 
 		operationID := rs.Primary.Attributes["operation_id"]
 
 		ctx := testAccProvider.Meta().(*ArmClient).StopContext
-		resp, err := conn.Get(ctx, resourceGroup, serviceName, apiName, operationID)
+		resp, err := conn.Get(ctx, resourceGroup, serviceName, apiName, operationID, apimanagement.PolicyExportFormatXML) // TODO: VERIFY FORMAT
 		if err != nil {
 			if utils.ResponseWasNotFound(resp.Response) {
 				return nil
