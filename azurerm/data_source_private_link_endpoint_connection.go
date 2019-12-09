ac403ee624f38fd1946fd720560153959f6c7fc0
tombuildsstuff <git@ibuildstuff.co.uk>
2019-12-08 20:11:28 -0700

    timeouts: adding timeouts to the data sources which are missing them

--- azurerm/data_source_private_link_endpoint_connection.go
+++ azurerm/data_source_private_link_endpoint_connection.go
@@ -2,17 +2,22 @@ package azurerm
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-09-01/network"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	aznet "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/network"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
 func dataSourceArmPrivateLinkEndpointConnection() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceArmPrivateLinkEndpointConnectionRead,
+		Timeouts: &schema.ResourceTimeout{
+			Read: schema.DefaultTimeout(5 * time.Minute),
+		},
 
 		Schema: map[string]*schema.Schema{
 			"name": {
@@ -56,7 +61,8 @@ func dataSourceArmPrivateLinkEndpointConnection() *schema.Resource {
 
 func dataSourceArmPrivateLinkEndpointConnectionRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*ArmClient).Network.PrivateEndpointClient
-	ctx := meta.(*ArmClient).StopContext
+	ctx, cancel := timeouts.ForRead(meta.(*ArmClient).StopContext, d)
+	defer cancel()
 
 	name := d.Get("name").(string)
 	resourceGroup := d.Get("resource_group_name").(string)
