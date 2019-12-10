42c9351695a4428d3155abd36449d3c9b40c9ac2
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2019-12-09 18:43:05 -0800

    Added a few more test cases

--- azurerm/internal/services/network/private_link_endpoint_test.go
+++ azurerm/internal/services/network/private_link_endpoint_test.go
@@ -11,13 +11,23 @@ func TestValidatePrivateLinkSubResourceName(t *testing.T) {
 		Valid bool
 	}{
 		{
-			Name:  "Empty",
+			Name:  "Empty Value",
 			Input: "",
+			Valid: false,
+		},
+		{
+			Name:  "Whitespace",
+			Input: " ",
 			Valid: false,
 		},
 		{
 			Name:  "Sql Server",
 			Input: "sqlServer",
+			Valid: true,
+		},
+		{
+			Name:  "Sql Server All Stop",
+			Input: "sql.Server",
 			Valid: true,
 		},
 		{
@@ -28,6 +38,11 @@ func TestValidatePrivateLinkSubResourceName(t *testing.T) {
 		{
 			Name:  "Blob Secondary Invalid",
 			Input: "blob-secondary",
+			Valid: false,
+		},
+		{
+			Name:  "Blob Secondary Space",
+			Input: "blob secondary",
 			Valid: false,
 		},
 		{
@@ -43,7 +58,7 @@ func TestValidatePrivateLinkSubResourceName(t *testing.T) {
 	}
 
 	for _, v := range testData {
-		t.Logf("[DEBUG] Testing %q", v.Input)
+		t.Logf("[DEBUG] Testing %q", v.Name)
 
 		_, errors := ValidatePrivateLinkSubResourceName(v.Input, "private_link_endpoint_subresource")
 		isValid := len(errors) == 0
