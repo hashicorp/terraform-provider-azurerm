d3e9f408dc256dc76ff49f2965ee06f1e00b76a7
Daniel Bradley <daniel.bradley@o2.com>
2019-12-11 13:17:15 +0000

    Fix up reference input test
    - Update imports to match stream input test.

--- azurerm/resource_arm_stream_analytics_reference_input_blob_test.go
+++ azurerm/resource_arm_stream_analytics_reference_input_blob_test.go
@@ -5,11 +5,12 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 
-	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/features"
 )
 
 func TestAccAzureRMStreamAnalyticsReferenceInputBlob_avro(t *testing.T) {
@@ -139,7 +140,7 @@ func TestAccAzureRMStreamAnalyticsReferenceInputBlob_update(t *testing.T) {
 }
 
 func TestAccAzureRMStreamAnalyticsReferenceInputBlob_requiresImport(t *testing.T) {
-	if !requireResourcesToBeImported {
+	if !features.ShouldResourcesBeImported() {
 		t.Skip("Skipping since resources aren't required to be imported")
 		return
 	}
