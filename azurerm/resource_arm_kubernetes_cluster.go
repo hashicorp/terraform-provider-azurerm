2c126ec5c5e5eb43dd878bb3bc29999a70a1bb42
Matthew Frahry <mbfrahry@gmail.com>
2019-12-13 15:11:56 -0800

    Start of kubernetes MSI work

--- azurerm/resource_arm_kubernetes_cluster.go
+++ azurerm/resource_arm_kubernetes_cluster.go
@@ -560,6 +560,32 @@ func resourceArmKubernetesCluster() *schema.Resource {
 				Computed:  true,
 				Sensitive: true,
 			},
+
+			"managed_cluster_identity": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"type": {
+							Type:     schema.TypeString,
+							Required: true,
+							ValidateFunc: validation.StringInSlice([]string{
+								string(containerservice.None),
+								string(containerservice.SystemAssigned),
+							}, false),
+						},
+						"principal_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"tenant_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
 		},
 	}
 }
@@ -640,6 +666,9 @@ func resourceArmKubernetesClusterCreate(d *schema.ResourceData, meta interface{}
 
 	enablePodSecurityPolicy := d.Get("enable_pod_security_policy").(bool)
 
+	managedClusterIdentityRaw := d.Get("managed_cluster_identity").([]interface{})
+	managedClusterIdentity := expandKubernetesClusterManagedClusterIdentity(managedClusterIdentityRaw)
+
 	parameters := containerservice.ManagedCluster{
 		Name:     &name,
 		Location: &location,
@@ -658,7 +687,8 @@ func resourceArmKubernetesClusterCreate(d *schema.ResourceData, meta interface{}
 			NodeResourceGroup:           utils.String(nodeResourceGroup),
 			EnablePodSecurityPolicy:     utils.Bool(enablePodSecurityPolicy),
 		},
-		Tags: tags.Expand(t),
+		Identity: managedClusterIdentity,
+		Tags:     tags.Expand(t),
 	}
 
 	future, err := client.CreateOrUpdate(ctx, resGroup, name, parameters)
@@ -790,6 +820,13 @@ func resourceArmKubernetesClusterUpdate(d *schema.ResourceData, meta interface{}
 		windowsProfileRaw := d.Get("windows_profile").([]interface{})
 		windowsProfile := expandKubernetesClusterWindowsProfile(windowsProfileRaw)
 		existing.ManagedClusterProperties.WindowsProfile = windowsProfile
+	}
+
+	if d.HasChange("managed_cluster_identity") {
+		updateCluster = true
+		managedClusterIdentityRaw := d.Get("managed_cluster_identity").([]interface{})
+		managedClusterIdentity := expandKubernetesClusterManagedClusterIdentity(managedClusterIdentityRaw)
+		existing.Identity = managedClusterIdentity
 	}
 
 	if updateCluster {
@@ -971,6 +1008,12 @@ func resourceArmKubernetesClusterRead(d *schema.ResourceData, meta interface{}) 
 		} else {
 			d.Set("kube_admin_config_raw", "")
 			d.Set("kube_admin_config", []interface{}{})
+		}
+	}
+
+	if identity := resp.Identity; identity != nil {
+		if err := d.Set("managed_cluster_identity", flattenKubernetesClusterManagedClusterIdentity(*identity)); err != nil {
+			return fmt.Errorf("Error setting `managed_cluster_identity`: %+v", err)
 		}
 	}
 
@@ -1403,6 +1446,20 @@ func expandKubernetesClusterRoleBasedAccessControl(input []interface{}, provider
 	return rbacEnabled, aad
 }
 
+func expandKubernetesClusterManagedClusterIdentity(input []interface{}) *containerservice.ManagedClusterIdentity {
+	if len(input) == 0 || input[0] == nil {
+		return nil
+	}
+
+	val := input[0].(map[string]interface{})
+
+	managedClusterIdentity := &containerservice.ManagedClusterIdentity{
+		Type: containerservice.ResourceIdentityType(val["type"].(string)),
+	}
+
+	return managedClusterIdentity
+}
+
 func flattenKubernetesClusterRoleBasedAccessControl(input *containerservice.ManagedClusterProperties, d *schema.ResourceData) []interface{} {
 	rbacEnabled := false
 	if input.EnableRBAC != nil {
@@ -1528,4 +1585,17 @@ func flattenKubernetesClusterKubeConfigAAD(config kubernetes.KubeConfigAAD) []in
 			"username":               name,
 		},
 	}
+}
+
+func flattenKubernetesClusterManagedClusterIdentity(input containerservice.ManagedClusterIdentity) []interface{} {
+	identity := make(map[string]interface{}, 0)
+	if input.PrincipalID != nil {
+		identity["principal_id"] = *input.PrincipalID
+	}
+	if input.TenantID != nil {
+		identity["tenant_id"] = *input.TenantID
+	}
+	identity["type"] = string(input.Type)
+
+	return []interface{}{identity}
 }
