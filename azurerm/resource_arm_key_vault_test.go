d304c1fc39ab3108c75099f02e9baae11f29b0ea
tombuildsstuff <git@ibuildstuff.co.uk>
2019-12-18 13:36:47 +0100

    r/key_vault: making the `network_acls` block computed

--- azurerm/resource_arm_key_vault_test.go
+++ azurerm/resource_arm_key_vault_test.go
@@ -695,7 +695,7 @@ resource "azurerm_key_vault" "test" {
   network_acls {
     default_action             = "Allow"
     bypass                     = "AzureServices"
-    ip_rules                   = ["10.0.0.102/32"]
+    ip_rules                   = ["123.0.0.102/32"]
     virtual_network_subnet_ids = ["${azurerm_subnet.test_a.id}"]
   }
 }
