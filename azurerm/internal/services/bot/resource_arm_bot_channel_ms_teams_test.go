59df731ab94615c54c2f3e7440241f7303bed419
Matthew Frahry <mbfrahry@gmail.com>
2019-12-19 15:37:49 -0800

    bot

--- azurerm/internal/services/bot/resource_arm_bot_channel_ms_teams_test.go
+++ azurerm/internal/services/bot/resource_arm_bot_channel_ms_teams_test.go
@@ -1,9 +1,8 @@
-package azurerm
+package bot
 
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/services/preview/botservice/mgmt/2018-07-12/botservice"
@@ -15,87 +14,80 @@ import (
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
-func TestAccAzureRMBotChannelEmail_basic(t *testing.T) {
-	if ok := skipEmailChannel(); ok {
-		t.Skip("Skipping as one of `ARM_TEST_EMAIL`, AND `ARM_TEST_EMAIL_PASSWORD` was not specified")
-	}
+func testAccAzureRMBotChannelMsTeams_basic(t *testing.T) {
 	ri := tf.AccRandTimeInt()
-	config := testAccAzureRMBotChannelEmail_basicConfig(ri, acceptance.Location())
-	resourceName := "azurerm_bot_channel_email.test"
+	config := testAccAzureRMBotChannelMsTeams_basicConfig(ri, acceptance.Location())
+	resourceName := "azurerm_bot_channel_ms_teams.test"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { acceptance.PreCheck(t) },
 		Providers:    acceptance.SupportedProviders,
-		CheckDestroy: testCheckAzureRMBotChannelEmailDestroy,
+		CheckDestroy: testCheckAzureRMBotChannelMsTeamsDestroy,
 		Steps: []resource.TestStep{
 			{
 				Config: config,
 				Check: resource.ComposeTestCheckFunc(
-					testCheckAzureRMBotChannelEmailExists(resourceName),
+					testCheckAzureRMBotChannelMsTeamsExists(resourceName),
 				),
 			},
 			{
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
-				ImportStateVerifyIgnore: []string{
-					"email_password",
-				},
 			},
 		},
 	})
 }
 
-func TestAccAzureRMBotChannelEmail_update(t *testing.T) {
-	if ok := skipEmailChannel(); ok {
-		t.Skip("Skipping as one of `ARM_TEST_EMAIL`, AND `ARM_TEST_EMAIL_PASSWORD` was not specified")
-	}
-	if ok := skipSlackChannel(); ok {
-		t.Skip("Skipping as one of `ARM_TEST_SLACK_CLIENT_ID`, `ARM_TEST_SLACK_CLIENT_SECRET`, or `ARM_TEST_SLACK_VERIFICATION_TOKEN` was not specified")
-	}
+func testAccAzureRMBotChannelMsTeams_update(t *testing.T) {
 	ri := tf.AccRandTimeInt()
-	config := testAccAzureRMBotChannelEmail_basicConfig(ri, acceptance.Location())
-	config2 := testAccAzureRMBotChannelEmail_basicUpdate(ri, acceptance.Location())
-	resourceName := "azurerm_bot_channel_email.test"
+	config := testAccAzureRMBotChannelMsTeams_basicConfig(ri, acceptance.Location())
+	config2 := testAccAzureRMBotChannelMsTeams_basicUpdate(ri, acceptance.Location())
+	resourceName := "azurerm_bot_channel_ms_teams.test"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { acceptance.PreCheck(t) },
 		Providers:    acceptance.SupportedProviders,
-		CheckDestroy: testCheckAzureRMBotChannelEmailDestroy,
+		CheckDestroy: testCheckAzureRMBotChannelMsTeamsDestroy,
 		Steps: []resource.TestStep{
 			{
 				Config: config,
 				Check: resource.ComposeTestCheckFunc(
-					testCheckAzureRMBotChannelEmailExists(resourceName),
+					testCheckAzureRMBotChannelMsTeamsExists(resourceName),
 				),
 			},
 			{
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
-				ImportStateVerifyIgnore: []string{
-					"email_password",
-				},
 			},
 			{
 				Config: config2,
 				Check: resource.ComposeTestCheckFunc(
-					testCheckAzureRMBotChannelEmailExists(resourceName),
+					testCheckAzureRMBotChannelMsTeamsExists(resourceName),
 				),
 			},
 			{
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
-				ImportStateVerifyIgnore: []string{
-					"email_password",
-				},
+			},
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMBotChannelMsTeamsExists(resourceName),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
 }
 
-func testCheckAzureRMBotChannelEmailExists(name string) resource.TestCheckFunc {
+func testCheckAzureRMBotChannelMsTeamsExists(name string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		// Ensure we have enough information in state to look up in API
 		rs, ok := s.RootModule().Resources[name]
@@ -106,85 +98,77 @@ func testCheckAzureRMBotChannelEmailExists(name string) resource.TestCheckFunc {
 		botName := rs.Primary.Attributes["bot_name"]
 		resourceGroup, hasResourceGroup := rs.Primary.Attributes["resource_group_name"]
 		if !hasResourceGroup {
-			return fmt.Errorf("Bad: no resource group found in state for Bot Channel Email")
+			return fmt.Errorf("Bad: no resource group found in state for Bot Channel MsTeams")
 		}
 
 		client := acceptance.AzureProvider.Meta().(*clients.Client).Bot.ChannelClient
 		ctx := acceptance.AzureProvider.Meta().(*clients.Client).StopContext
 
-		resp, err := client.Get(ctx, resourceGroup, botName, string(botservice.ChannelNameEmailChannel))
+		resp, err := client.Get(ctx, resourceGroup, botName, string(botservice.ChannelNameMsTeamsChannel))
 		if err != nil {
 			return fmt.Errorf("Bad: Get on botChannelClient: %+v", err)
 		}
 
 		if utils.ResponseWasNotFound(resp.Response) {
-			return fmt.Errorf("Bad: Bot Channel Email %q (resource group: %q / bot: %q) does not exist", name, resourceGroup, botName)
+			return fmt.Errorf("Bad: Bot Channel MsTeams %q (resource group: %q / bot: %q) does not exist", name, resourceGroup, botName)
 		}
 
 		return nil
 	}
 }
 
-func testCheckAzureRMBotChannelEmailDestroy(s *terraform.State) error {
+func testCheckAzureRMBotChannelMsTeamsDestroy(s *terraform.State) error {
 	client := acceptance.AzureProvider.Meta().(*clients.Client).Bot.ChannelClient
 	ctx := acceptance.AzureProvider.Meta().(*clients.Client).StopContext
 
 	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "azurerm_bot_channel_email" {
+		if rs.Type != "azurerm_bot_channel_ms_teams" {
 			continue
 		}
 
 		botName := rs.Primary.Attributes["bot_name"]
 		resourceGroup := rs.Primary.Attributes["resource_group_name"]
 
-		resp, err := client.Get(ctx, resourceGroup, botName, string(botservice.ChannelNameEmailChannel))
+		resp, err := client.Get(ctx, resourceGroup, botName, string(botservice.ChannelNameMsTeamsChannel))
 
 		if err != nil {
 			return nil
 		}
 
 		if resp.StatusCode != http.StatusNotFound {
-			return fmt.Errorf("Bot Channel Email still exists:\n%#v", resp.Properties)
+			return fmt.Errorf("Bot Channel MsTeams still exists:\n%#v", resp.Properties)
 		}
 	}
 
 	return nil
 }
 
-func testAccAzureRMBotChannelEmail_basicConfig(rInt int, location string) string {
+func testAccAzureRMBotChannelMsTeams_basicConfig(rInt int, location string) string {
 	template := testAccAzureRMBotChannelsRegistration_basicConfig(rInt, location)
 	return fmt.Sprintf(`
 %s
 
-resource "azurerm_bot_channel_email" "test" {
+resource "azurerm_bot_channel_ms_teams" "test" {
   bot_name            = "${azurerm_bot_channels_registration.test.name}"
   location            = "${azurerm_bot_channels_registration.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  email_address       = "%s"
-  email_password      = "%s"
+  calling_web_hook    = "https://example.com/"
+  enable_calling      = true
 }
-`, template, os.Getenv("ARM_TEST_EMAIL"), os.Getenv("ARM_TEST_EMAIL_PASSWORD"))
+`, template)
 }
 
-func testAccAzureRMBotChannelEmail_basicUpdate(rInt int, location string) string {
+func testAccAzureRMBotChannelMsTeams_basicUpdate(rInt int, location string) string {
 	template := testAccAzureRMBotChannelsRegistration_basicConfig(rInt, location)
 	return fmt.Sprintf(`
 %s
 
-resource "azurerm_bot_channel_email" "test" {
+resource "azurerm_bot_channel_ms_teams" "test" {
   bot_name            = "${azurerm_bot_channels_registration.test.name}"
   location            = "${azurerm_bot_channels_registration.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  email_address       = "%s"
-  email_password      = "%s"
+  calling_web_hook    = "https://example2.com/"
+  enable_calling      = false
 }
-`, template, os.Getenv("ARM_TEST_EMAIL"), os.Getenv("ARM_TEST_EMAIL_PASSWORD"))
-}
-
-func skipEmailChannel() bool {
-	if os.Getenv("ARM_TEST_EMAIL") == "" || os.Getenv("ARM_TEST_EMAIL_PASSWORD") == "" {
-		return true
-	}
-
-	return false
+`, template)
 }
--- azurerm/internal/services/bot/resource_arm_bot_web_app.go
+++ azurerm/internal/services/bot/resource_arm_bot_web_app.go
@@ -1,4 +1,4 @@
-package azurerm
+package bot
 
 import (
 	"fmt"
