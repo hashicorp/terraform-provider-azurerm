cd9168b614f9df5f0b960a2e065e2ec1f0675b67
Matthew Frahry <mbfrahry@gmail.com>
2019-12-19 16:58:00 -0800

    dns-final

--- azurerm/internal/services/dns/registration.go
+++ azurerm/internal/services/dns/registration.go
@@ -14,21 +14,21 @@ func (r Registration) Name() string {
 // SupportedDataSources returns the supported Data Sources supported by this Service
 func (r Registration) SupportedDataSources() map[string]*schema.Resource {
 	return map[string]*schema.Resource{
-		"azurerm_dns_zone":                      dataSourceArmDnsZone(),
+		"azurerm_dns_zone": dataSourceArmDnsZone(),
 	}
 }
 
 // SupportedResources returns the supported Resources supported by this Service
 func (r Registration) SupportedResources() map[string]*schema.Resource {
 	return map[string]*schema.Resource{
-		"azurerm_dns_a_record":                 resourceArmDnsARecord(),
-		"azurerm_dns_aaaa_record":              resourceArmDnsAAAARecord(),
-		"azurerm_dns_caa_record":               resourceArmDnsCaaRecord(),
-		"azurerm_dns_cname_record":             resourceArmDnsCNameRecord(),
-		"azurerm_dns_mx_record":                resourceArmDnsMxRecord(),
-		"azurerm_dns_ns_record":                resourceArmDnsNsRecord(),
-		"azurerm_dns_ptr_record":               resourceArmDnsPtrRecord(),
-		"azurerm_dns_srv_record":               resourceArmDnsSrvRecord(),
-		"azurerm_dns_txt_record":               resourceArmDnsTxtRecord(),
-		"azurerm_dns_zone":                     resourceArmDnsZone(),}
+		"azurerm_dns_a_record":     resourceArmDnsARecord(),
+		"azurerm_dns_aaaa_record":  resourceArmDnsAAAARecord(),
+		"azurerm_dns_caa_record":   resourceArmDnsCaaRecord(),
+		"azurerm_dns_cname_record": resourceArmDnsCNameRecord(),
+		"azurerm_dns_mx_record":    resourceArmDnsMxRecord(),
+		"azurerm_dns_ns_record":    resourceArmDnsNsRecord(),
+		"azurerm_dns_ptr_record":   resourceArmDnsPtrRecord(),
+		"azurerm_dns_srv_record":   resourceArmDnsSrvRecord(),
+		"azurerm_dns_txt_record":   resourceArmDnsTxtRecord(),
+		"azurerm_dns_zone":         resourceArmDnsZone()}
 }
