bdcdfbf66dde0841b1bc6786e0a9aed7f7e4eaef
Matthew Frahry <mbfrahry@gmail.com>
2019-12-19 17:54:42 -0800

    msi

--- azurerm/internal/services/msi/registration.go
+++ azurerm/internal/services/msi/registration.go
@@ -15,10 +15,14 @@ func (r Registration) Name() string {
 
 // SupportedDataSources returns the supported Data Sources supported by this Service
 func (r Registration) SupportedDataSources() map[string]*schema.Resource {
-	return map[string]*schema.Resource{}
+	return map[string]*schema.Resource{
+		"azurerm_user_assigned_identity": dataSourceArmUserAssignedIdentity(),
+	}
 }
 
 // SupportedResources returns the supported Resources supported by this Service
 func (r Registration) SupportedResources() map[string]*schema.Resource {
-	return map[string]*schema.Resource{}
+	return map[string]*schema.Resource{
+		"azurerm_user_assigned_identity": resourceArmUserAssignedIdentity(),
+	}
 }
--- azurerm/internal/services/msi/resource_arm_user_assigned_identity_test.go
+++ azurerm/internal/services/msi/resource_arm_user_assigned_identity_test.go
@@ -1,4 +1,4 @@
-package azurerm
+package msi
 
 import (
 	"fmt"
