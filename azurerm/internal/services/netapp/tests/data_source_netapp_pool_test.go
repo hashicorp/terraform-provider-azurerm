c6155789b9cad9602dc73c475161082c2f6912ec
kt <kt@katbyte.me>
2019-12-21 08:21:32 -0800

    netapp: switching to the new test structure

--- azurerm/internal/services/netapp/tests/data_source_netapp_pool_test.go
+++ azurerm/internal/services/netapp/tests/data_source_netapp_pool_test.go
@@ -1,39 +1,36 @@
-package netapp
+package tests
 
 import (
 	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
 )
 
 func TestAccDataSourceAzureRMNetAppPool_basic(t *testing.T) {
-	dataSourceName := "data.azurerm_netapp_pool.test"
-	ri := tf.AccRandTimeInt()
-	location := acceptance.Location()
+	data := acceptance.BuildTestData(t, "data.azurerm_netapp_pool", "test")
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { acceptance.PreCheck(t) },
 		Providers: acceptance.SupportedProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceNetAppPool_basic(ri, location),
+				Config: testAccDataSourceNetAppPool_basic(data),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(dataSourceName, "resource_group_name"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "name"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "account_name"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "service_level"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "size_in_tb"),
+					resource.TestCheckResourceAttrSet(data.ResourceName, "resource_group_name"),
+					resource.TestCheckResourceAttrSet(data.ResourceName, "name"),
+					resource.TestCheckResourceAttrSet(data.ResourceName, "account_name"),
+					resource.TestCheckResourceAttrSet(data.ResourceName, "service_level"),
+					resource.TestCheckResourceAttrSet(data.ResourceName, "size_in_tb"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceNetAppPool_basic(rInt int, location string) string {
-	config := testAccAzureRMNetAppPool_basic(rInt, location)
+func testAccDataSourceNetAppPool_basic(data acceptance.TestData) string {
+	config := testAccAzureRMNetAppPool_basic(data)
 	return fmt.Sprintf(`
 %s
 
