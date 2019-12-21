274f4b049a797944e0a627f7230146bb99dd5c92
kt <kt@katbyte.me>
2019-12-20 17:38:52 -0800

    managementgroup: switching to the new test structure

--- azurerm/internal/services/managementgroup/tests/data_source_management_group_test.go
+++ azurerm/internal/services/managementgroup/tests/data_source_management_group_test.go
@@ -1,34 +1,32 @@
-package managementgroup
+package tests
 
 import (
 	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
 )
 
 func TestAccDataSourceArmManagementGroup_basic(t *testing.T) {
-	dataSourceName := "data.azurerm_management_group.test"
-	ri := tf.AccRandTimeInt()
+	data := acceptance.BuildTestData(t, "data.azurerm_management_group", "test")
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { acceptance.PreCheck(t) },
 		Providers: acceptance.SupportedProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceArmManagementGroup_basic(ri),
+				Config: testAccDataSourceArmManagementGroup_basic(data),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(dataSourceName, "display_name", fmt.Sprintf("acctestmg-%d", ri)),
-					resource.TestCheckResourceAttr(dataSourceName, "subscription_ids.#", "0"),
+					resource.TestCheckResourceAttr(data.ResourceName, "display_name", fmt.Sprintf("acctestmg-%d", data.RandomInteger)),
+					resource.TestCheckResourceAttr(data.ResourceName, "subscription_ids.#", "0"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceArmManagementGroup_basic(rInt int) string {
+func testAccDataSourceArmManagementGroup_basic(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 resource "azurerm_management_group" "test" {
   display_name = "acctestmg-%d"
@@ -37,5 +35,5 @@ resource "azurerm_management_group" "test" {
 data "azurerm_management_group" "test" {
   group_id = "${azurerm_management_group.test.group_id}"
 }
-`, rInt)
+`, data.RandomInteger)
 }
