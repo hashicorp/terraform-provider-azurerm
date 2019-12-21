aeb331036a310d56a20ab3a128c11114027ca68a
tombuildsstuff <git@ibuildstuff.co.uk>
2019-12-21 17:58:54 +0100

    notificationhub: fixing the tests

--- azurerm/internal/services/notificationhub/tests/data_source_notification_hub_namespace_test.go
+++ azurerm/internal/services/notificationhub/tests/data_source_notification_hub_namespace_test.go
@@ -1,38 +1,33 @@
-package notificationhub
+package tests
 
 import (
 	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
 )
 
 func TestAccDataSourceAzureRMNotificationHubNamespace_free(t *testing.T) {
-	dataSourceName := "data.azurerm_notification_hub_namespace.test"
-	rInt := tf.AccRandTimeInt()
-	location := acceptance.Location()
-	config := testAccDataSourceAzureRMNotificationHubNamespaceFree(rInt, location)
-
+	data := acceptance.BuildTestData(t, "data.azurerm_notification_hub_namespace", "test")
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:     func() { acceptance.PreCheck(t) },
 		Providers:    acceptance.SupportedProviders,
 		CheckDestroy: testCheckAzureRMNotificationHubNamespaceDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: config,
+				Config: testAccDataSourceAzureRMNotificationHubNamespaceFree(data),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(dataSourceName, "namespace_type", "NotificationHub"),
-					resource.TestCheckResourceAttr(dataSourceName, "sku.0.name", "Free"),
+					resource.TestCheckResourceAttr(data.ResourceName, "namespace_type", "NotificationHub"),
+					resource.TestCheckResourceAttr(data.ResourceName, "sku.0.name", "Free"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceAzureRMNotificationHubNamespaceFree(rInt int, location string) string {
-	r := testAccAzureRMNotificationHubNamespace_free(rInt, location)
+func testAccDataSourceAzureRMNotificationHubNamespaceFree(data acceptance.TestData) string {
+	template := testAccAzureRMNotificationHubNamespace_free(data)
 	return fmt.Sprintf(`
 %s
 
@@ -40,5 +35,5 @@ data "azurerm_notification_hub_namespace" "test" {
   name                = "${azurerm_notification_hub_namespace.test.name}"
   resource_group_name = "${azurerm_notification_hub_namespace.test.resource_group_name}"
 }
-`, r)
+`, template)
 }
