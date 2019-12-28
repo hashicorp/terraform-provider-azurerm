7b25b377e643117ac99d36e4d128ae7596316614
David McPike <7037366+mcdafydd@users.noreply.github.com>
2019-12-27 19:23:26 -0600

    Add moved tests to registration

--- azurerm/internal/services/monitor/registration.go
+++ azurerm/internal/services/monitor/registration.go
@@ -16,19 +16,21 @@ func (r Registration) SupportedDataSources() map[string]*schema.Resource {
 	return map[string]*schema.Resource{
 		"azurerm_monitor_action_group":          dataSourceArmMonitorActionGroup(),
 		"azurerm_monitor_diagnostic_categories": dataSourceArmMonitorDiagnosticCategories(),
-		"azurerm_monitor_log_profile":           dataSourceArmMonitorLogProfile()}
+		"azurerm_monitor_log_profile":           dataSourceArmMonitorLogProfile(),
+		"azurerm_monitor_scheduled_query_rules": dataSourceArmMonitorScheduledQueryRules()}
 }
 
 // SupportedResources returns the supported Resources supported by this Service
 func (r Registration) SupportedResources() map[string]*schema.Resource {
 	return map[string]*schema.Resource{
-		"azurerm_autoscale_setting":          resourceArmAutoScaleSetting(),
-		"azurerm_metric_alertrule":           resourceArmMetricAlertRule(),
-		"azurerm_monitor_autoscale_setting":  resourceArmMonitorAutoScaleSetting(),
-		"azurerm_monitor_action_group":       resourceArmMonitorActionGroup(),
-		"azurerm_monitor_activity_log_alert": resourceArmMonitorActivityLogAlert(),
-		"azurerm_monitor_diagnostic_setting": resourceArmMonitorDiagnosticSetting(),
-		"azurerm_monitor_log_profile":        resourceArmMonitorLogProfile(),
-		"azurerm_monitor_metric_alert":       resourceArmMonitorMetricAlert(),
-		"azurerm_monitor_metric_alertrule":   resourceArmMonitorMetricAlertRule()}
+		"azurerm_autoscale_setting":             resourceArmAutoScaleSetting(),
+		"azurerm_metric_alertrule":              resourceArmMetricAlertRule(),
+		"azurerm_monitor_autoscale_setting":     resourceArmMonitorAutoScaleSetting(),
+		"azurerm_monitor_action_group":          resourceArmMonitorActionGroup(),
+		"azurerm_monitor_activity_log_alert":    resourceArmMonitorActivityLogAlert(),
+		"azurerm_monitor_diagnostic_setting":    resourceArmMonitorDiagnosticSetting(),
+		"azurerm_monitor_log_profile":           resourceArmMonitorLogProfile(),
+		"azurerm_monitor_metric_alert":          resourceArmMonitorMetricAlert(),
+		"azurerm_monitor_metric_alertrule":      resourceArmMonitorMetricAlertRule(),
+		"azurerm_monitor_scheduled_query_rules": resourceArmMonitorScheduledQueryRules()}
 }
