6d0d591d71baf7d3871201b04da804fd4c93ecfe
Tracy Holmes <tracypholmes@gmail.com>
2020-01-06 23:30:35 -0600

    services: eventhub

--- azurerm/internal/services/eventhub/tests/resource_arm_eventhub_test.go
+++ azurerm/internal/services/eventhub/tests/resource_arm_eventhub_test.go
@@ -384,6 +384,9 @@ func testCheckAzureRMEventHubDestroy(s *terraform.State) error {
 
 func testCheckAzureRMEventHubExists(resourceName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
+		conn := acceptance.AzureProvider.Meta().(*clients.Client).Eventhub.EventHubsClient
+		ctx := acceptance.AzureProvider.Meta().(*clients.Client).StopContext
+
 		// Ensure we have enough information in state to look up in API
 		rs, ok := s.RootModule().Resources[resourceName]
 		if !ok {
@@ -397,8 +400,7 @@ func testCheckAzureRMEventHubExists(resourceName string) resource.TestCheckFunc 
 			return fmt.Errorf("Bad: no resource group found in state for Event Hub: %s", name)
 		}
 
-		conn := acceptance.AzureProvider.Meta().(*clients.Client).Eventhub.EventHubsClient
-		ctx := acceptance.AzureProvider.Meta().(*clients.Client).StopContext
+
 
 		resp, err := conn.Get(ctx, resourceGroup, namespaceName, name)
 		if err != nil {
