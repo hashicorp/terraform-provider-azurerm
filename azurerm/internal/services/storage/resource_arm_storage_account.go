b5f34d953a698e71e47f8e4ee54e1bf724510fb7
magodo <wztwcy@gmail.com>
2020-01-09 06:40:17 +0800

    azurerm_storage_account - fix performance issue for accounts that don't support queues  (#5316)
    Not all storage account supports queue. Without this condition, user with unsupported kind will suffer a block (for about 10min) while reading the resource.

--- azurerm/internal/services/storage/resource_arm_storage_account.go
+++ azurerm/internal/services/storage/resource_arm_storage_account.go
@@ -1239,20 +1239,29 @@ func resourceArmStorageAccountRead(d *schema.ResourceData, meta interface{}) err
 		}
 	}
 
-	queueClient, err := storageClient.QueuesClient(ctx, *account)
-	if err != nil {
-		return fmt.Errorf("Error building Queues Client: %s", err)
+	// queue is only available for certain tier and kind (as specified below)
+	if resp.Sku == nil {
+		return fmt.Errorf("Error retrieving Storage Account %q (Resource Group %q): `sku` was nil", name, resGroup)
 	}
 
-	queueProps, err := queueClient.GetServiceProperties(ctx, name)
-	if err != nil {
-		if queueProps.Response.Response != nil && !utils.ResponseWasNotFound(queueProps.Response) {
-			return fmt.Errorf("Error reading queue properties for AzureRM Storage Account %q: %+v", name, err)
+	if resp.Sku.Tier == storage.Standard {
+		if resp.Kind == storage.Storage || resp.Kind == storage.StorageV2 {
+			queueClient, err := storageClient.QueuesClient(ctx, *account)
+			if err != nil {
+				return fmt.Errorf("Error building Queues Client: %s", err)
+			}
+
+			queueProps, err := queueClient.GetServiceProperties(ctx, name)
+			if err != nil {
+				if queueProps.Response.Response != nil && !utils.ResponseWasNotFound(queueProps.Response) {
+					return fmt.Errorf("Error reading queue properties for AzureRM Storage Account %q: %+v", name, err)
+				}
+			}
+
+			if err := d.Set("queue_properties", flattenQueueProperties(queueProps)); err != nil {
+				return fmt.Errorf("Error setting `queue_properties `for AzureRM Storage Account %q: %+v", name, err)
+			}
 		}
-	}
-
-	if err := d.Set("queue_properties", flattenQueueProperties(queueProps)); err != nil {
-		return fmt.Errorf("Error setting `queue_properties `for AzureRM Storage Account %q: %+v", name, err)
 	}
 
 	return tags.FlattenAndSet(d, resp.Tags)
