d8133bd5d10c18ba648ca3c0e0af72349fc262ac
jackofallops <ste@hashicorp.com>
2020-01-10 07:28:11 +0000

    separated relay namespace and hybrid_connection ID parse and validate

--- azurerm/internal/services/relay/hybrid_connection.go
+++ azurerm/internal/services/relay/hybrid_connection.go
@@ -12,11 +12,6 @@ type HybridConnectionResourceID struct {
 	NamespaceName string
 }
 
-type NamespaceResourceID struct {
-	ResourceGroup string
-	Name          string
-}
-
 func ParseHybridConnectionID(input string) (*HybridConnectionResourceID, error) {
 	id, err := azure.ParseAzureResourceID(input)
 	if err != nil {
@@ -41,28 +36,8 @@ func ParseHybridConnectionID(input string) (*HybridConnectionResourceID, error) 
 	return &hybridConnection, nil
 }
 
-func ParseNamespaceID(input string) (*NamespaceResourceID, error) {
-	id, err := azure.ParseAzureResourceID(input)
-	if err != nil {
-		return nil, fmt.Errorf("[ERROR] Unable to parse Relay Namespace ID %q: %+v", input, err)
-	}
-	nameSpace := NamespaceResourceID{
-		ResourceGroup: id.ResourceGroup,
-	}
-
-	if nameSpace.Name, err = id.PopSegment("namespaces"); err != nil {
-		return nil, err
-	}
-
-	if err := id.ValidateNoEmptySegments(input); err != nil {
-		return nil, err
-	}
-
-	return &nameSpace, nil
-}
-
 // ValidateHybridConnectionID validates that the specified ID is a valid Relay Hybrid Connection ID
-func ValidateHybridConnectionID(i interface{}, k string) (warnings []string, errors []error){
+func ValidateHybridConnectionID(i interface{}, k string) (warnings []string, errors []error) {
 	v, ok := i.(string)
 	if !ok {
 		errors = append(errors, fmt.Errorf("expected type of %q to be string", k))
@@ -71,22 +46,6 @@ func ValidateHybridConnectionID(i interface{}, k string) (warnings []string, err
 	if _, err := ParseHybridConnectionID(v); err != nil {
 		errors = append(errors, fmt.Errorf("Can not parse %q as a resource id: %v", v, err))
 	}
-
-	return warnings, errors
-}
-
-// ValidateNamespaceID validates that the specified ID is a valid Relay Namespace ID
-func ValidateNamespaceID(i interface{}, k string) (warnings []string, errors []error){
-	v, ok := i.(string)
-	if !ok {
-		errors = append(errors, fmt.Errorf("expected type of %q to be string", k))
-	}
-
-	if _, err := ParseNamespaceID(v); err != nil {
-		errors = append(errors, fmt.Errorf("Can not parse %q as a resource id: %v", k, err))
-		return
-	}
-
 
 	return warnings, errors
 }
--- azurerm/internal/services/relay/relay_namespace_test.go
+++ azurerm/internal/services/relay/relay_namespace_test.go
@@ -4,57 +4,6 @@ import (
 	"testing"
 )
 
-func TestParseHybridConnectionID(t *testing.T) {
-	testData := []struct {
-		Name     string
-		Input    string
-		Expected *HybridConnectionResourceID
-	}{
-		{
-			Name:     "Empty",
-			Input:    "",
-			Expected: nil,
-		},
-		{
-			Name:     "No Resource Groups Segment",
-			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000",
-			Expected: nil,
-		},
-		{
-			Name:     "No Resource Groups Value",
-			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/",
-			Expected: nil,
-		},
-		{
-			Name:     "Resource Group ID",
-			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/foo/",
-			Expected: nil,
-		},
-		// Todo: complete coverage
-	}
-	for _, v := range testData {
-		t.Logf("[DEBUG] Testing %q", v.Name)
-
-		actual, err := ParseHybridConnectionID(v.Input)
-		if err != nil {
-			if v.Expected == nil {
-				continue
-			}
-
-			t.Fatalf("Expected a value but got an error: %s", err)
-		}
-
-		if actual.Name != v.Expected.Name {
-			t.Fatalf("Expected %q but got %q for Name", v.Expected.Name, actual.Name)
-		}
-
-		if actual.ResourceGroup != v.Expected.ResourceGroup {
-			t.Fatalf("Expected %q but got %q for Resource Group", v.Expected.ResourceGroup, actual.ResourceGroup)
-		}
-	}
-
-}
-
 func TestParseRelayNamespaceID(t *testing.T) {
 	testData := []struct {
 		Name     string
