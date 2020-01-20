09d3617b7fa239e66a749ca29706cd83eb87cf94
kt <kt@katbyte.me>
2020-01-20 15:38:53 -0800

    Update validation regex

--- azurerm/internal/services/postgres/resource_arm_postgresql_server.go
+++ azurerm/internal/services/postgres/resource_arm_postgresql_server.go
@@ -23,7 +23,7 @@ import (
 )
 
 func ValidatePSQLServerName(i interface{}, k string) (_ []string, errors []error) {
-	if m, regexErrs := validate.RegExHelper(i, k, `^[a-z][0-9a-z][-0-9a-z]{0,60}[0-9a-z]$`); !m {
+	if m, regexErrs := validate.RegExHelper(i, k, `^[0-9a-z]{2}[-0-9a-z]{0,60}[0-9a-z]$`); !m {
 		errors = append(regexErrs, fmt.Errorf("%q can contain only lowercase letters, numbers, and '-', but can't start or end with '-' or have more than 63 characters.", k))
 	}
 
