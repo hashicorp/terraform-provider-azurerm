d42f4933e5e72b3eeea36cbc15ccb7cdec862281
WS <20408400+WodansSon@users.noreply.github.com>
2020-01-24 17:44:20 -0800

    Bug: `azurerm_frontdoor` fix for caching issue (#5358)
    * Port PR to new code base
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Update website/docs/r/front_door.html.markdown
    
    Co-Authored-By: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    
    * Progress
    
    * Saving progress
    
    * Almost right
    
    * Cache working
    
    * Fully working without defaults
    
    * Update docs to document new behavior
    
    * Fix tests and documentation
    
    * Add test case for cache regression
    
    * Fix test code terrafmt
    
    * Fixed linting error in comment
    
    * pull value through test
    
    * Update docs and remove commented code
    
    * Update test case
    
    * terrafmt
    
    * Minor doc update
    
    * Added nil check per PR review
    
    Co-authored-by: Tom Harvey <tombuildsstuff@users.noreply.github.com>
    Co-authored-by: kt <kt@katbyte.me>

--- azurerm/internal/services/frontdoor/tests/resource_arm_front_door_firewall_policy_test.go
+++ azurerm/internal/services/frontdoor/tests/resource_arm_front_door_firewall_policy_test.go
@@ -209,46 +209,18 @@ resource "azurerm_frontdoor_firewall_policy" "import" {
 }
 
 func testAccAzureRMFrontDoorFirewallPolicy_update(data acceptance.TestData, update bool) string {
-	inner := ""
 	if update {
-		inner = fmt.Sprintf(`
-custom_rule {
-  name                           = "Rule2"
-  enabled                        = true
-  priority                       = 2
-  rate_limit_duration_in_minutes = 1
-  rate_limit_threshold           = 10
-  type                           = "MatchRule"
-  action                         = "Block"
-
-  match_condition {
-    match_variable     = "RemoteAddr"
-    operator           = "IPMatch"
-    negation_condition = false
-    match_values       = ["192.168.1.0/24"]
-  }
-
-  match_condition {
-    match_variable     = "RequestHeader"
-    selector           = "UserAgent"
-    operator           = "Contains"
-    negation_condition = false
-    match_values       = ["windows"]
-    transforms         = ["Lowercase", "Trim"]
-  }
-}
-`)
+		return testAccAzureRMFrontDoorFirewallPolicy_updated(data)
 	}
-
 	return fmt.Sprintf(`
 resource "azurerm_resource_group" "test" {
   name     = "testAccRG-%d"
-  location = "%[2]s"
+  location = "%s"
 }
 
 resource "azurerm_frontdoor_firewall_policy" "test" {
   name                              = "testAccFrontDoorWAF%[1]d"
-  resource_group_name               = "${azurerm_resource_group.test.name}"
+  resource_group_name               = azurerm_resource_group.test.name
   enabled                           = true
   mode                              = "Prevention"
   redirect_url                      = "https://www.contoso.com"
@@ -272,7 +244,87 @@ resource "azurerm_frontdoor_firewall_policy" "test" {
     }
   }
 
-  %[3]s
+  managed_rule {
+    type    = "DefaultRuleSet"
+    version = "preview-0.1"
+
+    override {
+      rule_group_name = "PHP"
+
+      rule {
+        rule_id = "933111"
+        enabled = false
+        action  = "Block"
+      }
+    }
+  }
+
+  managed_rule {
+    type    = "BotProtection"
+    version = "preview-0.1"
+  }
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func testAccAzureRMFrontDoorFirewallPolicy_updated(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "testAccRG-%d"
+  location = "%[2]s"
+}
+
+resource "azurerm_frontdoor_firewall_policy" "test" {
+  name                              = "testAccFrontDoorWAF%[1]d"
+  resource_group_name               = azurerm_resource_group.test.name
+  enabled                           = true
+  mode                              = "Prevention"
+  redirect_url                      = "https://www.contoso.com"
+  custom_block_response_status_code = 403
+  custom_block_response_body        = "PGh0bWw+CjxoZWFkZXI+PHRpdGxlPkhlbGxvPC90aXRsZT48L2hlYWRlcj4KPGJvZHk+CkhlbGxvIHdvcmxkCjwvYm9keT4KPC9odG1sPg=="
+
+  custom_rule {
+    name                           = "Rule1"
+    enabled                        = true
+    priority                       = 1
+    rate_limit_duration_in_minutes = 1
+    rate_limit_threshold           = 10
+    type                           = "MatchRule"
+    action                         = "Block"
+
+    match_condition {
+      match_variable     = "RemoteAddr"
+      operator           = "IPMatch"
+      negation_condition = false
+      match_values       = ["192.168.1.0/24", "10.0.0.0/24"]
+    }
+  }
+
+  custom_rule {
+    name                           = "Rule2"
+    enabled                        = true
+    priority                       = 2
+    rate_limit_duration_in_minutes = 1
+    rate_limit_threshold           = 10
+    type                           = "MatchRule"
+    action                         = "Block"
+
+    match_condition {
+      match_variable     = "RemoteAddr"
+      operator           = "IPMatch"
+      negation_condition = false
+      match_values       = ["192.168.1.0/24"]
+    }
+
+    match_condition {
+      match_variable     = "RequestHeader"
+      selector           = "UserAgent"
+      operator           = "Contains"
+      negation_condition = false
+      match_values       = ["windows"]
+      transforms         = ["Lowercase", "Trim"]
+    }
+  }
 
   managed_rule {
     type    = "DefaultRuleSet"
@@ -294,5 +346,5 @@ resource "azurerm_frontdoor_firewall_policy" "test" {
     version = "preview-0.1"
   }
 }
-`, data.RandomInteger, data.Locations.Primary, inner)
+`, data.RandomInteger, data.Locations.Primary)
 }
