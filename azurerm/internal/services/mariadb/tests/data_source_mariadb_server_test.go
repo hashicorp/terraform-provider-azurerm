5067045dd74489d845146cbf81669f5def5e986e
Tracy Holmes <tracypholmes@gmail.com>
2020-01-24 23:02:28 -0600

    updates value in test to correct name

--- azurerm/internal/services/mariadb/tests/data_source_mariadb_server_test.go
+++ azurerm/internal/services/mariadb/tests/data_source_mariadb_server_test.go
@@ -56,7 +56,7 @@ resource "azurerm_mariadb_server" "test" {
 }
 
 data "azurerm_mariadb_server" "test" {
-  name                = "${azurerm_mariadb.test.name}"
+  name                = "${azurerm_mariadb_server.test.name}"
   resource_group_name = "${azurerm_resource_group.test.name}"
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
