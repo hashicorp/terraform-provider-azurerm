ec22e4462089460204434afdaaffb3339a62691d
Guillaume Stevens <guillaume.stevens@sncf.fr>
2020-01-27 21:16:06 +0100

    change blocks not correctly formatted

--- azurerm/internal/services/servicebus/tests/resource_arm_servicebus_namespace_test.go
+++ azurerm/internal/services/servicebus/tests/resource_arm_servicebus_namespace_test.go
@@ -410,24 +410,24 @@ resource "azurerm_servicebus_namespace" "test" {
 func testAccAzureRMServiceBusNamespace_networkrule_iprule(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 resource "azurerm_resource_group" "test" {
-	name = "acctestRG-%[1]d"
-	location = "%[2]s"
+  name = "acctestRG-%[1]d"
+  location = "%[2]s"
 }
 
 resource "azurerm_servicebus_namespace" "test" {
-	name 		= "acctestservicebusnamespace-%[1]d"
-	location	= "${azurerm_resource_group.test.location}"
-	resource_group_name = "${azurerm_resource_group.test.name}"
-	sku = "Premium"
-	capacity = 1
+  name 		= "acctestservicebusnamespace-%[1]d"
+  location	= "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  sku = "Premium"
+  capacity = 1
 	
-	network_rulesets {
-		default_action = "Deny"
-		ip_rule {
-			ip_mask = "10.0.0.0/16"
-			action = "Allow"
-		}
+  network_rulesets {
+    default_action = "Deny"
+    ip_rule {
+      ip_mask = "10.0.0.0/16"
+	  action = "Allow"
 	}
+  }
 }
 `, data.RandomInteger, data.Locations.Primary)
 }
@@ -435,39 +435,39 @@ resource "azurerm_servicebus_namespace" "test" {
 func testAccAzureRMServiceBusNamespace_networkrule_vnet(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 resource "azurerm_resource_group" "test" {
-	name = "acctestRG-[1]%d"
-	location = "%[2]s"
+  name = "acctestRG-[1]%d"
+  location = "%[2]s"
 }
 
 resource "azurerm_virtual_network" "test" {
-	name                = "acctvn-%[1]d"
-	address_space       = ["10.0.0.0/16"]
-	location            = "${azurerm_resource_group.test.location}"
-	resource_group_name = "${azurerm_resource_group.test.name}"
-  }
+  name                = "acctvn-%[1]d"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
   
-  resource "azurerm_subnet" "test" {
-	name                 = "acctsub-%[1]d"
-	resource_group_name  = "${azurerm_resource_group.test.name}"
-	virtual_network_name = "${azurerm_virtual_network.test.name}"
-	address_prefix       = "10.0.2.0/24"
-  }
+resource "azurerm_subnet" "test" {
+  name                 = "acctsub-%[1]d"
+  resource_group_name  = "${azurerm_resource_group.test.name}"
+  virtual_network_name = "${azurerm_virtual_network.test.name}"
+  address_prefix       = "10.0.2.0/24"
+}
 
 resource "azurerm_servicebus_namespace" "test" {
-	name 		= "acctestservicebusnamespace-%[1]d"
-	location	= "${azurerm_resource_group.test.location}"
-	resource_group_name = "${azurerm_resource_group.test.name}"
-	sku = "Premium"
-	capacity = 1
+  name 		= "acctestservicebusnamespace-%[1]d"
+  location	= "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  sku = "Premium"
+  capacity = 1
 	
-	network_rulesets {
-		default_action = "Deny"
-		virtual_network_rule {
-			subnet_id = "${azurerm_subnet.test.id}"
+  network_rulesets {
+    default_action = "Deny"
+	virtual_network_rule {
+	  subnet_id = "${azurerm_subnet.test.id}"
 	  
-			ignore_missing_virtual_network_service_endpoint = true
-		}
+	  ignore_missing_virtual_network_service_endpoint = true
 	}
+  }
 }
 `, data.RandomInteger, data.Locations.Primary)
 }
@@ -475,24 +475,24 @@ resource "azurerm_servicebus_namespace" "test" {
 func testAccAzureRMServiceBusNamespace_rule_set_basicSKU(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 resource "azurerm_resource_group" "test" {
-	name = "acctestRG-%[1]d"
-	location = "%[2]s"
+  name = "acctestRG-%[1]d"
+  location = "%[2]s"
 }
 
 resource "azurerm_servicebus_namespace" "test" {
-	name 		= "acctestservicebusnamespace-%[1]d"
-	location	= "${azurerm_resource_group.test.location}"
-	resource_group_name = "${azurerm_resource_group.test.name}"
-	sku = "Basic"
-	capacity = 0
+  name 		= "acctestservicebusnamespace-%[1]d"
+  location	= "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  sku = "Basic"
+  capacity = 0
 	
-	network_rulesets {
-		default_action = "Deny"
-		ip_rule {
-			ip_mask = "10.0.0.0/16"
-			action = "Allow"
-		}
-	}
+  network_rulesets {
+    default_action = "Deny"
+    ip_rule {
+      ip_mask = "10.0.0.0/16"
+      action = "Allow"
+    }
+  }
 }
 `, data.RandomInteger, data.Locations.Primary)
 }
@@ -500,24 +500,24 @@ resource "azurerm_servicebus_namespace" "test" {
 func testAccAzureRMServiceBusNamespace_rule_set_standardSKU(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 resource "azurerm_resource_group" "test" {
-	name = "acctestRG-%[1]d"
-	location = "%[2]s"
+  name = "acctestRG-%[1]d"
+  location = "%[2]s"
 }
 
 resource "azurerm_servicebus_namespace" "test" {
-	name 		= "acctestservicebusnamespace-%[1]d"
-	location	= "${azurerm_resource_group.test.location}"
-	resource_group_name = "${azurerm_resource_group.test.name}"
-	sku = "Standard"
-	capacity = 0
+  name 		= "acctestservicebusnamespace-%[1]d"
+  location	= "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  sku = "Standard"
+  capacity = 0
 	
-	network_rulesets {
-		default_action = "Deny"
-		ip_rule {
-			ip_mask = "10.0.0.0/16"
-			action = "Allow"
-		}
-	}
+  network_rulesets {
+    default_action = "Deny"
+    ip_rule {
+      ip_mask = "10.0.0.0/16"
+      action = "Allow"
+    }
+  }
 }
 `, data.RandomInteger, data.Locations.Primary)
 }
