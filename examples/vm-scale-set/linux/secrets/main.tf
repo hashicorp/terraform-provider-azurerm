d19c0f926bbd2043bc03dd503c129b891cc1d9cc
tombuildsstuff <git@ibuildstuff.co.uk>
2020-02-03 20:17:24 +0100

    examples: fixing the `init`

--- examples/vm-scale-set/linux/secrets/main.tf
+++ examples/vm-scale-set/linux/secrets/main.tf
@@ -31,7 +31,7 @@ resource "azurerm_key_vault" "main" {
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current._object_id
+    object_id = data.azurerm_client_config.current.object_id
 
     certificate_permissions = [
       "create",
