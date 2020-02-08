abd8429b996d9ea471b8aa76e7ebb94482c32def
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2020-02-07 16:56:13 -0800

    Fix test case lint issue

--- azurerm/internal/services/keyvault/tests/resource_arm_key_vault_key_test.go
+++ azurerm/internal/services/keyvault/tests/resource_arm_key_vault_key_test.go
@@ -781,7 +781,7 @@ resource "azurerm_key_vault" "test" {
   tenant_id           = "${data.azurerm_client_config.current.tenant_id}"
 
   sku_name = "premium"
-  
+
   access_policy {
     tenant_id = "${data.azurerm_client_config.current.tenant_id}"
     object_id = "${data.azurerm_client_config.current.service_principal_object_id}"
