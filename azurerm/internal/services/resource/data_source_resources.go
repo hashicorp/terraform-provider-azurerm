d2c2bfda6a966181033f290270d1590d6daa9c88
John Stevans <jstevans@users.noreply.github.com>
2020-02-13 15:09:37 -0800

    Allow "azurerm_resources" to be filtered by resource ID
    typo

--- azurerm/internal/services/resource/data_source_resources.go
+++ azurerm/internal/services/resource/data_source_resources.go
@@ -1,11 +1,13 @@
 package resource
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"strings"
 	"time"
 
+	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2018-05-01/resources"
 	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
@@ -22,6 +24,11 @@ func dataSourceArmResources() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
 			"name": {
 				Type:     schema.TypeString,
 				Optional: true,
@@ -71,17 +78,46 @@ func dataSourceArmResourcesRead(d *schema.ResourceData, meta interface{}) error 
 	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
+	resourceID := d.Get("id").(string)
 	resourceGroupName := d.Get("resource_group_name").(string)
 	resourceName := d.Get("name").(string)
 	resourceType := d.Get("type").(string)
 	requiredTags := d.Get("required_tags").(map[string]interface{})
 
-	if resourceGroupName == "" && resourceName == "" && resourceType == "" {
-		return fmt.Errorf("At least one of `name`, `resource_group_name` or `type` must be specified")
+	if resourceID == "" && resourceGroupName == "" && resourceName == "" && resourceType == "" {
+		return fmt.Errorf("At least one of `id`, `name`, `resource_group_name` or `type` must be specified")
 	}
 
-	var filter string
+	var resources []map[string]interface{}
 
+	if resourceID != "" {
+		resource, err := client.GetByID(ctx, resourceID, "")
+
+		if err != nil {
+			return fmt.Errorf("Error fetching resources: %+v", err)
+		}
+
+		resources = append(resources, *transformResource(resource))
+	} else {
+		resourceMap, err := getResourcesByFilter(ctx, client, resourceGroupName, resourceName, resourceType, requiredTags)
+
+		if err != nil {
+			return err
+		}
+
+		resources = resourceMap
+	}
+
+	d.SetId("resource-" + uuid.New().String())
+	if err := d.Set("resources", resources); err != nil {
+		return fmt.Errorf("Error setting `resources`: %+v", err)
+	}
+
+	return nil
+}
+
+func getResourcesByFilter(ctx context.Context, client *resources.Client, resourceGroupName, resourceName, resourceType string, requiredTags map[string]interface{}) ([]map[string]interface{}, error) {
+	var filter string
 	if resourceGroupName != "" {
 		v := fmt.Sprintf("resourceGroup eq '%s'", resourceGroupName)
 		filter = filter + v
@@ -102,11 +138,10 @@ func dataSourceArmResourcesRead(d *schema.ResourceData, meta interface{}) error 
 		v := fmt.Sprintf("resourceType eq '%s'", resourceType)
 		filter = filter + v
 	}
-
 	resources := make([]map[string]interface{}, 0)
 	resourcesResp, err := client.ListComplete(ctx, filter, "", nil)
 	if err != nil {
-		return fmt.Errorf("Error getting resources: %+v", err)
+		return nil, fmt.Errorf("Error getting resources: %+v", err)
 	}
 
 	for resourcesResp.NotDone() {
@@ -129,57 +164,56 @@ func dataSourceArmResourcesRead(d *schema.ResourceData, meta interface{}) error 
 		}
 
 		if tagMatches == len(requiredTags) {
-			resName := ""
-			if res.Name != nil {
-				resName = *res.Name
-			}
-
-			resID := ""
-			if res.ID != nil {
-				resID = *res.ID
-			}
-
-			resType := ""
-			if res.Type != nil {
-				resType = *res.Type
-			}
-
-			resLocation := ""
-			if res.Location != nil {
-				resLocation = *res.Location
-			}
-
-			resTags := make(map[string]interface{})
-			if res.Tags != nil {
-				resTags = make(map[string]interface{}, len(res.Tags))
-				for key, value := range res.Tags {
-					if value != nil {
-						resTags[key] = *value
-					}
-				}
-			}
-
-			resources = append(resources, map[string]interface{}{
-				"name":     resName,
-				"id":       resID,
-				"type":     resType,
-				"location": resLocation,
-				"tags":     resTags,
-			})
+			resources = append(resources, *transformResource(res))
 		} else {
 			log.Printf("[DEBUG] azurerm_resources - resources %q (id: %q) skipped as a required tag is not set or has the wrong value.", *res.Name, *res.ID)
 		}
 
 		err = resourcesResp.NextWithContext(ctx)
 		if err != nil {
-			return fmt.Errorf("Error loading Resource List: %s", err)
+			return nil, fmt.Errorf("Error loading Resource List: %s", err)
 		}
 	}
 
-	d.SetId("resource-" + uuid.New().String())
-	if err := d.Set("resources", resources); err != nil {
-		return fmt.Errorf("Error setting `resources`: %+v", err)
+	return resources, nil
+}
+
+func transformResource(res resources.GenericResource) *map[string]interface{} {
+	resName := ""
+	if res.Name != nil {
+		resName = *res.Name
 	}
 
-	return nil
+	resID := ""
+	if res.ID != nil {
+		resID = *res.ID
+	}
+
+	resType := ""
+	if res.Type != nil {
+		resType = *res.Type
+	}
+
+	resLocation := ""
+	if res.Location != nil {
+		resLocation = *res.Location
+	}
+
+	resTags := make(map[string]interface{})
+	if res.Tags != nil {
+		resTags = make(map[string]interface{}, len(res.Tags))
+		for key, value := range res.Tags {
+			if value != nil {
+				resTags[key] = *value
+			}
+		}
+	}
+
+	return &map[string]interface{}{
+		"name":           resName,
+		"id":             resID,
+		"type":           resType,
+		"location":       resLocation,
+		"tags":           resTags,
+	}
 }
