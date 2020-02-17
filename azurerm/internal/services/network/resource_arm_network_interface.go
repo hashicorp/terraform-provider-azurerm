7fe6c22b771cba2e08c58bd344a17aa17fb7d922
tombuildsstuff <git@ibuildstuff.co.uk>
2020-02-17 09:12:24 +0100

    r/network_interface: making the field computed temporarily (until it's removed in 2.0)

--- azurerm/internal/services/network/resource_arm_network_interface.go
+++ azurerm/internal/services/network/resource_arm_network_interface.go
@@ -56,6 +56,7 @@ func resourceArmNetworkInterface() *schema.Resource {
 			"network_security_group_id": {
 				Type:         schema.TypeString,
 				Optional:     true,
+				Computed:     true,
 				ValidateFunc: azure.ValidateResourceIDOrEmpty,
 			},
 
