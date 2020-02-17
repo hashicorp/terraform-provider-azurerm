8ed01245730d2501ccd08e32c4b9dfbac4197a31
tombuildsstuff <git@ibuildstuff.co.uk>
2020-02-17 17:28:13 +0100

    r/keyvault: removing dead code

--- azurerm/internal/services/keyvault/tests/resource_arm_key_vault_test.go
+++ azurerm/internal/services/keyvault/tests/resource_arm_key_vault_test.go
@@ -411,37 +411,6 @@ resource "azurerm_key_vault" "test" {
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
 }
 
-func testAccAzureRMKeyVault_basicNotDefined(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_resource_group" "test" {
-  name     = "acctestRG-%d"
-  location = "%s"
-}
-
-resource "azurerm_key_vault" "test" {
-  name                = "vault%d"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  tenant_id           = "${data.azurerm_client_config.current.tenant_id}"
-
-  access_policy {
-    tenant_id = "${data.azurerm_client_config.current.tenant_id}"
-    object_id = "${data.azurerm_client_config.current.client_id}"
-
-    key_permissions = [
-      "create",
-    ]
-
-    secret_permissions = [
-      "set",
-    ]
-  }
-}
-`, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
-}
-
 func testAccAzureRMKeyVault_requiresImport(data acceptance.TestData) string {
 	template := testAccAzureRMKeyVault_basic(data)
 	return fmt.Sprintf(`
