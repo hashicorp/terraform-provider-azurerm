a661838752f2bfbb9fba9d08d1e8aa5fdf7bf152
tombuildsstuff <git@ibuildstuff.co.uk>
2020-02-19 15:56:09 +0100

    d/app_service_environment: renaming the file to match the convention

--- azurerm/internal/services/web/tests/app_service_environment_data_source_test.go
+++ azurerm/internal/services/web/tests/app_service_environment_data_source_test.go
@@ -16,7 +16,7 @@ func TestAccDataSourceAzureRMAppServiceEnvironment_basic(t *testing.T) {
 		Providers: acceptance.SupportedProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDatasourceAppServiceEnvironment_basic(data),
+				Config: testAccDataSourceAppServiceEnvironment_basic(data),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet(data.ResourceName, "front_end_scale_factor"),
 					resource.TestCheckResourceAttrSet(data.ResourceName, "pricing_tier"),
@@ -26,14 +26,14 @@ func TestAccDataSourceAzureRMAppServiceEnvironment_basic(t *testing.T) {
 	})
 }
 
-func testAccDatasourceAppServiceEnvironment_basic(data acceptance.TestData) string {
+func testAccDataSourceAppServiceEnvironment_basic(data acceptance.TestData) string {
 	config := testAccAzureRMAppServiceEnvironment_basic(data)
 	return fmt.Sprintf(`
 %s
 
 data "azurerm_app_service_environment" "test" {
-  name                = "${azurerm_app_service_environment.test.name}"
-  resource_group_name = "${azurerm_app_service_environment.test.resource_group_name}"
+  name                = azurerm_app_service_environment.test.name
+  resource_group_name = azurerm_app_service_environment.test.resource_group_name
 }
 `, config)
 }
