11efc1b53ddb3c38ab486dabb56bd4bf35d4b0d7
neil-yechenwei <yechenwei2007@126.com>
2020-03-05 09:05:05 +0800

    Update code

--- azurerm/internal/services/powerbi/tests/resource_arm_powerbi_embedded_test.go
+++ azurerm/internal/services/powerbi/tests/resource_arm_powerbi_embedded_test.go
@@ -186,6 +186,8 @@ resource "azurerm_powerbi_embedded" "import" {
   name                = "${azurerm_powerbi_embedded.test.name}"
   location            = "${azurerm_powerbi_embedded.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
+  sku_name            = "A1"
+  administrators      = ["${data.azurerm_client_config.test.object_id}"]
 }
 `, testAccAzureRMPowerBIEmbedded_basic(data))
 }
