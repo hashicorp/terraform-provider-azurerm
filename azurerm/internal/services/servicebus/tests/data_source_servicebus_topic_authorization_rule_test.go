6fcc166cf08c60b21592a5633e240cf1b14840db
Tracy Holmes <tracypholmes@gmail.com>
2020-03-05 23:50:07 -0600

    Adds new datasource Fixes #5988

---
+++ azurerm/internal/services/servicebus/tests/data_source_servicebus_topic_authorization_rule_test.go
@@ -0,0 +1,45 @@
+package tests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
+)
+
+func TestAccDataSourceAzureRMServiceBusTopicAuthorizationRule_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "data.azurerm_servicebus_topic_authorization_rule", "test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acceptance.PreCheck(t) },
+		Providers:    acceptance.SupportedProviders,
+		CheckDestroy: testCheckAzureRMServiceBusTopicAuthorizationRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceAzureRMServiceBusTopicAuthorizationRule_basic(data),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMServiceBusTopicAuthorizationRuleExists(data.ResourceName),
+					resource.TestCheckResourceAttrSet(data.ResourceName, "primary_key"),
+					resource.TestCheckResourceAttrSet(data.ResourceName, "secondary_key"),
+					resource.TestCheckResourceAttrSet(data.ResourceName, "primary_connection_string"),
+					resource.TestCheckResourceAttrSet(data.ResourceName, "secondary_connection_string"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceAzureRMServiceBusTopicAuthorizationRule_basic(data acceptance.TestData) string {
+	template := testAccAzureRMServiceBusTopicAuthorizationRule_base(data, true, true, true)
+	return fmt.Sprintf(`
+%s
+
+data "azurerm_servicebus_topic_authorization_rule" "test" {
+  name                = "${azurerm_servicebus_topic_authorization_rule.test.name}"
+	namespace_name      = "${azurerm_servicebus_namespace.test.name}"
+	topic_name          = "${azurerm_servicebus_topic.test.name}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+`, template)
+}
