f5c7d1fe32cd0ec5e182744a284be5545603a22a
rob <r0bnet@users.noreply.github.com>
2020-03-08 21:10:25 +0100

    add method to test cache expiration duration

--- azurerm/helpers/validate/cdn.go
+++ azurerm/helpers/validate/cdn.go
@@ -54,3 +54,10 @@ func RuleActionUrlRedirectFragment() schema.SchemaValidateFunc {
 		"The Url Redirect Path must start with a slash.",
 	)
 }
+
+func RuleActionCacheExpirationDuration() schema.SchemaValidateFunc {
+	return validation.StringMatch(
+		regexp.MustCompile("^(\\d+\\.)?([0-1][0-9]|[2][0-3]):[0-5][0-9]:[0-5][0-9]$"),
+		"The Cache duration must be in this format [d.]hh:mm:ss.",
+	)
+}
