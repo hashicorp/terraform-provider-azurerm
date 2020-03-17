161d73f49e18ea3e26cfbad158b194d436b32d5e
Tracy Holmes <tracypholmes@gmail.com>
2020-03-17 09:59:52 -0500

    removes comments, adds HasSuffiix

--- azurerm/internal/services/servicebus/validate/servicebus_namespace.go
+++ azurerm/internal/services/servicebus/validate/servicebus_namespace.go
@@ -3,41 +3,23 @@ package validate
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 // validation
 func ServiceBusNamespaceName(v interface{}, k string) (warnings []string, errors []error) {
 	value := v.(string)
 
-	if regexp.MustCompile("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}").MatchString(value) {
+	if regexp.MustCompile("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$").MatchString(value) {
 		return
 	}
 
 	if !regexp.MustCompile("^[a-zA-Z][-a-zA-Z0-9]{4,48}[a-zA-Z0-9]$").MatchString(value) {
 		errors = append(errors, fmt.Errorf("%s must contain only letters, numbers, and hyphens. The namespace must start with a letter, and it must end with a letter or number and be between 6 and 50 characters long or be a GUID/UUID", k))
 	}
-
-	// Ask KT about this vs breaking them out separately
-	// 	if strings.HasSuffix(v, "-") || strings.HasSuffix(v, "-sb") || strings.HasSuffix(v, "-mgmt") {
-	// 	errors = append(errors, fmt.Errorf("%q cannot end with a hyphen, -sb, or -mgmt", k, value))
-	// }
-
-	// Cannot end in a hyphen
-	if regexp.MustCompile(`-$`).MatchString(value) {
-		errors = append(errors, fmt.Errorf(
-			"%q cannot end with a hyphen: %q", k, value))
-	}
-
-	// Cannot end in -sb
-	if regexp.MustCompile(`-sb$`).MatchString(value) {
-		errors = append(errors, fmt.Errorf(
-			"%q cannot end with -sb: %q", k, value))
-	}
-
-	// Cannot end in -mgmt
-	if regexp.MustCompile(`-mgmt$`).MatchString(value) {
-		errors = append(errors, fmt.Errorf(
-			"%q cannot end with -mgmt: %q", k, value))
+	// The name cannot end with “-“, “-sb“ or “-mgmt“
+	if strings.HasSuffix(value, "-") || strings.HasSuffix(value, "-sb") || strings.HasSuffix(value, "-mgmt") {
+		errors = append(errors, fmt.Errorf("%q cannot end with a hyphen, -sb, or -mgmt", k))
 	}
 
 	return warnings, errors
