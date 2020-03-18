57179a254027dc28576bd6b744873ffe5481f01a
Aris van Ommeren <avommeren@vxcompany.com>
2020-03-18 07:54:18 +0100

    Add new test to aks combined tests

--- azurerm/internal/services/containers/tests/resource_arm_kubernetes_cluster_test.go
+++ azurerm/internal/services/containers/tests/resource_arm_kubernetes_cluster_test.go
@@ -49,6 +49,7 @@ func TestAccAzureRMKubernetes_all(t *testing.T) {
 			"enableNodePublicIP":                          testAccAzureRMKubernetesCluster_enableNodePublicIP,
 			"internalNetwork":                             testAccAzureRMKubernetesCluster_internalNetwork,
 			"basicLoadBalancerProfile":                    testAccAzureRMKubernetesCluster_basicLoadBalancerProfile,
+			"changingLoadBalancerProfile":                 testAccAzureRMKubernetesCluster_changingLoadBalancerProfile,
 			"conflictingLoadBalancerProfile":              testAccAzureRMKubernetesCluster_conflictingLoadBalancerProfile,
 			"prefixedLoadBalancerProfile":                 testAccAzureRMKubernetesCluster_prefixedLoadBalancerProfile,
 			"standardLoadBalancer":                        testAccAzureRMKubernetesCluster_standardLoadBalancer,
