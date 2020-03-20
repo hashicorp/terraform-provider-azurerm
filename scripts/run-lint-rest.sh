de22d48bdc89d7b96d6357983f5f4fc49a77fadb
magodo <wztdyl@sina.com>
2020-03-21 05:58:23 +0800

    fix typo in run-lint-rest.sh (#6197)

--- scripts/run-lint-rest.sh
+++ scripts/run-lint-rest.sh
@@ -15,8 +15,8 @@ function checkForConditionalRun {
 
 function runLinters {
   echo "==> Checking source code against linters..."
-  (while true; do sleep 300; echo "(I'm still alive and linting!)"; done) & PID=$$!; echo $$PID; \
-  golangci-lint run ./... -v --concurrency 1 --config .golangci-travis.yml ; ES=$$?; kill -9 $$PID; exit $$ES
+  (while true; do sleep 300; echo "(I'm still alive and linting!)"; done) & PID="$!"; echo "Watcher subprocess: $PID"; \
+  golangci-lint run ./... -v --concurrency 1 --config .golangci-travis.yml ; ES="$?"; kill -9 "$PID"; exit "$ES"
 }
 
 function main {
