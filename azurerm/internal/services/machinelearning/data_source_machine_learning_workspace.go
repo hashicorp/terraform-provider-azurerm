132a84f987cddfee0e876289a2bb751e6e4e7aee
Dapeng Zhang <dapzhang@microsoft.com>
2020-03-24 11:29:14 +0800

    Goimports

--- azurerm/internal/services/machinelearning/data_source_machine_learning_workspace.go
+++ azurerm/internal/services/machinelearning/data_source_machine_learning_workspace.go
@@ -2,12 +2,12 @@ package machinelearning
 
 import (
 	"fmt"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/machinelearning/validate"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/machinelearning/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
