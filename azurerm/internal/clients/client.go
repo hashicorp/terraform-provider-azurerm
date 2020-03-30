ac1ef4236e4bf113e27442198ca3cc8abc4d62c7
njucz <740360112@qq.com>
2020-03-30 10:40:53 +0800

    fix conflict

--- azurerm/internal/clients/client.go
+++ azurerm/internal/clients/client.go
@@ -83,11 +83,11 @@ type Client struct {
 	Account  *ResourceManagerAccount
 	Features features.UserFeatures
 
-<<<<<<< HEAD
 	AnalysisServices   *analysisServices.Client
 	ApiManagement      *apiManagement.Client
 	AppConfiguration   *appConfiguration.Client
 	AppInsights        *applicationInsights.Client
+	AppPlatform        *appPlatform.Client
 	Authorization      *authorization.Client
 	Automation         *automation.Client
 	Batch              *batch.Client
@@ -151,74 +151,6 @@ type Client struct {
 	Sql                *sql.Client
 	TrafficManager     *trafficManager.Client
 	Web                *web.Client
-=======
-	AnalysisServices  *analysisServices.Client
-	ApiManagement     *apiManagement.Client
-	AppConfiguration  *appConfiguration.Client
-	AppInsights       *applicationInsights.Client
-	AppPlatform       *appPlatform.Client
-	Authorization     *authorization.Client
-	Automation        *automation.Client
-	Batch             *batch.Client
-	Bot               *bot.Client
-	Cdn               *cdn.Client
-	Cognitive         *cognitiveServices.Client
-	Compute           *compute.Client
-	Containers        *containerServices.Client
-	Cosmos            *cosmosdb.Client
-	CostManagement    *costmanagement.Client
-	DatabaseMigration *datamigration.Client
-	DataBricks        *databricks.Client
-	DataFactory       *datafactory.Client
-	Datalake          *datalake.Client
-	DevSpace          *devspace.Client
-	DevTestLabs       *devtestlabs.Client
-	Dns               *dns.Client
-	EventGrid         *eventgrid.Client
-	Eventhub          *eventhub.Client
-	Frontdoor         *frontdoor.Client
-	HDInsight         *hdinsight.Client
-	HealthCare        *healthcare.Client
-	IoTHub            *iothub.Client
-	IoTCentral        *iotcentral.Client
-	KeyVault          *keyvault.Client
-	Kusto             *kusto.Client
-	LogAnalytics      *loganalytics.Client
-	Logic             *logic.Client
-	MachineLearning   *machinelearning.Client
-	ManagementGroups  *managementgroup.Client
-	Maps              *maps.Client
-	MariaDB           *mariadb.Client
-	Media             *media.Client
-	MixedReality      *mixedreality.Client
-	Monitor           *monitor.Client
-	MSI               *msi.Client
-	MSSQL             *mssql.Client
-	MySQL             *mysql.Client
-	NetApp            *netapp.Client
-	Network           *network.Client
-	NotificationHubs  *notificationhub.Client
-	Policy            *policy.Client
-	Portal            *portal.Client
-	Postgres          *postgres.Client
-	PowerBI           *powerBI.Client
-	PrivateDns        *privatedns.Client
-	RecoveryServices  *recoveryServices.Client
-	Redis             *redis.Client
-	Relay             *relay.Client
-	Resource          *resource.Client
-	Search            *search.Client
-	SecurityCenter    *securityCenter.Client
-	ServiceBus        *serviceBus.Client
-	ServiceFabric     *serviceFabric.Client
-	SignalR           *signalr.Client
-	Storage           *storage.Client
-	StreamAnalytics   *streamAnalytics.Client
-	Subscription      *subscription.Client
-	Sql               *sql.Client
-	TrafficManager    *trafficManager.Client
-	Web               *web.Client
->>>>>>> new resource "azurerm_spring_cloud_service"
 }
 
 // NOTE: it should be possible for this method to become Private once the top level Client's removed
