37ecad721ef9b774a0b3f8140d240230199d55b4
Tom Bamford <tom@bamford.io>
2020-03-31 17:37:44 +0100

    Fix errant indent

--- azurerm/internal/services/privatedns/tests/resource_arm_private_dns_txt_record_test.go
+++ azurerm/internal/services/privatedns/tests/resource_arm_private_dns_txt_record_test.go
@@ -317,7 +317,7 @@ resource "azurerm_private_dns_txt_record" "test" {
   resource_group_name = azurerm_resource_group.test.name
   zone_name           = azurerm_private_dns_zone.test.name
   ttl                 = 300
-  
+
   record {
     value = "Quick brown fox"
   }
