0488b7a3a097f4ea6dbd884e1395d88ede26e0c9
Arcturus <dapzhang@microsoft.com>
2020-04-10 13:23:25 +0800

    New data source: `azurerm_policy_set_definition` (#6305)
    This PR adds data source azurerm_policy_set_definition, and also resolves some minor issues in the policy service.
    
    Fixes #6251

--- azurerm/internal/services/policy/parse/definition.go
+++ azurerm/internal/services/policy/parse/definition.go
@@ -10,10 +10,10 @@ type PolicyDefinitionId struct {
 	PolicyScopeId
 }
 
-// TODO: This paring function is currently suppressing every case difference due to github issue: https://github.com/Azure/azure-rest-api-specs/issues/8353
+// TODO: This parsing function is currently suppressing every case difference due to github issue: https://github.com/Azure/azure-rest-api-specs/issues/8353
 func PolicyDefinitionID(input string) (*PolicyDefinitionId, error) {
 	// in general, the id of a definition should be:
-	// {scope}/providers/Microsoft.PolicyInsights/remediations/{name}
+	// {scope}/providers/Microsoft.Authorization/policyDefinitions/{name}
 	regex := regexp.MustCompile(`/providers/[Mm]icrosoft\.[Aa]uthorization/policy[Dd]efinitions/`)
 	if !regex.MatchString(input) {
 		return nil, fmt.Errorf("unable to parse Policy Definition ID %q", input)
@@ -28,7 +28,7 @@ func PolicyDefinitionID(input string) (*PolicyDefinitionId, error) {
 	scope := segments[0]
 	name := segments[1]
 	if name == "" {
-		return nil, fmt.Errorf("unable to parse Policy Definition ID %q: assignment name is empty", input)
+		return nil, fmt.Errorf("unable to parse Policy Definition ID %q: definition name is empty", input)
 	}
 
 	scopeId, err := PolicyScopeID(scope)
