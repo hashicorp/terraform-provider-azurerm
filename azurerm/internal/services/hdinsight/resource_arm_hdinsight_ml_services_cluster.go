cb8fa93ee09e3022814d327c7588f3a0dd903596
kosinsky <kosinsky@github.com>
2020-04-12 17:03:37 -0700

    azurerm_hdinsight_* - support for `tls_min_version ` (#6440)
    Fixes: #6372

--- azurerm/internal/services/hdinsight/resource_arm_hdinsight_ml_services_cluster.go
+++ azurerm/internal/services/hdinsight/resource_arm_hdinsight_ml_services_cluster.go
@@ -80,6 +80,8 @@ func resourceArmHDInsightMLServicesCluster() *schema.Resource {
 
 			"tier": azure.SchemaHDInsightTier(),
 
+			"tls_min_version": azure.SchemaHDInsightTls(),
+
 			"gateway": azure.SchemaHDInsightsGateway(),
 
 			"rstudio": {
@@ -148,6 +150,7 @@ func resourceArmHDInsightMLServicesClusterCreate(d *schema.ResourceData, meta in
 	clusterVersion := d.Get("cluster_version").(string)
 	t := d.Get("tags").(map[string]interface{})
 	tier := hdinsight.Tier(d.Get("tier").(string))
+	tls := d.Get("tls_min_version").(string)
 
 	gatewayRaw := d.Get("gateway").([]interface{})
 	rStudio := d.Get("rstudio").(bool)
@@ -187,9 +190,10 @@ func resourceArmHDInsightMLServicesClusterCreate(d *schema.ResourceData, meta in
 	params := hdinsight.ClusterCreateParametersExtended{
 		Location: utils.String(location),
 		Properties: &hdinsight.ClusterCreateProperties{
-			Tier:           tier,
-			OsType:         hdinsight.Linux,
-			ClusterVersion: utils.String(clusterVersion),
+			Tier:                   tier,
+			OsType:                 hdinsight.Linux,
+			ClusterVersion:         utils.String(clusterVersion),
+			MinSupportedTLSVersion: utils.String(tls),
 			ClusterDefinition: &hdinsight.ClusterDefinition{
 				Kind:           utils.String("MLServices"),
 				Configurations: gateway,
@@ -272,6 +276,7 @@ func resourceArmHDInsightMLServicesClusterRead(d *schema.ResourceData, meta inte
 	if props := resp.Properties; props != nil {
 		d.Set("cluster_version", props.ClusterVersion)
 		d.Set("tier", string(props.Tier))
+		d.Set("tls_min_version", props.MinSupportedTLSVersion)
 
 		if def := props.ClusterDefinition; def != nil {
 			if err := d.Set("gateway", azure.FlattenHDInsightsConfigurations(configuration.Value)); err != nil {
--- azurerm/internal/services/hdinsight/tests/resource_arm_hdinsight_rserver_cluster_test.go
+++ azurerm/internal/services/hdinsight/tests/resource_arm_hdinsight_rserver_cluster_test.go
@@ -198,6 +198,35 @@ func TestAccAzureRMHDInsightRServerCluster_complete(t *testing.T) {
 	})
 }
 
+func TestAccAzureRMHDInsightRServerCluster_tls(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_hdinsight_rserver_cluster", "test")
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acceptance.PreCheck(t) },
+		Providers:    acceptance.SupportedProviders,
+		CheckDestroy: testCheckAzureRMHDInsightClusterDestroy(data.ResourceType),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMHDInsightRServerCluster_tls(data),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMHDInsightClusterExists(data.ResourceName),
+					resource.TestCheckResourceAttrSet(data.ResourceName, "edge_ssh_endpoint"),
+					resource.TestCheckResourceAttrSet(data.ResourceName, "https_endpoint"),
+					resource.TestCheckResourceAttrSet(data.ResourceName, "ssh_endpoint"),
+				),
+			},
+			data.ImportStep("roles.0.head_node.0.password",
+				"roles.0.head_node.0.vm_size",
+				"roles.0.worker_node.0.password",
+				"roles.0.worker_node.0.vm_size",
+				"roles.0.zookeeper_node.0.password",
+				"roles.0.zookeeper_node.0.vm_size",
+				"roles.0.edge_node.0.password",
+				"roles.0.edge_node.0.vm_size",
+				"storage_account"),
+		},
+	})
+}
+
 func testAccAzureRMHDInsightRServerCluster_basic(data acceptance.TestData) string {
 	template := testAccAzureRMHDInsightRServerCluster_template(data)
 	return fmt.Sprintf(`
@@ -639,4 +668,60 @@ resource "azurerm_storage_container" "test" {
   container_access_type = "private"
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomString)
+}
+
+func testAccAzureRMHDInsightRServerCluster_tls(data acceptance.TestData) string {
+	template := testAccAzureRMHDInsightRServerCluster_template(data)
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_hdinsight_rserver_cluster" "test" {
+  name                = "acctesthdi-%d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  cluster_version     = "3.6"
+  tier                = "Standard"
+  tls_min_version     = "1.2"
+  rstudio             = true
+
+  gateway {
+    enabled  = true
+    username = "acctestusrgw"
+    password = "TerrAform123!"
+  }
+
+  storage_account {
+    storage_container_id = azurerm_storage_container.test.id
+    storage_account_key  = azurerm_storage_account.test.primary_access_key
+    is_default           = true
+  }
+
+  roles {
+    head_node {
+      vm_size  = "Standard_D3_V2"
+      username = "acctestusrvm"
+      password = "AccTestvdSC4daf986!"
+    }
+
+    worker_node {
+      vm_size               = "Standard_D3_V2"
+      username              = "acctestusrvm"
+      password              = "AccTestvdSC4daf986!"
+      target_instance_count = 2
+    }
+
+    zookeeper_node {
+      vm_size  = "Standard_D3_V2"
+      username = "acctestusrvm"
+      password = "AccTestvdSC4daf986!"
+    }
+
+    edge_node {
+      vm_size  = "Standard_D3_V2"
+      username = "acctestusrvm"
+      password = "AccTestvdSC4daf986!"
+    }
+  }
+}
+`, template, data.RandomInteger)
 }
