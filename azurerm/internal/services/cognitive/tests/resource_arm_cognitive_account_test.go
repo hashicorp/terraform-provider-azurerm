e33a4e76178b4c96d0f4c35afca69c816f532f4b
Tracy Holmes <tracypholmes@gmail.com>
2020-04-20 16:18:10 -0500

    updates `cognitive`

--- azurerm/internal/services/cognitive/tests/resource_arm_cognitive_account_test.go
+++ azurerm/internal/services/cognitive/tests/resource_arm_cognitive_account_test.go
@@ -61,10 +61,7 @@ func TestAccAzureRMCognitiveAccount_speechServices(t *testing.T) {
 }
 
 func TestAccAzureRMCognitiveAccount_requiresImport(t *testing.T) {
-	if !features.ShouldResourcesBeImported() {
-		t.Skip("Skipping since resources aren't required to be imported")
-		return
-	}
+	
 
 	data := acceptance.BuildTestData(t, "azurerm_cognitive_account", "test")
 
