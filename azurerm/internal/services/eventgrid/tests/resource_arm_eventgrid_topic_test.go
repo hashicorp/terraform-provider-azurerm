be12ce3730747949926cc1fd998656b396e9b9b1
Tracy Holmes <tracypholmes@gmail.com>
2020-04-20 16:59:47 -0500

    updates `eventgrid`

--- azurerm/internal/services/eventgrid/tests/resource_arm_eventgrid_topic_test.go
+++ azurerm/internal/services/eventgrid/tests/resource_arm_eventgrid_topic_test.go
@@ -36,11 +36,7 @@ func TestAccAzureRMEventGridTopic_basic(t *testing.T) {
 }
 
 func TestAccAzureRMEventGridTopic_requiresImport(t *testing.T) {
-	if !features.ShouldResourcesBeImported() {
-		t.Skip("Skipping since resources aren't required to be imported")
-		return
-	}
-
+	
 	data := acceptance.BuildTestData(t, "azurerm_eventgrid_topic", "test")
 
 	resource.ParallelTest(t, resource.TestCase{
