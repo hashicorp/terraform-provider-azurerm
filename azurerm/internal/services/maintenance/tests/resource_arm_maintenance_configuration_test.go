c915308784840bbbef704b34a58735bddb6c190a
Tracy Holmes <tracypholmes@gmail.com>
2020-04-21 16:09:06 -0500

    Updates `maintenance`

--- azurerm/internal/services/maintenance/tests/resource_arm_maintenance_configuration_test.go
+++ azurerm/internal/services/maintenance/tests/resource_arm_maintenance_configuration_test.go
@@ -35,10 +35,6 @@ func TestAccAzureRMMaintenanceConfiguration_basic(t *testing.T) {
 }
 
 func TestAccAzureRMMaintenanceConfiguration_requiresImport(t *testing.T) {
-	if !features.ShouldResourcesBeImported() {
-		t.Skip("Skipping since resources aren't required to be imported")
-		return
-	}
 	data := acceptance.BuildTestData(t, "azurerm_maintenance_configuration", "test")
 
 	resource.ParallelTest(t, resource.TestCase{
