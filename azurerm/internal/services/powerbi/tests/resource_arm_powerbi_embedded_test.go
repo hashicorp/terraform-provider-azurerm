785ec27535d5ef1462712e88a8a7e9cbed3521e0
Tracy Holmes <tracypholmes@gmail.com>
2020-04-21 16:24:53 -0500

    Updates `powerbi`

--- azurerm/internal/services/powerbi/tests/resource_arm_powerbi_embedded_test.go
+++ azurerm/internal/services/powerbi/tests/resource_arm_powerbi_embedded_test.go
@@ -32,11 +32,6 @@ func TestAccAzureRMPowerBIEmbedded_basic(t *testing.T) {
 }
 
 func TestAccAzureRMPowerBIEmbedded_requiresImport(t *testing.T) {
-	if !features.ShouldResourcesBeImported() {
-		t.Skip("Skipping since resources aren't required to be imported")
-		return
-	}
-
 	data := acceptance.BuildTestData(t, "azurerm_powerbi_embedded", "test")
 
 	resource.ParallelTest(t, resource.TestCase{
