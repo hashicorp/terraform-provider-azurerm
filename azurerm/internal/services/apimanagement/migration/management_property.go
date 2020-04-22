8948b02a602dbf5a8dddcfc71d41cd29c5280c15
kt <kt@katbyte.me>
2020-04-22 00:22:58 -0700

    add migration function

--- azurerm/internal/services/apimanagement/migration/management_property.go
+++ azurerm/internal/services/apimanagement/migration/management_property.go
@@ -49,7 +49,7 @@ func APIManagementApiPropertyUpgradeV0Schema() *schema.Resource {
 
 func APIManagementApiPropertyUpgradeV0ToV1(rawState map[string]interface{}, meta interface{}) (map[string]interface{}, error) {
 	oldId := rawState["id"].(string)
-	newId := strings.Replace(rawState["id"].(string), "/properties/", "gremlinDatabases", 1)
+	newId := strings.Replace(rawState["id"].(string), "/properties/", "/namedValues/", 1)
 
 	log.Printf("[DEBUG] Updating ID from %q to %q", oldId, newId)
 
