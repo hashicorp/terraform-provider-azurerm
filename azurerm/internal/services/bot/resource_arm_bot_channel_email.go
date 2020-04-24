e96c38de39060f2b55ef4ef8252688a1a1cbd23e
Tracy Holmes <tracypholmes@gmail.com>
2020-04-24 12:31:43 -0500

    Updates `bot`

--- azurerm/internal/services/bot/resource_arm_bot_channel_email.go
+++ azurerm/internal/services/bot/resource_arm_bot_channel_email.go
@@ -12,7 +12,6 @@ import (
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/features"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
@@ -71,7 +70,7 @@ func resourceArmBotChannelEmailCreate(d *schema.ResourceData, meta interface{}) 
 	resourceGroup := d.Get("resource_group_name").(string)
 	botName := d.Get("bot_name").(string)
 
-	if features.ShouldResourcesBeImported() && d.IsNewResource() {
+	if d.IsNewResource() {
 		existing, err := client.Get(ctx, resourceGroup, string(botservice.ChannelNameEmailChannel), botName)
 		if err != nil {
 			if !utils.ResponseWasNotFound(existing.Response) {
