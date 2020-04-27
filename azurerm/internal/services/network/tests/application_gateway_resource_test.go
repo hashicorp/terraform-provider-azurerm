c3a654ccde9fc4d9530faf993b4386522cdb102e
Sune Keller <absukl@almbrand.dk>
2020-04-27 08:56:24 +0200

    Refer correctly to policy_settings in test
    Signed-off-by: Sune Keller <absukl@almbrand.dk>

--- azurerm/internal/services/network/tests/application_gateway_resource_test.go
+++ azurerm/internal/services/network/tests/application_gateway_resource_test.go
@@ -2027,7 +2027,7 @@ resource "azurerm_web_application_firewall_policy" "testfwp" {
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
 
-  policy_setting {
+  policy_settings {
     enabled = true
     mode    = "Prevention"
   }
