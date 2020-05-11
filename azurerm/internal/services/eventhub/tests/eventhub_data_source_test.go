48b534b671e6e60810d01a2b6986b1483fe88aff
Rob Selway <rob.selway@gmail.com>
2020-05-11 08:23:08 +0100

    Fixed tflint errors

--- azurerm/internal/services/eventhub/tests/eventhub_data_source_test.go
+++ azurerm/internal/services/eventhub/tests/eventhub_data_source_test.go
@@ -46,17 +46,17 @@ resource "azurerm_eventhub_namespace" "test" {
 }
 
 resource "azurerm_eventhub" "test" {
-	name           = "acctest-eh-%[1]d"
-	resource_group_name = azurerm_resource_group.test.name
-	namespace_name = azurerm_eventhub_namespace.test.name
-	partition_count     = 2
-  	message_retention   = 1
+  name                = "acctest-eh-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  namespace_name      = azurerm_eventhub_namespace.test.name
+  partition_count     = 2
+  message_retention   = 1
 }
 
 data "azurerm_eventhub" "test" {
-	name                = azurerm_eventhub.test.name
-	namespace_name      = azurerm_eventhub_namespace.test.name
-	resource_group_name = azurerm_resource_group.test.name
+  name                = azurerm_eventhub.test.name
+  namespace_name      = azurerm_eventhub_namespace.test.name
+  resource_group_name = azurerm_resource_group.test.name
 }
 `, data.RandomInteger, data.Locations.Primary)
 }
