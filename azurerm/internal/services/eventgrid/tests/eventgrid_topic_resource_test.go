04f79d8318d9c05a22bc3d68e2e0f69f9c08434d
Jochen Rauschenbusch <jochen.rauschenbusch@gmail.com>
2020-05-12 07:16:24 +0200

    fix: terrafmt

--- azurerm/internal/services/eventgrid/tests/eventgrid_topic_resource_test.go
+++ azurerm/internal/services/eventgrid/tests/eventgrid_topic_resource_test.go
@@ -210,7 +210,7 @@ resource "azurerm_eventgrid_topic" "test" {
   name                = "acctesteg-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  input_schema = "CustomEventSchema"
+  input_schema        = "CustomEventSchema"
   input_mapping_fields {
     topic      = "test"
     event_type = "test"
