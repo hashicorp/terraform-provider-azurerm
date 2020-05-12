a99bfcb0e8131a7df8421a76b25fa457ffdaf5a3
magodo <wztdyl@sina.com>
2020-05-13 04:43:02 +0800

    `azurerm_log_analytics_workspace` - Support `rentention_in_days` for Free Tier (#6844)

--- azurerm/internal/services/loganalytics/tests/log_analytics_workspace_resource_test.go
+++ azurerm/internal/services/loganalytics/tests/log_analytics_workspace_resource_test.go
@@ -115,6 +115,25 @@ func TestAccAzureRMLogAnalyticsWorkspace_complete(t *testing.T) {
 	})
 }
 
+func TestAccAzureRMLogAnalyticsWorkspace_freeTier(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_log_analytics_workspace", "test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acceptance.PreCheck(t) },
+		Providers:    acceptance.SupportedProviders,
+		CheckDestroy: testCheckAzureRMLogAnalyticsWorkspaceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMLogAnalyticsWorkspace_freeTier(data),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMLogAnalyticsWorkspaceExists(data.ResourceName),
+				),
+			},
+			data.ImportStep(),
+		},
+	})
+}
+
 func testCheckAzureRMLogAnalyticsWorkspaceDestroy(s *terraform.State) error {
 	conn := acceptance.AzureProvider.Meta().(*clients.Client).LogAnalytics.WorkspacesClient
 	ctx := acceptance.AzureProvider.Meta().(*clients.Client).StopContext
@@ -226,6 +245,27 @@ resource "azurerm_log_analytics_workspace" "test" {
   tags = {
     Environment = "Test"
   }
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
+}
+
+func testAccAzureRMLogAnalyticsWorkspace_freeTier(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_log_analytics_workspace" "test" {
+  name                = "acctestLAW-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "Free"
+  retention_in_days   = 7
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
 }
