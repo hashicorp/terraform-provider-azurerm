e6711482d1d3766003ea4e7f9adb94e181211983
Tom Bamford <tbamford@hashicorp.com>
2020-05-14 04:36:48 +0100

    mariadb_server: New attributes, flatten `storage_profile` block (#6865)

--- azurerm/internal/services/mariadb/mariadb_configuration_resource.go
+++ azurerm/internal/services/mariadb/mariadb_configuration_resource.go
@@ -3,7 +3,6 @@ package mariadb
 import (
 	"fmt"
 	"log"
-	"regexp"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/mariadb/mgmt/2018-06-01/mariadb"
@@ -11,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/mariadb/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
@@ -43,13 +43,10 @@ func resourceArmMariaDbConfiguration() *schema.Resource {
 			"resource_group_name": azure.SchemaResourceGroupName(),
 
 			"server_name": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-				ValidateFunc: validation.StringMatch(
-					regexp.MustCompile("^[-a-zA-Z0-9]{3,50}$"),
-					"server_name must be 3 - 50 characters long, and contain only letters, numbers and hyphens",
-				),
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validate.MariaDbServerServerName,
 			},
 
 			"value": {
--- azurerm/internal/services/mariadb/tests/mariadb_configuration_resource_test.go
+++ azurerm/internal/services/mariadb/tests/mariadb_configuration_resource_test.go
@@ -218,19 +218,15 @@ resource "azurerm_mariadb_server" "test" {
   name                = "acctestmariadbsvr-%d"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-
-  sku = "GP_Gen5_2"
-
-  storage_profile {
-    storage_mb            = 51200
-    backup_retention_days = 7
-    geo_redundant_backup  = "Disabled"
-  }
+  sku_name            = "GP_Gen5_2"
+  version             = "10.2"
 
   administrator_login          = "acctestun"
   administrator_login_password = "H@Sh1CoR3!"
-  version                      = "10.2"
-  ssl_enforcement              = "Enabled"
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+  ssl_enforcement_enabled      = true
+  storage_mb                   = 51200
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
 }
