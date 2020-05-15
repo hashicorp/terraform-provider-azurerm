524a41b61b0949ed66d67211c7d106f56cb66dac
njucz <740360112@qq.com>
2020-05-15 14:36:04 +0800

    update

--- azurerm/internal/services/automation/tests/automation_connection_service_principal_resource_test.go
+++ azurerm/internal/services/automation/tests/automation_connection_service_principal_resource_test.go
@@ -146,11 +146,11 @@ resource "azurerm_automation_connection_service_principal" "test" {
   name                    = "acctestACSP-%d"
   resource_group_name     = azurerm_resource_group.test.name
   automation_account_name = azurerm_automation_account.test.name
-  application_id         = "00000000-0000-0000-0000-000000000000"
-  tenant_id              = data.azurerm_client_config.test.tenant_id
-  subscription_id        = data.azurerm_client_config.test.subscription_id
-  certificate_thumbprint = file("testdata/automation_certificate_test.thumb")
-  description            = "acceptance test for automation connection"
+  application_id          = "00000000-0000-0000-0000-000000000000"
+  tenant_id               = data.azurerm_client_config.test.tenant_id
+  subscription_id         = data.azurerm_client_config.test.subscription_id
+  certificate_thumbprint  = file("testdata/automation_certificate_test.thumb")
+  description             = "acceptance test for automation connection"
 }
 `, template, data.RandomInteger)
 }
