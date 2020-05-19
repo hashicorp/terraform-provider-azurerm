b1609cde072d4e81d3866f1c017933adb18cc1b4
Konstantin Kosinsky <kosinsky@github.com>
2020-05-19 16:35:44 -0700

    Azure monitor for Spark

--- azurerm/internal/services/hdinsight/hdinsight_spark_cluster_resource.go
+++ azurerm/internal/services/hdinsight/hdinsight_spark_cluster_resource.go
@@ -119,6 +119,8 @@ func resourceArmHDInsightSparkCluster() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
+			"monitor": azure.SchemaHDInsightsMonitor(),
 		},
 	}
 }
@@ -221,6 +223,15 @@ func resourceArmHDInsightSparkClusterCreate(d *schema.ResourceData, meta interfa
 
 	d.SetId(*read.ID)
 
+	// We can only enable monitoring after creation
+	extensionsClient := meta.(*clients.Client).HDInsight.ExtensionsClient
+	if v, ok := d.GetOk("monitor"); ok {
+		monitorRaw := v.([]interface{})
+		if err := enableHDInsightMonitoring(ctx, extensionsClient, resourceGroup, name, monitorRaw); err != nil {
+			return err
+		}
+	}
+
 	return resourceArmHDInsightSparkClusterRead(d, meta)
 }
 
@@ -298,6 +309,15 @@ func resourceArmHDInsightSparkClusterRead(d *schema.ResourceData, meta interface
 		d.Set("https_endpoint", httpEndpoint)
 		sshEndpoint := azure.FindHDInsightConnectivityEndpoint("SSH", props.ConnectivityEndpoints)
 		d.Set("ssh_endpoint", sshEndpoint)
+
+		extensionsClient := meta.(*clients.Client).HDInsight.ExtensionsClient
+
+		monitor, err := extensionsClient.GetMonitoringStatus(ctx, resourceGroup, name)
+		if err != nil {
+			return fmt.Errorf("Error reading monitor configuation for HDInsight Hadoop Cluster %q (Resource Group %q): %+v", name, resourceGroup, err)
+		}
+
+		d.Set("monitor", flattenHDInsightMonitoring(monitor))
 	}
 
 	return tags.FlattenAndSet(d, resp.Tags)
