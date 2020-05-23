496953fc9cd80e635e3f48e09e682446ea4876b0
Dapeng Zhang <dapzhang@microsoft.com>
2020-05-23 11:57:31 +0800

    Fix for linux VMs

--- azurerm/internal/services/compute/tests/linux_virtual_machine_resource_orchestrated_test.go
+++ azurerm/internal/services/compute/tests/linux_virtual_machine_resource_orchestrated_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
 )
 
-func TestAccAzureRMLinuxVirtualMachine_orchestrated(t *testing.T) {
+func TestAccAzureRMLinuxVirtualMachine_orchestratedZonal(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_linux_virtual_machine", "test")
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -17,7 +17,7 @@ func TestAccAzureRMLinuxVirtualMachine_orchestrated(t *testing.T) {
 		CheckDestroy: checkLinuxVirtualMachineIsDestroyed,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAzureRMLinuxVirtualMachine_orchestrated(data),
+				Config: testAccAzureRMLinuxVirtualMachine_orchestratedZonal(data),
 				Check: resource.ComposeTestCheckFunc(
 					checkLinuxVirtualMachineExists(data.ResourceName),
 				),
@@ -26,7 +26,7 @@ func TestAccAzureRMLinuxVirtualMachine_orchestrated(t *testing.T) {
 	})
 }
 
-func TestAccAzureRMLinuxVirtualMachine_orchestratedMultiple(t *testing.T) {
+func TestAccAzureRMLinuxVirtualMachine_orchestratedNonZonal(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_linux_virtual_machine", "test")
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -35,7 +35,7 @@ func TestAccAzureRMLinuxVirtualMachine_orchestratedMultiple(t *testing.T) {
 		CheckDestroy: checkLinuxVirtualMachineIsDestroyed,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAzureRMLinuxVirtualMachine_orchestratedMultiple(data),
+				Config: testAccAzureRMLinuxVirtualMachine_orchestratedNonZonal(data),
 				Check: resource.ComposeTestCheckFunc(
 					checkLinuxVirtualMachineExists(data.ResourceName),
 				),
@@ -44,8 +44,47 @@ func TestAccAzureRMLinuxVirtualMachine_orchestratedMultiple(t *testing.T) {
 	})
 }
 
-func testAccAzureRMLinuxVirtualMachine_orchestrated(data acceptance.TestData) string {
-	template := testLinuxVirtualMachine_templateBaseForOchestratedVMSS(data)
+func TestAccAzureRMLinuxVirtualMachine_orchestratedMultipleZonal(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_linux_virtual_machine", "test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acceptance.PreCheck(t) },
+		Providers:    acceptance.SupportedProviders,
+		CheckDestroy: checkLinuxVirtualMachineIsDestroyed,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMLinuxVirtualMachine_orchestratedMultipleZonal(data),
+				Check: resource.ComposeTestCheckFunc(
+					checkLinuxVirtualMachineExists(data.ResourceName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureRMLinuxVirtualMachine_orchestratedMultipleNonZonal(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_linux_virtual_machine", "test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acceptance.PreCheck(t) },
+		Providers:    acceptance.SupportedProviders,
+		CheckDestroy: checkLinuxVirtualMachineIsDestroyed,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMLinuxVirtualMachine_orchestratedMultipleNonZonal(data),
+				Check: resource.ComposeTestCheckFunc(
+					checkLinuxVirtualMachineExists(data.ResourceName),
+				),
+			},
+		},
+	})
+}
+
+func testAccAzureRMLinuxVirtualMachine_orchestratedZonal(data acceptance.TestData) string {
+	// in VMSS VMO mode, the `platform_fault_domain_count` has different acceptable values for different locations,
+	// therefore this location is fixed to EastUS2 to make sure the acceptance test has no issues about this value
+	location := "EastUS2"
+	template := testLinuxVirtualMachine_templateBaseForOchestratedVMSS(data, location)
 	return fmt.Sprintf(`
 %s
 
@@ -106,8 +145,70 @@ resource "azurerm_linux_virtual_machine" "test" {
 `, template, data.RandomInteger, data.RandomInteger, data.RandomInteger)
 }
 
-func testAccAzureRMLinuxVirtualMachine_orchestratedMultiple(data acceptance.TestData) string {
-	template := testLinuxVirtualMachine_templateBaseForOchestratedVMSS(data)
+func testAccAzureRMLinuxVirtualMachine_orchestratedNonZonal(data acceptance.TestData) string {
+	template := testLinuxVirtualMachine_templateBaseForOchestratedVMSS(data, data.Locations.Primary)
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_network_interface" "test" {
+  name                = "acctestnic-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.test.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
+  name                = "acctestVMO-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  platform_fault_domain_count = 2
+  single_placement_group      = true
+
+  tags = {
+    ENV = "Test"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "test" {
+  name                            = "acctestVM-%d"
+  resource_group_name             = azurerm_resource_group.test.name
+  location                        = azurerm_resource_group.test.location
+  size                            = "Standard_F2"
+  admin_username                  = "adminuser"
+  admin_password                  = "P@ssw0rd1234!"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.test.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  virtual_machine_scale_set_id = azurerm_orchestrated_virtual_machine_scale_set.test.id
+}
+`, template, data.RandomInteger, data.RandomInteger, data.RandomInteger)
+}
+
+func testAccAzureRMLinuxVirtualMachine_orchestratedMultipleZonal(data acceptance.TestData) string {
+	// in VMSS VMO mode, the `platform_fault_domain_count` has different acceptable values for different locations,
+	// therefore this location is fixed to EastUS2 to make sure the acceptance test has no issues about this value
+	location := "EastUS2"
+	template := testLinuxVirtualMachine_templateBaseForOchestratedVMSS(data, location)
 	return fmt.Sprintf(`
 %s
 
@@ -208,10 +309,107 @@ resource "azurerm_linux_virtual_machine" "another" {
 `, template, data.RandomInteger, data.RandomInteger, data.RandomInteger, data.RandomInteger, data.RandomInteger)
 }
 
-func testLinuxVirtualMachine_templateBaseForOchestratedVMSS(data acceptance.TestData) string {
-	// in VMSS VMO mode, the `platform_fault_domain_count` has different acceptable values for different locations,
-	// therefore this location is fixed to EastUS2 to make sure the acceptance test has no issues about this value
-	location := "EastUS2"
+func testAccAzureRMLinuxVirtualMachine_orchestratedMultipleNonZonal(data acceptance.TestData) string {
+	template := testLinuxVirtualMachine_templateBaseForOchestratedVMSS(data, data.Locations.Primary)
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
+  name                = "acctestVMO-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  platform_fault_domain_count = 5
+  single_placement_group      = true
+
+  zones = ["1"]
+
+  tags = {
+    ENV = "Test"
+  }
+}
+
+resource "azurerm_network_interface" "first" {
+  name                = "acctestnic1-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.test.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "test" {
+  name                            = "acctestVM1-%d"
+  resource_group_name             = azurerm_resource_group.test.name
+  location                        = azurerm_resource_group.test.location
+  size                            = "Standard_F2"
+  admin_username                  = "adminuser"
+  admin_password                  = "P@ssw0rd1234!"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.first.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  virtual_machine_scale_set_id = azurerm_orchestrated_virtual_machine_scale_set.test.id
+}
+
+resource "azurerm_network_interface" "second" {
+  name                = "acctestnic2-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.test.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "another" {
+  name                            = "acctestVM2-%d"
+  resource_group_name             = azurerm_resource_group.test.name
+  location                        = azurerm_resource_group.test.location
+  size                            = "Standard_F2"
+  admin_username                  = "adminuser"
+  admin_password                  = "P@ssw0rd1234!"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.second.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  virtual_machine_scale_set_id = azurerm_orchestrated_virtual_machine_scale_set.test.id
+}
+`, template, data.RandomInteger, data.RandomInteger, data.RandomInteger, data.RandomInteger, data.RandomInteger)
+}
+
+func testLinuxVirtualMachine_templateBaseForOchestratedVMSS(data acceptance.TestData, location string) string {
 	return fmt.Sprintf(`
 locals {
   vm_name = "acctestvm%s"
