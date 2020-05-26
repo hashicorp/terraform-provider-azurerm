2771e20e335bad9eba126b50206cb33cdfddb088
v-cheye <v-cheye@microsoft.com>
2020-05-26 13:52:45 +0800

    Update code

--- azurerm/helpers/azure/servicebus_test.go
+++ azurerm/helpers/azure/servicebus_test.go
@@ -6,59 +6,71 @@ import (
 )
 
 func TestValidateServiceBusTopicName(t *testing.T) {
-	testData := []struct {
-		input    string
-		expected bool
+	tests := []struct {
+		name  string
+		input string
+		valid bool
 	}{
 		{
-			input:    "",
-			expected: false,
+			name:  "Empty value",
+			input: "",
+			valid: false,
 		},
 		{
-			input:    "a",
-			expected: true,
+			name:  "Invalid name with only 1 letter",
+			input: "a",
+			valid: true,
 		},
 		{
-			input:    "_a",
-			expected: false,
+			name:  "Invalid name starts with underscore",
+			input: "_a",
+			valid: false,
 		},
 		{
-			input:    "a-",
-			expected: false,
+			name:  "Invalid name ends with period",
+			input: "a.",
+			valid: false,
 		},
 		{
-			input:    "12345",
-			expected: true,
+			name:  "Valid name with numbers",
+			input: "12345",
+			valid: true,
 		},
 		{
-			input:    "1",
-			expected: true,
+			name:  "Valid name with only 1 number",
+			input: "1",
+			valid: true,
 		},
 		{
-			input:    "malcolm-in-the-middle",
-			expected: true,
+			name:  "Valid name with hyphens",
+			input: "malcolm-in-the-middle",
+			valid: true,
 		},
 		{
-			input:    strings.Repeat("w", 259),
-			expected: true,
+			name:  "Valid name with 259 characters",
+			input: strings.Repeat("w", 259),
+			valid: true,
 		},
 		{
-			input:    strings.Repeat("w", 260),
-			expected: true,
+			name:  "Valid name with 260 characters",
+			input: strings.Repeat("w", 260),
+			valid: true,
 		},
 		{
-			input:    strings.Repeat("w", 261),
-			expected: false,
+			name:  "Invalid name with 261 characters",
+			input: strings.Repeat("w", 261),
+			valid: false,
 		},
 	}
 
-	for _, v := range testData {
-		t.Logf("[DEBUG] Testing %q..", v.input)
-
-		_, errors := ValidateServiceBusTopicName(v.input, "name")
-		actual := len(errors) == 0
-		if v.expected != actual {
-			t.Fatalf("Expected %t but got %t", v.expected, actual)
-		}
+	var validationFunction = ValidateServiceBusTopicName()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := validationFunction(tt.input, "name")
+			valid := err == nil
+			if valid != tt.valid {
+				t.Errorf("Expected valid status %t but got %t for input %s", tt.valid, valid, tt.input)
+			}
+		})
 	}
 }
