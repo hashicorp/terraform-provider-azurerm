0950eae46e4917d797f8f4db354bdd56069814ef
github@automatetheneedful.com <github@automatetheneedful.com>
2020-06-03 07:09:28 -0700

    adding orchestrated vmss test

--- azurerm/internal/services/compute/tests/virtual_machine_scale_set_data_source_test.go
+++ azurerm/internal/services/compute/tests/virtual_machine_scale_set_data_source_test.go
@@ -47,6 +47,24 @@ func TestAccDataSourceAzureRMVirtualMachineScaleSet_basicWindows(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceAzureRMVirtualMachineScaleSet_orchestrated(t *testing.T) {
+	data := acceptance.BuildTestData(t, "data.azurerm_virtual_machine_scale_set", "test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acceptance.PreCheck(t) },
+		Providers:    acceptance.SupportedProviders,
+		CheckDestroy: testCheckAzureRMVirtualMachineDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceAzureRMVirtualMachineScaleSet_orchestrated(data),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(data.ResourceName, "id"),
+				),
+			},
+		},
+	})
+}
+
 func testAccDataSourceAzureRMVirtualMachineScaleSet_basicLinux(data acceptance.TestData) string {
 	template := testAccAzureRMLinuxVirtualMachineScaleSet_identitySystemAssigned(data)
 	return fmt.Sprintf(`
@@ -66,6 +84,18 @@ func testAccDataSourceAzureRMVirtualMachineScaleSet_basicWindows(data acceptance
 
 data "azurerm_virtual_machine_scale_set" "test" {
   name                = azurerm_windows_virtual_machine_scale_set.test.name
+  resource_group_name = azurerm_resource_group.test.name
+}
+`, template)
+}
+
+func testAccDataSourceAzureRMVirtualMachineScaleSet_orchestrated(data acceptance.TestData) string {
+	template := testAccAzureRMWindowsVirtualMachine_orchestratedZonal(data)
+	return fmt.Sprintf(`
+%s
+
+data "azurerm_virtual_machine_scale_set" "test" {
+  name                = azurerm_orchestrated_virtual_machine_scale_set.test.name
   resource_group_name = azurerm_resource_group.test.name
 }
 `, template)
