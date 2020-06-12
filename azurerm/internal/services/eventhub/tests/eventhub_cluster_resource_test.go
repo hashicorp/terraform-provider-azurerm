da20d62c35dca67ce9267c057d4d04f0e1fbe06d
Matthew Frahry <mbfrahry@gmail.com>
2020-06-12 08:45:24 -0700

    fmt

--- azurerm/internal/services/eventhub/tests/eventhub_cluster_resource_test.go
+++ azurerm/internal/services/eventhub/tests/eventhub_cluster_resource_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"testing"
-	
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
