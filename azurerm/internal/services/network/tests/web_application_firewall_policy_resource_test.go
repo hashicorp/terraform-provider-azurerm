4ea2acd88a1f1bbbacc4559f7734b33b6ffc3085
Magnus Niemann <magnus.niemann@bosch.io>
2020-06-16 17:31:06 +0200

    Tabs2spaces.

--- azurerm/internal/services/network/tests/web_application_firewall_policy_resource_test.go
+++ azurerm/internal/services/network/tests/web_application_firewall_policy_resource_test.go
@@ -253,7 +253,7 @@ resource "azurerm_web_application_firewall_policy" "test" {
 
   policy_settings {
     enabled                     = true
-	mode                        = "Detection"
+    mode                        = "Detection"
     request_body_check          = true
     file_upload_limit_in_mb     = 100
     max_request_body_size_in_kb = 128	
