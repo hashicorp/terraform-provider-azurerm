a9db7977eb147303ac36fa140b53c78fb7db5dff
Jochen Rauschenbusch <jochen.rauschenbusch@gmail.com>
2020-06-24 18:25:37 +0200

    Kusto Cluster Customer Managed Key support (Key Vault)

--- azurerm/internal/services/kusto/tests/kusto_cluster_resource_test.go
+++ azurerm/internal/services/kusto/tests/kusto_cluster_resource_test.go
@@ -425,6 +425,48 @@ resource "azurerm_key_vault" "test" {
   sku_name            = "standard"
 }
 
+resource "azurerm_key_vault_access_policy" "test" {
+  key_vault_id = azurerm_key_vault.test.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "create",
+    "get",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "test2" {
+  key_vault_id = azurerm_key_vault.test.id
+  tenant_id    = azurerm_kusto_cluster.test.identity.0.tenant_id
+  object_id    = azurerm_kusto_cluster.test.identity.0.principal_id
+  key_permissions = [
+    "create",
+    "get",
+  ]
+}
+
+resource "azurerm_key_vault_key" "test" {
+  name         = "acctestkc%s-key"
+  key_vault_id = azurerm_key_vault.test.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "verify",
+    "wrapKey",
+    "unwrapKey"
+  ]
+
+  depends_on = [
+    azurerm_key_vault_access_policy.test,
+  ]
+}
+
 resource "azurerm_kusto_cluster" "test" {
   name                = "acctestkc%s"
   location            = azurerm_resource_group.test.location
@@ -440,8 +482,8 @@ resource "azurerm_kusto_cluster" "test" {
   }
 
   key_vault {
-    key_name      = "acctestkc%s"
-    key_version   = "1"
+    key_name      = azurerm_key_vault_key.test.name
+    key_version   = azurerm_key_vault_key.test.version
     key_vault_uri = azurerm_key_vault.test.vault_uri
   }
 }
