89e1124a39bf5a84105c471ba71517b0d5c98988
Everton Macedo <everton.macedo@ptvgroup.com>
2020-06-24 12:31:35 +0200

    provide more fields to test

--- azurerm/internal/services/postgres/tests/postgresql_administrator_resource_test.go
+++ azurerm/internal/services/postgres/tests/postgresql_administrator_resource_test.go
@@ -161,12 +161,22 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_postgresql_server" "test" {
-  name                         = "acctestsqlserver%d"
-  resource_group_name          = azurerm_resource_group.test.name
-  location                     = azurerm_resource_group.test.location
-  version                      = "9.6"
-  administrator_login          = "mradministrator"
-  administrator_login_password = "thisIsDog11"
+	name                = "acctest-psql-server-%d"
+	location            = azurerm_resource_group.test.location
+	resource_group_name = azurerm_resource_group.test.name
+  
+	sku_name = "GP_Gen5_2"
+  
+	storage_profile {
+	  storage_mb            = 51200
+	  backup_retention_days = 7
+	  geo_redundant_backup  = "Disabled"
+	}
+  
+	administrator_login          = "acctestun"
+	administrator_login_password = "H@Sh1CoR3!"
+	version                      = "9.6"
+	ssl_enforcement              = "Enabled"
 }
 
 resource "azurerm_postgresql_active_directory_administrator" "test" {
@@ -208,12 +218,22 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_postgresql_server" "test" {
-  name                         = "acctestsqlserver%d"
-  resource_group_name          = azurerm_resource_group.test.name
-  location                     = azurerm_resource_group.test.location
-  version                      = "9.6"
-  administrator_login          = "mradministrator"
-  administrator_login_password = "thisIsDog11"
+	name                = "acctest-psql-server-%d"
+	location            = azurerm_resource_group.test.location
+	resource_group_name = azurerm_resource_group.test.name
+  
+	sku_name = "GP_Gen5_2"
+  
+	storage_profile {
+	  storage_mb            = 51200
+	  backup_retention_days = 7
+	  geo_redundant_backup  = "Disabled"
+	}
+  
+	administrator_login          = "acctestun"
+	administrator_login_password = "H@Sh1CoR3!"
+	version                      = "9.6"
+	ssl_enforcement              = "Enabled"
 }
 
 resource "azurerm_postgresql_active_directory_administrator" "test" {
