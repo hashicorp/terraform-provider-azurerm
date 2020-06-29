0eda378594d11f3e7ac9fd3ab85991b6ea70a79e
jbinko <jbinko+github@gmail.com>
2020-06-30 00:38:25 +0200

    Schema update, CreateUpdate v0.1 function and some validations

--- azurerm/internal/services/logic/integration_service_environment.go
+++ azurerm/internal/services/logic/integration_service_environment.go
@@ -1,6 +1,8 @@
 package logic
 
 import (
+	"fmt"
+	"log"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/logic/mgmt/2019-05-01/logic"
@@ -8,31 +10,38 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/set"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/features"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/logic/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
 func resourceArmIntegrationServiceEnvironment() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceArmIntegrationServiceEnvironmentCreate,
+		Create: resourceArmIntegrationServiceEnvironmentCreateUpdate,
 		Read:   resourceArmIntegrationServiceEnvironmentRead,
-		Update: resourceArmIntegrationServiceEnvironmentUpdate,
+		Update: resourceArmIntegrationServiceEnvironmentCreateUpdate,
 		Delete: resourceArmIntegrationServiceEnvironmentDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(30 * time.Minute),
+			Create: schema.DefaultTimeout(4 * time.Hour),
 			Read:   schema.DefaultTimeout(5 * time.Minute),
-			Update: schema.DefaultTimeout(30 * time.Minute),
-			Delete: schema.DefaultTimeout(30 * time.Minute),
+			Update: schema.DefaultTimeout(4 * time.Hour),
+			Delete: schema.DefaultTimeout(4 * time.Hour),
 		},
 
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validate.IntegrationServiceEnvironmentName(),
 			},
 
 			"location": azure.SchemaLocation(),
@@ -41,17 +50,15 @@ func resourceArmIntegrationServiceEnvironment() *schema.Resource {
 
 			"sku_name": {
 				Type:     schema.TypeString,
-				Optional: true,
-				Default:  string(logic.IntegrationServiceEnvironmentSkuNameNotSpecified),
+				Required: true,
+				ForceNew: true, // The SKU cannot be changed once integration service environment has been provisioned.
 				ValidateFunc: validation.StringInSlice([]string{
-					string(logic.IntegrationServiceEnvironmentSkuNameNotSpecified),
 					string(logic.IntegrationServiceEnvironmentSkuNameDeveloper),
 					string(logic.IntegrationServiceEnvironmentSkuNamePremium),
 				}, false),
 			},
 
-			// Maximum scale units that you can add	10
-			// https://docs.microsoft.com/en-US/azure/logic-apps/logic-apps-limits-and-config#integration-service-environment-ise
+			// Maximum scale units that you can add	10 - https://docs.microsoft.com/en-US/azure/logic-apps/logic-apps-limits-and-config#integration-service-environment-ise
 			"capacity": {
 				Type:         schema.TypeInt,
 				Optional:     true,
@@ -61,10 +68,9 @@ func resourceArmIntegrationServiceEnvironment() *schema.Resource {
 
 			"access_endpoint_type": {
 				Type:     schema.TypeString,
-				Optional: true,
-				Default:  string(logic.IntegrationServiceEnvironmentAccessEndpointTypeNotSpecified),
+				Required: true,
+				ForceNew: true, // The access end point type cannot be changed once the integration service environment is provisioned.
 				ValidateFunc: validation.StringInSlice([]string{
-					string(logic.IntegrationServiceEnvironmentAccessEndpointTypeNotSpecified),
 					string(logic.IntegrationServiceEnvironmentAccessEndpointTypeInternal),
 					string(logic.IntegrationServiceEnvironmentAccessEndpointTypeExternal),
 				}, false),
@@ -72,9 +78,15 @@ func resourceArmIntegrationServiceEnvironment() *schema.Resource {
 
 			"virtual_network_subnet_ids": {
 				Type:     schema.TypeSet,
-				Optional: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
+				Required: true,
+				ForceNew: true, // The network configuration subnets cannot be updated after integration service environment is created.
+				Elem: &schema.Schema{
+					Type:         schema.TypeString,
+					ValidateFunc: validate.ValidateSubnetID,
+				},
 				Set:      set.HashStringIgnoreCase,
+				MinItems: 4,
+				MaxItems: 4,
 			},
 
 			"connector_endpoint_ip_addresses": {
@@ -106,18 +118,96 @@ func resourceArmIntegrationServiceEnvironment() *schema.Resource {
 	}
 }
 
-func resourceArmIntegrationServiceEnvironmentCreate(d *schema.ResourceData, m interface{}) error {
-	return resourceArmIntegrationServiceEnvironmentRead(d, m)
+func resourceArmIntegrationServiceEnvironmentCreateUpdate(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*clients.Client).Logic.IntegrationServiceEnvironmentsClient
+	ctx, cancel := timeouts.ForCreate(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	log.Printf("[INFO] preparing arguments for Azure ARM Integration Service Environment creation.")
+
+	name := d.Get("name").(string)
+	resourceGroup := d.Get("resource_group_name").(string)
+
+	if features.ShouldResourcesBeImported() && d.IsNewResource() {
+		existing, err := client.Get(ctx, resourceGroup, name)
+		if err != nil {
+			if !utils.ResponseWasNotFound(existing.Response) {
+				return fmt.Errorf("Error checking for presence of existing Integration Service Environment %q (Resource Group %q): %s", name, resourceGroup, err)
+			}
+		}
+
+		if existing.ID != nil && *existing.ID != "" {
+			return tf.ImportAsExistsError("azurerm_integration_service_environment", *existing.ID)
+		}
+	}
+
+	location := azure.NormalizeLocation(d.Get("location").(string))
+	skuName := d.Get("sku_name").(string)
+	capacity := d.Get("capacity").(int)
+	accessEndpointType := d.Get("access_endpoint_type").(string)
+	virtualNetworkSubnetIds := d.Get("virtual_network_subnet_ids").(*schema.Set).List()
+	t := d.Get("tags").(map[string]interface{})
+
+	properties := logic.IntegrationServiceEnvironment{
+		Name:     &name,
+		Location: &location,
+		Properties: &logic.IntegrationServiceEnvironmentProperties{
+			NetworkConfiguration: &logic.NetworkConfiguration{
+				AccessEndpoint: &logic.IntegrationServiceEnvironmentAccessEndpoint{
+					Type: logic.IntegrationServiceEnvironmentAccessEndpointType(accessEndpointType),
+				},
+				Subnets: expandSubnetResourceID(virtualNetworkSubnetIds),
+			},
+		},
+		Sku: &logic.IntegrationServiceEnvironmentSku{
+			Name:     logic.IntegrationServiceEnvironmentSkuName(skuName),
+			Capacity: utils.Int32(int32(capacity)),
+		},
+		Tags: tags.Expand(t),
+	}
+
+	future, err := client.CreateOrUpdate(ctx, resourceGroup, name, properties)
+	if err != nil {
+		return fmt.Errorf("creating/updating Integration Service Environment %q (Resource Group %q): %+v", name, resourceGroup, err)
+	}
+
+	if err = future.WaitForCompletionRef(ctx, client.Client); err != nil {
+		return fmt.Errorf("waiting for completion of Integration Service Environment %q (Resource Group %q): %+v", name, resourceGroup, err)
+	}
+
+	read, err := client.Get(ctx, resourceGroup, name)
+	if err != nil {
+		return fmt.Errorf("read request on Integration Service Environment %q (Resource Group %q): %+v", name, resourceGroup, err)
+	}
+
+	if read.ID == nil {
+		return fmt.Errorf("cannot read Integration Service Environment %q (Resource Group %q) ID", name, resourceGroup)
+	}
+
+	d.SetId(*read.ID)
+
+	return resourceArmIntegrationServiceEnvironmentRead(d, meta)
 }
 
-func resourceArmIntegrationServiceEnvironmentRead(d *schema.ResourceData, m interface{}) error {
+func resourceArmIntegrationServiceEnvironmentRead(d *schema.ResourceData, meta interface{}) error {
+
 	return nil
 }
 
-func resourceArmIntegrationServiceEnvironmentUpdate(d *schema.ResourceData, m interface{}) error {
-	return resourceArmIntegrationServiceEnvironmentRead(d, m)
+func resourceArmIntegrationServiceEnvironmentDelete(d *schema.ResourceData, meta interface{}) error {
+
+	return nil
 }
 
-func resourceArmIntegrationServiceEnvironmentDelete(d *schema.ResourceData, m interface{}) error {
-	return nil
+func expandSubnetResourceID(input []interface{}) *[]logic.ResourceReference {
+	results := make([]logic.ResourceReference, 0)
+	for _, item := range input {
+		id := item.(string)
+
+		results = append(results, logic.ResourceReference{
+			ID: utils.String(id),
+		})
+	}
+	return &results
 }
