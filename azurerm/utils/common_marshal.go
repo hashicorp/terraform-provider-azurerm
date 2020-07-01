cbe6c432c0312933af7a91bdb1638ec9479c7eca
magodo <wztdyl@sina.com>
2020-07-01 16:58:02 +0800

    generalized expand slice/map

--- azurerm/utils/common_marshal.go
+++ azurerm/utils/common_marshal.go
@@ -1,5 +1,7 @@
 package utils
 
+import "reflect"
+
 func ExpandStringSlice(input []interface{}) *[]string {
 	result := make([]string, 0)
 	for _, item := range input {
@@ -79,4 +81,48 @@ func FlattenInt32Slice(input *[]int32) []interface{} {
 		}
 	}
 	return result
+}
+
+// ExpandSlice expands the input slice into slice of element whose type is specified by "t".
+// If "t" is different from the element type of input, then user has to specify a customized converter via
+// "convert", which guides the conversion from element of the input slice to the element of the output slice.
+// Otherwise, user can pass a nil "convert".
+func ExpandSlice(input []interface{}, t interface{}, convert func(interface{}) interface{}) interface{} {
+	targetType := reflect.TypeOf(t)
+	result := reflect.MakeSlice(reflect.SliceOf(targetType), 0, 0)
+	for _, item := range input {
+		if item != nil {
+			var vitem reflect.Value
+			if convert == nil {
+				vitem = reflect.New(targetType).Elem()
+				vitem.Set(reflect.ValueOf(item))
+			} else {
+				vitem = reflect.ValueOf(convert(item))
+			}
+			result = reflect.Append(result, vitem)
+		} else {
+			result = reflect.Append(result, reflect.Zero(targetType))
+		}
+	}
+	return result.Interface()
+}
+
+// ExpandMap expands the input map (key is of type string) into map (key is of type string) where the type of value is specified by "t".
+// If "t" is different from the element type of input, then user has to specify a customized converter via
+// "convert", which guides the conversion from value of the input map to the value of the output map.
+// Otherwise, user can pass a nil "convert".
+func ExpandMap(input map[string]interface{}, t interface{}, convert func(interface{}) interface{}) interface{} {
+	targetType := reflect.TypeOf(t)
+	result := reflect.MakeMap(reflect.MapOf(reflect.TypeOf(""), targetType))
+	for k, v := range input {
+		var vv reflect.Value
+		if convert == nil {
+			vv = reflect.New(targetType).Elem()
+			vv.Set(reflect.ValueOf(v))
+		} else {
+			vv = reflect.ValueOf(convert(v))
+		}
+		result.SetMapIndex(reflect.ValueOf(k), vv)
+	}
+	return result.Interface()
 }
---
+++ azurerm/utils/common_marshal_test.go
@@ -0,0 +1,210 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+func TestExpandSlice(t *testing.T) {
+	type T1 string
+	type T2 struct {
+		S string
+	}
+
+	cases := []struct {
+		input   []interface{}
+		t       interface{}
+		convert func(interface{}) interface{}
+		output  interface{}
+	}{
+		// slice of string -> slice of string
+		{
+			input:  []interface{}{"a", "b"},
+			t:      "",
+			output: []string{"a", "b"},
+		},
+		// slice of string -> slice of customized string type
+		{
+			input: []interface{}{"a", "b"},
+			t:     T1(""),
+			convert: func(x interface{}) interface{} {
+				return T1(x.(string))
+			},
+			output: []T1{"a", "b"},
+		},
+		// slice of string -> slice of customized structure containing string member
+		{
+			input: []interface{}{"a", "b"},
+			t:     T2{},
+			convert: func(x interface{}) interface{} {
+				return T2{S: x.(string)}
+			},
+			output: []T2{{"a"}, {"b"}},
+		},
+		// slice of int32 -> slice of int32
+		{
+			input:  []interface{}{int32(1), int32(2)},
+			t:      int32(0),
+			output: []int32{1, 2},
+		},
+		// slice of int32 -> slice of int64
+		{
+			input: []interface{}{int32(1), int32(2)},
+			t:     int64(0),
+			convert: func(x interface{}) interface{} {
+				return int64(x.(int32))
+			},
+			output: []int64{1, 2},
+		},
+		// slice of int32 -> slice of int
+		{
+			input: []interface{}{int32(1), int32(2)},
+			t:     0,
+			convert: func(x interface{}) interface{} {
+				return int(x.(int32))
+			},
+			output: []int{1, 2},
+		},
+		// slice of float64 -> slice of float32
+		{
+			input: []interface{}{float64(1), float64(2)},
+			t:     float32(0),
+			convert: func(x interface{}) interface{} {
+				return float32(x.(float64))
+			},
+			output: []float32{1, 2},
+		},
+		// slice of float64 -> slice of float64
+		{
+			input:  []interface{}{float64(1), float64(2)},
+			t:      float64(0),
+			output: []float64{1, 2},
+		},
+	}
+
+	for idx, c := range cases {
+		out := ExpandSlice(c.input, c.t, c.convert)
+		if !reflect.DeepEqual(out, c.output) {
+			t.Fatalf("%d failed\nexpected:\n%s\nactual:\n%s\n", idx, spew.Sdump(out), spew.Sdump(c.output))
+		}
+	}
+}
+
+func TestExpandMap(t *testing.T) {
+	type T1 string
+	type T2 struct {
+		S string
+	}
+
+	cases := []struct {
+		input   map[string]interface{}
+		t       interface{}
+		convert func(interface{}) interface{}
+		output  interface{}
+	}{
+		// map[string]string -> map[string]string
+		{
+			input: map[string]interface{}{
+				"a": "b",
+			},
+			t: "",
+			output: map[string]string{
+				"a": "b",
+			},
+		},
+		// map[string]string -> map[string](customized string type)
+		{
+			input: map[string]interface{}{
+				"a": "b",
+			},
+			t: T1(""),
+			convert: func(x interface{}) interface{} {
+				return T1(x.(string))
+			},
+			output: map[string]T1{
+				"a": "b",
+			},
+		},
+		// map[string]string -> map[string](customized type structure containing string member)
+		{
+			input: map[string]interface{}{
+				"a": "b",
+			},
+			t: T2{},
+			convert: func(x interface{}) interface{} {
+				return T2{S: x.(string)}
+			},
+			output: map[string]T2{
+				"a": {"b"},
+			},
+		},
+		// map[string]int32  -> map[string]int32
+		{
+			input: map[string]interface{}{
+				"a": int32(1),
+			},
+			t: int32(0),
+			output: map[string]int32{
+				"a": 1,
+			},
+		},
+		// map[string]int32  -> map[string]int64
+		{
+			input: map[string]interface{}{
+				"a": int32(1),
+			},
+			t: int64(0),
+			convert: func(x interface{}) interface{} {
+				return int64(x.(int32))
+			},
+			output: map[string]int64{
+				"a": 1,
+			},
+		},
+		// map[string]int32 -> map[string]int
+		{
+			input: map[string]interface{}{
+				"a": int32(1),
+			},
+			t: 0,
+			convert: func(x interface{}) interface{} {
+				return int(x.(int32))
+			},
+			output: map[string]int{
+				"a": 1,
+			},
+		},
+		// map[string]float64 -> map[string]float32
+		{
+			input: map[string]interface{}{
+				"a": float64(0),
+			},
+			t: float32(0),
+			convert: func(x interface{}) interface{} {
+				return float32(x.(float64))
+			},
+			output: map[string]float32{
+				"a": 0,
+			},
+		},
+		// map[string]float64 -> map[string]float64
+		{
+			input: map[string]interface{}{
+				"a": float64(0),
+			},
+			t: float64(0),
+			output: map[string]float64{
+				"a": 0,
+			},
+		},
+	}
+
+	for idx, c := range cases {
+		out := ExpandMap(c.input, c.t, c.convert)
+		if !reflect.DeepEqual(out, c.output) {
+			t.Fatalf("%d failed\nexpected:\n%s\nactual:\n%s\n", idx, spew.Sdump(out), spew.Sdump(c.output))
+		}
+	}
+}
