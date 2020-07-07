453d85076b8df5aa96ea70cc562087c2b5e1a98b
Rik Ribbers <rik.ribbers@gmail.com>
2020-07-07 18:48:30 +0200

    Update azurerm/internal/services/network/tests/web_application_firewall_policy_data_source_test.go
    Co-authored-by: Tom Bamford <tom@bamford.io>

--- azurerm/internal/services/network/tests/web_application_firewall_policy_data_source_test.go
+++ azurerm/internal/services/network/tests/web_application_firewall_policy_data_source_test.go
@@ -31,97 +31,11 @@ func TestAccDataSourceAzureRMWebApplicationFirewallPolicy_basic(t *testing.T) {
 
 func testAccDataSourceAzureRMWebApplicationFirewallPolicyBasic(data acceptance.TestData) string {
 	return fmt.Sprintf(`
-resource "azurerm_resource_group" "test" {
-  name     = "acctestRG-%d"
-  location = "%s"
-}
-
-resource "azurerm_web_application_firewall_policy" "test" {
-  name                = "example-wafpolicy"
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-
-  custom_rules {
-    name      = "Rule1"
-    priority  = 1
-    rule_type = "MatchRule"
-
-    match_conditions {
-      match_variables {
-        variable_name = "RemoteAddr"
-      }
-
-      operator           = "IPMatch"
-      negation_condition = false
-      match_values       = ["192.168.1.0/24", "10.0.0.0/24"]
-    }
-
-    action = "Block"
-  }
-
-  custom_rules {
-    name      = "Rule2"
-    priority  = 2
-    rule_type = "MatchRule"
-
-    match_conditions {
-      match_variables {
-        variable_name = "RemoteAddr"
-      }
-
-      operator           = "IPMatch"
-      negation_condition = false
-      match_values       = ["192.168.1.0/24"]
-    }
-
-    match_conditions {
-      match_variables {
-        variable_name = "RequestHeaders"
-        selector      = "UserAgent"
-      }
-
-      operator           = "Contains"
-      negation_condition = false
-      match_values       = ["Windows"]
-    }
-
-    action = "Block"
-  }
-
-  policy_settings {
-    enabled = true
-    mode    = "Prevention"
-  }
-
-  managed_rules {
-    exclusion {
-      match_variable          = "RequestHeaderNames"
-      selector                = "x-company-secret-header"
-      selector_match_operator = "Equals"
-    }
-    exclusion {
-      match_variable          = "RequestCookieNames"
-      selector                = "too-tasty"
-      selector_match_operator = "EndsWith"
-    }
-
-    managed_rule_set {
-      type    = "OWASP"
-      version = "3.1"
-      rule_group_override {
-        rule_group_name = "REQUEST-920-PROTOCOL-ENFORCEMENT"
-        disabled_rules = [
-          "920300",
-          "920440"
-        ]
-      }
-    }
-  }
-}
+%s
 
 data "azurerm_web_application_firewall_policy" "test" {
   resource_group_name = azurerm_resource_group.test.name
   name                = azurerm_web_application_firewall_policy.test.name
 }
-`, data.RandomInteger, data.Locations.Primary)
+`, testAccAzureRMWebApplicationFirewallPolicy_complete(data))
 }
