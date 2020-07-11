e6cc65d34d464ad11335c27aa180cf8486c74d02
njucz <740360112@qq.com>
2020-07-11 15:17:25 +0800

    update import func

--- azurerm/internal/services/notificationhub/notification_hub_resource.go
+++ azurerm/internal/services/notificationhub/notification_hub_resource.go
@@ -17,6 +17,7 @@ import (
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/features"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/notificationhub/parse"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
+	azSchema "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
@@ -34,9 +35,11 @@ func resourceArmNotificationHub() *schema.Resource {
 		Read:   resourceArmNotificationHubRead,
 		Update: resourceArmNotificationHubCreateUpdate,
 		Delete: resourceArmNotificationHubDelete,
-		Importer: &schema.ResourceImporter{
-			State: schema.ImportStatePassthrough,
-		},
+
+		Importer: azSchema.ValidateResourceIDPriorToImport(func(id string) error {
+			_, err := parse.NotificationHubID(id)
+			return err
+		}),
 
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(30 * time.Minute),
