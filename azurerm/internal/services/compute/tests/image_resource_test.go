7c3d3aa1f4c1d441c30bd513776a32877405ccf4
Dapeng Zhang <dapzhang@microsoft.com>
2020-07-13 15:24:59 +0800

    Update compute api-version to 2020-06-01

--- azurerm/internal/services/compute/tests/image_resource_test.go
+++ azurerm/internal/services/compute/tests/image_resource_test.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-12-01/compute"
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2020-06-01/compute"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
