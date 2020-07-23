9a43475e8fdb015e35d07e267dce1e52680ab324
Chang Li <lrxtom2@hotmail.com>
2020-07-23 15:50:14 +0800

    Add `publicNetworkAccess` into Search Service

--- vendor/github.com/Azure/azure-sdk-for-go/services/search/mgmt/2020-03-13/search/operations.go
+++ vendor/github.com/Azure/azure-sdk-for-go/services/search/mgmt/2020-03-13/search/operations.go
@@ -76,7 +76,7 @@ func (client OperationsClient) List(ctx context.Context) (result OperationListRe
 
 // ListPreparer prepares the List request.
 func (client OperationsClient) ListPreparer(ctx context.Context) (*http.Request, error) {
-	const APIVersion = "2015-08-19"
+	const APIVersion = "2020-03-13"
 	queryParameters := map[string]interface{}{
 		"api-version": APIVersion,
 	}
