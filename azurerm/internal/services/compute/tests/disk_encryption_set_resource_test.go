dda13d7a9f62b79ea241b6f311627331045fcbc5
Dapeng Zhang <dapzhang@microsoft.com>
2020-07-28 11:12:20 +0800

    Fix CI

--- azurerm/internal/services/compute/tests/disk_encryption_set_resource_test.go
+++ azurerm/internal/services/compute/tests/disk_encryption_set_resource_test.go
@@ -196,11 +196,11 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_key_vault" "test" {
-  name                = "acctestkv-%s"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "premium"
+  name                        = "acctestkv-%s"
+  location                    = azurerm_resource_group.test.location
+  resource_group_name         = azurerm_resource_group.test.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "premium"
   soft_delete_enabled         = true
   purge_protection_enabled    = true
   enabled_for_disk_encryption = true
