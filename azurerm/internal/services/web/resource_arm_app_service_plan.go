c73696bbfb237a483f4ce29bacc1ecaefcf2f772
njucz <zhcheng@microsoft.com>
2020-07-29 13:10:34 +0800

    Revert "Update doc for the reserved field of app service plan (#7661)"
    This reverts commit 83a5dfb1c607ac3c87700ec72a8527ac2cb258b9.

--- azurerm/internal/services/web/resource_arm_app_service_plan.go
+++ azurerm/internal/services/web/resource_arm_app_service_plan.go
@@ -193,10 +193,6 @@ func resourceArmAppServicePlanCreateUpdate(d *schema.ResourceData, meta interfac
 		return fmt.Errorf("`reserved` has to be set to true when kind is set to `Linux`")
 	}
 
-	if !strings.EqualFold(kind, "Linux") && reserved {
-		return fmt.Errorf("`reserved` has to be set to false when kind isn't set to `Linux`")
-	}
-
 	if v := d.Get("maximum_elastic_worker_count").(int); v > 0 {
 		appServicePlan.AppServicePlanProperties.MaximumElasticWorkerCount = utils.Int32(int32(v))
 	}
