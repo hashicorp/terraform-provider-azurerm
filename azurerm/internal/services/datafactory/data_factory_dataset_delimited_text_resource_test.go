4b9735726aef5aa7ee815803936468e6b5764f48
Mark Tinderholt <mark_tinderholt@persistent.com>
2020-07-29 19:44:03 -0400

    tflint

--- azurerm/internal/services/datafactory/data_factory_dataset_delimited_text_resource_test.go
+++ azurerm/internal/services/datafactory/data_factory_dataset_delimited_text_resource_test.go
@@ -350,8 +350,8 @@ resource "azurerm_resource_group" "test" {
 
 resource "azurerm_storage_account" "test" {
   name                     = "acctestdf%s"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  resource_group_name      = azurerm_resource_group.test.name
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
@@ -384,8 +384,8 @@ resource "azurerm_data_factory_dataset_delimited_text" "test" {
 
   azure_blob_storage_location {
     container = azurerm_storage_container.test.name
-    path         = "foo/bar/"
-    filename     = "foo.txt"
+    path      = "foo/bar/"
+    filename  = "foo.txt"
   }
 
   column_delimiter    = ","
