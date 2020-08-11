6985e34e04343e168cb79ebac3579197f330384d
jbinko <jbinko+github@gmail.com>
2020-08-11 11:52:37 +0200

    rename the resource group in the test cases
    Co-authored-by: WS <20408400+WodansSon@users.noreply.github.com>

--- azurerm/internal/services/logic/tests/integration_service_environment_resource_test.go
+++ azurerm/internal/services/logic/tests/integration_service_environment_resource_test.go
@@ -159,7 +159,7 @@ func TestAccAzureRMIntegrationServiceEnvironment_update(t *testing.T) {
 					testCheckAzureRMIntegrationServiceEnvironmentExists(data.ResourceName),
 					resource.TestCheckResourceAttr(data.ResourceName, "name", fmt.Sprintf("acctest-ise-%d", data.RandomInteger)),
 					resource.TestCheckResourceAttr(data.ResourceName, "location", data.Locations.Primary),
-					resource.TestCheckResourceAttr(data.ResourceName, "resource_group_name", fmt.Sprintf("acctest-ise-%d", data.RandomInteger)),
+					resource.TestCheckResourceAttr(data.ResourceName, "resource_group_name", fmt.Sprintf("acctestRG-logic-%d", data.RandomInteger)),
 					resource.TestCheckResourceAttr(data.ResourceName, "sku_name", "Premium_0"),
 					resource.TestCheckResourceAttr(data.ResourceName, "access_endpoint_type", "Internal"),
 					resource.TestCheckResourceAttr(data.ResourceName, "virtual_network_subnet_ids.#", "4"),
