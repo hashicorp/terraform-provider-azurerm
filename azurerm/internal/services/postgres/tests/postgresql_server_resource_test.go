c5794f7ca0e500a1f47361fd16758118aa26a41c
yupwei68 <yupwei@microsoft.com>
2020-08-12 15:28:12 +0800

    update

--- azurerm/internal/services/postgres/tests/postgresql_server_resource_test.go
+++ azurerm/internal/services/postgres/tests/postgresql_server_resource_test.go
@@ -742,7 +742,7 @@ resource "azurerm_postgresql_server" "test" {
   auto_grow_enabled     = false
 
   public_network_access_enabled    = true
-  ssl_enforcement_enabled          = false
+  ssl_enforcement_enabled          = true
   ssl_minimal_tls_version_enforced = "TLS1_1"
 
   threat_detection_policy {
