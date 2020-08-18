8a46a4f6f05498f2a6ec5bf8acff586ec9a808a3
neil-yechenwei <yechenwei2007@126.com>
2020-08-18 19:46:44 +0800

    Update code

--- examples/virtual-networks/virtual-network-gateway/vpngw1/main.tf
+++ examples/virtual-networks/virtual-network-gateway/vpngw1/main.tf
@@ -25,7 +25,8 @@ resource "azurerm_public_ip" "example" {
   name                = "${var.prefix}-pip"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_virtual_network_gateway" "example" {
