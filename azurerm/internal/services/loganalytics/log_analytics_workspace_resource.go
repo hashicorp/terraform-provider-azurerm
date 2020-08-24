0a8272c8331d3acb2edd7082de3f95e07bd47c0b
Matthew Frahry <mbfrahry@gmail.com>
2020-08-24 16:05:32 -0700

    goimports

--- azurerm/internal/services/loganalytics/log_analytics_workspace_resource.go
+++ azurerm/internal/services/loganalytics/log_analytics_workspace_resource.go
@@ -2,12 +2,12 @@ package loganalytics
 
 import (
 	"fmt"
-	"github.com/hashicorp/go-azure-helpers/response"
 	"log"
 	"regexp"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/preview/operationalinsights/mgmt/2020-03-01-preview/operationalinsights"
+	"github.com/hashicorp/go-azure-helpers/response"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
