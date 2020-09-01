6a0e7279861ccaf50cb53c1aa868605423870d36
jackofallops <ste@hashicorp.com>
2020-09-01 08:58:29 +0100

    refactor beta opt in for provider consistency

--- azurerm/internal/features/beta_features_opt_in.go
+++ azurerm/internal/features/beta_features_opt_in.go
@@ -2,6 +2,7 @@ package features
 
 import (
 	"os"
+	"strings"
 )
 
 // VMSSExtensionsBeta returns whether or not the beta for VMSS Extensions for Linux and Windows VMSS resources is
@@ -9,10 +10,5 @@ import (
 //
 // Set to any non-empty value to enable
 func VMSSExtensionsBeta() bool {
-	value := os.Getenv("ARM_PROVIDER_VMSS_EXTENSIONS_BETA")
-	if value == "" {
-		return false
-	} else {
-		return true
-	}
+	return strings.EqualFold(os.Getenv("ARM_PROVIDER_VMSS_EXTENSIONS_BETA"), "true")
 }
