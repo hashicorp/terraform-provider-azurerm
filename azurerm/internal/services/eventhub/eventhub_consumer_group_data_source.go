35f71edc93f8c616dfbc1dcdbec69168f135ad65
Vladimir Lazarenko <favoretti@gmail.com>
2020-09-09 09:11:49 +0200

    Ordering of imports

--- azurerm/internal/services/eventhub/eventhub_consumer_group_data_source.go
+++ azurerm/internal/services/eventhub/eventhub_consumer_group_data_source.go
@@ -2,10 +2,10 @@ package eventhub
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
