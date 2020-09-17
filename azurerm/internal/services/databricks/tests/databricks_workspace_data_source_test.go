11cfcf0a153e0f1f74b22ebfcf95159407268f3d
Vladimir Lazarenko <favoretti@gmail.com>
2020-09-17 13:09:09 +0200

    terrafmt

--- azurerm/internal/services/databricks/tests/databricks_workspace_data_source_test.go
+++ azurerm/internal/services/databricks/tests/databricks_workspace_data_source_test.go
@@ -39,7 +39,7 @@ resource "azurerm_resource_group" "example" {
 resource "azurerm_databricks_workspace" "example" {
   name     = "acctestRG-databricks-workspace-%[1]d"
   location = azurerm_resource_group.example.location
-  sku = "premium"
+  sku      = "premium"
 }
 
 data "azurerm_databricks_workspace" "example" {
