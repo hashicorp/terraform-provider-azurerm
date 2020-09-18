2a195a5b0e1a690a2ebf895bc00cf5ee8a5bd0bb
yupwei68 <yupwei@microsoft.com>
2020-09-18 09:55:42 +0800

    update

--- azurerm/internal/services/avs/avs_private_clouds_resource.go
+++ azurerm/internal/services/avs/avs_private_clouds_resource.go
@@ -28,10 +28,10 @@ func resourceArmAvsPrivateCloud() *schema.Resource {
 		Delete: resourceArmAvsPrivateCloudDelete,
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(240 * time.Minute),
+			Create: schema.DefaultTimeout(5 * time.Hour),
 			Read:   schema.DefaultTimeout(5 * time.Minute),
-			Update: schema.DefaultTimeout(240 * time.Minute),
-			Delete: schema.DefaultTimeout(240 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Hour),
+			Delete: schema.DefaultTimeout(5 * time.Hour),
 		},
 
 		Importer: azSchema.ValidateResourceIDPriorToImport(func(id string) error {
