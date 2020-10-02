60ce61b8327d2aff39529669cd3b7c5e23f363e1
Borko <borkod@gmail.com>
2020-10-01 20:08:52 -0400

    Fixed example to actually use Calico Network policy.

--- examples/kubernetes/network-policy-calico/variables.tf
+++ examples/kubernetes/network-policy-calico/variables.tf
@@ -1,7 +1,9 @@
 variable "prefix" {
   description = "A prefix used for all resources in this example"
+  default = "West US" // REMOVE
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be provisioned"
+  default = "canadacentral" // REMOVE
 }
