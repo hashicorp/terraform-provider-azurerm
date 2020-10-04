7345ccbd71a24a7bcead49caf4ef245f7724c30d
Jochen Rauschenbusch <jochen.rauschenbusch@gmail.com>
2020-10-04 08:54:28 +0200

    terrafmt

--- azurerm/internal/services/eventgrid/tests/eventgrid_system_topic_resource_test.go
+++ azurerm/internal/services/eventgrid/tests/eventgrid_system_topic_resource_test.go
@@ -210,8 +210,8 @@ resource "azurerm_eventgrid_system_topic" "test" {
   name                = "acctestEGST%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  source_resource_id = azurerm_storage_account.test.id
-  topic_type         = "Microsoft.Storage.StorageAccounts"
+  source_resource_id  = azurerm_storage_account.test.id
+  topic_type          = "Microsoft.Storage.StorageAccounts"
 
   tags = {
     "Foo" = "Bar"
