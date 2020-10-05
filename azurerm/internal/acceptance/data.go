fed53d6b92ceab3d302cefb701277d6008d84062
PriyankaRanganath <69572399+PriyankaRanganath@users.noreply.github.com>
2020-10-05 21:06:48 +0530

    Removed azuread dependency wherever applicable for AAD Diagnostic settings (#8580)
    Co-authored-by: Priyanka Ranganath <pranganath@salesforce.com>
    
    …ting feature
    
    This PR is for removing azuread dependency from azurerm wherever applicable. I have implemented azure AD diagnostic settings feature in azuread provider. But I am unable to test them due to cyclic dependency between azurerm and azuread providers.
    To avoid that, this PR has removed azuread dependencies wherever applicable and provided equivalent functions for the same.

--- azurerm/internal/acceptance/data.go
+++ azurerm/internal/acceptance/data.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/Azure/go-autorest/autorest/azure"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
-	"github.com/terraform-providers/terraform-provider-azuread/azuread/helpers/tf"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/features"
 )
 
