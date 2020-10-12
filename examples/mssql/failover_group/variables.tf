b1ebd035f847af8e37fd13710781c97434f83a11
yupwei68 <yupwei@microsoft.com>
2020-10-12 10:00:56 +0800

    update

---
+++ examples/mssql/failover_group/variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  description = "The Azure location where the primary resources in this example should be created."
+}
+
+variable "location_alt" {
+  description = "The Azure location where the secondary resources in this example should be created."
+}
+
+variable "prefix" {
+  description = "The prefix used for all resources used by this NetApp Account"
+}
