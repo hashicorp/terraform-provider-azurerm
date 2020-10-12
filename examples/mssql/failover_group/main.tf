ee0d589498e9e0ce6b23dbbe0b393d3ce3e2b558
yupwei68 <yupwei@microsoft.com>
2020-10-12 10:03:11 +0800

    update

--- examples/mssql/failover_group/main.tf
+++ examples/mssql/failover_group/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
-resource "azurerm_mssql_server" "example1" {
+resource "azurerm_mssql_server" "example" {
   name                         = "${var.prefix}-server-primary"
   resource_group_name          = azurerm_resource_group.example.name
   location                     = var.location
@@ -16,7 +16,7 @@ resource "azurerm_mssql_server" "example1" {
   administrator_login_password = "thisIsDog11"
 }
 
-resource "azurerm_mssql_server" "example2" {
+resource "azurerm_mssql_server" "secondary" {
   name                         = "${var.prefix}-server-secondary"
   resource_group_name          = azurerm_resource_group.example.name
   location                     = var.location_alt
@@ -27,7 +27,7 @@ resource "azurerm_mssql_server" "example2" {
 
 resource "azurerm_mssql_database" "example" {
   name         = "${var.prefix}-db-primary"
-  server_id    = azurerm_mssql_server.example1.id
+  server_id    = azurerm_mssql_server.example.id
   collation    = "SQL_AltDiction_CP850_CI_AI"
   license_type = "BasePrice"
   sku_name     = "GP_Gen5_2"
@@ -35,7 +35,7 @@ resource "azurerm_mssql_database" "example" {
 
 resource "azurerm_mssql_database" "secondary" {
   name                        = "${var.prefix}-db-secondary"
-  server_id                   = azurerm_mssql_server.example2.id
+  server_id                   = azurerm_mssql_server.secondary.id
   create_mode                 = "Secondary"
   creation_source_database_id = azurerm_mssql_database.example.id
 }
@@ -43,11 +43,11 @@ resource "azurerm_mssql_database" "secondary" {
 resource "azurerm_sql_failover_group" "example" {
   name                = "${var.prefix}-failover-group"
   resource_group_name = azurerm_resource_group.example.name
-  server_name         = azurerm_mssql_server.example1.name
+  server_name         = azurerm_mssql_server.example.name
   databases           = [azurerm_mssql_database.example.id]
 
   partner_servers {
-    id = azurerm_mssql_server.example2.id
+    id = azurerm_mssql_server.secondary.id
   }
 
   read_write_endpoint_failover_policy {
