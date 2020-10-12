9520754e0c1ec6870aba1b6ae8e1e0157972910e
Robert Lippens <rlippens39@gmail.com>
2020-10-12 08:54:11 -0700

    Update azurerm/internal/services/mysql/mysql_server_data_source.go
    Co-authored-by: Tom Bamford <tom@bamford.io>

--- azurerm/internal/services/mysql/mysql_server_data_source.go
+++ azurerm/internal/services/mysql/mysql_server_data_source.go
@@ -190,7 +190,7 @@ func dataSourceArmMySqlServerRead(d *schema.ResourceData, meta interface{}) erro
 	resp, err := client.Get(ctx, resourceGroup, name)
 	if err != nil {
 		if utils.ResponseWasNotFound(resp.Response) {
-			return fmt.Errorf("Error: MySQL server %q in Resource Group %q was not found", name, resourceGroup)
+			return fmt.Errorf("MySQL server %q in Resource Group %q was not found", name, resourceGroup)
 		}
 		return fmt.Errorf("Error making Read request on AzureRM MySQL server %q: %+v", name, err)
 	}
