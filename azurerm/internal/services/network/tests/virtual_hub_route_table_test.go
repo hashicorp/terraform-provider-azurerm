ddafee0e484c152b211c9fe912ffd9c2d063c50a
neil-yechenwei <yechenwei2007@126.com>
2020-10-19 20:18:09 +0800

    update code

--- azurerm/internal/services/network/tests/virtual_hub_route_table_test.go
+++ azurerm/internal/services/network/tests/virtual_hub_route_table_test.go
@@ -2,13 +2,13 @@ package tests
 
 import (
 	"fmt"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/network/parse"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/network/parse"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
