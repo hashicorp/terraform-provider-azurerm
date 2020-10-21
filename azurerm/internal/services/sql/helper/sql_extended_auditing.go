fbb80eb21913329eee1c09672aaa79999ad198ac
tombuildsstuff <git@ibuildstuff.co.uk>
2020-10-21 16:33:39 +0200

    r/sql_database: working around a broken API
    works around https://github.com/Azure/azure-rest-api-specs/issues/11271

--- azurerm/internal/services/sql/helper/sql_extended_auditing.go
+++ azurerm/internal/services/sql/helper/sql_extended_auditing.go
@@ -108,6 +108,10 @@ func ExpandAzureRmSqlDBBlobAuditingPolicies(input []interface{}) *sql.ExtendedDa
 	if len(input) == 0 {
 		return &sql.ExtendedDatabaseBlobAuditingPolicyProperties{
 			State: sql.BlobAuditingPolicyStateDisabled,
+
+			// NOTE: this works around a regression in the Azure API detailed here:
+			// https://github.com/Azure/azure-rest-api-specs/issues/11271
+			IsAzureMonitorTargetEnabled: utils.Bool(true),
 		}
 	}
 	dbBlobAuditingPolicies := input[0].(map[string]interface{})
