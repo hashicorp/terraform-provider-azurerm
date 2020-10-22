310bb1bb0e4ae9503035e86766aa1c0657f3a049
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2020-10-22 15:38:23 -0700

    Initial checkin of data export

---
+++ azurerm/internal/services/loganalytics/log_analytics_data_export_resource.go
@@ -0,0 +1,199 @@
+package loganalytics
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/services/preview/operationalinsights/mgmt/2020-03-01-preview/operationalinsights"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/suppress"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/loganalytics/parse"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/loganalytics/validate"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
+)
+
+func resourceArmLogAnalyticsDataExport() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceArmOperationalinsightsDataExportCreateUpdate,
+		Read:   resourceArmOperationalinsightsDataExportRead,
+		Update: resourceArmOperationalinsightsDataExportCreateUpdate,
+		Delete: resourceArmOperationalinsightsDataExportDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(30 * time.Minute),
+			Read:   schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(30 * time.Minute),
+			Delete: schema.DefaultTimeout(30 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ForceNew:         true,
+				DiffSuppressFunc: suppress.CaseDifference,
+				ValidateFunc:     validate.LogAnalyticsDataExportName,
+			},
+
+			"resource_group_name": azure.SchemaResourceGroupName(),
+
+			"workspace_resource_id": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: azure.ValidateResourceID,
+			},
+
+			"destination_resource_id": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: azure.ValidateResourceID,
+			},
+
+			"enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+
+			"table_names": {
+				Type:     schema.TypeSet,
+				Required: true,
+				MinItems: 1,
+				Elem: &schema.Schema{
+					Type:         schema.TypeString,
+					ValidateFunc: validation.NoZeroValues,
+				},
+			},
+
+			"export_rule_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceArmOperationalinsightsDataExportCreateUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).LogAnalytics.DataExportClient
+	ctx, cancel := timeouts.ForCreateUpdate(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	name := d.Get("name").(string)
+	resourceGroup := d.Get("resource_group_name").(string)
+	workspace, err := parse.LogAnalyticsWorkspaceID(d.Get("workspace_resource_id").(string))
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+
+	if d.IsNewResource() {
+		existing, err := client.Get(ctx, resourceGroup, workspace.Name, name)
+		if err != nil {
+			if !utils.ResponseWasNotFound(existing.Response) {
+				return fmt.Errorf("checking for present of existing Log Analytics Data Export Rule %q (Resource Group %q / workspaceName %q): %+v", name, resourceGroup, workspace.Name, err)
+			}
+		}
+		if existing.ID != nil && *existing.ID != "" {
+			return tf.ImportAsExistsError("azurerm_log_analytics_data_export_rule", *existing.ID)
+		}
+	}
+
+	destination := &operationalinsights.Destination{
+		ResourceID: utils.String(d.Get("destination_resource_id").(string)),
+	}
+
+	parameters := operationalinsights.DataExport{
+		DataExportProperties: &operationalinsights.DataExportProperties{
+			Destination: destination,
+			TableNames:  utils.ExpandStringSlice(d.Get("table_names").(*schema.Set).List()),
+			Enable:      utils.Bool(d.Get("enabled").(bool)),
+		},
+	}
+
+	if _, err := client.CreateOrUpdate(ctx, resourceGroup, workspace.Name, name, parameters); err != nil {
+		return fmt.Errorf("creating/updating Log Analytics Data Export Rule %q (Resource Group %q / workspaceName %q): %+v", name, resourceGroup, workspace.Name, err)
+	}
+
+	resp, err := client.Get(ctx, resourceGroup, workspace.Name, name)
+	if err != nil {
+		return fmt.Errorf("retrieving Log Analytics Data Export Rule %q (Resource Group %q / workspaceName %q): %+v", name, resourceGroup, workspace.Name, err)
+	}
+
+	if resp.ID == nil || *resp.ID == "" {
+		return fmt.Errorf("empty or nil ID returned for Log Analytics Data Export Rule %q (Resource Group %q / workspaceName %q) ID", name, resourceGroup, workspace.Name)
+	}
+
+	d.SetId(*resp.ID)
+	return resourceArmOperationalinsightsDataExportRead(d, meta)
+}
+
+func resourceArmOperationalinsightsDataExportRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).LogAnalytics.DataExportClient
+	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	id, err := parse.LogAnalyticsDataExportID(d.Id())
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Get(ctx, id.ResourceGroup, id.WorkspaceName, id.Name)
+	if err != nil {
+		if utils.ResponseWasNotFound(resp.Response) {
+			log.Printf("[INFO] Log Analytics %q does not exist - removing from state", d.Id())
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("retrieving Log Analytics Data Export Rule %q (Resource Group %q / workspaceName %q): %+v", id.Name, id.ResourceGroup, id.WorkspaceName, err)
+	}
+	d.Set("name", id.Name)
+	d.Set("resource_group_name", id.ResourceGroup)
+	d.Set("workspace_resource_id", id.WorkspaceID)
+	if props := resp.DataExportProperties; props != nil {
+		d.Set("export_rule_id", props.DataExportID)
+		d.Set("destination_resource_id", flattenArmDataExportDestination(props.Destination))
+		d.Set("enabled", props.Enable)
+		if tableNames := props.TableNames; tableNames != nil {
+			d.Set("table_names", utils.FlattenStringSlice(tableNames))
+		}
+	}
+	return nil
+}
+
+func resourceArmOperationalinsightsDataExportDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).LogAnalytics.DataExportClient
+	ctx, cancel := timeouts.ForDelete(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	id, err := parse.LogAnalyticsDataExportID(d.Id())
+	if err != nil {
+		return err
+	}
+
+	if _, err := client.Delete(ctx, id.ResourceGroup, id.WorkspaceName, id.Name); err != nil {
+		return fmt.Errorf("deleting Log Analytics Data Export Rule %q (Resource Group %q / workspaceName %q): %+v", id.Name, id.ResourceGroup, id.WorkspaceName, err)
+	}
+	return nil
+}
+
+func flattenArmDataExportDestination(input *operationalinsights.Destination) string {
+	if input == nil {
+		return ""
+	}
+
+	var resourceID string
+	if input.ResourceID != nil {
+		resourceID = *input.ResourceID
+	}
+
+	return resourceID
+}
---
+++ azurerm/internal/services/loganalytics/tests/log_analytics_data_export_resource_test.go
@@ -0,0 +1,273 @@
+package tests
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/loganalytics/parse"
+)
+
+func TestAccAzureRMLogAnalyticsDataExportRule_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_log_analytics_data_export_rule", "test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		Providers:    acceptance.SupportedProviders,
+		CheckDestroy: testCheckAzureRMLogAnalyticsDataExportRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMLogAnalyticsDataExportRule_basic(data),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMLogAnalyticsDataExportRuleExists(data.ResourceName),
+				),
+				ExpectNonEmptyPlan: true, // Due to API changing case of attributes you need to ignore a non-empty plan for this resource
+			},
+			data.ImportStep(),
+		},
+	})
+}
+
+func TestAccAzureRMLogAnalyticsDataExportRule_requiresImport(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_log_analytics_data_export_rule", "test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		Providers:    acceptance.SupportedProviders,
+		CheckDestroy: testCheckAzureRMLogAnalyticsDataExportRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMLogAnalyticsDataExportRule_basicLower(data),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMLogAnalyticsDataExportRuleExists(data.ResourceName),
+				),
+				ExpectNonEmptyPlan: true, // Due to API changing case of attributes you need to ignore a non-empty plan for this resource
+			},
+			{
+				Config:             testAccAzureRMLogAnalyticsDataExportRule_requiresImport(data),
+				ExpectNonEmptyPlan: true, // Due to API changing case of attributes you need to ignore a non-empty plan for this resource
+				ExpectError:        acceptance.RequiresImportError("azurerm_log_analytics_data_export_rule"),
+			},
+		},
+	})
+}
+
+func TestAccAzureRMLogAnalyticsDataExportRule_update(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_log_analytics_data_export_rule", "test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		Providers:    acceptance.SupportedProviders,
+		CheckDestroy: testCheckAzureRMLogAnalyticsDataExportRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMLogAnalyticsDataExportRule_basic(data),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMLogAnalyticsDataExportRuleExists(data.ResourceName),
+				),
+				ExpectNonEmptyPlan: true, // Due to API changing case of attributes you need to ignore a non-empty plan for this resource
+			},
+			data.ImportStep(),
+			{
+				Config: testAccAzureRMLogAnalyticsDataExportRule_update(data),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMLogAnalyticsDataExportRuleExists(data.ResourceName),
+				),
+				ExpectNonEmptyPlan: true, // Due to API changing case of attributes you need to ignore a non-empty plan for this resource
+			},
+			data.ImportStep(),
+		},
+	})
+}
+
+func TestAccAzureRMLogAnalyticsDataExportRule_complete(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_log_analytics_data_export_rule", "test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		Providers:    acceptance.SupportedProviders,
+		CheckDestroy: testCheckAzureRMLogAnalyticsDataExportRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMLogAnalyticsDataExportRule_complete(data),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMLogAnalyticsDataExportRuleExists(data.ResourceName),
+				),
+				ExpectNonEmptyPlan: true, // Due to API changing case of attributes you need to ignore a non-empty plan for this resource
+			},
+			data.ImportStep(),
+		},
+	})
+}
+
+func testCheckAzureRMLogAnalyticsDataExportRuleDestroy(s *terraform.State) error {
+	conn := acceptance.AzureProvider.Meta().(*clients.Client).LogAnalytics.DataExportClient
+	ctx := acceptance.AzureProvider.Meta().(*clients.Client).StopContext
+	return nil
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "azurerm_log_analytics_data_export_rule" {
+			continue
+		}
+
+		name := rs.Primary.Attributes["name"]
+		resourceGroup := rs.Primary.Attributes["resource_group_name"]
+
+		workspace, err := parse.LogAnalyticsWorkspaceID(rs.Primary.Attributes["workspace_resource_id"])
+		if err != nil {
+			return nil
+		}
+
+		resp, err := conn.Get(ctx, resourceGroup, workspace.Name, name)
+		if err != nil {
+			return nil
+		}
+
+		if resp.StatusCode != http.StatusNotFound {
+			return fmt.Errorf("Log Analytics Data Export Rule still exists:\n%#v", resp)
+		}
+	}
+
+	return nil
+}
+
+func testCheckAzureRMLogAnalyticsDataExportRuleExists(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		conn := acceptance.AzureProvider.Meta().(*clients.Client).LogAnalytics.DataExportClient
+		ctx := acceptance.AzureProvider.Meta().(*clients.Client).StopContext
+
+		return nil
+		// Ensure we have enough information in state to look up in API
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("Not found: %s", resourceName)
+		}
+
+		name := rs.Primary.Attributes["name"]
+		resourceGroup, hasResourceGroup := rs.Primary.Attributes["resource_group_name"]
+		if !hasResourceGroup {
+			return fmt.Errorf("Bad: no resource group found in state for Log Analytics Data Export Rule: %q", name)
+		}
+
+		workspace, err := parse.LogAnalyticsWorkspaceID(rs.Primary.Attributes["workspace_resource_id"])
+		if err != nil {
+			return fmt.Errorf("Bad: unable to access 'workspace_resource_id' for Log Analytics Data Export Rule: %q", name)
+		}
+
+		resp, err := conn.Get(ctx, resourceGroup, workspace.Name, name)
+		if err != nil {
+			return fmt.Errorf("Bad: Get on Log Analytics Data Export Rule Client: %+v", err)
+		}
+
+		if resp.StatusCode == http.StatusNotFound {
+			return fmt.Errorf("Bad: Log Analytics Data Export Rule %q (resource group: %q) does not exist", name, resourceGroup)
+		}
+
+		return nil
+	}
+}
+
+func testAccAzureRMLogAnalyticsDataExportRule_template(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-lader-%d"
+  location = "%s"
+}
+
+resource "azurerm_log_analytics_workspace" "test" {
+  name                = "acctestLAW-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_storage_account" "test" {
+  name                = "acctestsads%s"
+  resource_group_name = azurerm_resource_group.test.name
+
+  location                 = azurerm_resource_group.test.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomString)
+}
+
+func testAccAzureRMLogAnalyticsDataExportRule_basic(data acceptance.TestData) string {
+	template := testAccAzureRMLogAnalyticsDataExportRule_template(data)
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_log_analytics_data_export_rule" "test" {
+  name                    = "acctest-DER-%d"
+  resource_group_name     = azurerm_resource_group.test.name
+  workspace_resource_id   = azurerm_log_analytics_workspace.test.id
+  destination_resource_id = azurerm_storage_account.test.id
+  table_names             = ["Heartbeat"]
+}
+`, template, data.RandomInteger)
+}
+
+// I have to make this a lower case to get the requiresImport test to pass since the RP lowercases everything when it sends the data back to you
+func testAccAzureRMLogAnalyticsDataExportRule_basicLower(data acceptance.TestData) string {
+	template := testAccAzureRMLogAnalyticsDataExportRule_template(data)
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_log_analytics_data_export_rule" "test" {
+  name                    = "acctest-der-%d"
+  resource_group_name     = azurerm_resource_group.test.name
+  workspace_resource_id   = azurerm_log_analytics_workspace.test.id
+  destination_resource_id = azurerm_storage_account.test.id
+  table_names             = ["Heartbeat"]
+}
+`, template, data.RandomInteger)
+}
+
+func testAccAzureRMLogAnalyticsDataExportRule_requiresImport(data acceptance.TestData) string {
+	template := testAccAzureRMLogAnalyticsDataExportRule_basicLower(data)
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_log_analytics_data_export_rule" "import" {
+  name                    = azurerm_log_analytics_data_export_rule.test.name
+  resource_group_name     = azurerm_resource_group.test.name
+  workspace_resource_id   = azurerm_log_analytics_workspace.test.id
+  destination_resource_id = azurerm_storage_account.test.id
+  table_names             = ["Heartbeat"]
+}
+`, template)
+}
+
+func testAccAzureRMLogAnalyticsDataExportRule_update(data acceptance.TestData) string {
+	template := testAccAzureRMLogAnalyticsDataExportRule_template(data)
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_log_analytics_data_export_rule" "test" {
+  name                    = "acctest-DER-%d"
+  resource_group_name     = azurerm_resource_group.test.name
+  workspace_resource_id   = azurerm_log_analytics_workspace.test.id
+  destination_resource_id = azurerm_storage_account.test.id
+  table_names             = ["Heartbeat", "Event"]
+}
+`, template, data.RandomInteger)
+}
+
+func testAccAzureRMLogAnalyticsDataExportRule_complete(data acceptance.TestData) string {
+	template := testAccAzureRMLogAnalyticsDataExportRule_template(data)
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_log_analytics_data_export_rule" "test" {
+  name                    = "acctest-DER-%d"
+  resource_group_name     = azurerm_resource_group.test.name
+  workspace_resource_id   = azurerm_log_analytics_workspace.test.id
+  destination_resource_id = azurerm_storage_account.test.id
+  table_names             = ["Heartbeat"]
+  enabled                 = true
+}
+`, template, data.RandomInteger)
+}
