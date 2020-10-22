d179fe9122c36b0b9778ef8c20871a1f97f41176
magodo <wztdyl@sina.com>
2020-10-22 10:39:55 +0800

    add more test

--- azurerm/internal/services/cdn/tests/cdn_endpoint_resource_test.go
+++ azurerm/internal/services/cdn/tests/cdn_endpoint_resource_test.go
@@ -364,6 +364,25 @@ func TestAccAzureRMCdnEndpoint_dnsAlias(t *testing.T) {
 	})
 }
 
+func TestAccAzureRMCdnEndpoint_deliveryRuleOptionalMatchValue(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_cdn_endpoint", "test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acceptance.PreCheck(t) },
+		Providers:    acceptance.SupportedProviders,
+		CheckDestroy: testCheckAzureRMCdnEndpointDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMCdnEndpoint_deliveryRuleOptionalMatchValue(data),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMCdnEndpointExists(data.ResourceName),
+				),
+			},
+			data.ImportStep(),
+		},
+	})
+}
+
 func testCheckAzureRMCdnEndpointExists(resourceName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		conn := acceptance.AzureProvider.Meta().(*clients.Client).Cdn.EndpointsClient
@@ -1174,4 +1193,183 @@ resource "azurerm_dns_a_record" "test" {
   target_resource_id  = azurerm_cdn_endpoint.test.id
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger, data.RandomInteger, data.RandomInteger)
+}
+
+func testAccAzureRMCdnEndpoint_deliveryRuleOptionalMatchValue(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_cdn_profile" "test" {
+  name                = "acctestcdnprof%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "Standard_Microsoft"
+}
+
+resource "azurerm_cdn_endpoint" "test" {
+  name                = "acctestcdnend%d"
+  profile_name        = azurerm_cdn_profile.test.name
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  origin_host_header = "www.contoso.com"
+
+  origin {
+    name       = "acceptanceTestCdnOrigin1"
+    host_name  = "www.contoso.com"
+    https_port = 443
+    http_port  = 80
+  }
+
+  delivery_rule {
+    name  = "cookieCondition"
+    order = 1
+
+    cookies_condition {
+      selector = "abc"
+      operator = "Any"
+    }
+
+    modify_response_header_action {
+      action = "Delete"
+      name   = "Content-Language"
+    }
+  }
+
+  delivery_rule {
+    name  = "postArg"
+    order = 2
+
+    post_arg_condition {
+      selector = "abc"
+      operator = "Any"
+    }
+
+    modify_response_header_action {
+      action = "Delete"
+      name   = "Content-Language"
+    }
+  }
+
+  delivery_rule {
+    name  = "queryString"
+    order = 3
+
+    query_string_condition {
+      operator = "Any"
+    }
+
+    modify_response_header_action {
+      action = "Delete"
+      name   = "Content-Language"
+    }
+  }
+
+  delivery_rule {
+    name  = "remoteAddress"
+    order = 4
+
+    remote_address_condition {
+      operator = "Any"
+    }
+
+    modify_response_header_action {
+      action = "Delete"
+      name   = "Content-Language"
+    }
+  }
+
+  delivery_rule {
+    name  = "requestBody"
+    order = 5
+
+    request_body_condition {
+      operator = "Any"
+    }
+
+    modify_response_header_action {
+      action = "Delete"
+      name   = "Content-Language"
+    }
+  }
+
+  delivery_rule {
+    name  = "requestHeader"
+    order = 6
+
+    request_header_condition {
+      selector = "abc"
+      operator = "Any"
+    }
+
+    modify_response_header_action {
+      action = "Delete"
+      name   = "Content-Language"
+    }
+  }
+
+  delivery_rule {
+    name  = "requestUri"
+    order = 7
+
+    request_uri_condition {
+      operator = "Any"
+    }
+
+    modify_response_header_action {
+      action = "Delete"
+      name   = "Content-Language"
+    }
+  }
+
+  delivery_rule {
+    name  = "uriFileExtension"
+    order = 8
+
+    url_file_extension_condition {
+      operator = "Any"
+    }
+
+    modify_response_header_action {
+      action = "Delete"
+      name   = "Content-Language"
+    }
+  }
+
+  delivery_rule {
+    name  = "uriFileName"
+    order = 9
+
+    url_file_name_condition {
+      operator = "Any"
+    }
+
+    modify_response_header_action {
+      action = "Delete"
+      name   = "Content-Language"
+    }
+  }
+
+  delivery_rule {
+    name  = "uriPath"
+    order = 10
+
+    url_path_condition {
+      operator = "Any"
+    }
+
+    modify_response_header_action {
+      action = "Delete"
+      name   = "Content-Language"
+    }
+  }
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
 }
