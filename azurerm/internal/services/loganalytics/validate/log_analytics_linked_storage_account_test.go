92a2825850bf9a1e1ed06b64639f940eda072dcb
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2020-10-26 18:13:57 -0700

    Fix lint issues

--- azurerm/internal/services/loganalytics/validate/log_analytics_linked_storage_account_test.go
+++ azurerm/internal/services/loganalytics/validate/log_analytics_linked_storage_account_test.go
@@ -6,19 +6,64 @@ import (
 
 func TestLogAnalyticsLinkedStorageAccountWorkspaceName(t *testing.T) {
 	testCases := []struct {
+		Name     string
 		Input    string
 		Expected bool
 	}{
 		{
-			Input:    "your case",
+			Name:     "Too short",
+			Input:    "inv",
+			Expected: false,
+		},
+		{
+			Name:     "Invalid characters underscores",
+			Input:    "invalid_Linked_Storage_Account_Name",
+			Expected: false,
+		},
+		{
+			Name:     "Invalid characters space",
+			Input:    "invalid Linked Storage Account",
+			Expected: false,
+		},
+		{
+			Name:     "Invalid name starts with hyphen",
+			Input:    "-invalidLinkedStorageAccountName",
+			Expected: false,
+		},
+		{
+			Name:     "Invalid name ends with hyphen",
+			Input:    "invalidLinkedStorageAccountName-",
+			Expected: false,
+		},
+		{
+			Name:     "Invalid name too long",
+			Input:    "thisIsToLooooooooooooooooooooooooongForALinkedStorageAccountName",
+			Expected: false,
+		},
+		{
+			Name:     "Valid name",
+			Input:    "validLinkedStorageAccountName",
 			Expected: true,
 		},
 		{
-			Input:    "your case",
-			Expected: false,
+			Name:     "Valid name with hyphen",
+			Input:    "validLinkedStorageAccountName-2",
+			Expected: true,
+		},
+		{
+			Name:     "Valid name max length",
+			Input:    "thisIsTheLoooooooooooongestValidLinkedStorageAccountNameThereIs",
+			Expected: true,
+		},
+		{
+			Name:     "Valid name min length",
+			Input:    "vali",
+			Expected: true,
 		},
 	}
 	for _, v := range testCases {
+		t.Logf("[DEBUG] Testing %q..", v.Name)
+
 		_, errors := LogAnalyticsLinkedStorageAccountWorkspaceName(v.Input, "workspace_name")
 		result := len(errors) == 0
 		if result != v.Expected {
