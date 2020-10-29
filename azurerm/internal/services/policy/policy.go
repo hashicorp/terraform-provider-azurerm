06f14eef0a3f0cb10bac4fb813d9407cd151859c
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2020-10-29 14:30:10 +0000

    changed to not found utils

--- azurerm/internal/services/policy/policy.go
+++ azurerm/internal/services/policy/policy.go
@@ -6,8 +6,9 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
+
 	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2019-09-01/policy"
-	"github.com/Azure/go-autorest/autorest"
 )
 
 func getPolicyDefinitionByDisplayName(ctx context.Context, client *policy.DefinitionsClient, displayName, managementGroupName string) (policy.Definition, error) {
@@ -51,7 +52,7 @@ func getPolicyDefinitionByDisplayName(ctx context.Context, client *policy.Defini
 func getPolicyDefinitionByName(ctx context.Context, client *policy.DefinitionsClient, name string, managementGroupName string) (res policy.Definition, err error) {
 	if managementGroupName == "" {
 		res, err = client.Get(ctx, name)
-		if aerr, ok := err.(autorest.DetailedError); ok && aerr.StatusCode == 404 {
+		if utils.ResponseWasNotFound(res.Response) {
 			res, err = client.GetBuiltIn(ctx, name)
 		}
 	} else {
@@ -64,7 +65,7 @@ func getPolicyDefinitionByName(ctx context.Context, client *policy.DefinitionsCl
 func getPolicySetDefinitionByName(ctx context.Context, client *policy.SetDefinitionsClient, name string, managementGroupID string) (res policy.SetDefinition, err error) {
 	if managementGroupID == "" {
 		res, err = client.Get(ctx, name)
-		if aerr, ok := err.(autorest.DetailedError); ok && aerr.StatusCode == 404 {
+		if utils.ResponseWasNotFound(res.Response) {
 			res, err = client.GetBuiltIn(ctx, name)
 		}
 	} else {
