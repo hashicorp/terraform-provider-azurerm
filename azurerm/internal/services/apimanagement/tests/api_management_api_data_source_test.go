ac54e57f422b52cd3c92827d06fbab328456fe55
Matthew Frahry <mbfrahry@gmail.com>
2020-10-28 20:19:19 -0700

    This is it

--- azurerm/internal/services/apimanagement/tests/api_management_api_data_source_test.go
+++ azurerm/internal/services/apimanagement/tests/api_management_api_data_source_test.go
@@ -11,7 +11,7 @@ import (
 func TestAccDataSourceAzureRMApiManagementApi_basic(t *testing.T) {
 	data := acceptance.BuildTestData(t, "data.azurerm_api_management_api", "test")
 
-	// nolint:staticcheck
+	//lintignore:AT001
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { acceptance.PreCheck(t) },
 		Providers: acceptance.SupportedProviders,
