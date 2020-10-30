9c17642d6a7ea815bfdea70c50d4dc58dd61b442
jackofallops <ste@hashicorp.com>
2020-10-30 08:58:37 +0000

    address R001s

--- azurerm/internal/services/monitor/monitor_metric_alert_resource.go
+++ azurerm/internal/services/monitor/monitor_metric_alert_resource.go
@@ -534,7 +534,7 @@ func resourceArmMonitorMetricAlertRead(d *schema.ResourceData, meta interface{})
 		}
 
 		monitorMetricAlertCriteria := flattenMonitorMetricAlertCriteria(alert.Criteria)
-
+		// lintignore:R001
 		if err := d.Set(criteriaSchema, monitorMetricAlertCriteria); err != nil {
 			return fmt.Errorf("failed setting `%s`: %+v", criteriaSchema, err)
 		}
