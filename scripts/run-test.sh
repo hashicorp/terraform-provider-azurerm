218b25723c8d9f337d3096070139613520a9d12c
jackofallops <ste@hashicorp.com>
2020-11-02 09:40:27 +0000

    update ci scripts to remove travis targetted conditional checks

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,26 +1,12 @@
 #!/usr/bin/env bash
 
-function checkForConditionalRun {
-  if [ "$TRAVIS" == "ci" ];
-  then
-    echo "Checking if this should be conditionally run.."
-    result=$(git diff --name-only origin/master | grep azurerm/)
-    if [ "$result" = "" ];
-    then
-      echo "No changes committed to ./azurerm - nothing to lint - exiting"
-      exit 0
-    fi
-  fi
-}
-
 function runTests {
   echo "==> Running Unit Tests..."
   go test -i $TEST || exit 1
-  go test -v $TEST "$TESTARGS" -timeout=30s -parallel=4
+  go test -v $TEST "$TESTARGS" -timeout=30s -parallel=10
 }
 
 function main {
-  checkForConditionalRun
   runTests
 }
 
