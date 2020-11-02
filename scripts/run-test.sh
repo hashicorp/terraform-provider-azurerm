fc7352298b24e243f5e7d2a7990049a564bc4e4c
jackofallops <ste@hashicorp.com>
2020-11-02 10:21:55 +0000

    up unit test parallelism to 20

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -3,7 +3,7 @@
 function runTests {
   echo "==> Running Unit Tests..."
   go test -i $TEST || exit 1
-  go test -v $TEST "$TESTARGS" -timeout=30s -parallel=10
+  go test -v $TEST "$TESTARGS" -timeout=30s -parallel=20
 }
 
 function main {
