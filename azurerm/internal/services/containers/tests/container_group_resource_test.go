231e17fcb6b3505d1ddcc547ed53e2370a55bcf2
Sebastien Boury <sebastien.boury@gmail.com>
2020-11-02 09:32:19 +0100

    #5585 : support secret volumes in azurerm_container_group

--- azurerm/internal/services/containers/tests/container_group_resource_test.go
+++ azurerm/internal/services/containers/tests/container_group_resource_test.go
@@ -465,6 +465,25 @@ func TestAccAzureRMContainerGroup_gitRepoVolume(t *testing.T) {
 	})
 }
 
+func TestAccAzureRMContainerGroup_secretVolume(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_container_group", "test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { acceptance.PreCheck(t) },
+		Providers:    acceptance.SupportedProviders,
+		CheckDestroy: testCheckAzureRMContainerGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMContainerGroup_secretVolume(data),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMContainerGroupExists(data.ResourceName),
+				),
+			},
+			data.ImportStep("container.0.volume.0.secret"),
+		},
+	})
+}
+
 func testAccAzureRMContainerGroup_SystemAssignedIdentity(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
@@ -1307,6 +1326,52 @@ resource "azurerm_container_group" "test" {
   }
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
+}
+
+func testAccAzureRMContainerGroup_secretVolume(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_container_group" "test" {
+  name                = "acctestcontainergroup-%d"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  ip_address_type     = "public"
+  os_type             = "Linux"
+
+  container {
+    name   = "hw"
+    image  = "microsoft/aci-helloworld:latest"
+    cpu    = "0.5"
+    memory = "0.5"
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+
+    volume {
+      name       = "config"
+      mount_path = "/var/config"
+
+      secret = {
+        mysecret1 = "TXkgZmlyc3Qgc2VjcmV0IEZPTwo="
+        mysecret2 = "TXkgc2Vjb25kIHNlY3JldCBCQVIK"
+      }
+    }
+  }
+
+  tags = {
+    environment = "Testing"
+  }
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
 }
 
 func testCheckAzureRMContainerGroupExists(resourceName string) resource.TestCheckFunc {
