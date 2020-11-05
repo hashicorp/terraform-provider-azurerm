248d0001c4e275f41525cea9c6fa7a08c14f531b
jackofallops <ste@hashicorp.com>
2020-11-05 08:14:08 +0000

    rename var in parse to match resource

--- azurerm/internal/services/loganalytics/parse/log_analytics_storage_insights.go
+++ azurerm/internal/services/loganalytics/parse/log_analytics_storage_insights.go
@@ -19,17 +19,17 @@ func LogAnalyticsStorageInsightsID(input string) (*LogAnalyticsStorageInsightsId
 		return nil, fmt.Errorf("parsing Log Analytics Storage Insights ID %q: %+v", input, err)
 	}
 
-	logAnalyticsStorageInsightConfig := LogAnalyticsStorageInsightsId{
+	logAnalyticsStorageInsight := LogAnalyticsStorageInsightsId{
 		ResourceGroup: id.ResourceGroup,
 	}
-	if logAnalyticsStorageInsightConfig.WorkspaceName, err = id.PopSegment("workspaces"); err != nil {
+	if logAnalyticsStorageInsight.WorkspaceName, err = id.PopSegment("workspaces"); err != nil {
 		return nil, err
 	}
-	if logAnalyticsStorageInsightConfig.WorkspaceID = fmt.Sprintf("/subscriptions/%s/resourcegroups/%s/providers/%s/workspaces/%s", id.SubscriptionID, id.ResourceGroup, id.Provider, logAnalyticsStorageInsightConfig.WorkspaceName); err != nil {
+	if logAnalyticsStorageInsight.WorkspaceID = fmt.Sprintf("/subscriptions/%s/resourcegroups/%s/providers/%s/workspaces/%s", id.SubscriptionID, id.ResourceGroup, id.Provider, logAnalyticsStorageInsight.WorkspaceName); err != nil {
 		return nil, fmt.Errorf("formatting Log Analytics Storage Insights workspace ID %q", input)
 	}
-	if logAnalyticsStorageInsightConfig.Name, err = id.PopSegment("storageInsightConfigs"); err != nil {
-		if logAnalyticsStorageInsightConfig.Name, err = id.PopSegment("storageinsightconfigs"); err != nil {
+	if logAnalyticsStorageInsight.Name, err = id.PopSegment("storageInsightConfigs"); err != nil {
+		if logAnalyticsStorageInsight.Name, err = id.PopSegment("storageinsightconfigs"); err != nil {
 			return nil, err
 		}
 	}
@@ -37,5 +37,5 @@ func LogAnalyticsStorageInsightsID(input string) (*LogAnalyticsStorageInsightsId
 		return nil, err
 	}
 
-	return &logAnalyticsStorageInsightConfig, nil
+	return &logAnalyticsStorageInsight, nil
 }
