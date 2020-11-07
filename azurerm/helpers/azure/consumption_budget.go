b1b08e4d37edc95158e57d62f397cdddec629896
Marc Sensenich <sensenichm91@gmail.com>
2020-11-07 14:56:20 -0500

    Add DiffSuppressFunc for Azure Consumption time_period.end_date

--- azurerm/helpers/azure/consumption_budget.go
+++ azurerm/helpers/azure/consumption_budget.go
@@ -256,3 +256,11 @@ func FlattenAzureConsumptionBudgetFilter(input *consumption.Filters) []interface
 
 	return filters
 }
+
+// DiffSuppressFuncs
+func DiffSuppressFuncAzureConsumptionBudgetTimePeriodEndDate(k, old, new string, d *schema.ResourceData) bool {
+	// If the end date is not set by the user, Azure defaults this to 10 years
+	// from the start date. Therefore, the diff is suppressed if the user didn't
+	// set the value previously or in the current config
+	return new == ""
+}
