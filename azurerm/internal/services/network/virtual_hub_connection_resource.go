28f8135e626da161fae66576278841d6388fa081
neil-yechenwei <yechenwei2007@126.com>
2020-11-11 12:56:02 +0800

    update code

--- azurerm/internal/services/network/virtual_hub_connection_resource.go
+++ azurerm/internal/services/network/virtual_hub_connection_resource.go
@@ -8,7 +8,6 @@ import (
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2020-05-01/network"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/locks"
