1f2749385459ee08df8c0283f1aeeed3edc8105c
kt <kt@katbyte.me>
2020-11-12 13:39:55 -0800

    datasource

--- azurerm/internal/services/appconfiguration/app_configuration_data_source_test.go
+++ azurerm/internal/services/appconfiguration/app_configuration_data_source_test.go
@@ -6,46 +6,45 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance/check"
 )
+
+type AppConfigurationDataSource struct {
+}
 
 func TestAccAppConfigurationDataSource_basic(t *testing.T) {
 	data := acceptance.BuildTestData(t, "data.azurerm_app_configuration", "test")
 
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:     func() { acceptance.PreCheck(t) },
-		Providers:    acceptance.SupportedProviders,
-		CheckDestroy: testCheckAppConfigurationDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAppConfigurationResource_standard(data),
-			},
-			{
-				Config: testAppConfigurationDataSource_basic(data),
-				Check: resource.ComposeTestCheckFunc(
-					testCheckAppConfigurationExists(data.ResourceName),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "endpoint"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "location"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "sku"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "primary_read_key.0.connection_string"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "primary_read_key.0.id"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "primary_read_key.0.secret"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "primary_write_key.0.connection_string"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "primary_write_key.0.id"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "primary_write_key.0.secret"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "secondary_read_key.0.connection_string"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "secondary_read_key.0.id"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "secondary_read_key.0.secret"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "secondary_write_key.0.connection_string"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "secondary_write_key.0.id"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "secondary_write_key.0.secret"),
-				),
-			},
+	data.DataSourceTest(t, []resource.TestStep{
+		{
+			Config: AppConfigurationResource{}.standard(data),
+		},
+		{
+			Config: AppConfigurationDataSource{}.basic(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("endpoint").Exists(),
+				check.That(data.ResourceName).Key("location").Exists(),
+				check.That(data.ResourceName).Key("sku").Exists(),
+				check.That(data.ResourceName).Key("primary_read_key.0.connection_string").Exists(),
+				check.That(data.ResourceName).Key("primary_read_key.0.id").Exists(),
+				check.That(data.ResourceName).Key("primary_read_key.0.secret").Exists(),
+				check.That(data.ResourceName).Key("primary_write_key.0.connection_string").Exists(),
+				check.That(data.ResourceName).Key("primary_write_key.0.id").Exists(),
+				check.That(data.ResourceName).Key("primary_write_key.0.secret").Exists(),
+				check.That(data.ResourceName).Key("secondary_read_key.0.connection_string").Exists(),
+				check.That(data.ResourceName).Key("secondary_read_key.0.id").Exists(),
+				check.That(data.ResourceName).Key("secondary_read_key.0.secret").Exists(),
+				check.That(data.ResourceName).Key("secondary_write_key.0.connection_string").Exists(),
+				check.That(data.ResourceName).Key("secondary_write_key.0.id").Exists(),
+				check.That(data.ResourceName).Key("secondary_write_key.0.secret").Exists(),
+			),
 		},
 	})
 }
 
-func testAppConfigurationDataSource_basic(data acceptance.TestData) string {
-	template := testAppConfigurationResource_standard(data)
+func (AppConfigurationDataSource) basic(data acceptance.TestData) string {
+	template := AppConfigurationResource{}.standard(data)
 	return fmt.Sprintf(`
 %s
 
