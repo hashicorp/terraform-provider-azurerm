aef4eb970b2650d9f1ccc5340c9b1e4d554d0e4d
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-12 11:02:28 +0100

    r/storage_queue: moving the validation function out

--- azurerm/internal/services/storage/resource_arm_storage_queue.go
+++ azurerm/internal/services/storage/resource_arm_storage_queue.go
@@ -3,12 +3,12 @@ package storage
 import (
 	"fmt"
 	"log"
-	"regexp"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/storage/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 	"github.com/tombuildsstuff/giovanni/storage/2019-12-12/queue/queues"
@@ -38,7 +38,7 @@ func resourceArmStorageQueue() *schema.Resource {
 				Type:         schema.TypeString,
 				Required:     true,
 				ForceNew:     true,
-				ValidateFunc: ValidateArmStorageQueueName,
+				ValidateFunc: validate.StorageQueueName,
 			},
 
 			"storage_account_name": {
@@ -51,35 +51,6 @@ func resourceArmStorageQueue() *schema.Resource {
 			"metadata": MetaDataSchema(),
 		},
 	}
-}
-
-func ValidateArmStorageQueueName(v interface{}, k string) (warnings []string, errors []error) {
-	value := v.(string)
-
-	if !regexp.MustCompile(`^[a-z0-9-]+$`).MatchString(value) {
-		errors = append(errors, fmt.Errorf(
-			"only lowercase alphanumeric characters and hyphens allowed in %q", k))
-	}
-
-	if regexp.MustCompile(`^-`).MatchString(value) {
-		errors = append(errors, fmt.Errorf("%q cannot start with a hyphen", k))
-	}
-
-	if regexp.MustCompile(`-$`).MatchString(value) {
-		errors = append(errors, fmt.Errorf("%q cannot end with a hyphen", k))
-	}
-
-	if len(value) > 63 {
-		errors = append(errors, fmt.Errorf(
-			"%q cannot be longer than 63 characters", k))
-	}
-
-	if len(value) < 3 {
-		errors = append(errors, fmt.Errorf(
-			"%q must be at least 3 characters", k))
-	}
-
-	return warnings, errors
 }
 
 func resourceArmStorageQueueCreate(d *schema.ResourceData, meta interface{}) error {
