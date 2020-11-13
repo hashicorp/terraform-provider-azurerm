6304b0dde1e00aeaa01e291bb87458e6377434ca
neil-yechenwei <yechenwei2007@126.com>
2020-11-14 07:40:03 +0800

    upadte code

--- azurerm/internal/services/privatedns/tests/private_dns_zone_resource_test.go
+++ azurerm/internal/services/privatedns/tests/private_dns_zone_resource_test.go
@@ -239,12 +239,12 @@ resource "azurerm_private_dns_zone" "test" {
   resource_group_name = azurerm_resource_group.test.name
 
   soa_record {
-    email         = "testemail.com"
-    expire_time   = 2419200
-    minimum_ttl   = 200
-    refresh_time  = 2600
-    retry_time    = 200
-    ttl           = 100
+    email        = "testemail.com"
+    expire_time  = 2419200
+    minimum_ttl  = 200
+    refresh_time = 2600
+    retry_time   = 200
+    ttl          = 100
 
     tags = {
       ENv = "Test"
