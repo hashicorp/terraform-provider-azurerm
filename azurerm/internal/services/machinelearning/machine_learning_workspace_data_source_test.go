57f51e050667195f0fd47522f2825abbf1beb772
Dapeng Zhang <dapzhang@microsoft.com>
2020-11-17 12:02:26 +0800

    Migrate acceptance tests of ML

---
+++ azurerm/internal/services/machinelearning/machine_learning_workspace_data_source_test.go
@@ -0,0 +1,44 @@
+package machinelearning_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance/check"
+)
+
+type WorkspaceDataSource struct{}
+
+func TestAccDataSourceAzureRMMachineLearningWorkspace_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "data.azurerm_machine_learning_workspace", "test")
+	d := WorkspaceDataSource{}
+
+	data.DataSourceTest(t, []resource.TestStep{
+		{
+			Config: d.basic(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).Key("application_insights_id").Exists(),
+				check.That(data.ResourceName).Key("key_vault_id").Exists(),
+				check.That(data.ResourceName).Key("storage_account_id").Exists(),
+				check.That(data.ResourceName).Key("identity.#").HasValue("1"),
+				check.That(data.ResourceName).Key("identity.0.type").HasValue("SystemAssigned"),
+				check.That(data.ResourceName).Key("identity.0.principal_id").Exists(),
+				check.That(data.ResourceName).Key("identity.0.tenant_id").Exists(),
+			),
+		},
+	})
+}
+
+func (d WorkspaceDataSource) basic(data acceptance.TestData) string {
+	config := WorkspaceResource{}.basic(data)
+	return fmt.Sprintf(`
+%s
+
+data "azurerm_machine_learning_workspace" "test" {
+  name                = azurerm_machine_learning_workspace.test.name
+  resource_group_name = azurerm_machine_learning_workspace.test.resource_group_name
+}
+`, config)
+}
