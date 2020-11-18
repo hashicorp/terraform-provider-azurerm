b399fc573c72fd191bf32364998bd63c2e56ea05
Adam Coulter <adam@Adams-MacBook-Pro.local>
2020-11-18 19:22:44 +1100

    update dependencies

--- azurerm/internal/services/web/resource_arm_app_service.go
+++ azurerm/internal/services/web/resource_arm_app_service.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/services/web/mgmt/2019-08-01/web"
+	"github.com/Azure/azure-sdk-for-go/services/web/mgmt/2020-06-01/web"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
