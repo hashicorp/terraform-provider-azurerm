4b4fb403201240e23974416666782d3e2eb84b27
Etienne Deneuve <etienne@simplified.fr>
2020-11-19 11:08:20 +0100

    M azurerm/internal/services/network/virtual_wan_data_source.go Edit#    14237

--- azurerm/internal/services/network/virtual_wan_data_source.go
+++ azurerm/internal/services/network/virtual_wan_data_source.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2020-05-01/network"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
@@ -24,8 +25,9 @@ func dataSourceArmVirtualWan() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
 			},
 			"resource_group_name": azure.SchemaResourceGroupNameForDataSource(),
 
@@ -82,6 +84,9 @@ func dataSourceArmVirtualWanRead(d *schema.ResourceData, meta interface{}) error
 		return fmt.Errorf("Error reading Virtual Wan %q (Resource Group %q): %+v", name, resourceGroup, err)
 	}
 
+	if resp.ID == nil || *resp.ID == "" {
+		return fmt.Errorf("API returns a nil/empty id on Virtual Wan %q (resource group %q): %+v", name, resourceGroup, err)
+	}
 	d.SetId(*resp.ID)
 
 	d.Set("name", resp.Name)
@@ -94,19 +99,35 @@ func dataSourceArmVirtualWanRead(d *schema.ResourceData, meta interface{}) error
 	if props := resp.VirtualWanProperties; props != nil {
 		log.Printf("[DEBUG] ETIENNE ETIENNE ETIENNE %+v", props)
 		if abtbt := *props.AllowBranchToBranchTraffic; props.AllowBranchToBranchTraffic != nil {
-			d.Set("allow_branch_to_branch_traffic", abtbt)
+			if err := d.Set("allow_branch_to_branch_traffic", abtbt); err != nil {
+				return fmt.Errorf("error setting `allow_branch_to_branch_traffic`: %v", err)
+			}
 		} else {
-			d.Set("allow_branch_to_branch_traffic", &b)
+			if err := d.Set("allow_branch_to_branch_traffic", &b); err != nil {
+				return fmt.Errorf("error setting `allow_branch_to_branch_traffic`: %v", err)
+			}
 		}
 		if dve := *props.DisableVpnEncryption; props.DisableVpnEncryption != nil {
-			d.Set("disable_vpn_encryption", dve)
+			if err := d.Set("disable_vpn_encryption", dve); err != nil {
+				return fmt.Errorf("error setting `disable_vpn_encryption`: %v", err)
+			}
 		} else {
-			d.Set("disable_vpn_encryption", &b)
+			if err := d.Set("disable_vpn_encryption", &b); err != nil {
+				return fmt.Errorf("error setting `disable_vpn_encryption`: %v", err)
+			}
 		}
-		d.Set("office365_local_breakout_category", props.Office365LocalBreakoutCategory)
-		d.Set("sku", props.Type)
-		d.Set("virtual_hubs", flattenVirtualWanProperties(props.VirtualHubs))
-		d.Set("vpn_sites", flattenVirtualWanProperties(props.VpnSites))
+		if err := d.Set("office365_local_breakout_category", props.Office365LocalBreakoutCategory); err != nil {
+			return fmt.Errorf("error setting `office365_local_breakout_category`: %v", err)
+		}
+		if err := d.Set("sku", props.Type); err != nil {
+			return fmt.Errorf("error setting `sku`: %v", err)
+		}
+		if err := d.Set("virtual_hubs", flattenVirtualWanProperties(props.VirtualHubs)); err != nil {
+			return fmt.Errorf("error setting `virtual_hubs`: %v", err)
+		}
+		if err := d.Set("vpn_sites", flattenVirtualWanProperties(props.VpnSites)); err != nil {
+			return fmt.Errorf("error setting `vpn_sites`: %v", err)
+		}
 	}
 
 	return tags.FlattenAndSet(d, resp.Tags)
--- website/docs/d/virtual_wan.html.markdown
+++ website/docs/d/virtual_wan.html.markdown
@@ -21,42 +21,77 @@ data "azurerm_virtual_wan" "example" {
 output "id" {
   value = data.azurerm_virtual_wan.example.id
 }
+
+output "allow_branch_to_branch_traffic" {
+  value = data.azurerm_virtual_wan.example.allow_branch_to_branch_traffic
+}
+
+output "disable_vpn_encryption" {
+  value = data.azurerm_virtual_wan.exemple.disable_vpn_encryption
+}
+
+output "location" {
+  value = data.azurerm_virtual_wan.exemple.location
+}
+
+output "office365_local_breakout_category" {
+  value = data.azurerm_virtual_wan.exemple.office365_local_breakout_category
+}
+
+output "sku" {
+  value = data.azurerm_virtual_wan.exemple.sku
+}
+
+output "tags" {
+  value = data.azurerm_virtual_wan.exemple.tags
+}
+
+output "virtual_hubs" {
+  value = data.azurerm_virtual_wan.exemple.virtual_hubs
+}
+
+output "vpn_sites" {
+  value = data.azurerm_virtual_wan.exemple.vpn_sites
+}
+
 ```
 
 ## Arguments Reference
 
 The following arguments are supported:
 
-* `name` - (Required) The name of this Virtual Wan.
+- `name` - (Required) The name of this Virtual Wan.
 
-* `resource_group_name` - (Required) The name of the Resource Group where the Virtual Wan exists.
-
----
-
-* `allow_branch_to_branch_traffic` - (Optional) TODO.
-
-* `disable_vpn_encryption` - (Optional) TODO.
-
-* `office365_local_breakout_category` - (Optional) TODO.
-
-* `virtual_hubs` - (Optional) TODO.
-
-* `vpn_sites` - (Optional) TODO.
+- `resource_group_name` - (Required) The name of the Resource Group where the Virtual Wan exists.
 
 ## Attributes Reference
 
-In addition to the Arguments listed above - the following Attributes are exported: 
+In addition to the Arguments listed above - the following Attributes are exported:
 
-* `id` - The ID of the Virtual Wan.
+- `id` - The ID of the Virtual Wan.
 
-* `location` - The Azure Region where the Virtual Wan exists.
+- `allow_branch_to_branch_traffic` - (Optional) True, if allow branch to branch traffic is allowed.
 
-* `sku` - TODO.
+- `disable_vpn_encryption` - (Optional) True, if vpn encryption is disabled.
 
-* `tags` - A mapping of tags assigned to the Virtual Wan.
+- `location` - The Azure Region where the Virtual Wan exists.
+
+- `office365_local_breakout_category` - (Optional) Possible value are :
+    - `All`
+    - `None`
+    - `Optimize`
+    - `OptimizeAndAllow`.
+
+- `sku` - Type of Virtual Wan (Basic or Standard).
+
+- `tags` - A mapping of tags assigned to the Virtual Wan.
+
+- `virtual_hubs` - (Optional) list of virtual hubs attached.
+
+- `vpn_sites` - (Optional) list of vpn sites attached.
 
 ## Timeouts
 
 The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration/resources.html#timeouts) for certain actions:
 
-* `read` - (Defaults to 5 minutes) Used when retrieving the Virtual Wan.
+- `read` - (Defaults to 5 minutes) Used when retrieving the Virtual Wan.
