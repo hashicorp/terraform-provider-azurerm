12ef335cff5f21ea77297a92256c57a75843b35d
kt <kt@katbyte.me>
2020-11-18 20:32:00 -0800

    Update azurerm/internal/services/compute/virtual_machine_scale_set.go

--- azurerm/internal/services/compute/virtual_machine_scale_set.go
+++ azurerm/internal/services/compute/virtual_machine_scale_set.go
@@ -787,6 +787,8 @@ func VirtualMachineScaleSetDataDiskSchema() *schema.Schema {
 					Computed: true,
 				},
 
+
+				// TODO 3.0 - change this to ultra_ssd_disk_iops_read_write
 				"disk_mbps_read_write": {
 					Type:     schema.TypeInt,
 					Optional: true,
