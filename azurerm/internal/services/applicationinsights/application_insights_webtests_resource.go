4f1faaf37d80a409463742bd33b0132a4a92fa25
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-24 18:55:15 +0100

    r/application_insights_web_test: generating the resource id formatter/parser function

--- azurerm/internal/services/applicationinsights/application_insights_webtests_resource.go
+++ azurerm/internal/services/applicationinsights/application_insights_webtests_resource.go
@@ -16,6 +16,7 @@ import (
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/location"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/applicationinsights/parse"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
+	azSchema "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/suppress"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
@@ -27,9 +28,10 @@ func resourceArmApplicationInsightsWebTests() *schema.Resource {
 		Read:   resourceArmApplicationInsightsWebTestsRead,
 		Update: resourceArmApplicationInsightsWebTestsCreateUpdate,
 		Delete: resourceArmApplicationInsightsWebTestsDelete,
-		Importer: &schema.ResourceImporter{
-			State: schema.ImportStatePassthrough,
-		},
+		Importer: azSchema.ValidateResourceIDPriorToImport(func(id string) error {
+			_, err := parse.WebTestID(id)
+			return err
+		}),
 
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(30 * time.Minute),
