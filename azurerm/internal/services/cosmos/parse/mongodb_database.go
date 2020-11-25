bfb97490678308b95c776044f96e6ef240d2a448
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-25 13:29:42 +0100

    cosmos/mongodb_database: switching to use a generated Resource ID Formatter/Parser

--- azurerm/internal/services/cosmos/parse/mongodb_database.go
+++ azurerm/internal/services/cosmos/parse/mongodb_database.go
@@ -1,34 +1,55 @@
 package parse
 
+// NOTE: this file is generated via 'go:generate' - manual changes will be overwritten
+
 import (
 	"fmt"
 
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 )
 
-type MongoDbDatabaseId struct {
+type MongodbDatabaseId struct {
+	SubscriptionId      string
 	ResourceGroup       string
 	DatabaseAccountName string
 	Name                string
 }
 
-func MongoDbDatabaseID(input string) (*MongoDbDatabaseId, error) {
+func NewMongodbDatabaseID(subscriptionId, resourceGroup, databaseAccountName, name string) MongodbDatabaseId {
+	return MongodbDatabaseId{
+		SubscriptionId:      subscriptionId,
+		ResourceGroup:       resourceGroup,
+		DatabaseAccountName: databaseAccountName,
+		Name:                name,
+	}
+}
+
+func (id MongodbDatabaseId) ID(_ string) string {
+	fmtString := "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.DocumentDB/DatabaseAccounts/%s/mongodbDatabases/%s"
+	return fmt.Sprintf(fmtString, id.SubscriptionId, id.ResourceGroup, id.DatabaseAccountName, id.Name)
+}
+
+func MongodbDatabaseID(input string) (*MongodbDatabaseId, error) {
 	id, err := azure.ParseAzureResourceID(input)
 	if err != nil {
-		return nil, fmt.Errorf("[ERROR] Unable to parse MongoDb Database ID %q: %+v", input, err)
-	}
-
-	mongodbDatabase := MongoDbDatabaseId{
-		ResourceGroup: id.ResourceGroup,
-	}
-
-	if mongodbDatabase.DatabaseAccountName, err = id.PopSegment("databaseAccounts"); err != nil {
 		return nil, err
 	}
 
-	if mongodbDatabase.Name, err = id.PopSegment("mongodbDatabases"); err != nil {
+	resourceId := MongodbDatabaseId{
+		SubscriptionId: id.SubscriptionID,
+		ResourceGroup:  id.ResourceGroup,
+	}
+
+	if resourceId.DatabaseAccountName, err = id.PopSegment("DatabaseAccounts"); err != nil {
+		return nil, err
+	}
+	if resourceId.Name, err = id.PopSegment("mongodbDatabases"); err != nil {
 		return nil, err
 	}
 
-	return &mongodbDatabase, nil
+	if err := id.ValidateNoEmptySegments(input); err != nil {
+		return nil, err
+	}
+
+	return &resourceId, nil
 }
