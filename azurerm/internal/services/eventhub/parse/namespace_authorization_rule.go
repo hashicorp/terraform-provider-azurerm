d7376cae2a2186dc661f521147b5a611d153f352
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-25 17:39:32 +0100

    eventhub: switching to use generated resource id funcs

--- azurerm/internal/services/eventhub/parse/namespace_authorization_rule.go
+++ azurerm/internal/services/eventhub/parse/namespace_authorization_rule.go
@@ -1,11 +1,32 @@
 package parse
 
-import "github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
+// NOTE: this file is generated via 'go:generate' - manual changes will be overwritten
+
+import (
+	"fmt"
+
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
+)
 
 type NamespaceAuthorizationRuleId struct {
+	SubscriptionId        string
 	ResourceGroup         string
 	NamespaceName         string
 	AuthorizationRuleName string
+}
+
+func NewNamespaceAuthorizationRuleID(subscriptionId, resourceGroup, namespaceName, authorizationRuleName string) NamespaceAuthorizationRuleId {
+	return NamespaceAuthorizationRuleId{
+		SubscriptionId:        subscriptionId,
+		ResourceGroup:         resourceGroup,
+		NamespaceName:         namespaceName,
+		AuthorizationRuleName: authorizationRuleName,
+	}
+}
+
+func (id NamespaceAuthorizationRuleId) ID(_ string) string {
+	fmtString := "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.EventHub/namespaces/%s/authorizationRules/%s"
+	return fmt.Sprintf(fmtString, id.SubscriptionId, id.ResourceGroup, id.NamespaceName, id.AuthorizationRuleName)
 }
 
 func NamespaceAuthorizationRuleID(input string) (*NamespaceAuthorizationRuleId, error) {
@@ -14,15 +35,15 @@ func NamespaceAuthorizationRuleID(input string) (*NamespaceAuthorizationRuleId, 
 		return nil, err
 	}
 
-	rule := NamespaceAuthorizationRuleId{
-		ResourceGroup: id.ResourceGroup,
+	resourceId := NamespaceAuthorizationRuleId{
+		SubscriptionId: id.SubscriptionID,
+		ResourceGroup:  id.ResourceGroup,
 	}
 
-	if rule.NamespaceName, err = id.PopSegment("namespaces"); err != nil {
+	if resourceId.NamespaceName, err = id.PopSegment("namespaces"); err != nil {
 		return nil, err
 	}
-
-	if rule.AuthorizationRuleName, err = id.PopSegment("authorizationRules"); err != nil {
+	if resourceId.AuthorizationRuleName, err = id.PopSegment("authorizationRules"); err != nil {
 		return nil, err
 	}
 
@@ -30,5 +51,5 @@ func NamespaceAuthorizationRuleID(input string) (*NamespaceAuthorizationRuleId, 
 		return nil, err
 	}
 
-	return &rule, nil
+	return &resourceId, nil
 }
