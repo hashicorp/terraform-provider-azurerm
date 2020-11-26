62ceffc509245d3a4683640f1ea4a3561544501c
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-26 18:40:48 +0100

    keyvault: generating the `vault` id parser

--- azurerm/internal/services/keyvault/resourceids.go
+++ azurerm/internal/services/keyvault/resourceids.go
@@ -1,1 +1,3 @@
 package keyvault
+
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Vault -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.KeyVault/vaults/vault1
