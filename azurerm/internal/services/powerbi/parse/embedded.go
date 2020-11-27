1f6c0373439a869726cfee20aee9715090590730
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-27 15:49:40 +0100

    powerbi_embedded: switching to use a generated Resource ID func

--- azurerm/internal/services/powerbi/parse/embedded.go
+++ azurerm/internal/services/powerbi/parse/embedded.go
@@ -1,5 +1,7 @@
 package parse
 
+// NOTE: this file is generated via 'go:generate' - manual changes will be overwritten
+
 import (
 	"fmt"
 
@@ -7,21 +9,37 @@ import (
 )
 
 type EmbeddedId struct {
-	ResourceGroup string
-	CapacityName  string
+	SubscriptionId string
+	ResourceGroup  string
+	CapacityName   string
 }
 
+func NewEmbeddedID(subscriptionId, resourceGroup, capacityName string) EmbeddedId {
+	return EmbeddedId{
+		SubscriptionId: subscriptionId,
+		ResourceGroup:  resourceGroup,
+		CapacityName:   capacityName,
+	}
+}
+
+func (id EmbeddedId) ID(_ string) string {
+	fmtString := "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.PowerBIDedicated/capacities/%s"
+	return fmt.Sprintf(fmtString, id.SubscriptionId, id.ResourceGroup, id.CapacityName)
+}
+
+// EmbeddedID parses a Embedded ID into an EmbeddedId struct
 func EmbeddedID(input string) (*EmbeddedId, error) {
 	id, err := azure.ParseAzureResourceID(input)
 	if err != nil {
-		return nil, fmt.Errorf("[ERROR] Unable to parse PowerBI Embedded ID %q: %+v", input, err)
+		return nil, err
 	}
 
-	powerbiEmbedded := EmbeddedId{
-		ResourceGroup: id.ResourceGroup,
+	resourceId := EmbeddedId{
+		SubscriptionId: id.SubscriptionID,
+		ResourceGroup:  id.ResourceGroup,
 	}
 
-	if powerbiEmbedded.CapacityName, err = id.PopSegment("capacities"); err != nil {
+	if resourceId.CapacityName, err = id.PopSegment("capacities"); err != nil {
 		return nil, err
 	}
 
@@ -29,5 +47,5 @@ func EmbeddedID(input string) (*EmbeddedId, error) {
 		return nil, err
 	}
 
-	return &powerbiEmbedded, nil
+	return &resourceId, nil
 }
