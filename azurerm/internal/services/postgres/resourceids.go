0388cf91c4637964b32bbbb225186860a95dfd6d
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-27 15:06:21 +0100

    postgresql_server: switching to use a generated resource id func

---
+++ azurerm/internal/services/postgres/resourceids.go
@@ -0,0 +1,3 @@
+package postgres
+
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Server -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.DBforPostgreSQL/servers/server1
