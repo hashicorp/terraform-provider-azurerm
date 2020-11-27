b043378f84e49f2ae567038c3c4c198cfd474163
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-27 14:34:27 +0100

    r/notification_hub_namespace: enhanced import validation

--- azurerm/internal/services/notificationhub/notification_hub_namespace_resource.go
+++ azurerm/internal/services/notificationhub/notification_hub_namespace_resource.go
@@ -18,6 +18,7 @@ import (
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/notificationhub/parse"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
+	azSchema "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
@@ -30,10 +31,10 @@ func resourceArmNotificationHubNamespace() *schema.Resource {
 		Read:   resourceArmNotificationHubNamespaceRead,
 		Update: resourceArmNotificationHubNamespaceCreateUpdate,
 		Delete: resourceArmNotificationHubNamespaceDelete,
-
-		Importer: &schema.ResourceImporter{
-			State: schema.ImportStatePassthrough,
-		},
+		Importer: azSchema.ValidateResourceIDPriorToImport(func(id string) error {
+			_, err := parse.NamespaceID(id)
+			return err
+		}),
 
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(30 * time.Minute),
