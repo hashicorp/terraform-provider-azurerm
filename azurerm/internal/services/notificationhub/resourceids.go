53caf7dac3e6162c39283c6d3f0ccbb30d455fb0
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-27 14:23:55 +0100

    notificationhub: generating the resource id parser

---
+++ azurerm/internal/services/notificationhub/resourceids.go
@@ -0,0 +1,3 @@
+package notificationhub
+
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=NotificationHub -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.NotificationHubs/namespaces/namespace1/notificationHubs/hub1
