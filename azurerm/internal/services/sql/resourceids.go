5bc9a344719a6639639ec4e8e0c286c4d5573808
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-30 10:24:37 +0100

    sql: generating a Resource ID Formatter/Parser for FirewallRules

--- azurerm/internal/services/sql/resourceids.go
+++ azurerm/internal/services/sql/resourceids.go
@@ -1,4 +1,5 @@
 package sql
 
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Database -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Sql/servers/server1/databases/database1
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=FirewallRule -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Sql/servers/server1/firewallRules/rule1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Server -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Sql/servers/server1
