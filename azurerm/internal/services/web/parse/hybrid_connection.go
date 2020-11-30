d806f8a3f5f07868964134f57c3f7c97958d5f00
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-30 13:40:37 +0100

    web: generating the Hybrid Connection ID Parser

--- azurerm/internal/services/web/parse/hybrid_connection.go
+++ azurerm/internal/services/web/parse/hybrid_connection.go
@@ -1,5 +1,7 @@
 package parse
 
+// NOTE: this file is generated via 'go:generate' - manual changes will be overwritten
+
 import (
 	"fmt"
 
@@ -7,32 +9,61 @@ import (
 )
 
 type HybridConnectionId struct {
+	SubscriptionId                string
 	ResourceGroup                 string
 	SiteName                      string
-	RelayName                     string
 	HybridConnectionNamespaceName string
+	RelayName                     string
 }
 
+func NewHybridConnectionID(subscriptionId, resourceGroup, siteName, hybridConnectionNamespaceName, relayName string) HybridConnectionId {
+	return HybridConnectionId{
+		SubscriptionId:                subscriptionId,
+		ResourceGroup:                 resourceGroup,
+		SiteName:                      siteName,
+		HybridConnectionNamespaceName: hybridConnectionNamespaceName,
+		RelayName:                     relayName,
+	}
+}
+
+func (id HybridConnectionId) ID(_ string) string {
+	fmtString := "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Web/sites/%s/hybridConnectionNamespaces/%s/relays/%s"
+	return fmt.Sprintf(fmtString, id.SubscriptionId, id.ResourceGroup, id.SiteName, id.HybridConnectionNamespaceName, id.RelayName)
+}
+
+// HybridConnectionID parses a HybridConnection ID into an HybridConnectionId struct
 func HybridConnectionID(input string) (*HybridConnectionId, error) {
 	id, err := azure.ParseAzureResourceID(input)
 	if err != nil {
-		return nil, fmt.Errorf("[ERROR] Unable to parse Hybrid Connection ID %q: %+v", input, err)
-	}
-
-	hybridConnection := HybridConnectionId{
-		ResourceGroup: id.ResourceGroup,
-	}
-	if hybridConnection.RelayName, err = id.PopSegment("relays"); err != nil {
 		return nil, err
 	}
 
-	if hybridConnection.SiteName, err = id.PopSegment("sites"); err != nil {
+	resourceId := HybridConnectionId{
+		SubscriptionId: id.SubscriptionID,
+		ResourceGroup:  id.ResourceGroup,
+	}
+
+	if resourceId.SubscriptionId == "" {
+		return nil, fmt.Errorf("ID was missing the 'subscriptions' element")
+	}
+
+	if resourceId.ResourceGroup == "" {
+		return nil, fmt.Errorf("ID was missing the 'resourceGroups' element")
+	}
+
+	if resourceId.SiteName, err = id.PopSegment("sites"); err != nil {
+		return nil, err
+	}
+	if resourceId.HybridConnectionNamespaceName, err = id.PopSegment("hybridConnectionNamespaces"); err != nil {
+		return nil, err
+	}
+	if resourceId.RelayName, err = id.PopSegment("relays"); err != nil {
 		return nil, err
 	}
 
-	if hybridConnection.HybridConnectionNamespaceName, err = id.PopSegment("hybridConnectionNamespaces"); err != nil {
+	if err := id.ValidateNoEmptySegments(input); err != nil {
 		return nil, err
 	}
 
-	return &hybridConnection, nil
+	return &resourceId, nil
 }
--- azurerm/internal/services/web/parse/hybrid_connection_test.go
+++ azurerm/internal/services/web/parse/hybrid_connection_test.go
@@ -1,87 +1,144 @@
 package parse
 
-import "testing"
+// NOTE: this file is generated via 'go:generate' - manual changes will be overwritten
 
-func TestParseAppServiceHybridConnectionID(t *testing.T) {
+import (
+	"testing"
+
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/resourceid"
+)
+
+var _ resourceid.Formatter = HybridConnectionId{}
+
+func TestHybridConnectionIDFormatter(t *testing.T) {
+	actual := NewHybridConnectionID("12345678-1234-9876-4563-123456789012", "resGroup1", "site1", "hybridConnectionNamespace1", "relay1").ID("")
+	expected := "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Web/sites/site1/hybridConnectionNamespaces/hybridConnectionNamespace1/relays/relay1"
+	if actual != expected {
+		t.Fatalf("Expected %q but got %q", expected, actual)
+	}
+}
+
+func TestHybridConnectionID(t *testing.T) {
 	testData := []struct {
-		Name     string
 		Input    string
+		Error    bool
 		Expected *HybridConnectionId
 	}{
+
 		{
-			Name:     "Empty",
-			Input:    "",
-			Expected: nil,
-		},
-		{
-			Name:     "No Resource Groups Segment",
-			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000",
-			Expected: nil,
-		},
-		{
-			Name:     "No Resource Groups value",
-			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/",
-			Expected: nil,
-		},
-		{
-			Name:     "Missing Sites Value",
-			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testResourceGroup1/providers/Microsoft.Web/sites/",
-			Expected: nil,
-		},
-		{
-			Name:     "Missing Namespace Value",
-			Input:    "/subscriptions/00000000-0000-0000-0000-00000000000/resourceGroups/testResourceGroup1/providers/Microsoft.Web/sites/testApp1/hybridConnectionNamespaces/",
-			Expected: nil,
-		},
-		{
-			Name:     "Missing Relay Name value",
-			Input:    "/subscriptions/00000000-0000-0000-0000-00000000000/resourceGroups/testResourceGroup1/providers/Microsoft.Web/sites/testApp1/hybridConnectionNamespaces/testNamespace/relays/",
-			Expected: nil,
-		},
-		{
-			Name:     "Incorrect casing",
-			Input:    "/subscriptions/00000000-0000-0000-0000-00000000000/resourceGroups/testResourceGroup1/providers/Microsoft.Web/sites/testApp1/hybridConnectionNamespaces/testNamespace1/Relays/testRelay1",
-			Expected: nil,
+			// empty
+			Input: "",
+			Error: true,
 		},
 
 		{
-			Name:  "App Service Hybrid Connection Resource ID",
-			Input: "/subscriptions/00000000-0000-0000-0000-00000000000/resourceGroups/testResourceGroup1/providers/Microsoft.Web/sites/testApp1/hybridConnectionNamespaces/testNamespace1/relays/testRelay1",
+			// missing SubscriptionId
+			Input: "/",
+			Error: true,
+		},
+
+		{
+			// missing value for SubscriptionId
+			Input: "/subscriptions/",
+			Error: true,
+		},
+
+		{
+			// missing ResourceGroup
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/",
+			Error: true,
+		},
+
+		{
+			// missing value for ResourceGroup
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/",
+			Error: true,
+		},
+
+		{
+			// missing SiteName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Web/",
+			Error: true,
+		},
+
+		{
+			// missing value for SiteName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Web/sites/",
+			Error: true,
+		},
+
+		{
+			// missing HybridConnectionNamespaceName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Web/sites/site1/",
+			Error: true,
+		},
+
+		{
+			// missing value for HybridConnectionNamespaceName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Web/sites/site1/hybridConnectionNamespaces/",
+			Error: true,
+		},
+
+		{
+			// missing RelayName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Web/sites/site1/hybridConnectionNamespaces/hybridConnectionNamespace1/",
+			Error: true,
+		},
+
+		{
+			// missing value for RelayName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Web/sites/site1/hybridConnectionNamespaces/hybridConnectionNamespace1/relays/",
+			Error: true,
+		},
+
+		{
+			// valid
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Web/sites/site1/hybridConnectionNamespaces/hybridConnectionNamespace1/relays/relay1",
 			Expected: &HybridConnectionId{
-				ResourceGroup:                 "testResourceGroup1",
-				RelayName:                     "testRelay1",
-				SiteName:                      "testApp1",
-				HybridConnectionNamespaceName: "testNamespace1",
+				SubscriptionId:                "12345678-1234-9876-4563-123456789012",
+				ResourceGroup:                 "resGroup1",
+				SiteName:                      "site1",
+				HybridConnectionNamespaceName: "hybridConnectionNamespace1",
+				RelayName:                     "relay1",
 			},
+		},
+
+		{
+			// upper-cased
+			Input: "/SUBSCRIPTIONS/12345678-1234-9876-4563-123456789012/RESOURCEGROUPS/RESGROUP1/PROVIDERS/MICROSOFT.WEB/SITES/SITE1/HYBRIDCONNECTIONNAMESPACES/HYBRIDCONNECTIONNAMESPACE1/RELAYS/RELAY1",
+			Error: true,
 		},
 	}
 
 	for _, v := range testData {
-		t.Logf("[DEBUG] Testing %q", v.Name)
+		t.Logf("[DEBUG] Testing %q", v.Input)
 
 		actual, err := HybridConnectionID(v.Input)
 		if err != nil {
-			if v.Expected == nil {
+			if v.Error {
 				continue
 			}
 
-			t.Fatalf("Expected a value but got an error: %s", err)
+			t.Fatalf("Expect a value but got an error: %s", err)
+		}
+		if v.Error {
+			t.Fatal("Expect an error but didn't get one")
 		}
 
-		if actual.RelayName != v.Expected.RelayName {
-			t.Fatalf("Expected %s but got %s for Name", v.Expected.RelayName, actual.RelayName)
+		if actual.SubscriptionId != v.Expected.SubscriptionId {
+			t.Fatalf("Expected %q but got %q for SubscriptionId", v.Expected.SubscriptionId, actual.SubscriptionId)
 		}
-
 		if actual.ResourceGroup != v.Expected.ResourceGroup {
-			t.Fatalf("Expected %s but got %s for ResourceGroup", v.Expected.ResourceGroup, actual.ResourceGroup)
+			t.Fatalf("Expected %q but got %q for ResourceGroup", v.Expected.ResourceGroup, actual.ResourceGroup)
 		}
-
 		if actual.SiteName != v.Expected.SiteName {
-			t.Fatalf("Expected %s but got %s for SiteName", v.Expected.SiteName, actual.SiteName)
+			t.Fatalf("Expected %q but got %q for SiteName", v.Expected.SiteName, actual.SiteName)
 		}
-
 		if actual.HybridConnectionNamespaceName != v.Expected.HybridConnectionNamespaceName {
-			t.Fatalf("Expected %s but got %s for Namespace", v.Expected.HybridConnectionNamespaceName, actual.HybridConnectionNamespaceName)
+			t.Fatalf("Expected %q but got %q for HybridConnectionNamespaceName", v.Expected.HybridConnectionNamespaceName, actual.HybridConnectionNamespaceName)
+		}
+		if actual.RelayName != v.Expected.RelayName {
+			t.Fatalf("Expected %q but got %q for RelayName", v.Expected.RelayName, actual.RelayName)
 		}
 	}
 }
