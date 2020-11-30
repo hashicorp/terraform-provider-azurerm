9d5fd98b3a2364271611c7f8bf39c0e20b124f2e
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-30 10:44:38 +0100

    sql: generating an ID Parser for ElasticPool

---
+++ azurerm/internal/services/sql/parse/elastic_pool.go
@@ -0,0 +1,56 @@
+package parse
+
+// NOTE: this file is generated via 'go:generate' - manual changes will be overwritten
+
+import (
+	"fmt"
+
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
+)
+
+type ElasticPoolId struct {
+	SubscriptionId string
+	ResourceGroup  string
+	ServerName     string
+	Name           string
+}
+
+func NewElasticPoolID(subscriptionId, resourceGroup, serverName, name string) ElasticPoolId {
+	return ElasticPoolId{
+		SubscriptionId: subscriptionId,
+		ResourceGroup:  resourceGroup,
+		ServerName:     serverName,
+		Name:           name,
+	}
+}
+
+func (id ElasticPoolId) ID(_ string) string {
+	fmtString := "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Sql/servers/%s/elasticPools/%s"
+	return fmt.Sprintf(fmtString, id.SubscriptionId, id.ResourceGroup, id.ServerName, id.Name)
+}
+
+// ElasticPoolID parses a ElasticPool ID into an ElasticPoolId struct
+func ElasticPoolID(input string) (*ElasticPoolId, error) {
+	id, err := azure.ParseAzureResourceID(input)
+	if err != nil {
+		return nil, err
+	}
+
+	resourceId := ElasticPoolId{
+		SubscriptionId: id.SubscriptionID,
+		ResourceGroup:  id.ResourceGroup,
+	}
+
+	if resourceId.ServerName, err = id.PopSegment("servers"); err != nil {
+		return nil, err
+	}
+	if resourceId.Name, err = id.PopSegment("elasticPools"); err != nil {
+		return nil, err
+	}
+
+	if err := id.ValidateNoEmptySegments(input); err != nil {
+		return nil, err
+	}
+
+	return &resourceId, nil
+}
