24b4c4ed222803b42075fc415d61e20847524ec8
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-30 18:09:18 +0100

    appplatform: splitting and renaming the spring cloud service

--- azurerm/internal/services/appplatform/validate/spring_cloud_service_name.go
+++ azurerm/internal/services/appplatform/validate/spring_cloud_service_name.go
@@ -3,7 +3,6 @@ package validate
 import (
 	"fmt"
 	"regexp"
-	"strings"
 )
 
 func SpringCloudServiceName(i interface{}, k string) (_ []string, errors []error) {
@@ -22,21 +21,5 @@ func SpringCloudServiceName(i interface{}, k string) (_ []string, errors []error
 		errors = append(errors, fmt.Errorf("%s must begin with a letter, end with a letter or number, contain only lowercase letters, numbers and hyphens. The value must be between 4 and 32 characters long.", k))
 	}
 
-	return nil, errors
-}
-
-func ConfigServerURI(i interface{}, k string) (_ []string, errors []error) {
-	v, ok := i.(string)
-	if !ok {
-		return nil, append(errors, fmt.Errorf("expected type of %s to be string", k))
-	}
-
-	// the config server URI should be started with http://, https://, git@, or ssh://
-	if !strings.HasPrefix(v, "http://") &&
-		!strings.HasPrefix(v, "https://") &&
-		!strings.HasPrefix(v, "git@") &&
-		!strings.HasPrefix(v, "ssh://") {
-		errors = append(errors, fmt.Errorf("%s should be started with http://, https://, git@, or ssh://", k))
-	}
 	return nil, errors
 }
