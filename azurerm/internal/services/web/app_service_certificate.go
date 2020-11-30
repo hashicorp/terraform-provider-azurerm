cf463215417a522b9e5eed6a8e2bd23c8c4f7a6a
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-30 13:30:37 +0100

    web: removing the unused validate func

--- azurerm/internal/services/web/app_service_certificate.go
+++ azurerm/internal/services/web/app_service_certificate.go
@@ -31,19 +31,3 @@ func ParseAppServiceCertificateID(input string) (*AppServiceCertificateResourceI
 
 	return &certificate, nil
 }
-
-// ValidateAppServiceCertificateID validates that the specified ID is a valid App Service Certificate ID
-func ValidateAppServiceCertificateID(i interface{}, k string) (warnings []string, errors []error) {
-	v, ok := i.(string)
-	if !ok {
-		errors = append(errors, fmt.Errorf("expected type of %q to be string", k))
-		return
-	}
-
-	if _, err := ParseAppServiceCertificateID(v); err != nil {
-		errors = append(errors, fmt.Errorf("Can not parse %q as a resource id: %v", k, err))
-		return
-	}
-
-	return warnings, errors
-}
