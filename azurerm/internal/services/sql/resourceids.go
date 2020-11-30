9258740dfa78fb060eda3f4ab9994df240f580bd
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-30 11:02:09 +0100

    sql: generating the Resource ID Formatter/Parser for AD Administrator

--- azurerm/internal/services/sql/resourceids.go
+++ azurerm/internal/services/sql/resourceids.go
@@ -1,5 +1,6 @@
 package sql
 
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=AzureActiveDirectoryAdministrator -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.SQL/servers/server1/administrators/activeDirectory
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Database -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Sql/servers/server1/databases/database1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ElasticPool -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Sql/servers/server1/elasticPools/elasticPool1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=FailoverGroup -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Sql/servers/server1/failoverGroups/failoverGroup1
