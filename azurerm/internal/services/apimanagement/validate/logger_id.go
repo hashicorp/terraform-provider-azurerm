28e0455323bc011331226551d4c10c76f3406803
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-30 18:03:40 +0100

    apimanagement: renaming the LoggerID validate func

--- azurerm/internal/services/apimanagement/validate/logger_id.go
+++ azurerm/internal/services/apimanagement/validate/logger_id.go
@@ -6,7 +6,7 @@ import (
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/apimanagement/parse"
 )
 
-func ApiManagementLoggerID(i interface{}, k string) (warnings []string, errors []error) {
+func LoggerID(i interface{}, k string) (warnings []string, errors []error) {
 	v, ok := i.(string)
 	if !ok {
 		errors = append(errors, fmt.Errorf("expected type of %q to be string", k))
