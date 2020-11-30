17d0ed1c7c8d3ae77c0b573ddbf480df4ed64181
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-30 13:32:07 +0100

    web: generating the Certificate ID

--- azurerm/internal/services/web/parse/certificate.go
+++ azurerm/internal/services/web/parse/certificate.go
@@ -1,5 +1,7 @@
 package parse
 
+// NOTE: this file is generated via 'go:generate' - manual changes will be overwritten
+
 import (
 	"fmt"
 
@@ -7,21 +9,45 @@ import (
 )
 
 type CertificateId struct {
-	ResourceGroup string
-	Name          string
+	SubscriptionId string
+	ResourceGroup  string
+	Name           string
 }
 
+func NewCertificateID(subscriptionId, resourceGroup, name string) CertificateId {
+	return CertificateId{
+		SubscriptionId: subscriptionId,
+		ResourceGroup:  resourceGroup,
+		Name:           name,
+	}
+}
+
+func (id CertificateId) ID(_ string) string {
+	fmtString := "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Web/certificates/%s"
+	return fmt.Sprintf(fmtString, id.SubscriptionId, id.ResourceGroup, id.Name)
+}
+
+// CertificateID parses a Certificate ID into an CertificateId struct
 func CertificateID(input string) (*CertificateId, error) {
 	id, err := azure.ParseAzureResourceID(input)
 	if err != nil {
-		return nil, fmt.Errorf("[ERROR] Unable to parse App Service Certificate ID %q: %+v", input, err)
+		return nil, err
 	}
 
-	certificate := CertificateId{
-		ResourceGroup: id.ResourceGroup,
+	resourceId := CertificateId{
+		SubscriptionId: id.SubscriptionID,
+		ResourceGroup:  id.ResourceGroup,
 	}
 
-	if certificate.Name, err = id.PopSegment("certificates"); err != nil {
+	if resourceId.SubscriptionId == "" {
+		return nil, fmt.Errorf("ID was missing the 'subscriptions' element")
+	}
+
+	if resourceId.ResourceGroup == "" {
+		return nil, fmt.Errorf("ID was missing the 'resourceGroups' element")
+	}
+
+	if resourceId.Name, err = id.PopSegment("certificates"); err != nil {
 		return nil, err
 	}
 
@@ -29,5 +55,5 @@ func CertificateID(input string) (*CertificateId, error) {
 		return nil, err
 	}
 
-	return &certificate, nil
+	return &resourceId, nil
 }
