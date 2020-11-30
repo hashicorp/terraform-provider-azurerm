e5e68e897348101842a01206f44c843f9281bcd2
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-30 13:35:17 +0100

    web: removing an unused validate func

--- azurerm/internal/services/web/app_service_hybrid_connection.go
+++ azurerm/internal/services/web/app_service_hybrid_connection.go
@@ -36,17 +36,3 @@ func ParseAppServiceHybridConnectionID(input string) (*AppServiceHybridConnectio
 
 	return &hybridConnection, nil
 }
-
-func ValidateAppServiceHybridConnectionID(i interface{}, k string) (warnings []string, errors []error) {
-	v, ok := i.(string)
-	if !ok {
-		errors = append(errors, fmt.Errorf("expected type of %q to be string", k))
-		return
-	}
-
-	if _, err := ParseAppServiceHybridConnectionID(v); err != nil {
-		errors = append(errors, fmt.Errorf("Can not parse %q as a resource id: %v", k, err))
-	}
-
-	return warnings, errors
-}
