497d5a2985a2421c730be89a351f3cdce1e9e220
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-30 13:05:00 +0100

    web: generating virtual_network_swift_connection

--- azurerm/internal/services/web/parse/virtual_network_swift_connection.go
+++ azurerm/internal/services/web/parse/virtual_network_swift_connection.go
@@ -1,5 +1,7 @@
 package parse
 
+// NOTE: this file is generated via 'go:generate' - manual changes will be overwritten
+
 import (
 	"fmt"
 
@@ -7,23 +9,56 @@ import (
 )
 
 type VirtualNetworkSwiftConnectionId struct {
-	ResourceGroup string
-	SiteName      string
+	SubscriptionId    string
+	ResourceGroup     string
+	SiteName          string
+	NetworkconfigName string
 }
 
-func VirtualNetworkSwiftConnectionID(resourceId string) (*VirtualNetworkSwiftConnectionId, error) {
-	id, err := azure.ParseAzureResourceID(resourceId)
+func NewVirtualNetworkSwiftConnectionID(subscriptionId, resourceGroup, siteName, networkconfigName string) VirtualNetworkSwiftConnectionId {
+	return VirtualNetworkSwiftConnectionId{
+		SubscriptionId:    subscriptionId,
+		ResourceGroup:     resourceGroup,
+		SiteName:          siteName,
+		NetworkconfigName: networkconfigName,
+	}
+}
+
+func (id VirtualNetworkSwiftConnectionId) ID(_ string) string {
+	fmtString := "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Web/sites/%s/networkconfig/%s"
+	return fmt.Sprintf(fmtString, id.SubscriptionId, id.ResourceGroup, id.SiteName, id.NetworkconfigName)
+}
+
+// VirtualNetworkSwiftConnectionID parses a VirtualNetworkSwiftConnection ID into an VirtualNetworkSwiftConnectionId struct
+func VirtualNetworkSwiftConnectionID(input string) (*VirtualNetworkSwiftConnectionId, error) {
+	id, err := azure.ParseAzureResourceID(input)
 	if err != nil {
-		return nil, fmt.Errorf("Error parsing Azure Resource ID %q", id)
-	}
-
-	virtualNetworkId := &VirtualNetworkSwiftConnectionId{
-		ResourceGroup: id.ResourceGroup,
-	}
-
-	if virtualNetworkId.SiteName, err = id.PopSegment("sites"); err != nil {
 		return nil, err
 	}
 
-	return virtualNetworkId, nil
+	resourceId := VirtualNetworkSwiftConnectionId{
+		SubscriptionId: id.SubscriptionID,
+		ResourceGroup:  id.ResourceGroup,
+	}
+
+	if resourceId.SubscriptionId == "" {
+		return nil, fmt.Errorf("ID was missing the 'subscriptions' element")
+	}
+
+	if resourceId.ResourceGroup == "" {
+		return nil, fmt.Errorf("ID was missing the 'resourceGroups' element")
+	}
+
+	if resourceId.SiteName, err = id.PopSegment("sites"); err != nil {
+		return nil, err
+	}
+	if resourceId.NetworkconfigName, err = id.PopSegment("networkconfig"); err != nil {
+		return nil, err
+	}
+
+	if err := id.ValidateNoEmptySegments(input); err != nil {
+		return nil, err
+	}
+
+	return &resourceId, nil
 }
