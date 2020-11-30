cc19bb604a0adcb52b9f48c1c9aff8968c89b3e6
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-30 13:08:18 +0100

    web: generating the function app slot parser

--- azurerm/internal/services/web/parse/function_app_slot.go
+++ azurerm/internal/services/web/parse/function_app_slot.go
@@ -1,5 +1,7 @@
 package parse
 
+// NOTE: this file is generated via 'go:generate' - manual changes will be overwritten
+
 import (
 	"fmt"
 
@@ -7,28 +9,50 @@ import (
 )
 
 type FunctionAppSlotId struct {
-	ResourceGroup string
-	SiteName      string
-	SlotName      string
+	SubscriptionId string
+	ResourceGroup  string
+	SiteName       string
+	SlotName       string
 }
 
+func NewFunctionAppSlotID(subscriptionId, resourceGroup, siteName, slotName string) FunctionAppSlotId {
+	return FunctionAppSlotId{
+		SubscriptionId: subscriptionId,
+		ResourceGroup:  resourceGroup,
+		SiteName:       siteName,
+		SlotName:       slotName,
+	}
+}
+
+func (id FunctionAppSlotId) ID(_ string) string {
+	fmtString := "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Web/sites/%s/slots/%s"
+	return fmt.Sprintf(fmtString, id.SubscriptionId, id.ResourceGroup, id.SiteName, id.SlotName)
+}
+
+// FunctionAppSlotID parses a FunctionAppSlot ID into an FunctionAppSlotId struct
 func FunctionAppSlotID(input string) (*FunctionAppSlotId, error) {
 	id, err := azure.ParseAzureResourceID(input)
 	if err != nil {
-		return nil, fmt.Errorf("[ERROR] Unable to parse App Service Slot ID %q: %+v", input, err)
-	}
-
-	slot := FunctionAppSlotId{
-		ResourceGroup: id.ResourceGroup,
-		SiteName:      id.Path["sites"],
-		SlotName:      id.Path["slots"],
-	}
-
-	if slot.SiteName, err = id.PopSegment("sites"); err != nil {
 		return nil, err
 	}
 
-	if slot.SlotName, err = id.PopSegment("slots"); err != nil {
+	resourceId := FunctionAppSlotId{
+		SubscriptionId: id.SubscriptionID,
+		ResourceGroup:  id.ResourceGroup,
+	}
+
+	if resourceId.SubscriptionId == "" {
+		return nil, fmt.Errorf("ID was missing the 'subscriptions' element")
+	}
+
+	if resourceId.ResourceGroup == "" {
+		return nil, fmt.Errorf("ID was missing the 'resourceGroups' element")
+	}
+
+	if resourceId.SiteName, err = id.PopSegment("sites"); err != nil {
+		return nil, err
+	}
+	if resourceId.SlotName, err = id.PopSegment("slots"); err != nil {
 		return nil, err
 	}
 
@@ -36,5 +60,5 @@ func FunctionAppSlotID(input string) (*FunctionAppSlotId, error) {
 		return nil, err
 	}
 
-	return &slot, nil
+	return &resourceId, nil
 }
--- azurerm/internal/services/web/resourceids.go
+++ azurerm/internal/services/web/resourceids.go
@@ -1,5 +1,6 @@
 package web
 
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=FunctionAppSlot -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Web/sites/site1/slots/slot1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ManagedCertificate -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Web/certificates/customhost.contoso.com
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=SlotVirtualNetworkSwiftConnection -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Web/sites/site1/slots/slot1/networkconfig/virtualNetwork
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=VirtualNetworkSwiftConnection -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Web/sites/site1/networkconfig/virtualNetwork
