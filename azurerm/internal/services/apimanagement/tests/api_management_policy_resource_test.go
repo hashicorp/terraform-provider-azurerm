d67dc93b88b02b0b590f13bf4634ff805460b940
Tom Bamford <tom@bamford.io>
2020-12-01 11:26:45 +0000

    linting

--- azurerm/internal/services/apimanagement/tests/api_management_policy_resource_test.go
+++ azurerm/internal/services/apimanagement/tests/api_management_policy_resource_test.go
@@ -176,12 +176,12 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_api_management" "test" {
- name                = "acctestAM-%[1]d"
- location            = azurerm_resource_group.test.location
- resource_group_name = azurerm_resource_group.test.name
- publisher_name      = "pub1"
- publisher_email     = "pub1@email.com"
- sku_name            = "Developer_1"
+  name                = "acctestAM-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  publisher_name      = "pub1"
+  publisher_email     = "pub1@email.com"
+  sku_name            = "Developer_1"
 }
 
 resource "azurerm_api_management_policy" "test" {
