73a0ebcd2360be032108e124ad220aaf7b56c14f
Sune Keller <absukl@almbrand.dk>
2020-12-01 14:58:43 +0100

    New resource: `azurerm_api_management_policy` (#9215)
    * Implement new resource
    
    Signed-off-by: Sune Keller <absukl@almbrand.dk>
    
    * Add website docs
    
    Signed-off-by: Sune Keller <absukl@almbrand.dk>
    
    * Add acceptance tests
    
    Adapted from api_management_api_policy_resource_test.go.
    
    Signed-off-by: Sune Keller <absukl@almbrand.dk>
    
    * Include named value in acceptance test
    
    The purpose of the PR is to ensure named values are created before they
    are referenced in the APIM policy.
    
    Signed-off-by: Sune Keller <absukl@almbrand.dk>
    
    * Apply suggestions from code review
    
    Signed-off-by: Sune Keller <absukl@almbrand.dk>
    
    Co-authored-by: Tom Bamford <tom@bamford.io>
    
    * Add ExactlyOneOf to policy XML fields
    
    Signed-off-by: Sune Keller <absukl@almbrand.dk>
    
    * Remove import check, and replace with info in docs
    
    Signed-off-by: Sune Keller <absukl@almbrand.dk>
    
    * azurerm_api_management_policy: use correct ID field
    
    * azurerm_api_management_policy: fix up tests
    
    * linting
    
    * linting
    
    * azurerm_api_management_policy: docs fixes
    
    Co-authored-by: Tom Bamford <tom@bamford.io>

---
+++ azurerm/internal/services/apimanagement/parse/policy_id_test.go
@@ -0,0 +1,86 @@
+package parse
+
+import "testing"
+
+func TestApiManagementPolicyID(t *testing.T) {
+	testData := []struct {
+		Name     string
+		Input    string
+		Expected *ApiManagementPolicyId
+	}{
+		{
+			Name:     "Empty",
+			Input:    "",
+			Expected: nil,
+		},
+		{
+			Name:     "No Resource Groups Segment",
+			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000",
+			Expected: nil,
+		},
+		{
+			Name:     "No Resource Groups Value",
+			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/",
+			Expected: nil,
+		},
+		{
+			Name:     "Resource Group ID",
+			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/foo/",
+			Expected: nil,
+		},
+		{
+			Name:     "Missing Service Name",
+			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/",
+			Expected: nil,
+		},
+		{
+			Name:     "Missing Policy",
+			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/service1",
+			Expected: nil,
+		},
+		{
+			Name:     "Missing Policy Value",
+			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/service1/policies",
+			Expected: nil,
+		},
+		{
+			Name:  "Policy ID",
+			Input: "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/service1/policies/policy1",
+			Expected: &ApiManagementPolicyId{
+				Name:          "policy1",
+				ServiceName:   "service1",
+				ResourceGroup: "resGroup1",
+			},
+		},
+		{
+			Name:     "Wrong Casing",
+			Input:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/service1/Policies/policy1",
+			Expected: nil,
+		},
+	}
+
+	for _, v := range testData {
+		t.Logf("[DEBUG] Testing %q", v.Name)
+
+		actual, err := ApiManagementPolicyID(v.Input)
+		if err != nil {
+			if v.Expected == nil {
+				continue
+			}
+
+			t.Fatalf("Expected a value but got an error: %s", err)
+		}
+
+		if actual.Name != v.Expected.Name {
+			t.Fatalf("Expected %q but got %q for Name", v.Expected.Name, actual.Name)
+		}
+
+		if actual.ServiceName != v.Expected.ServiceName {
+			t.Fatalf("Expected %q but got %q for Service Name", v.Expected.Name, actual.Name)
+		}
+
+		if actual.ResourceGroup != v.Expected.ResourceGroup {
+			t.Fatalf("Expected %q but got %q for Resource Group", v.Expected.ResourceGroup, actual.ResourceGroup)
+		}
+	}
+}
