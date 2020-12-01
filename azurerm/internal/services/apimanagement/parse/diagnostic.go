d5fec84319c4de59ea4031a60b9c3d1d2548b758
tombuildsstuff <git@ibuildstuff.co.uk>
2020-12-01 18:54:44 +0100

    apimanagement: generating the Diagnostic Resource ID

--- azurerm/internal/services/apimanagement/parse/diagnostic.go
+++ azurerm/internal/services/apimanagement/parse/diagnostic.go
@@ -1,5 +1,7 @@
 package parse
 
+// NOTE: this file is generated via 'go:generate' - manual changes will be overwritten
+
 import (
 	"fmt"
 
@@ -7,26 +9,50 @@ import (
 )
 
 type DiagnosticId struct {
-	ResourceGroup string
-	ServiceName   string
-	Name          string
+	SubscriptionId string
+	ResourceGroup  string
+	ServiceName    string
+	Name           string
 }
 
+func NewDiagnosticID(subscriptionId, resourceGroup, serviceName, name string) DiagnosticId {
+	return DiagnosticId{
+		SubscriptionId: subscriptionId,
+		ResourceGroup:  resourceGroup,
+		ServiceName:    serviceName,
+		Name:           name,
+	}
+}
+
+func (id DiagnosticId) ID(_ string) string {
+	fmtString := "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.ApiManagement/service/%s/diagnostics/%s"
+	return fmt.Sprintf(fmtString, id.SubscriptionId, id.ResourceGroup, id.ServiceName, id.Name)
+}
+
+// DiagnosticID parses a Diagnostic ID into an DiagnosticId struct
 func DiagnosticID(input string) (*DiagnosticId, error) {
 	id, err := azure.ParseAzureResourceID(input)
 	if err != nil {
-		return nil, fmt.Errorf("parsing Api Management Diagnostic ID %q: %+v", input, err)
-	}
-
-	diagnostic := DiagnosticId{
-		ResourceGroup: id.ResourceGroup,
-	}
-
-	if diagnostic.ServiceName, err = id.PopSegment("service"); err != nil {
 		return nil, err
 	}
 
-	if diagnostic.Name, err = id.PopSegment("diagnostics"); err != nil {
+	resourceId := DiagnosticId{
+		SubscriptionId: id.SubscriptionID,
+		ResourceGroup:  id.ResourceGroup,
+	}
+
+	if resourceId.SubscriptionId == "" {
+		return nil, fmt.Errorf("ID was missing the 'subscriptions' element")
+	}
+
+	if resourceId.ResourceGroup == "" {
+		return nil, fmt.Errorf("ID was missing the 'resourceGroups' element")
+	}
+
+	if resourceId.ServiceName, err = id.PopSegment("service"); err != nil {
+		return nil, err
+	}
+	if resourceId.Name, err = id.PopSegment("diagnostics"); err != nil {
 		return nil, err
 	}
 
@@ -34,5 +60,5 @@ func DiagnosticID(input string) (*DiagnosticId, error) {
 		return nil, err
 	}
 
-	return &diagnostic, nil
+	return &resourceId, nil
 }
