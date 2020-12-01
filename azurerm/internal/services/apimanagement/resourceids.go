a695ce7522a39e8e3bca1eb10e29064661277d54
tombuildsstuff <git@ibuildstuff.co.uk>
2020-12-01 18:45:09 +0100

    apimanagement: generating the ApiManagement Service ID

--- azurerm/internal/services/apimanagement/resourceids.go
+++ azurerm/internal/services/apimanagement/resourceids.go
@@ -1,3 +1,4 @@
 package apimanagement
 
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ApiDiagnostic -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/service1/apis/api1/diagnostics/diagnostic1
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ApiManagement -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/service1
