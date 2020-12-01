bb1237d78667b32955a57545e5c9e4003e713e6e
tombuildsstuff <git@ibuildstuff.co.uk>
2020-12-01 19:35:35 +0100

    iothub: removing an unused func

--- azurerm/internal/services/iothub/validate/iothub.go
+++ azurerm/internal/services/iothub/validate/iothub.go
@@ -3,24 +3,7 @@ package validate
 import (
 	"fmt"
 	"regexp"
-
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/iothub/parse"
 )
-
-func IotHubID(i interface{}, k string) (warnings []string, errors []error) {
-	v, ok := i.(string)
-	if !ok {
-		errors = append(errors, fmt.Errorf("expected type of %q to be string", k))
-		return
-	}
-
-	if _, err := parse.IotHubID(v); err != nil {
-		errors = append(errors, fmt.Errorf("can not parse %q as a resource id: %v", k, err))
-		return
-	}
-
-	return warnings, errors
-}
 
 func IoTHubName(v interface{}, k string) (warnings []string, errors []error) {
 	value := v.(string)
