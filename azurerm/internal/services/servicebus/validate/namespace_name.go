7ae4ff30356a403aaa56f4e867986b784cc65883
tombuildsstuff <git@ibuildstuff.co.uk>
2020-12-02 13:27:27 +0100

    servicebus: removing the `Servicebus` prefix from the validate func to match the convention

--- azurerm/internal/services/servicebus/validate/namespace_name.go
+++ azurerm/internal/services/servicebus/validate/namespace_name.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-func ServiceBusNamespaceName(i interface{}, k string) (warnings []string, errors []error) {
+func NamespaceName(i interface{}, k string) (warnings []string, errors []error) {
 	v, ok := i.(string)
 	if !ok {
 		errors = append(errors, fmt.Errorf("expected type of %q to be string", k))
