92142959e90c3b12c94ebc5f87acda8bf298d808
jackofallops <ste@hashicorp.com>
2020-12-02 09:41:38 +0000

    correct typo in set for storage_authentication_tyep

--- azurerm/internal/services/media/media_services_account_resource.go
+++ azurerm/internal/services/media/media_services_account_resource.go
@@ -193,7 +193,7 @@ func resourceArmMediaServicesAccountRead(d *schema.ResourceData, meta interface{
 		if e := d.Set("storage_account", accounts); e != nil {
 			return fmt.Errorf("flattening `storage_account`: %s", e)
 		}
-		d.Set("storage_authentication", string(props.StorageAuthentication))
+		d.Set("storage_authentication_type", string(props.StorageAuthentication))
 	}
 
 	if err := d.Set("identity", flattenAzureRmMediaServicedentity(resp.Identity)); err != nil {
