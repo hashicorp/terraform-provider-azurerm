c6d35f5059c89f93655bb057422cf7c660e46222
jackofallops <ste@hashicorp.com>
2020-12-03 17:26:58 +0000

    removed setting location, since it's not in schema anyway

--- azurerm/internal/services/trafficmanager/traffic_manager_profile_data_source.go
+++ azurerm/internal/services/trafficmanager/traffic_manager_profile_data_source.go
@@ -8,7 +8,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/location"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
@@ -149,10 +148,6 @@ func dataSourceArmTrafficManagerProfileRead(d *schema.ResourceData, meta interfa
 	}
 
 	d.SetId(*resp.ID)
-
-	if loc := resp.Location; loc != nil {
-		d.Set("location", location.NormalizeNilable(loc))
-	}
 
 	if profile := resp.ProfileProperties; profile != nil {
 		d.Set("profile_status", profile.ProfileStatus)
