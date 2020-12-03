5947832756031b813368f1f76e10137d2953017b
jackofallops <ste@hashicorp.com>
2020-12-03 16:58:13 +0000

    goimports

--- azurerm/internal/services/trafficmanager/traffic_manager_profile_data_source.go
+++ azurerm/internal/services/trafficmanager/traffic_manager_profile_data_source.go
@@ -2,13 +2,14 @@ package trafficmanager
 
 import (
 	"fmt"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/location"
 	"time"
+
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/location"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
