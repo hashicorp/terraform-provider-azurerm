04b8e0192c7a5e4c90601a24e82bbb598c917a8f
jackofallops <ste@hashicorp.com>
2020-12-03 17:01:58 +0000

    fmt again...

--- azurerm/internal/services/trafficmanager/traffic_manager_profile_data_source.go
+++ azurerm/internal/services/trafficmanager/traffic_manager_profile_data_source.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"time"
 
-
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
