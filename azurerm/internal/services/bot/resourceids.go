a557e32e11293bf4d417b81a9d43af7b23713334
tombuildsstuff <git@ibuildstuff.co.uk>
2020-12-04 13:35:07 +0100

    bot: generating the Resource ID Formatter / Parser for Bot Channel

--- azurerm/internal/services/bot/resourceids.go
+++ azurerm/internal/services/bot/resourceids.go
@@ -1,4 +1,5 @@
 package bot
 
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=BotChannel -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.BotService/botServices/botService1/channels/Discovery1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=BotConnection -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.BotService/botServices/botService1/connections/connection1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=BotService -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.BotService/botServices/botService1
