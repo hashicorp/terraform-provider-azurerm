17a297e665213108e83aed89c1227c01806f4185
yupwei68 <yupwei@microsoft.com>
2020-12-04 17:13:46 +0800

    v1

---
+++ azurerm/internal/services/digitaltwins/digital_twins_endpoint_servicebus_resource_test.go
@@ -0,0 +1,308 @@
+package digitaltwins_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance/check"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/digitaltwins/parse"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
+)
+
+type DigitalTwinsEndpointServiceBusResource struct{}
+
+func TestAccDigitalTwinsEndpointServicebus_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_digital_twins_endpoint_servicebus", "test")
+	r := DigitalTwinsEndpointServiceBusResource{}
+
+	data.ResourceTest(t, r, []resource.TestStep{
+		{
+			Config: r.basic(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("servicebus_primary_connection_string", "servicebus_secondary_connection_string"),
+	})
+}
+
+func TestAccDigitalTwinsEndpointServicebus_requiresImport(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_digital_twins_endpoint_servicebus", "test")
+	r := DigitalTwinsEndpointServiceBusResource{}
+
+	data.ResourceTest(t, r, []resource.TestStep{
+		{
+			Config: r.basic(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.RequiresImportErrorStep(r.requiresImport),
+	})
+}
+
+func TestAccDigitalTwinsEndpointServicebus_updateServiceBus(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_digital_twins_endpoint_servicebus", "test")
+	r := DigitalTwinsEndpointServiceBusResource{}
+
+	data.ResourceTest(t, r, []resource.TestStep{
+		{
+			Config: r.basic(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("servicebus_primary_connection_string", "servicebus_secondary_connection_string"),
+		{
+			Config: r.updateServiceBus(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("servicebus_primary_connection_string", "servicebus_secondary_connection_string"),
+		{
+			Config: r.updateServiceBusRestore(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("servicebus_primary_connection_string", "servicebus_secondary_connection_string"),
+	})
+}
+
+func TestAccDigitalTwinsEndpointServicebus_updateDeadLetter(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_digital_twins_endpoint_servicebus", "test")
+	r := DigitalTwinsEndpointServiceBusResource{}
+
+	data.ResourceTest(t, r, []resource.TestStep{
+		{
+			Config: r.basic(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("servicebus_primary_connection_string", "servicebus_secondary_connection_string"),
+		{
+			Config: r.updateDeadLetter(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("servicebus_primary_connection_string", "servicebus_secondary_connection_string", "dead_letter_storage_secret"),
+		{
+			Config: r.updateDeadLetterRestore(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("servicebus_primary_connection_string", "servicebus_secondary_connection_string", "dead_letter_storage_secret"),
+	})
+}
+
+func (r DigitalTwinsEndpointServiceBusResource) Exists(ctx context.Context, client *clients.Client, state *terraform.InstanceState) (*bool, error) {
+	id, err := parse.DigitalTwinsEndpointID(state.ID)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := client.DigitalTwins.EndpointClient.Get(ctx, id.ResourceGroup, id.DigitalTwinsInstanceName, id.EndpointName)
+	if err != nil {
+		if utils.ResponseWasNotFound(resp.Response) {
+			return utils.Bool(false), nil
+		}
+		return nil, fmt.Errorf("retrieving Digital Twins Service Bus Endpoint %q (Resource Group %q / Digital Twins Instance Name %q): %+v", id.EndpointName, id.ResourceGroup, id.DigitalTwinsInstanceName, err)
+	}
+
+	return utils.Bool(true), nil
+}
+
+func (r DigitalTwinsEndpointServiceBusResource) template(data acceptance.TestData) string {
+	iR := DigitalTwinsInstanceResource{}
+	digitalTwinsInstance := iR.basic(data)
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_servicebus_namespace" "test" {
+  name                = "acctestservicebusnamespace-%[2]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "Standard"
+}
+
+resource "azurerm_servicebus_topic" "test" {
+  name                = "acctestservicebustopic-%[2]d"
+  namespace_name      = azurerm_servicebus_namespace.test.name
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_servicebus_topic_authorization_rule" "test" {
+  name                = "acctest-rule-%[2]d"
+  namespace_name      = azurerm_servicebus_namespace.test.name
+  resource_group_name = azurerm_resource_group.test.name
+  topic_name          = azurerm_servicebus_topic.test.name
+
+  listen = false
+  send   = true
+  manage = false
+}
+`, digitalTwinsInstance, data.RandomInteger)
+}
+
+func (r DigitalTwinsEndpointServiceBusResource) basic(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_digital_twins_endpoint_servicebus" "test" {
+  name                                   = "acctest-EndpointSB-%d"
+  digital_twins_id                       = azurerm_digital_twins_instance.test.id
+  servicebus_primary_connection_string   = azurerm_servicebus_topic_authorization_rule.test.primary_connection_string
+  servicebus_secondary_connection_string = azurerm_servicebus_topic_authorization_rule.test.secondary_connection_string
+}
+`, r.template(data), data.RandomInteger)
+}
+
+func (r DigitalTwinsEndpointServiceBusResource) requiresImport(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_digital_twins_endpoint_servicebus" "import" {
+  name                                   = azurerm_digital_twins_endpoint_servicebus.test.name
+  digital_twins_id                       = azurerm_digital_twins_endpoint_servicebus.test.digital_twins_id
+  servicebus_primary_connection_string   = azurerm_digital_twins_endpoint_servicebus.test.primary_connection_string
+  servicebus_secondary_connection_string = azurerm_digital_twins_endpoint_servicebus.test.secondary_connection_string
+}
+`, r.basic(data))
+}
+
+func (r DigitalTwinsEndpointServiceBusResource) updateServiceBus(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_servicebus_namespace" "test_alt" {
+  name                = "acctestservicebusnamespace-alt-%[2]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "basic"
+}
+
+resource "azurerm_servicebus_topic" "test_alt" {
+  name                = "acctestservicebustopic-alt-%[2]d"
+  namespace_name      = azurerm_servicebus_namespace.test.name
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_servicebus_topic_authorization_rule" "test_alt" {
+  name                = "acctest-rule-alt-%[2]d"
+  namespace_name      = azurerm_servicebus_namespace.test.name
+  resource_group_name = azurerm_resource_group.test.name
+  topic_name          = azurerm_servicebus_topic.test.name
+
+  listen = false
+  send   = true
+  manage = false
+}
+
+resource "azurerm_digital_twins_endpoint_servicebus" "test" {
+  name                                   = "acctest-EndpointSB-%[2]d"
+  digital_twins_id                       = azurerm_digital_twins_instance.test.id
+  servicebus_primary_connection_string   = azurerm_servicebus_topic_authorization_rule.test_alt.primary_connection_string
+  servicebus_secondary_connection_string = azurerm_servicebus_topic_authorization_rule.test_alt.secondary_connection_string
+}
+`, r.template(data), data.RandomInteger)
+}
+
+func (r DigitalTwinsEndpointServiceBusResource) updateServiceBusRestore(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_servicebus_namespace" "test_alt" {
+  name                = "acctestservicebusnamespace-alt-%[2]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "basic"
+}
+
+resource "azurerm_servicebus_topic" "test_alt" {
+  name                = "acctestservicebustopic-alt-%[2]d"
+  namespace_name      = azurerm_servicebus_namespace.test.name
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_servicebus_topic_authorization_rule" "test_alt" {
+  name                = "acctest-rule-alt-%[2]d"
+  namespace_name      = azurerm_servicebus_namespace.test.name
+  resource_group_name = azurerm_resource_group.test.name
+  topic_name          = azurerm_servicebus_topic.test.name
+
+  listen = false
+  send   = true
+  manage = false
+}
+
+resource "azurerm_digital_twins_endpoint_servicebus" "test" {
+  name                                   = "acctest-EndpointSB-%[2]d"
+  digital_twins_id                       = azurerm_digital_twins_instance.test.id
+  servicebus_primary_connection_string   = azurerm_servicebus_topic_authorization_rule.test.primary_connection_string
+  servicebus_secondary_connection_string = azurerm_servicebus_topic_authorization_rule.test.secondary_connection_string
+}
+`, r.template(data), data.RandomInteger)
+}
+
+func (r DigitalTwinsEndpointServiceBusResource) updateDeadLetter(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_storage_account" "test" {
+  name                     = "acctestacc%[2]s"
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "test" {
+  name                  = "vhds"
+  storage_account_name  = azurerm_storage_account.test.name
+  container_access_type = "private"
+}
+
+resource "azurerm_digital_twins_endpoint_servicebus" "test" {
+  name                                   = "acctest-EndpointSB-%[3]d"
+  digital_twins_id                       = azurerm_digital_twins_instance.test.id
+  servicebus_primary_connection_string   = azurerm_servicebus_namespace.test.primary_connection_string
+  servicebus_secondary_connection_string = azurerm_servicebus_namespace.test.secondary_connection_string
+  dead_letter_storage_secret             = "${azurerm_storage_container.test.id}?${azurerm_storage_account.test.primary_access_key}"
+}
+`, r.template(data), data.RandomString, data.RandomInteger)
+}
+
+func (r DigitalTwinsEndpointServiceBusResource) updateDeadLetterRestore(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_storage_account" "test" {
+  name                     = "acctestacc%[2]s"
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "test" {
+  name                  = "vhds"
+  storage_account_name  = azurerm_storage_account.test.name
+  container_access_type = "private"
+}
+
+resource "azurerm_digital_twins_endpoint_servicebus" "test" {
+  name                                   = "acctest-EndpointSB-%[3]d"
+  digital_twins_id                       = azurerm_digital_twins_instance.test.id
+  servicebus_primary_connection_string   = azurerm_servicebus_namespace.test.primary_connection_string
+  servicebus_secondary_connection_string = azurerm_servicebus_namespace.test.secondary_connection_string
+}
+`, r.template(data), data.RandomString, data.RandomInteger)
+}
