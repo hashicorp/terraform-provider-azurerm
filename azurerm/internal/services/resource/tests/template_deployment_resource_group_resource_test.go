360f189afe603623b12ef8dca86e41d4b5f51a19
neil-yechenwei <yechenwei2007@126.com>
2020-12-04 14:48:10 +0800

    update code

--- azurerm/internal/services/resource/tests/template_deployment_resource_group_resource_test.go
+++ azurerm/internal/services/resource/tests/template_deployment_resource_group_resource_test.go
@@ -797,6 +797,22 @@ func resourceGroupTemplateDeployment_withLastSuccessfulDeploymentConfig(data acc
 	return fmt.Sprintf(`
 %s
 
+resource "azurerm_resource_group_template_deployment" "test2" {
+  name                = "acctest-Deployment-%d"
+  resource_group_name = azurerm_resource_group.test.name
+  deployment_mode     = "Complete"
+
+  template_content = <<TEMPLATE
+{
+  "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {},
+  "variables": {},
+  "resources": []
+}
+TEMPLATE
+}
+
 resource "azurerm_resource_group_template_deployment" "test" {
   name                = "acctest-UpdatedDeployment-%d"
   resource_group_name = azurerm_resource_group.test.name
@@ -818,13 +834,29 @@ TEMPLATE
 
   depends_on = [azurerm_resource_group_template_deployment.test2]
 }
-`, template, data.RandomInteger)
+`, template, data.RandomInteger, data.RandomInteger)
 }
 
 func resourceGroupTemplateDeployment_withSpecificDeploymentConfig(data acceptance.TestData) string {
 	template := resourceGroupTemplateDeployment_template(data)
 	return fmt.Sprintf(`
 %s
+
+resource "azurerm_resource_group_template_deployment" "test2" {
+  name                = "acctest-Deployment-%d"
+  resource_group_name = azurerm_resource_group.test.name
+  deployment_mode     = "Complete"
+
+  template_content = <<TEMPLATE
+{
+  "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {},
+  "variables": {},
+  "resources": []
+}
+TEMPLATE
+}
 
 resource "azurerm_resource_group_template_deployment" "test" {
   name                = "acctest-UpdatedDeployment-%d"
@@ -846,7 +878,7 @@ resource "azurerm_resource_group_template_deployment" "test" {
 }
 TEMPLATE
 }
-`, template, data.RandomInteger)
+`, template, data.RandomInteger, data.RandomInteger)
 }
 
 func resourceGroupTemplateDeployment_template(data acceptance.TestData) string {
@@ -859,21 +891,5 @@ resource "azurerm_resource_group" "test" {
   name     = "acctestRG-deployment-%d"
   location = "%s"
 }
-
-resource "azurerm_resource_group_template_deployment" "test2" {
-  name                = "acctest-Deployment-%d"
-  resource_group_name = azurerm_resource_group.test.name
-  deployment_mode     = "Complete"
-
-  template_content = <<TEMPLATE
-{
-  "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
-  "contentVersion": "1.0.0.0",
-  "parameters": {},
-  "variables": {},
-  "resources": []
-}
-TEMPLATE
-}
-`, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
+`, data.RandomInteger, data.Locations.Primary)
 }
