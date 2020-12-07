e49bede3499df8a07e3760b4fb81b4b4d44079ad
tombuildsstuff <git@ibuildstuff.co.uk>
2020-12-07 20:06:16 +0100

    frontdoor: generating the Resource ID Formatter/Parser/Validator for BackendPool

---
+++ azurerm/internal/services/frontdoor/resourceids.go
@@ -0,0 +1,3 @@
+package frontdoor
+
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=BackendPool -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Network/frontDoors/frontdoor1/backendPools/pool1 -rewrite=true
