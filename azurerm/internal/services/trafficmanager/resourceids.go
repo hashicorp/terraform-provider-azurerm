77723c655e01b67abce0e8effb7980a3fff1683a
tombuildsstuff <git@ibuildstuff.co.uk>
2020-12-07 13:52:08 +0100

    trafficmanager: generating the Resource ID Parser/Validator for Profile

---
+++ azurerm/internal/services/trafficmanager/resourceids.go
@@ -0,0 +1,3 @@
+package trafficmanager
+
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=TrafficManagerProfile -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Network/trafficManagerProfiles/trafficManagerProfile1
