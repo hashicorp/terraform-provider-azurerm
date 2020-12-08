772d121e56110963ff074815f7f5adf677b38c15
tombuildsstuff <git@ibuildstuff.co.uk>
2020-12-08 19:36:23 +0100

    d/redis_cache: switching to use a Resource ID Formatter

---
+++ azurerm/internal/services/redis/resourceids.go
@@ -0,0 +1,5 @@
+package redis
+
+// TODO: fix the generator bug making this `Redi` rather than `Redis`
+
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Cache -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Cache/Redis/redis1
