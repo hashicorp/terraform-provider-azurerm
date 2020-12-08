2138d88cefc8b160bb8e0f82507fe88e66245cbf
Vladimir Lazarenko <favoretti@gmail.com>
2020-12-08 23:17:06 +0100

    Fix `azurerm_security_center_workspace`
    PR #9651 broke `azurerm_security_center_workspace` with passing empty
    subscriptionId to `parse.LogAnalyticsWorkspaceID.ID()`. This fixes the
    error.

--- azurerm/internal/services/securitycenter/security_center_workspace_resource.go
+++ azurerm/internal/services/securitycenter/security_center_workspace_resource.go
@@ -97,7 +97,7 @@ func resourceArmSecurityCenterWorkspaceCreateUpdate(d *schema.ResourceData, meta
 	contact := security.WorkspaceSetting{
 		WorkspaceSettingProperties: &security.WorkspaceSettingProperties{
 			Scope:       utils.String(d.Get("scope").(string)),
-			WorkspaceID: utils.String(workspaceID.ID("")),
+			WorkspaceID: utils.String(workspaceID.ID(client.SubscriptionID)),
 		},
 	}
 
