785a8e1fbf697f5c1adfa17f2723148cca5ef2b9
tombuildsstuff <git@ibuildstuff.co.uk>
2020-12-08 13:43:02 +0100

    r/servicebus_queue_authorization_rule: using the ID Formatter/Parser and enhanced import validation

--- azurerm/internal/services/servicebus/resourceids.go
+++ azurerm/internal/services/servicebus/resourceids.go
@@ -1,6 +1,7 @@
 package servicebus
 
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Queue -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ServiceBus/namespaces/namespace1/queues/queue1
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=QueueAuthorizationRule -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ServiceBus/namespaces/namespace1/queues/queue1/authorizationRules/authorizationRule1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Namespace -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ServiceBus/namespaces/namespace1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=NamespaceNetworkRuleSet -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ServiceBus/namespaces/namespace1/networkrulesets/networkRuleSet1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Subscription -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ServiceBus/namespaces/namespace1/topics/topic1/subscriptions/subscription1
