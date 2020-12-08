6b3fa537c3afbb49d6a39da17b043d2278e1afac
tombuildsstuff <git@ibuildstuff.co.uk>
2020-12-08 13:36:14 +0100

    r/servicebus_queue: switching to use a Resource ID Formatter, Parser and adding Enhanced Import Validation

--- azurerm/internal/services/servicebus/resourceids.go
+++ azurerm/internal/services/servicebus/resourceids.go
@@ -1,5 +1,6 @@
 package servicebus
 
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Queue -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ServiceBus/namespaces/namespace1/queues/queue1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Namespace -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ServiceBus/namespaces/namespace1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=NamespaceNetworkRuleSet -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ServiceBus/namespaces/namespace1/networkrulesets/networkRuleSet1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Subscription -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ServiceBus/namespaces/namespace1/topics/topic1/subscriptions/subscription1
