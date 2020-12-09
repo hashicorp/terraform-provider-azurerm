ee9650ff2cc895d64be7a8172ac309cedd75f4bb
tombuildsstuff <git@ibuildstuff.co.uk>
2020-12-09 07:26:36 +0100

    network: removing the now unnecessary loadbalancers section

--- azurerm/internal/services/network/registration.go
+++ azurerm/internal/services/network/registration.go
@@ -14,7 +14,6 @@ func (r Registration) Name() string {
 // WebsiteCategories returns a list of categories which can be used for the sidebar
 func (r Registration) WebsiteCategories() []string {
 	return []string{
-		"Load Balancer",
 		"Network",
 	}
 }
