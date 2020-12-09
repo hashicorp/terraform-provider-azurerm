ac2cd4985a190498dbe83ced1dee137c37f6df3f
tombuildsstuff <git@ibuildstuff.co.uk>
2020-12-09 06:07:34 +0100

    r/redis_linked_server: switching to use a Resource ID Formatter/Parser

--- azurerm/internal/services/redis/resourceids.go
+++ azurerm/internal/services/redis/resourceids.go
@@ -4,3 +4,4 @@ package redis
 
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Cache -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Cache/Redis/redis1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=FirewallRule -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Cache/Redis/redis1/firewallRules/firewallRule1
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=LinkedServer -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Cache/Redis/redis1/linkedServers/linkedServer1
