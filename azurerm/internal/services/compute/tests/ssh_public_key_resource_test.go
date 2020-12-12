4183850af0130d4b10764af556230452a1c9fdc3
Ali Allomani <ali.allomani@wirecard.com>
2020-12-12 06:39:53 +0100

    test fix

--- azurerm/internal/services/compute/tests/ssh_public_key_resource_test.go
+++ azurerm/internal/services/compute/tests/ssh_public_key_resource_test.go
@@ -5,16 +5,16 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2020-06-01/compute"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/compute/parse"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
 func TestAccAzureSshPublicKey_CreateUpdate(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_ssh_public_key", "test")
-	var d compute.SSHPublicKeyResource
 
 	key1 := "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+wWK73dCr+jgQOAxNsHAnNNNMEMWOHYEccp6wJm2gotpr9katuF/ZAdou5AaW1C61slRkHRkpRRX9FA9CYBiitZgvCCz+3nWNN7l/Up54Zps/pHWGZLHNJZRYyAB6j5yVLMVHIHriY49d/GZTZVNB8GoJv9Gakwc/fuEZYYl4YDFiGMBP///TzlI4jhiJzjKnEvqPFki5p2ZRJqcbCiF4pJrxUQR/RXqVFQdbRLZgYfJ8xGB878RENq3yQ39d8dVOkq4edbkzwcUmwwwkYVPIoDGsYLaRHnG+To7FvMeyO7xDVQkMKzopTQV8AuKpyvpqu0a9pWOMaiCyDytO7GGN you@me.com"
 	key2 := "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0/NDMj2wG6bSa6jbn6E3LYlUsYiWMp1CQ2sGAijPALW6OrSu30lz7nKpoh8Qdw7/A4nAJgweI5Oiiw5/BOaGENM70Go+VM8LQMSxJ4S7/8MIJEZQp5HcJZ7XDTcEwruknrd8mllEfGyFzPvJOx6QAQocFhXBW6+AlhM3gn/dvV5vdrO8ihjET2GoDUqXPYC57ZuY+/Fz6W3KV8V97BvNUhpY5yQrP5VpnyvvXNFQtzDfClTvZFPuoHQi3/KYPi6O0FSD74vo8JOBZZY09boInPejkm9fvHQqfh0bnN7B6XJoUwC1Qprrx+XIy7ust5AEn5XL7d4lOvcR14MxDDKEp you@me.com"
@@ -27,7 +27,7 @@ func TestAccAzureSshPublicKey_CreateUpdate(t *testing.T) {
 			{
 				Config: testAccAzureSshPublicKey_template(data, key1),
 				Check: resource.ComposeTestCheckFunc(
-					testCheckAzureSshPublicKeyExists(data.ResourceName, &d, true),
+					testCheckAzureSshPublicKeyExists(data.ResourceName),
 					resource.TestCheckResourceAttr(data.ResourceName, "public_key", key1),
 				),
 			},
@@ -35,7 +35,7 @@ func TestAccAzureSshPublicKey_CreateUpdate(t *testing.T) {
 			{
 				Config: testAccAzureSshPublicKey_template(data, key2),
 				Check: resource.ComposeTestCheckFunc(
-					testCheckAzureSshPublicKeyExists(data.ResourceName, &d, true),
+					testCheckAzureSshPublicKeyExists(data.ResourceName),
 					resource.TestCheckResourceAttr(data.ResourceName, "public_key", key2),
 				),
 			},
@@ -44,7 +44,7 @@ func TestAccAzureSshPublicKey_CreateUpdate(t *testing.T) {
 	})
 }
 
-func testCheckAzureSshPublicKeyExists(resourceName string, d *compute.SSHPublicKeyResource, shouldExist bool) resource.TestCheckFunc {
+func testCheckAzureSshPublicKeyExists(resourceName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		client := acceptance.AzureProvider.Meta().(*clients.Client).Compute.SSHPublicKeysClient
 		ctx := acceptance.AzureProvider.Meta().(*clients.Client).StopContext
@@ -54,25 +54,19 @@ func testCheckAzureSshPublicKeyExists(resourceName string, d *compute.SSHPublicK
 			return fmt.Errorf("Not found: %s", resourceName)
 		}
 
-		dName := rs.Primary.Attributes["name"]
-		resourceGroup, hasResourceGroup := rs.Primary.Attributes["resource_group_name"]
-		if !hasResourceGroup {
-			return fmt.Errorf("Bad: no resource group found in state for ssh public key: %s", dName)
+		id, err := parse.SSHPublicKeyID(rs.Primary.ID)
+		if err != nil {
+			return err
 		}
 
-		resp, err := client.Get(ctx, resourceGroup, dName)
+		resp, err := client.Get(ctx, id.ResourceGroup, id.Name)
 		if err != nil {
+			if utils.ResponseWasNotFound(resp.Response) {
+				return fmt.Errorf("Bad: SSH Public Key %q (Resource Group: %q) does not exist", id.Name, id.ResourceGroup)
+			}
+
 			return fmt.Errorf("Bad: Get on SSHPublicKeysClient: %+v", err)
 		}
-
-		if resp.StatusCode == http.StatusNotFound && shouldExist {
-			return fmt.Errorf("Bad: SSH Public Key %q (resource group %q) does not exist", dName, resourceGroup)
-		}
-		if resp.StatusCode != http.StatusNotFound && !shouldExist {
-			return fmt.Errorf("Bad: SSH Public Key %q (resource group %q) still exists", dName, resourceGroup)
-		}
-
-		*d = resp
 
 		return nil
 	}
@@ -110,7 +104,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "test" {
-	name     = "acctestRG-%d"
+	name     = "ACCTESTRG-%d"
 	location = "%s"
   }
 
