bd587c6f06089a3dee9e014651547acbab8006a4
neil-yechenwei <yechenwei2007@126.com>
2020-12-14 11:28:56 +0800

    update code

--- azurerm/internal/services/sentinel/registration.go
+++ azurerm/internal/services/sentinel/registration.go
@@ -28,7 +28,7 @@ func (r Registration) SupportedDataSources() map[string]*schema.Resource {
 // SupportedResources returns the supported Resources supported by this Service
 func (r Registration) SupportedResources() map[string]*schema.Resource {
 	return map[string]*schema.Resource{
-		"azurerm_sentinel_alert_rule_fusion":               resourceArmSentinelAlertRuleFusion(),
+		"azurerm_sentinel_alert_rule_fusion":               resourceSentinelAlertRuleFusion(),
 		"azurerm_sentinel_alert_rule_ms_security_incident": resourceSentinelAlertRuleMsSecurityIncident(),
 		"azurerm_sentinel_alert_rule_scheduled":            resourceSentinelAlertRuleScheduled(),
 	}
