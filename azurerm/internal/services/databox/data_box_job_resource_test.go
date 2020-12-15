f6305535f4b4e526d54a92c8147e58f9cef6dab9
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2020-12-14 16:59:24 -0800

    Removed update tests

--- azurerm/internal/services/databox/data_box_job_resource_test.go
+++ azurerm/internal/services/databox/data_box_job_resource_test.go
@@ -62,35 +62,6 @@ func TestAccDataBoxJob_requiresImport(t *testing.T) {
 	})
 }
 
-func TestAccDataBoxJob_update(t *testing.T) {
-	data := acceptance.BuildTestData(t, "azurerm_data_box_job", "test")
-	r := DataBoxJobResource{}
-
-	data.ResourceTest(t, r, []resource.TestStep{
-		{
-			Config: r.basic(data),
-			Check: resource.ComposeTestCheckFunc(
-				check.That(data.ResourceName).ExistsInAzure(r),
-			),
-		},
-		data.ImportStep("databox_disk_passkey", "expected_data_size_in_tb"),
-		{
-			Config: r.complete(data),
-			Check: resource.ComposeTestCheckFunc(
-				check.That(data.ResourceName).ExistsInAzure(r),
-			),
-		},
-		data.ImportStep("databox_disk_passkey", "expected_data_size_in_tb"),
-		{
-			Config: r.basic(data),
-			Check: resource.ComposeTestCheckFunc(
-				check.That(data.ResourceName).ExistsInAzure(r),
-			),
-		},
-		data.ImportStep("databox_disk_passkey", "expected_data_size_in_tb"),
-	})
-}
-
 func TestAccDataBoxJob_withCustomerManaged(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_data_box_job", "test")
 	r := DataBoxJobResource{}
@@ -265,87 +236,6 @@ resource "azurerm_data_box_job" "test" {
 
   tags = {
     env = "TEST"
-  }
-}
-`, template, data.RandomString, data.RandomString, data.RandomString)
-}
-
-func (DataBoxJobResource) update(data acceptance.TestData) string {
-	template := testAccDataBoxJob_template(data)
-	return fmt.Sprintf(`
-%s
-
-data "azurerm_subscription" "current" {}
-
-resource "azurerm_storage_account" "test2" {
-  name                = "acctestrgstorage2%s"
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-
-  account_kind             = "StorageV2"
-  account_tier             = "Standard"
-  account_replication_type = "RAGRS"
-}
-
-resource "azurerm_data_box_job" "test" {
-  name                = "acctest-DataBox-%s"
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-
-  contact_details {
-    name         = "TerraForm Test"
-    emails       = ["some.user@example.com", "some.user2@example.com"]
-    phone_number = "+11234567892"
-    phone_mobile = "+11234567892"
-    notification_preference {
-      at_azure_dc     = true
-      data_copied     = true
-      delivered       = true
-      device_prepared = false
-      dispatched      = true
-      picked_up       = true
-    }
-    phone_extension = "124"
-  }
-
-  destination_storage_account {
-    storage_account_id = azurerm_storage_account.test2.id
-    share_password     = "fddbc123123aa@"
-  }
-
-  destination_managed_disk {
-    resource_group_id          = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/TestManagedRG%s"
-    staging_storage_account_id = azurerm_storage_account.test.id
-  }
-
-  shipping_address {
-    city                  = "Redmond"
-    country               = "US"
-    postal_code           = "98052"
-    state_or_province     = "WA"
-    street_address_1      = "One Microsoft Way"
-    address_type          = "Residential"
-    company_name          = "Microsoft"
-    street_address_2      = "Four Microsoft Way"
-    street_address_3      = "Five Microsoft Way"
-    postal_code_plus_four = "6399"
-  }
-
-  expected_data_size_in_tb = 5
-  databox_disk_passkey     = "abcabc123123@"
-
-  databox_preferred_disk {
-    size_in_tb = 2
-    count      = 5
-  }
-
-  sku_name                     = "DataBoxDisk"
-  datacenter_region_preference = ["westus", "westcentralus"]
-  delivery_type                = "NonScheduled"
-  preferred_shipment_type      = "MicrosoftManaged"
-
-  tags = {
-    env = "TEST2"
   }
 }
 `, template, data.RandomString, data.RandomString, data.RandomString)
