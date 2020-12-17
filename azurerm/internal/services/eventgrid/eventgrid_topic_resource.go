9def93f310e0f08e397e5d9fa8443aaddaf32017
Jochen Rauschenbusch <jochen.rauschenbusch@gmail.com>
2020-12-17 19:44:09 +0100

    eventgrid support for inbound ip filter

--- azurerm/internal/services/eventgrid/eventgrid_topic_resource.go
+++ azurerm/internal/services/eventgrid/eventgrid_topic_resource.go
@@ -136,6 +136,10 @@ func resourceEventGridTopic() *schema.Resource {
 				},
 			},
 
+			"public_network_access_enabled": eventSubscriptionPublicNetworkAccessEnabled(),
+
+			"inbound_ip_rule": eventSubscriptionInboundIPRule(),
+
 			"endpoint": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -170,7 +174,7 @@ func resourceEventGridTopicCreateUpdate(d *schema.ResourceData, meta interface{}
 		existing, err := client.Get(ctx, resourceGroup, name)
 		if err != nil {
 			if !utils.ResponseWasNotFound(existing.Response) {
-				return fmt.Errorf("Error checking for presence of existing EventGrid Topic %q (Resource Group %q): %s", name, resourceGroup, err)
+				return fmt.Errorf("checking for presence of existing EventGrid Topic %q (Resource Group %q): %s", name, resourceGroup, err)
 			}
 		}
 
@@ -183,8 +187,10 @@ func resourceEventGridTopicCreateUpdate(d *schema.ResourceData, meta interface{}
 	t := d.Get("tags").(map[string]interface{})
 
 	topicProperties := &eventgrid.TopicProperties{
-		InputSchemaMapping: expandAzureRmEventgridTopicInputMapping(d),
-		InputSchema:        eventgrid.InputSchema(d.Get("input_schema").(string)),
+		InputSchemaMapping:  expandAzureRmEventgridTopicInputMapping(d),
+		InputSchema:         eventgrid.InputSchema(d.Get("input_schema").(string)),
+		PublicNetworkAccess: expandPublicNetworkAccess(d),
+		InboundIPRules:      expandInboundIPRules(d),
 	}
 
 	properties := eventgrid.Topic{
@@ -209,7 +215,7 @@ func resourceEventGridTopicCreateUpdate(d *schema.ResourceData, meta interface{}
 		return err
 	}
 	if read.ID == nil {
-		return fmt.Errorf("Cannot read EventGrid Topic %s (resource group %s) ID", name, resourceGroup)
+		return fmt.Errorf("reading EventGrid Topic %s (resource group %s) ID", name, resourceGroup)
 	}
 
 	d.SetId(*read.ID)
@@ -235,7 +241,7 @@ func resourceEventGridTopicRead(d *schema.ResourceData, meta interface{}) error 
 			return nil
 		}
 
-		return fmt.Errorf("Error making Read request on EventGrid Topic '%s': %+v", id.Name, err)
+		return fmt.Errorf("making Read request on EventGrid Topic '%s': %+v", id.Name, err)
 	}
 	if props := resp.TopicProperties; props != nil {
 		d.Set("endpoint", props.Endpoint)
@@ -247,7 +253,7 @@ func resourceEventGridTopicRead(d *schema.ResourceData, meta interface{}) error 
 			return fmt.Errorf("Unable to flatten `input_schema_mapping_fields` for EventGrid Topic %q (Resource Group %q): %s", id.Name, id.ResourceGroup, err)
 		}
 		if err := d.Set("input_mapping_fields", inputMappingFields); err != nil {
-			return fmt.Errorf("Error setting `input_schema_mapping_fields` for EventGrid Topic %q (Resource Group %q): %s", id.Name, id.ResourceGroup, err)
+			return fmt.Errorf("setting `input_schema_mapping_fields` for EventGrid Topic %q (Resource Group %q): %s", id.Name, id.ResourceGroup, err)
 		}
 
 		inputMappingDefaultValues, err := flattenAzureRmEventgridTopicInputMappingDefaultValues(props.InputSchemaMapping)
@@ -255,13 +261,23 @@ func resourceEventGridTopicRead(d *schema.ResourceData, meta interface{}) error 
 			return fmt.Errorf("Unable to flatten `input_schema_mapping_default_values` for EventGrid Topic %q (Resource Group %q): %s", id.Name, id.ResourceGroup, err)
 		}
 		if err := d.Set("input_mapping_default_values", inputMappingDefaultValues); err != nil {
-			return fmt.Errorf("Error setting `input_schema_mapping_fields` for EventGrid Topic %q (Resource Group %q): %s", id.Name, id.ResourceGroup, err)
+			return fmt.Errorf("setting `input_schema_mapping_fields` for EventGrid Topic %q (Resource Group %q): %s", id.Name, id.ResourceGroup, err)
+		}
+
+		publicNetworkAccessEnabled := flattenPublicNetworkAccess(props.PublicNetworkAccess)
+		if err := d.Set("public_network_access_enabled", publicNetworkAccessEnabled); err != nil {
+			return fmt.Errorf("setting `public_network_access_enabled` in EventGrid Topic %q (Resource Group %q): %+v", id.Name, id.ResourceGroup, err)
+		}
+
+		inboundIPRules := flattenInboundIPRules(props.InboundIPRules)
+		if err := d.Set("inbound_ip_rule", inboundIPRules); err != nil {
+			return fmt.Errorf("setting `inbound_ip_rule` in EventGrid Topic %q (Resource Group %q): %+v", id.Name, id.ResourceGroup, err)
 		}
 	}
 
 	keys, err := client.ListSharedAccessKeys(ctx, id.ResourceGroup, id.Name)
 	if err != nil {
-		return fmt.Errorf("Error retrieving Shared Access Keys for EventGrid Topic '%s': %+v", id.Name, err)
+		return fmt.Errorf("retrieving Shared Access Keys for EventGrid Topic '%s': %+v", id.Name, err)
 	}
 
 	d.Set("name", resp.Name)
@@ -295,14 +311,14 @@ func resourceEventGridTopicDelete(d *schema.ResourceData, meta interface{}) erro
 		if response.WasNotFound(future.Response()) {
 			return nil
 		}
-		return fmt.Errorf("Error deleting EventGrid Topic %q: %+v", id.Name, err)
+		return fmt.Errorf("deleting EventGrid Topic %q: %+v", id.Name, err)
 	}
 
 	if err = future.WaitForCompletionRef(ctx, client.Client); err != nil {
 		if response.WasNotFound(future.Response()) {
 			return nil
 		}
-		return fmt.Errorf("Error deleting EventGrid Topic %q: %+v", id.Name, err)
+		return fmt.Errorf("deleting EventGrid Topic %q: %+v", id.Name, err)
 	}
 
 	return nil
