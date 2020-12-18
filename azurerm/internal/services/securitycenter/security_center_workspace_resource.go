cff7cc2166a91942d1384d6761d75665c70b1263
Vladimir Lazarenko <favoretti@gmail.com>
2020-12-18 09:36:07 +0100

    Added nil checking

--- azurerm/internal/services/securitycenter/security_center_workspace_resource.go
+++ azurerm/internal/services/securitycenter/security_center_workspace_resource.go
@@ -188,11 +188,13 @@ func resourceSecurityCenterWorkspaceRead(d *schema.ResourceData, meta interface{
 	if properties := resp.WorkspaceSettingProperties; properties != nil {
 		d.Set("scope", properties.Scope)
 		workspaceId := ""
-		id, err := parse.LogAnalyticsWorkspaceID(*properties.WorkspaceID)
-		if err != nil {
-			return fmt.Errorf("Reading Security Center Log Analytics Workspace ID: %+v", err)
+		if properties.WorkspaceID != nil {
+			id, err := parse.LogAnalyticsWorkspaceID(*properties.WorkspaceID)
+			if err != nil {
+				return fmt.Errorf("Reading Security Center Log Analytics Workspace ID: %+v", err)
+			}
+			workspaceId = id.ID()
 		}
-		workspaceId = id.ID()
 		d.Set("workspace_id", utils.String(workspaceId))
 	}
 
