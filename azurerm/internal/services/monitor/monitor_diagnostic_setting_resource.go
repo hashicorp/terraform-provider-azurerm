31341dda6bf1c8771b2f57e54522961872dfcddd
Rob Selway <rob.selway@gmail.com>
2020-12-29 21:23:51 +0000

    azurerm_monitor_diagnostic_setting name validation (#10014)
    Fixes #9083
    
    I used the validation rules presented in the portal:
    
    The alert name can contain any characters, but the following characters: <, >, *, %, &, :, \, ?, +, /
    Required
    Max 260 characters long

--- azurerm/internal/services/monitor/monitor_diagnostic_setting_resource.go
+++ azurerm/internal/services/monitor/monitor_diagnostic_setting_resource.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
 	eventhubParse "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/eventhub/parse"
 	eventhubValidate "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/eventhub/validate"
@@ -44,12 +45,10 @@ func resourceMonitorDiagnosticSetting() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-				// NOTE: there's no validation requirements listed for this
-				// so we're intentionally doing the minimum we can here
-				ValidateFunc: validation.StringIsNotEmpty,
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validate.MonitorDiagnosticSettingName,
 			},
 
 			"target_resource_id": {
