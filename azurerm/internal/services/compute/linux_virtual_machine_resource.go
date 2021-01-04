635328c9cc3e4c5b0a72bb93b036b3f114e075ea
tombuildsstuff <git@ibuildstuff.co.uk>
2021-01-04 19:10:42 +0100

    r/linux|windows_virtual_machine: opting-out of the force-delete for now
    This isn't immediately clear, but is only in Preview for now so match the existing behaviour

--- azurerm/internal/services/compute/linux_virtual_machine_resource.go
+++ azurerm/internal/services/compute/linux_virtual_machine_resource.go
@@ -1100,7 +1100,10 @@ func resourceLinuxVirtualMachineDelete(d *schema.ResourceData, meta interface{})
 	log.Printf("[DEBUG] Powered Off Linux Virtual Machine %q (Resource Group %q).", id.Name, id.ResourceGroup)
 
 	log.Printf("[DEBUG] Deleting Linux Virtual Machine %q (Resource Group %q)..", id.Name, id.ResourceGroup)
-	forceDeletion := utils.Bool(skipShutdown) // TODO: support force deletion via a separate feature-flag
+	// @tombuildsstuff: sending `nil` here omits this value from being sent - which matches
+	// the previous behaviour - we're only splitting this out so it's clear why
+	// TODO: support force deletion once it's out of Preview, if applicable
+	var forceDeletion *bool = nil
 	deleteFuture, err := client.Delete(ctx, id.ResourceGroup, id.Name, forceDeletion)
 	if err != nil {
 		return fmt.Errorf("deleting Linux Virtual Machine %q (Resource Group %q): %+v", id.Name, id.ResourceGroup, err)
