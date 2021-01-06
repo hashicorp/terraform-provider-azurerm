58c6c75b06b35105476f30b75296a8d1289a9201
Ali Allomani <ali.allomani@wirecard.com>
2021-01-06 14:23:42 +0100

    added missing imports

--- azurerm/internal/services/compute/ssh_public_key_data_source.go
+++ azurerm/internal/services/compute/ssh_public_key_data_source.go
@@ -2,9 +2,11 @@ package compute
 
 import (
 	"fmt"
+	"regexp"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
