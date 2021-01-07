4220cd7682db37972daf5b2ca1b6f3620b0ab6ea
Matthew Frahry <mbfrahry@gmail.com>
2021-01-06 23:00:40 -0800

    lint

--- azurerm/internal/services/blueprints/blueprint_definition_data_source_test.go
+++ azurerm/internal/services/blueprints/blueprint_definition_data_source_test.go
@@ -108,8 +108,7 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_client_config" "current" {
-}
+data "azurerm_client_config" "current" {}
 
 data "azurerm_management_group" "root" {
   name = data.azurerm_client_config.current.tenant_id
