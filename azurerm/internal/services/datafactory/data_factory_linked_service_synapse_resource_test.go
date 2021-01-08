b271431a42c7e1459e6aacd7a17ab09d00595c98
Matthew Frahry <mbfrahry@gmail.com>
2021-01-08 13:47:48 -0800

    Test Shim DataFactory tests

--- azurerm/internal/services/datafactory/data_factory_linked_service_synapse_resource_test.go
+++ azurerm/internal/services/datafactory/data_factory_linked_service_synapse_resource_test.go
@@ -1,125 +1,82 @@
 package datafactory_test
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance/check"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
+type LinkedServiceSynapseResource struct {
+}
+
 func TestAccDataFactoryLinkedServiceSynapse_ConnectionString(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_data_factory_linked_service_synapse", "test")
+	r := LinkedServiceSynapseResource{}
 
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:     func() { acceptance.PreCheck(t) },
-		Providers:    acceptance.SupportedProviders,
-		CheckDestroy: testCheckDataFactoryLinkedServiceSynapseDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDataFactoryLinkedServiceSynapse_connection_string(data),
-				Check: resource.ComposeTestCheckFunc(
-					testCheckDataFactoryLinkedServiceSynapseExists(data.ResourceName),
-					resource.TestCheckResourceAttr(data.ResourceName, "parameters.%", "2"),
-					resource.TestCheckResourceAttr(data.ResourceName, "annotations.#", "3"),
-					resource.TestCheckResourceAttr(data.ResourceName, "additional_properties.%", "2"),
-					resource.TestCheckResourceAttr(data.ResourceName, "description", "test description"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "connection_string"),
-				),
-			},
-			data.ImportStep(),
+	data.ResourceTest(t, r, []resource.TestStep{
+		{
+			Config: r.connection_string(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("parameters.%").HasValue("2"),
+				check.That(data.ResourceName).Key("annotations.#").HasValue("3"),
+				check.That(data.ResourceName).Key("additional_properties.%").HasValue("2"),
+				check.That(data.ResourceName).Key("description").HasValue("test description"),
+				check.That(data.ResourceName).Key("connection_string").Exists(),
+			),
 		},
+		data.ImportStep(),
 	})
 }
 
 func TestAccDataFactoryLinkedServiceSynapse_KeyVaultReference(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_data_factory_linked_service_synapse", "test")
+	r := LinkedServiceSynapseResource{}
 
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:     func() { acceptance.PreCheck(t) },
-		Providers:    acceptance.SupportedProviders,
-		CheckDestroy: testCheckDataFactoryLinkedServiceSynapseDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDataFactoryLinkedServiceSynapse_key_vault_reference(data),
-				Check: resource.ComposeTestCheckFunc(
-					testCheckDataFactoryLinkedServiceSynapseExists(data.ResourceName),
-					resource.TestCheckResourceAttr(data.ResourceName, "parameters.%", "2"),
-					resource.TestCheckResourceAttr(data.ResourceName, "annotations.#", "3"),
-					resource.TestCheckResourceAttr(data.ResourceName, "additional_properties.%", "2"),
-					resource.TestCheckResourceAttr(data.ResourceName, "description", "test description"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "connection_string"),
-					resource.TestCheckResourceAttrSet(data.ResourceName, "key_vault_password.0.linked_service_name"),
-					resource.TestCheckResourceAttr(data.ResourceName, "key_vault_password.0.secret_name", "secret"),
-				),
-			},
-			data.ImportStep(),
+	data.ResourceTest(t, r, []resource.TestStep{
+		{
+			Config: r.key_vault_reference(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("parameters.%").HasValue("2"),
+				check.That(data.ResourceName).Key("annotations.#").HasValue("3"),
+				check.That(data.ResourceName).Key("additional_properties.%").HasValue("2"),
+				check.That(data.ResourceName).Key("description").HasValue("test description"),
+				check.That(data.ResourceName).Key("connection_string").Exists(),
+				check.That(data.ResourceName).Key("key_vault_password.0.linked_service_name").Exists(),
+				check.That(data.ResourceName).Key("key_vault_password.0.secret_name").HasValue("secret"),
+			),
 		},
+		data.ImportStep(),
 	})
 }
 
-func testCheckDataFactoryLinkedServiceSynapseExists(name string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		client := acceptance.AzureProvider.Meta().(*clients.Client).DataFactory.LinkedServiceClient
-		ctx := acceptance.AzureProvider.Meta().(*clients.Client).StopContext
-
-		// Ensure we have enough information in state to look up in API
-		rs, ok := s.RootModule().Resources[name]
-		if !ok {
-			return fmt.Errorf("Not found: %s", name)
-		}
-
-		name := rs.Primary.Attributes["name"]
-		resourceGroup, hasResourceGroup := rs.Primary.Attributes["resource_group_name"]
-		dataFactoryName := rs.Primary.Attributes["data_factory_name"]
-		if !hasResourceGroup {
-			return fmt.Errorf("Bad: no resource group found in state for Data Factory: %s", name)
-		}
-
-		resp, err := client.Get(ctx, resourceGroup, dataFactoryName, name, "")
-		if err != nil {
-			return fmt.Errorf("Bad: Get on dataFactoryLinkedServiceClient: %+v", err)
-		}
-
-		if utils.ResponseWasNotFound(resp.Response) {
-			return fmt.Errorf("Bad: Data Factory Linked Service Synapse %q (data factory name: %q / resource group: %q) does not exist", name, dataFactoryName, resourceGroup)
-		}
-
-		return nil
+func (t LinkedServiceSynapseResource) Exists(ctx context.Context, clients *clients.Client, state *terraform.InstanceState) (*bool, error) {
+	id, err := azure.ParseAzureResourceID(state.ID)
+	if err != nil {
+		return nil, err
 	}
-}
+	resourceGroup := id.ResourceGroup
+	dataFactoryName := id.Path["factories"]
+	name := id.Path["linkedservices"]
 
-func testCheckDataFactoryLinkedServiceSynapseDestroy(s *terraform.State) error {
-	client := acceptance.AzureProvider.Meta().(*clients.Client).DataFactory.LinkedServiceClient
-	ctx := acceptance.AzureProvider.Meta().(*clients.Client).StopContext
-
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "azurerm_data_factory_linked_service_synapse" {
-			continue
-		}
-
-		name := rs.Primary.Attributes["name"]
-		resourceGroup := rs.Primary.Attributes["resource_group_name"]
-		dataFactoryName := rs.Primary.Attributes["data_factory_name"]
-
-		resp, err := client.Get(ctx, resourceGroup, dataFactoryName, name, "")
-		if err != nil {
-			return nil
-		}
-
-		if resp.StatusCode != http.StatusNotFound {
-			return fmt.Errorf("Data Factory Linked Service Synapse still exists:\n%#v", resp.Properties)
-		}
+	resp, err := clients.DataFactory.LinkedServiceClient.Get(ctx, resourceGroup, dataFactoryName, name, "")
+	if err != nil {
+		return nil, fmt.Errorf("reading Data Factory Linked Service Synapse (%s): %+v", id, err)
 	}
 
-	return nil
+	return utils.Bool(resp.ID != nil), nil
 }
 
-func testAccDataFactoryLinkedServiceSynapse_connection_string(data acceptance.TestData) string {
+func (LinkedServiceSynapseResource) connection_string(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {}
@@ -159,7 +116,7 @@ resource "azurerm_data_factory_linked_service_synapse" "test" {
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
 }
 
-func testAccDataFactoryLinkedServiceSynapse_key_vault_reference(data acceptance.TestData) string {
+func (LinkedServiceSynapseResource) key_vault_reference(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {}
