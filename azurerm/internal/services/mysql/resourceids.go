5c800ffbe0501669b136981f5f610bc86a8faaef
tombuildsstuff <git@ibuildstuff.co.uk>
2021-01-08 18:09:47 +0100

    mysql: generating the Configuration Resource ID

--- azurerm/internal/services/mysql/resourceids.go
+++ azurerm/internal/services/mysql/resourceids.go
@@ -1,4 +1,5 @@
 package mysql
 
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Configuration -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.DBforMySQL/servers/server1/configurations/config1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Key -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.DBforMySQL/servers/server1/keys/key1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Server -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.DBforMySQL/servers/server1
