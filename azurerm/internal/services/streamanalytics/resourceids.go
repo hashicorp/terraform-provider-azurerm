b86a0a28a43976128be153bfda5ab1dd6289336f
tombuildsstuff <git@ibuildstuff.co.uk>
2021-01-11 10:02:39 +0100

    streamanalytics: generating the Output ID parser

--- azurerm/internal/services/streamanalytics/resourceids.go
+++ azurerm/internal/services/streamanalytics/resourceids.go
@@ -1,3 +1,4 @@
 package streamanalytics
 
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=StreamInput -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.StreamAnalytics/streamingjobs/streamingJob1/inputs/streamInput1
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Output -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.StreamAnalytics/streamingjobs/streamingJob1/outputs/output1
