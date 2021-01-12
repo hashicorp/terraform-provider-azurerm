a8bafb4a6e487fa88c0910cfaa169c7b0158936e
Matthew Frahry <mbfrahry@gmail.com>
2021-01-11 21:46:04 -0800

    Lint

--- azurerm/internal/services/compute/tests/windows_virtual_machine_scale_set_resource_test.go
+++ azurerm/internal/services/compute/tests/windows_virtual_machine_scale_set_resource_test.go
@@ -55,7 +55,7 @@ resource "azurerm_virtual_network" "test" {
 }
 
 resource "azurerm_subnet" "test" {
-name        															         = "internal"
+  name                 = "internal"
   resource_group_name  = azurerm_resource_group.test.name
   virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes     = ["10.0.2.0/24"]
