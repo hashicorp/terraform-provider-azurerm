c9ddeaafc46d4b3e8319263bfcae9060b2b5140e
Vladimir Lazarenko <favoretti@gmail.com>
2021-01-13 12:40:01 +0100

    azurerm_eventhub_namespace: Expose trusted_service_access_enabled in network_rulesets

--- azurerm/internal/services/eventhub/eventhub_namespace_resource_test.go
+++ azurerm/internal/services/eventhub/eventhub_namespace_resource_test.go
@@ -140,6 +140,21 @@ func TestAccEventHubNamespace_standardUpdateIdentity(t *testing.T) {
 	})
 }
 
+func TestAccEventHubNamespace_networkrule_iprule_trusted_services(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_eventhub_namespace", "test")
+	r := EventHubNamespaceResource{}
+
+	data.ResourceTest(t, r, []resource.TestStep{
+		{
+			Config: r.networkrule_iprule_trusted_services(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func TestAccEventHubNamespace_networkrule_iprule(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_eventhub_namespace", "test")
 	r := EventHubNamespaceResource{}
@@ -593,6 +608,35 @@ resource "azurerm_eventhub_namespace" "test" {
 
   network_rulesets {
     default_action = "Deny"
+    ip_rule {
+      ip_mask = "10.0.0.0/16"
+    }
+  }
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
+}
+
+func (EventHubNamespaceResource) networkrule_iprule_trusted_services(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-eh-%d"
+  location = "%s"
+}
+
+resource "azurerm_eventhub_namespace" "test" {
+  name                = "acctesteventhubnamespace-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "Standard"
+  capacity            = "2"
+
+  network_rulesets {
+    default_action                 = "Deny"
+    trusted_service_access_enabled = true
     ip_rule {
       ip_mask = "10.0.0.0/16"
     }
