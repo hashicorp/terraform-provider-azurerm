5a67b9495c32653a3c71c17b766d255df09cb022
Tom Bamford <tom@bamford.io>
2021-01-19 00:51:18 +0000

    new resource azurerm_api_management_identity_provider_aadb2c

---
+++ azurerm/internal/services/apimanagement/api_management_identity_provider_aadb2c_resource.go
@@ -0,0 +1,233 @@
+package apimanagement
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/services/apimanagement/mgmt/2019-12-01/apimanagement"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
+)
+
+func resourceArmApiManagementIdentityProviderAADB2C() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceArmApiManagementIdentityProviderAADB2CCreateUpdate,
+		Read:   resourceArmApiManagementIdentityProviderAADB2CRead,
+		Update: resourceArmApiManagementIdentityProviderAADB2CCreateUpdate,
+		Delete: resourceArmApiManagementIdentityProviderAADB2CDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(30 * time.Minute),
+			Read:   schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(30 * time.Minute),
+			Delete: schema.DefaultTimeout(30 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"resource_group_name": azure.SchemaResourceGroupName(),
+
+			"api_management_name": azure.SchemaApiManagementName(),
+
+			"client_id": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.IsUUID,
+			},
+
+			"client_secret": {
+				Type:         schema.TypeString,
+				Required:     true,
+				Sensitive:    true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			// For AADB2C identity providers, `allowed_tenants` must specify exactly one tenant
+			"allowed_tenant": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"signin_tenant": {
+				Type:     schema.TypeString,
+				Required: true,
+				// B2C tenant domains can be customized, and GUIDs might work here too
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"authority": {
+				Type:     schema.TypeString,
+				Required: true,
+				// B2C login domains can be customized and don't necessarily end in b2clogin.com
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"signup_policy": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"signin_policy": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"profile_editing_policy": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"password_reset_policy": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+		},
+	}
+}
+
+func resourceArmApiManagementIdentityProviderAADB2CCreateUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).ApiManagement.IdentityProviderClient
+	ctx, cancel := timeouts.ForCreateUpdate(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	resourceGroup := d.Get("resource_group_name").(string)
+	serviceName := d.Get("api_management_name").(string)
+
+	clientID := d.Get("client_id").(string)
+	clientSecret := d.Get("client_secret").(string)
+
+	allowedTenant := d.Get("allowed_tenant").(string)
+	signinTenant := d.Get("signin_tenant").(string)
+	authority := d.Get("authority").(string)
+	signupPolicy := d.Get("signup_policy").(string)
+
+	signinPolicy := d.Get("signin_policy").(string)
+	profileEditingPolicy := d.Get("profile_editing_policy").(string)
+	passwordResetPolicy := d.Get("password_reset_policy").(string)
+
+	if d.IsNewResource() {
+		existing, err := client.Get(ctx, resourceGroup, serviceName, apimanagement.AadB2C)
+		if err != nil {
+			if !utils.ResponseWasNotFound(existing.Response) {
+				return fmt.Errorf("checking for presence of existing Identity Provider %q (API Management Service %q / Resource Group %q): %s", apimanagement.AadB2C, serviceName, resourceGroup, err)
+			}
+		}
+
+		if existing.ID != nil && *existing.ID != "" {
+			return tf.ImportAsExistsError("azurerm_api_management_identity_provider_aadb2c", *existing.ID)
+		}
+	}
+
+	parameters := apimanagement.IdentityProviderCreateContract{
+		IdentityProviderCreateContractProperties: &apimanagement.IdentityProviderCreateContractProperties{
+			ClientID:                 utils.String(clientID),
+			ClientSecret:             utils.String(clientSecret),
+			Type:                     apimanagement.AadB2C,
+			AllowedTenants:           utils.ExpandStringSlice([]interface{}{allowedTenant}),
+			SigninTenant:             utils.String(signinTenant),
+			Authority:                utils.String(authority),
+			SignupPolicyName:         utils.String(signupPolicy),
+			SigninPolicyName:         utils.String(signinPolicy),
+			ProfileEditingPolicyName: utils.String(profileEditingPolicy),
+			PasswordResetPolicyName:  utils.String(passwordResetPolicy),
+		},
+	}
+
+	if _, err := client.CreateOrUpdate(ctx, resourceGroup, serviceName, apimanagement.AadB2C, parameters, ""); err != nil {
+		return fmt.Errorf("creating or updating Identity Provider %q (Resource Group %q / API Management Service %q): %+v", apimanagement.AadB2C, resourceGroup, serviceName, err)
+	}
+
+	resp, err := client.Get(ctx, resourceGroup, serviceName, apimanagement.AadB2C)
+	if err != nil {
+		return fmt.Errorf("retrieving Identity Provider %q (Resource Group %q / API Management Service %q): %+v", apimanagement.AadB2C, resourceGroup, serviceName, err)
+	}
+	if resp.ID == nil {
+		return fmt.Errorf("Cannot read ID for Identity Provider %q (Resource Group %q / API Management Service %q)", apimanagement.AadB2C, resourceGroup, serviceName)
+	}
+	d.SetId(*resp.ID)
+
+	return resourceArmApiManagementIdentityProviderAADB2CRead(d, meta)
+}
+
+func resourceArmApiManagementIdentityProviderAADB2CRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).ApiManagement.IdentityProviderClient
+	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	id, err := azure.ParseAzureResourceID(d.Id())
+	if err != nil {
+		return err
+	}
+	resourceGroup := id.ResourceGroup
+	serviceName := id.Path["service"]
+	identityProviderName := id.Path["identityProviders"]
+
+	resp, err := client.Get(ctx, resourceGroup, serviceName, apimanagement.IdentityProviderType(identityProviderName))
+	if err != nil {
+		if utils.ResponseWasNotFound(resp.Response) {
+			log.Printf("[DEBUG] Identity Provider %q (Resource Group %q / API Management Service %q) was not found - removing from state!", identityProviderName, resourceGroup, serviceName)
+			d.SetId("")
+			return nil
+		}
+
+		return fmt.Errorf("making Read request for Identity Provider %q (Resource Group %q / API Management Service %q): %+v", identityProviderName, resourceGroup, serviceName, err)
+	}
+
+	d.Set("resource_group_name", resourceGroup)
+	d.Set("api_management_name", serviceName)
+
+	if props := resp.IdentityProviderContractProperties; props != nil {
+		d.Set("client_id", props.ClientID)
+		d.Set("signin_tenant", props.SigninTenant)
+		d.Set("authority", props.Authority)
+		d.Set("signup_policy", props.SignupPolicyName)
+		d.Set("signin_policy", props.SigninPolicyName)
+		d.Set("profile_editing_policy", props.ProfileEditingPolicyName)
+		d.Set("password_reset_policy", props.PasswordResetPolicyName)
+
+		allowedTenant := ""
+		if allowedTenants := props.AllowedTenants; allowedTenants != nil && len(*allowedTenants) > 0 {
+			t := *allowedTenants
+			allowedTenant = t[0]
+		}
+		d.Set("allowed_tenant", allowedTenant)
+	}
+
+	return nil
+}
+
+func resourceArmApiManagementIdentityProviderAADB2CDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).ApiManagement.IdentityProviderClient
+	ctx, cancel := timeouts.ForDelete(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	id, err := azure.ParseAzureResourceID(d.Id())
+	if err != nil {
+		return err
+	}
+	resourceGroup := id.ResourceGroup
+	serviceName := id.Path["service"]
+	identityProviderName := id.Path["identityProviders"]
+
+	if resp, err := client.Delete(ctx, resourceGroup, serviceName, apimanagement.IdentityProviderType(identityProviderName), ""); err != nil {
+		if !utils.ResponseWasNotFound(resp) {
+			return fmt.Errorf("deleting Identity Provider %q (Resource Group %q / API Management Service %q): %+v", identityProviderName, resourceGroup, serviceName, err)
+		}
+	}
+
+	return nil
+}
---
+++ website/docs/r/api_management_identity_provider_aadb2c.html.markdown
@@ -0,0 +1,105 @@
+---
+subcategory: "API Management"
+layout: "azurerm"
+page_title: "Azure Resource Manager: azurerm_api_management_identity_provider_aadb2c"
+description: |-
+Manages an API Management AADB2C Identity Provider.
+---
+
+# azurerm_api_management_identity_provider_aadb2c
+
+Manages an API Management AADB2C Identity Provider.
+
+## Example Usage
+
+```hcl
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_api_management" "example" {
+  name                = "example-apim"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  publisher_name      = "My Company"
+  publisher_email     = "company@terraform.io"
+  sku_name            = "Developer_1"
+}
+
+resource "azuread_application" "example" {
+  name                       = "acctestAM-%[5]d"
+  oauth2_allow_implicit_flow = true
+  reply_urls                 = ["https://${azurerm_api_management.test.name}.developer.azure-api.net/signin"]
+}
+
+resource "azuread_application_password" "example" {
+  application_object_id = azuread_application.test.object_id
+  end_date_relative     = "36h"
+  value                 = "P@55w0rD!%[7]s"
+}
+
+resource "azurerm_api_management_identity_provider_aadb2c" "example" {
+  api_management_id = azurerm_api_management.example.id
+  client_id         = azuread_application.example.application_id
+  client_secret     = "P@55w0rD!%[7]s"
+  allowed_tenant    = "myb2ctenant.onmicrosoft.com"
+  signin_tenant     = "myb2ctenant.onmicrosoft.com"
+  authority         = "myb2ctenant.b2clogin.com"
+  signin_policy     = "B2C_1_Login"
+  signup_policy     = "B2C_1_Signup"
+
+  depends_on = [azuread_application_password.example]
+}
+```
+
+## Arguments Reference
+
+The following arguments are supported:
+
+* `api_management_name` - (Required) The Name of the API Management Service where this AAD Identity Provider should be created. Changing this forces a new resource to be created.
+
+* `resource_group_name` - (Required) The Name of the Resource Group where the API Management Service exists. Changing this forces a new resource to be created.
+
+* `client_id` - (Required) Client ID of the Application in your B2C tenant.
+
+* `client_secret` - (Required) Client secret of the Application in your B2C tenant.
+
+* `allowed_tenant` - (Required) The allowed AAD tenant, usually your B2C tenant domain.
+
+* `signin_tenant` - (Required) The tenant to use instead of Common when logging into Active Directory, usually your B2C tenant domain.
+
+* `authority` - (Required) OpenID Connect discovery endpoint hostname, usually your b2clogin.com domain.
+
+* `signin_policy` - (Required) Signup Policy Name.
+
+* `signup_policy` - (Required) Signin Policy Name.
+
+---
+
+* `password_reset_policy` - (Optional) Password reset Policy Name.
+
+* `profile_editing_policy` - (Optional) Profile editing Policy Name.
+
+## Attributes Reference
+
+In addition to the Arguments listed above - the following Attributes are exported:
+
+* `id` - The ID of the API Management Identity Provider Resource.
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration/resources.html#timeouts) for certain actions:
+
+* `create` - (Defaults to 30 minutes) Used when creating the API Management Resources.
+* `read` - (Defaults to 5 minutes) Used when retrieving the API Management Resources.
+* `update` - (Defaults to 30 minutes) Used when updating the API Management Resources.
+* `delete` - (Defaults to 30 minutes) Used when deleting the API Management Resources.
+
+## Import
+
+API Management Resourcess can be imported using the `resource id`, e.g.
+
+```shell
+terraform import azurerm_api_management_identity_provider_aadb2c.example /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/mygroup1/providers/Microsoft.ApiManagement/service1/identityProviders/AadB2C
+```
