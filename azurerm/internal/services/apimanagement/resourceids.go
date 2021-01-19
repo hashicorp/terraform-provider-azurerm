ebc320f85b1946635d4b6089a3d8241f7e0d252b
Vladimir Lazarenko <favoretti@gmail.com>
2021-01-19 12:10:21 +0100

    api

--- azurerm/internal/services/apimanagement/resourceids.go
+++ azurerm/internal/services/apimanagement/resourceids.go
@@ -1,5 +1,6 @@
 package apimanagement
 
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Api -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/service1/apis/api1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ApiDiagnostic -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/service1/apis/api1/diagnostics/diagnostic1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ApiManagement -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/service1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ApiVersionSet -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/service1/apiVersionSets/apiVersionSet1
