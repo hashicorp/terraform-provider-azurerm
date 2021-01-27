d27277eeea16d51f53e1625850bcc63c602d15b2
magodo <wztdyl@sina.com>
2021-01-27 16:29:51 +0800

    revert test constraint back

--- azurerm/internal/acceptance/testing.go
+++ azurerm/internal/acceptance/testing.go
@@ -21,8 +21,8 @@ var (
 
 func PreCheck(t *testing.T) {
 	variables := []string{
-		//"ARM_CLIENT_ID",
-		//"ARM_CLIENT_SECRET",
+		"ARM_CLIENT_ID",
+		"ARM_CLIENT_SECRET",
 		"ARM_SUBSCRIPTION_ID",
 		"ARM_TENANT_ID",
 		"ARM_TEST_LOCATION",
