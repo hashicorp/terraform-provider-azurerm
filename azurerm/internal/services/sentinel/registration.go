c9d882d7348b57ceeed83d391ec7da7bd829b65b
magodo <wztdyl@sina.com>
2021-01-27 14:07:47 +0800

    DC: mdatp

--- azurerm/internal/services/sentinel/registration.go
+++ azurerm/internal/services/sentinel/registration.go
@@ -29,15 +29,16 @@ func (r Registration) SupportedDataSources() map[string]*schema.Resource {
 // SupportedResources returns the supported Resources supported by this Service
 func (r Registration) SupportedResources() map[string]*schema.Resource {
 	return map[string]*schema.Resource{
-		"azurerm_sentinel_alert_rule_fusion":                               resourceSentinelAlertRuleFusion(),
-		"azurerm_sentinel_alert_rule_ms_security_incident":                 resourceSentinelAlertRuleMsSecurityIncident(),
-		"azurerm_sentinel_alert_rule_scheduled":                            resourceSentinelAlertRuleScheduled(),
-		"azurerm_sentinel_data_connector_azure_active_directory":           resourceSentinelDataConnectorAzureActiveDirectory(),
-		"azurerm_sentinel_data_connector_azure_advanced_threat_protection": resourceSentinelDataConnectorAzureAdvancedThreatProtection(),
-		"azurerm_sentinel_data_connector_azure_security_center":            resourceSentinelDataConnectorAzureSecurityCenter(),
-		"azurerm_sentinel_data_connector_microsoft_cloud_app_security":     resourceSentinelDataConnectorMicrosoftCloudAppSecurity(),
-		"azurerm_sentinel_data_connector_threat_intelligence":              resourceSentinelDataConnectorThreatIntelligence(),
-		"azurerm_sentinel_data_connector_office_365":                       resourceSentinelDataConnectorOffice365(),
-		"azurerm_sentinel_data_connector_office_atp":                       resourceSentinelDataConnectorOfficeATP(),
+		"azurerm_sentinel_alert_rule_fusion":                                            resourceSentinelAlertRuleFusion(),
+		"azurerm_sentinel_alert_rule_ms_security_incident":                              resourceSentinelAlertRuleMsSecurityIncident(),
+		"azurerm_sentinel_alert_rule_scheduled":                                         resourceSentinelAlertRuleScheduled(),
+		"azurerm_sentinel_data_connector_azure_active_directory":                        resourceSentinelDataConnectorAzureActiveDirectory(),
+		"azurerm_sentinel_data_connector_azure_advanced_threat_protection":              resourceSentinelDataConnectorAzureAdvancedThreatProtection(),
+		"azurerm_sentinel_data_connector_azure_security_center":                         resourceSentinelDataConnectorAzureSecurityCenter(),
+		"azurerm_sentinel_data_connector_microsoft_cloud_app_security":                  resourceSentinelDataConnectorMicrosoftCloudAppSecurity(),
+		"azurerm_sentinel_data_connector_microsoft_defender_advanced_threat_protection": resourceSentinelDataConnectorMicrosoftDefenderAdvancedThreatProtection(),
+		"azurerm_sentinel_data_connector_threat_intelligence":                           resourceSentinelDataConnectorThreatIntelligence(),
+		"azurerm_sentinel_data_connector_office_365":                                    resourceSentinelDataConnectorOffice365(),
+		"azurerm_sentinel_data_connector_office_atp":                                    resourceSentinelDataConnectorOfficeATP(),
 	}
 }
