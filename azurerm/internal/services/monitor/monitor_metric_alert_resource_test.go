39813415fe5377babfc58a65b7c2d96b01305fe0
yupwei68 <yupwei@microsoft.com>
2021-02-02 11:15:17 +0800

    update

--- azurerm/internal/services/monitor/monitor_metric_alert_resource_test.go
+++ azurerm/internal/services/monitor/monitor_metric_alert_resource_test.go
@@ -252,11 +252,12 @@ resource "azurerm_monitor_metric_alert" "test" {
   window_size         = "PT12H"
 
   criteria {
-    metric_namespace = "Microsoft.Storage/storageAccounts"
-    metric_name      = "Transactions"
-    aggregation      = "Total"
-    operator         = "GreaterThan"
-    threshold        = 99
+    metric_namespace       = "Microsoft.Storage/storageAccounts"
+    metric_name            = "Transactions"
+    aggregation            = "Total"
+    operator               = "GreaterThan"
+    threshold              = 99
+    skip_metric_validation = true
 
     dimension {
       name     = "GeoType"
@@ -327,6 +328,11 @@ resource "azurerm_linux_virtual_machine" "test" {
     azurerm_network_interface.test[count.index].id,
   ]
 
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+wWK73dCr+jgQOAxNsHAnNNNMEMWOHYEccp6wJm2gotpr9katuF/ZAdou5AaW1C61slRkHRkpRRX9FA9CYBiitZgvCCz+3nWNN7l/Up54Zps/pHWGZLHNJZRYyAB6j5yVLMVHIHriY49d/GZTZVNB8GoJv9Gakwc/fuEZYYl4YDFiGMBP///TzlI4jhiJzjKnEvqPFki5p2ZRJqcbCiF4pJrxUQR/RXqVFQdbRLZgYfJ8xGB878RENq3yQ39d8dVOkq4edbkzwcUmwwwkYVPIoDGsYLaRHnG+To7FvMeyO7xDVQkMKzopTQV8AuKpyvpqu0a9pWOMaiCyDytO7GGN you@me.com"
+  }
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
@@ -355,8 +361,9 @@ resource "azurerm_monitor_metric_alert" "test" {
     metric_name      = "Network In"
     aggregation      = "Total"
 
-    operator          = "GreaterOrLessThan"
-    alert_sensitivity = "Medium"
+    operator               = "GreaterOrLessThan"
+    alert_sensitivity      = "Medium"
+    skip_metric_validation = true
   }
   window_size              = "PT1H"
   frequency                = "PT5M"
