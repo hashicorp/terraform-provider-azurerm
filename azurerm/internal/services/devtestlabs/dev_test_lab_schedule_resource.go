dd4c688ec09a2fd073cde8ddae2ea0754f0548d9
tombuildsstuff <git@ibuildstuff.co.uk>
2021-02-02 12:38:40 +0100

    devtestlabs: removing the unused import

--- azurerm/internal/services/devtestlabs/dev_test_lab_schedule_resource.go
+++ azurerm/internal/services/devtestlabs/dev_test_lab_schedule_resource.go
@@ -10,7 +10,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
 	computeValidate "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/compute/validate"
 	devTestValidate "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/devtestlabs/validate"
