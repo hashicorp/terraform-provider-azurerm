49de0c6b74b4ddfa00ce9397a263d7e384b72812
yupwei68 <yupwei@microsoft.com>
2021-02-08 10:11:17 +0800

    update

--- azurerm/internal/services/postgres/tests/postgresql_flexible_server_resource_test.go
+++ azurerm/internal/services/postgres/tests/postgresql_flexible_server_resource_test.go
@@ -376,6 +376,7 @@ resource "azurerm_subnet" "test" {
   resource_group_name  = azurerm_resource_group.test.name
   virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
   delegation {
     name = "fs"
     service_delegation {
