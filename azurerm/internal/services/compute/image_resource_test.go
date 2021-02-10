350f70197b3593b790a67374a4b841218b3627de
tombuildsstuff <git@ibuildstuff.co.uk>
2021-02-10 13:14:44 +0100

    image: ubuntu 18 -> 16 for tests

--- azurerm/internal/services/compute/image_resource_test.go
+++ azurerm/internal/services/compute/image_resource_test.go
@@ -446,7 +446,7 @@ resource "azurerm_virtual_machine" "testsource" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
 
