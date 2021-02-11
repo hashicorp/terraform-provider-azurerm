c3146d264e3ffb502a7184b3315cf6b070337f31
Fernando de Oliveira <5161098+fernandoBRS@users.noreply.github.com>
2021-02-11 11:24:20 -0300

    test: azure-ssis integration runtime basic complete

--- azurerm/internal/services/datafactory/data_factory_integration_runtime_azure_ssis_resource_test.go
+++ azurerm/internal/services/datafactory/data_factory_integration_runtime_azure_ssis_resource_test.go
@@ -26,59 +26,44 @@ func TestAccDataFactoryIntegrationRuntimeManagedSsis_basic(t *testing.T) {
 			Config: r.basic(data),
 			Check: resource.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("name").HasValue("managed-integration-runtime"),
+				check.That(data.ResourceName).Key("data_factory_name").Exists(),
+				check.That(data.ResourceName).Key("resource_group_name").Exists(),
+				check.That(data.ResourceName).Key("location").HasValue(azure.NormalizeLocation(data.Locations.Primary)),
+				check.That(data.ResourceName).Key("node_size").HasValue("Standard_D8_v3"),
 			),
 		},
 		data.ImportStep(),
 	})
 }
 
-func TestAccDataFactoryIntegrationRuntimeManagedSsis_vnetIntegration(t *testing.T) {
+func TestAccDataFactoryIntegrationRuntimeManagedSsis_complete(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_data_factory_integration_runtime_azure_ssis", "test")
 	r := IntegrationRuntimeManagedSsisResource{}
 
 	data.ResourceTest(t, r, []resource.TestStep{
 		{
-			Config: r.vnetIntegration(data),
+			Config: r.complete(data),
 			Check: resource.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("name").HasValue("managed-integration-runtime"),
+				check.That(data.ResourceName).Key("description").HasValue("acctest"),
+				check.That(data.ResourceName).Key("data_factory_name").Exists(),
+				check.That(data.ResourceName).Key("resource_group_name").Exists(),
+				check.That(data.ResourceName).Key("location").HasValue(azure.NormalizeLocation(data.Locations.Primary)),
+				check.That(data.ResourceName).Key("node_size").HasValue("Standard_D8_v3"),
+				check.That(data.ResourceName).Key("number_of_nodes").HasValue("2"),
+				check.That(data.ResourceName).Key("max_parallel_executions_per_node").HasValue("8"),
+				check.That(data.ResourceName).Key("edition").HasValue("Standard"),
+				check.That(data.ResourceName).Key("license_type").HasValue("LicenseIncluded"),
 				check.That(data.ResourceName).Key("vnet_integration.#").HasValue("1"),
 				check.That(data.ResourceName).Key("vnet_integration.0.vnet_id").Exists(),
 				check.That(data.ResourceName).Key("vnet_integration.0.subnet_name").Exists(),
-			),
-		},
-		data.ImportStep(),
-	})
-}
-
-func TestAccDataFactoryIntegrationRuntimeManagedSsis_catalogInfo(t *testing.T) {
-	data := acceptance.BuildTestData(t, "azurerm_data_factory_integration_runtime_azure_ssis", "test")
-	r := IntegrationRuntimeManagedSsisResource{}
-
-	data.ResourceTest(t, r, []resource.TestStep{
-		{
-			Config: r.catalogInfo(data),
-			Check: resource.ComposeTestCheckFunc(
-				check.That(data.ResourceName).ExistsInAzure(r),
 				check.That(data.ResourceName).Key("catalog_info.#").HasValue("1"),
 				check.That(data.ResourceName).Key("catalog_info.0.server_endpoint").Exists(),
 				check.That(data.ResourceName).Key("catalog_info.0.administrator_login").HasValue("ssis_catalog_admin"),
 				check.That(data.ResourceName).Key("catalog_info.0.administrator_password").HasValue("my-s3cret-p4ssword!"),
 				check.That(data.ResourceName).Key("catalog_info.0.pricing_tier").HasValue("Basic"),
-			),
-		},
-		data.ImportStep("catalog_info.0.administrator_password"),
-	})
-}
-
-func TestAccDataFactoryIntegrationRuntimeManagedSsis_customSetupScript(t *testing.T) {
-	data := acceptance.BuildTestData(t, "azurerm_data_factory_integration_runtime_azure_ssis", "test")
-	r := IntegrationRuntimeManagedSsisResource{}
-
-	data.ResourceTest(t, r, []resource.TestStep{
-		{
-			Config: r.customSetupScript(data),
-			Check: resource.ComposeTestCheckFunc(
-				check.That(data.ResourceName).ExistsInAzure(r),
 				check.That(data.ResourceName).Key("custom_setup_script.#").HasValue("1"),
 				check.That(data.ResourceName).Key("custom_setup_script.0.blob_container_uri").Exists(),
 				check.That(data.ResourceName).Key("custom_setup_script.0.sas_token").Exists(),
@@ -110,17 +95,12 @@ resource "azurerm_data_factory_integration_runtime_azure_ssis" "test" {
   data_factory_name   = azurerm_data_factory.test.name
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
-
-  node_size                        = "Standard_D8_v3"
-  number_of_nodes                  = 2
-  max_parallel_executions_per_node = 8
-  edition                          = "Standard"
-  license_type                     = "LicenseIncluded"
+  node_size           = "Standard_D8_v3"
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
 }
 
-func (IntegrationRuntimeManagedSsisResource) vnetIntegration(data acceptance.TestData) string {
+func (IntegrationRuntimeManagedSsisResource) complete(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {}
@@ -143,89 +123,6 @@ resource "azurerm_subnet" "test" {
   resource_group_name  = "${azurerm_resource_group.test.name}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
   address_prefix       = "10.0.2.0/24"
-}
-
-resource "azurerm_data_factory" "test" {
-  name                = "acctestdfirm%d"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-}
-
-resource "azurerm_data_factory_integration_runtime_azure_ssis" "test" {
-  name                = "managed-integration-runtime"
-  data_factory_name   = azurerm_data_factory.test.name
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-
-  node_size = "Standard_D8_v3"
-
-  vnet_integration {
-    vnet_id     = "${azurerm_virtual_network.test.id}"
-    subnet_name = "${azurerm_subnet.test.name}"
-  }
-}
-`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger, data.RandomInteger)
-}
-
-func (IntegrationRuntimeManagedSsisResource) catalogInfo(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "test" {
-  name     = "acctestRG-df-%d"
-  location = "%s"
-}
-
-resource "azurerm_data_factory" "test" {
-  name                = "acctestdfirm%d"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-}
-
-resource "azurerm_sql_server" "test" {
-  name                         = "acctestsql%d"
-  resource_group_name          = "${azurerm_resource_group.test.name}"
-  location                     = "${azurerm_resource_group.test.location}"
-  version                      = "12.0"
-  administrator_login          = "ssis_catalog_admin"
-  administrator_login_password = "my-s3cret-p4ssword!"
-}
-
-resource "azurerm_data_factory_integration_runtime_azure_ssis" "test" {
-  name                = "managed-integration-runtime"
-  data_factory_name   = azurerm_data_factory.test.name
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-
-  node_size = "Standard_D8_v3"
-
-  catalog_info {
-    server_endpoint        = "${azurerm_sql_server.test.fully_qualified_domain_name}"
-    administrator_login    = "ssis_catalog_admin"
-    administrator_password = "my-s3cret-p4ssword!"
-    pricing_tier           = "Basic"
-  }
-}
-`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
-}
-
-func (IntegrationRuntimeManagedSsisResource) customSetupScript(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "test" {
-  name     = "acctestRG-df-%d"
-  location = "%s"
-}
-
-resource "azurerm_data_factory" "test" {
-  name                = "acctestdfirm%d"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
 }
 
 resource "azurerm_storage_account" "test" {
@@ -263,20 +160,52 @@ data "azurerm_storage_account_blob_container_sas" "test" {
   }
 }
 
+resource "azurerm_sql_server" "test" {
+  name                         = "acctestsql%d"
+  resource_group_name          = "${azurerm_resource_group.test.name}"
+  location                     = "${azurerm_resource_group.test.location}"
+  version                      = "12.0"
+  administrator_login          = "ssis_catalog_admin"
+  administrator_login_password = "my-s3cret-p4ssword!"
+}
+
+resource "azurerm_data_factory" "test" {
+  name                = "acctestdfirm%d"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
 resource "azurerm_data_factory_integration_runtime_azure_ssis" "test" {
-  name                = "managed-integration-runtime"
+	name                = "managed-integration-runtime"
+	description					= "acctest"
   data_factory_name   = azurerm_data_factory.test.name
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
 
-  node_size = "Standard_D8_v3"
+	node_size 											 = "Standard_D8_v3"
+	number_of_nodes                  = 2
+	max_parallel_executions_per_node = 8
+	edition                          = "Standard"
+	license_type                     = "LicenseIncluded"
 
-  custom_setup_script {
+  vnet_integration {
+    vnet_id     = "${azurerm_virtual_network.test.id}"
+    subnet_name = "${azurerm_subnet.test.name}"
+	}
+
+	catalog_info {
+    server_endpoint        = "${azurerm_sql_server.test.fully_qualified_domain_name}"
+    administrator_login    = "ssis_catalog_admin"
+    administrator_password = "my-s3cret-p4ssword!"
+    pricing_tier           = "Basic"
+  }
+	
+	custom_setup_script {
     blob_container_uri = "${azurerm_storage_account.test.primary_blob_endpoint}/${azurerm_storage_container.test.name}"
     sas_token          = "${data.azurerm_storage_account_blob_container_sas.test.sas}"
   }
 }
-`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomString)
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger, data.RandomString, data.RandomInteger, data.RandomInteger)
 }
 
 func (t IntegrationRuntimeManagedSsisResource) Exists(ctx context.Context, clients *clients.Client, state *terraform.InstanceState) (*bool, error) {
