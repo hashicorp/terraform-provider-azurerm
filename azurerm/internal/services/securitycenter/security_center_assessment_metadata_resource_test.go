226cd4b4c21dfeba81be9f466308a453218335e3
neil-yechenwei <yechenwei2007@126.com>
2021-02-20 12:47:08 +0800

    update code

--- azurerm/internal/services/securitycenter/security_center_assessment_metadata_resource_test.go
+++ azurerm/internal/services/securitycenter/security_center_assessment_metadata_resource_test.go
@@ -94,9 +94,9 @@ provider "azurerm" {
 }
 
 resource "azurerm_security_center_assessment_metadata" "test" {
-  display_name    = "Test Display Name"
-  severity        = "Medium"
-  description     = "Test Description"
+  display_name = "Test Display Name"
+  severity     = "Medium"
+  description  = "Test Description"
 }
 `
 }
