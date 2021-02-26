2c653aa2cc2d5acae89032cf725e31a80d0fbc99
magodo <wztdyl@sina.com>
2021-02-26 10:48:39 +0800

    fix new resource check bug

--- azurerm/internal/services/sentinel/sentinel_data_connector_threat_intelligence.go
+++ azurerm/internal/services/sentinel/sentinel_data_connector_threat_intelligence.go
@@ -81,7 +81,9 @@ func resourceSentinelDataConnectorThreatIntelligenceCreateUpdate(d *schema.Resou
 			}
 		}
 
-		return tf.ImportAsExistsError("azurerm_sentinel_data_connector_threat_intelligence", id.ID())
+		if !utils.ResponseWasNotFound(resp.Response) {
+			return tf.ImportAsExistsError("azurerm_sentinel_data_connector_threat_intelligence", id.ID())
+		}
 	}
 
 	tenantId := d.Get("tenant_id").(string)
