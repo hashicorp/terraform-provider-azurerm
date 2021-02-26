b2dc194c1b4c6ed470701b227c639ccee77c1a4a
magodo <wztdyl@sina.com>
2021-02-26 10:51:19 +0800

    fix new resource check bug

--- azurerm/internal/services/sentinel/sentinel_data_connector_aws_cloud_trail.go
+++ azurerm/internal/services/sentinel/sentinel_data_connector_aws_cloud_trail.go
@@ -81,7 +81,9 @@ func resourceSentinelDataConnectorAwsCloudTrailCreateUpdate(d *schema.ResourceDa
 			}
 		}
 
-		return tf.ImportAsExistsError("azurerm_sentinel_data_connector_aws_cloud_trail", id.ID())
+		if !utils.ResponseWasNotFound(resp.Response) {
+			return tf.ImportAsExistsError("azurerm_sentinel_data_connector_aws_cloud_trail", id.ID())
+		}
 	}
 
 	param := securityinsight.AwsCloudTrailDataConnector{
