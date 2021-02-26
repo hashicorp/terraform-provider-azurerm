189166702ac98be676a5045136fb28e2783f06a9
yupwei68 <yupwei@microsoft.com>
2021-02-26 09:05:16 +0800

    update

--- azurerm/internal/services/cosmos/cosmosdb_account_resource.go
+++ azurerm/internal/services/cosmos/cosmosdb_account_resource.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/services/preview/cosmos-db/mgmt/2020-04-01-preview/documentdb"
+	"github.com/Azure/azure-sdk-for-go/services/cosmos-db/mgmt/2021-01-15/documentdb"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
