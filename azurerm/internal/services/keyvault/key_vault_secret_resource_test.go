ac19140382077de3966e59396f9d415ee5013bd1
Vladimir Lazarenko <favoretti@users.noreply.github.com>
2021-02-27 00:01:49 +0100

    `azurerm_keyvault_secret`: support `versionless_id` attribute #10738

--- azurerm/internal/services/keyvault/key_vault_secret_resource_test.go
+++ azurerm/internal/services/keyvault/key_vault_secret_resource_test.go
@@ -93,6 +93,7 @@ func TestAccKeyVaultSecret_complete(t *testing.T) {
 				check.That(data.ResourceName).Key("expiration_date").HasValue("2020-01-01T01:02:03Z"),
 				check.That(data.ResourceName).Key("tags.%").HasValue("1"),
 				check.That(data.ResourceName).Key("tags.hello").HasValue("world"),
+				check.That(data.ResourceName).Key("versionless_id").HasValue(fmt.Sprintf("https://acctestkv-%s.vault.azure.net/secrets/secret-%s", data.RandomString, data.RandomString)),
 			),
 		},
 		data.ImportStep(),
