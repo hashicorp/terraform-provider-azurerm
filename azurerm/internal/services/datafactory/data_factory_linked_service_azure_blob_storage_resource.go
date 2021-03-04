3b3fa0c2cb21730289898e1ba7af9da1d76280f5
Jochen Rauschenbusch <jochen.rauschenbusch@gmail.com>
2021-03-04 09:37:00 +0100

    Fixing managed identity

--- azurerm/internal/services/datafactory/data_factory_linked_service_azure_blob_storage_resource.go
+++ azurerm/internal/services/datafactory/data_factory_linked_service_azure_blob_storage_resource.go
@@ -57,7 +57,7 @@ func resourceDataFactoryLinkedServiceAzureBlobStorage() *schema.Resource {
 				Optional:     true,
 				Sensitive:    true,
 				ValidateFunc: validation.StringIsNotEmpty,
-				ExactlyOneOf: []string{"connection_string", "sas_uri"},
+				ExactlyOneOf: []string{"connection_string", "sas_uri", "service_endpoint"},
 			},
 
 			"sas_uri": {
@@ -65,7 +65,7 @@ func resourceDataFactoryLinkedServiceAzureBlobStorage() *schema.Resource {
 				Optional:     true,
 				Sensitive:    true,
 				ValidateFunc: validation.StringIsNotEmpty,
-				ExactlyOneOf: []string{"connection_string", "sas_uri"},
+				ExactlyOneOf: []string{"connection_string", "sas_uri", "service_endpoint"},
 			},
 
 			"description": {
@@ -87,6 +87,15 @@ func resourceDataFactoryLinkedServiceAzureBlobStorage() *schema.Resource {
 				ConflictsWith: []string{
 					"service_principal_id",
 				},
+			},
+
+			"service_endpoint": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Sensitive:    true,
+				ValidateFunc: validation.StringIsNotEmpty,
+				RequiredWith: []string{"use_managed_identity"},
+				ExactlyOneOf: []string{"connection_string", "sas_uri", "service_endpoint"},
 			},
 
 			"service_principal_id": {
@@ -178,7 +187,9 @@ func resourceDataFactoryLinkedServiceBlobStorageCreateUpdate(d *schema.ResourceD
 	}
 
 	if d.Get("use_managed_identity").(bool) {
-		blobStorageProperties.Tenant = utils.String(d.Get("tenant_id").(string))
+		if v, ok := d.GetOk("service_endpoint"); ok {
+			blobStorageProperties.ServiceEndpoint = utils.String(v.(string))
+		}
 	} else {
 		secureString := datafactory.SecureString{
 			Value: utils.String(d.Get("service_principal_key").(string)),
@@ -276,6 +287,7 @@ func resourceDataFactoryLinkedServiceBlobStorageRead(d *schema.ResourceData, met
 			d.Set("service_principal_id", blobStorage.ServicePrincipalID)
 			d.Set("use_managed_identity", false)
 		} else {
+			d.Set("service_endpoint", blobStorage.ServiceEndpoint)
 			d.Set("use_managed_identity", true)
 		}
 	}
--- website/docs/r/data_factory_linked_service_azure_blob_storage.html.markdown
+++ website/docs/r/data_factory_linked_service_azure_blob_storage.html.markdown
@@ -62,11 +62,13 @@ The following supported arguments are common across all Azure Data Factory Linke
 
 The following supported arguments are specific to Azure Blob Storage Linked Service:
 
-* `connection_string` - (Optional) The connection string. Conflicts with `sas_uri`.
+* `connection_string` - (Optional) The connection string. Conflicts with `sas_uri` and `service_endpoint`.
 
-* `sas_uri` - (Optional) The SAS URI. Conflicts with `connection_string`.
+* `sas_uri` - (Optional) The SAS URI. Conflicts with `connection_string` and `service_endpoint`.
 
-* `use_managed_identity` - (Optional) Whether to use the Data Factory's managed identity to authenticate against the Azure Blob Storage account. Incompatible with `service_principal_id` and `service_principal_key`
+* `service_endpoint` - (Optional) The Service Endpoint. Conflicts with `connection_string` and `sas_uri`. Required with `use_managed_identity`.
+
+* `use_managed_identity` - (Optional) Whether to use the Data Factory's managed identity to authenticate against the Azure Blob Storage account. Incompatible with `service_principal_id` and `service_principal_key`.
 
 * `service_principal_id` - (Optional) The service principal id in which to authenticate against the Azure Blob Storage account. Required if `service_principal_key` is set.
 
