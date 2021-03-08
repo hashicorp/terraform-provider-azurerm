1f86ef7bcbe4d7b483786e523bd39665a835ff26
Sune Keller <absukl@almbrand.dk>
2021-03-08 10:56:37 +0100

    Move Application Gateway subresource ID parsing and validation to separate files
    Signed-off-by: Sune Keller <absukl@almbrand.dk>

--- azurerm/internal/services/network/validate/application_gateway_id.go
+++ azurerm/internal/services/network/validate/application_gateway_id.go
@@ -21,17 +21,3 @@ func ApplicationGatewayID(input interface{}, key string) (warnings []string, err
 
 	return
 }
-
-func ApplicationGatewayHTTPListenerID(i interface{}, k string) (warnings []string, errors []error) {
-	if _, err := parse.ApplicationGatewayHTTPListenerID(k); err != nil {
-		errors = append(errors, fmt.Errorf("Can not parse %q as an Application Gateway HTTP Listener ID: %v", k, err))
-	}
-	return
-}
-
-func ApplicationGatewayPathBasedRuleID(i interface{}, k string) (warnings []string, errors []error) {
-	if _, err := parse.ApplicationGatewayPathRuleID(k); err != nil {
-		errors = append(errors, fmt.Errorf("Can not parse %q as an Application Gateway Path Based Rule ID: %v", k, err))
-	}
-	return
-}
