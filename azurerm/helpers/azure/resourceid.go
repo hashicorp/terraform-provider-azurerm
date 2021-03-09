aa22a65d0696977d90562e7a71dbd2cfc906cb04
jackofallops <ste@hashicorp.com>
2021-03-09 15:58:03 +0000

    move subscriptionless ID parsing to helper func

--- azurerm/helpers/azure/resourceid.go
+++ azurerm/helpers/azure/resourceid.go
@@ -91,6 +91,49 @@ func ParseAzureResourceID(id string) (*ResourceID, error) {
 	return idObj, nil
 }
 
+func ParseAzureResourceIDWithoutSubscription(id string) (*ResourceID, error) {
+	idURL, err := url.ParseRequestURI(id)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot parse Azure ID: %s", err)
+	}
+
+	path := idURL.Path
+
+	path = strings.TrimPrefix(path, "/")
+	path = strings.TrimSuffix(path, "/")
+
+	components := strings.Split(path, "/")
+
+	// We should have an even number of key-value pairs.
+	if len(components)%2 != 0 {
+		return nil, fmt.Errorf("The number of path segments is not divisible by 2 in %q", path)
+	}
+
+	componentMap := make(map[string]string, len(components)/2)
+	for current := 0; current < len(components); current += 2 {
+		key := components[current]
+		value := components[current+1]
+
+		// Check key/value for empty strings.
+		if key == "" || value == "" {
+			return nil, fmt.Errorf("Key/Value cannot be empty strings. Key: '%s', Value: '%s'", key, value)
+		}
+		componentMap[key] = value
+	}
+
+	// Build up a TargetResourceID from the map
+	idObj := &ResourceID{}
+	idObj.Path = componentMap
+
+	// It is OK not to have a provider in the case of a resource group
+	if provider, ok := componentMap["providers"]; ok {
+		idObj.Provider = provider
+		delete(componentMap, "providers")
+	}
+
+	return idObj, nil
+}
+
 // PopSegment retrieves a segment from the Path and returns it
 // if found it removes it from the Path then return the value
 // if not found, this returns nil
