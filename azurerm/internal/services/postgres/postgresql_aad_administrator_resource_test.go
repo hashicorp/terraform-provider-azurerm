d7e9fed5eb3703f41ffbb91fd96c5259b492761d
kt <kt@katbyte.me>
2021-03-09 12:43:34 -0800

    Update azurerm/internal/services/postgres/postgresql_aad_administrator_resource_test.go

--- azurerm/internal/services/postgres/postgresql_aad_administrator_resource_test.go
+++ azurerm/internal/services/postgres/postgresql_aad_administrator_resource_test.go
@@ -79,7 +79,7 @@ func (r PostgreSqlAdministratorResource) Exists(ctx context.Context, clients *cl
 
 	resp, err := clients.Postgres.ServerAdministratorsClient.Get(ctx, id.ResourceGroup, id.ServerName)
 	if err != nil {
-		return nil, fmt.Errorf("reading Postgresql AAD Adminstrator (%s): %+v", id.String(), err)
+		return nil, fmt.Errorf("reading Postgresql AAD Administrator (%s): %+v", id.String(), err)
 	}
 
 	return utils.Bool(resp.ID != nil), nil
