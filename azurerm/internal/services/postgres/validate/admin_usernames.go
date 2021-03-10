9d3e249fc4102733ce750b44d061fae1e319b650
HarleyB123 <Harley.Bates@kpmg.co.uk>
2021-03-10 11:25:21 +0000

    fixing error message

--- azurerm/internal/services/postgres/validate/admin_usernames.go
+++ azurerm/internal/services/postgres/validate/admin_usernames.go
@@ -9,7 +9,7 @@ func AdminUsernames(i interface{}, k string) (_ []string, errors []error) {
 	disallowedLogins := [7]string{"azure_superuser", "azure_pg_admin", "admin", "administrator", "root", "guest", "public"}
 	for _, v := range disallowedLogins {
 		if v == strings.ToLower(i.(string)) {
-			return nil, append(errors, fmt.Errorf("Error - PostgreSQL AD Administrator login can not be %q", k))
+			return nil, append(errors, fmt.Errorf("Error - PostgreSQL AD Administrator login can not be %q", i.(string)))
 		}
 	}
 	if strings.HasPrefix(strings.ToLower(i.(string)), "pg_") {
