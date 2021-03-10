0685a3a6229d088d359a75d91982b19ea4b0f35b
HarleyB123 <Harley.Bates@kpmg.co.uk>
2021-03-10 10:41:17 +0000

    lowercase strings

--- azurerm/internal/services/postgres/validate/admin_usernames.go
+++ azurerm/internal/services/postgres/validate/admin_usernames.go
@@ -8,11 +8,11 @@ import (
 func AdminUsernames(i interface{}, k string) (_ []string, errors []error) {
 	disallowedLogins := [7]string{"azure_superuser", "azure_pg_admin", "admin", "administrator", "root", "guest", "public"}
 	for _, v := range disallowedLogins {
-		if v == i.(string) {
+		if v == strings.ToLower(i.(string)) {
 			return nil, append(errors, fmt.Errorf("Error - PostgreSQL AD Administrator login can not be %q", k))
 		}
 	}
-	if strings.HasPrefix(i.(string), "pg_") {
+	if strings.HasPrefix(strings.ToLower(i.(string)), "pg_") {
 		return nil, append(errors, fmt.Errorf("Error - PostgreSQL AD Administrator login can not start with 'pg_'"))
 	}
 
