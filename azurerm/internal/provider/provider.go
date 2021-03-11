83bb105bee103aa538f5209b48237cca48219424
tombuildsstuff <git@ibuildstuff.co.uk>
2021-03-11 10:25:46 +0100

    provider: removing the `custom_correlation_request_id` field from the provider block

--- azurerm/internal/provider/provider.go
+++ azurerm/internal/provider/provider.go
@@ -228,13 +228,6 @@ func azureProvider(supportLegacyTestSuite bool) terraform.ResourceProvider {
 				DefaultFunc: schema.EnvDefaultFunc("ARM_STORAGE_USE_AZUREAD", false),
 				Description: "Should the AzureRM Provider use AzureAD to access the Storage Data Plane API's?",
 			},
-
-			"custom_correlation_request_id": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     schema.EnvDefaultFunc("ARM_CORRELATION_REQUEST_ID", ""),
-				Description: "The value of the x-ms-correlation-request-id header (otherwise an auto-generated UUID will be used).",
-			},
 		},
 
 		DataSourcesMap: dataSources,
@@ -319,10 +312,13 @@ func providerConfigure(p *schema.Provider) schema.ConfigureFunc {
 			TerraformVersion:            terraformVersion,
 			PartnerId:                   d.Get("partner_id").(string),
 			DisableCorrelationRequestID: d.Get("disable_correlation_request_id").(bool),
-			CustomCorrelationRequestID:  d.Get("custom_correlation_request_id").(string),
 			DisableTerraformPartnerID:   d.Get("disable_terraform_partner_id").(bool),
 			Features:                    expandFeatures(d.Get("features").([]interface{})),
 			StorageUseAzureAD:           d.Get("storage_use_azuread").(bool),
+
+			// this field is intentionally not exposed in the provider block, since it's only used for
+			// platform level tracing
+			CustomCorrelationRequestID: os.Getenv("ARM_CORRELATION_REQUEST_ID"),
 		}
 		client, err := clients.Build(p.StopContext(), clientBuilder)
 		if err != nil {
--- website/docs/index.html.markdown
+++ website/docs/index.html.markdown
@@ -136,8 +136,6 @@ More information on [how to configure a Service Principal using Managed Service 
 
 For some advanced scenarios, such as where more granular permissions are necessary - the following properties can be set:
 
-* `custom_correlation_request_id` - (Optional) The value of the `x-ms-correlation-request-id` header, otherwise an auto-generated UUID will be used.
-
 * `disable_terraform_partner_id` - (Optional) Disable sending the Terraform Partner ID if a custom `partner_id` isn't specified, which allows Microsoft to better understand the usage of Terraform. The Partner ID does not give HashiCorp any direct access to usage information. This can also be sourced from the `ARM_DISABLE_TERRAFORM_PARTNER_ID` environment variable. Defaults to `false`.
 
 * `metadata_host` - (Optional) The Hostname of the Azure Metadata Service (for example `management.azure.com`), used to obtain the Cloud Environment when using a Custom Azure Environment. This can also be sourced from the `ARM_METADATA_HOST` Environment Variable.
