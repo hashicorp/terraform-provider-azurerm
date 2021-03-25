b9fc13bfd4afe90b371ae703d276f237b3bdd993
arcturusZhang <ufo54153@gmail.com>
2021-03-25 11:30:08 +0800

    trying to fix test cases

--- azurerm/internal/services/machinelearning/machine_learning_workspace_resource_test.go
+++ azurerm/internal/services/machinelearning/machine_learning_workspace_resource_test.go
@@ -320,6 +320,23 @@ resource "azurerm_key_vault" "test" {
   tenant_id           = data.azurerm_client_config.current.tenant_id
 
   sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    certificate_permissions = [
+      "managecontacts",
+    ]
+
+    key_permissions = [
+      "create",
+    ]
+
+    secret_permissions = [
+      "set",
+    ]
+  }
 }
 
 resource "azurerm_storage_account" "test" {
