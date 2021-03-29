090346b8ae5f9fc67798d171659919189bb17698
Pipeline <noreply@santanderconsumer.se>
2021-03-29 17:25:08 +0200

    more clear error message for parsing user identity id

--- azurerm/internal/services/msi/parse/user_assigned_identity.go
+++ azurerm/internal/services/msi/parse/user_assigned_identity.go
@@ -41,7 +41,7 @@ func (id UserAssignedIdentityId) ID() string {
 func UserAssignedIdentityID(input string) (*UserAssignedIdentityId, error) {
 	id, err := azure.ParseAzureResourceID(input)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("unable to parse User Assigned Identity ID: %q", input)
 	}
 
 	resourceId := UserAssignedIdentityId{
