966ec903a27615e73c23f85e3cf647914f72ace8
ArcturusZhang <dapzhang@microsoft.com>
2021-03-30 16:16:36 +0800

    responses util changes

--- azurerm/utils/response.go
+++ azurerm/utils/response.go
@@ -4,23 +4,52 @@ import (
 	"net"
 	"net/http"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/go-autorest/autorest"
 )
 
 func ResponseWasNotFound(resp autorest.Response) bool {
-	return ResponseWasStatusCode(resp, http.StatusNotFound)
+	return HTTPResponseWasStatusCode(resp.Response, http.StatusNotFound)
+}
+
+func Track2ResponseWasNotFound(err error) bool {
+	if v, ok := err.(azcore.HTTPResponse); ok {
+		return HTTPResponseWasStatusCode(v.RawResponse(), http.StatusNotFound)
+	}
+	return false
 }
 
 func ResponseWasBadRequest(resp autorest.Response) bool {
-	return ResponseWasStatusCode(resp, http.StatusBadRequest)
+	return HTTPResponseWasStatusCode(resp.Response, http.StatusBadRequest)
+}
+
+func Track2ResponseWasBadRequest(err error) bool {
+	if v, ok := err.(azcore.HTTPResponse); ok {
+		return HTTPResponseWasStatusCode(v.RawResponse(), http.StatusBadRequest)
+	}
+	return false
 }
 
 func ResponseWasForbidden(resp autorest.Response) bool {
-	return ResponseWasStatusCode(resp, http.StatusForbidden)
+	return HTTPResponseWasStatusCode(resp.Response, http.StatusForbidden)
+}
+
+func Track2ResponseWasForbidden(err error) bool {
+	if v, ok := err.(azcore.HTTPResponse); ok {
+		return HTTPResponseWasStatusCode(v.RawResponse(), http.StatusForbidden)
+	}
+	return false
 }
 
 func ResponseWasConflict(resp autorest.Response) bool {
-	return ResponseWasStatusCode(resp, http.StatusConflict)
+	return HTTPResponseWasStatusCode(resp.Response, http.StatusConflict)
+}
+
+func Track2ResponseWasConflict(err error) bool {
+	if v, ok := err.(azcore.HTTPResponse); ok {
+		return HTTPResponseWasStatusCode(v.RawResponse(), http.StatusConflict)
+	}
+	return false
 }
 
 func ResponseErrorIsRetryable(err error) bool {
@@ -47,4 +76,11 @@ func ResponseWasStatusCode(resp autorest.Response, statusCode int) bool { // nol
 	}
 
 	return false
+}
+
+func HTTPResponseWasStatusCode(resp *http.Response, statusCode int) bool { // nolint: unparam
+	if resp == nil {
+		return false
+	}
+	return resp.StatusCode == statusCode
 }
