f093278cbfc6a782cf0480db2109711ee9b11402
tombuildsstuff <git@ibuildstuff.co.uk>
2021-04-06 11:23:24 +0200

    r/linux_virtual_machine: support for Force Delete

--- azurerm/internal/services/compute/linux_virtual_machine_resource.go
+++ azurerm/internal/services/compute/linux_virtual_machine_resource.go
@@ -1156,10 +1156,14 @@ func resourceLinuxVirtualMachineDelete(d *schema.ResourceData, meta interface{})
 	}
 
 	log.Printf("[DEBUG] Deleting Linux Virtual Machine %q (Resource Group %q)..", id.Name, id.ResourceGroup)
-	// @tombuildsstuff: sending `nil` here omits this value from being sent - which matches
-	// the previous behaviour - we're only splitting this out so it's clear why
-	// TODO: support force deletion once it's out of Preview, if applicable
+
+	// Force Delete is in an opt-in Preview and can only be specified (true/false) if the feature is enabled
+	// as such we default this to `nil` which matches the previous behaviour (where this isn't sent) and
+	// conditionally set this if required
 	var forceDeletion *bool = nil
+	if meta.(*clients.Client).Features.VirtualMachine.ForceDelete {
+		forceDeletion = utils.Bool(true)
+	}
 	deleteFuture, err := client.Delete(ctx, id.ResourceGroup, id.Name, forceDeletion)
 	if err != nil {
 		return fmt.Errorf("deleting Linux Virtual Machine %q (Resource Group %q): %+v", id.Name, id.ResourceGroup, err)
