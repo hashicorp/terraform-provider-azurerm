507e3c98dec80cb6376b83d4d6d8c29d0137cef0
tombuildsstuff <git@ibuildstuff.co.uk>
2021-04-15 08:14:37 +0200

    r/iotcentral_application: parsing the old id insensitivity
    This works around issues within the API where this could be returned in
    either case, allowing us to ensure this is consistent in Terraform.

--- azurerm/internal/services/iotcentral/parse/application.go
+++ azurerm/internal/services/iotcentral/parse/application.go
@@ -67,3 +67,47 @@ func ApplicationID(input string) (*ApplicationId, error) {
 
 	return &resourceId, nil
 }
+
+// ApplicationIDInsensitively parses an Application ID into an ApplicationId struct, insensitively
+// This should only be used to parse an ID for rewriting, the ApplicationID
+// method should be used instead for validation etc.
+//
+// Whilst this may seem strange, this enables Terraform have consistent casing
+// which works around issues in Core, whilst handling broken API responses.
+func ApplicationIDInsensitively(input string) (*ApplicationId, error) {
+	id, err := azure.ParseAzureResourceID(input)
+	if err != nil {
+		return nil, err
+	}
+
+	resourceId := ApplicationId{
+		SubscriptionId: id.SubscriptionID,
+		ResourceGroup:  id.ResourceGroup,
+	}
+
+	if resourceId.SubscriptionId == "" {
+		return nil, fmt.Errorf("ID was missing the 'subscriptions' element")
+	}
+
+	if resourceId.ResourceGroup == "" {
+		return nil, fmt.Errorf("ID was missing the 'resourceGroups' element")
+	}
+
+	// find the correct casing for the 'IoTApps' segment
+	IoTAppsKey := "IoTApps"
+	for key := range id.Path {
+		if strings.EqualFold(key, IoTAppsKey) {
+			IoTAppsKey = key
+			break
+		}
+	}
+	if resourceId.IoTAppName, err = id.PopSegment(IoTAppsKey); err != nil {
+		return nil, err
+	}
+
+	if err := id.ValidateNoEmptySegments(input); err != nil {
+		return nil, err
+	}
+
+	return &resourceId, nil
+}
