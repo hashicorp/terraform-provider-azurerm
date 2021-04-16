3e37f2c3c3972b4981b0814d9d02598d24af6499
tombuildsstuff <git@ibuildstuff.co.uk>
2021-04-16 05:57:17 +0200

    scripts/fixerupper: filtering to specific filenames

--- scripts/plugin-sdk-fixerupper.sh
+++ scripts/plugin-sdk-fixerupper.sh
@@ -10,6 +10,10 @@ function updateTests {
   find "$SERVICE_DIRECTORY" -type f -iname "*_test.go" -exec sed -i '' -e 's/github\.com\/hashicorp\/terraform-plugin-sdk\/helper\/resource/github\.com\/terraform-providers\/terraform-provider-azurerm\/azurerm\/internal\/acceptance/g' {} \;
   find "$SERVICE_DIRECTORY" -type f -iname "*_test.go" -exec sed -i '' -e 's/resource\./acceptance\./g' {} \;
 
+  # references to `acctest.` should be `acceptance.`
+  find "$SERVICE_DIRECTORY" -type f -iname "*_test.go" -exec sed -i '' -e 's/github\.com\/hashicorp\/terraform-plugin-sdk\/helper\/acctest/github\.com\/terraform-providers\/terraform-provider-azurerm\/azurerm\/internal\/acceptance/g' {} \;
+  find "$SERVICE_DIRECTORY" -type f -iname "*_test.go" -exec sed -i '' -e 's/acctest\./acceptance\./g' {} \;
+
   # references to `terraform.` should be `pluginsdk.`
   find "$SERVICE_DIRECTORY" -type f -iname "*_test.go" -exec sed -i '' -e 's/github\.com\/hashicorp\/terraform-plugin-sdk\/terraform/github\.com\/terraform-providers\/terraform-provider-azurerm\/azurerm\/internal\/tf\/pluginsdk/g' {} \;
   find "$SERVICE_DIRECTORY" -type f -iname "*_test.go" -exec sed -i '' -e 's/terraform\./pluginsdk\./g' {} \;
@@ -18,11 +22,20 @@ function updateTests {
 function updateCode {
   echo "Updating Resources.."
   # references to `schema.` should be `pluginsdk.`
-  find "$SERVICE_DIRECTORY" -type f -exec sed -i '' -e 's/github\.com\/hashicorp\/terraform-plugin-sdk\/helper\/schema/github\.com\/terraform-providers\/terraform-provider-azurerm\/azurerm\/internal\/tf\/pluginsdk/g' {} \;
-  find "$SERVICE_DIRECTORY" -type f -exec sed -i '' -e 's/schema\./pluginsdk\./g' {} \;
+  find "$SERVICE_DIRECTORY" -type f -iname "*.go"  -exec sed -i '' -e 's/github\.com\/hashicorp\/terraform-plugin-sdk\/helper\/schema/github\.com\/terraform-providers\/terraform-provider-azurerm\/azurerm\/internal\/tf\/pluginsdk/g' {} \;
+  find "$SERVICE_DIRECTORY" -type f -iname "*.go"  -exec sed -i '' -e 's/schema\./pluginsdk\./g' {} \;
 
   # update the import for validation, the alised/unaliased part can remain the same
-  find "$SERVICE_DIRECTORY" -type f -exec sed -i '' -e 's/github\.com\/hashicorp\/terraform-plugin-sdk\/helper\/validation/github\.com\/terraform-providers\/terraform-provider-azurerm\/azurerm\/internal\/tf\/validation/g' {} \;
+  find "$SERVICE_DIRECTORY" -type f -iname "*.go"  -exec sed -i '' -e 's/github\.com\/hashicorp\/terraform-plugin-sdk\/helper\/validation/github\.com\/terraform-providers\/terraform-provider-azurerm\/azurerm\/internal\/tf\/validation/g' {} \;
+}
+
+function revertValidationFuncs {
+  echo "Reverting unintentional changes to client functions.."
+  git checkout -- "$SERVICE_DIRECTORY/client"
+  echo "Reverting unintentional changes to parse functions.."
+  git checkout -- "$SERVICE_DIRECTORY/parse"
+  echo "Reverting unintentional changes to validation functions.."
+  git checkout -- "$SERVICE_DIRECTORY/validate"
 }
 
 function format {
@@ -32,4 +45,5 @@ function format {
 
 updateTests
 updateCode
+revertValidationFuncs
 format
