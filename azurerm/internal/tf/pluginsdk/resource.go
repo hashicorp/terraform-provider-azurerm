2da5c7bad2152059f3dfe5fd13502b35c253982c
tombuildsstuff <git@ibuildstuff.co.uk>
2021-04-16 09:32:39 +0200

    more wrappers

--- azurerm/internal/tf/pluginsdk/resource.go
+++ azurerm/internal/tf/pluginsdk/resource.go
@@ -8,13 +8,16 @@ import (
 // This file is intended to provide a transition from Plugin SDKv1 to Plugin SDKv2
 // without introducing a merge conflict into every PR.
 
-type SchemaDiffSuppressFunc = schema.SchemaDiffSuppressFunc
+type BasicMapReader = schema.BasicMapReader
+type MapFieldReader = schema.MapFieldReader
 type Resource = schema.Resource
 type ResourceData = schema.ResourceData
 type ResourceDiff = schema.ResourceDiff
 type ResourceImporter = schema.ResourceImporter
-type SchemaValidateFunc schema.SchemaValidateFunc
+type SchemaDiffSuppressFunc = schema.SchemaDiffSuppressFunc
 type StateUpgrader = schema.StateUpgrader
+type SchemaValidateFunc schema.SchemaValidateFunc
+type ValueType = schema.ValueType
 
 // ImportStatePassthrough is an implementation of StateFunc that can be
 // used to simply pass the ID directly through. This should be used only
