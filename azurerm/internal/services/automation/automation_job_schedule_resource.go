03a2665e0f7d956a14573e81d7cef9679b8bc769
tombuildsstuff <git@ibuildstuff.co.uk>
2021-04-19 19:02:59 +0200

    automation: refactoring the validators out to their own package

--- azurerm/internal/services/automation/automation_job_schedule_resource.go
+++ azurerm/internal/services/automation/automation_job_schedule_resource.go
@@ -67,17 +67,7 @@ func resourceAutomationJobSchedule() *schema.Resource {
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
-				ValidateFunc: func(v interface{}, _ string) (warnings []string, errors []error) {
-					m := v.(map[string]interface{})
-
-					for k := range m {
-						if k != strings.ToLower(k) {
-							errors = append(errors, fmt.Errorf("Due to a bug in the implementation of Runbooks in Azure, the parameter names need to be specified in lowercase only. See: \"https://github.com/Azure/azure-sdk-for-go/issues/4780\" for more information."))
-						}
-					}
-
-					return warnings, errors
-				},
+				ValidateFunc: validate.ParameterNames,
 			},
 
 			"run_on": {
