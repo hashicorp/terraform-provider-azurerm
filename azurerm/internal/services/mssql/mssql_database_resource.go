a9e471b1df5c5115dfa3b400f74a34e17ec373d6
aristosvo <8375124+aristosvo@users.noreply.github.com>
2021-04-20 00:52:49 +0200

    `mssql_database` Update documentation and print warning for secondary db with `max_size_gb` (#11286)
    Fixes #11282
    
    I don't know how to signal this warning more explicitly than proposed, without breaking anything.

--- azurerm/internal/services/mssql/mssql_database_resource.go
+++ azurerm/internal/services/mssql/mssql_database_resource.go
@@ -302,7 +302,7 @@ func resourceMsSqlDatabase() *schema.Resource {
 		},
 
 		CustomizeDiff: customdiff.All(
-			customdiff.ForceNewIfChange("sku_name", func(old, new, meta interface{}) bool {
+			customdiff.ForceNewIfChange("sku_name", func(old, new, _ interface{}) bool {
 				// "hyperscale can not change to other sku
 				return strings.HasPrefix(old.(string), "HS") && !strings.HasPrefix(new.(string), "HS")
 			}),
@@ -396,6 +396,9 @@ func resourceMsSqlDatabaseCreateUpdate(d *schema.ResourceData, meta interface{})
 	}
 
 	if v, ok := d.GetOk("max_size_gb"); ok {
+		if createMode == string(sql.CreateModeOnlineSecondary) || createMode == string(sql.Secondary) {
+			return fmt.Errorf("it is not possible to change maximum size nor advised to configure maximum size on SQL Database %q (Resource Group %q, Server %q) in secondary create mode", name, serverId.ResourceGroup, serverId.Name)
+		}
 		params.DatabaseProperties.MaxSizeBytes = utils.Int64(int64(v.(int) * 1073741824))
 	}
 
