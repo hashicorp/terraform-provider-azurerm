ff696a2527fc175194f1eb89d334cad178f6765f
tombuildsstuff <git@ibuildstuff.co.uk>
2021-04-20 12:31:37 +0200

    r/user_assigned_identity: refactor for consistency

--- azurerm/internal/services/msi/migration/user_assigned_identity_V0_to_V1.go
+++ azurerm/internal/services/msi/migration/user_assigned_identity_V0_to_V1.go
@@ -3,10 +3,6 @@ package migration
 import (
 	"log"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
-
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/msi/parse"
 )
@@ -14,26 +10,37 @@ import (
 func UserAssignedIdentityV0ToV1() schema.StateUpgrader {
 	return schema.StateUpgrader{
 		Version: 0,
-		Type:    userAssignedIdentityV0Schema().CoreConfigSchema().ImpliedType(),
+		Type:    userAssignedIdentitySchemaForV0().CoreConfigSchema().ImpliedType(),
 		Upgrade: userAssignedIdentityUpgradeV0ToV1,
 	}
 }
 
-func userAssignedIdentityV0Schema() *schema.Resource {
+func userAssignedIdentitySchemaForV0() *schema.Resource {
 	return &schema.Resource{
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Type:         schema.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validation.StringLenBetween(3, 128),
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
 			},
 
-			"resource_group_name": azure.SchemaResourceGroupName(),
+			"resource_group_name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
 
-			"location": azure.SchemaLocation(),
+			"location": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
 
-			"tags": tags.Schema(),
+			"tags": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
 
 			"principal_id": {
 				Type:     schema.TypeString,
