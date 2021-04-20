8e89c054c15909ed52876b05dbfaeaf48898fae0
Aris van Ommeren <avommeren@vxcompany.com>
2021-04-20 22:50:37 +0200

    Reverse change and fix bug

--- azurerm/internal/services/mssql/mssql_database_resource.go
+++ azurerm/internal/services/mssql/mssql_database_resource.go
@@ -396,7 +396,7 @@ func resourceMsSqlDatabaseCreateUpdate(d *schema.ResourceData, meta interface{})
 	}
 
 	if v, ok := d.GetOk("max_size_gb"); ok {
-		if createMode == string(sql.CreateModeOnlineSecondary) || createMode == string(sql.Secondary) {
+		if d.HasChange("max_size_gb") && (createMode == string(sql.CreateModeOnlineSecondary) || createMode == string(sql.Secondary)) {
 			return fmt.Errorf("it is not possible to change maximum size nor advised to configure maximum size on SQL Database %q (Resource Group %q, Server %q) in secondary create mode", name, serverId.ResourceGroup, serverId.Name)
 		}
 		params.DatabaseProperties.MaxSizeBytes = utils.Int64(int64(v.(int) * 1073741824))
@@ -423,7 +423,7 @@ func resourceMsSqlDatabaseCreateUpdate(d *schema.ResourceData, meta interface{})
 		}
 	}
 
-	if v, ok := d.GetOk("creation_source_database_id"); ok && d.HasChange("creation_source_database_id") {
+	if v, ok := d.GetOk("creation_source_database_id"); ok {
 		params.DatabaseProperties.SourceDatabaseID = utils.String(v.(string))
 	}
 
