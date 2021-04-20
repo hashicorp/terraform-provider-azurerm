88d6cb934f6f69902ae5c336ad183980a0fea2ca
tombuildsstuff <git@ibuildstuff.co.uk>
2021-04-20 09:44:17 +0200

    validation: improving the error messages

--- azurerm/internal/services/databasemigration/validate/project_name.go
+++ azurerm/internal/services/databasemigration/validate/project_name.go
@@ -10,9 +10,9 @@ func ProjectName(i interface{}, k string) ([]string, []error) {
 	if !ok {
 		return nil, []error{fmt.Errorf("expected type of %q to be string", k)}
 	}
-	validName := regexp.MustCompile(`^[\d\w]+[\d\w\-_.]*$`)
+	validName := regexp.MustCompile(`^^[a-zA-Z0-9][a-zA-Z0-9\-_.]+$*$`)
 	if !validName.MatchString(v) {
-		return nil, []error{fmt.Errorf("invalid format of %q", k)}
+		return nil, []error{fmt.Errorf("%q must start with letters/numbers and can contain letters, numbers, underscores, dashes and periods - got %q", k, v)}
 	}
 	return nil, nil
 }
