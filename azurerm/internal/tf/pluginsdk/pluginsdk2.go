6ef80d826663b108cc7be328acf960ddb8c14ab1
tombuildsstuff <git@ibuildstuff.co.uk>
2021-04-21 10:39:32 +0200

    notes

--- azurerm/internal/tf/pluginsdk/pluginsdk2.go
+++ azurerm/internal/tf/pluginsdk/pluginsdk2.go
@@ -7,9 +7,12 @@ This is tracking the changes necessary for Plugin SDKv2
 2. Package validation
 	All validation functions need to be moved into `validation` - since otherwise the test namespace needs fixing
     Should we update resources to be Typed?
-2. Updating the Customize Diff functions to use the new wrapper
-3. Updating the Import functions to use the new wrappers
-4. Gradually updating each service package to use the new wrappers
+3. Updating the Migration functions
+4. Updating the Customize Diff functions to use the new wrapper
+5. Updating the Import functions to use the new wrappers
+6. Gradually updating each service package to use the new wrappers
 	NOTE: validation functions may need converting over to return the function rather than the type
-5. Upgrade to Plugin SDKv2
+7. Upgrade to Plugin SDKv2
+	For the moment the StopContext will need replacing with `context.TODO()` which
+	should give us the same behaviour
 */
