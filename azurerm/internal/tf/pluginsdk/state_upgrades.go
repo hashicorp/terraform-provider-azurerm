7bd58142c5a3751f7920bb1f378095c42c9b3994
tombuildsstuff <git@ibuildstuff.co.uk>
2021-04-21 10:08:17 +0200

    shim: downgrading to plugin sdkv1

--- azurerm/internal/tf/pluginsdk/state_upgrades.go
+++ azurerm/internal/tf/pluginsdk/state_upgrades.go
@@ -2,6 +2,7 @@ package pluginsdk
 
 import (
 	"context"
+	"fmt"
 	"sort"
 )
 
@@ -22,6 +23,9 @@ type StateUpgrade interface {
 	UpgradeFunc() StateUpgraderFunc
 }
 
+// StateUpgrades is a wrapper around the Plugin SDK's State Upgraders
+// which allows us to upgrade the Plugin SDK without breaking all open
+// PR's and attempts to make this interface a little less verbose.
 func StateUpgrades(upgrades map[int]StateUpgrade) []StateUpgrader {
 	versions := make([]int, len(upgrades))
 	for version := range versions {
@@ -30,7 +34,14 @@ func StateUpgrades(upgrades map[int]StateUpgrade) []StateUpgrader {
 	sort.Ints(versions)
 
 	out := make([]StateUpgrader, 0)
+	expectedVersion := 0
 	for _, version := range versions {
+		// sanity check that there's no missing versions
+		if expectedVersion != version {
+			panic(fmt.Sprintf("missing state upgrade for version %d", expectedVersion))
+		}
+		expectedVersion++
+
 		upgrade := upgrades[version]
 		resource := Resource{
 			Schema: upgrade.Schema(),
@@ -38,8 +49,10 @@ func StateUpgrades(upgrades map[int]StateUpgrade) []StateUpgrader {
 		// TODO: with Plugin SDK 1.x we'll need to add a wrapper here to inject ctx
 
 		out = append(out, StateUpgrader{
-			Type:    resource.CoreConfigSchema().ImpliedType(),
-			Upgrade: upgrade.UpgradeFunc(),
+			Type: resource.CoreConfigSchema().ImpliedType(),
+			Upgrade: func(rawState map[string]interface{}, meta interface{}) (map[string]interface{}, error) {
+				return upgrade.UpgradeFunc()(context.TODO(), rawState, meta)
+			},
 			Version: version,
 		})
 	}
