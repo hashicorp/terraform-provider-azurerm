34a8db9a9e69ca535ce2c04919901b9caab7c801
tombuildsstuff <git@ibuildstuff.co.uk>
2021-04-22 16:53:08 +0200

    network: refactoring to use the pluginsdk wrapper for migrations

--- azurerm/internal/services/network/migration/network_interface_application_security_group_association.go
+++ azurerm/internal/services/network/migration/network_interface_application_security_group_association.go
@@ -1,48 +1,46 @@
 package migration
 
 import (
+	"context"
 	"fmt"
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk"
 )
 
-func NetworkInterfaceApplicationSecurityGroupAssociationV0ToV1() schema.StateUpgrader {
-	return schema.StateUpgrader{
-		Type:    networkInterfaceApplicationSecurityGroupAssociationSchemaForV0().CoreConfigSchema().ImpliedType(),
-		Upgrade: networkInterfaceApplicationSecurityGroupAssociationUpgradeV0ToV1,
-		Version: 0,
-	}
-}
+var _ pluginsdk.StateUpgrade = NetworkInterfaceApplicationSecurityGroupAssociationV0ToV1{}
 
-func networkInterfaceApplicationSecurityGroupAssociationSchemaForV0() *schema.Resource {
-	return &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			"network_interface_id": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
+type NetworkInterfaceApplicationSecurityGroupAssociationV0ToV1 struct{}
 
-			"application_security_group_id": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
+func (NetworkInterfaceApplicationSecurityGroupAssociationV0ToV1) Schema() map[string]*pluginsdk.Schema {
+	return map[string]*schema.Schema{
+		"network_interface_id": {
+			Type:     schema.TypeString,
+			Required: true,
+			ForceNew: true,
+		},
+
+		"application_security_group_id": {
+			Type:     schema.TypeString,
+			Required: true,
+			ForceNew: true,
 		},
 	}
 }
 
-func networkInterfaceApplicationSecurityGroupAssociationUpgradeV0ToV1(rawState map[string]interface{}, meta interface{}) (map[string]interface{}, error) {
-	// after shipping support for this Resource Azure's since changed the behaviour to require that all IP Configurations
-	// are connected to the same Application Security Group
-	applicationSecurityGroupId := rawState["application_security_group_id"].(string)
-	networkInterfaceId := rawState["network_interface_id"].(string)
+func (NetworkInterfaceApplicationSecurityGroupAssociationV0ToV1) UpgradeFunc() pluginsdk.StateUpgraderFunc {
+	return func(ctx context.Context, rawState map[string]interface{}, meta interface{}) (map[string]interface{}, error) {
+		// after shipping support for this Resource Azure's since changed the behaviour to require that all IP Configurations
+		// are connected to the same Application Security Group
+		applicationSecurityGroupId := rawState["application_security_group_id"].(string)
+		networkInterfaceId := rawState["network_interface_id"].(string)
 
-	oldID := rawState["id"].(string)
-	newID := fmt.Sprintf("%s|%s", networkInterfaceId, applicationSecurityGroupId)
-	log.Printf("[DEBUG] Updating ID from %q to %q", oldID, newID)
+		oldID := rawState["id"].(string)
+		newID := fmt.Sprintf("%s|%s", networkInterfaceId, applicationSecurityGroupId)
+		log.Printf("[DEBUG] Updating ID from %q to %q", oldID, newID)
 
-	rawState["id"] = newID
-	return rawState, nil
+		rawState["id"] = newID
+		return rawState, nil
+	}
 }
