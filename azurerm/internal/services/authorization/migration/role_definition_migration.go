5cc14033b066290005419d75c2538ce4c1dafaa6
tombuildsstuff <git@ibuildstuff.co.uk>
2021-04-22 15:40:37 +0200

    authorization: switching over to use the pluginsdk alias

--- azurerm/internal/services/authorization/migration/role_definition_migration.go
+++ azurerm/internal/services/authorization/migration/role_definition_migration.go
@@ -1,112 +1,111 @@
 package migration
 
 import (
+	"context"
 	"fmt"
 	"log"
+
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
-func RoleDefinitionV0ToV1() schema.StateUpgrader {
-	return schema.StateUpgrader{
-		Type:    roleDefinitionSchemaForV0().CoreConfigSchema().ImpliedType(),
-		Upgrade: roleDefinitionUpgradeV0ToV1,
-		Version: 0,
-	}
-}
+var _ pluginsdk.StateUpgrade = RoleDefinitionV0ToV1{}
 
-func roleDefinitionSchemaForV0() *schema.Resource {
-	return &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			"role_definition_id": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-				ForceNew: true,
-			},
+type RoleDefinitionV0ToV1 struct{}
 
-			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
+func (RoleDefinitionV0ToV1) Schema() map[string]*pluginsdk.Schema {
+	return map[string]*schema.Schema{
+		"role_definition_id": {
+			Type:     schema.TypeString,
+			Optional: true,
+			Computed: true,
+			ForceNew: true,
+		},
 
-			"scope": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
+		"name": {
+			Type:     schema.TypeString,
+			Required: true,
+		},
 
-			"description": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
+		"scope": {
+			Type:     schema.TypeString,
+			Required: true,
+			ForceNew: true,
+		},
 
-			"permissions": {
-				Type:     schema.TypeList,
-				Required: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"actions": {
-							Type:     schema.TypeList,
-							Optional: true,
-							Elem: &schema.Schema{
-								Type: schema.TypeString,
-							},
+		"description": {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+
+		"permissions": {
+			Type:     schema.TypeList,
+			Required: true,
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"actions": {
+						Type:     schema.TypeList,
+						Optional: true,
+						Elem: &schema.Schema{
+							Type: schema.TypeString,
 						},
-						"not_actions": {
-							Type:     schema.TypeList,
-							Optional: true,
-							Elem: &schema.Schema{
-								Type: schema.TypeString,
-							},
+					},
+					"not_actions": {
+						Type:     schema.TypeList,
+						Optional: true,
+						Elem: &schema.Schema{
+							Type: schema.TypeString,
 						},
-						"data_actions": {
-							Type:     schema.TypeSet,
-							Optional: true,
-							Elem: &schema.Schema{
-								Type: schema.TypeString,
-							},
-							Set: schema.HashString,
+					},
+					"data_actions": {
+						Type:     schema.TypeSet,
+						Optional: true,
+						Elem: &schema.Schema{
+							Type: schema.TypeString,
 						},
-						"not_data_actions": {
-							Type:     schema.TypeSet,
-							Optional: true,
-							Elem: &schema.Schema{
-								Type: schema.TypeString,
-							},
-							Set: schema.HashString,
+						Set: schema.HashString,
+					},
+					"not_data_actions": {
+						Type:     schema.TypeSet,
+						Optional: true,
+						Elem: &schema.Schema{
+							Type: schema.TypeString,
 						},
+						Set: schema.HashString,
 					},
 				},
 			},
+		},
 
-			"assignable_scopes": {
-				Type:     schema.TypeList,
-				Optional: true,
-				Computed: true,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
-				},
+		"assignable_scopes": {
+			Type:     schema.TypeList,
+			Optional: true,
+			Computed: true,
+			Elem: &schema.Schema{
+				Type: schema.TypeString,
 			},
 		},
 	}
 }
 
-func roleDefinitionUpgradeV0ToV1(rawState map[string]interface{}, meta interface{}) (map[string]interface{}, error) {
-	log.Println("[DEBUG] Migrating ID from v0 to v1 format")
+func (RoleDefinitionV0ToV1) UpgradeFunc() pluginsdk.StateUpgraderFunc {
+	return func(ctx context.Context, rawState map[string]interface{}, meta interface{}) (map[string]interface{}, error) {
+		log.Println("[DEBUG] Migrating ID from v0 to v1 format")
 
-	oldID := rawState["id"].(string)
-	if oldID == "" {
-		return nil, fmt.Errorf("failed to migrate state: old ID empty")
+		oldID := rawState["id"].(string)
+		if oldID == "" {
+			return nil, fmt.Errorf("failed to migrate state: old ID empty")
+		}
+		scope := rawState["scope"].(string)
+		if scope == "" {
+			return nil, fmt.Errorf("failed to migrate state: scope missing")
+		}
+
+		newID := fmt.Sprintf("%s|%s", oldID, scope)
+
+		rawState["id"] = newID
+
+		return rawState, nil
 	}
-	scope := rawState["scope"].(string)
-	if scope == "" {
-		return nil, fmt.Errorf("failed to migrate state: scope missing")
-	}
-
-	newID := fmt.Sprintf("%s|%s", oldID, scope)
-
-	rawState["id"] = newID
-
-	return rawState, nil
 }
