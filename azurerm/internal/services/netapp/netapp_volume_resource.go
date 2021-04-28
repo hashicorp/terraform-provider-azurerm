be18494a23bce7d6b7c5e549c6b92d07861c8f72
Paulo <pmarques@microsoft.com>
2021-04-28 13:47:03 -0700

    Increasing default timeouts for volume creation/update

--- azurerm/internal/services/netapp/netapp_volume_resource.go
+++ azurerm/internal/services/netapp/netapp_volume_resource.go
@@ -33,9 +33,9 @@ func resourceNetAppVolume() *schema.Resource {
 		Delete: resourceNetAppVolumeDelete,
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(30 * time.Minute),
+			Create: schema.DefaultTimeout(60 * time.Minute),
 			Read:   schema.DefaultTimeout(5 * time.Minute),
-			Update: schema.DefaultTimeout(30 * time.Minute),
+			Update: schema.DefaultTimeout(60 * time.Minute),
 			Delete: schema.DefaultTimeout(60 * time.Minute),
 		},
 		Importer: azSchema.ValidateResourceIDPriorToImport(func(id string) error {
