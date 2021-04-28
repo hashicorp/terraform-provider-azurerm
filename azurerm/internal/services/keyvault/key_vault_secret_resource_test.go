904fa43a9b7ceaddf9c1914195cb88c982504180
Mattias <mattias.angehov@gmail.com>
2021-04-28 13:27:32 +0200

    update kv sdk 2016-10-01 to v7.1

--- azurerm/internal/services/keyvault/key_vault_secret_resource_test.go
+++ azurerm/internal/services/keyvault/key_vault_secret_resource_test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/services/keyvault/2016-10-01/keyvault"
+	"github.com/Azure/azure-sdk-for-go/services/keyvault/v7.1/keyvault"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
