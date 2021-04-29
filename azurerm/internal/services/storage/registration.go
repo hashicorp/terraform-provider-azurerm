9488fd081bf454d1148cc36aa1f7f2a8a36ddda0
Alvin Tang <alvintang@gmail.com>
2021-04-29 17:49:48 +0800

    added azurerm_storage_blob data source

--- azurerm/internal/services/storage/registration.go
+++ azurerm/internal/services/storage/registration.go
@@ -24,6 +24,7 @@ func (r Registration) SupportedDataSources() map[string]*schema.Resource {
 		"azurerm_storage_account_blob_container_sas": dataSourceStorageAccountBlobContainerSharedAccessSignature(),
 		"azurerm_storage_account_sas":                dataSourceStorageAccountSharedAccessSignature(),
 		"azurerm_storage_account":                    dataSourceStorageAccount(),
+		"azurerm_storage_blob":                       dataSourceStorageBlob(),
 		"azurerm_storage_container":                  dataSourceStorageContainer(),
 		"azurerm_storage_encryption_scope":           dataSourceStorageEncryptionScope(),
 		"azurerm_storage_management_policy":          dataSourceStorageManagementPolicy(),
