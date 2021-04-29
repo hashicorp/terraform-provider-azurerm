hashicorp/terraform-provider-azurerm

commit 663c09fffd7f77dbb170a86157b3ee5af7d4de46

`azurerm_public_ip` - Add support for `ip_tags` (#11270)

azurerm/internal/services/network/public_ip_data_source.go
@@ -130,6 +130,11 @@ func dataSourcePublicIPRead(d *schema.ResourceData, meta interface{}) error {
 		d.Set("ip_address", props.IPAddress)
 		d.Set("ip_version", string(props.PublicIPAddressVersion))
 		d.Set("idle_timeout_in_minutes", props.IdleTimeoutInMinutes)
+
+		iptags := flattenPublicIpPropsIpTags(*props.IPTags)
+		if iptags != nil {
+			d.Set("ip_tags", iptags)
+		}
 	}
 
 	return tags.FlattenAndSet(d, resp.Tags)

azurerm/internal/services/network/public_ip_data_source_test.go
@@ -32,6 +32,7 @@ func TestAccDataSourcePublicIP_static(t *testing.T) {
 				check.That(data.ResourceName).Key("ip_version").HasValue("IPv4"),
 				check.That(data.ResourceName).Key("tags.%").HasValue("1"),
 				check.That(data.ResourceName).Key("tags.environment").HasValue("test"),
+				check.That(data.ResourceName).Key("ip_tags.RoutingPreference").HasValue("Internet"),
 			),
 		},
 	})
@@ -79,6 +80,11 @@ resource "azurerm_public_ip" "test" {
   allocation_method       = "Static"
   domain_name_label       = "acctest-%d"
   idle_timeout_in_minutes = 30
+  sku                     = "Standard"
+
+  ip_tags = {
+    RoutingPreference = "Internet"
+  }
 
   tags = {
     environment = "test"

azurerm/internal/services/network/public_ip_resource.go
@@ -120,6 +120,15 @@ func resourcePublicIp() *schema.Resource {
 				ValidateFunc: azure.ValidateResourceID,
 			},
 
+			"ip_tags": {
+				Type:     schema.TypeMap,
+				Optional: true,
+				ForceNew: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+
 			"zones": azure.SchemaSingleZone(),
 
 			"tags": tags.Schema(),
@@ -209,6 +218,21 @@ func resourcePublicIpCreateUpdate(d *schema.ResourceData, meta interface{}) erro
 		publicIp.PublicIPAddressPropertiesFormat.DNSSettings = &dnsSettings
 	}
 
+	if v, ok := d.GetOk("ip_tags"); ok {
+		ipTags := v.(map[string]interface{})
+		newIpTags := []network.IPTag{}
+
+		for key, val := range ipTags {
+			ipTag := network.IPTag{
+				IPTagType: utils.String(key),
+				Tag:       utils.String(val.(string)),
+			}
+			newIpTags = append(newIpTags, ipTag)
+		}
+
+		publicIp.PublicIPAddressPropertiesFormat.IPTags = &newIpTags
+	}
+
 	future, err := client.CreateOrUpdate(ctx, id.ResourceGroup, id.Name, publicIp)
 	if err != nil {
 		return fmt.Errorf("creating/updating %s: %+v", id, err)
@@ -267,6 +291,11 @@ func resourcePublicIpRead(d *schema.ResourceData, meta interface{}) error {
 			d.Set("domain_name_label", settings.DomainNameLabel)
 		}
 
+		iptags := flattenPublicIpPropsIpTags(*props.IPTags)
+		if iptags != nil {
+			d.Set("ip_tags", iptags)
+		}
+
 		d.Set("ip_address", props.IPAddress)
 		d.Set("idle_timeout_in_minutes", props.IdleTimeoutInMinutes)
 	}
@@ -294,4 +323,15 @@ func resourcePublicIpDelete(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	return nil
+}
+
+func flattenPublicIpPropsIpTags(ipTags []network.IPTag) map[string]interface{} {
+	mapIpTags := make(map[string]interface{})
+
+	for _, tag := range ipTags {
+		if tag.IPTagType != nil {
+			mapIpTags[*tag.IPTagType] = tag.Tag
+		}
+	}
+	return mapIpTags
 }

azurerm/internal/services/network/public_ip_resource_test.go
@@ -341,6 +341,22 @@ func TestAccPublicIpStatic_canLabelBe63(t *testing.T) {
 	})
 }
 
+func TestAccPublicIpStatic_ipTags(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_public_ip", "test")
+	r := PublicIPResource{}
+
+	data.ResourceTest(t, r, []resource.TestStep{
+		{
+			Config: r.standard_IpTags(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("ip_tags.RoutingPreference").HasValue("Internet"),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func (t PublicIPResource) Exists(ctx context.Context, clients *clients.Client, state *terraform.InstanceState) (*bool, error) {
 	id, err := parse.PublicIpAddressID(state.ID)
 	if err != nil {
@@ -757,6 +773,31 @@ resource "azurerm_public_ip" "test" {
 
   allocation_method = "Static"
   domain_name_label = "k2345678-1-2345678-2-2345678-3-2345678-4-2345678-5-2345678-6-23"
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
+}
+
+func (PublicIPResource) standard_IpTags(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-network-%d"
+  location = "%s"
+}
+
+resource "azurerm_public_ip" "test" {
+  name                = "acctestpublicip-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+  ip_tags = {
+    RoutingPreference = "Internet"
+  }
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
 }

website/docs/d/public_ip.html.markdown
@@ -108,6 +108,7 @@ output "public_ip_address" {
 * `ip_address` - The IP address value that was allocated.
 * `ip_version` - The IP version being used, for example `IPv4` or `IPv6`.
 * `sku` - The SKU of the Public IP.
+* `ip_tags` - A mapping of tags to assigned to the resource.
 * `tags` - A mapping of tags to assigned to the resource.
 
 ## Timeouts

website/docs/r/public_ip.html.markdown
@@ -62,6 +62,10 @@ The following arguments are supported:
 
 * `public_ip_prefix_id` - (Optional) If specified then public IP address allocated will be provided from the public IP prefix resource.
 
+* `ip_tags` - (Optional) A mapping of IP tags to assign to the public IP.
+
+-> **Note** IP Tag `RoutingPreference` requires multiple `zones` and `Standard` SKU to be set.
+
 * `tags` - (Optional) A mapping of tags to assign to the resource.
 
 * `zones` - (Optional) A collection containing the availability zone to allocate the Public IP in.

commit aedd042da8d2ebfe9fa4039047c0f47b43e7a165

Fix datasource for azurerm_public_ip
Fixes regression introduced in #11270

azurerm/internal/services/network/public_ip_data_source.go
@@ -72,6 +72,14 @@ func dataSourcePublicIP() *schema.Resource {
 				Computed: true,
 			},
 
+			"ip_tags": {
+				Type:     schema.TypeMap,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+
 			"zones": azure.SchemaZonesComputed(),
 
 			"tags": tags.Schema(),