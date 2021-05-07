83f9ceabc757dcc7bfa0cdc1fe51e64703dff863
James <35963878+shrimma@users.noreply.github.com>
2021-05-07 14:00:01 +0100

    update api version and add G2 sku

--- azurerm/internal/services/maps/maps_account_resource.go
+++ azurerm/internal/services/maps/maps_account_resource.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/services/maps/mgmt/2018-05-01/maps"
+	"github.com/Azure/azure-sdk-for-go/services/maps/mgmt/2021-02-01/maps"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
@@ -53,6 +53,7 @@ func resourceMapsAccount() *schema.Resource {
 				Required: true,
 				ForceNew: true,
 				ValidateFunc: validation.StringInSlice([]string{
+					"G2",
 					"S0",
 					"S1",
 				}, false),
