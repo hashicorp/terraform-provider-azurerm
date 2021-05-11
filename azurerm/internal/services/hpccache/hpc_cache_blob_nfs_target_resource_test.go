9e9cf3f17b5b69cd85a28099cbc475d1e68fc4ab
magodo <wztdyl@sina.com>
2021-05-11 14:16:41 +0800

    modify test

--- azurerm/internal/services/hpccache/hpc_cache_blob_nfs_target_resource_test.go
+++ azurerm/internal/services/hpccache/hpc_cache_blob_nfs_target_resource_test.go
@@ -127,7 +127,7 @@ resource "azurerm_hpc_cache_blob_nfs_target" "test" {
   name                 = "acctest-HPCCTGT-%s"
   resource_group_name  = azurerm_resource_group.test.name
   cache_name           = azurerm_hpc_cache.test.name
-  storage_container_id = azurerm_storage_container.test.resource_manager_id
+  storage_container_id = jsondecode(azurerm_resource_group_template_deployment.storage-containers.output_content).id.value
   namespace_path       = "/p1"
   usage_model          = "READ_HEAVY_INFREQ"
 }
@@ -142,7 +142,7 @@ resource "azurerm_hpc_cache_blob_nfs_target" "test" {
   name                 = "acctest-HPCCTGT-%s"
   resource_group_name  = azurerm_resource_group.test.name
   cache_name           = azurerm_hpc_cache.test.name
-  storage_container_id = azurerm_storage_container.test.resource_manager_id
+  storage_container_id = jsondecode(azurerm_resource_group_template_deployment.storage-containers.output_content).id.value
   namespace_path       = "/p2"
   usage_model          = "READ_HEAVY_INFREQ"
 }
@@ -172,7 +172,7 @@ resource "azurerm_hpc_cache_blob_nfs_target" "test" {
   name                 = "acctest-HPCCTGT-%s"
   resource_group_name  = azurerm_resource_group.test.name
   cache_name           = azurerm_hpc_cache.test.name
-  storage_container_id = azurerm_storage_container.test.resource_manager_id
+  storage_container_id = jsondecode(azurerm_resource_group_template_deployment.storage-containers.output_content).id.value
   namespace_path       = "/p1"
   access_policy_name   = azurerm_hpc_cache_access_policy.test.name
   usage_model          = "READ_HEAVY_INFREQ"
@@ -204,7 +204,7 @@ resource "azurerm_hpc_cache_blob_nfs_target" "test" {
   name                 = "acctest-HPCCTGT-%s"
   resource_group_name  = azurerm_resource_group.test.name
   cache_name           = azurerm_hpc_cache.test.name
-  storage_container_id = azurerm_storage_container.test.resource_manager_id
+  storage_container_id = jsondecode(azurerm_resource_group_template_deployment.storage-containers.output_content).id.value
   namespace_path       = "/blob_storage1"
   access_policy_name   = azurerm_hpc_cache_access_policy.test.name
   usage_model          = "READ_HEAVY_INFREQ"
@@ -236,19 +236,19 @@ provider "azurerm" {
 provider "azuread" {}
 
 resource "azurerm_resource_group" "test" {
-  name     = "acctestRG-storage-%d"
-  location = "%s"
+  name     = "acctestRG-storage-%[1]d"
+  location = "%[2]s"
 }
 
 resource "azurerm_virtual_network" "test" {
-  name                = "acctest-VN-%d"
+  name                = "acctest-VN-%[1]d"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
 }
 
 resource "azurerm_subnet" "test" {
-  name                 = "acctestsub-%d"
+  name                 = "acctestsub-%[1]d"
   resource_group_name  = azurerm_resource_group.test.name
   virtual_network_name = azurerm_virtual_network.test.name
   address_prefix       = "10.0.2.0/24"
@@ -260,24 +260,92 @@ data "azuread_service_principal" "test" {
 }
 
 resource "azurerm_storage_account" "test" {
-  name                      = "accteststorgacc%s"
+  name                      = "accteststorgacc%[3]s"
   resource_group_name       = azurerm_resource_group.test.name
   location                  = azurerm_resource_group.test.location
-  account_tier              = "Premium"
-  account_kind              = "BlockBlobStorage"
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
   account_replication_type  = "LRS"
   is_hns_enabled            = true
   nfsv3_enabled             = true
   enable_https_traffic_only = false
   network_rules {
-    default_action             = "Allow"
+    default_action             = "Deny"
     virtual_network_subnet_ids = [azurerm_subnet.test.id]
   }
 }
 
-resource "azurerm_storage_container" "test" {
-  name                 = "acctest-strgctn-%s"
-  storage_account_name = azurerm_storage_account.test.name
+# Due to https://github.com/terraform-providers/terraform-provider-azurerm/issues/2977 and the fact
+# that the NFSv3 enabled storage account can't allow public network access - otherwise the NFSv3 protocol will fail,
+# we have to use the ARM template to deploy the storage container as a workaround.
+# Once the issue above got resolved, we can instead use the azurerm_storage_container resource.
+resource "azurerm_resource_group_template_deployment" "storage-containers" {
+  name                = "acctest-strgctn-deployment-%[1]d"
+  resource_group_name = azurerm_storage_account.test.resource_group_name
+  deployment_mode     = "Incremental"
+
+  parameters_content = jsonencode({
+    location           = {
+      value = azurerm_storage_account.test.location
+    },
+    storageAccountName = {
+      value = azurerm_storage_account.test.name
+    },
+    containerName      = {
+      value = "acctest-strgctn-hpc-%[1]d"
+    }
+  })
+
+  template_content = <<EOF
+{
+  "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "storageAccountName": {
+      "type": "String"
+    },
+    "containerName": {
+      "type": "String"
+    },
+    "location": {
+      "type": "String"
+    }
+  },
+  "resources": [
+    {
+      "type": "Microsoft.Storage/storageAccounts",
+      "apiVersion": "2019-06-01",
+      "name": "[parameters('storageAccountName')]",
+      "location": "[parameters('location')]",
+      "sku": {
+        "name": "Standard_LRS",
+        "tier": "Standard"
+      },
+      "kind": "StorageV2",
+      "properties": {
+        "accessTier": "Hot"
+      },
+      "resources": [
+        {
+          "type": "blobServices/containers",
+          "apiVersion": "2019-06-01",
+          "name": "[concat('default/', parameters('containerName'))]",
+          "dependsOn": [
+            "[parameters('storageAccountName')]"
+          ]
+        }
+      ]
+    }
+  ],
+
+  "outputs": {
+    "id": {
+      "type": "String",
+      "value": "[resourceId('Microsoft.Storage/storageAccounts/blobServices/containers', parameters('storageAccountName'), 'default', parameters('containerName'))]"
+    }
+  }
+}
+EOF
 }
 
 resource "azurerm_role_assignment" "test_storage_account_contrib" {
@@ -293,12 +361,12 @@ resource "azurerm_role_assignment" "test_storage_blob_data_contrib" {
 }
 
 resource "azurerm_hpc_cache" "test" {
-  name                = "acctest-HPCC-%d"
+  name                = "acctest-HPCC-%[1]d"
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
   cache_size_in_gb    = 3072
   subnet_id           = azurerm_subnet.test.id
   sku_name            = "Standard_2G"
 }
-`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger, data.RandomString, data.RandomString, data.RandomInteger)
+`, data.RandomInteger, data.Locations.Primary, data.RandomString)
 }
