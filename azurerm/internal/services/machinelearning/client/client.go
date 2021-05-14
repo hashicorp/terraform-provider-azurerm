52782d984b2e3be7b0fae161bc3360c5884eee23
Aris van Ommeren <avommeren@vxcompany.com>
2021-05-14 17:00:22 +0200

    Remove AKS clients from ML client package

--- azurerm/internal/services/machinelearning/client/client.go
+++ azurerm/internal/services/machinelearning/client/client.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-	"github.com/Azure/azure-sdk-for-go/services/containerservice/mgmt/2021-02-01/containerservice"
 	"github.com/Azure/azure-sdk-for-go/services/machinelearningservices/mgmt/2020-04-01/machinelearningservices"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/common"
 )
@@ -9,8 +8,6 @@ import (
 type Client struct {
 	WorkspacesClient             *machinelearningservices.WorkspacesClient
 	MachineLearningComputeClient *machinelearningservices.MachineLearningComputeClient
-	KubernetesClustersClient     *containerservice.ManagedClustersClient
-	AgentPoolsClient             *containerservice.AgentPoolsClient
 }
 
 func NewClient(o *common.ClientOptions) *Client {
@@ -20,16 +17,8 @@ func NewClient(o *common.ClientOptions) *Client {
 	MachineLearningComputeClient := machinelearningservices.NewMachineLearningComputeClientWithBaseURI(o.ResourceManagerEndpoint, o.SubscriptionId)
 	o.ConfigureClient(&MachineLearningComputeClient.Client, o.ResourceManagerAuthorizer)
 
-	KubernetesClustersClient := containerservice.NewManagedClustersClientWithBaseURI(o.ResourceManagerEndpoint, o.SubscriptionId)
-	o.ConfigureClient(&KubernetesClustersClient.Client, o.ResourceManagerAuthorizer)
-
-	agentPoolsClient := containerservice.NewAgentPoolsClientWithBaseURI(o.ResourceManagerEndpoint, o.SubscriptionId)
-	o.ConfigureClient(&agentPoolsClient.Client, o.ResourceManagerAuthorizer)
-
 	return &Client{
 		WorkspacesClient:             &WorkspacesClient,
 		MachineLearningComputeClient: &MachineLearningComputeClient,
-		KubernetesClustersClient:     &KubernetesClustersClient,
-		AgentPoolsClient:             &agentPoolsClient,
 	}
 }
