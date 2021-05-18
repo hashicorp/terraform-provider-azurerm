4c6dea27e96c6a54ecefdf6b7ef666c26d3f2642
yupwei68 <yupwei@microsoft.com>
2021-05-18 09:22:23 +0800

    update

--- azurerm/internal/services/storage/storage_account_resource.go
+++ azurerm/internal/services/storage/storage_account_resource.go
@@ -19,6 +19,8 @@ import (
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/locks"
+	msiparse "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/msi/parse"
+	msiValidate "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/msi/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/network"
 	networkValidate "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/network/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/storage/migration"
@@ -316,7 +318,6 @@ func resourceStorageAccount() *schema.Resource {
 			"identity": {
 				Type:     schema.TypeList,
 				Optional: true,
-				Computed: true,
 				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
@@ -326,6 +327,8 @@ func resourceStorageAccount() *schema.Resource {
 							DiffSuppressFunc: suppress.CaseDifference,
 							ValidateFunc: validation.StringInSlice([]string{
 								string(storage.IdentityTypeSystemAssigned),
+								string(storage.IdentityTypeSystemAssignedUserAssigned),
+								string(storage.IdentityTypeUserAssigned),
 							}, true),
 						},
 						"principal_id": {
@@ -335,6 +338,15 @@ func resourceStorageAccount() *schema.Resource {
 						"tenant_id": {
 							Type:     schema.TypeString,
 							Computed: true,
+						},
+						"identity_ids": {
+							Type:     schema.TypeSet,
+							Optional: true,
+							MinItems: 1,
+							Elem: &schema.Schema{
+								Type:         schema.TypeString,
+								ValidateFunc: msiValidate.UserAssignedIdentityID,
+							},
 						},
 					},
 				},
@@ -836,10 +848,11 @@ func resourceStorageAccountCreate(d *schema.ResourceData, meta interface{}) erro
 		parameters.AccountPropertiesCreateParameters.MinimumTLSVersion = storage.MinimumTLSVersion(minimumTLSVersion)
 	}
 
-	if _, ok := d.GetOk("identity"); ok {
-		storageAccountIdentity := expandAzureRmStorageAccountIdentity(d)
-		parameters.Identity = storageAccountIdentity
+	storageAccountIdentity, err := expandAzureRmStorageAccountIdentity(d.Get("identity").([]interface{}))
+	if err != nil {
+		return err
 	}
+	parameters.Identity = storageAccountIdentity
 
 	if v, ok := d.GetOk("azure_files_authentication"); ok {
 		expandAADFilesAuthentication, err := expandArmStorageAccountAzureFilesAuthentication(v.([]interface{}))
@@ -1181,12 +1194,16 @@ func resourceStorageAccountUpdate(d *schema.ResourceData, meta interface{}) erro
 	}
 
 	if d.HasChange("identity") {
+		storageAccountIdentity, err := expandAzureRmStorageAccountIdentity(d.Get("identity").([]interface{}))
+		if err != nil {
+			return err
+		}
 		opts := storage.AccountUpdateParameters{
-			Identity: expandAzureRmStorageAccountIdentity(d),
+			Identity: storageAccountIdentity,
 		}
 
 		if _, err := client.Update(ctx, resourceGroupName, storageAccountName, opts); err != nil {
-			return fmt.Errorf("Error updating Azure Storage Account identity %q: %+v", storageAccountName, err)
+			return fmt.Errorf("updating Azure Storage Account identity %q: %+v", storageAccountName, err)
 		}
 	}
 
@@ -1484,7 +1501,10 @@ func resourceStorageAccountRead(d *schema.ResourceData, meta interface{}) error 
 		d.Set("secondary_access_key", storageAccountKeys[1].Value)
 	}
 
-	identity := flattenAzureRmStorageAccountIdentity(resp.Identity)
+	identity, err := flattenAzureRmStorageAccountIdentity(resp.Identity)
+	if err != nil {
+		return err
+	}
 	if err := d.Set("identity", identity); err != nil {
 		return err
 	}
@@ -2441,32 +2461,76 @@ func flattenStorageAccountBypass(input storage.Bypass) []interface{} {
 	return bypass
 }
 
-func expandAzureRmStorageAccountIdentity(d *schema.ResourceData) *storage.Identity {
-	identities := d.Get("identity").([]interface{})
-	identity := identities[0].(map[string]interface{})
-	identityType := identity["type"].(string)
-	return &storage.Identity{
-		Type: storage.IdentityType(identityType),
+func expandAzureRmStorageAccountIdentity(vs []interface{}) (*storage.Identity, error) {
+	if len(vs) == 0 {
+		return &storage.Identity{
+			Type: storage.IdentityTypeNone,
+		}, nil
 	}
+
+	v := vs[0].(map[string]interface{})
+	identity := storage.Identity{
+		Type: storage.IdentityType(v["type"].(string)),
+	}
+
+	var identityIdSet []interface{}
+	if identityIds, exists := v["identity_ids"]; exists {
+		identityIdSet = identityIds.(*schema.Set).List()
+	}
+
+	// If type contains `UserAssigned`, `identity_ids` must be specified and have at least 1 element
+	if identity.Type == storage.IdentityTypeUserAssigned || identity.Type == storage.IdentityTypeSystemAssignedUserAssigned {
+		if len(identityIdSet) == 0 {
+			return nil, fmt.Errorf("`identity_ids` must have at least 1 element when `type` includes `UserAssigned`")
+		}
+
+		userAssignedIdentities := make(map[string]*storage.UserAssignedIdentity)
+		for _, id := range identityIdSet {
+			userAssignedIdentities[id.(string)] = &storage.UserAssignedIdentity{}
+		}
+
+		identity.UserAssignedIdentities = userAssignedIdentities
+	} else if len(identityIdSet) > 0 {
+		// If type does _not_ contain `UserAssigned` (i.e. is set to `SystemAssigned` or defaulted to `None`), `identity_ids` is not allowed
+		return nil, fmt.Errorf("`identity_ids` can only be specified when `type` includes `UserAssigned`; but `type` is currently %q", identity.Type)
+	}
+
+	return &identity, nil
 }
 
-func flattenAzureRmStorageAccountIdentity(identity *storage.Identity) []interface{} {
-	if identity == nil {
-		return make([]interface{}, 0)
+func flattenAzureRmStorageAccountIdentity(identity *storage.Identity) ([]interface{}, error) {
+	if identity == nil || identity.Type == storage.IdentityTypeNone {
+		return make([]interface{}, 0), nil
 	}
 
-	result := make(map[string]interface{})
-	if identity.Type != "" {
-		result["type"] = string(identity.Type)
-	}
+	var principalId, tenantId string
 	if identity.PrincipalID != nil {
-		result["principal_id"] = *identity.PrincipalID
-	}
-	if identity.TenantID != nil {
-		result["tenant_id"] = *identity.TenantID
+		principalId = *identity.PrincipalID
 	}
 
-	return []interface{}{result}
+	if identity.TenantID != nil {
+		tenantId = *identity.TenantID
+	}
+
+	identityIds := make([]interface{}, 0)
+	if identity.UserAssignedIdentities != nil {
+		for key := range identity.UserAssignedIdentities {
+			parsedId, err := msiparse.UserAssignedIdentityID(key)
+			if err != nil {
+				return nil, err
+			}
+			identityIds = append(identityIds, parsedId.ID())
+		}
+	}
+
+	return []interface{}{
+		map[string]interface{}{
+			"type":         string(identity.Type),
+			"principal_id": principalId,
+			"tenant_id":    tenantId,
+			"identity_ids": schema.NewSet(schema.HashString, identityIds),
+		},
+	}, nil
 }
 
 func getBlobConnectionString(blobEndpoint *string, acctName *string, acctKey *string) string {
