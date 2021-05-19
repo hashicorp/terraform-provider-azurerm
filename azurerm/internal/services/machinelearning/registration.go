57b11588ec2182a8e6160eb711ee37156db1c830
Michael Gross <leachimgross@gmail.com>
2021-05-20 01:25:34 +0200

    new resource `azurerm_machine_learning_inference_cluster` (#11550)
    Fixes #11252

--- azurerm/internal/services/machinelearning/registration.go
+++ azurerm/internal/services/machinelearning/registration.go
@@ -27,6 +27,7 @@ func (r Registration) SupportedDataSources() map[string]*schema.Resource {
 // SupportedResources returns the supported Resources supported by this Service
 func (r Registration) SupportedResources() map[string]*schema.Resource {
 	return map[string]*schema.Resource{
-		"azurerm_machine_learning_workspace": resourceMachineLearningWorkspace(),
+		"azurerm_machine_learning_workspace":         resourceMachineLearningWorkspace(),
+		"azurerm_machine_learning_inference_cluster": resourceAksInferenceCluster(),
 	}
 }
