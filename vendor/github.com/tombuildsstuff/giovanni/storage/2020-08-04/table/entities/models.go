9912ff2cba3d42cdda0c73ff44e7912b1c92dc0e
yupwei68 <yupwei@microsoft.com>
2021-05-24 15:23:56 +0800

    update

--- vendor/github.com/tombuildsstuff/giovanni/storage/2020-08-04/table/entities/models.go
+++
@@ -1,9 +0,0 @@
-package entities
-
-type MetaDataLevel string
-
-var (
-	NoMetaData      MetaDataLevel = "nometadata"
-	MinimalMetaData MetaDataLevel = "minimalmetadata"
-	FullMetaData    MetaDataLevel = "fullmetadata"
-)
