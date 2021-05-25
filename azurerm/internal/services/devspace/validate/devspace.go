12f3076ed9812cd2097ad7201334b34af6d73f87
kt <kt@katbyte.me>
2021-05-25 12:56:03 -0700

    sdkv2shim - devspace (#11903)

--- azurerm/internal/services/devspace/validate/devspace.go
+++ azurerm/internal/services/devspace/validate/devspace.go
@@ -3,11 +3,11 @@ package validate
 import (
 	"regexp"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/validation"
 )
 
-func DevSpaceName() schema.SchemaValidateFunc {
+func DevSpaceName() pluginsdk.SchemaValidateFunc {
 	return func(i interface{}, k string) (warnings []string, errors []error) {
 		// Length should be between 3 and 31.
 		if warnings, errors = validation.StringLenBetween(3, 31)(i, k); len(errors) > 0 {
