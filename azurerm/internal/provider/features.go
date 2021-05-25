50e3ee59f9829f61a81886b53daf005e52e839f0
kt <kt@katbyte.me>
2021-05-25 12:57:11 -0700

    provider - some misc tidying up (#11894)

--- azurerm/internal/provider/features.go
+++ azurerm/internal/provider/features.go
@@ -9,7 +9,7 @@ func schemaFeatures(supportLegacyTestSuite bool) *pluginsdk.Schema {
 	// NOTE: if there's only one nested field these want to be Required (since there's no point
 	//       specifying the block otherwise) - however for 2+ they should be optional
 	features := map[string]*pluginsdk.Schema{
-		//lintignore:XS003
+		// lintignore:XS003
 		"key_vault": {
 			Type:     pluginsdk.TypeList,
 			Optional: true,
@@ -69,7 +69,7 @@ func schemaFeatures(supportLegacyTestSuite bool) *pluginsdk.Schema {
 			},
 		},
 
-		//lintignore:XS003
+		// lintignore:XS003
 		"virtual_machine": {
 			Type:     pluginsdk.TypeList,
 			Optional: true,
