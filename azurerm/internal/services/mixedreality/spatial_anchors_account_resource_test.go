dc652d3fbd0beaaf7b3d34c89974c10d9570e0f5
Vladimir Lazarenko <favoretti@users.noreply.github.com>
2021-05-25 23:09:22 +0200

    sdkv2 shim - mixedreality (#11918)

--- azurerm/internal/services/mixedreality/spatial_anchors_account_resource_test.go
+++ azurerm/internal/services/mixedreality/spatial_anchors_account_resource_test.go
@@ -5,12 +5,11 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance/check"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/mixedreality/parse"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
@@ -21,10 +20,10 @@ func TestAccSpatialAnchorsAccount_basic(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_spatial_anchors_account", "test")
 	r := SpatialAnchorsAccountResource{}
 
-	data.ResourceTest(t, r, []resource.TestStep{
+	data.ResourceTest(t, r, []acceptance.TestStep{
 		{
 			Config: r.basic(data),
-			Check: resource.ComposeTestCheckFunc(
+			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 				check.That(data.ResourceName).Key("account_id").Exists(),
 				check.That(data.ResourceName).Key("account_domain").Exists(),
@@ -38,10 +37,10 @@ func TestAccSpatialAnchorsAccount_complete(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_spatial_anchors_account", "test")
 	r := SpatialAnchorsAccountResource{}
 
-	data.ResourceTest(t, r, []resource.TestStep{
+	data.ResourceTest(t, r, []acceptance.TestStep{
 		{
 			Config: r.complete(data),
-			Check: resource.ComposeTestCheckFunc(
+			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 				check.That(data.ResourceName).Key("tags.%").HasValue("1"),
 				check.That(data.ResourceName).Key("tags.Environment").HasValue("Production"),
@@ -51,7 +50,7 @@ func TestAccSpatialAnchorsAccount_complete(t *testing.T) {
 	})
 }
 
-func (SpatialAnchorsAccountResource) Exists(ctx context.Context, clients *clients.Client, state *terraform.InstanceState) (*bool, error) {
+func (SpatialAnchorsAccountResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	id, err := parse.SpatialAnchorsAccountID(state.ID)
 	if err != nil {
 		return nil, err
