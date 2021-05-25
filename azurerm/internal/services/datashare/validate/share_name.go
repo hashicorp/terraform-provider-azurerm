06a8521efe814d6ab19eb774d9d9644391cef4e4
kt <kt@katbyte.me>
2021-05-25 12:56:17 -0700

    sdkv2shim - datashare (#11907)

--- azurerm/internal/services/datashare/validate/share_name.go
+++ azurerm/internal/services/datashare/validate/share_name.go
@@ -3,11 +3,11 @@ package validate
 import (
 	"regexp"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/validation"
 )
 
-func ShareName() schema.SchemaValidateFunc {
+func ShareName() pluginsdk.SchemaValidateFunc {
 	return validation.StringMatch(
 		regexp.MustCompile(`^\w{2,90}$`), `DataShare name can only contain alphanumeric characters and _, and must be between 2 and 90 characters long.`,
 	)
