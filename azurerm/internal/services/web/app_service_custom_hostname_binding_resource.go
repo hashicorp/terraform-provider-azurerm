1a6a4a5302d4ca0f6e28afc862aa5c5a301c27cf
Vladimir Lazarenko <favoretti@users.noreply.github.com>
2021-05-25 20:34:04 +0200

    sdkv2 shim - web (#11870)

--- azurerm/internal/services/web/app_service_custom_hostname_binding_resource.go
+++ azurerm/internal/services/web/app_service_custom_hostname_binding_resource.go
@@ -6,22 +6,21 @@ import (
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/web/mgmt/2020-06-01/web"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/locks"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/web/parse"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
 var appServiceCustomHostnameBindingResourceName = "azurerm_app_service_custom_hostname_binding"
 
-func resourceAppServiceCustomHostnameBinding() *schema.Resource {
-	return &schema.Resource{
+func resourceAppServiceCustomHostnameBinding() *pluginsdk.Resource {
+	return &pluginsdk.Resource{
 		Create: resourceAppServiceCustomHostnameBindingCreate,
 		Read:   resourceAppServiceCustomHostnameBindingRead,
 		Delete: resourceAppServiceCustomHostnameBindingDelete,
@@ -30,16 +29,16 @@ func resourceAppServiceCustomHostnameBinding() *schema.Resource {
 			return err
 		}),
 
-		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(30 * time.Minute),
-			Read:   schema.DefaultTimeout(5 * time.Minute),
-			Update: schema.DefaultTimeout(30 * time.Minute),
-			Delete: schema.DefaultTimeout(30 * time.Minute),
+		Timeouts: &pluginsdk.ResourceTimeout{
+			Create: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
+			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
 		},
 
-		Schema: map[string]*schema.Schema{
+		Schema: map[string]*pluginsdk.Schema{
 			"hostname": {
-				Type:     schema.TypeString,
+				Type:     pluginsdk.TypeString,
 				Required: true,
 				ForceNew: true,
 			},
@@ -47,13 +46,13 @@ func resourceAppServiceCustomHostnameBinding() *schema.Resource {
 			"resource_group_name": azure.SchemaResourceGroupName(),
 
 			"app_service_name": {
-				Type:     schema.TypeString,
+				Type:     pluginsdk.TypeString,
 				Required: true,
 				ForceNew: true,
 			},
 
 			"ssl_state": {
-				Type:     schema.TypeString,
+				Type:     pluginsdk.TypeString,
 				Optional: true,
 				Computed: true,
 				ForceNew: true,
@@ -64,7 +63,7 @@ func resourceAppServiceCustomHostnameBinding() *schema.Resource {
 			},
 
 			"thumbprint": {
-				Type:         schema.TypeString,
+				Type:         pluginsdk.TypeString,
 				Optional:     true,
 				Computed:     true,
 				ForceNew:     true,
@@ -72,14 +71,14 @@ func resourceAppServiceCustomHostnameBinding() *schema.Resource {
 			},
 
 			"virtual_ip": {
-				Type:     schema.TypeString,
+				Type:     pluginsdk.TypeString,
 				Computed: true,
 			},
 		},
 	}
 }
 
-func resourceAppServiceCustomHostnameBindingCreate(d *schema.ResourceData, meta interface{}) error {
+func resourceAppServiceCustomHostnameBindingCreate(d *pluginsdk.ResourceData, meta interface{}) error {
 	client := meta.(*clients.Client).Web.AppServicesClient
 	ctx, cancel := timeouts.ForCreate(meta.(*clients.Client).StopContext, d)
 	defer cancel()
@@ -147,7 +146,7 @@ func resourceAppServiceCustomHostnameBindingCreate(d *schema.ResourceData, meta 
 	return resourceAppServiceCustomHostnameBindingRead(d, meta)
 }
 
-func resourceAppServiceCustomHostnameBindingRead(d *schema.ResourceData, meta interface{}) error {
+func resourceAppServiceCustomHostnameBindingRead(d *pluginsdk.ResourceData, meta interface{}) error {
 	client := meta.(*clients.Client).Web.AppServicesClient
 	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
 	defer cancel()
@@ -180,7 +179,7 @@ func resourceAppServiceCustomHostnameBindingRead(d *schema.ResourceData, meta in
 	return nil
 }
 
-func resourceAppServiceCustomHostnameBindingDelete(d *schema.ResourceData, meta interface{}) error {
+func resourceAppServiceCustomHostnameBindingDelete(d *pluginsdk.ResourceData, meta interface{}) error {
 	client := meta.(*clients.Client).Web.AppServicesClient
 	ctx, cancel := timeouts.ForDelete(meta.(*clients.Client).StopContext, d)
 	defer cancel()
