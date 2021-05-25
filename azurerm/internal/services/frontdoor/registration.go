f6447fbf3693708aa770c1661bb826123ba22e16
Vladimir Lazarenko <favoretti@users.noreply.github.com>
2021-05-26 01:10:23 +0200

    sdkv2 shim - frontdoor (#11942)

--- azurerm/internal/services/frontdoor/registration.go
+++ azurerm/internal/services/frontdoor/registration.go
@@ -1,7 +1,7 @@
 package frontdoor
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk"
 )
 
 // TODO: move this to the network one
@@ -21,13 +21,13 @@ func (r Registration) WebsiteCategories() []string {
 }
 
 // SupportedDataSources returns the supported Data Sources supported by this Service
-func (r Registration) SupportedDataSources() map[string]*schema.Resource {
-	return map[string]*schema.Resource{}
+func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
+	return map[string]*pluginsdk.Resource{}
 }
 
 // SupportedResources returns the supported Resources supported by this Service
-func (r Registration) SupportedResources() map[string]*schema.Resource {
-	return map[string]*schema.Resource{
+func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
+	return map[string]*pluginsdk.Resource{
 		"azurerm_frontdoor":                            resourceFrontDoor(),
 		"azurerm_frontdoor_firewall_policy":            resourceFrontDoorFirewallPolicy(),
 		"azurerm_frontdoor_custom_https_configuration": resourceFrontDoorCustomHttpsConfiguration(),
