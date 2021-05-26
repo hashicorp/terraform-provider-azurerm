0d96e91cb857833bb139f730be08e1f85024cb14
tombuildsstuff <git@ibuildstuff.co.uk>
2021-05-26 11:15:26 +0200

    sdk: downgrading to the `2017-04-01` API for some sdks

--- azurerm/internal/services/eventhub/sdk/authorizationrulesnamespaces/version.go
+++ azurerm/internal/services/eventhub/sdk/authorizationrulesnamespaces/version.go
@@ -2,7 +2,7 @@ package authorizationrulesnamespaces
 
 import "fmt"
 
-const defaultApiVersion = "2018-01-01-preview"
+const defaultApiVersion = "2017-04-01"
 
 func userAgent() string {
 	return fmt.Sprintf("pandora/authorizationrulesnamespaces/%s", defaultApiVersion)
--- azurerm/internal/services/eventhub/sdk/eventhubs/version.go
+++ azurerm/internal/services/eventhub/sdk/eventhubs/version.go
@@ -2,7 +2,7 @@ package eventhubs
 
 import "fmt"
 
-const defaultApiVersion = "2018-01-01-preview"
+const defaultApiVersion = "2017-04-01"
 
 func userAgent() string {
 	return fmt.Sprintf("pandora/eventhubs/%s", defaultApiVersion)
