1ec855cc1348e670e227a76840636696b4bcfd22
kohei3110 <kk.31108424@gmail.com>
2021-05-27 22:39:29 +0900

    fix azurerm_spatial_anchors_account tags argument always forces replacement of spatial_anchors_account resource #11037

--- azurerm/internal/services/mixedreality/spatial_anchors_account_resource.go
+++ azurerm/internal/services/mixedreality/spatial_anchors_account_resource.go
@@ -20,21 +20,20 @@ import (
 
 func resourceSpatialAnchorsAccount() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceSpatialAnchorsAccountCreate,
+		Create: resourceSpatialAnchorsAccountCreateUpdate,
 		Read:   resourceSpatialAnchorsAccountRead,
+		Update: resourceSpatialAnchorsAccountCreateUpdate,
 		Delete: resourceSpatialAnchorsAccountDelete,
 		Importer: pluginsdk.ImporterValidatingResourceId(func(id string) error {
 			_, err := parse.SpatialAnchorsAccountID(id)
 			return err
 		}),
-
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(30 * time.Minute),
 			Read:   schema.DefaultTimeout(5 * time.Minute),
 			Update: schema.DefaultTimeout(30 * time.Minute),
 			Delete: schema.DefaultTimeout(30 * time.Minute),
 		},
-
 		Schema: map[string]*schema.Schema{
 			"name": {
 				Type:     schema.TypeString,
@@ -45,26 +44,20 @@ func resourceSpatialAnchorsAccount() *schema.Resource {
 					"Spatial Anchors Account name must be 1 - 90 characters long, contain only word characters and underscores.",
 				),
 			},
-
-			"location": azure.SchemaLocation(),
-
+			"location":            azure.SchemaLocation(),
 			"resource_group_name": azure.SchemaResourceGroupName(),
-
-			"tags": tags.ForceNewSchema(),
+			"tags":                tags.Schema(),
 		},
 	}
 }
-
-func resourceSpatialAnchorsAccountCreate(d *schema.ResourceData, meta interface{}) error {
+func resourceSpatialAnchorsAccountCreateUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*clients.Client).MixedReality.SpatialAnchorsAccountClient
 	ctx, cancel := timeouts.ForCreateUpdate(meta.(*clients.Client).StopContext, d)
 	defer cancel()
-
 	name := d.Get("name").(string)
 	location := azure.NormalizeLocation(d.Get("location").(string))
 	resourceGroup := d.Get("resource_group_name").(string)
 	t := d.Get("tags").(map[string]interface{})
-
 	if d.IsNewResource() {
 		existing, err := client.Get(ctx, resourceGroup, name)
 		if err != nil {
@@ -72,80 +65,63 @@ func resourceSpatialAnchorsAccountCreate(d *schema.ResourceData, meta interface{
 				return fmt.Errorf("checking for presence of existing Spatial Anchors Account %q (Resource Group %q): %s", name, resourceGroup, err)
 			}
 		}
-
 		if existing.ID != nil && *existing.ID != "" {
 			return tf.ImportAsExistsError("azurerm_spatial_anchors_account", *existing.ID)
 		}
 	}
-
 	account := mixedreality.SpatialAnchorsAccount{
 		Location: &location,
 		Tags:     tags.Expand(t),
 	}
-
 	if _, err := client.Create(ctx, resourceGroup, name, account); err != nil {
 		return fmt.Errorf("creating/updating Spatial Anchors Account %q (Resource Group %q): %+v", name, resourceGroup, err)
 	}
-
 	resp, err := client.Get(ctx, resourceGroup, name)
 	if err != nil {
 		return fmt.Errorf("retrieving Spatial Anchors Account %q (Resource Group %q): %+v", name, resourceGroup, err)
 	}
-
 	if resp.ID == nil {
 		return fmt.Errorf("cannot read Spatial Anchors Account %q (Resource Group %q) ID", name, resourceGroup)
 	}
-
 	d.SetId(*resp.ID)
-
 	return resourceSpatialAnchorsAccountRead(d, meta)
 }
-
 func resourceSpatialAnchorsAccountRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*clients.Client).MixedReality.SpatialAnchorsAccountClient
 	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
 	defer cancel()
-
 	id, err := parse.SpatialAnchorsAccountID(d.Id())
 	if err != nil {
 		return err
 	}
-
 	resp, err := client.Get(ctx, id.ResourceGroup, id.Name)
 	if err != nil {
 		if utils.ResponseWasNotFound(resp.Response) {
 			d.SetId("")
 			return nil
 		}
-
 		return fmt.Errorf("retrieving Spatial Anchors Account %q (Resource Group %q): %+v", id.Name, id.ResourceGroup, err)
 	}
-
 	d.Set("name", resp.Name)
 	d.Set("resource_group_name", id.ResourceGroup)
 	if location := resp.Location; location != nil {
 		d.Set("location", azure.NormalizeLocation(*location))
 	}
-
 	return tags.FlattenAndSet(d, resp.Tags)
 }
-
 func resourceSpatialAnchorsAccountDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*clients.Client).MixedReality.SpatialAnchorsAccountClient
 	ctx, cancel := timeouts.ForDelete(meta.(*clients.Client).StopContext, d)
 	defer cancel()
-
 	id, err := parse.SpatialAnchorsAccountID(d.Id())
 	if err != nil {
 		return err
 	}
-
 	response, err := client.Delete(ctx, id.ResourceGroup, id.Name)
 	if err != nil {
 		if !utils.ResponseWasNotFound(response) {
 			return fmt.Errorf("deleting Spatial Anchors Account %q (Resource Group %q): %+v", id.Name, id.ResourceGroup, err)
 		}
 	}
-
 	return nil
 }
