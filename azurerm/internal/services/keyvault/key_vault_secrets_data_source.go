e37d06b637824188ffa5e6787c25e7e264f04dc6
Vladimir Lazarenko <favoretti@gmail.com>
2021-06-09 18:02:32 +0200

    New datasource: `azurerm_key_vault_secrets`
    TODO:
    [ ] Add docs once implementation is deemed ok
    
    ```
    2021/06/09 17:27:37 [DEBUG] not using binary driver name, it's no longer needed
    2021/06/09 17:27:38 [DEBUG] not using binary driver name, it's no longer needed
    === RUN   TestAccDataSourceKeyVaultSecrets_basic
    === PAUSE TestAccDataSourceKeyVaultSecrets_basic
    === CONT  TestAccDataSourceKeyVaultSecrets_basic
    --- PASS: TestAccDataSourceKeyVaultSecrets_basic (351.90s)
    PASS
    ```

---
+++ azurerm/internal/services/keyvault/key_vault_secrets_data_source.go
@@ -0,0 +1,103 @@
+package keyvault
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/keyvault/parse"
+	keyVaultValidate "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/keyvault/validate"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
+)
+
+func dataSourceKeyVaultSecrets() *pluginsdk.Resource {
+	return &pluginsdk.Resource{
+		Read: dataSourceKeyVaultSecretsRead,
+
+		Timeouts: &pluginsdk.ResourceTimeout{
+			Read: pluginsdk.DefaultTimeout(5 * time.Minute),
+		},
+
+		Schema: map[string]*pluginsdk.Schema{
+			"key_vault_id": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				ValidateFunc: keyVaultValidate.VaultID,
+			},
+
+			"max_results": {
+				Type:     pluginsdk.TypeInt,
+				Optional: true,
+				Default:  25,
+			},
+
+			"all": {
+				Type:     pluginsdk.TypeList,
+				Computed: true,
+				Elem: &pluginsdk.Schema{
+					Type: pluginsdk.TypeString,
+				},
+			},
+		},
+	}
+}
+
+func dataSourceKeyVaultSecretsRead(d *pluginsdk.ResourceData, meta interface{}) error {
+	keyVaultsClient := meta.(*clients.Client).KeyVault
+	client := meta.(*clients.Client).KeyVault.ManagementClient
+	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	maxResults := d.Get("max_results").(int)
+	keyVaultId, err := parse.VaultID(d.Get("key_vault_id").(string))
+	if err != nil {
+		return err
+	}
+
+	keyVaultBaseUri, err := keyVaultsClient.BaseUriForKeyVault(ctx, *keyVaultId)
+	if err != nil {
+		return fmt.Errorf("fetching base vault url from id %q: %+v", *keyVaultId, err)
+	}
+
+	// we always want to get the latest version
+	resp, err := client.GetSecrets(ctx, *keyVaultBaseUri, utils.Int32(int32(maxResults)))
+	if err != nil {
+		return fmt.Errorf("Error making Read request on Azure KeyVault %q: %+v", *keyVaultId, err)
+	}
+
+	d.SetId(keyVaultId.ID())
+
+	var all []string
+
+	if resp.Response().Value != nil {
+		for _, v := range *resp.Response().Value {
+			name, err := parseNameFromSecretUrl(*v.ID)
+			if err != nil {
+				return err
+			}
+			all = append(all, *name)
+		}
+	}
+
+	d.Set("all", all)
+	d.Set("key_vault_id", keyVaultId.ID())
+
+	return nil
+}
+
+func parseNameFromSecretUrl(input string) (*string, error) {
+	uri, err := url.Parse(input)
+	if err != nil {
+		return nil, err
+	}
+	// https://favoretti-keyvault.vault.azure.net/secrets/secret-name
+	segments := strings.Split(uri.Path, "/")
+	if len(segments) != 3 {
+		return nil, fmt.Errorf("expected a Path in the format `/secrets/secret-name` but got %q", uri.Path)
+	}
+	return &segments[2], nil
+}
---
+++ azurerm/internal/services/keyvault/key_vault_secrets_data_source_test.go
@@ -0,0 +1,38 @@
+package keyvault_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance/check"
+)
+
+type KeyVaultSecretsDataSource struct {
+}
+
+func TestAccDataSourceKeyVaultSecrets_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "data.azurerm_key_vault_secrets", "test")
+	r := KeyVaultSecretsDataSource{}
+
+	data.DataSourceTest(t, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).Key("all.#").HasValue("1"),
+			),
+		},
+	})
+}
+
+func (KeyVaultSecretsDataSource) basic(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+data "azurerm_key_vault_secrets" "test" {
+  key_vault_id = azurerm_key_vault.test.id
+
+  depends_on = [azurerm_key_vault_secret.test]
+}
+`, KeyVaultSecretResource{}.basic(data))
+}
