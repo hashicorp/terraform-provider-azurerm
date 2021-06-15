46b191f9623ce9c2e1c8aaa6fa6e8d526e26bda2
tombuildsstuff <git@ibuildstuff.co.uk>
2021-06-15 08:40:26 +0200

    go mod vendor

--- vendor/github.com/terraform-providers/terraform-provider-azuread/azuread/helpers/tf/errors.go
+++
@@ -1,8 +0,0 @@
-package tf
-
-import "fmt"
-
-func ImportAsExistsError(resourceName, id string) error {
-	msg := "A resource with the ID %q already exists - to be managed via Terraform this resource needs to be imported into the State. Please see the resource documentation for %q for more information."
-	return fmt.Errorf(msg, id, resourceName)
-}
