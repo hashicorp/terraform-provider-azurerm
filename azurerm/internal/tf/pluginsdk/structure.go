9733af137b1314543d3b10f4e1a412cb6926c572
tombuildsstuff <git@ibuildstuff.co.uk>
2021-06-15 07:54:03 +0200

    scripts: adding an alias for structure

---
+++ azurerm/internal/tf/pluginsdk/structure.go
@@ -0,0 +1,7 @@
+package pluginsdk
+
+import "github.com/hashicorp/terraform-plugin-sdk/helper/structure"
+
+var ExpandJsonFromString = structure.ExpandJsonFromString
+var FlattenJsonToString = structure.FlattenJsonToString
+var SuppressJsonDiff = structure.SuppressJsonDiff
