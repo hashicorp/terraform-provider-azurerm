bed36444d9128d2106fbff65a7a0f70caf74af72
HarleyB123 <Harley.Bates@kpmg.co.uk>
2021-06-18 11:29:39 +0100

    added groups to update function

--- azurerm/internal/services/policy/policy_set_definition_resource.go
+++ azurerm/internal/services/policy/policy_set_definition_resource.go
@@ -620,6 +620,7 @@ func expandAzureRMPolicySetDefinitionPolicyDefinitionsUpdate(d *pluginsdk.Resour
 			PolicyDefinitionID:          utils.String(d.Get(fmt.Sprintf("policy_definition_reference.%d.policy_definition_id", i)).(string)),
 			Parameters:                  parameters,
 			PolicyDefinitionReferenceID: utils.String(d.Get(fmt.Sprintf("policy_definition_reference.%d.reference_id", i)).(string)),
+			GroupNames:                  utils.ExpandStringSlice(i["policy_group_names"].(*pluginsdk.Set).List()),
 		})
 	}
 
