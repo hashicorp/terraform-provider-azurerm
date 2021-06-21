c06532a7dec7513bdd18b927105674001986ead2
tombuildsstuff <git@ibuildstuff.co.uk>
2021-06-21 09:42:04 +0200

    sdk: upgrading the Eventhub SDK to 2021-01-01-preview

--- azurerm/internal/services/eventhub/sdk/namespaces/model_keyvaultproperties.go
+++ azurerm/internal/services/eventhub/sdk/namespaces/model_keyvaultproperties.go
@@ -1,7 +1,8 @@
 package namespaces
 
 type KeyVaultProperties struct {
-	KeyName     *string `json:"keyName,omitempty"`
-	KeyVaultUri *string `json:"keyVaultUri,omitempty"`
-	KeyVersion  *string `json:"keyVersion,omitempty"`
+	Identity    *UserAssignedIdentityProperties `json:"identity,omitempty"`
+	KeyName     *string                         `json:"keyName,omitempty"`
+	KeyVaultUri *string                         `json:"keyVaultUri,omitempty"`
+	KeyVersion  *string                         `json:"keyVersion,omitempty"`
 }
