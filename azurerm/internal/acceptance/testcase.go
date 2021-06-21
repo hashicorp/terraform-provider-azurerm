d7e69d91e49b4db0c233e4f90d9830a4caec49c9
Tom Harvey <tombuildsstuff@users.noreply.github.com>
2021-06-21 16:14:33 +0200

    Update azurerm/internal/acceptance/testcase.go
    Co-authored-by: Tom Bamford <tbamford@hashicorp.com>

--- azurerm/internal/acceptance/testcase.go
+++ azurerm/internal/acceptance/testcase.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance/helpers"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance/testclient"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance/types"
