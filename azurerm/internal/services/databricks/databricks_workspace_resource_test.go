69f3ef2f4ea1195dcb44fa2c2d76f2651d82a041
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2021-06-23 16:37:07 -0700

    Attempt to fix complete test case

--- azurerm/internal/services/databricks/databricks_workspace_resource_test.go
+++ azurerm/internal/services/databricks/databricks_workspace_resource_test.go
@@ -318,6 +318,22 @@ resource "azurerm_subnet" "private" {
   address_prefix       = "10.0.2.0/24"
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "acctest-nsg-private-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "public" {
+  subnet_id                 = azurerm_subnet.public.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "private" {
+  subnet_id                 = azurerm_subnet.private.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 resource "azurerm_databricks_workspace" "test" {
   name                        = "acctestDBW-%[1]d"
   resource_group_name         = azurerm_resource_group.test.name
