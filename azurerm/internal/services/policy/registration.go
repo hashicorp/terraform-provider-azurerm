3cc35eb307c3e4dfc858ef757336f6ff7eb7902c
tombuildsstuff <git@ibuildstuff.co.uk>
2021-06-24 12:08:26 +0200

    r/policy_assignment: removing support in 3.0 mode

--- azurerm/internal/services/policy/registration.go
+++ azurerm/internal/services/policy/registration.go
@@ -1,6 +1,7 @@
 package policy
 
 import (
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/features"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/sdk"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk"
 )
@@ -45,11 +46,16 @@ func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
 
 // SupportedResources returns the supported Resources supported by this Service
 func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
-	return map[string]*pluginsdk.Resource{
-		"azurerm_policy_assignment":                               resourceArmPolicyAssignment(),
+	resources := map[string]*pluginsdk.Resource{
 		"azurerm_policy_definition":                               resourceArmPolicyDefinition(),
 		"azurerm_policy_set_definition":                           resourceArmPolicySetDefinition(),
 		"azurerm_policy_remediation":                              resourceArmPolicyRemediation(),
 		"azurerm_virtual_machine_configuration_policy_assignment": resourceVirtualMachineConfigurationPolicyAssignment(),
 	}
+
+	if !features.ThreePointOh() {
+		resources["azurerm_policy_assignment"] = resourceArmPolicyAssignment()
+	}
+
+	return resources
 }
