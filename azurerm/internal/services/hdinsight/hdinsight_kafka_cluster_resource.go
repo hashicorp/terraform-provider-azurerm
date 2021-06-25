4e8097c6e475b9ff20cf2b7a8437f7ddd4320044
Xiaochuan Gong <xiaochuan.gong@grabtaxi.com>
2021-06-25 08:16:52 +0800

    remove input nil check, as len() for nil slices is defined as zero

--- azurerm/internal/services/hdinsight/hdinsight_kafka_cluster_resource.go
+++ azurerm/internal/services/hdinsight/hdinsight_kafka_cluster_resource.go
@@ -388,7 +388,7 @@ func resourceHDInsightKafkaClusterRead(d *pluginsdk.ResourceData, meta interface
 }
 
 func expandHDInsightKafkaComponentVersion(input []interface{}) map[string]*string {
-	if input == nil || len(input) == 0 || input[0] == nil {
+	if len(input) == 0 || input[0] == nil {
 		return map[string]*string{"kafka": utils.String("")}
 	}
 	vs := input[0].(map[string]interface{})
