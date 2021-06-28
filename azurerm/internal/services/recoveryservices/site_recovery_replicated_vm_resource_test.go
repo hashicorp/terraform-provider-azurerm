b84c56229d0ad5dc733f989ed0f7adc588b751f2
Matthew Frahry <mbfrahry@gmail.com>
2021-06-28 09:43:21 -0700

    Afix test

--- azurerm/internal/services/recoveryservices/site_recovery_replicated_vm_resource_test.go
+++ azurerm/internal/services/recoveryservices/site_recovery_replicated_vm_resource_test.go
@@ -272,7 +272,11 @@ resource "azurerm_site_recovery_replicated_vm" "test" {
 func (SiteRecoveryReplicatedVmResource) des(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
 }
 
 data "azurerm_client_config" "current" {}
