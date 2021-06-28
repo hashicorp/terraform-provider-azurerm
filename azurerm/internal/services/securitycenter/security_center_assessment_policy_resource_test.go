d95fadb9fc6b03d22728212c387a30c2bd4b5524
neil-yechenwei <yechenwei2007@126.com>
2021-06-28 14:00:52 +0800

    Add support for the categories property of azurerm_security_center_assessment_policy

--- azurerm/internal/services/securitycenter/security_center_assessment_policy_resource_test.go
+++ azurerm/internal/services/securitycenter/security_center_assessment_policy_resource_test.go
@@ -67,6 +67,21 @@ func TestAccSecurityCenterAssessmentPolicy_update(t *testing.T) {
 	})
 }
 
+func TestAccSecurityCenterAssessmentPolicy_categories(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_security_center_assessment_policy", "test")
+	r := SecurityCenterAssessmentPolicyResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.categories(),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func (r SecurityCenterAssessmentPolicyResource) Exists(ctx context.Context, client *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	assessmentMetadataClient := client.SecurityCenter.AssessmentsMetadataClient
 	id, err := parse.AssessmentMetadataID(state.ID)
@@ -132,6 +147,21 @@ resource "azurerm_security_center_assessment_policy" "test" {
   remediation_description = "Updated Test Remediation Description"
   threats                 = ["DataExfiltration", "DataSpillage"]
   user_impact             = "Moderate"
+}
+`
+}
+
+func (r SecurityCenterAssessmentPolicyResource) categories() string {
+	return `
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_security_center_assessment_policy" "test" {
+  display_name = "Test Display Name"
+  severity     = "Medium"
+  description  = "Test Description"
+  categories   = ["Data"]
 }
 `
 }
