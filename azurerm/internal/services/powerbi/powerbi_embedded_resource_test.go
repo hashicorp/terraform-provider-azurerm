9de95571657ecd808c07f1333e0e71ac8917a364
Konstantin Kosinsky <kosinsky@github.com>
2021-06-28 09:16:50 -0700

    Linting

--- azurerm/internal/services/powerbi/powerbi_embedded_resource_test.go
+++ azurerm/internal/services/powerbi/powerbi_embedded_resource_test.go
@@ -187,7 +187,7 @@ resource "azurerm_powerbi_embedded" "test" {
   resource_group_name = "${azurerm_resource_group.test.name}"
   sku_name            = "A1"
   administrators      = ["${data.azurerm_client_config.test.object_id}"]
-  mode				  = "Gen2"
+  mode                = "Gen2"
 }
 `, template, data.RandomInteger)
 }
