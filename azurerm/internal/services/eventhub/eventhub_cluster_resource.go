8e828bfe98e3028f9f378112598bb64d15cc6677
Brian Flad <bflad417@gmail.com>
2021-07-01 19:14:04 -0400

    tests/provider: Fix tfproviderlint R006 ignore comments (#12446)
    The heuristics within `tfproviderlint` cannot determine that `github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk.RetryableError` is equivalent to `github.com/hashicorp/terraform-plugin-sdk/helper/resource.RetryableError` for the `R006` pass. Until an enhancement can potentially be made to allow passing an alias package path, this fixes the ignore comment to be at the end of the anonymous function definition since it is within a wrapping function call and allows the report to be properly skipped.

--- azurerm/internal/services/eventhub/eventhub_cluster_resource.go
+++ azurerm/internal/services/eventhub/eventhub_cluster_resource.go
@@ -146,7 +146,6 @@ func resourceEventHubClusterDelete(d *pluginsdk.ResourceData, meta interface{}) 
 	}
 
 	// The EventHub Cluster can't be deleted until four hours after creation so we'll keep retrying until it can be deleted.
-	//lintignore:R006
 	return pluginsdk.Retry(d.Timeout(pluginsdk.TimeoutDelete), func() *pluginsdk.RetryError {
 		future, err := client.ClustersDelete(ctx, *id)
 		if err != nil {
@@ -167,7 +166,7 @@ func resourceEventHubClusterDelete(d *pluginsdk.ResourceData, meta interface{}) 
 		}
 
 		return nil
-	})
+	}) //lintignore:R006
 }
 
 func expandEventHubClusterSkuName(skuName string) *eventhubsclusters.ClusterSku {
