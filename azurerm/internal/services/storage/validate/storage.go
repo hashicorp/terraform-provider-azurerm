996a494219bd768a911d68fa19ad26191a2b4ff1
cmalone <catriona@hashicorp.com>
2021-07-02 12:38:12 +0100

    updated storage share directory name regex to allow underscore

--- azurerm/internal/services/storage/validate/storage.go
+++ azurerm/internal/services/storage/validate/storage.go
@@ -8,7 +8,7 @@ import (
 func StorageShareDirectoryName(v interface{}, k string) (warnings []string, errors []error) {
 	value := v.(string)
 
-	if !regexp.MustCompile(`^[A-Za-z0-9-]+(/[A-Za-z0-9-]+)*$`).MatchString(value) {
+	if !regexp.MustCompile(`^[A-Za-z0-9\-_]+(/[A-Za-z0-9\-_]+)*$`).MatchString(value) {
 		errors = append(errors, fmt.Errorf("%s must contain only uppercase and lowercase alphanumeric characters, numbers and hyphens, and can be nested multiple levels", k))
 	}
 
