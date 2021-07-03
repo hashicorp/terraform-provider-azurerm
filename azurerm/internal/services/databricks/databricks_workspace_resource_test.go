763b184453d0adbf879ad602755711605af93d32
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2021-07-03 13:48:38 -0700

    Modify step one of cleanup

--- azurerm/internal/services/databricks/databricks_workspace_resource_test.go
+++ azurerm/internal/services/databricks/databricks_workspace_resource_test.go
@@ -89,10 +89,10 @@ func TestAccDatabricksWorkspace_complete(t *testing.T) {
 		},
 		data.ImportStep(),
 		{
-			Config: r.completeCleanupSubnetDelegation(data),
+			Config: r.cleanupSubnetDelegation(data),
 		},
 		{
-			Config: r.completeCleanupNsgAssociation(data),
+			Config: r.cleanupNsgAssociation(data),
 		},
 	})
 }
@@ -117,10 +117,10 @@ func TestAccDatabricksWorkspace_update(t *testing.T) {
 		},
 		data.ImportStep(),
 		{
-			Config: r.completeCleanupSubnetDelegation(data),
+			Config: r.cleanupSubnetDelegation(data),
 		},
 		{
-			Config: r.completeCleanupNsgAssociation(data),
+			Config: r.cleanupNsgAssociation(data),
 		},
 	})
 }
@@ -301,7 +301,7 @@ resource "azurerm_databricks_workspace" "test" {
 `, data.RandomInteger, data.Locations.Primary)
 }
 
-func (DatabricksWorkspaceResource) completeCleanupSubnetDelegation(data acceptance.TestData) string {
+func (DatabricksWorkspaceResource) cleanupSubnetDelegation(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {}
@@ -349,10 +349,30 @@ resource "azurerm_subnet_network_security_group_association" "private" {
   subnet_id                 = azurerm_subnet.private.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
+
+resource "azurerm_databricks_workspace" "test" {
+  name                        = "acctestDBW-%[1]d"
+  resource_group_name         = azurerm_resource_group.test.name
+  location                    = azurerm_resource_group.test.location
+  sku                         = "standard"
+  managed_resource_group_name = "acctestRG-DBW-%[1]d-managed"
+
+  custom_parameters {
+    no_public_ip        = true
+    public_subnet_name  = azurerm_subnet.public.name
+    private_subnet_name = azurerm_subnet.private.name
+    virtual_network_id  = azurerm_virtual_network.test.id
+  }
+
+  tags = {
+    Environment = "Production"
+    Pricing     = "Standard"
+  }
+}
 `, data.RandomInteger, data.Locations.Primary)
 }
 
-func (DatabricksWorkspaceResource) completeCleanupNsgAssociation(data acceptance.TestData) string {
+func (DatabricksWorkspaceResource) cleanupNsgAssociation(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {}
