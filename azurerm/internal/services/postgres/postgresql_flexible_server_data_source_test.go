010279faaedc9520860fc48ab201a3eb92459933
Nick Metz <nick.metz@ptvgroup.com>
2021-07-08 17:30:27 +0200

    `azurerm_postgresql_flexible_server` update postgres flexserver api version to 2021-06-01 (#12405)
    Changes:
    
    Update flexserver api version to 2021-06-01
    Complete flexserver tests use postgres version 13 now
    Noteworthy Changes:
    
    cmk_enabled (byokEnforcement) is not exposed in 2021-06-01 anymore
    Postgres version 13 also available now
    effected resources
    
    azurerm_postgresql_flexible_server
    azurerm_postgresql_flexible_server_firewall_rule
    Datasource: azurerm_postgresql_flexible_server
    This PR address:
    
    Support PostgreSQL Flexible Server Version 13 #12375
    New Resource azurerm_postgresql_flexible_server_configuration #12294
    Support for postgresql flexible server database, configuration and firewall_rule #11538

--- azurerm/internal/services/postgres/postgresql_flexible_server_data_source_test.go
+++ azurerm/internal/services/postgres/postgresql_flexible_server_data_source_test.go
@@ -24,9 +24,9 @@ func TestAccDataSourcePostgresqlflexibleServer_basic(t *testing.T) {
 				check.That(data.ResourceName).Key("storage_mb").Exists(),
 				check.That(data.ResourceName).Key("version").Exists(),
 				check.That(data.ResourceName).Key("backup_retention_days").Exists(),
-				check.That(data.ResourceName).Key("cmk_enabled").Exists(),
 				check.That(data.ResourceName).Key("fqdn").Exists(),
 				check.That(data.ResourceName).Key("public_network_access_enabled").Exists(),
+				check.That(data.ResourceName).Key("cmk_enabled").IsEmpty(),
 			),
 		},
 	})
