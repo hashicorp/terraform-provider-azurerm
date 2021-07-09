068bc6cd281f7aa6914c017a050012889d8bb280
Michael Gross <leachimgross@gmail.com>
2021-07-09 16:42:31 +0200

    Reuse ResourceGroup schema for data source

--- azurerm/internal/services/consumption/consumption_budget_resource_group_data_source.go
+++ azurerm/internal/services/consumption/consumption_budget_resource_group_data_source.go
@@ -4,13 +4,9 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/consumption/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/consumption/parse"
 	resourceParse "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/resource/parse"
-	resourceValidate "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/resource/validate"
-	// subscriptionParse "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/subscription/parse"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/validation"
 )
 
 func resourceArmConsumptionBudgetResourceGroupDataSource() *pluginsdk.Resource {
@@ -19,96 +15,7 @@ func resourceArmConsumptionBudgetResourceGroupDataSource() *pluginsdk.Resource {
 		Timeouts: &pluginsdk.ResourceTimeout{
 			Read: pluginsdk.DefaultTimeout(5 * time.Minute),
 		},
-		Schema: map[string]*pluginsdk.Schema{
-			"name": {
-				Type:         pluginsdk.TypeString,
-				Required:     true,
-				ValidateFunc: validate.ConsumptionBudgetName(),
-			},
-			"resource_group_id": {
-				Type:         pluginsdk.TypeString,
-				Optional:     true,
-				ValidateFunc: resourceValidate.ResourceGroupID,
-			},
-			"subscription_id": {
-				Type:         pluginsdk.TypeString,
-				Required:     true,
-				ValidateFunc: validation.IsUUID,
-			},
-			"amount": {
-				Type:     pluginsdk.TypeFloat,
-				Computed: true,
-			},
-
-			"filter": {
-				Type:     pluginsdk.TypeList,
-				Computed: true,
-				Elem: &pluginsdk.Resource{
-					Schema: map[string]*pluginsdk.Schema{
-						"dimension": {
-							Type:     pluginsdk.TypeSet,
-							Computed: true,
-							Set:      pluginsdk.HashResource(SchemaConsumptionBudgetFilterDimensionElement()),
-							Elem:     SchemaConsumptionBudgetFilterDimensionElement(),
-						},
-						"tag": {
-							Type:     pluginsdk.TypeSet,
-							Computed: true,
-							Set:      pluginsdk.HashResource(SchemaConsumptionBudgetFilterTagElement()),
-							Elem:     SchemaConsumptionBudgetFilterTagElement(),
-						},
-						"not": {
-							Type:     pluginsdk.TypeList,
-							Computed: true,
-							Elem: &pluginsdk.Resource{
-								Schema: map[string]*pluginsdk.Schema{
-									"dimension": {
-										Type:     pluginsdk.TypeList,
-										Computed: true,
-										Elem:     SchemaConsumptionBudgetFilterDimensionElement(),
-									},
-									"tag": {
-										Type:     pluginsdk.TypeList,
-										Computed: true,
-										Elem:     SchemaConsumptionBudgetFilterTagElement(),
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-
-			"notification": {
-				Type:     pluginsdk.TypeSet,
-				Computed: true,
-				Set:      pluginsdk.HashResource(SchemaConsumptionBudgetNotificationElement()),
-				Elem:     SchemaConsumptionBudgetNotificationElement(),
-			},
-
-			"time_grain": {
-				Type:     pluginsdk.TypeString,
-				Computed: true,
-			},
-
-			"time_period": {
-				Type:     pluginsdk.TypeList,
-				Computed: true,
-				Elem: &pluginsdk.Resource{
-					Schema: map[string]*pluginsdk.Schema{
-						"start_date": {
-							Type:     pluginsdk.TypeString,
-							Computed: true,
-						},
-						"end_date": {
-							Type:     pluginsdk.TypeString,
-							Optional: true,
-							Computed: true,
-						},
-					},
-				},
-			},
-		},
+		Schema: SchemaConsumptionBudgetResourceGroupResource(),
 	}
 }
 
