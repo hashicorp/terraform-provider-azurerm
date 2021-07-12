59ef8e17de9979fb95607d23c06fceb7a3f06e39
Tom Bamford <tom@bamford.io>
2021-07-12 16:41:21 +0100

    Tidy up powerbi tests

--- azurerm/internal/services/powerbi/powerbi_embedded_resource_test.go
+++ azurerm/internal/services/powerbi/powerbi_embedded_resource_test.go
@@ -31,24 +31,6 @@ func TestAccPowerBIEmbedded_basic(t *testing.T) {
 	})
 }
 
-func TestAccPowerBIEmbedded_requiresImport(t *testing.T) {
-	data := acceptance.BuildTestData(t, "azurerm_powerbi_embedded", "test")
-	r := PowerBIEmbeddedResource{}
-
-	data.ResourceTest(t, r, []acceptance.TestStep{
-		{
-			Config: r.basic(data),
-			Check: acceptance.ComposeTestCheckFunc(
-				check.That(data.ResourceName).ExistsInAzure(r),
-			),
-		},
-		{
-			Config:      r.requiresImport(data),
-			ExpectError: acceptance.RequiresImportError("azurerm_powerbi_embedded"),
-		},
-	})
-}
-
 func TestAccPowerBIEmbedded_complete(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_powerbi_embedded", "test")
 	r := PowerBIEmbeddedResource{}
@@ -58,8 +40,6 @@ func TestAccPowerBIEmbedded_complete(t *testing.T) {
 			Config: r.complete(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
-				check.That(data.ResourceName).Key("sku_name").HasValue("A2"),
-				check.That(data.ResourceName).Key("tags.ENV").HasValue("Test"),
 			),
 		},
 		data.ImportStep(),
@@ -75,7 +55,6 @@ func TestAccPowerBIEmbedded_update(t *testing.T) {
 			Config: r.basic(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
-				check.That(data.ResourceName).Key("sku_name").HasValue("A1"),
 			),
 		},
 		data.ImportStep(),
@@ -83,7 +62,6 @@ func TestAccPowerBIEmbedded_update(t *testing.T) {
 			Config: r.complete(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
-				check.That(data.ResourceName).Key("sku_name").HasValue("A2"),
 			),
 		},
 		data.ImportStep(),
@@ -91,7 +69,6 @@ func TestAccPowerBIEmbedded_update(t *testing.T) {
 			Config: r.basic(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
-				check.That(data.ResourceName).Key("sku_name").HasValue("A1"),
 			),
 		},
 		data.ImportStep(),
@@ -114,6 +91,24 @@ func TestAccPowerBIEmbedded_gen2(t *testing.T) {
 	})
 }
 
+func TestAccPowerBIEmbedded_requiresImport(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_powerbi_embedded", "test")
+	r := PowerBIEmbeddedResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		{
+			Config:      r.requiresImport(data),
+			ExpectError: acceptance.RequiresImportError("azurerm_powerbi_embedded"),
+		},
+	})
+}
+
 func (PowerBIEmbeddedResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	id, err := parse.EmbeddedID(state.ID)
 	if err != nil {
@@ -128,70 +123,6 @@ func (PowerBIEmbeddedResource) Exists(ctx context.Context, clients *clients.Clie
 	return utils.Bool(resp.DedicatedCapacityProperties != nil), nil
 }
 
-func (PowerBIEmbeddedResource) basic(data acceptance.TestData) string {
-	template := PowerBIEmbeddedResource{}.template(data)
-	return fmt.Sprintf(`
-%s
-
-resource "azurerm_powerbi_embedded" "test" {
-  name                = "acctestpowerbi%d"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  sku_name            = "A1"
-  administrators      = ["${data.azurerm_client_config.test.object_id}"]
-}
-`, template, data.RandomInteger)
-}
-
-func (r PowerBIEmbeddedResource) requiresImport(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-%s
-
-resource "azurerm_powerbi_embedded" "import" {
-  name                = "${azurerm_powerbi_embedded.test.name}"
-  location            = "${azurerm_powerbi_embedded.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  sku_name            = "A1"
-  administrators      = ["${data.azurerm_client_config.test.object_id}"]
-}
-`, r.basic(data))
-}
-
-func (PowerBIEmbeddedResource) complete(data acceptance.TestData) string {
-	template := PowerBIEmbeddedResource{}.template(data)
-	return fmt.Sprintf(`
-%s
-
-resource "azurerm_powerbi_embedded" "test" {
-  name                = "acctestpowerbi%d"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  sku_name            = "A2"
-  administrators      = ["${data.azurerm_client_config.test.object_id}"]
-
-  tags = {
-    ENV = "Test"
-  }
-}
-`, template, data.RandomInteger)
-}
-
-func (PowerBIEmbeddedResource) gen2(data acceptance.TestData) string {
-	template := PowerBIEmbeddedResource{}.template(data)
-	return fmt.Sprintf(`
-%s
-
-resource "azurerm_powerbi_embedded" "test" {
-  name                = "acctestpowerbi%d"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  sku_name            = "A1"
-  administrators      = ["${data.azurerm_client_config.test.object_id}"]
-  mode                = "Gen2"
-}
-`, template, data.RandomInteger)
-}
-
 func (PowerBIEmbeddedResource) template(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
@@ -199,10 +130,71 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "test" {
-  name     = "acctestRG-powerbi-%d"
-  location = "%s"
+  name     = "acctestRG-powerbi-%[1]d"
+  location = "%[2]s"
 }
 
 data "azurerm_client_config" "test" {}
 `, data.RandomInteger, data.Locations.Primary)
+}
+
+func (r PowerBIEmbeddedResource) basic(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_powerbi_embedded" "test" {
+  name                = "acctestpowerbi%[2]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku_name            = "A1"
+  administrators      = [data.azurerm_client_config.test.object_id]
+}
+`, r.template(data), data.RandomInteger)
+}
+
+func (r PowerBIEmbeddedResource) complete(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_powerbi_embedded" "test" {
+  name                = "acctestpowerbi%[2]d"
+  location            = azurerm_resource_group.test.location"
+  resource_group_name = azurerm_resource_group.test.name"
+  sku_name            = "A2"
+  administrators      = [data.azurerm_client_config.test.object_id]
+
+  tags = {
+    ENV = "Test"
+  }
+}
+`, r.template(data), data.RandomInteger)
+}
+
+func (r PowerBIEmbeddedResource) gen2(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_powerbi_embedded" "test" {
+  name                = "acctestpowerbi%[2]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku_name            = "A1"
+  administrators      = [data.azurerm_client_config.test.object_id]
+  mode                = "Gen2"
+}
+`, r.template(data), data.RandomInteger)
+}
+
+func (r PowerBIEmbeddedResource) requiresImport(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_powerbi_embedded" "import" {
+  name                = azurerm_powerbi_embedded.test.name
+  location            = azurerm_powerbi_embedded.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku_name            = "A1"
+  administrators      = [data.azurerm_client_config.test.object_id]
+}
+`, r.basic(data))
 }
