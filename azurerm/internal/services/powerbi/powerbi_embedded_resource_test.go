542c3c4f25cee5fe57e9ec71ffd5039addf78c93
Konstantin Kosinsky <kosinsky@github.com>
2021-07-13 10:48:02 -0700

    Fix tflint

--- azurerm/internal/services/powerbi/powerbi_embedded_resource_test.go
+++ azurerm/internal/services/powerbi/powerbi_embedded_resource_test.go
@@ -158,8 +158,8 @@ func (r PowerBIEmbeddedResource) complete(data acceptance.TestData) string {
 
 resource "azurerm_powerbi_embedded" "test" {
   name                = "acctestpowerbi%[2]d"
-  location            = azurerm_resource_group.test.location"
-  resource_group_name = azurerm_resource_group.test.name"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
   sku_name            = "A2"
   administrators      = [data.azurerm_client_config.test.object_id]
 
