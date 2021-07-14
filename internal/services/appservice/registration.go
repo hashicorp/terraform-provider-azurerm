b0a33dae135b05b64cf5719a64d84a1592f75dff
jackofallops <ste@hashicorp.com>
2021-07-14 12:21:02 +0100

    registration, services and 3.0 feature opt in so it will actually work...

---
+++ internal/services/appservice/registration.go
@@ -0,0 +1,47 @@
+package appservice
+
+import (
+	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/appservice/serviceplan"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/appservice/sourcecontrol"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/appservice/webapp"
+)
+
+var _ sdk.TypedServiceRegistration = Registration{}
+
+type Registration struct{}
+
+func (r Registration) PackagePath() string {
+	return "TODO: Not implemented yet"
+}
+
+func (r Registration) WebsiteCategories() []string {
+	return nil
+}
+
+func (r Registration) Name() string {
+	return "AppService"
+}
+
+func (r Registration) DataSources() []sdk.DataSource {
+	if features.ThreePointOh() {
+		return []sdk.DataSource{
+			sourcecontrol.AppServiceGitHubTokenDataSource{},
+		}
+	}
+	return []sdk.DataSource{}
+}
+
+func (r Registration) Resources() []sdk.Resource {
+	if features.ThreePointOh() {
+		return []sdk.Resource{
+			sourcecontrol.AppServiceSourceControlResource{},
+			sourcecontrol.AppServiceGitHubTokenResource{},
+			webapp.WindowsWebAppResource{},
+			webapp.LinuxWebAppResource{},
+			serviceplan.AppServicePlanResource{},
+		}
+	}
+	return []sdk.Resource{}
+}
