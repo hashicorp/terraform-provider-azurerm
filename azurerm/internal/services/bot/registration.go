81451f37bf2630332b028499fcc8706365e1cf6b
neil-yechenwei <yechenwei2007@126.com>
2021-07-16 15:52:23 +0800

    New Resource: azurerm_bot_channel_alexa

--- azurerm/internal/services/bot/registration.go
+++ azurerm/internal/services/bot/registration.go
@@ -26,6 +26,7 @@ func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
 // SupportedResources returns the supported Resources supported by this Service
 func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
 	return map[string]*pluginsdk.Resource{
+		"azurerm_bot_channel_alexa":         resourceBotChannelAlexa(),
 		"azurerm_bot_channel_directline":    resourceBotChannelDirectline(),
 		"azurerm_bot_channel_email":         resourceBotChannelEmail(),
 		"azurerm_bot_channel_ms_teams":      resourceBotChannelMsTeams(),
