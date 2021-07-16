4ffcc545032569ed53ee525968a30cb2f9ead39b
yupwei68 <yupwei@microsoft.com>
2021-07-16 09:54:49 +0800

    update

--- azurerm/internal/services/apimanagement/api_management_api_release.go
+++ azurerm/internal/services/apimanagement/api_management_api_release.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/services/apimanagement/mgmt/2020-12-01/apimanagement"
+	"github.com/Azure/azure-sdk-for-go/services/preview/apimanagement/mgmt/2021-01-01-preview/apimanagement"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/tf"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/apimanagement/parse"
