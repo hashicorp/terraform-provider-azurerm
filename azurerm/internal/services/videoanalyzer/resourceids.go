00ccec14e523fb295c0030e000620763c40bfc49
Eliise <elseling@microsoft.com>
2021-07-16 16:02:10 +0000

    Add inital draft of video_analyzer

---
+++ azurerm/internal/services/videoanalyzer/resourceids.go
@@ -0,0 +1,3 @@
+package media
+
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=VideoAnalyzer -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Media/videoanalyzers/analyzer1
