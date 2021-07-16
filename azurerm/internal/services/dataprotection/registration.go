5b64b319632d92295442f213d6cefc56f69810af
henglu <henglu@microsoft.com>
2021-07-16 10:18:48 +0800

    add resource "azurerm_data_protection_backup_instance_disk"

--- azurerm/internal/services/dataprotection/registration.go
+++ azurerm/internal/services/dataprotection/registration.go
@@ -27,6 +27,7 @@ func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
 		"azurerm_data_protection_backup_vault":               resourceDataProtectionBackupVault(),
 		"azurerm_data_protection_backup_policy_disk":         resourceDataProtectionBackupPolicyDisk(),
 		"azurerm_data_protection_backup_policy_postgresql":   resourceDataProtectionBackupPolicyPostgreSQL(),
+		"azurerm_data_protection_backup_instance_disk":       resourceDataProtectionBackupInstanceDisk(),
 		"azurerm_data_protection_backup_instance_postgresql": resourceDataProtectionBackupInstancePostgreSQL(),
 	}
 }
