44a99068a11fc321317b301e0d5ed9211af20b31
njucz <zhcheng@microsoft.com>
2021-07-16 14:45:49 +0800

    remove update timeout

--- azurerm/internal/services/datafactory/data_factory_managed_private_endpoint_resource.go
+++ azurerm/internal/services/datafactory/data_factory_managed_private_endpoint_resource.go
@@ -31,7 +31,6 @@ func resourceDataFactoryManagedPrivateEndpoint() *pluginsdk.Resource {
 		Timeouts: &pluginsdk.ResourceTimeout{
 			Create: pluginsdk.DefaultTimeout(30 * time.Minute),
 			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
-			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
 			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
 		},
 
