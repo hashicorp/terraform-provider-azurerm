4177162496a71cb2cc32326468ca1ddbf398b214
Aris van Ommeren <avommeren@vxcompany.com>
2021-07-19 22:04:34 +0200

    remove default `sku_tier` value from testcase

--- azurerm/internal/services/containers/kubernetes_cluster_other_resource_test.go
+++ azurerm/internal/services/containers/kubernetes_cluster_other_resource_test.go
@@ -1209,7 +1209,6 @@ resource "azurerm_kubernetes_cluster" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   dns_prefix          = "acctestaks%d"
-  sku_tier            = "Free"
 
   default_node_pool {
     name       = "default"
