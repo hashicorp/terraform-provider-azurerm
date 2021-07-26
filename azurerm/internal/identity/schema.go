dbf7f16f000dca16610eb6e264cc435f988b7677
tombuildsstuff <git@ibuildstuff.co.uk>
2021-07-26 12:14:54 +0200

    identity: adding a base type for the constant that we can reference elsewhere

--- azurerm/internal/identity/schema.go
+++ azurerm/internal/identity/schema.go
@@ -4,17 +4,19 @@ import (
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk"
 )
 
+type identityType string
+
 const (
-	none                       = "None"
-	systemAssigned             = "SystemAssigned"
-	userAssigned               = "UserAssigned"
-	systemAssignedUserAssigned = "SystemAssigned, UserAssigned"
+	none                       identityType = "None"
+	systemAssigned             identityType = "SystemAssigned"
+	userAssigned               identityType = "UserAssigned"
+	systemAssignedUserAssigned identityType = "SystemAssigned, UserAssigned"
 )
 
 type ExpandedConfig struct {
 	// Type is the type of User Assigned Identity, either `None`, `SystemAssigned`, `UserAssigned`
 	// or `SystemAssigned, UserAssigned`
-	Type                    string
+	Type                    identityType
 	PrincipalId             *string
 	TenantId                *string
 	UserAssignedIdentityIds *[]string
