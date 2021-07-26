cfb81e716a4f40e749e361df13cc1c28dae4ed56
jackofallops <ste@hashicorp.com>
2021-07-26 06:17:01 +0100

    add app_settings test

--- internal/services/appservice/webapp/linux_web_app_resource_test.go
+++ internal/services/appservice/webapp/linux_web_app_resource_test.go
@@ -553,6 +553,22 @@ func TestAccLinuxWebApp_withAutoHealRules(t *testing.T) {
 	})
 }
 
+func TestAccLinuxWebApp_appSettings(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_linux_web_app", "test")
+	r := LinuxWebAppResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.appSettings(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("app_settings.foo").HasValue("bar"),
+				check.That(data.ResourceName).Key("app_settings.secret").HasValue("sauce"),
+			),
+		},
+	})
+}
+
 // Exists func
 
 func (r LinuxWebAppResource) Exists(ctx context.Context, client *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
@@ -589,6 +605,28 @@ resource "azurerm_linux_web_app" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   service_plan_id     = azurerm_service_plan.test.id
+}
+`, r.baseTemplate(data), data.RandomInteger)
+}
+
+func (r LinuxWebAppResource) appSettings(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+%s
+
+resource "azurerm_linux_web_app" "test" {
+  name                = "acctestWA-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  service_plan_id     = azurerm_service_plan.test.id
+
+  app_settings = {
+    foo    = "bar"
+    secret = "sauce"
+  }
 }
 `, r.baseTemplate(data), data.RandomInteger)
 }
