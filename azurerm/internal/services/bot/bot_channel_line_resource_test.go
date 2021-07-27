096f10e4f3fbd4333736b837394d3ffca03e7e4e
neil-yechenwei <yechenwei2007@126.com>
2021-07-27 09:42:56 +0800

    update code

--- azurerm/internal/services/bot/bot_channel_line_resource_test.go
+++ azurerm/internal/services/bot/bot_channel_line_resource_test.go
@@ -3,6 +3,7 @@ package bot_test
 import (
 	"context"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/services/botservice/mgmt/2021-03-01/botservice"
@@ -18,12 +19,72 @@ type BotChannelLineResource struct {
 }
 
 func testAccBotChannelLine_basic(t *testing.T) {
+	skipLineChannel(t)
+
 	data := acceptance.BuildTestData(t, "azurerm_bot_channel_line", "test")
 	r := BotChannelLineResource{}
 
 	data.ResourceSequentialTest(t, r, []acceptance.TestStep{
 		{
 			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func testAccBotChannelLine_requiresImport(t *testing.T) {
+	skipLineChannel(t)
+
+	data := acceptance.BuildTestData(t, "azurerm_bot_channel_line", "test")
+	r := BotChannelLineResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.RequiresImportErrorStep(r.requiresImport),
+	})
+}
+
+func testAccBotChannelLine_complete(t *testing.T) {
+	skipLineChannel(t)
+
+	data := acceptance.BuildTestData(t, "azurerm_bot_channel_line", "test")
+	r := BotChannelLineResource{}
+
+	data.ResourceSequentialTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.complete(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func testAccBotChannelLine_update(t *testing.T) {
+	skipLineChannel(t)
+
+	data := acceptance.BuildTestData(t, "azurerm_bot_channel_line", "test")
+	r := BotChannelLineResource{}
+
+	data.ResourceSequentialTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.update(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 			),
@@ -56,9 +117,71 @@ resource "azurerm_bot_channel_line" "test" {
   resource_group_name = azurerm_resource_group.test.name
 
   line_registration {
-    channel_access_token = ""
-    channel_secret       = ""
+    channel_access_token = "%s"
+    channel_secret       = "%s"
   }
 }
-`, BotChannelsRegistrationResource{}.basicConfig(data))
+`, BotChannelsRegistrationResource{}.basicConfig(data), os.Getenv("ARM_TEST_CHANNEL_ACCESS_TOKEN"), os.Getenv("ARM_TEST_CHANNEL_SECRET"))
+}
+
+func (r BotChannelLineResource) requiresImport(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_bot_channel_line" "import" {
+  bot_name            = azurerm_bot_channel_line.test.bot_name
+  location            = azurerm_bot_channel_line.test.location
+  resource_group_name = azurerm_bot_channel_line.test.resource_group_name
+
+  line_registration {
+    channel_access_token = "%s"
+    channel_secret       = "%s"
+  }
+}
+`, r.basic(data), os.Getenv("ARM_TEST_CHANNEL_ACCESS_TOKEN"), os.Getenv("ARM_TEST_CHANNEL_SECRET"))
+}
+
+func (BotChannelLineResource) complete(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_bot_channel_line" "test" {
+  bot_name            = azurerm_bot_channels_registration.test.name
+  location            = azurerm_bot_channels_registration.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  line_registration {
+    channel_access_token = "%s"
+    channel_secret       = "%s"
+  }
+
+  line_registration {
+    channel_access_token = "%s"
+    channel_secret       = "%s"
+  }
+}
+`, BotChannelsRegistrationResource{}.basicConfig(data), os.Getenv("ARM_TEST_CHANNEL_ACCESS_TOKEN"), os.Getenv("ARM_TEST_CHANNEL_SECRET"), os.Getenv("ARM_TEST_CHANNEL_ACCESS_TOKEN2"), os.Getenv("ARM_TEST_CHANNEL_SECRET2"))
+}
+
+func (BotChannelLineResource) update(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_bot_channel_line" "test" {
+  bot_name            = azurerm_bot_channels_registration.test.name
+  location            = azurerm_bot_channels_registration.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  line_registration {
+    channel_access_token = "%s"
+    channel_secret       = "%s"
+  }
+}
+`, BotChannelsRegistrationResource{}.basicConfig(data), os.Getenv("ARM_TEST_CHANNEL_ACCESS_TOKEN2"), os.Getenv("ARM_TEST_CHANNEL_SECRET2"))
+}
+
+func skipLineChannel(t *testing.T) {
+	if os.Getenv("ARM_TEST_CHANNEL_ACCESS_TOKEN") == "" || os.Getenv("ARM_TEST_CHANNEL_SECRET") == "" || os.Getenv("ARM_TEST_CHANNEL_ACCESS_TOKEN2") == "" || os.Getenv("ARM_TEST_CHANNEL_SECRET2") == "" {
+		t.Skip("Skipping as one of `ARM_TEST_CHANNEL_ACCESS_TOKEN`, `ARM_TEST_CHANNEL_SECRET`, `ARM_TEST_CHANNEL_ACCESS_TOKEN2`, `ARM_TEST_CHANNEL_SECRET2` was not specified")
+	}
 }
