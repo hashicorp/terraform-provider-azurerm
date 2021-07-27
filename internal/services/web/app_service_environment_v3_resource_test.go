32f54106c7ad8cc119076cac8a492e639f2d369a
jackofallops <ste@hashicorp.com>
2021-07-27 13:51:03 +0100

    typo in test config

--- internal/services/web/app_service_environment_v3_resource_test.go
+++ internal/services/web/app_service_environment_v3_resource_test.go
@@ -227,7 +227,7 @@ resource "azurerm_virtual_network" "test2" {
 }
 
 resource "azurerm_subnet" "test2" {
-  name                 = "acctest-subnet2-%[1]d"
+  name                 = "acctest-subnet2-%[2]d"
   resource_group_name  = azurerm_resource_group.test.name
   virtual_network_name = azurerm_virtual_network.test2.name
   address_prefix       = "20.0.2.0/24"
