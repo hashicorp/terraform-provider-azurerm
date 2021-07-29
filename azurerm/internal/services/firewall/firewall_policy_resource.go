9fb86efa74c12c2d5f0683944416f06492665850
catriona-m <86247157+catriona-m@users.noreply.github.com>
2021-07-29 13:13:02 +0100

    azurerm_firewall_polcy - Added new property private_ip_ranges (#12696)
    * Add property private_ip_ranges to firewall_policy
    
    * change setting of privateIpRanges to set even if empty
    
    * correct formatting in azurerm_firewall_policy

--- azurerm/internal/services/firewall/firewall_policy_resource.go
+++ azurerm/internal/services/firewall/firewall_policy_resource.go
@@ -166,6 +166,19 @@ func resourceFirewallPolicy() *pluginsdk.Resource {
 				},
 			},
 
+			"private_ip_ranges": {
+				Type:     pluginsdk.TypeList,
+				Optional: true,
+				MinItems: 1,
+				Elem: &pluginsdk.Schema{
+					Type: pluginsdk.TypeString,
+					ValidateFunc: validation.Any(
+						validation.IsCIDR,
+						validation.IsIPv4Address,
+					),
+				},
+			},
+
 			"tags": tags.SchemaEnforceLowerCaseKeys(),
 		},
 	}
@@ -208,6 +221,13 @@ func resourceFirewallPolicyCreateUpdate(d *pluginsdk.ResourceData, meta interfac
 	if v, ok := d.GetOk("sku"); ok {
 		props.FirewallPolicyPropertiesFormat.Sku = &network.FirewallPolicySku{
 			Tier: network.FirewallPolicySkuTier(v.(string)),
+		}
+	}
+
+	if v, ok := d.GetOk("private_ip_ranges"); ok {
+		privateIpRanges := utils.ExpandStringSlice(v.([]interface{}))
+		props.FirewallPolicyPropertiesFormat.Snat = &network.FirewallPolicySNAT{
+			PrivateRanges: privateIpRanges,
 		}
 	}
 
@@ -286,6 +306,14 @@ func resourceFirewallPolicyRead(d *pluginsdk.ResourceData, meta interface{}) err
 
 		if err := d.Set("rule_collection_groups", flattenNetworkSubResourceID(prop.RuleCollectionGroups)); err != nil {
 			return fmt.Errorf(`setting "rule_collection_groups": %+v`, err)
+		}
+
+		var privateIpRanges []interface{}
+		if prop.Snat != nil {
+			privateIpRanges = utils.FlattenStringSlice(prop.Snat.PrivateRanges)
+		}
+		if err := d.Set("private_ip_ranges", privateIpRanges); err != nil {
+			return fmt.Errorf("Error setting `private_ip_ranges`: %+v", err)
 		}
 	}
 
