39362cc62c8c9bbc92c61cfa63a688127b292e66
ms-henglu <79895375+ms-henglu@users.noreply.github.com>
2021-07-30 09:43:41 +0800

    update containerservice to 2021-05-1 (#12747)

--- vendor/github.com/Azure/azure-sdk-for-go/services/containerservice/mgmt/2021-05-01/containerservice/privatelinkresources.go
+++ vendor/github.com/Azure/azure-sdk-for-go/services/containerservice/mgmt/2021-05-01/containerservice/privatelinkresources.go
@@ -88,7 +88,7 @@ func (client PrivateLinkResourcesClient) ListPreparer(ctx context.Context, resou
 		"subscriptionId":    autorest.Encode("path", client.SubscriptionID),
 	}
 
-	const APIVersion = "2021-03-01"
+	const APIVersion = "2021-05-01"
 	queryParameters := map[string]interface{}{
 		"api-version": APIVersion,
 	}
