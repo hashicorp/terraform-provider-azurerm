ef199f9fa992891d16894b910f11e506db6dc305
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2021-08-02 10:47:15 -0700

    Update test template

--- azurerm/internal/services/databricks/databricks_customer_managed_key_resource_test.go
+++ azurerm/internal/services/databricks/databricks_customer_managed_key_resource_test.go
@@ -220,7 +220,6 @@ resource "azurerm_key_vault" "test" {
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "premium"
 
-  purge_protection_enabled   = true
   soft_delete_retention_days = 7
 }
 
