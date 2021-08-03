71f0926dfdc852782141c53f223376d19a5462bd
patst <patrick.steinig@googlemail.com>
2021-08-03 14:22:33 +0200

    make fmt

--- azurerm/internal/services/apimanagement/api_management_subscription_resource.go
+++ azurerm/internal/services/apimanagement/api_management_subscription_resource.go
@@ -230,7 +230,7 @@ func resourceApiManagementSubscriptionRead(d *pluginsdk.ResourceData, meta inter
 		apiId := ""
 		// check if the subscription is for all apis or a specific product/ api
 		if *props.Scope != "" && !strings.HasSuffix(*props.Scope, "/apis") {
-		// the scope is either a product or api id
+			// the scope is either a product or api id
 			parseId, err := parse.ProductID(*props.Scope)
 			if err == nil {
 				productId = parseId.ID()
