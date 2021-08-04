e057e6c8d72b714d7eff7b36c30b8bccf4722549
cmalone <catriona@hashicorp.com>
2021-08-04 16:10:51 +0100

    sort vpn_gateway imports

--- azurerm/internal/services/network/vpn_gateway_data_source.go
+++ azurerm/internal/services/network/vpn_gateway_data_source.go
@@ -2,13 +2,13 @@ package network
 
 import (
 	"fmt"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/location"
 	"log"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2020-11-01/network"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/location"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
