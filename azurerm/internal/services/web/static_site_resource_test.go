c32c40462499761c7e9da59d5b2fa2b9dee28e89
Casper Naudts <22298989+CasperNaudts@users.noreply.github.com>
2021-08-05 06:20:48 +0000

    `azurerm_static_site` - support `Standard` SKU (#12510)

--- azurerm/internal/services/web/static_site_resource_test.go
+++ azurerm/internal/services/web/static_site_resource_test.go
@@ -131,6 +131,8 @@ resource "azurerm_static_site" "test" {
   name                = "acctestSS-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
+  sku_size            = "Standard"
+  sku_tier            = "Standard"
 
   tags = {
     environment = "acceptance"
