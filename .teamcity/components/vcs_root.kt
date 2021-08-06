bdf118a1e966ff727aeec17e1ee6a2155c192f5a
Tom Harvey <tombuildsstuff@users.noreply.github.com>
2021-08-06 10:01:46 +0200

    reverting to pull from `terraform-providers` org for now

--- .teamcity/components/vcs_root.kt
+++ .teamcity/components/vcs_root.kt
@@ -2,7 +2,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 object providerRepository : GitVcsRoot({
     name = "terraform-provider-azurerm"
-    url = "https://github.com/hashicorp/terraform-provider-azurerm.git"
+    url = "https://github.com/terraform-providers/terraform-provider-azurerm.git"
     agentCleanPolicy = AgentCleanPolicy.ALWAYS
     agentCleanFilesPolicy = AgentCleanFilesPolicy.ALL_UNTRACKED
     branchSpec = "+:*"
