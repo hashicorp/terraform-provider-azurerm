eb8749fdabd82a76814eb98b39e4ba32ea43516b
Eliise <elseling@microsoft.com>
2021-08-09 17:22:27 +0000

    Remove files in the wrong location

--- azurerm/internal/services/videoanalyzer/resourceids.go
+++
@@ -1,3 +0,0 @@
-package videoanalyzer
-
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=VideoAnalyzer -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Media/videoanalyzers/analyzer1
