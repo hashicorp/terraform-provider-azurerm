3e5d3a46ad29fd8f87bf800823e5acc66e7e2dac
Catriona <catriona@hashicorp.com>
2021-08-10 14:09:54 +0100

    move vpn_gateway files after rebase

---
+++ internal/services/network/vpn_gateway_data_source.go
@@ -0,0 +1,252 @@
+package network
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2020-11-01/network"
+	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/location"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/network/parse"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tags"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+)
+
+func dataSourceVPNGateway() *pluginsdk.Resource {
+	return &pluginsdk.Resource{
+		Read: dataSourceVPNGatewayRead,
+		// TODO: replace this with an importer which validates the ID during import
+		Importer: pluginsdk.DefaultImporter(),
+
+		Timeouts: &pluginsdk.ResourceTimeout{
+			Read: pluginsdk.DefaultTimeout(5 * time.Minute),
+		},
+
+		Schema: map[string]*pluginsdk.Schema{
+			"name": {
+				Type:     pluginsdk.TypeString,
+				Required: true,
+			},
+
+			"resource_group_name": azure.SchemaResourceGroupNameForDataSource(),
+
+			"location": location.SchemaComputed(),
+
+			"virtual_hub_id": {
+				Type:     pluginsdk.TypeString,
+				Computed: true,
+			},
+
+			"bgp_settings": {
+				Type:     pluginsdk.TypeList,
+				Computed: true,
+				Elem: &pluginsdk.Resource{
+					Schema: map[string]*pluginsdk.Schema{
+						"asn": {
+							Type:     pluginsdk.TypeInt,
+							Computed: true,
+						},
+
+						"peer_weight": {
+							Type:     pluginsdk.TypeInt,
+							Computed: true,
+						},
+
+						"bgp_peering_address": {
+							Type:     pluginsdk.TypeString,
+							Computed: true,
+						},
+
+						"instance_0_bgp_peering_address": {
+							Type:     pluginsdk.TypeList,
+							Computed: true,
+							Elem: &pluginsdk.Resource{
+								Schema: map[string]*pluginsdk.Schema{
+									"custom_ips": {
+										Type:     pluginsdk.TypeList,
+										Computed: true,
+										Elem: &pluginsdk.Schema{
+											Type: pluginsdk.TypeString,
+										},
+									},
+
+									"ip_configuration_id": {
+										Type:     pluginsdk.TypeString,
+										Computed: true,
+									},
+
+									"default_ips": {
+										Type:     pluginsdk.TypeList,
+										Computed: true,
+										Elem: &pluginsdk.Schema{
+											Type: pluginsdk.TypeString,
+										},
+									},
+
+									"tunnel_ips": {
+										Type:     pluginsdk.TypeList,
+										Computed: true,
+										Elem: &pluginsdk.Schema{
+											Type: pluginsdk.TypeString,
+										},
+									},
+								},
+							},
+						},
+
+						"instance_1_bgp_peering_address": {
+							Type:     pluginsdk.TypeList,
+							Computed: true,
+							Elem: &pluginsdk.Resource{
+								Schema: map[string]*pluginsdk.Schema{
+									"custom_ips": {
+										Type:     pluginsdk.TypeList,
+										Computed: true,
+										Elem: &pluginsdk.Schema{
+											Type: pluginsdk.TypeString,
+										},
+									},
+
+									"ip_configuration_id": {
+										Type:     pluginsdk.TypeString,
+										Computed: true,
+									},
+
+									"default_ips": {
+										Type:     pluginsdk.TypeList,
+										Computed: true,
+										Elem: &pluginsdk.Schema{
+											Type: pluginsdk.TypeString,
+										},
+									},
+
+									"tunnel_ips": {
+										Type:     pluginsdk.TypeList,
+										Computed: true,
+										Elem: &pluginsdk.Schema{
+											Type: pluginsdk.TypeString,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+
+			"scale_unit": {
+				Type:     pluginsdk.TypeInt,
+				Computed: true,
+			},
+
+			"tags": tags.SchemaDataSource(),
+		},
+	}
+}
+
+func dataSourceVPNGatewayRead(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).Network.VpnGatewaysClient
+	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
+	subscriptionId := meta.(*clients.Client).Account.SubscriptionId
+	defer cancel()
+
+	name := d.Get("name").(string)
+	resourceGroup := d.Get("resource_group_name").(string)
+	id := parse.NewVpnGatewayID(subscriptionId, resourceGroup, name)
+	resp, err := client.Get(ctx, id.ResourceGroup, id.Name)
+	if err != nil {
+		if utils.ResponseWasNotFound(resp.Response) {
+			log.Printf("[DEBUG] VPN Gateway %q was not found in Resource Group %q - removing from state", id.Name, id.ResourceGroup)
+			d.SetId("")
+			return nil
+		}
+
+		return fmt.Errorf("Error retrieving VPN Gateway %q (Resource Group %q): %+v", id.Name, id.ResourceGroup, err)
+	}
+
+	d.SetId(id.ID())
+	d.Set("name", id.Name)
+	d.Set("resource_group_name", id.ResourceGroup)
+	if location := resp.Location; location != nil {
+		d.Set("location", azure.NormalizeLocation(*location))
+	}
+
+	if props := resp.VpnGatewayProperties; props != nil {
+		if err := d.Set("bgp_settings", dataSourceFlattenVPNGatewayBGPSettings(props.BgpSettings)); err != nil {
+			return fmt.Errorf("Error setting `bgp_settings`: %+v", err)
+		}
+
+		scaleUnit := 0
+		if props.VpnGatewayScaleUnit != nil {
+			scaleUnit = int(*props.VpnGatewayScaleUnit)
+		}
+		d.Set("scale_unit", scaleUnit)
+
+		virtualHubId := ""
+		if props.VirtualHub != nil && props.VirtualHub.ID != nil {
+			virtualHubId = *props.VirtualHub.ID
+		}
+		d.Set("virtual_hub_id", virtualHubId)
+	}
+
+	return tags.FlattenAndSet(d, resp.Tags)
+}
+
+func dataSourceFlattenVPNGatewayBGPSettings(input *network.BgpSettings) []interface{} {
+	if input == nil {
+		return []interface{}{}
+	}
+
+	asn := 0
+	if input.Asn != nil {
+		asn = int(*input.Asn)
+	}
+
+	bgpPeeringAddress := ""
+	if input.BgpPeeringAddress != nil {
+		bgpPeeringAddress = *input.BgpPeeringAddress
+	}
+
+	peerWeight := 0
+	if input.PeerWeight != nil {
+		peerWeight = int(*input.PeerWeight)
+	}
+
+	var instance0BgpPeeringAddress, instance1BgpPeeringAddress []interface{}
+	if input.BgpPeeringAddresses != nil && len(*input.BgpPeeringAddresses) > 0 {
+		instance0BgpPeeringAddress = dataSourceFlattenVPNGatewayIPConfigurationBgpPeeringAddress((*input.BgpPeeringAddresses)[0])
+	}
+	if input.BgpPeeringAddresses != nil && len(*input.BgpPeeringAddresses) > 1 {
+		instance1BgpPeeringAddress = dataSourceFlattenVPNGatewayIPConfigurationBgpPeeringAddress((*input.BgpPeeringAddresses)[1])
+	}
+
+	return []interface{}{
+		map[string]interface{}{
+			"asn":                            asn,
+			"bgp_peering_address":            bgpPeeringAddress,
+			"instance_0_bgp_peering_address": instance0BgpPeeringAddress,
+			"instance_1_bgp_peering_address": instance1BgpPeeringAddress,
+			"peer_weight":                    peerWeight,
+		},
+	}
+}
+
+func dataSourceFlattenVPNGatewayIPConfigurationBgpPeeringAddress(input network.IPConfigurationBgpPeeringAddress) []interface{} {
+	ipConfigurationID := ""
+	if input.IpconfigurationID != nil {
+		ipConfigurationID = *input.IpconfigurationID
+	}
+
+	return []interface{}{
+		map[string]interface{}{
+			"ip_configuration_id": ipConfigurationID,
+			"custom_ips":          utils.FlattenStringSlice(input.CustomBgpIPAddresses),
+			"default_ips":         utils.FlattenStringSlice(input.DefaultBgpIPAddresses),
+			"tunnel_ips":          utils.FlattenStringSlice(input.TunnelIPAddresses),
+		},
+	}
+}
