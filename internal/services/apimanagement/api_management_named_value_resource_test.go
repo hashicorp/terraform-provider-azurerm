01ccf8bd385db356bd1977fb10c9b5a05b6ea54b
yupwei68 <yupwei@microsoft.com>
2021-08-11 10:38:54 +0800

    update

--- internal/services/apimanagement/api_management_named_value_resource_test.go
+++ internal/services/apimanagement/api_management_named_value_resource_test.go
@@ -46,6 +46,21 @@ func TestAccApiManagementNamedValue_keyVault(t *testing.T) {
 	})
 }
 
+func TestAccApiManagementNamedValue_keyVaultSystemAssigned(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_api_management_named_value", "test")
+	r := ApiManagementNamedValueResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.keyVaultSystemAssigned(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func TestAccApiManagementNamedValue_keyVaultUpdate(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_api_management_named_value", "test")
 	r := ApiManagementNamedValueResource{}
@@ -331,4 +346,105 @@ resource "azurerm_api_management_named_value" "test" {
   tags                = ["tag5", "tag6"]
 }
 `, r.keyVaultTemplate(data), data.RandomInteger)
+}
+
+func (r ApiManagementNamedValueResource) keyVaultSystemAssigned(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-Apim-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_api_management" "test" {
+  name                = "acctestAM-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  publisher_name      = "pub1"
+  publisher_email     = "pub1@email.com"
+
+  sku_name = "Consumption_0"
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "test" {
+  name                = "acctestKV-%[3]s"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "test" {
+  key_vault_id = azurerm_key_vault.test.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+  certificate_permissions = [
+    "Create",
+    "Delete",
+    "Deleteissuers",
+    "Get",
+    "Getissuers",
+    "Import",
+    "List",
+    "Listissuers",
+    "Managecontacts",
+    "Manageissuers",
+    "Setissuers",
+    "Update",
+    "Purge",
+  ]
+  secret_permissions = [
+    "Get",
+    "Delete",
+    "List",
+    "Purge",
+    "Recover",
+    "Set",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "test2" {
+  key_vault_id = azurerm_key_vault.test.id
+  tenant_id    = azurerm_api_management.test.identity.0.tenant_id
+  object_id    = azurerm_api_management.test.identity.0.principal_id
+  secret_permissions = [
+    "Get",
+    "List",
+  ]
+}
+
+resource "azurerm_key_vault_secret" "test" {
+  name         = "secret-%[3]s"
+  value        = "rick-and-morty"
+  key_vault_id = azurerm_key_vault.test.id
+
+  depends_on = [azurerm_key_vault_access_policy.test]
+}
+
+resource "azurerm_api_management_named_value" "test" {
+  name                = "acctestAMProperty-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  api_management_name = azurerm_api_management.test.name
+  display_name        = "TestKeyVault%[1]d"
+  secret              = true
+  value_from_key_vault {
+    secret_id = azurerm_key_vault_secret.test.id
+  }
+
+  depends_on = [azurerm_key_vault_access_policy.test2]
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomString)
 }
