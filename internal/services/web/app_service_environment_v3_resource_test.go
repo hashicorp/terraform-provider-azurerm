e409afb036531cb144cb228a861062a485d70f13
jackofallops <ste@hashicorp.com>
2021-08-12 16:38:18 +0100

    fix resource group reference for vnet update test

--- internal/services/web/app_service_environment_v3_resource_test.go
+++ internal/services/web/app_service_environment_v3_resource_test.go
@@ -264,7 +264,7 @@ resource "azurerm_subnet" "test2" {
 
 resource "azurerm_app_service_environment_v3" "test" {
   name                = "acctest-ase-%[2]d"
-  resource_group_name = azurerm_resource_group.test2.name
+  resource_group_name = azurerm_resource_group.test.name
   subnet_id           = azurerm_subnet.test2.id
 }
 `, template, data.RandomInteger)
