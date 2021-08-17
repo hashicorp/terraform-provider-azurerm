831aee61c60ea3be9a3c14ec17cfa2ef1fc2104c
Neil Ye <yechenwei2007@126.com>
2021-08-17 10:46:12 +0800

    New Resource: azurerm_logic_app_integration_account_certificate (#13003)
    Currently, TF doesn't support Logic App Integration Account Certificate. So I submitted this PR to implement it.
    
    --- PASS: TestAccLogicAppIntegrationAccountCertificate_complete (588.08s)
    --- PASS: TestAccLogicAppIntegrationAccountCertificate_basic (593.28s)
    --- PASS: TestAccLogicAppIntegrationAccountCertificate_requiresImport (663.53s)
    --- PASS: TestAccLogicAppIntegrationAccountCertificate_update (1002.16s)
    
    API Reference:
    Azure/azure-rest-api-specs@ceb6d11/specification/logic/resource-manager/Microsoft.Logic/stable/2019-05-01/logic.json#L5226

--- internal/services/logic/client/client.go
+++ internal/services/logic/client/client.go
@@ -7,6 +7,7 @@ import (
 
 type Client struct {
 	IntegrationAccountClient            *logic.IntegrationAccountsClient
+	IntegrationAccountCertificateClient *logic.IntegrationAccountCertificatesClient
 	IntegrationServiceEnvironmentClient *logic.IntegrationServiceEnvironmentsClient
 	WorkflowClient                      *logic.WorkflowsClient
 }
@@ -14,6 +15,9 @@ type Client struct {
 func NewClient(o *common.ClientOptions) *Client {
 	integrationAccountClient := logic.NewIntegrationAccountsClientWithBaseURI(o.ResourceManagerEndpoint, o.SubscriptionId)
 	o.ConfigureClient(&integrationAccountClient.Client, o.ResourceManagerAuthorizer)
+
+	integrationAccountCertificateClient := logic.NewIntegrationAccountCertificatesClientWithBaseURI(o.ResourceManagerEndpoint, o.SubscriptionId)
+	o.ConfigureClient(&integrationAccountCertificateClient.Client, o.ResourceManagerAuthorizer)
 
 	integrationServiceEnvironmentClient := logic.NewIntegrationServiceEnvironmentsClientWithBaseURI(o.ResourceManagerEndpoint, o.SubscriptionId)
 	o.ConfigureClient(&integrationServiceEnvironmentClient.Client, o.ResourceManagerAuthorizer)
@@ -23,6 +27,7 @@ func NewClient(o *common.ClientOptions) *Client {
 
 	return &Client{
 		IntegrationAccountClient:            &integrationAccountClient,
+		IntegrationAccountCertificateClient: &integrationAccountCertificateClient,
 		IntegrationServiceEnvironmentClient: &integrationServiceEnvironmentClient,
 		WorkflowClient:                      &workflowClient,
 	}
