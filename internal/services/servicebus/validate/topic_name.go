978f93c9dcf94f5933e408e37f25a6efb9c69636
Vladimir Lazarenko <favoretti@gmail.com>
2021-08-17 11:18:11 +0200

    Update regexp again

--- internal/services/servicebus/validate/topic_name.go
+++ internal/services/servicebus/validate/topic_name.go
@@ -9,7 +9,7 @@ import (
 
 func TopicName() pluginsdk.SchemaValidateFunc {
 	return validation.StringMatch(
-		regexp.MustCompile("^[A-Za-z0-9]$|^[A-Za-z0-9][\\w-\\.\\/\\~]*[A-Za-z0-9]$"),
+		regexp.MustCompile("^[a-zA-Z0-9]([-._~/a-zA-Z0-9]{0,258}[a-zA-Z0-9])?$"),
 		"The topic name can contain only letters, numbers, periods, hyphens, tildas, forward slashes and underscores. The namespace must start with a letter or number, and it must end with a letter or number and be less then 260 characters long.",
 	)
 }
