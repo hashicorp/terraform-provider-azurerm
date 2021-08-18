63ebaef48e971fb120eb93e01ca6a31f0f8a0f72
stephybun <steph@hashicorp.com>
2021-08-18 09:42:13 +0200

    add test case for at

--- helpers/validate/strings_test.go
+++ helpers/validate/strings_test.go
@@ -49,6 +49,11 @@ func TestLowerCasedStrings(t *testing.T) {
 			ErrCount: 1,
 		},
 		{
+			Value:    "@",
+			TestName: "At",
+			ErrCount: 1,
+		},
+		{
 			Value:    "Hello",
 			TestName: "TitleCaseSingleWord",
 			ErrCount: 1,
