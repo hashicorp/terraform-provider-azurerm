5d485ed162c1162ea65a90ea9c66e869d78adce5
yupwei68 <yupwei@microsoft.com>
2021-08-19 16:45:12 +0800

    update

--- internal/services/mysql/mysql_flexible_server_resource_test.go
+++ internal/services/mysql/mysql_flexible_server_resource_test.go
@@ -636,6 +636,7 @@ resource "azurerm_mysql_flexible_server" "test" {
     mode = "SameZone"
   }
   sku_name = "GP_Standard_D2ds_v4"
+  zone     = "1"
 }
 `, r.template(data), data.RandomInteger)
 }
@@ -655,6 +656,7 @@ resource "azurerm_mysql_flexible_server" "test" {
     standby_availability_zone = "2"
   }
   sku_name = "GP_Standard_D2ds_v4"
+  zone     = "1"
 }
 `, r.template(data), data.RandomInteger)
 }
@@ -674,6 +676,7 @@ resource "azurerm_mysql_flexible_server" "test" {
     standby_availability_zone = "3"
   }
   sku_name = "GP_Standard_D2ds_v4"
+  zone     = "1"
 }
 `, r.template(data), data.RandomInteger)
 }
