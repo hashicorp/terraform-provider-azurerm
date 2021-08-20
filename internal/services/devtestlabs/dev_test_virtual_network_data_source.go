ddc57029b1e7d696484ef872fc8f5b9e5dfe951b
Vladimir Lazarenko <favoretti@gmail.com>
2021-08-20 10:47:46 +0200

    `devtestlab`: Upgrade API to `2018-09-15` and...
    `dev_test_global_vm_shutdown_schedule`: add `email` parameter to notification settings

--- internal/services/devtestlabs/dev_test_virtual_network_data_source.go
+++ internal/services/devtestlabs/dev_test_virtual_network_data_source.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/services/devtestlabs/mgmt/2016-05-15/dtl"
+	"github.com/Azure/azure-sdk-for-go/services/devtestlabs/mgmt/2018-09-15/dtl"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/devtestlabs/validate"
