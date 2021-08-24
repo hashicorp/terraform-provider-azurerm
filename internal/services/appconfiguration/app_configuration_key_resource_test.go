0f9a2279ce85f6674555175d26e2e0addf53c56d
Kyriakos Oikonomakos <kyriakos@hashicorp.com>
2021-08-24 11:01:59 +0100

    add tests

---
+++ internal/services/appconfiguration/app_configuration_key_resource_test.go
@@ -0,0 +1,85 @@
+package appconfiguration_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/appconfiguration/parse"
+
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
+
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+)
+
+type AppConfigurationKeyResource struct {
+}
+
+func TestAccAppConfigurationKey_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_app_configuration_key", "test")
+	r := AppConfigurationKeyResource{}
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func (t AppConfigurationKeyResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
+
+	resourceID, err := parse.AppConfigurationKeyID(state.ID)
+	if err != nil {
+		return nil, fmt.Errorf("while parsing resource ID: %+v", err)
+	}
+
+	client, err := clients.AppConfiguration.DataPlaneClient(ctx, resourceID.ConfigurationStoreId)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := client.GetKeyValues(ctx, resourceID.Key, resourceID.Label, "", "", []string{})
+	if err != nil {
+		return nil, fmt.Errorf("while checking for key's %q existence: %+v", resourceID.Key, err)
+	}
+
+	return utils.Bool(res.Response().StatusCode == 200), nil
+}
+
+func (t AppConfigurationKeyResource) basic(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-appconfig-%d"
+  location = "%s"
+}
+
+resource "azurerm_app_configuration" "test" {
+  name                = "testacc-appconf%d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  sku                 = "free"
+}
+
+resource "azurerm_app_configuration_key" "test" {
+   configuration_store_id = azurerm_app_configuration.test.id
+   key = "acctest-ackey-%d"
+   content_type = "test"
+   label = "acctest-ackeylabel-%d"
+   value = "a test"
+}
+
+
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger, data.RandomInteger)
+}
