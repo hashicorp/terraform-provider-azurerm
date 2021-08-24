59435b181af47b1b46d9394b162cdc62ee9d1f6f
Vladimir Lazarenko <favoretti@gmail.com>
2021-08-24 11:56:15 +0200

    Add `azurerm_relay_hybrid_connection_authorization_rule` resource

--- internal/services/relay/internal.go
+++ internal/services/relay/internal.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/relay/sdk/2017-04-01/hybridconnections"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/relay/sdk/2017-04-01/namespaces"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 )
@@ -78,18 +79,53 @@ func expandAuthorizationRuleRights(d *pluginsdk.ResourceData) []namespaces.Acces
 func flattenAuthorizationRuleRights(rights []namespaces.AccessRights) (listen, send, manage bool) {
 	// zero (initial) value for a bool in go is false
 
-	if rights != nil {
-		for _, right := range rights {
-			switch right {
-			case namespaces.AccessRightsListen:
-				listen = true
-			case namespaces.AccessRightsSend:
-				send = true
-			case namespaces.AccessRightsManage:
-				manage = true
-			default:
-				log.Printf("[DEBUG] Unknown Authorization Rule Right '%s'", right)
-			}
+	for _, right := range rights {
+		switch right {
+		case namespaces.AccessRightsListen:
+			listen = true
+		case namespaces.AccessRightsSend:
+			send = true
+		case namespaces.AccessRightsManage:
+			manage = true
+		default:
+			log.Printf("[DEBUG] Unknown Authorization Rule Right '%s'", right)
+		}
+	}
+
+	return listen, send, manage
+}
+
+func expandHybridConnectionAuthorizationRuleRights(d *pluginsdk.ResourceData) []hybridconnections.AccessRights {
+	rights := make([]hybridconnections.AccessRights, 0)
+
+	if d.Get("listen").(bool) {
+		rights = append(rights, hybridconnections.AccessRightsListen)
+	}
+
+	if d.Get("send").(bool) {
+		rights = append(rights, hybridconnections.AccessRightsSend)
+	}
+
+	if d.Get("manage").(bool) {
+		rights = append(rights, hybridconnections.AccessRightsManage)
+	}
+
+	return rights
+}
+
+func flattenHybridConnectionAuthorizationRuleRights(rights []hybridconnections.AccessRights) (listen, send, manage bool) {
+	// zero (initial) value for a bool in go is false
+
+	for _, right := range rights {
+		switch right {
+		case hybridconnections.AccessRightsListen:
+			listen = true
+		case hybridconnections.AccessRightsSend:
+			send = true
+		case hybridconnections.AccessRightsManage:
+			manage = true
+		default:
+			log.Printf("[DEBUG] Unknown Authorization Rule Right '%s'", right)
 		}
 	}
 
