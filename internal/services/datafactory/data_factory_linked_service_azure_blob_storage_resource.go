65d042a5d251b2c2298f69fe854632d3f33b48c5
orestis.katsanakis <orestis.katsanakis@infinityworks.com>
2021-08-25 17:39:29 +0100

    set properties for sas token in read

--- internal/services/datafactory/data_factory_linked_service_azure_blob_storage_resource.go
+++ internal/services/datafactory/data_factory_linked_service_azure_blob_storage_resource.go
@@ -315,6 +315,25 @@ func resourceDataFactoryLinkedServiceBlobStorageRead(d *pluginsdk.ResourceData, 
 		}
 	}
 
+	if properties := blobStorage.AzureBlobStorageLinkedServiceTypeProperties; properties != nil {
+		if properties.SasURI != nil {
+			if val, ok := properties.SasURI.(string); ok {
+				d.Set("sas_uri", val)
+			} else {
+				d.Set("sas_uri", "")
+				log.Printf("[DEBUG] Skipping sas uri %q since it's not a string", val)
+			}
+		}
+
+		if sasToken := properties.SasToken; sasToken != nil {
+			if keyVaultPassword, ok := sasToken.AsAzureKeyVaultSecretReference; ok {
+				if err := d.Set("key_vault_sas_token", flattenAzureKeyVaultSecretReference(keyVaultPassword)); err != nil {
+					return fmt.Errorf("setting `key_vault_sas_token`: %+v", err)
+				}
+			}
+		}
+	}
+
 	d.Set("additional_properties", blobStorage.AdditionalProperties)
 	d.Set("description", blobStorage.Description)
 
