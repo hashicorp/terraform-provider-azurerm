376eb66a7993638827eaf8ddb58616a703fdc9b0
Kyriakos Oikonomakos <kyriakos@hashicorp.com>
2021-08-31 17:46:51 +0100

    Use pluginsdk package

--- internal/services/appconfiguration/app_configuration_key_resource.go
+++ internal/services/appconfiguration/app_configuration_key_resource.go
@@ -5,9 +5,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
@@ -16,6 +13,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/appconfiguration/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tags"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
@@ -77,8 +75,8 @@ func (k KeyResource) Arguments() map[string]*pluginsdk.Schema {
 	}
 }
 
-func (k KeyResource) Attributes() map[string]*schema.Schema {
-	return map[string]*schema.Schema{}
+func (k KeyResource) Attributes() map[string]*pluginsdk.Schema {
+	return map[string]*pluginsdk.Schema{}
 }
 
 func (k KeyResource) ModelObject() interface{} {
