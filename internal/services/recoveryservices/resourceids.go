1a43d2d93a1b9fade368c1c191c236af6af9e4e7
Catriona <catriona@hashicorp.com>
2021-09-09 16:24:40 +0100

    generate replication policy parser

--- internal/services/recoveryservices/resourceids.go
+++ internal/services/recoveryservices/resourceids.go
@@ -2,3 +2,4 @@ package recoveryservices
 
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ReplicationNetworkMapping -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/group1/providers/Microsoft.RecoveryServices/vaults/vault1/replicationFabrics/fabric1/replicationNetworks/network1/replicationNetworkMappings/mapping1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ReplicationFabric -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/group1/providers/Microsoft.RecoveryServices/vaults/vault1/replicationFabrics/fabric1
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ReplicationPolicy -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/group1/providers/Microsoft.RecoveryServices/vaults/vault1/replicationPolicies/policy1
