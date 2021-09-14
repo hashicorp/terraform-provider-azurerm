07e39bbaf38c88f7de526f84264202af3d87a481
Steph <steph@hashicorp.com>
2021-09-14 16:46:42 +0200

    fix import

--- internal/services/iothub/iothub_data_source.go
+++ internal/services/iothub/iothub_data_source.go
@@ -2,11 +2,11 @@ package iothub
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/services/iothub/parse"
 	"time"
 
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/iothub/parse"
 	iothubValidate "github.com/hashicorp/terraform-provider-azurerm/internal/services/iothub/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tags"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
