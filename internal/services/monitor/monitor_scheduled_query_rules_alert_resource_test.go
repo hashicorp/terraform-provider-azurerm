1fe0da238d0a4d3f21d189f633d0a7fac072ffcd
Vincent Cloutier <vincent@cloutier.co>
2021-09-15 09:31:19 -0400

    fix tf linting

--- internal/services/monitor/monitor_scheduled_query_rules_alert_resource_test.go
+++ internal/services/monitor/monitor_scheduled_query_rules_alert_resource_test.go
@@ -134,16 +134,16 @@ resource "azurerm_monitor_action_group" "test" {
   short_name          = "acctestag"
 }
 resource "azurerm_monitor_scheduled_query_rules_alert" "test" {
-  name                  = "acctestsqr-%d"
-  resource_group_name   = azurerm_resource_group.test.name
-  location              = azurerm_resource_group.test.location
-  data_source_id        = azurerm_application_insights.test.id
-  query                 = <<-QUERY
+  name                    = "acctestsqr-%d"
+  resource_group_name     = azurerm_resource_group.test.name
+  location                = azurerm_resource_group.test.location
+  data_source_id          = azurerm_application_insights.test.id
+  query                   = <<-QUERY
 	let d=datatable(TimeGenerated: datetime, usage_percent: double) [  '%s', 25.4, '%s', 75.4 ];
 	d | summarize AggregatedValue=avg(usage_percent) by bin(TimeGenerated, 1h)
 QUERY
-  frequency             = 60
-  time_window           = 60
+  frequency               = 60
+  time_window             = 60
   auto_mitigation_enabled = %s
   action {
     action_group = [azurerm_monitor_action_group.test.id]
