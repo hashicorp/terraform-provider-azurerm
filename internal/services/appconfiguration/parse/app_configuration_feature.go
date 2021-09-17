8930dd04b53a02d01ec2c6c3edb2cf1294b1289c
Kyriakos Oikonomakos <kyriakos@hashicorp.com>
2021-09-17 11:02:04 +0100

    Add parser and validator for app_configuration_feature

---
+++ internal/services/appconfiguration/parse/app_configuration_feature.go
@@ -0,0 +1,45 @@
+package parse
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
+)
+
+type AppConfigurationFeatureId struct {
+	ConfigurationStoreId string
+	Key                  string
+	Label                string
+}
+
+func (k AppConfigurationFeatureId) ID() string {
+	return fmt.Sprintf("%s/AppConfigurationFeature/%s/Label/%s", k.ConfigurationStoreId, k.Key, k.Label)
+}
+
+func FeatureId(input string) (*AppConfigurationFeatureId, error) {
+
+	resourceID, err := azure.ParseAzureResourceID(input)
+	if err != nil {
+		return nil, fmt.Errorf("while parsing resource ID: %+v", err)
+	}
+
+	keyName := resourceID.Path["AppConfigurationFeature"]
+	label := resourceID.Path["Label"]
+
+	appcfgID := AppConfigurationFeatureId{
+		Key:   keyName,
+		Label: label,
+	}
+
+	// Golang's URL parser will translate %00 to \000 (NUL). This will only happen if we're dealing with an empty
+	// label, so we set the label to the expected value (empty string) and trim the input string, so we can properly
+	// extract the configuration store ID out of it.
+	if label == "\000" {
+		appcfgID.Label = ""
+		input = strings.TrimSuffix(input, "%00")
+	}
+	appcfgID.ConfigurationStoreId = strings.TrimSuffix(input, fmt.Sprintf("/AppConfigurationFeature/%s/Label/%s", appcfgID.Key, appcfgID.Label))
+
+	return &appcfgID, nil
+}
