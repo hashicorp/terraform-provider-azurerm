e76316f2d066eca4c8e78e0fb17c5c748a12f319
Matthew Frahry <mbfrahry@gmail.com>
2021-09-28 14:04:49 -0700

    Another one

--- internal/services/compute/linux_virtual_machine_resource_disk_os_test.go
+++ internal/services/compute/linux_virtual_machine_resource_disk_os_test.go
@@ -433,7 +433,7 @@ resource "azurerm_key_vault" "test" {
   resource_group_name         = azurerm_resource_group.test.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
-  soft_delete_enabled         = true
+  soft_delete_enabled         = false
   purge_protection_enabled    = true
   enabled_for_disk_encryption = true
 
