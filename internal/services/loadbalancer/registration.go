c3a8b05e23d845ca9fb809107f2896d05406c52d
magodo <wztdyl@sina.com>
2021-09-29 11:01:42 +0800

    revert the tunnel interface resource && add prop to BAP

--- internal/services/loadbalancer/registration.go
+++ internal/services/loadbalancer/registration.go
@@ -53,6 +53,5 @@ func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
 func (r Registration) Resources() []sdk.Resource {
 	return []sdk.Resource{
 		BackendAddressPoolAddressResource{},
-		BackendAddressPoolTunnelInterfaceResource{},
 	}
 }
