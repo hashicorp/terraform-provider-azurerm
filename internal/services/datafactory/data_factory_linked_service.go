fd8ba4090a303abff87861a3d81c46c4609b096f
Steph <steph@hashicorp.com>
2021-10-05 14:49:34 +0200

    fix lint

--- internal/services/datafactory/data_factory_linked_service.go
+++ internal/services/datafactory/data_factory_linked_service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+
 	"github.com/Azure/azure-sdk-for-go/services/datafactory/mgmt/2018-06-01/datafactory"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/datafactory/parse"
