2174976811a7bec2a3a96411102beb9ec96ae90e
Fernando de Oliveira <5161098+fernandoBRS@users.noreply.github.com>
2021-10-07 13:11:23 -0300

    azurerm_redhatopenshift_cluster schema definition

---
+++ internal/services/redhatopenshift/enums.go
@@ -0,0 +1,10 @@
+package redhatopenshift
+
+type APIServerVisibility string
+
+const (
+	// Public ...
+	Public APIServerVisibility = "Public"
+	// Private ...
+	Private APIServerVisibility = "Private"
+)
