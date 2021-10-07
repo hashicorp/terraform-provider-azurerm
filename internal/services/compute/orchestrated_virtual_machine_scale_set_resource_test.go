ba7d54546f628b74c4c598310b56d27423850fe6
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2021-10-06 23:37:48 -0700

    Fix allocation_method to be Dynamic

--- internal/services/compute/orchestrated_virtual_machine_scale_set_resource_test.go
+++ internal/services/compute/orchestrated_virtual_machine_scale_set_resource_test.go
@@ -2674,7 +2674,7 @@ resource "azurerm_public_ip" "test" {
   name                = "acctest-pubip-%[1]d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  allocation_method   = "Static"
+  allocation_method   = "Dynamic"
 }
 
 resource "azurerm_application_gateway" "test" {
