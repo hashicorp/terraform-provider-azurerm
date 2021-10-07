569bad4f4dac9f0248fec57528c8d2b2ff59beaa
Fernando de Oliveira <5161098+fernandoBRS@users.noreply.github.com>
2021-10-07 14:42:42 -0300

    azurerm_redhatopenshift_cluster: random domain name generation

---
+++ internal/services/redhatopenshift/helpers.go
@@ -0,0 +1,27 @@
+package redhatopenshift
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func randomString(acceptedChars string, size int) string {
+	charSet := []rune(acceptedChars)
+	randomChars := make([]rune, size)
+
+	rand.Seed(time.Now().UnixNano())
+
+	for i := range randomChars {
+		randomChars[i] = charSet[rand.Intn(len(charSet))]
+	}
+
+	return string(randomChars)
+}
+
+func GenerateRandomDomainName() string {
+	randomPrefix := randomString("abcdefghijklmnopqrstuvwxyz", 1)
+	randomName := randomString("abcdefghijklmnopqrstuvwxyz1234567890", 7)
+
+	return fmt.Sprintf("%s%s", randomPrefix, randomName)
+}
