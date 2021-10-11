8418e302e6aa89cd9b223d2426ff0d1ae9c3cba2
Michael Gross <leachimgross@gmail.com>
2021-10-11 20:06:45 +0200

    simplify tests

--- internal/services/consumption/consumption_budget_subscription_data_source_test.go
+++ internal/services/consumption/consumption_budget_subscription_data_source_test.go
@@ -3,85 +3,37 @@ package consumption_test
 import (
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 )
 
-type BudgetSubscriptionDataSource struct{}
+type ConsumptionBudgetSubscriptionDataSource struct{}
 
 func TestAccBudgetSubscriptionDataSource_basic(t *testing.T) {
 	data := acceptance.BuildTestData(t, "data.azurerm_consumption_budget_subscription", "test")
-	r := BudgetSubscriptionDataSource{}
+	r := ConsumptionBudgetSubscriptionDataSource{}
 
 	data.DataSourceTest(t, []acceptance.TestStep{
-		{
-			Config: r.template(data),
-		},
 		{
 			Config: r.basic(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).Key("subscription_id").HasValue(data.Client().SubscriptionID),
-				check.That(data.ResourceName).Key("name").HasValue(fmt.Sprintf("acctestconsumptionbudget-%d", data.RandomInteger)),
+				check.That(data.ResourceName).Key("name").Exists(),
 			),
 		},
 	})
 }
 
-func (d BudgetSubscriptionDataSource) basic(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-provider "azurerm" {
-  features {}
-}
 
-data "azurerm_subscription" "current" {}
+func (d ConsumptionBudgetSubscriptionDataSource) basic(data acceptance.TestData) string {
+  config := ConsumptionBudgetResourceGroupResource{}.basic(data)
+  return fmt.Sprintf(`
+  %s
 
 data "azurerm_consumption_budget_subscription" "test" {
-  name            = "acctestconsumptionbudget-%d"
-  subscription_id = data.azurerm_subscription.current.subscription_id
+  name            = azurerm_subscription.test.name
+  subscription_id = azurerm_subscription.test.subscription_id
 }
-`, data.RandomInteger)
-}
-
-func (BudgetSubscriptionDataSource) template(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-provider "azurerm" {
-  features {}
-}
-
-data "azurerm_subscription" "current" {}
-
-resource "azurerm_consumption_budget_subscription" "test" {
-  name            = "acctestconsumptionbudget-%d"
-  subscription_id = data.azurerm_subscription.current.subscription_id
-
-  amount     = 1000
-  time_grain = "Monthly"
-
-  time_period {
-    start_date = "%s"
-  }
-
-  filter {
-    tag {
-      name = "foo"
-      values = [
-        "bar",
-      ]
-    }
-  }
-
-  notification {
-    enabled   = true
-    threshold = 90.0
-    operator  = "EqualTo"
-
-    contact_emails = [
-      "foo@example.com",
-      "bar@example.com",
-    ]
-  }
-}
-`, data.RandomInteger, consumptionBudgetTestStartDate().Format(time.RFC3339))
+`, config)
 }
