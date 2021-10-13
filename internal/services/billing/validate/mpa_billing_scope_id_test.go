d25ee4224b7c41378c9462d5bcada66210736fbe
David Hoeld <david.hoeld@fujitsu.com>
2021-10-13 10:40:43 +0200

    Add support for adding MPA subscriptions

---
+++ internal/services/billing/validate/mpa_billing_scope_id_test.go
@@ -0,0 +1,62 @@
+package validate
+
+import "testing"
+
+func TestMicrosoftPartnerAccountBillingScopeID(t *testing.T) {
+	cases := []struct {
+		Input string
+		Valid bool
+	}{
+
+		{
+			// empty
+			Input: "",
+			Valid: false,
+		},
+
+		{
+			// missing BillingAccountName
+			Input: "/providers/Microsoft.Billing/",
+			Valid: false,
+		},
+
+		{
+			// missing value for BillingAccountName
+			Input: "/providers/Microsoft.Billing/billingAccounts/",
+			Valid: false,
+		},
+
+		{
+			// missing CustomerName
+			Input: "/providers/Microsoft.Billing/billingAccounts/e879cf0f-2b4d-5431-109a-f72fc9868693:024cabf4-7321-4cf9-be59-df0c77ca51de_2019-05-31/",
+			Valid: false,
+		},
+
+		{
+			// missing value for CustomerName
+			Input: "/providers/Microsoft.Billing/billingAccounts/e879cf0f-2b4d-5431-109a-f72fc9868693:024cabf4-7321-4cf9-be59-df0c77ca51de_2019-05-31/customers/",
+			Valid: false,
+		},
+
+		{
+			// valid
+			Input: "/providers/Microsoft.Billing/billingAccounts/e879cf0f-2b4d-5431-109a-f72fc9868693:024cabf4-7321-4cf9-be59-df0c77ca51de_2019-05-31/customers/2281f543-7321-4cf9-1e23-edb4Oc31a31c",
+			Valid: true,
+		},
+
+		{
+			// upper-cased
+			Input: "/PROVIDERS/MICROSOFT.BILLING/BILLINGACCOUNTS/E879CF0F-2B4D-5431-109A-F72FC9868693:024CABF4-7321-4CF9-BE59-DF0C77CA51DE_2019-05-31/CUSTOMERS/2281F543-7321-4CF9-1E23-EDB4OC31A31C",
+			Valid: false,
+		},
+	}
+	for _, tc := range cases {
+		t.Logf("[DEBUG] Testing Value %s", tc.Input)
+		_, errors := MicrosoftPartnerAccountBillingScopeID(tc.Input, "test")
+		valid := len(errors) == 0
+
+		if tc.Valid != valid {
+			t.Fatalf("Expected %t but got %t", tc.Valid, valid)
+		}
+	}
+}
