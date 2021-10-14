540e222674008fd16900204ea6898dae75fbe965
tombuildsstuff <git@ibuildstuff.co.uk>
2021-10-14 15:18:24 +0200

    tooling: checking that each of the examples validates via `terraform fmt`

--- scripts/validate-examples.sh
+++ scripts/validate-examples.sh
@@ -6,6 +6,7 @@ exampleDirs=$(find ./examples -mindepth 2 -maxdepth 2 -type d '!' -exec test -e 
 examplesWithErrors=()
 hasError=false
 
+# first check each example validates via `terraform validate`..
 for d in $exampleDirs; do
   echo "Validating $d.."
   exampleHasErrors=false
@@ -32,4 +33,29 @@ if ${hasError}; then
   exit 1
 fi
 
+# Finally check everything is formatted
+for d in $exampleDirs; do
+  echo "Checking formatting for $d.."
+  exampleHasErrors=false
+  terraform fmt -check $d > /dev/null || exampleHasErrors=true
+  if ${exampleHasErrors}; then
+    examplesWithErrors[${#examplesWithErrors[@]}]=$d
+    hasError=true
+  fi
+done
+
+if ${hasError}; then
+  echo "------------------------------------------------"
+  echo ""
+  echo "The directories listed below aren't formatted using 'terraform fmt'."
+  echo "Please fix the validation errors for these, these can be found by running"
+  echo "'terraform fmt':"
+  for exampleDir in "${examplesWithErrors[@]}"
+  do
+       echo "- $exampleDir"
+  done
+  exit 1
+fi
+
+echo "All Examples Validate and Format"
 exit 0
