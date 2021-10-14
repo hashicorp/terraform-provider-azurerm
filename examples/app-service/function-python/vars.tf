87a2619672c09f9e27f7af06721adcd9b0b41801
tombuildsstuff <git@ibuildstuff.co.uk>
2021-10-14 14:12:45 +0200

    examples/app-service/functions-python: fixing the vars file

--- examples/app-service/function-python/vars.tf
+++ examples/app-service/function-python/vars.tf
@@ -1,6 +1,7 @@
 variable "location" {
   description = "The Azure Region in which all resources in this example should be provisioned"
   default = "Central US"
+}
 
 variable "storageaccount" {
   description = "Name of the storage account"
