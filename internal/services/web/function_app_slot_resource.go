0372aa5acba507dbf8be47826d49d5f8bd16cfb1
kt <kt@katbyte.me>
2021-10-14 12:21:27 -0700

    Update internal/services/web/function_app_slot_resource.go
    Co-authored-by: jackofallops <11830746+jackofallops@users.noreply.github.com>

--- internal/services/web/function_app_slot_resource.go
+++ internal/services/web/function_app_slot_resource.go
@@ -135,7 +135,7 @@ func resourceFunctionAppSlot() *pluginsdk.Resource {
 				Type:       pluginsdk.TypeBool,
 				Optional:   true,
 				Computed:   true,
-				Deprecated: "This doesn't do anything so it is deprecated and will be removed in 3.0 of the provider.",
+				Deprecated: "This property is no longer configurable in the service and has been deprecated. It will be removed in 3.0 of the provider.",
 			},
 
 			"connection_string": {
