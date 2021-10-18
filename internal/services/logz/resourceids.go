331fe4a8ad7efb74fd37ef386e8eb1b0f200b4d0
Zhenhua Hu <zhhu@microsoft.com>
2021-10-18 15:04:06 +0800

    new resource "logz_monitor"

---
+++ internal/services/logz/resourceids.go
@@ -0,0 +1,3 @@
+package logz
+
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=LogzMonitor -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resourceGroup1/providers/Microsoft.Logz/monitors/monitor1
