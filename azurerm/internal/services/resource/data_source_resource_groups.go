7e0c1d250945f2e5cf863ddf61c9c5e22a24f251
Michael Gross <leachimgross@gmail.com>
2021-10-18 19:39:58 +0200

    Updated resource groups data source - make things clearer

--- azurerm/internal/services/resource/data_source_resource_groups.go
+++ azurerm/internal/services/resource/data_source_resource_groups.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tags"
-
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/clients"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/resource/parse"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/tf/pluginsdk"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/timeouts"
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	resource "github.com/hashicorp/terraform-provider-azurerm/internal/services/resource/client"
+	"github.com/terraform-provider-azurerm/internal/services/resource/parse"
+	"github.com/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/terraform-provider-azurerm/internal/timeouts"
 )
 
 func dataSourceResourceGroups() *pluginsdk.Resource {
@@ -60,15 +60,20 @@ func dataSourceResourceGroups() *pluginsdk.Resource {
 
 func dataSourceResourceGroupsRead(d *pluginsdk.ResourceData, meta interface{}) error {
 	armClient := meta.(*clients.Client)
-	rgClient := armClient.Resource.GroupsClient
 	subClient := armClient.Subscription.Client
 	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
 	// ListComplete returns an iterator struct
-	results, err := rgClient.ListComplete(ctx, "", nil)
-	if err != nil {
-		return fmt.Errorf("listing resource groups: %+v", err)
+	results := resource.GroupListResultIterator{
+	}
+	var err error
+	for _, subId := range d.Get("subscription_ids").([]string){
+		rgSubGroupsClient := resource.NewGroupsClient(subId)
+		results, err = rgSubGroupsClient.ListComplete(ctx, "", nil)
+		if err != nil {
+			return fmt.Errorf("listing resource groups: %+v", err)
+		}
 	}
 
 	// iterate across each resource groups and append them to slice
@@ -76,44 +81,68 @@ func dataSourceResourceGroupsRead(d *pluginsdk.ResourceData, meta interface{}) e
 	for results.NotDone() {
 		val := results.Value()
 
-		rg := make(map[string]interface{})
-
+		rgId := ""
 		if v := val.ID; v != nil {
-			rg["id"] = *v
-			rgStruct, err := parse.ResourceGroupID(*v)
-			if err != nil {
-				return fmt.Errorf("parsing Resource Group ID")
-			}
-			rg["subscription_id"] = rgStruct.SubscriptionId
+			rgId = *v
 		}
 
-		resp, err := subClient.Get(ctx, rg["subscription_id"].(string))
+		rgSubId := ""
+		rgStruct, err := parse.ResourceGroupID(*val.ID)
+		if err != nil {
+			return fmt.Errorf("parsing Resource Group ID")
+		}
+		rgSubId = rgStruct.SubscriptionId
+		
+
+		rgTenantId := ""
+		resp, err := subClient.Get(ctx, rgSubId)
 		if err != nil {
 			return fmt.Errorf("reading subscription: %+v", err)
 		} else {
-			rg["tenant_id"] = *resp.TenantID
+			rgTenantId = *resp.TenantID
 		}
 
+		rgName := ""
 		if v := val.Name; v != nil {
-			rg["name"] = *v
+			rgName = *v
 		}
+
+		rgType := ""
 		if v := val.Type; v != nil {
-			rg["type"] = *v
+			rgType = *v
 		}
+
+		rgLocation := ""
 		if v := val.Location; v != nil {
-			rg["location"] = *v
+			rgLocation = *v
 		}
+
+		rgTags := make(map[string]interface{})
+		if val.Tags != nil {
+			rgTags = make(map[string]interface{}, len(val.Tags))
+			for key, value := range val.Tags {
+				if value != nil {
+					rgTags[key] = *value
+				}
+			}
+		}
+
 		if err = results.Next(); err != nil {
 			return fmt.Errorf("going to next resource groups value: %+v", err)
 		}
 
-		rg["tags"] = tags.Flatten(val.Tags)
-
-		resourceGroups = append(resourceGroups, rg)
-
+		resourceGroups = append(resourceGroups, map[string]interface{}{
+			"id": rgId,
+			"name": rgName,
+			"type":     rgType,
+			"location": rgLocation,
+			"subscriptionId": rgSubId,
+			"tenantId": rgTenantId,
+			"tags":     rgTags,
+		})
 	}
 
-	d.SetId("resource_groups-" + armClient.Account.TenantId)
+	d.SetId("resource_groups-" + uuid.New().String())
 	if err = d.Set("resource_groups", resourceGroups); err != nil {
 		return fmt.Errorf("setting `resource_groups`: %+v", err)
 	}
