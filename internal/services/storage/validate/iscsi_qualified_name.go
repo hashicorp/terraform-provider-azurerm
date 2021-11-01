7ee123dccc1e2c3c6808967c399afa601a50cffc
hezijie <lonegunmanb@hotmail.com>
2021-11-01 17:12:08 +0800

    add doc for how to validate iqn

--- internal/services/storage/validate/iscsi_qualified_name.go
+++ internal/services/storage/validate/iscsi_qualified_name.go
@@ -1,5 +1,6 @@
 package validate
 
+//https://docs.vmware.com/en/VMware-vSphere/5.5/com.vmware.vsphere.storage.doc/GUID-686D92B6-A2B2-4944-8718-F1B74F6A2C53.html
 func ValidateIQN(i interface{}, k string) (warnings []string, errors []error) {
 	panic("implement me")
 	//v, ok := i.(string)
