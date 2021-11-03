1a39c97d59bdf5e4e51ad50321cbe326abe871a2
Owen Farrell <owen.farrell@gmail.com>
2021-11-03 08:15:36 -0400

    new resource `azurerm_stream_analytics_output_synapse`
    Signed-off-by: Owen Farrell <owen.farrell@gmail.com>

---
+++ internal/services/streamanalytics/stream_analytics_output_synapse_resource.go
@@ -0,0 +1,208 @@
+package streamanalytics
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/services/preview/streamanalytics/mgmt/2020-03-01-preview/streamanalytics"
+	"github.com/hashicorp/go-azure-helpers/response"
+	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/streamanalytics/parse"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+)
+
+func resourceStreamAnalyticsOutputSynapse() *pluginsdk.Resource {
+	return &pluginsdk.Resource{
+		Create: resourceStreamAnalyticsOutputSynapseCreateUpdate,
+		Read:   resourceStreamAnalyticsOutputSynapseRead,
+		Update: resourceStreamAnalyticsOutputSynapseCreateUpdate,
+		Delete: resourceStreamAnalyticsOutputSynapseDelete,
+		Importer: pluginsdk.ImporterValidatingResourceId(func(id string) error {
+			_, err := parse.OutputID(id)
+			return err
+		}),
+
+		Timeouts: &pluginsdk.ResourceTimeout{
+			Create: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
+			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
+		},
+
+		Schema: map[string]*pluginsdk.Schema{
+			"name": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"stream_analytics_job_name": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"resource_group_name": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"server": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"database": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"table": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"user": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"password": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				Sensitive:    true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+		},
+	}
+}
+
+func resourceStreamAnalyticsOutputSynapseCreateUpdate(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).StreamAnalytics.OutputsClient
+	ctx, cancel := timeouts.ForCreateUpdate(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	subscriptionId := meta.(*clients.Client).Account.SubscriptionId
+	id := parse.NewOutputID(subscriptionId, d.Get("resource_group_name").(string), d.Get("stream_analytics_job_name").(string), d.Get("name").(string))
+
+	if d.IsNewResource() {
+		existing, err := client.Get(ctx, id.ResourceGroup, id.StreamingjobName, id.Name)
+		if err != nil && !utils.ResponseWasNotFound(existing.Response) {
+			return fmt.Errorf("checking for presence of %s: %+v", id, err)
+		}
+
+		if existing.ID != nil && *existing.ID != "" {
+			return tf.ImportAsExistsError("azurerm_stream_analytics_output_synapse", id.ID())
+		}
+	}
+
+	server := d.Get("server").(string)
+	databaseName := d.Get("database").(string)
+	tableName := d.Get("table").(string)
+	sqlUser := d.Get("user").(string)
+	sqlUserPassword := d.Get("password").(string)
+
+	props := streamanalytics.Output{
+		Name: utils.String(id.Name),
+		OutputProperties: &streamanalytics.OutputProperties{
+			Datasource: &streamanalytics.AzureSynapseOutputDataSource{
+				Type: streamanalytics.TypeMicrosoftSQLServerDataWarehouse,
+				AzureSynapseOutputDataSourceProperties: &streamanalytics.AzureSynapseOutputDataSourceProperties{
+					Server:   utils.String(server),
+					Database: utils.String(databaseName),
+					User:     utils.String(sqlUser),
+					Password: utils.String(sqlUserPassword),
+					Table:    utils.String(tableName),
+				},
+			},
+		},
+	}
+
+	if d.IsNewResource() {
+		if _, err := client.CreateOrReplace(ctx, props, id.ResourceGroup, id.StreamingjobName, id.Name, "", ""); err != nil {
+			return fmt.Errorf("creating %s: %+v", id, err)
+		}
+
+		d.SetId(id.ID())
+	} else if _, err := client.Update(ctx, props, id.ResourceGroup, id.StreamingjobName, id.Name, ""); err != nil {
+		return fmt.Errorf("updating %s: %+v", id, err)
+	}
+
+	return resourceStreamAnalyticsOutputSynapseRead(d, meta)
+}
+
+func resourceStreamAnalyticsOutputSynapseRead(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).StreamAnalytics.OutputsClient
+	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	id, err := parse.OutputID(d.Id())
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Get(ctx, id.ResourceGroup, id.StreamingjobName, id.Name)
+	if err != nil {
+		if utils.ResponseWasNotFound(resp.Response) {
+			log.Printf("[DEBUG] %s was not found - removing from state!", id)
+			d.SetId("")
+			return nil
+		}
+
+		return fmt.Errorf("retreving %s: %+v", *id, err)
+	}
+
+	d.Set("name", id.Name)
+	d.Set("stream_analytics_job_name", id.StreamingjobName)
+	d.Set("resource_group_name", id.ResourceGroup)
+
+	if props := resp.OutputProperties; props != nil {
+		v, ok := props.Datasource.AsAzureSynapseOutputDataSource()
+		if !ok {
+			return fmt.Errorf("converting Output Data Source to Synapse Output: %+v", err)
+		}
+
+		d.Set("server", v.Server)
+		d.Set("database", v.Database)
+		d.Set("table", v.Table)
+		d.Set("user", v.User)
+	}
+
+	return nil
+}
+
+func resourceStreamAnalyticsOutputSynapseDelete(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).StreamAnalytics.OutputsClient
+	ctx, cancel := timeouts.ForDelete(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	id, err := parse.OutputID(d.Id())
+	if err != nil {
+		return err
+	}
+
+	if resp, err := client.Delete(ctx, id.ResourceGroup, id.StreamingjobName, id.Name); err != nil {
+		if !response.WasNotFound(resp.Response) {
+			return fmt.Errorf("deleting %s: %+v", *id, err)
+		}
+	}
+
+	return nil
+}
