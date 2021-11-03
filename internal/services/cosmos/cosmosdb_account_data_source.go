0100e351fc2a02533af4fa1798ad7dfa017fb239
Theo van Kraay <thvankra@microsoft.com>
2021-11-03 23:36:43 +0000

    committ cassandra mi terraform

--- internal/services/cosmos/cosmosdb_account_data_source.go
+++ internal/services/cosmos/cosmosdb_account_data_source.go
@@ -6,7 +6,7 @@ import (
 	"sort"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/services/cosmos-db/mgmt/2021-06-15/documentdb"
+	"github.com/Azure/azure-sdk-for-go/services/cosmos-db/mgmt/2021-10-15/documentdb"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/cosmos/common"
--- internal/services/cosmos/registration.go
+++ internal/services/cosmos/registration.go
@@ -30,6 +30,8 @@ func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
 func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
 	return map[string]*pluginsdk.Resource{
 		"azurerm_cosmosdb_account":              resourceCosmosDbAccount(),
+		"azurerm_cosmosdb_cassandra_cluster":    resourceCassandraCluster(),
+		"azurerm_cosmosdb_cassandra_datacenter": resourceCassandraDatacenter(),
 		"azurerm_cosmosdb_cassandra_keyspace":   resourceCosmosDbCassandraKeyspace(),
 		"azurerm_cosmosdb_cassandra_table":      resourceCosmosDbCassandraTable(),
 		"azurerm_cosmosdb_gremlin_database":     resourceCosmosGremlinDatabase(),
