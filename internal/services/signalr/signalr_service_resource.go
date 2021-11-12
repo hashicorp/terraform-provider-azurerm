42d991293f3e75dc601d8be7d07c9bd15e0d49a6
tombuildsstuff <git@ibuildstuff.co.uk>
2021-11-12 12:29:11 +0100

    r/signalr_service: adding a state migration for the old/new ID format

--- internal/services/signalr/signalr_service_resource.go
+++ internal/services/signalr/signalr_service_resource.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/signalr/migration"
+
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
@@ -33,6 +35,11 @@ func resourceArmSignalRService() *pluginsdk.Resource {
 			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
 			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
 		},
+
+		StateUpgraders: pluginsdk.StateUpgrades(map[int]pluginsdk.StateUpgrade{
+			0: migration.ServiceV0ToV1{},
+		}),
+		SchemaVersion: 1,
 
 		Importer: pluginsdk.ImporterValidatingResourceId(func(id string) error {
 			_, err := signalr.ParseSignalRID(id)
