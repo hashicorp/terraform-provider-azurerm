89b85e00da493f871c13cf46077be1207a7decab
xuzhang3 <Zhangxu894765>
2021-11-16 15:59:54 +0800

    new resource azurerm_api_management_notification_recipient_email

---
+++ internal/services/apimanagement/api_management_notification_recipient_user_test.go
@@ -0,0 +1,136 @@
+package apimanagement_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/services/apimanagement/mgmt/2020-12-01/apimanagement"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/apimanagement/parse"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+)
+
+type ApiManagementNotificationRecipientUserResource struct{}
+
+//var config = `
+//data "azurerm_api_management" "example" {
+//  name                = "xz3-apim"
+//  resource_group_name = "xz3-apimanagement-test"
+//}
+//
+////resource "azurerm_api_management_notification_recipient_email" "example" {
+////  api_management_id = data.azurerm_api_management.example.id
+////  notification_type = "AccountClosedPublisher"
+////  email           = "foo@bar.com"
+////}
+//
+//resource "azurerm_api_management_notification_recipient_user" "example" {
+// api_management_id = data.azurerm_api_management.example.id
+// notification_type = "AccountClosedPublisher"
+// user_id           = "test-test-com"
+//}
+//`
+//
+//func TestAccApiManagementNotificationRecipientUser_DD(t *testing.T) {
+//	data := acceptance.BuildTestData(t, "azurerm_api_management_notification_recipient_user", "test")
+//	r := ApiManagementNotificationRecipientUserResource{}
+//
+//	data.ResourceTest(t, r, []acceptance.TestStep{
+//		{
+//			Config: config,
+//			Check: acceptance.ComposeTestCheckFunc(
+//				check.That(data.ResourceName).ExistsInAzure(r),
+//			),
+//		},
+//	})
+//}
+
+func TestAccApiManagementNotificationRecipientUser_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_api_management_notification_recipient_user", "test")
+	r := ApiManagementNotificationRecipientUserResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccApiManagementNotificationRecipientUser_requiresImport(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_api_management_notification_recipient_user", "test")
+	r := ApiManagementNotificationRecipientUserResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.RequiresImportErrorStep(r.requiresImport),
+	})
+}
+
+func (ApiManagementNotificationRecipientUserResource) Exists(ctx context.Context, client *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
+	id, err := parse.NotificationRecipientUserID(state.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	users, err := client.ApiManagement.NotificationRecipientUserClient.ListByNotification(ctx, id.ResourceGroup, id.ServiceName, apimanagement.NotificationName(id.NotificationName))
+	if err != nil {
+		if !utils.ResponseWasNotFound(users.Response) {
+			return nil, fmt.Errorf("retrieving Api Management Notification Recipient User %q (Resource Group %q): %+v", id.RecipientUserName, id.ResourceGroup, err)
+		}
+	}
+	if users.Value != nil {
+		for _, existing := range *users.Value {
+			if existing.RecipientUsersContractProperties != nil && existing.RecipientUsersContractProperties.UserID != nil && *existing.RecipientUsersContractProperties.UserID == id.RecipientUserName {
+				return utils.Bool(true), nil
+			}
+		}
+	}
+	return utils.Bool(false), nil
+}
+
+func (r ApiManagementNotificationRecipientUserResource) basic(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_api_management_user" "example" {
+  user_id             = "test_user"
+  api_management_name = azurerm_api_management.test.name
+  resource_group_name = azurerm_resource_group.test.name
+  first_name          = "Example"
+  last_name           = "User"
+  email               = "foo@bar.com"
+  state               = "active"
+}
+
+resource "azurerm_api_management_notification_recipient_user" "test" {
+  api_management_id = azurerm_api_management.test.id
+  notification_type = "AccountClosedPublisher"
+  user_id           = azurerm_api_management_user.example.user_id
+}
+`, ApiManagementResource{}.basic(data))
+}
+
+func (r ApiManagementNotificationRecipientUserResource) requiresImport(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_api_manageme_notification_recipient_user" "import" {
+  api_management_id = azurerm_api_management.test.id
+  notification_type = azurerm_api_management_notification_recipient_user.test.notification_type
+  user_id           = azurerm_api_management_notification_recipient_user.test.user_id
+}
+`, r.basic(data))
+}
---
+++ internal/services/apimanagement/parse/notification_recipient_user_test.go
@@ -0,0 +1,144 @@
+package parse
+
+// NOTE: this file is generated via 'go:generate' - manual changes will be overwritten
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-provider-azurerm/internal/resourceid"
+)
+
+var _ resourceid.Formatter = NotificationRecipientUserId{}
+
+func TestNotificationRecipientUserIDFormatter(t *testing.T) {
+	actual := NewNotificationRecipientUserID("12345678-1234-9876-4563-123456789012", "resGroup1", "apimservice1", "notificationName1", "userid1").ID()
+	expected := "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/apimservice1/notifications/notificationName1/recipientUsers/userid1"
+	if actual != expected {
+		t.Fatalf("Expected %q but got %q", expected, actual)
+	}
+}
+
+func TestNotificationRecipientUserID(t *testing.T) {
+	testData := []struct {
+		Input    string
+		Error    bool
+		Expected *NotificationRecipientUserId
+	}{
+
+		{
+			// empty
+			Input: "",
+			Error: true,
+		},
+
+		{
+			// missing SubscriptionId
+			Input: "/",
+			Error: true,
+		},
+
+		{
+			// missing value for SubscriptionId
+			Input: "/subscriptions/",
+			Error: true,
+		},
+
+		{
+			// missing ResourceGroup
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/",
+			Error: true,
+		},
+
+		{
+			// missing value for ResourceGroup
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/",
+			Error: true,
+		},
+
+		{
+			// missing ServiceName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/",
+			Error: true,
+		},
+
+		{
+			// missing value for ServiceName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/",
+			Error: true,
+		},
+
+		{
+			// missing NotificationName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/apimservice1/",
+			Error: true,
+		},
+
+		{
+			// missing value for NotificationName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/apimservice1/notifications/",
+			Error: true,
+		},
+
+		{
+			// missing RecipientUserName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/apimservice1/notifications/notificationName1/",
+			Error: true,
+		},
+
+		{
+			// missing value for RecipientUserName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/apimservice1/notifications/notificationName1/recipientUsers/",
+			Error: true,
+		},
+
+		{
+			// valid
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.ApiManagement/service/apimservice1/notifications/notificationName1/recipientUsers/userid1",
+			Expected: &NotificationRecipientUserId{
+				SubscriptionId:    "12345678-1234-9876-4563-123456789012",
+				ResourceGroup:     "resGroup1",
+				ServiceName:       "apimservice1",
+				NotificationName:  "notificationName1",
+				RecipientUserName: "userid1",
+			},
+		},
+
+		{
+			// upper-cased
+			Input: "/SUBSCRIPTIONS/12345678-1234-9876-4563-123456789012/RESOURCEGROUPS/RESGROUP1/PROVIDERS/MICROSOFT.APIMANAGEMENT/SERVICE/APIMSERVICE1/NOTIFICATIONS/NOTIFICATIONNAME1/RECIPIENTUSERS/USERID1",
+			Error: true,
+		},
+	}
+
+	for _, v := range testData {
+		t.Logf("[DEBUG] Testing %q", v.Input)
+
+		actual, err := NotificationRecipientUserID(v.Input)
+		if err != nil {
+			if v.Error {
+				continue
+			}
+
+			t.Fatalf("Expect a value but got an error: %s", err)
+		}
+		if v.Error {
+			t.Fatal("Expect an error but didn't get one")
+		}
+
+		if actual.SubscriptionId != v.Expected.SubscriptionId {
+			t.Fatalf("Expected %q but got %q for SubscriptionId", v.Expected.SubscriptionId, actual.SubscriptionId)
+		}
+		if actual.ResourceGroup != v.Expected.ResourceGroup {
+			t.Fatalf("Expected %q but got %q for ResourceGroup", v.Expected.ResourceGroup, actual.ResourceGroup)
+		}
+		if actual.ServiceName != v.Expected.ServiceName {
+			t.Fatalf("Expected %q but got %q for ServiceName", v.Expected.ServiceName, actual.ServiceName)
+		}
+		if actual.NotificationName != v.Expected.NotificationName {
+			t.Fatalf("Expected %q but got %q for NotificationName", v.Expected.NotificationName, actual.NotificationName)
+		}
+		if actual.RecipientUserName != v.Expected.RecipientUserName {
+			t.Fatalf("Expected %q but got %q for RecipientUserName", v.Expected.RecipientUserName, actual.RecipientUserName)
+		}
+	}
+}
