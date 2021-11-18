cceced8c3aa92ebb469aed6bf1caa1dbf506ac59
jackofallops <ste@hashicorp.com>
2021-11-18 15:40:41 +0000

    extend context timeouts for create and delete

--- internal/services/keyvault/key_vault_managed_hardware_security_module_resource..go
+++ internal/services/keyvault/key_vault_managed_hardware_security_module_resource..go
@@ -32,9 +32,9 @@ func resourceKeyVaultManagedHardwareSecurityModule() *pluginsdk.Resource {
 		}),
 
 		Timeouts: &pluginsdk.ResourceTimeout{
-			Create: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Create: pluginsdk.DefaultTimeout(60 * time.Minute),
 			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
-			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Delete: pluginsdk.DefaultTimeout(60 * time.Minute),
 		},
 
 		Schema: map[string]*pluginsdk.Schema{
