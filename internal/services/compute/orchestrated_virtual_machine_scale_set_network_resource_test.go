52a8934aa9ecf4fe0d2609c8a169b30c17595b85
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2021-11-22 15:11:31 -0700

    fix file extension for network tests

--- internal/services/compute/orchestrated_virtual_machine_scale_set_network_resource_test.go
+++ internal/services/compute/orchestrated_virtual_machine_scale_set_network_resource_test.go
@@ -1,11 +1,15 @@
 package compute_test
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2021-07-01/compute"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/compute/parse"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 )
 
