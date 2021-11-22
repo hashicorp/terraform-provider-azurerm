56aac3d76440133f0cfa714f0946a1ab92e45c7c
jackofallops <ste@hashicorp.com>
2021-11-22 07:04:00 +0000

    remove old todo

--- internal/services/appservice/service_plan_resource.go
+++ internal/services/appservice/service_plan_resource.go
@@ -83,7 +83,6 @@ func (r ServicePlanResource) Arguments() map[string]*pluginsdk.Schema {
 				"EP1", "EP2", "EP3", // Elastic Premium Plans - Function Apps
 				"WS1", "WS2", "WS3", // Workflow plans - Logic Apps
 			}, false),
-			// TODO - need to look at Isolated as separate property via ExactlyOneOf?
 		},
 
 		"os_type": {
