ec1dc70e0988ac98381a1e4d7fed404a852e9e3d
jackofallops <ste@hashicorp.com>
2021-11-24 10:18:56 +0000

    fix client_id ref in test

--- internal/services/appservice/windows_web_app_resource_test.go
+++ internal/services/appservice/windows_web_app_resource_test.go
@@ -2232,7 +2232,7 @@ resource "azurerm_windows_web_app" "test" {
 
   site_config {
     container_registry_use_managed_identity       = true
-    container_registry_managed_identity_client_id = azurerm_user_assigned_identity.test.id
+    container_registry_managed_identity_client_id = azurerm_user_assigned_identity.test.client_id
   }
 }
 `, r.baseTemplate(data), data.RandomInteger)
