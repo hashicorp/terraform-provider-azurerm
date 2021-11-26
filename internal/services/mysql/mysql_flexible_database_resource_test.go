08605e99d7912fe2b512d595c470aa550d92413c
aristosvo <8375124+aristosvo@users.noreply.github.com>
2021-11-26 17:55:16 +0100

    `azurerm_mysql_flexible_database` - New resource (#14285)
    Fixes #14248

---
+++ internal/services/mysql/mysql_flexible_database_resource_test.go
@@ -0,0 +1,197 @@
+package mysql_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/mysql/parse"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+)
+
+type MySQLFlexibleDatabaseResource struct {
+}
+
+func TestAccMySQLFlexibleDatabase_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_mysql_flexible_database", "test")
+	r := MySQLFlexibleDatabaseResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccMySQLFlexibleDatabase_requiresImport(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_mysql_flexible_database", "test")
+	r := MySQLFlexibleDatabaseResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		{
+			Config:      r.requiresImport(data),
+			ExpectError: acceptance.RequiresImportError("azurerm_mysql_flexible_database"),
+		},
+	})
+}
+
+func TestAccMySQLFlexibleDatabase_charsetUppercase(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_mysql_flexible_database", "test")
+	r := MySQLFlexibleDatabaseResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.charsetUppercase(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("charset").HasValue("utf8"),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccMySQLFlexibleDatabase_charsetMixedcase(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_mysql_flexible_database", "test")
+	r := MySQLFlexibleDatabaseResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.charsetMixedcase(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("charset").HasValue("utf8"),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func (r MySQLFlexibleDatabaseResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
+	id, err := parse.FlexibleDatabaseID(state.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := clients.MySQL.FlexibleDatabasesClient.Get(ctx, id.ResourceGroup, id.FlexibleServerName, id.DatabaseName)
+	if err != nil {
+		return nil, err
+	}
+
+	return utils.Bool(resp.ID != nil), nil
+}
+
+func (MySQLFlexibleDatabaseResource) basic(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+resource "azurerm_mysql_flexible_server" "test" {
+  name                   = "acctest-fs-%d"
+  resource_group_name    = azurerm_resource_group.test.name
+  location               = azurerm_resource_group.test.location
+  administrator_login    = "adminTerraform"
+  administrator_password = "QAZwsx123"
+  sku_name               = "B_Standard_B1s"
+}
+
+resource "azurerm_mysql_flexible_database" "test" {
+  name                = "acctestdb_%d"
+  resource_group_name = azurerm_resource_group.test.name
+  server_name         = azurerm_mysql_flexible_server.test.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
+}
+
+func (r MySQLFlexibleDatabaseResource) requiresImport(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_mysql_flexible_database" "import" {
+  name                = azurerm_mysql_flexible_database.test.name
+  resource_group_name = azurerm_mysql_flexible_database.test.resource_group_name
+  server_name         = azurerm_mysql_flexible_database.test.server_name
+  charset             = azurerm_mysql_flexible_database.test.charset
+  collation           = azurerm_mysql_flexible_database.test.collation
+}
+`, r.basic(data))
+}
+
+func (MySQLFlexibleDatabaseResource) charsetUppercase(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+resource "azurerm_mysql_flexible_server" "test" {
+  name                   = "acctest-fs-%d"
+  resource_group_name    = azurerm_resource_group.test.name
+  location               = azurerm_resource_group.test.location
+  administrator_login    = "adminTerraform"
+  administrator_password = "QAZwsx123"
+  sku_name               = "B_Standard_B1s"
+}
+
+resource "azurerm_mysql_flexible_database" "test" {
+  name                = "acctestdb_%d"
+  resource_group_name = azurerm_resource_group.test.name
+  server_name         = azurerm_mysql_flexible_server.test.name
+  charset             = "UTF8"
+  collation           = "utf8_unicode_ci"
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
+}
+
+func (MySQLFlexibleDatabaseResource) charsetMixedcase(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+resource "azurerm_mysql_flexible_server" "test" {
+  name                   = "acctest-fs-%d"
+  resource_group_name    = azurerm_resource_group.test.name
+  location               = azurerm_resource_group.test.location
+  administrator_login    = "adminTerraform"
+  administrator_password = "QAZwsx123"
+  sku_name               = "B_Standard_B1s"
+}
+
+resource "azurerm_mysql_flexible_database" "test" {
+  name                = "acctestdb_%d"
+  resource_group_name = azurerm_resource_group.test.name
+  server_name         = azurerm_mysql_flexible_server.test.name
+  charset             = "Utf8"
+  collation           = "utf8_unicode_ci"
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
+}
