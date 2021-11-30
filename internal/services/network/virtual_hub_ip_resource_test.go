1e0148f0226e1e110763b92384660a2f120064ae
Aris van Ommeren <avommeren@vxcompany.com>
2021-12-01 00:05:59 +0100

    `azurerm_virtual_hub_ip` - Fix tests

--- internal/services/network/virtual_hub_ip_resource_test.go
+++ internal/services/network/virtual_hub_ip_resource_test.go
@@ -162,6 +162,7 @@ resource "azurerm_public_ip" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   allocation_method   = "Dynamic"
+  sku                 = "Standard"
 }
 
 resource "azurerm_virtual_network" "test" {
