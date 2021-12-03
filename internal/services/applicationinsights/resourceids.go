4d4f2d490f5b89594d9079467865b41bb2fdfd8f
tombuildsstuff <git@ibuildstuff.co.uk>
2021-12-03 12:23:17 -0500

    applicationinsights: refactoring SmartDetectionRule to use a case insensitive parser

--- internal/services/applicationinsights/resourceids.go
+++ internal/services/applicationinsights/resourceids.go
@@ -1,7 +1,7 @@
 package applicationinsights
 
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Component -rewrite=true -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/group1/providers/Microsoft.Insights/components/component1
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=SmartDetectionRule -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/group1/providers/Microsoft.Insights/components/component1/smartDetectionRule/rule1
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=SmartDetectionRule -rewrite=true -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/group1/providers/Microsoft.Insights/components/component1/smartDetectionRule/rule1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=WebTest -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/group1/providers/Microsoft.Insights/webTests/test1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ApiKey -rewrite=true -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/group1/providers/Microsoft.Insights/components/component1/apiKeys/apikey1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=AnalyticsUserItem -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/group1/providers/Microsoft.Insights/components/component1/myAnalyticsItems/item1
