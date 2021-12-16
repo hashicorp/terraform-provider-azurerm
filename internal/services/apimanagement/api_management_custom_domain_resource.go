c6c099767b1b521d252f2bd63ab711334aa06af9
Steph <steph@hashicorp.com>
2021-12-16 15:07:53 +0100

    increase timeouts

--- internal/services/apimanagement/api_management_custom_domain_resource.go
+++ internal/services/apimanagement/api_management_custom_domain_resource.go
@@ -32,10 +32,10 @@ func resourceApiManagementCustomDomain() *pluginsdk.Resource {
 		}),
 
 		Timeouts: &pluginsdk.ResourceTimeout{
-			Create: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Create: pluginsdk.DefaultTimeout(45 * time.Minute),
 			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
-			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
-			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Update: pluginsdk.DefaultTimeout(45 * time.Minute),
+			Delete: pluginsdk.DefaultTimeout(45 * time.Minute),
 		},
 
 		Schema: map[string]*pluginsdk.Schema{
