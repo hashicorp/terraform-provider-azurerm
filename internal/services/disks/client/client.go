22a5f9862fcdf83b0fae7c4707dc1b84ae623a34
tombuildsstuff <git@tom.ibuildstuff.eu>
2021-12-20 13:50:24 +0100

    disks: registering the client

--- internal/services/disks/client/client.go
+++ internal/services/disks/client/client.go
@@ -2,11 +2,18 @@ package client
 
 import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/disks/sdk/2021-08-01/diskpools"
 )
 
 type Client struct {
+	DiskPoolsClient *diskpools.DiskPoolsClient
 }
 
 func NewClient(o *common.ClientOptions) *Client {
-	return &Client{}
+	diskPoolsClient := diskpools.NewDiskPoolsClientWithBaseURI(o.ResourceManagerEndpoint)
+	o.ConfigureClient(&diskPoolsClient.Client, o.ResourceManagerAuthorizer)
+
+	return &Client{
+		DiskPoolsClient: &diskPoolsClient,
+	}
 }
