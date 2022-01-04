bdd4e8e706968b2cae9dfe876c2b5ba46c01dece
Vladimir Lazarenko <favoretti@gmail.com>
2022-01-04 15:36:45 +0100

    make generate

--- internal/services/containers/parse/container_registry_agent_pool_test.go
+++ internal/services/containers/parse/container_registry_agent_pool_test.go
@@ -5,10 +5,10 @@ package parse
 import (
 	"testing"
 
-	"github.com/hashicorp/terraform-provider-azurerm/internal/resourceid"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/resourceids"
 )
 
-var _ resourceid.Formatter = ContainerRegistryAgentPoolId{}
+var _ resourceids.Id = ContainerRegistryAgentPoolId{}
 
 func TestContainerRegistryAgentPoolIDFormatter(t *testing.T) {
 	actual := NewContainerRegistryAgentPoolID("12345678-1234-9876-4563-123456789012", "resGroup1", "registry1", "agent_pool1").ID()
