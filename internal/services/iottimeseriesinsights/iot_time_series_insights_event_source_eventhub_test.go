8efe2032c67cd98e2b889a7ba133b53a7860b007
heiazuo <46072066+heiazuo@users.noreply.github.com>
2022-01-05 22:26:32 +0800

    Test time series insights (#14782)

--- internal/services/iottimeseriesinsights/iot_time_series_insights_event_source_eventhub_test.go
+++ internal/services/iottimeseriesinsights/iot_time_series_insights_event_source_eventhub_test.go
@@ -59,6 +59,21 @@ func TestAccIoTTimeSeriesInsightsEventSourceEventhub_update(t *testing.T) {
 	})
 }
 
+func TestAccIoTTimeSeriesInsightsEventSourceEventhub_complete(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_iot_time_series_insights_event_source_eventhub", "test")
+	r := IoTTimeSeriesInsightsEventSourceEventhubResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.complete(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("shared_access_key"),
+	})
+}
+
 func (IoTTimeSeriesInsightsEventSourceEventhubResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	id, err := parse.EventSourceID(state.ID)
 	if err != nil {
@@ -228,4 +243,85 @@ resource "azurerm_iot_time_series_insights_event_source_eventhub" "test" {
   timestamp_property_name  = "test"
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger, data.RandomInteger, data.RandomInteger, data.RandomString, data.RandomInteger, data.RandomInteger)
+}
+
+func (IoTTimeSeriesInsightsEventSourceEventhubResource) complete(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-tsi-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_eventhub_namespace" "test" {
+  name                = "acctesteventhubnamespace-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "Standard"
+}
+
+resource "azurerm_eventhub" "test" {
+  name                = "acctesteventhub-%[1]d"
+  namespace_name      = azurerm_eventhub_namespace.test.name
+  resource_group_name = azurerm_resource_group.test.name
+  partition_count     = 2
+  message_retention   = 7
+}
+
+resource "azurerm_eventhub_consumer_group" "test" {
+  name                = "acctesteventhubcg-%[1]d"
+  namespace_name      = azurerm_eventhub_namespace.test.name
+  eventhub_name       = azurerm_eventhub.test.name
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_eventhub_authorization_rule" "test" {
+  name                = "acctest-%[1]d"
+  namespace_name      = azurerm_eventhub_namespace.test.name
+  eventhub_name       = azurerm_eventhub.test.name
+  resource_group_name = azurerm_resource_group.test.name
+
+  listen = true
+  send   = false
+  manage = false
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "acctestsatsi%[3]s"
+  location                 = azurerm_resource_group.test.location
+  resource_group_name      = azurerm_resource_group.test.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_iot_time_series_insights_gen2_environment" "test" {
+  name                = "acctest_tsie%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku_name            = "L1"
+  id_properties       = ["id"]
+
+  storage {
+    name = azurerm_storage_account.storage.name
+    key  = azurerm_storage_account.storage.primary_access_key
+  }
+}
+
+resource "azurerm_iot_time_series_insights_event_source_eventhub" "test" {
+  name                     = "acctest_tsiesi%[1]d"
+  location                 = azurerm_resource_group.test.location
+  environment_id           = azurerm_iot_time_series_insights_gen2_environment.test.id
+  eventhub_name            = azurerm_eventhub.test.name
+  namespace_name           = azurerm_eventhub_namespace.test.name
+  shared_access_key        = azurerm_eventhub_authorization_rule.test.primary_key
+  shared_access_key_name   = azurerm_eventhub_authorization_rule.test.name
+  consumer_group_name      = azurerm_eventhub_consumer_group.test.name
+  event_source_resource_id = azurerm_eventhub.test.id
+  tags = {
+    ENV = "Test"
+  }
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomString)
 }
