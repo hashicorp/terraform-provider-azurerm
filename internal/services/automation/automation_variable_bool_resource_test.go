9fc3eabb55b1edc2a87aabb3a5a0e780e0409fd3
yingshu <v-yingshu@microsoft.com>
2022-01-07 15:30:43 +0800

    add test for azurerm_automation_variable_bool.encrypted

--- internal/services/automation/automation_variable_bool_resource_test.go
+++ internal/services/automation/automation_variable_bool_resource_test.go
@@ -76,6 +76,20 @@ func TestAccAutomationVariableBool_basicCompleteUpdate(t *testing.T) {
 		},
 	})
 }
+func TestAccAutomationVariableBool_encrypted(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_automation_variable_bool", "test")
+	r := AutomationVariableBoolResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.encrypted(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("value"),
+	})
+}
 
 func (t AutomationVariableBoolResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	return testCheckAzureRMAutomationVariableExists(ctx, clients, state, "Bool")
@@ -132,6 +146,33 @@ resource "azurerm_automation_variable_bool" "test" {
   automation_account_name = azurerm_automation_account.test.name
   description             = "This variable is created by Terraform acceptance test."
   value                   = true
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
+}
+func (AutomationVariableBoolResource) encrypted(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-auto-%d"
+  location = "%s"
+}
+
+resource "azurerm_automation_account" "test" {
+  name                = "acctestAutoAcct-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku_name            = "Basic"
+}
+
+resource "azurerm_automation_variable_bool" "test" {
+  name                    = "acctestAutoVar-%d"
+  resource_group_name     = azurerm_resource_group.test.name
+  automation_account_name = azurerm_automation_account.test.name
+  value                   = false
+  encrypted               = true
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
 }
