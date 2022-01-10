37c58d441eff12f4c0e9cefcbcd4e2aad3ab6321
yingshu <v-yingshu@microsoft.com>
2022-01-10 14:28:47 +0800

    update azurerm_automation_dsc_configuration.log_verbose

--- internal/services/automation/automation_dsc_configuration_resource_test.go
+++ internal/services/automation/automation_dsc_configuration_resource_test.go
@@ -60,7 +60,6 @@ func TestAccAutomationDscConfiguration_complete(t *testing.T) {
 			Config: r.complete(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
-				check.That(data.ResourceName).Key("location").Exists(),
 			),
 		},
 		data.ImportStep(),
