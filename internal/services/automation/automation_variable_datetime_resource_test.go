b6ea020e09cafd0436ec0c8fbd8318adf5fc1be9
yingshu <v-yingshu@microsoft.com>
2022-01-10 10:38:20 +0800

    add test for azurerm_automation_variable_datetime.encrypted

--- internal/services/automation/automation_variable_datetime_resource_test.go
+++ internal/services/automation/automation_variable_datetime_resource_test.go
@@ -77,6 +77,22 @@ func TestAccAutomationVariableDateTime_basicCompleteUpdate(t *testing.T) {
 	})
 }
 
+func TestAccAutomationVariableDateTime_encrypted(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_automation_variable_datetime", "test")
+	r := AutomationVariableDateTimeResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.encrypted(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("value").HasValue("2019-04-24T21:40:54.074Z"),
+			),
+		},
+		data.ImportStep("value"),
+	})
+}
+
 func (t AutomationVariableDateTimeResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	return testCheckAzureRMAutomationVariableExists(ctx, clients, state, "Datetime")
 }
@@ -132,6 +148,34 @@ resource "azurerm_automation_variable_datetime" "test" {
   automation_account_name = azurerm_automation_account.test.name
   description             = "This variable is created by Terraform acceptance test."
   value                   = "2019-04-20T08:40:04.02Z"
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
+}
+
+func (AutomationVariableDateTimeResource) encrypted(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-auto-%d"
+  location = "%s"
+}
+
+resource "azurerm_automation_account" "test" {
+  name                = "acctestAutoAcct-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku_name            = "Basic"
+}
+
+resource "azurerm_automation_variable_datetime" "test" {
+  name                    = "acctestAutoVar-%d"
+  resource_group_name     = azurerm_resource_group.test.name
+  automation_account_name = azurerm_automation_account.test.name
+  value                   = "2019-04-24T21:40:54.074Z"
+  encrypted               = true
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
 }
