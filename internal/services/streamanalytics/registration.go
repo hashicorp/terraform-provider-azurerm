3e84cb03407bdf27edcf5f1d88e3de1916ed03c4
Steph <steph@hashicorp.com>
2022-01-12 16:13:38 +0100

    output functions typed resource

--- internal/services/streamanalytics/registration.go
+++ internal/services/streamanalytics/registration.go
@@ -20,6 +20,7 @@ func (r Registration) DataSources() []sdk.DataSource {
 
 func (r Registration) Resources() []sdk.Resource {
 	return []sdk.Resource{
+		OutputFunctionResource{},
 		OutputTableResource{},
 		ClusterResource{},
 		ManagedPrivateEndpointResource{},
