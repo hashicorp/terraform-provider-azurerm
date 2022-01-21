a5c4042aece80c232123a18160d307d58f3fd33b
yingshu <v-yingshu@microsoft.com>
2022-01-21 15:42:04 +0800

    add test for timezone

--- internal/services/recoveryservices/backup_policy_file_share_resource_test.go
+++ internal/services/recoveryservices/backup_policy_file_share_resource_test.go
@@ -587,6 +587,7 @@ resource "azurerm_backup_policy_file_share" "test" {
   resource_group_name = azurerm_resource_group.test.name
   recovery_vault_name = azurerm_recovery_services_vault.test.name
 
+  timezone = "UTC"
   backup {
     frequency = "Daily"
     time      = "23:00"
