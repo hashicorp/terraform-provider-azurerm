ac509ff63106ff17df49e133197ad0bf0e48b8eb
yingshu <v-yingshu@microsoft.com>
2022-01-21 10:08:14 +0800

    updated fmt

--- internal/services/mysql/mysql_server_resource_test.go
+++ internal/services/mysql/mysql_server_resource_test.go
@@ -308,21 +308,21 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_mysql_server" "test" {
-  name                              = "acctestmysqlsvr-%[1]d"
-  location                          = azurerm_resource_group.test.location
-  resource_group_name               = azurerm_resource_group.test.name
-  sku_name                          = "GP_Gen5_2"
-  administrator_login               = "acctestun"
-  administrator_login_password      = "H@Sh1CoR3!"
-  auto_grow_enabled                 = true
-  backup_retention_days             = 7
-  create_mode                       = "Default"
-  geo_redundant_backup_enabled      = false
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
-  storage_mb                        = 51200
-  version                           = "%[3]s"
+  name                             = "acctestmysqlsvr-%[1]d"
+  location                         = azurerm_resource_group.test.location
+  resource_group_name              = azurerm_resource_group.test.name
+  sku_name                         = "GP_Gen5_2"
+  administrator_login              = "acctestun"
+  administrator_login_password     = "H@Sh1CoR3!"
+  auto_grow_enabled                = true
+  backup_retention_days            = 7
+  create_mode                      = "Default"
+  geo_redundant_backup_enabled     = false
+  public_network_access_enabled    = true
+  ssl_enforcement_enabled          = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
+  storage_mb                       = 51200
+  version                          = "%[3]s"
   threat_detection_policy {
     enabled              = true
     disabled_alerts      = ["Sql_Injection", "Data_Exfiltration"]
