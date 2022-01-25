ccfba8089879030e68f8da5a769633275e859ff2
catriona-m <86247157+catriona-m@users.noreply.github.com>
2022-01-25 12:01:22 +0000

    `azurerm_eventhub` - seperate update and create and add check for sku (#15088)
    * split update and create of eventhub and check ns sku
    
    * simplify parameters

--- internal/services/eventhub/eventhub_resource_test.go
+++ internal/services/eventhub/eventhub_resource_test.go
@@ -415,7 +415,8 @@ resource "azurerm_eventhub_namespace" "test" {
   name                = "acctesteventhubnamespace-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  sku                 = "Basic"
+  sku                 = "Premium"
+  zone_redundant      = true
 }
 
 resource "azurerm_eventhub" "test" {
