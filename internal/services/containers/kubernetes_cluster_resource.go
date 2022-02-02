42c295897ccce8d054491e809ad0017296bcda9e
Steph <steph@hashicorp.com>
2022-02-02 18:27:47 +0100

    use enum for skuTier for #15229

--- internal/services/containers/kubernetes_cluster_resource.go
+++ internal/services/containers/kubernetes_cluster_resource.go
@@ -1638,11 +1638,11 @@ func resourceKubernetesClusterUpdate(d *pluginsdk.ResourceData, meta interface{}
 			}
 		}
 
-		skuTier := "Free"
+		skuTier := containerservice.ManagedClusterSKUTierFree
 		if v := d.Get("sku_tier").(string); v != "" {
-			skuTier = v
+			skuTier = containerservice.ManagedClusterSKUTier(v)
 		}
-		existing.Sku.Tier = containerservice.ManagedClusterSKUTier(skuTier)
+		existing.Sku.Tier = skuTier
 	}
 
 	if d.HasChange("automatic_channel_upgrade") {
