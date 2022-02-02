97fe7b19affec65af2ed81c1b1a3d8609ee0f22c
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-02-02 10:52:12 +0100

    goimports

--- internal/services/eventhub/eventhub_namespace_resource.go
+++ internal/services/eventhub/eventhub_namespace_resource.go
@@ -3,7 +3,6 @@ package eventhub
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/go-azure-helpers/resourcemanager/identity"
 	"log"
 	"strconv"
 	"strings"
@@ -11,6 +10,7 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/identity"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/tags"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
