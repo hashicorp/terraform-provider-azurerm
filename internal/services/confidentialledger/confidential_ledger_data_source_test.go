6f9495d3e4dfd97c8f1c3568f005b347ef9bc493
Lyndon Shi <lyshi@microsoft.com>
2022-02-09 16:57:02 -0800

    data_source test

--- internal/services/confidentialledger/confidential_ledger_data_source_test.go
+++ internal/services/confidentialledger/confidential_ledger_data_source_test.go
@@ -15,34 +15,35 @@ func TestAccConfidentialLedgerDataSource_basic(t *testing.T) {
 
 	data.DataSourceTest(t, []acceptance.TestStep{
 		{
-			Config: ConfidentialLedgerResource{}.standard(data),
+			Config: ConfidentialLedgerResource{}.combinedServicePrincipals(data),
 		},
 		{
-			Config: ConfidentialLedgerDataSource{}.basic(data),
+			Config: ConfidentialLedgerDataSource{}.combinedServicePrincipals(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(ConfidentialLedgerResource{}),
-				check.That(data.ResourceName).Key("endpoint").Exists(),
+				check.That(data.ResourceName).Key("aad_based_security_principals.0.principal_id").Exists(),
+				check.That(data.ResourceName).Key("aad_based_security_principals.0.tenant_id").Exists(),
+				check.That(data.ResourceName).Key("aad_based_security_principals.0.ledger_role_name").Exists(),
+				check.That(data.ResourceName).Key("aad_based_security_principals.1.principal_id").Exists(),
+				check.That(data.ResourceName).Key("aad_based_security_principals.1.tenant_id").Exists(),
+				check.That(data.ResourceName).Key("aad_based_security_principals.1.ledger_role_name").Exists(),
+				check.That(data.ResourceName).Key("aad_based_security_principals.2.principal_id").Exists(),
+				check.That(data.ResourceName).Key("aad_based_security_principals.2.tenant_id").Exists(),
+				check.That(data.ResourceName).Key("aad_based_security_principals.2.ledger_role_name").Exists(),
+				check.That(data.ResourceName).Key("cert_based_security_principals.0.cert").Exists(),
+				check.That(data.ResourceName).Key("cert_based_security_principals.0.ledger_role_name").Exists(),
+				check.That(data.ResourceName).Key("name").Exists(),
+				check.That(data.ResourceName).Key("ledger_type").Exists(),
 				check.That(data.ResourceName).Key("location").Exists(),
-				check.That(data.ResourceName).Key("sku").Exists(),
-				check.That(data.ResourceName).Key("primary_read_key.0.connection_string").Exists(),
-				check.That(data.ResourceName).Key("primary_read_key.0.id").Exists(),
-				check.That(data.ResourceName).Key("primary_read_key.0.secret").Exists(),
-				check.That(data.ResourceName).Key("primary_write_key.0.connection_string").Exists(),
-				check.That(data.ResourceName).Key("primary_write_key.0.id").Exists(),
-				check.That(data.ResourceName).Key("primary_write_key.0.secret").Exists(),
-				check.That(data.ResourceName).Key("secondary_read_key.0.connection_string").Exists(),
-				check.That(data.ResourceName).Key("secondary_read_key.0.id").Exists(),
-				check.That(data.ResourceName).Key("secondary_read_key.0.secret").Exists(),
-				check.That(data.ResourceName).Key("secondary_write_key.0.connection_string").Exists(),
-				check.That(data.ResourceName).Key("secondary_write_key.0.id").Exists(),
-				check.That(data.ResourceName).Key("secondary_write_key.0.secret").Exists(),
+				check.That(data.ResourceName).Key("resource_group_name").Exists(),
+				check.That(data.ResourceName).Key("tags").Exists(),
 			),
 		},
 	})
 }
 
-func (ConfidentialLedgerDataSource) basic(data acceptance.TestData) string {
-	template := ConfidentialLedgerResource{}.standard(data)
+func (ConfidentialLedgerDataSource) combinedServicePrincipals(data acceptance.TestData) string {
+	template := ConfidentialLedgerResource{}.combinedServicePrincipals(data)
 	return fmt.Sprintf(`
 %s
 
