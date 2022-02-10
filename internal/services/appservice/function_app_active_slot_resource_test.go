c32eb2c0661f296be6e9d03dad7d335afead5072
jackofallops <ste@hashicorp.com>
2022-02-10 14:43:38 +0000

    switch tests to containerised functions for deployment funtimes

--- internal/services/appservice/function_app_active_slot_resource_test.go
+++ internal/services/appservice/function_app_active_slot_resource_test.go
@@ -217,7 +217,15 @@ resource "azurerm_linux_function_app" "test" {
   storage_account_name       = azurerm_storage_account.test.name
   storage_account_access_key = azurerm_storage_account.test.primary_access_key
 
-  site_config {}
+  site_config {
+    application_stack {
+      docker {
+        registry_url = "https://mcr.microsoft.com"
+        image_name   = "azure-functions/python"
+        image_tag    = "latest"
+      }
+    }
+  }
 }
 
 resource "azurerm_linux_function_app_slot" "test" {
@@ -226,7 +234,15 @@ resource "azurerm_linux_function_app_slot" "test" {
   storage_account_name       = azurerm_storage_account.test.name
   storage_account_access_key = azurerm_storage_account.test.primary_access_key
 
-  site_config {}
+  site_config {
+    application_stack {
+      docker {
+        registry_url = "https://mcr.microsoft.com"
+        image_name   = "azure-functions/python"
+        image_tag    = "latest"
+      }
+    }
+  }
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomString)
 }
