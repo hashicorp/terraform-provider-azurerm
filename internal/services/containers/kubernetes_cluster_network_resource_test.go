0eafe5ba81683a9c69d7957946df1e82d4159608
Steph <steph@hashicorp.com>
2022-02-14 12:40:26 +0100

    fix deprecation

--- internal/services/containers/kubernetes_cluster_network_resource_test.go
+++ internal/services/containers/kubernetes_cluster_network_resource_test.go
@@ -1759,6 +1759,7 @@ resource "azurerm_kubernetes_cluster" "test" {
     network_plugin    = "azure"
     load_balancer_sku = "Standard"
   }
+  run_command_enabled = false
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger, data.RandomInteger, data.RandomInteger, currentKubernetesVersion, data.RandomInteger)
 }
