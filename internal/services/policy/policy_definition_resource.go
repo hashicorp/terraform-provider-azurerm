1ccdb0ab7660d2371fa04d1071326464d9286bd9
Kyriakos Oikonomakos <kyriakos@hashicorp.com>
2022-02-14 22:49:07 +0000

    goimports

--- internal/services/policy/policy_definition_resource.go
+++ internal/services/policy/policy_definition_resource.go
@@ -13,6 +13,7 @@ import (
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/policy/parse"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
