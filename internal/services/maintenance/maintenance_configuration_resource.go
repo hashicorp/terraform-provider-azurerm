1bff3fbaaac46f60f0fee408fa9863e40ea4880a
Kyriakos Oikonomakos <kyriakos@hashicorp.com>
2022-02-15 18:14:21 +0000

    [3.0 Beta] `resource_group_name` for `maintenance_configuration_resource` is now case sensitive

--- internal/services/maintenance/maintenance_configuration_resource.go
+++ internal/services/maintenance/maintenance_configuration_resource.go
@@ -6,6 +6,8 @@ import (
 	"regexp"
 	"time"
 
+	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
+
 	"github.com/Azure/azure-sdk-for-go/services/maintenance/mgmt/2021-05-01/maintenance"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
@@ -55,10 +57,14 @@ func resourceArmMaintenanceConfiguration() *pluginsdk.Resource {
 
 			"location": azure.SchemaLocation(),
 
-			// TODO use `azure.SchemaResourceGroupName()` in version 3.0
 			// There's a bug in the Azure API where this is returned in lower-case
 			// BUG: https://github.com/Azure/azure-rest-api-specs/issues/8653
-			"resource_group_name": azure.SchemaResourceGroupNameDiffSuppress(),
+			"resource_group_name": func() *pluginsdk.Schema {
+				if !features.ThreePointOhBeta() {
+					return azure.SchemaResourceGroupNameDiffSuppress()
+				}
+				return azure.SchemaResourceGroupName()
+			}(),
 
 			"scope": {
 				Type:     pluginsdk.TypeString,
