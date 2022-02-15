c0b2799be9d2f51d7c3c76fcc7ee84d136605dcf
Kyriakos Oikonomakos <kyriakos@hashicorp.com>
2022-02-15 16:09:51 +0000

    [3.0 Beta] `zones` is now computed for resource `public_ip_prefix`

--- internal/services/network/public_ip_prefix_resource.go
+++ internal/services/network/public_ip_prefix_resource.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"time"
 
+	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
+
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2021-05-01/network"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
