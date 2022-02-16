b482f5e0bd24ffd57e739a76984a94b3d78bc4e0
catriona-m <86247157+catriona-m@users.noreply.github.com>
2022-02-16 11:13:06 +0000

    `azurerm_iothub_dps` - support for `ip_filter_rule` block and `public_network_access_enabled` (#15343)
    * add ip_filter_rule and public_network_access_enabled props
    
    * fix linting
    
    * add state migration
    
    * set public_network_access_enabled to true by default
    
    * undo migration

--- internal/services/iothub/iothub_dps_resource_test.go
+++ internal/services/iothub/iothub_dps_resource_test.go
@@ -97,6 +97,48 @@ func TestAccIotHubDPS_linkedHubs(t *testing.T) {
 	})
 }
 
+func TestAccIotHubDPS_ipFilterRules(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_iothub_dps", "test")
+	r := IotHubDPSResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.ipFilterRules(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("allocation_policy").Exists(),
+				check.That(data.ResourceName).Key("device_provisioning_host_name").Exists(),
+				check.That(data.ResourceName).Key("id_scope").Exists(),
+				check.That(data.ResourceName).Key("service_operations_host_name").Exists(),
+				check.That(data.ResourceName).Key("public_network_access_enabled").HasValue("true"),
+				check.That(data.ResourceName).Key("ip_filter_rule.0.name").HasValue("test"),
+				check.That(data.ResourceName).Key("ip_filter_rule.0.ip_mask").HasValue("10.0.0.0/31"),
+				check.That(data.ResourceName).Key("ip_filter_rule.0.action").HasValue("Accept"),
+				check.That(data.ResourceName).Key("ip_filter_rule.0.target").HasValue("All"),
+				check.That(data.ResourceName).Key("ip_filter_rule.1.name").HasValue("test2"),
+				check.That(data.ResourceName).Key("ip_filter_rule.1.ip_mask").HasValue("10.0.2.0/31"),
+				check.That(data.ResourceName).Key("ip_filter_rule.1.action").HasValue("Accept"),
+				check.That(data.ResourceName).Key("ip_filter_rule.1.target").HasValue("ServiceApi"),
+			),
+		},
+		{
+			Config: r.ipFilterRulesUpdated(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("ip_filter_rule.0.name").HasValue("test"),
+				check.That(data.ResourceName).Key("ip_filter_rule.0.ip_mask").HasValue("10.0.0.0/31"),
+				check.That(data.ResourceName).Key("ip_filter_rule.0.action").HasValue("Reject"),
+				check.That(data.ResourceName).Key("ip_filter_rule.0.target").HasValue("All"),
+				check.That(data.ResourceName).Key("ip_filter_rule.1.name").HasValue("test2"),
+				check.That(data.ResourceName).Key("ip_filter_rule.1.ip_mask").HasValue("10.0.2.0/31"),
+				check.That(data.ResourceName).Key("ip_filter_rule.1.action").HasValue("Reject"),
+				check.That(data.ResourceName).Key("ip_filter_rule.1.target").HasValue("DeviceApi"),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func (t IotHubDPSResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	id, err := parse.IotHubDpsID(state.ID)
 	if err != nil {
@@ -244,6 +286,96 @@ resource "azurerm_iothub_dps" "test" {
     location                = azurerm_resource_group.test.location
     allocation_weight       = 150
     apply_allocation_policy = true
+  }
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
+}
+
+func (IotHubDPSResource) ipFilterRules(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_iothub_dps" "test" {
+  name                          = "acctestIoTDPS-%d"
+  resource_group_name           = azurerm_resource_group.test.name
+  location                      = azurerm_resource_group.test.location
+  public_network_access_enabled = true
+
+  ip_filter_rule {
+    name    = "test"
+    ip_mask = "10.0.0.0/31"
+    action  = "Accept"
+    target  = "All"
+  }
+
+  ip_filter_rule {
+    name    = "test2"
+    ip_mask = "10.0.2.0/31"
+    action  = "Accept"
+    target  = "ServiceApi"
+  }
+
+  ip_filter_rule {
+    name    = "test3"
+    ip_mask = "10.0.3.0/31"
+    action  = "Accept"
+  }
+
+  sku {
+    name     = "S1"
+    capacity = "1"
+  }
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
+}
+
+func (IotHubDPSResource) ipFilterRulesUpdated(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_iothub_dps" "test" {
+  name                          = "acctestIoTDPS-%d"
+  resource_group_name           = azurerm_resource_group.test.name
+  location                      = azurerm_resource_group.test.location
+  public_network_access_enabled = true
+
+  ip_filter_rule {
+    name    = "test"
+    ip_mask = "10.0.0.0/31"
+    action  = "Reject"
+    target  = "All"
+  }
+
+  ip_filter_rule {
+    name    = "test2"
+    ip_mask = "10.0.2.0/31"
+    action  = "Reject"
+    target  = "DeviceApi"
+  }
+
+  ip_filter_rule {
+    name    = "test3"
+    ip_mask = "10.0.3.0/31"
+    action  = "Accept"
+  }
+
+  sku {
+    name     = "S1"
+    capacity = "1"
   }
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
