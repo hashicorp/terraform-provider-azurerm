8b7071ec17ea01c16b64e7d5eb753acb368df91c
kt <kt@katbyte.me>
2022-02-17 11:29:18 -0800

    make fmt

--- internal/services/springcloud/spring_cloud_app_resource.go
+++ internal/services/springcloud/spring_cloud_app_resource.go
@@ -15,7 +15,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/springcloud/parse"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/springcloud/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
-	`github.com/hashicorp/terraform-provider-azurerm/internal/tf/set`
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/set"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
