d3b6be8d3c3d006e60dc7919807e755e4b281194
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-02-24 18:10:07 +0100

    refactor: removing the old `StateFunc` function

--- internal/location/schema.go
+++ internal/location/schema.go
@@ -2,14 +2,9 @@ package location
 
 import (
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
-	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 )
 
 func Schema() *pluginsdk.Schema {
 	return commonschema.Location()
-}
-
-func StateFunc(input interface{}) string {
-	return location.StateFunc(input)
 }
