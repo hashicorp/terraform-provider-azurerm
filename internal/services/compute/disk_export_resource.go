bb449bfae618476e8ced24959249d84134e5a760
Harshavardhan Musanalli <harshavmb@gmail.com>
2022-02-25 19:01:45 +0100

    New resource azurerm_disk_export to manage disk exports instead of data sources

--- internal/services/compute/disk_export_resource.go
+++ internal/services/compute/disk_export_resource.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2021-07-01/compute"
@@ -12,15 +13,23 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/compute/parse"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/suppress"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
 )
 
-func dataSourceManagedDiskExport() *pluginsdk.Resource {
+func resourceDiskExport() *pluginsdk.Resource {
+
 	return &pluginsdk.Resource{
-		Read: dataSourceManagedDiskExportRead,
+		Create: resourceDiskExportCreateUpdate,
+		Read:   resourceDiskExportRead,
+		Update: resourceDiskExportCreateUpdate,
+		Delete: resourceDiskExportDelete,
 
 		Timeouts: &pluginsdk.ResourceTimeout{
-			Read: pluginsdk.DefaultTimeout(5 * time.Minute),
+			Create: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
+			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
 		},
 
 		Schema: map[string]*pluginsdk.Schema{
@@ -42,20 +51,25 @@ func dataSourceManagedDiskExport() *pluginsdk.Resource {
 				Default:  "Read",
 			},
 
-			"sas": {
-				Type:      pluginsdk.TypeString,
-				Computed:  true,
-				Sensitive: true,
+			"sas_url": {
+				Type:         pluginsdk.TypeString,
+				Optional:     true,
+				Computed:     true,
+				ForceNew:     true,
+				Sensitive:    true,
+				ValidateFunc: validation.IsURLWithScheme([]string{"http", "https"}),
 			},
 		},
 	}
+
 }
 
-func dataSourceManagedDiskExportRead(d *pluginsdk.ResourceData, meta interface{}) error {
+func resourceDiskExportCreateUpdate(d *pluginsdk.ResourceData, meta interface{}) error {
 	client := meta.(*clients.Client).Compute.DisksClient
-	ctx, cancel := timeouts.ForCreate(meta.(*clients.Client).StopContext, d)
+	ctx, cancel := timeouts.ForCreateUpdate(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
+	log.Printf("[INFO] preparing arguments for AzureRM Disk Export.")
 	managedDiskId := d.Get("managed_disk_id").(string)
 	durationInSeconds := int32(d.Get("duration_in_seconds").(int))
 	access := d.Get("access").(string)
@@ -92,11 +106,32 @@ func dataSourceManagedDiskExportRead(d *pluginsdk.ResourceData, meta interface{}
 	}
 
 	sasToken := *diskGrantResponse.AccessSAS
-
-	d.Set("sas", sasToken)
+	d.Set("sas_url", sasToken)
 	tokenHash := sha256.Sum256([]byte(sasToken))
 	d.SetId(hex.EncodeToString(tokenHash[:]))
 
-	return nil
+	return resourceDiskExportRead(d, meta)
 
+}
+
+func resourceDiskExportRead(d *pluginsdk.ResourceData, meta interface{}) error {
+	return nil
+}
+
+func resourceDiskExportDelete(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).Compute.DisksClient
+	ctx, cancel := timeouts.ForDelete(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	id, err := parse.ManagedDiskID(d.Get("managed_disk_id").(string))
+	if err != nil {
+		return err
+	}
+
+	future, err := client.RevokeAccess(ctx, id.ResourceGroup, id.DiskName)
+	if err != nil {
+		return err
+	}
+
+	return future.WaitForCompletionRef(ctx, client.Client)
 }
