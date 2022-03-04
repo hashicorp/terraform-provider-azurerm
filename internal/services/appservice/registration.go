79ab3d46058718aba78c4f66694370e9ef59daa7
jackofallops <11830746+jackofallops@users.noreply.github.com>
2022-03-04 07:58:01 +0000

    New Beta Resource - `azurerm_function_app_function` (#15605)

--- internal/services/appservice/registration.go
+++ internal/services/appservice/registration.go
@@ -40,6 +40,7 @@ func (r Registration) Resources() []sdk.Resource {
 		return []sdk.Resource{
 			AppServiceSourceControlTokenResource{},
 			FunctionAppActiveSlotResource{},
+			FunctionAppFunctionResource{},
 			LinuxFunctionAppResource{},
 			LinuxFunctionAppSlotResource{},
 			LinuxWebAppResource{},
