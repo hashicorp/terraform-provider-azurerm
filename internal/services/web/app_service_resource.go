f2103534c312fd90edaf82c759d0fc9de5d31294
jackofallops <11830746+jackofallops@users.noreply.github.com>
2022-03-07 08:33:43 +0000

    add 3.0 conditional deprecations to web (#15668)
    Co-authored-by: tombuildsstuff <git@tom.ibuildstuff.eu>

--- internal/services/web/app_service_resource.go
+++ internal/services/web/app_service_resource.go
@@ -31,6 +31,8 @@ func resourceAppService() *pluginsdk.Resource {
 		Update: resourceAppServiceUpdate,
 		Delete: resourceAppServiceDelete,
 
+		DeprecationMessage: features.DeprecatedInThreePointOh("The `azurerm_app_service` resource has been superseded by the `azurerm_linux_web_app` and `azurerm_windows_web_app` resources. Whilst this resource will continue to be available in the 2.x and 3.x releases it is feature-frozen for compatibility purposes, will no longer receive any updates and will be removed in a future major release of the Azure Provider."),
+
 		Importer: pluginsdk.ImporterValidatingResourceId(func(id string) error {
 			_, err := parse.AppServiceID(id)
 			return err
@@ -122,8 +124,8 @@ func resourceAppService() *pluginsdk.Resource {
 								string(web.ConnectionStringTypeServiceBus),
 								string(web.ConnectionStringTypeSQLAzure),
 								string(web.ConnectionStringTypeSQLServer),
-							}, !features.ThreePointOhBeta()),
-							DiffSuppressFunc: suppress.CaseDifferenceV2Only,
+							}, true),
+							DiffSuppressFunc: suppress.CaseDifference,
 						},
 
 						"value": {
