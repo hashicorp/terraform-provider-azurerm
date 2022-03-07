bdd88701ebff96786238032b767b4e1e13c24c26
Tom Bamford <tom@bamford.io>
2022-03-07 21:20:54 +0000

    Deprecation of `sql` package for v3.0 (#15676)

--- internal/services/sql/sql_managed_instance_failover_group_resource.go
+++ internal/services/sql/sql_managed_instance_failover_group_resource.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/mssql/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/sql/parse"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
@@ -24,6 +25,8 @@ func resourceSqlInstanceFailoverGroup() *pluginsdk.Resource {
 		Read:   resourceSqlInstanceFailoverGroupRead,
 		Update: resourceSqlInstanceFailoverGroupCreateUpdate,
 		Delete: resourceSqlInstanceFailoverGroupDelete,
+
+		DeprecationMessage: features.DeprecatedInThreePointOh("The `azurerm_sql_managed_instance_failover_group` resource is deprecated and will be removed in version 4.0 of the AzureRM provider. Please use the `azurerm_mssql_managed_instance_failover_group` resource instead."),
 
 		Importer: pluginsdk.ImporterValidatingResourceId(func(id string) error {
 			_, err := parse.InstanceFailoverGroupID(id)
