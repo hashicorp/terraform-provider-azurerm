e0557381b2e95942eac2f15237769e275f5747ca
Brandon Ros <brandonros1@gmail.com>
2022-03-08 13:55:05 -0500

    do not change azure_blob_resource yet

--- internal/services/datafactory/data_factory_dataset_azure_blob_resource.go
+++ internal/services/datafactory/data_factory_dataset_azure_blob_resource.go
@@ -87,12 +87,6 @@ func resourceDataFactoryDatasetAzureBlob() *pluginsdk.Resource {
 				ValidateFunc: validation.StringIsNotEmpty,
 			},
 
-			"dynamic_container_enabled": {
-				Type:     pluginsdk.TypeBool,
-				Optional: true,
-				Default:  false,
-			},
-
 			"dynamic_path_enabled": {
 				Type:     pluginsdk.TypeBool,
 				Optional: true,
