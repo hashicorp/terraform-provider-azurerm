f420433e6862fd318300520abbc42d5594e0848b
Matthew Frahry <mbfrahry@gmail.com>
2022-03-08 11:10:45 -0800

    Attempt to fix TestAccDiskEncryptionSet_basic

--- internal/services/compute/disk_encryption_set_resource_test.go
+++ internal/services/compute/disk_encryption_set_resource_test.go
@@ -139,7 +139,7 @@ provider "azurerm" {
   features {
     key_vault {
       recover_soft_deleted_key_vaults = false
-      purge_soft_delete_on_destroy    = false
+      purge_soft_delete_on_destroy    = true
     }
   }
 }
@@ -157,7 +157,7 @@ resource "azurerm_key_vault" "test" {
   resource_group_name         = azurerm_resource_group.test.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
-  purge_protection_enabled    = true
+  purge_protection_enabled    = false
   enabled_for_disk_encryption = true
 }
 
