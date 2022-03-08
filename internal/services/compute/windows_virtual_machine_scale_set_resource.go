80973db4bf92d35d0bad1c498a364ad6824f2f82
Yichun Ma <yicma@microsoft.com>
2022-03-08 08:19:05 +0800

    Update Azure compute sdk version to 2021-11-01 (#15099)
    Update Azure compute sdk version to 2021-11-01 #15099

--- internal/services/compute/windows_virtual_machine_scale_set_resource.go
+++ internal/services/compute/windows_virtual_machine_scale_set_resource.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 
-	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2021-07-01/compute"
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2021-11-01/compute"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
