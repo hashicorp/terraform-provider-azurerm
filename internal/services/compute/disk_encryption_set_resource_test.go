53fd5e19a93f9e5d7e378b7774e9a2a08c24cd72
Matthew Frahry <mbfrahry@gmail.com>
2022-03-09 10:56:36 -0800

    Fix various keyvault tests

--- internal/services/compute/disk_encryption_set_resource_test.go
+++ internal/services/compute/disk_encryption_set_resource_test.go
@@ -138,8 +138,9 @@ func (DiskEncryptionSetResource) dependencies(data acceptance.TestData) string {
 provider "azurerm" {
   features {
     key_vault {
-      recover_soft_deleted_key_vaults = false
-      purge_soft_delete_on_destroy    = false
+      recover_soft_deleted_key_vaults    = false
+      purge_soft_delete_on_destroy       = false
+      purge_soft_deleted_keys_on_destroy = false
     }
   }
 }
