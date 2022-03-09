3969719f69e1990300fe5a3deffe110ecc4e0be7
Catriona <catriona@hashicorp.com>
2022-03-09 14:16:01 +0000

    update sku casing

--- internal/services/digitaltwins/digital_twins_endpoint_servicebus_resource_test.go
+++ internal/services/digitaltwins/digital_twins_endpoint_servicebus_resource_test.go
@@ -182,7 +182,7 @@ resource "azurerm_servicebus_namespace" "test_alt" {
   name                = "acctestservicebusnamespace-alt-%[2]d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  sku                 = "basic"
+  sku                 = "Basic"
 }
 
 resource "azurerm_servicebus_topic" "test_alt" {
@@ -216,7 +216,7 @@ resource "azurerm_servicebus_namespace" "test_alt" {
   name                = "acctestservicebusnamespace-alt-%[2]d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  sku                 = "basic"
+  sku                 = "Basic"
 }
 
 resource "azurerm_servicebus_topic" "test_alt" {
