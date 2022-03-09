ceaa5994b6e64f1f3bf5c434b302516bf9c52843
xiaxin18 <xiaxyi@microsoft.com>
2022-03-09 16:37:48 +0800

    sort import

--- internal/services/healthcare/healthcare_apis_workspace_resource_test.go
+++ internal/services/healthcare/healthcare_apis_workspace_resource_test.go
@@ -3,13 +3,14 @@ package healthcare_test
 import (
 	"context"
 	"fmt"
+	"testing"
+
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/healthcare/parse"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
-	"testing"
 )
 
 type HealthCareWorkspaceResouce struct{}
