3e31a1f2635b3870f5173e5a3420db619a1f37c5
Matthew Frahry <mbfrahry@gmail.com>
2022-03-10 11:47:32 -0800

    any test containing keyvault

--- internal/services/synapse/synapse_workspace_sql_aad_admin_resource_test.go
+++ internal/services/synapse/synapse_workspace_sql_aad_admin_resource_test.go
@@ -67,7 +67,8 @@ func (r SynapseWorkspaceSqlAADAdminResource) template(data acceptance.TestData) 
 provider "azurerm" {
   features {
     key_vault {
-      purge_soft_delete_on_destroy = false
+      purge_soft_delete_on_destroy       = false
+      purge_soft_deleted_keys_on_destroy = false
     }
   }
 }
