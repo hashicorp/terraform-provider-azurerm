1d2b6ef30abd0cc18cddaaa85be9aa077b6f71df
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-03-18 12:31:17 +0100

    datalake: removing deprecated resources:
    * Data Source: `azurerm_data_lake_store` (Gen1)
    * Data Source: `azurerm_data_share_dataset_data_lake_gen1`
    * Resource: `azurerm_data_lake_store_file` (Gen1)
    * Resource: `azurerm_data_lake_store_firewall_rule` (Gen1)
    * Resource: `azurerm_data_lake_store` (Gen1)
    * Resource: `azurerm_data_lake_store_virtual_network_rule` (Gen1)
    * Resource: `azurerm_data_share_dataset_data_lake_gen1`

--- internal/services/datalake/registration.go
+++ internal/services/datalake/registration.go
@@ -1,7 +1,6 @@
 package datalake
 
 import (
-	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 )
@@ -28,24 +27,13 @@ func (r Registration) WebsiteCategories() []string {
 
 // SupportedDataSources returns the supported Data Sources supported by this Service
 func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
-	return map[string]*pluginsdk.Resource{
-		"azurerm_data_lake_store": dataSourceDataLakeStoreAccount(),
-	}
+	return map[string]*pluginsdk.Resource{}
 }
 
 // SupportedResources returns the supported Resources supported by this Service
 func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
-	out := map[string]*pluginsdk.Resource{
+	return map[string]*pluginsdk.Resource{
 		"azurerm_data_lake_analytics_account":       resourceDataLakeAnalyticsAccount(),
 		"azurerm_data_lake_analytics_firewall_rule": resourceDataLakeAnalyticsFirewallRule(),
 	}
-
-	if !features.ThreePointOhBeta() {
-		out["azurerm_data_lake_store_file"] = resourceDataLakeStoreFile()
-		out["azurerm_data_lake_store_firewall_rule"] = resourceDataLakeStoreFirewallRule()
-		out["azurerm_data_lake_store"] = resourceDataLakeStore()
-		out["azurerm_data_lake_store_virtual_network_rule"] = resourceDataLakeStoreVirtualNetworkRule()
-	}
-
-	return out
 }
