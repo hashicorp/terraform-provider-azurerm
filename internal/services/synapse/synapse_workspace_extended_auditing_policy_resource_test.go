1928693b17af90b1e5f5d91eb632523328cc1b47
henglu <henglu@microsoft.com>
2022-03-22 10:19:25 +0800

    remove features flags

--- internal/services/synapse/synapse_workspace_extended_auditing_policy_resource_test.go
+++ internal/services/synapse/synapse_workspace_extended_auditing_policy_resource_test.go
@@ -306,8 +306,7 @@ resource "azurerm_subnet" "test" {
 }
 
 resource "azurerm_storage_account_network_rules" "test" {
-  resource_group_name        = azurerm_resource_group.test.name
-  storage_account_name       = azurerm_storage_account.test.name
+  storage_account_id         = azurerm_storage_account.test.id
   default_action             = "Deny"
   ip_rules                   = ["127.0.0.1"]
   virtual_network_subnet_ids = [azurerm_subnet.test.id]
