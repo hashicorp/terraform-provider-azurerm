10204df6626f7808dc59b25a62a75249a0fafc6e
kt <kt@katbyte.me>
2022-03-23 12:11:15 -0700

    remaining tests and docs

--- internal/services/network/subnet_resource_test.go
+++ internal/services/network/subnet_resource_test.go
@@ -561,7 +561,7 @@ resource "azurerm_subnet" "import" {
   name                 = azurerm_subnet.test.name
   resource_group_name  = azurerm_subnet.test.resource_group_name
   virtual_network_name = azurerm_subnet.test.virtual_network_name
-  address_prefix       = azurerm_subnet.test.address_prefix
+  address_prefixes     = azurerm_subnet.test.address_prefixes
 }
 `, r.basic(data))
 }
@@ -643,7 +643,7 @@ resource "azurerm_subnet" "test" {
   name                        = "internal"
   resource_group_name         = azurerm_resource_group.test.name
   virtual_network_name        = azurerm_virtual_network.test.name
-  address_prefix              = "10.0.2.0/24"
+  address_prefixes            = ["10.0.2.0/24"]
   service_endpoints           = ["Microsoft.Sql"]
   service_endpoint_policy_ids = [azurerm_subnet_service_endpoint_storage_policy.test.id]
 }
