a29e5cfd5efcc7e875012946570a4c7ce5eab4c6
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-03-23 15:31:54 +0100

    r/app_service_slot: documenting the identity changes

--- internal/services/web/app_service_slot_resource.go
+++ internal/services/web/app_service_slot_resource.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/services/web/mgmt/2021-02-01/web"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
@@ -52,13 +51,7 @@ func resourceAppServiceSlot() *pluginsdk.Resource {
 				ValidateFunc: webValidate.AppServiceName,
 			},
 
-			"identity": func() *schema.Schema {
-				if !features.ThreePointOhBeta() {
-					return schemaAppServiceIdentity()
-				}
-
-				return commonschema.SystemAssignedUserAssignedIdentityOptional()
-			}(),
+			"identity": commonschema.SystemAssignedUserAssignedIdentityOptional(),
 
 			"resource_group_name": azure.SchemaResourceGroupName(),
 
