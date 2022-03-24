hashicorp/terraform-provider-azurerm

commit 48d9aadb1e1397d99fe3dc535a2e7c4928f441ae

typos and terrafmt

internal/services/loganalytics/log_analytics_workspace_resource.go
@@ -212,20 +212,10 @@ func resourceLogAnalyticsWorkspaceCreateUpdate(d *pluginsdk.ResourceData, meta i
 		}
 	}
 
-	// Handle typoed property name
-	propName := "reservation_capacity_in_gb_per_day"
-	capacityReservationLevel, ok := d.GetOk(propName)
-
-	if ok {
-		if strings.EqualFold(skuName, string(operationalinsights.WorkspaceSkuNameEnumCapacityReservation)) {
-			parameters.WorkspaceProperties.Sku.CapacityReservationLevel = utils.Int32((int32(capacityReservationLevel.(int))))
-		} else {
-			return fmt.Errorf("`%s` can only be used with the `CapacityReservation` SKU", propName)
-		}
+	if strings.EqualFold(skuName, string(operationalinsights.WorkspaceSkuNameEnumCapacityReservation)) {
+		parameters.WorkspaceProperties.Sku.CapacityReservationLevel = utils.Int32((int32(d.Get("reservation_capacity_in_gb_per_day").(int))))
 	} else {
-		if strings.EqualFold(skuName, string(operationalinsights.WorkspaceSkuNameEnumCapacityReservation)) {
-			return fmt.Errorf("`%s` must be set when using the `CapacityReservation` SKU", propName)
-		}
+		return fmt.Errorf("`reservation_capacity_in_gb_per_day` can only be used with the `CapacityReservation` SKU")
 	}
 
 	future, err := client.CreateOrUpdate(ctx, resourceGroup, name, parameters)

internal/services/loganalytics/log_analytics_workspace_resource_test.go
@@ -501,11 +501,11 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_log_analytics_workspace" "test" {
-  name                              = "acctestLAW-%d"
-  location                          = azurerm_resource_group.test.location
-  resource_group_name               = azurerm_resource_group.test.name
-  internet_query_enabled            = false
-  sku                               = "CapacityReservation"
+  name                               = "acctestLAW-%d"
+  location                           = azurerm_resource_group.test.location
+  resource_group_name                = azurerm_resource_group.test.name
+  internet_query_enabled             = false
+  sku                                = "CapacityReservation"
   reservation_capacity_in_gb_per_day = %d
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger, capacityReservation)

internal/services/network/vpn_gateway_connection_resource.go
@@ -713,6 +713,7 @@ func flattenVpnGatewayConnectionRoutingConfiguration(input *network.RoutingConfi
 
 	return []interface{}{
 		map[string]interface{}{
+			"propagated_route_table": flattenVpnGatewayConnectionPropagatedRouteTable(input.PropagatedRouteTables),
 			"associated_route_table": associateRouteTable,
 		},
 	}

internal/services/network/vpn_server_configuration_resource_test.go
@@ -71,6 +71,21 @@ func TestAccVPNServerConfiguration_requiresImport(t *testing.T) {
 	})
 }
 
+func TestAccVPNServerConfiguration_radius(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_vpn_server_configuration", "test")
+	r := VPNServerConfigurationResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.singleRadius(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func TestAccVPNServerConfiguration_multipleRadius(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_vpn_server_configuration", "test")
 	r := VPNServerConfigurationResource{}

commit 3488a6670fb2260bb067d735e97037cc7938940d

Rollback issues introduced in 48d9aadb1e1397d99fe3dc535a2e7c4928f441ae
This PR rolls back a bug introduced in https://github.com/hashicorp/terraform-provider-azurerm/commit/48d9aadb1e1397d99fe3dc535a2e7c4928f441ae, which causes the following config:

```hcl
resource "azurerm_log_analytics_workspace" "test" {
  name                = "acctestLAW-220119092533317533"
  location            = azurerm_resource_group.test.location
  resource_group_name = azurerm_resource_group.test.name
  sku                 = "PerGB2018"
}
```

raises error:

```
│ Error: `reservation_capacity_in_gb_per_day` can only be used with the `CapacityReservation` SKU
```

internal/services/loganalytics/log_analytics_workspace_resource.go
@@ -212,10 +212,18 @@ func resourceLogAnalyticsWorkspaceCreateUpdate(d *pluginsdk.ResourceData, meta i
 		}
 	}
 
-	if strings.EqualFold(skuName, string(operationalinsights.WorkspaceSkuNameEnumCapacityReservation)) {
-		parameters.WorkspaceProperties.Sku.CapacityReservationLevel = utils.Int32(int32(d.Get("reservation_capacity_in_gb_per_day").(int)))
+	propName := "reservation_capacity_in_gb_per_day"
+	capacityReservationLevel, ok := d.GetOk(propName)
+	if ok {
+		if strings.EqualFold(skuName, string(operationalinsights.WorkspaceSkuNameEnumCapacityReservation)) {
+			parameters.WorkspaceProperties.Sku.CapacityReservationLevel = utils.Int32((int32(capacityReservationLevel.(int))))
+		} else {
+			return fmt.Errorf("`%s` can only be used with the `CapacityReservation` SKU", propName)
+		}
 	} else {
-		return fmt.Errorf("`reservation_capacity_in_gb_per_day` can only be used with the `CapacityReservation` SKU")
+		if strings.EqualFold(skuName, string(operationalinsights.WorkspaceSkuNameEnumCapacityReservation)) {
+			return fmt.Errorf("`%s` must be set when using the `CapacityReservation` SKU", propName)
+		}
 	}
 
 	future, err := client.CreateOrUpdate(ctx, resourceGroup, name, parameters)