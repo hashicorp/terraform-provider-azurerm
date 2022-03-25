04a914b8259111921e19d6fa16d635c937287040
xiaxin18 <xiaxyi@microsoft.com>
2022-03-25 17:20:48 +0800

    update

--- internal/services/redisenterprise/redis_enterprise_database_resource.go
+++ internal/services/redisenterprise/redis_enterprise_database_resource.go
@@ -30,6 +30,7 @@ func resourceRedisEnterpriseDatabase() *pluginsdk.Resource {
 		Timeouts: &pluginsdk.ResourceTimeout{
 			Create: pluginsdk.DefaultTimeout(30 * time.Minute),
 			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
+			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
 			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
 		},
 
