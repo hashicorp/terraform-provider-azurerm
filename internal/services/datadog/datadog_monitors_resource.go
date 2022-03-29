006ea86a58cd54beb9e021423ce556eafb8cc8a9
Vidhi Kothari <vikotha@microsoft.com>
2022-03-29 19:08:13 +0530

    Add datadog plugin for dd monitor resource

---
+++ internal/services/datadog/datadog_monitors_resource.go
@@ -0,0 +1,493 @@
+package datadog
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/services/datadog/mgmt/2021-03-01/datadog"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
+	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
+	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/datadog/parse"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/datadog/validate"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tags"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+)
+
+func resourceDatadogMonitor() *pluginsdk.Resource {
+	return &pluginsdk.Resource{
+		Create: resourceDatadogMonitorCreate,
+		Read:   resourceDatadogMonitorRead,
+		Update: resourceDatadogMonitorUpdate,
+		Delete: resourceDatadogMonitorDelete,
+
+		Timeouts: &pluginsdk.ResourceTimeout{
+			Create: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
+			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
+		},
+
+		Importer: pluginsdk.ImporterValidatingResourceId(func(id string) error {
+			_, err := parse.DatadogMonitorID(id)
+			return err
+		}),
+
+		Schema: map[string]*pluginsdk.Schema{
+			"name": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validate.DatadogMonitorsName,
+			},
+
+			"resource_group_name": azure.SchemaResourceGroupName(),
+
+			"location": azure.SchemaLocation(),
+
+			"datadog_organization_properties": {
+				Type:     pluginsdk.TypeList,
+				Required: true,
+				MaxItems: 1,
+				Elem: &pluginsdk.Resource{
+					Schema: map[string]*pluginsdk.Schema{
+						"name": {
+							Type:     pluginsdk.TypeString,
+							Computed: true,
+						},
+
+						"api_key": {
+							Type:      pluginsdk.TypeString,
+							Required:  true,
+							ForceNew:  true,
+							Sensitive: true,
+						},
+
+						"application_key": {
+							Type:      pluginsdk.TypeString,
+							Required:  true,
+							ForceNew:  true,
+							Sensitive: true,
+						},
+
+						"enterprise_app_id": {
+							Type:     pluginsdk.TypeString,
+							Optional: true,
+							ForceNew: true,
+						},
+
+						"linking_auth_code": {
+							Type:      pluginsdk.TypeString,
+							Optional:  true,
+							ForceNew:  true,
+							Sensitive: true,
+						},
+
+						"linking_client_id": {
+							Type:      pluginsdk.TypeString,
+							Optional:  true,
+							ForceNew:  true,
+							Sensitive: true,
+						},
+
+						"redirect_uri": {
+							Type:     pluginsdk.TypeString,
+							Optional: true,
+							ForceNew: true,
+						},
+
+						"id": {
+							Type:     pluginsdk.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+
+			"identity": {
+				Type:     pluginsdk.TypeList,
+				Required: true,
+				MaxItems: 1,
+				Elem: &pluginsdk.Resource{
+					Schema: map[string]*pluginsdk.Schema{
+						"type": {
+							Type:     pluginsdk.TypeString,
+							Required: true,
+							ForceNew: true,
+							ValidateFunc: validation.StringInSlice([]string{
+								"SystemAssigned",
+								"UserAssigned",
+							}, false),
+						},
+
+						"principal_id": {
+							Type:     pluginsdk.TypeString,
+							Computed: true,
+						},
+
+						"tenant_id": {
+							Type:     pluginsdk.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+
+			"sku": {
+				Type:     pluginsdk.TypeList,
+				Required: true,
+				MaxItems: 1,
+				Elem: &pluginsdk.Resource{
+					Schema: map[string]*pluginsdk.Schema{
+						"name": {
+							Type:     pluginsdk.TypeString,
+							Required: true,
+						},
+					},
+				},
+			},
+
+			"user_info": {
+				Type:     pluginsdk.TypeList,
+				Required: true,
+				MaxItems: 1,
+				Elem: &pluginsdk.Resource{
+					Schema: map[string]*pluginsdk.Schema{
+						"name": {
+							Type:         pluginsdk.TypeString,
+							Required:     true,
+							ForceNew:     true,
+							ValidateFunc: validate.DatadogUsersName,
+						},
+
+						"email_address": {
+							Type:         pluginsdk.TypeString,
+							Required:     true,
+							ForceNew:     true,
+							ValidateFunc: validate.DatadogMonitorsEmailAddress,
+						},
+
+						"phone_number": {
+							Type:         pluginsdk.TypeString,
+							Optional:     true,
+							ForceNew:     true,
+							ValidateFunc: validate.DatadogMonitorsPhoneNumber,
+						},
+					},
+				},
+			},
+
+			"monitoring_status": {
+				Type:     pluginsdk.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+
+			"liftr_resource_category": {
+				Type:     pluginsdk.TypeString,
+				Computed: true,
+			},
+
+			"liftr_resource_preference": {
+				Type:     pluginsdk.TypeInt,
+				Computed: true,
+			},
+
+			"marketplace_subscription_status": {
+				Type:     pluginsdk.TypeString,
+				Computed: true,
+			},
+
+			"type": {
+				Type:     pluginsdk.TypeString,
+				Computed: true,
+			},
+
+			"tags": tags.Schema(),
+		},
+	}
+}
+func resourceDatadogMonitorCreate(d *pluginsdk.ResourceData, meta interface{}) error {
+	subscriptionId := meta.(*clients.Client).Account.SubscriptionId
+	client := meta.(*clients.Client).Datadog.MonitorsClient
+	ctx, cancel := timeouts.ForCreate(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	name := d.Get("name").(string)
+	resourceGroup := d.Get("resource_group_name").(string)
+
+	id := parse.NewDatadogMonitorID(subscriptionId, resourceGroup, name).ID()
+
+	existing, err := client.Get(ctx, resourceGroup, name)
+	if err != nil {
+		if !utils.ResponseWasNotFound(existing.Response) {
+			return fmt.Errorf("checking for existing Datadog Monitor %q (Resource Group %q): %+v", name, resourceGroup, err)
+		}
+	}
+	if !utils.ResponseWasNotFound(existing.Response) {
+		return tf.ImportAsExistsError("azurerm_datadog_monitor", id)
+	}
+
+	monitoringStatus := datadog.MonitoringStatusDisabled
+	if d.Get("monitoring_status").(bool) {
+		monitoringStatus = datadog.MonitoringStatusEnabled
+	}
+
+	body := datadog.MonitorResource{
+		Location: utils.String(location.Normalize(d.Get("location").(string))),
+		Identity: expandMonitorIdentityProperties(d.Get("identity").([]interface{})),
+		Sku:      expandMonitorResourceSku(d.Get("sku").([]interface{})),
+		Properties: &datadog.MonitorProperties{
+			DatadogOrganizationProperties: expandMonitorOrganizationProperties(d.Get("datadog_organization_properties").([]interface{})),
+			UserInfo:                      expandMonitorUserInfo(d.Get("user_info").([]interface{})),
+			MonitoringStatus:              monitoringStatus,
+		},
+		Tags: tags.Expand(d.Get("tags").(map[string]interface{})),
+	}
+	future, err := client.Create(ctx, resourceGroup, name, &body)
+	if err != nil {
+		return fmt.Errorf("creating Datadog Monitor %q (Resource Group %q): %+v", name, resourceGroup, err)
+	}
+
+	if err := future.WaitForCompletionRef(ctx, client.Client); err != nil {
+		return fmt.Errorf("waiting for creation of the Datadog Monitor %q (Resource Group %q): %+v", name, resourceGroup, err)
+	}
+
+	d.SetId(id)
+	return resourceDatadogMonitorRead(d, meta)
+}
+
+func resourceDatadogMonitorRead(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).Datadog.MonitorsClient
+	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	id, err := parse.DatadogMonitorID(d.Id())
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Get(ctx, id.ResourceGroup, id.MonitorName)
+	if err != nil {
+		if utils.ResponseWasNotFound(resp.Response) {
+			log.Printf("[INFO] Datadog monitor %q does not exist - removing from state", d.Id())
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("retrieving Datadog Monitor %q (Resource Group %q): %+v", id.MonitorName, id.ResourceGroup, err)
+	}
+	d.Set("name", id.MonitorName)
+	d.Set("resource_group_name", id.ResourceGroup)
+	d.Set("location", location.NormalizeNilable(resp.Location))
+	if err := d.Set("identity", flattenMonitorIdentityProperties(resp.Identity)); err != nil {
+		return fmt.Errorf("setting `identity`: %+v", err)
+	}
+	if props := resp.Properties; props != nil {
+		if err := d.Set("datadog_organization_properties", flattenMonitorOrganizationProperties(props.DatadogOrganizationProperties, d)); err != nil {
+			return fmt.Errorf("setting `datadog_organization_properties`: %+v", err)
+		}
+		if err := d.Set("user_info", flattenMonitorUserInfo(props.UserInfo, d)); err != nil {
+			return fmt.Errorf("setting `user_info`: %+v", err)
+		}
+		d.Set("monitoring_status", props.MonitoringStatus == datadog.MonitoringStatusEnabled)
+		d.Set("liftr_resource_category", props.LiftrResourceCategory)
+		d.Set("liftr_resource_preference", props.LiftrResourcePreference)
+		d.Set("marketplace_subscription_status", props.MarketplaceSubscriptionStatus)
+	}
+	d.Set("sku", flattenMonitorResourceSku("Linked"))
+	d.Set("type", resp.Type)
+	return tags.FlattenAndSet(d, resp.Tags)
+}
+
+func resourceDatadogMonitorUpdate(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).Datadog.MonitorsClient
+	ctx, cancel := timeouts.ForUpdate(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	id, err := parse.DatadogMonitorID(d.Id())
+	if err != nil {
+		return err
+	}
+
+	body := datadog.MonitorResourceUpdateParameters{
+		Properties: &datadog.MonitorUpdateProperties{},
+	}
+	if d.HasChange("monitoring_status") {
+		monitoringStatus := datadog.MonitoringStatusDisabled
+		if d.Get("monitoring_status").(bool) {
+			monitoringStatus = datadog.MonitoringStatusEnabled
+		}
+		body.Properties.MonitoringStatus = monitoringStatus
+	}
+	if d.HasChange("tags") {
+		body.Tags = tags.Expand(d.Get("tags").(map[string]interface{}))
+	}
+
+	if _, err := client.Update(ctx, id.ResourceGroup, id.MonitorName, &body); err != nil {
+		return fmt.Errorf("updating Datadog Monitor %q (Resource Group %q): %+v", id.MonitorName, id.ResourceGroup, err)
+	}
+	return resourceDatadogMonitorRead(d, meta)
+}
+
+func resourceDatadogMonitorDelete(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).Datadog.MonitorsClient
+	ctx, cancel := timeouts.ForDelete(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	id, err := parse.DatadogMonitorID(d.Id())
+	if err != nil {
+		return err
+	}
+
+	future, err := client.Delete(ctx, id.ResourceGroup, id.MonitorName)
+	if err != nil {
+		return fmt.Errorf("deleting Datadog Monitor %q (Resource Group %q): %+v", id.MonitorName, id.ResourceGroup, err)
+	}
+
+	if err := future.WaitForCompletionRef(ctx, client.Client); err != nil {
+		return fmt.Errorf("waiting for deletion of the Datadog Monitor %q (Resource Group %q): %+v", id.MonitorName, id.ResourceGroup, err)
+	}
+	return nil
+}
+
+func expandMonitorIdentityProperties(input []interface{}) *datadog.IdentityProperties {
+	if len(input) == 0 {
+		return nil
+	}
+	v := input[0].(map[string]interface{})
+	return &datadog.IdentityProperties{
+		Type: datadog.ManagedIdentityTypes(v["type"].(string)),
+	}
+}
+
+func expandMonitorResourceSku(input []interface{}) *datadog.ResourceSku {
+	if len(input) == 0 {
+		return nil
+	}
+	v := input[0].(map[string]interface{})
+	return &datadog.ResourceSku{
+		Name: utils.String(v["name"].(string)),
+	}
+}
+
+func expandMonitorOrganizationProperties(input []interface{}) *datadog.OrganizationProperties {
+	if len(input) == 0 {
+		return nil
+	}
+	v := input[0].(map[string]interface{})
+	return &datadog.OrganizationProperties{
+		LinkingAuthCode: utils.String(v["linking_auth_code"].(string)),
+		LinkingClientID: utils.String(v["linking_client_id"].(string)),
+		RedirectURI:     utils.String(v["redirect_uri"].(string)),
+		APIKey:          utils.String(v["api_key"].(string)),
+		ApplicationKey:  utils.String(v["application_key"].(string)),
+		EnterpriseAppID: utils.String(v["enterprise_app_id"].(string)),
+	}
+}
+
+func expandMonitorUserInfo(input []interface{}) *datadog.UserInfo {
+	if len(input) == 0 {
+		return nil
+	}
+	v := input[0].(map[string]interface{})
+	return &datadog.UserInfo{
+		Name:         utils.String(v["name"].(string)),
+		EmailAddress: utils.String(v["email_address"].(string)),
+		PhoneNumber:  utils.String(v["phone_number"].(string)),
+	}
+}
+
+func flattenMonitorIdentityProperties(input *datadog.IdentityProperties) []interface{} {
+	if input == nil {
+		return make([]interface{}, 0)
+	}
+
+	var t datadog.ManagedIdentityTypes
+	if input.Type != "" {
+		t = input.Type
+	}
+	var principalId string
+	if input.PrincipalID != nil {
+		principalId = *input.PrincipalID
+	}
+	var tenantId string
+	if input.TenantID != nil {
+		tenantId = *input.TenantID
+	}
+	return []interface{}{
+		map[string]interface{}{
+			"type":         t,
+			"principal_id": principalId,
+			"tenant_id":    tenantId,
+		},
+	}
+}
+
+func flattenMonitorOrganizationProperties(input *datadog.OrganizationProperties, d *pluginsdk.ResourceData) []interface{} {
+
+	organisationProperties := d.Get("datadog_organization_properties").([]interface{})
+	if len(organisationProperties) == 0 {
+		return make([]interface{}, 0)
+	}
+	v := organisationProperties[0].(map[string]interface{})
+
+	var name string
+	if input.Name != nil {
+		name = *input.Name
+	}
+	var id string
+	if input.ID != nil {
+		id = *input.ID
+	}
+	var redirectUri string
+	if input.RedirectURI != nil {
+		redirectUri = *input.RedirectURI
+	}
+	var enterpriseAppID string
+	if input.EnterpriseAppID != nil {
+		enterpriseAppID = *input.EnterpriseAppID
+	}
+	return []interface{}{
+		map[string]interface{}{
+			"name":              name,
+			"api_key":           utils.String(v["api_key"].(string)),
+			"application_key":   utils.String(v["application_key"].(string)),
+			"enterprise_app_id": enterpriseAppID,
+			"linking_auth_code": utils.String(v["linking_auth_code"].(string)),
+			"linking_client_id": utils.String(v["linking_client_id"].(string)),
+			"redirect_uri":      redirectUri,
+			"id":                id,
+		},
+	}
+}
+
+func flattenMonitorUserInfo(input *datadog.UserInfo, d *pluginsdk.ResourceData) []interface{} {
+
+	userInfo := d.Get("user_info").([]interface{})
+	if len(userInfo) == 0 {
+		return make([]interface{}, 0)
+	}
+
+	v := userInfo[0].(map[string]interface{})
+	return []interface{}{
+		map[string]interface{}{
+			"name":          utils.String(v["name"].(string)),
+			"email_address": utils.String(v["email_address"].(string)),
+			"phone_number":  utils.String(v["phone_number"].(string)),
+		},
+	}
+}
+
+func flattenMonitorResourceSku(input string) []interface{} {
+
+	return []interface{}{
+		map[string]interface{}{
+			"name": input,
+		},
+	}
+}
---
+++ internal/services/datadog/datadog_monitors_resource_test.go
@@ -0,0 +1,294 @@
+package datadog_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/datadog/parse"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+)
+
+type DatadogMonitorResource struct{}
+
+func TestAccDatadogMonitor_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_datadog_monitor", "test")
+	r := DatadogMonitorResource{}
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("user_info",
+			"user_info.0.name",
+			"user_info.0.email_address",
+			"datadog_organization_properties",
+			"datadog_organization_properties.0",
+			"datadog_organization_properties.0.id",
+			"datadog_organization_properties.0.name",
+			"datadog_organization_properties.0.api_key",
+			"datadog_organization_properties.0.application_key",
+			"datadog_organization_properties.0.enterprise_app_id",
+			"datadog_organization_properties.0.linking_auth_code",
+			"datadog_organization_properties.0.linking_client_id",
+			"datadog_organization_properties.0.redirect_uri"),
+	})
+}
+
+func TestAccDatadogMonitor_requiresImport(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_datadog_monitor", "test")
+	r := DatadogMonitorResource{}
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.RequiresImportErrorStep(r.requiresImport),
+	})
+}
+
+func TestAccDatadogMonitor_complete(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_datadog_monitor", "test")
+	r := DatadogMonitorResource{}
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.complete(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("user_info",
+			"user_info.0.name",
+			"user_info.0.email_address",
+			"datadog_organization_properties",
+			"datadog_organization_properties.0",
+			"datadog_organization_properties.0.id",
+			"datadog_organization_properties.0.name",
+			"datadog_organization_properties.0.api_key",
+			"datadog_organization_properties.0.application_key",
+			"datadog_organization_properties.0.enterprise_app_id",
+			"datadog_organization_properties.0.linking_auth_code",
+			"datadog_organization_properties.0.linking_client_id",
+			"datadog_organization_properties.0.redirect_uri"),
+	})
+}
+
+func TestAccDatadogMonitor_update(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_datadog_monitor", "test")
+	r := DatadogMonitorResource{}
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("user_info",
+			"user_info.0.name",
+			"user_info.0.email_address",
+			"datadog_organization_properties",
+			"datadog_organization_properties.0",
+			"datadog_organization_properties.0.id",
+			"datadog_organization_properties.0.name",
+			"datadog_organization_properties.0.api_key",
+			"datadog_organization_properties.0.application_key",
+			"datadog_organization_properties.0.enterprise_app_id",
+			"datadog_organization_properties.0.linking_auth_code",
+			"datadog_organization_properties.0.linking_client_id",
+			"datadog_organization_properties.0.redirect_uri"),
+		{
+			Config: r.update(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("user_info",
+			"user_info.0.name",
+			"user_info.0.email_address",
+			"datadog_organization_properties",
+			"datadog_organization_properties.0",
+			"datadog_organization_properties.0.id",
+			"datadog_organization_properties.0.name",
+			"datadog_organization_properties.0.api_key",
+			"datadog_organization_properties.0.application_key",
+			"datadog_organization_properties.0.enterprise_app_id",
+			"datadog_organization_properties.0.linking_auth_code",
+			"datadog_organization_properties.0.linking_client_id",
+			"datadog_organization_properties.0.redirect_uri"),
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("user_info",
+			"user_info.0.name",
+			"user_info.0.email_address",
+			"datadog_organization_properties",
+			"datadog_organization_properties.0",
+			"datadog_organization_properties.0.id",
+			"datadog_organization_properties.0.name",
+			"datadog_organization_properties.0.api_key",
+			"datadog_organization_properties.0.application_key",
+			"datadog_organization_properties.0.enterprise_app_id",
+			"datadog_organization_properties.0.linking_auth_code",
+			"datadog_organization_properties.0.linking_client_id",
+			"datadog_organization_properties.0.redirect_uri"),
+	})
+}
+
+func (r DatadogMonitorResource) Exists(ctx context.Context, client *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
+	id, err := parse.DatadogMonitorID(state.ID)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := client.Datadog.MonitorsClient.Get(ctx, id.ResourceGroup, id.MonitorName)
+	if err != nil {
+		if utils.ResponseWasNotFound(resp.Response) {
+			return utils.Bool(false), nil
+		}
+		return nil, fmt.Errorf("retrieving Datadog Monitor %q (Resource Group %q): %+v", id.MonitorName, id.ResourceGroup, err)
+	}
+	return utils.Bool(true), nil
+}
+
+func (r DatadogMonitorResource) template(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "test" {
+  name     = "acctest-datadog-%d"
+  location = "%s"
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (r DatadogMonitorResource) basic(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+	%s
+	
+	resource "azurerm_datadog_monitor" "test" {
+		name = "test-terraform-%d"
+		resource_group_name = azurerm_resource_group.test.name
+		location = "EAST US 2 EUAP"
+		datadog_organization_properties {
+			api_key = "XXX"
+			application_key = "XXX"
+		}
+		user_info {
+			name          = "Vidhi Kothari"
+			email_address = "vidhi.kothari@microsoft.com"
+		}
+		sku {
+			name = "Linked"
+		}
+		identity {
+			type = "SystemAssigned"
+		}
+	}
+`, r.template(data), data.RandomInteger%100)
+}
+
+func (r DatadogMonitorResource) update(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+	provider "azurerm" {
+		features {}
+	  }
+	  
+	  resource "azurerm_resource_group" "test" {
+		name     = "acctest-datadog-%d"
+		location = "%s"
+	  }
+	resource "azurerm_datadog_monitor" "test" {
+		name = "test-terraform-%d"
+		resource_group_name = azurerm_resource_group.test.name
+		location = "EAST US 2 EUAP"
+		datadog_organization_properties {
+			api_key = "XXX"
+			application_key = "XXX"
+		}
+		user_info {
+			name          = "Vidhi Kothari"
+			email_address = "vidhi.kothari@microsoft.com"
+		}
+		sku {
+			name = "Linked"
+		}
+		identity {
+			type = "SystemAssigned"
+		}
+		monitoring_status = false
+		tags = {
+			ENV = "Test"
+		}
+	}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger%100)
+}
+
+func (r DatadogMonitorResource) requiresImport(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+	%s
+	resource "azurerm_datadog_monitor" "import" {
+	name                = azurerm_datadog_monitor.test.name
+	resource_group_name =  azurerm_resource_group.test.name
+	location            = azurerm_datadog_monitor.test.location
+	datadog_organization_properties {
+		api_key = "XXX"
+		application_key = "XXX"
+	}
+	user_info {
+		name          = "Vidhi Kothari"
+		email_address = "vidhi.kothari@microsoft.com"
+	}
+	sku {
+		name = "Linked"
+	}
+	identity {
+		type = "SystemAssigned"
+	}
+	}
+`, r.basic(data))
+}
+
+func (r DatadogMonitorResource) complete(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+	%s
+	resource "azurerm_datadog_monitor" "test" {
+	name                = "test-terraform-%d"
+	resource_group_name = azurerm_resource_group.test.name
+	location            = azurerm_resource_group.test.location
+	datadog_organization_properties {
+		api_key = "XXX"
+		application_key = "XXX"
+		enterprise_app_id = ""
+		linking_auth_code = ""
+		linking_client_id = ""
+		redirect_uri      = ""
+	}
+	identity {
+		type = "SystemAssigned"
+	}
+	sku {
+		name = "Linked"
+	}
+	user_info {
+		name          = "Vidhi Kothari"
+		email_address = "vidhi.kothari@microsoft.com"
+		phone_number  = ""
+	}
+	monitoring_status = true
+	tags = {
+		ENV = "Test"
+	}
+	}
+`, r.template(data), data.RandomInteger%100)
+}
