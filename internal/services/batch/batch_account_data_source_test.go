cfeeb4fbda4bbb1d6588a2ade72aaeeba38d2532
Matthew Frahry <mbfrahry@gmail.com>
2022-03-29 14:46:41 -0700

    Fix cdn_endpoint_test

--- internal/services/batch/batch_account_data_source_test.go
+++ internal/services/batch/batch_account_data_source_test.go
@@ -217,7 +217,8 @@ func (BatchAccountDataSource) cmkData(data acceptance.TestData, tenantID string)
 provider "azurerm" {
   features {
     key_vault {
-      purge_soft_delete_on_destroy = false
+      purge_soft_delete_on_destroy       = false
+      purge_soft_deleted_keys_on_destroy = false
     }
   }
 }
