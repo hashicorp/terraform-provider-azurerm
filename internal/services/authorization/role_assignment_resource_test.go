4c299e35db006eb518144078efad94b339e9ea80
Matthew Frahry <mbfrahry@gmail.com>
2022-03-29 13:38:54 -0700

    Fix authorization tests

--- internal/services/authorization/role_assignment_resource_test.go
+++ internal/services/authorization/role_assignment_resource_test.go
@@ -610,16 +610,14 @@ data "azurerm_role_definition" "test" {
   name = "Monitoring Reader"
 }
 
-resource "azurerm_management_group" "test" {
-  group_id = "%s"
-}
+resource "azurerm_management_group" "test" {}
 
 resource "azurerm_role_assignment" "test" {
   scope              = azurerm_management_group.test.id
   role_definition_id = data.azurerm_role_definition.test.id
   principal_id       = data.azurerm_client_config.test.object_id
 }
-`, groupId)
+`)
 }
 
 func (RoleAssignmentResource) condition(groupId string) string {
