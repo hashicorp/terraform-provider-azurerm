6d29ad4f2ddb9733c78f0d0abe62276920932cac
Steph <steph@hashicorp.com>
2022-04-01 15:33:11 +0200

    remove alt subscription from testing

--- internal/services/containers/kubernetes_cluster_other_resource_test.go
+++ internal/services/containers/kubernetes_cluster_other_resource_test.go
@@ -1876,13 +1876,11 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "test" {
-  provider = azurerm-alt
   name     = "acctestRG-aks-%d"
   location = "%s"
 }
 
 resource "azurerm_kubernetes_cluster" "test" {
-  provider            = azurerm-alt
   name                = "acctestaks%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
