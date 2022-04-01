2f84273b3a52717ed5501d8d07f7683e8cf5e9a2
Matthew Frahry <mbfrahry@gmail.com>
2022-04-01 09:03:37 -0700

    Test Fixes - Post 3.0 (#16199)

--- internal/services/loadbalancer/loadbalancer_rule_data_source_test.go
+++ internal/services/loadbalancer/loadbalancer_rule_data_source_test.go
@@ -86,9 +86,8 @@ resource "azurerm_lb_probe" "test" {
 }
 
 resource "azurerm_lb_rule" "test" {
-  name                = "LbRule-%s"
-  resource_group_name = azurerm_resource_group.test.name
-  loadbalancer_id     = azurerm_lb.test.id
+  name            = "LbRule-%s"
+  loadbalancer_id = azurerm_lb.test.id
 
   protocol      = "Tcp"
   frontend_port = 3389
--- internal/services/maintenance/maintenance_assignment_virtual_machine_scale_set_resource_test.go
+++ internal/services/maintenance/maintenance_assignment_virtual_machine_scale_set_resource_test.go
@@ -127,7 +127,6 @@ resource "azurerm_lb_probe" "test" {
 
 resource "azurerm_lb_rule" "test" {
   name                           = "AccTestLBRule"
-  resource_group_name            = azurerm_resource_group.test.name
   loadbalancer_id                = azurerm_lb.test.id
   probe_id                       = azurerm_lb_probe.test.id
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.test.id]
