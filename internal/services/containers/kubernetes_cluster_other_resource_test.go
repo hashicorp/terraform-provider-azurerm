4a47d41a719c72c10f59c2ee079a7644feb104f9
Steph <steph@hashicorp.com>
2022-04-07 13:18:55 +0200

    terrafmt

--- internal/services/containers/kubernetes_cluster_other_resource_test.go
+++ internal/services/containers/kubernetes_cluster_other_resource_test.go
@@ -1944,10 +1944,10 @@ resource "azurerm_log_analytics_workspace" "test" {
   sku                 = "PerGB2018"
 }
 resource "azurerm_kubernetes_cluster" "test" {
-  name                = "acctestaks%d"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  dns_prefix          = "acctestaks%d"
+  name                              = "acctestaks%d"
+  location                          = azurerm_resource_group.test.location
+  resource_group_name               = azurerm_resource_group.test.name
+  dns_prefix                        = "acctestaks%d"
   role_based_access_control_enabled = true
   default_node_pool {
     name       = "default"
@@ -1980,10 +1980,10 @@ resource "azurerm_log_analytics_workspace" "test" {
   sku                 = "PerGB2018"
 }
 resource "azurerm_kubernetes_cluster" "test" {
-  name                = "acctestaks%d"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  dns_prefix          = "acctestaks%d"
+  name                              = "acctestaks%d"
+  location                          = azurerm_resource_group.test.location
+  resource_group_name               = azurerm_resource_group.test.name
+  dns_prefix                        = "acctestaks%d"
   role_based_access_control_enabled = true
   default_node_pool {
     name       = "default"
