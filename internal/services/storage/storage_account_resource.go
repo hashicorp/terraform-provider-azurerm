b8419fdac38db3f7fc2624dcce01587d3cae8148
magodo <wztdyl@sina.com>
2022-04-27 15:15:37 +0800

    typo

--- internal/services/storage/storage_account_resource.go
+++ internal/services/storage/storage_account_resource.go
@@ -510,7 +510,7 @@ func resourceStorageAccount() *pluginsdk.Resource {
 										Required:     true,
 										ValidateFunc: validation.StringIsNotEmpty,
 									},
-									// TODO 4.0: Remove this property and determine whether to enable based on existance of the out side block.
+									// TODO 4.0: Remove this property and determine whether to enable based on existence of the out side block.
 									"enabled": {
 										Type:     pluginsdk.TypeBool,
 										Required: true,
@@ -539,7 +539,7 @@ func resourceStorageAccount() *pluginsdk.Resource {
 										Required:     true,
 										ValidateFunc: validation.StringIsNotEmpty,
 									},
-									// TODO 4.0: Remove this property and determine whether to enable based on existance of the out side block.
+									// TODO 4.0: Remove this property and determine whether to enable based on existence of the out side block.
 									"enabled": {
 										Type:     pluginsdk.TypeBool,
 										Required: true,
