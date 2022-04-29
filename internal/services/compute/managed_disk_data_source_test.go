5a96e8b5e21c92a0fa6a1dbe333fc6cd9271471a
Yichun Ma <yicma@microsoft.com>
2022-04-29 14:14:32 +0800

    Fix test

--- internal/services/compute/managed_disk_data_source_test.go
+++ internal/services/compute/managed_disk_data_source_test.go
@@ -144,8 +144,10 @@ func (ManagedDiskDataSource) encryptionSettings(data acceptance.TestData) string
 provider "azurerm" {
   features {
     key_vault {
-      recover_soft_deleted_key_vaults = false
-      purge_soft_delete_on_destroy    = false
+      recover_soft_deleted_key_vaults       = false
+      purge_soft_delete_on_destroy          = false
+      purge_soft_deleted_keys_on_destroy    = false
+      purge_soft_deleted_secrets_on_destroy = false
     }
   }
 }
