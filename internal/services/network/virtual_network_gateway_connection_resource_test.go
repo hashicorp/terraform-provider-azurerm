63d08c5dc374015902ff8e1da9594e974c46e54d
HappyTobi <happytobi@tscoding.de>
2022-05-03 12:24:51 +0200

    [FIX] add bracket

--- internal/services/network/virtual_network_gateway_connection_resource_test.go
+++ internal/services/network/virtual_network_gateway_connection_resource_test.go
@@ -1072,6 +1072,7 @@ resource "azurerm_public_ip" "test" {
   resource_group_name = azurerm_resource_group.test.name
   allocation_method   = "Static"
   sku                 = "Standard"
+}
 
 resource "azurerm_virtual_network_gateway" "test" {
   name                = "acctestgw-%d"
