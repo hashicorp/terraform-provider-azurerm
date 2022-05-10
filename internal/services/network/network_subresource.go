f226143412dd0c268869008a23ed934da898da0a
C5306931 <happytobi@tscoding.de>
2022-05-10 09:47:14 +0200

    [UPDATE] imports after rebase

--- internal/services/network/network_subresource.go
+++ internal/services/network/network_subresource.go
@@ -2,7 +2,6 @@ package network
 
 import (
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2021-08-01/network"
-	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
 func flattenNetworkSubResourceID(input *[]network.SubResource) []interface{} {
