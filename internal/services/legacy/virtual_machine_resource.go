0e82e97123bf053c53e1c2bc13c86a5d17896a34
Zhenhua Hu <zhhu@microsoft.com>
2022-05-11 14:32:30 +0800

    upgrade api version

--- internal/services/legacy/virtual_machine_resource.go
+++ internal/services/legacy/virtual_machine_resource.go
@@ -12,7 +12,7 @@ import (
 	compute2 "github.com/hashicorp/terraform-provider-azurerm/internal/services/compute"
 
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2021-11-01/compute"
-	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2021-05-01/network"
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2021-08-01/network"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
