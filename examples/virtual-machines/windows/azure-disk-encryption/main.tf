f6d1cee9f8498a7119ad917932e726db4baea8cc
Yichun Ma <yicma@microsoft.com>
2022-05-11 11:43:00 +0800

    Fix keyvault

--- examples/virtual-machines/windows/azure-disk-encryption/main.tf
+++ examples/virtual-machines/windows/azure-disk-encryption/main.tf
@@ -1,7 +1,8 @@
 provider "azurerm" {
   features {
     key_vault {
-      purge_soft_delete_on_destroy = false
+      purge_soft_delete_on_destroy       = false
+      purge_soft_deleted_keys_on_destroy = false
     }
   }
 }
@@ -29,16 +30,16 @@ resource "azurerm_key_vault_access_policy" "service-principal" {
   object_id    = data.azurerm_client_config.current.object_id
 
   key_permissions = [
-    "create",
-    "delete",
-    "get",
-    "update",
+    "Create",
+    "Delete",
+    "Get",
+    "Update",
   ]
 
   secret_permissions = [
-    "get",
-    "delete",
-    "set",
+    "Get",
+    "Delete",
+    "Set",
   ]
 }
 
