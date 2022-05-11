dc1cb9a2ef49f76e1355ac75ac00284aca760b50
Yichun Ma <yicma@microsoft.com>
2022-05-11 11:32:01 +0800

    Rollback unnedded change on disk

--- examples/managed-disks/azure-disk-encryption/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "prefix" {
-  description = "The Prefix used for all resources in this example"
-}
-
-variable "location" {
-  description = "The Azure Region in which all resources in this example should be created."
-  default     = "North Europe"
-}
