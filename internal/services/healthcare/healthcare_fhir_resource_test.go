d0d4ecc6ea389a44c8e7107e9e630b58a97fc21d
xiaxyi <xiaxyi@microsoft.com>
2022-05-11 08:09:13 +0800

    update fmt

--- internal/services/healthcare/healthcare_fhir_resource_test.go
+++ internal/services/healthcare/healthcare_fhir_resource_test.go
@@ -369,7 +369,7 @@ resource "azurerm_healthcare_fhir_service" "test" {
     audience  = "https://acctestfhir.fhir.azurehealthcareapis.com"
   }
 
- access_policy_object_ids = [
+  access_policy_object_ids = [
     data.azurerm_client_config.current.object_id
   ]
 
