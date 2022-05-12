22f20fb8b685fe4c21fb73f2ed8bd6f8ad9e18fb
xiaxyi <xiaxyi@microsoft.com>
2022-05-12 18:09:16 +0800

    update fmt

--- internal/services/batch/batch_pool_data_source_test.go
+++ internal/services/batch/batch_pool_data_source_test.go
@@ -73,7 +73,7 @@ resource "azurerm_subnet" "test" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.test.name
   virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes       = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "test" {
