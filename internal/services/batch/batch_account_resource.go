47899486243989b56e4b8bd83c8792658a1cedd5
Zooopx <xuzhang3@microsoft.com>
2022-05-12 18:08:12 +0800

    format
    Signed-off-by: Zooopx <xuzhang3@microsoft.com>

--- internal/services/batch/batch_account_resource.go
+++ internal/services/batch/batch_account_resource.go
@@ -2,13 +2,13 @@ package batch
 
 import (
 	"fmt"
-	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
 	"log"
 	"strings"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/batch/mgmt/2021-06-01/batch"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/identity"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
