72ecaa5d21fdae260f995d310f48df73f55fadd5
Heng Lu <79895375+ms-henglu@users.noreply.github.com>
2022-05-13 11:23:31 +0800

    `azurerm_spring_cloud_app` supports `identity` (#16280)

--- internal/services/springcloud/spring_cloud_app_resource_test.go
+++ internal/services/springcloud/spring_cloud_app_resource_test.go
@@ -143,6 +143,42 @@ func TestAccSpringCloudApp_update(t *testing.T) {
 	})
 }
 
+func TestAccSpringCloudApp_identity(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_spring_cloud_app", "test")
+	r := SpringCloudAppResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.identityUserAssigned(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.identitySystemAssigned(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.identitySystemAssignedUserAssigned(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func (r SpringCloudAppResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	id, err := parse.SpringCloudAppID(state.ID)
 	if err != nil {
@@ -186,6 +222,12 @@ func (r SpringCloudAppResource) complete(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 %s
 
+resource "azurerm_user_assigned_identity" "test" {
+  name                = "acctestuai-%d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+}
+
 resource "azurerm_spring_cloud_app" "test" {
   name                = "acctest-sca-%d"
   resource_group_name = azurerm_spring_cloud_service.test.resource_group_name
@@ -195,7 +237,8 @@ resource "azurerm_spring_cloud_app" "test" {
   tls_enabled         = true
 
   identity {
-    type = "SystemAssigned"
+    type         = "SystemAssigned, UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.test.id]
   }
 
   persistent_disk {
@@ -203,7 +246,69 @@ resource "azurerm_spring_cloud_app" "test" {
     mount_path = "/persistent"
   }
 }
+`, r.template(data), data.RandomInteger, data.RandomInteger)
+}
+
+func (r SpringCloudAppResource) identitySystemAssigned(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_spring_cloud_app" "test" {
+  name                = "acctest-sca-%d"
+  resource_group_name = azurerm_spring_cloud_service.test.resource_group_name
+  service_name        = azurerm_spring_cloud_service.test.name
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
 `, r.template(data), data.RandomInteger)
+}
+
+func (r SpringCloudAppResource) identityUserAssigned(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_user_assigned_identity" "test" {
+  name                = "acctestuai-%d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+}
+
+resource "azurerm_spring_cloud_app" "test" {
+  name                = "acctest-sca-%d"
+  resource_group_name = azurerm_spring_cloud_service.test.resource_group_name
+  service_name        = azurerm_spring_cloud_service.test.name
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.test.id]
+  }
+}
+`, r.template(data), data.RandomInteger, data.RandomInteger)
+}
+
+func (r SpringCloudAppResource) identitySystemAssignedUserAssigned(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_user_assigned_identity" "test" {
+  name                = "acctestuai-%d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+}
+
+resource "azurerm_spring_cloud_app" "test" {
+  name                = "acctest-sca-%d"
+  resource_group_name = azurerm_spring_cloud_service.test.resource_group_name
+  service_name        = azurerm_spring_cloud_service.test.name
+
+  identity {
+    type         = "SystemAssigned, UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.test.id]
+  }
+}
+`, r.template(data), data.RandomInteger, data.RandomInteger)
 }
 
 func (r SpringCloudAppResource) customPersistentDisksWith(data acceptance.TestData, storageLabel string) string {
