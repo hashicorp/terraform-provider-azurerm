c83af6dcb45e6761ddd3d16d5186353189dd992f
Tobias Schug <4014687+HappyTobi@users.noreply.github.com>
2022-05-13 03:57:06 +0200

    `azurerm_virtual_network_gateway_connection` Add new feature to set `custom_bgp_addresses` (#16631)

--- internal/services/loadbalancer/rule_resource.go
+++ internal/services/loadbalancer/rule_resource.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/validate"
 	loadBalancerValidate "github.com/hashicorp/terraform-provider-azurerm/internal/services/loadbalancer/validate"
 
-	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2021-05-01/network"
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2021-08-01/network"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/locks"
