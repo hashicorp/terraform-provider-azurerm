050d1f07e2044df8b00e414bf5c4fa0b08efdae3
jackofallops <ste@hashicorp.com>
2022-05-25 14:49:20 +0100

    refresh windows-container example to make use of 3.x resources

--- examples/app-service/windows-basic/outputs.tf
+++ examples/app-service/windows-basic/outputs.tf
@@ -2,6 +2,6 @@ output "app_name" {
   value = azurerm_windows_web_app.example.name
 }
 
-output "app_service_default_hostname" {
+output "app_url" {
   value = "https://${azurerm_windows_web_app.example.default_hostname}"
 }
