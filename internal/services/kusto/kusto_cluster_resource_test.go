74e2106788156c25b591d0cedf7916268876eb20
Yun Liu <yunliu1@microsoft.com>
2022-05-26 11:06:51 +0800

    Fix a testing error. Availability zone 1 is not suitable for Dev 1 sku.

--- internal/services/kusto/kusto_cluster_resource_test.go
+++ internal/services/kusto/kusto_cluster_resource_test.go
@@ -157,7 +157,7 @@ func TestAccKustoCluster_zones(t *testing.T) {
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 				check.That(data.ResourceName).Key("zones.#").HasValue("1"),
-				check.That(data.ResourceName).Key("zones.0").HasValue("1"),
+				check.That(data.ResourceName).Key("zones.0").HasValue("3"),
 			),
 		},
 	})
@@ -564,7 +564,7 @@ resource "azurerm_kusto_cluster" "test" {
     capacity = 1
   }
 
-  zones = ["1"]
+  zones = ["3"]
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomString)
 }
