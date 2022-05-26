47fc46fd2f18c3e78322db72596d45c31a688239
Vladimir Lazarenko <favoretti@gmail.com>
2022-05-26 09:00:22 +0200

    Fix virtualNetwork acceptance test

--- internal/services/springcloud/spring_cloud_service_resource_test.go
+++ internal/services/springcloud/spring_cloud_service_resource_test.go
@@ -389,7 +389,11 @@ resource "azurerm_spring_cloud_service" "test" {
 func (SpringCloudServiceResource) virtualNetwork(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 provider "azuread" {}
