b04cf86a2db81b5376e9a5313121ef388a19d50e
jackofallops <11830746+jackofallops@users.noreply.github.com>
2022-05-27 01:09:39 +0100

    Update `examples/app-service/**` for 3.x resources (#16969)

---
+++ examples/app-service/linux-web-app-zip-deploy/outputs.tf
@@ -0,0 +1,16 @@
+output "app_name" {
+  value = azurerm_linux_web_app.example.name
+}
+
+output "app_url" {
+  value = "https://${azurerm_linux_web_app.example.default_hostname}"
+}
+
+output "app_uptime" {
+  value = "https://${azurerm_linux_web_app.example.default_hostname}/uptime"
+}
+
+output "app_healthcheck_endpoint" {
+  value = "https://${azurerm_linux_web_app.example.default_hostname}/health"
+}
+
