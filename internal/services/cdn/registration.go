9caf3de53768038f9ed25895de03c566141124eb
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-06-01 18:17:03 +0200

    New Data Source: `azurerm_cdn_frontdoor_profile`
    Co-authored-by: Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>

--- internal/services/cdn/registration.go
+++ internal/services/cdn/registration.go
@@ -28,7 +28,11 @@ func (r Registration) WebsiteCategories() []string {
 // SupportedDataSources returns the supported Data Sources supported by this Service
 func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
 	return map[string]*pluginsdk.Resource{
+		// CDN
 		"azurerm_cdn_profile": dataSourceCdnProfile(),
+
+		// FrontDoor
+		"azurerm_cdn_frontdoor_profile": dataSourceCdnFrontDoorProfile(),
 	}
 }
 
