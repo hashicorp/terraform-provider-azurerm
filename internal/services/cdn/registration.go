1f186a0b4e422a971ed136e7b0e01855b1966cec
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-06-03 13:20:54 +0200

    New Resource: `azurerm_cdn_frontdoor_origin`
    Splitting this out from #16671
    
    Co-authored-by: Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>

--- internal/services/cdn/registration.go
+++ internal/services/cdn/registration.go
@@ -48,6 +48,7 @@ func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
 
 		// FrontDoor
 		"azurerm_cdn_frontdoor_endpoint":     resourceCdnFrontDoorEndpoint(),
+		"azurerm_cdn_frontdoor_origin":       resourceCdnFrontDoorOrigin(),
 		"azurerm_cdn_frontdoor_origin_group": resourceCdnFrontDoorOriginGroup(),
 		"azurerm_cdn_frontdoor_profile":      resourceCdnFrontDoorProfile(),
 	}
