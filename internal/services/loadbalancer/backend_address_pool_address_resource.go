066d53ca3e5fd69729ebf7481146899f1d9ce249
xiaxyi <xiaxyi@microsoft.com>
2022-06-20 07:56:00 +0800

    add waiter

--- internal/services/loadbalancer/backend_address_pool_address_resource.go
+++ internal/services/loadbalancer/backend_address_pool_address_resource.go
@@ -267,6 +267,7 @@ func (r BackendAddressPoolAddressResource) Delete() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
 			client := metadata.Client.LoadBalancers.LoadBalancerBackendAddressPoolsClient
+			lbClient := metadata.Client.LoadBalancers.LoadBalancersClient
 			id, err := parse.BackendAddressPoolAddressID(metadata.ResourceData.Id())
 			if err != nil {
 				return err
@@ -281,6 +282,20 @@ func (r BackendAddressPoolAddressResource) Delete() sdk.ResourceFunc {
 			}
 			if pool.BackendAddressPoolPropertiesFormat == nil {
 				return fmt.Errorf("retrieving %s: `properties` was nil", *id)
+			}
+
+			timeout, _ := ctx.Deadline()
+			lbStatus := &pluginsdk.StateChangeConf{
+				Pending:                   []string{string(network.ProvisioningStateUpdating)},
+				Target:                    []string{string(network.ProvisioningStateSucceeded)},
+				MinTimeout:                5 * time.Second,
+				Refresh:                   loadbalacnerProvisioningStatusRefreshFunc(ctx, lbClient, *id),
+				ContinuousTargetOccurence: 10,
+				Timeout:                   time.Until(timeout),
+			}
+
+			if _, err := lbStatus.WaitForStateContext(ctx); err != nil {
+				return fmt.Errorf("waiting for parent resource loadbalancer status to be ready error: %+v", err)
 			}
 
 			addresses := make([]network.LoadBalancerBackendAddress, 0)
@@ -322,6 +337,7 @@ func (r BackendAddressPoolAddressResource) Update() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
 			client := metadata.Client.LoadBalancers.LoadBalancerBackendAddressPoolsClient
+			lbClient := metadata.Client.LoadBalancers.LoadBalancersClient
 			id, err := parse.BackendAddressPoolAddressID(metadata.ResourceData.Id())
 			if err != nil {
 				return err
@@ -373,6 +389,20 @@ func (r BackendAddressPoolAddressResource) Update() sdk.ResourceFunc {
 			}
 			pool.BackendAddressPoolPropertiesFormat.LoadBalancerBackendAddresses = &addresses
 
+			timeout, _ := ctx.Deadline()
+			lbStatus := &pluginsdk.StateChangeConf{
+				Pending:                   []string{string(network.ProvisioningStateUpdating)},
+				Target:                    []string{string(network.ProvisioningStateSucceeded)},
+				MinTimeout:                5 * time.Minute,
+				Refresh:                   loadbalacnerProvisioningStatusRefreshFunc(ctx, lbClient, *id),
+				ContinuousTargetOccurence: 10,
+				Timeout:                   time.Until(timeout),
+			}
+
+			if _, err := lbStatus.WaitForStateContext(ctx); err != nil {
+				return fmt.Errorf("waiting for parent resource loadbalancer status to be ready error: %+v", err)
+			}
+
 			future, err := client.CreateOrUpdate(ctx, id.ResourceGroup, id.LoadBalancerName, id.BackendAddressPoolName, pool)
 			if err != nil {
 				return fmt.Errorf("updating %s: %+v", *id, err)
@@ -383,5 +413,15 @@ func (r BackendAddressPoolAddressResource) Update() sdk.ResourceFunc {
 			return nil
 		},
 		Timeout: 30 * time.Minute,
+	}
+}
+
+func loadbalacnerProvisioningStatusRefreshFunc(ctx context.Context, client *network.LoadBalancersClient, id parse.BackendAddressPoolAddressId) pluginsdk.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		lbClient, err := client.Get(ctx, id.ResourceGroup, id.LoadBalancerName, "")
+		if err != nil {
+			return nil, "", fmt.Errorf("retrieving load balancer error： %+v", err)
+		}
+		return lbClient, string(lbClient.ProvisioningState), nil
 	}
 }
