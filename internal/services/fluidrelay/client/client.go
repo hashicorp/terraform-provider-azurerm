29f6b24bb585ce4dee4b83fdf6666ad039facbe3
xuwu1 <xuwu1@microsoft.com>
2022-06-22 17:02:49 +0800

    use go-azure-sdk for pandora sdk intead of embed in azurerm

--- internal/services/fluidrelay/client/client.go
+++ internal/services/fluidrelay/client/client.go
@@ -1,8 +1,8 @@
 package client
 
 import (
+	servers "github.com/hashicorp/go-azure-sdk/resource-manager/fluidrelay/2022-05-26/fluidrelayservers"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
-	servers "github.com/hashicorp/terraform-provider-azurerm/internal/services/fluidrelay/sdk/2022-05-26/fluidrelayservers"
 )
 
 type Client struct {
