a3f44f7b05ae068a1526e89bc071b00b0a895b43
bubbletroubles <42738824+bubbletroubles@users.noreply.github.com>
2022-06-22 19:36:12 +1000

    Update linux_web_app_resource_test.go

--- internal/services/appservice/linux_web_app_resource_test.go
+++ internal/services/appservice/linux_web_app_resource_test.go
@@ -2831,6 +2831,8 @@ resource "azurerm_linux_web_app" "test" {
   location                  = azurerm_resource_group.test.location
   resource_group_name       = azurerm_resource_group.test.name
   service_plan_id           = azurerm_service_plan.test.id
+
+  site_config {}
 }
 `, r.baseTemplate(data), data.RandomInteger, data.RandomInteger)
 }
@@ -2870,6 +2872,8 @@ resource "azurerm_linux_web_app" "test" {
   resource_group_name       = azurerm_resource_group.test.name
   service_plan_id           = azurerm_service_plan.test.id
   virtual_network_subnet_id = azurerm_subnet.test.id
+
+  site_config {}
 }
 `, r.baseTemplate(data), data.RandomInteger, data.RandomInteger)
 }
