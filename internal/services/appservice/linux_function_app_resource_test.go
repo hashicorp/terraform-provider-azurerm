8483b439213242b12b7e8848308d5432c279b863
xiaxin18 <xiaxyi@microsoft.com>
2022-06-27 14:10:45 +0800

    update

--- internal/services/appservice/linux_function_app_resource_test.go
+++ internal/services/appservice/linux_function_app_resource_test.go
@@ -462,7 +462,7 @@ func TestAccLinuxFunctionApp_elasticPremiumComplete(t *testing.T) {
 			Config: r.elasticComplete(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
-				check.That(data.ResourceName).Key("elastic_instance_minimum").HasValue("5"),
+				check.That(data.ResourceName).Key("site_config.0.elastic_instance_minimum").HasValue("5"),
 			),
 		},
 		data.ImportStep(),
