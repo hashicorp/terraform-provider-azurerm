173ba04eed29ad4313e8b0b7d6b1d265c07241b2
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-07-01 09:25:54 +0100

    hsm: dependencies

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/hardwaresecuritymodules/2018-10-31-preview/dedicatedhsms/model_dedicatedhsmproperties.go
@@ -0,0 +1,11 @@
+package dedicatedhsms
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type DedicatedHsmProperties struct {
+	NetworkProfile    *NetworkProfile `json:"networkProfile,omitempty"`
+	ProvisioningState *JsonWebKeyType `json:"provisioningState,omitempty"`
+	StampId           *string         `json:"stampId,omitempty"`
+	StatusMessage     *string         `json:"statusMessage,omitempty"`
+}
