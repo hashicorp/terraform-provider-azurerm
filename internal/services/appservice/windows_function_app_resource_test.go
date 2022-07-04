a4f6af64e41248c04847da39c4333b91f875c5b4
Benedikt Fuchs <benedikt.fuchs@crayon.com>
2022-07-04 09:53:44 +0200

    windows_function_app_resource_test use mount_path the same way as in windows_web_app_resource_test.go

--- internal/services/appservice/windows_function_app_resource_test.go
+++ internal/services/appservice/windows_function_app_resource_test.go
@@ -2911,7 +2911,7 @@ resource "azurerm_windows_function_app" "test" {
     account_name = azurerm_storage_account.test.name
     share_name   = azurerm_storage_share.test.name
     access_key   = azurerm_storage_account.test.primary_access_key
-    mount_path   = "/files"
+    mount_path   = "\\mounts\\files"
   }
 
 }
@@ -2942,7 +2942,7 @@ resource "azurerm_windows_function_app" "test" {
     account_name = azurerm_storage_account.test.name
     share_name   = azurerm_storage_share.test.name
     access_key   = azurerm_storage_account.test.primary_access_key
-    mount_path   = "/blob"
+    mount_path   = "\\mounts\\blob"
   }
 
 }
