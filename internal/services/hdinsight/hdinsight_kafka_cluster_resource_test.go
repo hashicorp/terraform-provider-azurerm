3d3873ac4ac973139cb6dc01cd666c0d23eff45d
JT <100274846+jiaweitao001@users.noreply.github.com>
2022-07-06 08:19:34 +0800

    `azurerm_hdinsight_kafka_cluster` add support for `network` block (#17259)

--- internal/services/hdinsight/hdinsight_kafka_cluster_resource_test.go
+++ internal/services/hdinsight/hdinsight_kafka_cluster_resource_test.go
@@ -863,9 +863,14 @@ resource "azurerm_hdinsight_kafka_cluster" "test" {
   }
 
   storage_account {
+    storage_resource_id  = azurerm_storage_account.test.id
     storage_container_id = azurerm_storage_container.test.id
     storage_account_key  = azurerm_storage_account.test.primary_access_key
     is_default           = true
+  }
+
+  network {
+    connection_direction = "Outbound"
   }
 
   roles {
