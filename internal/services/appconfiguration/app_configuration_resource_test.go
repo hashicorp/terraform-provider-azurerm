c55ae32228920471ffd0e2f1192e9a3bb69abdef
Lucas Fernández <lucas.fernandez@southworks.com>
2022-07-07 14:30:29 -0300

    Updating tests.

--- internal/services/appconfiguration/app_configuration_resource_test.go
+++ internal/services/appconfiguration/app_configuration_resource_test.go
@@ -203,10 +203,11 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_app_configuration" "test" {
-  name                = "testaccappconf%d"
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-  sku                 = "standard"
+  name                  = "testaccappconf%d"
+  resource_group_name   = azurerm_resource_group.test.name
+  location              = azurerm_resource_group.test.location
+  public_network_access = "Disabled"
+  sku                   = "standard"
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
 }
