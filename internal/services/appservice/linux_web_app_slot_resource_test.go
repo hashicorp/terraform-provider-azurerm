51dd16f1f7346130c1dd8301c3b122343aa7cea8
Your Name <you@example.com>
2022-07-15 09:23:58 +1000

    make terrafmt

--- internal/services/appservice/linux_web_app_slot_resource_test.go
+++ internal/services/appservice/linux_web_app_slot_resource_test.go
@@ -2202,34 +2202,34 @@ resource "azurerm_virtual_network" "test" {
 }
 
 resource "azurerm_subnet" "test1" {
-	name                 = "subnet1"
-	resource_group_name  = azurerm_resource_group.test.name
-	virtual_network_name = azurerm_virtual_network.test.name
-	address_prefixes     = ["10.0.1.0/24"]
-  
-	delegation {
-	  name = "delegation"
-  
-	  service_delegation {
-		name    = "Microsoft.Web/serverFarms"
-		actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-	  }
-	}
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
   }
-  
+}
+
 resource "azurerm_subnet" "test2" {
-	name                 = "subnet2"
-	resource_group_name  = azurerm_resource_group.test.name
-	virtual_network_name = azurerm_virtual_network.test.name
-	address_prefixes     = ["10.0.2.0/24"]
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["10.0.2.0/24"]
 
-	delegation {
-	name = "delegation"
+  delegation {
+    name = "delegation"
 
-	service_delegation {
-		name    = "Microsoft.Web/serverFarms"
-		actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-	}
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
   }
 }
 
@@ -2275,18 +2275,18 @@ resource "azurerm_subnet" "test1" {
 }
 
 resource "azurerm_subnet" "test2" {
-	name                 = "subnet2"
-	resource_group_name  = azurerm_resource_group.test.name
-	virtual_network_name = azurerm_virtual_network.test.name
-	address_prefixes     = ["10.0.2.0/24"]
-  
-	delegation {
-	  name = "delegation"
-  
-	  service_delegation {
-		name    = "Microsoft.Web/serverFarms"
-		actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-	}
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
   }
 }
 
@@ -2332,18 +2332,18 @@ resource "azurerm_subnet" "test1" {
 }
 
 resource "azurerm_subnet" "test2" {
-	name                 = "subnet2"
-	resource_group_name  = azurerm_resource_group.test.name
-	virtual_network_name = azurerm_virtual_network.test.name
-	address_prefixes     = ["10.0.2.0/24"]
-  
-	delegation {
-	  name = "delegation"
-  
-	  service_delegation {
-		name    = "Microsoft.Web/serverFarms"
-		actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-	}
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
   }
 }
 
