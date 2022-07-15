396d34e973859c6df9a727b77c2d3e22b304e881
Andy McCullough <amccullough84@gmail.com>
2022-07-15 18:20:36 +0100

    fix linting

--- internal/services/desktopvirtualization/virtual_desktop_host_pool_resource_test.go
+++ internal/services/desktopvirtualization/virtual_desktop_host_pool_resource_test.go
@@ -151,18 +151,18 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_virtual_desktop_host_pool" "test" {
-  name                            = "acctestHP%s"
-  location                        = azurerm_resource_group.test.location
-  resource_group_name             = azurerm_resource_group.test.name
-  type                            = "Pooled"
-  validate_environment            = true
-  load_balancer_type              = "BreadthFirst"
+  name                 = "acctestHP%s"
+  location             = azurerm_resource_group.test.location
+  resource_group_name  = azurerm_resource_group.test.name
+  type                 = "Pooled"
+  validate_environment = true
+  load_balancer_type   = "BreadthFirst"
   scheduled_agent_updates {
-	enabled = true
-	schedule {
-		day_of_week = "Saturday"
-    	hour_of_day = 2
-	}
+    enabled = true
+    schedule {
+      day_of_week = "Saturday"
+      hour_of_day = 2
+    }
   }
 }
 `, data.RandomInteger, data.Locations.Secondary, data.RandomString)
@@ -180,23 +180,23 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_virtual_desktop_host_pool" "test" {
-  name                                              = "acctestHP%s"
-  location                                          = azurerm_resource_group.test.location
-  resource_group_name                               = azurerm_resource_group.test.name
-  type                                              = "Pooled"
-  validate_environment                              = true
-  load_balancer_type                                = "BreadthFirst"
+  name                 = "acctestHP%s"
+  location             = azurerm_resource_group.test.location
+  resource_group_name  = azurerm_resource_group.test.name
+  type                 = "Pooled"
+  validate_environment = true
+  load_balancer_type   = "BreadthFirst"
   scheduled_agent_updates {
-	enabled = true
-	use_session_host_timezone = true
-	schedule {
-		day_of_week = "Saturday"
-    	hour_of_day = 2
-	}
-	schedule {
-		day_of_week = "Sunday"
-    	hour_of_day = 2
-	}
+    enabled                   = true
+    use_session_host_timezone = true
+    schedule {
+      day_of_week = "Saturday"
+      hour_of_day = 2
+    }
+    schedule {
+      day_of_week = "Sunday"
+      hour_of_day = 2
+    }
   }
 }
 `, data.RandomInteger, data.Locations.Secondary, data.RandomString)
@@ -214,15 +214,15 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_virtual_desktop_host_pool" "test" {
-  name                                              = "acctestHP%s"
-  location                                          = azurerm_resource_group.test.location
-  resource_group_name                               = azurerm_resource_group.test.name
-  type                                              = "Pooled"
-  validate_environment                              = true
-  load_balancer_type                                = "BreadthFirst"
+  name                 = "acctestHP%s"
+  location             = azurerm_resource_group.test.location
+  resource_group_name  = azurerm_resource_group.test.name
+  type                 = "Pooled"
+  validate_environment = true
+  load_balancer_type   = "BreadthFirst"
   scheduled_agent_updates {
-	enabled = false
-	use_session_host_timezone = true
+    enabled                   = false
+    use_session_host_timezone = true
   }
 }
 `, data.RandomInteger, data.Locations.Secondary, data.RandomString)
