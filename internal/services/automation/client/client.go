bc73af4271bc0b7140b68c848743b1d826d9ae58
xuwu1 <xuwu1@microsoft.com>
2022-07-15 19:07:12 +0800

    new automation source control

--- internal/services/automation/client/client.go
+++ internal/services/automation/client/client.go
@@ -24,6 +24,7 @@ type Client struct {
 	RunBookWgClient             *hybridrunbookworkergroup.HybridRunbookWorkerGroupClient
 	RunbookWorkerClient         *hybridrunbookworker.HybridRunbookWorkerClient
 	ScheduleClient              *automation.ScheduleClient
+	SourceControlClient         *automation.SourceControlClient
 	VariableClient              *automation.VariableClient
 	WebhookClient               *automation.WebhookClient
 }
@@ -71,6 +72,9 @@ func NewClient(o *common.ClientOptions) *Client {
 	runbookWorkerClient := hybridrunbookworker.NewHybridRunbookWorkerClientWithBaseURI(o.ResourceManagerEndpoint)
 	o.ConfigureClient(&runbookWorkerClient.Client, o.ResourceManagerAuthorizer)
 
+	sourceCtlClient := automation.NewSourceControlClientWithBaseURI(o.ResourceManagerEndpoint, o.SubscriptionId)
+	o.ConfigureClient(&sourceCtlClient.Client, o.ResourceManagerAuthorizer)
+
 	scheduleClient := automation.NewScheduleClientWithBaseURI(o.ResourceManagerEndpoint, o.SubscriptionId)
 	o.ConfigureClient(&scheduleClient.Client, o.ResourceManagerAuthorizer)
 
@@ -96,6 +100,7 @@ func NewClient(o *common.ClientOptions) *Client {
 		RunBookWgClient:             &runbookWgClient,
 		RunbookWorkerClient:         &runbookWorkerClient,
 		ScheduleClient:              &scheduleClient,
+		SourceControlClient:         &sourceCtlClient,
 		VariableClient:              &variableClient,
 		WebhookClient:               &webhookClient,
 	}
