1ca5c103f2080eab7b742c241a2b7d02bfc510e8
stephybun <steph@hashicorp.com>
2022-07-18 03:35:05 +0200

    `servicebus` - switch to `go-azure-sdk` (#17628)
    * switch to go-azure-sdk
    
    * lint
    
    * remove unused parsers and validators
    
    * remove references to old parse functions
    
    * vendor
    
    * import ordering
    
    * fix casing of error message in test case

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/servicebus/2021-06-01-preview/rules/predicates.go
@@ -0,0 +1,24 @@
+package rules
+
+type RuleOperationPredicate struct {
+	Id   *string
+	Name *string
+	Type *string
+}
+
+func (p RuleOperationPredicate) Matches(input Rule) bool {
+
+	if p.Id != nil && (input.Id == nil && *p.Id != *input.Id) {
+		return false
+	}
+
+	if p.Name != nil && (input.Name == nil && *p.Name != *input.Name) {
+		return false
+	}
+
+	if p.Type != nil && (input.Type == nil && *p.Type != *input.Type) {
+		return false
+	}
+
+	return true
+}
