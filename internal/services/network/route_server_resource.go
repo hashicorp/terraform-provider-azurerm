2a1ea26efebb473584060b23a24ae0dfe14ad717
xiaxin18 <xiaxyi@microsoft.com>
2022-07-19 16:21:37 +0800

    update sdk

--- internal/services/network/route_server_resource.go
+++ internal/services/network/route_server_resource.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2021-05-01/network"
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2021-08-01/network"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
