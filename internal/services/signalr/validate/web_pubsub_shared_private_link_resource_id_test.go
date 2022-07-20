971915d0c807a825066d0e7c68817c00f2da3c55
Xiaxin <92154856+xiaxyi@users.noreply.github.com>
2022-07-20 11:32:26 +0800

    Adding shared private link resource for the Azure web pubsub resource (#15550)

--- internal/services/signalr/validate/web_pubsub_shared_private_link_resource_id_test.go
+++ internal/services/signalr/validate/web_pubsub_shared_private_link_resource_id_test.go
@@ -4,7 +4,7 @@ package validate
 
 import "testing"
 
-func TestWebPubsubID(t *testing.T) {
+func TestWebPubsubSharedPrivateLinkResourceID(t *testing.T) {
 	cases := []struct {
 		Input string
 		Valid bool
@@ -53,20 +53,32 @@ func TestWebPubsubID(t *testing.T) {
 		},
 
 		{
+			// missing SharedPrivateLinkResourceName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.SignalRService/webPubSub/Webpubsub1/",
+			Valid: false,
+		},
+
+		{
+			// missing value for SharedPrivateLinkResourceName
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.SignalRService/webPubSub/Webpubsub1/sharedPrivateLinkResources/",
+			Valid: false,
+		},
+
+		{
 			// valid
-			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.SignalRService/webPubSub/Webpubsub1",
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.SignalRService/webPubSub/Webpubsub1/sharedPrivateLinkResources/resource1",
 			Valid: true,
 		},
 
 		{
 			// upper-cased
-			Input: "/SUBSCRIPTIONS/12345678-1234-9876-4563-123456789012/RESOURCEGROUPS/RESGROUP1/PROVIDERS/MICROSOFT.SIGNALRSERVICE/WEBPUBSUB/WEBPUBSUB1",
+			Input: "/SUBSCRIPTIONS/12345678-1234-9876-4563-123456789012/RESOURCEGROUPS/RESGROUP1/PROVIDERS/MICROSOFT.SIGNALRSERVICE/WEBPUBSUB/WEBPUBSUB1/SHAREDPRIVATELINKRESOURCES/RESOURCE1",
 			Valid: false,
 		},
 	}
 	for _, tc := range cases {
 		t.Logf("[DEBUG] Testing Value %s", tc.Input)
-		_, errors := WebPubsubID(tc.Input, "test")
+		_, errors := WebPubsubSharedPrivateLinkResourceID(tc.Input, "test")
 		valid := len(errors) == 0
 
 		if tc.Valid != valid {
