a0091304dfad94438dba8076c53ff4af649e98ba
xiaxin18 <xiaxyi@microsoft.com>
2022-07-25 18:09:33 +0800

    add support for azure search shared private links

--- internal/services/search/client/client.go
+++ internal/services/search/client/client.go
@@ -4,13 +4,15 @@ import (
 	"github.com/hashicorp/go-azure-sdk/resource-manager/search/2020-03-13/adminkeys"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/search/2020-03-13/querykeys"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/search/2020-03-13/services"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/search/2020-08-01/sharedprivatelinkresources"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
 
 type Client struct {
-	AdminKeysClient *adminkeys.AdminKeysClient
-	QueryKeysClient *querykeys.QueryKeysClient
-	ServicesClient  *services.ServicesClient
+	AdminKeysClient                       *adminkeys.AdminKeysClient
+	QueryKeysClient                       *querykeys.QueryKeysClient
+	ServicesClient                        *services.ServicesClient
+	SearchSharedPrivateLinkResourceClient *sharedprivatelinkresources.SharedPrivateLinkResourcesClient
 }
 
 func NewClient(o *common.ClientOptions) *Client {
@@ -23,9 +25,13 @@ func NewClient(o *common.ClientOptions) *Client {
 	servicesClient := services.NewServicesClientWithBaseURI(o.ResourceManagerEndpoint)
 	o.ConfigureClient(&servicesClient.Client, o.ResourceManagerAuthorizer)
 
+	searchSharedPrivateLinkResourceClient := sharedprivatelinkresources.NewSharedPrivateLinkResourcesClientWithBaseURI(o.ResourceManagerEndpoint)
+	o.ConfigureClient(&searchSharedPrivateLinkResourceClient.Client, o.ResourceManagerAuthorizer)
+
 	return &Client{
-		AdminKeysClient: &adminKeysClient,
-		QueryKeysClient: &queryKeysClient,
-		ServicesClient:  &servicesClient,
+		AdminKeysClient:                       &adminKeysClient,
+		QueryKeysClient:                       &queryKeysClient,
+		ServicesClient:                        &servicesClient,
+		SearchSharedPrivateLinkResourceClient: &searchSharedPrivateLinkResourceClient,
 	}
 }
