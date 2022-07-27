e9aa960052f4cfbd0ea6c0da26af583053563962
xiaxin18 <xiaxyi@microsoft.com>
2022-07-27 17:53:19 +0800

    remove test folder

--- test/testDatabricksModule/dataBricksCluster/output.tf
+++
@@ -1,3 +0,0 @@
-output "databricks_cluster_id" {
-  value = databricks_cluster.interactive_cluster.id
-}
--- test/testDatabricksModule/dataBricksWorkspace/output.tf
+++
@@ -1,7 +0,0 @@
-output "databricks_workspace_id"{
-  value = azurerm_databricks_workspace.test.workspace_id
-}
-
-output "databricks_workspace_URL" {
-  value = azurerm_databricks_workspace.test.workspace_url
-}
