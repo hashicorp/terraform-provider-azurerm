7b4195cfc52197176fe1f42b9a17124e61b359bf
xuzhang3 <Zhangxu894765>
2022-07-29 14:05:25 +0800

    support api management product tag

---
+++ internal/services/apimanagement/parse/product_tag.go
@@ -0,0 +1,81 @@
+package parse
+
+// NOTE: this file is generated via 'go:generate' - manual changes will be overwritten
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/resourceids"
+)
+
+type ProductTagId struct {
+	SubscriptionId string
+	ResourceGroup  string
+	ServiceName    string
+	ProductName    string
+	TagName        string
+}
+
+func NewProductTagID(subscriptionId, resourceGroup, serviceName, productName, tagName string) ProductTagId {
+	return ProductTagId{
+		SubscriptionId: subscriptionId,
+		ResourceGroup:  resourceGroup,
+		ServiceName:    serviceName,
+		ProductName:    productName,
+		TagName:        tagName,
+	}
+}
+
+func (id ProductTagId) String() string {
+	segments := []string{
+		fmt.Sprintf("Tag Name %q", id.TagName),
+		fmt.Sprintf("Product Name %q", id.ProductName),
+		fmt.Sprintf("Service Name %q", id.ServiceName),
+		fmt.Sprintf("Resource Group %q", id.ResourceGroup),
+	}
+	segmentsStr := strings.Join(segments, " / ")
+	return fmt.Sprintf("%s: (%s)", "Product Tag", segmentsStr)
+}
+
+func (id ProductTagId) ID() string {
+	fmtString := "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.ApiManagement/service/%s/products/%s/tags/%s"
+	return fmt.Sprintf(fmtString, id.SubscriptionId, id.ResourceGroup, id.ServiceName, id.ProductName, id.TagName)
+}
+
+// ProductTagID parses a ProductTag ID into an ProductTagId struct
+func ProductTagID(input string) (*ProductTagId, error) {
+	id, err := resourceids.ParseAzureResourceID(input)
+	if err != nil {
+		return nil, err
+	}
+
+	resourceId := ProductTagId{
+		SubscriptionId: id.SubscriptionID,
+		ResourceGroup:  id.ResourceGroup,
+	}
+
+	if resourceId.SubscriptionId == "" {
+		return nil, fmt.Errorf("ID was missing the 'subscriptions' element")
+	}
+
+	if resourceId.ResourceGroup == "" {
+		return nil, fmt.Errorf("ID was missing the 'resourceGroups' element")
+	}
+
+	if resourceId.ServiceName, err = id.PopSegment("service"); err != nil {
+		return nil, err
+	}
+	if resourceId.ProductName, err = id.PopSegment("products"); err != nil {
+		return nil, err
+	}
+	if resourceId.TagName, err = id.PopSegment("tags"); err != nil {
+		return nil, err
+	}
+
+	if err := id.ValidateNoEmptySegments(input); err != nil {
+		return nil, err
+	}
+
+	return &resourceId, nil
+}
