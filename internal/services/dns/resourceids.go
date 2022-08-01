ac3699370301de3a3e818c4824ef4d3cc56cc89d
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-08-01 18:26:01 +0100

    refactor: `dns_zone` now uses go-azure-sdk

--- internal/services/dns/resourceids.go
+++
@@ -1,3 +0,0 @@
-package dns
-
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=DnsZone -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Network/dnszones/zone1
