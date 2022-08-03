329401981c487840d7e1f3afcad93e0a16902132
xuwu1 <xuwu1@microsoft.com>
2022-08-03 14:36:53 +0800

    fix acc test

--- internal/services/firewall/firewall_policy_resource_test.go
+++ internal/services/firewall/firewall_policy_resource_test.go
@@ -127,13 +127,6 @@ func TestAccFirewallPolicy_updatePremium(t *testing.T) {
 			),
 		},
 		data.ImportStep(),
-		{
-			Config: r.basic(data),
-			Check: acceptance.ComposeTestCheckFunc(
-				check.That(data.ResourceName).ExistsInAzure(r),
-			),
-		},
-		data.ImportStep(),
 	})
 }
 
