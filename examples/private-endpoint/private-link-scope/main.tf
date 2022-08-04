0f73fdfeeb1f554ed395fca6bcee4fcfddee7fd5
Zhenhua Hu <zhhu@microsoft.com>
2022-08-04 12:57:48 +0800

    format

--- examples/private-endpoint/private-link-scope/main.tf
+++ examples/private-endpoint/private-link-scope/main.tf
@@ -25,10 +25,10 @@ resource "azurerm_virtual_network" "example" {
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "${var.prefix}-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
+  name                                          = "${var.prefix}-subnet"
+  resource_group_name                           = azurerm_resource_group.example.name
+  virtual_network_name                          = azurerm_virtual_network.example.name
+  address_prefixes                              = ["10.0.1.0/24"]
   enforce_private_link_service_network_policies = true
 }
 
