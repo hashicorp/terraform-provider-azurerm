8185c6c0aee6082d3cf50b44a526e85ba5057600
elena <sinbai@hotmail.com>
2022-08-11 10:44:26 +0800

    support host name configuration for api management gateway

--- internal/services/mssql/helper/sql_virtual_machine_storage_settings.go
+++ internal/services/mssql/helper/sql_virtual_machine_storage_settings.go
@@ -28,3 +28,37 @@ func StorageSettingSchema() *pluginsdk.Schema {
 		},
 	}
 }
+
+//DataFileCount     *int64   `json:"dataFileCount,omitempty"`
+//DataFileSize      *int64   `json:"dataFileSize,omitempty"`
+//DataGrowth        *int64   `json:"dataGrowth,omitempty"`
+//DefaultFilePath   *string  `json:"defaultFilePath,omitempty"`
+//LogFileSize       *int64   `json:"logFileSize,omitempty"`
+//LogGrowth         *int64   `json:"logGrowth,omitempty"`
+//Luns              *[]int64 `json:"luns,omitempty"`
+//PersistFolder     *bool    `json:"persistFolder,omitempty"`
+//PersistFolderPath *string  `json:"persistFolderPath,omitempty"`
+
+func TempDbSettings() *pluginsdk.Schema {
+	return &pluginsdk.Schema{
+		Type:     pluginsdk.TypeList,
+		Optional: true,
+		MaxItems: 1,
+		Elem: &pluginsdk.Resource{
+			Schema: map[string]*pluginsdk.Schema{
+				"luns": {
+					Type:     pluginsdk.TypeList,
+					Required: true,
+					Elem: &pluginsdk.Schema{
+						Type: pluginsdk.TypeInt,
+					},
+				},
+				"default_file_path": {
+					Type:         pluginsdk.TypeString,
+					Required:     true,
+					ValidateFunc: validation.StringIsNotEmpty,
+				},
+			},
+		},
+	}
+}
