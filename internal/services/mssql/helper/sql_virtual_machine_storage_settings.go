5a8dfd185eca19271e57d111b8c97d5a80ee0091
elena <sinbai@hotmail.com>
2022-08-11 13:45:10 +0800

    update code

--- internal/services/mssql/helper/sql_virtual_machine_storage_settings.go
+++ internal/services/mssql/helper/sql_virtual_machine_storage_settings.go
@@ -28,27 +28,3 @@ func StorageSettingSchema() *pluginsdk.Schema {
 		},
 	}
 }
-
-func TempDbSettings() *pluginsdk.Schema {
-	return &pluginsdk.Schema{
-		Type:     pluginsdk.TypeList,
-		Optional: true,
-		MaxItems: 1,
-		Elem: &pluginsdk.Resource{
-			Schema: map[string]*pluginsdk.Schema{
-				"luns": {
-					Type:     pluginsdk.TypeList,
-					Required: true,
-					Elem: &pluginsdk.Schema{
-						Type: pluginsdk.TypeInt,
-					},
-				},
-				"default_file_path": {
-					Type:         pluginsdk.TypeString,
-					Required:     true,
-					ValidateFunc: validation.StringIsNotEmpty,
-				},
-			},
-		},
-	}
-}
