2222ca90119613a96b7116144c207b04e3c935c9
kt <kt@katbyte.me>
2022-08-11 20:16:20 -0700

    Update internal/services/network/application_gateway_resource.go

--- internal/services/network/application_gateway_resource.go
+++ internal/services/network/application_gateway_resource.go
@@ -966,7 +966,7 @@ func resourceApplicationGateway() *pluginsdk.Resource {
 								Schema: map[string]*pluginsdk.Schema{
 									"body": {
 										Type:     pluginsdk.TypeString,
-										Required: false,
+										Optional: true,
 									},
 
 									"status_code": {
