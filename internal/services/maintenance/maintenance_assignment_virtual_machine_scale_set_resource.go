7af8529d6626ca10233baa70c52e2fa16a618243
kt <kt@katbyte.me>
2022-08-18 10:02:22 -0700

    Update internal/services/maintenance/maintenance_assignment_virtual_machine_scale_set_resource.go

--- internal/services/maintenance/maintenance_assignment_virtual_machine_scale_set_resource.go
+++ internal/services/maintenance/maintenance_assignment_virtual_machine_scale_set_resource.go
@@ -46,7 +46,7 @@ func resourceArmMaintenanceAssignmentVirtualMachineScaleSet() *pluginsdk.Resourc
 				Required:         true,
 				ForceNew:         true,
 				ValidateFunc:     maintenanceconfigurations.ValidateMaintenanceConfigurationID,
-				DiffSuppressFunc: suppress.CaseDifference, // TODO remove in 4.0
+				DiffSuppressFunc: suppress.CaseDifference, // TODO remove in 4.0 with a work around or when https://github.com/Azure/azure-rest-api-specs/issues/8653 is fixed
 			},
 
 			"virtual_machine_scale_set_id": {
