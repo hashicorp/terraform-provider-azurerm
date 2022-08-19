78cfe93430e327dca8a58945ec64ae1bac9278a6
Steph <steph@hashicorp.com>
2022-08-19 17:47:45 +0200

    parse resource ids insensitively in the read functions

--- internal/services/dns/dns_srv_record_resource.go
+++ internal/services/dns/dns_srv_record_resource.go
@@ -147,7 +147,7 @@ func resourceDnsSrvRecordRead(d *pluginsdk.ResourceData, meta interface{}) error
 	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
-	id, err := recordsets.ParseRecordTypeID(d.Id())
+	id, err := recordsets.ParseRecordTypeIDInsensitively(d.Id())
 	if err != nil {
 		return err
 	}
