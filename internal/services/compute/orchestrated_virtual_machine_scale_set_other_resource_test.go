be75255cc23cd27cd4c2eacedc90e86d6c2f2e11
Yichun Ma <yicma@microsoft.com>
2022-08-26 21:36:36 +0800

    Fix TestAccOrchestratedVirtualMachineScaleSet_otherUltraSsd

--- internal/services/compute/orchestrated_virtual_machine_scale_set_other_resource_test.go
+++ internal/services/compute/orchestrated_virtual_machine_scale_set_other_resource_test.go
@@ -1324,9 +1324,9 @@ resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
 
-  zones = []
+  zones = ["1"]
 
-  sku_name  = "Standard_D1_v2"
+  sku_name  = "Standard_D2s_v3"
   instances = 1
 
   platform_fault_domain_count = 2
