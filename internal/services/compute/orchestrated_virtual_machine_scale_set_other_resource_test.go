168e3eaaa8648410b84d35a30b9b4a4f9939f48e
Yichun Ma <yicma@microsoft.com>
2022-08-30 22:21:51 +0800

    patch_assessment_mode update test removal

--- internal/services/compute/orchestrated_virtual_machine_scale_set_other_resource_test.go
+++ internal/services/compute/orchestrated_virtual_machine_scale_set_other_resource_test.go
@@ -100,13 +100,6 @@ func TestAccOrchestratedVirtualMachineScaleSet_PatchAssessmentModeLinux(t *testi
 			),
 		},
 		data.ImportStep("os_profile.0.linux_configuration.0.admin_password"),
-		{
-			Config: r.otherPatchAssessmentModeLinuxUpdated(data),
-			Check: acceptance.ComposeTestCheckFunc(
-				check.That(data.ResourceName).ExistsInAzure(r),
-			),
-		},
-		data.ImportStep("os_profile.0.linux_configuration.0.admin_password"),
 	})
 }
 
@@ -179,13 +172,6 @@ func TestAccOrchestratedVirtualMachineScaleSet_PatchAssessmentModeWindows(t *tes
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 				check.That(data.ResourceName).Key("os_profile.0.windows_configuration.0.patch_assessment_mode").HasValue("ImageDefault"),
-			),
-		},
-		data.ImportStep("os_profile.0.windows_configuration.0.admin_password"),
-		{
-			Config: r.otherPatchAssessmentModeWindowsUpdated(data),
-			Check: acceptance.ComposeTestCheckFunc(
-				check.That(data.ResourceName).ExistsInAzure(r),
 			),
 		},
 		data.ImportStep("os_profile.0.windows_configuration.0.admin_password"),
@@ -595,88 +581,6 @@ resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
 `, data.RandomInteger, data.Locations.Primary, r.natgateway_template(data))
 }
 
-func (OrchestratedVirtualMachineScaleSetResource) otherPatchAssessmentModeWindowsUpdated(data acceptance.TestData) string {
-	r := OrchestratedVirtualMachineScaleSetResource{}
-	return fmt.Sprintf(`
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "test" {
-  name     = "acctestRG-OVMSS-%[1]d"
-  location = "%[2]s"
-}
-
-%[3]s
-
-resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
-  name                = "acctestOVMSS-%[1]d"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-
-  sku_name = "Standard_D1_v2"
-
-  # Orchestrated VMSS allocation will timeout at service side due to extension, set instances to 0 to avoid the timeout
-  instances = 0
-
-  platform_fault_domain_count = 2
-
-  os_profile {
-    windows_configuration {
-      computer_name_prefix = "testvm"
-      admin_username       = "myadmin"
-      admin_password       = "Passwword1234"
-
-      patch_assessment_mode = "AutomaticByPlatform"
-    }
-  }
-
-  network_interface {
-    name    = "TestNetworkProfile"
-    primary = true
-
-    ip_configuration {
-      name      = "TestIPConfiguration"
-      primary   = true
-      subnet_id = azurerm_subnet.test.id
-
-      public_ip_address {
-        name                    = "TestPublicIPConfiguration"
-        domain_name_label       = "test-domain-label"
-        idle_timeout_in_minutes = 4
-      }
-    }
-  }
-
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
-
-  extension {
-    name                               = "HealthExtension"
-    publisher                          = "Microsoft.ManagedServices"
-    type                               = "ApplicationHealthWindows"
-    type_handler_version               = "1.0"
-    auto_upgrade_minor_version_enabled = true
-
-    settings = jsonencode({
-      "protocol"    = "http"
-      "port"        = "80"
-      "requestPath" = "/healthEndpoint"
-    })
-  }
-}
-`, data.RandomInteger, data.Locations.Primary, r.natgateway_template(data))
-}
-
 func (OrchestratedVirtualMachineScaleSetResource) linuxVMGuestPatching(data acceptance.TestData, patchMode string) string {
 	r := OrchestratedVirtualMachineScaleSetResource{}
 	return fmt.Sprintf(`
@@ -798,92 +702,6 @@ resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
         username   = "myadmin"
         public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDvXYZAjVUt2aojUV3XIA+PY6gXrgbvktXwf2NoIHGlQFhogpMEyOfqgogCtTBM7MNCS3ELul6SV+mlpH08Ki45ADIQuDXdommCvsMFW096JrsHOJpGfjCsJ1gbbv7brB3Ag+BSGb4qO3pRsEVTtZCeJDwfH5D7vmqP5xXcELKR4UAtKQKUhLvt6mhW90sFLTJeOTiYGbavIKqfCUFSeSMQkUPr8o3uzOfeWyCw7tc7szLuvfwJ5poGHuve73KKAlUnDTPUrhyj7iITZSDl+/i+bpDzPyCyJWDMsC0ON7q2fDr2mEz0L9ACrsI5Nx3lt5fe+IaHSrjivqnL8SqUWSN45o9Qp99sGWFiuTfos8f1jp+AXzC4ArVtKyRg/CnzKRiK0CGSxBJ5s9zAoa7yBBmjCszq89vFa0eMgpEIZFwa6kKJKt9AfRBXgO9YGPV4uaN7topy92/p2pE+vF8IafarbvnTDOQt62mS07tXYqYg1DhecrmBVWKlq9oafBweoeTjoq52SoGsuDc/YAOzIgWVIuvV8yKoh9KbXPWowjLtxDhRIS/d1nMMNdNI8X0TQivgi5+umMgAXhsVAKSNDUauLt4jimYkWAuE+R6KoCqVFdaB9bQDySBjAziruDSe3reToydjzzluvHMjWK8QiDynxs41pi4zZz6gAlca3QPkEQ== hello@world.com"
       }
-    }
-  }
-
-  network_interface {
-    name    = "TestNetworkProfile"
-    primary = true
-
-    ip_configuration {
-      name      = "TestIPConfiguration"
-      primary   = true
-      subnet_id = azurerm_subnet.test.id
-
-      public_ip_address {
-        name                    = "TestPublicIPConfiguration"
-        domain_name_label       = "test-domain-label"
-        idle_timeout_in_minutes = 4
-      }
-    }
-  }
-
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-
-  extension {
-    name                               = "HealthExtension"
-    publisher                          = "Microsoft.ManagedServices"
-    type                               = "ApplicationHealthLinux"
-    type_handler_version               = "1.0"
-    auto_upgrade_minor_version_enabled = true
-
-    settings = jsonencode({
-      "protocol"    = "http"
-      "port"        = 80
-      "requestPath" = "/healthEndpoint"
-    })
-  }
-}
-`, data.RandomInteger, data.Locations.Primary, r.natgateway_template(data))
-}
-
-func (OrchestratedVirtualMachineScaleSetResource) otherPatchAssessmentModeLinuxUpdated(data acceptance.TestData) string {
-	r := OrchestratedVirtualMachineScaleSetResource{}
-	return fmt.Sprintf(`
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "test" {
-  name     = "acctestRG-OVMSS-%[1]d"
-  location = "%[2]s"
-}
-
-%[3]s
-
-resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
-  name                = "acctestOVMSS-%[1]d"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-
-  sku_name = "Standard_F2"
-
-  # Orchestrated VMSS allocation will timeout at service side due to extension, set instances to 0 to avoid the timeout
-  instances = 0
-
-  platform_fault_domain_count = 2
-
-  os_profile {
-    linux_configuration {
-      computer_name_prefix = "testvm"
-      admin_username       = "myadmin"
-
-      admin_ssh_key {
-        username   = "myadmin"
-        public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDvXYZAjVUt2aojUV3XIA+PY6gXrgbvktXwf2NoIHGlQFhogpMEyOfqgogCtTBM7MNCS3ELul6SV+mlpH08Ki45ADIQuDXdommCvsMFW096JrsHOJpGfjCsJ1gbbv7brB3Ag+BSGb4qO3pRsEVTtZCeJDwfH5D7vmqP5xXcELKR4UAtKQKUhLvt6mhW90sFLTJeOTiYGbavIKqfCUFSeSMQkUPr8o3uzOfeWyCw7tc7szLuvfwJ5poGHuve73KKAlUnDTPUrhyj7iITZSDl+/i+bpDzPyCyJWDMsC0ON7q2fDr2mEz0L9ACrsI5Nx3lt5fe+IaHSrjivqnL8SqUWSN45o9Qp99sGWFiuTfos8f1jp+AXzC4ArVtKyRg/CnzKRiK0CGSxBJ5s9zAoa7yBBmjCszq89vFa0eMgpEIZFwa6kKJKt9AfRBXgO9YGPV4uaN7topy92/p2pE+vF8IafarbvnTDOQt62mS07tXYqYg1DhecrmBVWKlq9oafBweoeTjoq52SoGsuDc/YAOzIgWVIuvV8yKoh9KbXPWowjLtxDhRIS/d1nMMNdNI8X0TQivgi5+umMgAXhsVAKSNDUauLt4jimYkWAuE+R6KoCqVFdaB9bQDySBjAziruDSe3reToydjzzluvHMjWK8QiDynxs41pi4zZz6gAlca3QPkEQ== hello@world.com"
-      }
-
-      patch_assessment_mode = "AutomaticByPlatform"
     }
   }
 
