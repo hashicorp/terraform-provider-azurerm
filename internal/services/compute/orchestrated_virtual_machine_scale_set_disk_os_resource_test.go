da355aa2855cd99ccd4a50e37ab850a11a970422
Yichun Ma <yicma@microsoft.com>
2022-08-30 22:31:18 +0800

    Fix `TestAccOrchestratedVirtualMachineScaleSet_disksOSDiskCaching`

--- internal/services/compute/orchestrated_virtual_machine_scale_set_disk_os_resource_test.go
+++ internal/services/compute/orchestrated_virtual_machine_scale_set_disk_os_resource_test.go
@@ -48,7 +48,7 @@ resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
 
-  sku_name  = "Standard_F2s_v2"
+  sku_name  = "Standard_F4s_v2"
   instances = 1
 
   platform_fault_domain_count = 2
