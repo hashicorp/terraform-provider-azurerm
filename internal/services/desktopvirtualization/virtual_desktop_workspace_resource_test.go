b60142d665db644ee918665ea911b5b4eb8ae8aa
amccullough84 <amccullough84@gmail.com>
2022-09-03 17:51:08 +0100

    Update Desktop Virtualization API to 2022-02-10; scheduled_agent_updates  supports scheduled_agent_updates (#17489)
    Co-authored-by: Andy.McCullough <andy.mccullough@ultima.com>

--- internal/services/desktopvirtualization/virtual_desktop_workspace_resource_test.go
+++ internal/services/desktopvirtualization/virtual_desktop_workspace_resource_test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/go-azure-sdk/resource-manager/desktopvirtualization/2021-09-03-preview/workspace"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/desktopvirtualization/2022-02-10-preview/workspace"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
