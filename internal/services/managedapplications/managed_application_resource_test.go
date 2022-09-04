aa89c304cbea36ec6f5ae59310da093731afdfde
Zhenhua Hu <zhhu@microsoft.com>
2022-09-04 22:28:00 +0800

    add code

--- internal/services/managedapplications/managed_application_resource_test.go
+++ internal/services/managedapplications/managed_application_resource_test.go
@@ -196,9 +196,9 @@ func (r ManagedApplicationResource) complete(data acceptance.TestData) string {
 %s
 
 resource "azurerm_marketplace_agreement" "test" {
-  publisher = "cisco"
-  offer     = "cisco-meraki-vmx"
-  plan      = "cisco-meraki-vmx"
+  publisher = "quantinuumllc1640113159771"
+  offer     = "quantinuum-aq"
+  plan      = "credits1"
 }
 
 resource "azurerm_managed_application" "test" {
@@ -212,21 +212,11 @@ resource "azurerm_managed_application" "test" {
     name      = azurerm_marketplace_agreement.test.plan
     product   = azurerm_marketplace_agreement.test.offer
     publisher = azurerm_marketplace_agreement.test.publisher
-    version   = "15.37.1"
+    version   = "1.0.0"
   }
 
   parameters = {
-    zone                        = "0"
     location                    = azurerm_resource_group.test.location
-    merakiAuthToken             = "f451adfb-d00b-4612-8799-b29294217d4a"
-    subnetAddressPrefix         = "10.0.0.0/24"
-    subnetName                  = "acctestSubnet"
-    virtualMachineSize          = "Standard_DS12_v2"
-    virtualNetworkAddressPrefix = "10.0.0.0/16"
-    virtualNetworkName          = "acctestVnet"
-    virtualNetworkNewOrExisting = "new"
-    virtualNetworkResourceGroup = "acctestVnetRg"
-    vmName                      = "acctestVM"
   }
 
   tags = {
