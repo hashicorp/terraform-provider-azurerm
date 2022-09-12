c96ce93cf9beeb93205e7be64fd8f8300ab8c04e
gettek <info@get-tek.uk>
2022-09-13 00:33:57 +0100

    fix tests

--- internal/services/policy/remediation_resource_test.go
+++ internal/services/policy/remediation_resource_test.go
@@ -119,7 +119,7 @@ resource "azurerm_resource_policy_remediation" "test" {
   resource_id             = azurerm_virtual_network.test.id
   policy_assignment_id    = azurerm_resource_policy_assignment.test.id
   location_filters        = ["westus"]
-  policy_definition_id    = azurerm_policy_definition.test.id
+  policy_definition_id    = data.azurerm_policy_definition.test.id
   resource_discovery_mode = "ReEvaluateCompliance"
   failure_percentage      = 0.5
   parallel_deployments    = 3
