1e15610626343de93121bb160a5b7f1b8d02f4da
Yun Liu <yunliu1@microsoft.com>
2022-09-19 09:19:11 +0800

    update with latest api skus

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/kusto/2022-07-07/clusters/method_get_autorest.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/kusto/2022-07-07/clusters/method_get_autorest.go
@@ -1,8 +1,7 @@
-package serversecurityalertpolicies
+package clusters
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/Azure/go-autorest/autorest"
@@ -14,26 +13,26 @@ import (
 
 type GetOperationResponse struct {
 	HttpResponse *http.Response
-	Model        *ServerSecurityAlertPolicy
+	Model        *Cluster
 }
 
 // Get ...
-func (c ServerSecurityAlertPoliciesClient) Get(ctx context.Context, id ServerId) (result GetOperationResponse, err error) {
+func (c ClustersClient) Get(ctx context.Context, id ClusterId) (result GetOperationResponse, err error) {
 	req, err := c.preparerForGet(ctx, id)
 	if err != nil {
-		err = autorest.NewErrorWithError(err, "serversecurityalertpolicies.ServerSecurityAlertPoliciesClient", "Get", nil, "Failure preparing request")
+		err = autorest.NewErrorWithError(err, "clusters.ClustersClient", "Get", nil, "Failure preparing request")
 		return
 	}
 
 	result.HttpResponse, err = c.Client.Send(req, azure.DoRetryWithRegistration(c.Client))
 	if err != nil {
-		err = autorest.NewErrorWithError(err, "serversecurityalertpolicies.ServerSecurityAlertPoliciesClient", "Get", result.HttpResponse, "Failure sending request")
+		err = autorest.NewErrorWithError(err, "clusters.ClustersClient", "Get", result.HttpResponse, "Failure sending request")
 		return
 	}
 
 	result, err = c.responderForGet(result.HttpResponse)
 	if err != nil {
-		err = autorest.NewErrorWithError(err, "serversecurityalertpolicies.ServerSecurityAlertPoliciesClient", "Get", result.HttpResponse, "Failure responding to request")
+		err = autorest.NewErrorWithError(err, "clusters.ClustersClient", "Get", result.HttpResponse, "Failure responding to request")
 		return
 	}
 
@@ -41,7 +40,7 @@ func (c ServerSecurityAlertPoliciesClient) Get(ctx context.Context, id ServerId)
 }
 
 // preparerForGet prepares the Get request.
-func (c ServerSecurityAlertPoliciesClient) preparerForGet(ctx context.Context, id ServerId) (*http.Request, error) {
+func (c ClustersClient) preparerForGet(ctx context.Context, id ClusterId) (*http.Request, error) {
 	queryParameters := map[string]interface{}{
 		"api-version": defaultApiVersion,
 	}
@@ -50,14 +49,14 @@ func (c ServerSecurityAlertPoliciesClient) preparerForGet(ctx context.Context, i
 		autorest.AsContentType("application/json; charset=utf-8"),
 		autorest.AsGet(),
 		autorest.WithBaseURL(c.baseUri),
-		autorest.WithPath(fmt.Sprintf("%s/securityAlertPolicies/default", id.ID())),
+		autorest.WithPath(id.ID()),
 		autorest.WithQueryParameters(queryParameters))
 	return preparer.Prepare((&http.Request{}).WithContext(ctx))
 }
 
 // responderForGet handles the response to the Get request. The method always
 // closes the http.Response Body.
-func (c ServerSecurityAlertPoliciesClient) responderForGet(resp *http.Response) (result GetOperationResponse, err error) {
+func (c ClustersClient) responderForGet(resp *http.Response) (result GetOperationResponse, err error) {
 	err = autorest.Respond(
 		resp,
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
