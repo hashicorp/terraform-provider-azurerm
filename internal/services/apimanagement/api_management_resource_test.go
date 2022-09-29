3eb980677ad5c89952d7eb673272e526f2038e1d
xuzhang3 <57888764+xuzhang3@users.noreply.github.com>
2022-09-30 00:06:14 +0800

    Enhance: `azurerm_api_management` - Support `additional_location.gateway_disabled`, `certificate_source`, `certificate_status` (#18508)
    Co-authored-by: xuzhang3 <Zhangxu894765>

--- internal/services/apimanagement/api_management_resource_test.go
+++ internal/services/apimanagement/api_management_resource_test.go
@@ -862,6 +862,20 @@ func TestAccApiManagement_tenantAccess(t *testing.T) {
 	})
 }
 
+func TestAccApiManagement_additionalLocationGateway(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_api_management", "test")
+	r := ApiManagementResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.additionalLocationGateway(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r)),
+		},
+		data.ImportStep(),
+	})
+}
+
 func (ApiManagementResource) basic(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
@@ -883,6 +897,40 @@ resource "azurerm_api_management" "test" {
   sku_name = "Developer_1"
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
+}
+
+func (ApiManagementResource) additionalLocationGateway(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+
+resource "azurerm_resource_group" "test2" {
+  name     = "acctestRG2-%[1]d"
+  location = "%[3]s"
+}
+
+resource "azurerm_api_management" "test" {
+  name                = "acctestAM-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  publisher_name      = "pub1"
+  publisher_email     = "pub1@email.com"
+
+  sku_name = "Premium_2"
+
+  additional_location {
+    location         = azurerm_resource_group.test2.location
+    gateway_disabled = true
+  }
+}
+`, data.RandomInteger, data.Locations.Primary, data.Locations.Secondary)
 }
 
 func (ApiManagementResource) standardSku(data acceptance.TestData) string {
