a4b1f57f80db160178466d9e4421cdad8e9c56c3
Xu Wu <xuwu1@microsoft.com>
2022-09-29 11:22:47 +0800

    new resource of nginx certificate

--- internal/services/nginx/registration.go
+++ internal/services/nginx/registration.go
@@ -32,6 +32,7 @@ func (r Registration) DataSources() []sdk.DataSource {
 // Resources ...
 func (r Registration) Resources() []sdk.Resource {
 	return []sdk.Resource{
+		CertificateResource{},
 		DeploymentResource{},
 	}
 }
