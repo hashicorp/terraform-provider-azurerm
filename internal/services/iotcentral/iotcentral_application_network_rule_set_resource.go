254d5b316916207b6ea554a20c97b7ae0c7997b8
Yichun Ma <yicma@microsoft.com>
2022-09-30 14:41:36 +0800

    New resource `azurerm_iotcentral_application_network_rule_set`

---
+++ internal/services/iotcentral/iotcentral_application_network_rule_set_resource.go
@@ -0,0 +1,333 @@
+package iotcentral
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/iotcentral/2021-11-01-preview/apps"
+	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
+	"github.com/hashicorp/terraform-provider-azurerm/helpers/validate"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/locks"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
+	iothubValidate "github.com/hashicorp/terraform-provider-azurerm/internal/services/iothub/validate"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+)
+
+type IotCentralApplicationNetworkRuleSetResource struct{}
+
+var (
+	_ sdk.ResourceWithUpdate = IotCentralApplicationNetworkRuleSetResource{}
+)
+
+type IotCentralApplicationNetworkRuleSetModel struct {
+	IotCentralApplicationId string             `tfschema:"iotcentral_application_id"`
+	ApplyToDevice           bool               `tfschema:"apply_to_device"`
+	DefaultAction           apps.NetworkAction `tfschema:"default_action"`
+	IPRule                  []IPRule           `tfschema:"ip_rule"`
+}
+
+type IPRule struct {
+	Name   string `tfschema:"name"`
+	IPMask string `tfschema:"ip_mask"`
+}
+
+func (r IotCentralApplicationNetworkRuleSetResource) Arguments() map[string]*pluginsdk.Schema {
+	return map[string]*pluginsdk.Schema{
+		"iotcentral_application_id": {
+			Type:         pluginsdk.TypeString,
+			Required:     true,
+			ForceNew:     true,
+			ValidateFunc: apps.ValidateIotAppID,
+		},
+
+		"apply_to_device": {
+			Type:     pluginsdk.TypeBool,
+			Optional: true,
+			Default:  true,
+		},
+
+		"default_action": {
+			Type:     pluginsdk.TypeString,
+			Optional: true,
+			Default:  string(apps.NetworkActionDeny),
+			ValidateFunc: validation.StringInSlice([]string{
+				string(apps.NetworkActionAllow),
+				string(apps.NetworkActionDeny),
+			}, false),
+		},
+
+		"ip_rule": {
+			Type:     pluginsdk.TypeList,
+			Optional: true,
+			Elem: &pluginsdk.Resource{
+				Schema: map[string]*pluginsdk.Schema{
+					"name": {
+						Type:         pluginsdk.TypeString,
+						Required:     true,
+						ValidateFunc: iothubValidate.IoTHubIpRuleName,
+					},
+					"ip_mask": {
+						Type:         pluginsdk.TypeString,
+						Required:     true,
+						ValidateFunc: validate.CIDR,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (r IotCentralApplicationNetworkRuleSetResource) Attributes() map[string]*pluginsdk.Schema {
+	return map[string]*pluginsdk.Schema{}
+}
+
+func (r IotCentralApplicationNetworkRuleSetResource) ResourceType() string {
+	return "azurerm_iotcentral_application_network_rule_set"
+}
+
+func (r IotCentralApplicationNetworkRuleSetResource) ModelObject() interface{} {
+	return &IotCentralApplicationNetworkRuleSetModel{}
+}
+
+func (r IotCentralApplicationNetworkRuleSetResource) IDValidationFunc() pluginsdk.SchemaValidateFunc {
+	return apps.ValidateIotAppID
+}
+
+func (r IotCentralApplicationNetworkRuleSetResource) Create() sdk.ResourceFunc {
+	return sdk.ResourceFunc{
+		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.IoTCentral.AppsClient
+			var state IotCentralApplicationNetworkRuleSetModel
+			if err := metadata.Decode(&state); err != nil {
+				return err
+			}
+
+			id, err := apps.ParseIotAppID(state.IotCentralApplicationId)
+			if err != nil {
+				return err
+			}
+
+			locks.ByID(id.ID())
+			defer locks.UnlockByID(id.ID())
+
+			app, err := client.Get(ctx, *id)
+			if err != nil || app.Model == nil {
+				return fmt.Errorf("checking for the presence of existing %q: %+v", id, err)
+			}
+
+			model := app.Model
+
+			// This resource is unique to the corresponding IoT Central Application.
+			// It will be created automatically along with the IoT Central Application, therefore we check whether this resource is identical to a "deleted" one
+			if property := model.Properties; property != nil {
+				if property.NetworkRuleSets != nil {
+					if !isNetworkRuleSetNullified(*property.NetworkRuleSets) {
+						return tf.ImportAsExistsError(r.ResourceType(), id.ID())
+					}
+				}
+			}
+
+			if model.Properties == nil {
+				model.Properties = &apps.AppProperties{}
+			}
+
+			model.Properties.NetworkRuleSets = &apps.NetworkRuleSets{
+				ApplyToDevices: utils.Bool(state.ApplyToDevice),
+				// ApplyToIoTCentral must be set to false explicitly
+				ApplyToIoTCentral: utils.Bool(false),
+				DefaultAction:     &state.DefaultAction,
+				IPRules:           expandIotCentralApplicationNetworkRuleSetIPRule(state.IPRule),
+			}
+
+			if err := client.CreateOrUpdateThenPoll(ctx, *id, *model); err != nil {
+				return fmt.Errorf("creating Network Rule Set of %s: %+v", id, err)
+			}
+
+			metadata.SetID(id)
+			return nil
+		},
+		Timeout: 30 * time.Minute,
+	}
+}
+
+func (r IotCentralApplicationNetworkRuleSetResource) Read() sdk.ResourceFunc {
+	return sdk.ResourceFunc{
+		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.IoTCentral.AppsClient
+			id, err := apps.ParseIotAppID(metadata.ResourceData.Id())
+			if err != nil {
+				return err
+			}
+
+			resp, err := client.Get(ctx, *id)
+			if err != nil {
+				if response.WasNotFound(resp.HttpResponse) {
+					return metadata.MarkAsGone(id)
+				}
+
+				return fmt.Errorf("retrieving %s: %+v", *id, err)
+			}
+
+			if resp.Model == nil || resp.Model.Properties == nil || resp.Model.Properties.NetworkRuleSets == nil {
+				return fmt.Errorf("reading Network Rule Set of %q: %+v", id, err)
+			}
+
+			networkRuleSet := resp.Model.Properties.NetworkRuleSets
+			state := IotCentralApplicationNetworkRuleSetModel{
+				IotCentralApplicationId: id.ID(),
+				IPRule:                  flattenIotCentralApplicationNetworkRuleSetIPRule(networkRuleSet.IPRules),
+			}
+
+			applyToDevice := true
+			if networkRuleSet.ApplyToDevices != nil {
+				applyToDevice = *networkRuleSet.ApplyToDevices
+			}
+			state.ApplyToDevice = applyToDevice
+
+			defaultAction := apps.NetworkActionDeny
+			if networkRuleSet.DefaultAction != nil {
+				defaultAction = *networkRuleSet.DefaultAction
+			}
+			state.DefaultAction = defaultAction
+
+			return metadata.Encode(&state)
+		},
+		Timeout: 5 * time.Minute,
+	}
+}
+
+func (r IotCentralApplicationNetworkRuleSetResource) Update() sdk.ResourceFunc {
+	return sdk.ResourceFunc{
+		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.IoTCentral.AppsClient
+			id, err := apps.ParseIotAppID(metadata.ResourceData.Id())
+			if err != nil {
+				return err
+			}
+
+			var state IotCentralApplicationNetworkRuleSetModel
+			if err := metadata.Decode(&state); err != nil {
+				return err
+			}
+
+			existing, err := client.Get(ctx, *id)
+			if err != nil || existing.Model == nil {
+				return fmt.Errorf("retrieving %s: %+v", *id, err)
+			}
+
+			if existing.Model.Properties == nil {
+				existing.Model.Properties = &apps.AppProperties{}
+			}
+
+			if existing.Model.Properties.NetworkRuleSets == nil {
+				existing.Model.Properties.NetworkRuleSets = &apps.NetworkRuleSets{}
+			}
+
+			// ApplyToIoTCentral must be set to false explicitly
+			existing.Model.Properties.NetworkRuleSets.ApplyToIoTCentral = utils.Bool(false)
+
+			if metadata.ResourceData.HasChange("apply_to_device") {
+				existing.Model.Properties.NetworkRuleSets.ApplyToDevices = utils.Bool(state.ApplyToDevice)
+			}
+
+			if metadata.ResourceData.HasChange("default_action") {
+				existing.Model.Properties.NetworkRuleSets.DefaultAction = &state.DefaultAction
+			}
+
+			if metadata.ResourceData.HasChange("ip_rule") {
+				existing.Model.Properties.NetworkRuleSets.IPRules = expandIotCentralApplicationNetworkRuleSetIPRule(state.IPRule)
+			}
+
+			if err := client.CreateOrUpdateThenPoll(ctx, *id, *existing.Model); err != nil {
+				return fmt.Errorf("updating Network Rule Set of %s: %+v", id, err)
+			}
+
+			return nil
+		},
+		Timeout: 30 * time.Minute,
+	}
+}
+
+func (r IotCentralApplicationNetworkRuleSetResource) Delete() sdk.ResourceFunc {
+	return sdk.ResourceFunc{
+		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.IoTCentral.AppsClient
+			id, err := apps.ParseIotAppID(metadata.ResourceData.Id())
+			if err != nil {
+				return err
+			}
+
+			app, err := client.Get(ctx, *id)
+			if err != nil || app.Model == nil {
+				return fmt.Errorf("retrieving %s: %+v", *id, err)
+			}
+
+			if app.Model.Properties == nil {
+				app.Model.Properties = &apps.AppProperties{}
+			}
+
+			app.Model.Properties.NetworkRuleSets = nil
+
+			if err := client.CreateOrUpdateThenPoll(ctx, *id, *app.Model); err != nil {
+				return fmt.Errorf("deleting Network Rule Set of %s: %+v", id, err)
+			}
+
+			return nil
+		},
+		Timeout: 30 * time.Minute,
+	}
+}
+
+func expandIotCentralApplicationNetworkRuleSetIPRule(input []IPRule) *[]apps.NetworkRuleSetIPRule {
+	results := make([]apps.NetworkRuleSetIPRule, 0)
+	for _, item := range input {
+		results = append(results, apps.NetworkRuleSetIPRule{
+			FilterName: utils.String(item.Name),
+			IPMask:     utils.String(item.IPMask),
+		})
+	}
+	return &results
+}
+
+func flattenIotCentralApplicationNetworkRuleSetIPRule(input *[]apps.NetworkRuleSetIPRule) []IPRule {
+	if input == nil {
+		return nil
+	}
+
+	results := make([]IPRule, 0)
+	for _, item := range *input {
+		obj := IPRule{}
+
+		if item.FilterName != nil {
+			obj.Name = *item.FilterName
+		}
+
+		if item.IPMask != nil {
+			obj.IPMask = *item.IPMask
+		}
+
+		results = append(results, obj)
+	}
+	return results
+}
+
+func isNetworkRuleSetNullified(networkRuleSet apps.NetworkRuleSets) bool {
+	if networkRuleSet.ApplyToDevices != nil && *networkRuleSet.ApplyToDevices {
+
+		return false
+	}
+
+	if networkRuleSet.DefaultAction != nil && *networkRuleSet.DefaultAction != apps.NetworkActionAllow {
+		return false
+	}
+
+	if networkRuleSet.IPRules != nil && len(*networkRuleSet.IPRules) > 0 {
+		return false
+	}
+
+	return true
+}
--- internal/services/iotcentral/iotcentral_application_resource_test.go
+++ internal/services/iotcentral/iotcentral_application_resource_test.go
@@ -41,8 +41,6 @@ func TestAccIoTCentralApplication_complete(t *testing.T) {
 			Config: r.complete(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
-				check.That(data.ResourceName).Key("template").HasValue("iotc-pnp-preview@1.0.0"),
-				check.That(data.ResourceName).Key("tags.ENV").HasValue("Test"),
 			),
 		},
 		data.ImportStep(),
@@ -62,11 +60,31 @@ func TestAccIoTCentralApplication_update(t *testing.T) {
 		},
 		data.ImportStep(),
 		{
-			Config: r.update(data),
+			Config: r.complete(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
-				check.That(data.ResourceName).Key("sku").HasValue("ST1"),
-				check.That(data.ResourceName).Key("tags.ENV").HasValue("Test"),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccIoTCentralApplication_displayName(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_iotcentral_application", "test")
+	r := IoTCentralApplicationResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.displayName(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.displayNameUpdated(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
 			),
 		},
 		data.ImportStep(),
@@ -137,6 +155,87 @@ func TestAccIoTCentralApplication_publicNetworkAccessEnabled(t *testing.T) {
 	})
 }
 
+func TestAccIoTCentralApplication_sku(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_iotcentral_application", "test")
+	r := IoTCentralApplicationResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.sku(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.skuUpdated(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccIoTCentralApplication_subDomain(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_iotcentral_application", "test")
+	r := IoTCentralApplicationResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.subDomain(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.subDomainUpdated(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccIoTCentralApplication_tags(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_iotcentral_application", "test")
+	r := IoTCentralApplicationResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.tags(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.tagsUpdated(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccIoTCentralApplication_templateProperty(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_iotcentral_application", "test")
+	r := IoTCentralApplicationResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.templateProperty(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func TestAccIoTCentralApplication_requiresImport(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_iotcentral_application", "test")
 	r := IoTCentralApplicationResource{}
@@ -182,7 +281,6 @@ resource "azurerm_iotcentral_application" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   sub_domain          = "subdomain-%[1]d"
-  sku                 = "ST1"
 }
 `, data.RandomInteger, data.Locations.Primary)
 }
@@ -235,6 +333,205 @@ resource "azurerm_iotcentral_application" "test" {
 `, data.RandomInteger, data.Locations.Primary)
 }
 
+func (IoTCentralApplicationResource) displayName(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_iotcentral_application" "test" {
+  name                = "acctest-iotcentralapp-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sub_domain          = "subdomain-%[1]d"
+
+  display_name = "display-name"
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (IoTCentralApplicationResource) displayNameUpdated(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_iotcentral_application" "test" {
+  name                = "acctest-iotcentralapp-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sub_domain          = "subdomain-%[1]d"
+
+  display_name = "display-name-2"
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (IoTCentralApplicationResource) sku(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_iotcentral_application" "test" {
+  name                = "acctest-iotcentralapp-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sub_domain          = "subdomain-%[1]d"
+
+  sku = "ST2"
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (IoTCentralApplicationResource) skuUpdated(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_iotcentral_application" "test" {
+  name                = "acctest-iotcentralapp-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sub_domain          = "subdomain-%[1]d"
+
+  sku = "ST0"
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (IoTCentralApplicationResource) subDomain(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_iotcentral_application" "test" {
+  name                = "acctest-iotcentralapp-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sub_domain          = "subdomain-%[1]d"
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (IoTCentralApplicationResource) subDomainUpdated(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_iotcentral_application" "test" {
+  name                = "acctest-iotcentralapp-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sub_domain          = "subdomain2-%[1]d"
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (IoTCentralApplicationResource) tags(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_iotcentral_application" "test" {
+  name                = "acctest-iotcentralapp-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sub_domain          = "subdomain-%[1]d"
+
+  tags = {
+    ENV = "Test"
+  }
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (IoTCentralApplicationResource) tagsUpdated(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_iotcentral_application" "test" {
+  name                = "acctest-iotcentralapp-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sub_domain          = "subdomain-%[1]d"
+
+  tags = {
+    ENV     = "Test2"
+    Purpose = "AccTests"
+  }
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (IoTCentralApplicationResource) templateProperty(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_iotcentral_application" "test" {
+  name                = "acctest-iotcentralapp-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sub_domain          = "subdomain-%[1]d"
+
+  template = "iotc-distribution@1.0.0"
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
 func (IoTCentralApplicationResource) complete(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
@@ -250,35 +547,10 @@ resource "azurerm_iotcentral_application" "test" {
   name                = "acctest-iotcentralapp-%[1]d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  sub_domain          = "subdomain-%[1]d"
+  sub_domain          = "subdomain2-%[1]d"
   display_name        = "some-display-name"
-  sku                 = "ST1"
-  template            = "iotc-pnp-preview@1.0.0"
-  tags = {
-    ENV = "Test"
-  }
-}
-`, data.RandomInteger, data.Locations.Primary)
-}
+  sku                 = "ST0"
 
-func (IoTCentralApplicationResource) update(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "test" {
-  name     = "acctestRG-%[1]d"
-  location = "%[2]s"
-}
-
-resource "azurerm_iotcentral_application" "test" {
-  name                = "acctest-iotcentralapp-%[1]d"
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-  sub_domain          = "subdomain-%[1]d"
-  display_name        = "some-display-name"
-  sku                 = "ST1"
   tags = {
     ENV = "Test"
   }
