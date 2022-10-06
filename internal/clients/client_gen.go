9e1df2c3dd6a7e31b6b5958cb8d6b72ef6c10ccd
jackofallops <11830746+jackofallops@users.noreply.github.com>
2022-10-06 06:51:37 +0100

    fix clients_gen.go after accidental removal of import (#18646)

--- internal/clients/client_gen.go
+++ internal/clients/client_gen.go
@@ -3,19 +3,20 @@ package clients
 // NOTE: this file is generated - manual changes will be overwritten.
 
 import (
-	loadtests_v2021_12_01_preview "github.com/hashicorp/go-azure-sdk/resource-manager/loadtestservice/2021-12-01-preview"
-  managedidentity_2018_11_30 "github.com/hashicorp/go-azure-sdk/resource-manager/managedidentity/2018-11-30"
+	loadtestservice_v2021_12_01_preview "github.com/hashicorp/go-azure-sdk/resource-manager/loadtestservice/2021-12-01-preview"
+	managedidentity_v2018_11_30 "github.com/hashicorp/go-azure-sdk/resource-manager/managedidentity/2018-11-30"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
-	loadtest "github.com/hashicorp/terraform-provider-azurerm/internal/services/loadtestservice/client"
+	loadtestservice "github.com/hashicorp/terraform-provider-azurerm/internal/services/loadtestservice/client"
+	managedidentity "github.com/hashicorp/terraform-provider-azurerm/internal/services/managedidentity/client"
 )
 
 type autoClient struct {
-	LoadTestService *loadtests_v2021_12_01_preview.Client
-	ManagedIdentity *managedidentity_2018_11_30.Client
+	LoadTestService *loadtestservice_v2021_12_01_preview.Client
+	ManagedIdentity *managedidentity_v2018_11_30.Client
 }
 
 func buildAutoClients(client *autoClient, o *common.ClientOptions) error {
-	client.LoadTestService = loadtest.NewClient(o)
+	client.LoadTestService = loadtestservice.NewClient(o)
 	client.ManagedIdentity = managedidentity.NewClient(o)
 	return nil
 }
