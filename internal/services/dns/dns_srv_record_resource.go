1567a1b847103f12e9bbef77d6baaa966a43c3f0
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-10-10 13:16:26 -0700

    r/dns_srv_record: adding a state migration/removing the case-insensitive hack

--- internal/services/dns/dns_srv_record_resource.go
+++ internal/services/dns/dns_srv_record_resource.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/dns/migration"
+
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/tags"
@@ -28,6 +30,7 @@ func resourceDnsSrvRecord() *pluginsdk.Resource {
 			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
 			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
 		},
+
 		Importer: pluginsdk.ImporterValidatingResourceId(func(id string) error {
 			parsed, err := recordsets.ParseRecordTypeID(id)
 			if err != nil {
@@ -38,6 +41,12 @@ func resourceDnsSrvRecord() *pluginsdk.Resource {
 			}
 			return nil
 		}),
+
+		SchemaVersion: 1,
+		StateUpgraders: pluginsdk.StateUpgrades(map[int]pluginsdk.StateUpgrade{
+			0: migration.SRVRecordV0ToV1{},
+		}),
+
 		Schema: map[string]*pluginsdk.Schema{
 			"name": {
 				Type:     pluginsdk.TypeString,
@@ -147,7 +156,7 @@ func resourceDnsSrvRecordRead(d *pluginsdk.ResourceData, meta interface{}) error
 	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
-	id, err := recordsets.ParseRecordTypeIDInsensitively(d.Id())
+	id, err := recordsets.ParseRecordTypeID(d.Id())
 	if err != nil {
 		return err
 	}
