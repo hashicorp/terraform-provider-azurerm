0b2177eab0ebe6898577586a4c5c08a260d0f054
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-10-10 08:48:27 -0700

    r/dns_a_record: adding a state migration for the ID field

--- internal/services/dns/dns_a_record_resource.go
+++ internal/services/dns/dns_a_record_resource.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/dns/migration"
+
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/tags"
@@ -38,6 +40,11 @@ func resourceDnsARecord() *pluginsdk.Resource {
 				return fmt.Errorf("this resource only supports 'A' records")
 			}
 			return nil
+		}),
+
+		SchemaVersion: 1,
+		StateUpgraders: pluginsdk.StateUpgrades(map[int]pluginsdk.StateUpgrade{
+			0: migration.ARecordV0ToV1{},
 		}),
 
 		Schema: map[string]*pluginsdk.Schema{
@@ -149,7 +156,7 @@ func resourceDnsARecordRead(d *pluginsdk.ResourceData, meta interface{}) error {
 	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
-	id, err := recordsets.ParseRecordTypeIDInsensitively(d.Id())
+	id, err := recordsets.ParseRecordTypeID(d.Id())
 	if err != nil {
 		return err
 	}
