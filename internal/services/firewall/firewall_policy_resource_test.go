4465a8f694205d0848f0987c2d1f476252d21a4b
Lars Maes <lars.maes@sogeti.com>
2022-10-12 17:19:39 +0200

    forgot quotes

--- internal/services/firewall/firewall_policy_resource_test.go
+++ internal/services/firewall/firewall_policy_resource_test.go
@@ -214,7 +214,7 @@ resource "azurerm_firewall_policy" "test" {
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
   tags = {
-     Env = "Test
+     Env = "Test"
   }
 }
 `, template, data.RandomInteger)
