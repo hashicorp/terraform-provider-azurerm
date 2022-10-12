1c55e1fb5656ba69f48a77c4cafcbf4958ff3121
Matthew <mbfrahry@gmail.com>
2022-10-12 16:54:05 -0700

    Add datasource for cosmosdb_sql_database

--- internal/services/cosmos/registration.go
+++ internal/services/cosmos/registration.go
@@ -44,6 +44,7 @@ func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
 		"azurerm_cosmosdb_account":                      dataSourceCosmosDbAccount(),
 		"azurerm_cosmosdb_mongo_database":               dataSourceCosmosDbMongoDatabase(),
 		"azurerm_cosmosdb_restorable_database_accounts": dataSourceCosmosDbRestorableDatabaseAccounts(),
+		"azurerm_cosmosdb_sql_database":                 dataSourceCosmosDbSQLDatabase(),
 	}
 }
 
