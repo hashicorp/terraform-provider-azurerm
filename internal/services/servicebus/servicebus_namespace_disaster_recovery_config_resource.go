bf356592ea162c76ae151a4b0c02df4b954c53a2
xiaxin18 <xiaxyi@microsoft.com>
2022-10-13 13:09:44 +0800

    sorts imports

--- internal/services/servicebus/servicebus_namespace_disaster_recovery_config_resource.go
+++ internal/services/servicebus/servicebus_namespace_disaster_recovery_config_resource.go
@@ -3,7 +3,6 @@ package servicebus
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"log"
 	"net/http"
 	"strconv"
@@ -16,6 +15,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/locks"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
