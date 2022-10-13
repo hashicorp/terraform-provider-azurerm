343b655a4dcdc3b4526044f7394a0899fb882690
aristosvo <8375124+aristosvo@users.noreply.github.com>
2022-10-14 01:16:22 +0200

    Update internal/services/mssql/mssql_server_microsoft_support_auditing_policy_resource_test.go

--- internal/services/mssql/mssql_server_microsoft_support_auditing_policy_resource_test.go
+++ internal/services/mssql/mssql_server_microsoft_support_auditing_policy_resource_test.go
@@ -309,8 +309,8 @@ resource "azurerm_role_assignment" "test" {
 }
 
 resource "azurerm_mssql_server_microsoft_support_auditing_policy" "test" {
-  server_id        = azurerm_mssql_server.test.id
-  storage_endpoint = azurerm_storage_account.test.primary_blob_endpoint
+  server_id             = azurerm_mssql_server.test.id
+  blob_storage_endpoint = azurerm_storage_account.test.primary_blob_endpoint
 
   depends_on = [
     azurerm_role_assignment.test,
