c383bc4560c6042d604dc916a9be45847753751c
Matthew <mbfrahry@gmail.com>
2022-10-13 13:50:36 -0700

    Add data.azurerm_cosmosdb_sql_role_definition datasource

--- internal/services/cosmos/registration.go
+++ internal/services/cosmos/registration.go
@@ -45,6 +45,7 @@ func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
 		"azurerm_cosmosdb_mongo_database":               dataSourceCosmosDbMongoDatabase(),
 		"azurerm_cosmosdb_restorable_database_accounts": dataSourceCosmosDbRestorableDatabaseAccounts(),
 		"azurerm_cosmosdb_sql_database":                 dataSourceCosmosDbSQLDatabase(),
+		"azurerm_cosmosdb_sql_role_definition":          dataSourceCosmosDbSQLRoleDefinition(),
 	}
 }
 
