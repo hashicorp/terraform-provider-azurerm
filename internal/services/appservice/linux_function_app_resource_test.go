39ee1072191476c85167178c91b16144335b9b83
jackofallops <ste@hashicorp.com>
2022-10-13 10:34:37 +0100

    fix update test config

--- internal/services/appservice/linux_function_app_resource_test.go
+++ internal/services/appservice/linux_function_app_resource_test.go
@@ -3702,6 +3702,9 @@ resource "azurerm_linux_function_app" "test" {
   resource_group_name = azurerm_resource_group.test.name
   service_plan_id     = azurerm_service_plan.test.id
 
+  storage_account_name       = azurerm_storage_account.test.name
+  storage_account_access_key = azurerm_storage_account.test.primary_access_key
+
   site_config {}
 
   storage_account {
