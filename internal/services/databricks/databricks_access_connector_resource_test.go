c25cd7e4156095bef4ed208376ec7020c117d726
Vladimir Lazarenko <favoretti@gmail.com>
2022-10-14 10:15:55 +0200

    Identity and registration fixes

--- internal/services/databricks/databricks_access_connector_resource_test.go
+++ internal/services/databricks/databricks_access_connector_resource_test.go
@@ -90,6 +90,9 @@ resource "azurerm_databricks_access_connector" "import" {
   name                = azurerm_databricks_access_connector.test.name
   resource_group_name = azurerm_databricks_access_connector.test.resource_group_name
   location            = azurerm_databricks_access_connector.test.location
+  identity {
+    type = "SystemAssigned"
+  }
 }
 `, template)
 }
