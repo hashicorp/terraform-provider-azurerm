ba5859318ed5939bc96fab337ce869497d463b01
magodo <wztdyl@sina.com>
2022-10-17 16:12:30 +0800

    `azurerm_network_security_{group|rule}` - Setting correct casing `protocol` to state

--- internal/services/network/network_security_rule_resource.go
+++ internal/services/network/network_security_rule_resource.go
@@ -2,6 +2,7 @@ package network
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2021-08-01/network"
@@ -316,9 +317,16 @@ func resourceNetworkSecurityRuleRead(d *pluginsdk.ResourceData, meta interface{}
 	d.Set("resource_group_name", id.ResourceGroup)
 	d.Set("network_security_group_name", id.NetworkSecurityGroupName)
 
+	// For fixing the case insensitive issue for the NSR protocol in Azure
+	// See: https://github.com/hashicorp/terraform-provider-azurerm/issues/16092
+	protocolMap := map[string]network.SecurityRuleProtocol{}
+	for _, protocol := range network.PossibleSecurityRuleProtocolValues() {
+		protocolMap[strings.ToLower(string(protocol))] = protocol
+	}
+
 	if props := resp.SecurityRulePropertiesFormat; props != nil {
 		d.Set("description", props.Description)
-		d.Set("protocol", string(props.Protocol))
+		d.Set("protocol", string(protocolMap[strings.ToLower(string(props.Protocol))]))
 		d.Set("destination_address_prefix", props.DestinationAddressPrefix)
 		d.Set("destination_address_prefixes", props.DestinationAddressPrefixes)
 		d.Set("destination_port_range", props.DestinationPortRange)
