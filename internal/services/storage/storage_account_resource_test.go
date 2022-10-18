bac9226c9219cef5f0e8d920fc9b739d72649e6d
magodo <wztdyl@sina.com>
2022-10-18 22:45:46 +0800

    `azurerm_storage_account` - Refactor the service access based on the kind and sku tier (#17274)

--- internal/services/storage/storage_account_resource_test.go
+++ internal/services/storage/storage_account_resource_test.go
@@ -1260,6 +1260,21 @@ func TestAccStorageAccount_edgeZone(t *testing.T) {
 	})
 }
 
+func TestAccStorageAccount_storageV1StandardZRS(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_storage_account", "test")
+	r := StorageAccountResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.storageV1StandardZRS(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func TestAccStorageAccount_smbMultichannel(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_storage_account", "test")
 	r := StorageAccountResource{}
@@ -3896,6 +3911,29 @@ resource "azurerm_storage_account" "test" {
   tags = {
     environment = "production"
   }
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomString)
+}
+
+func (r StorageAccountResource) storageV1StandardZRS(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-storage-%d"
+  location = "%s"
+}
+
+resource "azurerm_storage_account" "test" {
+  name                = "unlikely23exst2acct%s"
+  resource_group_name = azurerm_resource_group.test.name
+
+  location                 = azurerm_resource_group.test.location
+  account_kind             = "Storage"
+  account_tier             = "Standard"
+  account_replication_type = "ZRS"
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomString)
 }
