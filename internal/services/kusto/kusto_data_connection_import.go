b1887cc5d83887048efaa9877b1d57bc3eec8093
Yun Liu <yunliu1@microsoft.com>
2022-10-19 17:13:22 +0800

    Update on azurerm_kusto_eventhub_data_connection and azurerm_kusto_eventgrid_data_connection

--- internal/services/kusto/kusto_data_connection_import.go
+++ internal/services/kusto/kusto_data_connection_import.go
@@ -9,7 +9,11 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 )
 
-func importDataConnection() pluginsdk.ImporterFunc {
+func hasDiffType(a, b interface{}) bool {
+	return fmt.Sprintf("%T", a) != fmt.Sprintf("%T", b)
+}
+
+func importDataConnection(kind dataconnections.DataConnection) pluginsdk.ImporterFunc {
 	return func(ctx context.Context, d *pluginsdk.ResourceData, meta interface{}) (data []*pluginsdk.ResourceData, err error) {
 		id, err := dataconnections.ParseDataConnectionID(d.Id())
 		if err != nil {
@@ -17,9 +21,21 @@ func importDataConnection() pluginsdk.ImporterFunc {
 		}
 
 		client := meta.(*clients.Client).Kusto.DataConnectionsClient
-		_, err = client.Get(ctx, *id)
+		dataConnection, err := client.Get(ctx, *id)
 		if err != nil {
 			return []*pluginsdk.ResourceData{}, fmt.Errorf("retrieving Kusto Data Connection %q (Resource Group %q): %+v", id.DataConnectionName, id.ResourceGroupName, err)
+		}
+
+		if dataCon, ok := (*dataConnection.Model).(dataconnections.EventHubDataConnection); ok && hasDiffType(kind, dataCon) {
+			return nil, fmt.Errorf(`kusto data connection "kind" mismatch, expected "%T", got "%T"`, kind, dataCon)
+		}
+
+		if dataCon, ok := (*dataConnection.Model).(dataconnections.IotHubDataConnection); ok && hasDiffType(kind, dataCon) {
+			return nil, fmt.Errorf(`kusto data connection "kind" mismatch, expected "%T", got "%T"`, kind, dataCon)
+		}
+
+		if dataCon, ok := (*dataConnection.Model).(dataconnections.EventGridDataConnection); ok && hasDiffType(kind, dataCon) {
+			return nil, fmt.Errorf(`kusto data connection "kind" mismatch, expected "%T", got "%T"`, kind, dataCon)
 		}
 
 		return []*pluginsdk.ResourceData{d}, nil
