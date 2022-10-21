e2eca1b9ba842b7ed521fdf345c26a85f620ad2a
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2022-10-20 22:31:50 -0600

    Add WAF data source

--- internal/services/cdn/registration.go
+++ internal/services/cdn/registration.go
@@ -32,11 +32,12 @@ func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
 		"azurerm_cdn_profile": dataSourceCdnProfile(),
 
 		// FrontDoor
-		"azurerm_cdn_frontdoor_endpoint":     dataSourceCdnFrontDoorEndpoint(),
-		"azurerm_cdn_frontdoor_origin_group": dataSourceCdnFrontDoorOriginGroup(),
-		"azurerm_cdn_frontdoor_profile":      dataSourceCdnFrontDoorProfile(),
-		"azurerm_cdn_frontdoor_rule_set":     dataSourceCdnFrontDoorRuleSet(),
-		"azurerm_cdn_frontdoor_secret":       dataSourceCdnFrontDoorSecret(),
+		"azurerm_cdn_frontdoor_endpoint":        dataSourceCdnFrontDoorEndpoint(),
+		"azurerm_cdn_frontdoor_firewall_policy": dataSourceCdnFrontDoorFirewallPolicy(),
+		"azurerm_cdn_frontdoor_origin_group":    dataSourceCdnFrontDoorOriginGroup(),
+		"azurerm_cdn_frontdoor_profile":         dataSourceCdnFrontDoorProfile(),
+		"azurerm_cdn_frontdoor_rule_set":        dataSourceCdnFrontDoorRuleSet(),
+		"azurerm_cdn_frontdoor_secret":          dataSourceCdnFrontDoorSecret(),
 	}
 }
 
