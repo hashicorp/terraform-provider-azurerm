81435a16124781b7f472ab906bef4b934745f64f
Steph <steph@hashicorp.com>
2022-10-28 16:59:42 +0200

    nil check publicNetworkAccess

--- internal/services/containers/kubernetes_cluster_resource.go
+++ internal/services/containers/kubernetes_cluster_resource.go
@@ -1907,7 +1907,12 @@ func resourceKubernetesClusterRead(d *pluginsdk.ResourceData, meta interface{}) 
 		d.Set("node_resource_group", props.NodeResourceGroup)
 		d.Set("enable_pod_security_policy", props.EnablePodSecurityPolicy)
 		d.Set("local_account_disabled", props.DisableLocalAccounts)
-		d.Set("public_network_access_enabled", *props.PublicNetworkAccess != managedclusters.PublicNetworkAccessDisabled)
+
+		publicNetworkAccess := "Enabled"
+		if props.PublicNetworkAccess != nil {
+			publicNetworkAccess = publicNetworkAccess
+		}
+		d.Set("public_network_access_enabled", publicNetworkAccess != string(managedclusters.PublicNetworkAccessDisabled))
 
 		upgradeChannel := ""
 		if profile := props.AutoUpgradeProfile; profile != nil && *profile.UpgradeChannel != managedclusters.UpgradeChannelNone {
