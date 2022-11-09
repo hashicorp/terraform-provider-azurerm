8c3202e7f83990e106b071f406f8d6ffdc332873
ricardo-snyk <ricardo.ferrazleal@snyk.io>
2022-11-09 17:54:05 -0500

    fugue PR 18: remove validation func for snapshot name

--- internal/services/compute/snapshot_resource.go
+++ internal/services/compute/snapshot_resource.go
@@ -15,7 +15,6 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/compute/migration"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/services/compute/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
@@ -48,10 +47,10 @@ func resourceSnapshot() *pluginsdk.Resource {
 
 		Schema: map[string]*pluginsdk.Schema{
 			"name": {
-				Type:         pluginsdk.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validate.SnapshotName,
+				Type:     pluginsdk.TypeString,
+				Required: true,
+				ForceNew: true,
+				// ValidateFunc: validate.SnapshotName,
 			},
 
 			"location": commonschema.Location(),
