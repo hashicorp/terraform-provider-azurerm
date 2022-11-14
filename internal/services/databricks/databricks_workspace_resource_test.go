3e7516a3220e9a41fd95a91785523afa5e9740e4
Vladimir Lazarenko <favoretti@gmail.com>
2022-11-14 13:25:27 +0100

    `azurerm_databricks_workspace`: Add support for CMK for disk encryption
    2 new arguments:
    `managed_disk_cmk_key_vault_key_id`
    `managed_disk_cmk_rotation_to_latest_version_enabled`

--- internal/services/databricks/databricks_workspace_resource_test.go
+++ internal/services/databricks/databricks_workspace_resource_test.go
@@ -234,6 +234,22 @@ func TestAccDatabricksWorkspace_managedServicesAndDbfsCMK(t *testing.T) {
 	})
 }
 
+func TestAccDatabricksWorkspace_managedServicesAndDiskCMK(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_databricks_workspace", "test")
+	databricksPrincipalID := getDatabricksPrincipalId(data.Client().SubscriptionID)
+	r := DatabricksWorkspaceResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.managedServicesAndDiskCMK(data, databricksPrincipalID),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("custom_parameters.0.public_subnet_network_security_group_association_id", "custom_parameters.0.private_subnet_network_security_group_association_id"),
+	})
+}
+
 func TestAccDatabricksWorkspace_managedServicesDbfsCMKAndPrivateLink(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_databricks_workspace", "test")
 	databricksPrincipalID := getDatabricksPrincipalId(data.Client().SubscriptionID)
@@ -1123,7 +1139,7 @@ resource "azurerm_resource_group" "test" {
   name     = "acctestRG-databricks-%[1]d"
   location = "%[2]s"
 }
-
+ 
 resource "azurerm_databricks_workspace" "test" {
   depends_on = [azurerm_key_vault_access_policy.managed]
 
@@ -1135,6 +1151,123 @@ resource "azurerm_databricks_workspace" "test" {
 
   customer_managed_key_enabled          = true
   managed_services_cmk_key_vault_key_id = azurerm_key_vault_key.test.id
+
+  tags = {
+    Environment = "Production"
+    Pricing     = "Premium"
+  }
+}
+
+resource "azurerm_databricks_workspace_customer_managed_key" "test" {
+  depends_on = [azurerm_key_vault_access_policy.databricks]
+
+  workspace_id     = azurerm_databricks_workspace.test.id
+  key_vault_key_id = azurerm_key_vault_key.test.id
+}
+
+resource "azurerm_key_vault" "test" {
+  name                = "acctest-kv-%[3]s"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "premium"
+
+  soft_delete_retention_days = 7
+}
+
+resource "azurerm_key_vault_key" "test" {
+  depends_on = [azurerm_key_vault_access_policy.terraform]
+
+  name         = "acctest-certificate"
+  key_vault_id = azurerm_key_vault.test.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "terraform" {
+  key_vault_id = azurerm_key_vault.test.id
+  tenant_id    = azurerm_key_vault.test.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get",
+    "List",
+    "Create",
+    "Decrypt",
+    "Encrypt",
+    "Sign",
+    "UnwrapKey",
+    "Verify",
+    "WrapKey",
+    "Delete",
+    "Restore",
+    "Recover",
+    "Update",
+    "Purge",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "managed" {
+  key_vault_id = azurerm_key_vault.test.id
+  tenant_id    = azurerm_key_vault.test.tenant_id
+  object_id    = "%[4]s"
+
+  key_permissions = [
+    "Get",
+    "UnwrapKey",
+    "WrapKey",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "databricks" {
+  depends_on = [azurerm_databricks_workspace.test]
+
+  key_vault_id = azurerm_key_vault.test.id
+  tenant_id    = azurerm_databricks_workspace.test.storage_account_identity.0.tenant_id
+  object_id    = azurerm_databricks_workspace.test.storage_account_identity.0.principal_id
+
+  key_permissions = [
+    "Get",
+    "UnwrapKey",
+    "WrapKey",
+  ]
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomString, databricksPrincipalID)
+}
+
+func (DatabricksWorkspaceResource) managedServicesAndDiskCMK(data acceptance.TestData, databricksPrincipalID string) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-databricks-%[1]d"
+  location = "%[2]s"
+}
+ 
+resource "azurerm_databricks_workspace" "test" {
+  depends_on = [azurerm_key_vault_access_policy.managed]
+
+  name                        = "acctestDBW-%[1]d"
+  resource_group_name         = azurerm_resource_group.test.name
+  location                    = azurerm_resource_group.test.location
+  sku                         = "premium"
+  managed_resource_group_name = "acctestRG-DBW-%[1]d-managed"
+
+  customer_managed_key_enabled      = true
+  managed_disk_cmk_key_vault_key_id = azurerm_key_vault_key.test.id
 
   tags = {
     Environment = "Production"
