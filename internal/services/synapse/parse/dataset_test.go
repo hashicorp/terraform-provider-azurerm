4ad4cf4afc8fa1d7029cfef383b0c69dca0b1353
Heng Lu <henglu@microsoft.com>
2022-11-15 10:50:34 +0800

    go generate

--- internal/services/synapse/parse/dataset_test.go
+++ internal/services/synapse/parse/dataset_test.go
@@ -5,10 +5,10 @@ package parse
 import (
 	"testing"
 
-	"github.com/hashicorp/terraform-provider-azurerm/internal/resourceid"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/resourceids"
 )
 
-var _ resourceid.Formatter = DatasetId{}
+var _ resourceids.Id = DatasetId{}
 
 func TestDatasetIDFormatter(t *testing.T) {
 	actual := NewDatasetID("12345678-1234-9876-4563-123456789012", "resGroup1", "workspace1", "dataset1").ID()
