c6e26c171fcc27791c6797f7ac6c59a3dd5218aa
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-11-15 07:45:35 +0100

    make fmt

--- internal/services/media/migration/streaming_policy_v0_to_v1.go
+++ internal/services/media/migration/streaming_policy_v0_to_v1.go
@@ -3,7 +3,7 @@ package migration
 import (
 	"context"
 	"log"
-	
+
 	"github.com/hashicorp/go-azure-sdk/resource-manager/media/2020-05-01/streamingpoliciesandstreaminglocators"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 )
