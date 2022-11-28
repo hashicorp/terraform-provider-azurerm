04811c17096226fbe0d1e6149f650ced6a96ff29
Harshavardhan Musanalli <Harshavardhan.MUSANALLI@amadeus.com>
2022-11-28 17:21:34 +0100

    terrafmt fix for managed_disk_resource_test.go file

--- internal/services/compute/managed_disk_resource_test.go
+++ internal/services/compute/managed_disk_resource_test.go
@@ -1208,9 +1208,9 @@ resource "azurerm_resource_group" "test" {
 resource "azurerm_managed_disk" "test" {
   name                 = "acctestd-%d"
   location             = azurerm_resource_group.test.location
-  resource_group_name  = azurerm_resource_group.test.name  
-  create_option        = "Upload"  
-  storage_account_type = "Standard_LRS"  
+  resource_group_name  = azurerm_resource_group.test.name
+  create_option        = "Upload"
+  storage_account_type = "Standard_LRS"
   upload_size_bytes    = 21475885568
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
