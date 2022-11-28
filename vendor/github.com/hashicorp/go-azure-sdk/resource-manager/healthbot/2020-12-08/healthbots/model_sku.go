06ae7a3acdd858d47e7b10afe5a78053b9f93829
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-11-28 13:57:27 +0100

    go mod vendor

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/healthbot/2020-12-08/healthbots/model_sku.go
@@ -0,0 +1,8 @@
+package healthbots
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type Sku struct {
+	Name SkuName `json:"name"`
+}
