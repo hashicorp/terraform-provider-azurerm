f0d4728c2d73a737d92779a834e912b75d2e2052
Lucas Fernández <lucas.fernandez@southworks.com>
2022-12-06 16:07:30 -0300

    Updating test.

--- internal/services/containers/kubernetes_cluster_resource_test.go
+++ internal/services/containers/kubernetes_cluster_resource_test.go
@@ -487,7 +487,7 @@ resource "azurerm_kubernetes_cluster" "test" {
   storage_profile {
     blob_driver_enabled         = true
     disk_driver_enabled         = true
-    disk_driver_version         = "v2"
+    disk_driver_version         = "v1"
     file_driver_enabled         = false
     snapshot_controller_enabled = false
   }
