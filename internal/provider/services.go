97499a7d5b8d8617b0545b4687ac2577fb5d918c
Tao <104055472+teowa@users.noreply.github.com>
2022-12-07 11:58:58 +0800

    New Resource `azurerm_network_manager` (#19334)

--- internal/provider/services.go
+++ internal/provider/services.go
@@ -141,6 +141,7 @@ func SupportedTypedServices() []sdk.TypedServiceRegistration {
 		loganalytics.Registration{},
 		monitor.Registration{},
 		mssql.Registration{},
+		network.Registration{},
 		nginx.Registration{},
 		policy.Registration{},
 		privatednsresolver.Registration{},
