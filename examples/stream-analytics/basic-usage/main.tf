a0ab2c5415fc6402c0735b2a20143b934dff160e
Alexander Guth <alexander.guth@pwc.com>
2022-12-13 12:47:43 +0100

    add msi example

--- examples/stream-analytics/basic-usage/main.tf
+++ examples/stream-analytics/basic-usage/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "${var.prefix}-examplestoracc"
+  name                     = "${var.prefix}examplestoracc"
   resource_group_name      = "${azurerm_resource_group.example.name}"
   location                 = "${azurerm_resource_group.example.location}"
   account_tier             = "Standard"
---
+++ examples/stream-analytics/msi-auth/README.md
@@ -0,0 +1,3 @@
+# Azure Stream Analytics: MSI authentication
+
+This example provisions an example that uses MSI authentication for the Stream Analytics inputs and outputs.
