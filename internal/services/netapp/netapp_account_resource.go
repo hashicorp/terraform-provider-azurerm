043e4dd329048a440240056d83a16ce8741b38ad
Paulo Marques <paulomarquesc@users.noreply.github.com>
2022-12-16 11:14:13 -0700

    ANF Volumes - Availability Zone Support for Volumes (#19669)

--- internal/services/netapp/netapp_account_resource.go
+++ internal/services/netapp/netapp_account_resource.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/tags"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/netapp/2021-10-01/netappaccounts"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/netapp/2022-05-01/netappaccounts"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/validate"
