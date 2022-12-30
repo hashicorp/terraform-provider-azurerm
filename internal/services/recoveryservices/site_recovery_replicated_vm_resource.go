f5522591f650705a23af1b36db4ad40a3fc93900
ziyeqf <zyqf16@gmail.com>
2022-12-31 00:36:18 +0800

    `azurerm_site_recovery_replicated_vm` - support `network_interface.is_primary`

--- internal/services/recoveryservices/site_recovery_replicated_vm_resource.go
+++ internal/services/recoveryservices/site_recovery_replicated_vm_resource.go
@@ -239,6 +239,12 @@ func networkInterfaceResource() *pluginsdk.Resource {
 				ForceNew:     false,
 				ValidateFunc: azure.ValidateResourceID,
 			},
+			"is_primary": {
+				Type:     pluginsdk.TypeBool,
+				Optional: true,
+				Computed: true,
+				Default:  false,
+			},
 		},
 	}
 }
@@ -423,27 +429,33 @@ func resourceSiteRecoveryReplicatedItemUpdateInternal(ctx context.Context, d *pl
 	}
 
 	var vmNics []replicationprotecteditems.VMNicInputDetails
-	for _, raw := range d.Get("network_interface").(*pluginsdk.Set).List() {
+	nicList := d.Get("network_interface").(*pluginsdk.Set).List()
+	for _, raw := range nicList {
 		vmNicInput := raw.(map[string]interface{})
 		sourceNicId := vmNicInput["source_network_interface_id"].(string)
 		targetStaticIp := vmNicInput["target_static_ip"].(string)
 		targetSubnetName := vmNicInput["target_subnet_name"].(string)
 		recoveryPublicIPAddressID := vmNicInput["recovery_public_ip_address_id"].(string)
+		isPrimary := vmNicInput["is_primary"].(bool)
+		if len(nicList) == 1 {
+			isPrimary = true
+		}
 
 		nicId := findNicId(state, sourceNicId)
 		if nicId == nil {
 			return fmt.Errorf("updating replicated vm %s (vault %s): Trying to update NIC that is not known by Azure %s", name, vaultName, sourceNicId)
 		}
-		vmNics = append(vmNics, replicationprotecteditems.VMNicInputDetails{
-			NicId: nicId,
-			IPConfigs: &[]replicationprotecteditems.IPConfigInputDetails{
-				{
-					RecoverySubnetName:        &targetSubnetName,
-					RecoveryStaticIPAddress:   &targetStaticIp,
-					RecoveryPublicIPAddressId: &recoveryPublicIPAddressID,
-					IsPrimary:                 utils.Bool(true), // we only support one ip configured for one NIC for now.
-				},
+		ipConfig := []replicationprotecteditems.IPConfigInputDetails{
+			{
+				RecoverySubnetName:        &targetSubnetName,
+				RecoveryStaticIPAddress:   &targetStaticIp,
+				RecoveryPublicIPAddressId: &recoveryPublicIPAddressID,
+				IsPrimary:                 &isPrimary,
 			},
+		}
+		vmNics = append(vmNics, replicationprotecteditems.VMNicInputDetails{
+			NicId:     nicId,
+			IPConfigs: &ipConfig,
 		})
 	}
 
