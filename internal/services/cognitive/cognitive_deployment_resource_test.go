aad526ab2fdac815f0c99c162424c30fd5dc444a
hc-github-team-tf-azure <>
2023-01-02 09:30:24 +0000

    Updating based on c2554b6e

--- internal/services/cognitive/cognitive_deployment_resource_test.go
+++ internal/services/cognitive/cognitive_deployment_resource_test.go
@@ -5,9 +5,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/go-azure-sdk/resource-manager/cognitive/2022-10-01/deployments"
-
 	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/cognitive/2022-10-01/deployments"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
