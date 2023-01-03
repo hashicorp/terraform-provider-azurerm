3dfd4539acd95924aa804234d3cbdbbf9ddd0e1e
Yun Liu <yunliu1@microsoft.com>
2023-01-03 16:56:22 +0800

    fixes test errors

--- internal/services/network/private_endpoint_application_security_group_association.go
+++ internal/services/network/private_endpoint_application_security_group_association.go
@@ -118,7 +118,7 @@ func (p PrivateEndpointApplicationSecurityGroupAssociationResource) Create() sdk
 			}
 
 			if ASGInPE {
-				return fmt.Errorf("ApplicationSecurityGroup %q already linked with with Private Endpoint, duplicated resource creation is not allowed", ASGId)
+				return fmt.Errorf("A resource with the ID %q already exists - to be managed via Terraform this resource needs to be imported into the State. Please see the resource documentation for %q for more information.", resourceId.ID(), "azurerm_private_endpoint_application_security_group_association")
 			}
 
 			if ASGList != nil {
