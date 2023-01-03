2eec85c73cd8b98d37b6fbb1814818c73ab39900
xiaxin18 <xiaxyi@microsoft.com>
2023-01-03 17:52:56 +0800

    fix capture feature not being set issue

--- internal/services/eventhub/eventhub_resource.go
+++ internal/services/eventhub/eventhub_resource.go
@@ -237,6 +237,7 @@ func resourceEventHubUpdate(d *pluginsdk.ResourceData, meta interface{}) error {
 			PartitionCount:         utils.Int64(int64(d.Get("partition_count").(int))),
 			MessageRetentionInDays: utils.Int64(int64(d.Get("message_retention").(int))),
 			Status:                 &eventhubStatus,
+			CaptureDescription:     expandEventHubCaptureDescription(d),
 		},
 	}
 
