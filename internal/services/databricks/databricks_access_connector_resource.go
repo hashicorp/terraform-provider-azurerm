f0f4aa90752b8d7fa6429fe2bf7168580ace020b
Yichun Ma <yicma@microsoft.com>
2023-01-05 22:08:09 +0800

    Fix several wrong logic with 404 response in read

--- internal/services/databricks/databricks_access_connector_resource.go
+++ internal/services/databricks/databricks_access_connector_resource.go
@@ -153,7 +153,7 @@ func (r AccessConnectorResource) Read() sdk.ResourceFunc {
 
 			resp, err := client.Get(ctx, *id)
 			if err != nil {
-				if !response.WasNotFound(resp.HttpResponse) {
+				if response.WasNotFound(resp.HttpResponse) {
 					return metadata.MarkAsGone(id)
 				}
 				return fmt.Errorf("retrieving %s: %+v", *id, err)
