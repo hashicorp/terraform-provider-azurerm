b40f794635293245c970c8e77f53c6e2b50f52fe
aristosvo <8375124+aristosvo@users.noreply.github.com>
2023-01-09 20:19:06 +0100

    `azurerm_cost_anomaly_alert` - New resource (#19899)
    Fixes https://github.com/hashicorp/terraform-provider-azurerm/issues/18062

---
+++ internal/services/costmanagement/validate/cost_anomaly_alert_name.go
@@ -0,0 +1,22 @@
+package validate
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func CostAnomalyAlertName(i interface{}, k string) (_ []string, errors []error) {
+	v, ok := i.(string)
+	if !ok {
+		return nil, append(errors, fmt.Errorf("expected type of %s to be string", k))
+	}
+
+	// The name attribute rules (as far as we know) are :
+	// 1. can contain only lowercase letters, numbers and hyphens.
+
+	if !regexp.MustCompile(`^([a-z\d-]*)$`).MatchString(v) {
+		errors = append(errors, fmt.Errorf("%s must contain only lowercase letters, numbers and hyphens.", k))
+	}
+
+	return nil, errors
+}
