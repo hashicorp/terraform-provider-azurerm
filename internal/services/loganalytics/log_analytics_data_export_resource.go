3631927deae44a86cab5c79a9dae3f20a953feda
Elena Xin <39109137+sinbai@users.noreply.github.com>
2023-01-11 17:43:33 +0800

    `azurerm_log_analytics_data_export_rule` - fix `destination_resource_id` doesn't accept Event Hub Namespace (#19868)
    * fix issue 19861
    
    * update code
    
    * update code

--- internal/services/loganalytics/log_analytics_data_export_resource.go
+++ internal/services/loganalytics/log_analytics_data_export_resource.go
@@ -137,14 +137,23 @@ func resourceOperationalinsightsDataExportCreateUpdate(d *pluginsdk.ResourceData
 	}
 
 	if strings.Contains(destinationId, "Microsoft.EventHub") {
-		eventhubId, err := eventhubs.ParseEventhubID(destinationId)
-		if err != nil {
-			return fmt.Errorf("parsing destination eventhub id error: %+v", err)
-		}
-		destinationId = namespaces.NewNamespaceID(eventhubId.SubscriptionId, eventhubId.ResourceGroupName, eventhubId.NamespaceName).ID()
-		parameters.Properties.Destination.ResourceId = destinationId
-		parameters.Properties.Destination.MetaData = &dataexport.DestinationMetaData{
-			EventHubName: utils.String(eventhubId.EventHubName),
+		_, err := eventhubs.ValidateNamespaceID(destinationId, "destination_resource_id")
+		if err == nil {
+			eventhubNamespace, err := eventhubs.ParseNamespaceID(destinationId)
+			if err != nil {
+				return fmt.Errorf("parsing destination eventhub namespaces id error: %+v", err)
+			}
+			parameters.Properties.Destination.ResourceId = eventhubNamespace.ID()
+		} else {
+			eventhubId, err := eventhubs.ParseEventhubID(destinationId)
+			if err != nil {
+				return fmt.Errorf("parsing destination eventhub id error: %+v", err)
+			}
+			destinationId = namespaces.NewNamespaceID(eventhubId.SubscriptionId, eventhubId.ResourceGroupName, eventhubId.NamespaceName).ID()
+			parameters.Properties.Destination.ResourceId = destinationId
+			parameters.Properties.Destination.MetaData = &dataexport.DestinationMetaData{
+				EventHubName: utils.String(eventhubId.EventHubName),
+			}
 		}
 	}
 
