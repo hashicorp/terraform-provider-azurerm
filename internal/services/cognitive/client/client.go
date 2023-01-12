4e55f969a480a56b86a89e3c699ed31eca1e5af2
Vladimir Lazarenko <favoretti@gmail.com>
2023-01-12 23:47:01 +0100

    Rebase

--- internal/services/cognitive/client/client.go
+++ internal/services/cognitive/client/client.go
@@ -2,24 +2,18 @@ package client
 
 import (
 	"github.com/hashicorp/go-azure-sdk/resource-manager/cognitive/2022-10-01/cognitiveservicesaccounts"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/cognitive/2022-10-01/deployments"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
 
 type Client struct {
-	AccountsClient    *cognitiveservicesaccounts.CognitiveServicesAccountsClient
-	DeploymentsClient *deployments.DeploymentsClient
+	AccountsClient *cognitiveservicesaccounts.CognitiveServicesAccountsClient
 }
 
 func NewClient(o *common.ClientOptions) *Client {
-
 	accountsClient := cognitiveservicesaccounts.NewCognitiveServicesAccountsClientWithBaseURI(o.ResourceManagerEndpoint)
 	o.ConfigureClient(&accountsClient.Client, o.ResourceManagerAuthorizer)
 
-	deploymentsClient := deployments.NewDeploymentsClientWithBaseURI(o.ResourceManagerEndpoint)
-	o.ConfigureClient(&deploymentsClient.Client, o.ResourceManagerAuthorizer)
 	return &Client{
-		AccountsClient:    &accountsClient,
-		DeploymentsClient: &deploymentsClient,
+		AccountsClient: &accountsClient,
 	}
 }
