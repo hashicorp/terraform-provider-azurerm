aa97ca00c65796db8143354591e8919586834166
Jiawei Tao <jiaweitao@microsoft.com>
2023-01-13 16:46:02 +0800

    Update acc test parameter

--- internal/services/orbital/contact_resource_test.go
+++ internal/services/orbital/contact_resource_test.go
@@ -55,7 +55,7 @@ func (r ContactResource) basic(data acceptance.TestData) string {
 resource "azurerm_orbital_contact" "test" {
   name                   = "testcontact-%[2]d"
   resource_group_name    = azurerm_resource_group.test.name
-  spacecraft_id          = azurerm_orbital_spacecraft.test.id
+  spacecraft_id          = "/subscriptions/1a6092a6-137e-4025-9a7c-ef77f76f2c02/resourceGroups/spaceymcspaceface/providers/Microsoft.Orbital/spacecrafts/spaceymcspaceface"
   reservation_start_time = "2020-07-16T20:35:00.00Z"
   reservation_end_time   = "2020-07-16T20:55:00.00Z"
   ground_station_name    = "WESTUS2_0"
@@ -73,22 +73,6 @@ provider "azurerm" {
 resource "azurerm_resource_group" "test" {
   name     = "acctestRG-%[1]d"
   location = "%[2]s"
-}
-
-resource "azurerm_orbital_spacecraft" "test" {
-  name                = "acctestspacecraft-%[1]d"
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-  norad_id            = "12345"
-  links {
-    bandwidth_mhz        = 100
-    center_frequency_mhz = 101
-    direction            = "Uplink"
-    polarization         = "LHCP"
-    name                 = "linkname"
-  }
-  two_line_elements = ["1 23455U 94089A   97320.90946019  .00000140  00000-0  10191-3 0  2621", "2 23455  99.0090 272.6745 0008546 223.1686 136.8816 14.11711747148495"]
-  title_line        = "AQUA"
 }
 
 resource "azurerm_virtual_network" "test" {
