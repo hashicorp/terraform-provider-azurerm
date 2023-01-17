916320d57e5b913617ba19d4c07e38fdc29ade3c
Heng Lu <henglu@microsoft.com>
2023-01-17 09:43:36 +0800

    add issue

--- internal/services/springcloud/spring_cloud_app_resource.go
+++ internal/services/springcloud/spring_cloud_app_resource.go
@@ -560,6 +560,9 @@ func flattenAppCustomPersistentDiskResourceArray(input *[]appplatform.CustomPers
 	for _, item := range *input {
 		var storageName string
 		if item.StorageID != nil {
+			// The returned value has inconsistent casing
+			// TODO: Remove the normalization codes once the following issue is fixed.
+			// Issue: https://github.com/Azure/azure-rest-api-specs/issues/22205
 			if id, err := parse.SpringCloudStorageIDInsensitively(*item.StorageID); err == nil {
 				storageName = id.StorageName
 			}
