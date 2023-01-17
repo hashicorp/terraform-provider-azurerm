aefa815053f39511b41332e860490f6d3503f036
Paul Chvl <pchanvallon@gmail.com>
2023-01-17 21:17:28 +0100

    `data.azurerm_private_dns_resolver` - refactor from other review comments(#20042)

--- internal/services/privatednsresolver/private_dns_resolver_data_source.go
+++ internal/services/privatednsresolver/private_dns_resolver_data_source.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/dnsresolver/2022-07-01/dnsresolvers"
@@ -75,19 +74,16 @@ func (r PrivateDNSResolverDnsResolverDataSource) Read() sdk.ResourceFunc {
 				return fmt.Errorf("decoding: %+v", err)
 			}
 
-			var top int64 = 1
-			resp, err := client.ListByResourceGroupCompleteMatchingPredicate(ctx,
-				commonids.NewResourceGroupID(metadata.Client.Account.SubscriptionId, state.ResourceGroupName),
-				dnsresolvers.ListByResourceGroupOperationOptions{Top: &top},
-				dnsresolvers.DnsResolverOperationPredicate{Name: &state.Name})
-			if err != nil || len(resp.Items) != int(top) {
-				return fmt.Errorf("retrieving %s: %+v", state.Name, err)
+			id := dnsresolvers.NewDnsResolverID(
+				metadata.Client.Account.SubscriptionId, state.ResourceGroupName, state.Name)
+			resp, err := client.Get(ctx, id)
+			if err != nil {
+				return fmt.Errorf("retrieving %s: %+v", id, err)
 			}
 
-			model := resp.Items[0]
-			id, err := dnsresolvers.ParseDnsResolverID(*model.Id)
-			if err != nil {
-				return err
+			model := resp.Model
+			if model == nil {
+				return fmt.Errorf("retrieving %s: model was nil", id)
 			}
 
 			state.Location = location.Normalize(model.Location)
