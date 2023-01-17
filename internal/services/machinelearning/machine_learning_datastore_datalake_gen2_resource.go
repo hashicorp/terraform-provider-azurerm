a88e24983848acda8dffa294f57df7a4db791f2e
xuzhang3 <Zhangxu894765>
2023-01-17 13:15:18 +0800

    machine learning datastore data lake gen2

---
+++ internal/services/machinelearning/machine_learning_datastore_datalake_gen2_resource.go
@@ -0,0 +1,326 @@
+package machinelearning
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/machinelearningservices/2022-05-01/datastore"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/machinelearningservices/2022-05-01/workspaces"
+	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/machinelearning/validate"
+	storageparse "github.com/hashicorp/terraform-provider-azurerm/internal/services/storage/parse"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+)
+
+func resourceMachineLearningDataStoreDataLakeGen2() *pluginsdk.Resource {
+	resource := &pluginsdk.Resource{
+		Create: resourceMachineLearningDataStoreDataLakeGen2Create,
+		Read:   resourceMachineLearningDataStoreDataLakeGen2Read,
+		Update: resourceMachineLearningDataStoreDataLakeGen2Update,
+		Delete: resourceMachineLearningDataStoreDataLakeGen2Delete,
+
+		Importer: pluginsdk.ImporterValidatingResourceId(func(id string) error {
+			_, err := datastore.ParseDataStoreID(id)
+			return err
+		}),
+
+		Timeouts: &pluginsdk.ResourceTimeout{
+			Create: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
+			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
+		},
+
+		Schema: map[string]*pluginsdk.Schema{
+			"name": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validate.DataStoreName,
+			},
+
+			"workspace_id": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validate.WorkspaceID,
+			},
+
+			"storage_container_id": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"tenant_id": {
+				Type:         pluginsdk.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.IsUUID,
+				RequiredWith: []string{"client_id", "client_secret"},
+			},
+
+			"client_id": {
+				Type:         pluginsdk.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.IsUUID,
+				RequiredWith: []string{"tenant_id", "client_secret"},
+			},
+
+			"client_secret": {
+				Type:         pluginsdk.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+				RequiredWith: []string{"tenant_id", "client_id"},
+			},
+
+			"description": {
+				Type:     pluginsdk.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+
+			"is_default": {
+				Type:     pluginsdk.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+
+			"service_data_auth_identity": {
+				Type:     pluginsdk.TypeString,
+				Optional: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					string(datastore.ServiceDataAccessAuthIdentityNone),
+					string(datastore.ServiceDataAccessAuthIdentityWorkspaceSystemAssignedIdentity),
+					string(datastore.ServiceDataAccessAuthIdentityWorkspaceUserAssignedIdentity),
+				},
+					false),
+				Default: string(datastore.ServiceDataAccessAuthIdentityNone),
+			},
+
+			"authority_url": {
+				Type:         pluginsdk.TypeString,
+				Optional:     true,
+				Computed:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
+
+			"tags": commonschema.TagsForceNew(),
+		},
+	}
+	return resource
+}
+
+func resourceMachineLearningDataStoreDataLakeGen2Create(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).MachineLearning.DatastoreClient
+	subscriptionId := meta.(*clients.Client).Account.SubscriptionId
+	ctx, cancel := timeouts.ForCreate(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	workspaceId, err := workspaces.ParseWorkspaceID(d.Get("workspace_id").(string))
+	if err != nil {
+		return err
+	}
+
+	id := datastore.NewDataStoreID(subscriptionId, workspaceId.ResourceGroupName, workspaceId.WorkspaceName, d.Get("name").(string))
+	if d.IsNewResource() {
+		existing, err := client.Get(ctx, id)
+		if err != nil {
+			if !response.WasNotFound(existing.HttpResponse) {
+				return fmt.Errorf("checking for presence of existing %s: %+v", id, err)
+			}
+		}
+		if !response.WasNotFound(existing.HttpResponse) {
+			return tf.ImportAsExistsError("azurerm_machine_learning_datastore_datalake_gen2", id.ID())
+		}
+	}
+
+	containerId, err := storageparse.StorageContainerResourceManagerID(d.Get("storage_container_id").(string))
+	if err != nil {
+		return err
+	}
+
+	datastoreRaw := datastore.DatastoreResource{
+		Name: utils.String(d.Get("name").(string)),
+		Type: utils.ToPtr(string(datastore.DatastoreTypeAzureDataLakeGenTwo)),
+	}
+
+	props := &datastore.AzureDataLakeGen2Datastore{
+		AccountName:                   containerId.StorageAccountName,
+		Filesystem:                    containerId.ContainerName,
+		Description:                   utils.String(d.Get("description").(string)),
+		ServiceDataAccessAuthIdentity: utils.ToPtr(datastore.ServiceDataAccessAuthIdentity(d.Get("service_data_auth_identity").(string))),
+		IsDefault:                     utils.Bool(d.Get("is_default").(bool)),
+		Tags:                          utils.ToPtr(expandTags(d.Get("tags").(map[string]interface{}))),
+	}
+
+	creds := map[string]interface{}{
+		"credentialsType": "None",
+	}
+
+	tenantId := d.Get("tenant_id").(string)
+	clientId := d.Get("client_id").(string)
+	clientSecret := d.Get("client_secret").(string)
+	if len(tenantId) != 0 && len(clientId) != 0 && len(clientSecret) != 0 {
+		creds = map[string]interface{}{
+			"credentialsType": string(datastore.CredentialsTypeServicePrincipal),
+			"authorityUrl":    d.Get("authority_url").(string),
+			"resourceUrl":     "https://datalake.azure.net/",
+			"tenantId":        tenantId,
+			"clientId":        clientId,
+			"secrets": map[string]interface{}{
+				"secretsType":  "ServicePrincipal",
+				"clientSecret": clientSecret,
+			},
+		}
+	}
+	props.Credentials = creds
+	datastoreRaw.Properties = props
+
+	_, err = client.CreateOrUpdate(ctx, id, datastoreRaw, datastore.DefaultCreateOrUpdateOperationOptions())
+	if err != nil {
+		return fmt.Errorf("creating/updating %s: %+v", id, err)
+	}
+
+	d.SetId(id.ID())
+	return resourceMachineLearningDataStoreDataLakeGen2Read(d, meta)
+}
+
+func resourceMachineLearningDataStoreDataLakeGen2Update(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).MachineLearning.DatastoreClient
+	subscriptionId := meta.(*clients.Client).Account.SubscriptionId
+	ctx, cancel := timeouts.ForCreate(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	workspaceId, err := workspaces.ParseWorkspaceID(d.Get("workspace_id").(string))
+	if err != nil {
+		return err
+	}
+
+	id := datastore.NewDataStoreID(subscriptionId, workspaceId.ResourceGroupName, workspaceId.WorkspaceName, d.Get("name").(string))
+
+	containerId, err := storageparse.StorageContainerResourceManagerID(d.Get("storage_container_id").(string))
+	if err != nil {
+		return err
+	}
+
+	datastoreRaw := datastore.DatastoreResource{
+		Name: utils.String(id.Name),
+		Type: utils.ToPtr(string(datastore.DatastoreTypeAzureDataLakeGenTwo)),
+	}
+
+	props := &datastore.AzureDataLakeGen2Datastore{
+		AccountName:                   containerId.StorageAccountName,
+		Filesystem:                    containerId.ContainerName,
+		Description:                   utils.String(d.Get("description").(string)),
+		ServiceDataAccessAuthIdentity: utils.ToPtr(datastore.ServiceDataAccessAuthIdentity(d.Get("service_data_auth_identity").(string))),
+		IsDefault:                     utils.Bool(d.Get("is_default").(bool)),
+		Tags:                          utils.ToPtr(expandTags(d.Get("tags").(map[string]interface{}))),
+	}
+
+	creds := map[string]interface{}{
+		"credentialsType": "None",
+	}
+
+	tenantId := d.Get("tenant_id").(string)
+	clientId := d.Get("client_id").(string)
+	clientSecret := d.Get("client_secret").(string)
+	if len(tenantId) != 0 && len(clientId) != 0 && len(clientSecret) != 0 {
+		creds = map[string]interface{}{
+			"credentialsType": string(datastore.CredentialsTypeServicePrincipal),
+			"authorityUrl":    d.Get("authority_url").(string),
+			"resourceUrl":     "https://datalake.azure.net/",
+			"tenantId":        tenantId,
+			"clientId":        clientId,
+			"secrets": map[string]interface{}{
+				"secretsType":  "ServicePrincipal",
+				"clientSecret": clientSecret,
+			},
+		}
+	}
+	props.Credentials = creds
+	datastoreRaw.Properties = props
+
+	_, err = client.CreateOrUpdate(ctx, id, datastoreRaw, datastore.DefaultCreateOrUpdateOperationOptions())
+	if err != nil {
+		return fmt.Errorf("creating/updating %s: %+v", id, err)
+	}
+
+	d.SetId(id.ID())
+	return resourceMachineLearningDataStoreDataLakeGen2Read(d, meta)
+}
+
+func resourceMachineLearningDataStoreDataLakeGen2Read(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).MachineLearning.DatastoreClient
+	subscriptionId := meta.(*clients.Client).Account.SubscriptionId
+	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	id, err := datastore.ParseDataStoreID(d.Id())
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Get(ctx, *id)
+	if err != nil {
+		if response.WasNotFound(resp.HttpResponse) {
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("reading %s: %+v", *id, err)
+	}
+
+	workspaceId := workspaces.NewWorkspaceID(subscriptionId, id.ResourceGroupName, id.WorkspaceName)
+	d.Set("name", resp.Model.Name)
+	d.Set("workspace_id", workspaceId.ID())
+
+	data := resp.Model.Properties.(datastore.AzureDataLakeGen2Datastore)
+
+	serviceDataAuth := ""
+	if v := data.ServiceDataAccessAuthIdentity; v != nil {
+		serviceDataAuth = string(*v)
+	}
+	d.Set("service_data_auth_identity", serviceDataAuth)
+
+	containerId := storageparse.NewStorageContainerResourceManagerID(subscriptionId, workspaceId.ResourceGroupName, data.AccountName, "default", data.Filesystem)
+	d.Set("storage_container_id", containerId.ID())
+
+	if creds, ok := data.Credentials.(datastore.ServicePrincipalDatastoreCredentials); ok {
+		if !strings.EqualFold(creds.TenantId, "00000000-0000-0000-0000-000000000000") && !strings.EqualFold(creds.ClientId, "00000000-0000-0000-0000-000000000000") {
+			d.Set("tenant_id", creds.TenantId)
+			d.Set("client_id", creds.ClientId)
+		}
+	}
+
+	desc := ""
+	if v := data.Description; v != nil {
+		d.Set("description", desc)
+	}
+
+	d.Set("is_default", data.IsDefault)
+	return flattenAndSetTags(d, *data.Tags)
+}
+
+func resourceMachineLearningDataStoreDataLakeGen2Delete(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).MachineLearning.DatastoreClient
+	ctx, cancel := timeouts.ForDelete(meta.(*clients.Client).StopContext, d)
+	defer cancel()
+
+	id, err := datastore.ParseDataStoreID(d.Id())
+	if err != nil {
+		return err
+	}
+
+	if _, err := client.Delete(ctx, *id); err != nil {
+		return fmt.Errorf("deleting %s: %+v", *id, err)
+	}
+
+	return nil
+}
--- internal/services/machinelearning/registration.go
+++ internal/services/machinelearning/registration.go
@@ -34,11 +34,12 @@ func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
 // SupportedResources returns the supported Resources supported by this Service
 func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
 	return map[string]*pluginsdk.Resource{
-		"azurerm_machine_learning_compute_cluster":       resourceComputeCluster(),
-		"azurerm_machine_learning_compute_instance":      resourceComputeInstance(),
-		"azurerm_machine_learning_datastore_blobstorage": resourceMachineLearningDataStoreBlobStorage(),
-		"azurerm_machine_learning_inference_cluster":     resourceAksInferenceCluster(),
-		"azurerm_machine_learning_synapse_spark":         resourceSynapseSpark(),
-		"azurerm_machine_learning_workspace":             resourceMachineLearningWorkspace(),
+		"azurerm_machine_learning_compute_cluster":         resourceComputeCluster(),
+		"azurerm_machine_learning_compute_instance":        resourceComputeInstance(),
+		"azurerm_machine_learning_datastore_blobstorage":   resourceMachineLearningDataStoreBlobStorage(),
+		"azurerm_machine_learning_datastore_datalake_gen2": resourceMachineLearningDataStoreDataLakeGen2(),
+		"azurerm_machine_learning_inference_cluster":       resourceAksInferenceCluster(),
+		"azurerm_machine_learning_synapse_spark":           resourceSynapseSpark(),
+		"azurerm_machine_learning_workspace":               resourceMachineLearningWorkspace(),
 	}
 }
