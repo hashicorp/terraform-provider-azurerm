dae5f478c7ed214a45db63d5ec43bde31dc4427f
Tao <104055472+teowa@users.noreply.github.com>
2023-01-20 05:05:59 +0800

    `azurerm_monitor_diagnostic_setting` - fix update issue (#20048)
    Resolves https://github.com/hashicorp/terraform-provider-azurerm/issues/20019

--- internal/services/monitor/monitor_diagnostic_setting_resource_test.go
+++ internal/services/monitor/monitor_diagnostic_setting_resource_test.go
@@ -153,6 +153,29 @@ func TestAccMonitorDiagnosticSetting_activityLog(t *testing.T) {
 	})
 }
 
+func TestAccMonitorDiagnosticSetting_logAnalyticsDestinationType(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_monitor_diagnostic_setting", "test")
+	r := MonitorDiagnosticSettingResource{}
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.logAnalyticsDestinationTypeOmit(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("log_analytics_destination_type").HasValue("AzureDiagnostics"),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.logAnalyticsDestinationTypeUpdated(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("log_analytics_destination_type").HasValue("Dedicated"),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func TestAccMonitorDiagnosticSetting_enabledLogs(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_monitor_diagnostic_setting", "test")
 	r := MonitorDiagnosticSettingResource{}
@@ -248,7 +271,6 @@ resource "azurerm_monitor_diagnostic_setting" "test" {
   target_resource_id             = azurerm_key_vault.test.id
   eventhub_authorization_rule_id = azurerm_eventhub_namespace_authorization_rule.test.id
   eventhub_name                  = azurerm_eventhub.test.name
-  log_analytics_destination_type = "AzureDiagnostics"
 
   log {
     category = "AuditEvent"
@@ -334,7 +356,6 @@ resource "azurerm_monitor_diagnostic_setting" "test" {
   target_resource_id             = azurerm_key_vault.test.id
   eventhub_authorization_rule_id = azurerm_eventhub_namespace_authorization_rule.test.id
   eventhub_name                  = azurerm_eventhub.test.name
-  log_analytics_destination_type = "AzureDiagnostics"
 
   log {
     category_group = "Audit"
@@ -367,6 +388,7 @@ resource "azurerm_monitor_diagnostic_setting" "test" {
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomIntOfLength(17))
 }
+
 func (r MonitorDiagnosticSettingResource) requiresImport(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 %s
@@ -632,10 +654,9 @@ resource "azurerm_elastic_cloud_elasticsearch" "test" {
 }
 
 resource "azurerm_monitor_diagnostic_setting" "test" {
-  name                           = "acctest-DS-%[1]d"
-  target_resource_id             = azurerm_key_vault.test.id
-  partner_solution_id            = azurerm_elastic_cloud_elasticsearch.test.id
-  log_analytics_destination_type = "AzureDiagnostics"
+  name                = "acctest-DS-%[1]d"
+  target_resource_id  = azurerm_key_vault.test.id
+  partner_solution_id = azurerm_elastic_cloud_elasticsearch.test.id
 
   log {
     category = "AuditEvent"
@@ -700,10 +721,9 @@ resource "azurerm_key_vault" "test" {
 }
 
 resource "azurerm_monitor_diagnostic_setting" "test" {
-  name                           = "acctest-DS-%[1]d"
-  target_resource_id             = azurerm_key_vault.test.id
-  storage_account_id             = azurerm_storage_account.test.id
-  log_analytics_destination_type = "AzureDiagnostics"
+  name               = "acctest-DS-%[1]d"
+  target_resource_id = azurerm_key_vault.test.id
+  storage_account_id = azurerm_storage_account.test.id
 
   log {
     category = "AuditEvent"
@@ -766,10 +786,9 @@ resource "azurerm_storage_account" "test" {
 
 
 resource "azurerm_monitor_diagnostic_setting" "test" {
-  name                           = "acctest-DS-%[1]d"
-  target_resource_id             = data.azurerm_subscription.current.id
-  storage_account_id             = azurerm_storage_account.test.id
-  log_analytics_destination_type = "AzureDiagnostics"
+  name               = "acctest-DS-%[1]d"
+  target_resource_id = data.azurerm_subscription.current.id
+  storage_account_id = azurerm_storage_account.test.id
 
   log {
     category = "Administrative"
@@ -971,4 +990,272 @@ resource "azurerm_monitor_diagnostic_setting" "test" {
   }
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomIntOfLength(17))
+}
+
+func (MonitorDiagnosticSettingResource) logAnalyticsDestinationTypeOmit(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "current" {
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_log_analytics_workspace" "test" {
+  name                = "acctest-LAW-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_data_factory" "test" {
+  name                = "acctest-DF-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_monitor_diagnostic_setting" "test" {
+  name                       = "acctest-DS-%[1]d"
+  target_resource_id         = azurerm_data_factory.test.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.test.id
+
+  log {
+    category = "ActivityRuns"
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
+  }
+
+  log {
+    category = "PipelineRuns"
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
+  }
+
+  log {
+    category = "TriggerRuns"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SSISIntegrationRuntimeLogs"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SSISPackageEventMessageContext"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SSISPackageEventMessages"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SSISPackageExecutableStatistics"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SSISPackageExecutionComponentPhases"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SSISPackageExecutionDataStatistics"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SandboxActivityRuns"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SandboxPipelineRuns"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = false
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (MonitorDiagnosticSettingResource) logAnalyticsDestinationTypeUpdated(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "current" {
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_log_analytics_workspace" "test" {
+  name                = "acctest-LAW-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_data_factory" "test" {
+  name                = "acctest-DF-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_monitor_diagnostic_setting" "test" {
+  name                       = "acctest-DS-%[1]d"
+  target_resource_id         = azurerm_data_factory.test.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.test.id
+
+  log_analytics_destination_type = "Dedicated"
+
+  log {
+    category = "ActivityRuns"
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
+  }
+
+  log {
+    category = "PipelineRuns"
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
+  }
+
+  log {
+    category = "TriggerRuns"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SSISIntegrationRuntimeLogs"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SSISPackageEventMessageContext"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SSISPackageEventMessages"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SSISPackageExecutableStatistics"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SSISPackageExecutionComponentPhases"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SSISPackageExecutionDataStatistics"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SandboxActivityRuns"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "SandboxPipelineRuns"
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = false
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+}
+`, data.RandomInteger, data.Locations.Primary)
 }
