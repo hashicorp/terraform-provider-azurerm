28cb0fbcb14f413f3ff442fc3d2d6a354dbc5f96
ziyeqf <zyqf16@gmail.com>
2023-01-20 13:45:57 +0800

    New Resource: `azurerm_mobile_network_site`

--- internal/services/mobilenetwork/client/client.go
+++ internal/services/mobilenetwork/client/client.go
@@ -2,18 +2,24 @@ package client
 
 import (
 	"github.com/hashicorp/go-azure-sdk/resource-manager/mobilenetwork/2022-11-01/mobilenetwork"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/mobilenetwork/2022-11-01/site"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
 
 type Client struct {
 	MobileNetworkClient *mobilenetwork.MobileNetworkClient
+	SiteClient          *site.SiteClient
 }
 
 func NewClient(o *common.ClientOptions) *Client {
 	mobileNetworkClient := mobilenetwork.NewMobileNetworkClientWithBaseURI(o.ResourceManagerEndpoint)
 	o.ConfigureClient(&mobileNetworkClient.Client, o.ResourceManagerAuthorizer)
 
+	siteClient := site.NewSiteClientWithBaseURI(o.ResourceManagerEndpoint)
+	o.ConfigureClient(&siteClient.Client, o.ResourceManagerAuthorizer)
+
 	return &Client{
 		MobileNetworkClient: &mobileNetworkClient,
+		SiteClient:          &siteClient,
 	}
 }
