f11c3a676f2bcff283f42447c5b2d01ac85360ee
ziyeqf <zyqf16@gmail.com>
2023-01-20 13:51:02 +0800

    New Resource: `azurerm_mobile_network_slice`

--- internal/services/mobilenetwork/client/client.go
+++ internal/services/mobilenetwork/client/client.go
@@ -2,18 +2,25 @@ package client
 
 import (
 	"github.com/hashicorp/go-azure-sdk/resource-manager/mobilenetwork/2022-11-01/mobilenetwork"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/mobilenetwork/2022-11-01/slice"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
 
 type Client struct {
 	MobileNetworkClient *mobilenetwork.MobileNetworkClient
+	SliceClient         *slice.SliceClient
 }
 
 func NewClient(o *common.ClientOptions) *Client {
+
 	mobileNetworkClient := mobilenetwork.NewMobileNetworkClientWithBaseURI(o.ResourceManagerEndpoint)
 	o.ConfigureClient(&mobileNetworkClient.Client, o.ResourceManagerAuthorizer)
 
+	sliceClient := slice.NewSliceClientWithBaseURI(o.ResourceManagerEndpoint)
+	o.ConfigureClient(&sliceClient.Client, o.ResourceManagerAuthorizer)
+
 	return &Client{
 		MobileNetworkClient: &mobileNetworkClient,
+		SliceClient:         &sliceClient,
 	}
 }
