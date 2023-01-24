5030ea22db76c9024fddfa14cb2c0b2399bb5571
Matthew Frahry <mbfrahry@gmail.com>
2023-01-24 11:16:32 -0800

    Logic: Swap to using go-azure-sdk (#20144)

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/logic/2019-05-01/integrationaccountsessions/model_integrationaccountsessionproperties.go
@@ -0,0 +1,40 @@
+package integrationaccountsessions
+
+import (
+	"time"
+
+	"github.com/hashicorp/go-azure-helpers/lang/dates"
+)
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type IntegrationAccountSessionProperties struct {
+	ChangedTime *string      `json:"changedTime,omitempty"`
+	Content     *interface{} `json:"content,omitempty"`
+	CreatedTime *string      `json:"createdTime,omitempty"`
+}
+
+func (o *IntegrationAccountSessionProperties) GetChangedTimeAsTime() (*time.Time, error) {
+	if o.ChangedTime == nil {
+		return nil, nil
+	}
+	return dates.ParseAsFormat(o.ChangedTime, "2006-01-02T15:04:05Z07:00")
+}
+
+func (o *IntegrationAccountSessionProperties) SetChangedTimeAsTime(input time.Time) {
+	formatted := input.Format("2006-01-02T15:04:05Z07:00")
+	o.ChangedTime = &formatted
+}
+
+func (o *IntegrationAccountSessionProperties) GetCreatedTimeAsTime() (*time.Time, error) {
+	if o.CreatedTime == nil {
+		return nil, nil
+	}
+	return dates.ParseAsFormat(o.CreatedTime, "2006-01-02T15:04:05Z07:00")
+}
+
+func (o *IntegrationAccountSessionProperties) SetCreatedTimeAsTime(input time.Time) {
+	formatted := input.Format("2006-01-02T15:04:05Z07:00")
+	o.CreatedTime = &formatted
+}
