3e912c7a8e11a5cd1b91bcd4f978bb33539396bf
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-01-25 16:27:25 +0100

    resourceids: adding todo's for state migrations
    Each of these Resource IDs is going to require a state migration prior to being used in `hashicorp/go-azure-sdk`, rather
    than finding this out retroactively, I figured it'd worth calling this out explicitly via TODOs for when these are removed.

--- internal/services/logic/resourceids.go
+++ internal/services/logic/resourceids.go
@@ -1,4 +1,6 @@
 package logic
 
+// @tombuildsstuff: @mbfrahry is going to send a PR to remove/clean these up
+
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=LogicAppStandard -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Web/sites/site1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Action -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/group1/providers/Microsoft.Logic/workflows/workflow1/actions/action1
