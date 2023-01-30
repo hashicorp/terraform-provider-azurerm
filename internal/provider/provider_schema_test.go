bb0dca7a347fa42a0105e85d49dec9d035c170b2
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-01-30 12:23:19 +0100

    internal/provider: validating that the `name` field doesn't contain a default value of `Default`
    This would signify a design issue, since there can only be one of these nested resources
    this resource should be nested within the parent resource instead

--- internal/provider/provider_schema_test.go
+++ internal/provider/provider_schema_test.go
@@ -11,8 +11,6 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 )
 
-// TODO: Data Sources and Resources cannot contain a field `name` with the default of `default`
-
 func TestDataSourcesHaveSensitiveFieldsMarkedAsSensitive(t *testing.T) {
 	provider := TestAzureProvider()
 
@@ -198,6 +196,114 @@ func schemaContainsEnabledFieldsNotDefinedAsABoolean(input map[string]*schema.Sc
 		if field.Type == pluginsdk.TypeSet && field.Elem != nil {
 			if val, ok := field.Elem.(*pluginsdk.Resource); ok && val.Schema != nil {
 				if err := schemaContainsEnabledFieldsNotDefinedAsABoolean(val.Schema, fieldsToBeAddressed); err != nil {
+					return fmt.Errorf("the field %q is a Set: %+v", fieldName, err)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func TestDataSourcesDoNotContainANameFieldWithADefaultOfDefault(t *testing.T) {
+	provider := TestAzureProvider()
+
+	// intentionally sorting these so the output is consistent
+	dataSourceNames := make([]string, 0)
+	for dataSourceName := range provider.DataSourcesMap {
+		dataSourceNames = append(dataSourceNames, dataSourceName)
+	}
+	sort.Strings(dataSourceNames)
+
+	for _, dataSourceName := range dataSourceNames {
+		dataSource := provider.DataSourcesMap[dataSourceName]
+		if err := schemaContainsANameFieldWithADefaultValueOfDefault(dataSource.Schema, map[string]struct{}{}); err != nil {
+			t.Fatalf("the Data Source %q contains a `name` field with a default value of `default` - this Data Source should be exposed as part of the parent Data Source it's located within: %+v", dataSourceName, err)
+		}
+	}
+}
+
+func TestResourcesDoNotContainANameFieldWithADefaultOfDefault(t *testing.T) {
+	provider := TestAzureProvider()
+
+	// intentionally sorting these so the output is consistent
+	resourceNames := make([]string, 0)
+	for resourceName := range provider.ResourcesMap {
+		resourceNames = append(resourceNames, resourceName)
+	}
+	sort.Strings(resourceNames)
+
+	// TODO: 4.0 - work through this list
+	resourceFieldsWhichNeedToBeAddressed := map[string]map[string]struct{}{
+		// 1: to be addressed in 4.0
+		"azurerm_datadog_monitor_sso_configuration": {
+			// TODO: in 4.0 this resource probably wants embedding within `azurerm_datadog_monitor`
+			// which'll also need the Monitor resource to have Create call Update
+			"name": {},
+		},
+		"azurerm_datadog_monitor_tag_rule": {
+			// TODO: in 4.0 this resource probably wants embedding within `azurerm_datadog_monitor`
+			// which'll also need the Monitor resource to have Create call Update
+			"name": {},
+		},
+
+		// 2. False Positives?
+		"azurerm_redis_enterprise_database": {
+			"name": {},
+		},
+	}
+
+	for _, resourceName := range resourceNames {
+		resource := provider.ResourcesMap[resourceName]
+		fieldsToBeAddressed := resourceFieldsWhichNeedToBeAddressed[resourceName]
+
+		if err := schemaContainsANameFieldWithADefaultValueOfDefault(resource.Schema, fieldsToBeAddressed); err != nil {
+			t.Fatalf("the Resource %q contains a `name` field with a default value of `default` - this Resource should be exposed as part of the parent Resource it's located within: %+v", resourceName, err)
+		}
+	}
+}
+
+func schemaContainsANameFieldWithADefaultValueOfDefault(input map[string]*schema.Schema, fieldsToBeAddressed map[string]struct{}) error {
+	// intentionally sorting these so the output is consistent
+	fieldNames := make([]string, 0)
+	for fieldName := range input {
+		fieldNames = append(fieldNames, fieldName)
+	}
+	sort.Strings(fieldNames)
+
+	for _, fieldName := range fieldNames {
+		key := strings.ToLower(fieldName)
+		field := input[fieldName]
+
+		// @tombuildsstuff: we have some Resources which will need to be addressed in the next major version (v4.0)
+		// if this field name matches one we're intentionally ignoring, let's ignore it for now
+		if _, shouldIgnore := fieldsToBeAddressed[key]; shouldIgnore {
+			continue
+		}
+
+		if strings.EqualFold(key, "name") {
+			var defaultValue any
+			if field.Default != nil {
+				defaultValue = field.Default
+			}
+			if v, ok := defaultValue.(string); ok {
+				if strings.EqualFold(v, "default") {
+					return fmt.Errorf("field %q is a `name` field which contains a default value of `default`", fieldName)
+				}
+			}
+		}
+
+		if field.Type == pluginsdk.TypeList && field.Elem != nil {
+			if val, ok := field.Elem.(*pluginsdk.Resource); ok && val.Schema != nil {
+				if err := schemaContainsANameFieldWithADefaultValueOfDefault(val.Schema, fieldsToBeAddressed); err != nil {
+					return fmt.Errorf("the field %q is a List: %+v", fieldName, err)
+				}
+			}
+		}
+
+		if field.Type == pluginsdk.TypeSet && field.Elem != nil {
+			if val, ok := field.Elem.(*pluginsdk.Resource); ok && val.Schema != nil {
+				if err := schemaContainsANameFieldWithADefaultValueOfDefault(val.Schema, fieldsToBeAddressed); err != nil {
 					return fmt.Errorf("the field %q is a Set: %+v", fieldName, err)
 				}
 			}
