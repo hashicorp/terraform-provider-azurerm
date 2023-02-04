5823196c3d25917ed8f9584fc5b8853db15d3dfd
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-02-04 14:40:16 +0100

    vendoring: refactoring `redis` to use `hashicorp/go-azure-sdk`

--- internal/services/redis/resourceids.go
+++
@@ -1,7 +0,0 @@
-package redis
-
-// @tombuildsstuff: these three are going to need state migrations to account for `Redis` -> `redis` prior to adopting `hashicorp/go-azure-sdk`
-
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Cache -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Cache/Redis/redis1
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=FirewallRule -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Cache/Redis/redis1/firewallRules/firewallRule1
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=LinkedServer -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Cache/Redis/redis1/linkedServers/linkedServer1
---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/redis/2021-06-01/patchschedules/constants.go
@@ -0,0 +1,55 @@
+package patchschedules
+
+import "strings"
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type DayOfWeek string
+
+const (
+	DayOfWeekEveryday  DayOfWeek = "Everyday"
+	DayOfWeekFriday    DayOfWeek = "Friday"
+	DayOfWeekMonday    DayOfWeek = "Monday"
+	DayOfWeekSaturday  DayOfWeek = "Saturday"
+	DayOfWeekSunday    DayOfWeek = "Sunday"
+	DayOfWeekThursday  DayOfWeek = "Thursday"
+	DayOfWeekTuesday   DayOfWeek = "Tuesday"
+	DayOfWeekWednesday DayOfWeek = "Wednesday"
+	DayOfWeekWeekend   DayOfWeek = "Weekend"
+)
+
+func PossibleValuesForDayOfWeek() []string {
+	return []string{
+		string(DayOfWeekEveryday),
+		string(DayOfWeekFriday),
+		string(DayOfWeekMonday),
+		string(DayOfWeekSaturday),
+		string(DayOfWeekSunday),
+		string(DayOfWeekThursday),
+		string(DayOfWeekTuesday),
+		string(DayOfWeekWednesday),
+		string(DayOfWeekWeekend),
+	}
+}
+
+func parseDayOfWeek(input string) (*DayOfWeek, error) {
+	vals := map[string]DayOfWeek{
+		"everyday":  DayOfWeekEveryday,
+		"friday":    DayOfWeekFriday,
+		"monday":    DayOfWeekMonday,
+		"saturday":  DayOfWeekSaturday,
+		"sunday":    DayOfWeekSunday,
+		"thursday":  DayOfWeekThursday,
+		"tuesday":   DayOfWeekTuesday,
+		"wednesday": DayOfWeekWednesday,
+		"weekend":   DayOfWeekWeekend,
+	}
+	if v, ok := vals[strings.ToLower(input)]; ok {
+		return &v, nil
+	}
+
+	// otherwise presume it's an undefined value and best-effort it
+	out := DayOfWeek(input)
+	return &out, nil
+}
