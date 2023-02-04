5d05a6fa81e386aa4ae2ddbfce18341cf0926286
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-02-04 12:19:58 +0100

    r/datadog_monitor_sso_configuration: adding a note to consider inlining this resource in 4.0

--- internal/services/datadog/azurerm_datadog_monitor_sso_configuration.go
+++ internal/services/datadog/azurerm_datadog_monitor_sso_configuration.go
@@ -14,6 +14,9 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
+// @tombuildsstuff: in 4.0 consider inlining this within the `azurerm_datadog_monitors` resource
+// since this appears to be a 1:1 with it (given the name defaults to `default`)
+
 func resourceDatadogSingleSignOnConfigurations() *pluginsdk.Resource {
 	return &pluginsdk.Resource{
 		Create: resourceDatadogSingleSignOnConfigurationsCreateorUpdate,
