c5bcd382e7866b007eaeca95d5ab7a7c085250c0
elena <sinbai@hotmail.com>
2023-02-07 13:51:38 +0800

    update code

--- internal/services/databricks/databricks_access_connector_resource_test.go
+++ internal/services/databricks/databricks_access_connector_resource_test.go
@@ -87,9 +87,6 @@ resource "azurerm_databricks_access_connector" "import" {
   name                = azurerm_databricks_access_connector.test.name
   resource_group_name = azurerm_databricks_access_connector.test.resource_group_name
   location            = azurerm_databricks_access_connector.test.location
-  identity {
-    type = "SystemAssigned"
-  }
 }
 `, template)
 }
