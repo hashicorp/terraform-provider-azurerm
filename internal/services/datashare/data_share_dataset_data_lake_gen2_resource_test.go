a30602069f639edacc367a088a2ef4846524535b
stephybun <steph@hashicorp.com>
2023-02-16 22:15:14 +0100

    `datashare` - switch to use `hashicorp/go-azure-sdk` (#20501)

--- internal/services/datashare/data_share_dataset_data_lake_gen2_resource_test.go
+++ internal/services/datashare/data_share_dataset_data_lake_gen2_resource_test.go
@@ -5,11 +5,10 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/services/datashare/mgmt/2019-11-01/datashare" // nolint: staticcheck
+	"github.com/hashicorp/go-azure-sdk/resource-manager/datashare/2019-11-01/dataset"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/services/datashare/parse"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
@@ -80,81 +79,83 @@ func TestAccDataShareDataLakeGen2DataSet_requiresImport(t *testing.T) {
 }
 
 func (t DataShareDataSetDataLakeGen2Resource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
-	id, err := parse.DataSetID(state.ID)
+	id, err := dataset.ParseDataSetID(state.ID)
 	if err != nil {
 		return nil, err
 	}
 
-	resp, err := clients.DataShare.DataSetClient.Get(ctx, id.ResourceGroup, id.AccountName, id.ShareName, id.Name)
+	resp, err := clients.DataShare.DataSetClient.Get(ctx, *id)
 	if err != nil {
-		return nil, fmt.Errorf("retrieving Data Share Data Set %q (resource group: %q): %+v", id.Name, id.ResourceGroup, err)
+		return nil, fmt.Errorf("retrieving %s: %+v", *id, err)
 	}
 
-	switch resp := resp.Value.(type) {
-	case datashare.ADLSGen2FileDataSet:
-		return utils.Bool(resp.ADLSGen2FileProperties != nil), nil
-
-	case datashare.ADLSGen2FolderDataSet:
-		return utils.Bool(resp.ADLSGen2FolderProperties != nil), nil
-
-	case datashare.ADLSGen2FileSystemDataSet:
-		return utils.Bool(resp.ADLSGen2FileSystemProperties != nil), nil
+	if model := resp.Model; model != nil {
+		ds := *model
+		if _, ok := ds.(dataset.ADLSGen2FileDataSet); ok {
+			return utils.Bool(true), nil
+		}
+		if _, ok := ds.(dataset.ADLSGen2FolderDataSet); ok {
+			return utils.Bool(true), nil
+		}
+		if _, ok := ds.(dataset.ADLSGen2FileSystemDataSet); ok {
+			return utils.Bool(true), nil
+		}
 	}
 
-	return nil, fmt.Errorf("Data Share Data %q (Resource Group %q / accountName %q / shareName %q) is not a datalake store gen2 dataset", id.Name, id.ResourceGroup, id.AccountName, id.ShareName)
+	return nil, fmt.Errorf("%s is not a datalake store gen2 dataset", *id)
 }
 
 func (DataShareDataSetDataLakeGen2Resource) template(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
-  features {}
+ features {}
 }
 
 provider "azuread" {
 }
 
 resource "azurerm_resource_group" "test" {
-  name     = "acctestRG-datashare-%[1]d"
-  location = "%[2]s"
+ name     = "acctestRG-datashare-%[1]d"
+ location = "%[2]s"
 }
 
 resource "azurerm_data_share_account" "test" {
-  name                = "acctest-dsa-%[1]d"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  identity {
-    type = "SystemAssigned"
-  }
+ name                = "acctest-dsa-%[1]d"
+ location            = azurerm_resource_group.test.location
+ resource_group_name = azurerm_resource_group.test.name
+ identity {
+   type = "SystemAssigned"
+ }
 }
 
 resource "azurerm_data_share" "test" {
-  name       = "acctest_ds_%[1]d"
-  account_id = azurerm_data_share_account.test.id
-  kind       = "CopyBased"
+ name       = "acctest_ds_%[1]d"
+ account_id = azurerm_data_share_account.test.id
+ kind       = "CopyBased"
 }
 
 resource "azurerm_storage_account" "test" {
-  name                     = "accteststr%[3]s"
-  resource_group_name      = azurerm_resource_group.test.name
-  location                 = azurerm_resource_group.test.location
-  account_kind             = "BlobStorage"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+ name                     = "accteststr%[3]s"
+ resource_group_name      = azurerm_resource_group.test.name
+ location                 = azurerm_resource_group.test.location
+ account_kind             = "BlobStorage"
+ account_tier             = "Standard"
+ account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "test" {
-  name               = "acctest-%[1]d"
-  storage_account_id = azurerm_storage_account.test.id
+ name               = "acctest-%[1]d"
+ storage_account_id = azurerm_storage_account.test.id
 }
 
 data "azuread_service_principal" "test" {
-  display_name = azurerm_data_share_account.test.name
+ display_name = azurerm_data_share_account.test.name
 }
 
 resource "azurerm_role_assignment" "test" {
-  scope                = azurerm_storage_account.test.id
-  role_definition_name = "Storage Blob Data Reader"
-  principal_id         = data.azuread_service_principal.test.object_id
+ scope                = azurerm_storage_account.test.id
+ role_definition_name = "Storage Blob Data Reader"
+ principal_id         = data.azuread_service_principal.test.object_id
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomString)
 }
@@ -164,14 +165,14 @@ func (r DataShareDataSetDataLakeGen2Resource) basicFile(data acceptance.TestData
 %s
 
 resource "azurerm_data_share_dataset_data_lake_gen2" "test" {
-  name               = "acctest-dlds-%d"
-  share_id           = azurerm_data_share.test.id
-  storage_account_id = azurerm_storage_account.test.id
-  file_system_name   = azurerm_storage_data_lake_gen2_filesystem.test.name
-  file_path          = "myfile.txt"
-  depends_on = [
-    azurerm_role_assignment.test,
-  ]
+ name               = "acctest-dlds-%d"
+ share_id           = azurerm_data_share.test.id
+ storage_account_id = azurerm_storage_account.test.id
+ file_system_name   = azurerm_storage_data_lake_gen2_filesystem.test.name
+ file_path          = "myfile.txt"
+ depends_on = [
+   azurerm_role_assignment.test,
+ ]
 }
 `, r.template(data), data.RandomInteger)
 }
@@ -181,14 +182,14 @@ func (r DataShareDataSetDataLakeGen2Resource) basicFolder(data acceptance.TestDa
 %s
 
 resource "azurerm_data_share_dataset_data_lake_gen2" "test" {
-  name               = "acctest-dlds-%d"
-  share_id           = azurerm_data_share.test.id
-  storage_account_id = azurerm_storage_account.test.id
-  file_system_name   = azurerm_storage_data_lake_gen2_filesystem.test.name
-  folder_path        = "test"
-  depends_on = [
-    azurerm_role_assignment.test,
-  ]
+ name               = "acctest-dlds-%d"
+ share_id           = azurerm_data_share.test.id
+ storage_account_id = azurerm_storage_account.test.id
+ file_system_name   = azurerm_storage_data_lake_gen2_filesystem.test.name
+ folder_path        = "test"
+ depends_on = [
+   azurerm_role_assignment.test,
+ ]
 }
 `, r.template(data), data.RandomInteger)
 }
@@ -198,13 +199,13 @@ func (r DataShareDataSetDataLakeGen2Resource) basicSystem(data acceptance.TestDa
 %s
 
 resource "azurerm_data_share_dataset_data_lake_gen2" "test" {
-  name               = "acctest-dlds-%d"
-  share_id           = azurerm_data_share.test.id
-  storage_account_id = azurerm_storage_account.test.id
-  file_system_name   = azurerm_storage_data_lake_gen2_filesystem.test.name
-  depends_on = [
-    azurerm_role_assignment.test,
-  ]
+ name               = "acctest-dlds-%d"
+ share_id           = azurerm_data_share.test.id
+ storage_account_id = azurerm_storage_account.test.id
+ file_system_name   = azurerm_storage_data_lake_gen2_filesystem.test.name
+ depends_on = [
+   azurerm_role_assignment.test,
+ ]
 }
 `, r.template(data), data.RandomInteger)
 }
@@ -214,11 +215,11 @@ func (r DataShareDataSetDataLakeGen2Resource) requiresImport(data acceptance.Tes
 %s
 
 resource "azurerm_data_share_dataset_data_lake_gen2" "import" {
-  name               = azurerm_data_share_dataset_data_lake_gen2.test.name
-  share_id           = azurerm_data_share.test.id
-  storage_account_id = azurerm_data_share_dataset_data_lake_gen2.test.storage_account_id
-  file_system_name   = azurerm_data_share_dataset_data_lake_gen2.test.file_system_name
-  file_path          = azurerm_data_share_dataset_data_lake_gen2.test.file_path
+ name               = azurerm_data_share_dataset_data_lake_gen2.test.name
+ share_id           = azurerm_data_share.test.id
+ storage_account_id = azurerm_data_share_dataset_data_lake_gen2.test.storage_account_id
+ file_system_name   = azurerm_data_share_dataset_data_lake_gen2.test.file_system_name
+ file_path          = azurerm_data_share_dataset_data_lake_gen2.test.file_path
 }
 `, r.basicFile(data))
 }
---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/datashare/2019-11-01/dataset/model_adlsgen1folderdataset.go
@@ -0,0 +1,44 @@
+package dataset
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+var _ DataSet = ADLSGen1FolderDataSet{}
+
+type ADLSGen1FolderDataSet struct {
+	Properties ADLSGen1FolderProperties `json:"properties"`
+
+	// Fields inherited from DataSet
+	Id   *string `json:"id,omitempty"`
+	Name *string `json:"name,omitempty"`
+	Type *string `json:"type,omitempty"`
+}
+
+var _ json.Marshaler = ADLSGen1FolderDataSet{}
+
+func (s ADLSGen1FolderDataSet) MarshalJSON() ([]byte, error) {
+	type wrapper ADLSGen1FolderDataSet
+	wrapped := wrapper(s)
+	encoded, err := json.Marshal(wrapped)
+	if err != nil {
+		return nil, fmt.Errorf("marshaling ADLSGen1FolderDataSet: %+v", err)
+	}
+
+	var decoded map[string]interface{}
+	if err := json.Unmarshal(encoded, &decoded); err != nil {
+		return nil, fmt.Errorf("unmarshaling ADLSGen1FolderDataSet: %+v", err)
+	}
+	decoded["kind"] = "AdlsGen1Folder"
+
+	encoded, err = json.Marshal(decoded)
+	if err != nil {
+		return nil, fmt.Errorf("re-marshaling ADLSGen1FolderDataSet: %+v", err)
+	}
+
+	return encoded, nil
+}
