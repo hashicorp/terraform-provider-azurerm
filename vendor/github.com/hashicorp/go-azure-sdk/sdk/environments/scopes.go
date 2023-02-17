ccac3bc88e1c9985cb46135f3b0f6df5c28e118c
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-02-17 15:19:56 +0100

    dependencies: updating to `v0.20230217.1150808` of `github.com/hashicorp/go-azure-sdk`

--- vendor/github.com/hashicorp/go-azure-sdk/sdk/environments/scopes.go
+++ vendor/github.com/hashicorp/go-azure-sdk/sdk/environments/scopes.go
@@ -18,7 +18,13 @@ func Resource(endpoint Api) (*string, error) {
 func Scope(endpoint Api) (*string, error) {
 	e, ok := endpoint.ResourceIdentifier()
 	if !ok {
-		return nil, fmt.Errorf("the endpoint %q is not supported in this Azure Environment", endpoint.Name())
+		// if this API has been defined in-line it may not have a Resource Identifier - however
+		// the scope will be the endpoint instead, so we can best-effort to obtain the auth token
+		e2, ok2 := endpoint.Endpoint()
+		if !ok2 {
+			return nil, fmt.Errorf("the endpoint %q is not supported in this Azure Environment", endpoint.Name())
+		}
+		e = e2
 	}
 	out := fmt.Sprintf("%s/.default", *e)
 	return pointer.To(out), nil
