7c9663bba26442c7a687521b8b936b1145029232
ziyeqf <zyqf16@gmail.com>
2023-02-17 11:21:30 +0800

    fix a fmt error in test file

--- internal/services/recoveryservices/recovery_services_vault_resource_test.go
+++ internal/services/recoveryservices/recovery_services_vault_resource_test.go
@@ -550,7 +550,7 @@ resource "azurerm_recovery_services_vault" "test" {
   resource_group_name = azurerm_resource_group.test.name
   sku                 = "Standard"
 
-  cross_region_restore_enabled = %v
+  cross_region_restore_enabled = %t
 
   soft_delete_enabled = false
 }
