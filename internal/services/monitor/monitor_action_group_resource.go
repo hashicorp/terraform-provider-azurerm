4ccc7cde666c6347526b9b384ff20134e37a3db9
Michael Madeja <28938328+mikemadeja@users.noreply.github.com>
2023-02-23 09:07:48 -0600

    Added schema dependency

--- internal/services/monitor/monitor_action_group_resource.go
+++ internal/services/monitor/monitor_action_group_resource.go
@@ -22,6 +22,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func resourceMonitorActionGroup() *pluginsdk.Resource {
