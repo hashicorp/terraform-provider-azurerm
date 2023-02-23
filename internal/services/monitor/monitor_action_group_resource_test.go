95a42543d715a2e7196cb70e58a815edd4168078
Mike Madeja <mike@madeja.us>
2023-02-23 16:14:03 -0600

    fixed test fmt

--- internal/services/monitor/monitor_action_group_resource_test.go
+++ internal/services/monitor/monitor_action_group_resource_test.go
@@ -1212,7 +1212,7 @@ resource "azurerm_monitor_action_group" "test" {
   resource_group_name = azurerm_resource_group.test.name
   short_name          = "acctestag"
   enabled             = false
-  location	      = "global"
+  location	          = "global"
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
 }
