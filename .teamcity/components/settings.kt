a5281c5d05b8e09fe3f23821a8b062274aec61bc
Elena Xin <39109137+sinbai@users.noreply.github.com>
2023-02-23 17:43:28 +0800

    `azurerm_mssql_managed_instance` : fix acc tests failing due to missing route settings when running tests in francecentral (#20620)
    * fix acc tests
    
    * update code

--- .teamcity/components/settings.kt
+++ .teamcity/components/settings.kt
@@ -157,7 +157,7 @@ var serviceTestConfigurationOverrides = mapOf(
         "eventhub" to testConfiguration(useDevTestSubscription = true),
         "firewall" to testConfiguration(useDevTestSubscription = true),
         "keyvault" to testConfiguration(useDevTestSubscription = true),
-        "postgres" to testConfiguration(locationOverride = LocationConfiguration("westeurope", "centralus", "eastus", false), useDevTestSubscription = true),
+        "postgres" to testConfiguration(locationOverride = LocationConfiguration("northeurope", "centralus", "eastus", false), useDevTestSubscription = true),
         "privatedns" to testConfiguration(useDevTestSubscription = true),
         "redis" to testConfiguration(useDevTestSubscription = true)
 )
