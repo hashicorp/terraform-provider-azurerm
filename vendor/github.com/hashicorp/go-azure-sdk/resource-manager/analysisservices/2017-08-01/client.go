8dad636fd31304cf21a9cefc14bb06dba6710fe1
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-02-28 08:16:20 +0100

    go mod tidy/go mod vendor

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/analysisservices/2017-08-01/client.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/analysisservices/2017-08-01/client.go
@@ -6,6 +6,9 @@ import (
 	"github.com/hashicorp/go-azure-sdk/resource-manager/analysisservices/2017-08-01/servers"
 )
 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
 type Client struct {
 	AnalysisServices *analysisservices.AnalysisServicesClient
 	Servers          *servers.ServersClient
--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/storage/2022-05-01/client.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/storage/2022-05-01/client.go
@@ -22,6 +22,9 @@ import (
 	"github.com/hashicorp/go-azure-sdk/resource-manager/storage/2022-05-01/tableserviceproperties"
 )
 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
 type Client struct {
 	BlobContainers             *blobcontainers.BlobContainersClient
 	BlobInventoryPolicies      *blobinventorypolicies.BlobInventoryPoliciesClient
