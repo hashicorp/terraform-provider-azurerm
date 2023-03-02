b6efc487de004605245d1edd1946c7e6048e9f97
Yun Liu <yunliu1@microsoft.com>
2023-03-02 14:30:30 +0800

    modify new resource `azurerm_hybrid_compute_machine_extension`

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/hybridcompute/2022-11-10/machineextensions/version.go
@@ -0,0 +1,12 @@
+package machineextensions
+
+import "fmt"
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+const defaultApiVersion = "2022-11-10"
+
+func userAgent() string {
+	return fmt.Sprintf("hashicorp/go-azure-sdk/machineextensions/%s", defaultApiVersion)
+}
