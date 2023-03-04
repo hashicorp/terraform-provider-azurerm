d25ed01b12545f95c3ddab74731923710ba2b0ce
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2023-03-04 00:14:20 -0700

    Checking-in progress...

---
+++ internal/services/databricks/databricks_virtual_network_peerings_resource_test.go
@@ -0,0 +1,319 @@
+package databricks_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2023-02-01/vnetpeering"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+)
+
+type DatabricksVirtualNetworkPeeringResource struct{}
+
+func TestAccDatabricksVirtualNetworkPeering_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_databricks_virtual_network_peering", "test")
+	r := DatabricksVirtualNetworkPeeringResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data, "standard"),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("workspace_id"),
+	})
+}
+
+func TestAccDatabricksVirtualNetworkPeering_requiresImport(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_databricks_virtual_network_peering", "test")
+	r := DatabricksVirtualNetworkPeeringResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data, "standard"),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.RequiresImportErrorStep(r.requiresImport),
+	})
+}
+
+func TestAccDatabricksVirtualNetworkPeering_update(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_databricks_virtual_network_peering", "test")
+	r := DatabricksVirtualNetworkPeeringResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data, "standard"),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("workspace_id"),
+		{
+			Config: r.update(data, "standard"),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("workspace_id"),
+	})
+}
+
+func TestAccDatabricksVirtualNetworkPeering_complete(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_databricks_virtual_network_peering", "test")
+	r := DatabricksVirtualNetworkPeeringResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.complete(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("workspace_id"),
+	})
+}
+
+func TestAccDatabricksVirtualNetworkPeering_completeUpdate(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_databricks_virtual_network_peering", "test")
+	r := DatabricksVirtualNetworkPeeringResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.complete(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("workspace_id"),
+		{
+			Config: r.completeUpdate(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("workspace_id"),
+	})
+}
+
+func (DatabricksVirtualNetworkPeeringResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
+	id, err := vnetpeering.ParseVirtualNetworkPeeringID(state.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := clients.DataBricks.VnetPeeringsClient.Get(ctx, *id)
+	if err != nil {
+		return nil, fmt.Errorf("making Read request on Databricks %s: %+v", *id, err)
+	}
+
+	return utils.Bool(resp.Model != nil), nil
+}
+
+func (DatabricksVirtualNetworkPeeringResource) update(data acceptance.TestData, sku string) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-databricks-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_virtual_network" "remote" {
+  name                = "acctest-vnet-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  address_space       = ["10.0.1.0/24"]
+  location            = azurerm_resource_group.test.location
+}
+
+resource "azurerm_databricks_workspace" "test" {
+  name                = "acctest-workspace-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  sku                 = "standard"
+}
+
+resource "azurerm_databricks_virtual_network_peering" "test" {
+  name                = "acctest-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  workspace_id        = azurerm_databricks_workspace.test.id
+
+  remote_address_space_prefixes = azurerm_virtual_network.remote.address_space
+  remote_virtual_network_id     = azurerm_virtual_network.remote.id
+
+  virtual_network_access_enabled = false
+}
+
+resource "azurerm_virtual_network_peering" "remote" {
+  name                      = "to-acctest-%[1]d"
+  resource_group_name       = azurerm_resource_group.test.name
+  virtual_network_name      = azurerm_virtual_network.remote.name
+  remote_virtual_network_id = azurerm_databricks_virtual_network_peering.test.virtual_network_id
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (DatabricksVirtualNetworkPeeringResource) requiresImport(data acceptance.TestData) string {
+	template := DatabricksVirtualNetworkPeeringResource{}.basic(data, "standard")
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_databricks_virtual_network_peering" "import" {
+  name                = "acctest-%d"
+  resource_group_name = azurerm_resource_group.test.name
+  workspace_id        = azurerm_databricks_workspace.test.id
+
+  remote_address_space_prefixes = azurerm_virtual_network.remote.address_space
+  remote_virtual_network_id     = azurerm_virtual_network.remote.id
+}
+`, template, data.RandomInteger)
+}
+
+func (DatabricksVirtualNetworkPeeringResource) basic(data acceptance.TestData, sku string) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-databricks-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_virtual_network" "remote" {
+  name                = "acctest-vnet-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  address_space       = ["10.0.1.0/24"]
+  location            = azurerm_resource_group.test.location
+}
+
+resource "azurerm_databricks_workspace" "test" {
+  name                = "acctest-workspace-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  sku                 = "standard"
+}
+
+resource "azurerm_databricks_virtual_network_peering" "test" {
+  name                = "acctest-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  workspace_id        = azurerm_databricks_workspace.test.id
+
+  remote_address_space_prefixes = azurerm_virtual_network.remote.address_space
+  remote_virtual_network_id     = azurerm_virtual_network.remote.id
+}
+
+resource "azurerm_virtual_network_peering" "remote" {
+  name                      = "to-acctest-%[1]d"
+  resource_group_name       = azurerm_resource_group.test.name
+  virtual_network_name      = azurerm_virtual_network.remote.name
+  remote_virtual_network_id = azurerm_databricks_virtual_network_peering.test.virtual_network_id
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (DatabricksVirtualNetworkPeeringResource) complete(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-databricks-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_virtual_network" "remote" {
+  name                = "acctest-vnet-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  address_space       = ["10.0.1.0/24"]
+  location            = azurerm_resource_group.test.location
+}
+
+resource "azurerm_databricks_workspace" "test" {
+  name                = "acctest-workspace-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  sku                 = "standard"
+}
+
+resource "azurerm_databricks_virtual_network_peering" "test" {
+  name                = "acctest-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  workspace_id        = azurerm_databricks_workspace.test.id
+
+  remote_address_space_prefixes = azurerm_virtual_network.remote.address_space
+  remote_virtual_network_id     = azurerm_virtual_network.remote.id
+
+  virtual_network_access_enabled = true
+  forwarded_traffic_enabled      = true
+  gateway_transit_enabled        = false
+  remote_gateways_enabled        = false
+}
+
+resource "azurerm_virtual_network_peering" "remote" {
+  name                      = "to-acctest-%[1]d"
+  resource_group_name       = azurerm_resource_group.test.name
+  virtual_network_name      = azurerm_virtual_network.remote.name
+  remote_virtual_network_id = azurerm_databricks_virtual_network_peering.test.virtual_network_id
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (DatabricksVirtualNetworkPeeringResource) completeUpdate(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-databricks-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_virtual_network" "remote" {
+  name                = "acctest-vnet-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  address_space       = ["10.0.1.0/24"]
+  location            = azurerm_resource_group.test.location
+}
+
+resource "azurerm_databricks_workspace" "test" {
+  name                = "acctest-workspace-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  sku                 = "standard"
+}
+
+resource "azurerm_databricks_virtual_network_peering" "test" {
+  name                = "acctest-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  workspace_id        = azurerm_databricks_workspace.test.id
+
+  remote_address_space_prefixes = azurerm_virtual_network.remote.address_space
+  remote_virtual_network_id     = azurerm_virtual_network.remote.id
+
+  virtual_network_access_enabled = false
+  forwarded_traffic_enabled      = false
+  gateway_transit_enabled        = false
+  remote_gateways_enabled        = false
+}
+
+resource "azurerm_virtual_network_peering" "remote" {
+  name                      = "to-acctest-%[1]d"
+  resource_group_name       = azurerm_resource_group.test.name
+  virtual_network_name      = azurerm_virtual_network.remote.name
+  remote_virtual_network_id = azurerm_databricks_virtual_network_peering.test.virtual_network_id
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
---
+++ website/docs/r/databricks_virtual_network_peering.html.markdown
@@ -0,0 +1,107 @@
+---
+subcategory: "Databricks"
+layout: "azurerm"
+page_title: "Azure Resource Manager: azurerm_databricks_virtual_network_peering"
+description: |-
+  Manages a Databricks Virtual Network Peering
+---
+
+# azurerm_databricks_virtual_network_peering
+
+Manages a Databricks Virtual Network Peering
+
+## Example Usage
+
+```hcl
+resource "azurerm_resource_group" "example" {
+  name     = "databricks-vnet-peering"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "remote" {
+  name                = "remote-vnet"
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.1.0/24"]
+  location            = azurerm_resource_group.example.location
+}
+
+resource "azurerm_databricks_workspace" "example" {
+  name                = "example-workspace"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "standard"
+}
+
+resource "azurerm_databricks_virtual_network_peering" "example" {
+  name                = "databricks-vnet-peer"
+  resource_group_name = azurerm_resource_group.example.name
+  workspace_id        = azurerm_databricks_workspace.example.id
+  
+  remote_address_space_prefixes  = azurerm_virtual_network.remote.address_space
+  remote_virtual_network_id      = azurerm_virtual_network.remote.id
+  virtual_network_access_enabled = true
+}
+
+resource "azurerm_virtual_network_peering" "remote" {
+  name                         = "peer-to-databricks"
+  resource_group_name          = azurerm_resource_group.example.name
+  virtual_network_name         = azurerm_virtual_network.remote.name
+  remote_virtual_network_id    = azurerm_databricks_virtual_network_peering.example.virtual_network_id
+  allow_virtual_network_access = true
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required) Specifies the name of the Databricks Virtual Network Peering resource. Possible valid values must begin with a letter or number, end with a letter, number or underscore, and may contain only letters, numbers, underscores, periods, or hyphens and must be between 1 and 80 characters in length. Changing this forces a new resource to be created.
+
+* `resource_group_name` - (Required) The name of the Resource Group in which the Databricks Virtual Network Peering should exist. Changing this forces a new resource to be created.
+
+* `workspace_id` - (Required) The Id of the Databricks Workspace that this Databricks Virtual Network Peering is bound. Changing this forces a new resource to be created.
+
+* `address_space_prefixes` - (Required) A list of address blocks reserved for this virtual network in CIDR notation. Changing this forces a new resource to be created.
+
+* `remote_address_space_prefixes` - (Required) A list of address blocks reserved for the remote virtual network in CIDR notation. Changing this forces a new resource to be created.
+
+* `remote_virtual_network_id` - (Required) The Id of the remote virtual network. Changing this forces a new resource to be created.
+
+~> **NOTE:** The remote virtual network should be in the same region as the databricks workspace. Please see the [product documentation](https://learn.microsoft.com/azure/databricks/administration-guide/cloud-configurations/azure/vnet-peering) for more information.
+
+* `virtual_network_access_enabled` - (Optional) Can the VMs in the local virtual network space access the VMs in the remote virtual network space? Defaults to `true`.
+
+* `forwarded_traffic_enabled` - (Optional) Can the forwarded traffic from the VMs in the local virtual network be forwarded to the remote virtual network? Defaults to `false`.
+
+* `gateway_transit_enabled` - (Optional) Can the gateway links be used in the remote virtual network to link to the Databricks virtual network? Defaults to `false`.
+
+* `remote_gateways_enabled` - (Optional) Can remote gateways be used on the Databricks virtual network? Defaults to `false`.
+
+~> **NOTE:** If the `remote_gateways_enabled` is set to `true`, and `gateway_transit_enabled` on the remote peering is also `true`, the virtual network will use the gateways of the remote virtual network for transit. Only one peering can have this flag set to `true`. `remote_gateways_enabled` cannot be set if the virtual network already has a gateway.
+
+* `virtual_network_id` - (Computed) The Id of the internal databricks virtual network.
+
+~> **NOTE:** The `virtual_network_id` field is the value you must supply to the `azurerm_virtual_network_peering` resources `remote_virtual_network_id` field to successfully peer the Databricks Virtual Network with the remote virtual network.
+
+## Attributes Reference
+
+In addition to the Arguments listed above - the following Attributes are exported:
+
+* `id` - The ID of the Databricks Virtual Network Peering in the Azure management plane.
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/language/resources/syntax#operation-timeouts) for certain actions:
+
+* `create` - (Defaults to 30 minutes) Used when creating the Databricks Virtual Network Peering.
+* `update` - (Defaults to 30 minutes) Used when updating the Databricks Virtual Network Peering.
+* `read` - (Defaults to 5 minutes) Used when retrieving the Databricks Virtual Network Peering.
+* `delete` - (Defaults to 30 minutes) Used when deleting the Databricks Virtual Network Peering.
+
+## Import
+
+Databrick Virtual Network Peerings can be imported using the `resource id`, e.g.
+
+```shell
+terraform import azurerm_databricks_virtual_network_peering.example /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/group1/providers/Microsoft.Databricks/workspaces/workspace1/virtualNetworkPeerings/peering1
+```
