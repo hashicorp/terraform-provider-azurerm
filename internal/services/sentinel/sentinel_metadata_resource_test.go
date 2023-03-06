e41dd2b8253240e90f157968664d7efc1ef35839
ziyeqf <zyqf16@gmail.com>
2023-03-06 18:25:05 +0800

    refresh vendor & update testcase

--- internal/services/sentinel/sentinel_metadata_resource_test.go
+++ internal/services/sentinel/sentinel_metadata_resource_test.go
@@ -60,21 +60,6 @@ func TestAccMetadata_complete(t *testing.T) {
 	})
 }
 
-func TestAccMetadata_completeWithSolution(t *testing.T) {
-	data := acceptance.BuildTestData(t, "azurerm_sentinel_metadata", "test")
-	r := MetadataResource{}
-
-	data.ResourceTest(t, r, []acceptance.TestStep{
-		{
-			Config: r.completeWithSolution(data),
-			Check: acceptance.ComposeTestCheckFunc(
-				check.That(data.ResourceName).ExistsInAzure(r),
-			),
-		},
-		data.ImportStep(),
-	})
-}
-
 func TestAccMetadata_update(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_sentinel_metadata", "test")
 	r := MetadataResource{}
@@ -157,84 +142,7 @@ resource "azurerm_sentinel_metadata" "test" {
           {
             contentId = "045d06d0-ee72-4794-aba4-cf5646e4c756",
             kind      = "DataConnector",
-            name      = "Microsoft Defender for Endpoint"
           },
-          {
-            contentId = "dbfcb2cc-d782-40ef-8d94-fe7af58a6f2d",
-            kind      = "DataConnector"
-          },
-          {
-            contentId = "de4dca9b-eb37-47d6-a56f-b8b06b261593",
-            kind      = "DataConnector",
-            version   = "2.0"
-          }
-        ]
-      },
-      {
-        kind      = "Playbook",
-        contentId = "31ee11cc-9989-4de8-b176-5e0ef5c4dbab",
-        version   = "1.0"
-      },
-      {
-        kind      = "Parser",
-        contentId = "21ba424a-9438-4444-953a-7059539a7a1b"
-      }
-    ]
-  })
-
-}
-`, SentinelAlertRuleNrtResource{}.basic(data))
-}
-
-func (MetadataResource) completeWithSolution(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-%s
-
-resource "azurerm_sentinel_metadata" "test" {
-  name                       = "acctest"
-  workspace_id               = azurerm_log_analytics_solution.test.workspace_resource_id
-  content_id                 = azurerm_sentinel_alert_rule_nrt.test.name
-  kind                       = "Solution"
-  parent_id                  = azurerm_sentinel_alert_rule_nrt.test.id
-  providers                  = ["testprovider1", "testprovider2"]
-  preview_images             = ["firstImage.png"]
-  preview_images_dark        = ["firstImageDark.png"]
-  content_schema_version     = "2.0"
-  custom_version             = "1.0"
-  threat_analysis_tactics    = ["Reconnaissance", "CommandAndControl"]
-  threat_analysis_techniques = ["T1548", "t1548.001"]
-  first_publish_date         = "2021-06-24"
-  last_publish_date          = "2021-07-24"
-  version = "1.0.0"
-  source {
-    kind = "Solution"
-    name = "test Solution"
-    guid = "b688a130-76f4-4a07-bf57-762222a3cadf"
-  }
-
-  author {
-    name  = "test user"
-    email = "acc@test.com"
-  }
-
-  support {
-    name  = "acc test"
-    email = "acc@test.com"
-    link  = "https://acc.test.com"
-    tier  = "Partner"
-  }
-
-  category {
-    domains   = ["Application"]
-    verticals = ["Healthcare"]
-  }
-
-  dependency = jsonencode({
-    operator = "AND",
-    criteria = [
-      {
-        operator = "OR",
-        criteria = [
           {
             contentId = "dbfcb2cc-d782-40ef-8d94-fe7af58a6f2d",
             kind      = "DataConnector"
