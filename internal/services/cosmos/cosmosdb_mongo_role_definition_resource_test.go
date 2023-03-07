e06be593a06920538bdf8dbefaed583c22c1a496
neil-yechenwei <yechenwei2007@hotmail.com>
2023-03-07 19:45:56 +0800

    update code

---
+++ internal/services/cosmos/cosmosdb_mongo_role_definition_resource_test.go
@@ -0,0 +1,101 @@
+package cosmos_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/go-azure-sdk/resource-manager/cosmosdb/2022-11-15/mongorbacs"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+)
+
+type CosmosMongoRoleDefinitionResource struct{}
+
+func TestAccCosmosDbMongoRoleDefinition_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_cosmosdb_mongo_role_definition", "test")
+	r := CosmosMongoRoleDefinitionResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeAggregateTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func (r CosmosMongoRoleDefinitionResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
+	id, err := mongorbacs.ParseMongodbRoleDefinitionID(state.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := clients.Cosmos.MongoRBACClient.MongoDBResourcesGetMongoRoleDefinition(ctx, *id)
+	if err != nil {
+		return nil, fmt.Errorf("reading %q: %+v", id, err)
+	}
+
+	return utils.Bool(resp.Model != nil), nil
+}
+
+func (r CosmosMongoRoleDefinitionResource) basic(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_cosmosdb_mongo_role_definition" "test" {
+  account_id = azurerm_cosmosdb_account.test.id
+  db_name    = azurerm_cosmosdb_mongo_database.test.name
+  role_name  = "acctest-mongoroledef-%d"
+}
+`, r.template(data), data.RandomInteger)
+}
+
+func (r CosmosMongoRoleDefinitionResource) template(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-mongoroledef-%d"
+  location = "%s"
+}
+
+resource "azurerm_cosmosdb_account" "test" {
+  name                = "acctest-ca-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  capabilities {
+    name = "EnableMongoRoleBasedAccessControl"
+  }
+
+  consistency_policy {
+    consistency_level = "Strong"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.test.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_mongo_database" "test" {
+  name                = "acctest-mongodb-%d"
+  resource_group_name = azurerm_cosmosdb_account.test.resource_group_name
+  account_name        = azurerm_cosmosdb_account.test.name
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
+}
