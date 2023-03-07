7f814f96aa02327d4a2bb6b38ed1863520994751
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-03-07 10:21:24 +0100

    refactor: updating `dashboard`, `media`, `portal` and `servicebus` to use `go-azure-sdk`'s base layer

--- internal/services/dashboard/client/client.go
+++ internal/services/dashboard/client/client.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"fmt"
+
 	"github.com/hashicorp/go-azure-sdk/resource-manager/dashboard/2022-08-01/grafanaresource"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
@@ -10,10 +12,13 @@ type Client struct {
 }
 
 func NewClient(o *common.ClientOptions) (*Client, error) {
-	grafanaResourceClient := grafanaresource.NewGrafanaResourceClientWithBaseURI(o.ResourceManagerEndpoint)
-	o.ConfigureClient(&grafanaResourceClient.Client, o.ResourceManagerAuthorizer)
+	grafanaResourceClient, err := grafanaresource.NewGrafanaResourceClientWithBaseURI(o.Environment.ResourceManager)
+	if err != nil {
+		return nil, fmt.Errorf("building GrafanaResource client: %+v", err)
+	}
+	o.Configure(grafanaResourceClient.Client, o.Authorizers.ResourceManager)
 
 	return &Client{
-		GrafanaResourceClient: &grafanaResourceClient,
+		GrafanaResourceClient: grafanaResourceClient,
 	}, nil
 }
--- internal/services/portal/client/client.go
+++ internal/services/portal/client/client.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"fmt"
+
 	"github.com/hashicorp/go-azure-sdk/resource-manager/portal/2019-01-01-preview/dashboard"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/portal/2019-01-01-preview/tenantconfiguration"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
@@ -12,14 +14,20 @@ type Client struct {
 }
 
 func NewClient(o *common.ClientOptions) (*Client, error) {
-	dashboardsClient := dashboard.NewDashboardClientWithBaseURI(o.ResourceManagerEndpoint)
-	o.ConfigureClient(&dashboardsClient.Client, o.ResourceManagerAuthorizer)
+	dashboardsClient, err := dashboard.NewDashboardClientWithBaseURI(o.Environment.ResourceManager)
+	if err != nil {
+		return nil, fmt.Errorf("building Dashboards client: %+v", err)
+	}
+	o.Configure(dashboardsClient.Client, o.Authorizers.ResourceManager)
 
-	tenantConfigurationsClient := tenantconfiguration.NewTenantConfigurationClientWithBaseURI(o.ResourceManagerEndpoint)
-	o.ConfigureClient(&tenantConfigurationsClient.Client, o.ResourceManagerAuthorizer)
+	tenantConfigurationsClient, err := tenantconfiguration.NewTenantConfigurationClientWithBaseURI(o.Environment.ResourceManager)
+	if err != nil {
+		return nil, fmt.Errorf("building TenantConfiguration client: %+v", err)
+	}
+	o.Configure(tenantConfigurationsClient.Client, o.Authorizers.ResourceManager)
 
 	return &Client{
-		DashboardsClient:           &dashboardsClient,
-		TenantConfigurationsClient: &tenantConfigurationsClient,
+		DashboardsClient:           dashboardsClient,
+		TenantConfigurationsClient: tenantConfigurationsClient,
 	}, nil
 }
