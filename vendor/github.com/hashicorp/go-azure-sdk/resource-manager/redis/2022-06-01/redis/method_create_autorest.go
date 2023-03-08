cc89f2e9a3d9903aca9b01c398b81866d7e468e2
xuzhang3 <Zhangxu894765>
2023-03-08 17:39:52 +0800

    upgrade API to 2022-06-01

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/redis/2022-06-01/redis/method_create_autorest.go
@@ -0,0 +1,79 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/hashicorp/go-azure-helpers/polling"
+)
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type CreateOperationResponse struct {
+	Poller       polling.LongRunningPoller
+	HttpResponse *http.Response
+}
+
+// Create ...
+func (c RedisClient) Create(ctx context.Context, id RediId, input RedisCreateParameters) (result CreateOperationResponse, err error) {
+	req, err := c.preparerForCreate(ctx, id, input)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "redis.RedisClient", "Create", nil, "Failure preparing request")
+		return
+	}
+
+	result, err = c.senderForCreate(ctx, req)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "redis.RedisClient", "Create", result.HttpResponse, "Failure sending request")
+		return
+	}
+
+	return
+}
+
+// CreateThenPoll performs Create then polls until it's completed
+func (c RedisClient) CreateThenPoll(ctx context.Context, id RediId, input RedisCreateParameters) error {
+	result, err := c.Create(ctx, id, input)
+	if err != nil {
+		return fmt.Errorf("performing Create: %+v", err)
+	}
+
+	if err := result.Poller.PollUntilDone(); err != nil {
+		return fmt.Errorf("polling after Create: %+v", err)
+	}
+
+	return nil
+}
+
+// preparerForCreate prepares the Create request.
+func (c RedisClient) preparerForCreate(ctx context.Context, id RediId, input RedisCreateParameters) (*http.Request, error) {
+	queryParameters := map[string]interface{}{
+		"api-version": defaultApiVersion,
+	}
+
+	preparer := autorest.CreatePreparer(
+		autorest.AsContentType("application/json; charset=utf-8"),
+		autorest.AsPut(),
+		autorest.WithBaseURL(c.baseUri),
+		autorest.WithPath(id.ID()),
+		autorest.WithJSON(input),
+		autorest.WithQueryParameters(queryParameters))
+	return preparer.Prepare((&http.Request{}).WithContext(ctx))
+}
+
+// senderForCreate sends the Create request. The method will close the
+// http.Response Body if it receives an error.
+func (c RedisClient) senderForCreate(ctx context.Context, req *http.Request) (future CreateOperationResponse, err error) {
+	var resp *http.Response
+	resp, err = c.Client.Send(req, azure.DoRetryWithRegistration(c.Client))
+	if err != nil {
+		return
+	}
+
+	future.Poller, err = polling.NewPollerFromResponse(ctx, resp, c.Client, req.Method)
+	return
+}
