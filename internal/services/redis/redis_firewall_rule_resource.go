75d820dff5ec9190dd749a5fd7ecbfa69995a3b5
xuzhang3 <Zhangxu894765>
2023-03-08 17:39:32 +0800

    upgrade API to 2022-06-01

--- internal/services/redis/redis_firewall_rule_resource.go
+++ internal/services/redis/redis_firewall_rule_resource.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/redis/2021-06-01/firewallrules"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/redis/2022-06-01/firewallrules"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/redis/migration"
