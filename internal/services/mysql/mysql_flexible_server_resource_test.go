75b65e666ef02f52b1216d7364c6c9326bae2084
neil-yechenwei <yechenwei2007@hotmail.com>
2023-03-10 13:23:20 +0800

    update code

--- internal/services/mysql/mysql_flexible_server_resource_test.go
+++ internal/services/mysql/mysql_flexible_server_resource_test.go
@@ -1171,5 +1171,5 @@ resource "azurerm_mysql_flexible_server" "test" {
     geo_backup_user_assigned_identity_id = azurerm_user_assigned_identity.test2.id
   }
 }
-`, r.cmkTemplate(data), data.RandomInteger, data.Locations.Secondary, data.RandomString, data.RandomString, data.RandomInteger)
+`, r.cmkTemplate(data), data.RandomInteger, "francecentral", data.RandomString, data.RandomString, data.RandomInteger)
 }
