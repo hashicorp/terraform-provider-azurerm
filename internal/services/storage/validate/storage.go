563e075d65f17703079bae4360cf926f1793de33
magodo <wztdyl@sina.com>
2023-03-15 14:29:32 +0800

    Allow forward slash

--- internal/services/storage/validate/storage.go
+++ internal/services/storage/validate/storage.go
@@ -11,8 +11,9 @@ func StorageShareDirectoryName(v interface{}, k string) (warnings []string, erro
 	if regexp.MustCompile(`^\.+$`).MatchString(value) {
 		errors = append(errors, fmt.Errorf(`%s must not only contain dots`, k))
 	}
-	if !regexp.MustCompile(`^[^"\/:|<>*?]{1,255}$`).MatchString(value) {
-		errors = append(errors, fmt.Errorf(`%s must not contain following characters: "\/:|<>*?`, k))
+	// Note that we didn't forbid the forward slash in the non-head segment here as it seems to be allowed as the directory name for constructing directory hierarchy.
+	if !regexp.MustCompile(`^[^"/\:|<>*?]+(/[^"\:|<>*?]+)*$`).MatchString(value) {
+		errors = append(errors, fmt.Errorf(`%s must not contain following characters: "\:|<>*?`, k))
 	}
 
 	return warnings, errors
