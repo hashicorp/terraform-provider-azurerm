77554b2665e84a08131ef61cd8b06fbc41ec2684
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-03-27 08:30:29 +0100

    r/(linux|windows)_virtual_machine_scale_set: validating `public_ip_prefix_id`

--- internal/services/compute/virtual_machine_scale_set.go
+++ internal/services/compute/virtual_machine_scale_set.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/identity"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/compute/2021-07-01/galleryapplicationversions"
-	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	azValidate "github.com/hashicorp/terraform-provider-azurerm/helpers/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/compute/validate"
@@ -809,7 +808,7 @@ func virtualMachineScaleSetPublicIPAddressSchema() *pluginsdk.Schema {
 					Type:         pluginsdk.TypeString,
 					Optional:     true,
 					ForceNew:     true,
-					ValidateFunc: azure.ValidateResourceIDOrEmpty,
+					ValidateFunc: networkValidate.PublicIpPrefixID,
 				},
 			},
 		},
