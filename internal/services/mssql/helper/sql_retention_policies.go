2d6ae6c10f41bc4819912b00bd5cd1f58828d974
Matthew <mbfrahry@gmail.com>
2023-03-28 15:39:56 -0700

    goimports

--- internal/services/mssql/helper/sql_retention_policies.go
+++ internal/services/mssql/helper/sql_retention_policies.go
@@ -1,12 +1,13 @@
 package helper
 
 import (
+	"strconv"
+
 	"github.com/Azure/azure-sdk-for-go/services/preview/sql/mgmt/v5.0/sql" // nolint: staticcheck
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
-	"strconv"
 )
 
 func LongTermRetentionPolicySchema() *pluginsdk.Schema {
