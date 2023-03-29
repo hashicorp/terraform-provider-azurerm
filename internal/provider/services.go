b4fc1eccabde0f4f741dcd6fcb08533b241ba973
Xiaxin <92154856+xiaxyi@users.noreply.github.com>
2023-03-30 00:41:24 +0800

    New Resource - `azurerm_web_pubsub_custom_certificate` (#21114)

--- internal/provider/services.go
+++ internal/provider/services.go
@@ -165,6 +165,7 @@ func SupportedTypedServices() []sdk.TypedServiceRegistration {
 		serviceconnector.Registration{},
 		servicefabricmanaged.Registration{},
 		storage.Registration{},
+		signalr.Registration{},
 		orbital.Registration{},
 		streamanalytics.Registration{},
 		search.Registration{},
