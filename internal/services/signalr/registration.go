d0fe0a6d09cd31517db434f45445f53b7d7dc69e
xiaxin18 <xiaxyi@microsoft.com>
2023-03-31 14:45:56 +0800

    add dependency

--- internal/services/signalr/registration.go
+++ internal/services/signalr/registration.go
@@ -21,6 +21,7 @@ func (r Registration) Resources() []sdk.Resource {
 		CustomDomainSignalrServiceResource{},
 		CustomDomainWebPubsubResource{},
 		CustomCertWebPubsubResource{},
+		CustomCertSignalrServiceResource{},
 	}
 }
 
