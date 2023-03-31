bb75659f6a0e1fbeb24db8a7ca26167da0ad43bd
jackofallops <11830746+jackofallops@users.noreply.github.com>
2023-03-31 08:27:11 +0100

    BC-Detection - plug in DS rules, remove magic strings (#21217)

--- internal/tools/schema-api/schema-rules/property_type.go
+++ internal/tools/schema-api/schema-rules/property_type.go
@@ -15,7 +15,7 @@ type propertyType struct{}
 
 // Check - Checks for invalid type changes. At the time of writing the only allowed change is a Set to a List
 func (propertyType) Check(base providerjson.SchemaJSON, current providerjson.SchemaJSON, propertyName string) *string {
-	if (base.Type != "" && current.Type != "" && base.Type != "TypeSet") && base.Type != current.Type {
+	if (base.Type != "" && current.Type != "" && base.Type != providerjson.SchemaTypeSet) && base.Type != current.Type {
 		return pointer.To(fmt.Sprintf("schema type has changed for %q (%+v to %+v)", propertyName, base.Type, current.Type))
 	}
 
