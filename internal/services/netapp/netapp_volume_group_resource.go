6c12b1e3ec28d743dfe21c59619968347052ec2f
Paulo <pmarques@microsoft.com>
2023-03-31 13:36:35 +0000

    replacing LocationComputed with LocationWithoutForceNew

--- internal/services/netapp/netapp_volume_group_resource.go
+++ internal/services/netapp/netapp_volume_group_resource.go
@@ -259,7 +259,7 @@ func (r NetAppVolumeGroupResource) Arguments() map[string]*pluginsdk.Schema {
 									ValidateFunc: validation.StringInSlice(volumegroups.PossibleValuesForEndpointType(), false),
 								},
 
-								"remote_volume_location": commonschema.LocationComputed(),
+								"remote_volume_location": commonschema.LocationWithoutForceNew(),
 
 								"remote_volume_resource_id": {
 									Type:         pluginsdk.TypeString,
@@ -515,7 +515,6 @@ func (r NetAppVolumeGroupResource) Update() sdk.ResourceFunc {
 						if err := waitForVolumeCreateOrUpdate(ctx, volumeClient, volumeId); err != nil {
 							return err
 						}
-
 					}
 				}
 			}
