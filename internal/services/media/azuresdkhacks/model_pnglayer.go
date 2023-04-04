5ef57fb7a62d8f47e6ae1209c550834a6f0f0bff
Tao <104055472+teowa@users.noreply.github.com>
2023-04-04 23:59:36 +0800

    remove unused code

--- internal/services/media/azuresdkhacks/model_pnglayer.go
+++
@@ -1,9 +0,0 @@
-package azuresdkhacks
-
-// Hacking the SDK model
-
-type PngLayer struct {
-	Height *string `json:"height,omitempty"`
-	Label  *string `json:"label,omitempty"`
-	Width  *string `json:"width,omitempty"`
-}
