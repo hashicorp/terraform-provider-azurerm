6301ec698efdddb6961639929bb384b63520a8d8
Paulo Marques <paulomarquesc@users.noreply.github.com>
2023-04-11 13:31:24 -0700

    Update internal/services/netapp/netapp_snapshot_resource_test.go
    Co-authored-by: Matthew Frahry <mbfrahry@gmail.com>

--- internal/services/netapp/netapp_snapshot_resource_test.go
+++ internal/services/netapp/netapp_snapshot_resource_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/hashicorp/go-azure-sdk/resource-manager/netapp/2022-05-01/snapshots"
-
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
