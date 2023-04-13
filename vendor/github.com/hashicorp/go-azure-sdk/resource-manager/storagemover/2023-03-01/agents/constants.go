bda7d85233c8f6c58b7909957421280d185900ea
elena <sinbai@hotmail.com>
2023-04-13 17:34:28 +0800

    updat code

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/storagemover/2023-03-01/agents/constants.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/storagemover/2023-03-01/agents/constants.go
@@ -1,6 +1,10 @@
 package agents
 
-import "strings"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
@@ -25,6 +29,19 @@ func PossibleValuesForAgentStatus() []string {
 		string(AgentStatusRequiresAttention),
 		string(AgentStatusUnregistering),
 	}
+}
+
+func (s *AgentStatus) UnmarshalJSON(bytes []byte) error {
+	var decoded string
+	if err := json.Unmarshal(bytes, &decoded); err != nil {
+		return fmt.Errorf("unmarshaling: %+v", err)
+	}
+	out, err := parseAgentStatus(decoded)
+	if err != nil {
+		return fmt.Errorf("parsing %q: %+v", decoded, err)
+	}
+	*s = *out
+	return nil
 }
 
 func parseAgentStatus(input string) (*AgentStatus, error) {
@@ -55,6 +72,19 @@ func PossibleValuesForProvisioningState() []string {
 	return []string{
 		string(ProvisioningStateSucceeded),
 	}
+}
+
+func (s *ProvisioningState) UnmarshalJSON(bytes []byte) error {
+	var decoded string
+	if err := json.Unmarshal(bytes, &decoded); err != nil {
+		return fmt.Errorf("unmarshaling: %+v", err)
+	}
+	out, err := parseProvisioningState(decoded)
+	if err != nil {
+		return fmt.Errorf("parsing %q: %+v", decoded, err)
+	}
+	*s = *out
+	return nil
 }
 
 func parseProvisioningState(input string) (*ProvisioningState, error) {
