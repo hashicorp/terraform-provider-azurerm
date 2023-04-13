d93bfe304b323dc83b77081e44455b8ff6e6de0b
Catriona <catriona@hashicorp.com>
2023-04-13 11:07:05 +0100

    fix parsers

--- internal/services/monitor/resourceids.go
+++ internal/services/monitor/resourceids.go
@@ -1,3 +1,4 @@
 package monitor
 
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ActionGroup -rewrite=true -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/group1/providers/Microsoft.Insights/actionGroups/actionGroup1
+//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ActionRule -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/group1/providers/Microsoft.AlertsManagement/actionRules/actionRule1
