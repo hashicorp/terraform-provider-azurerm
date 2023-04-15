33d8de1aebe128bccfe6116b87636c9384ab023c
Zhenhua Hu <zhhu@microsoft.com>
2023-04-15 08:54:18 +0800

    resolve comments

--- internal/services/containers/kubernetes_cluster_extension_resource_test.go
+++ internal/services/containers/kubernetes_cluster_extension_resource_test.go
@@ -5,9 +5,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/go-azure-sdk/resource-manager/kubernetesconfiguration/2022-11-01/extensions"
-
 	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/kubernetesconfiguration/2022-11-01/extensions"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
