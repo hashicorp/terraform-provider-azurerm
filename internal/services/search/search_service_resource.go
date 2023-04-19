ad175db5c3d733eec1b7413e606128ed2f59d056
Elijah Buck <elijah@github.com>
2023-04-19 02:02:33 -0700

    mark search keys sensitive (#21469)
    * mark search keys sensitive
    
    * mark search_service key sensitive
    
    * make fmt
    
    ---------
    
    Co-authored-by: Steph <steph@hashicorp.com>

--- internal/services/search/search_service_resource.go
+++ internal/services/search/search_service_resource.go
@@ -82,13 +82,15 @@ func resourceSearchService() *pluginsdk.Resource {
 			},
 
 			"primary_key": {
-				Type:     pluginsdk.TypeString,
-				Computed: true,
+				Type:      pluginsdk.TypeString,
+				Computed:  true,
+				Sensitive: true,
 			},
 
 			"secondary_key": {
-				Type:     pluginsdk.TypeString,
-				Computed: true,
+				Type:      pluginsdk.TypeString,
+				Computed:  true,
+				Sensitive: true,
 			},
 
 			"query_keys": {
