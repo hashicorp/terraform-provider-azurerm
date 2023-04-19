9e9080f7028757c57a3eb212307a9e7601ffb65a
Aris van Ommeren <avommeren@vxcompany.com>
2023-04-19 23:36:05 +0200

    Remove unused generated ID

--- internal/services/costmanagement/resourceids.go
+++ internal/services/costmanagement/resourceids.go
@@ -5,4 +5,3 @@ package costmanagement
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=AnomalyAlertView -id=/subscriptions/12345678-1234-9876-4563-123456789012/providers/Microsoft.CostManagement/views/ms:DailyAnomalyByResourceGroup
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=SubscriptionCostManagementView -id=/subscriptions/12345678-1234-9876-4563-123456789012/providers/Microsoft.CostManagement/views/view1
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ResourceGroupCostManagementView -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.CostManagement/views/view1
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=SubscriptionCostManagementScheduledAction -id=/subscriptions/12345678-1234-9876-4563-123456789012/providers/Microsoft.CostManagement/scheduledActions/scheduledaction1
