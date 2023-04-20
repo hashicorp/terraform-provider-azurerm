f4ef172f5d9b411f3878e33e8d466b17cdbfc892
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-04-20 19:07:07 +0200

    keyvault: updating the Validate function to wrap calling `commonids`

--- internal/services/keyvault/validate/vault_id.go
+++ internal/services/keyvault/validate/vault_id.go
@@ -1,7 +1,6 @@
 package validate
 
 import (
-	"fmt"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
 )
 
@@ -9,15 +8,5 @@ import (
 // since this Resource ID is used all over the place
 
 func VaultID(input interface{}, key string) (warnings []string, errors []error) {
-	v, ok := input.(string)
-	if !ok {
-		errors = append(errors, fmt.Errorf("expected %q to be a string", key))
-		return
-	}
-
-	if _, err := commonids.ParseKeyVaultID(v); err != nil {
-		errors = append(errors, err)
-	}
-
-	return
+	return commonids.ValidateKeyVaultID(input, key)
 }
