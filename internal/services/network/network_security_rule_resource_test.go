481da7474e35e58a505e869d70a33956cdfbe204
Tao <104055472+teowa@users.noreply.github.com>
2023-04-24 15:17:24 +0800

    fmt

--- internal/services/network/network_security_rule_resource_test.go
+++ internal/services/network/network_security_rule_resource_test.go
@@ -218,9 +218,10 @@ resource "azurerm_network_security_group" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   tags = {
-     foo = "bar"
+    foo = "bar"
   }
 }
+
 
 `, data.RandomInteger, data.Locations.Primary)
 }
