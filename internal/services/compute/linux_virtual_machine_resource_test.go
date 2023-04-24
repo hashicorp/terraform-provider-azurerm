09d8e1bf1020fdbbe130258c32394a6a1502ac7a
Yichun Ma <yicma@microsoft.com>
2023-04-24 20:18:39 +0800

    `test` - fix `edge_zone` tests in `compute`/`network` (#21515)

--- internal/services/compute/linux_virtual_machine_resource_test.go
+++ internal/services/compute/linux_virtual_machine_resource_test.go
@@ -27,13 +27,19 @@ func (t LinuxVirtualMachineResource) Exists(ctx context.Context, clients *client
 	return utils.Bool(resp.Model != nil), nil
 }
 
-func (LinuxVirtualMachineResource) templateBase(data acceptance.TestData) string {
-	return fmt.Sprintf(`
+func (LinuxVirtualMachineResource) templateBasePublicKey() string {
+	return `
 # note: whilst these aren't used in all tests, it saves us redefining these everywhere
 locals {
   first_public_key  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+wWK73dCr+jgQOAxNsHAnNNNMEMWOHYEccp6wJm2gotpr9katuF/ZAdou5AaW1C61slRkHRkpRRX9FA9CYBiitZgvCCz+3nWNN7l/Up54Zps/pHWGZLHNJZRYyAB6j5yVLMVHIHriY49d/GZTZVNB8GoJv9Gakwc/fuEZYYl4YDFiGMBP///TzlI4jhiJzjKnEvqPFki5p2ZRJqcbCiF4pJrxUQR/RXqVFQdbRLZgYfJ8xGB878RENq3yQ39d8dVOkq4edbkzwcUmwwwkYVPIoDGsYLaRHnG+To7FvMeyO7xDVQkMKzopTQV8AuKpyvpqu0a9pWOMaiCyDytO7GGN you@me.com"
   second_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0/NDMj2wG6bSa6jbn6E3LYlUsYiWMp1CQ2sGAijPALW6OrSu30lz7nKpoh8Qdw7/A4nAJgweI5Oiiw5/BOaGENM70Go+VM8LQMSxJ4S7/8MIJEZQp5HcJZ7XDTcEwruknrd8mllEfGyFzPvJOx6QAQocFhXBW6+AlhM3gn/dvV5vdrO8ihjET2GoDUqXPYC57ZuY+/Fz6W3KV8V97BvNUhpY5yQrP5VpnyvvXNFQtzDfClTvZFPuoHQi3/KYPi6O0FSD74vo8JOBZZY09boInPejkm9fvHQqfh0bnN7B6XJoUwC1Qprrx+XIy7ust5AEn5XL7d4lOvcR14MxDDKEp you@me.com"
 }
+`
+}
+
+func (r LinuxVirtualMachineResource) templateBase(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
 
 resource "azurerm_resource_group" "test" {
   name     = "acctestRG-%d"
@@ -53,7 +59,7 @@ resource "azurerm_subnet" "test" {
   virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes     = ["10.0.2.0/24"]
 }
-`, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
+`, r.templateBasePublicKey(), data.RandomInteger, data.Locations.Primary, data.RandomInteger)
 }
 
 func (r LinuxVirtualMachineResource) template(data acceptance.TestData) string {
