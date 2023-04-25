a017d3de20c1c5c97f0c214956a4d55cd18d1b28
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-04-25 10:25:16 +0200

    go mod vendor

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/storagemover/2023-03-01/endpoints/model_endpoint.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/storagemover/2023-03-01/endpoints/model_endpoint.go
@@ -42,7 +42,7 @@ func (s *Endpoint) UnmarshalJSON(bytes []byte) error {
 		if err != nil {
 			return fmt.Errorf("unmarshaling field 'Properties' for 'Endpoint': %+v", err)
 		}
-		s.Properties = impl
+		s.Properties = &impl
 	}
 	return nil
 }
