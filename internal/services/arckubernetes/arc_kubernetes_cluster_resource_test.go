8e6c41b2eb31fda01d0468736b6cd79aa53f2044
hc-github-team-tf-azure <>
2023-04-26 10:02:50 +0000

    Updating based on 84ba7477

--- internal/services/arckubernetes/arc_kubernetes_cluster_resource_test.go
+++ internal/services/arckubernetes/arc_kubernetes_cluster_resource_test.go
@@ -12,10 +12,9 @@ import (
 	"os"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	arckubernetes "github.com/hashicorp/go-azure-sdk/resource-manager/hybridkubernetes/2021-10-01/connectedclusters"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
