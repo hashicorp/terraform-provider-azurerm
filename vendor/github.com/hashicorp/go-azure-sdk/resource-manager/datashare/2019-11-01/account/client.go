f660a401312b37f702d1bbfb08a4c2f5d9403e4a
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-04-26 17:30:06 +0200

    dependencies: updating to `v0.20230425.1021638` of `github.com/hashicorp/go-azure-sdk`

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/datashare/2019-11-01/account/client.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/datashare/2019-11-01/account/client.go
@@ -1,18 +1,26 @@
 package account
 
-import "github.com/Azure/go-autorest/autorest"
+import (
+	"fmt"
+
+	"github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager"
+	"github.com/hashicorp/go-azure-sdk/sdk/environments"
+)
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
 type AccountClient struct {
-	Client  autorest.Client
-	baseUri string
+	Client *resourcemanager.Client
 }
 
-func NewAccountClientWithBaseURI(endpoint string) AccountClient {
-	return AccountClient{
-		Client:  autorest.NewClientWithUserAgent(userAgent()),
-		baseUri: endpoint,
+func NewAccountClientWithBaseURI(api environments.Api) (*AccountClient, error) {
+	client, err := resourcemanager.NewResourceManagerClient(api, "account", defaultApiVersion)
+	if err != nil {
+		return nil, fmt.Errorf("instantiating AccountClient: %+v", err)
 	}
+
+	return &AccountClient{
+		Client: client,
+	}, nil
 }
