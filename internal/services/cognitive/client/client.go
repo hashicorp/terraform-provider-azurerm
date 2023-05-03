d0a25331fa17a0ec396995a1ca0b485467b72add
Steph <steph@hashicorp.com>
2023-05-03 12:31:31 +0200

    lint

--- internal/services/cognitive/client/client.go
+++ internal/services/cognitive/client/client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"fmt"
+
 	"github.com/hashicorp/go-azure-sdk/resource-manager/cognitive/2022-10-01/cognitiveservicesaccounts"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/cognitive/2022-10-01/deployments"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
