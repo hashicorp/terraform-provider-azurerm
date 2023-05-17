b91c9bdecfa530954be78fbf180b7399b073a6e0
Lucas Juviniano <lucasjuviniano@gmail.com>
2023-05-17 15:24:22 -0300

    Go back to less permissive NSG

--- internal/services/machinelearning/machine_learning_compute_instance_resource_test.go
+++ internal/services/machinelearning/machine_learning_compute_instance_resource_test.go
@@ -163,24 +163,13 @@ resource "azurerm_network_security_group" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   security_rule {
-    name                       = "AllowAllInbound"
+    name                       = "test123"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "*"
+    protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "AllowAllOutbound"
-    priority                   = 110
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_range     = "29876-29877"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
