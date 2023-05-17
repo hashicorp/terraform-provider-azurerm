19538b73e0dba4c8d9ece8b1cdd3ac4e104842e0
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-05-17 03:12:26 +0200

    go mod tidy / go mod vendor

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/redis/2022-06-01/privateendpointconnections/constants.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/redis/2022-06-01/privateendpointconnections/constants.go
@@ -1,6 +1,10 @@
 package privateendpointconnections
 
-import "strings"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
@@ -21,6 +25,19 @@ func PossibleValuesForPrivateEndpointConnectionProvisioningState() []string {
 		string(PrivateEndpointConnectionProvisioningStateFailed),
 		string(PrivateEndpointConnectionProvisioningStateSucceeded),
 	}
+}
+
+func (s *PrivateEndpointConnectionProvisioningState) UnmarshalJSON(bytes []byte) error {
+	var decoded string
+	if err := json.Unmarshal(bytes, &decoded); err != nil {
+		return fmt.Errorf("unmarshaling: %+v", err)
+	}
+	out, err := parsePrivateEndpointConnectionProvisioningState(decoded)
+	if err != nil {
+		return fmt.Errorf("parsing %q: %+v", decoded, err)
+	}
+	*s = *out
+	return nil
 }
 
 func parsePrivateEndpointConnectionProvisioningState(input string) (*PrivateEndpointConnectionProvisioningState, error) {
@@ -53,6 +70,19 @@ func PossibleValuesForPrivateEndpointServiceConnectionStatus() []string {
 		string(PrivateEndpointServiceConnectionStatusPending),
 		string(PrivateEndpointServiceConnectionStatusRejected),
 	}
+}
+
+func (s *PrivateEndpointServiceConnectionStatus) UnmarshalJSON(bytes []byte) error {
+	var decoded string
+	if err := json.Unmarshal(bytes, &decoded); err != nil {
+		return fmt.Errorf("unmarshaling: %+v", err)
+	}
+	out, err := parsePrivateEndpointServiceConnectionStatus(decoded)
+	if err != nil {
+		return fmt.Errorf("parsing %q: %+v", decoded, err)
+	}
+	*s = *out
+	return nil
 }
 
 func parsePrivateEndpointServiceConnectionStatus(input string) (*PrivateEndpointServiceConnectionStatus, error) {
