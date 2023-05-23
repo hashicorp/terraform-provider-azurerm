aed204cf719b0e61fca3e4c162e2f50abde816b4
Erlend M <ermab95@gmail.com>
2023-05-23 10:42:20 +0200

    changes test resource to None

--- internal/services/mssql/mssql_server_resource_test.go
+++ internal/services/mssql/mssql_server_resource_test.go
@@ -303,7 +303,7 @@ resource "azurerm_mssql_server" "test" {
   version                      = "12.0"
   administrator_login          = "missadministrator"
   administrator_login_password = "thisIsKat11"
-  minimum_tls_version          = "Disabled"
+  minimum_tls_version          = "None"
 
   identity {
     type = "SystemAssigned"
