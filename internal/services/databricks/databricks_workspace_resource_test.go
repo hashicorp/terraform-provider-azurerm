a3d5fbcf31006d03defcd66bfffaae7c19a16ac6
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2023-05-30 15:21:56 -0600

    Add feature flag to machine learning...

--- internal/services/databricks/databricks_workspace_resource_test.go
+++ internal/services/databricks/databricks_workspace_resource_test.go
@@ -780,7 +780,11 @@ resource "azurerm_databricks_workspace" "test" {
 func (DatabricksWorkspaceResource) machineLearning(data acceptance.TestData, sku string) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 data "azurerm_client_config" "current" {}
