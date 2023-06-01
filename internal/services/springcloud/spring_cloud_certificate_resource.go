bdd7c0ed984b190c73b6c916c69e0f813a74c221
Heng Lu <79895375+ms-henglu@users.noreply.github.com>
2023-06-02 06:58:35 +0800

    `azurerm_spring_cloud_certificate` - support for the `exclude_private_key` property (#21942)

--- internal/services/springcloud/spring_cloud_certificate_resource.go
+++ internal/services/springcloud/spring_cloud_certificate_resource.go
@@ -68,6 +68,12 @@ func resourceSpringCloudCertificate() *pluginsdk.Resource {
 				ValidateFunc: validation.StringIsNotEmpty,
 			},
 
+			"exclude_private_key": {
+				Type:     pluginsdk.TypeBool,
+				Optional: true,
+				ForceNew: true,
+			},
+
 			"key_vault_certificate_id": {
 				Type:         pluginsdk.TypeString,
 				Optional:     true,
@@ -112,8 +118,9 @@ func resourceSpringCloudCertificateCreate(d *pluginsdk.ResourceData, meta interf
 			return err
 		}
 		cert.Properties = &appplatform.KeyVaultCertificateProperties{
-			VaultURI:         utils.String(strings.TrimSuffix(keyVaultCertificateId.KeyVaultBaseUrl, "/")),
-			KeyVaultCertName: &keyVaultCertificateId.Name,
+			VaultURI:          utils.String(strings.TrimSuffix(keyVaultCertificateId.KeyVaultBaseUrl, "/")),
+			KeyVaultCertName:  &keyVaultCertificateId.Name,
+			ExcludePrivateKey: utils.Bool(d.Get("exclude_private_key").(bool)),
 		}
 	}
 	if value, ok := d.GetOk("certificate_content"); ok {
@@ -159,6 +166,7 @@ func resourceSpringCloudCertificateRead(d *pluginsdk.ResourceData, meta interfac
 
 	if props, ok := resp.Properties.AsKeyVaultCertificateProperties(); ok && props != nil {
 		d.Set("thumbprint", props.Thumbprint)
+		d.Set("exclude_private_key", props.ExcludePrivateKey)
 	}
 	if props, ok := resp.Properties.AsContentCertificateProperties(); ok && props != nil {
 		d.Set("thumbprint", props.Thumbprint)
