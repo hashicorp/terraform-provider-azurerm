17cde7b60462a352ef82759be964296e0cb9baf1
jackofallops <ste@hashicorp.com>
2023-06-05 12:49:40 +0200

    fix error message in always_on check on create

--- internal/services/appservice/linux_web_app_resource.go
+++ internal/services/appservice/linux_web_app_resource.go
@@ -317,7 +317,7 @@ func (r LinuxWebAppResource) Create() sdk.ResourceFunc {
 			if servicePlan.Sku != nil && servicePlan.Sku.Name != nil {
 				if helpers.IsFreeOrSharedServicePlan(*servicePlan.Sku.Name) {
 					if sc.AlwaysOn {
-						return fmt.Errorf("always_on feature has to be turned off before switching to a free/shared Sku")
+						return fmt.Errorf("always_on cannot be set to true when using Free, F1, D1 Sku")
 					}
 				}
 			}
