f8538cdccc3e722a4cd1620a098c0bf01f58e8f4
Tao <104055472+teowa@users.noreply.github.com>
2023-06-06 14:27:38 +0800

    remove azValidate import alias

--- internal/services/monitor/monitor_autoscale_setting_resource.go
+++ internal/services/monitor/monitor_autoscale_setting_resource.go
@@ -13,7 +13,6 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/validate"
-	azValidate "github.com/hashicorp/terraform-provider-azurerm/helpers/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/monitor/migration"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tags"
@@ -76,7 +75,7 @@ func resourceMonitorAutoScaleSetting() *pluginsdk.Resource {
 			"predictive_scale_look_ahead_time": {
 				Type:         pluginsdk.TypeString,
 				Optional:     true,
-				ValidateFunc: azValidate.ISO8601DurationBetween("PT1M", "PT1H"),
+				ValidateFunc: validate.ISO8601DurationBetween("PT1M", "PT1H"),
 			},
 
 			"predictive_scale_mode": {
