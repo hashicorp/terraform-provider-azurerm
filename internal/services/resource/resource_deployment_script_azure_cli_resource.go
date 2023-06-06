cf007c7b0777ef1bfbc57178f9d2d8af9439422e
Steph <steph@hashicorp.com>
2023-06-06 11:52:08 +0200

    goimports

--- internal/services/resource/resource_deployment_script_azure_cli_resource.go
+++ internal/services/resource/resource_deployment_script_azure_cli_resource.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/hashicorp/go-azure-helpers/resourcemanager/identity"
 	"time"
 
 	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/identity"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/resources/2020-10-01/deploymentscripts"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
