2653e1e0d6fe1436916aacf432e9dad45e85a60e
ziyeqf <51212351+ziyeqf@users.noreply.github.com>
2023-06-07 08:58:31 +0800

    Revert "golint"
    This reverts commit 04fbcd3dd65f54f956c7e68642f1e9511fbfd39e.

--- internal/services/sentinel/sentinel_data_connector_microsoft_threat_intelligence.go
+++ internal/services/sentinel/sentinel_data_connector_microsoft_threat_intelligence.go
@@ -60,8 +60,15 @@ func (s DataConnectorMicrosoftThreatIntelligenceResource) Arguments() map[string
 		"microsoft_emerging_threat_feed_lookback_date": {
 			Type:         pluginsdk.TypeString,
 			ForceNew:     true,
-			Required:     true,
+			Optional:     !features.FourPointOh(),
+			Required:     features.FourPointOh(),
 			ValidateFunc: validation.IsRFC3339Time,
+			AtLeastOneOf: func() []string {
+				if !features.FourPointOh() {
+					return []string{"bing_safety_phishing_url_lookback_date", "microsoft_emerging_threat_feed_lookback_date"}
+				}
+				return []string{}
+			}(),
 		},
 	}
 
@@ -74,6 +81,7 @@ func (s DataConnectorMicrosoftThreatIntelligenceResource) Arguments() map[string
 			Optional:     true,
 			ForceNew:     true,
 			ValidateFunc: validation.IsRFC3339Time,
+			AtLeastOneOf: []string{"bing_safety_phishing_url_lookback_date", "microsoft_emerging_threat_feed_lookback_date"},
 		}
 	}
 
