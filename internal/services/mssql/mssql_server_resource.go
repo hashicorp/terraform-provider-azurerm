ce0937024d3fdda8e0bc4251c6cbb4469249db4a
Elena Xin <39109137+sinbai@users.noreply.github.com>
2023-06-09 07:18:04 +0800

    `azurerm_mssql_server` - Fix issue where `minimum_tls_version` is being returned as `None` instead of `Disabled` (#22067)

--- internal/services/mssql/mssql_server_resource.go
+++ internal/services/mssql/mssql_server_resource.go
@@ -517,7 +517,8 @@ func resourceMsSqlServerRead(d *pluginsdk.ResourceData, meta interface{}) error 
 		d.Set("version", props.Version)
 		d.Set("administrator_login", props.AdministratorLogin)
 		d.Set("fully_qualified_domain_name", props.FullyQualifiedDomainName)
-		if v := props.MinimalTLSVersion; v == nil {
+		// todo remove `|| *v == "None"` when https://github.com/Azure/azure-rest-api-specs/issues/24348 is addressed
+		if v := props.MinimalTLSVersion; v == nil || *v == "None" {
 			d.Set("minimum_tls_version", "Disabled")
 		} else {
 			d.Set("minimum_tls_version", props.MinimalTLSVersion)
@@ -737,7 +738,8 @@ func flattenSqlServerRestorableDatabases(resp sql.RestorableDroppedDatabaseListR
 
 func msSqlMinimumTLSVersionDiff(ctx context.Context, d *pluginsdk.ResourceDiff, _ interface{}) (err error) {
 	old, new := d.GetChange("minimum_tls_version")
-	if old != "" && old != "Disabled" && new == "Disabled" {
+	// todo remove `old != "None"` when https://github.com/Azure/azure-rest-api-specs/issues/24348 is addressed
+	if old != "" && old != "None" && old != "Disabled" && new == "Disabled" {
 		err = fmt.Errorf("`minimum_tls_version` cannot be removed once set, please set a valid value for this property")
 	}
 	return
