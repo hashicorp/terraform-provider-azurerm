273af466afa269ebd616d5eac1c863e56a0bd7b0
xiaxin18 <xiaxyi@microsoft.com>
2023-06-09 15:33:56 +0800

    sorting imports

--- internal/services/appservice/linux_function_app_data_source.go
+++ internal/services/appservice/linux_function_app_data_source.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/appservice/helpers"
