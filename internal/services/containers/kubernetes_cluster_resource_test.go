95c6d6d725c357b99c453519270b68d160b5334e
Mark Rzasa <mark.rzasa@nuance.com>
2023-06-13 10:13:39 -0400

    fix go fmt errors

--- internal/services/containers/kubernetes_cluster_resource_test.go
+++ internal/services/containers/kubernetes_cluster_resource_test.go
@@ -783,5 +783,5 @@ resource "azurerm_kubernetes_cluster" "test" {
     type = "SystemAssigned"
   }
 }
-`  , data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger, controlPlaneVersion, osSku)
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger, controlPlaneVersion, osSku)
 }
