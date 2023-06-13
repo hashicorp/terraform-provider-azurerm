f8c35899b2fbee26cf15ced1611dd012a983d1aa
Yichun Ma <yicma@microsoft.com>
2023-06-13 08:42:50 +0800

    `dependencies` - update `compute/gallery*` to `2022-03-03` (#21999)

--- internal/services/compute/gallery_application_version_resource_test.go
+++ internal/services/compute/gallery_application_version_resource_test.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/compute/2021-07-01/galleryapplicationversions"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/compute/2022-03-03/galleryapplicationversions"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
