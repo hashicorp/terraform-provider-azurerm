c163b17d126b0ad4f6f20e846114839e24f5c339
jackofallops <ste@hashicorp.com>
2023-07-03 14:27:46 +0200

    add tracking issue for missing enums

--- internal/services/appservice/helpers/enums.go
+++ internal/services/appservice/helpers/enums.go
@@ -1,5 +1,8 @@
 package helpers
 
+// publicNetworkAccess Enums are missing in api-specs
+// https://github.com/Azure/azure-rest-api-specs/issues/24680
+
 const (
 	PublicNetworkAccessEnabled  string = "Enabled"
 	PublicNetworkAccessDisabled string = "Disabled"
