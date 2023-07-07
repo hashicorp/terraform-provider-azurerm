16e4f3407305e9bd46788253f9df181f5a5e155e
Tom Harvey <tombuildsstuff@users.noreply.github.com>
2023-07-07 18:57:53 +0200

    `azurestackhcl`: updating to use API Version `2023-03-01` (#22411)

--- internal/services/azurestackhci/stack_hci_cluster_resource_test.go
+++ internal/services/azurestackhci/stack_hci_cluster_resource_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/go-azure-helpers/lang/response"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/azurestackhci/2022-12-01/clusters"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/azurestackhci/2023-03-01/clusters"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
