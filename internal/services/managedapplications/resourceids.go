ccecc1ee02e525f31c7d0b154246881143e28583
kt <kt@katbyte.me>
2023-07-07 12:52:36 -0700

    copyright headers: services m-n (#22421)

--- internal/services/managedapplications/resourceids.go
+++ internal/services/managedapplications/resourceids.go
@@ -1,3 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package managedapplications
 
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Application -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Solutions/applications/app1
