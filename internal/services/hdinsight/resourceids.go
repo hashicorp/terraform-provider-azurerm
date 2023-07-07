79c570c93f4100a070e985706d1d14607639caa2
kt <kt@katbyte.me>
2023-07-07 12:28:57 -0700

    Copyrigth Headers: services h-l

--- internal/services/hdinsight/resourceids.go
+++ internal/services/hdinsight/resourceids.go
@@ -1,3 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package hdinsight
 
 //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=Cluster -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.HDInsight/clusters/cluster1
