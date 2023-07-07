0aa3500fac7104bdc7a9d7daa0593ec89ab273f5
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-07-07 15:59:27 +0200

    go mod vendor

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/kusto/2023-05-02/databases/predicates.go
@@ -0,0 +1,12 @@
+package databases
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type DatabaseOperationPredicate struct {
+}
+
+func (p DatabaseOperationPredicate) Matches(input Database) bool {
+
+	return true
+}
