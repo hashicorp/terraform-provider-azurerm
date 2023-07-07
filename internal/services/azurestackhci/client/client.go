f68447f4688e18e98ba78340bc4c8b4c8d74e8a7
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-07-07 16:15:41 +0200

    `azurestackhci`: updating to API Version `2023-03-01`

--- internal/services/azurestackhci/client/client.go
+++ internal/services/azurestackhci/client/client.go
@@ -2,12 +2,12 @@ package client
 
 import (
 	"github.com/Azure/go-autorest/autorest"
-	azurestackhci_v2022_12_01 "github.com/hashicorp/go-azure-sdk/resource-manager/azurestackhci/2022-12-01"
+	azurestackhci_v2023_03_01 "github.com/hashicorp/go-azure-sdk/resource-manager/azurestackhci/2023-03-01"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
 
-func NewClient(o *common.ClientOptions) *azurestackhci_v2022_12_01.Client {
-	client := azurestackhci_v2022_12_01.NewClientWithBaseURI(o.ResourceManagerEndpoint, func(c *autorest.Client) {
+func NewClient(o *common.ClientOptions) *azurestackhci_v2023_03_01.Client {
+	client := azurestackhci_v2023_03_01.NewClientWithBaseURI(o.ResourceManagerEndpoint, func(c *autorest.Client) {
 		o.ConfigureClient(c, o.ResourceManagerAuthorizer)
 	})
 
