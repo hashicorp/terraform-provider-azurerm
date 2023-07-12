ad680474d1d1087cda4d8db12298c1c94dca49eb
Bindewald, André (UIT) <Andre.Bindewald@union-investment.de>
2023-07-12 17:28:00 +0200

    fix(azurerm_api_management_api): wsdl_selector is only required if the wsdl contains multiple services/interfaces. Fixes #22489

--- internal/services/apimanagement/api_management_api_resource_test.go
+++ internal/services/apimanagement/api_management_api_resource_test.go
@@ -599,6 +599,27 @@ resource "azurerm_api_management_api" "test" {
   import {
     content_value  = file("testdata/api_management_api_wsdl.xml")
     content_format = "wsdl"
+  }
+}
+`, r.template(data, SkuNameConsumption), data.RandomInteger)
+}
+
+func (r ApiManagementApiResource) importWsdlWithSelector(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_api_management_api" "test" {
+  name                = "acctestapi-%d"
+  resource_group_name = azurerm_resource_group.test.name
+  api_management_name = azurerm_api_management.test.name
+  display_name        = "api1"
+  path                = "api1"
+  protocols           = ["https"]
+  revision            = "1"
+
+  import {
+    content_value  = file("testdata/api_management_api_wsdl_multiple.xml")
+    content_format = "wsdl"
 
     wsdl_selector {
       service_name  = "Calculator"
