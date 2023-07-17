f2e53699a94d36660d63f5afcc276813b4e2bc60
hc-github-team-tf-azure <>
2023-07-17 08:46:38 +0000

    Updating based on 7230ac2a8

--- internal/services/containers/client/client_gen.go
+++ internal/services/containers/client/client_gen.go
@@ -3,11 +3,13 @@ package client
 import (
 	"github.com/Azure/go-autorest/autorest"
 	containerserviceV20220902Preview "github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2022-09-02-preview"
+	containerserviceV20230402Preview "github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2023-04-02-preview"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
 
 type AutoClient struct {
 	V20220902Preview containerserviceV20220902Preview.Client
+	V20230402Preview containerserviceV20230402Preview.Client
 }
 
 func NewClient(o *common.ClientOptions) (*AutoClient, error) {
@@ -16,7 +18,12 @@ func NewClient(o *common.ClientOptions) (*AutoClient, error) {
 		c.Authorizer = o.ResourceManagerAuthorizer
 	})
 
+	v20230402PreviewClient := containerserviceV20230402Preview.NewClientWithBaseURI(o.ResourceManagerEndpoint, func(c *autorest.Client) {
+		c.Authorizer = o.ResourceManagerAuthorizer
+	})
+
 	return &AutoClient{
 		V20220902Preview: v20220902PreviewClient,
+		V20230402Preview: v20230402PreviewClient,
 	}, nil
 }
