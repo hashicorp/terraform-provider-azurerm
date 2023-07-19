1ad600ed0ddd9d4b248f1bf183ec5ba75b8c6328
xuzhang3 <Zhangxu894765>
2023-07-19 14:54:10 +0800

    add validtion

--- internal/services/redis/redis_cache_resource.go
+++ internal/services/redis/redis_cache_resource.go
@@ -821,14 +821,20 @@ func expandRedisConfiguration(d *pluginsdk.ResourceData) (*redis.RedisCommonProp
 	v, valExists := d.GetOkExists("redis_configuration.0.rdb_backup_enabled")
 	if valExists {
 		skuName := d.Get("sku_name").(string)
+		rdbBackupEnabled := raw["rdb_backup_enabled"].(bool)
+
 		// rdb_backup_enabled is available when SKU is Premium
 		if strings.EqualFold(skuName, string(redis.SkuNamePremium)) {
-			if v := raw["rdb_backup_enabled"].(bool); v {
+			if rdbBackupEnabled {
 				if connStr := raw["rdb_storage_connection_string"].(string); connStr == "" {
 					return nil, fmt.Errorf("The rdb_storage_connection_string property must be set when rdb_backup_enabled is true")
 				}
 			}
-			output.RdbBackupEnabled = utils.String(strconv.FormatBool(v.(bool)))
+			output.RdbBackupEnabled = utils.String(strconv.FormatBool(rdbBackupEnabled))
+		} else {
+			if rdbBackupEnabled && !strings.EqualFold(skuName, string(redis.SkuNamePremium)) {
+				return nil, fmt.Errorf("The `rdb_backup_enabled` property requires a `Premium` sku to be set")
+			}
 		}
 	}
 
