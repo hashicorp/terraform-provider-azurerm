74c2b9244392d160fe7b73cb5e14c32c09d72df4
hc-github-team-tf-azure <>
2023-07-21 12:46:12 +0000

    Updating based on 7bfef67a2

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2023-03-02-preview/resolveprivatelinkserviceid/version.go
@@ -0,0 +1,12 @@
+package resolveprivatelinkserviceid
+
+import "fmt"
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+const defaultApiVersion = "2023-03-02-preview"
+
+func userAgent() string {
+	return fmt.Sprintf("hashicorp/go-azure-sdk/resolveprivatelinkserviceid/%s", defaultApiVersion)
+}
