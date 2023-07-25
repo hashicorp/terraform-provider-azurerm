0c134b2f91c20c337f353044d418f0f5a450456c
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-07-25 13:05:27 +0200

    eventgrid: refactoring `system_topic` to use `hashicorp/go-azure-sdk`

--- internal/services/eventgrid/resourceids.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
-package eventgrid
-
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=SystemTopic -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.EventGrid/systemTopics/systemTopic1
