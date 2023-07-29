ba091769afce0b7a9a3925ca6df93547f6b114f7
KenSpur <spur.ken@justbeawesome.net>
2023-07-29 10:22:19 +0200

    add iotcentral_ad_group_user_resource & iotcentral_ad_group_user_resource_test

--- internal/services/iotcentral/registration.go
+++ internal/services/iotcentral/registration.go
@@ -31,9 +31,10 @@ func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
 // SupportedResources returns the supported Resources supported by this Service
 func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
 	return map[string]*pluginsdk.Resource{
-		"azurerm_iotcentral_application":  resourceIotCentralApplication(),
-		"azurerm_iotcentral_organization": resourceIotCentralOrganization(),
-		"azurerm_iotcentral_email_user":   resourceIotCentralEmailUser(),
+		"azurerm_iotcentral_application":   resourceIotCentralApplication(),
+		"azurerm_iotcentral_organization":  resourceIotCentralOrganization(),
+		"azurerm_iotcentral_email_user":    resourceIotCentralEmailUser(),
+		"azurerm_iotcentral_ad_group_user": resourceIotCentralADGroupUser(),
 	}
 }
 
