363c46441985bd7c1850e666b2bb88df444de287
jackofallops <ste@hashicorp.com>
2023-07-31 17:20:45 +0200

    missed initialising the meta client

--- internal/services/paloalto/client/client.go
+++ internal/services/paloalto/client/client.go
@@ -1,7 +1,10 @@
 package client
 
 import (
+	"fmt"
+
 	paloalto_2022_08_29 "github.com/hashicorp/go-azure-sdk/resource-manager/paloaltonetworks/2022-08-29"
+	"github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
 
@@ -9,7 +12,15 @@ type Client struct {
 	*paloalto_2022_08_29.Client
 }
 
-func NewClient(_ *common.ClientOptions) (*Client, error) {
+func NewClient(o *common.ClientOptions) (*Client, error) {
+	client, err := paloalto_2022_08_29.NewClientWithBaseURI(o.Environment.ResourceManager, func(c *resourcemanager.Client) {
+		o.Configure(c, o.Authorizers.ResourceManager)
+	})
+	if err != nil {
+		return nil, fmt.Errorf("building clients for Network: %+v", err)
+	}
 
-	return &Client{}, nil
+	return &Client{
+		Client: client,
+	}, nil
 }
