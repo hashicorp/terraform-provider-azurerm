b8457985432536cfe088548dcb4b8ff88e154fb8
Zhenhua Hu <zhhu@microsoft.com>
2023-08-03 20:29:24 +0800

    fix depscheck

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/networkfunction/2022-11-01/collectorpolicies/client.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/networkfunction/2022-11-01/collectorpolicies/client.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager"
-	"github.com/hashicorp/go-azure-sdk/sdk/environments"
+	sdkEnv "github.com/hashicorp/go-azure-sdk/sdk/environments"
 )
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
@@ -14,8 +14,8 @@ type CollectorPoliciesClient struct {
 	Client *resourcemanager.Client
 }
 
-func NewCollectorPoliciesClientWithBaseURI(api environments.Api) (*CollectorPoliciesClient, error) {
-	client, err := resourcemanager.NewResourceManagerClient(api, "collectorpolicies", defaultApiVersion)
+func NewCollectorPoliciesClientWithBaseURI(sdkApi sdkEnv.Api) (*CollectorPoliciesClient, error) {
+	client, err := resourcemanager.NewResourceManagerClient(sdkApi, "collectorpolicies", defaultApiVersion)
 	if err != nil {
 		return nil, fmt.Errorf("instantiating CollectorPoliciesClient: %+v", err)
 	}
