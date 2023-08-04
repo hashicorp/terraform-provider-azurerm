437010ebfb180d4604844824af7b7d76698234b4
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-08-04 09:52:49 +0200

    dependencies: updating to `v0.20230804.1074947` of `github.com/hashicorp/go-azure-sdk`

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/hybridkubernetes/2021-10-01/connectedclusters/method_connectedclusterlistbysubscription.go
@@ -0,0 +1,90 @@
+package connectedclusters
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
+	"github.com/hashicorp/go-azure-sdk/sdk/client"
+	"github.com/hashicorp/go-azure-sdk/sdk/odata"
+)
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type ConnectedClusterListBySubscriptionOperationResponse struct {
+	HttpResponse *http.Response
+	OData        *odata.OData
+	Model        *[]ConnectedCluster
+}
+
+type ConnectedClusterListBySubscriptionCompleteResult struct {
+	Items []ConnectedCluster
+}
+
+// ConnectedClusterListBySubscription ...
+func (c ConnectedClustersClient) ConnectedClusterListBySubscription(ctx context.Context, id commonids.SubscriptionId) (result ConnectedClusterListBySubscriptionOperationResponse, err error) {
+	opts := client.RequestOptions{
+		ContentType: "application/json; charset=utf-8",
+		ExpectedStatusCodes: []int{
+			http.StatusOK,
+		},
+		HttpMethod: http.MethodGet,
+		Path:       fmt.Sprintf("%s/providers/Microsoft.Kubernetes/connectedClusters", id.ID()),
+	}
+
+	req, err := c.Client.NewRequest(ctx, opts)
+	if err != nil {
+		return
+	}
+
+	var resp *client.Response
+	resp, err = req.ExecutePaged(ctx)
+	if resp != nil {
+		result.OData = resp.OData
+		result.HttpResponse = resp.Response
+	}
+	if err != nil {
+		return
+	}
+
+	var values struct {
+		Values *[]ConnectedCluster `json:"value"`
+	}
+	if err = resp.Unmarshal(&values); err != nil {
+		return
+	}
+
+	result.Model = values.Values
+
+	return
+}
+
+// ConnectedClusterListBySubscriptionComplete retrieves all the results into a single object
+func (c ConnectedClustersClient) ConnectedClusterListBySubscriptionComplete(ctx context.Context, id commonids.SubscriptionId) (ConnectedClusterListBySubscriptionCompleteResult, error) {
+	return c.ConnectedClusterListBySubscriptionCompleteMatchingPredicate(ctx, id, ConnectedClusterOperationPredicate{})
+}
+
+// ConnectedClusterListBySubscriptionCompleteMatchingPredicate retrieves all the results and then applies the predicate
+func (c ConnectedClustersClient) ConnectedClusterListBySubscriptionCompleteMatchingPredicate(ctx context.Context, id commonids.SubscriptionId, predicate ConnectedClusterOperationPredicate) (result ConnectedClusterListBySubscriptionCompleteResult, err error) {
+	items := make([]ConnectedCluster, 0)
+
+	resp, err := c.ConnectedClusterListBySubscription(ctx, id)
+	if err != nil {
+		err = fmt.Errorf("loading results: %+v", err)
+		return
+	}
+	if resp.Model != nil {
+		for _, v := range *resp.Model {
+			if predicate.Matches(v) {
+				items = append(items, v)
+			}
+		}
+	}
+
+	result = ConnectedClusterListBySubscriptionCompleteResult{
+		Items: items,
+	}
+	return
+}
--- vendor/github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager/poller_lro.go
+++ vendor/github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager/poller_lro.go
@@ -174,9 +174,10 @@ func (p *longRunningOperationPoller) Poll(ctx context.Context) (result *pollers.
 			"Updating": pollers.PollingStatusInProgress,
 			// StorageSync@2020-03-01 returns `validateInput`, `newPrivateDnsEntries`, `finishNewStorageSyncService` rather than `InProgress` during creation/update
 			// See: https://github.com/hashicorp/go-azure-sdk/issues/565
-			"validateInput":               pollers.PollingStatusInProgress,
-			"newPrivateDnsEntries":        pollers.PollingStatusInProgress,
-			"finishNewStorageSyncService": pollers.PollingStatusInProgress,
+			"validateInput":                    pollers.PollingStatusInProgress,
+			"newPrivateDnsEntries":             pollers.PollingStatusInProgress,
+			"newManagedIdentityCredentialStep": pollers.PollingStatusInProgress,
+			"finishNewStorageSyncService":      pollers.PollingStatusInProgress,
 			// StorageSync@2020-03-01 (CloudEndpoints) returns `newReplicaGroup` rather than `InProgress` during creation/update
 			// See: https://github.com/hashicorp/go-azure-sdk/issues/565
 			"newReplicaGroup": pollers.PollingStatusInProgress,
