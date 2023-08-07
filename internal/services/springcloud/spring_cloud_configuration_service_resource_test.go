7781269dd7bab51e2456938577e0fde029b37d9c
Heng Lu <henglu@microsoft.com>
2023-08-07 14:07:22 +0800

    update

--- internal/services/springcloud/spring_cloud_configuration_service_resource_test.go
+++ internal/services/springcloud/spring_cloud_configuration_service_resource_test.go
@@ -264,7 +264,7 @@ data "azurerm_client_config" "current" {
 }
 
 data "azuread_service_principal" "test" {
-  display_name = "Azure Spring Cloud Domain-Management"
+  display_name = "Azure Spring Cloud Resource Provider"
 }
 
 resource "azurerm_key_vault" "test" {
@@ -359,6 +359,7 @@ resource "azurerm_spring_cloud_certificate" "test" {
   resource_group_name      = azurerm_spring_cloud_service.test.resource_group_name
   service_name             = azurerm_spring_cloud_service.test.name
   key_vault_certificate_id = azurerm_key_vault_certificate.test.id
+  exclude_private_key      = true
 }
 
 resource "azurerm_spring_cloud_configuration_service" "test" {
