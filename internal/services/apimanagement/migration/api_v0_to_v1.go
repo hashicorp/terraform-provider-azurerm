ac5ffd39e0b969f2b9da8d6da77f9fd1e615edc5
Kevin Allen <kallen@nexient.com>
2023-08-09 14:20:27 -0400

    make migration point-in-time, remove fields

--- internal/services/apimanagement/migration/api_v0_to_v1.go
+++ internal/services/apimanagement/migration/api_v0_to_v1.go
@@ -3,14 +3,12 @@ package migration
 import (
 	"context"
 	"fmt"
+	"log"
 
-	"github.com/Azure/azure-sdk-for-go/services/apimanagement/mgmt/2021-08-01/apimanagement"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/apimanagement/parse"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/apimanagement/schemaz"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/services/apimanagement/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 )
 
 var _ pluginsdk.StateUpgrade = ApiV0ToV1{}
@@ -26,17 +24,15 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 		"resource_group_name": commonschema.ResourceGroupName(),
 
 		"display_name": {
-			Type:         pluginsdk.TypeString,
-			Optional:     true,
-			Computed:     true,
-			ValidateFunc: validation.StringIsNotEmpty,
+			Type:     pluginsdk.TypeString,
+			Optional: true,
+			Computed: true,
 		},
 
 		"path": {
-			Type:         pluginsdk.TypeString,
-			Optional:     true,
-			Computed:     true,
-			ValidateFunc: validate.ApiManagementApiPath,
+			Type:     pluginsdk.TypeString,
+			Optional: true,
+			Computed: true,
 		},
 
 		"protocols": {
@@ -45,26 +41,18 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 			Computed: true,
 			Elem: &pluginsdk.Schema{
 				Type: pluginsdk.TypeString,
-				ValidateFunc: validation.StringInSlice([]string{
-					string(apimanagement.ProtocolHTTP),
-					string(apimanagement.ProtocolHTTPS),
-					string(apimanagement.ProtocolWs),
-					string(apimanagement.ProtocolWss),
-				}, false),
 			},
 		},
 
 		"revision": {
-			Type:         pluginsdk.TypeString,
-			Required:     true,
-			ForceNew:     true,
-			ValidateFunc: validation.StringIsNotEmpty,
+			Type:     pluginsdk.TypeString,
+			Required: true,
+			ForceNew: true,
 		},
 
 		"revision_description": {
-			Type:         pluginsdk.TypeString,
-			Optional:     true,
-			ValidateFunc: validation.StringIsNotEmpty,
+			Type:     pluginsdk.TypeString,
+			Optional: true,
 		},
 
 		// Optional
@@ -72,12 +60,6 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 			Type:     pluginsdk.TypeString,
 			Optional: true,
 			Computed: true,
-			ValidateFunc: validation.StringInSlice([]string{
-				string(apimanagement.APITypeGraphql),
-				string(apimanagement.APITypeHTTP),
-				string(apimanagement.APITypeSoap),
-				string(apimanagement.APITypeWebsocket),
-			}, false),
 		},
 
 		"contact": {
@@ -88,19 +70,16 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 			Elem: &pluginsdk.Resource{
 				Schema: map[string]*pluginsdk.Schema{
 					"email": {
-						Type:         pluginsdk.TypeString,
-						Optional:     true,
-						ValidateFunc: validate.EmailAddress,
+						Type:     pluginsdk.TypeString,
+						Optional: true,
 					},
 					"name": {
-						Type:         pluginsdk.TypeString,
-						Optional:     true,
-						ValidateFunc: validation.StringIsNotEmpty,
+						Type:     pluginsdk.TypeString,
+						Optional: true,
 					},
 					"url": {
-						Type:         pluginsdk.TypeString,
-						Optional:     true,
-						ValidateFunc: validation.IsURLWithHTTPorHTTPS,
+						Type:     pluginsdk.TypeString,
+						Optional: true,
 					},
 				},
 			},
@@ -118,26 +97,13 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 			Elem: &pluginsdk.Resource{
 				Schema: map[string]*pluginsdk.Schema{
 					"content_value": {
-						Type:         pluginsdk.TypeString,
-						Required:     true,
-						ValidateFunc: validation.StringIsNotEmpty,
+						Type:     pluginsdk.TypeString,
+						Required: true,
 					},
 
 					"content_format": {
 						Type:     pluginsdk.TypeString,
 						Required: true,
-						ValidateFunc: validation.StringInSlice([]string{
-							string(apimanagement.ContentFormatOpenapi),
-							string(apimanagement.ContentFormatOpenapijson),
-							string(apimanagement.ContentFormatOpenapijsonLink),
-							string(apimanagement.ContentFormatOpenapiLink),
-							string(apimanagement.ContentFormatSwaggerJSON),
-							string(apimanagement.ContentFormatSwaggerLinkJSON),
-							string(apimanagement.ContentFormatWadlLinkJSON),
-							string(apimanagement.ContentFormatWadlXML),
-							string(apimanagement.ContentFormatWsdl),
-							string(apimanagement.ContentFormatWsdlLink),
-						}, false),
 					},
 
 					"wsdl_selector": {
@@ -147,15 +113,13 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 						Elem: &pluginsdk.Resource{
 							Schema: map[string]*pluginsdk.Schema{
 								"service_name": {
-									Type:         pluginsdk.TypeString,
-									Required:     true,
-									ValidateFunc: validation.StringIsNotEmpty,
+									Type:     pluginsdk.TypeString,
+									Required: true,
 								},
 
 								"endpoint_name": {
-									Type:         pluginsdk.TypeString,
-									Required:     true,
-									ValidateFunc: validation.StringIsNotEmpty,
+									Type:     pluginsdk.TypeString,
+									Required: true,
 								},
 							},
 						},
@@ -172,14 +136,12 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 			Elem: &pluginsdk.Resource{
 				Schema: map[string]*pluginsdk.Schema{
 					"name": {
-						Type:         pluginsdk.TypeString,
-						Optional:     true,
-						ValidateFunc: validation.StringIsNotEmpty,
+						Type:     pluginsdk.TypeString,
+						Optional: true,
 					},
 					"url": {
-						Type:         pluginsdk.TypeString,
-						Optional:     true,
-						ValidateFunc: validation.IsURLWithHTTPorHTTPS,
+						Type:     pluginsdk.TypeString,
+						Optional: true,
 					},
 				},
 			},
@@ -199,14 +161,12 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 			Elem: &pluginsdk.Resource{
 				Schema: map[string]*pluginsdk.Schema{
 					"header": {
-						Type:         pluginsdk.TypeString,
-						Required:     true,
-						ValidateFunc: validation.StringIsNotEmpty,
+						Type:     pluginsdk.TypeString,
+						Required: true,
 					},
 					"query": {
-						Type:         pluginsdk.TypeString,
-						Required:     true,
-						ValidateFunc: validation.StringIsNotEmpty,
+						Type:     pluginsdk.TypeString,
+						Required: true,
 					},
 				},
 			},
@@ -215,19 +175,16 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 		"subscription_required": {
 			Type:     pluginsdk.TypeBool,
 			Optional: true,
-			Default:  true,
 		},
 
 		"terms_of_service_url": {
-			Type:         pluginsdk.TypeString,
-			Optional:     true,
-			ValidateFunc: validation.IsURLWithHTTPorHTTPS,
+			Type:     pluginsdk.TypeString,
+			Optional: true,
 		},
 
 		"source_api_id": {
-			Type:         pluginsdk.TypeString,
-			Optional:     true,
-			ValidateFunc: validate.ApiID,
+			Type:     pluginsdk.TypeString,
+			Optional: true,
 		},
 
 		"oauth2_authorization": {
@@ -237,9 +194,8 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 			Elem: &pluginsdk.Resource{
 				Schema: map[string]*pluginsdk.Schema{
 					"authorization_server_name": {
-						Type:         pluginsdk.TypeString,
-						Required:     true,
-						ValidateFunc: validate.ApiManagementChildName,
+						Type:     pluginsdk.TypeString,
+						Required: true,
 					},
 					"scope": {
 						Type:     pluginsdk.TypeString,
@@ -257,19 +213,14 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 			Elem: &pluginsdk.Resource{
 				Schema: map[string]*pluginsdk.Schema{
 					"openid_provider_name": {
-						Type:         pluginsdk.TypeString,
-						Required:     true,
-						ValidateFunc: validate.ApiManagementChildName,
+						Type:     pluginsdk.TypeString,
+						Required: true,
 					},
 					"bearer_token_sending_methods": {
 						Type:     pluginsdk.TypeSet,
 						Optional: true,
 						Elem: &pluginsdk.Schema{
 							Type: pluginsdk.TypeString,
-							ValidateFunc: validation.StringInSlice([]string{
-								string(apimanagement.BearerTokenSendingMethodsAuthorizationHeader),
-								string(apimanagement.BearerTokenSendingMethodsQuery),
-							}, false),
 						},
 					},
 				},
@@ -287,6 +238,12 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 			Computed: true,
 		},
 
+		"soap_pass_through": {
+			Type:     pluginsdk.TypeBool,
+			Computed: true,
+			Optional: true,
+		},
+
 		"version": {
 			Type:     pluginsdk.TypeString,
 			Computed: true,
@@ -294,9 +251,8 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 		},
 
 		"version_description": {
-			Type:         pluginsdk.TypeString,
-			Optional:     true,
-			ValidateFunc: validation.StringIsNotEmpty,
+			Type:     pluginsdk.TypeString,
+			Optional: true,
 		},
 
 		"version_set_id": {
@@ -306,24 +262,19 @@ func (ApiV0ToV1) Schema() map[string]*pluginsdk.Schema {
 		},
 	}
 
-	if !features.FourPointOhBeta() {
-		schema["api_type"].ConflictsWith = []string{"soap_pass_through"}
-
-		schema["soap_pass_through"] = &pluginsdk.Schema{
-			Type:          pluginsdk.TypeBool,
-			Optional:      true,
-			Computed:      true,
-			Deprecated:    "`soap_pass_through` will be removed in favour of the property `api_type` in version 4.0 of the AzureRM Provider",
-			ConflictsWith: []string{"api_type"},
-		}
-	}
-
 	return schema
 }
 
 func (ApiV0ToV1) UpgradeFunc() pluginsdk.StateUpgraderFunc {
 	return func(ctx context.Context, rawState map[string]interface{}, meta interface{}) (map[string]interface{}, error) {
-		rawState["id"] = fmt.Sprintf("%s;rev=%s", rawState["id"].(string), rawState["revision"].(string))
+		apiId := fmt.Sprintf("%s;rev=%s", rawState["name"], rawState["revision"])
+		oldId, err := parse.ApiID(rawState["id"].(string))
+		if err != nil {
+			return rawState, err
+		}
+		newId := parse.NewApiID(oldId.SubscriptionId, oldId.ResourceGroup, oldId.ServiceName, apiId).ID()
+		log.Printf("[DEBUG] Updating ID from %q to %q", oldId, newId)
+		rawState["id"] = newId
 		return rawState, nil
 	}
 }
