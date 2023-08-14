5b57b457254f1198a1c63ae05f5d7dfd33fac260
Yichun Ma <yicma@microsoft.com>
2023-08-14 19:43:26 +0800

    New resources `azurerm_mssql_virtual_machine_group` and `azurerm_mssql_virtual_machine_availability_group_listener` (#22808)
    * sql vm high availability
    
    * update
    
    * resolve comments
    
    * flatten private_ip_address and update doc
    
    * fix lint
    
    ---------
    
    Co-authored-by: Catriona <catriona@hashicorp.com>

---
+++ internal/services/mssql/validate/sql_image_offer_name.go
@@ -0,0 +1,16 @@
+package validate
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func SqlImageOfferName(v interface{}, k string) (warnings []string, errors []error) {
+	value := v.(string)
+
+	if !regexp.MustCompile(`^SQL[A-Za-z0-9]*-WS[A-Za-z0-9]*$`).MatchString(value) {
+		errors = append(errors, fmt.Errorf("%q should be in the form SQL<SQLversion>-WS<OSversion>, for example SQL2019-WS2019: %q", k, value))
+	}
+
+	return warnings, errors
+}
