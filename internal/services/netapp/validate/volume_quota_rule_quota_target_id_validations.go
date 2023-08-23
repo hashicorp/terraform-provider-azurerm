250d3a377f85000a3750ef1f8b0226143d92719f
Paulo <pmarques@microsoft.com>
2023-08-23 00:26:16 +0000

    Fixing 32bit int issue

--- internal/services/netapp/validate/volume_quota_rule_quota_target_id_validations.go
+++ internal/services/netapp/validate/volume_quota_rule_quota_target_id_validations.go
@@ -11,23 +11,25 @@ import (
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 )
 
+const maxQuotaTargetIDSizeInKiB int64 = 4294967295
+
 func ValidateUnixUserIDOrGroupID(v interface{}, k string) (warnings []string, errors []error) {
-	var value int
+	var value int64
 	var err error
 
 	switch v.(type) {
 	case int:
-		value = v.(int)
+		value = v.(int64)
 	case string:
-		if _, err := strconv.Atoi(v.(string)); err != nil {
+		if _, err := strconv.ParseInt(v.(string), 10, 64); err != nil {
 			errors = append(errors, fmt.Errorf("%q must be an integer or a string that can be converted to an integer", k))
 			return warnings, errors
 		}
-		if _, err := strconv.Atoi(v.(string)); err == nil && !regexp.MustCompile(`^\d+$`).MatchString(v.(string)) {
+		if _, err := strconv.ParseInt(v.(string), 10, 64); err == nil && !regexp.MustCompile(`^\d+$`).MatchString(v.(string)) {
 			errors = append(errors, fmt.Errorf("%q must be an integer or a string that contains only digits", k))
 			return warnings, errors
 		}
-		value, err = strconv.Atoi(v.(string))
+		value, err = strconv.ParseInt(v.(string), 10, 64)
 		if err != nil {
 			errors = append(errors, fmt.Errorf("%q must be an integer or a string that can be converted to an integer", k))
 			return warnings, errors
@@ -37,7 +39,7 @@ func ValidateUnixUserIDOrGroupID(v interface{}, k string) (warnings []string, er
 		return warnings, errors
 	}
 
-	if value < 1 || value > 4294967295 {
+	if value < 1 || value > maxQuotaTargetIDSizeInKiB {
 		errors = append(errors, fmt.Errorf("%q must be between 1 and 4294967295", k))
 		return warnings, errors
 	}
