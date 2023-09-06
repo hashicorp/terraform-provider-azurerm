34674f40dd5c9b2e7a0b9d31784f6b7524ac3cdc
Paulo <pmarques@microsoft.com>
2023-09-06 15:29:04 +0000

    Fixing linting issues

--- internal/services/netapp/netapp_volume_quota_rule_resource_test.go
+++ internal/services/netapp/netapp_volume_quota_rule_resource_test.go
@@ -103,12 +103,12 @@ func (NetAppVolumeQuotaRuleResource) individualGroupQuotaType(data acceptance.Te
 %[1]s
 
 resource "azurerm_netapp_volume_quota_rule" "test" {
-  name                = "acctest-NetAppVolumeQuotaRule-%[2]d"
-  location            = azurerm_resource_group.test.location
-  volume_id           = azurerm_netapp_volume.test.id
-  quota_target        = "3001"
-  quota_size_in_kib   = 1024
-  quota_type          = "IndividualGroupQuota"
+  name              = "acctest-NetAppVolumeQuotaRule-%[2]d"
+  location          = azurerm_resource_group.test.location
+  volume_id         = azurerm_netapp_volume.test.id
+  quota_target      = "3001"
+  quota_size_in_kib = 1024
+  quota_type        = "IndividualGroupQuota"
 }
 `, template, data.RandomInteger)
 }
@@ -119,12 +119,12 @@ func (NetAppVolumeQuotaRuleResource) individualUserQuotaType(data acceptance.Tes
 %[1]s
 
 resource "azurerm_netapp_volume_quota_rule" "test" {
-  name                = "acctest-NetAppVolumeQuotaRule-%[2]d"
-  location            = azurerm_resource_group.test.location
-  volume_id           = azurerm_netapp_volume.test.id
-  quota_target        = "3001"
-  quota_size_in_kib   = 1024
-  quota_type          = "IndividualUserQuota"
+  name              = "acctest-NetAppVolumeQuotaRule-%[2]d"
+  location          = azurerm_resource_group.test.location
+  volume_id         = azurerm_netapp_volume.test.id
+  quota_target      = "3001"
+  quota_size_in_kib = 1024
+  quota_type        = "IndividualUserQuota"
 }
 `, template, data.RandomInteger)
 }
@@ -135,11 +135,11 @@ func (NetAppVolumeQuotaRuleResource) defaultUserQuotaType(data acceptance.TestDa
 %[1]s
 
 resource "azurerm_netapp_volume_quota_rule" "test" {
-  name                = "acctest-NetAppVolumeQuotaRule-Default-Usr-%[2]d"
-  location            = azurerm_resource_group.test.location
-  volume_id           = azurerm_netapp_volume.test.id
-  quota_size_in_kib   = 2048
-  quota_type          = "DefaultUserQuota"
+  name              = "acctest-NetAppVolumeQuotaRule-Default-Usr-%[2]d"
+  location          = azurerm_resource_group.test.location
+  volume_id         = azurerm_netapp_volume.test.id
+  quota_size_in_kib = 2048
+  quota_type        = "DefaultUserQuota"
 }
 `, template, data.RandomInteger)
 }
@@ -150,11 +150,11 @@ func (NetAppVolumeQuotaRuleResource) defaultGroupQuotaType(data acceptance.TestD
 %[1]s
 
 resource "azurerm_netapp_volume_quota_rule" "test" {
-  name                = "acctest-NetAppVolumeQuotaRule-Default-Grp-%[2]d"
-  location            = azurerm_resource_group.test.location
-  volume_id           = azurerm_netapp_volume.test.id
-  quota_size_in_kib   = 2048
-  quota_type          = "DefaultGroupQuota"
+  name              = "acctest-NetAppVolumeQuotaRule-Default-Grp-%[2]d"
+  location          = azurerm_resource_group.test.location
+  volume_id         = azurerm_netapp_volume.test.id
+  quota_size_in_kib = 2048
+  quota_type        = "DefaultGroupQuota"
 }
 `, template, data.RandomInteger)
 }
