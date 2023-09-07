8c6804f095217fe92a344ae53f20d870bbe9700a
Elena Xin <39109137+sinbai@users.noreply.github.com>
2023-09-07 22:38:07 +0800

    `azurerm_api_management_api_policy`, `azurerm_api_management_api_operation_policy`, `azurerm_api_management_product_policy` : fix parsing resource id error (#23128)
    * fix parsing id error
    
    * update code

--- internal/services/apimanagement/api_management_api_operation_policy_resource.go
+++ internal/services/apimanagement/api_management_api_operation_policy_resource.go
@@ -39,9 +39,10 @@ func resourceApiManagementApiOperationPolicy() *pluginsdk.Resource {
 			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
 		},
 
-		SchemaVersion: 1,
+		SchemaVersion: 2,
 		StateUpgraders: pluginsdk.StateUpgrades(map[int]pluginsdk.StateUpgrade{
 			0: migration.ApiManagementApiOperationPolicyV0ToV1{},
+			1: migration.ApiManagementApiOperationPolicyV1ToV2{},
 		}),
 
 		Schema: map[string]*pluginsdk.Schema{
