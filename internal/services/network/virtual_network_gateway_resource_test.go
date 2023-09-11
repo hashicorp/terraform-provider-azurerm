7bb9aa1e13546915598093f444500ea093685e9a
neil-yechenwei <yechenwei2007@hotmail.com>
2023-09-11 08:57:36 +0800

    update code

--- internal/services/network/virtual_network_gateway_resource_test.go
+++ internal/services/network/virtual_network_gateway_resource_test.go
@@ -1788,7 +1788,7 @@ resource "azurerm_virtual_network_gateway" "test" {
   resource_group_name                   = azurerm_resource_group.test.name
   type                                  = "Vpn"
   vpn_type                              = "RouteBased"
-  sku                                   = "Basic"
+  sku                                   = "VpnGw1"
   remote_vnet_traffic_enabled           = true
   virtual_wan_traffic_enabled           = true
   bgp_route_translation_for_nat_enabled = true
@@ -1842,7 +1842,7 @@ resource "azurerm_virtual_network_gateway" "test" {
   resource_group_name                   = azurerm_resource_group.test.name
   type                                  = "Vpn"
   vpn_type                              = "RouteBased"
-  sku                                   = "Basic"
+  sku                                   = "VpnGw1"
   remote_vnet_traffic_enabled           = false
   virtual_wan_traffic_enabled           = false
   bgp_route_translation_for_nat_enabled = false
