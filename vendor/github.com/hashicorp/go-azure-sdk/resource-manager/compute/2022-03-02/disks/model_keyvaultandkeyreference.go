df1672c2765d53fed09fa18a57aa3a820f5eaa37
Zhenhua Hu <zhhu@microsoft.com>
2023-09-11 16:42:24 +0800

    go mod vendor

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2022-03-02/disks/model_keyvaultandkeyreference.go
+++
@@ -1,9 +0,0 @@
-package disks
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type KeyVaultAndKeyReference struct {
-	KeyUrl      string      `json:"keyUrl"`
-	SourceVault SourceVault `json:"sourceVault"`
-}
--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2022-03-02/disks/model_shareinfoelement.go
+++
@@ -1,8 +0,0 @@
-package disks
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type ShareInfoElement struct {
-	VMUri *string `json:"vmUri,omitempty"`
-}
