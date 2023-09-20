b15a66e835b684af9a2fe3269c6c5b69e9b20c17
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-09-20 13:19:52 +0200

    policy: adding `legacy` to the existing id parsers/validators so we can sort these in turn

--- internal/services/policy/resourceids.go
+++ internal/services/policy/resourceids.go
@@ -3,9 +3,10 @@
 
 package policy
 
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ResourceGroupAssignment -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Authorization/policyAssignments/assignment1
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=SubscriptionAssignment -id=/subscriptions/12345678-1234-9876-4563-123456789012/providers/Microsoft.Authorization/policyAssignments/assignment1
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ResourceGroupPolicyRemediation -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.PolicyInsights/remediations/remediation1
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=SubscriptionPolicyRemediation -id=/subscriptions/12345678-1234-9876-4563-123456789012/providers/Microsoft.PolicyInsights/remediations/remediation1
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ResourceGroupPolicyExemption -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Authorization/policyExemptions/exemption1
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=SubscriptionPolicyExemption -id=/subscriptions/12345678-1234-9876-4563-123456789012/providers/Microsoft.Authorization/policyExemptions/exemption1
+// @tombuildsstuff: these are intentionally disabled to aid with the migration to go-azure-sdk
+// //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ResourceGroupAssignment -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Authorization/policyAssignments/assignment1
+// //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=SubscriptionAssignment -id=/subscriptions/12345678-1234-9876-4563-123456789012/providers/Microsoft.Authorization/policyAssignments/assignment1
+// //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ResourceGroupPolicyRemediation -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.PolicyInsights/remediations/remediation1
+// //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=SubscriptionPolicyRemediation -id=/subscriptions/12345678-1234-9876-4563-123456789012/providers/Microsoft.PolicyInsights/remediations/remediation1
+// //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ResourceGroupPolicyExemption -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.Authorization/policyExemptions/exemption1
+// //go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=SubscriptionPolicyExemption -id=/subscriptions/12345678-1234-9876-4563-123456789012/providers/Microsoft.Authorization/policyExemptions/exemption1
