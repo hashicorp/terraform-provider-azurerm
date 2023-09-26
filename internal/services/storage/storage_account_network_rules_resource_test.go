eb4a195ba85cbf2db2954cfbfe2612b728651f35
magodo <wztdyl@sina.com>
2023-09-26 13:54:11 +0800

    fmt

--- internal/services/storage/storage_account_network_rules_resource_test.go
+++ internal/services/storage/storage_account_network_rules_resource_test.go
@@ -450,7 +450,7 @@ resource "azurerm_storage_account" "test" {
 }
 
 resource "azurerm_storage_account_network_rules" "test" {
-  storage_account_id = azurerm_storage_account.test.id
+  storage_account_id         = azurerm_storage_account.test.id
   default_action             = "Deny"
   ip_rules                   = ["127.0.0.1"]
   virtual_network_subnet_ids = [azurerm_subnet.test.id]
