f1125b4276ed3126d0d2567fbda4b6ce474ef566
Elena Xin <39109137+sinbai@users.noreply.github.com>
2023-10-30 15:23:49 +0800

    `azurerm_redis_enterprise_cluster` - upgrade api version and support new skus (#23708)
    * upgrade api version and support new skus
    
    * update code

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2023-10-01-preview/redisenterprise/model_clusterproperties.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2023-10-01-preview/redisenterprise/model_clusterproperties.go
@@ -4,6 +4,7 @@ package redisenterprise
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
 type ClusterProperties struct {
+	Encryption                 *ClusterPropertiesEncryption `json:"encryption,omitempty"`
 	HostName                   *string                      `json:"hostName,omitempty"`
 	MinimumTlsVersion          *TlsVersion                  `json:"minimumTlsVersion,omitempty"`
 	PrivateEndpointConnections *[]PrivateEndpointConnection `json:"privateEndpointConnections,omitempty"`
