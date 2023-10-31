b714a30da96a224c62aa17e147683bfb5774a2d4
Paulo <pmarques@microsoft.com>
2023-10-31 20:31:51 +0000

    running terrform fmt on example

--- examples/netapp/nfsv3_volume_cmk_userassigned/main.tf
+++ examples/netapp/nfsv3_volume_cmk_userassigned/main.tf
@@ -14,102 +14,102 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_virtual_network" "example" {
-	name                = "${var.prefix}-vnet"
-	location            = azurerm_resource_group.example.location
-	resource_group_name = azurerm_resource_group.example.name
-	address_space       = ["10.6.0.0/16"]
+  name                = "${var.prefix}-vnet"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.6.0.0/16"]
 }
 
 resource "azurerm_subnet" "example-delegated" {
-	name                 = "${var.prefix}-delegated-subnet"
-	resource_group_name  = azurerm_resource_group.example.name
-	virtual_network_name = azurerm_virtual_network.example.name
-	address_prefixes     = ["10.6.1.0/24"]
+  name                 = "${var.prefix}-delegated-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.6.1.0/24"]
 
-	delegation {
-		name = "exampledelegation"
+  delegation {
+    name = "exampledelegation"
 
-		service_delegation {
-		name    = "Microsoft.Netapp/volumes"
-		actions = ["Microsoft.Network/networkinterfaces/*", "Microsoft.Network/virtualNetworks/subnets/join/action"]
-		}
-	}
+    service_delegation {
+      name    = "Microsoft.Netapp/volumes"
+      actions = ["Microsoft.Network/networkinterfaces/*", "Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
 }
 
 resource "azurerm_subnet" "example-non-delegated" {
-	name                 = "${var.prefix}-non-delegated-subnet"
-	resource_group_name  = azurerm_resource_group.example.name
-	virtual_network_name = azurerm_virtual_network.example.name
-	address_prefixes     = ["10.6.0.0/24"]
+  name                 = "${var.prefix}-non-delegated-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.6.0.0/24"]
 }
 
 resource "azurerm_key_vault" "example" {
-	name                            = "${var.prefix}anfakv"
-	location                        = azurerm_resource_group.example.location
-	resource_group_name             = azurerm_resource_group.example.name
-	enabled_for_disk_encryption     = true
-	enabled_for_deployment          = true
-	enabled_for_template_deployment = true
-	purge_protection_enabled        = true
-	tenant_id                       = var.tenant_id
-  
-	sku_name = "standard"
+  name                            = "${var.prefix}anfakv"
+  location                        = azurerm_resource_group.example.location
+  resource_group_name             = azurerm_resource_group.example.name
+  enabled_for_disk_encryption     = true
+  enabled_for_deployment          = true
+  enabled_for_template_deployment = true
+  purge_protection_enabled        = true
+  tenant_id                       = var.tenant_id
 
-	access_policy {
-		tenant_id = var.tenant_id
-		object_id = data.azurerm_client_config.current.object_id
-	
-		key_permissions = [
-		  "Get",
-		  "Create",
-		  "Delete",
-		  "WrapKey",
-		  "UnwrapKey",
-		  "GetRotationPolicy",
-		  "SetRotationPolicy",
-		]
-	}
+  sku_name = "standard"
 
-	access_policy {
-		tenant_id = var.tenant_id
-		object_id = azurerm_user_assigned_identity.example.principal_id
-	
-		key_permissions = [
-		  "Get",
-		  "Encrypt",
-		  "Decrypt"
-		]
-	}
+  access_policy {
+    tenant_id = var.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+      "Create",
+      "Delete",
+      "WrapKey",
+      "UnwrapKey",
+      "GetRotationPolicy",
+      "SetRotationPolicy",
+    ]
+  }
+
+  access_policy {
+    tenant_id = var.tenant_id
+    object_id = azurerm_user_assigned_identity.example.principal_id
+
+    key_permissions = [
+      "Get",
+      "Encrypt",
+      "Decrypt"
+    ]
+  }
 }
 
 resource "azurerm_key_vault_key" "example" {
-	name         = "${var.prefix}anfenckey"
-	key_vault_id = azurerm_key_vault.example.id
-	key_type     = "RSA"
-	key_size     = 2048
-  
-	key_opts = [
-	  "decrypt",
-	  "encrypt",
-	  "sign",
-	  "unwrapKey",
-	  "verify",
-	  "wrapKey",
-	]
+  name         = "${var.prefix}anfenckey"
+  key_vault_id = azurerm_key_vault.example.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
 }
 
 resource "azurerm_private_endpoint" "example" {
-	name                   = "${var.prefix}-pe-akv"
-	location               = azurerm_resource_group.example.location
-	resource_group_name    = azurerm_resource_group.example.name
-	subnet_id              = azurerm_subnet.example-non-delegated.id
+  name                = "${var.prefix}-pe-akv"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  subnet_id           = azurerm_subnet.example-non-delegated.id
 
-	private_service_connection {
-	  name                          = "${var.prefix}-pe-sc-akv"
-	  private_connection_resource_id = azurerm_key_vault.example.id
-	  is_manual_connection          = false
-	  subresource_names             = ["Vault"]
-	}
+  private_service_connection {
+    name                           = "${var.prefix}-pe-sc-akv"
+    private_connection_resource_id = azurerm_key_vault.example.id
+    is_manual_connection           = false
+    subresource_names              = ["Vault"]
+  }
 }
 
 resource "azurerm_user_assigned_identity" "example" {
@@ -132,13 +132,13 @@ resource "azurerm_netapp_account" "example" {
 }
 
 resource "azurerm_netapp_account_encryption" "example" {
-	netapp_account_id = azurerm_netapp_account.example.id
-	
-	user_assigned_identity_id = azurerm_user_assigned_identity.example.id
+  netapp_account_id = azurerm_netapp_account.example.id
 
-	encryption {
-		key_vault_key_id = azurerm_key_vault_key.example.versionless_id
-	}
+  user_assigned_identity_id = azurerm_user_assigned_identity.example.id
+
+  encryption {
+    key_vault_key_id = azurerm_key_vault_key.example.versionless_id
+  }
 }
 
 resource "azurerm_netapp_pool" "example" {
