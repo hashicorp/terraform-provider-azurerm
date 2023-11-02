eee8e70f9a2c47908b667dc2dcee40e0ed1d5107
teowa <104055472+teowa@users.noreply.github.com>
2023-11-02 07:56:17 +0000

    fix lint

--- internal/services/network/application_gateway_resource_test.go
+++ internal/services/network/application_gateway_resource_test.go
@@ -1441,7 +1441,7 @@ resource "azurerm_application_gateway" "test" {
   name                = "acctestag-%d"
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
-  firewall_policy_id                = azurerm_web_application_firewall_policy.test.id
+  firewall_policy_id  = azurerm_web_application_firewall_policy.test.id
   sku {
     name     = "WAF_v2"
     tier     = "WAF_v2"
