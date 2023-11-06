10ec320401aa4ba06494e671b7092c5e770a087d
Heng Lu <henglu@microsoft.com>
2023-11-06 09:17:45 +0800

    sort goimports

--- internal/services/springcloud/spring_cloud_customized_accelerator_resource.go
+++ internal/services/springcloud/spring_cloud_customized_accelerator_resource.go
@@ -6,11 +6,11 @@ package springcloud
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"time"
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/appplatform/2023-09-01-preview/appplatform"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
