824137baabb0c97c3e32dceb54c0bcae3684fff6
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-11-10 09:59:22 +0100

    dependencies: vendoring `redisenterprise` @ `2023-11-01`

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2023-11-01/databases/README.md
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2023-11-01/databases/README.md
@@ -1,14 +1,14 @@
 
-## `github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2023-07-01/databases` Documentation
+## `github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2023-11-01/databases` Documentation
 
-The `databases` SDK allows for interaction with the Azure Resource Manager Service `redisenterprise` (API Version `2023-07-01`).
+The `databases` SDK allows for interaction with the Azure Resource Manager Service `redisenterprise` (API Version `2023-11-01`).
 
 This readme covers example usages, but further information on [using this SDK can be found in the project root](https://github.com/hashicorp/go-azure-sdk/tree/main/docs).
 
 ### Import Path
 
 ```go
-import "github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2023-07-01/databases"
+import "github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2023-11-01/databases"
 ```
 
 
--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2023-11-01/redisenterprise/constants.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2023-11-01/redisenterprise/constants.go
@@ -591,12 +591,9 @@ func parseResourceState(input string) (*ResourceState, error) {
 type SkuName string
 
 const (
-	SkuNameEnterpriseEFive                SkuName = "Enterprise_E5"
 	SkuNameEnterpriseEFiveZero            SkuName = "Enterprise_E50"
-	SkuNameEnterpriseEFourHundred         SkuName = "Enterprise_E400"
 	SkuNameEnterpriseEOneHundred          SkuName = "Enterprise_E100"
 	SkuNameEnterpriseEOneZero             SkuName = "Enterprise_E10"
-	SkuNameEnterpriseETwoHundred          SkuName = "Enterprise_E200"
 	SkuNameEnterpriseETwoZero             SkuName = "Enterprise_E20"
 	SkuNameEnterpriseFlashFOneFiveHundred SkuName = "EnterpriseFlash_F1500"
 	SkuNameEnterpriseFlashFSevenHundred   SkuName = "EnterpriseFlash_F700"
@@ -605,12 +602,9 @@ const (
 
 func PossibleValuesForSkuName() []string {
 	return []string{
-		string(SkuNameEnterpriseEFive),
 		string(SkuNameEnterpriseEFiveZero),
-		string(SkuNameEnterpriseEFourHundred),
 		string(SkuNameEnterpriseEOneHundred),
 		string(SkuNameEnterpriseEOneZero),
-		string(SkuNameEnterpriseETwoHundred),
 		string(SkuNameEnterpriseETwoZero),
 		string(SkuNameEnterpriseFlashFOneFiveHundred),
 		string(SkuNameEnterpriseFlashFSevenHundred),
@@ -633,12 +627,9 @@ func (s *SkuName) UnmarshalJSON(bytes []byte) error {
 
 func parseSkuName(input string) (*SkuName, error) {
 	vals := map[string]SkuName{
-		"enterprise_e5":         SkuNameEnterpriseEFive,
 		"enterprise_e50":        SkuNameEnterpriseEFiveZero,
-		"enterprise_e400":       SkuNameEnterpriseEFourHundred,
 		"enterprise_e100":       SkuNameEnterpriseEOneHundred,
 		"enterprise_e10":        SkuNameEnterpriseEOneZero,
-		"enterprise_e200":       SkuNameEnterpriseETwoHundred,
 		"enterprise_e20":        SkuNameEnterpriseETwoZero,
 		"enterpriseflash_f1500": SkuNameEnterpriseFlashFOneFiveHundred,
 		"enterpriseflash_f700":  SkuNameEnterpriseFlashFSevenHundred,
