2b96500d78e92ac0419a8748d628004733c3ff58
Jiawei Tao <jiaweitao@microsoft.com>
2023-11-10 14:44:15 +0800

    Add acc tests

--- internal/services/containerapps/container_app_job_resource_test.go
+++ internal/services/containerapps/container_app_job_resource_test.go
@@ -62,6 +62,36 @@ func TestAccContainerAppJob_eventTrigger(t *testing.T) {
 	})
 }
 
+func TestAccContainerAppJob_manualTrigger(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_container_app_job", "test")
+	r := ContainerAppJobResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.manualTrigger(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestFAccContainerAppJob_scheduleTrigger(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_container_app_job", "test")
+	r := ContainerAppJobResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.scheduleTrigger(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func (r ContainerAppJobResource) eventTrigger(data acceptance.TestData) string {
 	template := r.template(data)
 	return fmt.Sprintf(`
@@ -129,6 +159,125 @@ resource "azurerm_container_app_job" "test" {
 	  }
 	}
   }	
+}
+`, template, data.RandomInteger)
+}
+
+func (r ContainerAppJobResource) manualTrigger(data acceptance.TestData) string {
+	template := r.template(data)
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_container_app_job" "test" {
+  name = "acctest-cajob%[2]d"
+  resource_group_name = azurerm_resource_group.test.name
+  location = azurerm_resource_group.test.location
+  container_app_environment_id = azurerm_container_app_environment.test.id
+
+  configuration {
+	trigger_type = "Manual"
+	replica_timeout = 10
+	replica_retry_limit = 10
+	manual_trigger_config {
+	  parallelism = 4
+	  replica_completion_count = 1
+	}
+  }
+  
+  template {
+	containers {
+	  image = "repo/testcontainerAppsJob0:v1"
+	  name = "testcontainerappsjob0"
+	  probes {
+	    http_get {
+		  http_headers {
+		    name = "testheader"
+			value = "testvalue"
+		  }
+		  path = "/testpath"
+		  port = 8080
+		  host = "testhost"
+		  scheme = "HTTPS"
+		}
+		initial_delay_seconds = 10
+		period_seconds = 10
+		type = "Liveness"
+		failure_threshold = 1
+		success_threshold = 1
+		timeout_seconds = 1
+	  }
+	  resources {
+		cpu = 0.5
+		memory = "1Gi"
+	  }
+	}
+
+    init_containers {
+	  args = ["testarg"]
+	  command = ["testcommand"]
+	  image = "repo/testcontainerAppsJob0:v1"
+	  name = "testcontainerappsjob0"
+	  resources {
+	    cpu = 0.5
+		memory = "1Gi"
+	  }
+	}
+  }
+}
+`, template, data.RandomInteger)
+}
+
+func (r ContainerAppJobResource) scheduleTrigger(data acceptance.TestData) string {
+	template := r.template(data)
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_container_app_job" "test" {
+  name                = "acctest-cajob%[2]d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  container_app_environment_id = azurerm_container_app_environment.test.id
+
+  configuration {
+	trigger_type = "Schedule"
+	replica_timeout = 1800
+	replica_retry_limit = 0
+	schedule_trigger_config {
+	  cron_expression = "*/1 * * * *"
+	  parallelism = 1
+	  replica_completion_count = 1
+	}
+  }
+  template {
+	volumes {
+	  name = "appsettings-volume"
+	  storage_type = "EmptyDir"
+	}
+	containers {
+	  image = "repo/testcontainerAppsJob0:v1"
+	  name = "testcontainerappsjob0"
+	  probes {
+	    tcp_socket {
+		  host = "testhost"
+		  port = 8080
+		}
+		initial_delay_seconds = 5
+		timeout_seconds = 1
+		success_threshold = 1
+		failure_threshold = 1
+		period_seconds = 3
+		type = "Liveness"
+	  }
+	  resources {
+		cpu = 0.5
+		memory = "1Gi"
+	  }
+	  volume_mounts {
+		mount_path = "/appsettings"
+	    volume_name = "appsettings-volume"
+	  }
+	}
+  }
 }
 `, template, data.RandomInteger)
 }
