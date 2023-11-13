ecda019290b2aa3b138d0a1b3da371d5a7b96eff
Heng Lu <henglu@microsoft.com>
2023-11-13 12:21:52 +0800

    update

--- internal/services/springcloud/spring_cloud_container_deployment_resource_test.go
+++ internal/services/springcloud/spring_cloud_container_deployment_resource_test.go
@@ -206,8 +206,8 @@ func (SpringCloudContainerDeploymentResource) template(data acceptance.TestData)
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
+    application_insights {
+      disable_generated_rule = true
     }
   }
 }
