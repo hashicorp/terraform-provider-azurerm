fbb153a8b80cc05764bae95e207376d18a46c6ff
Heng Lu <henglu@microsoft.com>
2023-11-14 16:10:42 +0800

    update

--- internal/services/springcloud/spring_cloud_dynatrace_application_performance_monitoring_resource.go
+++ internal/services/springcloud/spring_cloud_dynatrace_application_performance_monitoring_resource.go
@@ -6,10 +6,10 @@ import (
 	"time"
 
 	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/appplatform/2023-09-01-preview/appplatform"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/services/springcloud/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
@@ -52,12 +52,7 @@ func (s SpringCloudDynatraceApplicationPerformanceMonitoringResource) Arguments(
 			ValidateFunc: validation.StringIsNotEmpty,
 		},
 
-		"spring_cloud_service_id": {
-			Type:         pluginsdk.TypeString,
-			Required:     true,
-			ForceNew:     true,
-			ValidateFunc: validate.SpringCloudServiceID,
-		},
+		"spring_cloud_service_id": commonschema.ResourceIDReferenceRequiredForceNew(appplatform.SpringId{}),
 
 		"globally_enabled": {
 			Type:     pluginsdk.TypeBool,
