563cf3f9bf1f4e653bbe0ee5c744661efb1e34e7
Mateusz Haligowski <mhaligowski@gmail.com>
2023-11-26 22:14:02 -0800

    Fix the vendor versions

--- internal/services/containers/client/client.go
+++ internal/services/containers/client/client.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/go-azure-sdk/resource-manager/containerinstance/2023-05-01/containerinstance"
 	containerregistry_v2019_06_01_preview "github.com/hashicorp/go-azure-sdk/resource-manager/containerregistry/2019-06-01-preview"
 	containerregistry_v2021_08_01_preview "github.com/hashicorp/go-azure-sdk/resource-manager/containerregistry/2021-08-01-preview"
+	containerregistry_v2023_07_01 "github.com/hashicorp/go-azure-sdk/resource-manager/containerregistry/2023-07-01"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/containerregistry/2023-07-01/cacherules"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2019-08-01/containerservices"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2023-04-02-preview/agentpools"
@@ -24,8 +25,8 @@ import (
 
 type Client struct {
 	AgentPoolsClient                            *agentpools.AgentPoolsClient
-	CacheRulesClient                            *cacherules.CacheRulesClient
 	ContainerInstanceClient                     *containerinstance.ContainerInstanceClient
+	ContainerRegistryClient_v2023_07_01         *containerregistry_v2023_07_01.Client
 	ContainerRegistryClient_v2021_08_01_preview *containerregistry_v2021_08_01_preview.Client
 	// v2019_06_01_preview is needed for container registry agent pools and tasks
 	ContainerRegistryClient_v2019_06_01_preview *containerregistry_v2019_06_01_preview.Client
@@ -62,6 +63,13 @@ func NewContainersClient(o *common.ClientOptions) (*Client, error) {
 		return nil, err
 	}
 
+	containerRegistryClient_v2023_07_01, err := containerregistry_v2023_07_01.NewClientWithBaseURI(o.Environment.ResourceManager, func(c *resourcemanager.Client) {
+		o.Configure(c, o.Authorizers.ResourceManager)
+	})
+	if err != nil {
+		return nil, err
+	}
+
 	// AKS
 	kubernetesClustersClient := managedclusters.NewManagedClustersClientWithBaseURI(o.ResourceManagerEndpoint)
 	o.ConfigureClient(&kubernetesClustersClient.Client, o.ResourceManagerAuthorizer)
@@ -92,8 +100,8 @@ func NewContainersClient(o *common.ClientOptions) (*Client, error) {
 
 	return &Client{
 		AgentPoolsClient:                            &agentPoolsClient,
-		CacheRulesClient:                            cacheRulesClient,
 		ContainerInstanceClient:                     &containerInstanceClient,
+		containerRegistryClient_v2023_07_01:         containerRegistryClient_v2023_07_01,
 		ContainerRegistryClient_v2021_08_01_preview: containerRegistryClient_v2021_08_01_preview,
 		ContainerRegistryClient_v2019_06_01_preview: containerRegistryClient_v2019_06_01_preview,
 		KubernetesClustersClient:                    &kubernetesClustersClient,
