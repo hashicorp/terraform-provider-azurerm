3c2b97a002d60e3da9504ebbe706e4b6b5236435
stephybun <steph@hashicorp.com>
2023-12-04 13:43:37 +0100

    update elastic san sdk (#24108)

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/elasticsan/2023-01-01/elasticsans/id_elasticsan.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/elasticsan/2023-01-01/elasticsans/id_elasticsan.go
@@ -36,19 +36,9 @@ func ParseElasticSanID(input string) (*ElasticSanId, error) {
 		return nil, fmt.Errorf("parsing %q: %+v", input, err)
 	}
 
-	var ok bool
 	id := ElasticSanId{}
-
-	if id.SubscriptionId, ok = parsed.Parsed["subscriptionId"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "subscriptionId", *parsed)
-	}
-
-	if id.ResourceGroupName, ok = parsed.Parsed["resourceGroupName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "resourceGroupName", *parsed)
-	}
-
-	if id.ElasticSanName, ok = parsed.Parsed["elasticSanName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "elasticSanName", *parsed)
+	if err := id.FromParseResult(*parsed); err != nil {
+		return nil, err
 	}
 
 	return &id, nil
@@ -63,22 +53,30 @@ func ParseElasticSanIDInsensitively(input string) (*ElasticSanId, error) {
 		return nil, fmt.Errorf("parsing %q: %+v", input, err)
 	}
 
-	var ok bool
 	id := ElasticSanId{}
-
-	if id.SubscriptionId, ok = parsed.Parsed["subscriptionId"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "subscriptionId", *parsed)
-	}
-
-	if id.ResourceGroupName, ok = parsed.Parsed["resourceGroupName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "resourceGroupName", *parsed)
-	}
-
-	if id.ElasticSanName, ok = parsed.Parsed["elasticSanName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "elasticSanName", *parsed)
+	if err := id.FromParseResult(*parsed); err != nil {
+		return nil, err
 	}
 
 	return &id, nil
+}
+
+func (id *ElasticSanId) FromParseResult(input resourceids.ParseResult) error {
+	var ok bool
+
+	if id.SubscriptionId, ok = input.Parsed["subscriptionId"]; !ok {
+		return resourceids.NewSegmentNotSpecifiedError(id, "subscriptionId", input)
+	}
+
+	if id.ResourceGroupName, ok = input.Parsed["resourceGroupName"]; !ok {
+		return resourceids.NewSegmentNotSpecifiedError(id, "resourceGroupName", input)
+	}
+
+	if id.ElasticSanName, ok = input.Parsed["elasticSanName"]; !ok {
+		return resourceids.NewSegmentNotSpecifiedError(id, "elasticSanName", input)
+	}
+
+	return nil
 }
 
 // ValidateElasticSanID checks that 'input' can be parsed as a Elastic San ID
