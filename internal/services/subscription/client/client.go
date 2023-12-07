8a66536e0aa8a817bbaf4a5bea4643220cd197e4
Tom Harvey <tombuildsstuff@users.noreply.github.com>
2023-12-07 06:28:12 +0100

    r/subscription: consolidating the SDK clients onto `hashicorp/go-azure-sdk` (#24003)

--- internal/services/subscription/client/client.go
+++ internal/services/subscription/client/client.go
@@ -5,15 +5,13 @@ package client
 
 import (
 	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2021-01-01/subscriptions"                                // nolint: staticcheck
-	subscriptionAlias "github.com/Azure/azure-sdk-for-go/services/subscription/mgmt/2020-09-01/subscription"            // nolint: staticcheck
 	subscriptionAliasPandora "github.com/hashicorp/go-azure-sdk/resource-manager/subscription/2021-10-01/subscriptions" // nolint: staticcheck
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
 
 type Client struct {
-	Client             *subscriptions.Client
-	AliasClient        *subscriptionAliasPandora.SubscriptionsClient
-	SubscriptionClient *subscriptionAlias.Client
+	Client      *subscriptions.Client
+	AliasClient *subscriptionAliasPandora.SubscriptionsClient
 }
 
 func NewClient(o *common.ClientOptions) *Client {
@@ -23,12 +21,8 @@ func NewClient(o *common.ClientOptions) *Client {
 	aliasClient := subscriptionAliasPandora.NewSubscriptionsClientWithBaseURI(o.ResourceManagerEndpoint)
 	o.ConfigureClient(&aliasClient.Client, o.ResourceManagerAuthorizer)
 
-	subscriptionClient := subscriptionAlias.NewClientWithBaseURI(o.ResourceManagerEndpoint)
-	o.ConfigureClient(&subscriptionClient.Client, o.ResourceManagerAuthorizer)
-
 	return &Client{
-		AliasClient:        &aliasClient,
-		Client:             &client,
-		SubscriptionClient: &subscriptionClient,
+		AliasClient: &aliasClient,
+		Client:      &client,
 	}
 }
