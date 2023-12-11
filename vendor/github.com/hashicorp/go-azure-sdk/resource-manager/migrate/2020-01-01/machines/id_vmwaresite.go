1cb83012123b2279b74119376eee332e3154d09c
ziyeqf <51212351+ziyeqf@users.noreply.github.com>
2023-12-11 16:07:22 +0800

    refresh vendor

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/migrate/2020-01-01/machines/id_vmwaresite.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/migrate/2020-01-01/machines/id_vmwaresite.go
@@ -36,19 +36,9 @@ func ParseVMwareSiteID(input string) (*VMwareSiteId, error) {
 		return nil, fmt.Errorf("parsing %q: %+v", input, err)
 	}
 
-	var ok bool
 	id := VMwareSiteId{}
-
-	if id.SubscriptionId, ok = parsed.Parsed["subscriptionId"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "subscriptionId", *parsed)
-	}
-
-	if id.ResourceGroupName, ok = parsed.Parsed["resourceGroupName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "resourceGroupName", *parsed)
-	}
-
-	if id.VmwareSiteName, ok = parsed.Parsed["vmwareSiteName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "vmwareSiteName", *parsed)
+	if err := id.FromParseResult(*parsed); err != nil {
+		return nil, err
 	}
 
 	return &id, nil
@@ -63,22 +53,30 @@ func ParseVMwareSiteIDInsensitively(input string) (*VMwareSiteId, error) {
 		return nil, fmt.Errorf("parsing %q: %+v", input, err)
 	}
 
-	var ok bool
 	id := VMwareSiteId{}
-
-	if id.SubscriptionId, ok = parsed.Parsed["subscriptionId"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "subscriptionId", *parsed)
-	}
-
-	if id.ResourceGroupName, ok = parsed.Parsed["resourceGroupName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "resourceGroupName", *parsed)
-	}
-
-	if id.VmwareSiteName, ok = parsed.Parsed["vmwareSiteName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "vmwareSiteName", *parsed)
+	if err := id.FromParseResult(*parsed); err != nil {
+		return nil, err
 	}
 
 	return &id, nil
+}
+
+func (id *VMwareSiteId) FromParseResult(input resourceids.ParseResult) error {
+	var ok bool
+
+	if id.SubscriptionId, ok = input.Parsed["subscriptionId"]; !ok {
+		return resourceids.NewSegmentNotSpecifiedError(id, "subscriptionId", input)
+	}
+
+	if id.ResourceGroupName, ok = input.Parsed["resourceGroupName"]; !ok {
+		return resourceids.NewSegmentNotSpecifiedError(id, "resourceGroupName", input)
+	}
+
+	if id.VmwareSiteName, ok = input.Parsed["vmwareSiteName"]; !ok {
+		return resourceids.NewSegmentNotSpecifiedError(id, "vmwareSiteName", input)
+	}
+
+	return nil
 }
 
 // ValidateVMwareSiteID checks that 'input' can be parsed as a V Mware Site ID
