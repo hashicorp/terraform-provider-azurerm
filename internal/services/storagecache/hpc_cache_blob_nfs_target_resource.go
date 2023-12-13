d7fa4d310f085090e2d21503cd6acbb6e11296f0
neil-yechenwei <yechenwei2007@hotmail.com>
2023-12-13 09:23:14 +0800

    update code

--- internal/services/storagecache/hpc_cache_blob_nfs_target_resource.go
+++ internal/services/storagecache/hpc_cache_blob_nfs_target_resource.go
@@ -5,7 +5,6 @@ package storagecache
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-provider-azurerm/utils"
 	"log"
 	"time"
 
@@ -20,6 +19,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
 func resourceHPCCacheBlobNFSTarget() *pluginsdk.Resource {
