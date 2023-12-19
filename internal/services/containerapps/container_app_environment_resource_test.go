0a331e00639411e18bbf1abf0004c99287436a57
jiawei <jiaweitao@microsoft.com>
2023-12-19 06:53:03 +0000

    use vnet template

--- internal/services/containerapps/container_app_environment_resource_test.go
+++ internal/services/containerapps/container_app_environment_resource_test.go
@@ -254,27 +254,6 @@ provider "azurerm" {
 
 %[1]s
 
-resource "azurerm_virtual_network" "test" {
-  name                = "acctestvirtnet%[2]d"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-}
-
-resource "azurerm_subnet" "control" {
-  name                 = "control-plane"
-  resource_group_name  = azurerm_resource_group.test.name
-  virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["10.0.0.0/23"]
-  delegation {
-    name = "acctestdelegation%[2]d"
-    service_delegation {
-      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
-      name    = "Microsoft.App/environments"
-    }
-  }
-}
-
 resource "azurerm_container_app_environment" "test" {
   name                     = "acctest-CAEnv%[2]d"
   resource_group_name      = azurerm_resource_group.test.name
@@ -288,7 +267,7 @@ resource "azurerm_container_app_environment" "test" {
     secret = "sauce"
   }
 }
-`, r.template(data), data.RandomInteger)
+`, r.templateVNet(data), data.RandomInteger)
 }
 
 func (r ContainerAppEnvironmentResource) completeWithWorkloadProfile(data acceptance.TestData) string {
