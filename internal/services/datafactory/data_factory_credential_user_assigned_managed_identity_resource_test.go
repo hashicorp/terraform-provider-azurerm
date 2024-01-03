1826bc1314acc4a9fc47fe10d813e348a5084201
Bruce Harrison <bruce.harrison@hashicorp.com>
2024-01-03 14:01:54 -0600

    add test for resource update

--- internal/services/datafactory/data_factory_credential_user_assigned_managed_identity_resource_test.go
+++ internal/services/datafactory/data_factory_credential_user_assigned_managed_identity_resource_test.go
@@ -25,10 +25,34 @@ func TestAccDataFactoryCredentialUserAssignedManagedIdentity_basic(t *testing.T)
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 				check.That(data.ResourceName).Key("annotations.#").HasValue("1"),
-				check.That(data.ResourceName).Key("description").IsNotEmpty(),
+				check.That(data.ResourceName).Key("description").HasValue("ORIGINAL DESCRIPTION"),
 			),
 		},
 		data.ImportStep(),
+	})
+}
+
+func TestAccDataFactoryCredentialUserAssignedManagedIdentity_update(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_data_factory_credential_user_managed_identity", "test")
+	r := CredentialUserAssignedManagedIdentityResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("annotations.#").HasValue("1"),
+				check.That(data.ResourceName).Key("description").HasValue("ORIGINAL DESCRIPTION"),
+			),
+		},
+		{
+			Config: r.update(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("annotations.#").HasValue("2"),
+				check.That(data.ResourceName).Key("description").HasValue("UPDATED DESCRIPTION"),
+			),
+		},
 	})
 }
 
@@ -46,7 +70,7 @@ func (t CredentialUserAssignedManagedIdentityResource) Exists(ctx context.Contex
 	return utils.Bool(resp.Model.Id != nil), nil
 }
 
-func (CredentialUserAssignedManagedIdentityResource) basic(data acceptance.TestData) string {
+func templateBase(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {}
@@ -72,14 +96,37 @@ resource "azurerm_data_factory" "test" {
     type         = "UserAssigned"
     identity_ids = [azurerm_user_assigned_identity.test.id]
   }
+}`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
 }
+
+func (r CredentialUserAssignedManagedIdentityResource) basic(data acceptance.TestData) string {
+	base := templateBase(data)
+
+	return fmt.Sprintf(`
+%s
 
 resource "azurerm_data_factory_credential_user_managed_identity" "test" {
   name            = "credential%d"
-  description     = "%s"
+  description     = "ORIGINAL DESCRIPTION"
   data_factory_id = azurerm_data_factory.test.id
   identity_id     = azurerm_user_assigned_identity.test.id
-  annotations     = ["%s"]
+  annotations     = ["1"]
 }
-`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger, data.RandomInteger, data.RandomString, data.RandomString)
+`, base, data.RandomInteger)
+}
+
+func (r CredentialUserAssignedManagedIdentityResource) update(data acceptance.TestData) string {
+	base := templateBase(data)
+
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_data_factory_credential_user_managed_identity" "test" {
+  name            = "credential%d"
+  description     = "UPDATED DESCRIPTION"
+  data_factory_id = azurerm_data_factory.test.id
+  identity_id     = azurerm_user_assigned_identity.test.id
+  annotations     = ["1", "2"]
+}
+`, base, data.RandomInteger)
 }
