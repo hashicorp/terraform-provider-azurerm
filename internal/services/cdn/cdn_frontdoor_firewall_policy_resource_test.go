76c808bd837a3414420e62fd3697c458b7f5ccef
Harshavardhan Musanalli <10049720+harshavmb@users.noreply.github.com>
2024-01-05 15:41:44 +0100

    #24401: Support request_body_check part of request_body_check

--- internal/services/cdn/cdn_frontdoor_firewall_policy_resource_test.go
+++ internal/services/cdn/cdn_frontdoor_firewall_policy_resource_test.go
@@ -56,6 +56,7 @@ func TestAccCdnFrontDoorFirewallPolicy_update(t *testing.T) {
 			Config: r.update(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("request_body_check").HasValue("false"),
 			),
 		},
 		data.ImportStep(),
@@ -333,6 +334,7 @@ resource "azurerm_cdn_frontdoor_firewall_policy" "test" {
   custom_rule {
     name                           = "Rule1"
     enabled                        = true
+    request_body_check             = false
     priority                       = 1
     rate_limit_duration_in_minutes = 1
     rate_limit_threshold           = 10
