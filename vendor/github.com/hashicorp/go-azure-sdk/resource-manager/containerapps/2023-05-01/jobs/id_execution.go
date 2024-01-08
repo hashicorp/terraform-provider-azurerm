599685c3b6cf57f39d212f6256c73cf0e1cf6af0
jiawei <jiaweitao@microsoft.com>
2024-01-08 07:58:51 +0000

    Add support for init_container

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/containerapps/2023-05-01/jobs/id_execution.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/containerapps/2023-05-01/jobs/id_execution.go
@@ -38,23 +38,9 @@ func ParseExecutionID(input string) (*ExecutionId, error) {
 		return nil, fmt.Errorf("parsing %q: %+v", input, err)
 	}
 
-	var ok bool
 	id := ExecutionId{}
-
-	if id.SubscriptionId, ok = parsed.Parsed["subscriptionId"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "subscriptionId", *parsed)
-	}
-
-	if id.ResourceGroupName, ok = parsed.Parsed["resourceGroupName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "resourceGroupName", *parsed)
-	}
-
-	if id.JobName, ok = parsed.Parsed["jobName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "jobName", *parsed)
-	}
-
-	if id.ExecutionName, ok = parsed.Parsed["executionName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "executionName", *parsed)
+	if err := id.FromParseResult(*parsed); err != nil {
+		return nil, err
 	}
 
 	return &id, nil
@@ -69,26 +55,34 @@ func ParseExecutionIDInsensitively(input string) (*ExecutionId, error) {
 		return nil, fmt.Errorf("parsing %q: %+v", input, err)
 	}
 
-	var ok bool
 	id := ExecutionId{}
-
-	if id.SubscriptionId, ok = parsed.Parsed["subscriptionId"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "subscriptionId", *parsed)
-	}
-
-	if id.ResourceGroupName, ok = parsed.Parsed["resourceGroupName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "resourceGroupName", *parsed)
-	}
-
-	if id.JobName, ok = parsed.Parsed["jobName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "jobName", *parsed)
-	}
-
-	if id.ExecutionName, ok = parsed.Parsed["executionName"]; !ok {
-		return nil, resourceids.NewSegmentNotSpecifiedError(id, "executionName", *parsed)
+	if err := id.FromParseResult(*parsed); err != nil {
+		return nil, err
 	}
 
 	return &id, nil
+}
+
+func (id *ExecutionId) FromParseResult(input resourceids.ParseResult) error {
+	var ok bool
+
+	if id.SubscriptionId, ok = input.Parsed["subscriptionId"]; !ok {
+		return resourceids.NewSegmentNotSpecifiedError(id, "subscriptionId", input)
+	}
+
+	if id.ResourceGroupName, ok = input.Parsed["resourceGroupName"]; !ok {
+		return resourceids.NewSegmentNotSpecifiedError(id, "resourceGroupName", input)
+	}
+
+	if id.JobName, ok = input.Parsed["jobName"]; !ok {
+		return resourceids.NewSegmentNotSpecifiedError(id, "jobName", input)
+	}
+
+	if id.ExecutionName, ok = input.Parsed["executionName"]; !ok {
+		return resourceids.NewSegmentNotSpecifiedError(id, "executionName", input)
+	}
+
+	return nil
 }
 
 // ValidateExecutionID checks that 'input' can be parsed as a Execution ID
--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/containerapps/2023-05-01/jobs/predicates.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/containerapps/2023-05-01/jobs/predicates.go
@@ -32,28 +32,18 @@ func (p JobOperationPredicate) Matches(input Job) bool {
 }
 
 type JobExecutionOperationPredicate struct {
-	EndTime   *string
-	Id        *string
-	Name      *string
-	StartTime *string
-	Type      *string
+	Id   *string
+	Name *string
+	Type *string
 }
 
 func (p JobExecutionOperationPredicate) Matches(input JobExecution) bool {
-
-	if p.EndTime != nil && (input.EndTime == nil || *p.EndTime != *input.EndTime) {
-		return false
-	}
 
 	if p.Id != nil && (input.Id == nil || *p.Id != *input.Id) {
 		return false
 	}
 
 	if p.Name != nil && (input.Name == nil || *p.Name != *input.Name) {
-		return false
-	}
-
-	if p.StartTime != nil && (input.StartTime == nil || *p.StartTime != *input.StartTime) {
 		return false
 	}
 
