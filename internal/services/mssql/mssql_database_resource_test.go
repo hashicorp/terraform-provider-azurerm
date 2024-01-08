a93a56bcd278079fe45d4e04591618133fcaec79
Denis Kuzmenok <denis.kuzmenok@ubs.com>
2024-01-08 11:38:15 +0100

    Fixed tests

--- internal/services/mssql/mssql_database_resource_test.go
+++ internal/services/mssql/mssql_database_resource_test.go
@@ -980,14 +980,6 @@ func (r MsSqlDatabaseResource) complete(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 %[1]s
 
-data "azurerm_client_config" "test" {}
-
-resource "azurerm_user_assigned_identity" "test" {
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-  name                = "test_identity_2112"
-}
-
 resource "azurerm_mssql_database" "test" {
   name         = "acctest-db-%[2]d"
   server_id    = azurerm_mssql_server.test.id
@@ -1001,54 +993,9 @@ resource "azurerm_mssql_database" "test" {
   maintenance_configuration_name = "%[3]s"
   storage_account_type           = "Local"
 
-  identity {
-    type         = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.test.id]
-  }
-
-  transparent_data_encryption_key_vault_key_id = azurerm_key_vault_key.test.id
-  auto_key_rotation_enabled                    = true
-
   tags = {
     ENV = "Test"
   }
-}
-
-resource "azurerm_key_vault" "test" {
-  name                        = "vault%[2]d"
-  location                    = azurerm_resource_group.test.location
-  resource_group_name         = azurerm_resource_group.test.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = azurerm_user_assigned_identity.test.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = true
-
-  sku_name = "standard"
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.test.tenant_id
-    object_id = data.azurerm_client_config.test.object_id
-
-    key_permissions = ["Get", "List", "Create", "Delete", "Update", "Recover", "Purge", "GetRotationPolicy"]
-  }
-
-  access_policy {
-    tenant_id = azurerm_user_assigned_identity.test.tenant_id
-    object_id = azurerm_user_assigned_identity.test.principal_id
-
-    key_permissions = ["Get", "WrapKey", "UnwrapKey"]
-  }
-}
-
-resource "azurerm_key_vault_key" "test" {
-  depends_on = [azurerm_key_vault.test]
-
-  name         = "key-%[4]s"
-  key_vault_id = azurerm_key_vault.test.id
-  key_type     = "RSA"
-  key_size     = 2048
-
-  key_opts = ["unwrapKey", "wrapKey"]
 }
 `, r.template(data), data.RandomInteger, configName, data.RandomString)
 }
