8cb0df4a43adaf555edba5f12a7aa87dd83bf1fd
jackofallops <ste@hashicorp.com>
2024-01-12 14:56:08 +0100

    go mod

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/web/2023-01-01/appserviceenvironments/model_resourcemetricavailability.go
+++
@@ -1,9 +0,0 @@
-package appserviceenvironments
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type ResourceMetricAvailability struct {
-	Retention *string `json:"retention,omitempty"`
-	TimeGrain *string `json:"timeGrain,omitempty"`
-}
--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/web/2023-01-01/webapps/model_nonce.go
+++
@@ -1,9 +0,0 @@
-package webapps
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type Nonce struct {
-	NonceExpirationInterval *string `json:"nonceExpirationInterval,omitempty"`
-	ValidateNonce           *bool   `json:"validateNonce,omitempty"`
-}
