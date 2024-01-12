dd1bbef6fc2a8bd75d43dab230a125581ad71961
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-01-12 11:15:30 +0100

    dependencies: updating to `v0.20240112.1095456` of `github.com/hashicorp/go-azure-sdk`

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/kusto/2023-08-15/scripts/constants.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/kusto/2023-08-15/scripts/constants.go
@@ -1,6 +1,10 @@
 package scripts
 
-import "strings"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
@@ -27,6 +31,19 @@ func PossibleValuesForProvisioningState() []string {
 		string(ProvisioningStateRunning),
 		string(ProvisioningStateSucceeded),
 	}
+}
+
+func (s *ProvisioningState) UnmarshalJSON(bytes []byte) error {
+	var decoded string
+	if err := json.Unmarshal(bytes, &decoded); err != nil {
+		return fmt.Errorf("unmarshaling: %+v", err)
+	}
+	out, err := parseProvisioningState(decoded)
+	if err != nil {
+		return fmt.Errorf("parsing %q: %+v", decoded, err)
+	}
+	*s = *out
+	return nil
 }
 
 func parseProvisioningState(input string) (*ProvisioningState, error) {
@@ -62,6 +79,19 @@ func PossibleValuesForReason() []string {
 	}
 }
 
+func (s *Reason) UnmarshalJSON(bytes []byte) error {
+	var decoded string
+	if err := json.Unmarshal(bytes, &decoded); err != nil {
+		return fmt.Errorf("unmarshaling: %+v", err)
+	}
+	out, err := parseReason(decoded)
+	if err != nil {
+		return fmt.Errorf("parsing %q: %+v", decoded, err)
+	}
+	*s = *out
+	return nil
+}
+
 func parseReason(input string) (*Reason, error) {
 	vals := map[string]Reason{
 		"alreadyexists": ReasonAlreadyExists,
@@ -86,6 +116,19 @@ func PossibleValuesForScriptType() []string {
 	return []string{
 		string(ScriptTypeMicrosoftPointKustoClustersDatabasesScripts),
 	}
+}
+
+func (s *ScriptType) UnmarshalJSON(bytes []byte) error {
+	var decoded string
+	if err := json.Unmarshal(bytes, &decoded); err != nil {
+		return fmt.Errorf("unmarshaling: %+v", err)
+	}
+	out, err := parseScriptType(decoded)
+	if err != nil {
+		return fmt.Errorf("parsing %q: %+v", decoded, err)
+	}
+	*s = *out
+	return nil
 }
 
 func parseScriptType(input string) (*ScriptType, error) {
