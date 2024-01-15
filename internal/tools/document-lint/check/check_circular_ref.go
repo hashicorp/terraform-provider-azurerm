4ec5d527ad32451b659cf476d50b39824ed4ba56
Xu Wu <xuwu1@microsoft.com>
2024-01-16 03:31:20 +0800

    Docs: update document linter tool for partial force new and circular reference (#24475)
    * update document linter tool for part forcenew and circular reference in document
    
    * fix some documents

---
+++ internal/tools/document-lint/check/check_circular_ref.go
@@ -0,0 +1,41 @@
+package check
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tools/document-lint/model"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tools/document-lint/util"
+)
+
+type circularRef struct {
+	checkBase
+	fieldName string
+	md        *model.ResourceDoc
+}
+
+func newCircularRef(name string, md *model.ResourceDoc) *circularRef {
+	base := newCheckBase(0, "", nil)
+	return &circularRef{
+		checkBase: base,
+		fieldName: name,
+		md:        md,
+	}
+}
+
+// Fix implements Checker.
+func (*circularRef) Fix(line string) (result string, err error) {
+	return line, nil
+}
+
+// Key implements Checker.
+// ShouldSkip implements Checker.
+func (*circularRef) ShouldSkip() bool {
+	return false
+}
+
+// String implements Checker.
+func (c *circularRef) String() string {
+	return fmt.Sprintf("0 document has circular reference in block name: %s", util.Bold(c.fieldName))
+}
+
+var _ Checker = (*circularRef)(nil)
