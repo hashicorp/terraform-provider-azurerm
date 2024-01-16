f3a692b80344d7ce1fa6d0249e8e6bbeb9d1585b
Zhenhua Hu <zhhu@microsoft.com>
2024-01-16 14:50:04 +0800

    add code (#24461)

--- internal/services/compute/windows_virtual_machine_scale_set_resource_network_test.go
+++ internal/services/compute/windows_virtual_machine_scale_set_resource_network_test.go
@@ -639,6 +639,10 @@ resource "azurerm_application_security_group" "test" {
   name                = "acctestasg-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "azurerm_windows_virtual_machine_scale_set" "test" {
@@ -685,12 +689,20 @@ resource "azurerm_application_security_group" "test" {
   name                = "acctestasg-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "azurerm_application_security_group" "other" {
   name                = "acctestasg2-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "azurerm_windows_virtual_machine_scale_set" "test" {
