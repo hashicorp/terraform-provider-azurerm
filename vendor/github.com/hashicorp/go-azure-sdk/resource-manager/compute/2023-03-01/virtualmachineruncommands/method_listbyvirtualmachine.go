78d565f4ebfd512408ec3260d79514c0c92d8e0e
hc-github-team-tf-azure <>
2024-01-18 08:34:47 +0000

    Updating based on 4b2af46dd

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2023-03-01/virtualmachineruncommands/method_listbyvirtualmachine.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2023-03-01/virtualmachineruncommands/method_listbyvirtualmachine.go
@@ -19,7 +19,8 @@ type ListByVirtualMachineOperationResponse struct {
 }
 
 type ListByVirtualMachineCompleteResult struct {
-	Items []VirtualMachineRunCommand
+	LatestHttpResponse *http.Response
+	Items              []VirtualMachineRunCommand
 }
 
 type ListByVirtualMachineOperationOptions struct {
@@ -111,7 +112,8 @@ func (c VirtualMachineRunCommandsClient) ListByVirtualMachineCompleteMatchingPre
 	}
 
 	result = ListByVirtualMachineCompleteResult{
-		Items: items,
+		LatestHttpResponse: resp.HttpResponse,
+		Items:              items,
 	}
 	return
 }
