a7f1eb54844e3d8a8efa3129167636205f398964
Matthew <mbfrahry@gmail.com>
2024-01-24 14:33:27 -0800

    go mod vendor

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/streamanalytics/2021-10-01-preview/streamingjobs/method_createorreplace_autorest.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/streamanalytics/2021-10-01-preview/streamingjobs/method_createorreplace_autorest.go
@@ -16,6 +16,7 @@ import (
 type CreateOrReplaceOperationResponse struct {
 	Poller       polling.LongRunningPoller
 	HttpResponse *http.Response
+	Model        *StreamingJob
 }
 
 type CreateOrReplaceOperationOptions struct {
