70a709dfcde7e1995fca9040a0ebb6b4416d3ff8
hc-github-team-tf-azure <>
2024-01-25 17:32:17 +0000

    dependencies: updating `nginx` to API Version "2023-09-01" from "2023-04-01"

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/nginx/2023-09-01/nginxcertificate/model_nginxcertificateerrorresponsebody.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/nginx/2023-09-01/nginxcertificate/model_nginxcertificateerrorresponsebody.go
@@ -1,12 +1,9 @@
 package nginxcertificate
 
-import "fmt"
-
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-const defaultApiVersion = "2023-04-01"
-
-func userAgent() string {
-	return fmt.Sprintf("hashicorp/go-azure-sdk/nginxcertificate/%s", defaultApiVersion)
+type NginxCertificateErrorResponseBody struct {
+	Code    *string `json:"code,omitempty"`
+	Message *string `json:"message,omitempty"`
 }
