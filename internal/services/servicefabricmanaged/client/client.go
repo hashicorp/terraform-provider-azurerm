c44fb6b49d293d7bcae3116f689997b5b582f635
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-01-26 13:01:08 +0100

    linting

--- internal/services/servicefabricmanaged/client/client.go
+++ internal/services/servicefabricmanaged/client/client.go
@@ -5,6 +5,7 @@ package client
 
 import (
 	"fmt"
+
 	"github.com/hashicorp/go-azure-sdk/resource-manager/servicefabricmanagedcluster/2021-05-01/managedcluster"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/servicefabricmanagedcluster/2021-05-01/nodetype"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
