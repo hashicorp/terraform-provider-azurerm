df4d8bb45b4b393928d5a614088d9504e036b156
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-01-26 16:43:50 +0100

    `subscriptions`: updating the aliases

--- internal/services/subscription/client/client.go
+++ internal/services/subscription/client/client.go
@@ -6,21 +6,21 @@ package client
 import (
 	"fmt"
 
-	resourceManager "github.com/hashicorp/go-azure-sdk/resource-manager/resources/2022-12-01/subscriptions"
-	subscriptionAliasPandora "github.com/hashicorp/go-azure-sdk/resource-manager/subscription/2021-10-01/subscriptions" // nolint: staticcheck
+	"github.com/hashicorp/go-azure-sdk/resource-manager/resources/2022-12-01/subscriptions"
+	subscriptionAlias "github.com/hashicorp/go-azure-sdk/resource-manager/subscription/2021-10-01/subscriptions"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
 
 type Client struct {
-	AliasClient         *subscriptionAliasPandora.SubscriptionsClient
-	SubscriptionsClient *resourceManager.SubscriptionsClient
+	AliasClient         *subscriptionAlias.SubscriptionsClient
+	SubscriptionsClient *subscriptions.SubscriptionsClient
 }
 
 func NewClient(o *common.ClientOptions) (*Client, error) {
-	aliasClient := subscriptionAliasPandora.NewSubscriptionsClientWithBaseURI(o.ResourceManagerEndpoint)
+	aliasClient := subscriptionAlias.NewSubscriptionsClientWithBaseURI(o.ResourceManagerEndpoint)
 	o.ConfigureClient(&aliasClient.Client, o.ResourceManagerAuthorizer)
 
-	subscriptionsClient, err := resourceManager.NewSubscriptionsClientWithBaseURI(o.Environment.ResourceManager)
+	subscriptionsClient, err := subscriptions.NewSubscriptionsClientWithBaseURI(o.Environment.ResourceManager)
 	if err != nil {
 		return nil, fmt.Errorf("building Subscriptions client: %+v", err)
 	}
