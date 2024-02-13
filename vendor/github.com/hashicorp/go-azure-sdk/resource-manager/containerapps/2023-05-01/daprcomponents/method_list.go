32dfd7015f064d76a60d67a6e0dfcdfc1fbaf451
catriona-m <86247157+catriona-m@users.noreply.github.com>
2024-02-13 12:12:19 +0000

    dependencies: updating to v0.20240212.1141705 of `github.com/hashicorp/go-azure-sdk` (#24862)
    * update go-azure-sdk to v0.20240212.1141705
    
    * update go-azure-sdk to v0.20240212.1141705
    
    * sort imports

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/containerapps/2023-05-01/daprcomponents/method_list.go
@@ -0,0 +1,91 @@
+package daprcomponents
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/hashicorp/go-azure-sdk/sdk/client"
+	"github.com/hashicorp/go-azure-sdk/sdk/odata"
+)
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type ListOperationResponse struct {
+	HttpResponse *http.Response
+	OData        *odata.OData
+	Model        *[]DaprComponent
+}
+
+type ListCompleteResult struct {
+	LatestHttpResponse *http.Response
+	Items              []DaprComponent
+}
+
+// List ...
+func (c DaprComponentsClient) List(ctx context.Context, id ManagedEnvironmentId) (result ListOperationResponse, err error) {
+	opts := client.RequestOptions{
+		ContentType: "application/json; charset=utf-8",
+		ExpectedStatusCodes: []int{
+			http.StatusOK,
+		},
+		HttpMethod: http.MethodGet,
+		Path:       fmt.Sprintf("%s/daprComponents", id.ID()),
+	}
+
+	req, err := c.Client.NewRequest(ctx, opts)
+	if err != nil {
+		return
+	}
+
+	var resp *client.Response
+	resp, err = req.ExecutePaged(ctx)
+	if resp != nil {
+		result.OData = resp.OData
+		result.HttpResponse = resp.Response
+	}
+	if err != nil {
+		return
+	}
+
+	var values struct {
+		Values *[]DaprComponent `json:"value"`
+	}
+	if err = resp.Unmarshal(&values); err != nil {
+		return
+	}
+
+	result.Model = values.Values
+
+	return
+}
+
+// ListComplete retrieves all the results into a single object
+func (c DaprComponentsClient) ListComplete(ctx context.Context, id ManagedEnvironmentId) (ListCompleteResult, error) {
+	return c.ListCompleteMatchingPredicate(ctx, id, DaprComponentOperationPredicate{})
+}
+
+// ListCompleteMatchingPredicate retrieves all the results and then applies the predicate
+func (c DaprComponentsClient) ListCompleteMatchingPredicate(ctx context.Context, id ManagedEnvironmentId, predicate DaprComponentOperationPredicate) (result ListCompleteResult, err error) {
+	items := make([]DaprComponent, 0)
+
+	resp, err := c.List(ctx, id)
+	if err != nil {
+		err = fmt.Errorf("loading results: %+v", err)
+		return
+	}
+	if resp.Model != nil {
+		for _, v := range *resp.Model {
+			if predicate.Matches(v) {
+				items = append(items, v)
+			}
+		}
+	}
+
+	result = ListCompleteResult{
+		LatestHttpResponse: resp.HttpResponse,
+		Items:              items,
+	}
+	return
+}
