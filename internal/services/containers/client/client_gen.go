68cd37654a9ddd3306e8009e33e9ae8d25faa21e
hc-github-team-tf-azure <>
2024-02-13 17:23:30 +0000

    Updating based on 5a9b86a8

--- internal/services/containers/client/client_gen.go
+++ internal/services/containers/client/client_gen.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"fmt"
+
 	containerserviceV20220902Preview "github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2022-09-02-preview"
 	containerserviceV20230302Preview "github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2023-03-02-preview"
 	containerserviceV20231015 "github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2023-10-15"
@@ -9,9 +11,9 @@ import (
 )
 
 type AutoClient struct {
-	V20220902Preview *containerserviceV20220902Preview.Client
-	V20230302Preview *containerserviceV20230302Preview.Client
-	V20231015        *containerserviceV20231015.Client
+	V20220902Preview containerserviceV20220902Preview.Client
+	V20230302Preview containerserviceV20230302Preview.Client
+	V20231015        containerserviceV20231015.Client
 }
 
 func NewClient(o *common.ClientOptions) (*AutoClient, error) {
@@ -20,26 +22,26 @@ func NewClient(o *common.ClientOptions) (*AutoClient, error) {
 		o.Configure(c, o.Authorizers.ResourceManager)
 	})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("building client for containerservice V20220902Preview: %+v", err)
 	}
 
 	v20230302PreviewClient, err := containerserviceV20230302Preview.NewClientWithBaseURI(o.Environment.ResourceManager, func(c *resourcemanager.Client) {
 		o.Configure(c, o.Authorizers.ResourceManager)
 	})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("building client for containerservice V20230302Preview: %+v", err)
 	}
 
 	v20231015Client, err := containerserviceV20231015.NewClientWithBaseURI(o.Environment.ResourceManager, func(c *resourcemanager.Client) {
 		o.Configure(c, o.Authorizers.ResourceManager)
 	})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("building client for containerservice V20231015: %+v", err)
 	}
 
 	return &AutoClient{
-		V20220902Preview: v20220902PreviewClient,
-		V20230302Preview: v20230302PreviewClient,
-		V20231015:        v20231015Client,
+		V20220902Preview: *v20220902PreviewClient,
+		V20230302Preview: *v20230302PreviewClient,
+		V20231015:        *v20231015Client,
 	}, nil
 }
