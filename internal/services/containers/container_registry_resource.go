hashicorp/terraform-provider-azurerm

commit a55ec6b72c4287e9e0a1b374ba34d168bb91098c

Address TODO items in preparation for 3.0 beta (3/many) (#15340)
* [3.0 Beta] Removed `network_rule_fqdn_enabled` attribute
* [3.0 Beta] Remove `EnableAnalyticalStorage` from capabilities
* [3.0 Beta] Remove location attribute. Move 'enforce_backend_pools_certificate_name_check' and 'backend_pools_send_receive_timeout_seconds' into a 'backend_pool_settings' block
* [3.0 Beta] Use commonids for subscription ID parsing
* [3.0 Beta] Remove `storage_account_id` and `georeplication_locations` attributes
* [3.0 Beta] Deprecate `disk_iops_read_write` and `disk_mbps_read_write` in favour of `ultra_ssd_disk_iops_read_write` and `ultra_ssd_disk_mbps_read_write`

internal/services/containers/container_registry_resource.go
@@ -54,283 +54,18 @@ func resourceContainerRegistry() *pluginsdk.Resource {
 			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
 		},
 
-		Schema: map[string]*pluginsdk.Schema{
-			"name": {
-				Type:         pluginsdk.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validate2.ContainerRegistryName,
-			},
-
-			"resource_group_name": azure.SchemaResourceGroupName(),
-
-			"location": azure.SchemaLocation(),
-
-			"sku": {
-				Type:             pluginsdk.TypeString,
-				Optional:         true,
-				Default:          string(containerregistry.SkuNameClassic),
-				DiffSuppressFunc: suppress.CaseDifferenceV2Only,
-				ValidateFunc: validation.StringInSlice([]string{
-					string(containerregistry.SkuNameClassic),
-					string(containerregistry.SkuNameBasic),
-					string(containerregistry.SkuNameStandard),
-					string(containerregistry.SkuNamePremium),
-				}, !features.ThreePointOh()),
-			},
-
-			"admin_enabled": {
-				Type:     pluginsdk.TypeBool,
-				Optional: true,
-				Default:  false,
-			},
-			// TODO 3.0 - Remove below property
-			"georeplication_locations": {
-				Type:          pluginsdk.TypeSet,
-				Optional:      true,
-				Deprecated:    "Deprecated in favour of `georeplications`",
-				Computed:      true,
-				ConflictsWith: []string{"georeplications"},
-				Elem: &pluginsdk.Schema{
-					Type:         pluginsdk.TypeString,
-					ValidateFunc: validation.StringIsNotEmpty,
-				},
-				Set: location.HashCode,
-			},
-
-			"georeplications": {
-				// Don't make this a TypeSet since TypeSet has bugs when there is a nested property using `StateFunc`.
-				// See: https://github.com/hashicorp/terraform-plugin-sdk/issues/160
-				Type:          pluginsdk.TypeList,
-				Optional:      true,
-				Computed:      true, // TODO -- remove this when deprecation resolves
-				ConflictsWith: []string{"georeplication_locations"},
-				ConfigMode:    pluginsdk.SchemaConfigModeAttr, // TODO -- remove in 3.0, because this property is optional and computed, it has to be declared as empty array to remove existed values
-				Elem: &pluginsdk.Resource{
-					Schema: map[string]*pluginsdk.Schema{
-						"location": location.SchemaWithoutForceNew(),
-
-						"zone_redundancy_enabled": {
-							Type:     pluginsdk.TypeBool,
-							Optional: true,
-							Default:  false,
-						},
-
-						"regional_endpoint_enabled": {
-							Type:     pluginsdk.TypeBool,
-							Optional: true,
-						},
-
-						"tags": tags.Schema(),
-					},
-				},
-			},
-
-			"public_network_access_enabled": {
-				Type:     pluginsdk.TypeBool,
-				Optional: true,
-				Default:  true,
-			},
-
-			// TODO 3.0 - Remove this property as all the Classic sku instances are now deprecated and out of support at the serice side.
-			"storage_account_id": {
-				Type:       pluginsdk.TypeString,
-				Optional:   true,
-				Computed:   true,
-				ForceNew:   true,
-				Deprecated: "this attribute is no longer recognized by the API and is not functional anymore, thus this property will be removed in v3.0",
-			},
-
-			"login_server": {
-				Type:     pluginsdk.TypeString,
-				Computed: true,
-			},
-
-			"admin_username": {
-				Type:     pluginsdk.TypeString,
-				Computed: true,
-			},
-
-			"admin_password": {
-				Type:      pluginsdk.TypeString,
-				Computed:  true,
-				Sensitive: true,
-			},
-
-			"identity": commonschema.SystemAssignedUserAssignedIdentityOptional(),
-
-			"encryption": {
-				Type:       pluginsdk.TypeList,
-				Optional:   true,
-				Computed:   true,
-				MaxItems:   1,
-				ConfigMode: pluginsdk.SchemaConfigModeAttr,
-				Elem: &pluginsdk.Resource{
-					Schema: map[string]*pluginsdk.Schema{
-						"enabled": {
-							Type:     pluginsdk.TypeBool,
-							Optional: true,
-							Default:  false,
-						},
-						"identity_client_id": {
-							Type:         pluginsdk.TypeString,
-							Required:     true,
-							ValidateFunc: validation.IsUUID,
-						},
-						"key_vault_key_id": {
-							Type:         pluginsdk.TypeString,
-							Required:     true,
-							ValidateFunc: keyVaultValidate.NestedItemIdWithOptionalVersion,
-						},
-					},
-				},
-			},
-
-			"network_rule_set": {
-				Type:       pluginsdk.TypeList,
-				Optional:   true,
-				Computed:   true,
-				MaxItems:   1,
-				ConfigMode: pluginsdk.SchemaConfigModeAttr, // make sure we can set this to an empty array for Premium -> Basic
-				Elem: &pluginsdk.Resource{
-					Schema: map[string]*pluginsdk.Schema{
-						"default_action": {
-							Type:     pluginsdk.TypeString,
-							Optional: true,
-							Default:  containerregistry.DefaultActionAllow,
-							ValidateFunc: validation.StringInSlice([]string{
-								string(containerregistry.DefaultActionAllow),
-								string(containerregistry.DefaultActionDeny),
-							}, false),
-						},
-
-						"ip_rule": {
-							Type:       pluginsdk.TypeSet,
-							Optional:   true,
-							ConfigMode: pluginsdk.SchemaConfigModeAttr,
-							Elem: &pluginsdk.Resource{
-								Schema: map[string]*pluginsdk.Schema{
-									"action": {
-										Type:     pluginsdk.TypeString,
-										Required: true,
-										ValidateFunc: validation.StringInSlice([]string{
-											string(containerregistry.ActionAllow),
-										}, false),
-									},
-									"ip_range": {
-										Type:         pluginsdk.TypeString,
-										Required:     true,
-										ValidateFunc: validate.CIDR,
-									},
-								},
-							},
-						},
-
-						"virtual_network": {
-							Type:       pluginsdk.TypeSet,
-							Optional:   true,
-							ConfigMode: pluginsdk.SchemaConfigModeAttr,
-							Elem: &pluginsdk.Resource{
-								Schema: map[string]*pluginsdk.Schema{
-									"action": {
-										Type:     pluginsdk.TypeString,
-										Required: true,
-										ValidateFunc: validation.StringInSlice([]string{
-											string(containerregistry.ActionAllow),
-										}, false),
-									},
-									"subnet_id": {
-										Type:         pluginsdk.TypeString,
-										Required:     true,
-										ValidateFunc: azure.ValidateResourceID,
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-
-			"quarantine_policy_enabled": {
-				Type:     pluginsdk.TypeBool,
-				Optional: true,
-			},
-
-			"retention_policy": {
-				Type:       pluginsdk.TypeList,
-				MaxItems:   1,
-				Optional:   true,
-				Computed:   true,
-				ConfigMode: pluginsdk.SchemaConfigModeAttr,
-				Elem: &pluginsdk.Resource{
-					Schema: map[string]*pluginsdk.Schema{
-						"days": {
-							Type:     pluginsdk.TypeInt,
-							Optional: true,
-							Default:  7,
-						},
-
-						"enabled": {
-							Type:     pluginsdk.TypeBool,
-							Optional: true,
-							Default:  false,
-						},
-					},
-				},
-			},
-
-			"trust_policy": {
-				Type:       pluginsdk.TypeList,
-				MaxItems:   1,
-				Optional:   true,
-				Computed:   true,
-				ConfigMode: pluginsdk.SchemaConfigModeAttr,
-				Elem: &pluginsdk.Resource{
-					Schema: map[string]*pluginsdk.Schema{
-						"enabled": {
-							Type:     pluginsdk.TypeBool,
-							Optional: true,
-							Default:  false,
-						},
-					},
-				},
-			},
-
-			"zone_redundancy_enabled": {
-				Type:     pluginsdk.TypeBool,
-				ForceNew: true,
-				Optional: true,
-				Default:  false,
-			},
-
-			"anonymous_pull_enabled": {
-				Type:     pluginsdk.TypeBool,
-				Optional: true,
-			},
-
-			"data_endpoint_enabled": {
-				Type:     pluginsdk.TypeBool,
-				Optional: true,
-			},
-
-			"network_rule_bypass_option": {
-				Type:     pluginsdk.TypeString,
-				Optional: true,
-				ValidateFunc: validation.StringInSlice([]string{
-					string(containerregistry.NetworkRuleBypassOptionsAzureServices),
-					string(containerregistry.NetworkRuleBypassOptionsNone),
-				}, false),
-				Default: string(containerregistry.NetworkRuleBypassOptionsAzureServices),
-			},
-
-			"tags": tags.Schema(),
-		},
+		Schema: resourceContainerRegistrySchema(),
 
 		CustomizeDiff: pluginsdk.CustomizeDiffShim(func(ctx context.Context, d *pluginsdk.ResourceDiff, v interface{}) error {
 			sku := d.Get("sku").(string)
-			geoReplicationLocations := d.Get("georeplication_locations").(*pluginsdk.Set)
+
+			hasGeoReplications := false
+			if !features.ThreePointOhBeta() {
+				geoReplicationLocations := d.Get("georeplication_locations").(*pluginsdk.Set)
+				hasGeoReplications = geoReplicationLocations.Len() > 0
+			}
 			geoReplications := d.Get("georeplications").([]interface{})
-			hasGeoReplicationsApplied := geoReplicationLocations.Len() > 0 || len(geoReplications) > 0
+			hasGeoReplicationsApplied := hasGeoReplications || len(geoReplications) > 0
 			// if locations have been specified for geo-replication then, the SKU has to be Premium
 			if hasGeoReplicationsApplied && !strings.EqualFold(sku, string(containerregistry.SkuNamePremium)) {
 				return fmt.Errorf("ACR geo-replication can only be applied when using the Premium Sku.")
@@ -423,7 +158,6 @@ func resourceContainerRegistryCreate(d *pluginsdk.ResourceData, meta interface{}
 	sku := d.Get("sku").(string)
 	adminUserEnabled := d.Get("admin_enabled").(bool)
 	t := d.Get("tags").(map[string]interface{})
-	geoReplicationLocations := d.Get("georeplication_locations").(*pluginsdk.Set)
 	geoReplications := d.Get("georeplications").([]interface{})
 
 	networkRuleSet := expandNetworkRuleSet(d.Get("network_rule_set").([]interface{}))
@@ -494,10 +228,12 @@ func resourceContainerRegistryCreate(d *pluginsdk.ResourceData, meta interface{}
 
 	// the ACR is being created so no previous geo-replication locations
 	var oldGeoReplicationLocations, newGeoReplicationLocations []containerregistry.Replication
-	if geoReplicationLocations != nil && geoReplicationLocations.Len() > 0 {
-		newGeoReplicationLocations = expandReplicationsFromLocations(geoReplicationLocations.List())
-	} else {
-		newGeoReplicationLocations = expandReplications(geoReplications)
+	newGeoReplicationLocations = expandReplications(geoReplications)
+	if !features.ThreePointOhBeta() {
+		geoReplicationLocations := d.Get("georeplication_locations").(*pluginsdk.Set)
+		if geoReplicationLocations != nil && geoReplicationLocations.Len() > 0 {
+			newGeoReplicationLocations = expandReplicationsFromLocations(geoReplicationLocations.List())
+		}
 	}
 	// geo replications have been specified
 	if len(newGeoReplicationLocations) > 0 {
@@ -531,11 +267,6 @@ func resourceContainerRegistryUpdate(d *pluginsdk.ResourceData, meta interface{}
 
 	adminUserEnabled := d.Get("admin_enabled").(bool)
 	t := d.Get("tags").(map[string]interface{})
-
-	old, new := d.GetChange("georeplication_locations")
-	hasGeoReplicationLocationsChanges := d.HasChange("georeplication_locations")
-	oldGeoReplicationLocations := old.(*pluginsdk.Set)
-	newGeoReplicationLocations := new.(*pluginsdk.Set)
 
 	oldReplicationsRaw, newReplicationsRaw := d.GetChange("georeplications")
 	hasGeoReplicationsChanges := d.HasChange("georeplications")
@@ -590,8 +321,21 @@ func resourceContainerRegistryUpdate(d *pluginsdk.ResourceData, meta interface{}
 		Tags:     tags.Expand(t),
 	}
 
+	var hasGeoReplicationLocationsChanges bool
+	var hasNewGeoReplicationLocations bool
+	oldGeoReplicationLocations := make([]interface{}, 0)
+	newGeoReplicationLocations := make([]interface{}, 0)
+
+	if !features.ThreePointOhBeta() {
+		hasGeoReplicationLocationsChanges = d.HasChange("georeplication_locations")
+		old, new := d.GetChange("georeplication_locations")
+		oldGeoReplicationLocations = old.(*pluginsdk.Set).List()
+		newGeoReplicationLocations := new.(*pluginsdk.Set).List()
+		hasNewGeoReplicationLocations = len(newGeoReplicationLocations) > 0
+	}
+
 	// geo replication is only supported by Premium Sku
-	hasGeoReplicationsApplied := newGeoReplicationLocations.Len() > 0 || len(newReplications) > 0
+	hasGeoReplicationsApplied := hasNewGeoReplicationLocations || len(newReplications) > 0
 	if hasGeoReplicationsApplied && !strings.EqualFold(sku, string(containerregistry.SkuNamePremium)) {
 		return fmt.Errorf("ACR geo-replication can only be applied when using the Premium Sku.")
 	}
@@ -602,7 +346,7 @@ func resourceContainerRegistryUpdate(d *pluginsdk.ResourceData, meta interface{}
 			return fmt.Errorf("applying geo replications for %s: %+v", id, err)
 		}
 	} else if hasGeoReplicationLocationsChanges {
-		err := applyGeoReplicationLocations(d, meta, id.ResourceGroup, id.Name, expandReplicationsFromLocations(oldGeoReplicationLocations.List()), expandReplicationsFromLocations(newGeoReplicationLocations.List()))
+		err := applyGeoReplicationLocations(d, meta, id.ResourceGroup, id.Name, expandReplicationsFromLocations(oldGeoReplicationLocations), expandReplicationsFromLocations(newGeoReplicationLocations))
 		if err != nil {
 			return fmt.Errorf("applying geo replications for %s: %+v", id, err)
 		}
@@ -798,11 +542,12 @@ func resourceContainerRegistryRead(d *pluginsdk.ResourceData, meta interface{}) 
 		}
 	}
 
-	d.Set("georeplication_locations", geoReplicationLocations)
-	d.Set("georeplications", geoReplications)
+	if !features.ThreePointOhBeta() {
+		d.Set("georeplication_locations", geoReplicationLocations)
+		d.Set("storage_account_id", "")
+	}
 
-	// Deprecated as it is not returned by the API now.
-	d.Set("storage_account_id", "")
+	d.Set("georeplications", geoReplications)
 
 	return tags.FlattenAndSet(d, resp.Tags)
 }

internal/services/compute/orchestrated_virtual_machine_scale_set.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2021-07-01/compute"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	azValidate "github.com/hashicorp/terraform-provider-azurerm/helpers/validate"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/compute/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
@@ -432,7 +433,7 @@ func computerPrefixLinuxSchema() *pluginsdk.Schema {
 }
 
 func OrchestratedVirtualMachineScaleSetDataDiskSchema() *pluginsdk.Schema {
-	return &pluginsdk.Schema{
+	out := &pluginsdk.Schema{
 		Type:     pluginsdk.TypeList,
 		Optional: true,
 		Elem: &pluginsdk.Resource{
@@ -496,15 +497,13 @@ func OrchestratedVirtualMachineScaleSetDataDiskSchema() *pluginsdk.Schema {
 					Default:  false,
 				},
 
-				// TODO 3.0 - change this to ultra_ssd_disk_iops_read_write
-				"disk_iops_read_write": {
+				"ultra_ssd_disk_iops_read_write": {
 					Type:     pluginsdk.TypeInt,
 					Optional: true,
 					Computed: true,
 				},
 
-				// TODO 3.0 - change this to ultra_ssd_disk_iops_read_write
-				"disk_mbps_read_write": {
+				"ultra_ssd_disk_mbps_read_write": {
 					Type:     pluginsdk.TypeInt,
 					Optional: true,
 					Computed: true,
@@ -512,6 +511,26 @@ func OrchestratedVirtualMachineScaleSetDataDiskSchema() *pluginsdk.Schema {
 			},
 		},
 	}
+
+	if !features.ThreePointOhBeta() {
+		o := out.Elem.(*pluginsdk.Resource)
+
+		o.Schema["disk_iops_read_write"] = &pluginsdk.Schema{
+			Type:       pluginsdk.TypeInt,
+			Optional:   true,
+			Computed:   true,
+			Deprecated: "This property has been renamed to `ultra_ssd_disk_iops_read_write` and will be removed in v3.0 of the provider",
+		}
+
+		o.Schema["disk_mbps_read_write"] = &pluginsdk.Schema{
+			Type:       pluginsdk.TypeInt,
+			Optional:   true,
+			Computed:   true,
+			Deprecated: "This property has been renamed to `ultra_ssd_disk_mbps_read_write` and will be removed in v3.0 of the provider",
+		}
+	}
+
+	return out
 }
 
 func OrchestratedVirtualMachineScaleSetOSDiskSchema() *pluginsdk.Schema {
@@ -1156,19 +1175,27 @@ func ExpandOrchestratedVirtualMachineScaleSetDataDisk(input []interface{}, ultra
 			}
 		}
 
-		if iops := raw["disk_iops_read_write"].(int); iops != 0 {
-			if !ultraSSDEnabled {
-				return nil, fmt.Errorf("`disk_iops_read_write` are only available for UltraSSD disks")
-			}
-			disk.DiskIOPSReadWrite = utils.Int64(int64(iops))
+		var iops int
+		if diskIops, ok := raw["disk_iops_read_write"]; ok && diskIops.(int) > 0 {
+			iops = diskIops.(int)
+		} else if ssdIops, ok := raw["ultra_ssd_disk_iops_read_write"]; ok && ssdIops.(int) > 0 {
+			iops = ssdIops.(int)
 		}
+		if iops > 0 && !ultraSSDEnabled {
+			return nil, fmt.Errorf("disk_iops_read_write and ultra_ssd_disk_iops_read_write are only available for UltraSSD disks")
+		}
+		disk.DiskIOPSReadWrite = utils.Int64(int64(iops))
 
-		if mbps := raw["disk_mbps_read_write"].(int); mbps != 0 {
-			if !ultraSSDEnabled {
-				return nil, fmt.Errorf("`disk_mbps_read_write` are only available for UltraSSD disks")
-			}
-			disk.DiskMBpsReadWrite = utils.Int64(int64(mbps))
+		var mbps int
+		if diskMbps, ok := raw["disk_mbps_read_write"]; ok && diskMbps.(int) > 0 {
+			mbps = diskMbps.(int)
+		} else if ssdMbps, ok := raw["ultra_ssd_disk_mbps_read_write"]; ok && ssdMbps.(int) > 0 {
+			mbps = ssdMbps.(int)
 		}
+		if mbps > 0 && !ultraSSDEnabled {
+			return nil, fmt.Errorf("disk_mbps_read_write and ultra_ssd_disk_mbps_read_write are only available for UltraSSD disks")
+		}
+		disk.DiskMBpsReadWrite = utils.Int64(int64(mbps))
 
 		disks = append(disks, disk)
 	}
@@ -1711,15 +1738,17 @@ func FlattenOrchestratedVirtualMachineScaleSetDataDisk(input *[]compute.VirtualM
 		}
 
 		output = append(output, map[string]interface{}{
-			"caching":                   string(v.Caching),
-			"create_option":             string(v.CreateOption),
-			"lun":                       lun,
-			"disk_encryption_set_id":    diskEncryptionSetId,
-			"disk_size_gb":              diskSizeGb,
-			"storage_account_type":      storageAccountType,
-			"write_accelerator_enabled": writeAcceleratorEnabled,
-			"disk_iops_read_write":      iops,
-			"disk_mbps_read_write":      mbps,
+			"caching":                        string(v.Caching),
+			"create_option":                  string(v.CreateOption),
+			"lun":                            lun,
+			"disk_encryption_set_id":         diskEncryptionSetId,
+			"disk_size_gb":                   diskSizeGb,
+			"storage_account_type":           storageAccountType,
+			"write_accelerator_enabled":      writeAcceleratorEnabled,
+			"ultra_ssd_disk_iops_read_write": iops,
+			"ultra_ssd_mbps_read_write":      mbps,
+			"disk_iops_read_write":           iops,
+			"disk_mbps_read_write":           mbps,
 		})
 	}
 

internal/services/compute/virtual_machine_scale_set.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2021-07-01/compute"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	azValidate "github.com/hashicorp/terraform-provider-azurerm/helpers/validate"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/compute/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
@@ -842,7 +843,7 @@ func flattenVirtualMachineScaleSetPublicIPAddress(input compute.VirtualMachineSc
 }
 
 func VirtualMachineScaleSetDataDiskSchema() *pluginsdk.Schema {
-	return &pluginsdk.Schema{
+	out := &pluginsdk.Schema{
 		// TODO: does this want to be a Set?
 		Type:     pluginsdk.TypeList,
 		Optional: true,
@@ -906,23 +907,23 @@ func VirtualMachineScaleSetDataDiskSchema() *pluginsdk.Schema {
 					Optional: true,
 					Default:  false,
 				},
-
-				// TODO 3.0 - change this to ultra_ssd_disk_iops_read_write
-				"disk_iops_read_write": {
-					Type:     pluginsdk.TypeInt,
-					Optional: true,
-					Computed: true,
-				},
-
-				// TODO 3.0 - change this to ultra_ssd_disk_iops_read_write
-				"disk_mbps_read_write": {
-					Type:     pluginsdk.TypeInt,
-					Optional: true,
-					Computed: true,
-				},
 			},
 		},
 	}
+
+	o := out.Elem.(*pluginsdk.Resource).Schema
+	keys := []string{"disk_iops_read_write", "disk_mbps_read_write"}
+	if !features.ThreePointOhBeta() {
+		keys = []string{"ultra_ssd_disk_iops_read_write", "ultra_ssd_mbps_read_write"}
+	}
+	for _, k := range keys {
+		o[k] = &pluginsdk.Schema{
+			Type:     pluginsdk.TypeInt,
+			Optional: true,
+			Computed: true,
+		}
+	}
+	return out
 }
 
 func ExpandVirtualMachineScaleSetDataDisk(input []interface{}, ultraSSDEnabled bool) (*[]compute.VirtualMachineScaleSetDataDisk, error) {
@@ -948,16 +949,23 @@ func ExpandVirtualMachineScaleSetDataDisk(input []interface{}, ultraSSDEnabled b
 			}
 		}
 
-		if iops := raw["disk_iops_read_write"].(int); iops != 0 {
+		iopsRW := "ultra_ssd_disk_iops_read_write"
+		mbpsRW := "ultra_ssd_mbps_read_write"
+		if !features.ThreePointOhBeta() {
+			iopsRW = "disk_iops_read_write"
+			mbpsRW = "disk_mbps_read_write"
+		}
+
+		if iops := raw[iopsRW].(int); iops != 0 {
 			if !ultraSSDEnabled {
-				return nil, fmt.Errorf("`disk_iops_read_write` are only available for UltraSSD disks")
+				return nil, fmt.Errorf("`%s` are only available for UltraSSD disks", iopsRW)
 			}
 			disk.DiskIOPSReadWrite = utils.Int64(int64(iops))
 		}
 
-		if mbps := raw["disk_mbps_read_write"].(int); mbps != 0 {
+		if mbps := raw[mbpsRW].(int); mbps != 0 {
 			if !ultraSSDEnabled {
-				return nil, fmt.Errorf("`disk_mbps_read_write` are only available for UltraSSD disks")
+				return nil, fmt.Errorf("`%s` are only available for UltraSSD disks", mbpsRW)
 			}
 			disk.DiskMBpsReadWrite = utils.Int64(int64(mbps))
 		}
@@ -1010,6 +1018,12 @@ func FlattenVirtualMachineScaleSetDataDisk(input *[]compute.VirtualMachineScaleS
 			mbps = int(*v.DiskMBpsReadWrite)
 		}
 
+		iopsRW := "ultra_ssd_disk_iops_read_write"
+		mbpsRW := "ultra_ssd_mbps_read_write"
+		if !features.ThreePointOhBeta() {
+			iopsRW = "disk_iops_read_write"
+			mbpsRW = "disk_mbps_read_write"
+		}
 		output = append(output, map[string]interface{}{
 			"caching":                   string(v.Caching),
 			"create_option":             string(v.CreateOption),
@@ -1018,8 +1032,8 @@ func FlattenVirtualMachineScaleSetDataDisk(input *[]compute.VirtualMachineScaleS
 			"disk_size_gb":              diskSizeGb,
 			"storage_account_type":      storageAccountType,
 			"write_accelerator_enabled": writeAcceleratorEnabled,
-			"disk_iops_read_write":      iops,
-			"disk_mbps_read_write":      mbps,
+			iopsRW:                      iops,
+			mbpsRW:                      mbps,
 		})
 	}
 

internal/services/consumption/consumption_budget_base.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
 	"github.com/hashicorp/go-uuid"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/consumption/parse"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/consumption/validate"
@@ -325,7 +326,7 @@ func (br consumptionBudgetBaseResource) attributes() map[string]*pluginsdk.Schem
 	return map[string]*pluginsdk.Schema{}
 }
 
-// TODO remove in 3.0
+// CLEANUP remove in 3.0
 func parseScope(scope string) (string, error) {
 	// The validation behaviour of subscription_id isn't correct, it should only accept
 	// the resource ID format for the subscription. This ensures backward compatibility
@@ -345,10 +346,9 @@ func (br consumptionBudgetBaseResource) createFunc(resourceName, scopeFieldName 
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
 			client := metadata.Client.Consumption.BudgetsClient
 
-			// TODO remove this in 3.0
 			var err error
 			scope := metadata.ResourceData.Get(scopeFieldName).(string)
-			if scopeFieldName == "subscription_id" {
+			if scopeFieldName == "subscription_id" && !features.ThreePointOhBeta() {
 				scope, err = parseScope(metadata.ResourceData.Get(scopeFieldName).(string))
 				if err != nil {
 					return err

internal/services/consumption/consumption_budget_subscription_resource.go
@@ -3,6 +3,8 @@ package consumption
 import (
 	"strings"
 
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/consumption/migration"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/consumption/validate"
@@ -32,16 +34,25 @@ func (r SubscriptionConsumptionBudget) Arguments() map[string]*pluginsdk.Schema 
 			Type:     pluginsdk.TypeString,
 			Required: true,
 			ForceNew: true,
-			// ValidateFunc: commonids.ValidateSubscriptionID, // TODO uncomment this in 3.0
-			// TODO remove in 3.0
-			DiffSuppressFunc: func(k, old, new string, d *pluginsdk.ResourceData) bool {
-				n := strings.Split(old, "/")
-				if len(n) >= 3 {
-					subscriptionID := n[2]
-					return new == subscriptionID
+			ValidateFunc: func() pluginsdk.SchemaValidateFunc {
+				if !features.ThreePointOhBeta() {
+					return nil
 				}
-				return false
-			},
+				return commonids.ValidateSubscriptionID
+			}(),
+			DiffSuppressFunc: func() pluginsdk.SchemaDiffSuppressFunc {
+				if !features.ThreePointOh() {
+					return func(k, old, new string, d *pluginsdk.ResourceData) bool {
+						n := strings.Split(old, "/")
+						if len(n) >= 3 {
+							subscriptionID := n[2]
+							return new == subscriptionID
+						}
+						return false
+					}
+				}
+				return nil
+			}(),
 		},
 	}
 	return r.base.arguments(schema)

commit f515fe0fa14bb33d8763199314bcc0d82649e6d6

Fix some issue introduced from #15340

internal/services/containers/container_registry_resource.go
@@ -65,11 +65,9 @@ func resourceContainerRegistry() *pluginsdk.Resource {
 		CustomizeDiff: pluginsdk.CustomizeDiffShim(func(ctx context.Context, d *pluginsdk.ResourceDiff, v interface{}) error {
 			sku := d.Get("sku").(string)
 
-			hasGeoReplications := false
 			geoReplications := d.Get("georeplications").([]interface{})
-			hasGeoReplicationsApplied := hasGeoReplications || len(geoReplications) > 0
 			// if locations have been specified for geo-replication then, the SKU has to be Premium
-			if hasGeoReplicationsApplied && !strings.EqualFold(sku, string(registries.SkuNamePremium)) {
+			if len(geoReplications) > 0 && !strings.EqualFold(sku, string(registries.SkuNamePremium)) {
 				return fmt.Errorf("ACR geo-replication can only be applied when using the Premium Sku.")
 			}
 
@@ -318,24 +316,13 @@ func resourceContainerRegistryUpdate(d *pluginsdk.ResourceData, meta interface{}
 		Tags:     tags.Expand(d.Get("tags").(map[string]interface{})),
 	}
 
-	var hasGeoReplicationLocationsChanges bool
-	var hasNewGeoReplicationLocations bool
-	oldGeoReplicationLocations := make([]interface{}, 0)
-	newGeoReplicationLocations := make([]interface{}, 0)
-
 	// geo replication is only supported by Premium Sku
-	hasGeoReplicationsApplied := hasNewGeoReplicationLocations || len(newReplications) > 0
-	if hasGeoReplicationsApplied && !strings.EqualFold(sku, string(registries.SkuNamePremium)) {
+	if len(newReplications) > 0 && !strings.EqualFold(sku, string(registries.SkuNamePremium)) {
 		return fmt.Errorf("ACR geo-replication can only be applied when using the Premium Sku.")
 	}
 
 	if hasGeoReplicationsChanges {
 		err := applyGeoReplicationLocations(ctx, meta, *id, expandReplications(oldReplications), expandReplications(newReplications))
-		if err != nil {
-			return fmt.Errorf("applying geo replications for %s: %+v", id, err)
-		}
-	} else if hasGeoReplicationLocationsChanges {
-		err := applyGeoReplicationLocations(ctx, meta, *id, expandReplicationsFromLocations(oldGeoReplicationLocations), expandReplicationsFromLocations(newGeoReplicationLocations))
 		if err != nil {
 			return fmt.Errorf("applying geo replications for %s: %+v", id, err)
 		}