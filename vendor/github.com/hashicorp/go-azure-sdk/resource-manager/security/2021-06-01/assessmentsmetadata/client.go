04eb1754b30ea3d86fd0a23d00aedfc86434d2ec
catriona-m <86247157+catriona-m@users.noreply.github.com>
2024-02-29 09:56:00 +0000

    `dependencies`: updating to v0.20240228.1142829 of `github.com/hashicorp/go-azure-sdk` (#25081)
    * update go-azure-sdk to v0.20240228.1142829
    
    * sort imports

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/security/2021-06-01/assessmentsmetadata/client.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/security/2021-06-01/assessmentsmetadata/client.go
@@ -1,18 +1,26 @@
 package assessmentsmetadata
 
-import "github.com/Azure/go-autorest/autorest"
+import (
+	"fmt"
+
+	"github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager"
+	sdkEnv "github.com/hashicorp/go-azure-sdk/sdk/environments"
+)
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
 type AssessmentsMetadataClient struct {
-	Client  autorest.Client
-	baseUri string
+	Client *resourcemanager.Client
 }
 
-func NewAssessmentsMetadataClientWithBaseURI(endpoint string) AssessmentsMetadataClient {
-	return AssessmentsMetadataClient{
-		Client:  autorest.NewClientWithUserAgent(userAgent()),
-		baseUri: endpoint,
+func NewAssessmentsMetadataClientWithBaseURI(sdkApi sdkEnv.Api) (*AssessmentsMetadataClient, error) {
+	client, err := resourcemanager.NewResourceManagerClient(sdkApi, "assessmentsmetadata", defaultApiVersion)
+	if err != nil {
+		return nil, fmt.Errorf("instantiating AssessmentsMetadataClient: %+v", err)
 	}
+
+	return &AssessmentsMetadataClient{
+		Client: client,
+	}, nil
 }
