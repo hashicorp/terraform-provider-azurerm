ee35804ce463cf6e538f6ff9385a901a69c547a5
neil-yechenwei <yechenwei2007@hotmail.com>
2024-02-29 15:55:47 +0800

    Upgrade Network API version from 2023-06-01 2023-09-01

--- internal/services/network/private_endpoint_application_security_group_association_test.go
+++ internal/services/network/private_endpoint_application_security_group_association_test.go
@@ -11,8 +11,8 @@ import (
 	"time"
 
 	"github.com/hashicorp/go-azure-helpers/lang/response"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/network/2023-06-01/applicationsecuritygroups"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/network/2023-06-01/privateendpoints"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/network/2023-09-01/applicationsecuritygroups"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/network/2023-09-01/privateendpoints"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
