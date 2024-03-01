9be53faab8d0d60fe11138bcb300aabe6860d53e
Xu Wu <xuwu1@microsoft.com>
2024-03-01 16:07:14 +0800

    update ManagedHardwareSecurityModuleName to allow hyphens (#25111)

--- internal/services/managedhsm/validate/managed_hsm_name.go
+++ internal/services/managedhsm/validate/managed_hsm_name.go
@@ -6,6 +6,7 @@ package validate
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 func ManagedHardwareSecurityModuleName(i interface{}, k string) (warnings []string, errors []error) {
@@ -15,11 +16,9 @@ func ManagedHardwareSecurityModuleName(i interface{}, k string) (warnings []stri
 	}
 
 	// The name attribute rules are :
-	// 1. can contain only alphanumeric characters.
-	// 2. The first character must be a letter.
-	// 3. The last character must be a letter or number
-	// 4. The value must be between 3 and 24 characters long
-	if !regexp.MustCompile(`^[a-zA-Z][a-zA-Z\d]{2,23}$`).MatchString(v) {
+	// Must be a 3-24 character string, containing only 0-9, a-z. A-Z, and -
+	// The name must begin with a letter, end with a letter or digit, and not contain consecutive hyphens.
+	if strings.Contains(v, "--") || !regexp.MustCompile(`^[a-zA-Z][-a-zA-Z\d]{1,22}[a-zA-Z\d]$`).MatchString(v) {
 		errors = append(errors, fmt.Errorf("%s must begin with a letter, end with a letter or number, contain only alphanumeric characters. The value must be between 3 and 24 characters long", k))
 	}
 
