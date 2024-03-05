bb0f423647718d6b356755032f0d9c5325d35f71
catriona-m <86247157+catriona-m@users.noreply.github.com>
2024-03-05 10:13:43 +0000

    update go-azure-sdk to v0.20240229.1102109 (#25102)

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/insights/2016-03-01/logprofiles/client.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/insights/2016-03-01/logprofiles/client.go
@@ -1,18 +1,26 @@
 package logprofiles
 
-import "github.com/Azure/go-autorest/autorest"
+import (
+	"fmt"
+
+	"github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager"
+	sdkEnv "github.com/hashicorp/go-azure-sdk/sdk/environments"
+)
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
 type LogProfilesClient struct {
-	Client  autorest.Client
-	baseUri string
+	Client *resourcemanager.Client
 }
 
-func NewLogProfilesClientWithBaseURI(endpoint string) LogProfilesClient {
-	return LogProfilesClient{
-		Client:  autorest.NewClientWithUserAgent(userAgent()),
-		baseUri: endpoint,
+func NewLogProfilesClientWithBaseURI(sdkApi sdkEnv.Api) (*LogProfilesClient, error) {
+	client, err := resourcemanager.NewResourceManagerClient(sdkApi, "logprofiles", defaultApiVersion)
+	if err != nil {
+		return nil, fmt.Errorf("instantiating LogProfilesClient: %+v", err)
 	}
+
+	return &LogProfilesClient{
+		Client: client,
+	}, nil
 }
