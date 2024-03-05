15f40097c502a04206ca65b1076118d3064d91a6
neil-yechenwei <yechenwei2007@hotmail.com>
2024-03-05 19:09:03 +0800

    update code

--- internal/services/web/app_service_slot_virtual_network_swift_connection_resource.go
+++ internal/services/web/app_service_slot_virtual_network_swift_connection_resource.go
@@ -5,10 +5,10 @@ package web
 
 import (
 	"fmt"
-	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/web/mgmt/2021-02-01/web" // nolint: staticcheck
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
