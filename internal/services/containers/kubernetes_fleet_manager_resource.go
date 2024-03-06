8f9ae166ba8236dc5e4c1f0e98029e8eebf2371a
Heng Lu <79895375+ms-henglu@users.noreply.github.com>
2024-03-06 11:59:14 +0800

    `azurerm_kubernetes_fleet_manager` - deprecate `hub_profile` (#25010)
    * `azurerm_kubernetes_fleet_manager` - deprecate `hub_profile`
    
    * update
    
    * update doc examples
    
    * update the deprecate notice
    
    * rename files
    
    * fix golint

--- internal/services/containers/kubernetes_fleet_manager_resource.go
+++ internal/services/containers/kubernetes_fleet_manager_resource.go
@@ -8,12 +8,11 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/tags"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2022-09-02-preview/fleets"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2023-10-15/fleets"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 )
@@ -28,11 +27,10 @@ func (r KubernetesFleetManagerResource) ModelObject() interface{} {
 }
 
 type KubernetesFleetManagerResourceSchema struct {
-	HubProfile        []KubernetesFleetManagerResourceFleetHubProfileSchema `tfschema:"hub_profile"`
-	Location          string                                                `tfschema:"location"`
-	Name              string                                                `tfschema:"name"`
-	ResourceGroupName string                                                `tfschema:"resource_group_name"`
-	Tags              map[string]interface{}                                `tfschema:"tags"`
+	Location          string                 `tfschema:"location"`
+	Name              string                 `tfschema:"name"`
+	ResourceGroupName string                 `tfschema:"resource_group_name"`
+	Tags              map[string]interface{} `tfschema:"tags"`
 }
 
 func (r KubernetesFleetManagerResource) IDValidationFunc() pluginsdk.SchemaValidateFunc {
@@ -51,6 +49,7 @@ func (r KubernetesFleetManagerResource) Arguments() map[string]*pluginsdk.Schema
 		},
 		"resource_group_name": commonschema.ResourceGroupName(),
 		"hub_profile": {
+			Deprecated: "The service team has indicated this field is now deprecated and not to be used, as such we are marking it as such and no longer sending it to the API, please see url: https://learn.microsoft.com/en-us/azure/kubernetes-fleet/architectural-overview",
 			Elem: &pluginsdk.Resource{
 				Schema: map[string]*pluginsdk.Schema{
 					"dns_prefix": {
@@ -82,7 +81,7 @@ func (r KubernetesFleetManagerResource) Create() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Timeout: 30 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
-			client := metadata.Client.ContainerService.V20220902Preview.Fleets
+			client := metadata.Client.ContainerService.V20231015.Fleets
 
 			var config KubernetesFleetManagerResourceSchema
 			if err := metadata.Decode(&config); err != nil {
@@ -104,9 +103,7 @@ func (r KubernetesFleetManagerResource) Create() sdk.ResourceFunc {
 			}
 
 			var payload fleets.Fleet
-			if err := r.mapKubernetesFleetManagerResourceSchemaToFleet(config, &payload); err != nil {
-				return fmt.Errorf("mapping schema model to sdk model: %+v", err)
-			}
+			r.mapKubernetesFleetManagerResourceSchemaToFleet(config, &payload)
 
 			if err := client.CreateOrUpdateThenPoll(ctx, id, payload, fleets.DefaultCreateOrUpdateOperationOptions()); err != nil {
 				return fmt.Errorf("creating %s: %+v", id, err)
@@ -121,7 +118,7 @@ func (r KubernetesFleetManagerResource) Read() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Timeout: 5 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
-			client := metadata.Client.ContainerService.V20220902Preview.Fleets
+			client := metadata.Client.ContainerService.V20231015.Fleets
 			schema := KubernetesFleetManagerResourceSchema{}
 
 			id, err := fleets.ParseFleetID(metadata.ResourceData.Id())
@@ -140,9 +137,7 @@ func (r KubernetesFleetManagerResource) Read() sdk.ResourceFunc {
 			if model := resp.Model; model != nil {
 				schema.Name = id.FleetName
 				schema.ResourceGroupName = id.ResourceGroupName
-				if err := r.mapFleetToKubernetesFleetManagerResourceSchema(*model, &schema); err != nil {
-					return fmt.Errorf("flattening model: %+v", err)
-				}
+				r.mapFleetToKubernetesFleetManagerResourceSchema(*model, &schema)
 			}
 
 			return metadata.Encode(&schema)
@@ -153,7 +148,7 @@ func (r KubernetesFleetManagerResource) Delete() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Timeout: 30 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
-			client := metadata.Client.ContainerService.V20220902Preview.Fleets
+			client := metadata.Client.ContainerService.V20231015.Fleets
 
 			id, err := fleets.ParseFleetID(metadata.ResourceData.Id())
 			if err != nil {
@@ -172,7 +167,7 @@ func (r KubernetesFleetManagerResource) Update() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Timeout: 30 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
-			client := metadata.Client.ContainerService.V20220902Preview.Fleets
+			client := metadata.Client.ContainerService.V20231015.Fleets
 
 			id, err := fleets.ParseFleetID(metadata.ResourceData.Id())
 			if err != nil {
@@ -193,9 +188,7 @@ func (r KubernetesFleetManagerResource) Update() sdk.ResourceFunc {
 			}
 			payload := *existing.Model
 
-			if err := r.mapKubernetesFleetManagerResourceSchemaToFleet(config, &payload); err != nil {
-				return fmt.Errorf("mapping schema model to sdk model: %+v", err)
-			}
+			r.mapKubernetesFleetManagerResourceSchemaToFleet(config, &payload)
 
 			if err := client.CreateOrUpdateThenPoll(ctx, *id, payload, fleets.DefaultCreateOrUpdateOperationOptions()); err != nil {
 				return fmt.Errorf("updating %s: %+v", *id, err)
@@ -206,93 +199,20 @@ func (r KubernetesFleetManagerResource) Update() sdk.ResourceFunc {
 	}
 }
 
-type KubernetesFleetManagerResourceFleetHubProfileSchema struct {
-	DnsPrefix         string `tfschema:"dns_prefix"`
-	Fqdn              string `tfschema:"fqdn"`
-	KubernetesVersion string `tfschema:"kubernetes_version"`
-}
-
-func (r KubernetesFleetManagerResource) mapKubernetesFleetManagerResourceFleetHubProfileSchemaToFleetHubProfile(input KubernetesFleetManagerResourceFleetHubProfileSchema, output *fleets.FleetHubProfile) error {
-	output.DnsPrefix = input.DnsPrefix
-
-	return nil
-}
-
-func (r KubernetesFleetManagerResource) mapFleetHubProfileToKubernetesFleetManagerResourceFleetHubProfileSchema(input fleets.FleetHubProfile, output *KubernetesFleetManagerResourceFleetHubProfileSchema) error {
-	output.DnsPrefix = input.DnsPrefix
-	output.Fqdn = pointer.From(input.Fqdn)
-	output.KubernetesVersion = pointer.From(input.KubernetesVersion)
-	return nil
-}
-
-func (r KubernetesFleetManagerResource) mapKubernetesFleetManagerResourceFleetHubProfileSchemaToFleetProperties(input KubernetesFleetManagerResourceFleetHubProfileSchema, output *fleets.FleetProperties) error {
-
-	if output.HubProfile == nil {
-		output.HubProfile = &fleets.FleetHubProfile{}
-	}
-	if err := r.mapKubernetesFleetManagerResourceFleetHubProfileSchemaToFleetHubProfile(input, output.HubProfile); err != nil {
-		return fmt.Errorf("mapping Schema to SDK Field %q / Model %q: %+v", "FleetHubProfile", "HubProfile", err)
-	}
-
-	return nil
-}
-
-func (r KubernetesFleetManagerResource) mapFleetPropertiesToKubernetesFleetManagerResourceFleetHubProfileSchema(input fleets.FleetProperties, output *KubernetesFleetManagerResourceFleetHubProfileSchema) error {
-
-	if input.HubProfile == nil {
-		input.HubProfile = &fleets.FleetHubProfile{}
-	}
-	if err := r.mapFleetHubProfileToKubernetesFleetManagerResourceFleetHubProfileSchema(*input.HubProfile, output); err != nil {
-		return fmt.Errorf("mapping SDK Field %q / Model %q to Schema: %+v", "FleetHubProfile", "HubProfile", err)
-	}
-
-	return nil
-}
-
-func (r KubernetesFleetManagerResource) mapKubernetesFleetManagerResourceSchemaToFleet(input KubernetesFleetManagerResourceSchema, output *fleets.Fleet) error {
+func (r KubernetesFleetManagerResource) mapKubernetesFleetManagerResourceSchemaToFleet(input KubernetesFleetManagerResourceSchema, output *fleets.Fleet) {
 	output.Location = location.Normalize(input.Location)
 	output.Tags = tags.Expand(input.Tags)
 
 	if output.Properties == nil {
 		output.Properties = &fleets.FleetProperties{}
 	}
-	if err := r.mapKubernetesFleetManagerResourceSchemaToFleetProperties(input, output.Properties); err != nil {
-		return fmt.Errorf("mapping Schema to SDK Field %q / Model %q: %+v", "FleetProperties", "Properties", err)
-	}
-
-	return nil
 }
 
-func (r KubernetesFleetManagerResource) mapFleetToKubernetesFleetManagerResourceSchema(input fleets.Fleet, output *KubernetesFleetManagerResourceSchema) error {
+func (r KubernetesFleetManagerResource) mapFleetToKubernetesFleetManagerResourceSchema(input fleets.Fleet, output *KubernetesFleetManagerResourceSchema) {
 	output.Location = location.Normalize(input.Location)
 	output.Tags = tags.Flatten(input.Tags)
 
 	if input.Properties == nil {
 		input.Properties = &fleets.FleetProperties{}
 	}
-	if err := r.mapFleetPropertiesToKubernetesFleetManagerResourceSchema(*input.Properties, output); err != nil {
-		return fmt.Errorf("mapping SDK Field %q / Model %q to Schema: %+v", "FleetProperties", "Properties", err)
-	}
-
-	return nil
-}
-
-func (r KubernetesFleetManagerResource) mapKubernetesFleetManagerResourceSchemaToFleetProperties(input KubernetesFleetManagerResourceSchema, output *fleets.FleetProperties) error {
-	if len(input.HubProfile) > 0 {
-		if err := r.mapKubernetesFleetManagerResourceFleetHubProfileSchemaToFleetProperties(input.HubProfile[0], output); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (r KubernetesFleetManagerResource) mapFleetPropertiesToKubernetesFleetManagerResourceSchema(input fleets.FleetProperties, output *KubernetesFleetManagerResourceSchema) error {
-	tmpHubProfile := &KubernetesFleetManagerResourceFleetHubProfileSchema{}
-	if err := r.mapFleetPropertiesToKubernetesFleetManagerResourceFleetHubProfileSchema(input, tmpHubProfile); err != nil {
-		return err
-	} else {
-		output.HubProfile = make([]KubernetesFleetManagerResourceFleetHubProfileSchema, 0)
-		output.HubProfile = append(output.HubProfile, *tmpHubProfile)
-	}
-	return nil
 }
