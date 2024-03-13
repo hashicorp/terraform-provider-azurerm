2db894e205ecb8a551dcd15852bb9c6764517d7f
Zhenhua Hu <zhhu@microsoft.com>
2024-03-13 14:08:27 +0800

    add comment for the change

--- .teamcity/components/settings.kt
+++ .teamcity/components/settings.kt
@@ -65,6 +65,7 @@ var serviceTestConfigurationOverrides = mapOf(
         "confidentialledger" to testConfiguration(locationOverride = LocationConfiguration("eastus","southcentralus","westeurope", false)),
 
         // Container App Managed Environments are limited to 20 per location, using 10 as they can take some time to clear
+        // Enable rotation test to mitigate resource burden in a single region
         "containerapps" to testConfiguration(parallelism = 10, locationOverride = LocationConfiguration("eastus2","westus2","southcentralus", true)),
 
         // The AKS API has a low rate limit
