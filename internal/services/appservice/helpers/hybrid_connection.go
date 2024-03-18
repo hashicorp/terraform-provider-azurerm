d359c48d9324f274449e421074f6e2e1e12d723d
Stuart Brown <brown@kieback-peter.de>
2024-03-18 11:28:48 +0100

    fix: hybrid connection uses authorization rule of the hybrid connection not the namespace

--- internal/services/appservice/helpers/hybrid_connection.go
+++ internal/services/appservice/helpers/hybrid_connection.go
@@ -7,14 +7,14 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/hashicorp/go-azure-sdk/resource-manager/relay/2021-11-01/namespaces"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/relay/2021-11-01/hybridconnections"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/web/2023-01-01/webapps"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 )
 
 func GetSendKeyValue(ctx context.Context, metadata sdk.ResourceMetaData, id webapps.RelayId, sendKeyName string) (*string, error) {
-	relayClient := metadata.Client.Relay.NamespacesClient
-	connectionId := namespaces.NewAuthorizationRuleID(id.SubscriptionId, id.ResourceGroupName, id.HybridConnectionNamespaceName, sendKeyName)
+	relayClient := metadata.Client.Relay.HybridConnectionsClient
+ 	connectionId := hybridconnections.NewHybridConnectionAuthorizationRuleID(id.SubscriptionId, id.ResourceGroupName, id.HybridConnectionNamespaceName, id.RelayName, sendKeyName)
 	keys, err := relayClient.ListKeys(ctx, connectionId)
 	if err != nil {
 		return nil, fmt.Errorf("listing Send Keys for %s in %s: %+v", connectionId, id, err)
