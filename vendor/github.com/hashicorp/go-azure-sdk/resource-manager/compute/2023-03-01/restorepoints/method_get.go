2c78e274d7f6c3abf5c8c9b6f5e0413fca56c41e
kt <kt@katbyte.me>
2024-03-21 17:24:07 -0700

    go mod vendor; go mod tidy (#25361)

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2023-03-01/restorepoints/method_get.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2023-03-01/restorepoints/method_get.go
@@ -72,7 +72,10 @@ func (c RestorePointsClient) Get(ctx context.Context, id RestorePointId, options
 		return
 	}
 
-	if err = resp.Unmarshal(&result.Model); err != nil {
+	var model RestorePoint
+	result.Model = &model
+
+	if err = resp.Unmarshal(result.Model); err != nil {
 		return
 	}
 
