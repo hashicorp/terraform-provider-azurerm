7168368e56b03448492b3e977b88ad3b01758aa7
Guilhem Lettron <guilhem@barpilot.io>
2024-03-26 00:12:35 +0100

    feat: add cognitve deployment dynamic_throttling
    Signed-off-by: Guilhem Lettron <guilhem@barpilot.io>

--- internal/services/cognitive/cognitive_account_data_source.go
+++ internal/services/cognitive/cognitive_account_data_source.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/identity"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/tags"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/cognitive/2023-05-01/cognitiveservicesaccounts"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/cognitive/2023-10-01-preview/cognitiveservicesaccounts"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
