7d32c68996dc9ae26451696f4c78e8129897e0da
xuwu1 <xuwu1@microsoft.com>
2024-03-26 13:14:20 +0800

    remove id generate file

--- internal/services/managedhsm/resourceids.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
-package managedhsm
-
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ManagedHSMNestedKeyWithVersion -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.KeyVault/managedHSMs/mhsm1/keys/key1/versions/version1
-//go:generate go run ../../tools/generator-resource-id/main.go -path=./ -name=ManagedHSMNestedKeyVersionless -id=/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/resGroup1/providers/Microsoft.KeyVault/managedHSMs/mhsm1/keys/key1
