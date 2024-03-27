4d41a1c19ec030665907450d0fedff11d70daa18
Bruce Harrison <bruce.harrison@hashicorp.com>
2024-03-27 11:29:27 -0500

    enable purge protection

--- internal/services/bot/bot_service_azure_bot_resource_test.go
+++ internal/services/bot/bot_service_azure_bot_resource_test.go
@@ -339,7 +339,7 @@ resource "azurerm_key_vault" "test" {
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+  purge_protection_enabled    = true
   enable_rbac_authorization   = true
 
   sku_name = "standard"
