6ac5b60e912ef24bf50bd2e041740005f48631b4
Apokalypt <58733915+Apokalypt@users.noreply.github.com>
2024-03-28 15:48:24 +0100

    fix(azurerm_windows_web_app): Add back a control removed by error

--- internal/services/appservice/windows_web_app_resource.go
+++ internal/services/appservice/windows_web_app_resource.go
@@ -909,7 +909,7 @@ func (r WindowsWebAppResource) Update() sdk.ResourceFunc {
 			}
 
 			// (@jackofallops) - App Settings can clobber logs configuration so must be updated before we send any Log updates
-			if shouldSaveAppSettings || metadata.ResourceData.HasChanges("app_settings") || metadata.ResourceData.HasChange("site_config.0.health_check_eviction_time_in_min") || metadata.ResourceData.HasChange("site_config.0.application_stack.0.node_version") {
+			if shouldSaveAppSettings || metadata.ResourceData.HasChanges("app_settings", "site_config") || metadata.ResourceData.HasChange("site_config.0.health_check_eviction_time_in_min") || metadata.ResourceData.HasChange("site_config.0.application_stack.0.node_version") {
 				appSettingsUpdate := helpers.ExpandAppSettingsForUpdate(model.Properties.SiteConfig.AppSettings)
 				appSettingsProps := *appSettingsUpdate.Properties
 				appSettingsProps["WEBSITE_HEALTHCHECK_MAXPINGFAILURES"] = strconv.Itoa(int(state.SiteConfig[0].HealthCheckEvictionTime))
