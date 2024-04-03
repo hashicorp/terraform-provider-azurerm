1b1de586255ad44525464ce406e3351cfbdeb55a
Steph <steph@hashicorp.com>
2024-04-03 16:22:11 +0200

    goimports

--- internal/services/compute/shared_image_version_data_source.go
+++ internal/services/compute/shared_image_version_data_source.go
@@ -6,9 +6,9 @@ package compute
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"time"
 
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
