d9e282db5006c3c1feb381f941918f2b27e441b6
kt <kt@katbyte.me>
2024-04-03 21:03:00 -0700

    azurerm_windows_web_app - prevent panic

--- internal/services/appservice/migration/windows_web_app_slot.go
+++ internal/services/appservice/migration/windows_web_app_slot.go
@@ -1803,9 +1803,15 @@ func (w WindowsWebAppSlotV0toV1) Schema() map[string]*pluginsdk.Schema {
 
 func (w WindowsWebAppSlotV0toV1) UpgradeFunc() pluginsdk.StateUpgraderFunc {
 	return func(ctx context.Context, rawState map[string]interface{}, meta interface{}) (map[string]interface{}, error) {
+		v, ok := rawState["service_plan_id"]
+		if !ok || v == nil {
+			return rawState, nil
+		}
+
 		oldId := rawState["service_plan_id"].(string)
 		// service_plan_id can be empty if it is not in a different Service Plan to the "parent" app
 		if oldId == "" {
+
 			return rawState, nil
 		}
 		parsedId, err := commonids.ParseAppServicePlanIDInsensitively(oldId)
