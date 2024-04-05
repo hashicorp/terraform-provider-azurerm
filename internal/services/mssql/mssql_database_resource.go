5727e50ec7cd50136aa9a179d97dee36c60d063c
kt <kt@katbyte.me>
2024-04-04 19:33:16 -0700

    Update internal/services/mssql/mssql_database_resource.go

--- internal/services/mssql/mssql_database_resource.go
+++ internal/services/mssql/mssql_database_resource.go
@@ -214,9 +214,7 @@ func resourceMsSqlDatabaseCreate(d *pluginsdk.ResourceData, meta interface{}) er
 	locks.ByID(id.ID())
 	defer locks.UnlockByID(id.ID())
 
-	// PER THE SERVICE TEAM: If the config doesn’t specify any value for the 'enclave_type'
-	// field it shouldn’t be passed as part of the ARM API request for database or
-	// elastic pool, as it is an optional parameter and not a required one.
+	// NOTE: The service default is actually nil/empty which indicates enclave is disabled. the value `Default` is NOT the default.
 	var enclaveType databases.AlwaysEncryptedEnclaveType
 	if v, ok := d.GetOk("enclave_type"); ok && v.(string) != "" {
 		enclaveType = databases.AlwaysEncryptedEnclaveType(v.(string))
