38ed40c2755c513224874251e90d5134ce2d5036
Frederic Jung <fjaeckel@github.com>
2024-04-18 10:41:33 +0200

    Use `Static` Public IP address

--- internal/services/network/virtual_network_gateway_connection_resource_test.go
+++ internal/services/network/virtual_network_gateway_connection_resource_test.go
@@ -514,7 +514,7 @@ resource "azurerm_public_ip" "test" {
   name                = "acctest-${var.random}"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"
 }
 
@@ -621,7 +621,7 @@ resource "azurerm_public_ip" "test" {
   name                = "acctest-${var.random}"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"
 }
 
