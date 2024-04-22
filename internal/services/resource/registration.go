17b93e36fd1d8b2de8c7cab4283132d6bd75b6cb
Steph <steph@hashicorp.com>
2024-04-22 12:30:45 +0200

    conditionally register deprecated resources and data sources

--- internal/services/resource/registration.go
+++ internal/services/resource/registration.go
@@ -4,6 +4,7 @@
 package resource
 
 import (
+	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 )
@@ -44,15 +45,20 @@ func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
 
 // SupportedResources returns the supported Resources supported by this Service
 func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
-	return map[string]*pluginsdk.Resource{
+	resources := map[string]*pluginsdk.Resource{
 		"azurerm_management_lock":                      resourceManagementLock(),
 		"azurerm_management_group_template_deployment": managementGroupTemplateDeploymentResource(),
 		"azurerm_resource_group":                       resourceResourceGroup(),
 		"azurerm_resource_group_template_deployment":   resourceGroupTemplateDeploymentResource(),
 		"azurerm_subscription_template_deployment":     subscriptionTemplateDeploymentResource(),
-		"azurerm_template_deployment":                  resourceTemplateDeployment(),
 		"azurerm_tenant_template_deployment":           tenantTemplateDeploymentResource(),
 	}
+
+	if !features.FourPointOhBeta() {
+		resources["azurerm_template_deployment"] = resourceTemplateDeployment()
+	}
+
+	return resources
 }
 
 // DataSources returns a list of Data Sources supported by this Service
