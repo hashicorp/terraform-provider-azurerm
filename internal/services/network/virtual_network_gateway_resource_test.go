a7284af5a8b43bfdf57fa21df383f9392aa83e3c
neil-yechenwei <yechenwei2007@hotmail.com>
2024-04-22 20:23:56 +0800

    update code

--- internal/services/network/virtual_network_gateway_resource_test.go
+++ internal/services/network/virtual_network_gateway_resource_test.go
@@ -1854,10 +1854,10 @@ data "azurerm_virtual_network_gateway" "test" {
 resource "azurerm_virtual_network_gateway_nat_rule" "test" {
   name                       = "acctestvngnatrule-%d"
   resource_group_name        = azurerm_resource_group.test.name
-  virtual_network_gateway_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${acctestRG- % d}/providers/Microsoft.Network/virtualNetworkGateways/${acctestvng- % d}"
+  virtual_network_gateway_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/acctestRG-%d/providers/Microsoft.Network/virtualNetworkGateways/acctestvng-%d"
   mode                       = "EgressSnat"
   type                       = "Dynamic"
-  ip_configuration_id        = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${acctestRG- % d}/providers/Microsoft.Network/virtualNetworkGateways/${acctestvng- % d}/ipConfigurations/default"
+  ip_configuration_id        = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/acctestRG-%d/providers/Microsoft.Network/virtualNetworkGateways/acctestvng-%d/ipConfigurations/default"
 
   external_mapping {
     address_space = "10.1.0.0/26"
