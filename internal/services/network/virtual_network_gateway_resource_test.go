9060a40d3323a0144bd52e3aab91760c6f74ca14
neil-yechenwei <yechenwei2007@hotmail.com>
2024-04-23 10:10:48 +0800

    update code

--- internal/services/network/virtual_network_gateway_resource_test.go
+++ internal/services/network/virtual_network_gateway_resource_test.go
@@ -1647,8 +1647,7 @@ resource "azurerm_public_ip" "test" {
   name                = "acctestpip-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+  allocation_method   = "Dynamic"
 }
 
 data "azurerm_extended_locations" "test" {
@@ -1663,7 +1662,7 @@ resource "azurerm_virtual_network_gateway" "test" {
 
   type     = "Vpn"
   vpn_type = "RouteBased"
-  sku      = "VpnGw2"
+  sku      = "Basic"
 
   ip_configuration {
     public_ip_address_id          = azurerm_public_ip.test.id
