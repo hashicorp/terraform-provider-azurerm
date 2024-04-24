f92eac91d79e57732eeb29d32e88921022be4689
teowa <104055472+teowa@users.noreply.github.com>
2024-04-24 09:13:11 +0000

    new data source azurerm_arc_resource_bridge_appliance

---
+++ internal/services/arcresourcebridge/arc_resource_bridge_appliance_data_source.go
@@ -0,0 +1,121 @@
+package arcresourcebridge
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"time"
+
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
+	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/identity"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/tags"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/resourceconnector/2022-10-27/appliances"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
+)
+
+type ArcResourceBridgeApplianceDataSource struct{}
+
+var _ sdk.DataSource = ArcResourceBridgeApplianceDataSource{}
+
+func (r ArcResourceBridgeApplianceDataSource) ResourceType() string {
+	return "azurerm_arc_resource_bridge_appliance"
+}
+
+func (r ArcResourceBridgeApplianceDataSource) ModelObject() interface{} {
+	return &ApplianceModel{}
+}
+
+func (r ArcResourceBridgeApplianceDataSource) Arguments() map[string]*pluginsdk.Schema {
+	return map[string]*pluginsdk.Schema{
+		"name": {
+			Type:     pluginsdk.TypeString,
+			Required: true,
+			ValidateFunc: validation.All(
+				validation.StringLenBetween(1, 260),
+				validation.StringMatch(regexp.MustCompile(`[^+#%&'?/,%\\]+$`), "any of '+', '#', '%', '&', ''', '?', '/', ',', '%', '&', '\\', are not allowed"),
+			),
+		},
+
+		"resource_group_name": commonschema.ResourceGroupNameForDataSource(),
+	}
+}
+
+func (r ArcResourceBridgeApplianceDataSource) Attributes() map[string]*pluginsdk.Schema {
+	return map[string]*pluginsdk.Schema{
+		"location": commonschema.LocationComputed(),
+
+		"distro": {
+			Type:     pluginsdk.TypeString,
+			Computed: true,
+		},
+
+		"identity": commonschema.SystemAssignedIdentityComputed(),
+
+		"infrastructure_provider": {
+			Type:     pluginsdk.TypeString,
+			Computed: true,
+		},
+
+		"public_key_base64": {
+			Type:     pluginsdk.TypeString,
+			Computed: true,
+		},
+
+		"tags": commonschema.TagsDataSource(),
+	}
+}
+
+func (r ArcResourceBridgeApplianceDataSource) Read() sdk.ResourceFunc {
+	return sdk.ResourceFunc{
+		Timeout: 5 * time.Minute,
+		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.ArcResourceBridge.AppliancesClient
+			subscriptionId := metadata.Client.Account.SubscriptionId
+
+			var model ApplianceModel
+			if err := metadata.Decode(&model); err != nil {
+				return fmt.Errorf("decoding: %+v", err)
+			}
+
+			id := appliances.NewApplianceID(subscriptionId, model.ResourceGroupName, model.Name)
+
+			resp, err := client.Get(ctx, id)
+			if err != nil {
+				if response.WasNotFound(resp.HttpResponse) {
+					return fmt.Errorf("%s does not exist", id)
+				}
+
+				return fmt.Errorf("retrieving %s: %+v", id, err)
+			}
+
+			state := ApplianceModel{
+				Name:              model.Name,
+				ResourceGroupName: model.ResourceGroupName,
+			}
+
+			if model := resp.Model; model != nil {
+				state.Location = location.Normalize(model.Location)
+				state.Identity = identity.FlattenSystemAssignedToModel(model.Identity)
+				state.Tags = tags.Flatten(model.Tags)
+
+				if props := model.Properties; props != nil {
+					state.Distro = pointer.From(props.Distro)
+					state.PublicKeyBase64 = pointer.From(props.PublicKey)
+
+					if infraConfig := props.InfrastructureConfig; infraConfig != nil {
+						state.Provider = pointer.From(infraConfig.Provider)
+					}
+				}
+			}
+
+			metadata.SetID(id)
+
+			return metadata.Encode(&state)
+		},
+	}
+}
