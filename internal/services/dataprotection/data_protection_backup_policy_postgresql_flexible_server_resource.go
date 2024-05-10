4b99c5fddf7dfdb738374174ded37d21935029f6
neil-yechenwei <yechenwei2007@hotmail.com>
2024-05-10 13:22:59 +0800

    use commonschema for vault it

--- internal/services/dataprotection/data_protection_backup_policy_postgresql_flexible_server_resource.go
+++ internal/services/dataprotection/data_protection_backup_policy_postgresql_flexible_server_resource.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/dataprotection/2024-04-01/backuppolicies"
 	azValidate "github.com/hashicorp/terraform-provider-azurerm/helpers/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
@@ -69,12 +70,7 @@ func (r DataProtectionBackupPolicyPostgreSQLFlexibleServerResource) Arguments() 
 			ValidateFunc: validate.BackupPolicyPostgreSQLFlexibleServerName,
 		},
 
-		"vault_id": {
-			Type:         pluginsdk.TypeString,
-			Required:     true,
-			ForceNew:     true,
-			ValidateFunc: backuppolicies.ValidateBackupVaultID,
-		},
+		"vault_id": commonschema.ResourceIDReferenceRequiredForceNew(pointer.To(backuppolicies.BackupVaultId{})),
 
 		"backup_repeating_time_intervals": {
 			Type:     pluginsdk.TypeList,
