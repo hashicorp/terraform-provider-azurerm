6dbe1ecfb059035e0f8dbb687c2e133556ab5fb1
cschar <cschar@users.noreply.github.com>
2024-05-13 05:57:16 -0400

    fix virtual_machine_scale_set probe example (#25939)
    - Updated the example to remove the unnecessary 'resource_group_name' attribute from the 'azurerm_lb_backend_address_pool' and 'azurerm_lb_probe' resources.

--- examples/vm-scale-set/virtual_machine_scale_set/loadbalancer-probe/main.tf
+++ examples/vm-scale-set/virtual_machine_scale_set/loadbalancer-probe/main.tf
@@ -44,13 +44,11 @@ resource "azurerm_lb" "example" {
 }
 
 resource "azurerm_lb_backend_address_pool" "example" {
-  resource_group_name = azurerm_resource_group.example.name
   loadbalancer_id     = azurerm_lb.example.id
   name                = "BackEndAddressPool"
 }
 
 resource "azurerm_lb_probe" "example" {
-  resource_group_name = azurerm_resource_group.example.name
   loadbalancer_id     = azurerm_lb.example.id
   name                = "ssh-probe"
   protocol            = "Tcp"
