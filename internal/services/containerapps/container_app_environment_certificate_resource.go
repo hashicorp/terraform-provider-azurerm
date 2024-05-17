f899edaed7433bcec42e6f0e8fb02b5cc5c3a481
Malthe Poulsen <malthe@grundtvigsvej.dk>
2024-05-17 08:55:40 +0200

    Add optional creation and management of managed certs based on creation params

--- internal/services/containerapps/container_app_environment_certificate_resource.go
+++ internal/services/containerapps/container_app_environment_certificate_resource.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/tags"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/containerapps/2023-05-01/certificates"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/containerapps/2023-05-01/managedenvironments"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/containerapps/2024-03-01/managedcertificates"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/containerapps/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
@@ -27,16 +28,22 @@ type ContainerAppCertificateModel struct {
 	ManagedEnvironmentId string                 `tfschema:"container_app_environment_id"`
 	Tags                 map[string]interface{} `tfschema:"tags"`
 
-	// Write only?
+	// Fields for Bring Your Own Certificate
 	CertificatePassword string `tfschema:"certificate_password"`
 	CertificateBlob     string `tfschema:"certificate_blob_base64"`
 
-	// Read Only
-	SubjectName    string `tfschema:"subject_name"`
+	// Fields for Managed Certificate
+	SubjectName             string `tfschema:"subject_name"`
+	DomainControlValidation string `tfschema:"domain_control_validation"`
+
+	// Read Only BYO
 	Issuer         string `tfschema:"issuer"`
 	IssueDate      string `tfschema:"issue_date"`
 	ExpirationDate string `tfschema:"expiration_date"`
 	Thumbprint     string `tfschema:"thumbprint"`
+
+	// Read Only Managed Certificate
+	ValidationToken string `tfschema:"validation_token"`
 }
 
 var _ sdk.ResourceWithUpdate = ContainerAppEnvironmentCertificateResource{}
@@ -73,18 +80,32 @@ func (r ContainerAppEnvironmentCertificateResource) Arguments() map[string]*plug
 
 		"certificate_blob_base64": {
 			Type:         pluginsdk.TypeString,
-			Required:     true,
+			Optional:     true,
 			ForceNew:     true,
 			ValidateFunc: validation.StringIsBase64,
-			Description:  "The Certificate Private Key as a base64 encoded PFX or PEM.",
+			Description:  "The Certificate Private Key as a base64 encoded PFX or PEM. Required for BYO Certificate.",
 		},
 
 		"certificate_password": {
 			Type:        pluginsdk.TypeString,
-			Required:    true,
+			Optional:    true,
 			ForceNew:    true,
 			Sensitive:   true,
-			Description: "The password for the Certificate.",
+			Description: "The password for the Certificate. Required for BYO Certificate.",
+		},
+
+		"subject_name": {
+			Type:        pluginsdk.TypeString,
+			Optional:    true,
+			ForceNew:    true,
+			Description: "The subject name of the managed certificate.",
+		},
+
+		"domain_control_validation": {
+			Type:        pluginsdk.TypeString,
+			Optional:    true,
+			ForceNew:    true,
+			Description: "The domain control validation method for the managed certificate.",
 		},
 
 		"tags": commonschema.Tags(),
@@ -129,8 +150,9 @@ func (r ContainerAppEnvironmentCertificateResource) Create() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Timeout: 30 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
-			client := metadata.Client.ContainerApps.CertificatesClient
-			environmentsClient := metadata.Client.ContainerApps.ManagedEnvironmentClient
+			certClient := metadata.Client.ContainerApps.CertificatesClient
+			envClient := metadata.Client.ContainerApps.ManagedEnvironmentClient
+			managedCertClient := metadata.Client.ContainerApps.ManagedCertificatesClient
 
 			var cert ContainerAppCertificateModel
 
@@ -143,45 +165,105 @@ func (r ContainerAppEnvironmentCertificateResource) Create() sdk.ResourceFunc {
 				return err
 			}
 
-			id := certificates.NewCertificateID(metadata.Client.Account.SubscriptionId, envId.ResourceGroupName, envId.ManagedEnvironmentName, cert.Name)
-			env, err := environmentsClient.Get(ctx, *envId)
-			if err != nil {
-				return fmt.Errorf("reading %s for %s: %+v", *envId, id, err)
-			}
+			if cert.CertificateBlob != "" && cert.CertificatePassword != "" {
+				// Create BYO Certificate
+				id := certificates.NewCertificateID(metadata.Client.Account.SubscriptionId, envId.ResourceGroupName, envId.ManagedEnvironmentName, cert.Name)
+				env, err := envClient.Get(ctx, *envId)
+				if err != nil {
+					return fmt.Errorf("reading %s for %s: %+v", *envId, id, err)
+				}
 
-			model := certificates.Certificate{
-				Location: env.Model.Location,
-				Name:     pointer.To(id.CertificateName),
-				Properties: &certificates.CertificateProperties{
-					Password: pointer.To(cert.CertificatePassword),
-					Value:    pointer.To(cert.CertificateBlob),
-				},
-				Tags: tags.Expand(cert.Tags),
-			}
+				model := certificates.Certificate{
+					Location: env.Model.Location,
+					Name:     pointer.To(id.CertificateName),
+					Properties: &certificates.CertificateProperties{
+						Password: pointer.To(cert.CertificatePassword),
+						Value:    pointer.To(cert.CertificateBlob),
+					},
+					Tags: tags.Expand(cert.Tags),
+				}
 
-			if _, err := client.CreateOrUpdate(ctx, id, model); err != nil {
-				return fmt.Errorf("creating %s: %+v", id, err)
-			}
+				if _, err := certClient.CreateOrUpdate(ctx, id, model); err != nil {
+					return fmt.Errorf("creating %s: %+v", id, err)
+				}
 
-			metadata.SetID(id)
+				metadata.SetID(id)
+			} else if cert.SubjectName != "" && cert.DomainControlValidation != "" {
+				// Create Managed Certificate
+				domainControlValidation, err := parseDomainControlValidation(cert.DomainControlValidation)
+				if err != nil {
+					return err
+				}
+
+				id := managedcertificates.NewManagedCertificateID(metadata.Client.Account.SubscriptionId, envId.ResourceGroupName, envId.ManagedEnvironmentName, cert.Name)
+				env, err := envClient.Get(ctx, *envId)
+				if err != nil {
+					return fmt.Errorf("reading %s for %s: %+v", *envId, id, err)
+				}
+
+				model := managedcertificates.ManagedCertificate{
+					Location: env.Model.Location,
+					Properties: &managedcertificates.ManagedCertificateProperties{
+						SubjectName:             pointer.To(cert.SubjectName),
+						DomainControlValidation: domainControlValidation,
+					},
+					Tags: tags.Expand(cert.Tags),
+				}
+
+				if err := managedCertClient.CreateOrUpdateThenPoll(ctx, id, model); err != nil {
+					return fmt.Errorf("creating %s: %+v", id, err)
+				}
+
+				metadata.SetID(id)
+			} else {
+				return fmt.Errorf("either certificate_blob_base64 and certificate_password or subject_name and domain_control_validation must be provided")
+			}
 
 			return nil
 		},
 	}
 }
-
 func (r ContainerAppEnvironmentCertificateResource) Read() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Timeout: 5 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
-			client := metadata.Client.ContainerApps.CertificatesClient
+			certClient := metadata.Client.ContainerApps.CertificatesClient
+			managedCertClient := metadata.Client.ContainerApps.ManagedCertificatesClient
 
 			id, err := certificates.ParseCertificateID(metadata.ResourceData.Id())
 			if err != nil {
-				return err
+				id, err := managedcertificates.ParseManagedCertificateID(metadata.ResourceData.Id())
+				if err != nil {
+					return err
+				}
+
+				existing, err := managedCertClient.Get(ctx, *id)
+				if err != nil {
+					if response.WasNotFound(existing.HttpResponse) {
+						return metadata.MarkAsGone(id)
+					}
+					return fmt.Errorf("reading %s: %+v", *id, err)
+				}
+
+				var state ContainerAppCertificateModel
+
+				state.Name = id.ManagedCertificateName
+				state.ManagedEnvironmentId = managedcertificates.NewManagedEnvironmentID(id.SubscriptionId, id.ResourceGroupName, id.ManagedEnvironmentName).ID()
+
+				if model := existing.Model; model != nil {
+					state.Tags = tags.Flatten(model.Tags)
+
+					if props := model.Properties; props != nil {
+						state.SubjectName = pointer.From(props.SubjectName)
+						state.ValidationToken = pointer.From(props.ValidationToken)
+
+					}
+				}
+
+				return metadata.Encode(&state)
 			}
 
-			existing, err := client.Get(ctx, *id)
+			existing, err := certClient.Get(ctx, *id)
 			if err != nil {
 				if response.WasNotFound(existing.HttpResponse) {
 					return metadata.MarkAsGone(id)
@@ -197,7 +279,6 @@ func (r ContainerAppEnvironmentCertificateResource) Read() sdk.ResourceFunc {
 			if model := existing.Model; model != nil {
 				state.Tags = tags.Flatten(model.Tags)
 
-				// The Certificate Blob and Password are not retrievable in any way, so grab them back from config if we can. Imports will need `ignore_changes`.
 				if certBlob, ok := metadata.ResourceData.GetOk("certificate_blob_base64"); ok {
 					state.CertificateBlob = certBlob.(string)
 				}
@@ -219,19 +300,28 @@ func (r ContainerAppEnvironmentCertificateResource) Read() sdk.ResourceFunc {
 		},
 	}
 }
-
 func (r ContainerAppEnvironmentCertificateResource) Delete() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Timeout: 30 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
-			client := metadata.Client.ContainerApps.CertificatesClient
+			certClient := metadata.Client.ContainerApps.CertificatesClient
+			managedCertClient := metadata.Client.ContainerApps.ManagedCertificatesClient
 
 			id, err := certificates.ParseCertificateID(metadata.ResourceData.Id())
 			if err != nil {
-				return err
+				id, err := managedcertificates.ParseManagedCertificateID(metadata.ResourceData.Id())
+				if err != nil {
+					return err
+				}
+
+				if _, err := managedCertClient.Delete(ctx, *id); err != nil {
+					return fmt.Errorf("deleting %s: %+v", *id, err)
+				}
+
+				return nil
 			}
 
-			if _, err := client.Delete(ctx, *id); err != nil {
+			if _, err := certClient.Delete(ctx, *id); err != nil {
 				return fmt.Errorf("deleting %s: %+v", *id, err)
 			}
 
@@ -244,7 +334,8 @@ func (r ContainerAppEnvironmentCertificateResource) Update() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Timeout: 30 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
-			client := metadata.Client.ContainerApps.CertificatesClient
+			certClient := metadata.Client.ContainerApps.CertificatesClient
+			managedCertClient := metadata.Client.ContainerApps.ManagedCertificatesClient
 
 			var cert ContainerAppCertificateModel
 
@@ -254,7 +345,22 @@ func (r ContainerAppEnvironmentCertificateResource) Update() sdk.ResourceFunc {
 
 			id, err := certificates.ParseCertificateID(metadata.ResourceData.Id())
 			if err != nil {
-				return err
+				id, err := managedcertificates.ParseManagedCertificateID(metadata.ResourceData.Id())
+				if err != nil {
+					return err
+				}
+
+				if metadata.ResourceData.HasChange("tags") {
+					patch := managedcertificates.ManagedCertificatePatch{
+						Tags: tags.Expand(cert.Tags),
+					}
+
+					if _, err = managedCertClient.Update(ctx, *id, patch); err != nil {
+						return fmt.Errorf("updating tags for %s: %+v", *id, err)
+					}
+				}
+
+				return nil
 			}
 
 			if metadata.ResourceData.HasChange("tags") {
@@ -262,7 +368,7 @@ func (r ContainerAppEnvironmentCertificateResource) Update() sdk.ResourceFunc {
 					Tags: tags.Expand(cert.Tags),
 				}
 
-				if _, err = client.Update(ctx, *id, patch); err != nil {
+				if _, err = certClient.Update(ctx, *id, patch); err != nil {
 					return fmt.Errorf("updating tags for %s: %+v", *id, err)
 				}
 			}
@@ -270,4 +376,16 @@ func (r ContainerAppEnvironmentCertificateResource) Update() sdk.ResourceFunc {
 			return nil
 		},
 	}
+}
+
+func parseDomainControlValidation(input string) (*managedcertificates.ManagedCertificateDomainControlValidation, error) {
+	vals := map[string]managedcertificates.ManagedCertificateDomainControlValidation{
+		"CNAME": managedcertificates.ManagedCertificateDomainControlValidationCNAME,
+		"HTTP":  managedcertificates.ManagedCertificateDomainControlValidationHTTP,
+		"TXT":   managedcertificates.ManagedCertificateDomainControlValidationTXT,
+	}
+	if v, ok := vals[input]; ok {
+		return &v, nil
+	}
+	return nil, fmt.Errorf("invalid DomainControlValidation value: %s", input)
 }
