0f68ccc8b999309977ae6bbba0bc981d125e8c2f
Vladimir Lazarenko <favoretti@gmail.com>
2024-05-22 19:49:32 +0200

    WIP

--- internal/services/network/network_watcher_flow_log_resource.go
+++ internal/services/network/network_watcher_flow_log_resource.go
@@ -73,7 +73,6 @@ func resourceNetworkWatcherFlowLog() *pluginsdk.Resource {
 
 			"target_resource_id": {
 				Type:         pluginsdk.TypeString,
-				Required:     true,
 				ForceNew:     true,
 				ValidateFunc: azure.ValidateResourceID,
 				ExactlyOneOf: []string{"network_security_group_id", "target_resource_id"},
@@ -175,7 +174,6 @@ func resourceNetworkWatcherFlowLog() *pluginsdk.Resource {
 	if !features.FivePointOh() {
 		resource.Schema["network_security_group_id"] = &pluginsdk.Schema{
 			Type:         pluginsdk.TypeString,
-			Required:     true,
 			ForceNew:     true,
 			ValidateFunc: azure.ValidateResourceID,
 			Deprecated:   "The property `network_security_group_id` has been superseded by `target_resource_id` and will be removed in version 5.0 of the AzureRM Provider.",
