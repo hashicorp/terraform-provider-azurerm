e3df483d79d33123257be508aca47c46f9e76476
neil-yechenwei <yechenwei2007@hotmail.com>
2024-05-23 19:58:03 +0800

    update doc

--- internal/services/network/bastion_host_resource_test.go
+++ internal/services/network/bastion_host_resource_test.go
@@ -32,18 +32,17 @@ func TestAccBastionHost_basic(t *testing.T) {
 	})
 }
 
-func TestAccBastionHost_developerSku(t *testing.T) {
+func TestAccBastionHost_standardSku(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_bastion_host", "test")
 	r := BastionHostResource{}
 
 	data.ResourceTest(t, r, []acceptance.TestStep{
 		{
-			Config: r.developerSku(data),
+			Config: r.standardSku(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 			),
 		},
-		data.ImportStep(),
 	})
 }
 
@@ -124,6 +123,21 @@ func TestAccBastionHost_sku(t *testing.T) {
 	})
 }
 
+func TestAccBastionHost_developerSku(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_bastion_host", "test")
+	r := BastionHostResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.developerSku(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func (BastionHostResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	id, err := bastionhosts.ParseBastionHostID(state.ID)
 	if err != nil {
@@ -185,44 +199,7 @@ resource "azurerm_bastion_host" "test" {
 `, data.RandomInteger, data.Locations.Primary, data.RandomString, data.RandomInteger, data.RandomString)
 }
 
-func (BastionHostResource) developerSku(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "test" {
-  name     = "acctestRG-bastion-%d"
-  location = "%s"
-}
-
-resource "azurerm_virtual_network" "test" {
-  name                = "acctestVNet%s"
-  address_space       = ["192.168.1.0/24"]
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-}
-
-resource "azurerm_subnet" "test" {
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = azurerm_resource_group.test.name
-  virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["192.168.1.224/27"]
-}
-
-resource "azurerm_bastion_host" "test" {
-  name                = "acctestBastion%s"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  sku                 = "Developer"
-  virtual_network_id  = azurerm_virtual_network.test.id
-
-  depends_on = [azurerm_subnet.test]
-}
-`, data.RandomInteger, data.Locations.Ternary, data.RandomString, data.RandomString)
-}
-
-func (BastionHostResource) complete(data acceptance.TestData) string {
+func (BastionHostResource) standardSku(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {}
@@ -260,12 +237,59 @@ resource "azurerm_bastion_host" "test" {
   location               = azurerm_resource_group.test.location
   resource_group_name    = azurerm_resource_group.test.name
   sku                    = "Standard"
-  copy_paste_enabled     = false
   file_copy_enabled      = true
   ip_connect_enabled     = true
   kerberos_enabled       = true
   shareable_link_enabled = true
   tunneling_enabled      = true
+
+  ip_configuration {
+    name                 = "ip-configuration"
+    subnet_id            = azurerm_subnet.test.id
+    public_ip_address_id = azurerm_public_ip.test.id
+  }
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomString, data.RandomInteger, data.RandomString)
+}
+
+func (BastionHostResource) complete(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-bastion-%d"
+  location = "%s"
+}
+
+resource "azurerm_virtual_network" "test" {
+  name                = "acctestVNet%s"
+  address_space       = ["192.168.1.0/24"]
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["192.168.1.224/27"]
+}
+
+resource "azurerm_public_ip" "test" {
+  name                = "acctestBastionPIP%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "test" {
+  name                = "acctestBastion%s"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  copy_paste_enabled  = false
 
   ip_configuration {
     name                 = "ip-configuration"
@@ -392,4 +416,33 @@ resource "azurerm_bastion_host" "test" {
   }
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomString, data.RandomInteger, data.RandomString, sku)
+}
+
+func (BastionHostResource) developerSku(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-bastion-%d"
+  location = "%s"
+}
+
+resource "azurerm_virtual_network" "test" {
+  name                = "acctestVNet%s"
+  address_space       = ["192.168.1.0/24"]
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_bastion_host" "test" {
+  name                = "acctestBastion%s"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "Developer"
+  virtual_network_id  = azurerm_virtual_network.test.id
+  copy_paste_enabled  = false
+}
+`, data.RandomInteger, data.Locations.Ternary, data.RandomString, data.RandomString)
 }
