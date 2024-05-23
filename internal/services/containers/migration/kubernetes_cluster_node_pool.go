09afd9bd8127829cea01feca7c4e67cc8c0479c4
Itay Grudev <itay.grudev@essentim.com>
2024-05-23 14:46:17 +0300

    Updated default values

--- internal/services/containers/migration/kubernetes_cluster_node_pool.go
+++ internal/services/containers/migration/kubernetes_cluster_node_pool.go
@@ -458,10 +458,12 @@ func (k KubernetesClusterNodePoolV0ToV1) Schema() map[string]*pluginsdk.Schema {
 					"drain_timeout_in_minutes": {
 						Type:     pluginsdk.TypeInt,
 						Optional: true,
+						Default:  30,
 					},
 					"node_soak_duration_in_minutes": {
 						Type:     pluginsdk.TypeInt,
 						Optional: true,
+						Default:  0,
 					},
 				},
 			},
