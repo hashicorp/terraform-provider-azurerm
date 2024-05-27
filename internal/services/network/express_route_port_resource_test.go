804beaec06c647f4f25d7e37393087c05e40be4f
Steph <steph@hashicorp.com>
2024-05-27 09:07:32 +0200

    update peering locations for circuit connection and port tests

--- internal/services/network/express_route_port_resource_test.go
+++ internal/services/network/express_route_port_resource_test.go
@@ -270,7 +270,7 @@ resource "azurerm_express_route_port" "test" {
   name                = "acctestERP-%[2]d"
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
-  peering_location    = "CDC-Canberra2"
+  peering_location    = "Airtel-Chennai2-CLS"
   bandwidth_in_gbps   = 10
   encapsulation       = "Dot1Q"
   identity {
