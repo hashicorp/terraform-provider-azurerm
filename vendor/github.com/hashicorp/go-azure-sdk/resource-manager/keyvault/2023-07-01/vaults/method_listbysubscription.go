60bfa0dcbec5e0b31eba9a0345e75c52089c796a
Steph <steph@hashicorp.com>
2024-05-27 16:05:37 +0200

    fix vendor deps

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/keyvault/2023-07-01/vaults/method_listbysubscription.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/keyvault/2023-07-01/vaults/method_listbysubscription.go
@@ -101,6 +101,7 @@ func (c VaultsClient) ListBySubscriptionCompleteMatchingPredicate(ctx context.Co
 
 	resp, err := c.ListBySubscription(ctx, id, options)
 	if err != nil {
+		result.LatestHttpResponse = resp.HttpResponse
 		err = fmt.Errorf("loading results: %+v", err)
 		return
 	}
