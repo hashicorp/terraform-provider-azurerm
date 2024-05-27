3ed9cbb8a40d408595d2bbd0889db7f2ed348f8f
stephybun <steph@hashicorp.com>
2024-05-27 16:16:41 +0200

    fix vendor deps (#26113)

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/keyvault/2023-07-01/vaults/method_list.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/keyvault/2023-07-01/vaults/method_list.go
@@ -105,6 +105,7 @@ func (c VaultsClient) ListCompleteMatchingPredicate(ctx context.Context, id comm
 
 	resp, err := c.List(ctx, id, options)
 	if err != nil {
+		result.LatestHttpResponse = resp.HttpResponse
 		err = fmt.Errorf("loading results: %+v", err)
 		return
 	}
--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/resources/2015-11-01/resources/method_list.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/resources/2015-11-01/resources/method_list.go
@@ -109,6 +109,7 @@ func (c ResourcesClient) ListCompleteMatchingPredicate(ctx context.Context, id c
 
 	resp, err := c.List(ctx, id, options)
 	if err != nil {
+		result.LatestHttpResponse = resp.HttpResponse
 		err = fmt.Errorf("loading results: %+v", err)
 		return
 	}
