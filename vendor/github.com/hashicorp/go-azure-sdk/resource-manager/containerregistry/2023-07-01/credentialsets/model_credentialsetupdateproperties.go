251b135f5dce1a6319769f545bc6d76024db1277
Noah Kontur <nkontur@amperon.co>
2024-05-28 10:32:01 -0400

    feat: update go dep files

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/containerregistry/2023-07-01/credentialsets/model_credentialsetupdateproperties.go
+++
@@ -1,8 +0,0 @@
-package credentialsets
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type CredentialSetUpdateProperties struct {
-	AuthCredentials *[]AuthCredential `json:"authCredentials,omitempty"`
-}
--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/containerregistry/2023-07-01/webhooks/model_source.go
+++
@@ -1,9 +0,0 @@
-package webhooks
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type Source struct {
-	Addr       *string `json:"addr,omitempty"`
-	InstanceID *string `json:"instanceID,omitempty"`
-}
