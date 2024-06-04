22b839a19b46294e58e76fd0db1596771a7c7524
Jerome Brown <Jerome@SoliDeoGloria.tech>
2024-06-04 22:52:10 +1200

    Update deps

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/authorization/2020-10-01/rolemanagementpolicyassignments/method_listforscope.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/authorization/2020-10-01/rolemanagementpolicyassignments/method_listforscope.go
@@ -73,6 +73,7 @@ func (c RoleManagementPolicyAssignmentsClient) ListForScopeCompleteMatchingPredi
 
 	resp, err := c.ListForScope(ctx, id)
 	if err != nil {
+		result.LatestHttpResponse = resp.HttpResponse
 		err = fmt.Errorf("loading results: %+v", err)
 		return
 	}
