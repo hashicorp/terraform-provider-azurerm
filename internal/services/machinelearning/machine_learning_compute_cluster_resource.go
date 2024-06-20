bcb49009a0ae9a8913066a50a0dea37deda7c5e4
xu <xuzhang3@microsoft.com>
2024-06-20 15:45:10 +0800

    add tiomeout

--- internal/services/machinelearning/machine_learning_compute_cluster_resource.go
+++ internal/services/machinelearning/machine_learning_compute_cluster_resource.go
@@ -38,6 +38,7 @@ func resourceComputeCluster() *pluginsdk.Resource {
 		Timeouts: &pluginsdk.ResourceTimeout{
 			Create: pluginsdk.DefaultTimeout(30 * time.Minute),
 			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
+			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
 			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
 		},
 
