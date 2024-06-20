d76d286b40a2c7dd26c6bb9c1b7f9a639e4bc940
stephybun <steph@hashicorp.com>
2024-06-20 07:07:44 +0200

    `azurerm_maps_account` - add `cors`, `data_store`, `location` and `identity` (#26397)
    * add cors, data_store, location and identity to maps account
    
    * make allowed_origins required
    
    * casing on global location
    
    * add location override for maps
    
    * add tags to update

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/maps/2023-06-01/creators/README.md
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/maps/2023-06-01/creators/README.md
@@ -1,14 +1,14 @@
 
-## `github.com/hashicorp/go-azure-sdk/resource-manager/maps/2021-02-01/creators` Documentation
+## `github.com/hashicorp/go-azure-sdk/resource-manager/maps/2023-06-01/creators` Documentation
 
-The `creators` SDK allows for interaction with the Azure Resource Manager Service `maps` (API Version `2021-02-01`).
+The `creators` SDK allows for interaction with the Azure Resource Manager Service `maps` (API Version `2023-06-01`).
 
 This readme covers example usages, but further information on [using this SDK can be found in the project root](https://github.com/hashicorp/go-azure-sdk/tree/main/docs).
 
 ### Import Path
 
 ```go
-import "github.com/hashicorp/go-azure-sdk/resource-manager/maps/2021-02-01/creators"
+import "github.com/hashicorp/go-azure-sdk/resource-manager/maps/2023-06-01/creators"
 ```
 
 
--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/maps/2023-06-01/creators/model_creator.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/maps/2023-06-01/creators/model_creator.go
@@ -1,4 +1,4 @@
-package accounts
+package creators
 
 import (
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/systemdata"
@@ -7,13 +7,11 @@ import (
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-type MapsAccount struct {
+type Creator struct {
 	Id         *string                `json:"id,omitempty"`
-	Kind       *Kind                  `json:"kind,omitempty"`
 	Location   string                 `json:"location"`
 	Name       *string                `json:"name,omitempty"`
-	Properties *MapsAccountProperties `json:"properties,omitempty"`
-	Sku        Sku                    `json:"sku"`
+	Properties CreatorProperties      `json:"properties"`
 	SystemData *systemdata.SystemData `json:"systemData,omitempty"`
 	Tags       *map[string]string     `json:"tags,omitempty"`
 	Type       *string                `json:"type,omitempty"`
