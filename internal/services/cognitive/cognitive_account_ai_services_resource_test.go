c8e01d3cbd40850988fcf3695e27ca512a453fd5
xu <xuzhang3@microsoft.com>
2024-06-20 10:28:24 +0800

    update test case

--- internal/services/cognitive/cognitive_account_ai_services_resource_test.go
+++ internal/services/cognitive/cognitive_account_ai_services_resource_test.go
@@ -912,10 +912,9 @@ resource "azurerm_key_vault_certificate" "cert" {
   }
 }
 resource "azurerm_key_vault_managed_hardware_security_module" "test" {
-  name                = "kvHsm%[1]d"
-  resource_group_name = azurerm_resource_group.test.name
-  #   location                 = azurerm_resource_group.test.location
-  location                 = "westus"
+  name                     = "kvHsm%[1]d"
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
   sku_name                 = "Standard_B1"
   tenant_id                = data.azurerm_client_config.current.tenant_id
   admin_object_ids         = [data.azurerm_client_config.current.object_id]
