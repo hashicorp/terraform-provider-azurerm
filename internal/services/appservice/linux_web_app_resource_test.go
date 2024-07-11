13c5baf5cf05f9e24e29b780f19330d674b609f2
Marcelo Zambrana Villarroel <marcech.am@gmail.com>
2024-07-11 10:22:06 -0400

    azurerm_linux_web_app  - support 8.3 for the php_version property (#26194)
    * Adding support for PHP 8.3 in Linux
    
    * fixing linting in test cases
    
    * Update internal/services/appservice/linux_web_app_slot_resource_test.go
    
    removing optional properties
    
    Co-authored-by: stephybun <steph@hashicorp.com>
    
    * fixing linting issues
    
    * removing not needed properties
    
    ---------
    
    Co-authored-by: stephybun <steph@hashicorp.com>

--- internal/services/appservice/linux_web_app_resource_test.go
+++ internal/services/appservice/linux_web_app_resource_test.go
@@ -846,6 +846,21 @@ func TestAccLinuxWebApp_withPhp82(t *testing.T) {
 	})
 }
 
+func TestAccLinuxWebApp_withPhp83(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_linux_web_app", "test")
+	r := LinuxWebAppResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.php(data, "8.3"),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("site_credential.0.password"),
+	})
+}
+
 func TestAccLinuxWebApp_withPython37(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_linux_web_app", "test")
 	r := LinuxWebAppResource{}
