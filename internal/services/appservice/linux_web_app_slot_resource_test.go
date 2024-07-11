e79ead08b9feb29f4af8e38042b012ea8787c227
Marcelo Zambrana Villarroel <marcech.am@gmail.com>
2024-07-11 03:38:36 +0000

    fixing linting issues

--- internal/services/appservice/linux_web_app_slot_resource_test.go
+++ internal/services/appservice/linux_web_app_slot_resource_test.go
@@ -2990,10 +2990,10 @@ resource "azurerm_service_plan" "test" {
 }
 
 resource "azurerm_linux_web_app" "test" {
-  name                                           = "acctestWA-%[1]d"
-  location                                       = azurerm_resource_group.test.location
-  resource_group_name                            = azurerm_resource_group.test.name
-  service_plan_id                                = azurerm_service_plan.test.id
+  name                = "acctestWA-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  service_plan_id     = azurerm_service_plan.test.id
 
   site_config {}
 }
