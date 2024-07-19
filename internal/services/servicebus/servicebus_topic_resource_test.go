f1fd24c1a60279952f072aaaa50c79811d8ff243
Xiaxin Yi (from Dev Box) <xiaxyi@microsoft.com>
2024-07-19 14:00:35 +0800

    simplify the test checks

--- internal/services/servicebus/servicebus_topic_resource_test.go
+++ internal/services/servicebus/servicebus_topic_resource_test.go
@@ -262,7 +262,6 @@ func TestAccServiceBusTopic_partitionedPremiumNamespace(t *testing.T) {
 			Config: r.PremiumNamespacePartitioned(data, true),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
-				check.That(data.ResourceName).Key("enable_partitioning").HasValue("true"),
 			),
 		},
 		data.ImportStep(),
