0630100b0e8459469ead02caa487e94b2df54989
wuxu92 <wuxu92@outlook.com>
2024-07-23 09:22:22 +0800

    remove basic sku

--- internal/services/network/network_interface_nat_rule_association_resource_test.go
+++ internal/services/network/network_interface_nat_rule_association_resource_test.go
@@ -287,7 +287,6 @@ resource "azurerm_public_ip" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   allocation_method   = "Static"
-  sku                 = "Basic"
 }
 
 resource "azurerm_lb" "test" {
--- internal/services/network/network_interface_resource_test.go
+++ internal/services/network/network_interface_resource_test.go
@@ -859,7 +859,6 @@ resource "azurerm_public_ip" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   allocation_method   = "Static"
-  sku                 = "Basic"
 }
 `, r.template(data), data.RandomInteger)
 }
