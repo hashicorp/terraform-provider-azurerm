3c688020e9eef794d7e8070346f8bb2da1280254
wuxu92 <wuxu92@outlook.com>
2024-07-23 09:05:50 +0800

    add miss ignore_changes for subnet

--- internal/services/network/subnet_data_source_test.go
+++ internal/services/network/subnet_data_source_test.go
@@ -288,6 +288,10 @@ resource "azurerm_virtual_network" "test" {
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
   address_space       = ["10.0.0.0/16"]
+
+  lifecycle {
+    ignore_changes = [subnet]
+  }
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
 }
