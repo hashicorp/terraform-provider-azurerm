fe575ebf49fcd8181c700ddb6165eabc8e973050
Steph <steph@hashicorp.com>
2024-07-23 14:03:13 +0200

    fix various failures and panics

--- internal/services/compute/windows_virtual_machine_resource_network_test.go
+++ internal/services/compute/windows_virtual_machine_resource_network_test.go
@@ -476,6 +476,7 @@ resource "azurerm_public_ip" "first" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   allocation_method   = "Dynamic"
+  sku                 = "Basic"
 }
 
 resource "azurerm_network_interface" "first" {
@@ -496,6 +497,7 @@ resource "azurerm_public_ip" "second" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   allocation_method   = "Dynamic"
+  sku                 = "Basic"
 }
 
 resource "azurerm_network_interface" "second" {
