c628815fea2d0985157f020a2cdff77abfbc3c29
Tom Bamford <tom@bamford.io>
2024-07-26 14:36:42 +0100

    `elasticsan`, `eventhub`, `healthcare`, `hsm` - reverting `ignore_changes` (#26834)
    * elasticsan: reverting `ignore_changes`
    
    * eventhub: reverting `ignore_changes`
    
    * healthcare: reverting `ignore_changes`
    
    * hsm: reverting `ignore_changes`

--- internal/services/hsm/dedicated_hardware_security_module_resource_test.go
+++ internal/services/hsm/dedicated_hardware_security_module_resource_test.go
@@ -134,10 +134,6 @@ resource "azurerm_virtual_network" "test" {
   address_space       = ["10.2.0.0/16"]
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-
-  lifecycle {
-    ignore_changes = [subnet]
-  }
 }
 
 resource "azurerm_subnet" "test2" {
