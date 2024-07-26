01533eeed3c9bf8a00d5a15f020b2354e916cc08
Zhenhua Hu <zhhu@microsoft.com>
2024-07-26 15:54:43 +0800

    fix lint

--- internal/services/advisor/advisor_recommendations_data_source_test.go
+++ internal/services/advisor/advisor_recommendations_data_source_test.go
@@ -84,9 +84,9 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_storage_account" "test" {
-  name                = "accteststr%s"
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
+  name                     = "accteststr%s"
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
