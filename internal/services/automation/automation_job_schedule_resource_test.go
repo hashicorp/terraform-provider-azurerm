a2d6e31104496983665d4dc37c4ade51524a1636
Tom Bamford <tom@bamford.io>
2024-07-26 14:09:20 +0100

    automation: reverting `ignore_changes`

--- internal/services/automation/automation_job_schedule_resource_test.go
+++ internal/services/automation/automation_job_schedule_resource_test.go
@@ -183,10 +183,6 @@ resource "azurerm_automation_runbook" "test" {
   )
   "Hello, " + $Output + "!"
 EOF
-
-  lifecycle {
-    ignore_changes = [job_schedule] # not computed from 4.0
-  }
 }
 
 resource "azurerm_automation_schedule" "test" {
