9838a8e8acc215386f52f943489bb043188cd0fd
catriona-m <86247157+catriona-m@users.noreply.github.com>
2024-07-26 11:11:48 +0100

    `appservice` - remove ignore_changes in tests (#26810)

--- internal/services/appservice/linux_web_app_resource_test.go
+++ internal/services/appservice/linux_web_app_resource_test.go
@@ -3824,10 +3824,6 @@ resource "azurerm_virtual_network" "test" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-
-  lifecycle {
-    ignore_changes = [subnet]
-  }
 }
 
 resource "azurerm_subnet" "test1" {
@@ -3886,10 +3882,6 @@ resource "azurerm_virtual_network" "test" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-
-  lifecycle {
-    ignore_changes = [subnet]
-  }
 }
 
 resource "azurerm_subnet" "test1" {
@@ -3949,10 +3941,6 @@ resource "azurerm_virtual_network" "test" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-
-  lifecycle {
-    ignore_changes = [subnet]
-  }
 }
 
 resource "azurerm_subnet" "test1" {
