e280ab8bf9f30e9c2849b4fc2c37d0a0e7718fbe
Zhenhua Hu <zhhu@microsoft.com>
2024-07-30 15:32:15 +0800

    `Compute` - fix acc tests with `connection timed out` error (#26843)
    * fix `connection timed out` error test cases
    
    * fix `connection timeout` error in `linux_virtual_machine_resource_images_test.go`

--- internal/services/compute/image_resource_test.go
+++ internal/services/compute/image_resource_test.go
@@ -919,6 +919,7 @@ resource "azurerm_public_ip" "test" {
   resource_group_name = azurerm_resource_group.test.name
   allocation_method   = "Static"
   domain_name_label   = local.domain_name_label
+  sku                 = "Basic"
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomString, data.RandomString, data.RandomInteger, data.RandomInteger)
 }
