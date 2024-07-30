a2c0fba3330564519d7dbe0d02930d5c47fd1ea5
neil-yechenwei <yechenwei2007@hotmail.com>
2024-07-30 15:06:07 +0800

    New Resource: azurerm_dev_center_project_pool

--- internal/services/devcenter/registration.go
+++ internal/services/devcenter/registration.go
@@ -35,6 +35,7 @@ func (r Registration) Resources() []sdk.Resource {
 		DevCenterCatalogsResource{},
 		DevCenterDevBoxDefinitionResource{},
 		DevCenterEnvironmentTypeResource{},
+		DevCenterProjectPoolResource{},
 	}
 	return append(resources, r.autoRegistration.Resources()...)
 }
