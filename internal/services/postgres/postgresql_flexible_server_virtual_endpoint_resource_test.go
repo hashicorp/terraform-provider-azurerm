d26fe7fd2154528805e6b55471c0c7ba6591d466
Bruce Harrison <bruce.harrison@hashicorp.com>
2024-08-01 09:15:41 -0500

    remove test provider feature

--- internal/services/postgres/postgresql_flexible_server_virtual_endpoint_resource_test.go
+++ internal/services/postgres/postgresql_flexible_server_virtual_endpoint_resource_test.go
@@ -72,11 +72,7 @@ func (r PostgresqlFlexibleServerVirtualEndpointResource) Destroy(ctx context.Con
 func (PostgresqlFlexibleServerVirtualEndpointResource) basic(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+  features {}
 }
 
 resource "azurerm_resource_group" "test" {
