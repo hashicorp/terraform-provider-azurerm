4c316163a01bdcf10324102fc9c1ab86f72b2f48
teowa <104055472+teowa@users.noreply.github.com>
2024-08-01 06:08:29 +0000

    fix prometheus rule group update test

--- internal/services/monitor/monitor_alert_prometheus_rule_group_resource_test.go
+++ internal/services/monitor/monitor_alert_prometheus_rule_group_resource_test.go
@@ -291,6 +291,9 @@ resource "azurerm_kubernetes_cluster" "test2" {
     node_count             = 2
     vm_size                = "Standard_DS2_v2"
     enable_host_encryption = true
+    upgrade_settings {
+      max_surge = "10%%"
+    }
   }
 
   identity {
