ac3b07a436b8ca034ebfdf8ae02b0f88550c2241
Bruce Harrison <bruce.harrison@hashicorp.com>
2024-08-05 11:14:20 -0500

    move description to correct property

--- internal/services/compute/image_resource.go
+++ internal/services/compute/image_resource.go
@@ -146,6 +146,7 @@ func resourceImage() *pluginsdk.Resource {
 
 						"storage_type": {
 							Type:         pluginsdk.TypeString,
+							Description:  "The type of storage disk",
 							Optional:     true,
 							ForceNew:     true,
 							ValidateFunc: validation.StringInSlice(images.PossibleValuesForStorageAccountTypes(), false),
@@ -173,7 +174,6 @@ func resourceImage() *pluginsdk.Resource {
 
 						"blob_uri": {
 							Type:         pluginsdk.TypeString,
-							Description:  "The type of storage disk",
 							Optional:     true,
 							Computed:     true,
 							ValidateFunc: validation.IsURLWithScheme([]string{"http", "https"}),
