fcc137d0da4bd0123e921f1a88f9908c17205244
Tom Bamford <tom@bamford.io>
2024-08-08 12:10:52 +0100

    azurerm_storage_account: make `large_file_share_enabled` optional+computed again, since it is conditionally ForceNew and has a moving default

--- internal/services/storage/storage_account_resource_test.go
+++ internal/services/storage/storage_account_resource_test.go
@@ -888,6 +888,7 @@ func TestAccStorageAccount_replicationTypeGZRS(t *testing.T) {
 }
 
 func TestAccStorageAccount_largeFileShare(t *testing.T) {
+	// Note: this test causes a ForceNew when disabling large file shares
 	data := acceptance.BuildTestData(t, "azurerm_storage_account", "test")
 	r := StorageAccountResource{}
 
@@ -4122,7 +4123,6 @@ resource "azurerm_storage_account" "test" {
   account_tier                      = "Premium"
   account_replication_type          = "LRS"
   infrastructure_encryption_enabled = true
-  large_file_share_enabled          = true # defaulted on the API side
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomString)
 }
@@ -4612,7 +4612,6 @@ resource "azurerm_storage_account" "test" {
   account_tier             = "Premium"
   account_kind             = "FileStorage"
   account_replication_type = "ZRS"
-  large_file_share_enabled = true # defaulted in the API when FileStorage & Premium
 
   share_properties {
     smb {
