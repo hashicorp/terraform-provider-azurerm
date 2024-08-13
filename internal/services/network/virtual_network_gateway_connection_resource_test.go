2a64975cce5467a5063234e857d9d9a315771e4c
neil-yechenwei <yechenwei2007@hotmail.com>
2024-08-14 07:32:37 +0800

    update tc

--- internal/services/network/virtual_network_gateway_connection_resource_test.go
+++ internal/services/network/virtual_network_gateway_connection_resource_test.go
@@ -67,7 +67,7 @@ func TestAccVirtualNetworkGatewayConnection_siteToSiteWithoutSharedKey(t *testin
 }
 
 func TestAccVirtualNetworkGatewayConnection_expressRoute(t *testing.T) {
-	data := acceptance.BuildTestData(t, "azurerm_virtual_network_gateway_connection", "test")
+	data := acceptance.BuildTestData(t, "test ", "test")
 	r := VirtualNetworkGatewayConnectionResource{}
 
 	data.ResourceTest(t, r, []acceptance.TestStep{
@@ -586,6 +586,7 @@ resource "azurerm_virtual_network_gateway_connection" "test" {
   express_route_circuit_id   = azurerm_express_route_circuit.test.id
   authorization_key          = azurerm_express_route_circuit_authorization.test.authorization_key
   routing_weight             = "0"
+  shared_key                 = "4-v3ry-53cr37-1p53c-5h4r3d-k3y"
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
 }
@@ -695,6 +696,7 @@ resource "azurerm_virtual_network_gateway_connection" "test" {
   routing_weight                 = "0"
   express_route_gateway_bypass   = true
   private_link_fast_path_enabled = true
+  shared_key                     = "4-v3ry-53cr37-1p53c-5h4r3d-k3y"
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
 }
