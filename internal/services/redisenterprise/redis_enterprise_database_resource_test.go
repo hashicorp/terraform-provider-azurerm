1fb9b2d558c62ac7a2b31eb05cc2216ca58743a4
xu <xuzhang3@microsoft.com>
2024-08-16 17:13:06 +0800

    format file

--- internal/services/redisenterprise/redis_enterprise_database_resource_test.go
+++ internal/services/redisenterprise/redis_enterprise_database_resource_test.go
@@ -169,20 +169,20 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_redis_enterprise_cluster" "test" {
-  name                = "acctest-rec-%d"
-  location            = azurerm_resource_group.test.location
+  name     = "acctest-rec-%d"
+  location = azurerm_resource_group.test.location
 
   sku_name = "Enterprise_E20-4"
 }
 resource "azurerm_redis_enterprise_cluster" "test1" {
-  name                = "acctest-rec-%d"
-  location            = azurerm_resource_group.test.location
+  name     = "acctest-rec-%d"
+  location = azurerm_resource_group.test.location
 
   sku_name = "Enterprise_E20-4"
 }
 resource "azurerm_redis_enterprise_cluster" "test2" {
-  name                = "acctest-rec-%d"
-  location            = azurerm_resource_group.test.location
+  name     = "acctest-rec-%d"
+  location = azurerm_resource_group.test.location
 
   sku_name = "Enterprise_E20-4"
 }
