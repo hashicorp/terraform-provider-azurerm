882fecdf6c89a298bdeeab9cd59e6e94af080834
Gerry Tan <gerry.g.tan@gmail.com>
2024-08-19 15:33:22 +1000

    Added nil check on redis_cache_data_source.go
    Co-authored-by: magodo <wztdyl@sina.com>

--- internal/services/redis/redis_cache_data_source.go
+++ internal/services/redis/redis_cache_data_source.go
@@ -349,7 +349,11 @@ func dataSourceRedisCacheRead(d *pluginsdk.ResourceData, meta interface{}) error
 		d.Set("primary_connection_string", getRedisConnectionString(*props.HostName, *props.SslPort, *keys.Model.PrimaryKey, enableSslPort))
 		d.Set("secondary_connection_string", getRedisConnectionString(*props.HostName, *props.SslPort, *keys.Model.SecondaryKey, enableSslPort))
 
-		d.Set("access_keys_authentication_enabled", !(*props.DisableAccessKeyAuthentication))
+                accessKeyAuthEnabled := true
+                if props.DisableAccessKeyAuthentication != nil {
+                    accessKeyAuthEnabled = !(*props.DisableAccessKeyAuthentication)
+                }
+		d.Set("access_keys_authentication_enabled", accessKeyAuthEnabled)
 
 		if err := tags.FlattenAndSet(d, model.Tags); err != nil {
 			return fmt.Errorf("setting `tags`: %+v", err)
