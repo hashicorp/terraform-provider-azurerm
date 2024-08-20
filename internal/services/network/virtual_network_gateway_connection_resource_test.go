9d6dd6a3ed78e0ab63e544f74cf9682faa04fccc
lev <slive77@gmail.com>
2024-08-20 11:52:45 +0200

    Update internal/services/network/virtual_network_gateway_connection_resource_test.go
    Co-authored-by: stephybun <steph@hashicorp.com>

--- internal/services/network/virtual_network_gateway_connection_resource_test.go
+++ internal/services/network/virtual_network_gateway_connection_resource_test.go
@@ -1094,7 +1094,7 @@ resource "azurerm_virtual_network_gateway_connection" "test" {
     ipsec_encryption = "AES256"
     ipsec_integrity  = "SHA256"
     pfs_group        = "PFS2048"
-    sa_datasize      = 0
+    sa_datasize      = 102400000
     sa_lifetime      = 27000
   }
 
