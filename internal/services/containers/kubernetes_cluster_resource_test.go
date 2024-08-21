756aa920cefa6edb960e42c605c4c1ec4f54741a
Heng Lu <henglu@microsoft.com>
2024-08-21 09:46:13 +0800

    make terrafmt

--- internal/services/containers/kubernetes_cluster_resource_test.go
+++ internal/services/containers/kubernetes_cluster_resource_test.go
@@ -438,9 +438,9 @@ resource "azurerm_subnet_nat_gateway_association" "node" {
 }
 
 resource "azurerm_route_table" "test" {
-  name                          = "test"
-  location                      = azurerm_resource_group.test.location
-  resource_group_name           = azurerm_resource_group.test.name
+  name                = "test"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
   route {
     name           = "internal"
     address_prefix = tolist(azurerm_virtual_network.test.address_space)[0]
