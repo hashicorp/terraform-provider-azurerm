6f59477d3355472e0eb4538c765fddf5c6d943b8
Xiaxin Yi (from Dev Box) <xiaxyi@microsoft.com>
2024-08-27 10:30:26 +0800

    update vendor

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/web/2023-01-01/webapps/model_sitecloneabilitycriterion.go
+++
@@ -1,9 +0,0 @@
-package webapps
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type SiteCloneabilityCriterion struct {
-	Description *string `json:"description,omitempty"`
-	Name        *string `json:"name,omitempty"`
-}
