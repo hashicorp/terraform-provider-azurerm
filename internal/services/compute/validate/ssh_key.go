2d7377c4683e3cc8c7b77583a13e4b3c1a6499ff
Gerry Tan <gerry.tan@microsoft.com>
2024-08-28 13:53:40 +1000

    Merge changes from #26615

--- internal/services/compute/validate/ssh_key.go
+++ internal/services/compute/validate/ssh_key.go
@@ -4,6 +4,7 @@
 package validate
 
 import (
+	"crypto/ed25519"
 	"crypto/rsa"
 	"encoding/base64"
 	"fmt"
@@ -46,11 +47,13 @@ func SSHKey(i interface{}, k string) (warnings []string, errors []error) {
 				return nil, []error{fmt.Errorf("- the provided RSA SSH key has %d bits. Only ssh-rsa keys with 2048 bits or higher are supported by Azure", rsaPubKeyBits)}
 			}
 		case ssh.KeyAlgoED25519:
-			return
+			_, ok := pubKey.(ssh.CryptoPublicKey).CryptoPublicKey().(ed25519.PublicKey)
+			if !ok {
+				return nil, []error{fmt.Errorf("- could not retrieve the ED25519 public key from the SSH public key")}
+			}
 		default:
-			return nil, []error{fmt.Errorf("- the provided %s SSH key is not supported. Only RSA and ed25519 SSH keys are supported by Azure", pubKey.Type())}
+			return nil, []error{fmt.Errorf("- the provided %s SSH key is not supported. Only RSA and ED25519 SSH keys are supported by Azure", pubKey.Type())}
 		}
-
 	} else {
 		return nil, []error{fmt.Errorf("%q is not a complete SSH2 Public Key", k)}
 	}
