ff695d50f590ae056f58a13d67bfda6c21904436
neil-yechenwei <yechenwei2007@hotmail.com>
2024-09-05 20:12:09 +0800

    azurerm_mysql_flexible_server - set source_server_id in the state file

--- internal/services/mysql/mysql_flexible_server_resource_test.go
+++ internal/services/mysql/mysql_flexible_server_resource_test.go
@@ -535,7 +535,7 @@ resource "azurerm_resource_group" "test" {
   name     = "acctestRG-mysql-%d"
   location = "%s"
 }
-`, data.RandomInteger, data.Locations.Ternary)
+`, data.RandomInteger, data.Locations.Primary)
 }
 
 func (r MySqlFlexibleServerResource) basic(data acceptance.TestData) string {
