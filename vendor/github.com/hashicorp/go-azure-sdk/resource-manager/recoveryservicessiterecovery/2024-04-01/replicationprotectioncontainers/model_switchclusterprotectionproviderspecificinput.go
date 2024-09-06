cf2c9256a6629f6f9a8f954ff553bcb2e3aed2f1
catriona-m <86247157+catriona-m@users.noreply.github.com>
2024-09-06 09:58:38 +0100

    upgrade recoveryservicessiterecovery to 2024-04-01 (#27281)

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/recoveryservicessiterecovery/2024-04-01/replicationprotectioncontainers/model_switchclusterprotectionproviderspecificinput.go
@@ -0,0 +1,53 @@
+package replicationprotectioncontainers
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type SwitchClusterProtectionProviderSpecificInput interface {
+}
+
+// RawSwitchClusterProtectionProviderSpecificInputImpl is returned when the Discriminated Value
+// doesn't match any of the defined types
+// NOTE: this should only be used when a type isn't defined for this type of Object (as a workaround)
+// and is used only for Deserialization (e.g. this cannot be used as a Request Payload).
+type RawSwitchClusterProtectionProviderSpecificInputImpl struct {
+	Type   string
+	Values map[string]interface{}
+}
+
+func unmarshalSwitchClusterProtectionProviderSpecificInputImplementation(input []byte) (SwitchClusterProtectionProviderSpecificInput, error) {
+	if input == nil {
+		return nil, nil
+	}
+
+	var temp map[string]interface{}
+	if err := json.Unmarshal(input, &temp); err != nil {
+		return nil, fmt.Errorf("unmarshaling SwitchClusterProtectionProviderSpecificInput into map[string]interface: %+v", err)
+	}
+
+	value, ok := temp["instanceType"].(string)
+	if !ok {
+		return nil, nil
+	}
+
+	if strings.EqualFold(value, "A2A") {
+		var out A2ASwitchClusterProtectionInput
+		if err := json.Unmarshal(input, &out); err != nil {
+			return nil, fmt.Errorf("unmarshaling into A2ASwitchClusterProtectionInput: %+v", err)
+		}
+		return out, nil
+	}
+
+	out := RawSwitchClusterProtectionProviderSpecificInputImpl{
+		Type:   value,
+		Values: temp,
+	}
+	return out, nil
+
+}
