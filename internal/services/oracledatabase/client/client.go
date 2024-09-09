5500b39a3b740ed5d83ba730c6150c8dd4b17f1f
Eamon El-Homsi <eamon.el-homsi@oracle.com>
2024-09-09 09:37:41 +0100

    * Explain disabling of correlationId being provided by AzureRM.

--- internal/services/oracledatabase/client/client.go
+++ internal/services/oracledatabase/client/client.go
@@ -13,6 +13,9 @@ type Client struct {
 }
 
 func NewClient(o *common.ClientOptions) (*Client, error) {
+	// ORP (backend for ODB@A) partly builds its idempotency keys based on correlationIds sent by client.
+	// It seems that AzureRM provider sends the same correlationId for each request during an apply.
+	// We need each request to have a different correlationId. By disabling this, Azure will provide a unique correlationId instead.
 	o.DisableCorrelationRequestID = true
 	oracleDatabaseClient, err := oracedatabase.NewClientWithBaseURI(o.Environment.ResourceManager, func(c *resourcemanager.Client) {
 		o.Configure(c, o.Authorizers.ResourceManager)
