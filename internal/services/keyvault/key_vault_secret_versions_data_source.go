0e4d7d1036876410753af69309c298830ff51330
ned1313 <ned.bellavance@gmail.com>
2024-09-12 13:35:29 -0400

    int32 error

--- internal/services/keyvault/key_vault_secret_versions_data_source.go
+++ internal/services/keyvault/key_vault_secret_versions_data_source.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	keyVaultValidate "github.com/hashicorp/terraform-provider-azurerm/internal/services/keyvault/validate"
 	"github.com/tombuildsstuff/kermit/sdk/keyvault/7.4/keyvault"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 )
 
 var _ sdk.DataSource = KeyVaultSecretVersionsDataSource{}
@@ -22,6 +23,7 @@ type KeyVaultSecretVersionsDataSource struct{}
 type KeyVaultSecretVersionsDataSourceModel struct {
 	Name       string               `tfschema:"name"`
 	KeyVaultId string               `tfschema:"key_vault_id"`
+	MaxResults int32                `tfschema:"max_results"`
 	Versions   []secretVersionModel `tfschema:"versions"`
 }
 
@@ -45,7 +47,7 @@ func (r KeyVaultSecretVersionsDataSource) Arguments() map[string]*schema.Schema 
 		"key_vault_id": {
 			Type:         schema.TypeString,
 			Required:     true,
-			ValidateFunc: keyVaultValidate.NestedItemId,
+			ValidateFunc: commonids.ValidateKeyVaultID,
 		},
 
 		"max_results": {
@@ -111,31 +113,34 @@ func (r KeyVaultSecretVersionsDataSource) Read() sdk.ResourceFunc {
 		Timeout: 5 * time.Minute,
 
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
-			keyVaultsClient := metadata.Client.KeyVault.VaultsClient
 			client := metadata.Client.KeyVault.ManagementClient
-			name := metadata.ResourceData.Get("name").(string)
-			maxResults := metadata.ResourceData.Get("max_results").(int32)
-			keyVaultId, err := commonids.ParseKeyVaultID(metadata.ResourceData.Get("key_vault_id").(string))
+
+			var model KeyVaultSecretVersionsDataSourceModel
+
+			if err := metadata.Decode(&model); err != nil {
+				return fmt.Errorf("decoding: %+v", err)
+			}
+
+			keyVaultId, err := commonids.ParseKeyVaultID(model.KeyVaultId)
 			if err != nil {
 				return err
 			}
 
-			keyVaultBaseUri, err := keyVaultsClient.Get(ctx, *keyVaultId)
+			keyVaultUri, err := metadata.Client.KeyVault.BaseUriForKeyVault(ctx, *keyVaultId)
 			if err != nil {
-				return fmt.Errorf("looking up Secret %q vault url from id %q: %+v", name, *keyVaultId, err)
+				return err
 			}
 
-			resp, err := client.GetSecretVersions(ctx, *keyVaultBaseUri.Model.Properties.VaultUri, name, &maxResults)
+			resp, err := client.GetSecretVersions(ctx, *keyVaultUri, model.Name, &model.MaxResults)
 			if err != nil {
-				return fmt.Errorf("making List Versions request on Azure KeyVault Secret %s: %+v", name, err)
+				return fmt.Errorf("making List Versions request on Azure KeyVault Secret %s: %+v", model.Name, err)
 			}
-
-			var versions []map[string]interface{}
 
 			if resp.Values() != nil {
 				for resp.NotDone() {
 					for _, v := range resp.Values() {
-						versions = append(versions, expandSecretVersion(&v))
+						model.Versions = append(model.Versions, expandSecretVersion(&v))
+						//versions = append(versions, expandSecretVersion(&v))
 					}
 					err = resp.NextWithContext(ctx)
 					if err != nil {
@@ -144,26 +149,27 @@ func (r KeyVaultSecretVersionsDataSource) Read() sdk.ResourceFunc {
 				}
 			}
 
-			metadata.ResourceData.Set("versions", versions)
+			metadata.ResourceData.SetId(fmt.Sprintf("%s/%s", model.KeyVaultId, model.Name))
 
-			return nil
+			return metadata.Encode(&model)
 		},
 	}
 }
 
-func expandSecretVersion(v *keyvault.SecretItem) map[string]interface{} {
-	item := make(map[string]interface{})
-	item["id"] = *v.ID
-	item["created_date"] = time.Time(*v.Attributes.Created).Format(time.RFC3339)
-	item["enabled"] = *v.Attributes.Enabled
+func expandSecretVersion(v *keyvault.SecretItem) secretVersionModel {
+	var item secretVersionModel
+	
+	item.ID = *v.ID
+	item.CreatedDate = time.Time(*v.Attributes.Created).Format(time.RFC3339)
+	item.Enabled = *v.Attributes.Enabled
 	if notBefore := v.Attributes.NotBefore; notBefore != nil {
-		item["not_before_date"] = time.Time(*notBefore).Format(time.RFC3339)
+		item.NotBeforeDate = time.Time(*notBefore).Format(time.RFC3339)
 	}
 	if expires := v.Attributes.Expires; expires != nil {
-		item["expiration_date"] = time.Time(*expires).Format(time.RFC3339)
+		item.ExpirationDate = time.Time(*expires).Format(time.RFC3339)
 	}
 	if updated := v.Attributes.Updated; updated != nil {
-		item["updated_date"] = time.Time(*updated).Format(time.RFC3339)
+		item.UpdatedDate = time.Time(*updated).Format(time.RFC3339)
 	}
 
 	return item
