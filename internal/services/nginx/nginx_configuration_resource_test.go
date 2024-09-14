ab90090b8e7c008587525125d202f544b6a5ce69
sarna <P.Sarna@f5.com>
2024-09-13 18:14:08 -0700

    Change NGINXaaS SKU to standardv2_Monthly
    NGINXaaS now supports a newer SKU
    `standardv2_Monthly` and the `standard_Monthly`
    SKU has been deprecated.
    
    This change updates the acceptance tests to use
    the new SKU.

--- internal/services/nginx/nginx_configuration_resource_test.go
+++ internal/services/nginx/nginx_configuration_resource_test.go
@@ -285,7 +285,7 @@ resource "azurerm_subnet" "test" {
 resource "azurerm_nginx_deployment" "test" {
   name                = "acctest-%[1]d"
   resource_group_name = azurerm_resource_group.test.name
-  sku                 = "standard_Monthly"
+  sku                 = "standardv2_Monthly"
   capacity            = 10
   location            = azurerm_resource_group.test.location
 
