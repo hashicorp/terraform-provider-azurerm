e095372ac1b304e70b829bb786e8c4ba392fe065
Bruce Harrison <leeharrison1984@gmail.com>
2024-10-02 15:05:42 -0500

    Update internal/services/postgres/postgresql_flexible_server_virtual_endpoint_resource_test.go
    Co-authored-by: jackofallops <11830746+jackofallops@users.noreply.github.com>

--- internal/services/postgres/postgresql_flexible_server_virtual_endpoint_resource_test.go
+++ internal/services/postgres/postgresql_flexible_server_virtual_endpoint_resource_test.go
@@ -409,7 +409,7 @@ resource "azurerm_virtual_network_peering" "west" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "west" {
-  name                  = "%[1]d-west-pdzvnetlink.com"
+  name                  = "acctest%[1]d-west-pdzvnetlink.com"
   private_dns_zone_name = azurerm_private_dns_zone.west.name
   virtual_network_id    = azurerm_virtual_network.west.id
   resource_group_name   = azurerm_resource_group.west.name
