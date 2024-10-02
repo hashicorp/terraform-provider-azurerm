360432c54c0686fe8f3bd33d988c7728fc1d9471
Bruce Harrison <leeharrison1984@gmail.com>
2024-10-02 15:05:31 -0500

    Update internal/services/postgres/postgresql_flexible_server_virtual_endpoint_resource_test.go
    Co-authored-by: jackofallops <11830746+jackofallops@users.noreply.github.com>

--- internal/services/postgres/postgresql_flexible_server_virtual_endpoint_resource_test.go
+++ internal/services/postgres/postgresql_flexible_server_virtual_endpoint_resource_test.go
@@ -393,7 +393,7 @@ resource "azurerm_subnet_network_security_group_association" "west" {
 }
 
 resource "azurerm_private_dns_zone" "west" {
-  name                = "%[1]d-pdz.postgres.database.azure.com"
+  name                = "acctest%[1]d-pdz.postgres.database.azure.com"
   resource_group_name = azurerm_resource_group.west.name
 
   depends_on = [azurerm_subnet_network_security_group_association.west]
