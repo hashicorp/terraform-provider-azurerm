adc1a6b0b1b1a8102e89bc2c52d2a727fba85f2e
xuzhang3 <57888764+xuzhang3@users.noreply.github.com>
2024-10-17 11:17:51 +0800

    Bump Redis Enterprise API to `2024-06-01-preview` (#27597)
    * bump redis enterprise API to 2024-06-01-preview
    
    * delete test code
    
    * update test case
    
    * update vendor files

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2024-06-01-preview/databases/model_forcelinkparameters.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2024-06-01-preview/databases/model_forcelinkparameters.go
@@ -3,8 +3,7 @@ package databases
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-const defaultApiVersion = "2023-07-01"
-
-func userAgent() string {
-	return "hashicorp/go-azure-sdk/databases/2023-07-01"
+type ForceLinkParameters struct {
+	GroupNickname   string           `json:"groupNickname"`
+	LinkedDatabases []LinkedDatabase `json:"linkedDatabases"`
 }
