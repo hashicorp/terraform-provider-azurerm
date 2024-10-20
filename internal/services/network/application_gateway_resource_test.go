a25af2be76c8ab89dbb96ae93c4aaf71d301f0fb
Ed Smith <ed@edintheclouds.io>
2024-10-20 11:27:21 +0100

    tests(application_gateway): hashicorp#25973 update test public ip resource to be standard sku for compatibility with agw v2

--- internal/services/network/application_gateway_resource_test.go
+++ internal/services/network/application_gateway_resource_test.go
@@ -6355,8 +6355,8 @@ resource "azurerm_public_ip" "test" {
   name                = "acctest-pubip-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger, data.RandomInteger)
 }
