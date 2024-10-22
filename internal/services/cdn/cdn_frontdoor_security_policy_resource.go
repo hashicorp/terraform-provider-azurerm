faa750a984bdb6785c9d8e94307f0a7838bbc126
richardfric <69456996+richardfric@users.noreply.github.com>
2024-10-22 12:48:33 +0200

    Fix casing for cdn_frontdoor_firewall_policy_id
    This is a follow up for #22666 fix

--- internal/services/cdn/cdn_frontdoor_security_policy_resource.go
+++ internal/services/cdn/cdn_frontdoor_security_policy_resource.go
@@ -15,6 +15,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/cdn/parse"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/cdn/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/suppress"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
@@ -46,10 +47,11 @@ func resourceCdnFrontDoorSecurityPolicy() *pluginsdk.Resource {
 			},
 
 			"cdn_frontdoor_profile_id": {
-				Type:         pluginsdk.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validate.FrontDoorProfileID,
+				Type:             pluginsdk.TypeString,
+				Required:         true,
+				ForceNew:         true,
+				ValidateFunc:     validate.FrontDoorProfileID,
+				DiffSuppressFunc: suppress.CaseDifference,
 			},
 
 			"security_policies": {
