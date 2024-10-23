d95a6b02d27a132264859ef2a89b037e63ed3116
teowa <104055472+teowa@users.noreply.github.com>
2024-10-23 02:40:38 +0000

    remove linked issue

--- internal/services/consumption/consumption_budget_base.go
+++ internal/services/consumption/consumption_budget_base.go
@@ -151,7 +151,6 @@ func (br consumptionBudgetBaseResource) arguments(fields map[string]*pluginsdk.S
 						Required:     true,
 						ValidateFunc: validation.IntBetween(0, 1000),
 					},
-					// Issue: https://github.com/Azure/azure-rest-api-specs/issues/16240
 					"threshold_type": {
 						Type:     pluginsdk.TypeString,
 						Optional: true,
