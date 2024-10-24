a5274b997826c562c996a5558b444c4975d473bc
neil-yechenwei <yechenwei2007@hotmail.com>
2024-10-24 18:01:22 +0800

    go mod

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/network/2023-11-01/firewallpolicyrulecollectiongroups/model_firewallpolicyfilterrulecollectionaction.go
+++
@@ -1,8 +0,0 @@
-package firewallpolicyrulecollectiongroups
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type FirewallPolicyFilterRuleCollectionAction struct {
-	Type *FirewallPolicyFilterRuleCollectionActionType `json:"type,omitempty"`
-}
--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/network/2023-11-01/networkvirtualappliances/model_virtualappliancenetworkinterfaceconfiguration.go
+++
@@ -1,9 +0,0 @@
-package networkvirtualappliances
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type VirtualApplianceNetworkInterfaceConfiguration struct {
-	Properties *VirtualApplianceNetworkInterfaceConfigurationProperties `json:"properties,omitempty"`
-	Type       *NicTypeInRequest                                        `json:"type,omitempty"`
-}
