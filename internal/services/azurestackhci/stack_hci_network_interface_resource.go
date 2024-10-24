fed7d970726775bdf19566aedd783990081d14e7
teowa <104055472+teowa@users.noreply.github.com>
2024-10-24 08:35:05 +0000

    fix message

--- internal/services/azurestackhci/stack_hci_network_interface_resource.go
+++ internal/services/azurestackhci/stack_hci_network_interface_resource.go
@@ -64,7 +64,7 @@ func (StackHCINetworkInterfaceResource) Arguments() map[string]*pluginsdk.Schema
 			ForceNew: true,
 			ValidateFunc: validation.StringMatch(
 				regexp.MustCompile(`^[a-zA-Z0-9][\-\.\_a-zA-Z0-9]{0,78}[a-zA-Z0-9]$`),
-				"name must be between 2 and 80 characters and can only contain alphanumberic characters, hyphen(-), period(.) and underline(_). It must begin and end with a alphanumberic character.",
+				"name must be between 2 and 80 characters and can only contain alphanumberic characters, hyphens(-), periods(.) and underlines(_). It must begin and end with an alphanumberic character.",
 			),
 		},
 
