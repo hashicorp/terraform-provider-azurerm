c03d4871efd7c65874413f56a7f20adcdc1d04f1
neil-yechenwei <yechenwei2007@hotmail.com>
2024-10-25 09:44:23 +0800

    update pr per comments

--- internal/services/iothub/iothub_resource.go
+++ internal/services/iothub/iothub_resource.go
@@ -377,7 +377,7 @@ func resourceIotHub() *pluginsdk.Resource {
 
 						"resource_group_name": commonschema.ResourceGroupNameOptional(),
 
-						// `Computed: true` is required since this property would always be set even if it isn't specified in the tf config, otherwise it would cause difference and block the existing users
+						// NOTE: O+C : required since this property would always be set even if it isn't specified in the tf config, otherwise it would cause a diff and break existing users
 						"subscription_id": {
 							Type:         pluginsdk.TypeString,
 							Optional:     true,
@@ -1285,7 +1285,10 @@ func expandIoTHubEndpoints(d *pluginsdk.ResourceData, subscriptionId string) (*d
 		resourceGroup := endpoint["resource_group_name"].(string)
 		authenticationType := devices.AuthenticationType(endpoint["authentication_type"].(string))
 
-		subscriptionID := endpoint["subscription_id"].(string)
+		var subscriptionID string
+		if v, ok := endpoint["subscription_id"]; ok && v.(string) != "" {
+			subscriptionID = v.(string)
+		}
 		// To align with the previous TF behavior, `subscription_id` needs to be set with the provider's subscription Id when is isn't specified in the tf config, otherwise TF behavior is different than before and it may block the existing users
 		// From the business perspective, the raw config handling is only deant for the case that the user has an EventHub whose Endpoint's subscription is not the provider's one. Then the user wants to reset it to the provider's one by unset the subscription_id
 		// From the TF code perspective, given `Computed: true` is enabled, TF would always get the value from the last apply when this property isn't set in the tf config. So `d.GetRawConfig()` is required to determine if it's set in the tf config
