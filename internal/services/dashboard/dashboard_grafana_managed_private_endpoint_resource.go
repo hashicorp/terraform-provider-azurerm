8a990b27a132fbcb4b2392a0eabda3edfdab3417
djryanj <ryan@ryanjjacobs.com>
2024-10-26 09:24:32 -0600

    working resource; acceptance test scaffold

--- internal/services/dashboard/dashboard_grafana_managed_private_endpoint_resource.go
+++ internal/services/dashboard/dashboard_grafana_managed_private_endpoint_resource.go
@@ -7,103 +7,13 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
-	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/dashboard/2023-09-01/managedprivateendpoints"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/services/dashboard/parse"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
-	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
-// "context"
-// "fmt"
-// "regexp"
-// "time"
-
-// "github.com/hashicorp/go-azure-helpers/lang/response"
-// "github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
-// "github.com/hashicorp/go-azure-helpers/resourcemanager/identity"
-// "github.com/hashicorp/go-azure-helpers/resourcemanager/location"
-// "github.com/hashicorp/go-azure-sdk/resource-manager/dashboard/2023-09-01/managedprivateendpoints"
-// "github.com/hashicorp/terraform-provider-azurerm/internal/services/dashboard/parse"
-//
-// "github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
-// "github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
-// "github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
-// "github.com/hashicorp/terraform-provider-azurerm/utils"
-
-// func resourceDashboardGrafanaManagedPrivateEndpoint() *pluginsdk.Resource {
-// 	return &pluginsdk.Resource{
-// 		Create: resourceDashboardGrafanaManagedPrivateEndpointCreateUpdate,
-// 		Read:   resourceDashboardGrafanaManagedPrivateEndpointRead,
-// 		Update: resourceDashboardGrafanaManagedPrivateEndpointCreateUpdate,
-// 		Delete: resourceDashboardGrafanaManagedPrivateEndpointDelete,
-
-// 		SchemaVersion: 1,
-
-// 		Importer: pluginsdk.ImporterValidatingResourceId(func(id string) error {
-// 			_, err := managedprivateendpoints.ParseManagedPrivateEndpointID(id)
-// 			return err
-// 		}),
-
-// 		Timeouts: &pluginsdk.ResourceTimeout{
-// 			Create: pluginsdk.DefaultTimeout(60 * time.Minute),
-// 			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
-// 			Update: pluginsdk.DefaultTimeout(60 * time.Minute),
-// 			Delete: pluginsdk.DefaultTimeout(60 * time.Minute),
-// 		},
-
-// 		Schema: map[string]*pluginsdk.Schema{
-// "name": {
-// 	Type:         pluginsdk.TypeString,
-// 	Required:     true,
-// 	ForceNew:     true,
-// 	ValidateFunc: validation.StringIsNotEmpty,
-// },
-
-// "location": commonschema.Location(),
-
-// "tags": commonschema.Tags(),
-
-// "managed_grafana_id": {
-// 	Type:         pluginsdk.TypeString,
-// 	Required:     true,
-// 	ForceNew:     true,
-// 	ValidateFunc: managedprivateendpoints.ValidateGrafanaID,
-// },
-
-// "private_link_resource_id": {
-// 	Type:         pluginsdk.TypeString,
-// 	Required:     true,
-// 	ForceNew:     true,
-// 	ValidateFunc: azure.ValidateResourceID,
-// },
-
-// "group_ids": {
-// 	Type:     pluginsdk.TypeList,
-// 	Optional: true,
-// 	ForceNew: true,
-// 	// no validation here because an empty group id is valid for a generic private link resource
-// },
-
-// "private_link_resource_region": {
-// 	Type:         pluginsdk.TypeString,
-// 	Optional:     true,
-// 	ForceNew:     true,
-// 	ValidateFunc: validation.StringIsNotEmpty,
-// },
-
-//	"request_message": {
-//		Type:         pluginsdk.TypeString,
-//		Optional:     true,
-//		ValidateFunc: validation.StringIsNotEmpty,
-//	},
-//
-//			},
-//		}
-//	}
 type ManagedPrivateEndpointResource struct{}
 
 type ManagedPrivateEndpointModel struct {
@@ -113,10 +23,9 @@ type ManagedPrivateEndpointModel struct {
 	PrivateLinkResourceId     string            `tfschema:"private_link_resource_id"`
 	PrivateLinkResourceRegion string            `tfschema:"private_link_resource_region"`
 	Tags                      map[string]string `tfschema:"tags"`
-	GroupIds                  [string]string    `tfschema:"group_ids"`
+	GroupIds                  []string          `tfschema:"group_ids"`
+	RequestMessage            string            `tfschema:"request_message"`
 }
-
-var _ sdk.ResourceWithStateMigration = ManagedPrivateEndpointResource{}
 
 func (r ManagedPrivateEndpointResource) ModelObject() interface{} {
 	return &ManagedPrivateEndpointModel{}
@@ -127,7 +36,7 @@ func (r ManagedPrivateEndpointResource) ResourceType() string {
 }
 
 func (r ManagedPrivateEndpointResource) IDValidationFunc() pluginsdk.SchemaValidateFunc {
-	return parse.ManagedPrivateEndpointID
+	return managedprivateendpoints.ValidateManagedPrivateEndpointID
 }
 
 func (r ManagedPrivateEndpointResource) Arguments() map[string]*pluginsdk.Schema {
@@ -161,6 +70,9 @@ func (r ManagedPrivateEndpointResource) Arguments() map[string]*pluginsdk.Schema
 			Type:     pluginsdk.TypeList,
 			Optional: true,
 			ForceNew: true,
+			Elem: &pluginsdk.Schema{
+				Type: pluginsdk.TypeString,
+			},
 			// no validation here because an empty group id is valid for a generic private link resource
 		},
 
@@ -171,11 +83,11 @@ func (r ManagedPrivateEndpointResource) Arguments() map[string]*pluginsdk.Schema
 			ValidateFunc: validation.StringIsNotEmpty,
 		},
 
-		// "request_message": {
-		// 	Type:         pluginsdk.TypeString,
-		// 	Optional:     true,
-		// 	ValidateFunc: validation.StringIsNotEmpty,
-		// },
+		"request_message": {
+			Type:         pluginsdk.TypeString,
+			Optional:     true,
+			ValidateFunc: validation.StringIsNotEmpty,
+		},
 	}
 }
 
@@ -194,7 +106,11 @@ func (r ManagedPrivateEndpointResource) Create() sdk.ResourceFunc {
 
 			client := metadata.Client.Dashboard.ManagedPrivateEndpointsClient
 			subscriptionId := metadata.Client.Account.SubscriptionId
-			grafanaId := managedprivateendpoints.ParseGrafanaID(model.managed_grafana_id)
+			grafanaId, err := managedprivateendpoints.ParseGrafanaID(model.ManagedGrafanaId)
+
+			if err != nil {
+				return fmt.Errorf("invalid grafana id %s: %+v", model.ManagedGrafanaId, err)
+			}
 
 			id := managedprivateendpoints.NewManagedPrivateEndpointID(subscriptionId, grafanaId.ResourceGroupName, grafanaId.GrafanaName, model.Name)
 
@@ -206,27 +122,19 @@ func (r ManagedPrivateEndpointResource) Create() sdk.ResourceFunc {
 				return metadata.ResourceRequiresImport(r.ResourceType(), id)
 			}
 
-			props := &managedprivateendpoints.ManagedPrivateEndpointModel{
-				Location: utils.String(location.Normalize(model.Location)),
+			props := managedprivateendpoints.ManagedPrivateEndpointModel{
+				Location: model.Location,
 				Name:     &model.Name,
 				Properties: &managedprivateendpoints.ManagedPrivateEndpointModelProperties{
 					GroupIds:                  &model.GroupIds,
 					PrivateLinkResourceId:     &model.PrivateLinkResourceId,
 					PrivateLinkResourceRegion: &model.PrivateLinkResourceRegion,
+					RequestMessage:            &model.RequestMessage,
 				},
 				Tags: &model.Tags,
 			}
 
-			// managedprivateendpoints.ManagedPrivateEndpointModel {
-			// 	Properties: &managedprivateendpoints.PrivateEndpointProperties{
-			// 				PrivateLinkServiceId: utils.String(model.TargetResourceId),
-			// 				GroupIds:             &[]string{model.SubResourceName},
-			// 		},
-			// 	},
-			// }
-
-			var opts managedprivateendpoints.CreateOrUpdateOperationOptions
-			if _, err := client.CreateOrUpdate(ctx, id, props, opts); err != nil {
+			if err := client.CreateThenPoll(ctx, id, props); err != nil {
 				return fmt.Errorf("creating %s: %+v", id, err)
 			}
 
@@ -262,7 +170,7 @@ func (r ManagedPrivateEndpointResource) Read() sdk.ResourceFunc {
 
 			state := ManagedPrivateEndpointModel{
 				Name:     id.ManagedPrivateEndpointName,
-				Location: location.NormalizeNilable(model.Location),
+				Location: model.Location,
 			}
 
 			if props := model.Properties; props != nil {
@@ -289,7 +197,7 @@ func (r ManagedPrivateEndpointResource) Delete() sdk.ResourceFunc {
 		Timeout: 5 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
 			client := metadata.Client.Dashboard.ManagedPrivateEndpointsClient
-			id, err := managedprivatendpoints.ParseManagedPrivateEndpointID(metadata.ResourceData.Id())
+			id, err := managedprivateendpoints.ParseManagedPrivateEndpointID(metadata.ResourceData.Id())
 			if err != nil {
 				return err
 			}
@@ -304,3 +212,51 @@ func (r ManagedPrivateEndpointResource) Delete() sdk.ResourceFunc {
 		},
 	}
 }
+
+func (r ManagedPrivateEndpointResource) Update() sdk.ResourceFunc {
+	return sdk.ResourceFunc{
+		Timeout: 30 * time.Minute,
+		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.Dashboard.ManagedPrivateEndpointsClient
+
+			id, err := managedprivateendpoints.ParseManagedPrivateEndpointID(metadata.ResourceData.Id())
+			if err != nil {
+				return err
+			}
+
+			var model ManagedPrivateEndpointModel
+			if err := metadata.Decode(&model); err != nil {
+				return fmt.Errorf("decoding: %+v", err)
+			}
+
+			resp, err := client.Get(ctx, *id)
+			if err != nil {
+				return fmt.Errorf("retrieving %s: %+v", *id, err)
+			}
+
+			properties := resp.Model
+			if properties == nil {
+				return fmt.Errorf("retrieving %s: properties was nil", id)
+			}
+
+			if metadata.ResourceData.HasChange("tags") {
+				properties.Tags = &model.Tags
+			}
+
+			if err := client.CreateThenPoll(ctx, *id, *properties); err != nil {
+				return fmt.Errorf("updating %s: %+v", *id, err)
+			}
+
+			return nil
+		},
+	}
+}
+
+// func (r ManagedPrivateEndpointResource) StateUpgraders() sdk.StateUpgradeData {
+// 	return sdk.StateUpgradeData{
+// 		SchemaVersion: 1,
+// 		Upgraders: map[int]pluginsdk.StateUpgrade{
+// 			0: migration.StreamAnalyticsManagedPrivateEndpointV0ToV1{},
+// 		},
+// 	}
+// }
