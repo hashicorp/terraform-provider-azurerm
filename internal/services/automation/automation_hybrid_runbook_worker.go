75abf6e26c838a46ac550b17067172a507532de5
wuxu92 <wuxu92@outlook.com>
2024-10-29 09:42:10 +0800

    add markasgone if resource not found in get

--- internal/services/automation/automation_hybrid_runbook_worker.go
+++ internal/services/automation/automation_hybrid_runbook_worker.go
@@ -159,6 +159,9 @@ func (m HybridRunbookWorkerResource) Read() sdk.ResourceFunc {
 			client := meta.Client.Automation.HybridRunbookWorker
 			result, err := client.Get(ctx, *id)
 			if err != nil {
+				if response.WasNotFound(result.HttpResponse) {
+					return meta.MarkAsGone(id)
+				}
 				return err
 			}
 			if result.Model == nil {
