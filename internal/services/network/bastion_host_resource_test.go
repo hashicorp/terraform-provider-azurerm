7f1c47b874469cd1a1aeeee0245ff5af723ecb3c
Lucas Fernández <lucas.fernandez@southworks.com>
2024-11-06 09:35:01 -0300

    Updated test.

--- internal/services/network/bastion_host_resource_test.go
+++ internal/services/network/bastion_host_resource_test.go
@@ -199,14 +199,12 @@ resource "azurerm_public_ip" "test" {
   resource_group_name = azurerm_resource_group.test.name
   allocation_method   = "Static"
   sku                 = "Standard"
-  zones               = ["1", "2", "3"]
 }
 
 resource "azurerm_bastion_host" "test" {
   name                = "acctestBastion%s"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  zones               = azurerm_public_ip.test.zones
 
   ip_configuration {
     name                 = "ip-configuration"
@@ -301,6 +299,7 @@ resource "azurerm_public_ip" "test" {
   resource_group_name = azurerm_resource_group.test.name
   allocation_method   = "Static"
   sku                 = "Standard"
+  zones               = ["1", "2", "3"]
 }
 
 resource "azurerm_bastion_host" "test" {
@@ -308,6 +307,7 @@ resource "azurerm_bastion_host" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   copy_paste_enabled  = false
+  zones               = azurerm_public_ip.test.zones
 
   ip_configuration {
     name                 = "ip-configuration"
