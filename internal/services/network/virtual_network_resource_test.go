46c2e4560b82a1184dde8f218db3c61e10a61adf
neil-yechenwei <yechenwei2007@hotmail.com>
2024-11-06 10:02:08 +0800

    update tc

--- internal/services/network/virtual_network_resource_test.go
+++ internal/services/network/virtual_network_resource_test.go
@@ -488,8 +488,7 @@ resource "azurerm_virtual_network" "test" {
 }
 
 func (VirtualNetworkResource) complete(data acceptance.TestData) string {
-	if !features.FourPointOhBeta() {
-		return fmt.Sprintf(`
+	return fmt.Sprintf(`
 provider "azurerm" {
   features {}
 }
@@ -506,39 +505,6 @@ resource "azurerm_virtual_network" "test" {
   resource_group_name            = azurerm_resource_group.test.name
   dns_servers                    = ["10.7.7.2", "10.7.7.7", "10.7.7.1", ]
   private_endpoint_vnet_policies = "Basic"
-
-  encryption {
-    enforcement = "AllowUnencrypted"
-  }
-
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.10.1.0/24"
-  }
-}
-`, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
-	}
-	return fmt.Sprintf(`
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "test" {
-  name     = "acctestRG-%d"
-  location = "%s"
-}
-
-resource "azurerm_virtual_network" "test" {
-  name                = "acctestvirtnet%d"
-  address_space       = ["10.0.0.0/16", "10.10.0.0/16"]
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  dns_servers         = ["10.7.7.2", "10.7.7.7", "10.7.7.1", ]
 
   encryption {
     enforcement = "AllowUnencrypted"
