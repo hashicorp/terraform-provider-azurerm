bf16f498ba356133f790136079705efb6111aaef
jackofallops <ste@hashicorp.com>
2024-11-07 10:40:23 +0100

    remove local replace used for testing

--- go.mod
+++ go.mod
@@ -98,5 +98,3 @@ require (
 go 1.22.0
 
 toolchain go1.22.7
-
-replace github.com/hashicorp/go-azure-helpers => ../go-azure-helpers
--- vendor/github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
-// Package stringplanmodifier provides plan modifiers for types.String attributes.
-package stringplanmodifier
