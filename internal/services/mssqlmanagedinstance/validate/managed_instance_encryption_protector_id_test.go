b504b6f0ff742c085490e6eb591313c33c1ee33b
kt <kt@katbyte.me>
2024-11-11 13:12:49 -0800

    make generate

--- internal/services/mssqlmanagedinstance/validate/managed_instance_encryption_protector_id_test.go
+++ internal/services/mssqlmanagedinstance/validate/managed_instance_encryption_protector_id_test.go
@@ -12,7 +12,6 @@ func TestManagedInstanceEncryptionProtectorID(t *testing.T) {
 		Input string
 		Valid bool
 	}{
-
 		{
 			// empty
 			Input: "",
--- internal/services/network/validate/private_dns_zone_group_id_test.go
+++ internal/services/network/validate/private_dns_zone_group_id_test.go
@@ -12,7 +12,6 @@ func TestPrivateDnsZoneGroupID(t *testing.T) {
 		Input string
 		Valid bool
 	}{
-
 		{
 			// empty
 			Input: "",
