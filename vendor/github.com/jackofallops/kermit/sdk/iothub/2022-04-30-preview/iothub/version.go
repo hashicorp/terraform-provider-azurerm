8744d96cd9e1c0c00e38af95e06979153923e724
Marcelo Zambrana Villarroel <marcech.am@gmail.com>
2024-11-14 04:11:30 +0000

    fixing vendor dependencies

--- vendor/github.com/jackofallops/kermit/sdk/iothub/2022-04-30-preview/iothub/version.go
+++ vendor/github.com/jackofallops/kermit/sdk/iothub/2022-04-30-preview/iothub/version.go
@@ -1,6 +1,6 @@
 package devices
 
-import "github.com/tombuildsstuff/kermit/version"
+import "github.com/jackofallops/kermit/version"
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
@@ -10,7 +10,7 @@ import "github.com/tombuildsstuff/kermit/version"
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-	return "tombuildsstuff/kermit/" + Version() + " devices/2022-04-30-preview"
+	return "jackofallops/kermit/" + Version() + " devices/2022-04-30-preview"
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
