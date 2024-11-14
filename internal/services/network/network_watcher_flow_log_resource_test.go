1ca0e4adfd2ae0ff45069c318033b6272d48cd93
Vladimir Lazarenko <favoretti@gmail.com>
2024-11-14 21:29:30 +0100

    terrafmt

--- internal/services/network/network_watcher_flow_log_resource_test.go
+++ internal/services/network/network_watcher_flow_log_resource_test.go
@@ -358,9 +358,9 @@ resource "azurerm_network_watcher_flow_log" "test" {
   resource_group_name  = azurerm_resource_group.test.name
   name                 = "flowlog-%d"
 
-  target_resource_id        = azurerm_virtual_network.test.id
-  storage_account_id        = azurerm_storage_account.test.id
-  enabled                   = true
+  target_resource_id = azurerm_virtual_network.test.id
+  storage_account_id = azurerm_storage_account.test.id
+  enabled            = true
 
   retention_policy {
     enabled = false
