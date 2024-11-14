19db93921bc02e9ada2633ecdb5e18504377798c
kt <kt@katbyte.me>
2024-11-14 14:45:05 -0800

    re-enable nilerr

--- internal/services/storage/storage_account_static_website_data_plane_resource.go
+++ internal/services/storage/storage_account_static_website_data_plane_resource.go
@@ -195,7 +195,7 @@ func (a AccountStaticWebsiteResource) Delete() sdk.ResourceFunc {
 
 			accountDetails, err := storageClient.FindAccount(ctx, id.SubscriptionId, id.StorageAccountName)
 			if err != nil {
-				return nil // nolint: nilerr If we don't find the account we can safely assume we don't need to remove the website since it must already be deleted
+				return nil // lint:ignore nilerr If we don't find the account we can safely assume we don't need to remove the website since it must already be deleted
 			}
 
 			properties := accounts.StorageServiceProperties{
