6df5ec4f30706fc2df63c609d791bf786fd911fd
kt <kt@katbyte.me>
2024-11-15 12:30:06 -0800

    fix import

--- internal/services/datafactory/data_factory_integration_runtime_azure_resource.go
+++ internal/services/datafactory/data_factory_integration_runtime_azure_resource.go
@@ -11,7 +11,6 @@ import (
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/datafactory/2018-06-01/factories"
-	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/datafactory/parse"
