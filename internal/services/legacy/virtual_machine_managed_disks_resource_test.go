c528c7a390266a4e5087a196166b55df951c9c5b
magodo <wztdyl@sina.com>
2024-11-22 21:05:32 +1100

    Acctest: Add back the missing provider config (#28052)

--- internal/services/legacy/virtual_machine_managed_disks_resource_test.go
+++ internal/services/legacy/virtual_machine_managed_disks_resource_test.go
@@ -633,6 +633,10 @@ variable "prefix" {
   default = "acctest%s"
 }
 
+provider "azurerm" {
+  features {}
+}
+
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "test" {
