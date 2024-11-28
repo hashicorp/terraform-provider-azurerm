efa3dc056e7c5e7448c7e5aee15704c6974a2057
kt <kt@katbyte.me>
2024-11-27 22:05:34 -0800

    provider: update changelog via tag workflow instead of release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -52,17 +52,6 @@ if [[ "${GOTIME}" != "1" ]]; then
   exit 1
 fi
 
-if [[ "$(uname)" == "Darwin" ]]; then
-  echo "(Using BSD sed)"
-  SED="sed -E"
-else
-  echo "(Using GNU sed)"
-  SED="sed -r"
-fi
-
-DATE="$(date '+%B %d, %Y')"
-PROVIDER_URL="https:\/\/github.com\/hashicorp\/terraform-provider-azurerm\/issues"
-
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 if [[ "${BRANCH}" != "${TRUNK}" ]]; then
   if [[ "${FORCE}" == "1" ]]; then
@@ -87,49 +76,13 @@ else
   ( set -x; TF_ACC= scripts/run-test.sh )
 fi
 
-echo "Preparing changelog for release..."
-
-if [[ ! -f CHANGELOG.md ]]; then
-  echo "Error: CHANGELOG.md not found."
-  exit 2
-fi
-
-# Get the next release
-RELEASE="$($SED -n 's/^## v?([0-9.]+) \(Unreleased\)/\1/p' CHANGELOG.md)"
-if [[ "${RELEASE}" == "" ]]; then
-  echo "Error: could not determine next release in CHANGELOG.md" >&2
-  exit 3
-fi
-
 # Ensure latest changes are checked out
 ( set -x; ${debug}git pull --rebase origin "${TRUNK}" )
-
-# Replace [GH-nnnn] references with issue links
-( set -x; ${debug}$SED -i.bak "s/\[GH-([0-9]+)\]/\(\[#\1\]\(${PROVIDER_URL}\/\1\)\)/g" CHANGELOG.md )
-
-# Set the date for the latest release
-( set -x; ${debug}$SED -i.bak "s/^(## v?[0-9.]+) \(Unreleased\)/\1 (${DATE})/i" CHANGELOG.md )
-
-${debug}rm CHANGELOG.md.bak
 
 if [[ "${NOTAG}" == "1" ]]; then
   echo "Warning: Skipping commit, tag and push."
   exit 0
 fi
-
-echo "exporting Provider Schema JSON"
-(
-  set -x
-  ${debug}go run internal/tools/schema-api/main.go -export .release/provider-schema.json
-)
-
-echo "Committing changelog and provider schema..."
-(
-  set -x
-  ${debug}git commit CHANGELOG.md .release/provider-schema.json -m v"${RELEASE}"
-  ${debug}git push origin "${BRANCH}"
-)
-
 
 echo "Releasing v${RELEASE}..."
 
