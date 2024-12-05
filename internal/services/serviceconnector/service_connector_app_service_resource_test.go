a84ac6536583e5b28ef3af4393237d7e92e7853d
Jiawei Tao <jiaweitao@microsoft.com>
2024-12-05 16:21:07 +0800

    fix subnet upgrade issue

--- internal/services/serviceconnector/service_connector_app_service_resource_test.go
+++ internal/services/serviceconnector/service_connector_app_service_resource_test.go
@@ -507,6 +507,7 @@ resource "azurerm_subnet" "test1" {
   resource_group_name  = azurerm_resource_group.test.name
   virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes     = ["10.0.1.0/24"]
+  private_endpoint_network_policies = "Enabled"
 
   delegation {
     name = "delegation"
