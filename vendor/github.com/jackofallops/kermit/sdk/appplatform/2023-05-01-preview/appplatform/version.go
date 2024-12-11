961149dbaa674d661db19c36944433c720c6a39e
Marcelo Zambrana Villarroel <marcech.am@gmail.com>
2024-12-11 03:13:36 +0000

    fixing vendor dependencies

--- vendor/github.com/jackofallops/kermit/sdk/appplatform/2023-05-01-preview/appplatform/version.go
+++ vendor/github.com/jackofallops/kermit/sdk/appplatform/2023-05-01-preview/appplatform/version.go
@@ -1,6 +1,6 @@
 package appplatform
 
-import "github.com/tombuildsstuff/kermit/version"
+import "github.com/jackofallops/kermit/version"
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
@@ -10,7 +10,7 @@ import "github.com/tombuildsstuff/kermit/version"
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-	return "tombuildsstuff/kermit/" + Version() + " appplatform/2023-05-01-preview"
+	return "jackofallops/kermit/" + Version() + " appplatform/2023-05-01-preview"
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
