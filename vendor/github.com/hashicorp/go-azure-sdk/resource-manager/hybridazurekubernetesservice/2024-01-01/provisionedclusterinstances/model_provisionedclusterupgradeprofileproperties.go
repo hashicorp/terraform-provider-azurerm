3de516d348eee09e124396f33daa9d2c5fc834b2
teowa <104055472+teowa@users.noreply.github.com>
2024-12-16 02:31:51 +0000

    go mod vendor

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/hybridazurekubernetesservice/2024-01-01/provisionedclusterinstances/model_provisionedclusterupgradeprofileproperties.go
+++
@@ -1,9 +0,0 @@
-package provisionedclusterinstances
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type ProvisionedClusterUpgradeProfileProperties struct {
-	ControlPlaneProfile ProvisionedClusterPoolUpgradeProfile `json:"controlPlaneProfile"`
-	ProvisioningState   *ResourceProvisioningState           `json:"provisioningState,omitempty"`
-}
