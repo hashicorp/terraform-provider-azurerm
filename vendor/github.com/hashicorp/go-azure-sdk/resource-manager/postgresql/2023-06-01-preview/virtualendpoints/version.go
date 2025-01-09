f1c5f20c79ddf277f5c93b61179aa70a0f8c8b68
jackofallops <11830746+jackofallops@users.noreply.github.com>
2025-01-09 17:55:31 +0100

    Revert "Upgrade Postgresql API version from `2023-06-01-preview/2022-12-01` to `2024-08-01`" (#28467)
    This reverts commit 8fe05cc5fa5037d284dfb5c0a26091cb65b84640.

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2023-06-01-preview/virtualendpoints/version.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2023-06-01-preview/virtualendpoints/version.go
@@ -1,10 +1,10 @@
-package servers
+package virtualendpoints
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-const defaultApiVersion = "2024-08-01"
+const defaultApiVersion = "2023-06-01-preview"
 
 func userAgent() string {
-	return "hashicorp/go-azure-sdk/servers/2024-08-01"
+	return "hashicorp/go-azure-sdk/virtualendpoints/2023-06-01-preview"
 }
