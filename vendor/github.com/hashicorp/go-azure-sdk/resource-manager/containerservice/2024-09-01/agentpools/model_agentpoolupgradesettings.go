83c2dcecb8b646a4741a738b3ef402b16bbda0ae
hc-github-team-tf-azure <>
2025-01-15 14:21:44 +0000

    dependencies: updating `containerservice` to API Version "2024-09-01" from "2023-03-02-preview"

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2024-09-01/agentpools/model_agentpoolupgradesettings.go
@@ -0,0 +1,10 @@
+package agentpools
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type AgentPoolUpgradeSettings struct {
+	DrainTimeoutInMinutes     *int64  `json:"drainTimeoutInMinutes,omitempty"`
+	MaxSurge                  *string `json:"maxSurge,omitempty"`
+	NodeSoakDurationInMinutes *int64  `json:"nodeSoakDurationInMinutes,omitempty"`
+}
