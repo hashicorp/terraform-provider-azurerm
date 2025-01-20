570057d8fdd5d7a765ff185691a63c722bf8fdc4
Zijie He <hezijie@microsoft.com>
2025-01-20 16:05:07 +0800

    fix lint issue

--- internal/services/compute/windows_virtual_machine_scale_set_resource_other_test.go
+++ internal/services/compute/windows_virtual_machine_scale_set_resource_other_test.go
@@ -1342,10 +1342,10 @@ resource "azurerm_virtual_network" "test" {
 }
 
 resource "azurerm_subnet" "test" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.test.name
-  virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["10.0.2.0/24"]
+  name                            = "internal"
+  resource_group_name             = azurerm_resource_group.test.name
+  virtual_network_name            = azurerm_virtual_network.test.name
+  address_prefixes                = ["10.0.2.0/24"]
   default_outbound_access_enabled = false
 }
 
