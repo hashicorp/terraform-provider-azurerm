63ac893cb03d0cd6bf42ea6861fede022de67648
Zijie He <hezijie@microsoft.com>
2025-01-20 15:20:19 +0800

    fix windows_virtual_machine_scale_set resource tests

--- internal/services/compute/windows_virtual_machine_scale_set_resource_other_test.go
+++ internal/services/compute/windows_virtual_machine_scale_set_resource_other_test.go
@@ -891,7 +891,7 @@ func TestAccWindowsVirtualMachineScaleSet_otherCancelRollingUpgrades(t *testing.
 						return err
 					}
 
-					ctx2, cancel := context.WithTimeout(ctx, 5*time.Minute)
+					ctx2, cancel := context.WithTimeout(ctx, 60*time.Minute)
 					defer cancel()
 					options := virtualmachinescalesets.DefaultGetOperationOptions()
 					options.Expand = pointer.To(virtualmachinescalesets.ExpandTypesForGetVMScaleSetsUserData)
@@ -1346,6 +1346,7 @@ resource "azurerm_subnet" "test" {
   resource_group_name  = azurerm_resource_group.test.name
   virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes     = ["10.0.2.0/24"]
+  default_outbound_access_enabled = false
 }
 
 resource "azurerm_windows_virtual_machine_scale_set" "test" {
