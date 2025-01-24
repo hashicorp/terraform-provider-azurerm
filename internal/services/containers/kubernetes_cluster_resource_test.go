5f53256bc91180a0706262a886ac9afe3aafdd9d
Thijs Brobbel <thijsbrobbel@gmail.com>
2025-01-24 20:11:11 +0100

    chore: Update .../containerservice/{2024-05-01 -> 2024-09-01}/*
    This is to prepare to add support for controlling AKS Advanced Container
    Networking Services.

--- internal/services/containers/kubernetes_cluster_resource_test.go
+++ internal/services/containers/kubernetes_cluster_resource_test.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2024-05-01/agentpools"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2024-09-01/agentpools"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
