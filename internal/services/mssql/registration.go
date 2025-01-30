9d4b61042ff3be430db27ee427d32d307162c3f8
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2025-01-30 08:13:14 -0700

    New resource: `azurerm_mssql_job_target_group` (#28492)
    * New resource: azurerm_mssql_job_target_group
    
    * fix lint
    
    * gofumpt and generate
    
    * Minor fixes
    
    * Apply suggestions from code review
    
    Co-authored-by: stephybun <steph@hashicorp.com>
    
    * Move note under correct argument
    
    * Add var to update method
    
    * Change job_target.type to computed attr, move validation to CustomizeDiff
    
    * Update resource documentation
    
    * Update website/docs/r/mssql_job_target_group.html.markdown
    
    Co-authored-by: stephybun <steph@hashicorp.com>
    
    * Remove excessive information
    
    ---------
    
    Co-authored-by: stephybun <steph@hashicorp.com>

--- internal/services/mssql/registration.go
+++ internal/services/mssql/registration.go
@@ -73,6 +73,7 @@ func (r Registration) Resources() []sdk.Resource {
 		MsSqlFailoverGroupResource{},
 		MsSqlJobResource{},
 		MsSqlJobScheduleResource{},
+		MsSqlJobTargetGroupResource{},
 		MsSqlVirtualMachineAvailabilityGroupListenerResource{},
 		MsSqlVirtualMachineGroupResource{},
 		ServerDNSAliasResource{},
