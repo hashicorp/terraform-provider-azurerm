ed4908d5a435dae96cbc94aef830ae0b6c0e8bc4
Zhenhua Hu <zhhu@microsoft.com>
2025-02-06 16:32:35 +0800

    update api version of galleryimageversions

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2023-07-03/galleryimageversions/model_uefikey.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2023-07-03/galleryimageversions/model_uefikey.go
@@ -3,8 +3,7 @@ package galleryimageversions
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-const defaultApiVersion = "2022-03-03"
-
-func userAgent() string {
-	return "hashicorp/go-azure-sdk/galleryimageversions/2022-03-03"
+type UefiKey struct {
+	Type  *UefiKeyType `json:"type,omitempty"`
+	Value *[]string    `json:"value,omitempty"`
 }
