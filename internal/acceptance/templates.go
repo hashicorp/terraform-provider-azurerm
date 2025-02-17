9c2a9ae830f787e2735e2284f413986b617f4781
stephybun <steph@hashicorp.com>
2025-02-17 14:48:40 +0100

    test template indentation
    Co-authored-by: jackofallops <11830746+jackofallops@users.noreply.github.com>

--- internal/acceptance/templates.go
+++ internal/acceptance/templates.go
@@ -12,41 +12,41 @@ func WriteOnlyKeyVaultSecretTemplate(data TestData, secret string) string {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "test" {
-name                       = "acctestkv-%[1]s"
-location                   = azurerm_resource_group.test.location
-resource_group_name        = azurerm_resource_group.test.name
-tenant_id                  = data.azurerm_client_config.current.tenant_id
-sku_name                   = "standard"
-soft_delete_retention_days = 7
+  name                       = "acctestkv-%[1]s"
+  location                   = azurerm_resource_group.test.location
+  resource_group_name        = azurerm_resource_group.test.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
 
-access_policy {
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azurerm_client_config.current.object_id
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
 
-  key_permissions = [
-    "Get",
-  ]
+    key_permissions = [
+      "Get",
+    ]
 
-  secret_permissions = [
-    "Get",
-    "Delete",
-    "List",
-    "Purge",
-    "Recover",
-    "Set",
-  ]
-}
+    secret_permissions = [
+      "Get",
+      "Delete",
+      "List",
+      "Purge",
+      "Recover",
+      "Set",
+    ]
+  }
 }
 
 resource "azurerm_key_vault_secret" "test" {
-name         = "secret-%[1]s"
-value        = "%[2]s"
-key_vault_id = azurerm_key_vault.test.id
+  name         = "secret-%[1]s"
+  value        = "%[2]s"
+  key_vault_id = azurerm_key_vault.test.id
 }
 
 ephemeral "azurerm_key_vault_secret" "test" {
-name         = azurerm_key_vault_secret.test.name
-key_vault_id = azurerm_key_vault.test.id
+  name         = azurerm_key_vault_secret.test.name
+  key_vault_id = azurerm_key_vault.test.id
 }
 `, data.RandomString, secret)
 }
