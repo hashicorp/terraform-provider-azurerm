e10711248e95c63c0264199c04889c0fd6917918
Wyatt Fry <wyattfry@gmail.com>
2025-02-18 14:30:07 -0500

    organize imports

--- internal/services/datadog/azurerm_datadog_monitor_sso_configuration_test.go
+++ internal/services/datadog/azurerm_datadog_monitor_sso_configuration_test.go
@@ -6,7 +6,6 @@ package datadog_test
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"os"
 	"testing"
 
@@ -14,6 +13,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
