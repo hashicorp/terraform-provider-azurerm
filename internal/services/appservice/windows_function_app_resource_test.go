96a35860fd706d42c207a9ccd58f69cd84408789
Sven Grosen <3759836+ledbutter@users.noreply.github.com>
2025-02-19 11:47:20 -0600

    Update Test Storage Account Based on Review
    Co-authored-by: sreallymatt <106555974+sreallymatt@users.noreply.github.com>

--- internal/services/appservice/windows_function_app_resource_test.go
+++ internal/services/appservice/windows_function_app_resource_test.go
@@ -3543,12 +3543,11 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_storage_account" "test" {
-  name                            = "acctestsa%s"
-  resource_group_name             = azurerm_resource_group.test.name
-  location                        = azurerm_resource_group.test.location
-  account_tier                    = "Standard"
-  account_replication_type        = "LRS"
-  allow_nested_items_to_be_public = false
+  name                     = "acctestsa%s"
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 resource "azurerm_service_plan" "test" {
