a9c2d6c3ba6270937fd6eac36761b110223b47b9
Mohamed Hassanin <mhassani@mathworks.com>
2025-03-03 12:31:27 -0500

    remove old

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/apimanagement/2022-08-01/apitagdescription/model_tagdescriptioncreateparameters.go
+++
@@ -1,8 +0,0 @@
-package apitagdescription
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type TagDescriptionCreateParameters struct {
-	Properties *TagDescriptionBaseProperties `json:"properties,omitempty"`
-}
