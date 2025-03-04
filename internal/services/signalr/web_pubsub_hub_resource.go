f8f8c30bba2d420da32658c8d83dbe902332117d
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2025-03-04 07:28:30 -0700

    `signalr` / `webpubsub` - upgrade API version to 2024-03-01 (#28940)

--- internal/services/signalr/web_pubsub_hub_resource.go
+++ internal/services/signalr/web_pubsub_hub_resource.go
@@ -11,7 +11,7 @@ import (
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/webpubsub/2023-02-01/webpubsub"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/webpubsub/2024-03-01/webpubsub"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	eventhubValidate "github.com/hashicorp/terraform-provider-azurerm/internal/services/eventhub/validate"
