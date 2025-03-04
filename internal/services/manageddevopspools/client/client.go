bff8103f1ff8620c4805c8acadf587ee9638d11a
Ajay Krishna Nalisetty <ajaykn@microsoft.com>
2025-03-05 04:49:31 +0900

    Adding pools Client

--- internal/services/manageddevopspools/client/client.go
+++ internal/services/manageddevopspools/client/client.go
@@ -1,11 +1,24 @@
 package client
 
 import (
+	"fmt"
+
+	"github.com/hashicorp/go-azure-sdk/resource-manager/devopsinfrastructure/2024-10-19/pools"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
 
-type Client struct{}
+type Client struct {
+	Pools *pools.PoolsClient
+}
 
 func NewClient(o *common.ClientOptions) (*Client, error) {
-	return &Client{}, nil
+	poolsClient, err := pools.NewPoolsClientWithBaseURI(o.Environment.ResourceManager)
+	if err != nil {
+		return nil, fmt.Errorf("building Pools client: %+v", err)
+	}
+	o.configureFunc(poolsClient.Client, o.Authorizer.ResourceManager)
+
+	return &Client{
+		Pools: poolsClient,
+	}, nil
 }
