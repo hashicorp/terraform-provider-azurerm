39238c1efc3cc7c6845a863416aa3bf717275f4e
BubbleTroubles <bubbletroubles@gmail.com>
2025-03-07 20:58:09 +1100

    fmt

--- internal/services/network/custom_ip_prefix_resource_test.go
+++ internal/services/network/custom_ip_prefix_resource_test.go
@@ -258,7 +258,7 @@ resource "azurerm_custom_ip_prefix" "global" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
 
-  cidr = "%[3]s"
+  cidr                          = "%[3]s"
   prefix_type                   = "Parent"
   roa_validity_end_date         = "2199-12-12"
   wan_validation_signed_message = "signed message for WAN validation"
@@ -270,7 +270,7 @@ resource "azurerm_custom_ip_prefix" "regional" {
   resource_group_name        = azurerm_resource_group.test.name
   parent_custom_ip_prefix_id = azurerm_custom_ip_prefix.global.id
   prefix_type                = "Child"
-  
+
   cidr  = cidrsubnet(azurerm_custom_ip_prefix.global.cidr, 16, 1)
   zones = ["1"]
 }
@@ -348,7 +348,7 @@ resource "azurerm_custom_ip_prefix" "regional" {
   resource_group_name        = azurerm_resource_group.test.name
   parent_custom_ip_prefix_id = azurerm_custom_ip_prefix.global.id
   prefix_type                = "Child"
-  
+
   cidr  = cidrsubnet(azurerm_custom_ip_prefix.global.cidr, 16, 1)
   zones = ["1"]
 
