80a2451387147c0349b600bfb4591584a89ed7ce
Xiaxin Yi (from Dev Box) <xiaxyi@microsoft.com>
2025-03-08 01:53:00 +1100

    update test cases and merge upstream

--- internal/services/appservice/linux_function_app_resource_test.go
+++ internal/services/appservice/linux_function_app_resource_test.go
@@ -4732,7 +4732,7 @@ provider "azurerm" {
 %s
 
 resource "azurerm_linux_function_app" "test" {
-  name                = "acctest-WFA-%d"
+  name                = "acctest-LFA-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   service_plan_id     = azurerm_service_plan.test.id
