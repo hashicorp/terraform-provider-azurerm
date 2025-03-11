b9f54a22047c85e87411d7d7dc0994842c8b75be
Xiaxin Yi (from Dev Box) <xiaxyi@microsoft.com>
2025-03-11 18:39:21 +1100

    revert unrelated changes

--- internal/services/web/app_service_virtual_network_swift_connection_resource_test.go
+++ internal/services/web/app_service_virtual_network_swift_connection_resource_test.go
@@ -153,7 +153,9 @@ resource "azurerm_virtual_network" "test" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  webdeploy_publish_basic_authentication_enabled
+  lifecycle {
+    ignore_changes = [ddos_protection_plan]
+  }
 }
 
 resource "azurerm_subnet" "test1" {
