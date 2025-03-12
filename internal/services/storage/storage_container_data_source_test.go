442a87634cb1238d10b3fb29533a75bd4b6fab10
magodo <wztwcy@gmail.com>
2025-03-12 11:24:46 +1100

    Update ds test

--- internal/services/storage/storage_container_data_source_test.go
+++ internal/services/storage/storage_container_data_source_test.go
@@ -28,7 +28,7 @@ func TestAccStorageContainerDataSource_basic(t *testing.T) {
 				check.That(data.ResourceName).Key("metadata.%").HasValue("2"),
 				check.That(data.ResourceName).Key("metadata.k1").HasValue("v1"),
 				check.That(data.ResourceName).Key("metadata.k2").HasValue("v2"),
-				check.That(data.ResourceName).Key("url").HasValue(fmt.Sprintf("https://acctestacc%s.blob.core.windows.net/vhds", data.RandomString)),
+				check.That(data.ResourceName).Key("url").HasValue(fmt.Sprintf("https://acctestacc%[1]s.blob.core.windows.net/acctest-container-%[1]s", data.RandomString)),
 			),
 		},
 	})
