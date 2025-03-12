480589069f93310347b5a14a9606c0147ed3a9d1
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2025-03-12 07:30:52 -0600

    azurerm_log_analytics_cluster_customer_managed_key - add managed_hsm_key_id

--- internal/services/loganalytics/log_analytics_cluster_customer_managed_key_resource_test.go
+++ internal/services/loganalytics/log_analytics_cluster_customer_managed_key_resource_test.go
@@ -9,17 +9,18 @@ import (
 	"os"
 	"testing"
 
+	"github.com/google/uuid"
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/operationalinsights/2022-10-01/clusters"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
-	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
 type LogAnalyticsClusterCustomerManagedKeyResource struct{}
 
-func TestAccLogAnalyticsClusterCustomerManagedKey_basic(t *testing.T) {
+func TestAccLogAnalyticsClusterCustomerManagedKey_keyVaultKey(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_log_analytics_cluster_customer_managed_key", "test")
 	r := LogAnalyticsClusterCustomerManagedKeyResource{}
 
@@ -30,7 +31,21 @@ func TestAccLogAnalyticsClusterCustomerManagedKey_basic(t *testing.T) {
 
 	data.ResourceTest(t, r, []acceptance.TestStep{
 		{
-			Config: r.complete(data),
+			Config: r.keyVaultKey(data, "azurerm_key_vault_key.test1.id"),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.keyVaultKey(data, "azurerm_key_vault_key.test1.versionless_id"),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.keyVaultKey(data, "azurerm_key_vault_key.test2.id"),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 			),
@@ -39,7 +54,7 @@ func TestAccLogAnalyticsClusterCustomerManagedKey_basic(t *testing.T) {
 	})
 }
 
-func TestAccLogAnalyticsClusterCustomerManagedKey_update(t *testing.T) {
+func TestAccLogAnalyticsClusterCustomerManagedKey_managedHsmKey(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_log_analytics_cluster_customer_managed_key", "test")
 	r := LogAnalyticsClusterCustomerManagedKeyResource{}
 
@@ -50,14 +65,21 @@ func TestAccLogAnalyticsClusterCustomerManagedKey_update(t *testing.T) {
 
 	data.ResourceTest(t, r, []acceptance.TestStep{
 		{
-			Config: r.complete(data),
+			Config: r.managedHsmKey(data, "azurerm_key_vault_managed_hardware_security_module_key.test1.versioned_id"),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 			),
 		},
 		data.ImportStep(),
 		{
-			Config: r.update(data),
+			Config: r.managedHsmKey(data, "azurerm_key_vault_managed_hardware_security_module_key.test1.id"),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.managedHsmKey(data, "azurerm_key_vault_managed_hardware_security_module_key.test2.versioned_id"),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 			),
@@ -74,19 +96,207 @@ func (t LogAnalyticsClusterCustomerManagedKeyResource) Exists(ctx context.Contex
 
 	resp, err := clients.LogAnalytics.ClusterClient.Get(ctx, *id)
 	if err != nil {
-		return nil, fmt.Errorf("reading %s: %+v", *id, err)
+		return nil, fmt.Errorf("retrieving %s: %+v", *id, err)
 	}
 
-	enabled := false
+	exists := false
 	if model := resp.Model; model != nil {
 		if props := model.Properties; props != nil {
 			if kv := props.KeyVaultProperties; kv != nil {
-				enabled = kv.KeyVaultUri != nil && kv.KeyVersion != nil && kv.KeyName != nil
+				exists = kv.KeyVaultUri != nil && kv.KeyVersion != nil && kv.KeyName != nil
 			}
 		}
 	}
 
-	return utils.Bool(enabled), nil
+	return pointer.To(exists), nil
+}
+
+func (r LogAnalyticsClusterCustomerManagedKeyResource) keyVaultKey(data acceptance.TestData, keyId string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_log_analytics_cluster_customer_managed_key" "test" {
+  log_analytics_cluster_id = azurerm_log_analytics_cluster.test.id
+  key_vault_key_id         = %s
+
+  depends_on = [azurerm_key_vault_access_policy.system_identity]
+}
+`, r.keyVaultKeyTemplate(data), keyId)
+}
+
+func (r LogAnalyticsClusterCustomerManagedKeyResource) managedHsmKey(data acceptance.TestData, keyId string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_log_analytics_cluster_customer_managed_key" "test" {
+  log_analytics_cluster_id = azurerm_log_analytics_cluster.test.id
+  managed_hsm_key_id         = %s
+
+  depends_on = [azurerm_key_vault_access_policy.system_identity]
+}
+
+`, r.managedHsmKeyTemplate(data), keyId)
+}
+
+func (r LogAnalyticsClusterCustomerManagedKeyResource) keyVaultKeyTemplate(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_key_vault_key" "test1" {
+  name         = "key1-%[2]s"
+  key_vault_id = azurerm_key_vault.test.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+
+  depends_on = [azurerm_key_vault_access_policy.principal]
+}
+
+resource "azurerm_key_vault_key" "test2" {
+  name         = "key2-%[2]s"
+  key_vault_id = azurerm_key_vault.test.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+
+  depends_on = [azurerm_key_vault_access_policy.principal]
+}
+`, r.template(data), data.RandomString)
+}
+
+func (r LogAnalyticsClusterCustomerManagedKeyResource) managedHsmKeyTemplate(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_key_vault_certificate" "test" {
+  count        = 3
+  name         = "acctest%[2]d${count.index}"
+  key_vault_id = azurerm_key_vault.test.id
+
+  certificate_policy {
+    issuer_parameters {
+      name = "Self"
+    }
+
+    key_properties {
+      exportable = true
+      key_size   = 2048
+      key_type   = "RSA"
+      reuse_key  = true
+    }
+
+    secret_properties {
+      content_type = "application/x-pkcs12"
+    }
+
+    lifetime_action {
+      action {
+        action_type = "AutoRenew"
+      }
+
+      trigger {
+        days_before_expiry = 30
+      }
+    }
+
+    x509_certificate_properties {
+      extended_key_usage = []
+      key_usage = [
+        "cRLSign",
+        "dataEncipherment",
+        "digitalSignature",
+        "keyAgreement",
+        "keyCertSign",
+        "keyEncipherment",
+      ]
+
+      subject            = "CN=hello-world"
+      validity_in_months = 12
+    }
+  }
+
+  depends_on = [azurerm_key_vault_access_policy.principal]
+}
+
+resource "azurerm_key_vault_managed_hardware_security_module" "test" {
+  name                     = "acctest-%[3]s"
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  sku_name                 = "Standard_B1"
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  admin_object_ids         = [data.azurerm_client_config.current.object_id]
+  purge_protection_enabled = false
+
+  security_domain_key_vault_certificate_ids = [for cert in azurerm_key_vault_certificate.test : cert.id]
+  security_domain_quorum                    = 3
+}
+
+resource "azurerm_key_vault_managed_hardware_security_module_role_assignment" "crypto_user_system" {
+  managed_hsm_id     = azurerm_key_vault_managed_hardware_security_module.test.id
+  name               = "%[4]s"
+  scope              = "/"
+  role_definition_id = "/Microsoft.KeyVault/providers/Microsoft.Authorization/roleDefinitions/21dbd100-6940-42c2-9190-5d6cb909625b"
+  principal_id       = azurerm_log_analytics_cluster.test.identity[0].principal_id
+}
+
+resource "azurerm_key_vault_managed_hardware_security_module_role_assignment" "crypto_user_principal" {
+  managed_hsm_id     = azurerm_key_vault_managed_hardware_security_module.test.id
+  name               = "%[5]s"
+  scope              = "/"
+  role_definition_id = "/Microsoft.KeyVault/providers/Microsoft.Authorization/roleDefinitions/21dbd100-6940-42c2-9190-5d6cb909625b"
+  principal_id       = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_key_vault_managed_hardware_security_module_role_assignment" "crypto_officer_principal" {
+  managed_hsm_id     = azurerm_key_vault_managed_hardware_security_module.test.id
+  name               = "%[6]s"
+  scope              = "/"
+  role_definition_id = "/Microsoft.KeyVault/providers/Microsoft.Authorization/roleDefinitions/515eb02d-2335-4d2d-92f2-b1cbdf9c3778"
+  principal_id       = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_key_vault_managed_hardware_security_module_key" "test1" {
+  name           = "acctest1%[2]d"
+  managed_hsm_id = azurerm_key_vault_managed_hardware_security_module.test.id
+  key_type       = "RSA-HSM"
+  key_size       = 2048
+  key_opts       = ["sign", "unwrapKey", "wrapKey"]
+
+  depends_on = [
+    azurerm_key_vault_managed_hardware_security_module_role_assignment.crypto_user_principal,
+    azurerm_key_vault_managed_hardware_security_module_role_assignment.crypto_officer_principal
+  ]
+}
+
+resource "azurerm_key_vault_managed_hardware_security_module_key" "test2" {
+  name           = "acctest2%[2]d"
+  managed_hsm_id = azurerm_key_vault_managed_hardware_security_module.test.id
+  key_type       = "RSA-HSM"
+  key_size       = 2048
+  key_opts       = ["sign", "unwrapKey", "wrapKey"]
+
+  depends_on = [
+    azurerm_key_vault_managed_hardware_security_module_role_assignment.crypto_user_principal,
+    azurerm_key_vault_managed_hardware_security_module_role_assignment.crypto_officer_principal
+  ]
+}
+`, r.template(data), data.RandomInteger, data.RandomString, uuid.New(), uuid.New(), uuid.New())
 }
 
 func (LogAnalyticsClusterCustomerManagedKeyResource) template(data acceptance.TestData) string {
@@ -112,9 +322,8 @@ resource "azurerm_log_analytics_cluster" "test" {
   }
 }
 
-
 resource "azurerm_key_vault" "test" {
-  name                = "vault%[3]s"
+  name                = "acctestvault%[3]s"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
@@ -126,8 +335,11 @@ resource "azurerm_key_vault" "test" {
 }
 
 
-resource "azurerm_key_vault_access_policy" "terraform" {
+resource "azurerm_key_vault_access_policy" "principal" {
   key_vault_id = azurerm_key_vault.test.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
 
   key_permissions = [
     "Create",
@@ -139,35 +351,17 @@ resource "azurerm_key_vault_access_policy" "terraform" {
     "GetRotationPolicy",
   ]
 
-  secret_permissions = [
-    "Get",
-    "Delete",
-    "Set",
-  ]
-
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azurerm_client_config.current.object_id
+  certificate_permissions = [
+      "Create",
+      "Delete",
+      "DeleteIssuers",
+      "Get",
+      "Purge",
+      "Update"
+    ]
 }
 
-resource "azurerm_key_vault_key" "test" {
-  name         = "key-%[3]s"
-  key_vault_id = azurerm_key_vault.test.id
-  key_type     = "RSA"
-  key_size     = 2048
-
-  key_opts = [
-    "decrypt",
-    "encrypt",
-    "sign",
-    "unwrapKey",
-    "verify",
-    "wrapKey",
-  ]
-
-  depends_on = [azurerm_key_vault_access_policy.terraform]
-}
-
-resource "azurerm_key_vault_access_policy" "test" {
+resource "azurerm_key_vault_access_policy" "system_identity" {
   key_vault_id = azurerm_key_vault.test.id
 
   key_permissions = [
@@ -180,52 +374,7 @@ resource "azurerm_key_vault_access_policy" "test" {
   tenant_id = azurerm_log_analytics_cluster.test.identity.0.tenant_id
   object_id = azurerm_log_analytics_cluster.test.identity.0.principal_id
 
-  depends_on = [azurerm_key_vault_access_policy.terraform]
+  depends_on = [azurerm_key_vault_access_policy.principal]
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomString)
-}
-
-func (r LogAnalyticsClusterCustomerManagedKeyResource) complete(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-%s
-
-resource "azurerm_log_analytics_cluster_customer_managed_key" "test" {
-  log_analytics_cluster_id = azurerm_log_analytics_cluster.test.id
-  key_vault_key_id         = azurerm_key_vault_key.test.id
-
-  depends_on = [azurerm_key_vault_access_policy.test]
-}
-`, r.template(data))
-}
-
-func (r LogAnalyticsClusterCustomerManagedKeyResource) update(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-%s
-
-resource "azurerm_key_vault_key" "test2" {
-  name         = "key2-%[2]s"
-  key_vault_id = azurerm_key_vault.test.id
-  key_type     = "RSA"
-  key_size     = 2048
-
-  key_opts = [
-    "decrypt",
-    "encrypt",
-    "sign",
-    "unwrapKey",
-    "verify",
-    "wrapKey",
-  ]
-
-  depends_on = [azurerm_key_vault_access_policy.terraform]
-}
-
-resource "azurerm_log_analytics_cluster_customer_managed_key" "test" {
-  log_analytics_cluster_id = azurerm_log_analytics_cluster.test.id
-  key_vault_key_id         = azurerm_key_vault_key.test2.id
-
-  depends_on = [azurerm_key_vault_access_policy.test]
-}
-
-`, r.template(data), data.RandomString)
 }
--- website/docs/r/key_vault_managed_hardware_security_module_key.html.markdown
+++ website/docs/r/key_vault_managed_hardware_security_module_key.html.markdown
@@ -36,7 +36,7 @@ resource "azurerm_key_vault_managed_hardware_security_module" "example" {
 
 // this gives your service principal the HSM Crypto User role which lets you create and destroy hsm keys
 resource "azurerm_key_vault_managed_hardware_security_module_role_assignment" "hsm-crypto-user" {
-  managed_hsm_id     = azurerm_key_vault_managed_hardware_security_module.test.id
+  managed_hsm_id     = azurerm_key_vault_managed_hardware_security_module.example.id
   name               = "1e243909-064c-6ac3-84e9-1c8bf8d6ad22"
   scope              = "/keys"
   role_definition_id = "/Microsoft.KeyVault/providers/Microsoft.Authorization/roleDefinitions/21dbd100-6940-42c2-9190-5d6cb909625b"
@@ -45,7 +45,7 @@ resource "azurerm_key_vault_managed_hardware_security_module_role_assignment" "h
 
 // this gives your service principal the HSM Crypto Officer role which lets you purge hsm keys
 resource "azurerm_key_vault_managed_hardware_security_module_role_assignment" "hsm-crypto-officer" {
-  managed_hsm_id     = azurerm_key_vault_managed_hardware_security_module.test.id
+  managed_hsm_id     = azurerm_key_vault_managed_hardware_security_module.example.id
   name               = "1e243909-064c-6ac3-84e9-1c8bf8d6ad23"
   scope              = "/keys"
   role_definition_id = "/Microsoft.KeyVault/providers/Microsoft.Authorization/roleDefinitions/515eb02d-2335-4d2d-92f2-b1cbdf9c3778"
@@ -54,14 +54,14 @@ resource "azurerm_key_vault_managed_hardware_security_module_role_assignment" "h
 
 resource "azurerm_key_vault_managed_hardware_security_module_key" "example" {
   name           = "example"
-  managed_hsm_id = azurerm_key_vault_managed_hardware_security_module.test.id
+  managed_hsm_id = azurerm_key_vault_managed_hardware_security_module.example.id
   key_type       = "EC-HSM"
   curve          = "P-521"
   key_opts       = ["sign"]
 
   depends_on = [
-    azurerm_key_vault_managed_hardware_security_module_role_assignment.test,
-    azurerm_key_vault_managed_hardware_security_module_role_assignment.test1
+    azurerm_key_vault_managed_hardware_security_module_role_assignment.hsm-crypto-user,
+    azurerm_key_vault_managed_hardware_security_module_role_assignment.hsm-crypto-officer
   ]
 }
 ```
@@ -74,19 +74,21 @@ The following arguments are supported:
 
 * `managed_hsm_id` - (Required) Specifies the ID of the Key Vault Managed Hardware Security Module that they key will be owned by. Changing this forces a new resource to be created.
 
+* `key_opts` - (Required) A list of JSON web key operations. Possible values include: `decrypt`, `encrypt`, `sign`, `unwrapKey`, `verify` and `wrapKey`. Please note these values are case-sensitive.
+
 * `key_type` - (Required) Specifies the Key Type to use for this Key Vault Managed Hardware Security Module Key. Possible values are `EC-HSM`, `oct-HSM` and `RSA-HSM`. More details see [HSM-protected keys](https://learn.microsoft.com/en-us/azure/key-vault/keys/about-keys#hsm-protected-keys). Changing this forces a new resource to be created.
 
-* `key_size` - (Optional) Specifies the Size of the RSA key to create in bytes. For example, 1024 or 2048. *Note*: This field is required if `key_type` is `RSA-HSM` or `oct-HSM`. Changing this forces a new resource to be created.
+---
 
 * `curve` - (Optional) Specifies the curve to use when creating an `EC-HSM` key. Possible values are `P-256`, `P-256K`, `P-384`, and `P-521`. This field is required if `key_type` is `EC-HSM`. Changing this forces a new resource to be created.
 
-* `key_opts` - (Required) A list of JSON web key operations. Possible values include: `decrypt`, `encrypt`, `sign`, `unwrapKey`, `verify` and `wrapKey`. Please note these values are case-sensitive.
-
-* `not_before_date` - (Optional) Key not usable before the provided UTC datetime (Y-m-d'T'H:M:S'Z').
+* `expiration_date` - (Optional) Expiration UTC datetime (Y-m-d'T'H:M:S'Z'). When this parameter gets changed on reruns, if newer date is ahead of current date, an update is performed. If the newer date is before the current date, resource will be force created.
 
 ~> **Note:** Once `expiration_date` is set, it's not possible to unset the key even if it is deleted & recreated as underlying Azure API uses the restore of the purged key.
 
-* `expiration_date` - (Optional) Expiration UTC datetime (Y-m-d'T'H:M:S'Z'). When this parameter gets changed on reruns, if newer date is ahead of current date, an update is performed. If the newer date is before the current date, resource will be force created.
+* `key_size` - (Optional) Specifies the Size of the RSA key to create in bytes. For example, 1024 or 2048. *Note*: This field is required if `key_type` is `RSA-HSM` or `oct-HSM`. Changing this forces a new resource to be created.
+
+* `not_before_date` - (Optional) Key not usable before the provided UTC datetime (Y-m-d'T'H:M:S'Z').
 
 * `tags` - (Optional) A mapping of tags to assign to the resource.
 
@@ -94,9 +96,9 @@ The following arguments are supported:
 
 In addition to the Arguments listed above - the following Attributes are exported:
 
-* `id` - The Key Vault Secret Managed Hardware Security Module Key ID.
+* `id` - The Key Vault Managed Hardware Security Module Key ID.
 
-* `versioned_id` - The versioned Key Vault Secret Managed Hardware Security Module Key ID.
+* `versioned_id` - The versioned Key Vault Managed Hardware Security Module Key ID.
 
 
 ## Timeouts
@@ -104,8 +106,8 @@ In addition to the Arguments listed above - the following Attributes are exporte
 The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/language/resources/syntax#operation-timeouts) for certain actions:
 
 * `create` - (Defaults to 60 minutes) Used when creating the Key Vault Managed Hardware Security Module Key.
-* `update` - (Defaults to 30 minutes) Used when updating the Key Vault Managed Hardware Security Module Key.
 * `read` - (Defaults to 5 minutes) Used when retrieving the Key Vault Managed Hardware Security Module Key.
+* `update` - (Defaults to 30 minutes) Used when updating the Key Vault Managed Hardware Security Module Key.
 * `delete` - (Defaults to 60 minutes) Used when deleting the Key Vault Managed Hardware Security Module Key.
 
 ## Import
