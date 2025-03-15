faf9a5ce50fb1f3e7168d5b0e1d00e7b50e60785
Jeecie <jean.christophe.gauvreau@hotmail.com>
2025-03-15 15:38:43 -0400

    update node app stack description

--- internal/services/appservice/helpers/function_app_schema.go
+++ internal/services/appservice/helpers/function_app_schema.go
@@ -1425,7 +1425,7 @@ func linuxFunctionAppStackSchema() *pluginsdk.Schema {
 						"site_config.0.application_stack.0.docker",
 						"site_config.0.application_stack.0.use_custom_runtime",
 					},
-					Description: "The version of Node to use. Possible values include `12`, `14`, `16`, `18` and `20`",
+					Description: "The version of Node to use. Possible values include `12`, `14`, `16`, `18`, `20` and `22`",
 				},
 
 				"powershell_core_version": {
