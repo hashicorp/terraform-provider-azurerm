4a8a6e2dd37ea58c7e541e844d4bbfeeee18b2f8
teowa <104055472+teowa@users.noreply.github.com>
2025-03-24 16:11:11 +0800

    `azurerm_network_manager` - `scope_accesses` is now optional (#28781)
    * mark scope_accesses as optional
    
    * fix test
    
    * fix doc

--- internal/services/network/network_manager_resource_test.go
+++ internal/services/network/network_manager_resource_test.go
@@ -231,7 +231,6 @@ resource "azurerm_network_manager" "test" {
   scope {
     subscription_ids = [data.azurerm_subscription.current.id]
   }
-  scope_accesses = ["SecurityAdmin"]
 }
 `, r.template(data), data.RandomInteger)
 }
@@ -246,7 +245,6 @@ resource "azurerm_network_manager" "import" {
   scope {
     subscription_ids = azurerm_network_manager.test.scope.0.subscription_ids
   }
-  scope_accesses = azurerm_network_manager.test.scope_accesses
 }
 `, r.basic(data))
 }
