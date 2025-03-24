bb67c9ad669f8e8e6dd9e79ec9f4eb368d7dad14
neil-yechenwei <yechenwei2007@hotmail.com>
2025-03-24 13:40:55 +0800

    azurerm_powerbi_embedded - support for new value for sku_name

--- internal/services/powerbi/powerbi_embedded_resource_test.go
+++ internal/services/powerbi/powerbi_embedded_resource_test.go
@@ -162,7 +162,7 @@ resource "azurerm_powerbi_embedded" "test" {
   name                = "acctestpowerbi%[2]d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  sku_name            = "A2"
+  sku_name            = "A8"
   administrators      = [data.azurerm_client_config.test.object_id]
 
   tags = {
