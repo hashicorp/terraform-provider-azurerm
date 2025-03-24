7b75e993ef34910db211e725a307551f1f876405
neil-yechenwei <yechenwei2007@hotmail.com>
2025-03-24 21:09:45 +0800

    rever tc

--- internal/services/powerbi/powerbi_embedded_resource_test.go
+++ internal/services/powerbi/powerbi_embedded_resource_test.go
@@ -180,7 +180,7 @@ resource "azurerm_powerbi_embedded" "test" {
   name                = "acctestpowerbi%[2]d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  sku_name            = "A7"
+  sku_name            = "A1"
   administrators      = [data.azurerm_client_config.test.object_id]
   mode                = "Gen2"
 }
