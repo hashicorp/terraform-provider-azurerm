069731f2989a0062ab930ea3cecc8bf2ea1a6249
neil-yechenwei <yechenwei2007@hotmail.com>
2025-03-26 16:02:51 +0800

    sort pkg

--- internal/services/network/vpn_gateway_data_source.go
+++ internal/services/network/vpn_gateway_data_source.go
@@ -5,9 +5,9 @@ package network
 
 import (
 	"fmt"
-	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"time"
 
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
