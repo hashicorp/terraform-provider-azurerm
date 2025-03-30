b67ebdc82ae9ac2e6191e0277d74498be1788955
Ajay Krishna Nalisetty <ajaykn@microsoft.com>
2025-03-30 23:53:31 +0900

    add missing import

--- internal/services/manageddevopspools/managed_devops_pool_data_source_test.go
+++ internal/services/manageddevopspools/managed_devops_pool_data_source_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 )
