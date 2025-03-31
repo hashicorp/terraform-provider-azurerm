881b4bb22e9abb086fbc2a68dc2f5b2e5226ca06
neil-yechenwei <yechenwei2007@hotmail.com>
2025-03-31 14:06:32 +0800

    update tc

--- internal/services/network/subnet_network_security_group_association_resource_test.go
+++ internal/services/network/subnet_network_security_group_association_resource_test.go
@@ -219,6 +219,18 @@ resource "azurerm_network_security_group" "test" {
   name                = "acctestnsg%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
 }
