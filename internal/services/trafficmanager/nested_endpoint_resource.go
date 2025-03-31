3099f5dbb8d69d6fab17aa702f304a923d84af9d
ziyeqf <51212351+ziyeqf@users.noreply.github.com>
2025-03-31 05:22:05 +0000

    `azurerm_traffic_manager_nested_endpoint` - revert priority back to O+C since API increments the default value

--- internal/services/trafficmanager/nested_endpoint_resource.go
+++ internal/services/trafficmanager/nested_endpoint_resource.go
@@ -118,8 +118,10 @@ func resourceNestedEndpoint() *pluginsdk.Resource {
 			},
 
 			"priority": {
-				Type:         pluginsdk.TypeInt,
-				Optional:     true,
+				Type:     pluginsdk.TypeInt,
+				Optional: true,
+				// NOTE: O+C the API dynamically increments the default value for priority depending on the number of endpoints
+				Computed:     true,
 				Default:      1,
 				ValidateFunc: validation.IntBetween(1, 1000),
 			},
