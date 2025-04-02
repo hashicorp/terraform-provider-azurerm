f228c1daf048218b4927401b6029a3d1a5a8851f
Wyatt Fry <wyattfry@gmail.com>
2025-04-02 17:32:57 -0400

    remove unused import

--- internal/services/monitor/monitor_diagnostic_setting_resource.go
+++ internal/services/monitor/monitor_diagnostic_setting_resource.go
@@ -26,7 +26,6 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
-	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
 func resourceMonitorDiagnosticSetting() *pluginsdk.Resource {
