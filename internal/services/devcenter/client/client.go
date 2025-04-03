f4e596309529d21cad4edba4e0346d8e8de81ce5
Neil Ye <yechenwei2007@hotmail.com>
2025-04-03 15:25:06 +0800

    Upgrade DevCenter API version from 2023-04-01 to 2025-02-01 (#29145)
    * Upgrade DevCenter API version from 2024-05-01 to 2025-02-01
    
    * use pointer.from
    
    * update image resource id
    
    * remove test step for recreation

--- internal/services/devcenter/client/client.go
+++ internal/services/devcenter/client/client.go
@@ -6,24 +6,24 @@ package client
 import (
 	"fmt"
 
-	devcenterV20230401 "github.com/hashicorp/go-azure-sdk/resource-manager/devcenter/2023-04-01"
+	devcenterV20250201 "github.com/hashicorp/go-azure-sdk/resource-manager/devcenter/2025-02-01"
 	"github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
 
 type AutoClient struct {
-	V20230401 devcenterV20230401.Client
+	V20250201 devcenterV20250201.Client
 }
 
 func NewClient(o *common.ClientOptions) (*AutoClient, error) {
-	v20230401Client, err := devcenterV20230401.NewClientWithBaseURI(o.Environment.ResourceManager, func(c *resourcemanager.Client) {
+	v20250201Client, err := devcenterV20250201.NewClientWithBaseURI(o.Environment.ResourceManager, func(c *resourcemanager.Client) {
 		o.Configure(c, o.Authorizers.ResourceManager)
 	})
 	if err != nil {
-		return nil, fmt.Errorf("building client for devcenter V20230401: %+v", err)
+		return nil, fmt.Errorf("building client for devcenter V20250201: %+v", err)
 	}
 
 	return &AutoClient{
-		V20230401: *v20230401Client,
+		V20250201: *v20250201Client,
 	}, nil
 }
