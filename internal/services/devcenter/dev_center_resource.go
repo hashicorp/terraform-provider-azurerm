13a3013148be9d95e0836f46a67c360407d76e52
neil-yechenwei <yechenwei2007@hotmail.com>
2025-04-07 15:03:15 +0800

    azurerm_dev_center - support new property for dev_center_project_catalog_item_sync_enabled

--- internal/services/devcenter/dev_center_resource.go
+++ internal/services/devcenter/dev_center_resource.go
@@ -31,12 +31,13 @@ func (r DevCenterResource) ModelObject() interface{} {
 }
 
 type DevCenterResourceSchema struct {
-	DevCenterUri      string                                     `tfschema:"dev_center_uri"`
-	Identity          []identity.ModelSystemAssignedUserAssigned `tfschema:"identity"`
-	Location          string                                     `tfschema:"location"`
-	Name              string                                     `tfschema:"name"`
-	ResourceGroupName string                                     `tfschema:"resource_group_name"`
-	Tags              map[string]interface{}                     `tfschema:"tags"`
+	DevCenterUri                           string                                     `tfschema:"dev_center_uri"`
+	Identity                               []identity.ModelSystemAssignedUserAssigned `tfschema:"identity"`
+	Location                               string                                     `tfschema:"location"`
+	Name                                   string                                     `tfschema:"name"`
+	ResourceGroupName                      string                                     `tfschema:"resource_group_name"`
+	DevCenterProjectCatalogItemSyncEnabled bool                                       `tfschema:"dev_center_project_catalog_item_sync_enabled"`
+	Tags                                   map[string]interface{}                     `tfschema:"tags"`
 }
 
 func (r DevCenterResource) IDValidationFunc() pluginsdk.SchemaValidateFunc {
@@ -56,8 +57,12 @@ func (r DevCenterResource) Arguments() map[string]*pluginsdk.Schema {
 			Type:     pluginsdk.TypeString,
 		},
 		"resource_group_name": commonschema.ResourceGroupName(),
-		"identity":            commonschema.SystemAssignedUserAssignedIdentityOptional(),
-		"tags":                commonschema.Tags(),
+		"dev_center_project_catalog_item_sync_enabled": {
+			Type:     pluginsdk.TypeBool,
+			Optional: true,
+		},
+		"identity": commonschema.SystemAssignedUserAssignedIdentityOptional(),
+		"tags":     commonschema.Tags(),
 	}
 }
 
@@ -215,6 +220,14 @@ func (r DevCenterResource) mapDevCenterResourceSchemaToDevCenter(input DevCenter
 		output.Properties = &devcenters.DevCenterProperties{}
 	}
 
+	catalogItemSyncEnableStatus := devcenters.CatalogItemSyncEnableStatusDisabled
+	if input.DevCenterProjectCatalogItemSyncEnabled {
+		catalogItemSyncEnableStatus = devcenters.CatalogItemSyncEnableStatusEnabled
+	}
+	output.Properties.ProjectCatalogSettings = &devcenters.DevCenterProjectCatalogSettings{
+		CatalogItemSyncEnableStatus: pointer.To(catalogItemSyncEnableStatus),
+	}
+
 	return nil
 }
 
@@ -233,6 +246,10 @@ func (r DevCenterResource) mapDevCenterToDevCenterResourceSchema(input devcenter
 	}
 
 	output.DevCenterUri = pointer.From(input.Properties.DevCenterUri)
+
+	if v := input.Properties.ProjectCatalogSettings; v != nil && v.CatalogItemSyncEnableStatus != nil {
+		output.DevCenterProjectCatalogItemSyncEnabled = pointer.From(input.Properties.ProjectCatalogSettings.CatalogItemSyncEnableStatus) == devcenters.CatalogItemSyncEnableStatusEnabled
+	}
 
 	return nil
 }
