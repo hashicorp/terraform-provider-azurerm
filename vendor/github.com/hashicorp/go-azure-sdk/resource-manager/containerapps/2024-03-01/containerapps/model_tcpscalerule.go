eeaec7555b25bc322c476b27a02405b6b2497b8d
xuwu1 <xuwu1@microsoft.com>
2025-04-09 15:06:31 +1000

    update 2024-03-01 to 2025-01-01

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/containerapps/2024-03-01/containerapps/model_tcpscalerule.go
+++
@@ -1,9 +0,0 @@
-package containerapps
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type TcpScaleRule struct {
-	Auth     *[]ScaleRuleAuth   `json:"auth,omitempty"`
-	Metadata *map[string]string `json:"metadata,omitempty"`
-}
