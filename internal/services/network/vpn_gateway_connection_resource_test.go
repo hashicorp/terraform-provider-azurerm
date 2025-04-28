e83b76ae7c06cb810eba0ef3f0944ba233b29ce2
neil-yechenwei <yechenwei2007@hotmail.com>
2025-04-28 15:55:09 +0800

    azurerm_vpn_gateway_connection - support for new property dpd_timeout_seconds

--- internal/services/network/vpn_gateway_connection_resource_test.go
+++ internal/services/network/vpn_gateway_connection_resource_test.go
@@ -292,12 +292,13 @@ func (r VPNGatewayConnectionResource) complete(data acceptance.TestData) string 
 %s
 
 resource "azurerm_vpn_gateway_connection" "test" {
-  name               = "acctest-VpnGwConn-%[2]d"
-  vpn_gateway_id     = azurerm_vpn_gateway.test.id
-  remote_vpn_site_id = azurerm_vpn_site.test.id
+  name                = "acctest-VpnGwConn-%[2]d"
+  vpn_gateway_id      = azurerm_vpn_gateway.test.id
+  remote_vpn_site_id  = azurerm_vpn_site.test.id
   vpn_link {
-    name             = "link1"
-    vpn_site_link_id = azurerm_vpn_site.test.link[0].id
+    name                = "link1"
+    dpd_timeout_seconds = 30
+    vpn_site_link_id    = azurerm_vpn_site.test.link[0].id
   }
   vpn_link {
     name             = "link2"
