4add304503badf505265ad0cf0e524983faa5db9
Wyatt Fry <wyattfry@gmail.com>
2025-04-29 08:07:15 -0400

    fix replication schedule

--- internal/services/netapp/netapp_volume_group_sap_hana_resource_test.go
+++ internal/services/netapp/netapp_volume_group_sap_hana_resource_test.go
@@ -8,13 +8,13 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/netapp/2025-01-01/volumegroups"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
-	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
 type NetAppVolumeGroupSAPHanaResource struct{}
@@ -134,12 +134,12 @@ func (t NetAppVolumeGroupSAPHanaResource) Exists(ctx context.Context, clients *c
 	resp, err := clients.NetApp.VolumeGroupClient.Get(ctx, *id)
 	if err != nil {
 		if response.WasNotFound(resp.HttpResponse) {
-			return utils.Bool(false), nil
+			return pointer.To(false), nil
 		}
 		return nil, fmt.Errorf("retrieving %s: %+v", id, err)
 	}
 
-	return utils.Bool(true), nil
+	return pointer.To(true), nil
 }
 
 func (NetAppVolumeGroupSAPHanaResource) basic(data acceptance.TestData) string {
@@ -950,7 +950,7 @@ resource "azurerm_netapp_volume_group_sap_hana" "test_secondary" {
       endpoint_type             = "dst"
       remote_volume_location    = azurerm_netapp_volume_group_sap_hana.test_primary.location
       remote_volume_resource_id = azurerm_netapp_volume_group_sap_hana.test_primary.volume[0].id
-      replication_frequency     = "10minutes"
+      replication_frequency     = "Daily"
     }
 
     tags = {
@@ -1017,7 +1017,7 @@ resource "azurerm_netapp_volume_group_sap_hana" "test_secondary" {
       endpoint_type             = "dst"
       remote_volume_location    = azurerm_netapp_volume_group_sap_hana.test_primary.location
       remote_volume_resource_id = azurerm_netapp_volume_group_sap_hana.test_primary.volume[2].id
-      replication_frequency     = "10minutes"
+      replication_frequency     = "Daily"
     }
 
     tags = {
