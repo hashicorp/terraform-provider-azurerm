bff502ffd09fe17524f3f6c9b7fb4891c37a0ebf
Xiaxin Yi (from Dev Box) <xiaxyi@microsoft.com>
2025-04-29 17:06:13 +0900

    add vnet image pull to linux web app

--- internal/services/appservice/linux_web_app_resource_test.go
+++ internal/services/appservice/linux_web_app_resource_test.go
@@ -1784,6 +1784,25 @@ resource "azurerm_linux_web_app" "test" {
 `, r.baseTemplate(data), data.RandomInteger)
 }
 
+func (r LinuxWebAppResource) vnetImagePull(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+%s
+
+resource "azurerm_linux_web_app" "test" {
+  name                = "acctestWA-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  service_plan_id     = azurerm_service_plan.test.id
+
+  site_config {}
+}
+`, r.baseTemplate(data), data.RandomInteger)
+}
+
 func (r LinuxWebAppResource) basicWithTags(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
@@ -2011,6 +2030,7 @@ resource "azurerm_linux_web_app" "test" {
     mount_path   = "/storage/files"
   }
 
+  vnet_image_pull_enabled                        = true
   ftp_publish_basic_authentication_enabled       = false
   webdeploy_publish_basic_authentication_enabled = false
 
