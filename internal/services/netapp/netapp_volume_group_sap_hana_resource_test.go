f7f3767d5fbf736fceea84aed05da09afd84996e
Wyatt Fry <wyattfry@gmail.com>
2025-05-05 14:27:08 -0400

    fix paired region

--- internal/services/netapp/netapp_volume_group_sap_hana_resource_test.go
+++ internal/services/netapp/netapp_volume_group_sap_hana_resource_test.go
@@ -914,9 +914,20 @@ resource "azurerm_netapp_volume_group_sap_hana" "test_primary" {
   ]
 }
 
+locals {
+  pairs = tomap({
+    # This map must be manually kept up-to-date
+    # https://learn.microsoft.com/en-us/azure/azure-netapp-files/cross-region-replication-introduction
+    "westeurope" : "northeurope",
+    "eastus2" : "centralus",
+    "westus2" : "eastus",
+  })
+  location_pair = lookup(local.pairs, azurerm_resource_group.test.location, null)
+}
+
 resource "azurerm_netapp_volume_group_sap_hana" "test_secondary" {
   name                   = "acctest-NetAppVolumeGroup-Secondary-%[2]d"
-  location               = "%[3]s"
+  location               = local.location_pair
   resource_group_name    = azurerm_resource_group.test.name
   account_name           = azurerm_netapp_account.test_secondary.name
   group_description      = "Test volume group"
@@ -1033,7 +1044,7 @@ resource "azurerm_netapp_volume_group_sap_hana" "test_secondary" {
 }
 
 
-`, template, data.RandomInteger, data.Locations.Secondary)
+`, template, data.RandomInteger)
 }
 
 func (r NetAppVolumeGroupSAPHanaResource) templateForAvgCrossRegionReplication(data acceptance.TestData) string {
