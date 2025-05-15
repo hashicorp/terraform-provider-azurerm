3d6bb4727eca9e06de5ff7d745ab44e1050e8692
elena <sinbai@hotmail.com>
2025-05-15 17:19:31 +0800

    update code

--- internal/services/network/web_application_firewall_policy_resource.go
+++ internal/services/network/web_application_firewall_policy_resource.go
@@ -41,7 +41,7 @@ func resourceWebApplicationFirewallPolicy() *pluginsdk.Resource {
 			Create: pluginsdk.DefaultTimeout(30 * time.Minute),
 			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
 			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
-			Delete: pluginsdk.DefaultTimeout(50 * time.Minute),
+			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
 		},
 
 		SchemaVersion: 1,
