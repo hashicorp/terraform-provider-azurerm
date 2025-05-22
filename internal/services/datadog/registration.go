1ae547acda61b9bd03e1e3c68ae50941d9715610
Wyatt Fry <wyattfry@gmail.com>
2025-05-22 16:52:08 -0400

    remove datadog sso conf in v5

--- internal/services/datadog/registration.go
+++ internal/services/datadog/registration.go
@@ -3,7 +3,10 @@
 
 package datadog
 
-import "github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+import (
+	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+)
 
 type Registration struct{}
 
@@ -30,9 +33,14 @@ func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
 
 // SupportedResources returns the supported Resources supported by this Service
 func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
-	return map[string]*pluginsdk.Resource{
-		"azurerm_datadog_monitor":                   resourceDatadogMonitor(),
-		"azurerm_datadog_monitor_tag_rule":          resourceDatadogTagRules(),
-		"azurerm_datadog_monitor_sso_configuration": resourceDatadogSingleSignOnConfigurations(),
+	resources := map[string]*pluginsdk.Resource{
+		"azurerm_datadog_monitor":          resourceDatadogMonitor(),
+		"azurerm_datadog_monitor_tag_rule": resourceDatadogTagRules(),
 	}
+
+	if !features.FivePointOh() {
+		resources["azurerm_datadog_monitor_sso_configuration"] = resourceDatadogSingleSignOnConfigurations()
+	}
+
+	return resources
 }
