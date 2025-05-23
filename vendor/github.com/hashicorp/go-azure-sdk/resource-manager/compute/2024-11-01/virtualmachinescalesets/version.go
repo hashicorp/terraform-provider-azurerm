34bf1d4a71cc653c9de9e4d7c537140943b4d9ed
Zhenhua Hu <zhhu@microsoft.com>
2025-05-24 05:51:25 +0800

    upgrade compute to 2024-11-01 (#29666)

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2024-11-01/virtualmachinescalesets/version.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2024-11-01/virtualmachinescalesets/version.go
@@ -3,7 +3,8 @@ package virtualmachinescalesets
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-type ScaleInPolicy struct {
-	ForceDeletion *bool                                 `json:"forceDeletion,omitempty"`
-	Rules         *[]VirtualMachineScaleSetScaleInRules `json:"rules,omitempty"`
+const defaultApiVersion = "2024-11-01"
+
+func userAgent() string {
+	return "hashicorp/go-azure-sdk/virtualmachinescalesets/2024-11-01"
 }
