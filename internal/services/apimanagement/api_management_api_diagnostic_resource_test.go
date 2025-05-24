hashicorp/terraform-provider-azurerm

commit 6e8c18e858e4cd5a7b1ff82d160dd9c9570894ee

azurerm_api_management_api_diagnostic & azurerm_api_management_diagnostic - error out when operation_name_format is set with identity not applicationinsights (#27630)
* azurerm_api_management_api_diagnostic & azurerm_api_management_diagnostic - error out when operation_name_format is set with identity not applicationinsights

* use customize diff because matthew said so

* update cdiff

* Update internal/services/apimanagement/api_management_api_diagnostic_resource.go

Co-authored-by: stephybun <steph@hashicorp.com>

* Update internal/services/apimanagement/api_management_diagnostic_resource.go

Co-authored-by: stephybun <steph@hashicorp.com>

---------

Co-authored-by: stephybun <steph@hashicorp.com>

internal/services/apimanagement/api_management_api_diagnostic_resource.go
@@ -4,6 +4,7 @@
 package apimanagement
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"time"
@@ -123,6 +124,16 @@ func resourceApiManagementApiDiagnostic() *pluginsdk.Resource {
 				}, false),
 			},
 		},
+
+		CustomizeDiff: func(ctx context.Context, d *pluginsdk.ResourceDiff, i interface{}) error {
+			if _, n := d.GetChange("operation_name_format"); n != "" {
+				if d.Get("identifier") != "applicationinsights" {
+					return fmt.Errorf("`operation_name_format` cannot be set when `identifier` is not `applicationinsights`")
+				}
+			}
+
+			return nil
+		},
 	}
 }
 
@@ -191,8 +202,10 @@ func resourceApiManagementApiDiagnosticCreateUpdate(d *pluginsdk.ResourceData, m
 		},
 	}
 
-	if d.Get("identifier") == "applicationinsights" {
-		parameters.Properties.OperationNameFormat = pointer.To(apidiagnostic.OperationNameFormat(d.Get("operation_name_format").(string)))
+	if operationNameFormat, ok := d.GetOk("operation_name_format"); ok {
+		if d.Get("identifier") == "applicationinsights" {
+			parameters.Properties.OperationNameFormat = pointer.To(apidiagnostic.OperationNameFormat(operationNameFormat.(string)))
+		}
 	}
 
 	samplingPercentage := d.GetRawConfig().AsValueMap()["sampling_percentage"]

internal/services/apimanagement/api_management_diagnostic_resource.go
@@ -4,6 +4,7 @@
 package apimanagement
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"time"
@@ -120,6 +121,16 @@ func resourceApiManagementDiagnostic() *pluginsdk.Resource {
 				}, false),
 			},
 		},
+
+		CustomizeDiff: func(ctx context.Context, d *pluginsdk.ResourceDiff, i interface{}) error {
+			if _, n := d.GetChange("operation_name_format"); n != "" {
+				if d.Get("identifier") != "applicationinsights" {
+					return fmt.Errorf("`operation_name_format` cannot be set when `identifier` is not `applicationinsights`")
+				}
+			}
+
+			return nil
+		},
 	}
 }
 
@@ -150,8 +161,8 @@ func resourceApiManagementDiagnosticCreateUpdate(d *pluginsdk.ResourceData, meta
 		},
 	}
 
-	if d.Get("identifier") == "applicationinsights" {
-		if operationNameFormat, ok := d.GetOk("operation_name_format"); ok {
+	if operationNameFormat, ok := d.GetOk("operation_name_format"); ok {
+		if d.Get("identifier") == "applicationinsights" {
 			parameters.Properties.OperationNameFormat = pointer.To(diagnostic.OperationNameFormat(operationNameFormat.(string)))
 		}
 	}

commit ed049110262fd7e62d8893c995899226db3b21d2

azurerm_api_management_api_diagnostic - fix bug that rejected "azuremonitor" identifier
Previously, `CustomizeDiff` in `resourceApiManagementApiDiagnostic`
always returned an error when the `identifier` was "azuremonitor"
because the `operation_name_format` attribute has a default value.
This regression was introduced in
https://github.com/hashicorp/terraform-provider-azurerm/pull/27630.
This commit fixes the issue by checking whether the value is
explicitly set.

internal/services/apimanagement/api_management_api_diagnostic_resource.go
@@ -126,10 +126,9 @@ func resourceApiManagementApiDiagnostic() *pluginsdk.Resource {
 		},
 
 		CustomizeDiff: func(ctx context.Context, d *pluginsdk.ResourceDiff, i interface{}) error {
-			if _, n := d.GetChange("operation_name_format"); n != "" {
-				if d.Get("identifier") != "applicationinsights" {
-					return fmt.Errorf("`operation_name_format` cannot be set when `identifier` is not `applicationinsights`")
-				}
+			format := d.GetRawConfig().GetAttr("operation_name_format")
+			if !format.IsNull() && d.Get("identifier") != "applicationinsights" {
+				return fmt.Errorf("`operation_name_format` cannot be set when `identifier` is not `applicationinsights`")
 			}
 
 			return nil

internal/services/apimanagement/api_management_api_diagnostic_resource_test.go
@@ -6,6 +6,7 @@ package apimanagement_test
 import (
 	"context"
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
@@ -126,6 +127,33 @@ func TestAccApiManagementApiDiagnostic_dataMasking(t *testing.T) {
 			),
 		},
 		data.ImportStep(),
+	})
+}
+
+func TestAccApiManagementApiDiagnostic_azuremonitor(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_api_management_api_diagnostic", "test")
+	r := ApiManagementApiDiagnosticResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.azuremonitor(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccApiManagementApiDiagnostic_azuremonitorWithOperationNameFormat(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_api_management_api_diagnostic", "test")
+	r := ApiManagementApiDiagnosticResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config:      r.azuremonitorWithOperationNameFormat(data),
+			ExpectError: regexp.MustCompile("`operation_name_format` cannot be set when `identifier` is not `applicationinsights`"),
+		},
 	})
 }
 
@@ -435,4 +463,114 @@ resource "azurerm_api_management_api_diagnostic" "test" {
   }
 }
 `, r.template(data))
+}
+
+func (ApiManagementApiDiagnosticResource) templateAzureMonitor(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_log_analytics_workspace" "test" {
+  name                = "acctestLAW-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  sku = "PerGB2018"
+
+  internet_ingestion_enabled = false
+}
+
+resource "azurerm_api_management" "test" {
+  name                = "acctestAM-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  publisher_name      = "pub1"
+  publisher_email     = "pub1@email.com"
+  sku_name            = "Developer_1" # Consumption_0 does not support Azure Monitor
+}
+
+resource "azurerm_monitor_diagnostic_setting" "test" {
+  name                       = "acctest-DS-%[1]d"
+  target_resource_id         = azurerm_api_management.test.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.test.id
+
+  enabled_log {
+    category_group = "allLogs"
+  }
+
+  metric {
+    enabled  = false
+    category = "AllMetrics"
+  }
+}
+
+resource "azurerm_api_management_api" "test" {
+  name                = "acctestAMA-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  api_management_name = azurerm_api_management.test.name
+  revision            = "1"
+  display_name        = "Test API"
+  path                = "test"
+  protocols           = ["https"]
+
+  import {
+    content_format = "swagger-link-json"
+    content_value  = "https://raw.githubusercontent.com/hashicorp/terraform-provider-azurerm/refs/heads/main/internal/services/apimanagement/testdata/api_management_api_swagger.json"
+  }
+}
+
+resource "terraform_data" "logger_waiter" {
+  triggers_replace = [
+    # The azuremonitor logger is automatically created after the diagnostic setting is created
+    azurerm_monitor_diagnostic_setting.test.id,
+  ]
+
+  provisioner "local-exec" {
+    command = <<-EOF
+    while [ $(az rest --method get --url 'https://management.azure.com${azurerm_api_management.test.id}/loggers?api-version=2024-05-01' --query count) -eq 0 ]; do
+      echo Waiting for the azuremonitor logger to be created...
+      sleep 15
+    done
+    EOF
+  }
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (r ApiManagementApiDiagnosticResource) azuremonitor(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_api_management_api_diagnostic" "test" {
+  identifier               = "azuremonitor"
+  resource_group_name      = azurerm_resource_group.test.name
+  api_management_name      = azurerm_api_management.test.name
+  api_name                 = azurerm_api_management_api.test.name
+  api_management_logger_id = "${azurerm_api_management.test.id}/loggers/azuremonitor"
+
+  # Wait until the azuremonitor logger is created
+  depends_on = [terraform_data.logger_waiter]
+}
+`, r.templateAzureMonitor(data))
+}
+
+func (r ApiManagementApiDiagnosticResource) azuremonitorWithOperationNameFormat(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_api_management_api_diagnostic" "test" {
+  identifier               = "azuremonitor"
+  resource_group_name      = azurerm_resource_group.test.name
+  api_management_name      = azurerm_api_management.test.name
+  api_name                 = azurerm_api_management_api.test.name
+  api_management_logger_id = "${azurerm_api_management.test.id}/loggers/azuremonitor"
+  operation_name_format    = "Name"
+}
+`, r.templateAzureMonitor(data))
 }