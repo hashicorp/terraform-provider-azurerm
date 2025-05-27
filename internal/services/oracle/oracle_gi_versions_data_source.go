a9a7fa2d31680d1342c54f4999bf7315ee735895
mihret <mihret.kidane@oracle.com>
2025-05-27 12:11:57 -0500

    nit

--- internal/services/oracle/oracle_gi_versions_data_source.go
+++ internal/services/oracle/oracle_gi_versions_data_source.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/oracledatabase/2025-03-01/giversions"
