45a7c7011ff0e979a63ecf1e1486a564afa6c8f6
vasyl-sw14 <77231350+vasyl-sw14@users.noreply.github.com>
2025-06-04 21:50:27 +0200

    upgrade api version and generate vendor

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2025-02-01/managedclusters/model_kubernetespatchversion.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2025-02-01/managedclusters/model_kubernetespatchversion.go
@@ -1,10 +1,8 @@
-package agentpools
+package managedclusters
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-const defaultApiVersion = "2024-09-01"
-
-func userAgent() string {
-	return "hashicorp/go-azure-sdk/agentpools/2024-09-01"
+type KubernetesPatchVersion struct {
+	Upgrades *[]string `json:"upgrades,omitempty"`
 }
