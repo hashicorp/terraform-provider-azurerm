617ab664f9ce727770154adb4d15afdbba6e93e1
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2025-06-09 22:58:50 -0600

    Adress PR comments...

--- internal/services/compute/orchestrated_virtual_machine_scale_set_resource_other_test.go
+++ internal/services/compute/orchestrated_virtual_machine_scale_set_resource_other_test.go
@@ -310,15 +310,15 @@ func TestAccOrchestratedVirtualMachineScaleSet_otherUpgradePolicyErrorValidation
 	data.ResourceTest(t, r, []acceptance.TestStep{
 		{
 			Config:      r.otherRollingWithoutHealthExtension(data),
-			ExpectError: regexp.MustCompile("a health extension must be specified when `upgrade_mode` is set to `Rolling`"),
+			ExpectError: regexp.MustCompile("health extension is required when `upgrade_mode` is set"),
 		},
 		{
 			Config:      r.otherManualWithUpgradePolicy(data),
-			ExpectError: regexp.MustCompile("a `rolling_upgrade_policy` block cannot be specified"),
+			ExpectError: regexp.MustCompile("`rolling_upgrade_policy` cannot be specified when `upgrade_mode` is set"),
 		},
 		{
 			Config:      r.otherRollingWithoutUpgradePolicy(data),
-			ExpectError: regexp.MustCompile("a `rolling_upgrade_policy` block must be specified when `upgrade_mode` is set"),
+			ExpectError: regexp.MustCompile("`rolling_upgrade_policy` is required when `upgrade_mode` is set"),
 		},
 	})
 }
