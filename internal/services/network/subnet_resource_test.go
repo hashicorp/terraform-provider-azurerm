471d1934f74c078a9091e1eb8e008bcd5e5ededd
teowa <104055472+teowa@users.noreply.github.com>
2025-06-11 11:39:55 +0000

    format test config

--- internal/services/network/subnet_resource_test.go
+++ internal/services/network/subnet_resource_test.go
@@ -971,7 +971,7 @@ resource "azurerm_subnet" "test" {
   name                 = "acctestsubnet%[1]d"
   resource_group_name  = azurerm_resource_group.test.name
   virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["10.0.0.0/24"] 
+  address_prefixes     = ["10.0.0.0/24"]
 }
 `, data.RandomInteger, data.Locations.Primary)
 }
