6d5a154ce9c5c95461ccd1ef710db6790b2eccf6
teowa <104055472+teowa@users.noreply.github.com>
2025-06-12 05:18:03 +0000

    fix import test

--- internal/services/network/network_manager_ipam_pool_resource_test.go
+++ internal/services/network/network_manager_ipam_pool_resource_test.go
@@ -163,7 +163,6 @@ resource "azurerm_network_manager_ipam_pool" "import" {
   name               = azurerm_network_manager_ipam_pool.test.name
   network_manager_id = azurerm_network_manager.test.id
   location           = azurerm_resource_group.test.location
-  display_name       = azurerm_network_manager_ipam_pool.test.display_name
   address_prefixes   = azurerm_network_manager_ipam_pool.test.address_prefixes
 }
 `, r.basic(data))
