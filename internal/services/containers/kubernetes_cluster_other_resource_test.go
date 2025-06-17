9cefc5d3f8b072ed5103d2da24418e8c30fbd2fa
Heng Lu <henglu@microsoft.com>
2025-06-17 17:44:31 +0800

    make terrafmt

--- internal/services/containers/kubernetes_cluster_other_resource_test.go
+++ internal/services/containers/kubernetes_cluster_other_resource_test.go
@@ -3051,7 +3051,7 @@ resource "azurerm_kubernetes_cluster" "test" {
   }
 
   web_app_routing {
-    dns_zone_ids = []
+    dns_zone_ids             = []
     default_nginx_controller = "%s"
   }
 }
