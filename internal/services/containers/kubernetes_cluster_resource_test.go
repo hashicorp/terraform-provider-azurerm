7470635d106893a30755a828ab5e2ee4dee8db7a
SANTHOSH PAKALA <santhoshpakala@SANTHOSHs-MacBook-Pro.local>
2025-06-24 07:59:23 -0500

    removed the comments

--- internal/services/containers/kubernetes_cluster_resource_test.go
+++ internal/services/containers/kubernetes_cluster_resource_test.go
@@ -507,7 +507,7 @@ resource "azurerm_kubernetes_cluster" "test" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "standard_a2_v2"
+    vm_size    = "Standard_DS2_v2"
     upgrade_settings {
       max_surge = %q
     }
