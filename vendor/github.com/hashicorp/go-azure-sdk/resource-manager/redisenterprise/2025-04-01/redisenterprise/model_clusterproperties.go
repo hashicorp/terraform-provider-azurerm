8dfce3a6e0895ee34200ba428bd130fc46d9fbaa
Gerry Tan <gerry.tan@microsoft.com>
2025-06-30 11:23:31 +1000

    Vendor redisenterprise/2025-04-10

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2025-04-01/redisenterprise/model_clusterproperties.go
@@ -0,0 +1,16 @@
+package redisenterprise
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type ClusterProperties struct {
+	Encryption                 *ClusterPropertiesEncryption `json:"encryption,omitempty"`
+	HighAvailability           *HighAvailability            `json:"highAvailability,omitempty"`
+	HostName                   *string                      `json:"hostName,omitempty"`
+	MinimumTlsVersion          *TlsVersion                  `json:"minimumTlsVersion,omitempty"`
+	PrivateEndpointConnections *[]PrivateEndpointConnection `json:"privateEndpointConnections,omitempty"`
+	ProvisioningState          *ProvisioningState           `json:"provisioningState,omitempty"`
+	RedisVersion               *string                      `json:"redisVersion,omitempty"`
+	RedundancyMode             *RedundancyMode              `json:"redundancyMode,omitempty"`
+	ResourceState              *ResourceState               `json:"resourceState,omitempty"`
+}
