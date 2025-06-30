5043352c3df9bf5dbcd54e53cebefbd971f30634
Gerry Tan <gerry.tan@microsoft.com>
2025-06-30 14:13:02 +1000

    validate package, no unit test migrated yet

---
+++ internal/services/redismanaged/validate/managed_redis_cluster_sku_name.go
@@ -0,0 +1,70 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package validate
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/go-azure-sdk/resource-manager/redisenterprise/2025-04-01/redisenterprise"
+	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
+)
+
+// ManagedRedisClusterSkuName - validates if passed input string contains a valid Managed Redis Cluster Sku
+func ManagedRedisClusterSkuName(v interface{}, k string) (warnings []string, errors []error) {
+	validSku := false
+	validCapacity := false
+
+	value, ok := v.(string)
+	if !ok {
+		errors = append(errors, fmt.Errorf("expected type of %q to be string", k))
+		return warnings, errors
+	}
+
+	skuParts := strings.Split(value, "-")
+	validSkus := redisenterprise.PossibleValuesForSkuName()
+	validValues := "2, 4, 6, ..."
+	// Validate the SKU Name section
+	for _, str := range validSkus {
+		if skuParts[0] == str {
+			validSku = true
+			break
+		}
+	}
+
+	isFlash := strings.Contains(skuParts[0], "Flash")
+
+	if isFlash {
+		validValues = "3, 9, 15, ..."
+	}
+
+	if len(skuParts) > 1 {
+		i, err := strconv.ParseInt(skuParts[1], 10, 32)
+		if err != nil {
+			errors = append(errors, fmt.Errorf("expected %q %q segment to fit into an int32 value, got %q", k, "capacity", skuParts[1]))
+			return warnings, errors
+		}
+
+		skuCapacity := int32(i)
+
+		if isFlash {
+			if skuCapacity%6 == 3 {
+				validCapacity = true
+			}
+		} else if skuCapacity%2 == 0 {
+			validCapacity = true
+		}
+	}
+
+	if !validSku {
+		errors = append(errors, fmt.Errorf("expected %q %q segment to be one of [%s], got %q", k, "name", azure.QuotedStringSlice(validSkus), value))
+	}
+
+	if !validCapacity {
+		errors = append(errors, fmt.Errorf("expected %q %q segment to be one of [%s], got %q", k, "capacity", validValues, value))
+	}
+
+	return warnings, errors
+}
