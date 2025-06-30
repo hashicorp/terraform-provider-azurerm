a3a79d04048b7ddd02890f69f771abe143eda15c
Gerry Tan <gerry.tan@microsoft.com>
2025-06-30 15:19:01 +1000

    registration.go: register ManagedRedisClusterResource

--- internal/services/redismanaged/registration.go
+++ internal/services/redismanaged/registration.go
@@ -24,5 +24,7 @@ func (r Registration) DataSources() []sdk.DataSource {
 }
 
 func (r Registration) Resources() []sdk.Resource {
-	return []sdk.Resource{}
+	return []sdk.Resource{
+		ManagedRedisClusterResource{},
+	}
 }
