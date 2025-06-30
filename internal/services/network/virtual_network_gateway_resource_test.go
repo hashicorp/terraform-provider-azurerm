9a60dc63089ad614f74f523b314532a2749c6955
neil-yechenwei <yechenwei2007@hotmail.com>
2025-06-30 14:57:45 +0800

    format tc file

--- internal/services/network/virtual_network_gateway_resource_test.go
+++ internal/services/network/virtual_network_gateway_resource_test.go
@@ -2044,8 +2044,8 @@ resource "azurerm_virtual_network_gateway" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
 
-  type     = "ExpressRoute"
-  sku      = "ErGw1AZ"
+  type = "ExpressRoute"
+  sku  = "ErGw1AZ"
 
   ip_configuration {
     name                          = "default"
