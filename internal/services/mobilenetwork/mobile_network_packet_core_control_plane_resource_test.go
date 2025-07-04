a0067ce411f237a0c5f5c318869aae0a364c8db6
teowa <104055472+teowa@users.noreply.github.com>
2025-07-04 06:55:30 +0000

    add update test for site id

--- internal/services/mobilenetwork/mobile_network_packet_core_control_plane_resource_test.go
+++ internal/services/mobilenetwork/mobile_network_packet_core_control_plane_resource_test.go
@@ -121,6 +121,13 @@ func TestAccMobileNetworkPacketCoreControlPlane_update(t *testing.T) {
 			),
 		},
 		data.ImportStep(),
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
 	})
 }
 
@@ -401,14 +408,20 @@ resource "azurerm_mobile_network_packet_core_control_plane" "import" {
 
 func (r MobileNetworkPacketCoreControlPlaneResource) update(data acceptance.TestData) string {
 	return fmt.Sprintf(`
-			%s
+			%[1]s
+
+resource "azurerm_mobile_network_site" "test2" {
+  name              = "acctest-mns2-%[2]d"
+  mobile_network_id = azurerm_mobile_network.test.id
+  location          = azurerm_mobile_network.test.location
+}
 
 resource "azurerm_mobile_network_packet_core_control_plane" "test" {
-  name                              = "acctest-mnpccp-%d"
+  name                              = "acctest-mnpccp-%[2]d"
   resource_group_name               = azurerm_resource_group.test.name
-  location                          = "%s"
+  location                          = "%[3]s"
   sku                               = "G0"
-  site_ids                          = [azurerm_mobile_network_site.test.id]
+  site_ids                          = [azurerm_mobile_network_site.test.id, azurerm_mobile_network_site.test2.id]
   control_plane_access_name         = "default-interface"
   control_plane_access_ipv4_address = "192.168.1.199"
   control_plane_access_ipv4_gateway = "192.168.1.1"
