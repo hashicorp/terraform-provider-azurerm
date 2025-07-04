29ef09e6e98c12983e5a734b7366d1f75aee8889
teowa <104055472+teowa@users.noreply.github.com>
2025-07-04 11:10:38 +0000

    revert change; mark as forcenew

--- internal/services/mobilenetwork/mobile_network_packet_core_control_plane_resource_test.go
+++ internal/services/mobilenetwork/mobile_network_packet_core_control_plane_resource_test.go
@@ -410,18 +410,12 @@ func (r MobileNetworkPacketCoreControlPlaneResource) update(data acceptance.Test
 	return fmt.Sprintf(`
 			%[1]s
 
-resource "azurerm_mobile_network_site" "test2" {
-  name              = "acctest-mns2-%[2]d"
-  mobile_network_id = azurerm_mobile_network.test.id
-  location          = azurerm_mobile_network.test.location
-}
-
 resource "azurerm_mobile_network_packet_core_control_plane" "test" {
   name                              = "acctest-mnpccp-%[2]d"
   resource_group_name               = azurerm_resource_group.test.name
   location                          = "%[3]s"
   sku                               = "G0"
-  site_ids                          = [azurerm_mobile_network_site.test2.id]
+  site_ids                          = [azurerm_mobile_network_site.test.id]
   control_plane_access_name         = "default-interface"
   control_plane_access_ipv4_address = "192.168.1.199"
   control_plane_access_ipv4_gateway = "192.168.1.1"
