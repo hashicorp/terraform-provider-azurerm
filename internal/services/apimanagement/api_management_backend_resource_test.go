d6a0fd3d348da06915eae89c978a7b95f161a63c
domroutley <dominic.routley@gmail.com>
2025-07-04 14:16:38 +0100

    update tests sdk version

--- internal/services/apimanagement/api_management_backend_resource_test.go
+++ internal/services/apimanagement/api_management_backend_resource_test.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/apimanagement/2022-08-01/backend"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/apimanagement/2024-05-01/backend"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
@@ -329,7 +329,7 @@ resource "azurerm_api_management_backend" "test" {
 
 func (r ApiManagementAuthorizationBackendResource) serviceFabric(data acceptance.TestData) string {
 	// nolint: dupword
-	return fmt.Sprintf(` 
+	return fmt.Sprintf(`
 %s
 
 resource "azurerm_api_management_certificate" "test" {
