77467813707ac84f3d69c5923d3b192f46671ba1
elena <sinbai@hotmail.com>
2025-07-07 14:05:10 +0800

    fix comments

--- internal/services/appservice/service_plan_resource_test.go
+++ internal/services/appservice/service_plan_resource_test.go
@@ -294,11 +294,11 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_service_plan" "test" {
-  name                   = "acctest-SP-%[1]d"
-  resource_group_name    = azurerm_resource_group.test.name
-  location               = azurerm_resource_group.test.location
-  sku_name               = "B1"
-  os_type                = "Windows"
+  name                = "acctest-SP-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  sku_name            = "B1"
+  os_type             = "Windows"
 
   tags = {
     environment = "AccTest"
