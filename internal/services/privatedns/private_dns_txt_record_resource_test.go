7f2cffeef45b6d45eb43517e1ed279a6727fbab9
Wyatt Fry <wyattfry@gmail.com>
2025-07-07 14:54:35 -0400

    fix test config

--- internal/services/privatedns/private_dns_txt_record_resource_test.go
+++ internal/services/privatedns/private_dns_txt_record_resource_test.go
@@ -186,7 +186,7 @@ func (r PrivateDnsTxtRecordResource) requiresImport(data acceptance.TestData) st
 
 resource "azurerm_private_dns_txt_record" "import" {
   name            = azurerm_private_dns_txt_record.test.name
-  private_zone_id = azurerm_private_dns_txt_record.test.id
+  private_zone_id = azurerm_private_dns_zone.test.id
   ttl             = 300
 
   record {
