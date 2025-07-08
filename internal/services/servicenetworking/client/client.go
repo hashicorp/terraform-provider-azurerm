2c995c13f3524d031fa8f232a843dbe208ed1333
teowa <104055472+teowa@users.noreply.github.com>
2025-07-08 07:12:48 +0000

    azurerm_application_load_balancer: upgrade API version from 2023-11-01 to 2025-01-01

--- internal/services/servicenetworking/client/client.go
+++ internal/services/servicenetworking/client/client.go
@@ -6,7 +6,7 @@ package client
 import (
 	"fmt"
 
-	servicenetworking_2023_11_01 "github.com/hashicorp/go-azure-sdk/resource-manager/servicenetworking/2023-11-01"
+	servicenetworking_2023_11_01 "github.com/hashicorp/go-azure-sdk/resource-manager/servicenetworking/2025-01-01"
 	"github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
