c3c58cc69522929243d8b234d2aa124655685f5b
teowa <104055472+teowa@users.noreply.github.com>
2025-07-09 00:12:43 +0000

    update import alias

--- internal/services/servicenetworking/client/client.go
+++ internal/services/servicenetworking/client/client.go
@@ -6,17 +6,17 @@ package client
 import (
 	"fmt"
 
-	servicenetworking_2023_11_01 "github.com/hashicorp/go-azure-sdk/resource-manager/servicenetworking/2025-01-01"
+	servicenetworking_2025_01_01 "github.com/hashicorp/go-azure-sdk/resource-manager/servicenetworking/2025-01-01"
 	"github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
 
 type Client struct {
-	ServiceNetworkingClient *servicenetworking_2023_11_01.Client
+	ServiceNetworkingClient *servicenetworking_2025_01_01.Client
 }
 
-func NewClient(o *common.ClientOptions) (*servicenetworking_2023_11_01.Client, error) {
-	client, err := servicenetworking_2023_11_01.NewClientWithBaseURI(o.Environment.ResourceManager, func(c *resourcemanager.Client) {
+func NewClient(o *common.ClientOptions) (*servicenetworking_2025_01_01.Client, error) {
+	client, err := servicenetworking_2025_01_01.NewClientWithBaseURI(o.Environment.ResourceManager, func(c *resourcemanager.Client) {
 		o.Configure(c, o.Authorizers.ResourceManager)
 	})
 	if err != nil {
