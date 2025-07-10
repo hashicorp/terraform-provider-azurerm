57d87b2d05dfdb0516f22d88c88c3020107a4f8e
ilyapelyovin <46128782+ilyapelyovin@users.noreply.github.com>
2025-07-10 21:51:09 +0200

    Update internal/services/compute/marketplace_agreement_data_source.go
    Co-authored-by: sreallymatt <106555974+sreallymatt@users.noreply.github.com>

--- internal/services/compute/marketplace_agreement_data_source.go
+++ internal/services/compute/marketplace_agreement_data_source.go
@@ -87,11 +87,7 @@ func dataSourceMarketplaceAgreementRead(d *pluginsdk.ResourceData, meta interfac
 		if props := model.Properties; props != nil {
 			d.Set("license_text_link", props.LicenseTextLink)
 			d.Set("privacy_policy_link", props.PrivacyPolicyLink)
-			accepted := false
-			if acc := props.Accepted; acc != nil {
-				accepted = *acc
-			}
-			d.Set("accepted", accepted)
+			d.Set("accepted", pointer.From(props.Accepted))
 		}
 	}
 	return nil
