cdd11af6bcffa229e58814e7e46a88698333b455
Heng Lu <henglu@microsoft.com>
2025-07-15 14:06:41 +0800

    fix a test

--- internal/services/containers/kubernetes_flux_configuration_resource_test.go
+++ internal/services/containers/kubernetes_flux_configuration_resource_test.go
@@ -671,7 +671,7 @@ resource "azuread_application" "test" {
 }
 
 resource "azuread_service_principal" "test" {
-  application_id = azuread_application.test.application_id
+  client_id = azuread_application.test.client_id
 }
 
 resource "azuread_service_principal_password" "test" {
