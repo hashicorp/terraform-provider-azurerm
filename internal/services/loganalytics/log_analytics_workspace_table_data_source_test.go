b5a7c1d5a8120d916befb092b940385e4b70c9f7
aiell0 <lawrence@rogo.ai>
2025-07-19 13:55:40 -0400

    feat: add log analytics workspace table data source

---
+++ internal/services/loganalytics/log_analytics_workspace_table_data_source_test.go
@@ -0,0 +1,58 @@
+package loganalytics_test
+
+import (
+	"fmt"
+	"testing"
+
+	// "github.com/hashicorp/go-azure-helpers/resourcemanager/location"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
+)
+
+type LogAnalyticsWorkspaceTableDataSource struct{}
+
+func TestLogAnalyticsWorkspaceTableDataSource_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "data.azurerm_log_analytics_workspace_table", "this")
+	r := LogAnalyticsWorkspaceTableDataSource{}
+
+	data.DataSourceTest(t, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).Key("retention_in_days").HasValue("30"),
+				check.That(data.ResourceName).Key("total_retention_in_days").HasValue("30"),
+			),
+		},
+	})
+}
+
+func (LogAnalyticsWorkspaceTableDataSource) basic(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "test-resource-group"
+  location = "%s"
+}
+
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = "test"
+  resource_group_name = "test-resource-group"
+  location            = "%s"
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  tags = {
+  	env = "test"
+  }
+}
+
+data "azurerm_log_analytics_workspace_table" "this" {
+	name = "InsightsMetrics"
+  workspace_id = azurerm_log_analytics_workspace.this.id
+  resource_group_name = "test-resource-group"
+}
+`, data.Locations.Primary, data.Locations.Primary)
+}
---
+++ website/docs/d/log_analytics_workspace_table.html.markdown
@@ -0,0 +1,50 @@
+---
+subcategory: "Log Analytics"
+layout: "azurerm"
+page_title: "Azure Resource Manager: Data Source: azurerm_log_analytics_workspace_table"
+description: |-
+  Gets information about an existing log_analytics_workspace.
+---
+
+# Data Source: azurerm_log_analytics_workspace_table
+
+Use this data source to access information about an existing log_analytics_workspace table.
+
+## Example Usage
+
+```hcl
+
+data "azurerm_log_analytics_workspace_table" "this" {
+	name = "InsightsMetrics"
+  workspace_id = azurerm_log_analytics_workspace.this.id
+  resource_group_name = "test-resource-group"
+}
+
+output "retention_in_days" {
+  value = data.azurerm_log_analytics_workspace_table.this.retention_in_days
+}
+```
+
+## Arguments Reference
+
+The following arguments are supported:
+
+* `name` - (Required) The name of this log analytics workspace table.
+
+* `resource_group_name` - (Required) The name of the Resource Group where the log analytics workspace exists.
+
+* `workspace_id` - (Required) The ID of the log analytics workspace the table belongs to.
+
+## Attributes Reference
+
+In addition to the Arguments listed above - the following Attributes are exported: 
+
+* `retention_in_days` - table retention.
+
+* `total_retention_in_days` - total table retention.
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/language/resources/syntax#operation-timeouts) for certain actions:
+
+* `read` - (Defaults to 5 minutes) Used when retrieving the log_analytics_workspace.
