58ee4f1dc32fbd70da95fdb66404a81960e571fc
Wyatt Fry <wyattfry@gmail.com>
2025-08-01 17:57:59 -0400

    restore versions from main

--- internal/services/appservice/windows_web_app_resource_test.go
+++ internal/services/appservice/windows_web_app_resource_test.go
@@ -10,13 +10,13 @@ import (
 	"testing"
 	"time"
 
-	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
 type WindowsWebAppResource struct{}
@@ -1781,14 +1781,14 @@ func (r WindowsWebAppResource) Exists(ctx context.Context, client *clients.Clien
 	resp, err := client.AppService.WebAppsClient.Get(ctx, *id)
 	if err != nil {
 		if response.WasNotFound(resp.HttpResponse) {
-			return pointer.To(false), nil
+			return utils.Bool(false), nil
 		}
 		return nil, fmt.Errorf("retrieving Windows %s: %+v", id, err)
 	}
 	if response.WasNotFound(resp.HttpResponse) {
-		return pointer.To(false), nil
+		return utils.Bool(false), nil
 	}
-	return pointer.To(true), nil
+	return utils.Bool(true), nil
 }
 
 func (r WindowsWebAppResource) basic(data acceptance.TestData) string {
--- internal/services/devtestlabs/dev_test_lab_resource.go
+++ internal/services/devtestlabs/dev_test_lab_resource.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"time"
 
-	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
@@ -21,6 +20,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tags"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
 func resourceDevTestLab() *pluginsdk.Resource {
@@ -123,7 +123,7 @@ func resourceDevTestLabCreateUpdate(d *pluginsdk.ResourceData, meta interface{})
 	location := azure.NormalizeLocation(d.Get("location").(string))
 
 	parameters := labs.Lab{
-		Location: pointer.To(location),
+		Location: utils.String(location),
 		Tags:     expandTags(d.Get("tags").(map[string]interface{})),
 	}
 
