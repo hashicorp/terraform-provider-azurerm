ae9f8c0eab1e87624727be1520c09f8af92e327a
Evgen <evgen@users.noreply.github.com>
2025-08-04 19:47:51 -0400

    Fix code style for oracle_autonomous_database_cross_region_disaster_recovery

--- internal/services/oracle/oracle_autonomous_database_cross_region_disaster_recovery_resource_test.go
+++ internal/services/oracle/oracle_autonomous_database_cross_region_disaster_recovery_resource_test.go
@@ -5,6 +5,8 @@ package oracle_test
 import (
 	"context"
 	"fmt"
+	"testing"
+
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/oracledatabase/2025-03-01/autonomousdatabases"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
@@ -12,7 +14,6 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/oracle"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
-	"testing"
 )
 
 type AdbsCrossRegionDisasterRecoveryResource struct{}
@@ -74,6 +75,9 @@ func TestAdbsCrossRegionDisasterRecoveryResource_requiresImport(t *testing.T) {
 func (a AdbsCrossRegionDisasterRecoveryResource) complete(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 
+
+
+
 %s
 
 provider "azurerm" {
@@ -81,7 +85,7 @@ provider "azurerm" {
 }
 
 locals {
-crdr_dbname = "OFakeC%[2]d"
+  crdr_dbname = "OFakeC%[2]d"
 }
 
 resource "azurerm_oracle_autonomous_database" "adbs_primary_for_crdr" {
@@ -105,25 +109,25 @@ resource "azurerm_oracle_autonomous_database" "adbs_primary_for_crdr" {
   national_character_set           = "AL16UTF16"
   subnet_id                        = azurerm_subnet.iad_vnet_subnet_test.id
   virtual_network_id               = azurerm_virtual_network.iad_vnet_test.id
-  customer_contacts = ["test@test.com"]
+  customer_contacts                = ["test@test.com"]
 }
 
 resource "azurerm_oracle_autonomous_database_cross_region_disaster_recovery" "adbs_secondary_crdr" {
   name = local.crdr_dbname
 
-  display_name                     = local.crdr_dbname
-  location                         = "%[4]s"
-  database_type                    = "CrossRegionDisasterRecovery"
-  source                           = "CrossRegionDisasterRecovery"
-  source_id      				   = azurerm_oracle_autonomous_database.adbs_primary_for_crdr.id
-  source_ocid                      = azurerm_oracle_autonomous_database.adbs_primary_for_crdr.ocid
-  remote_disaster_recovery_type    = "Adg"
-  replicate_automatic_backups	   = true
-  subnet_id                        = azurerm_subnet.fra_vnet_subnet_test.id
-  virtual_network_id               = azurerm_virtual_network.fra_vnet_test.id
+  display_name                        = local.crdr_dbname
+  location                            = "%[4]s"
+  database_type                       = "CrossRegionDisasterRecovery"
+  source                              = "CrossRegionDisasterRecovery"
+  source_id                           = azurerm_oracle_autonomous_database.adbs_primary_for_crdr.id
+  source_ocid                         = azurerm_oracle_autonomous_database.adbs_primary_for_crdr.ocid
+  remote_disaster_recovery_type       = "Adg"
+  replicate_automatic_backups_enabled = true
+  subnet_id                           = azurerm_subnet.fra_vnet_subnet_test.id
+  virtual_network_id                  = azurerm_virtual_network.fra_vnet_test.id
 
   resource_group_name              = azurerm_resource_group.crdr_rg.name
-  source_location				   = azurerm_oracle_autonomous_database.adbs_primary_for_crdr.location
+  source_location                  = azurerm_oracle_autonomous_database.adbs_primary_for_crdr.location
   license_model                    = azurerm_oracle_autonomous_database.adbs_primary_for_crdr.license_model
   backup_retention_period_in_days  = azurerm_oracle_autonomous_database.adbs_primary_for_crdr.backup_retention_period_in_days
   auto_scaling_enabled             = azurerm_oracle_autonomous_database.adbs_primary_for_crdr.auto_scaling_enabled
@@ -148,31 +152,31 @@ func (a AdbsCrossRegionDisasterRecoveryResource) requiresImport(data acceptance.
 resource "azurerm_oracle_autonomous_database_cross_region_disaster_recovery" "import" {
   name = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.name
 
-  display_name                     = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.name
-  location                         = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.location
-  database_type                    = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.database_type
-  source                           = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.source
-  source_id      				   = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.source_id
-  source_ocid                      = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.source_ocid
-  remote_disaster_recovery_type    = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.remote_disaster_recovery_type
-  replicate_automatic_backups	   = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.replicate_automatic_backups
-  subnet_id                        = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.subnet_id
-  virtual_network_id               = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.virtual_network_id
-  resource_group_name              = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.resource_group_name
-  source_location				   = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.source_location
-  license_model                    = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.license_model
-  backup_retention_period_in_days  = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.backup_retention_period_in_days
-  auto_scaling_enabled             = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.auto_scaling_enabled
-  auto_scaling_for_storage_enabled = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.auto_scaling_for_storage_enabled
-  mtls_connection_required         = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.mtls_connection_required
-  data_storage_size_in_tbs         = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.data_storage_size_in_tbs
-  compute_model                    = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.compute_model
-  compute_count                    = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.compute_count
-  db_workload                      = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.db_workload
-  db_version                       = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.db_version
-  admin_password                   = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.admin_password
-  character_set                    = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.character_set
-  national_character_set           = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.national_character_set
+  display_name                        = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.name
+  location                            = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.location
+  database_type                       = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.database_type
+  source                              = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.source
+  source_id                           = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.source_id
+  source_ocid                         = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.source_ocid
+  remote_disaster_recovery_type       = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.remote_disaster_recovery_type
+  replicate_automatic_backups_enabled = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.replicate_automatic_backups
+  subnet_id                           = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.subnet_id
+  virtual_network_id                  = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.virtual_network_id
+  resource_group_name                 = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.resource_group_name
+  source_location                     = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.source_location
+  license_model                       = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.license_model
+  backup_retention_period_in_days     = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.backup_retention_period_in_days
+  auto_scaling_enabled                = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.auto_scaling_enabled
+  auto_scaling_for_storage_enabled    = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.auto_scaling_for_storage_enabled
+  mtls_connection_required            = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.mtls_connection_required
+  data_storage_size_in_tbs            = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.data_storage_size_in_tbs
+  compute_model                       = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.compute_model
+  compute_count                       = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.compute_count
+  db_workload                         = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.db_workload
+  db_version                          = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.db_version
+  admin_password                      = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.admin_password
+  character_set                       = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.character_set
+  national_character_set              = azurerm_oracle_autonomous_database_cross_region_disaster_recovery.adbs_secondary_crdr.national_character_set
 }
 `, a.complete(data))
 }
@@ -180,36 +184,39 @@ resource "azurerm_oracle_autonomous_database_cross_region_disaster_recovery" "im
 func (a AdbsCrossRegionDisasterRecoveryResource) update(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 
+
+
+
 %s
 
 resource "azurerm_oracle_autonomous_database_cross_region_disaster_recovery" "adbs_secondary_crdr" {
-    name =  "OFakeC%[2]d"
+  name = "OFakeC%[2]d"
 
-  display_name                     = "OFakeC%[2]d"
-  remote_disaster_recovery_type    = "Adg"
-  replicate_automatic_backups	   = false
-  subnet_id                        = azurerm_subnet.fra_vnet_subnet_test.id
-  virtual_network_id               = azurerm_virtual_network.fra_vnet_test.id
-  location                         = "%[4]s"
-  database_type                    = "CrossRegionDisasterRecovery"
-  source                           = "CrossRegionDisasterRecovery"
-  source_id      				   = azurerm_oracle_autonomous_database.adbs_primary_for_crdr.id
-  source_ocid                      = azurerm_oracle_autonomous_database.adbs_primary_for_crdr.ocid
-  resource_group_name              = azurerm_resource_group.crdr_rg.name
-  source_location				   = "%[3]s"
-  license_model                    = "LicenseIncluded"
-  backup_retention_period_in_days  = 12
-  auto_scaling_enabled             = false
-  auto_scaling_for_storage_enabled = false
-  mtls_connection_required         = false
-  data_storage_size_in_tbs         = 1
-  compute_model                    = "ECPU"
-  compute_count                    = 2
-  db_workload                      = "DW"
-  admin_password                   = "TestPass#2024#"
-  db_version                       = "19c"
-  character_set                    = "AL32UTF8"
-  national_character_set           = "AL16UTF16"
+  display_name                        = "OFakeC%[2]d"
+  remote_disaster_recovery_type       = "Adg"
+  replicate_automatic_backups_enabled = false
+  subnet_id                           = azurerm_subnet.fra_vnet_subnet_test.id
+  virtual_network_id                  = azurerm_virtual_network.fra_vnet_test.id
+  location                            = "%[4]s"
+  database_type                       = "CrossRegionDisasterRecovery"
+  source                              = "CrossRegionDisasterRecovery"
+  source_id                           = azurerm_oracle_autonomous_database.adbs_primary_for_crdr.id
+  source_ocid                         = azurerm_oracle_autonomous_database.adbs_primary_for_crdr.ocid
+  resource_group_name                 = azurerm_resource_group.crdr_rg.name
+  source_location                     = "%[3]s"
+  license_model                       = "LicenseIncluded"
+  backup_retention_period_in_days     = 12
+  auto_scaling_enabled                = false
+  auto_scaling_for_storage_enabled    = false
+  mtls_connection_required            = false
+  data_storage_size_in_tbs            = 1
+  compute_model                       = "ECPU"
+  compute_count                       = 2
+  db_workload                         = "DW"
+  admin_password                      = "TestPass#2024#"
+  db_version                          = "19c"
+  character_set                       = "AL32UTF8"
+  national_character_set              = "AL16UTF16"
 }
 `, a.complete(data), data.RandomInteger, "eastus", "westus")
 }
--- internal/services/oracle/validate/autonomous_database_cross_region_disaster_recovery.go
+++ internal/services/oracle/validate/autonomous_database_cross_region_disaster_recovery.go
@@ -5,6 +5,7 @@ package validate
 
 import (
 	"fmt"
+
 	"github.com/hashicorp/go-azure-sdk/resource-manager/oracledatabase/2025-03-01/autonomousdatabases"
 )
 
@@ -41,7 +42,7 @@ func DatabaseType(i interface{}, k string) (warnings []string, errors []error) {
 	}
 
 	if v != string(autonomousdatabases.DataBaseTypeClone) && v != string(autonomousdatabases.DataBaseTypeCloneFromBackupTimestamp) && v != string(autonomousdatabases.DataBaseTypeCrossRegionDisasterRecovery) && v != string(autonomousdatabases.DataBaseTypeRegular) {
-		return []string{}, append(errors, fmt.Errorf("%v must be %v or %v", k, string(autonomousdatabases.DataBaseTypeClone), string(autonomousdatabases.DataBaseTypeCloneFromBackupTimestamp), string(autonomousdatabases.DataBaseTypeCrossRegionDisasterRecovery), string(autonomousdatabases.DataBaseTypeRegular)))
+		return []string{}, append(errors, fmt.Errorf("%v must be %v, %v, %v, or %v", k, string(autonomousdatabases.DataBaseTypeClone), string(autonomousdatabases.DataBaseTypeCloneFromBackupTimestamp), string(autonomousdatabases.DataBaseTypeCrossRegionDisasterRecovery), string(autonomousdatabases.DataBaseTypeRegular)))
 	}
 
 	return []string{}, []error{}
