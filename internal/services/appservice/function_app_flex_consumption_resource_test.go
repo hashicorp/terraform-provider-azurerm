bdc047f2188594f4a5284ed3a93eb6d02b491a30
Xiaxin Yi (from Dev Box) <xiaxyi@microsoft.com>
2025-08-05 14:54:26 +1000

    update name of test case

--- internal/services/appservice/function_app_flex_consumption_resource_test.go
+++ internal/services/appservice/function_app_flex_consumption_resource_test.go
@@ -548,7 +548,7 @@ provider "azurerm" {
 %s
 
 resource "azurerm_function_app_flex_consumption" "test" {
-  name                = "acctest-LFA-tf%d"
+  name                = "acctest-LFA-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   service_plan_id     = azurerm_service_plan.test.id
