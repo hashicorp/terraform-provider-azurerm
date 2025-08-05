1ebb9363be2cdf621b9947ae47b918ddaa219fa6
Lucia Lu <qixialu@hotmail.com>
2025-08-05 15:18:39 +1000

    Improve from PR review

--- internal/services/network/application_gateway_resource.go
+++ internal/services/network/application_gateway_resource.go
@@ -4948,7 +4948,8 @@ func applicationGatewayProbeHash(v interface{}) int {
 				statusCodes := attr["status_code"].([]interface{})
 
 				// Only include in hash if it's not the default
-				if body != "" || !(len(statusCodes) == 1 && statusCodes[0] == "200-399") {
+				isDefault := body == "" && len(statusCodes) == 1 && statusCodes[0].(string) == "200-399"
+				if !isDefault {
 					buf.WriteString(fmt.Sprintf("%s-%+v", body, statusCodes))
 				}
 			}
