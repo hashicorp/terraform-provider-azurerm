df337e5795888ea67ab276b48436bdea0f340c8c
Matthew <mbfrahry@gmail.com>
2025-08-07 13:47:18 -0700

    Address review

--- internal/services/datafactory/data_factory_customer_managed_key_resource.go
+++ internal/services/datafactory/data_factory_customer_managed_key_resource.go
@@ -166,7 +166,7 @@ func (r DataFactoryCustomerManagedKeyResource) Read() sdk.ResourceFunc {
 			}
 
 			if encIdentity := encryption.Identity; encIdentity != nil && encIdentity.UserAssignedIdentity != nil {
-				parsed, err := commonids.ParseUserAssignedIdentityIDInsensitively(*encIdentity.UserAssignedIdentity)
+				parsed, err := commonids.ParseUserAssignedIdentityIDInsensitively(pointer.From(encIdentity.UserAssignedIdentity))
 				if err != nil {
 					return fmt.Errorf("parsing %q: %+v", *encIdentity.UserAssignedIdentity, err)
 				}
