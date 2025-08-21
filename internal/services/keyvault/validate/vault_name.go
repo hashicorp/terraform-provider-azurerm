51d7410bced003b38647942a88e826b4b75bcd96
Cary Symes <Cary.Symes@azzo.com.au>
2025-08-21 11:40:53 +1000

    feat: apply additional key_vault name verification per MS docs

--- internal/services/keyvault/validate/vault_name.go
+++ internal/services/keyvault/validate/vault_name.go
@@ -6,12 +6,21 @@ package validate
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 func VaultName(v interface{}, k string) (warnings []string, errors []error) {
 	value := v.(string)
 	if matched := regexp.MustCompile(`^[a-zA-Z0-9-]{3,24}$`).Match([]byte(value)); !matched {
 		errors = append(errors, fmt.Errorf("%q may only contain alphanumeric characters and dashes and must be between 3-24 chars", k))
+	}
+
+	if matched2 := regexp.MustCompile(`^[a-zA-Z].*[a-zA-Z0-9]$`).Match([]byte(value)); !matched2 {
+		errors = append(errors, fmt.Errorf("%q must start with a letter and end with a letter or number", k))
+	}
+
+	if strings.Contains(value, "--") {
+		errors = append(errors, fmt.Errorf("%q cannot contain consecutive hyphens (\"--\")", k))
 	}
 
 	return warnings, errors
