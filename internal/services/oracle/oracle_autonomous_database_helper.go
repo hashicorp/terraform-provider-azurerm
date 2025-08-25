5b417c15bc741cad732a5c4cb9fd4f9a9a3fc238
mihret <mihret.kidane@oracle.com>
2025-08-24 22:38:42 -0500

    nit

--- internal/services/oracle/oracle_autonomous_database_helper.go
+++ internal/services/oracle/oracle_autonomous_database_helper.go
@@ -6,11 +6,11 @@ package oracle
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/oracledatabase/2025-03-01/autonomousdatabasebackups"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/oracledatabase/2025-03-01/autonomousdatabases"
-	"strings"
 )
 
 func FlattenLongTermBackUpScheduleDetails(longTermBackUpScheduleDetails *autonomousdatabases.LongTermBackUpScheduleDetails) []LongTermBackUpScheduleDetails {
