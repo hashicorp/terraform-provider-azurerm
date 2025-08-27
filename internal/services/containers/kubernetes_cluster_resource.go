e27981802ac0aa65c36e9c5911832463ac83181e
Catriona <catriona@hashicorp.com>
2025-08-27 13:52:00 +0100

    nil check and test fix

--- internal/services/containers/kubernetes_cluster_resource.go
+++ internal/services/containers/kubernetes_cluster_resource.go
@@ -3440,7 +3440,7 @@ func expandKubernetesClusterAdvancedNetworking(input []interface{}, d *pluginsdk
 }
 
 func flattenKubernetesClusterAdvancedNetworking(advancedNetworking *managedclusters.AdvancedNetworking) []interface{} {
-	if !pointer.From(advancedNetworking.Enabled) {
+	if advancedNetworking == nil || !pointer.From(advancedNetworking.Enabled) {
 		return []interface{}{}
 	}
 
