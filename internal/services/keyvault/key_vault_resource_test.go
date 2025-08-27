6b8ede2de0c8266a3671f3743cc6684695da8666
Wyatt Fry <wyattfry@gmail.com>
2025-08-27 12:46:31 -0400

    Apply suggestion from @catriona-m
    Co-authored-by: catriona-m <86247157+catriona-m@users.noreply.github.com>

--- internal/services/keyvault/key_vault_resource_test.go
+++ internal/services/keyvault/key_vault_resource_test.go
@@ -777,7 +777,7 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_key_vault" "test" {
-  name                       = "vault%d"
+  name                       = "acctestvault%d"
   location                   = azurerm_resource_group.test.location
   resource_group_name        = azurerm_resource_group.test.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
