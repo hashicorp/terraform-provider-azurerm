cecc9ee6aff1910c27006d1edd381008673f0830
elena <sinbai@hotmail.com>
2025-08-29 13:42:53 +0800

    update code

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/apimanagement/2024-05-01/authorizationserver/model_tokenbodyparametercontract.go
+++
@@ -1,9 +0,0 @@
-package authorizationserver
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type TokenBodyParameterContract struct {
-	Name  string `json:"name"`
-	Value string `json:"value"`
-}
--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/apimanagement/2024-05-01/gatewayapi/model_apilicenseinformation.go
+++
@@ -1,9 +0,0 @@
-package gatewayapi
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type ApiLicenseInformation struct {
-	Name *string `json:"name,omitempty"`
-	Url  *string `json:"url,omitempty"`
-}
