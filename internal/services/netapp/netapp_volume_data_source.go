2a0846b30a2919c08efb2dfa2a95e2abeb604498
Paulo <pmarques@microsoft.com>
2025-09-04 22:47:01 +0000

    fix: correct indentation for setting short-term clone split capacity pool

--- internal/services/netapp/netapp_volume_data_source.go
+++ internal/services/netapp/netapp_volume_data_source.go
@@ -232,7 +232,7 @@ func dataSourceNetAppVolumeRead(d *pluginsdk.ResourceData, meta interface{}) err
 		}
 		d.Set("smb_access_based_enumeration_enabled", smbAccessBasedEnumeration)
 
-			d.Set("accept_grow_capacity_pool_for_short_term_clone_split", pointer.FromEnum(props.AcceptGrowCapacityPoolForShortTermCloneSplit))
+		d.Set("accept_grow_capacity_pool_for_short_term_clone_split", pointer.FromEnum(props.AcceptGrowCapacityPoolForShortTermCloneSplit))
 
 		protocolTypes := make([]string, 0)
 		if prtclTypes := props.ProtocolTypes; prtclTypes != nil {
