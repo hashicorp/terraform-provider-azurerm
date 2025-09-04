ec0074063208ce372cc1d071e72885ae95cd51b3
ziyeqf <51212351+ziyeqf@users.noreply.github.com>
2025-09-04 03:44:39 +0000

    revert missing test case

--- internal/services/recoveryservices/backup_protected_vm_resource_test.go
+++ internal/services/recoveryservices/backup_protected_vm_resource_test.go
@@ -1038,6 +1038,73 @@ resource "azurerm_backup_protected_vm" "test" {
 }
 
 // For update backup policy id test
+func (BackupProtectedVmResource) basePolicyTest(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-backup-%d-1"
+  location = "%s"
+}
+
+resource "azurerm_virtual_network" "test" {
+  name                = "vnet"
+  location            = azurerm_resource_group.test.location
+  address_space       = ["10.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = "acctest_subnet"
+  virtual_network_name = azurerm_virtual_network.test.name
+  resource_group_name  = azurerm_resource_group.test.name
+  address_prefixes     = ["10.0.10.0/24"]
+}
+
+resource "azurerm_network_interface" "test" {
+  name                = "acctest_nic"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  ip_configuration {
+    name                          = "acctestipconfig"
+    subnet_id                     = azurerm_subnet.test.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.test.id
+  }
+}
+
+resource "azurerm_public_ip" "test" {
+  name                = "acctest-ip"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "Basic"
+  allocation_method   = "Dynamic"
+  domain_name_label   = "acctestip%d"
+}
+
+resource "azurerm_storage_account" "test" {
+  name                     = "acctest%s"
+  location                 = azurerm_resource_group.test.location
+  resource_group_name      = azurerm_resource_group.test.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_managed_disk" "test" {
+  name                 = "acctest-datadisk"
+  location             = azurerm_resource_group.test.location
+  resource_group_name  = azurerm_resource_group.test.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1023"
+}
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomString)
+}
+
+// For update backup policy id test
 func (r BackupProtectedVmResource) withBasePolicy(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 %s
