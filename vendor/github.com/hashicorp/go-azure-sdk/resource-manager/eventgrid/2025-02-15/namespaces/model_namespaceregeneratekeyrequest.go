6d1e937fae5c18f84b6874e7310059a430b91233
Wyatt Fry <wyattfry@gmail.com>
2025-09-05 13:18:15 -0400

    update

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/eventgrid/2025-02-15/namespaces/model_namespaceregeneratekeyrequest.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/eventgrid/2025-02-15/namespaces/model_namespaceregeneratekeyrequest.go
@@ -3,11 +3,6 @@ package namespaces
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-<<<<<<<< HEAD:vendor/github.com/hashicorp/go-azure-sdk/resource-manager/cosmosdb/2025-04-15/cosmosdb/model_vectorembeddingpolicy.go
-type VectorEmbeddingPolicy struct {
-	VectorEmbeddings *[]VectorEmbedding `json:"vectorEmbeddings,omitempty"`
-========
 type NamespaceRegenerateKeyRequest struct {
 	KeyName string `json:"keyName"`
->>>>>>>> 8a975e53093a43a490f697073e410759040138c3:vendor/github.com/hashicorp/go-azure-sdk/resource-manager/eventgrid/2025-02-15/namespaces/model_namespaceregeneratekeyrequest.go
 }
