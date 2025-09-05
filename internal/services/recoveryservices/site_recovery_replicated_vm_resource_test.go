b3355af1e810194d31d4c7d984e874a3f534df2a
ziyeqf <51212351+ziyeqf@users.noreply.github.com>
2025-09-05 04:05:36 +0000

    update test case

--- internal/services/recoveryservices/site_recovery_replicated_vm_resource_test.go
+++ internal/services/recoveryservices/site_recovery_replicated_vm_resource_test.go
@@ -2426,7 +2426,7 @@ resource "azurerm_storage_account" "test" {
 
 resource "azurerm_storage_container" "test" {
   name                  = "vhds"
-  storage_account_name  = azurerm_storage_account.test.name
+  storage_account_id    = azurerm_storage_account.test.id
   container_access_type = "blob"
 }
 
