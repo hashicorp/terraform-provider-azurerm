1c64f6b7ab481e81b44f3a2cbb4584ca894d749b
Lucia Lu <qixialu@hotmail.com>
2025-09-05 17:27:15 +1000

    Fixing LRO in the sdk

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/recoveryservicesbackup/2025-02-01/protecteditems/method_createorupdate_autorest.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/recoveryservicesbackup/2025-02-01/protecteditems/method_createorupdate_autorest.go
@@ -2,16 +2,19 @@ package protecteditems
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/hashicorp/go-azure-helpers/polling"
 )
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
 type CreateOrUpdateOperationResponse struct {
+	Poller       polling.LongRunningPoller
 	HttpResponse *http.Response
 	Model        *ProtectedItemResource
 }
@@ -48,19 +51,27 @@ func (c ProtectedItemsClient) CreateOrUpdate(ctx context.Context, id ProtectedIt
 		return
 	}
 
-	result.HttpResponse, err = c.Client.Send(req, azure.DoRetryWithRegistration(c.Client))
+	result, err = c.senderForCreateOrUpdate(ctx, req)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "protecteditems.ProtectedItemsClient", "CreateOrUpdate", result.HttpResponse, "Failure sending request")
 		return
 	}
 
-	result, err = c.responderForCreateOrUpdate(result.HttpResponse)
+	return
+}
+
+// CreateOrUpdateThenPoll performs CreateOrUpdate then polls until it's completed
+func (c ProtectedItemsClient) CreateOrUpdateThenPoll(ctx context.Context, id ProtectedItemId, input ProtectedItemResource, options CreateOrUpdateOperationOptions) error {
+	result, err := c.CreateOrUpdate(ctx, id, input, options)
 	if err != nil {
-		err = autorest.NewErrorWithError(err, "protecteditems.ProtectedItemsClient", "CreateOrUpdate", result.HttpResponse, "Failure responding to request")
-		return
+		return fmt.Errorf("performing CreateOrUpdate: %+v", err)
 	}
 
-	return
+	if err := result.Poller.PollUntilDone(); err != nil {
+		return fmt.Errorf("polling after CreateOrUpdate: %+v", err)
+	}
+
+	return nil
 }
 
 // preparerForCreateOrUpdate prepares the CreateOrUpdate request.
@@ -84,15 +95,15 @@ func (c ProtectedItemsClient) preparerForCreateOrUpdate(ctx context.Context, id 
 	return preparer.Prepare((&http.Request{}).WithContext(ctx))
 }
 
-// responderForCreateOrUpdate handles the response to the CreateOrUpdate request. The method always
-// closes the http.Response Body.
-func (c ProtectedItemsClient) responderForCreateOrUpdate(resp *http.Response) (result CreateOrUpdateOperationResponse, err error) {
-	err = autorest.Respond(
-		resp,
-		azure.WithErrorUnlessStatusCode(http.StatusAccepted, http.StatusOK),
-		autorest.ByUnmarshallingJSON(&result.Model),
-		autorest.ByClosing())
-	result.HttpResponse = resp
+// senderForCreateOrUpdate sends the CreateOrUpdate request. The method will close the
+// http.Response Body if it receives an error.
+func (c ProtectedItemsClient) senderForCreateOrUpdate(ctx context.Context, req *http.Request) (future CreateOrUpdateOperationResponse, err error) {
+	var resp *http.Response
+	resp, err = c.Client.Send(req, azure.DoRetryWithRegistration(c.Client))
+	if err != nil {
+		return
+	}
 
+	future.Poller, err = polling.NewPollerFromResponse(ctx, resp, c.Client, req.Method)
 	return
 }
