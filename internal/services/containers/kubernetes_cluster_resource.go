c7fce3e45c2c2f587ba6d0e1346f2c69a0a1ee13
christianjedro <cj@cloudeteer.de>
2025-09-07 20:07:38 +0200

    dont remove tempAgentProfile.Properties

--- internal/services/containers/kubernetes_cluster_resource.go
+++ internal/services/containers/kubernetes_cluster_resource.go
@@ -2530,6 +2530,12 @@ func resourceKubernetesClusterUpdate(d *pluginsdk.ResourceData, meta interface{}
 
 			tempAgentProfile := agentProfile
 			tempAgentProfile.Name = &temporaryNodePoolName
+			if tempAgentProfile.Properties != nil {
+				tempAgentProfile.Properties.NodeImageVersion = nil
+			}
+			if agentProfile.Properties != nil {
+				agentProfile.Properties.NodeImageVersion = nil
+			}
 			// if the temp node pool already exists due to a previous failure, don't bother spinning it up
 			if tempExisting.Model == nil {
 				if err := retryNodePoolCreation(ctx, nodePoolsClient, tempNodePoolId, tempAgentProfile); err != nil {
