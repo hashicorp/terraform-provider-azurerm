847f004491d596b324d275e177a548335f3e513b
Jiawei Tao <jiaweitao@junchestudio.fareast.corp.microsoft.com>
2025-09-19 16:34:05 +0800

    New Resource: azurerm_kubernetes_cluster_connection

---
+++ internal/services/serviceconnector/kubernetes_cluster_connection_resource_test.go
@@ -0,0 +1,462 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package serviceconnector_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/servicelinker/2024-04-01/servicelinker"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+)
+
+type ServiceConnectorKubernetesClusterResource struct{}
+
+func (r ServiceConnectorKubernetesClusterResource) Exists(ctx context.Context, client *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
+	id, err := servicelinker.ParseScopedLinkerID(state.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.ServiceConnector.ServiceLinkerClient.LinkerGet(ctx, *id)
+	if err != nil {
+		if response.WasNotFound(resp.HttpResponse) {
+			return utils.Bool(false), nil
+		}
+		return nil, fmt.Errorf("retrieving %s: %+v", *id, err)
+	}
+	return utils.Bool(true), nil
+}
+
+func TestAccServiceConnectorKubernetesClusterCosmosdb_secretAuth(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_kubernetes_cluster_connection", "test")
+	r := ServiceConnectorKubernetesClusterResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.cosmosdbWithSecretAuth(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("authentication"),
+	})
+}
+
+func TestAccServiceConnectorKubernetesClusterCosmosdb_servicePrincipalSecretAuth(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_kubernetes_cluster_connection", "test")
+	r := ServiceConnectorKubernetesClusterResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.cosmosdbWithServicePrincipalSecretAuth(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("authentication"),
+	})
+}
+
+func TestAccServiceConnectorKubernetesClusterCosmosdb_userAssignedIdentity(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_kubernetes_cluster_connection", "test")
+	r := ServiceConnectorKubernetesClusterResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.cosmosdbWithUserAssignedIdentity(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("authentication"),
+	})
+}
+
+func TestAccServiceConnectorKubernetesClusterStorageBlob_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_kubernetes_cluster_connection", "test")
+	r := ServiceConnectorKubernetesClusterResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.storageBlob(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccServiceConnectorKubernetesClusterStorageBlob_secretStore(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_kubernetes_cluster_connection", "test")
+	r := ServiceConnectorKubernetesClusterResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.secretStore(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccServiceConnectorKubernetesCluster_complete(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_kubernetes_cluster_connection", "test")
+	r := ServiceConnectorKubernetesClusterResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.complete(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func (r ServiceConnectorKubernetesClusterResource) storageBlob(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[3]d"
+  location = "%[1]s"
+}
+
+resource "azurerm_storage_account" "test" {
+  name                     = "acctestacc%[2]s"
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_kubernetes_cluster" "test" {
+  name                = "acctestaks%[3]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  dns_prefix          = "acctestaks%[3]d"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "standard_a2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_connection" "test" {
+  name                  = "acctestserviceconnector%[3]d"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.test.id
+  target_resource_id    = azurerm_storage_account.test.id
+  authentication {
+    type   = "secret"
+    name   = "foo"
+    secret = "bar"
+  }
+}
+`, data.Locations.Primary, data.RandomString, data.RandomInteger)
+}
+
+func (r ServiceConnectorKubernetesClusterResource) cosmosdbWithSecretAuth(data acceptance.TestData) string {
+	template := r.template(data)
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_kubernetes_cluster_connection" "test" {
+  name                  = "acctestserviceconnector%[2]d"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.test.id
+  target_resource_id    = azurerm_cosmosdb_sql_database.test.id
+  authentication {
+    type   = "secret"
+    name   = "foo"
+    secret = "bar"
+  }
+}
+`, template, data.RandomInteger)
+}
+
+func (r ServiceConnectorKubernetesClusterResource) cosmosdbWithServicePrincipalSecretAuth(data acceptance.TestData) string {
+	template := r.template(data)
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_user_assigned_identity" "test" {
+  name                = "acctest%[2]s"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+}
+
+resource "azurerm_kubernetes_cluster_connection" "test" {
+  name                  = "acctestserviceconnector%[3]d"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.test.id
+  target_resource_id    = azurerm_cosmosdb_sql_database.test.id
+  authentication {
+    type         = "servicePrincipalSecret"
+    client_id    = "someclientid"
+    principal_id = azurerm_user_assigned_identity.test.principal_id
+    secret       = "somesecret"
+  }
+}
+`, template, data.RandomString, data.RandomInteger)
+}
+
+func (r ServiceConnectorKubernetesClusterResource) cosmosdbWithUserAssignedIdentity(data acceptance.TestData) string {
+	template := r.template(data)
+	return fmt.Sprintf(`
+%[1]s
+
+data "azurerm_subscription" "test" {}
+
+resource "azurerm_user_assigned_identity" "test" {
+  name                = "acctest%[2]s"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+}
+
+resource "azurerm_kubernetes_cluster_connection" "test" {
+  name                  = "acctestserviceconnector%[3]d"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.test.id
+  target_resource_id    = azurerm_cosmosdb_sql_database.test.id
+  authentication {
+    type            = "userAssignedIdentity"
+    subscription_id = data.azurerm_subscription.test.subscription_id
+    client_id       = azurerm_user_assigned_identity.test.client_id
+  }
+}
+`, template, data.RandomString, data.RandomInteger)
+}
+
+func (r ServiceConnectorKubernetesClusterResource) secretStore(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {
+    key_vault {
+      recover_soft_deleted_key_vaults    = false
+      purge_soft_delete_on_destroy       = false
+      purge_soft_deleted_keys_on_destroy = false
+    }
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[2]d"
+  location = "%[1]s"
+}
+
+resource "azurerm_storage_account" "test" {
+  name                     = "acctestacc%[4]s"
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_kubernetes_cluster" "test" {
+  name                = "acctestaks%[2]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  dns_prefix          = "acctestaks%[2]d"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "standard_a2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_key_vault" "test" {
+  name                     = "accAKV-%[4]s"
+  location                 = azurerm_resource_group.test.location
+  resource_group_name      = azurerm_resource_group.test.name
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  sku_name                 = "standard"
+  purge_protection_enabled = true
+}
+
+resource "azurerm_kubernetes_cluster_connection" "test" {
+  name                  = "acctestserviceconnector%[3]d"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.test.id
+  target_resource_id    = azurerm_storage_account.test.id
+  client_type           = "java"
+
+  secret_store {
+    key_vault_id = azurerm_key_vault.test.id
+  }
+  authentication {
+    type   = "secret"
+    name   = "foo"
+    secret = "bar"
+  }
+}
+`, data.Locations.Primary, data.RandomInteger, data.RandomInteger, data.RandomString)
+}
+
+func (r ServiceConnectorKubernetesClusterResource) complete(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[2]d"
+  location = "%[1]s"
+}
+
+resource "azurerm_cosmosdb_account" "test" {
+  name                = "acctestacc%[4]s"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.test.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "test" {
+  name                = "cosmos-sql-db"
+  resource_group_name = azurerm_cosmosdb_account.test.resource_group_name
+  account_name        = azurerm_cosmosdb_account.test.name
+  throughput          = 400
+}
+
+resource "azurerm_cosmosdb_sql_container" "test" {
+  name                = "example-container"
+  resource_group_name = azurerm_cosmosdb_account.test.resource_group_name
+  account_name        = azurerm_cosmosdb_account.test.name
+  database_name       = azurerm_cosmosdb_sql_database.test.name
+  partition_key_paths = ["/definition"]
+}
+
+resource "azurerm_kubernetes_cluster" "test" {
+  name                = "acctestaks%[2]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  dns_prefix          = "acctestaks%[2]d"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "standard_a2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_connection" "test" {
+  name                  = "acctestserviceconnector%[3]d"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.test.id
+  target_resource_id    = azurerm_cosmosdb_sql_database.test.id
+  client_type           = "java"
+  vnet_solution         = "privateLink"
+  authentication {
+    type   = "secret"
+    name   = "foo"
+    secret = "bar"
+  }
+}
+`, data.Locations.Primary, data.RandomInteger, data.RandomInteger, data.RandomString)
+}
+
+func (r ServiceConnectorKubernetesClusterResource) template(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[2]d"
+  location = "%[1]s"
+}
+
+resource "azurerm_cosmosdb_account" "test" {
+  name                = "acctestacc%[4]s"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.test.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "test" {
+  name                = "cosmos-sql-db"
+  resource_group_name = azurerm_cosmosdb_account.test.resource_group_name
+  account_name        = azurerm_cosmosdb_account.test.name
+  throughput          = 400
+}
+
+resource "azurerm_cosmosdb_sql_container" "test" {
+  name                = "example-container"
+  resource_group_name = azurerm_cosmosdb_account.test.resource_group_name
+  account_name        = azurerm_cosmosdb_account.test.name
+  database_name       = azurerm_cosmosdb_sql_database.test.name
+  partition_key_paths = ["/definition"]
+}
+
+resource "azurerm_kubernetes_cluster" "test" {
+  name                = "acctestaks%[2]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  dns_prefix          = "acctestaks%[2]d"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "standard_a2_v2"
+
+    upgrade_settings {
+      max_surge                     = "10%%"
+      drain_timeout_in_minutes      = 0
+      node_soak_duration_in_minutes = 0
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+`, data.Locations.Primary, data.RandomInteger, data.RandomInteger, data.RandomString)
+}
---
+++ website/docs/r/kubernetes_cluster_connection.html.markdown
@@ -0,0 +1,149 @@
+---
+subcategory: "Container (Kubernetes)"
+layout: "azurerm"
+page_title: "Azure Resource Manager: azurerm_kubernetes_cluster_connection"
+description: |-
+  Manages a service connector for kubernetes cluster.
+---
+
+# azurerm_kubernetes_cluster_connection
+
+Manages a service connector for kubernetes cluster.
+
+## Example Usage
+
+```hcl
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_cosmosdb_account" "example" {
+  name                = "example-cosmosdb-account"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.example.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "example" {
+  name                = "cosmos-sql-db"
+  resource_group_name = azurerm_cosmosdb_account.example.resource_group_name
+  account_name        = azurerm_cosmosdb_account.example.name
+  throughput          = 400
+}
+
+resource "azurerm_cosmosdb_sql_container" "example" {
+  name                = "example-container"
+  resource_group_name = azurerm_cosmosdb_account.example.resource_group_name
+  account_name        = azurerm_cosmosdb_account.example.name
+  database_name       = azurerm_cosmosdb_sql_database.example.name
+  partition_key_path  = "/definition"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks-cluster"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_connection" "example" {
+  name                  = "example-serviceconnector"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.example.id
+  target_resource_id    = azurerm_cosmosdb_sql_database.example.id
+  authentication {
+    type   = "secret"
+    name   = "foo"
+    secret = "bar"
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required) The name of the service connection. Changing this forces a new resource to be created.
+
+* `kubernetes_cluster_id` - (Required) The ID of the kubernetes cluster. Changing this forces a new resource to be created.
+
+* `target_resource_id` - (Required) The ID of the target resource. Changing this forces a new resource to be created. Possible target resources are `Postgres`, `PostgresFlexible`, `Mysql`, `Sql`, `Redis`, `RedisEnterprise`, `CosmosCassandra`, `CosmosGremlin`, `CosmosMongo`, `CosmosSql`, `CosmosTable`, `StorageBlob`, `StorageQueue`, `StorageFile`, `StorageTable`, `AppConfig`, `EventHub`, `ServiceBus`, `SignalR`, `WebPubSub`, `ConfluentKafka`. The integration guide can be found [here](https://learn.microsoft.com/en-us/azure/service-connector/how-to-integrate-postgres).
+
+* `authentication` - (Required) The authentication info. An `authentication` block as defined below.
+
+* `client_type` - (Optional) The application client type. Possible values are `none`, `dotnet`, `java`, `python`, `go`, `php`, `ruby`, `django`, `nodejs` and `springBoot`. Defaults to `none`.
+
+* `vnet_solution` - (Optional) The type of VNet solution. Possible values are `serviceEndpoint`, `privateLink`.
+
+* `secret_store` - (Optional) An option to store secret value in secure place. An `secret_store` block as defined below.
+
+-> **Note:** If a Managed Identity is used, this will need to be configured on the Kubernetes Cluster.
+
+---
+
+An `authentication` block supports the following:
+
+* `type` - (Required) The authentication type. Possible values are `userAssignedIdentity`, `servicePrincipalSecret`, `servicePrincipalCertificate`, `secret`. Changing this forces a new resource to be created.
+
+* `certificate` - (Optional) The certificate for `servicePrincipalCertificate` authentication. Should be specified when `type` is set to `servicePrincipalCertificate`.
+
+* `client_id` - (Optional) The client ID for `userAssignedIdentity` or `servicePrincipalSecret` authentication. Should be specified when `type` is set to `servicePrincipalSecret` or `userAssignedIdentity`.
+
+* `name` - (Optional) The name of the secret for `secret` authentication. Should be specified when `type` is set to `secret`.
+
+* `principal_id` - (Optional) The principal ID for `servicePrincipalSecret` authentication. Should be specified when `type` is set to `servicePrincipalSecret`.
+
+* `secret` - (Optional) The secret for `secret` or `servicePrincipalSecret` authentication. Should be specified when `type` is set to `secret` or `servicePrincipalSecret`.
+
+* `subscription_id` - (Optional) The subscription ID for `userAssignedIdentity` authentication. Should be specified when `type` is set to `userAssignedIdentity`.
+
+---
+
+An `secret_store` block supports the following:
+
+* `key_vault_id` - (Required) The key vault id to store secret.
+
+## Attributes Reference
+
+In addition to the Arguments listed above - the following Attributes are exported:
+
+* `id` - The ID of the service connector.
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration/resources.html#timeouts) for certain actions:
+
+* `create` - (Defaults to 30 minutes) Used when creating the Service Connector.
+* `read` - (Defaults to 5 minutes) Used when retrieving the Service Connector.
+* `update` - (Defaults to 30 minutes) Used when updating the Service Connector.
+* `delete` - (Defaults to 30 minutes) Used when deleting the Service Connector.
+
+## Import
+
+Service Connector for kubernetes cluster can be imported using the `resource id`, e.g.
+
+```shell
+terraform import azurerm_kubernetes_cluster_connection.example /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/group1/providers/Microsoft.ContainerService/managedClusters/cluster1/providers/Microsoft.ServiceLinker/linkers/link1
+```
