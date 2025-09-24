2bbca8622da22317a3138d083ec585ae3e8977f9
Neil Ye <yechenwei2007@hotmail.com>
2025-09-24 22:34:04 +0800

    [TestCase] `azurerm_*_cost_management_export` - Update `container_id` (#30686)

--- internal/services/costmanagement/resource_group_cost_management_export_resource_test.go
+++ internal/services/costmanagement/resource_group_cost_management_export_resource_test.go
@@ -129,7 +129,7 @@ resource "azurerm_resource_group_cost_management_export" "test" {
   recurrence_period_end_date   = "%sT00:00:00Z"
 
   export_data_storage_location {
-    container_id     = azurerm_storage_container.test.id
+    container_id     = "${azurerm_storage_account.test.id}/blobServices/default/containers/${azurerm_storage_container.test.name}"
     root_folder_path = "/root"
   }
   export_data_options {
@@ -174,7 +174,7 @@ resource "azurerm_resource_group_cost_management_export" "test" {
   file_format                  = "Csv"
 
   export_data_storage_location {
-    container_id     = azurerm_storage_container.test.id
+    container_id     = "${azurerm_storage_account.test.id}/blobServices/default/containers/${azurerm_storage_container.test.name}"
     root_folder_path = "/root"
   }
   export_data_options {
@@ -198,7 +198,7 @@ resource "azurerm_resource_group_cost_management_export" "import" {
   recurrence_period_end_date   = azurerm_resource_group_cost_management_export.test.recurrence_period_start_date
 
   export_data_storage_location {
-    container_id     = azurerm_storage_container.test.id
+    container_id     = "${azurerm_storage_account.test.id}/blobServices/default/containers/${azurerm_storage_container.test.name}"
     root_folder_path = "/root"
   }
 
