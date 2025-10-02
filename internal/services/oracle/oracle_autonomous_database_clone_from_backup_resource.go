4f4b26cb3a1c7ccf85fab4fc620cad6a66eb9217
mihret <mihret.kidane@oracle.com>
2025-10-02 16:56:33 -0500

    comment resolution

--- internal/services/oracle/oracle_autonomous_database_clone_from_backup_resource.go
+++ internal/services/oracle/oracle_autonomous_database_clone_from_backup_resource.go
@@ -6,7 +6,6 @@ package oracle
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"time"
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
@@ -14,6 +13,7 @@ import (
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/oracledatabase/2025-03-01/autonomousdatabases"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/oracle/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
@@ -302,7 +302,6 @@ func (r AutonomousDatabaseCloneFromBackupResource) Create() sdk.ResourceFunc {
 			if model.BackupTimestamp != "" {
 				properties.Timestamp = pointer.To(model.BackupTimestamp)
 				properties.UseLatestAvailableBackupTimeStamp = pointer.To(false)
-
 			} else {
 				properties.UseLatestAvailableBackupTimeStamp = pointer.To(true)
 			}
