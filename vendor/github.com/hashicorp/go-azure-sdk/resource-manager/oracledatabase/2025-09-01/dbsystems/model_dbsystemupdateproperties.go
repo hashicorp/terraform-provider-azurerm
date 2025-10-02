b304a635514d9e73472b714c4fd4761314822aa6
Evgen Rusakov <evgen.rusakov@oracle.com>
2025-10-02 16:50:23 -0400

    Update oracledatabase version to 2025-09-01

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/oracledatabase/2025-09-01/dbsystems/model_dbsystemupdateproperties.go
@@ -0,0 +1,8 @@
+package dbsystems
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type DbSystemUpdateProperties struct {
+	Source *Source `json:"source,omitempty"`
+}
