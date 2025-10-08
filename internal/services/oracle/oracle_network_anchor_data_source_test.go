dd57af8bed97e8905670638dd736b2fa6b0451be
Evgen Rusakov <evgen.rusakov@oracle.com>
2025-10-08 15:39:06 -0400

    Oracle - add network anchor

---
+++ internal/services/oracle/oracle_network_anchor_data_source_test.go
@@ -0,0 +1,44 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package oracle_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/oracle"
+)
+
+type NetworkAnchorDataSource struct{}
+
+func TestNetworkAnchorDataSource_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, fmt.Sprintf("data.%[1]s", oracle.NetworkAnchorDataSource{}.ResourceType()), "test")
+	r := NetworkAnchorDataSource{}
+	data.DataSourceTest(t, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).Key("location").Exists(),
+				check.That(data.ResourceName).Key("name").Exists(),
+				check.That(data.ResourceName).Key("resource_group_name").Exists(),
+				check.That(data.ResourceName).Key("resource_anchor_id").Exists(),
+				check.That(data.ResourceName).Key("subnet_id").Exists(),
+				check.That(data.ResourceName).Key("id").Exists(),
+			),
+		},
+	})
+}
+
+func (d NetworkAnchorDataSource) basic(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+data "azurerm_oracle_network_anchor" "test" {
+  name                = azurerm_oracle_network_anchor.test.name
+  resource_group_name = azurerm_oracle_network_anchor.test.resource_group_name
+}
+`, NetworkAnchorResource{}.basic(data))
+}
