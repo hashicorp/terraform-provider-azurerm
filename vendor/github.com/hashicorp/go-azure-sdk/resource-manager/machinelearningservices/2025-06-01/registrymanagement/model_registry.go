6f090d2da402b4f4632890b6bbc002afaeb0ad33
Wyatt Fry <wyattfry@gmail.com>
2025-10-10 20:16:05 -0400

    wip

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/machinelearningservices/2025-06-01/registrymanagement/model_registry.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/machinelearningservices/2025-06-01/registrymanagement/model_registry.go
@@ -7,6 +7,7 @@ type Registry struct {
 	DiscoveryURL                       *string                              `json:"discoveryUrl,omitempty"`
 	IntellectualPropertyPublisher      *string                              `json:"intellectualPropertyPublisher,omitempty"`
 	ManagedResourceGroup               *ArmResourceId                       `json:"managedResourceGroup,omitempty"`
+	ManagedResourceGroupSettings       *ManagedResourceGroupSettings        `json:"managedResourceGroupSettings,omitempty"`
 	MlFlowRegistryUri                  *string                              `json:"mlFlowRegistryUri,omitempty"`
 	PublicNetworkAccess                *string                              `json:"publicNetworkAccess,omitempty"`
 	RegionDetails                      *[]RegistryRegionArmDetails          `json:"regionDetails,omitempty"`
