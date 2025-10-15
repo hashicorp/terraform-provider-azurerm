c5c726ed0684c17facba9b94f064bed6fb9d7fba
djryanj <ryan@ryanjjacobs.com>
2025-10-15 15:37:27 -0600

    Fixes #30869

--- internal/services/search/search_service_resource.go
+++ internal/services/search/search_service_resource.go
@@ -285,7 +285,7 @@ func resourceSearchServiceCreate(d *pluginsdk.ResourceData, meta interface{}) er
 	}
 
 	if !localAuthenticationEnabled && authenticationFailureMode != "" {
-		return errors.New("'authentication_failure_mode' cannot be defined if 'local_authentication_enabled' has been set to 'true'")
+		return errors.New("'authentication_failure_mode' cannot be defined if 'local_authentication_enabled' has been set to 'false'")
 	}
 
 	// API Only Mode (Default) (e.g. localAuthenticationEnabled = true)...
