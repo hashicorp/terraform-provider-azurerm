84e3737029c730dcc7a29adaf4198720df02f853
honglinq <harryqu@microsoft.com>
2025-10-21 02:54:12 +1100

    Update managed_application_resource_test.go

--- internal/services/managedapplications/managed_application_resource_test.go
+++ internal/services/managedapplications/managed_application_resource_test.go
@@ -172,6 +172,20 @@ func TestAccManagedApplication_identity(t *testing.T) {
 			),
 		},
 		data.ImportStep(),
+		{
+			Config: r.userAssignedIdentity(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
 	})
 }
 
