c9c087c129b8012ae43e8c319797cf5eae70ea2a
Paulo Marques <paulomarquesc@users.noreply.github.com>
2025-10-21 11:54:03 -0700

    `azurerm_netapp_volume_resource` - support for Cross Zone Region replication through the `data_protection_replication` block (#30872)
    [ENHANCEMENT] * `azurerm_netapp_volume_resource` - support for Cross Zone Region replication through the `data_protection_replication` block

--- internal/services/netapp/netapp_account_encryption_resource_test.go
+++ internal/services/netapp/netapp_account_encryption_resource_test.go
@@ -210,7 +210,7 @@ data "azurerm_client_config" "current" {
 }
 
 resource "azurerm_key_vault" "test" {
-  name                            = "acctest%[2]d"
+  name                            = "acct%[2]d"
   location                        = azurerm_resource_group.test.location
   resource_group_name             = azurerm_resource_group.test.name
   enabled_for_disk_encryption     = true
