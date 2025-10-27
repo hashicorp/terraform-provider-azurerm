c4bea886564260fb5df1615ca763650f21cc87e7
Wyatt Fry <wyattfry@gmail.com>
2025-10-27 17:01:48 -0400

    add create test for svc principal

--- internal/services/machinelearning/machine_learning_datastore_blobstorage_resource_test.go
+++ internal/services/machinelearning/machine_learning_datastore_blobstorage_resource_test.go
@@ -107,6 +107,21 @@ func TestAccMachineLearningDataStoreBlobStorage_Update(t *testing.T) {
 	})
 }
 
+func TestAccMachineLearningDataStoreBlobStorage_servicePrincipal(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_machine_learning_datastore_blobstorage", "test")
+	r := MachineLearningDataStoreBlobStorage{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.blobStorageServicePrincipal(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("client_secret"),
+	})
+}
+
 func TestAccMachineLearningDataStoreBlobStorage_requiresImport(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_machine_learning_datastore_blobstorage", "test")
 	r := MachineLearningDataStoreBlobStorage{}
