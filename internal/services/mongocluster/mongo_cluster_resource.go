f4df2830448f752f0aab8f678672ac635395b674
neil-yechenwei <yechenwei2007@hotmail.com>
2025-10-30 12:20:31 +0800

    Upgrade MongoCluster API version from 2024-07-01 to 2025-09-01

--- internal/services/mongocluster/mongo_cluster_resource.go
+++ internal/services/mongocluster/mongo_cluster_resource.go
@@ -14,7 +14,7 @@ import (
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/mongocluster/2024-07-01/mongoclusters"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/mongocluster/2025-09-01/mongoclusters"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/mongocluster/2025-09-01/mongoclusters/model_authconfigproperties.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/mongocluster/2025-09-01/mongoclusters/model_authconfigproperties.go
@@ -3,6 +3,6 @@ package mongoclusters
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-type StorageProperties struct {
-	SizeGb *int64 `json:"sizeGb,omitempty"`
+type AuthConfigProperties struct {
+	AllowedModes *[]AuthenticationMode `json:"allowedModes,omitempty"`
 }
