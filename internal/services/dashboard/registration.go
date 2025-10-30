6a1ae2a81ec0aa8e468b241f4e13d99d8c230b22
JT <jiaweitao@microsoft.com>
2025-10-30 15:52:42 +0800

    New Resource: azurerm_dashboard

--- internal/services/dashboard/registration.go
+++ internal/services/dashboard/registration.go
@@ -51,6 +51,7 @@ func (r Registration) DataSources() []sdk.DataSource {
 // Resources returns a list of Resources supported by this Service
 func (r Registration) Resources() []sdk.Resource {
 	return []sdk.Resource{
+		DashboardResource{},
 		DashboardGrafanaResource{},
 		ManagedPrivateEndpointResource{},
 	}
