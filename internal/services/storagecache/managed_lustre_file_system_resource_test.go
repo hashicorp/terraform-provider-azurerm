5e70afd721ddc376ceb17760bfff0f4b7e097aca
Anurag Upadhaya <anurag.upadhaya@hashicorp.com>
2025-11-18 16:49:12 +0530

    Storagecache : Fix "Expected a StorageContainer ID that matched (containing 12 segments)" (#31126)

--- internal/services/storagecache/managed_lustre_file_system_resource_test.go
+++ internal/services/storagecache/managed_lustre_file_system_resource_test.go
@@ -282,13 +282,13 @@ resource "azurerm_storage_account" "test" {
 
 resource "azurerm_storage_container" "test" {
   name                  = "storagecontainer"
-  storage_account_name  = azurerm_storage_account.test.name
+  storage_account_id    = azurerm_storage_account.test.id
   container_access_type = "private"
 }
 
 resource "azurerm_storage_container" "test2" {
   name                  = "storagecontainer2"
-  storage_account_name  = azurerm_storage_account.test.name
+  storage_account_id    = azurerm_storage_account.test.id
   container_access_type = "private"
 }
 
