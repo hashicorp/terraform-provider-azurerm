1aa23a48c31c7650a3266d6f81f68ea9ad369d5a
Lucia Lu <qixialu@hotmail.com>
2025-11-19 11:16:25 +1100

    Update sdk to 2025-08-01

--- internal/services/recoveryservices/recovery_services_vault_hyperv_host_registration_test.go
+++ internal/services/recoveryservices/recovery_services_vault_hyperv_host_registration_test.go
@@ -10,7 +10,7 @@ import (
 	"time"
 
 	"github.com/hashicorp/go-azure-helpers/lang/response"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/recoveryservices/2024-01-01/vaults"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/recoveryservices/2025-08-01/vaults"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/recoveryservices/2024-04-01/vaultcertificates"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/recoveryservicessiterecovery/2024-04-01/replicationfabrics"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
