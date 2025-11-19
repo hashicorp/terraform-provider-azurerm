d1f47b60fcc8f7b6e42c604f0008c959c5b5ed8d
Gerry Tan <2316198+gerrytan@users.noreply.github.com>
2025-11-20 07:58:46 +1100

    azurerm_mysql_flexible_server: 8.4 version support (#31099)
    Co-authored-by: sreallymatt <106555974+sreallymatt@users.noreply.github.com>

--- internal/services/mysql/mysql_flexible_server_resource_test.go
+++ internal/services/mysql/mysql_flexible_server_resource_test.go
@@ -610,6 +610,21 @@ func TestAccMySqlFlexibleServer_publicNetworkAccess(t *testing.T) {
 	})
 }
 
+func TestAccMySqlFlexibleServer_eightPointFour(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_mysql_flexible_server", "test")
+	r := MySqlFlexibleServerResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.eightPointFour(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("administrator_password"),
+	})
+}
+
 func (MySqlFlexibleServerResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	id, err := servers.ParseFlexibleServerID(state.ID)
 	if err != nil {
@@ -1647,4 +1662,21 @@ resource "azurerm_mysql_flexible_server_configuration" "test" {
   value               = "ERROR_FOR_DIVISION_BY_ZERO,NO_ZERO_DATE,NO_ZERO_IN_DATE,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_AUTO_VALUE_ON_ZERO"
 }
 `, r.template(data), data.RandomInteger, version)
+}
+
+func (r MySqlFlexibleServerResource) eightPointFour(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_mysql_flexible_server" "test" {
+  name                   = "acctest-fs-%d"
+  resource_group_name    = azurerm_resource_group.test.name
+  location               = azurerm_resource_group.test.location
+  administrator_login    = "_admin_Terraform_892123456789312"
+  administrator_password = "QAZwsx123"
+  sku_name               = "B_Standard_B1ms"
+
+  version = "8.4"
+}
+`, r.template(data), data.RandomInteger)
 }
