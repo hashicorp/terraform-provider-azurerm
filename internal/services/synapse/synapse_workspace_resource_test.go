b60a9667a3e8a2592b1ba8e00c0dfd9612ea493b
Elizabeth Zhong <ezhong@microsoft.com>
2025-11-19 08:03:06 +0000

    fix validation issue for key vault in tests

--- internal/services/synapse/synapse_workspace_resource_test.go
+++ internal/services/synapse/synapse_workspace_resource_test.go
@@ -606,7 +606,7 @@ func (r SynapseWorkspaceResource) customerManagedKey(data acceptance.TestData) s
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "test" {
-  name                       = "acctest%d"
+  name                       = "acctest%s"
   location                   = azurerm_resource_group.test.location
   resource_group_name        = azurerm_resource_group.test.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
@@ -654,7 +654,7 @@ resource "azurerm_synapse_workspace" "test" {
     type = "SystemAssigned"
   }
 }
-`, template, data.RandomInteger, data.RandomInteger)
+`, template, data.RandomString, data.RandomInteger)
 }
 
 func (r SynapseWorkspaceResource) azureAdOnlyAuthentication(data acceptance.TestData) string {
@@ -727,13 +727,13 @@ func (r SynapseWorkspaceResource) cmkWithAADAdmin(data acceptance.TestData) stri
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_user_assigned_identity" "test" {
-  name                = "acctestuaid%[2]d"
+  name                = "acctestuaid%[2]s"
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
 }
 
 resource "azurerm_key_vault" "test" {
-  name                       = "acctest%[2]d"
+  name                       = "acctest%[2]s"
   location                   = azurerm_resource_group.test.location
   resource_group_name        = azurerm_resource_group.test.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
@@ -776,7 +776,7 @@ resource "azurerm_key_vault_key" "test" {
 }
 
 resource "azurerm_synapse_workspace" "test" {
-  name                                 = "acctestsw%[2]d"
+  name                                 = "acctestsw%[2]s"
   resource_group_name                  = azurerm_resource_group.test.name
   location                             = azurerm_resource_group.test.location
   storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.test.id
@@ -794,5 +794,5 @@ resource "azurerm_synapse_workspace" "test" {
     identity_ids = [azurerm_user_assigned_identity.test.id]
   }
 }
-`, template, data.RandomInteger)
+`, template, data.RandomString)
 }
