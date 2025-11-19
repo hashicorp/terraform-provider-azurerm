e15f0c41f6b8b02fc2d5caae0e5ac5f3db0f6083
Julian <166389663+avendretter@users.noreply.github.com>
2025-11-19 21:56:26 +0100

    New Data Source: `azurerm_api_management_workspace` (#30241)
    Co-authored-by: Julian Gog <julian.gog@enpal.de>
    Co-authored-by: sreallymatt <106555974+sreallymatt@users.noreply.github.com>

--- internal/services/apimanagement/registration.go
+++ internal/services/apimanagement/registration.go
@@ -100,6 +100,7 @@ func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
 func (r Registration) DataSources() []sdk.DataSource {
 	return []sdk.DataSource{
 		ApiManagementSubscriptionDataSource{},
+		ApiManagementWorkspaceDataSource{},
 	}
 }
 
