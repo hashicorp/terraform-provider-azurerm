6b6e9c2cf618bd3aa62b46551fe7ba6b2ea27b10
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2025-11-21 15:57:27 +0800

    `azurerm_machine_learning_workspace_network_outbound_rule_private_endpoint` - Refine fix of `TestAccMachineLearningWorkspaceNetworkOutboundRulePrivateEndpoint_redis`

--- internal/services/machinelearning/machine_learning_workspace_network_outbound_rule_private_endpoint_resource_test.go
+++ internal/services/machinelearning/machine_learning_workspace_network_outbound_rule_private_endpoint_resource_test.go
@@ -386,6 +386,9 @@ func (r WorkspaceNetworkOutboundPrivateEndpointResource) withRedis(data acceptan
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
     key_vault {
       purge_soft_delete_on_destroy       = false
       purge_soft_deleted_keys_on_destroy = false
