a7e5a3a87dcd403a51a68cb8adda0c1c1ab201cd
Lucia Lu <qixialu@hotmail.com>
2025-11-21 16:16:46 +1100

    Removed unused vendor

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/recoveryservices/2025-08-01/vaults/model_sourcescanconfiguration.go
+++
@@ -1,9 +0,0 @@
-package vaults
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type SourceScanConfiguration struct {
-	SourceScanIdentity *AssociatedIdentity `json:"sourceScanIdentity,omitempty"`
-	State              *State              `json:"state,omitempty"`
-}
