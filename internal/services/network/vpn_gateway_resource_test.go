162c3f489728017f6a4c8da3e147c999fb187c7f
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2025-12-01 11:29:19 -0700

    services `N*` - deprecated function clean up

--- internal/services/network/vpn_gateway_resource_test.go
+++ internal/services/network/vpn_gateway_resource_test.go
@@ -8,12 +8,12 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/network/2025-01-01/virtualwans"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
-	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
 type VPNGatewayResource struct{}
@@ -181,7 +181,7 @@ func (t VPNGatewayResource) Exists(ctx context.Context, clients *clients.Client,
 		return nil, fmt.Errorf("retrieving %s: %+v", id, err)
 	}
 
-	return utils.Bool(resp.Model != nil), nil
+	return pointer.To(resp.Model != nil), nil
 }
 
 func (r VPNGatewayResource) basic(data acceptance.TestData) string {
