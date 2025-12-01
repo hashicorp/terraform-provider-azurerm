701388ce0aa9c9be2f0a4f8eca6a2aa9fc29bb89
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2025-12-01 11:08:36 -0700

    services `A*` - deprecated function clean up

--- internal/services/automanage/arc_machine_automanage_configuration_assignment_resource_test.go
+++ internal/services/automanage/arc_machine_automanage_configuration_assignment_resource_test.go
@@ -15,7 +15,6 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
-	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
 type ArcMachineConfigurationAssignmentResource struct{}
@@ -46,7 +45,7 @@ func (r ArcMachineConfigurationAssignmentResource) Exists(ctx context.Context, c
 	resp, err := client.Get(ctx, *id)
 	if err != nil {
 		if response.WasNotFound(resp.HttpResponse) {
-			return utils.Bool(false), nil
+			return pointer.To(false), nil
 		}
 		return nil, fmt.Errorf("retrieving %s: %+v", id, err)
 	}
