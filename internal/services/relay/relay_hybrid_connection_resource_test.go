453f1968aaa4c523f311da62f9cc577196d98550
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2025-12-01 16:20:48 -0700

    services `R*` - deprecated function clean up (#31239)

--- internal/services/relay/relay_hybrid_connection_resource_test.go
+++ internal/services/relay/relay_hybrid_connection_resource_test.go
@@ -8,12 +8,12 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/relay/2021-11-01/hybridconnections"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
-	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
 type RelayHybridConnectionResource struct{}
@@ -99,7 +99,7 @@ func (t RelayHybridConnectionResource) Exists(ctx context.Context, clients *clie
 		return nil, fmt.Errorf("retrieving %s: %+v", *id, err)
 	}
 
-	return utils.Bool(resp.Model != nil), nil
+	return pointer.To(resp.Model != nil), nil
 }
 
 func (RelayHybridConnectionResource) basic(data acceptance.TestData) string {
