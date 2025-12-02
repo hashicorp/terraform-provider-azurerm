b265fb2916852208ec45892a1fad523fb98252bb
longwan <longwan@microsoft.com>
2025-12-02 23:37:11 +0000

    add container network logs support for terraform

--- internal/services/containers/kubernetes_cluster_data_source.go
+++ internal/services/containers/kubernetes_cluster_data_source.go
@@ -354,6 +354,10 @@ func dataSourceKubernetesCluster() *pluginsdk.Resource {
 							Type:     pluginsdk.TypeBool,
 							Computed: true,
 						},
+						"container_network_logs_enabled": {
+							Type:     pluginsdk.TypeBool,
+							Computed: true,
+						},
 						"oms_agent_identity": {
 							Type:     pluginsdk.TypeList,
 							Computed: true,
@@ -1077,6 +1081,7 @@ func flattenKubernetesClusterDataSourceAddOns(profile map[string]managedclusters
 	if enabled := omsAgent.Enabled; enabled {
 		workspaceID := ""
 		useAADAuth := false
+		containerNetworkLogsEnabled := false
 
 		if v := kubernetesAddonProfilelocateInConfig(omsAgent.Config, "logAnalyticsWorkspaceResourceID"); v != "" {
 			if lawid, err := workspaces.ParseWorkspaceID(v); err == nil {
@@ -1088,11 +1093,16 @@ func flattenKubernetesClusterDataSourceAddOns(profile map[string]managedclusters
 			useAADAuth = true
 		}
 
+		if v := kubernetesAddonProfilelocateInConfig(omsAgent.Config, "enableRetinaNetworkFlags"); v != "false" && v != "" {
+			containerNetworkLogsEnabled = true
+		}
+
 		omsAgentIdentity := flattenKubernetesClusterAddOnIdentityProfile(omsAgent.Identity)
 
 		omsAgents = append(omsAgents, map[string]interface{}{
 			"log_analytics_workspace_id":      workspaceID,
 			"msi_auth_for_monitoring_enabled": useAADAuth,
+			"container_network_logs_enabled":  containerNetworkLogsEnabled,
 			"oms_agent_identity":              omsAgentIdentity,
 		})
 	}
