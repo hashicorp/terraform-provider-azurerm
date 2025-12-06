2a595269622c3f572d56e2f9653fda151f77fe8c
Pierre-Emmanuel MERCIER <p.mercier@betclicgroup.com>
2025-12-06 18:01:02 +0100

    report previous developed code

--- internal/services/containerapps/helpers/container_apps.go
+++ internal/services/containerapps/helpers/container_apps.go
@@ -3162,6 +3162,7 @@ func AzureQueueScaleRuleSchemaComputed() *pluginsdk.Schema {
 type CustomScaleRule struct {
 	Name            string                    `tfschema:"name"`
 	Metadata        map[string]string         `tfschema:"metadata"`
+	Identity        string                    `tfschema:"identity"`
 	CustomRuleType  string                    `tfschema:"custom_rule_type"`
 	Authentications []ScaleRuleAuthentication `tfschema:"authentication"`
 }
@@ -3184,6 +3185,16 @@ func CustomScaleRuleSchema() *pluginsdk.Schema {
 					Elem: &pluginsdk.Schema{
 						Type: pluginsdk.TypeString,
 					},
+				},
+
+				"identity": {
+					Type:     pluginsdk.TypeString,
+					Optional: true,
+					ValidateFunc: validation.Any(
+						commonids.ValidateUserAssignedIdentityID,
+						validation.StringInSlice([]string{"system"}, true),
+					),
+					Description: "The identity to use for accessing key vault reference.",
 				},
 
 				"custom_rule_type": {
@@ -3247,6 +3258,12 @@ func CustomScaleRuleSchemaComputed() *pluginsdk.Schema {
 					Elem: &pluginsdk.Schema{
 						Type: pluginsdk.TypeString,
 					},
+				},
+
+				"identity": {
+					Type:        pluginsdk.TypeString,
+					Computed:    true,
+					Description: "The identity to use for accessing key vault reference.",
 				},
 
 				"custom_rule_type": {
