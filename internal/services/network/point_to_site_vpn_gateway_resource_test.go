b34dcab32ba9dd8033988b21cfab52381f152d9d
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2025-12-08 12:21:35 +0800

    Network - fix `TestAccPointToSiteVPNGateway_connectionConfiguration`

--- internal/services/network/point_to_site_vpn_gateway_resource_test.go
+++ internal/services/network/point_to_site_vpn_gateway_resource_test.go
@@ -10,6 +10,8 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
+  "github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
@@ -37,7 +39,7 @@ func TestAccPointToSiteVPNGateway_connectionConfiguration(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_point_to_site_vpn_gateway", "test")
 	r := PointToSiteVPNGatewayResource{}
 
-	data.ResourceTest(t, r, []acceptance.TestStep{
+	data.ResourceTestIgnoreRecreate(t, r, []acceptance.TestStep{
 		{
 			Config: r.singleConnectionConfiguration(data),
 			Check: acceptance.ComposeTestCheckFunc(
@@ -50,6 +52,11 @@ func TestAccPointToSiteVPNGateway_connectionConfiguration(t *testing.T) {
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 			),
+      ConfigPlanChecks: resource.ConfigPlanChecks{
+				PreApply: []plancheck.PlanCheck{
+					plancheck.ExpectResourceAction(data.ResourceName, plancheck.ResourceActionReplace),
+				},
+			},
 		},
 		data.ImportStep(),
 		{
@@ -57,6 +64,11 @@ func TestAccPointToSiteVPNGateway_connectionConfiguration(t *testing.T) {
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 			),
+      ConfigPlanChecks: resource.ConfigPlanChecks{
+				PreApply: []plancheck.PlanCheck{
+					plancheck.ExpectResourceAction(data.ResourceName, plancheck.ResourceActionReplace),
+				},
+			},
 		},
 		data.ImportStep(),
 	})
