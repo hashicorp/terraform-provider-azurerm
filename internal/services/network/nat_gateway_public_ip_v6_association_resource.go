15a2879e564f0e5b1b8325a2726b06f4227c04a2
“neil-yechenwei” <yechenwei2007@hotmail.com>
2025-12-09 14:52:19 +0800

    update style

--- internal/services/network/nat_gateway_public_ip_v6_association_resource.go
+++ internal/services/network/nat_gateway_public_ip_v6_association_resource.go
@@ -59,6 +59,7 @@ func (r NatGatewayPublicIpV6AssociationResource) ResourceType() string {
 
 func (r NatGatewayPublicIpV6AssociationResource) Create() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
+		Timeout: 30 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
 			client := metadata.Client.Network.NatGateways
 
@@ -88,34 +89,32 @@ func (r NatGatewayPublicIpV6AssociationResource) Create() sdk.ResourceFunc {
 				return fmt.Errorf("retrieving %s: %+v", natGatewayId, err)
 			}
 
-			if natGateway.Model == nil {
-				return fmt.Errorf("retrieving %s: `model` was nil", natGatewayId)
-			}
-			if natGateway.Model.Properties == nil {
-				return fmt.Errorf("retrieving %s: `properties` was nil", natGatewayId)
-			}
-
 			id := commonids.NewCompositeResourceID(natGatewayId, publicIpAddressId)
 
-			publicIpAddressesV6 := make([]natgateways.SubResource, 0)
-			if natGateway.Model.Properties.PublicIPAddressesV6 != nil {
-				for _, existingPublicIPAddress := range *natGateway.Model.Properties.PublicIPAddressesV6 {
-					if existingPublicIPAddress.Id == nil {
-						continue
+			if model := natGateway.Model; model != nil {
+				if props := model.Properties; props != nil {
+					publicIpAddressesV6 := make([]natgateways.SubResource, 0)
+
+					if publicIPAddressesV6 := props.PublicIPAddressesV6; publicIPAddressesV6 != nil {
+						for _, existingPublicIPAddress := range *publicIPAddressesV6 {
+							if existingPublicIPAddress.Id == nil {
+								continue
+							}
+
+							if strings.EqualFold(*existingPublicIPAddress.Id, publicIpAddressId.ID()) {
+								return metadata.ResourceRequiresImport(r.ResourceType(), id)
+							}
+
+							publicIpAddressesV6 = append(publicIpAddressesV6, existingPublicIPAddress)
+						}
 					}
 
-					if strings.EqualFold(*existingPublicIPAddress.Id, publicIpAddressId.ID()) {
-						return metadata.ResourceRequiresImport(r.ResourceType(), id)
-					}
-
-					publicIpAddressesV6 = append(publicIpAddressesV6, existingPublicIPAddress)
+					publicIpAddressesV6 = append(publicIpAddressesV6, natgateways.SubResource{
+						Id: pointer.To(state.PublicIpAddressId),
+					})
+					props.PublicIPAddressesV6 = pointer.To(publicIpAddressesV6)
 				}
 			}
-
-			publicIpAddressesV6 = append(publicIpAddressesV6, natgateways.SubResource{
-				Id: pointer.To(state.PublicIpAddressId),
-			})
-			natGateway.Model.Properties.PublicIPAddressesV6 = pointer.To(publicIpAddressesV6)
 
 			if err := client.CreateOrUpdateThenPoll(ctx, *natGatewayId, *natGateway.Model); err != nil {
 				return fmt.Errorf("updating %s: %+v", natGatewayId, err)
@@ -124,12 +123,12 @@ func (r NatGatewayPublicIpV6AssociationResource) Create() sdk.ResourceFunc {
 			metadata.SetID(id)
 			return nil
 		},
-		Timeout: 30 * time.Minute,
 	}
 }
 
 func (r NatGatewayPublicIpV6AssociationResource) Read() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
+		Timeout: 5 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
 			client := metadata.Client.Network.NatGateways
 
@@ -146,115 +145,96 @@ func (r NatGatewayPublicIpV6AssociationResource) Read() sdk.ResourceFunc {
 				return fmt.Errorf("retrieving %s: %+v", *id.First, err)
 			}
 
-			if natGateway.Model == nil || natGateway.Model.Properties == nil {
-				return metadata.MarkAsGone(id)
-			}
-
-			if natGateway.Model.Properties.PublicIPAddressesV6 == nil {
-				return metadata.MarkAsGone(id)
-			}
-
-			publicIPAddressFound := false
-			for _, pip := range *natGateway.Model.Properties.PublicIPAddressesV6 {
-				if pip.Id == nil {
-					continue
-				}
-
-				if strings.EqualFold(*pip.Id, id.Second.ID()) {
-					publicIPAddressFound = true
-					break
-				}
-			}
-
-			if !publicIPAddressFound {
-				return metadata.MarkAsGone(id)
-			}
-
 			state := NatGatewayPublicIpV6AssociationModel{
 				NatGatewayId:      id.First.ID(),
 				PublicIpAddressId: id.Second.ID(),
 			}
 
+			if model := natGateway.Model; model != nil {
+				if props := model.Properties; props != nil {
+					if props.PublicIPAddressesV6 == nil {
+						return metadata.MarkAsGone(id)
+					}
+
+					publicIPAddressFound := false
+					for _, pip := range *props.PublicIPAddressesV6 {
+						if pip.Id == nil {
+							continue
+						}
+
+						if strings.EqualFold(*pip.Id, id.Second.ID()) {
+							publicIPAddressFound = true
+							break
+						}
+					}
+
+					if !publicIPAddressFound {
+						return metadata.MarkAsGone(id)
+					}
+				}
+			}
+
 			return metadata.Encode(&state)
 		},
-		Timeout: 5 * time.Minute,
 	}
 }
 
 func (r NatGatewayPublicIpV6AssociationResource) Delete() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
+		Timeout: 30 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
 			client := metadata.Client.Network.NatGateways
 
-			var state NatGatewayPublicIpV6AssociationModel
-			if err := metadata.Decode(&state); err != nil {
-				return err
-			}
-
-			publicIpAddressId, err := commonids.ParsePublicIPAddressID(state.PublicIpAddressId)
+			id, err := commonids.ParseCompositeResourceID(metadata.ResourceData.Id(), &natgateways.NatGatewayId{}, &commonids.PublicIPAddressId{})
 			if err != nil {
 				return err
 			}
 
-			natGatewayId, err := natgateways.ParseNatGatewayID(state.NatGatewayId)
-			if err != nil {
-				return err
-			}
+			locks.ByName(id.First.NatGatewayName, natGatewayResourceName)
+			defer locks.UnlockByName(id.First.NatGatewayName, natGatewayResourceName)
 
-			locks.ByName(natGatewayId.NatGatewayName, natGatewayResourceName)
-			defer locks.UnlockByName(natGatewayId.NatGatewayName, natGatewayResourceName)
-
-			natGateway, err := client.Get(ctx, *natGatewayId, natgateways.DefaultGetOperationOptions())
+			natGateway, err := client.Get(ctx, *id.First, natgateways.DefaultGetOperationOptions())
 			if err != nil {
 				if response.WasNotFound(natGateway.HttpResponse) {
-					return fmt.Errorf("%s was not found", natGatewayId)
+					return fmt.Errorf("%s was not found", id.First)
 				}
-				return fmt.Errorf("retrieving %s: %+v", natGatewayId, err)
+				return fmt.Errorf("retrieving %s: %+v", id.First, err)
 			}
 
-			if natGateway.Model == nil {
-				return fmt.Errorf("retrieving %s: `model` was nil", natGatewayId)
-			}
-			if natGateway.Model.Properties == nil {
-				return fmt.Errorf("retrieving %s: `properties` was nil", natGatewayId)
-			}
+			if model := natGateway.Model; model != nil {
+				if props := model.Properties; props != nil {
+					publicIpAddressesV6 := make([]natgateways.SubResource, 0)
 
-			publicIpAddressesV6 := make([]natgateways.SubResource, 0)
-			if publicIPAddresses := natGateway.Model.Properties.PublicIPAddressesV6; publicIPAddresses != nil {
-				for _, publicIPAddress := range *publicIPAddresses {
-					if publicIPAddress.Id == nil {
-						continue
+					if publicIPAddresses := props.PublicIPAddressesV6; publicIPAddresses != nil {
+						for _, publicIPAddress := range *publicIPAddresses {
+							if publicIPAddress.Id == nil {
+								continue
+							}
+
+							if !strings.EqualFold(*publicIPAddress.Id, id.Second.ID()) {
+								publicIpAddressesV6 = append(publicIpAddressesV6, publicIPAddress)
+							}
+						}
 					}
-
-					if !strings.EqualFold(*publicIPAddress.Id, publicIpAddressId.ID()) {
-						publicIpAddressesV6 = append(publicIpAddressesV6, publicIPAddress)
-					}
+					props.PublicIPAddressesV6 = pointer.To(publicIpAddressesV6)
 				}
 			}
-			natGateway.Model.Properties.PublicIPAddressesV6 = pointer.To(publicIpAddressesV6)
 
-			if err := client.CreateOrUpdateThenPoll(ctx, *natGatewayId, *natGateway.Model); err != nil {
-				return fmt.Errorf("removing association between %s and %s: %+v", natGatewayId, publicIpAddressId, err)
+			if err := client.CreateOrUpdateThenPoll(ctx, *id.First, *natGateway.Model); err != nil {
+				return fmt.Errorf("removing association between %s and %s: %+v", id.First, id.Second, err)
 			}
 
 			return nil
 		},
-		Timeout: 30 * time.Minute,
 	}
 }
 
 func (r NatGatewayPublicIpV6AssociationResource) IDValidationFunc() pluginsdk.SchemaValidateFunc {
 	return func(input interface{}, key string) (warnings []string, errors []error) {
-		v, ok := input.(string)
-		if !ok {
-			errors = append(errors, fmt.Errorf("expected %q to be a string", key))
-			return
-		}
-
-		if _, err := commonids.ParseCompositeResourceID(v, &natgateways.NatGatewayId{}, &commonids.PublicIPAddressId{}); err != nil {
+		_, err := commonids.ParseCompositeResourceID(input.(string), &natgateways.NatGatewayId{}, &commonids.PublicIPAddressId{})
+		if err != nil {
 			errors = append(errors, err)
 		}
-
-		return
+		return warnings, errors
 	}
 }
