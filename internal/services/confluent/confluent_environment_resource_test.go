80af761419abf4df2dc9f4e5422f48c36ecfc0b9
Vladimir Lazarenko <favoretti@gmail.com>
2025-12-09 15:27:46 +0100

    fmt, terrafmt, document-fix

--- internal/services/confluent/confluent_environment_resource_test.go
+++ internal/services/confluent/confluent_environment_resource_test.go
@@ -8,13 +8,13 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/confluent/2024-07-01/scenvironmentrecords"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
-	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 )
 
 type ConfluentEnvironmentResource struct{}
