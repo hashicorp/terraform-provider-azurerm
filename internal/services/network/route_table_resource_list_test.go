881a74936112db3fd7b9190000b7016a79fdbc53
jackofallops <11830746+jackofallops@users.noreply.github.com>
2025-12-10 15:45:48 +0100

    `dependencies` - update `terraform-plugin-testing` to `v1.14.0` (#31334)
    [ENHANCEMENT] * `dependencies` - update `terraform-plugin-testing` to `v1.14.0`

--- internal/services/network/route_table_resource_list_test.go
+++ internal/services/network/route_table_resource_list_test.go
@@ -29,12 +29,12 @@ func TestAccRouteTable_list_basic(t *testing.T) {
 			{
 				Query:             true,
 				Config:            r.basicQuery(),
-				ConfigQueryChecks: []querycheck.QueryCheck{}, // TODO
+				QueryResultChecks: []querycheck.QueryResultCheck{}, // TODO
 			},
 			{
 				Query:             true,
 				Config:            r.basicQueryByResourceGroupName(data),
-				ConfigQueryChecks: []querycheck.QueryCheck{}, // TODO
+				QueryResultChecks: []querycheck.QueryResultCheck{}, // TODO
 			},
 		},
 	})
