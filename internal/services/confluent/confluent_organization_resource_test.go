966ae3b705879a5c954a80858fdf4058e7b6a07a
Vladimir Lazarenko <favoretti@gmail.com>
2025-12-10 14:02:47 +0100

    utils.Bool vs poinetr.To

--- internal/services/confluent/confluent_organization_resource_test.go
+++ internal/services/confluent/confluent_organization_resource_test.go
@@ -8,13 +8,13 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/confluent/2024-07-01/organizationresources"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
-	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
 type ConfluentOrganizationResource struct{}
@@ -95,12 +95,12 @@ func (r ConfluentOrganizationResource) Exists(ctx context.Context, client *clien
 	resp, err := client.Confluent.OrganizationResourcesClient.OrganizationGet(ctx, *id)
 	if err != nil {
 		if response.WasNotFound(resp.HttpResponse) {
-			return utils.Bool(false), nil
+			return pointer.To(false), nil
 		}
 		return nil, fmt.Errorf("retrieving %s: %+v", *id, err)
 	}
 
-	return utils.Bool(true), nil
+	return pointer.To(true), nil
 }
 
 func (r ConfluentOrganizationResource) template(data acceptance.TestData) string {
