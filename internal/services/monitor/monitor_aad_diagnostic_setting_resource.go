938a3ee83388077e5a1e2c628af9b47e09e5c587
teowa <104055472+teowa@users.noreply.github.com>
2025-12-24 06:35:01 +0000

    prelong timeout

--- internal/services/monitor/monitor_aad_diagnostic_setting_resource.go
+++ internal/services/monitor/monitor_aad_diagnostic_setting_resource.go
@@ -39,10 +39,10 @@ func resourceMonitorAADDiagnosticSetting() *pluginsdk.Resource {
 		}),
 
 		Timeouts: &pluginsdk.ResourceTimeout{
-			Create: pluginsdk.DefaultTimeout(5 * time.Minute),
+			Create: pluginsdk.DefaultTimeout(30 * time.Minute),
 			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
-			Update: pluginsdk.DefaultTimeout(5 * time.Minute),
-			Delete: pluginsdk.DefaultTimeout(5 * time.Minute),
+			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
 		},
 
 		Schema: map[string]*pluginsdk.Schema{
