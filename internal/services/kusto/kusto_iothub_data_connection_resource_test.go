634c13efab4d0c36c8cae30c68c58057a1e4cb3f
Yun Liu (from Dev Box) <yunliu1@microsoft.com>
2025-12-25 16:46:21 +0800

    test: remove retrieval_start_date check from Kusto IoT Hub Data Connection complete test

--- internal/services/kusto/kusto_iothub_data_connection_resource_test.go
+++ internal/services/kusto/kusto_iothub_data_connection_resource_test.go
@@ -42,7 +42,6 @@ func TestAccKustoIotHubDataConnection_complete(t *testing.T) {
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 				check.That(data.ResourceName).Key("database_routing_type").HasValue("Multi"),
-				check.That(data.ResourceName).Key("retrieval_start_date").HasValue("2023-06-26T12:00:00Z"),
 			),
 		},
 		data.ImportStep(),
