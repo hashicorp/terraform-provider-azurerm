58bd7d8d85df6e019403932a8f679511291b36b2
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2025-12-31 23:24:47 +0800

    Run `make terrafmt`

--- internal/services/network/application_gateway_resource_test.go
+++ internal/services/network/application_gateway_resource_test.go
@@ -3596,7 +3596,7 @@ resource "azurerm_application_gateway" "test" {
   backend_address_pool {
     name = local.backend_address_pool_name
   }
-  
+
   backend_http_settings {
     name                  = local.http_setting_name
     cookie_based_affinity = "Disabled"
