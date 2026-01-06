8e72447c17103c8119046dbeba7c30ebe230a63a
Yun Liu <17947406+liuwuliuyun@users.noreply.github.com>
2026-01-07 05:39:13 +0800

    `azurerm_kusto_eventhub_data_connection` - support for`retrieval_start_date` (#31445)
    [ENHANCEMENT] * `azurerm_kusto_eventhub_data_connection` - support for`retrieval_start_date`

--- internal/services/kusto/kusto_eventhub_data_connection_resource_test.go
+++ internal/services/kusto/kusto_eventhub_data_connection_resource_test.go
@@ -123,6 +123,35 @@ func TestAccKustoEventHubDataConnection_databaseRoutingType(t *testing.T) {
 	})
 }
 
+func TestAccKustoEventHubDataConnection_update(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_kusto_eventhub_data_connection", "test")
+	r := KustoEventHubDataConnectionResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.withRetrievalStartDate(data, "2023-06-26T12:00:00Z"),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.withRetrievalStartDate(data, "2024-01-15T08:30:00Z"),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func (KustoEventHubDataConnectionResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	id, err := dataconnections.ParseDataConnectionID(state.ID)
 	if err != nil {
@@ -295,6 +324,24 @@ resource "azurerm_kusto_eventhub_data_connection" "test" {
   database_routing_type = "Multi"
 }
 `, r.template(data), data.RandomInteger)
+}
+
+func (r KustoEventHubDataConnectionResource) withRetrievalStartDate(data acceptance.TestData, retrievalStartDate string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_kusto_eventhub_data_connection" "test" {
+  name                = "acctestkedc-%d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  cluster_name        = azurerm_kusto_cluster.test.name
+  database_name       = azurerm_kusto_database.test.name
+
+  eventhub_id          = azurerm_eventhub.test.id
+  consumer_group       = azurerm_eventhub_consumer_group.test.name
+  retrieval_start_date = "%s"
+}
+`, r.template(data), data.RandomInteger, retrievalStartDate)
 }
 
 func (KustoEventHubDataConnectionResource) template(data acceptance.TestData) string {
