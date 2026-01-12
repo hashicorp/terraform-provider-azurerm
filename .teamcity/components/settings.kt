9c950ed83e69ff707878c23a0680c27dccf92198
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2026-01-12 14:02:34 -0700

    testing - bump Terraform core version to latest stable

--- .teamcity/components/settings.kt
+++ .teamcity/components/settings.kt
@@ -13,7 +13,7 @@ var defaultParallelism = 20
 var defaultTimeout = 12
 
 // specifies the default version of Terraform Core which should be used for testing
-var defaultTerraformCoreVersion = "1.12.2"
+var defaultTerraformCoreVersion = "1.14.3"
 
 // This represents a cron view of days of the week, Monday - Friday.
 const val defaultDaysOfWeek = "2,3,4,5,6"
