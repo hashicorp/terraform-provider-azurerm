62d617808f01410d544b5c7b492414974d04e780
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2026-01-14 10:02:05 -0700

    dependencies: update `go-azure-sdk` to `v0.20260114.1164646`

--- internal/services/batch/batch_pool_resource_test.go
+++ internal/services/batch/batch_pool_resource_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2024-07-01/pool"
+	pool "github.com/hashicorp/go-azure-sdk/resource-manager/batch/2024-07-01/pools"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
