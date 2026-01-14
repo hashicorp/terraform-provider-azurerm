1bdd8b81afe5710bad61b2634f7bea93e56e2299
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-14 15:44:13 +0800

    Draft

--- internal/services/databricks/databricks_workspace_resource_test.go
+++ internal/services/databricks/databricks_workspace_resource_test.go
@@ -3231,7 +3231,12 @@ resource "azurerm_databricks_workspace" "test" {
 func (DatabricksWorkspaceResource) serverlessComplete(data acceptance.TestData, databricksPrincipalID string) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy       = false
+      purge_soft_deleted_keys_on_destroy = false
+    }
+  }
 }
 
 resource "azurerm_resource_group" "test" {
