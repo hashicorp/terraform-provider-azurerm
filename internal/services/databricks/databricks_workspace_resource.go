9c2708edbeb4be27f190cdffbccda9b739040303
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-14 13:50:19 +0800

    Draft

--- internal/services/databricks/databricks_workspace_resource.go
+++ internal/services/databricks/databricks_workspace_resource.go
@@ -792,6 +792,7 @@ func resourceDatabricksWorkspaceCreate(d *pluginsdk.ResourceData, meta interface
 	workspace.Properties.EnhancedSecurityCompliance = expandWorkspaceEnhancedSecurity(enhancedSecurityCompliance.([]interface{}))
 
 	if err := client.CreateOrUpdateThenPoll(ctx, id, workspace); err != nil {
+		panic("debug2")
 		return fmt.Errorf("creating %s: %+v", id, err)
 	}
 
