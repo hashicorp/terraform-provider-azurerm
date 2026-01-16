a20d021dac06590e573bf9f4d8ec29ecc7a2a1bf
magodo <wztwcy@gmail.com>
2026-01-16 15:35:38 +1100

    ContainerRegistry Service: Upgrade API version from 2025-04-01 to 2025-11-01

--- internal/services/containers/container_registry_resource_test.go
+++ internal/services/containers/container_registry_resource_test.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/containerregistry/2025-04-01/registries"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/containerregistry/2025-11-01/registries"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
