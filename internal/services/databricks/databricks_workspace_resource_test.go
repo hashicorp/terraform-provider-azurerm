eec0a26059b12a87d2958e3a4bb955b0ab732cfe
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-16 08:36:11 +0800

    Draft

--- internal/services/databricks/databricks_workspace_resource_test.go
+++ internal/services/databricks/databricks_workspace_resource_test.go
@@ -3345,7 +3345,7 @@ resource "azurerm_key_vault_key" "test" {
 }
 
 resource "azurerm_databricks_access_connector" "test" {
-  name                = "acctestDBWACC%[3]d"
+  name                = "acctestDBWACC%[1]d"
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
 
