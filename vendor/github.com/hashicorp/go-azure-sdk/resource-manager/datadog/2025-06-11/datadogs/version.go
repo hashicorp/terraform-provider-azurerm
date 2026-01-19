26df2eac8fca8a328f8285da88310cea315b4919
JT <jiaweitao@microsoft.com>
2026-01-19 13:48:26 +0800

    datadog: Bump API version to 2025-06-11

---
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/datadog/2025-06-11/datadogs/version.go
@@ -0,0 +1,10 @@
+package datadogs
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+const defaultApiVersion = "2025-06-11"
+
+func userAgent() string {
+	return "hashicorp/go-azure-sdk/datadogs/2025-06-11"
+}
