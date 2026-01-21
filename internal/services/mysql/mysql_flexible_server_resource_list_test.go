24e3b3dd892fda825aa4675045e982423896b3a9
Todd Giguere <todd.giguere@ibm.com>
2026-01-21 10:27:38 -0500

    refactor: simplify test and reduce resources created

--- internal/services/mysql/mysql_flexible_server_resource_list_test.go
+++ internal/services/mysql/mysql_flexible_server_resource_list_test.go
@@ -19,8 +19,7 @@ import (
 )
 
 func TestAccMySqlFlexibleServer_list_no_config(t *testing.T) {
-	data1 := acceptance.BuildTestData(t, "azurerm_mysql_flexible_server", "testlist1")
-	data2 := acceptance.BuildTestData(t, "azurerm_mysql_flexible_server", "testlist2")
+	data := acceptance.BuildTestData(t, "azurerm_mysql_flexible_server", "testlist1")
 	r := MySqlFlexibleServerResource{}
 
 	resource.Test(t, resource.TestCase{
@@ -30,26 +29,19 @@ func TestAccMySqlFlexibleServer_list_no_config(t *testing.T) {
 		ProtoV5ProviderFactories: framework.ProtoV5ProviderFactoriesInit(context.Background(), "azurerm"),
 		Steps: []resource.TestStep{
 			{
-				Config: r.basicListResources(data1, data2), // provision first server + RG
+				Config: r.basic(data),
 			},
 			{
 				Query:  true,
 				Config: r.basicListQuery(),
 				QueryResultChecks: []querycheck.QueryResultCheck{
-					querycheck.ExpectLengthAtLeast("azurerm_mysql_flexible_server.list", 2), // expect at least the 2 we created
+					querycheck.ExpectLengthAtLeast("azurerm_mysql_flexible_server.list", 1), // expect at least the 1 we created
 					querycheck.ExpectIdentity(
 						"azurerm_mysql_flexible_server.list",
 						map[string]knownvalue.Check{
-							"name":                knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data1.RandomInteger))),
-							"resource_group_name": knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data1.RandomInteger))),
-							"subscription_id":     knownvalue.StringExact(data1.Subscriptions.Primary)},
-					),
-					querycheck.ExpectIdentity(
-						"azurerm_mysql_flexible_server.list",
-						map[string]knownvalue.Check{
-							"name":                knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data2.RandomInteger))),
-							"resource_group_name": knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data2.RandomInteger))),
-							"subscription_id":     knownvalue.StringExact(data2.Subscriptions.Primary)},
+							"name":                knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data.RandomInteger))),
+							"resource_group_name": knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data.RandomInteger))),
+							"subscription_id":     knownvalue.StringExact(data.Subscriptions.Primary)},
 					),
 				},
 			},
@@ -58,8 +50,7 @@ func TestAccMySqlFlexibleServer_list_no_config(t *testing.T) {
 }
 
 func TestAccMySqlFlexibleServer_list_by_resource_group(t *testing.T) {
-	data1 := acceptance.BuildTestData(t, "azurerm_mysql_flexible_server", "testlist1")
-	data2 := acceptance.BuildTestData(t, "azurerm_mysql_flexible_server", "testlist2")
+	data := acceptance.BuildTestData(t, "azurerm_mysql_flexible_server", "testlist")
 	r := MySqlFlexibleServerResource{}
 
 	resource.Test(t, resource.TestCase{
@@ -69,60 +60,24 @@ func TestAccMySqlFlexibleServer_list_by_resource_group(t *testing.T) {
 		ProtoV5ProviderFactories: framework.ProtoV5ProviderFactoriesInit(context.Background(), "azurerm"),
 		Steps: []resource.TestStep{
 			{
-				Config: r.basicListResources(data1, data2), // provision first server + RG
+				Config: r.basic(data),
 			},
 			{
 				Query:  true,
-				Config: r.basicListQueryByResourceGroupName(data1),
+				Config: r.basicListQueryByResourceGroupName(data),
 				QueryResultChecks: []querycheck.QueryResultCheck{
 					querycheck.ExpectLength("azurerm_mysql_flexible_server.list", 1), // only 1 should be returned
 					querycheck.ExpectIdentity(
 						"azurerm_mysql_flexible_server.list",
 						map[string]knownvalue.Check{
-							"name":                knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data1.RandomInteger))),
-							"resource_group_name": knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data1.RandomInteger))),
-							"subscription_id":     knownvalue.StringExact(data1.Subscriptions.Primary)},
+							"name":                knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data.RandomInteger))),
+							"resource_group_name": knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data.RandomInteger))),
+							"subscription_id":     knownvalue.StringExact(data.Subscriptions.Primary)},
 					),
 				},
 			},
 		},
 	})
-}
-
-func (r MySqlFlexibleServerResource) basicListResources(data1 acceptance.TestData, data2 acceptance.TestData) string {
-	return fmt.Sprintf(`
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "test1" {
-  name     = "acctestRG-mysql-%d"
-  location = "%s"
-}
-
-resource "azurerm_resource_group" "test2" {
-  name     = "acctestRG-mysql-%d"
-  location = "%s"
-}
-
-resource "azurerm_mysql_flexible_server" "test1" {
-  name                   = "acctest-fs-%d-1"
-  resource_group_name    = azurerm_resource_group.test1.name
-  location               = azurerm_resource_group.test1.location
-  administrator_login    = "_admin_Terraform_892123456789312"
-  administrator_password = "QAZwsx123"
-  sku_name               = "B_Standard_B1ms"
-}
-
-resource "azurerm_mysql_flexible_server" "test2" {
-  name                   = "acctest-fs-%d-2"
-  resource_group_name    = azurerm_resource_group.test2.name
-  location               = azurerm_resource_group.test2.location
-  administrator_login    = "_admin_Terraform_892123456789312"
-  administrator_password = "QAZwsx456"
-  sku_name               = "B_Standard_B1ms"
-}
-`, data1.RandomInteger, data1.Locations.Ternary, data2.RandomInteger, data2.Locations.Ternary, data1.RandomInteger, data2.RandomInteger)
 }
 
 func (r MySqlFlexibleServerResource) basicListQuery() string {
