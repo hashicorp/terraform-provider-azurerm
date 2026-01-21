0a665c210fb83f1c32508747a3d67428704c2c71
aayushsingh2502 <aayush.singh@hashicorp.com>
2026-01-21 12:51:37 +0530

    branch create

--- internal/services/network/private_endpoint_resource_test.go
+++ internal/services/network/private_endpoint_resource_test.go
@@ -400,7 +400,7 @@ resource "azurerm_virtual_network" "test" {
 
 resource "azurerm_subnet" "service" {
   name                 = "acctestsnetservice-%d"
-  resource_group_name  = azurerm_resource_group.test.name
+  resource_group_name  = azurerm_resource_group.test.name 
   virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes     = ["10.5.1.0/24"]
 
