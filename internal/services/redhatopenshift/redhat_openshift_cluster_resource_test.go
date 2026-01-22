b112db8d2b99ca9567806ceefde2b15506d587ef
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-22 17:01:23 +0800

    Draft

--- internal/services/redhatopenshift/redhat_openshift_cluster_resource_test.go
+++ internal/services/redhatopenshift/redhat_openshift_cluster_resource_test.go
@@ -214,7 +214,7 @@ resource "azurerm_redhat_openshift_cluster" "test" {
 
   cluster_profile {
     domain  = "aro-%[3]s.com"
-    version = "4.18.0"
+    version = "4.17.27"
   }
 
   network_profile {
