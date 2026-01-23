bc9aaac3ccaf0602acaaef74d3acb221f66233f0
Mark Stroobach <37002354+mstroob@users.noreply.github.com>
2026-01-23 11:20:01 -0500

    Update internal/services/network/vpn_server_configuration_resource.go
    Co-authored-by: sreallymatt <106555974+sreallymatt@users.noreply.github.com>

--- internal/services/network/vpn_server_configuration_resource.go
+++ internal/services/network/vpn_server_configuration_resource.go
@@ -872,7 +872,8 @@ func flattenVpnServerConfigurationRadius(input *virtualwans.VpnServerConfigurati
 
 			servers = append(servers, map[string]interface{}{
 				"address": v.RadiusServerAddress,
-				"secret":  pointer.To(setSecret),
+				// setting this because the azure api does not return the secret, so need to read it in from the config
+				"secret":  d.Get(fmt.Sprintf("radius.0.server.%d.secret", i)),
 				"score":   pointer.From(v.RadiusServerScore),
 			})
 		}
