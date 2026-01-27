17fb6c67345cb8aa17ecf00e38c13051406320e2
ziyeqf <51212351+ziyeqf@users.noreply.github.com>
2026-01-27 00:11:22 +0000

    upgrade `virtualmachinescalesets` API verison from `2024-11-01` to `2025-04-01`

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2025-04-01/virtualmachinescalesets/model_vmscalesetscaleoutinputproperties.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2025-04-01/virtualmachinescalesets/model_vmscalesetscaleoutinputproperties.go
@@ -3,7 +3,6 @@ package virtualmachinescalesets
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-type AdditionalCapabilities struct {
-	HibernationEnabled *bool `json:"hibernationEnabled,omitempty"`
-	UltraSSDEnabled    *bool `json:"ultraSSDEnabled,omitempty"`
+type VMScaleSetScaleOutInputProperties struct {
+	Zone *string `json:"zone,omitempty"`
 }
