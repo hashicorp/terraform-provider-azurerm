e1fc62be99b50de221bea9aa4e00cae5a9f307dd
Todd Giguere <todd.giguere@ibm.com>
2026-01-27 17:25:42 -0500

    test: add mysql firewall rule list

---
+++ internal/services/mysql/mysql_flexible_server_firewall_rule_resource_list_test.go
@@ -0,0 +1,70 @@
+package mysql_test
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/querycheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/provider/framework"
+)
+
+func TestAccMySqlFlexibleFirewallRule_list_by_resource_group(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_mysql_flexible_server_firewall_rule", "testlist")
+	r := MysqlFlexibleServerFirewallRuleResource{}
+
+	resource.Test(t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		ProtoV5ProviderFactories: framework.ProtoV5ProviderFactoriesInit(context.Background(), "azurerm"),
+		Steps: []resource.TestStep{
+			{
+				Config: r.basic(data),
+			},
+			{
+				Query:  true,
+				Config: r.basicListQueryByResourceGroupName(data),
+				QueryResultChecks: []querycheck.QueryResultCheck{
+					// mysql will have several admin db by default, so there will be more than just the one provisioned
+					querycheck.ExpectLength("azurerm_mysql_flexible_server_firewall_rule.list", 1),
+					querycheck.ExpectIdentity(
+						"azurerm_mysql_flexible_server_firewall_rule.list",
+						map[string]knownvalue.Check{
+							"name":                 knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data.RandomInteger))),
+							"resource_group_name":  knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data.RandomInteger))),
+							"flexible_server_name": knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data.RandomInteger))),
+							"subscription_id":      knownvalue.StringExact(data.Subscriptions.Primary),
+						},
+					),
+				},
+			},
+		},
+	})
+}
+
+func (r MysqlFlexibleServerFirewallRuleResource) basicListQueryByResourceGroupName(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+list "azurerm_mysql_flexible_server" "list" {
+  provider = azurerm
+  include_resource = true
+  config {
+	subscription_id     = "%s"
+	resource_group_name = "acctestRG-%d"
+  }
+}
+
+list "azurerm_mysql_flexible_server_firewall_rule" "list" {
+  provider = azurerm
+  config {
+    flexible_server_id = list.azurerm_mysql_flexible_server.list.data[0].state.id
+  }
+}
+`, data.Subscriptions.Primary, data.RandomInteger)
+}
