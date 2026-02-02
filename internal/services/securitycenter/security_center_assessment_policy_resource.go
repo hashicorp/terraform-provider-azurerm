c47736c7184dd544d6361e03a2b6f12d0006c4b3
ziyeqf <51212351+ziyeqf@users.noreply.github.com>
2026-02-02 01:49:13 +0000

    ci

--- internal/services/securitycenter/security_center_assessment_policy_resource.go
+++ internal/services/securitycenter/security_center_assessment_policy_resource.go
@@ -16,7 +16,6 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/suppress"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
