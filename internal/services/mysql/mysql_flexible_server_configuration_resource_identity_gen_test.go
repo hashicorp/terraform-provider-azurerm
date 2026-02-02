065ffe49b6f47c38241ca8304e83c94fdaa362a9
Todd Giguere <todd.giguere@ibm.com>
2026-02-02 09:05:57 -0500

    test: iden test regen after template changes

--- internal/services/mysql/mysql_flexible_server_configuration_resource_identity_gen_test.go
+++ internal/services/mysql/mysql_flexible_server_configuration_resource_identity_gen_test.go
@@ -10,16 +10,25 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	customstatecheck "github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/statecheck"
 )
 
 func TestAccMysqlFlexibleServerConfiguration_resourceIdentity(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_mysql_flexible_server_configuration", "test")
 	r := MysqlFlexibleServerConfigurationResource{}
 
+	checkedFields := map[string]struct{}{
+		"subscription_id":      {},
+		"flexible_server_name": {},
+		"name":                 {},
+		"resource_group_name":  {},
+	}
+
 	data.ResourceIdentityTest(t, []acceptance.TestStep{
 		{
 			Config: r.characterSetServer(data),
 			ConfigStateChecks: []statecheck.StateCheck{
+				customstatecheck.ExpectAllIdentityFieldsAreChecked("azurerm_mysql_flexible_server_configuration.test", checkedFields),
 				statecheck.ExpectIdentityValue("azurerm_mysql_flexible_server_configuration.test", tfjsonpath.New("subscription_id"), knownvalue.StringExact(data.Subscriptions.Primary)),
 				statecheck.ExpectIdentityValueMatchesStateAtPath("azurerm_mysql_flexible_server_configuration.test", tfjsonpath.New("flexible_server_name"), tfjsonpath.New("server_name")),
 				statecheck.ExpectIdentityValueMatchesStateAtPath("azurerm_mysql_flexible_server_configuration.test", tfjsonpath.New("name"), tfjsonpath.New("name")),
