5eaaed17d27516157b93452c046ca9da755c338d
anurag-hashicorp <anurag.upadhaya@hashicorp.com>
2026-02-02 10:26:53 +0530

    Regenerate identity test with updated template

--- internal/services/workloads/workloads_sap_three_tier_virtual_instance_resource_identity_gen_test.go
+++ internal/services/workloads/workloads_sap_three_tier_virtual_instance_resource_identity_gen_test.go
@@ -10,16 +10,24 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	customstatecheck "github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/statecheck"
 )
 
 func TestAccWorkloadsSAPThreeTierVirtualInstance_resourceIdentity(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_workloads_sap_three_tier_virtual_instance", "test")
 	r := WorkloadsSAPThreeTierVirtualInstanceResource{}
 
+	checkedFields := map[string]struct{}{
+		"subscription_id":     {},
+		"name":                {},
+		"resource_group_name": {},
+	}
+
 	data.ResourceIdentityTest(t, []acceptance.TestStep{
 		{
 			Config: r.basic(data, 10+(data.RandomInteger%90)),
 			ConfigStateChecks: []statecheck.StateCheck{
+				customstatecheck.ExpectAllIdentityFieldsAreChecked("azurerm_workloads_sap_three_tier_virtual_instance.test", checkedFields),
 				statecheck.ExpectIdentityValue("azurerm_workloads_sap_three_tier_virtual_instance.test", tfjsonpath.New("subscription_id"), knownvalue.StringExact(data.Subscriptions.Primary)),
 				statecheck.ExpectIdentityValueMatchesStateAtPath("azurerm_workloads_sap_three_tier_virtual_instance.test", tfjsonpath.New("name"), tfjsonpath.New("name")),
 				statecheck.ExpectIdentityValueMatchesStateAtPath("azurerm_workloads_sap_three_tier_virtual_instance.test", tfjsonpath.New("resource_group_name"), tfjsonpath.New("resource_group_name")),
