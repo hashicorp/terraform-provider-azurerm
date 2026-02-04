c438c9b4fc027ebcab460b8b7d942f00c10f393c
Gerry Tan <2316198+gerrytan@users.noreply.github.com>
2026-02-04 17:53:20 +1100

    Use sdk.EncodeListResult

--- internal/services/cognitive/cognitive_account_resource_list.go
+++ internal/services/cognitive/cognitive_account_resource_list.go
@@ -92,31 +92,7 @@ func (r CognitiveAccountListResource) List(ctx context.Context, request list.Lis
 				return
 			}
 
-			tfTypeIdentity, err := rd.TfTypeIdentityState()
-			if err != nil {
-				sdk.SetListIteratorErrorDiagnostic(result, push, "converting Identity State", err)
-				return
-			}
-
-			if err := result.Identity.Set(ctx, *tfTypeIdentity); err != nil {
-				sdk.SetListIteratorErrorDiagnostic(result, push, "setting Identity Data", err)
-				return
-			}
-
-			tfTypeResourceState, err := rd.TfTypeResourceState()
-			if err != nil {
-				sdk.SetListIteratorErrorDiagnostic(result, push, "converting Resource State", err)
-				return
-			}
-
-			if err := result.Resource.Set(ctx, *tfTypeResourceState); err != nil {
-				sdk.SetListIteratorErrorDiagnostic(result, push, "setting Resource Data", err)
-				return
-			}
-
-			if !push(result) {
-				return
-			}
+			sdk.EncodeListResult(ctx, rd, result, push)
 		}
 	}
 }
