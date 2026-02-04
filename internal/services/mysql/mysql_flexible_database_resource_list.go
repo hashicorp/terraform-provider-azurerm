7caa6b0db1bfd18427b65afb9a82e355e2ecee74
Todd Giguere <todd.giguere@ibm.com>
2026-02-04 11:24:14 -0500

    refactor: replace list encode with new func

--- internal/services/mysql/mysql_flexible_database_resource_list.go
+++ internal/services/mysql/mysql_flexible_database_resource_list.go
@@ -92,31 +92,7 @@ func (r MysqlFlexibleDatabaseListResource) List(ctx context.Context, request lis
 				return
 			}
 
-			tfTypeIdentity, err := rd.TfTypeIdentityState()
-			if err != nil {
-				sdk.SetListIteratorErrorDiagnostic(result, push, "converting Identity State", err)
-				return
-			}
-
-			if err := result.Identity.Set(ctx, *tfTypeIdentity); err != nil {
-				sdk.SetListIteratorErrorDiagnostic(result, push, "setting Identity Data", err)
-				return
-			}
-
-			tfTypeResourceState, err := rd.TfTypeResourceState()
-			if err != nil {
-				sdk.SetListIteratorErrorDiagnostic(result, push, "converting Resource State", err)
-				return
-			}
-
-			if err := result.Resource.Set(ctx, *tfTypeResourceState); err != nil {
-				sdk.SetListIteratorErrorDiagnostic(result, push, "setting Resource Data", err)
-				return
-			}
-
-			if !push(result) {
-				return
-			}
+			sdk.EncodeListResult(ctx, rd, result, push)
 		}
 	}
 }
