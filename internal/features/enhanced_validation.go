39f508db209ee017a380b802e317462402f79867
kt <kt@katbyte.me>
2026-02-04 12:18:37 -0800

    disabled ARM_PROVIDER_ENHANCED_VALIDATION in 5.0

--- internal/features/enhanced_validation.go
+++ internal/features/enhanced_validation.go
@@ -11,14 +11,17 @@ import (
 // EnhancedValidationEnabled returns whether the feature for Enhanced Validation is enabled.
 //
 // This functionality calls out to the Azure MetaData Service to cache the list of supported
-// Azure Locations for the specified Endpoint - and then uses that to provide enhanced validation
+// Azure Locations and Resource Providers for the specified Endpoint - and then uses that to
+// provide enhanced validation. When enabled, invalid locations or resource providers are caught
+// at `terraform plan` time. When disabled, these errors are caught at `terraform apply` time
+// when Azure rejects the request.
 //
-// This is enabled by default as of version 2.20 of the Azure Provider, and can be disabled by
-// setting the Environment Variable `ARM_PROVIDER_ENHANCED_VALIDATION` to `false`.
+// This is disabled by default as of version 5.0 of the Azure Provider, and can be enabled by
+// setting the Environment Variable `ARM_PROVIDER_ENHANCED_VALIDATION` to `true`.
 func EnhancedValidationEnabled() bool {
 	value := os.Getenv("ARM_PROVIDER_ENHANCED_VALIDATION")
 	if value == "" {
-		return true
+		return false
 	}
 
 	return strings.EqualFold(value, "true")
