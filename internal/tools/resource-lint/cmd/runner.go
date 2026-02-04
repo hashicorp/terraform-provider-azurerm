45214867cd02e776da970917c2fe0ce964d7635f
Lucia Lu <qixialu@hotmail.com>
2026-02-03 21:15:46 -0800

    Revert "Remove tests which loads same pkg for at least twice"
    This reverts commit 224f3a5e598eb1dc465d69e92bedbcea697060f9.

--- internal/tools/resource-lint/cmd/runner.go
+++ internal/tools/resource-lint/cmd/runner.go
@@ -77,6 +77,7 @@ func (r *Runner) Run(ctx context.Context) ExitCode {
 	log.Printf("Loading packages...")
 	cfg := &packages.Config{
 		Mode:  packages.LoadAllSyntax,
+		Tests: true,
 	}
 	pkgs, err := packages.Load(cfg, patterns...)
 	if err != nil {
