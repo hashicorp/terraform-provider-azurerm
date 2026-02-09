2fda07b88f00e7b8d0f74e3598488cd4fd8ebdf1
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2026-02-09 14:08:21 -0700

    tests - fix `TestAccMonitorDiagnosticSetting_partnerSolution` by randomizing email address

--- internal/services/monitor/monitor_diagnostic_setting_resource_test.go
+++ internal/services/monitor/monitor_diagnostic_setting_resource_test.go
@@ -760,7 +760,7 @@ resource "azurerm_elastic_cloud_elasticsearch" "test" {
   resource_group_name         = azurerm_resource_group.test.name
   location                    = azurerm_resource_group.test.location
   sku_name                    = "ess-consumption-2024_Monthly"
-  elastic_cloud_email_address = "user@example.com"
+  elastic_cloud_email_address = "user-%[1]d@example.com"
 }
 
 resource "azurerm_monitor_diagnostic_setting" "test" {
