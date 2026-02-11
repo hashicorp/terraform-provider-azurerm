58046bb7d168167e932ee502c4f370f3c6c2e36b
Xu Wu <xuwu1@microsoft.com>
2026-02-12 02:15:26 +1100

    Resource List: `azurerm_service_plan` (#31610)
    [FEATURE] * **New List Resource**: `azurerm_service_plan`

---
+++ internal/services/appservice/service_plan_resource_list_test.go
@@ -0,0 +1,103 @@
+package appservice_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/querycheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/provider/framework"
+)
+
+func TestAccServicePlan_list_basic(t *testing.T) {
+	r := ServicePlanResource{}
+	listResourceAddress := "azurerm_service_plan.list"
+
+	data := acceptance.BuildTestData(t, "azurerm_service_plan", "test1")
+
+	resource.Test(t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		ProtoV5ProviderFactories: framework.ProtoV5ProviderFactoriesInit(context.Background(), "azurerm"),
+		Steps: []resource.TestStep{
+			{
+				Config: r.basicList(data),
+			},
+			{
+				Query:  true,
+				Config: r.basicQuery(),
+				QueryResultChecks: []querycheck.QueryResultCheck{
+					querycheck.ExpectLengthAtLeast(listResourceAddress, 3),
+				},
+			},
+			{
+				Query:  true,
+				Config: r.basicQueryByResourceGroupName(data),
+				QueryResultChecks: []querycheck.QueryResultCheck{
+					querycheck.ExpectLength(listResourceAddress, 3),
+				},
+			},
+		},
+	})
+}
+
+func (r ServicePlanResource) basicList(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-appserviceplan-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_service_plan" "test1" {
+  name                = "acctest-1-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  sku_name            = "B1"
+  os_type             = "Windows"
+}
+
+resource "azurerm_service_plan" "test2" {
+  name                = "acctest-2-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  sku_name            = "B1"
+  os_type             = "Windows"
+}
+
+resource "azurerm_service_plan" "test3" {
+  name                = "acctest-3-%[1]d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  sku_name            = "B1"
+  os_type             = "Windows"
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (r ServicePlanResource) basicQuery() string {
+	return `
+list "azurerm_service_plan" "list" {
+  provider = azurerm
+  config {}
+}
+`
+}
+
+func (r ServicePlanResource) basicQueryByResourceGroupName(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+list "azurerm_service_plan" "list" {
+  provider = azurerm
+  config {
+    resource_group_name = "acctestRG-appserviceplan-%[1]d"
+  }
+}
+`, data.RandomInteger)
+}
