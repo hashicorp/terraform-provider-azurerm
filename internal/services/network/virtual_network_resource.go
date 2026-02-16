3258ae176aa00e8760dc58e0fa2450adbe140db7
Vibhu <vibhuanand@outlook.com>
2026-02-16 16:01:25 -0500

    network: gofmt

--- internal/services/network/virtual_network_resource.go
+++ internal/services/network/virtual_network_resource.go
@@ -88,7 +88,7 @@ func resourceVirtualNetworkSchema() map[string]*pluginsdk.Schema {
 			DiffSuppressFunc: func(_, old, new string, d *schema.ResourceData) bool {
 				// If `ip_address_pool` is used instead of `address_space` there is a perpetual diff
 				// due to the API returning a CIDR range provisioned by the IP Address Management Pool.
-				// Note: using `GetRawConfig` to avoid suppressing a diff if a user updates from `ip_address_pool` to `address_space`.				
+				// Note: using `GetRawConfig` to avoid suppressing a diff if a user updates from `ip_address_pool` to `address_space`.
 				rawConfig := d.GetRawConfig()
 				if rawConfig.IsNull() {
 					return false
