d8e398030a7a48c52ec308231cb9cc583b9d2b08
Alex Karpenko <alex@Mac.lan>
2026-02-17 23:31:24 +0000

    azurerm_virtual_network_gateway: expand unit coverage for IP configuration flattening

--- internal/services/network/virtual_network_gateway_resource_unit_test.go
+++ internal/services/network/virtual_network_gateway_resource_unit_test.go
@@ -4,6 +4,7 @@
 package network
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
@@ -18,40 +19,112 @@ func TestVirtualNetworkGatewayResource_NoCustomizeDiff(t *testing.T) {
 	}
 }
 
-func TestFlattenVirtualNetworkGatewayIPConfigurations_IncludesPublicIPAddress(t *testing.T) {
-	input := &[]virtualnetworkgateways.VirtualNetworkGatewayIPConfiguration{
+func TestFlattenVirtualNetworkGatewayIPConfigurations(t *testing.T) {
+	subnetID := "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.Network/virtualNetworks/vnet/subnets/GatewaySubnet"
+	publicIPID := "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.Network/publicIPAddresses/pip"
+
+	tests := []struct {
+		name  string
+		input *[]virtualnetworkgateways.VirtualNetworkGatewayIPConfiguration
+		want  []map[string]interface{}
+	}{
 		{
-			Name: pointer.To("vnetGatewayConfig"),
-			Properties: &virtualnetworkgateways.VirtualNetworkGatewayIPConfigurationPropertiesFormat{
-				PrivateIPAllocationMethod: pointer.To(virtualnetworkgateways.IPAllocationMethodDynamic),
-				Subnet: &virtualnetworkgateways.SubResource{
-					Id: pointer.To("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.Network/virtualNetworks/vnet/subnets/GatewaySubnet"),
+			name:  "nil input returns empty output",
+			input: nil,
+			want:  []map[string]interface{}{},
+		},
+		{
+			name:  "empty input returns empty output",
+			input: &[]virtualnetworkgateways.VirtualNetworkGatewayIPConfiguration{},
+			want:  []map[string]interface{}{},
+		},
+		{
+			name: "includes public ip address id",
+			input: &[]virtualnetworkgateways.VirtualNetworkGatewayIPConfiguration{
+				{
+					Name: pointer.To("vnetGatewayConfig"),
+					Properties: &virtualnetworkgateways.VirtualNetworkGatewayIPConfigurationPropertiesFormat{
+						PrivateIPAllocationMethod: pointer.To(virtualnetworkgateways.IPAllocationMethodDynamic),
+						Subnet: &virtualnetworkgateways.SubResource{
+							Id: pointer.To(subnetID),
+						},
+						PublicIPAddress: &virtualnetworkgateways.SubResource{
+							Id: pointer.To(publicIPID),
+						},
+					},
 				},
-				PublicIPAddress: &virtualnetworkgateways.SubResource{
-					Id: pointer.To("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.Network/publicIPAddresses/pip"),
+			},
+			want: []map[string]interface{}{
+				{
+					"name":                          "vnetGatewayConfig",
+					"private_ip_address_allocation": "Dynamic",
+					"subnet_id":                     subnetID,
+					"public_ip_address_id":          publicIPID,
+				},
+			},
+		},
+		{
+			name: "omits optional fields when ids and name are absent",
+			input: &[]virtualnetworkgateways.VirtualNetworkGatewayIPConfiguration{
+				{
+					Properties: &virtualnetworkgateways.VirtualNetworkGatewayIPConfigurationPropertiesFormat{
+						PrivateIPAllocationMethod: pointer.To(virtualnetworkgateways.IPAllocationMethodStatic),
+					},
+				},
+			},
+			want: []map[string]interface{}{
+				{
+					"private_ip_address_allocation": "Static",
+				},
+			},
+		},
+		{
+			name: "preserves order for multiple configurations",
+			input: &[]virtualnetworkgateways.VirtualNetworkGatewayIPConfiguration{
+				{
+					Name: pointer.To("first"),
+					Properties: &virtualnetworkgateways.VirtualNetworkGatewayIPConfigurationPropertiesFormat{
+						PrivateIPAllocationMethod: pointer.To(virtualnetworkgateways.IPAllocationMethodDynamic),
+					},
+				},
+				{
+					Name: pointer.To("second"),
+					Properties: &virtualnetworkgateways.VirtualNetworkGatewayIPConfigurationPropertiesFormat{
+						PrivateIPAllocationMethod: pointer.To(virtualnetworkgateways.IPAllocationMethodStatic),
+					},
+				},
+			},
+			want: []map[string]interface{}{
+				{
+					"name":                          "first",
+					"private_ip_address_allocation": "Dynamic",
+				},
+				{
+					"name":                          "second",
+					"private_ip_address_allocation": "Static",
 				},
 			},
 		},
 	}
 
-	output := flattenVirtualNetworkGatewayIPConfigurations(input)
-	if len(output) != 1 {
-		t.Fatalf("expected one flattened ip_configuration but got %d", len(output))
-	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			gotRaw := flattenVirtualNetworkGatewayIPConfigurations(tt.input)
+			got := make([]map[string]interface{}, 0, len(gotRaw))
 
-	config, ok := output[0].(map[string]interface{})
-	if !ok {
-		t.Fatalf("expected flattened item to be a map[string]interface{}")
-	}
+			for i, item := range gotRaw {
+				cfg, ok := item.(map[string]interface{})
+				if !ok {
+					t.Fatalf("expected flattened item at index %d to be a map[string]interface{}", i)
+				}
 
-	publicIPID, ok := config["public_ip_address_id"].(string)
-	if !ok {
-		t.Fatalf("expected public_ip_address_id to be present in flattened output")
-	}
+				got = append(got, cfg)
+			}
 
-	expectedPublicIPID := "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.Network/publicIPAddresses/pip"
-	if publicIPID != expectedPublicIPID {
-		t.Fatalf("expected public_ip_address_id %q but got %q", expectedPublicIPID, publicIPID)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("unexpected flattened output: got %#v, want %#v", got, tt.want)
+			}
+		})
 	}
 }
-
