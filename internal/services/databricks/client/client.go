d692b828ebad31963ded3ab201127a96faf12399
v-yhyeo0202 <v-yyeo@microsoft.com>
2026-02-19 13:10:43 +0800

    Databricks - bump all Databricks 2022-10-01-preview and 2024-05-01 API to 2026-01-01

--- internal/services/databricks/client/client.go
+++ internal/services/databricks/client/client.go
@@ -6,8 +6,8 @@ package client
 import (
 	"fmt"
 
-	"github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2022-10-01-preview/accessconnector"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2024-05-01/vnetpeering"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2026-01-01/accessconnector"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2026-01-01/vnetpeering"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2026-01-01/workspaces"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2026-01-01/accessconnector/version.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2026-01-01/accessconnector/version.go
@@ -1,10 +1,10 @@
-package workspaces
+package accessconnector
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-const defaultApiVersion = "2022-04-01-preview"
+const defaultApiVersion = "2026-01-01"
 
 func userAgent() string {
-	return "hashicorp/go-azure-sdk/workspaces/2022-04-01-preview"
+	return "hashicorp/go-azure-sdk/accessconnector/2026-01-01"
 }
