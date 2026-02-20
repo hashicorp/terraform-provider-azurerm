b1f33b637516ce976becd32db5bfe33893f8e963
mstroob <37002354+mstroob@users.noreply.github.com>
2026-02-20 13:07:06 -0500

    Remove duplicate MssqlElasticPoolListResource

--- internal/services/mssql/registration.go
+++ internal/services/mssql/registration.go
@@ -107,9 +107,9 @@ func (r Registration) EphemeralResources() []func() ephemeral.EphemeralResource 
 func (r Registration) ListResources() []sdk.FrameworkListWrappedResource {
 	return []sdk.FrameworkListWrappedResource{
 		MssqlDatabaseListResource{},
+		MssqlElasticPoolListResource{},
 		MssqlJobAgentListResource{},
 		MssqlServerListResource{},
 		MssqlVirtualMachineListResource{},
-		MssqlElasticPoolListResource{},
 	}
 }
