a8a326211964e9c7d96b3fe0361b17b4797ea8b0
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2026-02-27 17:55:53 -0700

    `azurerm_data_factory[_customer_managed_key]` - fix parsing error and removal of encryption UAMI (#31858)

--- internal/services/datafactory/data_factory_customer_managed_key_resource.go
+++ internal/services/datafactory/data_factory_customer_managed_key_resource.go
@@ -168,7 +168,7 @@ func (r DataFactoryCustomerManagedKeyResource) Read() sdk.ResourceFunc {
 				customerManagedKeyId = keyId.ID()
 			}
 
-			if encIdentity := encryption.Identity; encIdentity != nil && encIdentity.UserAssignedIdentity != nil {
+			if encIdentity := encryption.Identity; encIdentity != nil && pointer.From(encIdentity.UserAssignedIdentity) != "" {
 				parsed, err := commonids.ParseUserAssignedIdentityIDInsensitively(pointer.From(encIdentity.UserAssignedIdentity))
 				if err != nil {
 					return fmt.Errorf("parsing %q: %+v", *encIdentity.UserAssignedIdentity, err)
@@ -229,9 +229,7 @@ func (r DataFactoryCustomerManagedKeyResource) Update() sdk.ResourceFunc {
 			}
 
 			if metadata.ResourceData.HasChange("user_assigned_identity_id") {
-				encryption.Identity = &factories.CMKIdentityDefinition{
-					UserAssignedIdentity: pointer.To(customerManagedKey.UserAssignedIdentityID),
-				}
+				encryption.Identity = expandDataFactoryEncryptionIdentity(customerManagedKey.UserAssignedIdentityID)
 			}
 
 			payload.Properties.Encryption = encryption
