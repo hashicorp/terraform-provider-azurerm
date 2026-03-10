ec72fecfe5db871a8128c8895b205424ff58bab2
v-yhyeo0202 <v-yyeo@microsoft.com>
2026-03-10 16:42:26 +0800

    `azurerm_databricks_workspace_serverless` - fix `TestAccDatabricksWorkspaceServerless_privateLink`

--- internal/services/databricks/databricks_workspace_serverless_resource_test.go
+++ internal/services/databricks/databricks_workspace_serverless_resource_test.go
@@ -179,7 +179,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "test" {
-  name     = "acctest-rg-databricks-%d"
+  name     = "acctest-rg-dbws-%d"
   location = "%s"
 }
 `, data.RandomInteger, data.Locations.Primary)
@@ -312,74 +312,15 @@ resource "azurerm_virtual_network" "test" {
   name                = "acctest-vnet-%[2]d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/24"]
 }
 
-resource "azurerm_subnet" "public" {
-  name                 = "acctest-sn-public-%[2]d"
-  resource_group_name  = azurerm_resource_group.test.name
-  virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-  delegation {
-    name = "acctest"
-
-    service_delegation {
-      name = "Microsoft.Databricks/workspaces"
-
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/join/action",
-        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
-        "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action",
-      ]
-    }
-  }
-}
-
-resource "azurerm_subnet" "private" {
-  name                 = "acctest-sn-private-%[2]d"
-  resource_group_name  = azurerm_resource_group.test.name
-  virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["10.0.2.0/24"]
-
-  delegation {
-    name = "acctest"
-
-    service_delegation {
-      name = "Microsoft.Databricks/workspaces"
-
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/join/action",
-        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
-        "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action",
-      ]
-    }
-  }
-}
-
-resource "azurerm_subnet" "privatelink" {
-  name                 = "acctest-snpl-%[2]d"
-  resource_group_name  = azurerm_resource_group.test.name
-  virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["10.0.3.0/24"]
-
+resource "azurerm_subnet" "test" {
+  name                              = "acctest-snet-%[2]d"
+  resource_group_name               = azurerm_resource_group.test.name
+  virtual_network_name              = azurerm_virtual_network.test.name
+  address_prefixes                  = ["10.0.0.0/28"]
   private_endpoint_network_policies = "Enabled"
-}
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "acctest-nsg-%[2]d"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-}
-
-resource "azurerm_subnet_network_security_group_association" "public" {
-  subnet_id                 = azurerm_subnet.public.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-resource "azurerm_subnet_network_security_group_association" "private" {
-  subnet_id                 = azurerm_subnet.private.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_databricks_workspace_serverless" "test" {
@@ -387,18 +328,31 @@ resource "azurerm_databricks_workspace_serverless" "test" {
   resource_group_name           = azurerm_resource_group.test.name
   location                      = azurerm_resource_group.test.location
   public_network_access_enabled = false
-
-  tags = {
-    Environment = "Sandbox"
-    Label       = "Test"
-  }
 }
 
-resource "azurerm_private_endpoint" "databricks" {
-  name                = "acctest-endpoint-%[2]d"
-  location            = azurerm_resource_group.test.location
+resource "azurerm_private_dns_zone" "test" {
+  name                = "privatelink.azuredatabricks.net"
   resource_group_name = azurerm_resource_group.test.name
-  subnet_id           = azurerm_subnet.privatelink.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "test" {
+  name                  = "acctest-pdzvnl-%[2]d"
+  resource_group_name   = azurerm_resource_group.test.name
+  private_dns_zone_name = azurerm_private_dns_zone.test.name
+  virtual_network_id    = azurerm_virtual_network.test.id
+}
+
+resource "azurerm_private_endpoint" "test" {
+  name                          = "acctest-pe-%[2]d"
+  location                      = azurerm_resource_group.test.location
+  resource_group_name           = azurerm_resource_group.test.name
+  subnet_id                     = azurerm_subnet.test.id
+  custom_network_interface_name = "acctest-nic-%[2]d"
+
+  private_dns_zone_group {
+    name                 = "acctest-pdzg-%[2]d"
+    private_dns_zone_ids = [azurerm_private_dns_zone.test.id]
+  }
 
   private_service_connection {
     name                           = "acctest-psc-%[2]d"
@@ -408,19 +362,26 @@ resource "azurerm_private_endpoint" "databricks" {
   }
 }
 
-resource "azurerm_private_dns_zone" "test" {
-  name                = "privatelink.azuredatabricks.net"
-  resource_group_name = azurerm_resource_group.test.name
+resource "azurerm_private_endpoint" "test2" {
+  name                          = "acctest-pe2-%[2]d"
+  location                      = azurerm_resource_group.test.location
+  resource_group_name           = azurerm_resource_group.test.name
+  subnet_id                     = azurerm_subnet.test.id
+  custom_network_interface_name = "acctest-nic2-%[2]d"
 
-  depends_on = [azurerm_private_endpoint.databricks]
-}
+  private_dns_zone_group {
+    name                 = "acctest-pdzg2-%[2]d"
+    private_dns_zone_ids = [azurerm_private_dns_zone.test.id]
+  }
 
-resource "azurerm_private_dns_cname_record" "test" {
-  name                = azurerm_databricks_workspace_serverless.test.workspace_url
-  zone_name           = azurerm_private_dns_zone.test.name
-  resource_group_name = azurerm_resource_group.test.name
-  ttl                 = 300
-  record              = "eastus2-c2.azuredatabricks.net"
+  private_service_connection {
+    name                           = "acctest-psc2-%[2]d"
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_databricks_workspace_serverless.test.id
+    subresource_names              = ["browser_authentication"]
+  }
+
+  depends_on = [azurerm_private_endpoint.test]
 }
 `, r.template(data), data.RandomInteger)
 }
