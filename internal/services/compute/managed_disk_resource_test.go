4ec9ae977431bd454177eb11664abde707cb89b5
Lucia Lu <qixialu@hotmail.com>
2026-03-14 01:40:38 +1100

    Update test name

--- internal/services/compute/managed_disk_resource_test.go
+++ internal/services/compute/managed_disk_resource_test.go
@@ -2413,7 +2413,7 @@ resource "azurerm_disk_access" "test" {
 }
 
 resource "azurerm_disk_access" "test2" {
-  name                = "accda2%d"
+  name                = "acctest2%d"
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
 
