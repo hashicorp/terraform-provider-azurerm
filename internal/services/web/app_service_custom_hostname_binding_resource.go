da0d1f10684282d4810fb1edacee9d89aa033430
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2026-03-18 11:41:17 -0600

    `azurerm_app_service_custom_hostname_binding` - migrate to `go-azure-sdk`

--- internal/services/web/app_service_custom_hostname_binding_resource.go
+++ internal/services/web/app_service_custom_hostname_binding_resource.go
@@ -5,23 +5,22 @@ package web
 
 import (
 	"fmt"
-	"log"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/services/web/mgmt/2021-02-01/web" // nolint: staticcheck
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
+	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/web/2023-12-01/webapps"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/locks"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/services/web/parse"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/web/migration"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
-	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
-var appServiceCustomHostnameBindingResourceName = "azurerm_app_service_custom_hostname_binding"
+const appServiceCustomHostnameBindingResourceName = "azurerm_app_service_custom_hostname_binding"
 
 func resourceAppServiceCustomHostnameBinding() *pluginsdk.Resource {
 	return &pluginsdk.Resource{
@@ -29,8 +28,13 @@ func resourceAppServiceCustomHostnameBinding() *pluginsdk.Resource {
 		Read:   resourceAppServiceCustomHostnameBindingRead,
 		Delete: resourceAppServiceCustomHostnameBindingDelete,
 		Importer: pluginsdk.ImporterValidatingResourceId(func(id string) error {
-			_, err := parse.AppServiceCustomHostnameBindingID(id)
+			_, err := webapps.ParseHostNameBindingID(id)
 			return err
+		}),
+
+		SchemaVersion: 1,
+		StateUpgraders: pluginsdk.StateUpgrades(map[int]pluginsdk.StateUpgrade{
+			0: migration.AppServiceCustomHostnameBindingV0ToV1{},
 		}),
 
 		Timeouts: &pluginsdk.ResourceTimeout{
@@ -60,8 +64,8 @@ func resourceAppServiceCustomHostnameBinding() *pluginsdk.Resource {
 				Computed: true,
 				ForceNew: true,
 				ValidateFunc: validation.StringInSlice([]string{
-					string(web.SslStateIPBasedEnabled),
-					string(web.SslStateSniEnabled),
+					string(webapps.SslStateIPBasedEnabled),
+					string(webapps.SslStateSniEnabled),
 				}, false),
 			},
 
@@ -82,46 +86,38 @@ func resourceAppServiceCustomHostnameBinding() *pluginsdk.Resource {
 }
 
 func resourceAppServiceCustomHostnameBindingCreate(d *pluginsdk.ResourceData, meta interface{}) error {
-	client := meta.(*clients.Client).Web.AppServicesClientV1
+	client := meta.(*clients.Client).Web.WebAppsClient
+
 	ctx, cancel := timeouts.ForCreate(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
-	log.Printf("[INFO] preparing arguments for App Service Hostname Binding creation.")
+	id := webapps.NewHostNameBindingID(meta.(*clients.Client).Account.SubscriptionId, d.Get("resource_group_name").(string), d.Get("app_service_name").(string), d.Get("hostname").(string))
 
-	resourceGroup := d.Get("resource_group_name").(string)
-	appServiceName := d.Get("app_service_name").(string)
-	hostname := d.Get("hostname").(string)
-	sslState := d.Get("ssl_state").(string)
-	thumbprint := d.Get("thumbprint").(string)
+	locks.ByName(id.HostNameBindingName, appServiceCustomHostnameBindingResourceName)
+	defer locks.UnlockByName(id.HostNameBindingName, appServiceCustomHostnameBindingResourceName)
 
-	locks.ByName(appServiceName, appServiceCustomHostnameBindingResourceName)
-	defer locks.UnlockByName(appServiceName, appServiceCustomHostnameBindingResourceName)
-
-	if d.IsNewResource() {
-		existing, err := client.GetHostNameBinding(ctx, resourceGroup, appServiceName, hostname)
+	existing, err := client.GetHostNameBinding(ctx, id)
+	if !response.WasNotFound(existing.HttpResponse) {
 		if err != nil {
-			if !utils.ResponseWasNotFound(existing.Response) {
-				return fmt.Errorf("checking for presence of existing Custom Hostname Binding %q (App Service %q / Resource Group %q): %s", hostname, appServiceName, resourceGroup, err)
-			}
+			return fmt.Errorf("checking for presence of existing %s: %w", id, err)
 		}
-
-		if !utils.ResponseWasNotFound(existing.Response) {
-			return tf.ImportAsExistsError("azurerm_app_service_custom_hostname_binding", *existing.ID)
-		}
+		return tf.ImportAsExistsError("azurerm_app_service_custom_hostname_binding", id.ID())
 	}
 
-	properties := web.HostNameBinding{
-		HostNameBindingProperties: &web.HostNameBindingProperties{
-			SiteName: pointer.To(appServiceName),
+	payload := webapps.HostNameBinding{
+		Properties: &webapps.HostNameBindingProperties{
+			SiteName: pointer.To(id.SiteName),
 		},
 	}
 
+	sslState := d.Get("ssl_state").(string)
+	thumbprint := d.Get("thumbprint").(string)
 	if sslState != "" {
 		if thumbprint == "" {
 			return fmt.Errorf("`thumbprint` must be specified when `ssl_state` is set")
 		}
 
-		properties.SslState = web.SslState(sslState)
+		payload.Properties.SslState = pointer.ToEnum[webapps.SslState](sslState)
 	}
 
 	if thumbprint != "" {
@@ -129,78 +125,71 @@ func resourceAppServiceCustomHostnameBindingCreate(d *pluginsdk.ResourceData, me
 			return fmt.Errorf("`ssl_state` must be specified when `thumbprint` is set")
 		}
 
-		properties.Thumbprint = pointer.To(thumbprint)
+		payload.Properties.Thumbprint = pointer.To(thumbprint)
 	}
 
-	if _, err := client.CreateOrUpdateHostNameBinding(ctx, resourceGroup, appServiceName, hostname, properties); err != nil {
-		return fmt.Errorf("creating/updating Custom Hostname Binding %q (App Service %q / Resource Group %q): %+v", hostname, appServiceName, resourceGroup, err)
+	if _, err := client.CreateOrUpdateHostNameBinding(ctx, id, payload); err != nil {
+		return fmt.Errorf("creating %s: %+v", id, err)
 	}
 
-	read, err := client.GetHostNameBinding(ctx, resourceGroup, appServiceName, hostname)
-	if err != nil {
-		return fmt.Errorf("retrieving Custom Hostname Binding %q (App Service %q / Resource Group %q): %+v", hostname, appServiceName, resourceGroup, err)
-	}
-	if read.ID == nil {
-		return fmt.Errorf("cannot read Hostname Binding %q (App Service %q / Resource Group %q) ID", hostname, appServiceName, resourceGroup)
-	}
-
-	d.SetId(*read.ID)
+	d.SetId(id.ID())
 
 	return resourceAppServiceCustomHostnameBindingRead(d, meta)
 }
 
 func resourceAppServiceCustomHostnameBindingRead(d *pluginsdk.ResourceData, meta interface{}) error {
-	client := meta.(*clients.Client).Web.AppServicesClientV1
+	client := meta.(*clients.Client).Web.WebAppsClient
+
 	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
-	id, err := parse.AppServiceCustomHostnameBindingID(d.Id())
+	id, err := webapps.ParseHostNameBindingID(d.Id())
 	if err != nil {
 		return err
 	}
 
-	resp, err := client.GetHostNameBinding(ctx, id.ResourceGroup, id.AppServiceName, id.Name)
+	resp, err := client.GetHostNameBinding(ctx, *id)
 	if err != nil {
-		if utils.ResponseWasNotFound(resp.Response) {
-			log.Printf("[DEBUG] App Service Hostname Binding %q (App Service %q / Resource Group %q) was not found - removing from state", id.Name, id.AppServiceName, id.ResourceGroup)
+		if response.WasNotFound(resp.HttpResponse) {
 			d.SetId("")
 			return nil
 		}
-		return fmt.Errorf("retrieving Custom Hostname Binding %q (App Service %q / Resource Group %q): %+v", id.Name, id.AppServiceName, id.ResourceGroup, err)
+		return fmt.Errorf("retrieving %s: %+v", id, err)
 	}
 
-	d.Set("hostname", id.Name)
-	d.Set("app_service_name", id.AppServiceName)
-	d.Set("resource_group_name", id.ResourceGroup)
+	d.Set("hostname", id.HostNameBindingName)
+	d.Set("app_service_name", id.SiteName)
+	d.Set("resource_group_name", id.ResourceGroupName)
 
-	if props := resp.HostNameBindingProperties; props != nil {
-		d.Set("ssl_state", props.SslState)
-		d.Set("thumbprint", props.Thumbprint)
-		d.Set("virtual_ip", props.VirtualIP)
+	if model := resp.Model; model != nil {
+		if props := model.Properties; props != nil {
+			d.Set("ssl_state", props.SslState)
+			d.Set("thumbprint", props.Thumbprint)
+			d.Set("virtual_ip", props.VirtualIP)
+		}
 	}
 
 	return nil
 }
 
 func resourceAppServiceCustomHostnameBindingDelete(d *pluginsdk.ResourceData, meta interface{}) error {
-	client := meta.(*clients.Client).Web.AppServicesClientV1
+	client := meta.(*clients.Client).Web.WebAppsClient
+
 	ctx, cancel := timeouts.ForDelete(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
-	id, err := parse.AppServiceCustomHostnameBindingID(d.Id())
+	id, err := webapps.ParseHostNameBindingID(d.Id())
 	if err != nil {
 		return err
 	}
 
-	locks.ByName(id.AppServiceName, appServiceCustomHostnameBindingResourceName)
-	defer locks.UnlockByName(id.AppServiceName, appServiceCustomHostnameBindingResourceName)
+	locks.ByName(id.HostNameBindingName, appServiceCustomHostnameBindingResourceName)
+	defer locks.UnlockByName(id.HostNameBindingName, appServiceCustomHostnameBindingResourceName)
 
-	log.Printf("[DEBUG] Deleting App Service Hostname Binding %q (App Service %q / Resource Group %q)", id.Name, id.AppServiceName, id.ResourceGroup)
-
-	resp, err := client.DeleteHostNameBinding(ctx, id.ResourceGroup, id.AppServiceName, id.Name)
+	resp, err := client.DeleteHostNameBinding(ctx, *id)
 	if err != nil {
-		if !utils.ResponseWasNotFound(resp) {
-			return fmt.Errorf("deleting Custom Hostname Binding %q (App Service %q / Resource Group %q): %+v", id.Name, id.AppServiceName, id.ResourceGroup, err)
+		if !response.WasNotFound(resp.HttpResponse) {
+			return fmt.Errorf("deleting %s: %+v", id, err)
 		}
 	}
 
