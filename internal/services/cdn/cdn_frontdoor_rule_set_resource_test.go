912be3c30c72ade047949a8eaeee9e9183fa42a8
Jeffrey Cline <20408400+WodansSon@users.noreply.github.com>
2026-03-19 02:15:23 -0600

    Add tests for CDN Front Door Rule Set with attached/unattached routes

--- internal/services/cdn/cdn_frontdoor_rule_set_resource_test.go
+++ internal/services/cdn/cdn_frontdoor_rule_set_resource_test.go
@@ -18,12 +18,28 @@ import (
 
 type CdnFrontDoorRuleSetResource struct{}
 
-func TestAccCdnFrontDoorRuleSet_basic(t *testing.T) {
+func TestAccCdnFrontDoorRuleSet_basic_unattachedRoute(t *testing.T) {
+	t.Skip(unattachedFrontDoorRuleSetRegressionSkipMessage)
+
 	data := acceptance.BuildTestData(t, "azurerm_cdn_frontdoor_rule_set", "test")
 	r := CdnFrontDoorRuleSetResource{}
 	data.ResourceTest(t, r, []acceptance.TestStep{
 		{
-			Config: r.basic(data),
+			Config: r.basic(data, false),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccCdnFrontDoorRuleSet_basic_attachedRoute(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_cdn_frontdoor_rule_set", "test")
+	r := CdnFrontDoorRuleSetResource{}
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data, true),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 			),
@@ -37,7 +53,7 @@ func TestAccCdnFrontDoorRuleSet_requiresImport(t *testing.T) {
 	r := CdnFrontDoorRuleSetResource{}
 	data.ResourceTest(t, r, []acceptance.TestStep{
 		{
-			Config: r.basic(data),
+			Config: r.basic(data, false),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 			),
@@ -46,12 +62,28 @@ func TestAccCdnFrontDoorRuleSet_requiresImport(t *testing.T) {
 	})
 }
 
-func TestAccCdnFrontDoorRuleSet_complete(t *testing.T) {
+func TestAccCdnFrontDoorRuleSet_complete_unattachedRoute(t *testing.T) {
+	t.Skip(unattachedFrontDoorRuleSetRegressionSkipMessage)
+
 	data := acceptance.BuildTestData(t, "azurerm_cdn_frontdoor_rule_set", "test")
 	r := CdnFrontDoorRuleSetResource{}
 	data.ResourceTest(t, r, []acceptance.TestStep{
 		{
-			Config: r.complete(data),
+			Config: r.complete(data, false),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccCdnFrontDoorRuleSet_complete_attachedRoute(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_cdn_frontdoor_rule_set", "test")
+	r := CdnFrontDoorRuleSetResource{}
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.complete(data, true),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 			),
@@ -75,8 +107,8 @@ func (r CdnFrontDoorRuleSetResource) Exists(ctx context.Context, clients *client
 	return pointer.To(true), nil
 }
 
-func (r CdnFrontDoorRuleSetResource) basic(data acceptance.TestData) string {
-	template := r.template(data)
+func (r CdnFrontDoorRuleSetResource) basic(data acceptance.TestData, attachRoute bool) string {
+	template := r.templateWithAttachedRoute(data, attachRoute)
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {}
@@ -92,7 +124,7 @@ resource "azurerm_cdn_frontdoor_rule_set" "test" {
 }
 
 func (r CdnFrontDoorRuleSetResource) requiresImport(data acceptance.TestData) string {
-	config := r.basic(data)
+	config := r.basic(data, false)
 	return fmt.Sprintf(`
 %s
 
@@ -103,8 +135,8 @@ resource "azurerm_cdn_frontdoor_rule_set" "import" {
 `, config)
 }
 
-func (r CdnFrontDoorRuleSetResource) complete(data acceptance.TestData) string {
-	template := r.template(data)
+func (r CdnFrontDoorRuleSetResource) complete(data acceptance.TestData, attachRoute bool) string {
+	template := r.templateWithAttachedRoute(data, attachRoute)
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {}
@@ -117,6 +149,61 @@ resource "azurerm_cdn_frontdoor_rule_set" "test" {
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.test.id
 }
 `, template, data.RandomIntOfLength(8))
+}
+
+func (r CdnFrontDoorRuleSetResource) templateWithAttachedRoute(data acceptance.TestData, attachRoute bool) string {
+	template := r.template(data)
+	if !attachRoute {
+		return template
+	}
+
+	return fmt.Sprintf(`%s
+
+%s`, template, r.routeTemplate(data))
+}
+
+func (CdnFrontDoorRuleSetResource) routeTemplate(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+resource "azurerm_cdn_frontdoor_origin_group" "test" {
+	name                     = "acctest-fdog-%[1]d"
+	cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.test.id
+
+	load_balancing {
+		additional_latency_in_milliseconds = 0
+		sample_size                        = 16
+		successful_samples_required        = 3
+	}
+}
+
+resource "azurerm_cdn_frontdoor_origin" "test" {
+	name                          = "acctest-fdorigin-%[1]d"
+	cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.test.id
+	enabled                       = true
+
+	certificate_name_check_enabled = false
+	host_name                      = "contoso.com"
+	http_port                      = 80
+	https_port                     = 443
+	origin_host_header             = "www.contoso.com"
+	priority                       = 1
+	weight                         = 1
+}
+
+resource "azurerm_cdn_frontdoor_endpoint" "test" {
+	name                     = "acctest-fdendpoint-%[1]d"
+	cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.test.id
+}
+
+resource "azurerm_cdn_frontdoor_route" "test" {
+	name                          = "acctest-fdroute-%[1]d"
+	cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.test.id
+	cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.test.id
+	cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.test.id]
+	cdn_frontdoor_rule_set_ids    = [azurerm_cdn_frontdoor_rule_set.test.id]
+	patterns_to_match             = ["/*"]
+	supported_protocols           = ["Http", "Https"]
+}
+`, data.RandomInteger)
 }
 
 func (CdnFrontDoorRuleSetResource) template(data acceptance.TestData) string {
