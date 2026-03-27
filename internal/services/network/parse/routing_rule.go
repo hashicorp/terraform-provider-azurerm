2160ab7616a909f7b1e70d39edbca1ca9279a09a
teowa <104055472+teowa@users.noreply.github.com>
2026-03-28 03:36:30 +1100

    `azurerm_application_gateway` - support for `backend`, `listener` and `routing_rule` block (#30376)
    [ENHANCEMENT] * `azurerm_application_gateway` - add support for the `backend`, `listener`, and `routing_rule` arguments

---
+++ internal/services/network/parse/routing_rule.go
@@ -0,0 +1,135 @@
+// Copyright IBM Corp. 2014, 2025
+// SPDX-License-Identifier: MPL-2.0
+
+package parse
+
+// NOTE: this file is generated via 'go:generate' - manual changes will be overwritten
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/resourceids"
+)
+
+type RoutingRuleId struct {
+	SubscriptionId         string
+	ResourceGroup          string
+	ApplicationGatewayName string
+	Name                   string
+}
+
+func NewRoutingRuleID(subscriptionId, resourceGroup, applicationGatewayName, name string) RoutingRuleId {
+	return RoutingRuleId{
+		SubscriptionId:         subscriptionId,
+		ResourceGroup:          resourceGroup,
+		ApplicationGatewayName: applicationGatewayName,
+		Name:                   name,
+	}
+}
+
+func (id RoutingRuleId) String() string {
+	segments := []string{
+		fmt.Sprintf("Name %q", id.Name),
+		fmt.Sprintf("Application Gateway Name %q", id.ApplicationGatewayName),
+		fmt.Sprintf("Resource Group %q", id.ResourceGroup),
+	}
+	segmentsStr := strings.Join(segments, " / ")
+	return fmt.Sprintf("%s: (%s)", "Routing Rule", segmentsStr)
+}
+
+func (id RoutingRuleId) ID() string {
+	fmtString := "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Network/applicationGateways/%s/routingRules/%s"
+	return fmt.Sprintf(fmtString, id.SubscriptionId, id.ResourceGroup, id.ApplicationGatewayName, id.Name)
+}
+
+// RoutingRuleID parses a RoutingRule ID into an RoutingRuleId struct
+func RoutingRuleID(input string) (*RoutingRuleId, error) {
+	id, err := resourceids.ParseAzureResourceID(input)
+	if err != nil {
+		return nil, fmt.Errorf("parsing %q as an RoutingRule ID: %+v", input, err)
+	}
+
+	resourceId := RoutingRuleId{
+		SubscriptionId: id.SubscriptionID,
+		ResourceGroup:  id.ResourceGroup,
+	}
+
+	if resourceId.SubscriptionId == "" {
+		return nil, errors.New("ID was missing the 'subscriptions' element")
+	}
+
+	if resourceId.ResourceGroup == "" {
+		return nil, errors.New("ID was missing the 'resourceGroups' element")
+	}
+
+	if resourceId.ApplicationGatewayName, err = id.PopSegment("applicationGateways"); err != nil {
+		return nil, err
+	}
+	if resourceId.Name, err = id.PopSegment("routingRules"); err != nil {
+		return nil, err
+	}
+
+	if err := id.ValidateNoEmptySegments(input); err != nil {
+		return nil, err
+	}
+
+	return &resourceId, nil
+}
+
+// RoutingRuleIDInsensitively parses an RoutingRule ID into an RoutingRuleId struct, insensitively
+// This should only be used to parse an ID for rewriting, the RoutingRuleID
+// method should be used instead for validation etc.
+//
+// Whilst this may seem strange, this enables Terraform have consistent casing
+// which works around issues in Core, whilst handling broken API responses.
+func RoutingRuleIDInsensitively(input string) (*RoutingRuleId, error) {
+	id, err := resourceids.ParseAzureResourceID(input)
+	if err != nil {
+		return nil, err
+	}
+
+	resourceId := RoutingRuleId{
+		SubscriptionId: id.SubscriptionID,
+		ResourceGroup:  id.ResourceGroup,
+	}
+
+	if resourceId.SubscriptionId == "" {
+		return nil, errors.New("ID was missing the 'subscriptions' element")
+	}
+
+	if resourceId.ResourceGroup == "" {
+		return nil, errors.New("ID was missing the 'resourceGroups' element")
+	}
+
+	// find the correct casing for the 'applicationGateways' segment
+	applicationGatewaysKey := "applicationGateways"
+	for key := range id.Path {
+		if strings.EqualFold(key, applicationGatewaysKey) {
+			applicationGatewaysKey = key
+			break
+		}
+	}
+	if resourceId.ApplicationGatewayName, err = id.PopSegment(applicationGatewaysKey); err != nil {
+		return nil, err
+	}
+
+	// find the correct casing for the 'routingRules' segment
+	routingRulesKey := "routingRules"
+	for key := range id.Path {
+		if strings.EqualFold(key, routingRulesKey) {
+			routingRulesKey = key
+			break
+		}
+	}
+	if resourceId.Name, err = id.PopSegment(routingRulesKey); err != nil {
+		return nil, err
+	}
+
+	if err := id.ValidateNoEmptySegments(input); err != nil {
+		return nil, err
+	}
+
+	return &resourceId, nil
+}
