522e9ac6482986eaff203fdc756a5401b99cbbff
Lingyu Tang <lingyutang@microsoft.com>
2026-04-08 20:43:12 +1000

    format test file, remove extra white space.

--- internal/services/datafactory/data_factory_linked_service_mysql_resource_test.go
+++ internal/services/datafactory/data_factory_linked_service_mysql_resource_test.go
@@ -195,25 +195,25 @@ resource "azurerm_data_factory_linked_service_mysql" "test" {
 func (LinkedServiceMySQLResource) withDriverVersionV1(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
-	features {}
+  features {}
 }
 
 resource "azurerm_resource_group" "test" {
-	name     = "acctestRG-df-%d"
-	location = "%s"
+  name     = "acctestRG-df-%d"
+  location = "%s"
 }
 
 resource "azurerm_data_factory" "test" {
-	name                = "acctestdf%d"
-	location            = azurerm_resource_group.test.location
-	resource_group_name = azurerm_resource_group.test.name
+  name                = "acctestdf%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
 }
 
 resource "azurerm_data_factory_linked_service_mysql" "test" {
-	name              = "acctestlssql%d"
-	data_factory_id   = azurerm_data_factory.test.id
-	connection_string = "Server=test;Port=3306;Database=test;User=test;SSLMode=1;UseSystemTrustStore=0;Password=test"
-	driver_version    = "V1"
+  name              = "acctestlssql%d"
+  data_factory_id   = azurerm_data_factory.test.id
+  connection_string = "Server=test;Port=3306;Database=test;User=test;SSLMode=1;UseSystemTrustStore=0;Password=test"
+  driver_version    = "V1"
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger)
 }
