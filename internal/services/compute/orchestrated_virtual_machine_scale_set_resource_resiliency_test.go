2779753cf6664de94af4a38789c5852d9242d658
Zhenhua Hu <zhhu@microsoft.com>
2026-04-13 16:42:01 +0800

    fix format

--- internal/services/compute/orchestrated_virtual_machine_scale_set_resource_resiliency_test.go
+++ internal/services/compute/orchestrated_virtual_machine_scale_set_resource_resiliency_test.go
@@ -66,7 +66,7 @@ resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
 
-  zones = [1,2]
+  zones                              = [1, 2]
   automatic_zone_rebalancing_enabled = %[5]t
 
   sku_name = "Standard_F2ads_v7"
