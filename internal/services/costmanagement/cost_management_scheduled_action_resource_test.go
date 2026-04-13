b2fafe42aa47b7e0730249c15bfc290533573cc7
magodo <wztwcy@gmail.com>
2026-04-13 13:50:21 +1000

    terrafmt

--- internal/services/costmanagement/cost_management_scheduled_action_resource_test.go
+++ internal/services/costmanagement/cost_management_scheduled_action_resource_test.go
@@ -358,9 +358,9 @@ resource "azurerm_cost_management_scheduled_action" "test" {
 
   view_id = "${data.azurerm_subscription.test.id}/providers/Microsoft.CostManagement/views/ms:CostByService"
 
-  display_name         = "CostByServiceView%s"
-  email_subject        = substr("Cost Management Report for ${data.azurerm_subscription.test.display_name} Subscription", 0, 70)
-  email_addresses      = ["test@test.com", "hashicorp@test.com"]
+  display_name    = "CostByServiceView%s"
+  email_subject   = substr("Cost Management Report for ${data.azurerm_subscription.test.display_name} Subscription", 0, 70)
+  email_addresses = ["test@test.com", "hashicorp@test.com"]
 
   frequency  = "Daily"
   start_date = "%s"
