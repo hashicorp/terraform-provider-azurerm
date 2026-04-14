848b18c83047cf7d151124a24ce85999d0a8bddb
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2026-04-14 08:05:19 -0600

    remove redundant nil check

--- internal/services/automation/automation_runbook_resource.go
+++ internal/services/automation/automation_runbook_resource.go
@@ -429,10 +429,7 @@ func resourceAutomationRunbookRead(d *pluginsdk.ResourceData, meta interface{}) 
 			return fmt.Errorf("retrieving content for Automation Runbook %s: %+v", id, err)
 		}
 	}
-
-	if v := contentResp.Model; v != nil {
-		d.Set("content", *v)
-	}
+	d.Set("content", contentResp.Model)
 
 	jsMap := make(map[uuid.UUID]jobschedule.JobScheduleProperties)
 	automationAccountId := jobschedule.NewAutomationAccountID(id.SubscriptionId, id.ResourceGroupName, id.AutomationAccountName)
