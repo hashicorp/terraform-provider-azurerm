b948bc55dedcfedeb83f9fb8c8111f0710d4b69d
kt <kt@katbyte.me>
2026-05-01 15:15:38 -0700

    chore: remove test files

--- internal/services/resource/fake_thing_resource_list.go
+++
@@ -1,7 +0,0 @@
-// Copyright IBM Corp. 2014, 2025
-// SPDX-License-Identifier: MPL-2.0
-
-package resource
-
-// This file exists only to test the enforce-list-resources CI check.
-// It will be removed before merging.
