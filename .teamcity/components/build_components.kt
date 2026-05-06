515dc9f8648c73f112c86bd637a7b3e6c152b1bf
mstroob <37002354+mstroob@users.noreply.github.com>
2026-05-06 17:03:32 -0400

    ai told me this will work and fix the tests

--- .teamcity/components/build_components.kt
+++ .teamcity/components/build_components.kt
@@ -35,10 +35,10 @@ fun BuildSteps.ConfigureGoEnv() {
         name = "Configure Go Version"
         scriptContent = """
                         BUILD_TIME=$(date +%s)
-                        echo "##teamcity[setParameter name='env.BUILD_START_TIME' value='$BUILD_TIME']"
+                        echo "##teamcity[setParameter name='env.BUILD_START_TIME' value='${'$'}BUILD_TIME']"
 
                         GO_VERSION=$(goenv local)
-                        goenv install -s "$GO_VERSION"
+                        goenv install -s "${'$'}GO_VERSION"
                         goenv rehash
                         """.trimIndent()
     })
