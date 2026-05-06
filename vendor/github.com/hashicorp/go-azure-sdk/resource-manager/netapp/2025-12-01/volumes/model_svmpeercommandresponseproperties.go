6c9e046d7ad19fbeee54f85f61299c32936f12c5
teowa <104055472+teowa@users.noreply.github.com>
2026-05-06 08:36:32 +0000

    update go-azure-sdk to v0.20260504.1170848

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/netapp/2025-12-01/volumes/model_svmpeercommandresponseproperties.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/netapp/2025-12-01/volumes/model_svmpeercommandresponseproperties.go
@@ -1,9 +1,8 @@
-package activitylogs
+package volumes
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-type LocalizableString struct {
-	LocalizedValue *string `json:"localizedValue,omitempty"`
-	Value          string  `json:"value"`
+type SVMPeerCommandResponseProperties struct {
+	SVMPeeringCommand *string `json:"svmPeeringCommand,omitempty"`
 }
