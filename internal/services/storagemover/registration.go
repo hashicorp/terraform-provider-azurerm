6a6bf767684fcb58264ccb429f64a2b3244bfcb8
anurag-hashicorp <anurag.upadhaya@hashicorp.com>
2026-05-07 14:19:05 +0530

    Fix : Lint error

--- internal/services/storagemover/registration.go
+++ internal/services/storagemover/registration.go
@@ -68,7 +68,7 @@ func (r Registration) EphemeralResources() []func() ephemeral.EphemeralResource 
 func (r Registration) ListResources() []sdk.FrameworkListWrappedResource {
 	return []sdk.FrameworkListWrappedResource{
 		StorageMoverAgentListResource{},
-    StorageMoverJobDefinitionListResource{},
+		StorageMoverJobDefinitionListResource{},
 		StorageMoverListResource{},
 		StorageMoverSourceEndpointListResource{},
 	}
