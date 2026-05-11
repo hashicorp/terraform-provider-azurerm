e02ee27d554ae58d7b2c7004cf9388dabb87fc1f
Anurag Upadhaya <anurag.upadhaya@hashicorp.com>
2026-05-11 20:02:48 +0530

    service/storagemover: add list support for `azurerm_storage_mover_job_definition` (#32184)
    Co-authored-by: catriona-m <86247157+catriona-m@users.noreply.github.com>

--- internal/services/storagemover/registration.go
+++ internal/services/storagemover/registration.go
@@ -68,6 +68,7 @@ func (r Registration) EphemeralResources() []func() ephemeral.EphemeralResource 
 func (r Registration) ListResources() []sdk.FrameworkListWrappedResource {
 	return []sdk.FrameworkListWrappedResource{
 		StorageMoverAgentListResource{},
+		StorageMoverJobDefinitionListResource{},
 		StorageMoverListResource{},
 		StorageMoverSourceEndpointListResource{},
 	}
