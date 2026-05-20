54a5347678131e061fa1da81d6445e9abc23ed02
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2026-05-20 07:34:01 -0600

    `azurerm_data_factory_integration_runtime_azure` - add support for `interactive_authoring_time_to_live_in_minutes` (#32316)
    [ENHANCEMENT] * `azurerm_data_factory_integration_runtime_azure` - add support for the `interactive_authoring_time_to_live_in_minutes` property

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/netapp/2025-12-01/netappaccounts/version.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/netapp/2025-12-01/netappaccounts/version.go
@@ -8,3 +8,7 @@ const defaultApiVersion = "2025-12-01"
 func userAgent() string {
 	return "hashicorp/go-azure-sdk/netappaccounts/2025-12-01"
 }
+
+func AzureAPIVersion() string {
+	return defaultApiVersion
+}
