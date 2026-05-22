10c1774e8dcc774b2a96f786eeba050d56122422
Lucia Lu <qixialu@hotmail.com>
2026-05-22 13:15:57 +1000

    Remove unused client

--- internal/services/keyvault/client/client.go
+++ internal/services/keyvault/client/client.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 
 	dataplane7_4 "github.com/hashicorp/go-azure-sdk/data-plane/keyvault/7-4"
-	vaults20230701 "github.com/hashicorp/go-azure-sdk/resource-manager/keyvault/2023-07-01/vaults"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/keyvault/2026-02-01/deletedvaults"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/keyvault/2026-02-01/vaults"
 	resources20151101 "github.com/hashicorp/go-azure-sdk/resource-manager/resources/2015-11-01/resources"
@@ -31,25 +30,9 @@ type Client struct {
 
 	// NOTE: @tombuildsstuff: this client is intentionally internal-only so that it's not used directly
 	resources20151101Client *resources20151101.ResourcesClient
-
-	// @tombuildsstuff: I'm intentionally vendoring this API version separately to take advantage
-	// of the updated List API behaviour/new base layer (since the API now always returns a nextLink)
-	// which `Azure/go-autorest` doesn't handle cleanly. Before migrating the Key Vault resources over
-	// to using this new API Version, there's a few Enum/casing related items to resolve, and as such
-	// that's intentionally split-out into a separate task. For now, please continue to use VaultsClient
-	// for regular operations, and we can remove this internal client one the newer API version is used
-	// across the Provider.
-	vaults20230701Client *vaults20230701.VaultsClient
 }
 
 func NewClient(o *common.ClientOptions) (*Client, error) {
-	// These clients use `hashicorp/go-azure-sdk`
-	updatedVaultsClient, err := vaults20230701.NewVaultsClientWithBaseURI(o.Environment.ResourceManager)
-	if err != nil {
-		return nil, fmt.Errorf("building Vaults client: %+v", err)
-	}
-	o.Configure(updatedVaultsClient.Client, o.Authorizers.ResourceManager)
-
 	resources20151101Client, err := resources20151101.NewResourcesClientWithBaseURI(o.Environment.ResourceManager)
 	if err != nil {
 		return nil, fmt.Errorf("building legacy Resources client: %+v", err)
@@ -87,6 +70,5 @@ func NewClient(o *common.ClientOptions) (*Client, error) {
 
 		// intentionally internal to this package for now, see above.
 		resources20151101Client: resources20151101Client,
-		vaults20230701Client:    updatedVaultsClient,
 	}, nil
 }
