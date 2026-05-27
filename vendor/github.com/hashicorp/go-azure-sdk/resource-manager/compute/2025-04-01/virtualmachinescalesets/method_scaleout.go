906b03799a6e631d9c15b5bc618181e5939a3b6e
ziyeqf <51212351+ziyeqf@users.noreply.github.com>
2026-05-27 05:28:42 +0000

    refresh vendor

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2025-04-01/virtualmachinescalesets/method_scaleout.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2025-04-01/virtualmachinescalesets/method_scaleout.go
@@ -59,11 +59,22 @@ func (c VirtualMachineScaleSetsClient) ScaleOut(ctx context.Context, id VirtualM
 	return
 }
 
-// CancelThenPoll performs Cancel then polls until it's completed
-func (c VirtualMachineScaleSetRollingUpgradesClient) CancelThenPoll(ctx context.Context, id VirtualMachineScaleSetId) error {
-	result, err := c.Cancel(ctx, id)
+// ScaleOutThenPoll performs ScaleOut then polls until it's completed
+func (c VirtualMachineScaleSetsClient) ScaleOutThenPoll(ctx context.Context, id VirtualMachineScaleSetId, input VMScaleSetScaleOutInput) error {
+	return c.ScaleOutCallbackThenPoll(ctx, id, input, nil)
+}
+
+// ScaleOutCallbackThenPoll performs ScaleOut, runs the optional callback function, then polls until it's completed
+func (c VirtualMachineScaleSetsClient) ScaleOutCallbackThenPoll(ctx context.Context, id VirtualMachineScaleSetId, input VMScaleSetScaleOutInput, callback func() error) error {
+	result, err := c.ScaleOut(ctx, id, input)
 	if err != nil {
-		return fmt.Errorf("performing Cancel: %+v", err)
+		return fmt.Errorf("performing ScaleOut: %+v", err)
+	}
+
+	if callback != nil {
+		if err := callback(); err != nil {
+			return fmt.Errorf("executing callback function: %+v", err)
+		}
 	}
 
 	if err := result.Poller.PollUntilDone(ctx); err != nil {
