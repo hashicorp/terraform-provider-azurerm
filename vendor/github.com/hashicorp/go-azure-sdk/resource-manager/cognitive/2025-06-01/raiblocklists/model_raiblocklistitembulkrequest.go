b1bc55cbbbea366b5bfa641f325ca8dfbccc0f11
Xiaxin Yi <xiaxyi@microsoft.com>
2026-06-01 17:00:54 +1000

    update vendor

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/cognitive/2025-06-01/raiblocklists/model_raiblocklistitembulkrequest.go
+++
@@ -1,9 +0,0 @@
-package raiblocklists
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type RaiBlocklistItemBulkRequest struct {
-	Name       *string                     `json:"name,omitempty"`
-	Properties *RaiBlocklistItemProperties `json:"properties,omitempty"`
-}
