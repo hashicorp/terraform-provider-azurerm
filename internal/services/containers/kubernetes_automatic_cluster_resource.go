1fef8dc5dc460fc824759d49b360b1a176cb4e4a
mstroob <37002354+mstroob@users.noreply.github.com>
2026-06-02 14:44:09 -0400

    add changes to support new features

--- internal/services/containers/kubernetes_automatic_cluster_resource.go
+++ internal/services/containers/kubernetes_automatic_cluster_resource.go
@@ -2413,14 +2413,14 @@ func (r KubernetesAutomaticClusterResource) Create() sdk.ResourceFunc {
 
 			id := commonids.NewKubernetesClusterID(subscriptionId, model.ResourceGroupName, model.Name)
 
-			existing, err := client.Get(ctx, id)
-			if err != nil {
-				if !response.WasNotFound(existing.HttpResponse) {
+			if !metadata.Client.Features.SkipImportCheckOnCreateAndAllowOverwritingExistingResources {
+				existing, err := client.Get(ctx, id)
+				if err != nil && !response.WasNotFound(existing.HttpResponse) {
 					return fmt.Errorf("checking for presence of existing %s: %+v", id, err)
 				}
-			}
-			if !response.WasNotFound(existing.HttpResponse) {
-				return metadata.ResourceRequiresImport(r.ResourceType(), id)
+				if !response.WasNotFound(existing.HttpResponse) {
+					return metadata.ResourceRequiresImport(r.ResourceType(), id)
+				}
 			}
 
 			if err := validateKubernetesAutomaticClusterTyped(&model, nil); err != nil {
@@ -2552,7 +2552,7 @@ func (r KubernetesAutomaticClusterResource) Create() sdk.ResourceFunc {
 				parameters.Properties.SupportPlan = pointer.To(managedclusters.KubernetesSupportPlan(model.SupportPlan))
 			}
 
-			err = client.CreateOrUpdateThenPoll(ctx, id, parameters, managedclusters.DefaultCreateOrUpdateOperationOptions())
+			err = client.CreateOrUpdateCallbackThenPoll(ctx, id, parameters, managedclusters.DefaultCreateOrUpdateOperationOptions(), metadata.SetIDCallback(&id))
 			if err != nil {
 				return fmt.Errorf("creating %s: %+v", id, err)
 			}
