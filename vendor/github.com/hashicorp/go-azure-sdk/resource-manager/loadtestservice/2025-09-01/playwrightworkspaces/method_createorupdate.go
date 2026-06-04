cdc55ebddf87ee2b4bf7c56af6586d358ffd942d
v-yhyeo0202 <v-yyeo@microsoft.com>
2026-06-04 10:42:31 +0800

    Run `go mod tidy; go mod vendor`

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/loadtestservice/2025-09-01/playwrightworkspaces/method_createorupdate.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/loadtestservice/2025-09-01/playwrightworkspaces/method_createorupdate.go
@@ -62,9 +62,20 @@ func (c PlaywrightWorkspacesClient) CreateOrUpdate(ctx context.Context, id Playw
 
 // CreateOrUpdateThenPoll performs CreateOrUpdate then polls until it's completed
 func (c PlaywrightWorkspacesClient) CreateOrUpdateThenPoll(ctx context.Context, id PlaywrightWorkspaceId, input PlaywrightWorkspace) error {
+	return c.CreateOrUpdateCallbackThenPoll(ctx, id, input, nil)
+}
+
+// CreateOrUpdateCallbackThenPoll performs CreateOrUpdate, runs the optional callback function, then polls until it's completed
+func (c PlaywrightWorkspacesClient) CreateOrUpdateCallbackThenPoll(ctx context.Context, id PlaywrightWorkspaceId, input PlaywrightWorkspace, callback func() error) error {
 	result, err := c.CreateOrUpdate(ctx, id, input)
 	if err != nil {
 		return fmt.Errorf("performing CreateOrUpdate: %+v", err)
+	}
+
+	if callback != nil {
+		if err := callback(); err != nil {
+			return fmt.Errorf("executing callback function: %+v", err)
+		}
 	}
 
 	if err := result.Poller.PollUntilDone(ctx); err != nil {
