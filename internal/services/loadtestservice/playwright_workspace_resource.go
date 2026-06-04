9ccf3d80e2934d7289ccaf367fbf7a58dda86ee4
v-yhyeo0202 <v-yyeo@microsoft.com>
2026-06-04 10:37:53 +0800

    `azurerm_playwright_workspace` - add `SkipImportCheckOnCreateAndAllowOverwritingExistingResources`

--- internal/services/loadtestservice/playwright_workspace_resource.go
+++ internal/services/loadtestservice/playwright_workspace_resource.go
@@ -89,12 +89,14 @@ func (r PlaywrightWorkspaceResource) Create() sdk.ResourceFunc {
 			}
 			id := playwrightworkspaces.NewPlaywrightWorkspaceID(subscriptionId, config.ResourceGroupName, config.Name)
 
-			existing, err := client.Get(ctx, id)
-			if err != nil && !response.WasNotFound(existing.HttpResponse) {
-				return fmt.Errorf("checking for presence of existing %s: %+v", id, err)
-			}
-			if !response.WasNotFound(existing.HttpResponse) {
-				return metadata.ResourceRequiresImport(r.ResourceType(), id)
+			if !metadata.Client.Features.SkipImportCheckOnCreateAndAllowOverwritingExistingResources {
+				existing, err := client.Get(ctx, id)
+				if err != nil && !response.WasNotFound(existing.HttpResponse) {
+					return fmt.Errorf("checking for presence of existing %s: %+v", id, err)
+				}
+				if !response.WasNotFound(existing.HttpResponse) {
+					return metadata.ResourceRequiresImport(r.ResourceType(), id)
+				}
 			}
 
 			param := playwrightworkspaces.PlaywrightWorkspace{
