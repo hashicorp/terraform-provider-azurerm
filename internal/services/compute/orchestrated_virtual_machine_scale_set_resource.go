7c1b7b972da0e231125e8bbc87d2c51f07a51eeb
Máté Baranyai <cl_baranyaimate@otp.group>
2026-06-06 16:43:38 +0200

    Add RequiredWith sku_name validation to attributes ignored in legacy mode

--- internal/services/compute/orchestrated_virtual_machine_scale_set_resource.go
+++ internal/services/compute/orchestrated_virtual_machine_scale_set_resource.go
@@ -169,6 +169,7 @@ func resourceOrchestratedVirtualMachineScaleSet() *pluginsdk.Resource {
 				ConflictsWith: []string{
 					"proximity_placement_group_id",
 				},
+				RequiredWith: []string{"sku_name"},
 			},
 
 			"data_disk": OrchestratedVirtualMachineScaleSetDataDiskSchema(),
@@ -177,8 +178,9 @@ func resourceOrchestratedVirtualMachineScaleSet() *pluginsdk.Resource {
 			"additional_capabilities": OrchestratedVirtualMachineScaleSetAdditionalCapabilitiesSchema(),
 
 			"encryption_at_host_enabled": {
-				Type:     pluginsdk.TypeBool,
-				Optional: true,
+				Type:         pluginsdk.TypeBool,
+				Optional:     true,
+				RequiredWith: []string{"sku_name"},
 			},
 
 			"eviction_policy": {
@@ -190,6 +192,7 @@ func resourceOrchestratedVirtualMachineScaleSet() *pluginsdk.Resource {
 					string(virtualmachinescalesets.VirtualMachineEvictionPolicyTypesDeallocate),
 					string(virtualmachinescalesets.VirtualMachineEvictionPolicyTypesDelete),
 				}, false),
+				RequiredWith: []string{"sku_name"},
 			},
 
 			"extension_operations_enabled": {
@@ -207,6 +210,7 @@ func resourceOrchestratedVirtualMachineScaleSet() *pluginsdk.Resource {
 				Optional:     true,
 				Default:      "PT1H30M",
 				ValidateFunc: validate.ISO8601DurationBetween("PT15M", "PT2H"),
+				RequiredWith: []string{"sku_name"},
 			},
 
 			// whilst the Swagger defines multiple at this time only UAI is supported
@@ -227,6 +231,7 @@ func resourceOrchestratedVirtualMachineScaleSet() *pluginsdk.Resource {
 
 					return false
 				},
+				RequiredWith: []string{"sku_name"},
 			},
 
 			"max_bid_price": {
@@ -234,6 +239,7 @@ func resourceOrchestratedVirtualMachineScaleSet() *pluginsdk.Resource {
 				Optional:     true,
 				Default:      -1,
 				ValidateFunc: computeValidate.SpotMaxPrice,
+				RequiredWith: []string{"sku_name"},
 			},
 
 			"plan": planSchema(),
@@ -253,6 +259,7 @@ func resourceOrchestratedVirtualMachineScaleSet() *pluginsdk.Resource {
 					string(virtualmachinescalesets.VirtualMachinePriorityTypesRegular),
 					string(virtualmachinescalesets.VirtualMachinePriorityTypesSpot),
 				}, false),
+				RequiredWith: []string{"sku_name"},
 			},
 
 			"proximity_placement_group_id": {
@@ -293,15 +300,17 @@ func resourceOrchestratedVirtualMachineScaleSet() *pluginsdk.Resource {
 				ConflictsWith: []string{
 					"source_image_reference",
 				},
+				RequiredWith: []string{"sku_name"},
 			},
 
 			"source_image_reference": sourceImageReferenceSchemaOrchestratedVMSS(),
 
 			"zone_balance": {
-				Type:     pluginsdk.TypeBool,
-				Optional: true,
-				ForceNew: true,
-				Default:  false,
+				Type:         pluginsdk.TypeBool,
+				Optional:     true,
+				ForceNew:     true,
+				Default:      false,
+				RequiredWith: []string{"sku_name"},
 			},
 
 			"termination_notification": OrchestratedVirtualMachineScaleSetTerminationNotificationSchema(),
@@ -333,6 +342,7 @@ func resourceOrchestratedVirtualMachineScaleSet() *pluginsdk.Resource {
 				Optional:     true,
 				Sensitive:    true,
 				ValidateFunc: validation.StringIsBase64,
+				RequiredWith: []string{"sku_name"},
 			},
 
 			"priority_mix": OrchestratedVirtualMachineScaleSetPriorityMixPolicySchema(),
@@ -359,15 +369,6 @@ func resourceOrchestratedVirtualMachineScaleSet() *pluginsdk.Resource {
 				}
 
 				return false
-			}),
-
-			// encryption_at_host_enabled requires a full VM profile
-			pluginsdk.CustomizeDiffShim(func(ctx context.Context, diff *pluginsdk.ResourceDiff, v interface{}) error {
-				_, hasSkuName := diff.GetOk("sku_name")
-				if v, ok := diff.GetOk("encryption_at_host_enabled"); ok && v.(bool) && !hasSkuName {
-					return fmt.Errorf("`sku_name` must be configured when `encryption_at_host_enabled` is set")
-				}
-				return nil
 			}),
 
 			// SKU Profile validation
@@ -480,6 +481,23 @@ func resourceOrchestratedVirtualMachineScaleSet() *pluginsdk.Resource {
 				return nil
 			}),
 		),
+	}
+
+	for _, field := range []string{
+		"automatic_instance_repair",
+		"boot_diagnostics",
+		"data_disk",
+		"extension",
+		"identity",
+		"network_interface",
+		"os_disk",
+		"os_profile",
+		"plan",
+		"priority_mix",
+		"source_image_reference",
+		"termination_notification",
+	} {
+		resource.Schema[field].RequiredWith = []string{"sku_name"}
 	}
 
 	if !features.FivePointOh() {
--- internal/services/compute/orchestrated_virtual_machine_scale_set_resource_legacy_test.go
+++ internal/services/compute/orchestrated_virtual_machine_scale_set_resource_legacy_test.go
@@ -4,12 +4,15 @@
 package compute_test
 
 import (
+	"context"
 	"fmt"
 	"regexp"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/provider/framework"
 )
 
 func TestAccOrchestratedVirtualMachineScaleSet_legacyBasicZonal(t *testing.T) {
@@ -173,14 +176,67 @@ resource "azurerm_resource_group" "test" {
 `, data.RandomInteger, data.Locations.Primary)
 }
 
-func TestAccOrchestratedVirtualMachineScaleSet_legacyEncryptionAtHost(t *testing.T) {
-	data := acceptance.BuildTestData(t, "azurerm_orchestrated_virtual_machine_scale_set", "test")
+func TestOrchestratedVirtualMachineScaleSet_legacyEncryptionAtHost(t *testing.T) {
 	r := OrchestratedVirtualMachineScaleSetResource{}
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV5ProviderFactories: framework.ProtoV5ProviderFactoriesInit(context.Background(), "azurerm"),
+		Steps: []resource.TestStep{
+			{
+				Config:      r.legacyEncryptionAtHostConfig(),
+				ExpectError: regexp.MustCompile("all of `encryption_at_host_enabled,sku_name` must be specified"),
+			},
+		},
+	})
+}
 
-	data.ResourceTest(t, r, []acceptance.TestStep{
-		{
-			Config:      r.legacyEncryptionAtHost(data),
-			ExpectError: regexp.MustCompile("`sku_name` must be configured when `encryption_at_host_enabled` is set"),
+func TestOrchestratedVirtualMachineScaleSet_legacyZoneBalance(t *testing.T) {
+	r := OrchestratedVirtualMachineScaleSetResource{}
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV5ProviderFactories: framework.ProtoV5ProviderFactoriesInit(context.Background(), "azurerm"),
+		Steps: []resource.TestStep{
+			{
+				Config:      r.legacyZoneBalanceConfig(),
+				ExpectError: regexp.MustCompile("all of `sku_name,zone_balance` must be specified"),
+			},
+		},
+	})
+}
+
+func TestOrchestratedVirtualMachineScaleSet_legacyUserDataBase64(t *testing.T) {
+	r := OrchestratedVirtualMachineScaleSetResource{}
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV5ProviderFactories: framework.ProtoV5ProviderFactoriesInit(context.Background(), "azurerm"),
+		Steps: []resource.TestStep{
+			{
+				Config:      r.legacyUserDataBase64Config(),
+				ExpectError: regexp.MustCompile("all of `sku_name,user_data_base64` must be specified"),
+			},
+		},
+	})
+}
+
+func TestOrchestratedVirtualMachineScaleSet_legacyLicenseType(t *testing.T) {
+	r := OrchestratedVirtualMachineScaleSetResource{}
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV5ProviderFactories: framework.ProtoV5ProviderFactoriesInit(context.Background(), "azurerm"),
+		Steps: []resource.TestStep{
+			{
+				Config:      r.legacyLicenseTypeConfig(),
+				ExpectError: regexp.MustCompile("all of `license_type,sku_name` must be specified"),
+			},
+		},
+	})
+}
+
+func TestOrchestratedVirtualMachineScaleSet_legacyPriority(t *testing.T) {
+	r := OrchestratedVirtualMachineScaleSetResource{}
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV5ProviderFactories: framework.ProtoV5ProviderFactoriesInit(context.Background(), "azurerm"),
+		Steps: []resource.TestStep{
+			{
+				Config:      r.legacyPriorityConfig(),
+				ExpectError: regexp.MustCompile("all of `priority,sku_name` must be specified"),
+			},
 		},
 	})
 }
@@ -205,4 +261,189 @@ resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
   }
 }
 `, r.legacyTemplate(data), data.RandomInteger)
+}
+
+func (r OrchestratedVirtualMachineScaleSetResource) legacyEncryptionAtHostConfig() string {
+	return `
+provider "azurerm" {
+  features {}
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+}
+
+resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
+  name                = "acctestOVMSS"
+  location            = "eastus"
+  resource_group_name = "acctestRG"
+
+  platform_fault_domain_count = 1
+
+  encryption_at_host_enabled = true
+}
+`
+}
+
+func (r OrchestratedVirtualMachineScaleSetResource) legacyZoneBalance(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
+  name                = "acctestOVMSS-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  platform_fault_domain_count = 1
+
+  zone_balance = true
+
+  zones = ["1", "2", "3"]
+
+  tags = {
+    ENV = "Test"
+  }
+}
+`, r.legacyTemplate(data), data.RandomInteger)
+}
+
+func (r OrchestratedVirtualMachineScaleSetResource) legacyZoneBalanceConfig() string {
+	return `
+provider "azurerm" {
+  features {}
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+}
+
+resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
+  name                = "acctestOVMSS"
+  location            = "eastus"
+  resource_group_name = "acctestRG"
+
+  platform_fault_domain_count = 1
+
+  zone_balance = true
+
+  zones = ["1", "2", "3"]
+}
+`
+}
+
+func (r OrchestratedVirtualMachineScaleSetResource) legacyUserDataBase64(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
+  name                = "acctestOVMSS-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  platform_fault_domain_count = 1
+
+  user_data_base64 = "dGVzdA=="
+
+  zones = ["1"]
+
+  tags = {
+    ENV = "Test"
+  }
+}
+`, r.legacyTemplate(data), data.RandomInteger)
+}
+
+func (r OrchestratedVirtualMachineScaleSetResource) legacyUserDataBase64Config() string {
+	return `
+provider "azurerm" {
+  features {}
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+}
+
+resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
+  name                = "acctestOVMSS"
+  location            = "eastus"
+  resource_group_name = "acctestRG"
+
+  platform_fault_domain_count = 1
+
+  user_data_base64 = "dGVzdA=="
+}
+`
+}
+
+func (r OrchestratedVirtualMachineScaleSetResource) legacyLicenseType(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
+  name                = "acctestOVMSS-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  platform_fault_domain_count = 1
+
+  license_type = "Windows_Server"
+
+  zones = ["1"]
+
+  tags = {
+    ENV = "Test"
+  }
+}
+`, r.legacyTemplate(data), data.RandomInteger)
+}
+
+func (r OrchestratedVirtualMachineScaleSetResource) legacyLicenseTypeConfig() string {
+	return `
+provider "azurerm" {
+  features {}
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+}
+
+resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
+  name                = "acctestOVMSS"
+  location            = "eastus"
+  resource_group_name = "acctestRG"
+
+  platform_fault_domain_count = 1
+
+  license_type = "Windows_Server"
+}
+`
+}
+
+func (r OrchestratedVirtualMachineScaleSetResource) legacyPriority(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
+  name                = "acctestOVMSS-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  platform_fault_domain_count = 1
+
+  priority = "Spot"
+
+  zones = ["1"]
+
+  tags = {
+    ENV = "Test"
+  }
+}
+`, r.legacyTemplate(data), data.RandomInteger)
+}
+
+func (r OrchestratedVirtualMachineScaleSetResource) legacyPriorityConfig() string {
+	return `
+provider "azurerm" {
+  features {}
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+}
+
+resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
+  name                = "acctestOVMSS"
+  location            = "eastus"
+  resource_group_name = "acctestRG"
+
+  platform_fault_domain_count = 1
+
+  priority = "Spot"
+}
+`
 }
