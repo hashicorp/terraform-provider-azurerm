8c26d0c7c2e4a3cfc65668edb194538398711c7a
aayushsingh2502 <aayush.singh@hashicorp.com>
2026-06-11 17:26:29 +0530

    tags support in network interface implemented

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2025-04-01/virtualmachinescalesets/version.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/compute/2025-04-01/virtualmachinescalesets/version.go
@@ -3,10 +3,10 @@ package virtualmachinescalesets
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-const defaultApiVersion = "2024-11-01"
+const defaultApiVersion = "2025-04-01"
 
 func userAgent() string {
-	return "hashicorp/go-azure-sdk/virtualmachinescalesets/2024-11-01"
+	return "hashicorp/go-azure-sdk/virtualmachinescalesets/2025-04-01"
 }
 
 func AzureAPIVersion() string {
