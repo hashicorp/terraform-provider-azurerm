1bf2cef1dc8b873b37d37d67161c1d45c624a97b
hqhqhqhqhqhqhqhqhqhqhqhq <harryqu@microsoft.com>
2026-06-11 16:28:56 +1000

    New Resource: azurerm_storage_mover_smb_file_share_target_endpoint

--- internal/services/storagemover/registration.go
+++ internal/services/storagemover/registration.go
@@ -40,12 +40,13 @@ func (r Registration) DataSources() []sdk.DataSource {
 // Resources returns a list of Resources supported by this Service
 func (r Registration) Resources() []sdk.Resource {
 	return []sdk.Resource{
-		StorageMoverResource{},
 		StorageMoverAgentResource{},
+		StorageMoverJobDefinitionResource{},
+		StorageMoverProjectResource{},
+		StorageMoverResource{},
+		StorageMoverSmbFileShareTargetEndpointResource{},
 		StorageMoverSourceEndpointResource{},
 		StorageMoverTargetEndpointResource{},
-		StorageMoverProjectResource{},
-		StorageMoverJobDefinitionResource{},
 	}
 }
 
@@ -71,6 +72,7 @@ func (r Registration) ListResources() []sdk.FrameworkListWrappedResource {
 		StorageMoverJobDefinitionListResource{},
 		StorageMoverListResource{},
 		StorageMoverProjectListResource{},
+		StorageMoverSmbFileShareTargetEndpointListResource{},
 		StorageMoverSourceEndpointListResource{},
 	}
 }
