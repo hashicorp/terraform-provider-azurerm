3a121ab45f8e341404afdebcf275f0e26c0402df
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2026-06-17 16:49:00 -0600

    add missed import

--- internal/services/compute/dedicated_host_resource_test.go
+++ internal/services/compute/dedicated_host_resource_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 )
 
