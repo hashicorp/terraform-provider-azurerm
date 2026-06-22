8510b432e8cfa7c07d0ef48bb31a5fec6fea822b
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2026-06-22 10:30:04 -0600

    `batch` - migrate to `go-azure-sdk`

--- internal/services/batch/batch_job_resource_test.go
+++ internal/services/batch/batch_job_resource_test.go
@@ -9,13 +9,14 @@ import (
 	"testing"
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
+	"github.com/hashicorp/go-azure-sdk/data-plane/batch/2022-01-01-15-0/jobs"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2024-07-01/batchaccount"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/batch"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/batch/parse"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
-	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
 type BatchJobResource struct{}
@@ -100,42 +101,41 @@ func (r BatchJobResource) Exists(ctx context.Context, clients *clients.Client, s
 		return nil, err
 	}
 
-	client, err := clients.Batch.JobClient(ctx, batchaccount.NewBatchAccountID(id.SubscriptionId, id.ResourceGroup, id.BatchAccountName))
+	endpoint, err := batch.BatchJobResource{}.GetEndpoint(ctx, clients.Batch.AccountClient, batchaccount.NewBatchAccountID(id.SubscriptionId, id.ResourceGroup, id.BatchAccountName))
 	if err != nil {
 		return nil, err
 	}
+	client := clients.Batch.JobsClient.Clone(endpoint)
 
-	if resp, err := client.Get(ctx, id.Name, "", "", nil, nil, nil, nil, "", "", nil, nil); err != nil {
-		if utils.ResponseWasNotFound(resp.Response) {
-			return pointer.To(false), nil
-		}
+	idSDK := jobs.NewJobID(endpoint, id.Name)
+
+	resp, err := client.JobGet(ctx, idSDK, jobs.DefaultJobGetOperationOptions())
+	if err != nil {
 		return nil, fmt.Errorf("retrieving %s: %+v", id, err)
 	}
 
-	return pointer.To(true), nil
+	return pointer.To(resp.Model != nil), nil
 }
 
 func (r BatchJobResource) basic(data acceptance.TestData) string {
-	template := r.template(data)
 	return fmt.Sprintf(`
-%s
+%[1]s
 
 resource "azurerm_batch_job" "test" {
-  name          = "testaccbj-%d"
+  name          = "acctest-job-%[2]d"
   batch_pool_id = azurerm_batch_pool.test.id
 }
-`, template, data.RandomInteger)
+`, r.template(data), data.RandomInteger)
 }
 
 func (r BatchJobResource) complete(data acceptance.TestData) string {
-	template := r.template(data)
 	return fmt.Sprintf(`
-%s
+%[1]s
 
 resource "azurerm_batch_job" "test" {
-  name          = "testaccbj-%[2]d"
+  name          = "acctest-job-%[2]d"
   batch_pool_id = azurerm_batch_pool.test.id
-  display_name  = "testaccbj-display-%[2]d"
+  display_name  = "acctest-job-display-%[2]d"
   common_environment_properties = {
     env       = "Test"
     terraform = "true"
@@ -143,33 +143,31 @@ resource "azurerm_batch_job" "test" {
   priority           = 1
   task_retry_maximum = 1
 }
-`, template, data.RandomInteger)
+`, r.template(data), data.RandomInteger)
 }
 
 func (r BatchJobResource) update(data acceptance.TestData) string {
-	template := r.template(data)
 	return fmt.Sprintf(`
-%s
+%[1]s
 
 resource "azurerm_batch_job" "test" {
-  name               = "testaccbj-%d"
+  name               = "acctest-job-%[2]d"
   batch_pool_id      = azurerm_batch_pool.test.id
   priority           = 2
   task_retry_maximum = -1
 }
-`, template, data.RandomInteger)
+`, r.template(data), data.RandomInteger)
 }
 
 func (r BatchJobResource) requiresImport(data acceptance.TestData) string {
-	template := r.basic(data)
 	return fmt.Sprintf(`
-%s
+%[1]s
 
 resource "azurerm_batch_job" "import" {
   name          = azurerm_batch_job.test.name
   batch_pool_id = azurerm_batch_job.test.batch_pool_id
 }
-`, template)
+`, r.basic(data))
 }
 
 func (r BatchJobResource) template(data acceptance.TestData) string {
@@ -184,13 +182,13 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_batch_account" "test" {
-  name                = "testaccbatch%[2]s"
+  name                = "acctestaccount%[2]s"
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
 }
 
 resource "azurerm_batch_pool" "test" {
-  name                = "testaccpool-%[1]d"
+  name                = "acctest-pool-%[1]d"
   resource_group_name = azurerm_resource_group.test.name
   account_name        = azurerm_batch_account.test.name
   node_agent_sku_id   = "batch.node.ubuntu 22.04"
--- internal/services/batch/client/client.go
+++ internal/services/batch/client/client.go
@@ -4,28 +4,29 @@
 package client
 
 import (
-	"context"
 	"fmt"
 
-	"github.com/Azure/go-autorest/autorest"
+	"github.com/hashicorp/go-azure-sdk/data-plane/batch/2022-01-01-15-0/jobs"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2024-07-01/application"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2024-07-01/batchaccount"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2024-07-01/certificate"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2024-07-01/pool"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
-	batchDataplane "github.com/jackofallops/kermit/sdk/batch/2022-01.15.0/batch"
 )
 
 type Client struct {
+	// Resource Manager
 	AccountClient     *batchaccount.BatchAccountClient
 	ApplicationClient *application.ApplicationClient
 	CertificateClient *certificate.CertificateClient
 	PoolClient        *pool.PoolClient
 
-	BatchManagementAuthorizer autorest.Authorizer
+	// Data Plane
+	JobsClient *jobs.JobsClient
 }
 
 func NewClient(o *common.ClientOptions) (*Client, error) {
+	// Resource Manager
 	accountClient, err := batchaccount.NewBatchAccountClientWithBaseURI(o.Environment.ResourceManager)
 	if err != nil {
 		return nil, fmt.Errorf("building Account client: %+v", err)
@@ -50,33 +51,19 @@ func NewClient(o *common.ClientOptions) (*Client, error) {
 	}
 	o.Configure(poolClient.Client, o.Authorizers.ResourceManager)
 
-	return &Client{
-		AccountClient:             accountClient,
-		ApplicationClient:         applicationClient,
-		CertificateClient:         certificateClient,
-		PoolClient:                poolClient,
-		BatchManagementAuthorizer: o.BatchManagementAuthorizer,
-	}, nil
-}
-
-func (r *Client) JobClient(ctx context.Context, accountId batchaccount.BatchAccountId) (*batchDataplane.JobClient, error) {
-	// Retrieve the batch account to find the batch account endpoint
-	accountClient := r.AccountClient
-	account, err := accountClient.Get(ctx, accountId)
+	// Data Plane
+	jobsClient, err := jobs.NewJobsClientUnconfigured()
 	if err != nil {
-		return nil, fmt.Errorf("retrieving %s: %v", accountId, err)
+		return nil, fmt.Errorf("building Jobs client: %+v", err)
 	}
+	o.Configure(jobsClient.Client, o.Authorizers.BatchManagement)
 
-	endpoint := ""
-	if account.Model != nil && account.Model.Properties != nil {
-		endpoint = "https://" + *account.Model.Properties.AccountEndpoint
-	}
-	if endpoint == "" {
-		return nil, fmt.Errorf("retrieving %s: `properties.AccountEndpoint` was empty", accountId)
-	}
+	return &Client{
+		AccountClient:     accountClient,
+		ApplicationClient: applicationClient,
+		CertificateClient: certificateClient,
+		PoolClient:        poolClient,
 
-	// Copy the client since we'll manipulate its BatchURL
-	c := batchDataplane.NewJobClient(endpoint)
-	c.Authorizer = r.BatchManagementAuthorizer
-	return &c, nil
+		JobsClient: jobsClient,
+	}, nil
 }
