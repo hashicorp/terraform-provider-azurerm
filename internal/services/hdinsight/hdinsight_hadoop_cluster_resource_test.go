ccb6de2db6ec7d4fd6d8ad244ea6c10b64c3a6e7
Todd Giguere <todd.giguere@ibm.com>
2026-06-24 16:25:12 -0400

    test: hdinsight 5.0 storage account fix

--- internal/services/hdinsight/hdinsight_hadoop_cluster_resource_test.go
+++ internal/services/hdinsight/hdinsight_hadoop_cluster_resource_test.go
@@ -1492,7 +1492,7 @@ resource "azurerm_storage_account" "test" {
 
 resource "azurerm_storage_container" "test" {
   name                  = "acctest"
-  storage_account_name  = "${azurerm_storage_account.test.name}"
+  storage_account_id    = azurerm_storage_account.test.id
   container_access_type = "private"
 }
 
@@ -1907,7 +1907,7 @@ resource "azurerm_storage_account" "test" {
 
 resource "azurerm_storage_container" "test" {
   name                  = "acctest"
-  storage_account_name  = azurerm_storage_account.test.name
+  storage_account_id    = azurerm_storage_account.test.id
   container_access_type = "private"
 }
 		`, data.RandomInteger, data.Locations.Primary, data.RandomString)
--- internal/services/hdinsight/hdinsight_interactive_query_cluster_resource_test.go
+++ internal/services/hdinsight/hdinsight_interactive_query_cluster_resource_test.go
@@ -1275,7 +1275,7 @@ resource "azurerm_storage_account" "test" {
 
 resource "azurerm_storage_container" "test" {
   name                  = "acctest"
-  storage_account_name  = azurerm_storage_account.test.name
+  storage_account_id    = azurerm_storage_account.test.id
   container_access_type = "private"
 }
 		`, data.RandomInteger, data.Locations.Primary, data.RandomString)
