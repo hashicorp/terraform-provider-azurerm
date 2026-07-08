3d591e4ba027d7e35d9674886ad18771381058d3
Todd Giguere <todd.giguere@ibm.com>
2026-07-08 14:29:48 -0400

    test: storage - 5.0 param name changes

--- internal/services/storage/storage_sync_server_endpoint_resource_test.go
+++ internal/services/storage/storage_sync_server_endpoint_resource_test.go
@@ -228,9 +228,9 @@ resource "azurerm_storage_account" "test" {
 }
 
 resource "azurerm_storage_share" "test" {
-  name                 = "acctest-share-%[1]d"
-  storage_account_name = azurerm_storage_account.test.name
-  quota                = 1
+  name               = "acctest-share-%[1]d"
+  storage_account_id = azurerm_storage_account.test.id
+  quota              = 1
 
   acl {
     id = "GhostedRecall"
