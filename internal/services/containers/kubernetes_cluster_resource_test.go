eff99853afe1e81a9585b1a07dcae6a02d8582c0
hqhqhqhqhqhqhqhqhqhqhqhq <harryqu@microsoft.com>
2026-07-08 17:41:02 +1000

    containers: update node resource group restriction test config

--- internal/services/containers/kubernetes_cluster_resource_test.go
+++ internal/services/containers/kubernetes_cluster_resource_test.go
@@ -1501,6 +1501,11 @@ resource "azurerm_kubernetes_cluster" "test" {
     }
   }
 
+  node_provisioning_profile {
+    mode               = "Manual"
+    default_node_pools = "Auto"
+  }
+
   identity {
     type = "SystemAssigned"
   }
