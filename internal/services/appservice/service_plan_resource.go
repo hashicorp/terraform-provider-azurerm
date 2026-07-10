506e3e3d2800572230546e0c7b26653a69ffcf78
Xiaxin Yi <xiaxyi@microsoft.com>
2026-07-10 16:21:09 +1000

    update upstream

--- internal/services/appservice/service_plan_resource.go
+++ internal/services/appservice/service_plan_resource.go
@@ -16,12 +16,8 @@ import (
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/resourceids"
-<<<<<<< HEAD
 	"github.com/hashicorp/go-azure-sdk/resource-manager/web/2025-05-01/appserviceplans"
-=======
-	"github.com/hashicorp/go-azure-sdk/resource-manager/web/2023-12-01/appserviceplans"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/preflight"
->>>>>>> upstream/main
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/appservice/helpers"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/appservice/migration"
