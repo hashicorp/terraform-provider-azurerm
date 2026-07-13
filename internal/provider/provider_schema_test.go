53863add397b4630b687204072cb279f57942661
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2026-07-13 16:25:03 -0600

    `redisenterprise` - deprecate resources + data sources

--- internal/provider/provider_schema_test.go
+++ internal/provider/provider_schema_test.go
@@ -289,28 +289,24 @@ func TestResourcesDoNotContainANameFieldWithADefaultOfDefault(t *testing.T) {
 			// which'll also need the Monitor resource to have Create call Update
 			"name": {},
 		},
+
+		// @sreallymatt: The Spring Cloud service is being retired, so there is no sense in updating these as this stage.
 		"azurerm_spring_cloud_accelerator": {
-			// TODO: in 4.0 this resource probably wants embedding within `azurerm_spring_cloud_service`
 			"name": {},
 		},
 		"azurerm_spring_cloud_api_portal": {
-			// TODO: in 4.0 this resource probably wants embedding within `azurerm_spring_cloud_service`
 			"name": {},
 		},
 		"azurerm_spring_cloud_application_live_view": {
-			// TODO: in 4.0 this resource probably wants embedding within `azurerm_spring_cloud_service`
 			"name": {},
 		},
 		"azurerm_spring_cloud_configuration_service": {
-			// TODO: in 4.0 this resource probably wants embedding within `azurerm_spring_cloud_service`
 			"name": {},
 		},
 		"azurerm_spring_cloud_dev_tool_portal": {
-			// TODO: in 4.0 this resource probably wants embedding within `azurerm_spring_cloud_service`
 			"name": {},
 		},
 		"azurerm_spring_cloud_gateway": {
-			// TODO: in 4.0 this resource probably wants embedding within `azurerm_spring_cloud_service`
 			"name": {},
 		},
 
@@ -319,12 +315,8 @@ func TestResourcesDoNotContainANameFieldWithADefaultOfDefault(t *testing.T) {
 			"name": {},
 		},
 
-		// 3: Deprecated / to be removed in 4.0
-		"azurerm_cosmosdb_notebook_workspace": {
-			"name": {},
-		},
-
-		// 4: Deprecated / to be removed in 5.0
+		// 3: Deprecated / to be removed in 5.0
+		// TODO 5.0: remove this entry
 		"azurerm_redis_enterprise_database": {
 			"name": {},
 		},
