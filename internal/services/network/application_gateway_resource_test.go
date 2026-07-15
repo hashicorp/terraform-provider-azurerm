b3c03d5b8ef3e61f447429b1abcf1cedd9ca9f2c
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2026-07-15 08:39:26 -0600

    fix: test

--- internal/services/network/application_gateway_resource_test.go
+++ internal/services/network/application_gateway_resource_test.go
@@ -6420,11 +6420,12 @@ locals {
 }
 
 resource "azurerm_storage_account" "errors" {
-  name                     = "acctestsa%s"
-  resource_group_name      = azurerm_resource_group.test.name
-  location                 = azurerm_resource_group.test.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                            = "acctestsa%s"
+  resource_group_name             = azurerm_resource_group.test.name
+  location                        = azurerm_resource_group.test.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = true
 }
 
 resource "azurerm_storage_account_static_website" "errors" {
