18260b170abea1c80c97f12fa25889720a56cc4d
ziyeqf <51212351+ziyeqf@users.noreply.github.com>
2026-07-24 08:20:12 +0000

    update test

--- internal/services/recoveryservices/site_recovery_replicated_vm_resource_test.go
+++ internal/services/recoveryservices/site_recovery_replicated_vm_resource_test.go
@@ -716,8 +716,6 @@ resource "azurerm_recovery_services_vault" "test" {
   location            = azurerm_resource_group.recovery.location
   resource_group_name = azurerm_resource_group.recovery.name
   sku                 = "Standard"
-
-  soft_delete_enabled = false
 }
 
 resource "azurerm_site_recovery_fabric" "source" {
