hashicorp/terraform-provider-azurerm

commit bbcdf11fea42042acc1c63ebe323eb6f49c93450

`azurerm_subnet` - allow create-time NSG and RT via Write Only values. (#32847)
Co-authored-by: sreallymatt <106555974+sreallymatt@users.noreply.github.com>

internal/services/network/network_interface_security_group_association_resource.go
@@ -66,16 +66,16 @@ func resourceNetworkInterfaceSecurityGroupAssociationCreate(d *pluginsdk.Resourc
 		return err
 	}
 
-	locks.ByName(nicId.NetworkInterfaceName, networkInterfaceResourceName)
-	defer locks.UnlockByName(nicId.NetworkInterfaceName, networkInterfaceResourceName)
+	locks.ByID(nicId.ID())
+	defer locks.UnlockByID(nicId.ID())
 
 	nsgId, err := networksecuritygroups.ParseNetworkSecurityGroupID(d.Get("network_security_group_id").(string))
 	if err != nil {
 		return err
 	}
 
-	locks.ByName(nsgId.NetworkSecurityGroupName, networkSecurityGroupResourceName)
-	defer locks.UnlockByName(nsgId.NetworkSecurityGroupName, networkSecurityGroupResourceName)
+	locks.ByID(nsgId.ID())
+	defer locks.UnlockByID(nsgId.ID())
 
 	read, err := client.Get(ctx, *nicId, networkinterfaces.DefaultGetOperationOptions())
 	if err != nil {
@@ -160,8 +160,8 @@ func resourceNetworkInterfaceSecurityGroupAssociationDelete(d *pluginsdk.Resourc
 		return err
 	}
 
-	locks.ByName(id.First.NetworkInterfaceName, networkInterfaceResourceName)
-	defer locks.UnlockByName(id.First.NetworkInterfaceName, networkInterfaceResourceName)
+	locks.ByID(id.First.ID())
+	defer locks.UnlockByID(id.First.ID())
 
 	read, err := client.Get(ctx, *id.First, networkinterfaces.DefaultGetOperationOptions())
 	if err != nil {

internal/services/network/network_ddos_protection_plan_resource.go
@@ -28,8 +28,6 @@ import (
 
 //go:generate go run ../../tools/generator-tests resourceidentity -resource-name network_ddos_protection_plan -test-name basicConfigIdentity -service-package-name network -properties "name,resource_group_name"
 
-const ddosProtectionPlanResourceName = "azurerm_network_ddos_protection_plan"
-
 func resourceNetworkDDoSProtectionPlan() *pluginsdk.Resource {
 	return &pluginsdk.Resource{
 		Create: resourceNetworkDDoSProtectionPlanCreate,
@@ -80,17 +78,17 @@ func resourceNetworkDDoSProtectionPlanCreate(d *pluginsdk.ResourceData, meta int
 	ctx, cancel := timeouts.ForCreate(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
-	vnetsToLock, err := expandNetworkDDoSProtectionPlanVnetNames(d.Get("virtual_network_ids").([]interface{}))
+	vnetsToLock, err := expandNetworkDDoSProtectionPlanVnetIDs(d.Get("virtual_network_ids").([]interface{}))
 	if err != nil {
-		return fmt.Errorf("extracting names of Virtual Network: %+v", err)
+		return fmt.Errorf("extracting IDs of Virtual Network: %+v", err)
 	}
 
 	id := ddosprotectionplans.NewDdosProtectionPlanID(subscriptionId, d.Get("resource_group_name").(string), d.Get("name").(string))
 
-	locks.ByName(id.DdosProtectionPlanName, ddosProtectionPlanResourceName)
-	defer locks.UnlockByName(id.DdosProtectionPlanName, ddosProtectionPlanResourceName)
-	locks.MultipleByName(vnetsToLock, VirtualNetworkResourceName)
-	defer locks.UnlockMultipleByName(vnetsToLock, VirtualNetworkResourceName)
+	locks.ByID(id.ID())
+	defer locks.UnlockByID(id.ID())
+	locks.MultipleByID(vnetsToLock)
+	defer locks.UnlockMultipleByID(vnetsToLock)
 
 	if !meta.(*clients.Client).Features.SkipImportCheckOnCreateAndAllowOverwritingExistingResources {
 		existing, err := client.Get(ctx, id)
@@ -127,9 +125,9 @@ func resourceNetworkDDoSProtectionPlanUpdate(d *pluginsdk.ResourceData, meta int
 	ctx, cancel := timeouts.ForUpdate(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
-	vnetsToLock, err := expandNetworkDDoSProtectionPlanVnetNames(d.Get("virtual_network_ids").([]interface{}))
+	vnetsToLock, err := expandNetworkDDoSProtectionPlanVnetIDs(d.Get("virtual_network_ids").([]interface{}))
 	if err != nil {
-		return fmt.Errorf("extracting names of Virtual Network: %+v", err)
+		return fmt.Errorf("extracting IDs of Virtual Network: %+v", err)
 	}
 
 	id, err := ddosprotectionplans.ParseDdosProtectionPlanID(d.Id())
@@ -137,10 +135,10 @@ func resourceNetworkDDoSProtectionPlanUpdate(d *pluginsdk.ResourceData, meta int
 		return fmt.Errorf("retrieving %s: %+v", id, err)
 	}
 
-	locks.ByName(id.DdosProtectionPlanName, ddosProtectionPlanResourceName)
-	defer locks.UnlockByName(id.DdosProtectionPlanName, ddosProtectionPlanResourceName)
-	locks.MultipleByName(vnetsToLock, VirtualNetworkResourceName)
-	defer locks.UnlockMultipleByName(vnetsToLock, VirtualNetworkResourceName)
+	locks.ByID(id.ID())
+	defer locks.UnlockByID(id.ID())
+	locks.MultipleByID(vnetsToLock)
+	defer locks.UnlockMultipleByID(vnetsToLock)
 
 	existing, err := client.Get(ctx, *id)
 	if err != nil {
@@ -236,16 +234,16 @@ func resourceNetworkDDoSProtectionPlanDelete(d *pluginsdk.ResourceData, meta int
 	}
 	// if there's no VirtualNetworks configured, it's possible for this to be nil
 	subResources := existing.Model.Properties.VirtualNetworks
-	virtualNetworksNamesToLock, err := extractVnetNames(subResources)
+	virtualNetworkIDsToLock, err := extractVnetIDs(subResources)
 	if err != nil {
-		return fmt.Errorf("extracting names of Virtual Network: %+v", err)
+		return fmt.Errorf("extracting IDs of Virtual Network: %+v", err)
 	}
 
-	locks.ByName(id.DdosProtectionPlanName, ddosProtectionPlanResourceName)
-	defer locks.UnlockByName(id.DdosProtectionPlanName, ddosProtectionPlanResourceName)
+	locks.ByID(id.ID())
+	defer locks.UnlockByID(id.ID())
 
-	locks.MultipleByName(virtualNetworksNamesToLock, VirtualNetworkResourceName)
-	defer locks.UnlockMultipleByName(virtualNetworksNamesToLock, VirtualNetworkResourceName)
+	locks.MultipleByID(virtualNetworkIDsToLock)
+	defer locks.UnlockMultipleByID(virtualNetworkIDsToLock)
 
 	if err := client.DeleteThenPoll(ctx, *id); err != nil {
 		return fmt.Errorf("deleting %s: %+v", *id, err)
@@ -254,8 +252,8 @@ func resourceNetworkDDoSProtectionPlanDelete(d *pluginsdk.ResourceData, meta int
 	return nil
 }
 
-func expandNetworkDDoSProtectionPlanVnetNames(input []interface{}) (*[]string, error) {
-	vnetNames := make([]string, 0)
+func expandNetworkDDoSProtectionPlanVnetIDs(input []interface{}) (*[]string, error) {
+	vnetIDs := make([]string, 0)
 
 	for _, vnetID := range input {
 		vnetResourceID, err := commonids.ParseVirtualNetworkID(vnetID.(string))
@@ -263,12 +261,12 @@ func expandNetworkDDoSProtectionPlanVnetNames(input []interface{}) (*[]string, e
 			return nil, err
 		}
 
-		if !utils.SliceContainsValue(vnetNames, vnetResourceID.VirtualNetworkName) {
-			vnetNames = append(vnetNames, vnetResourceID.VirtualNetworkName)
+		if !utils.SliceContainsValue(vnetIDs, vnetResourceID.ID()) {
+			vnetIDs = append(vnetIDs, vnetResourceID.ID())
 		}
 	}
 
-	return &vnetNames, nil
+	return &vnetIDs, nil
 }
 
 func flattenNetworkDDoSProtectionPlanVirtualNetworkIDs(input *[]ddosprotectionplans.SubResource) []string {
@@ -287,8 +285,8 @@ func flattenNetworkDDoSProtectionPlanVirtualNetworkIDs(input *[]ddosprotectionpl
 	return vnetIDs
 }
 
-func extractVnetNames(input *[]ddosprotectionplans.SubResource) (*[]string, error) {
-	vnetNames := make([]string, 0)
+func extractVnetIDs(input *[]ddosprotectionplans.SubResource) (*[]string, error) {
+	vnetIDs := make([]string, 0)
 
 	if input != nil {
 		for _, subresource := range *input {
@@ -301,11 +299,11 @@ func extractVnetNames(input *[]ddosprotectionplans.SubResource) (*[]string, erro
 				return nil, err
 			}
 
-			if !utils.SliceContainsValue(vnetNames, id.VirtualNetworkName) {
-				vnetNames = append(vnetNames, id.VirtualNetworkName)
+			if !utils.SliceContainsValue(vnetIDs, id.ID()) {
+				vnetIDs = append(vnetIDs, id.ID())
 			}
 		}
 	}
 
-	return &vnetNames, nil
+	return &vnetIDs, nil
 }

internal/services/network/network_interface_locking.go
@@ -11,30 +11,30 @@ import (
 )
 
 type networkInterfaceIPConfigurationLockingDetails struct {
-	subnetNamesToLock         []string
-	virtualNetworkNamesToLock []string
+	subnetIdsToLock         []string
+	virtualNetworkIdsToLock []string
 }
 
 func (details networkInterfaceIPConfigurationLockingDetails) lock() {
-	locks.MultipleByName(&details.virtualNetworkNamesToLock, VirtualNetworkResourceName)
-	locks.MultipleByName(&details.subnetNamesToLock, SubnetResourceName)
+	locks.MultipleByID(&details.virtualNetworkIdsToLock)
+	locks.MultipleByID(&details.subnetIdsToLock)
 }
 
 func (details networkInterfaceIPConfigurationLockingDetails) unlock() {
-	locks.UnlockMultipleByName(&details.subnetNamesToLock, SubnetResourceName)
-	locks.UnlockMultipleByName(&details.virtualNetworkNamesToLock, VirtualNetworkResourceName)
+	locks.UnlockMultipleByID(&details.subnetIdsToLock)
+	locks.UnlockMultipleByID(&details.virtualNetworkIdsToLock)
 }
 
 func determineResourcesToLockFromIPConfiguration(input *[]networkinterfaces.NetworkInterfaceIPConfiguration) (*networkInterfaceIPConfigurationLockingDetails, error) {
 	if input == nil {
 		return &networkInterfaceIPConfigurationLockingDetails{
-			subnetNamesToLock:         []string{},
-			virtualNetworkNamesToLock: []string{},
+			subnetIdsToLock:         []string{},
+			virtualNetworkIdsToLock: []string{},
 		}, nil
 	}
 
-	subnetNamesToLock := make([]string, 0)
-	virtualNetworkNamesToLock := make([]string, 0)
+	subnetIdsToLock := make([]string, 0)
+	virtualNetworkIdsToLock := make([]string, 0)
 
 	for _, config := range *input {
 		if config.Properties == nil || config.Properties.Subnet == nil || config.Properties.Subnet.Id == nil {
@@ -46,20 +46,19 @@ func determineResourcesToLockFromIPConfiguration(input *[]networkinterfaces.Netw
 			return nil, err
 		}
 
-		virtualNetworkName := id.VirtualNetworkName
-		subnetName := id.SubnetName
+		vnetId := commonids.NewVirtualNetworkID(id.SubscriptionId, id.ResourceGroupName, id.VirtualNetworkName)
 
-		if !utils.SliceContainsValue(virtualNetworkNamesToLock, virtualNetworkName) {
-			virtualNetworkNamesToLock = append(virtualNetworkNamesToLock, virtualNetworkName)
+		if !utils.SliceContainsValue(virtualNetworkIdsToLock, vnetId.ID()) {
+			virtualNetworkIdsToLock = append(virtualNetworkIdsToLock, vnetId.ID())
 		}
 
-		if !utils.SliceContainsValue(subnetNamesToLock, subnetName) {
-			subnetNamesToLock = append(subnetNamesToLock, subnetName)
+		if !utils.SliceContainsValue(subnetIdsToLock, id.ID()) {
+			subnetIdsToLock = append(subnetIdsToLock, id.ID())
 		}
 	}
 
 	return &networkInterfaceIPConfigurationLockingDetails{
-		subnetNamesToLock:         subnetNamesToLock,
-		virtualNetworkNamesToLock: virtualNetworkNamesToLock,
+		subnetIdsToLock:         subnetIdsToLock,
+		virtualNetworkIdsToLock: virtualNetworkIdsToLock,
 	}, nil
 }

internal/services/network/network_profile_resource.go
@@ -133,14 +133,14 @@ func resourceNetworkProfileCreate(d *pluginsdk.ResourceData, meta interface{}) e
 		return fmt.Errorf("extracting names of Subnet and Virtual Network: %+v", err)
 	}
 
-	locks.ByName(id.NetworkProfileName, azureNetworkProfileResourceName)
-	defer locks.UnlockByName(id.NetworkProfileName, azureNetworkProfileResourceName)
+	locks.ByID(id.ID())
+	defer locks.UnlockByID(id.ID())
 
-	locks.MultipleByName(vnetsToLock, VirtualNetworkResourceName)
-	defer locks.UnlockMultipleByName(vnetsToLock, VirtualNetworkResourceName)
+	locks.MultipleByID(vnetsToLock)
+	defer locks.UnlockMultipleByID(vnetsToLock)
 
-	locks.MultipleByName(subnetsToLock, SubnetResourceName)
-	defer locks.UnlockMultipleByName(subnetsToLock, SubnetResourceName)
+	locks.MultipleByID(subnetsToLock)
+	defer locks.UnlockMultipleByID(subnetsToLock)
 
 	payload := networkprofiles.NetworkProfile{
 		Location: pointer.To(location.Normalize(d.Get("location").(string))),
@@ -192,14 +192,14 @@ func resourceNetworkProfileUpdate(d *pluginsdk.ResourceData, meta interface{}) e
 		return fmt.Errorf("extracting names of Subnet and Virtual Network: %+v", err)
 	}
 
-	locks.ByName(id.NetworkProfileName, azureNetworkProfileResourceName)
-	defer locks.UnlockByName(id.NetworkProfileName, azureNetworkProfileResourceName)
+	locks.ByID(id.ID())
+	defer locks.UnlockByID(id.ID())
 
-	locks.MultipleByName(vnetsToLock, VirtualNetworkResourceName)
-	defer locks.UnlockMultipleByName(vnetsToLock, VirtualNetworkResourceName)
+	locks.MultipleByID(vnetsToLock)
+	defer locks.UnlockMultipleByID(vnetsToLock)
 
-	locks.MultipleByName(subnetsToLock, SubnetResourceName)
-	defer locks.UnlockMultipleByName(subnetsToLock, SubnetResourceName)
+	locks.MultipleByID(subnetsToLock)
+	defer locks.UnlockMultipleByID(subnetsToLock)
 
 	if d.HasChange("container_network_interface") {
 		payload.Properties.ContainerNetworkInterfaceConfigurations = containerNetworkInterfaceConfigurations
@@ -288,14 +288,14 @@ func resourceNetworkProfileDelete(d *pluginsdk.ResourceData, meta interface{}) e
 		return fmt.Errorf("extracting names of Subnet and Virtual Network: %+v", err)
 	}
 
-	locks.ByName(id.NetworkProfileName, azureNetworkProfileResourceName)
-	defer locks.UnlockByName(id.NetworkProfileName, azureNetworkProfileResourceName)
+	locks.ByID(id.ID())
+	defer locks.UnlockByID(id.ID())
 
-	locks.MultipleByName(vnetsToLock, VirtualNetworkResourceName)
-	defer locks.UnlockMultipleByName(vnetsToLock, VirtualNetworkResourceName)
+	locks.MultipleByID(vnetsToLock)
+	defer locks.UnlockMultipleByID(vnetsToLock)
 
-	locks.MultipleByName(subnetsToLock, SubnetResourceName)
-	defer locks.UnlockMultipleByName(subnetsToLock, SubnetResourceName)
+	locks.MultipleByID(subnetsToLock)
+	defer locks.UnlockMultipleByID(subnetsToLock)
 
 	if err := client.DeleteThenPoll(ctx, *id); err != nil {
 		return fmt.Errorf("deleting %s: %+v", *id, err)
@@ -344,8 +344,8 @@ func expandNetworkProfileContainerNetworkInterface(input []interface{}) *[]netwo
 }
 
 func expandNetworkProfileVirtualNetworkSubnetNames(input *[]networkprofiles.ContainerNetworkInterfaceConfiguration) (*[]string, *[]string, error) {
-	subnetNames := make([]string, 0)
-	vnetNames := make([]string, 0)
+	subnetIds := make([]string, 0)
+	vnetIds := make([]string, 0)
 
 	if input != nil {
 		for _, item := range *input {
@@ -363,18 +363,20 @@ func expandNetworkProfileVirtualNetworkSubnetNames(input *[]networkprofiles.Cont
 					return nil, nil, err
 				}
 
-				if !utils.SliceContainsValue(subnetNames, subnetId.SubnetName) {
-					subnetNames = append(subnetNames, subnetId.SubnetName)
+				vnetId := commonids.NewVirtualNetworkID(subnetId.SubscriptionId, subnetId.ResourceGroupName, subnetId.VirtualNetworkName)
+
+				if !utils.SliceContainsValue(subnetIds, subnetId.ID()) {
+					subnetIds = append(subnetIds, subnetId.ID())
 				}
 
-				if !utils.SliceContainsValue(vnetNames, subnetId.VirtualNetworkName) {
-					vnetNames = append(vnetNames, subnetId.VirtualNetworkName)
+				if !utils.SliceContainsValue(vnetIds, vnetId.ID()) {
+					vnetIds = append(vnetIds, vnetId.ID())
 				}
 			}
 		}
 	}
 
-	return &subnetNames, &vnetNames, nil
+	return &subnetIds, &vnetIds, nil
 }
 
 func flattenNetworkProfileContainerNetworkInterface(input *[]networkprofiles.ContainerNetworkInterfaceConfiguration) []interface{} {

internal/services/network/network_security_group_resource.go
@@ -225,8 +225,8 @@ func resourceNetworkSecurityGroupCreate(d *pluginsdk.ResourceData, meta interfac
 		return fmt.Errorf("building list of Network Security Group Rules: %+v", sgErr)
 	}
 
-	locks.ByName(id.NetworkSecurityGroupName, networkSecurityGroupResourceName)
-	defer locks.UnlockByName(id.NetworkSecurityGroupName, networkSecurityGroupResourceName)
+	locks.ByID(id.ID())
+	defer locks.UnlockByID(id.ID())
 
 	sg := networksecuritygroups.NetworkSecurityGroup{
 		Name:     pointer.To(id.NetworkSecurityGroupName),
@@ -286,8 +286,8 @@ func resourceNetworkSecurityGroupUpdate(d *pluginsdk.ResourceData, meta interfac
 		payload.Tags = tags.Expand(d.Get("tags").(map[string]interface{}))
 	}
 
-	locks.ByName(id.NetworkSecurityGroupName, networkSecurityGroupResourceName)
-	defer locks.UnlockByName(id.NetworkSecurityGroupName, networkSecurityGroupResourceName)
+	locks.ByID(id.ID())
+	defer locks.UnlockByID(id.ID())
 
 	if err := client.CreateOrUpdateThenPoll(ctx, *id, *payload); err != nil {
 		return fmt.Errorf("updating %s: %+v", id, err)
@@ -353,6 +353,9 @@ func resourceNetworkSecurityGroupDelete(d *pluginsdk.ResourceData, meta interfac
 	if err != nil {
 		return err
 	}
+
+	locks.ByID(id.ID())
+	defer locks.UnlockByID(id.ID())
 
 	if err := client.DeleteThenPoll(ctx, *id); err != nil {
 		return fmt.Errorf("deleting %s: %+v", id, err)

internal/services/network/route_resource.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/network/2025-01-01/routes"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/network/2025-01-01/routetables"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
@@ -113,8 +114,9 @@ func resourceRouteCreate(d *pluginsdk.ResourceData, meta interface{}) error {
 		}
 	}
 
-	locks.ByName(id.RouteTableName, routeTableResourceName)
-	defer locks.UnlockByName(id.RouteTableName, routeTableResourceName)
+	rtId := routetables.NewRouteTableID(id.SubscriptionId, id.ResourceGroupName, id.RouteTableName)
+	locks.ByID(rtId.ID())
+	defer locks.UnlockByID(rtId.ID())
 
 	route := routes.Route{
 		Name: pointer.To(id.RouteName),
@@ -164,8 +166,9 @@ func resourceRouteUpdate(d *pluginsdk.ResourceData, meta interface{}) error {
 
 	payload := existing.Model
 
-	locks.ByName(id.RouteTableName, routeTableResourceName)
-	defer locks.UnlockByName(id.RouteTableName, routeTableResourceName)
+	rtId := routetables.NewRouteTableID(id.SubscriptionId, id.ResourceGroupName, id.RouteTableName)
+	locks.ByID(rtId.ID())
+	defer locks.UnlockByID(rtId.ID())
 
 	if d.HasChange("address_prefix") {
 		payload.Properties.AddressPrefix = pointer.To(d.Get("address_prefix").(string))
@@ -233,8 +236,9 @@ func resourceRouteDelete(d *pluginsdk.ResourceData, meta interface{}) error {
 		return err
 	}
 
-	locks.ByName(id.RouteTableName, routeTableResourceName)
-	defer locks.UnlockByName(id.RouteTableName, routeTableResourceName)
+	rtId := routetables.NewRouteTableID(id.SubscriptionId, id.ResourceGroupName, id.RouteTableName)
+	locks.ByID(rtId.ID())
+	defer locks.UnlockByID(rtId.ID())
 
 	if err := client.DeleteThenPoll(ctx, *id); err != nil {
 		return fmt.Errorf("deleting %s: %+v", *id, err)

internal/services/network/route_table_resource.go
@@ -16,6 +16,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/locks"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/network/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
@@ -140,6 +141,9 @@ func resourceRouteTableCreate(d *pluginsdk.ResourceData, meta interface{}) error
 
 	bgpRoutePropagationEnabled := d.Get("bgp_route_propagation_enabled").(bool)
 
+	locks.ByID(id.ID())
+	defer locks.UnlockByID(id.ID())
+
 	routeSet := routetables.RouteTable{
 		Name:     &id.RouteTableName,
 		Location: pointer.To(location.Normalize(d.Get("location").(string))),
@@ -197,6 +201,9 @@ func resourceRouteTableUpdate(d *pluginsdk.ResourceData, meta interface{}) error
 	if d.HasChange("tags") {
 		payload.Tags = tags.Expand(d.Get("tags").(map[string]interface{}))
 	}
+
+	locks.ByID(id.ID())
+	defer locks.UnlockByID(id.ID())
 
 	if err := client.CreateOrUpdateThenPoll(ctx, *id, *payload); err != nil {
 		return fmt.Errorf("updating %s: %+v", id, err)
@@ -266,6 +273,9 @@ func resourceRouteTableDelete(d *pluginsdk.ResourceData, meta interface{}) error
 	if err != nil {
 		return err
 	}
+
+	locks.ByID(id.ID())
+	defer locks.UnlockByID(id.ID())
 
 	if err = client.DeleteThenPoll(ctx, *id); err != nil {
 		return fmt.Errorf("deleting %s: %+v", id, err)

internal/services/network/subnet_data_source_test.go
@@ -24,7 +24,6 @@ func TestAccDataSourceSubnet_basic(t *testing.T) {
 				check.That(data.ResourceName).Key("name").Exists(),
 				check.That(data.ResourceName).Key("resource_group_name").Exists(),
 				check.That(data.ResourceName).Key("virtual_network_name").Exists(),
-				check.That(data.ResourceName).Key("address_prefix").Exists(),
 				check.That(data.ResourceName).Key("network_security_group_id").HasValue(""),
 				check.That(data.ResourceName).Key("route_table_id").HasValue(""),
 			),
@@ -66,7 +65,7 @@ func TestAccDataSourceSubnet_networkSecurityGroup(t *testing.T) {
 				check.That(data.ResourceName).Key("name").Exists(),
 				check.That(data.ResourceName).Key("resource_group_name").Exists(),
 				check.That(data.ResourceName).Key("virtual_network_name").Exists(),
-				check.That(data.ResourceName).Key("address_prefix").Exists(),
+				check.That(data.ResourceName).Key("address_prefixes.#").Exists(),
 				check.That(data.ResourceName).Key("network_security_group_id").Exists(),
 				check.That(data.ResourceName).Key("route_table_id").HasValue(""),
 			),
@@ -89,7 +88,7 @@ func TestAccDataSourceSubnet_routeTable(t *testing.T) {
 				check.That(data.ResourceName).Key("name").Exists(),
 				check.That(data.ResourceName).Key("resource_group_name").Exists(),
 				check.That(data.ResourceName).Key("virtual_network_name").Exists(),
-				check.That(data.ResourceName).Key("address_prefix").Exists(),
+				check.That(data.ResourceName).Key("address_prefixes.#").Exists(),
 				check.That(data.ResourceName).Key("route_table_id").Exists(),
 				check.That(data.ResourceName).Key("network_security_group_id").HasValue(""),
 			),
@@ -108,7 +107,7 @@ func TestAccDataSourceSubnet_serviceEndpoints(t *testing.T) {
 				check.That(data.ResourceName).Key("name").Exists(),
 				check.That(data.ResourceName).Key("resource_group_name").Exists(),
 				check.That(data.ResourceName).Key("virtual_network_name").Exists(),
-				check.That(data.ResourceName).Key("address_prefix").Exists(),
+				check.That(data.ResourceName).Key("address_prefixes.#").Exists(),
 				check.That(data.ResourceName).Key("network_security_group_id").HasValue(""),
 				check.That(data.ResourceName).Key("route_table_id").HasValue(""),
 				check.That(data.ResourceName).Key("service_endpoints.#").HasValue("2"),

commit 2c6dca291ecb17878973858b5a4cfcc305edb088

r/network_interface_security_group_association: consistently locking on the Name not the Resource ID
This commit fixes a regression introduced in #32847 where the Resource Name is used for locking in
most of the association resources, but the `azurerm_network_interface_security_group_association`
resource uses a lock on the Resource ID and not the Resource Name, which means when you update
the NIC in question, associating a Network Security Group knocks out any other changes.

Confirmation of the issue:

┌────────────────────────────────────────────┬─────────────────────────────────────────────────┬───────────────────────────────────────────┐
│                  Resource                  │                    Lock call                    │                 Mutex key                 │
├────────────────────────────────────────────┼─────────────────────────────────────────────────┼───────────────────────────────────────────┤
│ ..._security_group_association             │ locks.ByID(nicId.ID())                          │ /subscriptions/…/networkInterfaces/tf-nic │
├────────────────────────────────────────────┼─────────────────────────────────────────────────┼───────────────────────────────────────────┤
│ ..._backend_address_pool_association       │ locks.ByName(name, "azurerm_network_interface") │ azurerm_network_interface.tf-nic          │
├────────────────────────────────────────────┼─────────────────────────────────────────────────┼───────────────────────────────────────────┤
│ ..._nat_rule_association                   │ locks.ByName(…)                                 │ azurerm_network_interface.tf-nic          │
├────────────────────────────────────────────┼─────────────────────────────────────────────────┼───────────────────────────────────────────┤
│ ..._application_security_group_association │ locks.ByName(…)                                 │ azurerm_network_interface.tf-nic          │

internal/services/network/network_interface_security_group_association_resource.go
@@ -66,16 +66,16 @@ func resourceNetworkInterfaceSecurityGroupAssociationCreate(d *pluginsdk.Resourc
 		return err
 	}
 
-	locks.ByID(nicId.ID())
-	defer locks.UnlockByID(nicId.ID())
+	locks.ByName(nicId.NetworkInterfaceName, networkInterfaceResourceName)
+	defer locks.UnlockByName(nicId.NetworkInterfaceName, networkInterfaceResourceName)
 
 	nsgId, err := networksecuritygroups.ParseNetworkSecurityGroupID(d.Get("network_security_group_id").(string))
 	if err != nil {
 		return err
 	}
 
-	locks.ByID(nsgId.ID())
-	defer locks.UnlockByID(nsgId.ID())
+	locks.ByName(nsgId.NetworkSecurityGroupName, networkSecurityGroupResourceName)
+	defer locks.UnlockByName(nsgId.NetworkSecurityGroupName, networkSecurityGroupResourceName)
 
 	read, err := client.Get(ctx, *nicId, networkinterfaces.DefaultGetOperationOptions())
 	if err != nil {
@@ -160,8 +160,8 @@ func resourceNetworkInterfaceSecurityGroupAssociationDelete(d *pluginsdk.Resourc
 		return err
 	}
 
-	locks.ByID(id.First.ID())
-	defer locks.UnlockByID(id.First.ID())
+	locks.ByName(id.First.NetworkInterfaceName, networkInterfaceResourceName)
+	defer locks.UnlockByName(id.First.NetworkInterfaceName, networkInterfaceResourceName)
 
 	read, err := client.Get(ctx, *id.First, networkinterfaces.DefaultGetOperationOptions())
 	if err != nil {