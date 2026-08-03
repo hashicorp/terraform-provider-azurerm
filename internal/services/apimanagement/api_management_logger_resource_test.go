e9fb0eb18c5806d958e6c897bf4c11cf814c1e76
Malik Dagher <malikdagher@dhcp-9-16-56-241.atx-us.ibm.com>
2026-08-03 12:11:53 -0500

    fmt fix

--- internal/services/apimanagement/api_management_logger_resource_test.go
+++ internal/services/apimanagement/api_management_logger_resource_test.go
@@ -742,10 +742,10 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_application_insights" "test" {
-  name                          = "acctestappinsights-%[1]d"
-  location                      = azurerm_resource_group.test.location
-  resource_group_name           = azurerm_resource_group.test.name
-  application_type              = "other"
+  name                         = "acctestappinsights-%[1]d"
+  location                     = azurerm_resource_group.test.location
+  resource_group_name          = azurerm_resource_group.test.name
+  application_type             = "other"
   local_authentication_enabled = false
 }
 
