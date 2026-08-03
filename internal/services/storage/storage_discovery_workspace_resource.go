9778ad8b3d700c107643c41db833a57ea2f08f70
hqhqhqhqhqhqhqhqhqhqhqhq <harryqu@microsoft.com>
2026-08-03 10:44:43 +1000

    storage: update discovery workspace identity generator

--- internal/services/storage/storage_discovery_workspace_resource.go
+++ internal/services/storage/storage_discovery_workspace_resource.go
@@ -21,7 +21,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 )
 
-//go:generate go run ../../tools/generator-tests resourceidentity -resource-name storage_discovery_workspace -service-package-name storage -properties "name,resource_group_name" -known-values "subscription_id:data.Subscriptions.Primary"
+//go:generate go run ../../tools/generator-tests resourceidentity
 
 var (
 	_ sdk.ResourceWithUpdate        = StorageDiscoveryWorkspaceResource{}
