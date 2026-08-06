b3e7bb8ba0fa6712f8c8f28e1779453e64ad96d5
Malik Dagher <malikdagher@dhcp-9-16-39-237.atx-us.ibm.com>
2026-08-06 13:39:01 -0500

    Removed deprecated block

--- internal/services/eventgrid/eventgrid_event_subscription_resource_test.go
+++ internal/services/eventgrid/eventgrid_event_subscription_resource_test.go
@@ -1757,104 +1757,11 @@ resource "azurerm_eventgrid_event_subscription" "test" {
 }
 
 func (EventGridEventSubscriptionResource) userIdentity(data acceptance.TestData) string {
-	if !features.FivePointOh() {
-		return fmt.Sprintf(`
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "test" {
-  name     = "acctestRG-eg-%[1]d"
-  location = "%[2]s"
-}
-
-resource "azurerm_storage_account" "test" {
-  name                     = "acctestacc%[3]s"
-  resource_group_name      = azurerm_resource_group.test.name
-  location                 = azurerm_resource_group.test.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "staging"
-  }
-}
-
-resource "azurerm_storage_queue" "test" {
-  name                 = "mysamplequeue-%[1]d"
-  storage_account_name = azurerm_storage_account.test.name
-}
-
-resource "azurerm_storage_container" "test" {
-  name                  = "dead-letter-destination"
-  storage_account_id    = azurerm_storage_account.test.id
-  container_access_type = "private"
-}
-
-resource "azurerm_user_assigned_identity" "test" {
-  name                = "acctestUAI-%[1]d"
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-}
-
-resource "azurerm_eventgrid_topic" "test" {
-  name                = "acctesteg-%[1]d"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-
-  identity {
-    type = "UserAssigned"
-    identity_ids = [
-      azurerm_user_assigned_identity.test.id
-    ]
-  }
-}
-
-resource "azurerm_role_assignment" "contributor" {
-  scope                = azurerm_storage_account.test.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.test.principal_id
-}
-
-resource "azurerm_role_assignment" "sender" {
-  scope                = azurerm_storage_account.test.id
-  role_definition_name = "Storage Queue Data Message Sender"
-  principal_id         = azurerm_user_assigned_identity.test.principal_id
-}
-
-resource "azurerm_eventgrid_event_subscription" "test" {
-  name  = "acctesteg-%[1]d"
-  scope = azurerm_eventgrid_topic.test.id
-
-  delivery_identity {
-    type                   = "UserAssigned"
-    user_assigned_identity = azurerm_user_assigned_identity.test.id
-  }
-
-  dead_letter_identity {
-    type                   = "UserAssigned"
-    user_assigned_identity = azurerm_user_assigned_identity.test.id
-  }
-
-  storage_queue_endpoint {
-    storage_account_id = azurerm_storage_account.test.id
-    queue_name         = azurerm_storage_queue.test.name
-  }
-
-  storage_blob_dead_letter_destination {
-    storage_account_id          = azurerm_storage_account.test.id
-    storage_blob_container_name = azurerm_storage_container.test.name
-  }
-
-  depends_on = [
-    azurerm_role_assignment.contributor,
-    azurerm_role_assignment.sender
-  ]
-
-}
-		`, data.RandomInteger, data.Locations.Primary, data.RandomString)
-	}
 	return fmt.Sprintf(`
+  
+
+
+
 provider "azurerm" {
   features {}
 }
