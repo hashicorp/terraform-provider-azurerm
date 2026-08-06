023d595730abb50ab1267574c90cccb4a53b17ed
magodo <wztwcy@gmail.com>
2026-08-07 09:37:36 +1000

    remove a test case which is not needed

--- internal/services/storage/storage_container_resource_v1_to_v2_test.go
+++ internal/services/storage/storage_container_resource_v1_to_v2_test.go
@@ -31,29 +31,6 @@ func TestAccStorageContainer_V1ToV2_4810(t *testing.T) {
 	}, "4.81.0")
 }
 
-// TestAccStorageContainer_V1ToV2_3380 tests the fallback `FindAccount` path of the state migration
-// It uses v3.38.0 as the setup version because `resource_manager_id` was introduced in v3.39.0.
-func TestAccStorageContainer_V1ToV2_3380(t *testing.T) {
-	data := acceptance.BuildTestData(t, "azurerm_storage_container", "test")
-	r := StorageContainerResource{}
-
-	data.ResourceRegressionTest(t, r, []acceptance.TestStep{
-		{
-			Config: r.basicV1(data),
-			Check: acceptance.ComposeTestCheckFunc(
-				check.That(data.ResourceName).Key("id").HasValue(fmt.Sprintf("https://acctestacc%s.blob.core.windows.net/vhds", data.RandomString)),
-			),
-		},
-		{
-			Config: r.basic(data),
-			Check: acceptance.ComposeTestCheckFunc(
-				check.That(data.ResourceName).ExistsInAzure(r),
-				check.That(data.ResourceName).Key("id").HasValue(fmt.Sprintf("/subscriptions/%[1]s/resourceGroups/acctestRG-%[2]d/providers/Microsoft.Storage/storageAccounts/acctestacc%[3]s/blobServices/default/containers/vhds", data.Subscriptions.Primary, data.RandomInteger, data.RandomString)),
-			),
-		},
-	}, "3.38.0")
-}
-
 func (r StorageContainerResource) basicV1(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 %s
