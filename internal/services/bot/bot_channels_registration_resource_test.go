acd20f1fee08618e632987da0f4acc4a0b97b80c
Malik Dagher <malikdagher@Maliks-MacBook-Pro.local>
2026-08-07 09:55:29 -0500

    Test Fix:

--- internal/services/bot/bot_channels_registration_resource_test.go
+++ internal/services/bot/bot_channels_registration_resource_test.go
@@ -209,7 +209,6 @@ resource "azurerm_bot_channels_registration" "test" {
   sku                     = "F0"
 
   endpoint                              = "https://example.com"
-  developer_app_insights_api_key        = azurerm_application_insights_api_key.test.api_key
   developer_app_insights_application_id = azurerm_application_insights.test.app_id
   developer_app_insights_key            = azurerm_application_insights.test.instrumentation_key
 
