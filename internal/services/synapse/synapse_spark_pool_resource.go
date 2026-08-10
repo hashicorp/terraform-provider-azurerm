0450b6758e44a01b3e7c96fcaa64b22255becf56
kt <kt@katbyte.me>
2026-08-10 10:04:25 -0700

    linters - enable tfproviderlint checks 5/5 - Services synapse & systemcentervirtualmachinemanager, shared validators, enable V001/V011/V012/V013

--- internal/services/synapse/synapse_spark_pool_resource.go
+++ internal/services/synapse/synapse_spark_pool_resource.go
@@ -239,6 +239,7 @@ func resourceSynapseSparkPool() *pluginsdk.Resource {
 					"3.4",
 					"3.5",
 				}, false),
+				// lintignore:V013 // false positive - this emits a deprecation warning for specific values, it does not reject values
 				func(v interface{}, k string) (warnings []string, errors []error) {
 					if val, ok := v.(string); ok && (val == "3.2" || val == "3.3") {
 						warnings = append(warnings, fmt.Sprintf("Spark version %s is deprecated and will be removed in a future version of the AzureRM provider. Please consider upgrading to version 3.4 or later.", val))
