4a2ca2ccd705dca2643fcb5e351d3912a0b2c136
kt <kt@katbyte.me>
2026-08-10 20:27:51 -0700

    replace custom validators with sdkv2 validation helpers where possible

--- internal/services/desktopvirtualization/validate/workspace_name.go
+++ internal/services/desktopvirtualization/validate/workspace_name.go
@@ -4,45 +4,18 @@
 package validate
 
 import (
-	"fmt"
 	"regexp"
-	"strings"
+
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 )
 
-// lintignore:V012,V011 // false positive - this validates a string, not an int; the int comparison here checks the string length
 func WorkspaceName(i interface{}, k string) (warnings []string, errors []error) {
-	v, ok := i.(string)
-
-	if !ok {
-		errors = append(errors, fmt.Errorf("expected %q to be a string but it wasn't", k))
-		return
-	}
-
-	// The value must not be empty.
-	if strings.TrimSpace(v) == "" {
-		errors = append(errors, fmt.Errorf("%q must not be empty", k))
-		return
-	}
-
-	const minLength = 3
-	const maxLength = 64
-
 	// Workspace name can be 3-64 characters in length
-	if len(v) > maxLength || len(v) < minLength {
-		errors = append(errors, fmt.Errorf("%q must be between %d-%d characters, got %d", k, minLength, maxLength, len(v)))
-	}
-
-	if matched := regexp.MustCompile(`^[a-zA-Z0-9._-]+$`).Match([]byte(v)); !matched {
-		errors = append(errors, fmt.Errorf("%q may only contain alphanumeric characters, dots, dashes and underscores", k))
-	}
-
-	if matched := regexp.MustCompile(`^[a-zA-Z0-9]`).Match([]byte(v)); !matched {
-		errors = append(errors, fmt.Errorf("%q must begin with an alphanumeric character", k))
-	}
-
-	if matched := regexp.MustCompile(`\w$`).Match([]byte(v)); !matched {
-		errors = append(errors, fmt.Errorf("%q must end with an alphanumeric character or underscore", k))
-	}
-
-	return warnings, errors
+	return validation.All(
+		validation.StringIsNotWhiteSpace,
+		validation.StringLenBetween(3, 64),
+		validation.StringMatch(regexp.MustCompile(`^[a-zA-Z0-9._-]+$`), "may only contain alphanumeric characters, dots, dashes and underscores"),
+		validation.StringMatch(regexp.MustCompile(`^[a-zA-Z0-9]`), "must begin with an alphanumeric character"),
+		validation.StringMatch(regexp.MustCompile(`\w$`), "must end with an alphanumeric character or underscore"),
+	)(i, k)
 }
--- internal/services/iothub/validate/iot_hub_dps_certificate_name.go
+++ internal/services/iothub/validate/iot_hub_dps_certificate_name.go
@@ -4,25 +4,15 @@
 package validate
 
 import (
-	"fmt"
 	"regexp"
+
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 )
 
-// lintignore:V013,V011 // false positive - this validates a name; the string comparison checks for empty values
 func IoTHubDpsCertificateName(v interface{}, k string) (warnings []string, errors []error) {
-	value := v.(string)
-
-	if value == "" {
-		errors = append(errors, fmt.Errorf("%q must not be empty", k))
-	}
-
-	if len(value) > 64 {
-		errors = append(errors, fmt.Errorf("%q may contain at most 64 characters", k))
-	}
-
-	if matched := regexp.MustCompile(`^[0-9a-zA-Z-._]+$`).Match([]byte(value)); !matched {
-		errors = append(errors, fmt.Errorf("%q may only contain alphanumeric characters or the following: -._", k))
-	}
-
-	return warnings, errors
+	return validation.All(
+		validation.StringIsNotEmpty,
+		validation.StringLenBetween(1, 64),
+		validation.StringMatch(regexp.MustCompile(`^[0-9a-zA-Z-._]+$`), "may only contain alphanumeric characters or the following: -._"),
+	)(v, k)
 }
