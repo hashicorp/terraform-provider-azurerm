a66bd5595893d0fa0adf9bb402c60d08a29d7f60
kt <kt@katbyte.me>
2026-08-11 15:12:04 -0700

    remove now-unused features imports from test files after merging main

--- internal/services/mssql/mssql_database_resource_test.go
+++ internal/services/mssql/mssql_database_resource_test.go
@@ -19,7 +19,6 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 )
 
