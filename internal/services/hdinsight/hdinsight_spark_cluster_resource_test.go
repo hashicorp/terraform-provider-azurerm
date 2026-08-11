a525eda0f724d4a141f32c65d9e0bc6f695badcd
Hill Feng <33737119+ZiChuanBuXiu@users.noreply.github.com>
2026-08-11 13:29:44 +1000

    Fix

--- internal/services/hdinsight/hdinsight_spark_cluster_resource_test.go
+++ internal/services/hdinsight/hdinsight_spark_cluster_resource_test.go
@@ -1883,7 +1883,7 @@ resource "azurerm_hdinsight_spark_cluster" "test" {
   }
 
   disk_encryption {
-      encryption_at_host_enabled    = true
+    encryption_at_host_enabled    = true
     encryption_algorithm          = "RSA-OAEP"
     key_vault_key_id              = azurerm_key_vault_key.test.id
     key_vault_managed_identity_id = azurerm_user_assigned_identity.test.id
