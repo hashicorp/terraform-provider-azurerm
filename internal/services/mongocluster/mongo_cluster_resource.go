4b330de45fff6d18b5aec55107d9e6ad8a471ca5
Hill Feng <33737119+ZiChuanBuXiu@users.noreply.github.com>
2026-08-17 10:52:55 +1000

    Fix go import

--- internal/services/mongocluster/mongo_cluster_resource.go
+++ internal/services/mongocluster/mongo_cluster_resource.go
@@ -19,7 +19,6 @@ import (
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/keyvault"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/mongocluster/2026-06-01/mongoclusters"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
