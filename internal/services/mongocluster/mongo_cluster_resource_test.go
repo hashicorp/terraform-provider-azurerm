88baf2e049bbde044748092a6797d1eee0b96062
Hill Feng <33737119+ZiChuanBuXiu@users.noreply.github.com>
2026-08-21 10:43:06 +1000

    Add test

--- internal/services/mongocluster/mongo_cluster_resource_test.go
+++ internal/services/mongocluster/mongo_cluster_resource_test.go
@@ -205,6 +205,22 @@ func TestAccMongoCluster_authenticationMethodsValidation(t *testing.T) {
 	})
 }
 
+func TestAccMongoCluster_networkBypassModeValidation(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_mongo_cluster", "test")
+	r := MongoClusterResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config:      r.networkBypassModeValidation(data, `["MicrosoftEntraID"]`, "Enabled"),
+			ExpectError: regexp.MustCompile("`public_network_access` must be `Disabled` when `network_bypass_mode` is `AzureCosmosDB`"),
+		},
+		{
+			Config:      r.networkBypassModeValidation(data, `["MicrosoftEntraID", "NativeAuth"]`, "Disabled"),
+			ExpectError: regexp.MustCompile("`authentication_methods` must contain only `MicrosoftEntraID` when `network_bypass_mode` is `AzureCosmosDB`"),
+		},
+	})
+}
+
 func TestAccMongoCluster_networkBypassMode(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_mongo_cluster", "test")
 	r := MongoClusterResource{}
@@ -569,6 +585,28 @@ resource "azurerm_mongo_cluster" "test" {
   public_network_access  = "Disabled"
 }
 `, r.template(data, data.Locations.Primary), data.RandomInteger)
+}
+
+func (r MongoClusterResource) networkBypassModeValidation(data acceptance.TestData, authenticationMethods, publicNetworkAccess string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_mongo_cluster" "test" {
+  name                   = "acctest-mc%d"
+  resource_group_name    = azurerm_resource_group.test.name
+  location               = azurerm_resource_group.test.location
+  administrator_username = "adminTerraform"
+  administrator_password = "QAZwsx123basic"
+  shard_count            = "1"
+  compute_tier           = "M30"
+  high_availability_mode = "Disabled"
+  storage_size_in_gb     = "32"
+  version                = "7.0"
+  authentication_methods = %s
+  network_bypass_mode    = "AzureCosmosDB"
+  public_network_access  = "%s"
+}
+`, r.template(data, data.Locations.Primary), data.RandomInteger, authenticationMethods, publicNetworkAccess)
 }
 
 func (r MongoClusterResource) networkBypassModeDisabled(data acceptance.TestData) string {
