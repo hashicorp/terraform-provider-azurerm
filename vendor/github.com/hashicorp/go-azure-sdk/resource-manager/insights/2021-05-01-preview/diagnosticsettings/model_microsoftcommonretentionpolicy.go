4b242c5872a248c7dc0d5528bf6de75cb9319074
aayushsingh2502 <aayush.singh@hashicorp.com>
2026-08-24 12:00:28 +0530

    dependencies: update `go-azure-sdk` to `v0.20260819.1085932`
    Also fixes collateral compilation breaks introduced by this SDK version:
    - `diagnosticsettings.LogSettings` renamed to `DiagnosticsLogSettings`
    - `diagnosticsettings.MetricSettings` renamed to `DiagnosticsMetricSettings`
    - `diagnosticsettings.RetentionPolicy` renamed to `MicrosoftCommonRetentionPolicy`
    - `DiagnosticSettingsCategoryListOperationResponse.Model` changed from wrapper
      struct with `.Value` field to direct `*[]DiagnosticSettingsCategoryResource`
    
    Acceptance tested: TestAccMonitorDiagnosticSetting_enabledLogs,
    TestAccMonitorDiagnosticSetting_enabledLogsMix,
    TestAccMonitorDiagnosticSetting_enabledLogsCategoryGroup,
    TestAccMonitorDiagnosticSetting_updateEnabledMetric,
    TestAccDataSourceMonitorDiagnosticCategories_appService,
    TestAccDataSourceMonitorDiagnosticCategories_storageAccount — all PASS

--- vendor/github.com/hashicorp/go-azure-sdk/resource-manager/insights/2021-05-01-preview/diagnosticsettings/model_microsoftcommonretentionpolicy.go
+++ vendor/github.com/hashicorp/go-azure-sdk/resource-manager/insights/2021-05-01-preview/diagnosticsettings/model_microsoftcommonretentionpolicy.go
@@ -3,7 +3,7 @@ package diagnosticsettings
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-type RetentionPolicy struct {
+type MicrosoftCommonRetentionPolicy struct {
 	Days    int64 `json:"days"`
 	Enabled bool  `json:"enabled"`
 }
