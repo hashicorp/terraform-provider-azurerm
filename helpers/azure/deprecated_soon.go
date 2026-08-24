422a7e490865e8c999fe228e76350a7bdae93922
kt <kt@katbyte.me>
2026-08-24 14:34:22 -0700

    helpers: fix final V001/V013 findings - swap CIDR to validation.StringMatch and ValidateResourceIDOrEmpty to validation.Any

--- helpers/azure/deprecated_soon.go
+++ helpers/azure/deprecated_soon.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/resourcegroups"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/suppress"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 )
 
 // SchemaResourceGroupNameDiffSuppress will be deprecated in the near future
@@ -49,16 +50,7 @@ func ValidateResourceID(i interface{}, k string) (warnings []string, errors []er
 // Deprecated: use a more specific Resource ID validator instead, however note that empty strings should not
 // be allowed as a validation value. Rather than specifying an empty string, users can omit the field to use
 // an unset value.
-func ValidateResourceIDOrEmpty(i interface{}, k string) (_ []string, errors []error) {
-	v, ok := i.(string)
-	if !ok {
-		errors = append(errors, fmt.Errorf("expected type of %q to be string", k))
-		return
-	}
-
-	if v == "" {
-		return
-	}
-
-	return ValidateResourceID(i, k)
+func ValidateResourceIDOrEmpty(i interface{}, k string) ([]string, []error) {
+	// an empty string is valid in addition to a resource ID
+	return validation.Any(validation.StringIsEmpty, ValidateResourceID)(i, k)
 }
