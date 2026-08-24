aceab055504f7da32c2ed8716c4031d9722405f9
aayushsingh2502 <aayush.singh@hashicorp.com>
2026-08-24 14:38:41 +0530

    fix: gofmt formatting in mssql_database_resource_identity_gen_test.go

--- internal/services/mssql/mssql_database_resource_identity_gen_test.go
+++ internal/services/mssql/mssql_database_resource_identity_gen_test.go
@@ -4,13 +4,11 @@
 package mssql_test
 
 import (
-	"testing"
-
-	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	customstatecheck "github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/statecheck"
+	"testing"
 )
 
 func TestAccMssqlDatabase_resourceIdentity(t *testing.T) {
