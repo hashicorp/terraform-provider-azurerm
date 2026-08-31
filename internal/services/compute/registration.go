f5f10b70c9414ed889d92fd1156986fde643cf5a
mstroob <37002354+mstroob@users.noreply.github.com>
2026-08-31 19:59:32 -0400

    `dedicated_host_group` - add List Resource support (#33257)
    [FEATURE] * **New List Resource**: `dedicated_host_group`

--- internal/services/compute/registration.go
+++ internal/services/compute/registration.go
@@ -125,5 +125,6 @@ func (r Registration) EphemeralResources() []func() ephemeral.EphemeralResource 
 func (r Registration) ListResources() []sdk.FrameworkListWrappedResource {
 	return []sdk.FrameworkListWrappedResource{
 		AvailabilitySetListResource{},
+		DedicatedHostGroupListResource{},
 	}
 }
