914681a2558d58106c714852f202bbb8297be71f
mstroob <37002354+mstroob@users.noreply.github.com>
2026-08-31 22:23:14 -0400

    fix identity gen

--- internal/services/network/network_watcher_resource.go
+++ internal/services/network/network_watcher_resource.go
@@ -20,7 +20,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
 )
 
-//go:generate go run ../../tools/generator-tests resourceidentity
+//go:generate go run ../../tools/generator-tests resourceidentity -test-name basicConfig
 
 var azureNetworkWatcherResourceName = "azurerm_network_watcher"
 
