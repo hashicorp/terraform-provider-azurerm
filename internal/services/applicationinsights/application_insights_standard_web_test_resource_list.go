68b6c836a399f65692359bb565d7f8ae18c97d24
mstroob <37002354+mstroob@users.noreply.github.com>
2026-09-01 13:24:36 -0400

    application_insights_standard_web_test - add List Resource support (#33243)
    [FEATURE] * **New List Resource**: `azurerm_application_insights_standard_web_test`

---
+++ internal/services/applicationinsights/application_insights_standard_web_test_resource_list.go
@@ -0,0 +1,92 @@
+package applicationinsights
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
+	webtests "github.com/hashicorp/go-azure-sdk/resource-manager/applicationinsights/2022-06-15/webtestsapis"
+	"github.com/hashicorp/terraform-plugin-framework/list"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+)
+
+type ApplicationInsightsStandardWebTestListResource struct{}
+
+var _ sdk.FrameworkListWrappedResource = new(ApplicationInsightsStandardWebTestListResource)
+
+func (ApplicationInsightsStandardWebTestListResource) Metadata(_ context.Context, _ resource.MetadataRequest, response *resource.MetadataResponse) {
+	response.TypeName = ApplicationInsightsStandardWebTestResource{}.ResourceType()
+}
+
+func (ApplicationInsightsStandardWebTestListResource) ResourceFunc() *pluginsdk.Resource {
+	return sdk.WrappedResource(ApplicationInsightsStandardWebTestResource{})
+}
+
+func (ApplicationInsightsStandardWebTestListResource) List(ctx context.Context, request list.ListRequest, stream *list.ListResultsStream, metadata sdk.ResourceMetadata) {
+	client := metadata.Client.AppInsights.StandardWebTestsClient
+
+	var data sdk.DefaultListModel
+	diags := request.Config.Get(ctx, &data)
+	if diags.HasError() {
+		stream.Results = list.ListResultsStreamDiagnostics(diags)
+		return
+	}
+
+	var results []webtests.WebTest
+	subscriptionID := metadata.SubscriptionId
+	if !data.SubscriptionId.IsNull() {
+		subscriptionID = data.SubscriptionId.ValueString()
+	}
+
+	resource := ApplicationInsightsStandardWebTestResource{}
+
+	switch {
+	case !data.ResourceGroupName.IsNull():
+		resp, err := client.WebTestsListByResourceGroupComplete(ctx, commonids.NewResourceGroupID(subscriptionID, data.ResourceGroupName.ValueString()))
+		if err != nil {
+			sdk.SetResponseErrorDiagnostic(stream, fmt.Sprintf("listing `%s`", resource.ResourceType()), err)
+			return
+		}
+		results = resp.Items
+	default:
+		resp, err := client.WebTestsListComplete(ctx, commonids.NewSubscriptionID(subscriptionID))
+		if err != nil {
+			sdk.SetResponseErrorDiagnostic(stream, fmt.Sprintf("listing `%s`", resource.ResourceType()), err)
+			return
+		}
+		results = resp.Items
+	}
+
+	stream.Results = func(push func(list.ListResult) bool) {
+		for _, item := range results {
+			result := request.NewListResult(ctx)
+			result.DisplayName = pointer.From(item.Name)
+
+			id, err := webtests.ParseWebTestIDInsensitively(pointer.From(item.Id))
+			if err != nil {
+				sdk.SetErrorDiagnosticAndPushListResult(result, push, fmt.Sprintf("parsing %s ID", resource.ResourceType()), err)
+				return
+			}
+
+			meta := sdk.NewResourceMetaData(metadata.Client, resource)
+			meta.SetID(id)
+
+			if err := resource.flatten(meta, id, &item); err != nil {
+				sdk.SetErrorDiagnosticAndPushListResult(result, push, fmt.Sprintf("encoding `%s` resource data", resource.ResourceType()), err)
+				return
+			}
+
+			sdk.EncodeListResult(ctx, meta.ResourceData, &result)
+			if result.Diagnostics.HasError() {
+				push(result)
+				return
+			}
+			if !push(result) {
+				return
+			}
+		}
+	}
+}
