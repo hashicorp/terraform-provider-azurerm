9ca6c76517a5e3b96bfaa6fdfd3496ad4a210c48
mstroob <37002354+mstroob@users.noreply.github.com>
2026-09-03 09:38:26 -0400

    `log_analytics_workspace` - add List Resource support (#33259)
    [ENHANCEMENT] * **NEW LIST RESOURCE**: `azurerm_log_analytics_workspace`

---
+++ internal/services/loganalytics/log_analytics_workspace_resource_list_test.go
@@ -0,0 +1,98 @@
+package loganalytics_test
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/querycheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/provider/framework"
+)
+
+func TestAccLogAnalyticsWorkspace_listBySubscriptionAndRG(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_log_analytics_workspace", "test")
+	r := LogAnalyticsWorkspaceResource{}
+	listResourceAddress := "azurerm_log_analytics_workspace.list"
+
+	resource.Test(t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		ProtoV5ProviderFactories: framework.ProtoV5ProviderFactoriesInit(context.Background(), "azurerm"),
+		Steps: []resource.TestStep{
+			{
+				Config: r.basicList(data),
+			},
+			{
+				Query:  true,
+				Config: r.basicQuery(),
+				QueryResultChecks: []querycheck.QueryResultCheck{
+					querycheck.ExpectLengthAtLeast(listResourceAddress, 3),
+					querycheck.ExpectIdentity(
+						listResourceAddress,
+						map[string]knownvalue.Check{
+							"name":                knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data.RandomInteger))),
+							"resource_group_name": knownvalue.StringRegexp(regexp.MustCompile(strconv.Itoa(data.RandomInteger))),
+							"subscription_id":     knownvalue.StringExact(data.Subscriptions.Primary),
+						},
+					),
+				},
+			},
+			{
+				Query:  true,
+				Config: r.basicQueryByResourceGroupName(),
+				QueryResultChecks: []querycheck.QueryResultCheck{
+					querycheck.ExpectLength(listResourceAddress, 3),
+				},
+			},
+		},
+	})
+}
+
+func (r LogAnalyticsWorkspaceResource) basicList(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_log_analytics_workspace" "test" {
+  count               = 3
+  name                = "acctestLAW${count.index}-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (r LogAnalyticsWorkspaceResource) basicQuery() string {
+	return `
+list "azurerm_log_analytics_workspace" "list" {
+  provider = azurerm
+  config {}
+}
+`
+}
+
+func (r LogAnalyticsWorkspaceResource) basicQueryByResourceGroupName() string {
+	return `
+list "azurerm_log_analytics_workspace" "list" {
+  provider = azurerm
+  config {
+    resource_group_name = azurerm_resource_group.test.name
+  }
+}
+`
+}
