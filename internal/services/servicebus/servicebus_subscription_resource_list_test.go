75fb5a974602c6fa3c03dab8400ba5f9c6e24db2
mstroob <37002354+mstroob@users.noreply.github.com>
2026-09-04 10:07:08 -0400

    rename type and make test count

--- internal/services/servicebus/servicebus_subscription_resource_list_test.go
+++ internal/services/servicebus/servicebus_subscription_resource_list_test.go
@@ -2,6 +2,7 @@ package servicebus_test
 
 import (
 	"context"
+	"fmt"
 	"regexp"
 	"strconv"
 	"testing"
@@ -16,7 +17,7 @@ import (
 
 func TestAccServiceBusSubscription_listByTopicID(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_servicebus_subscription", "testlist1")
-	r := ServiceBusSubscriptionResource{}
+	r := ServicebusSubscriptionResource{}
 
 	resource.Test(t, resource.TestCase{
 		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
@@ -25,13 +26,13 @@ func TestAccServiceBusSubscription_listByTopicID(t *testing.T) {
 		ProtoV5ProviderFactories: framework.ProtoV5ProviderFactoriesInit(context.Background(), "azurerm"),
 		Steps: []resource.TestStep{
 			{
-				Config: r.basic(data),
+				Config: r.basicList(data),
 			},
 			{
 				Query:  true,
 				Config: r.basicQuery(),
 				QueryResultChecks: []querycheck.QueryResultCheck{
-					querycheck.ExpectLengthAtLeast("azurerm_servicebus_subscription.list", 1),
+					querycheck.ExpectLengthAtLeast("azurerm_servicebus_subscription.list", 3),
 					querycheck.ExpectIdentity(
 						"azurerm_servicebus_subscription.list",
 						map[string]knownvalue.Check{
@@ -48,7 +49,39 @@ func TestAccServiceBusSubscription_listByTopicID(t *testing.T) {
 	})
 }
 
-func (ServiceBusSubscriptionResource) basicQuery() string {
+func (ServicebusSubscriptionResource) basicList(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_servicebus_namespace" "test" {
+  name                = "acctestservicebusnamespace-%[1]d"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  sku                 = "Standard"
+}
+
+resource "azurerm_servicebus_topic" "test" {
+  name         = "acctestservicebustopic-%[1]d"
+  namespace_id = azurerm_servicebus_namespace.test.id
+}
+
+resource "azurerm_servicebus_subscription" "test" {
+  count              = 3
+  name               = "_acctestservicebussubscription-%[1]d-${count.index}_"
+  topic_id           = azurerm_servicebus_topic.test.id
+  max_delivery_count = 10
+}
+`, data.RandomInteger, data.Locations.Primary)
+}
+
+func (ServicebusSubscriptionResource) basicQuery() string {
 	return `
 list "azurerm_servicebus_subscription" "list" {
   provider = azurerm
