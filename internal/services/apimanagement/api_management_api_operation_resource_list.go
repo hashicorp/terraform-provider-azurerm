13c869b04ab157ef286bd223508dd8a8f9a41b2e
mstroob <37002354+mstroob@users.noreply.github.com>
2026-09-08 23:19:34 -0400

    Add List Resource support for api_management_api_operation

---
+++ internal/services/apimanagement/api_management_api_operation_resource_list.go
@@ -0,0 +1,99 @@
+package apimanagement
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/go-azure-helpers/framework/typehelpers"
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/apimanagement/2022-08-01/apioperation"
+	"github.com/hashicorp/terraform-plugin-framework/list"
+	"github.com/hashicorp/terraform-plugin-framework/list/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+)
+
+type ApiManagementApiOperationListResource struct{}
+
+type ApiManagementApiOperationListModel struct {
+	ApiId types.String `tfsdk:"api_id"`
+}
+
+var _ sdk.FrameworkListWrappedResource = new(ApiManagementApiOperationListResource)
+
+func (ApiManagementApiOperationListResource) Metadata(_ context.Context, _ resource.MetadataRequest, response *resource.MetadataResponse) {
+	response.TypeName = azureApiManagementApiOperationResourceName
+}
+
+func (ApiManagementApiOperationListResource) ResourceFunc() *pluginsdk.Resource {
+	return resourceApiManagementApiOperation()
+}
+
+func (ApiManagementApiOperationListResource) ListResourceConfigSchema(_ context.Context, _ list.ListResourceSchemaRequest, response *list.ListResourceSchemaResponse) {
+	response.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"api_id": schema.StringAttribute{
+				Required: true,
+				Validators: []validator.String{
+					typehelpers.WrappedStringValidator{Func: apioperation.ValidateApiID},
+				},
+			},
+		},
+	}
+}
+
+func (ApiManagementApiOperationListResource) List(ctx context.Context, request list.ListRequest, stream *list.ListResultsStream, metadata sdk.ResourceMetadata) {
+	client := metadata.Client.ApiManagement.ApiOperationsClient
+
+	var data ApiManagementApiOperationListModel
+	diags := request.Config.Get(ctx, &data)
+	if diags.HasError() {
+		stream.Results = list.ListResultsStreamDiagnostics(diags)
+		return
+	}
+
+	parentID, err := apioperation.ParseApiID(data.ApiId.ValueString())
+	if err != nil {
+		sdk.SetResponseErrorDiagnostic(stream, fmt.Sprintf("parsing Api ID for `%s`", azureApiManagementApiOperationResourceName), err)
+		return
+	}
+
+	resp, err := client.ListByApiComplete(ctx, *parentID, apioperation.DefaultListByApiOperationOptions())
+	if err != nil {
+		sdk.SetResponseErrorDiagnostic(stream, fmt.Sprintf("listing `%s`", azureApiManagementApiOperationResourceName), err)
+		return
+	}
+
+	stream.Results = func(push func(list.ListResult) bool) {
+		for _, item := range resp.Items {
+			result := request.NewListResult(ctx)
+			result.DisplayName = pointer.From(item.Name)
+
+			rd := resourceApiManagementApiOperation().Data(&terraform.InstanceState{})
+			id, err := apioperation.ParseOperationIDInsensitively(pointer.From(item.Id))
+			if err != nil {
+				sdk.SetErrorDiagnosticAndPushListResult(result, push, fmt.Sprintf("parsing ID for `%s`", azureApiManagementApiOperationResourceName), err)
+				return
+			}
+			rd.SetId(id.ID())
+
+			if err := resourceApiManagementApiOperationFlatten(rd, id, &item); err != nil {
+				sdk.SetErrorDiagnosticAndPushListResult(result, push, fmt.Sprintf("encoding `%s` resource data", azureApiManagementApiOperationResourceName), err)
+				return
+			}
+
+			sdk.EncodeListResult(ctx, rd, &result)
+			if result.Diagnostics.HasError() {
+				push(result)
+				return
+			}
+			if !push(result) {
+				return
+			}
+		}
+	}
+}
