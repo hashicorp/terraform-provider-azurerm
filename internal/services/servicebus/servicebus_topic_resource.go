b2597c76a81244074b162ef5106b96d6ff65528b
mstroob <37002354+mstroob@users.noreply.github.com>
2026-09-10 09:31:55 -0400

    fix generate

--- internal/services/servicebus/servicebus_topic_resource.go
+++ internal/services/servicebus/servicebus_topic_resource.go
@@ -22,7 +22,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
 )
 
-//go:generate go run ../../tools/generator-tests resourceidentity -compare-values "subscription_id:namespace_id,resource_group_name:namespace_id,namespace_name:namespace_id,topic_name:name"
+//go:generate go run ../../tools/generator-tests resourceidentity -properties "name" -compare-values "subscription_id:namespace_id,resource_group_name:namespace_id,namespace_name:namespace_id"
 
 const serviceBusTopicResourceName = "azurerm_servicebus_topic"
 
--- internal/services/servicebus/servicebus_topic_resource_identity_gen_test.go
+++ internal/services/servicebus/servicebus_topic_resource_identity_gen_test.go
@@ -17,10 +17,10 @@ func TestAccServicebusTopic_resourceIdentity(t *testing.T) {
 	r := ServicebusTopicResource{}
 
 	checkedFields := map[string]struct{}{
+		"name":                {},
 		"namespace_name":      {},
 		"resource_group_name": {},
 		"subscription_id":     {},
-		"topic_name":          {},
 	}
 
 	data.ResourceIdentityTest(t, []acceptance.TestStep{
@@ -28,10 +28,10 @@ func TestAccServicebusTopic_resourceIdentity(t *testing.T) {
 			Config: r.basic(data),
 			ConfigStateChecks: []statecheck.StateCheck{
 				customstatecheck.ExpectAllIdentityFieldsAreChecked("azurerm_servicebus_topic.test", checkedFields),
+				statecheck.ExpectIdentityValueMatchesStateAtPath("azurerm_servicebus_topic.test", tfjsonpath.New("name"), tfjsonpath.New("name")),
 				customstatecheck.ExpectStateContainsIdentityValueAtPath("azurerm_servicebus_topic.test", tfjsonpath.New("namespace_name"), tfjsonpath.New("namespace_id")),
 				customstatecheck.ExpectStateContainsIdentityValueAtPath("azurerm_servicebus_topic.test", tfjsonpath.New("resource_group_name"), tfjsonpath.New("namespace_id")),
 				customstatecheck.ExpectStateContainsIdentityValueAtPath("azurerm_servicebus_topic.test", tfjsonpath.New("subscription_id"), tfjsonpath.New("namespace_id")),
-				customstatecheck.ExpectStateContainsIdentityValueAtPath("azurerm_servicebus_topic.test", tfjsonpath.New("topic_name"), tfjsonpath.New("name")),
 			},
 		},
 		data.ImportBlockWithResourceIdentityStep(false),
