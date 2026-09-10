df3c2df2cdfef14a33305838ba9e0454e690e616
mstroob <37002354+mstroob@users.noreply.github.com>
2026-09-10 09:53:08 -0400

    fix whitespace

--- internal/services/apimanagement/api_management_named_value_resource.go
+++ internal/services/apimanagement/api_management_named_value_resource.go
@@ -16,6 +16,7 @@ import (
 	"github.com/hashicorp/go-azure-sdk/resource-manager/apimanagement/2022-08-01/namedvalue"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers"
+
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
