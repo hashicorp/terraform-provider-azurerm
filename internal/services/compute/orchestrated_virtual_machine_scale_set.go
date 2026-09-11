af70c76beedcc41b06788aad454cbc5925d7f647
Keith Adler <2363749+Talador12@users.noreply.github.com>
2026-09-10 20:48:33 -0500

    azurerm_orchestrated_virtual_machine_scale_set: support NvmeDisk ephemeral placement
    Allow NvmeDisk in os_disk diff_disk_settings placement for v6 orchestrated
    VMSS ephemeral OS disks, with acceptance test coverage.
    
    Signed-off-by: Keith Adler <2363749+Talador12@users.noreply.github.com>

--- internal/services/compute/orchestrated_virtual_machine_scale_set.go
+++ internal/services/compute/orchestrated_virtual_machine_scale_set.go
@@ -636,6 +636,7 @@ func OrchestratedVirtualMachineScaleSetOSDiskSchema() *pluginsdk.Schema {
 								ValidateFunc: validation.StringInSlice([]string{
 									string(virtualmachinescalesets.DiffDiskPlacementCacheDisk),
 									string(virtualmachinescalesets.DiffDiskPlacementResourceDisk),
+									string(virtualmachinescalesets.DiffDiskPlacementNVMeDisk),
 								}, false),
 							},
 						},
--- internal/services/compute/orchestrated_virtual_machine_scale_set_resource_disk_os_test.go
+++ internal/services/compute/orchestrated_virtual_machine_scale_set_resource_disk_os_test.go
@@ -41,6 +41,21 @@ func TestAccOrchestratedVirtualMachineScaleSet_diskOSControllerTypeNVMe(t *testi
 	})
 }
 
+func TestAccOrchestratedVirtualMachineScaleSet_disksOSDiskEphemeralNvmeDisk(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_orchestrated_virtual_machine_scale_set", "test")
+	r := OrchestratedVirtualMachineScaleSetResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.disksOSDiskEphemeralNvmeDisk(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("os_profile.0.linux_configuration.0.admin_password"),
+	})
+}
+
 func TestAccOrchestratedVirtualMachineScaleSet_disksOSDiskCaching(t *testing.T) {
 	t.Skipf("Needs investigation into recreate issue due to `diff_disk_settings` change which is ForceNew, skipping for now.")
 	data := acceptance.BuildTestData(t, "azurerm_orchestrated_virtual_machine_scale_set", "test")
@@ -412,6 +427,79 @@ resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
   os_disk {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
+`, data.RandomInteger, data.Locations.Primary, r.natgateway_template(data))
+}
+
+func (r OrchestratedVirtualMachineScaleSetResource) disksOSDiskEphemeralNvmeDisk(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-OVMSS-%[1]d"
+  location = "%[2]s"
+}
+
+%[3]s
+
+resource "azurerm_orchestrated_virtual_machine_scale_set" "test" {
+  name                = "acctestOVMSS-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  zones = []
+
+  sku_name  = "Standard_D2ads_v6"
+  instances = 1
+
+  platform_fault_domain_count = 2
+  disk_controller_type        = "NVMe"
+
+  os_profile {
+    linux_configuration {
+      computer_name_prefix = "testvm-%[1]d"
+      admin_username       = "myadmin"
+      admin_password       = "Passwword1234"
+
+      disable_password_authentication = false
+    }
+  }
+
+  network_interface {
+    name    = "TestNetworkProfile-%[1]d"
+    primary = true
+
+    ip_configuration {
+      name      = "TestIPConfiguration"
+      primary   = true
+      subnet_id = azurerm_subnet.test.id
+
+      public_ip_address {
+        name                    = "TestPublicIPConfiguration"
+        domain_name_label       = "test-domain-label"
+        idle_timeout_in_minutes = 4
+      }
+    }
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadOnly"
+
+    diff_disk_settings {
+      option    = "Local"
+      placement = "NvmeDisk"
+    }
   }
 
   source_image_reference {
