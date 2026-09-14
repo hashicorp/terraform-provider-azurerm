b71b6ff2d4dd5ac5c57c0950a19dd967b84913df
jackofallops <ste@hashicorp.com>
2026-09-14 13:07:55 +0200

    refactor advisor for dir per resource

---
+++ internal/services/advisor/suppression/create.go
@@ -0,0 +1,60 @@
+// Copyright IBM Corp. 2014, 2025
+// SPDX-License-Identifier: MPL-2.0
+
+package suppression
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
+	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/advisor/2023-01-01/suppressions"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
+)
+
+func (r Resource) Create() sdk.ResourceFunc {
+	return sdk.ResourceFunc{
+		Timeout: 30 * time.Minute,
+		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.Advisor.SuppressionsClient
+
+			var model ResourceModel
+
+			if err := metadata.Decode(&model); err != nil {
+				return err
+			}
+
+			id := suppressions.NewScopedSuppressionID(model.ResourceID, model.RecommendationID, model.Name)
+
+			if !metadata.Client.Features.SkipImportCheckOnCreateAndAllowOverwritingExistingResources {
+				existing, err := client.Get(ctx, id)
+				if err != nil && !response.WasNotFound(existing.HttpResponse) {
+					return fmt.Errorf("checking for presence of existing %s: %+v", id, err)
+				}
+				if !response.WasNotFound(existing.HttpResponse) {
+					return metadata.ResourceRequiresImport(r.ResourceType(), id)
+				}
+			}
+
+			param := suppressions.SuppressionContract{
+				Name: pointer.To(model.Name),
+				Properties: &suppressions.SuppressionProperties{
+					SuppressionId: pointer.To(model.SuppressionID),
+				},
+			}
+
+			if model.TTL != "" {
+				param.Properties.Ttl = pointer.To(model.TTL)
+			}
+
+			if _, err := client.Create(ctx, id, param); err != nil {
+				return fmt.Errorf("creating %s: %+v", id, err)
+			}
+
+			metadata.SetID(id)
+			return nil
+		},
+	}
+}
--- internal/services/advisor/suppression/resource_test.go
+++ internal/services/advisor/suppression/resource_test.go
@@ -1,7 +1,7 @@
 // Copyright IBM Corp. 2014, 2025
 // SPDX-License-Identifier: MPL-2.0
 
-package advisor_test
+package suppression_test
 
 import (
 	"context"
