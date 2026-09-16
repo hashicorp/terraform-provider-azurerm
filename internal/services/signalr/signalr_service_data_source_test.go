609f855595939b531fc4b6b886306d5100646b14
Molk <142650145+MalikDagher@users.noreply.github.com>
2026-09-16 12:26:03 -0500

    removed test bloat

--- internal/services/signalr/signalr_service_data_source_test.go
+++ internal/services/signalr/signalr_service_data_source_test.go
@@ -41,88 +41,31 @@ func TestAccDataSourceSignalRService_basic(t *testing.T) {
 	})
 }
 
-func TestAccDataSourceSignalRService_cors(t *testing.T) {
+func TestAccDataSourceSignalRService_complete(t *testing.T) {
 	data := acceptance.BuildTestData(t, "data.azurerm_signalr_service", "test")
 
 	data.DataSourceTest(t, []acceptance.TestStep{
 		{
-			Config: SignalRServiceDataSource{}.cors(data),
+			Config: SignalRServiceDataSource{}.complete(data),
 			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).Key("sku.#").HasValue("1"),
+				check.That(data.ResourceName).Key("sku.0.name").HasValue("Standard_S1"),
+				check.That(data.ResourceName).Key("sku.0.capacity").HasValue("1"),
+				check.That(data.ResourceName).Key("service_mode").HasValue("Serverless"),
+				check.That(data.ResourceName).Key("connectivity_logs_enabled").HasValue("true"),
+				check.That(data.ResourceName).Key("messaging_logs_enabled").HasValue("false"),
+				check.That(data.ResourceName).Key("http_request_logs_enabled").HasValue("false"),
+				check.That(data.ResourceName).Key("live_trace.#").HasValue("1"),
+				check.That(data.ResourceName).Key("live_trace.0.enabled").HasValue("true"),
+				check.That(data.ResourceName).Key("live_trace.0.connectivity_logs_enabled").HasValue("false"),
+				check.That(data.ResourceName).Key("live_trace.0.messaging_logs_enabled").HasValue("true"),
 				check.That(data.ResourceName).Key("cors.#").HasValue("1"),
 				check.That(data.ResourceName).Key("cors.0.allowed_origins.#").HasValue("2"),
-			),
-		},
-	})
-}
-
-func TestAccDataSourceSignalRService_identity(t *testing.T) {
-	data := acceptance.BuildTestData(t, "data.azurerm_signalr_service", "test")
-
-	data.DataSourceTest(t, []acceptance.TestStep{
-		{
-			Config: SignalRServiceDataSource{}.identity(data),
-			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).Key("upstream_endpoint.#").HasValue("4"),
 				check.That(data.ResourceName).Key("identity.#").HasValue("1"),
 				check.That(data.ResourceName).Key("identity.0.type").HasValue("SystemAssigned"),
 				check.That(data.ResourceName).Key("identity.0.principal_id").IsUUID(),
 				check.That(data.ResourceName).Key("identity.0.tenant_id").IsUUID(),
-			),
-		},
-	})
-}
-
-func TestAccDataSourceSignalRService_liveTrace(t *testing.T) {
-	data := acceptance.BuildTestData(t, "data.azurerm_signalr_service", "test")
-
-	data.DataSourceTest(t, []acceptance.TestStep{
-		{
-			Config: SignalRServiceDataSource{}.liveTrace(data),
-			Check: acceptance.ComposeTestCheckFunc(
-				check.That(data.ResourceName).Key("live_trace.#").HasValue("1"),
-				check.That(data.ResourceName).Key("live_trace.0.enabled").HasValue("true"),
-				check.That(data.ResourceName).Key("live_trace.0.connectivity_logs_enabled").HasValue("true"),
-				check.That(data.ResourceName).Key("live_trace.0.messaging_logs_enabled").HasValue("false"),
-			),
-		},
-	})
-}
-
-func TestAccDataSourceSignalRService_resourceLogs(t *testing.T) {
-	data := acceptance.BuildTestData(t, "data.azurerm_signalr_service", "test")
-
-	data.DataSourceTest(t, []acceptance.TestStep{
-		{
-			Config: SignalRServiceDataSource{}.resourceLogs(data),
-			Check: acceptance.ComposeTestCheckFunc(
-				check.That(data.ResourceName).Key("connectivity_logs_enabled").HasValue("true"),
-				check.That(data.ResourceName).Key("messaging_logs_enabled").HasValue("false"),
-				check.That(data.ResourceName).Key("http_request_logs_enabled").HasValue("false"),
-			),
-		},
-	})
-}
-
-func TestAccDataSourceSignalRService_serviceMode(t *testing.T) {
-	data := acceptance.BuildTestData(t, "data.azurerm_signalr_service", "test")
-
-	data.DataSourceTest(t, []acceptance.TestStep{
-		{
-			Config: SignalRServiceDataSource{}.serviceMode(data),
-			Check: acceptance.ComposeTestCheckFunc(
-				check.That(data.ResourceName).Key("service_mode").HasValue("Serverless"),
-			),
-		},
-	})
-}
-
-func TestAccDataSourceSignalRService_upstreamEndpoint(t *testing.T) {
-	data := acceptance.BuildTestData(t, "data.azurerm_signalr_service", "test")
-
-	data.DataSourceTest(t, []acceptance.TestStep{
-		{
-			Config: SignalRServiceDataSource{}.upstreamEndpoint(data),
-			Check: acceptance.ComposeTestCheckFunc(
-				check.That(data.ResourceName).Key("upstream_endpoint.#").HasValue("4"),
 			),
 		},
 	})
@@ -140,74 +83,81 @@ data "azurerm_signalr_service" "test" {
 `, template)
 }
 
-func (r SignalRServiceDataSource) cors(data acceptance.TestData) string {
-	template := SignalRServiceResource{}.withCors(data)
+func (r SignalRServiceDataSource) complete(data acceptance.TestData) string {
 	return fmt.Sprintf(`
-%s
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%[1]d"
+  location = "%[2]s"
+}
+
+resource "azurerm_signalr_service" "test" {
+  name                = "acctestSignalR-%[1]d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  sku {
+    name     = "Standard_S1"
+    capacity = 1
+  }
+
+  service_mode               = "Serverless"
+  connectivity_logs_enabled = true
+  messaging_logs_enabled    = false
+  http_request_logs_enabled = false
+
+  live_trace {
+    enabled                    = true
+    connectivity_logs_enabled = false
+    messaging_logs_enabled    = true
+  }
+
+  cors {
+    allowed_origins = [
+      "https://example.com",
+      "https://contoso.com",
+    ]
+  }
+
+  upstream_endpoint {
+    category_pattern = ["*"]
+    event_pattern    = ["*"]
+    hub_pattern      = ["*"]
+    url_template     = "http://foo.com/{hub}/api/{category}/{event}"
+  }
+
+  upstream_endpoint {
+    category_pattern = ["connections", "messages"]
+    event_pattern    = ["*"]
+    hub_pattern      = ["hub1"]
+    url_template     = "http://foo.com"
+  }
+
+  upstream_endpoint {
+    category_pattern = ["*"]
+    event_pattern    = ["connect", "disconnect"]
+    hub_pattern      = ["hub1", "hub2"]
+    url_template     = "http://foo3.com"
+  }
+
+  upstream_endpoint {
+    category_pattern = ["connections"]
+    event_pattern    = ["disconnect"]
+    hub_pattern      = ["*"]
+    url_template     = "http://foo4.com"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
 
 data "azurerm_signalr_service" "test" {
   name                = azurerm_signalr_service.test.name
   resource_group_name = azurerm_resource_group.test.name
 }
-`, template)
-}
-
-func (r SignalRServiceDataSource) identity(data acceptance.TestData) string {
-	template := SignalRServiceResource{}.systemAssignedIdentity(data)
-	return fmt.Sprintf(`
-%s
-
-data "azurerm_signalr_service" "test" {
-  name                = azurerm_signalr_service.test.name
-  resource_group_name = azurerm_resource_group.test.name
-}
-`, template)
-}
-
-func (r SignalRServiceDataSource) liveTrace(data acceptance.TestData) string {
-	template := SignalRServiceResource{}.liveTrace(data)
-	return fmt.Sprintf(`
-%s
-
-data "azurerm_signalr_service" "test" {
-  name                = azurerm_signalr_service.test.name
-  resource_group_name = azurerm_resource_group.test.name
-}
-`, template)
-}
-
-func (r SignalRServiceDataSource) resourceLogs(data acceptance.TestData) string {
-	template := SignalRServiceResource{}.resourceLogs(data)
-	return fmt.Sprintf(`
-%s
-
-data "azurerm_signalr_service" "test" {
-  name                = azurerm_signalr_service.test.name
-  resource_group_name = azurerm_resource_group.test.name
-}
-`, template)
-}
-
-func (r SignalRServiceDataSource) serviceMode(data acceptance.TestData) string {
-	template := SignalRServiceResource{}.withServiceMode(data, "Serverless")
-	return fmt.Sprintf(`
-%s
-
-data "azurerm_signalr_service" "test" {
-  name                = azurerm_signalr_service.test.name
-  resource_group_name = azurerm_resource_group.test.name
-}
-`, template)
-}
-
-func (r SignalRServiceDataSource) upstreamEndpoint(data acceptance.TestData) string {
-	template := SignalRServiceResource{}.withUpstreamEndpoints(data)
-	return fmt.Sprintf(`
-%s
-
-data "azurerm_signalr_service" "test" {
-  name                = azurerm_signalr_service.test.name
-  resource_group_name = azurerm_resource_group.test.name
-}
-`, template)
+`, data.RandomInteger, data.Locations.Primary)
 }
