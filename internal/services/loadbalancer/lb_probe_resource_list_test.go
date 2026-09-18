935394cd868823ee77163f2ad2e753de3a438a60
mstroob <37002354+mstroob@users.noreply.github.com>
2026-09-18 14:01:50 -0400

    Update internal/services/loadbalancer/lb_probe_resource_list_test.go
    Co-authored-by: sreallymatt <106555974+sreallymatt@users.noreply.github.com>

--- internal/services/loadbalancer/lb_probe_resource_list_test.go
+++ internal/services/loadbalancer/lb_probe_resource_list_test.go
@@ -63,7 +63,7 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_public_ip" "test" {
-  name                = "test-ip-%d"
+  name                = "acctest-ip-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   allocation_method   = "Static"
