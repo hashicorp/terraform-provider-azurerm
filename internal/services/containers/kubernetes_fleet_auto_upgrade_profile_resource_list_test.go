714b30228406a39b24f199aa0ce86cd245c3e85d
hqhqhqhqhqhqhqhqhqhqhqhq <harryqu@microsoft.com>
2026-09-22 09:36:19 +1000

    Cover Fleet auto-upgrade lifecycle and listing edges

--- internal/services/containers/kubernetes_fleet_auto_upgrade_profile_resource_list_test.go
+++ internal/services/containers/kubernetes_fleet_auto_upgrade_profile_resource_list_test.go
@@ -11,6 +11,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/querycheck"
+	"github.com/hashicorp/terraform-plugin-testing/querycheck/queryfilter"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/provider/framework"
@@ -47,6 +49,46 @@ func TestAccKubernetesFleetAutoUpgradeProfile_list(t *testing.T) {
 	})
 }
 
+func TestAccKubernetesFleetAutoUpgradeProfile_listMultiple(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_kubernetes_fleet_auto_upgrade_profile", "test")
+	r := KubernetesFleetAutoUpgradeProfileResource{}
+	listAddress := "azurerm_kubernetes_fleet_auto_upgrade_profile.list"
+	checks := []querycheck.QueryResultCheck{querycheck.ExpectLength(listAddress, 2)}
+	for name, channel := range map[string]string{"stable": "Stable", "rapid": "Rapid"} {
+		profileName := fmt.Sprintf("acctestfaup-%d-%s", data.RandomInteger, name)
+		checks = append(checks,
+			querycheck.ExpectIdentity(listAddress, map[string]knownvalue.Check{
+				"name":                knownvalue.StringExact(profileName),
+				"fleet_name":          knownvalue.StringExact(fmt.Sprintf("acctestkfm-%d", data.RandomInteger)),
+				"resource_group_name": knownvalue.StringExact(fmt.Sprintf("acctest-rg-%d", data.RandomInteger)),
+				"subscription_id":     knownvalue.StringExact(data.Subscriptions.Primary),
+			}),
+			querycheck.ExpectResourceKnownValues(listAddress, queryfilter.ByDisplayName(knownvalue.StringExact(profileName)), []querycheck.KnownValueCheck{
+				{Path: tfjsonpath.New("name"), KnownValue: knownvalue.StringExact(profileName)},
+				{Path: tfjsonpath.New("channel"), KnownValue: knownvalue.StringExact(channel)},
+				{Path: tfjsonpath.New("enabled"), KnownValue: knownvalue.Bool(false)},
+				{Path: tfjsonpath.New("node_image_selection_type"), KnownValue: knownvalue.StringExact("")},
+				{Path: tfjsonpath.New("update_strategy_id"), KnownValue: knownvalue.StringExact("")},
+			}),
+		)
+	}
+
+	resource.Test(t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		ProtoV5ProviderFactories: framework.ProtoV5ProviderFactoriesInit(context.Background(), "azurerm"),
+		Steps: []resource.TestStep{
+			{Config: r.listMultipleConfig(data)},
+			{
+				Query:             true,
+				Config:            r.listQueryWithResource(),
+				QueryResultChecks: checks,
+			},
+		},
+	})
+}
+
 func (r KubernetesFleetAutoUpgradeProfileResource) listConfig(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 %s
@@ -67,6 +109,42 @@ func (r KubernetesFleetAutoUpgradeProfileResource) listQuery() string {
 	return `
 list "azurerm_kubernetes_fleet_auto_upgrade_profile" "list" {
   provider = azurerm
+  config {
+    kubernetes_fleet_manager_id = azurerm_kubernetes_fleet_manager.test.id
+  }
+}
+`
+}
+
+func (r KubernetesFleetAutoUpgradeProfileResource) listMultipleConfig(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_kubernetes_fleet_auto_upgrade_profile" "stable" {
+  name                        = "acctestfaup-%[2]d-stable"
+  kubernetes_fleet_manager_id = azurerm_kubernetes_fleet_manager.test.id
+  channel                     = "Stable"
+  enabled                     = false
+}
+
+resource "azurerm_kubernetes_fleet_auto_upgrade_profile" "rapid" {
+  name                        = "acctestfaup-%[2]d-rapid"
+  kubernetes_fleet_manager_id = azurerm_kubernetes_fleet_manager.test.id
+  channel                     = "Rapid"
+  enabled                     = false
+}
+`, r.template(data), data.RandomInteger)
+}
+
+func (r KubernetesFleetAutoUpgradeProfileResource) listQueryWithResource() string {
+	return `
+list "azurerm_kubernetes_fleet_auto_upgrade_profile" "list" {
+  provider         = azurerm
+  include_resource = true
   config {
     kubernetes_fleet_manager_id = azurerm_kubernetes_fleet_manager.test.id
   }
--- internal/services/containers/kubernetes_fleet_auto_upgrade_profile_resource_test.go
+++ internal/services/containers/kubernetes_fleet_auto_upgrade_profile_resource_test.go
@@ -10,6 +10,8 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/containerservice/2025-03-01/autoupgradeprofiles"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
@@ -80,6 +82,107 @@ func TestAccKubernetesFleetAutoUpgradeProfile_update(t *testing.T) {
 			Config: r.update(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccKubernetesFleetAutoUpgradeProfile_selectionRemoval(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_kubernetes_fleet_auto_upgrade_profile", "test")
+	r := KubernetesFleetAutoUpgradeProfileResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.selection(data, "Latest"),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("node_image_selection_type").HasValue("Latest"),
+				check.That(data.ResourceName).Key("enabled").HasValue("false"),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.selection(data, ""),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("node_image_selection_type").HasValue(""),
+				check.That(data.ResourceName).Key("enabled").HasValue("false"),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.selection(data, "Latest"),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("node_image_selection_type").HasValue("Latest"),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccKubernetesFleetAutoUpgradeProfile_reenable(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_kubernetes_fleet_auto_upgrade_profile", "test")
+	r := KubernetesFleetAutoUpgradeProfileResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.selection(data, ""),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("enabled").HasValue("false"),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("enabled").HasValue("true"),
+				check.That(data.ResourceName).Key("node_image_selection_type").HasValue(""),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccKubernetesFleetAutoUpgradeProfile_updateStrategyRemoval(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_kubernetes_fleet_auto_upgrade_profile", "test")
+	r := KubernetesFleetAutoUpgradeProfileResource{}
+
+	data.ResourceTestIgnoreRecreate(t, r, []acceptance.TestStep{
+		{
+			Config: r.strategy(data, true),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("update_strategy_id").MatchesOtherKey(check.That("azurerm_kubernetes_fleet_update_strategy.test").Key("id")),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.strategy(data, false),
+			ConfigPlanChecks: resource.ConfigPlanChecks{
+				PreApply: []plancheck.PlanCheck{
+					plancheck.ExpectResourceAction(data.ResourceName, plancheck.ResourceActionReplace),
+				},
+			},
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("update_strategy_id").HasValue(""),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.strategy(data, true),
+			ConfigPlanChecks: resource.ConfigPlanChecks{
+				PreApply: []plancheck.PlanCheck{
+					plancheck.ExpectResourceAction(data.ResourceName, plancheck.ResourceActionReplace),
+				},
+			},
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("update_strategy_id").MatchesOtherKey(check.That("azurerm_kubernetes_fleet_update_strategy.test").Key("id")),
 			),
 		},
 		data.ImportStep(),
@@ -201,4 +304,59 @@ resource "azurerm_kubernetes_fleet_manager" "test" {
   resource_group_name = azurerm_resource_group.test.name
 }
 `, data.Locations.Primary, data.RandomInteger)
+}
+
+func (r KubernetesFleetAutoUpgradeProfileResource) selection(data acceptance.TestData, selection string) string {
+	selectionConfig := ""
+	if selection != "" {
+		selectionConfig = fmt.Sprintf("node_image_selection_type = %q", selection)
+	}
+	return fmt.Sprintf(`
+%s
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_kubernetes_fleet_auto_upgrade_profile" "test" {
+  name                        = "acctestfaup-%[2]d"
+  kubernetes_fleet_manager_id = azurerm_kubernetes_fleet_manager.test.id
+  channel                     = "Stable"
+  enabled                     = false
+  %[3]s
+}
+`, r.template(data), data.RandomInteger, selectionConfig)
+}
+
+func (r KubernetesFleetAutoUpgradeProfileResource) strategy(data acceptance.TestData, useStrategy bool) string {
+	strategyConfig := ""
+	if useStrategy {
+		strategyConfig = "update_strategy_id = azurerm_kubernetes_fleet_update_strategy.test.id"
+	}
+	return fmt.Sprintf(`
+%s
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_kubernetes_fleet_update_strategy" "test" {
+  name                        = "acctestfus-%[2]d"
+  kubernetes_fleet_manager_id = azurerm_kubernetes_fleet_manager.test.id
+  stage {
+    name = "test"
+    group {
+      name = "test"
+    }
+  }
+}
+
+resource "azurerm_kubernetes_fleet_auto_upgrade_profile" "test" {
+  name                        = "acctestfaup-%[2]d"
+  kubernetes_fleet_manager_id = azurerm_kubernetes_fleet_manager.test.id
+  channel                     = "Stable"
+  enabled                     = false
+  %[3]s
+}
+`, r.template(data), data.RandomInteger, strategyConfig)
 }
