d860bfc929bc189f16f6c5eba2a3ff1329695998
kt <kt@katbyte.me>
2026-09-24 08:46:30 -0700

    Update internal/services/compute/validate/disk_encryption_set_name.go
    Co-authored-by: catriona-m <86247157+catriona-m@users.noreply.github.com>

--- internal/services/compute/validate/disk_encryption_set_name.go
+++ internal/services/compute/validate/disk_encryption_set_name.go
@@ -15,5 +15,5 @@ import (
 // additionally, the name cannot end with hyphen or dot.
 // Golang regex does not support "negative look ahead" (aka `?<!`), therefore I transformed this regex to the following regular expression.
 func DiskEncryptionSetName(v interface{}, k string) ([]string, []error) {
-	return validation.StringMatch(regexp.MustCompile(`^[^_\W][\w-._]{0,78}\w$`), "must be between 1 - 80 characters long, and contains only a-z, A-Z, 0-9 and _")(v, k)
+	return validation.StringMatch(regexp.MustCompile(`^[^_\W][\w-._]{0,78}\w$`), "must be between 1 - 80 characters long, and contain only a-z, A-Z, 0-9 and _")(v, k)
 }
